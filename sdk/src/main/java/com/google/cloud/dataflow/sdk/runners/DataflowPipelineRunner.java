cda8313c2d8b487d377c31dc502857d2bbde1825
malo <malo@google.com>
2014-12-14 20:15:06 -0800

    Set the sdk major_version to 0. [] ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=82113067

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
@@ -196,9 +196,7 @@ public class DataflowPipelineRunner extends PipelineRunner<DataflowPipelineJob> 
 
     // Requirements about the service.
     Map<String, Object> environmentVersion = new HashMap<>();
-    // TODO: Specify the environment major version.
-    // environmentVersion.put(PropertyNames.ENVIRONMENT_VERSION_MAJOR_KEY,
-    // ENVIRONMENT_MAJOR_VERSION);
+    environmentVersion.put(PropertyNames.ENVIRONMENT_VERSION_MAJOR_KEY, ENVIRONMENT_MAJOR_VERSION);
     newJob.getEnvironment().setVersion(environmentVersion);
     // Default jobType is DATA_PARALLEL which is for java batch.
     String jobType = "DATA_PARALLEL";
