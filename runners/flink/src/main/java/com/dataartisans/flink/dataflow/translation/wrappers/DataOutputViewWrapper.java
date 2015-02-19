b5c3ab3d8cb526a9a2f9edbd7dc918e8e2fa2388
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2015-02-19 12:48:06 +0100

    Rename utils to wrappers, add Javadoc

--- runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/wrappers/DataOutputViewWrapper.java
+++ runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/wrappers/DataOutputViewWrapper.java
@@ -1,15 +1,21 @@
-package com.dataartisans.flink.dataflow.translation.utils;
+package com.dataartisans.flink.dataflow.translation.wrappers;
 
 import org.apache.flink.core.memory.DataOutputView;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class DataOutputStreamWrapper extends OutputStream {
+/**
+ * Wrapper for {@link org.apache.flink.core.memory.DataOutputView}. We need this because
+ * Flink writes data using a {@link org.apache.flink.core.memory.DataInputView} while
+ * Dataflow {@link com.google.cloud.dataflow.sdk.coders.Coder}s expect an
+ * {@link java.io.OutputStream}.
+ */
+public class DataOutputViewWrapper extends OutputStream {
 	
 	private DataOutputView outputView;
 
-	public DataOutputStreamWrapper(DataOutputView outputView) {
+	public DataOutputViewWrapper(DataOutputView outputView) {
 		this.outputView = outputView;
 	}
 
