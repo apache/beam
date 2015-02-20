6b0d3a4978419c77a66c5207bb9ef0711cea0403
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2015-02-20 15:24:42 +0100

    Add InputFormat wrapper for Source, along with ITCase

---
+++ runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/wrappers/SourceInputFormat.java
@@ -0,0 +1,169 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dataartisans.flink.dataflow.translation.wrappers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.util.Lists;
+import com.google.cloud.dataflow.sdk.coders.Coder;
+import com.google.cloud.dataflow.sdk.io.Source;
+import com.google.cloud.dataflow.sdk.options.PipelineOptions;
+import com.google.cloud.dataflow.sdk.util.WindowedValue;
+import org.apache.flink.api.common.io.InputFormat;
+import org.apache.flink.api.common.io.statistics.BaseStatistics;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.io.InputSplit;
+import org.apache.flink.core.io.InputSplitAssigner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+/**
+ * A Flink {@link org.apache.flink.api.common.io.InputFormat} that wraps a
+ * Dataflow {@link com.google.cloud.dataflow.sdk.io.Source}.
+ */
+public class SourceInputFormat<T> implements InputFormat<T, SourceInputSplit<T>> {
+	private static final Logger LOG = LoggerFactory.getLogger(SourceInputFormat.class);
+
+	private final Source<T> initialSource;
+	private transient PipelineOptions options;
+	private final Coder<T> coder;
+
+	private Source.Reader<WindowedValue<T>> reader = null;
+	private boolean reachedEnd = true;
+
+	public SourceInputFormat(Source<T> initialSource, PipelineOptions options, Coder<T> coder) {
+		this.initialSource = initialSource;
+		this.options = options;
+		this.coder = coder;
+	}
+
+	private void writeObject(ObjectOutputStream out)
+			throws IOException, ClassNotFoundException {
+		out.defaultWriteObject();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(out, options);
+	}
+
+	private void readObject(ObjectInputStream in)
+			throws IOException, ClassNotFoundException {
+		in.defaultReadObject();
+		ObjectMapper mapper = new ObjectMapper();
+		options = mapper.readValue(in, PipelineOptions.class);
+	}
+
+	@Override
+	public void configure(Configuration configuration) {}
+
+	@Override
+	public void open(SourceInputSplit<T> sourceInputSplit) throws IOException {
+		WindowedValue.ValueOnlyWindowedValueCoder<T> windowedCoder = WindowedValue
+				.ValueOnlyWindowedValueCoder.of(coder);
+
+		reader = sourceInputSplit.getSource().createWindowedReader(options, windowedCoder, null);
+		reachedEnd = false;
+	}
+
+	@Override
+	public BaseStatistics getStatistics(BaseStatistics baseStatistics) throws IOException {
+		try {
+			final long estimatedSize = initialSource.getEstimatedSizeBytes(options);
+
+			return new BaseStatistics() {
+				@Override
+				public long getTotalInputSize() {
+					return estimatedSize;
+
+				}
+
+				@Override
+				public long getNumberOfRecords() {
+					return BaseStatistics.NUM_RECORDS_UNKNOWN;
+				}
+
+				@Override
+				public float getAverageRecordWidth() {
+					return BaseStatistics.AVG_RECORD_BYTES_UNKNOWN;
+				}
+			};
+		} catch (Exception e) {
+			LOG.warn("Could not read Source statistics: {}", e);
+		}
+
+		return null;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public SourceInputSplit<T>[] createInputSplits(int numSplits) throws IOException {
+		long desiredSizeBytes = 10000;
+		try {
+			desiredSizeBytes = initialSource.getEstimatedSizeBytes(options) / numSplits;
+			List<? extends Source<T>> shards = initialSource.splitIntoShards(desiredSizeBytes,
+					options);
+			List<SourceInputSplit<T>> splits = Lists.newArrayList();
+			int splitCount = 0;
+			for (Source<T> shard: shards) {
+				splits.add(new SourceInputSplit<>(shard, splitCount++));
+			}
+			return splits.toArray(new SourceInputSplit[splits.size()]);
+		} catch (Exception e) {
+			throw new IOException("Could not create input splits from Source.", e);
+		}
+	}
+
+	@Override
+	public InputSplitAssigner getInputSplitAssigner(final SourceInputSplit[] sourceInputSplits) {
+		return new InputSplitAssigner() {
+			private int index = 0;
+			private final SourceInputSplit[] splits = sourceInputSplits;
+			@Override
+			public InputSplit getNextInputSplit(String host) {
+				if (index < splits.length) {
+					return splits[index++];
+				} else {
+					return null;
+				}
+			}
+		};
+	}
+
+
+	@Override
+	public boolean reachedEnd() throws IOException {
+		return reachedEnd;
+	}
+
+	@Override
+	public T nextRecord(T t) throws IOException {
+
+		reachedEnd = !reader.advance();
+		if (!reachedEnd) {
+			return reader.getCurrent().getValue();
+		}
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		reader.close();
+	}
+}
---
+++ runners/flink/src/test/java/com/dataartisans/flink/dataflow/ReadSourceITCase.java
@@ -0,0 +1,153 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dataartisans.flink.dataflow;
+
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.coders.BigEndianIntegerCoder;
+import com.google.cloud.dataflow.sdk.coders.Coder;
+import com.google.cloud.dataflow.sdk.io.ReadSource;
+import com.google.cloud.dataflow.sdk.io.Source;
+import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
+import com.google.cloud.dataflow.sdk.options.PipelineOptions;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.google.cloud.dataflow.sdk.util.ExecutionContext;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.common.base.Joiner;
+import org.apache.flink.test.util.JavaProgramTestBase;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReadSourceITCase extends JavaProgramTestBase {
+
+	protected String resultPath;
+
+	public ReadSourceITCase(){
+	}
+
+	static final String[] EXPECTED_RESULT = new String[] {
+			"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+	@Override
+	protected void preSubmit() throws Exception {
+		resultPath = getTempDirPath("result");
+	}
+
+	@Override
+	protected void postSubmit() throws Exception {
+		compareResultsByLinesInMemory(Joiner.on('\n').join(EXPECTED_RESULT), resultPath);
+	}
+
+	@Override
+	protected void testProgram() throws Exception {
+		runProgram(resultPath);
+	}
+
+	private static void runProgram(String resultPath) {
+
+		Pipeline p = FlinkTestPipeline.create();
+
+		PCollection<String> result = p
+				.apply(ReadSource.from(new Read(1, 10)))
+				.apply(ParDo.of(new DoFn<Integer, String>() {
+					@Override
+					public void processElement(ProcessContext c) throws Exception {
+						c.output(c.element().toString());
+					}
+				}));
+
+		result.apply(TextIO.Write.to(resultPath));
+		p.run();
+	}
+}
+
+class Read extends Source<Integer> {
+	final int from;
+	final int to;
+
+	Read(int from, int to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public List<Read> splitIntoShards(long desiredShardSizeBytes, PipelineOptions options)
+			throws Exception {
+		List<Read> res = new ArrayList<>();
+		DataflowPipelineOptions dataflowOptions = options.as(DataflowPipelineOptions.class);
+		float step = 1.0f * (to - from) / dataflowOptions.getNumWorkers();
+		for (int i = 0; i < dataflowOptions.getNumWorkers(); ++i) {
+			res.add(new Read(Math.round(from + i * step), Math.round(from + (i + 1) * step)));
+		}
+		return res;
+	}
+
+	@Override
+	public long getEstimatedSizeBytes(PipelineOptions options) throws Exception {
+		return 8 * (to - from);
+	}
+
+	@Override
+	public boolean producesSortedKeys(PipelineOptions options) throws Exception {
+		return true;
+	}
+
+	@Override
+	public Reader<Integer> createBasicReader(PipelineOptions options, Coder<Integer> coder,
+	                                         @Nullable ExecutionContext executionContext) throws IOException {
+		return new RangeReader(this);
+	}
+
+	@Override
+	public void validate() {}
+
+	@Override
+	public Coder<Integer> getDefaultOutputCoder() {
+		return BigEndianIntegerCoder.of();
+	}
+
+	private class RangeReader implements Reader<Integer> {
+		private int current;
+
+		public RangeReader(Read source) {
+			this.current = source.from - 1;
+		}
+
+		@Override
+		public boolean advance() throws IOException {
+			current++;
+			return (current < to);
+		}
+
+		@Override
+		public Integer getCurrent() {
+			return current;
+		}
+
+		@Override
+		public void close() throws IOException {
+			// Nothing
+		}
+	}
+}
+
