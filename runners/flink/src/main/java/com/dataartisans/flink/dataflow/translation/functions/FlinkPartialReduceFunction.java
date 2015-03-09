7c90c0a01d00ecdaac39e19dce5990d649cc28d7
Max <max@posteo.de>
2015-03-09 19:01:28 +0100

    integrate partialGroupReduce operator to properly optimize Combine.perKey

--- runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/functions/FlinkPartialReduceFunction.java
+++ runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/functions/FlinkPartialReduceFunction.java
@@ -3,56 +3,42 @@ package com.dataartisans.flink.dataflow.translation.functions;
 import com.google.cloud.dataflow.sdk.transforms.Combine;
 import com.google.cloud.dataflow.sdk.values.KV;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
-import org.apache.flink.api.common.functions.MapPartitionFunction;
 import org.apache.flink.util.Collector;
 
 import java.util.Iterator;
 
 /**
- * Flink {@link org.apache.flink.api.common.functions.MapPartitionFunction} for executing a
+ * Flink {@link org.apache.flink.api.common.functions.GroupReduceFunction} for executing a
  * {@link com.google.cloud.dataflow.sdk.transforms.Combine.PerKey} operation. This reads the input
- * {@link com.google.cloud.dataflow.sdk.values.KV} elements, extracts the key and emits accumulated
- * values.
- *
- * This assumes that the input values are sorted. We basically implement a GroupReduce operation
- * inside a partition here.
+ * {@link com.google.cloud.dataflow.sdk.values.KV} elements VI, extracts the key and emits accumulated
+ * values which have the intermediate format VA.
  */
-public class FlinkPartialReduceFunction<K, VI, VA, VO> implements MapPartitionFunction<KV<K, VI>, KV<K, VA>> {
+public class FlinkPartialReduceFunction<K, VI, VA> implements GroupReduceFunction<KV<K, VI>, KV<K, VA>> {
 
-	private final Combine.KeyedCombineFn<K, VI, VA, VO> keyedCombineFn;
+	private final Combine.KeyedCombineFn<K, VI, VA, ?> keyedCombineFn;
 
-	public FlinkPartialReduceFunction(Combine.KeyedCombineFn<K, VI, VA, VO>
+	public FlinkPartialReduceFunction(Combine.KeyedCombineFn<K, VI, VA, ?>
 			                                  keyedCombineFn) {
 		this.keyedCombineFn = keyedCombineFn;
 	}
 
 	@Override
-	public void mapPartition(Iterable<KV<K, VI>> values, Collector<KV<K, VA>> out) throws
-			Exception {
-		Iterator<KV<K, VI>> it = values.iterator();
-		if (!it.hasNext()) {
-			return;
+	public void reduce(Iterable<KV<K, VI>> elements, Collector<KV<K, VA>> out) throws Exception {
+
+		final Iterator<KV<K, VI>> iterator = elements.iterator();
+		// create accumulator using the first elements key
+		KV<K, VI> first = iterator.next();
+		K key = first.getKey();
+		VI value = first.getValue();
+		VA accumulator = keyedCombineFn.createAccumulator(key);
+		// manually add for the first element
+		keyedCombineFn.addInput(key, accumulator, value);
+
+		while(iterator.hasNext()) {
+			value = iterator.next().getValue();
+			keyedCombineFn.addInput(key, accumulator, value);
 		}
 
-		KV<K, VI> current = it.next();
-		K currentKey = current.getKey();
-		VA accumulator = keyedCombineFn.createAccumulator(currentKey);
-		keyedCombineFn.addInput(currentKey, accumulator, current.getValue());
-
-		while (it.hasNext()) {
-			current = it.next();
-			if (currentKey == null && current.getKey() == null || current.getKey().equals(currentKey)) {
-				keyedCombineFn.addInput(currentKey, accumulator, current.getValue());
-			} else {
-				// output current accumulation value, start new accumulation
-				out.collect(KV.of(currentKey, accumulator));
-				currentKey = current.getKey();
-				accumulator = keyedCombineFn.createAccumulator(currentKey);
-				keyedCombineFn.addInput(currentKey, accumulator, current.getValue());
-			}
-		}
-
-		// also emit last accumulation
-		out.collect(KV.of(currentKey, accumulator));
+		out.collect(KV.of(key, accumulator));
 	}
 }
--- runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/functions/FlinkReduceFunction.java
+++ runners/flink/src/main/java/com/dataartisans/flink/dataflow/translation/functions/FlinkReduceFunction.java
@@ -12,27 +12,29 @@ import java.util.Iterator;
  * Flink {@link org.apache.flink.api.common.functions.GroupReduceFunction} for executing a
  * {@link com.google.cloud.dataflow.sdk.transforms.Combine.PerKey} operation. This reads the input
  * {@link com.google.cloud.dataflow.sdk.values.KV} elements, extracts the key and merges the
- * accumulators resulting from the PartialReduce in the MapPartition operation.
+ * accumulators resulting from the PartialReduce which produced the input VA.
  */
-public class FlinkReduceFunction<K, VI, VA, VO> implements GroupReduceFunction<KV<K, VA>, KV<K, VO>> {
+public class FlinkReduceFunction<K, VA, VO> implements GroupReduceFunction<KV<K, VA>, KV<K, VO>> {
 
-	private final Combine.KeyedCombineFn<K, VI, VA, VO> keyedCombineFn;
+	private final Combine.KeyedCombineFn<K, ?, VA, VO> keyedCombineFn;
 
-	public FlinkReduceFunction(Combine.KeyedCombineFn<K, VI, VA, VO> keyedCombineFn) {
+	public FlinkReduceFunction(Combine.KeyedCombineFn<K, ?, VA, VO> keyedCombineFn) {
 		this.keyedCombineFn = keyedCombineFn;
 	}
 
 	@Override
 	public void reduce(Iterable<KV<K, VA>> values, Collector<KV<K, VO>> out) throws Exception {
 		Iterator<KV<K, VA>> it = values.iterator();
+
 		KV<K, VA> current = it.next();
 		K k = current.getKey();
 		VA accumulator = current.getValue();
+
 		while (it.hasNext()) {
 			current = it.next();
 			keyedCombineFn.mergeAccumulators(k, ImmutableList.of(accumulator, current.getValue()) );
 		}
-		out.collect(KV.of(k, keyedCombineFn.extractOutput(k,
-				accumulator)));
+
+		out.collect(KV.of(k, keyedCombineFn.extractOutput(k, accumulator)));
 	}
 }
