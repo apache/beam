8192a7ef617c12e9b7cced32b32c7216f1524e83
boyuan <boyuan@google.com>
2015-03-25 19:13:50 -0700

    Change the ENVIRONMENT_MAJOR_VERSION to "1"
    ----Release Notes----
    []
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=89562565

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
@@ -80,7 +80,7 @@ public class DataflowPipelineRunner extends PipelineRunner<DataflowPipelineJob> 
   private DataflowPipelineRunnerHooks hooks;
 
   // Environment version information
-  private static final String ENVIRONMENT_MAJOR_VERSION = "2";
+  private static final String ENVIRONMENT_MAJOR_VERSION = "1";
 
   /**
    * Construct a runner from the provided options.
