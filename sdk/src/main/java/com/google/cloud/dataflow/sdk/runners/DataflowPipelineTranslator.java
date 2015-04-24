b06caf4825aa52255aaf7e6ddae9c98489edcfd0
klk <klk@google.com>
2015-04-24 10:32:47 -0700

    Remove DataflowPipelineWorkerPoolOptions.setDiskSourceImage ----Release Notes---- [] ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=91994024

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineTranslator.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineTranslator.java
@@ -405,9 +405,6 @@ public class DataflowPipelineTranslator {
 
       workerPool.setPackages(packages);
       workerPool.setNumWorkers(options.getNumWorkers());
-      if (options.getDiskSourceImage() != null) {
-        workerPool.setDiskSourceImage(options.getDiskSourceImage());
-      }
 
       if (options.isStreaming()) {
         // Use separate data disk for streaming.
