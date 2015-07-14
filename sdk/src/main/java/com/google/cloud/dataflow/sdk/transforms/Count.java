aaa640446ab668f96c672ca49a2020bf0fb5a00f
klk <klk@google.com>
2015-07-14 11:39:43 -0700

    Add support for Count.globally().asSingletonView()
    This change removes the mostly extraneous Count.Globally class.
    Now Count.globally() returns an appropriately named Combine.Globally
    instance, exposing all of its features to the user of Count. This
    makes the Count transforms more analogous to other composite Combine
    transforms like Max, Mean, Min, and Sum.
    
    ----Release Notes----
    
    []
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=98234802

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/transforms/Count.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/transforms/Count.java
@@ -16,32 +16,38 @@
 
 package com.google.cloud.dataflow.sdk.transforms;
 
+import com.google.cloud.dataflow.sdk.transforms.Combine.CombineFn;
 import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 
 /**
  * {@code PTransorm}s to count the elements in a {@link PCollection}.
  *
- * <p> {@link PerElement Count.PerElement} can be used to count the number of occurrences of each
- * distinct element in the PCollection. {@link Globally Count.Globally} can
+ * <p> {@link Count#perElement()} can be used to count the number of occurrences of each
+ * distinct element in the PCollection. {@link Count#globally()} can
  * be used to count the total number of elements in a PCollection.
  */
 public class Count {
 
   /**
-   * Returns a {@link Globally Count.Globally} {@link PTransform}
-   * that counts the number of elements in its input {@link PCollection}.
-   *
-   * <p> See {@link Globally Count.Globally} for more details.
+   * Returns a {@link Combine.Globally} {@link PTransform} that counts the number of elements in
+   * its input {@link PCollection}.
    */
-  public static <T> Globally<T> globally() {
-    return new Globally<>();
+  public static <T> Combine.Globally<T, Long> globally() {
+    return Combine.globally(new CountFn<T>()).named("Count.Globally");
   }
 
   /**
-   * Returns a {@link PerElement Count.PerElement} {@link PTransform}
-   * that counts the number of occurrences of each element in its
-   * input {@link PCollection}.
+   * Returns a {@link Combine.PerKey} {@link PTransform} that counts the number of elements
+   * associated with each key of its input {@link PCollection}.
+   */
+  public static <K, V> Combine.PerKey<K, V, Long> perKey() {
+    return Combine.<K, V, Long>perKey(new CountFn<V>()).named("Count.PerKey");
+  }
+
+  /**
+   * Returns a {@link PerElement Count.PerElement} {@link PTransform} that counts the number of
+   * occurrences of each element in its input {@link PCollection}.
    *
    * <p> See {@link PerElement Count.PerElement} for more details.
    */
@@ -49,93 +55,20 @@ public class Count {
     return new PerElement<>();
   }
 
-  ///////////////////////////////////////
-
-  /**
-   * {@code Count.Globally<T>} takes a {@code PCollection<T>} and returns a
-   * {@code PCollection<Long>} containing a single element that is the total
-   * number of elements in the {@code PCollection}.
-   *
-   * <p> Example of use:
-   * <pre> {@code
-   * PCollection<String> words = ...;
-   * PCollection<Long> wordCount =
-   *     words.apply(Count.<String>globally());
-   * } </pre>
-   *
-   * @param <T> the type of the elements of the input {@code PCollection}
-   */
-  @SuppressWarnings("serial")
-  public static class Globally<T>
-      extends PTransform<PCollection<T>, PCollection<Long>> {
-
-    private final boolean withoutDefaults;
-    private final int fanout;
-
-    public Globally() {
-      this.withoutDefaults = false;
-      this.fanout = 0;
-    }
-
-    private Globally(boolean withoutDefaults, int fanout) {
-      this.withoutDefaults = withoutDefaults;
-      this.fanout = fanout;
-    }
-
-    /**
-     * Returns a {@link PTransform} identical to Globally(), but that does not attempt to
-     * provide a default value in the case of empty input.
-     */
-    public Globally<T> withoutDefaults() {
-      return new Globally<T>(true /* withoutDefaults */, fanout);
-    }
-
-    /**
-     * Returns a {@link PTransform} identical to Globally(), but that uses an intermedate combining
-     * node to improve performance.  See {@link Combine.Globally#withFanout}.
-     */
-    public Globally<T> withHotKeyFanout(int fanout) {
-      return new Globally<T>(withoutDefaults, fanout);
-    }
-
-    @Override
-    public PCollection<Long> apply(PCollection<T> input) {
-      Combine.Globally<Long, Long> sumGlobally;
-      if (withoutDefaults) {
-        sumGlobally = Sum.longsGlobally().withoutDefaults().withFanout(fanout);
-      } else {
-        sumGlobally = Sum.longsGlobally().withFanout(fanout);
-      }
-      return
-          input
-          .apply(ParDo.named("Init")
-                 .of(new DoFn<T, Long>() {
-                     @Override
-                     public void processElement(ProcessContext c) {
-                       c.output(1L);
-                     }
-                   }))
-          .apply(sumGlobally);
-    }
-  }
-
   /**
    * {@code Count.PerElement<T>} takes a {@code PCollection<T>} and returns a
-   * {@code PCollection<KV<T, Long>>} representing a map from each
-   * distinct element of the input {@code PCollection} to the number of times
-   * that element occurs in the input.  Each of the keys in the output
-   * {@code PCollection} is unique.
+   * {@code PCollection<KV<T, Long>>} representing a map from each distinct element of the input
+   * {@code PCollection} to the number of times that element occurs in the input. Each key in the
+   * output {@code PCollection} is unique.
    *
-   * <p> This transform compares two values of type {@code T} by first
-   * encoding each element using the input {@code PCollection}'s
-   * {@code Coder}, then comparing the encoded bytes. Because of this,
-   * the input coder must be deterministic. (See
-   * {@link com.google.cloud.dataflow.sdk.coders.Coder#verifyDeterministic()} for more detail).
+   * <p> This transform compares two values of type {@code T} by first encoding each element using
+   * the input {@code PCollection}'s {@code Coder}, then comparing the encoded bytes. Because of
+   * this, the input coder must be deterministic.
+   * (See {@link com.google.cloud.dataflow.sdk.coders.Coder#verifyDeterministic()} for more detail).
    * Performing the comparison in this manner admits efficient parallel evaluation.
    *
-   * <p> By default, the {@code Coder} of the keys of the output
-   * {@code PCollection} is the same as the {@code Coder} of the
-   * elements of the input {@code PCollection}.
+   * <p> By default, the {@code Coder} of the keys of the output {@code PCollection} is the same as
+   * the {@code Coder} of the elements of the input {@code PCollection}.
    *
    * <p> Example of use:
    * <pre> {@code
@@ -144,12 +77,13 @@ public class Count {
    *     words.apply(Count.<String>perElement());
    * } </pre>
    *
-   * @param <T> the type of the elements of the input {@code PCollection}, and
-   * the type of the keys of the output {@code PCollection}
+   * @param <T> the type of the elements of the input {@code PCollection}, and the type of the keys
+   * of the output {@code PCollection}
    */
-  @SuppressWarnings("serial")
   public static class PerElement<T>
       extends PTransform<PCollection<T>, PCollection<KV<T, Long>>> {
+
+    private static final long serialVersionUID = 0L;
 
     public PerElement() { }
 
@@ -157,14 +91,47 @@ public class Count {
     public PCollection<KV<T, Long>> apply(PCollection<T> input) {
       return
           input
-          .apply(ParDo.named("Init")
-                 .of(new DoFn<T, KV<T, Long>>() {
-                     @Override
-                     public void processElement(ProcessContext c) {
-                       c.output(KV.of(c.element(), 1L));
-                     }
-                   }))
-          .apply(Sum.<T>longsPerKey());
+          .apply(ParDo.named("Init").of(new DoFn<T, KV<T, Void>>() {
+            private static final long serialVersionUID = 0L;
+
+            @Override
+            public void processElement(ProcessContext c) {
+              c.output(KV.of(c.element(), (Void) null));
+            }
+          }))
+          .apply(Count.<T, Void>perKey());
+    }
+  }
+
+  /**
+   * A {@link CombineFn} that counts elements.
+   */
+  private static class CountFn<T> extends CombineFn<T, Long, Long> {
+
+    private static final long serialVersionUID = 0L;
+
+    @Override
+    public Long createAccumulator() {
+      return 0L;
+    }
+
+    @Override
+    public Long addInput(Long accumulator, T input) {
+      return accumulator + 1;
+    }
+
+    @Override
+    public Long mergeAccumulators(Iterable<Long> accumulators) {
+      long result = 0L;
+      for (Long accum : accumulators) {
+        result += accum;
+      }
+      return result;
+    }
+
+    @Override
+    public Long extractOutput(Long accumulator) {
+      return accumulator;
     }
   }
 }
