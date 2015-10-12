2b2c413287f9ef3f85d4676534904c4a1c2df7c6
dhalperi <dhalperi@google.com>
2015-10-12 10:32:05 -0700

    WorkerDiskType option: document and add reference URL
    The format of the string expected by this option is set by
    GCE and is non-obvious. Provide an example and a link for
    more information.
    
    Fixes #68
    
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=105223902

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/options/DataflowPipelineWorkerPoolOptions.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/options/DataflowPipelineWorkerPoolOptions.java
@@ -173,9 +173,16 @@ public interface DataflowPipelineWorkerPoolOptions extends PipelineOptions {
   void setFilesToStage(List<String> value);
 
   /**
-   * Specifies what type of persistent disk should be used.
+   * Specifies what type of persistent disk should be used. The value should be a full or partial
+   * URL of a disk type resource, e.g., zones/us-central1-f/disks/pd-standard. For
+   * more information, see the
+   * <a href="https://cloud.google.com/compute/docs/reference/latest/diskTypes">API reference
+   * documentation for DiskTypes</a>.
    */
-  @Description("Specifies what type of persistent disk should be used.")
+  @Description("Specifies what type of persistent disk should be used. The value should be a full "
+      + "or partial URL of a disk type resource, e.g., zones/us-central1-f/disks/pd-standard. For "
+      + "more information, see the API reference documentation for DiskTypes: "
+      + "https://cloud.google.com/compute/docs/reference/latest/diskTypes")
   String getWorkerDiskType();
   void setWorkerDiskType(String value);
 }
