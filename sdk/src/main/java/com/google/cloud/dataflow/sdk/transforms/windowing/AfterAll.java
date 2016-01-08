2bc1033fa125a4fe16803055a7cd0d98b3503531
klk <klk@google.com>
2016-01-07 16:18:50 -0800

    Fix AfterAll merge logic
    ----Release Notes----
    
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=111649265

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/transforms/windowing/AfterAll.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/transforms/windowing/AfterAll.java
@@ -76,11 +76,11 @@ public class AfterAll<W extends BoundedWindow> extends OnceTrigger<W> {
     // trigger itself was already finished in some window.
     // FIRE_AND_FINISH otherwise: It means this trigger is ready to fire (because all subtriggers
     // are satisfied) but has never fired as a whole.
-    boolean anyContinue = true;
+    boolean anyContinue = false;
     boolean alreadyFinished = true;
     for (ExecutableTrigger<W> subTrigger : c.trigger().subTriggers()) {
       MergeResult result = subTrigger.invokeMerge(c);
-      anyContinue |= !(result.isFire() && result.isFinish());
+      anyContinue |= !result.isFire() && !result.isFinish();
       alreadyFinished &= !result.isFire() && result.isFinish();
     }
 
