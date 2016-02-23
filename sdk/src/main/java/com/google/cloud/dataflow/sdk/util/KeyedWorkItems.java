635541a74438a02d18910fba6a76c5aacc2c7816
tgroh <tgroh@google.com>
2016-02-23 10:12:17 -0800

    Add KeyedWorkItemCoder
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=115354168

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/util/KeyedWorkItems.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/util/KeyedWorkItems.java
@@ -16,8 +16,11 @@
 package com.google.cloud.dataflow.sdk.util;
 
 import com.google.cloud.dataflow.sdk.util.TimerInternals.TimerData;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Iterables;
 
 import java.util.Collections;
+import java.util.Objects;
 
 /**
  * Static utility methods that provide {@link KeyedWorkItem} implementations.
@@ -63,7 +66,6 @@ public class KeyedWorkItems {
    * iterable.
    */
   public static class ComposedKeyedWorkItem<K, ElemT> implements KeyedWorkItem<K, ElemT> {
-
     private final K key;
     private final Iterable<TimerData> timers;
     private final Iterable<WindowedValue<ElemT>> elements;
@@ -88,6 +90,31 @@ public class KeyedWorkItems {
     @Override
     public Iterable<WindowedValue<ElemT>> elementsIterable() {
       return elements;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+      if (other == null || !(other instanceof ComposedKeyedWorkItem)) {
+        return false;
+      }
+      KeyedWorkItem<?, ?> that = (KeyedWorkItem<?, ?>) other;
+      return Objects.equals(this.key, that.key())
+          && Iterables.elementsEqual(this.timersIterable(), that.timersIterable())
+          && Iterables.elementsEqual(this.elementsIterable(), that.elementsIterable());
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(key, timers, elements);
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(ComposedKeyedWorkItem.class)
+          .add("key", key)
+          .add("elements", elements)
+          .add("timers", timers)
+          .toString();
     }
   }
 }
