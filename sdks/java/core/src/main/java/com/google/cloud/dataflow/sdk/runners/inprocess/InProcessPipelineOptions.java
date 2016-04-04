740242c330ce99916ed76af7c6e68638d1e3c0e3
Thomas Groh <tgroh@google.com>
2016-04-04 16:48:15 -0700

    Apply ModelEnforcement in the InProcessPipelineRunner
    This ensures that user code does not violate the model.
    
    Add a flag to control application of immutability enforcement. This flag
    is enabled by default.

--- sdks/java/core/src/main/java/com/google/cloud/dataflow/sdk/runners/inprocess/InProcessPipelineOptions.java
+++ sdks/java/core/src/main/java/com/google/cloud/dataflow/sdk/runners/inprocess/InProcessPipelineOptions.java
@@ -89,4 +89,13 @@ public interface InProcessPipelineOptions extends PipelineOptions, ApplicationNa
   boolean isBlockOnRun();
 
   void setBlockOnRun(boolean b);
+
+  @Default.Boolean(true)
+  @Description(
+      "Controls whether the runner should ensure that all of the elements of every "
+          + "PCollection are not mutated. PTransforms are not permitted to mutate input elements "
+          + "at any point, or output elements after they are output.")
+  boolean isTestImmutability();
+
+  void setTestImmutability(boolean test);
 }
