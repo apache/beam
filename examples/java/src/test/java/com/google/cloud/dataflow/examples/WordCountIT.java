7f5274594e4fb025f5bb50b99a02d11642736d6d
Steve Wheeler <stevewheeler@google.com>
2016-04-05 15:12:35 -0700

    Added logic to pass bucket name as part of -DdataflowOptions
    Signed-off-by: Steve Wheeler <stevewheeler@google.com>

--- examples/java/src/test/java/com/google/cloud/dataflow/examples/WordCountIT.java
+++ examples/java/src/test/java/com/google/cloud/dataflow/examples/WordCountIT.java
@@ -21,6 +21,9 @@ package com.google.cloud.dataflow.examples;
 import com.google.cloud.dataflow.sdk.PipelineResult;
 import com.google.cloud.dataflow.sdk.testing.RunnableOnService;
 import com.google.cloud.dataflow.sdk.testing.TestDataflowPipelineRunner;
+import com.google.common.base.Splitter;
+
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -39,12 +42,20 @@ public class WordCountIT extends BatchE2ETest {
   @Test
   @Category(RunnableOnService.class)
   public void testE2EWordCountOnDataflow() throws Exception {
+    String dataflowOptions = 
+        System.getProperty("dataflowOptions", "bucket=apache-beam-testing-temp-storage");
+    System.out.println("dataflowOptions = " + dataflowOptions);
+    Map<String, String> dataflowOptionsMap = 
+        Splitter.on(",").withKeyValueSeparator("=").split(dataflowOptions);
+    String bucket = dataflowOptionsMap.get("bucket");
+    System.out.println("bucket = " + bucket);
+    
     String jobName = "wordcount-" + generateTestIdentifier() + "-prod";
     String[] args = {
         "--jobName=" + jobName,
         "--runner=com.google.cloud.dataflow.sdk.testing.TestDataflowPipelineRunner",
-        "--stagingLocation=gs://apache-beam-testing-temp-storage/staging/" + jobName,
-        "--output=gs://apache-beam-testing-temp-storage/output/" + jobName + "/results",
+        "--stagingLocation=" + bucket + "/staging/" + jobName,
+        "--output=" + bucket + "/output/" + jobName + "/results",
         "--workerLogLevelOverrides="
         + "{\"com.google.cloud.dataflow.sdk.util.UploadIdResponseInterceptor\":\"DEBUG\"}"};
 
