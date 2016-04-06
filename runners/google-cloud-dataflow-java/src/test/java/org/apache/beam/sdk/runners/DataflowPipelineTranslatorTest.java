c1ce371260bae50b2e043b5c35b664f87a231923
swegner <swegner@google.com>
2016-04-06 16:57:56 -0700

    Integration fixes
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=119217803

--- runners/google-cloud-dataflow-java/src/test/java/org/apache/beam/sdk/runners/DataflowPipelineTranslatorTest.java
+++ runners/google-cloud-dataflow-java/src/test/java/org/apache/beam/sdk/runners/DataflowPipelineTranslatorTest.java
@@ -72,9 +72,9 @@ import com.google.api.services.dataflow.model.Step;
 import com.google.api.services.dataflow.model.WorkerPool;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -872,9 +872,9 @@ public class DataflowPipelineTranslatorTest implements Serializable {
       public void populateDisplayData(DisplayData.Builder builder) {
         builder
             .add("foo", "bar")
-            .add("foo2", DataflowPipelineTranslatorTest.class)
-                .withLabel("Test Class")
-                .withLinkUrl("http://www.google.com");
+            .add("foo2", 123)
+            .withLabel("Test Value")
+            .withLinkUrl("http://www.google.com");
       }
     };
 
@@ -890,10 +890,12 @@ public class DataflowPipelineTranslatorTest implements Serializable {
       }
     };
 
+    ParDo.Bound<Integer, Integer> parDo1 = ParDo.of(fn1);
+    ParDo.Bound<Integer, Integer> parDo2 = ParDo.of(fn2);
     pipeline
       .apply(Create.of(1, 2, 3))
-      .apply(ParDo.of(fn1))
-      .apply(ParDo.of(fn2));
+      .apply(parDo1)
+      .apply(parDo2);
 
     Job job =
         translator
@@ -910,43 +912,38 @@ public class DataflowPipelineTranslatorTest implements Serializable {
     Map<String, Object> parDo2Properties = steps.get(2).getProperties();
     assertThat(parDo1Properties, hasKey("display_data"));
 
+    Collection<Map<String, String>> fn1displayData =
+            (Collection<Map<String, String>>) parDo1Properties.get("display_data");
+    Collection<Map<String, String>> fn2displayData =
+            (Collection<Map<String, String>>) parDo2Properties.get("display_data");
 
-    @SuppressWarnings("unchecked")
-    Collection<Map<String, Object>> fn1displayData =
-            (Collection<Map<String, Object>>) parDo1Properties.get("display_data");
-    @SuppressWarnings("unchecked")
-    Collection<Map<String, Object>> fn2displayData =
-            (Collection<Map<String, Object>>) parDo2Properties.get("display_data");
+    ImmutableSet<ImmutableMap<String, String>> expectedFn1DisplayData = ImmutableSet.of(
+        ImmutableMap.<String, String>builder()
+            .put("key", "foo")
+            .put("type", "STRING")
+            .put("value", "bar")
+            .put("namespace", fn1.getClass().getName())
+            .build(),
+        ImmutableMap.<String, String>builder()
+            .put("key", "foo2")
+            .put("type", "INTEGER")
+            .put("value", "123")
+            .put("namespace", fn1.getClass().getName())
+            .put("label", "Test Value")
+            .put("linkUrl", "http://www.google.com")
+            .build()
+    );
 
-    @SuppressWarnings("unchecked")
-    Matcher<Iterable<? extends Map<String, Object>>> fn1expectedData =
-        Matchers.<Map<String, Object>>containsInAnyOrder(
-            ImmutableMap.<String, Object>builder()
-                .put("namespace", fn1.getClass().getName())
-                .put("key", "foo")
-                .put("type", "STRING")
-                .put("value", "bar")
-                .build(),
-            ImmutableMap.<String, Object>builder()
-                .put("namespace", fn1.getClass().getName())
-                .put("key", "foo2")
-                .put("type", "JAVA_CLASS")
-                .put("value", DataflowPipelineTranslatorTest.class.getName())
-                .put("shortValue", DataflowPipelineTranslatorTest.class.getSimpleName())
-                .put("label", "Test Class")
-                .put("linkUrl", "http://www.google.com")
-                .build());
+    ImmutableSet<ImmutableMap<String, String>> expectedFn2DisplayData = ImmutableSet.of(
+        ImmutableMap.<String, String>builder()
+            .put("key", "foo3")
+            .put("type", "STRING")
+            .put("value", "barge")
+            .put("namespace", fn2.getClass().getName())
+            .build()
+    );
 
-    @SuppressWarnings("unchecked")
-    Matcher<Iterable<? extends Map<String, Object>>> fn2expectedData =
-        Matchers.<Map<String, Object>>contains(
-            ImmutableMap.<String, Object>builder()
-                .put("namespace", fn2.getClass().getName())
-                .put("key", "foo3")
-                .put("type", "INTEGER")
-                .put("value", 1234L)
-                .build());
-    assertThat(fn1displayData, fn1expectedData);
-    assertThat(fn2displayData, fn2expectedData);
+    assertEquals(expectedFn1DisplayData, ImmutableSet.copyOf(fn1displayData));
+    assertEquals(expectedFn2DisplayData, ImmutableSet.copyOf(fn2displayData));
   }
 }
