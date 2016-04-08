d84b921176e926cd71c540285f07fd37f921b0ec
Luke Cwik <lcwik@google.com>
2016-04-08 15:49:53 -0700

    [BEAM-151] Expand javadoc in TestPipeline explaining usage
    Update TestDataflowPipelineRunner to capture all the failure messages.

--- sdks/java/core/src/main/java/com/google/cloud/dataflow/sdk/testing/TestDataflowPipelineRunner.java
+++ sdks/java/core/src/main/java/com/google/cloud/dataflow/sdk/testing/TestDataflowPipelineRunner.java
@@ -212,10 +212,15 @@ public class TestDataflowPipelineRunner extends PipelineRunner<DataflowPipelineJ
     return "TestDataflowPipelineRunner#" + options.getAppName();
   }
 
+  /**
+   * Cancels the workflow on the first error message it sees.
+   *
+   * <p>Creates an error message representing the concatenation of all error messages seen.
+   */
   private static class CancelWorkflowOnError implements JobMessagesHandler {
     private final DataflowPipelineJob job;
     private final JobMessagesHandler messageHandler;
-    private volatile String errorMessage;
+    private volatile String errorMessage = "";
     private CancelWorkflowOnError(DataflowPipelineJob job, JobMessagesHandler messageHandler) {
       this.job = job;
       this.messageHandler = messageHandler;
@@ -227,15 +232,17 @@ public class TestDataflowPipelineRunner extends PipelineRunner<DataflowPipelineJ
       for (JobMessage message : messages) {
         if (message.getMessageImportance() != null
             && message.getMessageImportance().equals("JOB_MESSAGE_ERROR")) {
-          errorMessage = message.getMessageText();
-          LOG.info("Dataflow job {} threw exception, cancelling. Exception was: {}",
-              job.getJobId(), errorMessage);
-          try {
-            job.cancel();
-          } catch (Exception e) {
-            throw Throwables.propagate(e);
-          }
-
+          LOG.info("Dataflow job {} threw exception. Failure message was: {}",
+              job.getJobId(), message.getMessageText());
+          errorMessage += message.getMessageText();
+        }
+      }
+      if (!errorMessage.isEmpty()) {
+        LOG.info("Cancelling Dataflow job {}", job.getJobId());
+        try {
+          job.cancel();
+        } catch (Exception e) {
+          throw Throwables.propagate(e);
         }
       }
     }
