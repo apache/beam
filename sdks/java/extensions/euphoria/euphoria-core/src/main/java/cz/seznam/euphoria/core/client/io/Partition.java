cd56b70ac4a097455e816b1d389f88dc98cdf3b5
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-04-13 11:07:38 +0200

    #! Split partition description from its reader

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/io/Partition.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/io/Partition.java
@@ -1,15 +1,22 @@
 
 package cz.seznam.euphoria.core.client.io;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Set;
 
 /**
  * Single partition of dataset.
  */
-public interface Partition<T> extends Iterable<T>, Serializable {
+public interface Partition<T> extends Serializable {
 
   /** Get location strings (hostnames) of this partition. */
   Set<String> getLocations();
+
+  /**
+   * Opens a reader over this partition. It the caller's
+   * responsibility to close the reader once not needed anymore.
+   */
+  Reader<T> openReader() throws IOException;
 
 }
