404b633d43d23940a7f11a93a472980f7bb09ce7
Jean-Baptiste Onofré <jbonofre@apache.org>
2016-04-15 08:48:10 +0200

    [BEAM-77] Move hadoop contrib into hdfs IO

--- sdks/java/io/hdfs/src/test/java/org/apache/beam/sdk/io/hdfs/WritableCoderTest.java
+++ sdks/java/io/hdfs/src/test/java/org/apache/beam/sdk/io/hdfs/WritableCoderTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.contrib.hadoop;
+package org.apache.beam.sdk.io.hdfs;
 
 import org.apache.beam.sdk.testing.CoderProperties;
 
