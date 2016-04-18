1e3f660bfec0d09463a512d8b2393fb1266e8c5a
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-04-18 15:10:55 +0200

    #! Make dataset aware of its consumers

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/dataset/PCollection.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/dataset/PCollection.java
@@ -2,30 +2,20 @@
 package cz.seznam.euphoria.core.client.dataset;
 
 import cz.seznam.euphoria.core.client.flow.Flow;
+import java.util.Collection;
 
 /**
  * A batch dataset.
  */
-public abstract class PCollection<T> implements Dataset<T> {
-
-
-  private final Flow flow;
-
+public abstract class PCollection<T> extends FlowAwareDataset<T> {
 
   public PCollection(Flow flow) {
-    this.flow = flow;
+    super(flow);
   }
 
   @Override
   public final boolean isBounded() {
     return true;
   }
-
-  @Override
-  public Flow getFlow() {
-    return flow;
-  }
-
-
 
 }
