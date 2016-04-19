3bb9c0aacdfff372e41d7a8d4c43e71535bff930
silviuc <silviuc@google.com>
2016-04-19 10:45:43 -0700

    Remove perf regression in not yet finished size estimation code
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=120245168

--- sdks/python/google/cloud/dataflow/worker/opcounters.py
+++ sdks/python/google/cloud/dataflow/worker/opcounters.py
@@ -16,7 +16,6 @@
 
 from __future__ import absolute_import
 
-from google.cloud.dataflow.utils.counters import Accumulator
 from google.cloud.dataflow.utils.counters import Counter
 
 
@@ -29,13 +28,11 @@ class OperationCounters(object):
     self.mean_byte_counter = counter_factory.get_counter(
         '%s-out%d-MeanByteCount' % (step_name, output_index), Counter.MEAN)
     self.coder = coder
-    self._active_accumulators = []
 
   def update_from(self, windowed_value, coder=None):
     """Add one value to this counter."""
     self.element_counter.update(1)
-    byte_size_accumulator = Accumulator(self.mean_byte_counter.name)
-    self._active_accumulators.append(byte_size_accumulator)
+    # TODO(silviuc): Implement estimated size sampling.
     # TODO(gildea):
     # Actually compute the encoded size of this value.
     # In spirit, something like this:
@@ -50,9 +47,8 @@ class OperationCounters(object):
     Now that the element has been processed, we ask our accumulator
     for the total and store the result in a counter.
     """
-    for pending in self._active_accumulators:
-      self.mean_byte_counter.update(pending.total)
-    self._active_accumulators = []
+    # TODO(silviuc): Implement estimated size sampling.
+    pass
 
   def __str__(self):
     return '<%s [%s]>' % (self.__class__.__name__,
