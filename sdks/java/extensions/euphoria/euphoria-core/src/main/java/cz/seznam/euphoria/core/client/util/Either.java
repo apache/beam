b5b6df13b982ac99980b0f6133a27267e58d758f
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-04-22 16:00:35 +0200

    #! polishing after code review

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/util/Either.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/util/Either.java
@@ -1,33 +1,52 @@
 
 package cz.seznam.euphoria.core.client.util;
 
+import java.util.Objects;
+
 /**
  * Either LEFT or RIGHT element.
  */
 public final class Either<LEFT, RIGHT> {
+
   final LEFT left;
   final RIGHT right;
+
   public static <LEFT, RIGHT> Either<LEFT, RIGHT> left(LEFT left) {
+    Objects.requireNonNull(left);
     return new Either<>(left, (RIGHT) null);
   }
+
+
   public static <LEFT, RIGHT> Either<LEFT, RIGHT> right(RIGHT right) {
+    Objects.requireNonNull(right);
     return new Either<>((LEFT) null, right);
   }
+
+
   private Either(LEFT left, RIGHT right) {
     this.left = left;
     this.right = right;
   }
+
+
   public boolean isLeft() {
     return left != null;
   }
+
+
   public boolean isRight() {
     return right != null;
   }
+
+
   public LEFT left() {
     return left;
   }
+
+
   public RIGHT right() {
     return right;
   }
+
 }
 
