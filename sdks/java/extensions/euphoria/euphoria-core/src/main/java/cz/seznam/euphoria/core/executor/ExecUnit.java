52c3a0af36e461f95f1aee3a3480ec34f0a8584e
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-04-25 11:34:58 +0200

    #! clone datasets to all consumers in inmem executor

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/ExecUnit.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/ExecUnit.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Sets;
 import cz.seznam.euphoria.core.client.dataset.Dataset;
 import cz.seznam.euphoria.core.client.flow.Flow;
 import cz.seznam.euphoria.core.client.graph.DAG;
+import cz.seznam.euphoria.core.client.graph.Node;
 import cz.seznam.euphoria.core.client.operator.Operator;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,80 +27,28 @@ public class ExecUnit {
   final List<Dataset<?>> inputs = new ArrayList<>();
   /** All outputs of this exec unit. */
   final List<Dataset<?>> outputs = new ArrayList<>();
-  /** All operators consisting this exec unit. */
+  /** All dag consisting this exec unit. */
   final DAG<Operator<?, ?, ?>> operators;
-  /** Flow associated with this unit. */
-  final Flow flow;
 
   /** Split Flow into series of execution units. */
-  public static List<ExecUnit> split(Flow flow) {
-    Collection<Operator<?, ?, ?>> operators = flow.operators();
-    Set<Dataset<?>> availableDatasets = Sets.newHashSet(flow.sources());
-    List<ExecUnit> ret = new ArrayList<>();
-    List<Operator<?, ?, ?>> currentUnit = new ArrayList<>();
-    while (!operators.isEmpty()) {
-      Collection<Operator<?, ?, ?>> unfinishedOperators = new ArrayList<>();
-      for (Operator<?, ?, ?> op : operators) {
-        if (availableDatasets.containsAll(op.listInputs())) {
-          availableDatasets.add(op.output());
-          currentUnit.add(op);
-          boolean isOutput = op.output().getOutputSink() != null
-              || op.output().getCheckpointSink() != null;
-          if (isOutput) {
-            // finish this unit
-            ret.add(new ExecUnit(currentUnit, flow));
-            currentUnit.clear();
-          }
-        } else {
-          unfinishedOperators.add(op);
-        }
-      }
-      if (operators.size() == unfinishedOperators.size()) {
-        // we need to lower the number of unfinished operators
-        // otherwise we would end up in an infinite cycle
-        throw new IllegalStateException("Invalid flow: " + flow);
-      }
-      operators = unfinishedOperators;
-    }
-    return ret;
+  public static List<ExecUnit> split(DAG<Operator<?, ?, ?>> unfoldedFlow) {
+    return unfoldedFlow
+        .nodes()
+        .filter(ExecUnit::isOutput)
+        .map(unfoldedFlow::parentSubGraph)
+        .map(ExecUnit::new)
+        .collect(Collectors.toList());
   }
 
-  private ExecUnit(List<Operator<?, ?, ?>> operators, Flow flow) {
-    Set<Dataset<?>> inputDatasets = new HashSet<>();
-    Set<Dataset<?>> outputDatasets = new HashSet<>();
-    Set<Operator<?, ?, ?>> inputOperators = new HashSet<>();
-    for (Operator<?, ?, ?> op : operators) {
-      inputDatasets.addAll(
-          op.listInputs()
-            .stream()
-            .filter(d -> d.getSource() != null)
-            .collect(Collectors.toList()));
-      outputDatasets.addAll(
-          op.listInputs()
-            .stream()
-            .filter(
-                d -> d.getCheckpointSink()!= null || d.getOutputSink() != null)
-            .collect(Collectors.toList()));
-      if (op.listInputs().stream().allMatch(d -> d.getSource() != null)) {
-        inputOperators.add(op);
-      }
-    }
-    this.inputs.addAll(inputDatasets);
-    this.outputs.addAll(outputDatasets);
-    
-    this.flow = flow;
-    this.operators = DAG.of(
-        inputOperators.stream().collect(Collectors.toList()));
 
-    for (Operator<?, ?, ?> op : operators) {
-      if (!inputOperators.contains(op)) {
-        List<Operator<?, ?, ?>> parents = op.listInputs().stream()
-            .filter(i -> i.getProducer() != null)
-            .map(Dataset::getProducer)
-            .collect(Collectors.toList());
-        this.operators.add(op, parents);
-      }
-    }
+  /** Is given operator an output operator? */
+  private static boolean isOutput(Operator<?, ?, ?> operator) {
+    return operator.output().getOutputSink() != null
+        || operator.output().getCheckpointSink() != null;
+  }
+
+  private ExecUnit(DAG<Operator<?, ?, ?>> operators) {
+    this.operators = operators;
   }
 
 
@@ -117,7 +66,7 @@ public class ExecUnit {
 
   /** Retrieve exec paths for this unit. */
   public Collection<ExecPath> getPaths() {
-    Collection<DAG.Node<Operator<?, ?, ?>>> leafs = operators.getLeafs();
+    Collection<Node<Operator<?, ?, ?>>> leafs = operators.getLeafs();
     return leafs.stream()
         .map(l -> ExecPath.of(operators.parentSubGraph(l.get())))
         .collect(Collectors.toList());
