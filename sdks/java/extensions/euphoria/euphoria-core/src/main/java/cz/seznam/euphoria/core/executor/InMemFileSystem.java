4172b2b8d91027258a012be3f02da8a7fc7d3870
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-04-25 10:03:38 +0200

    #! Remove unused imports

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/InMemFileSystem.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/InMemFileSystem.java
@@ -20,7 +20,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
