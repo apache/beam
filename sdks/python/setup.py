4af243076f677d7ccda723d26aa6ea584eb85986
robertwb <robertwb@google.com>
2016-04-27 10:27:34 -0700

    Don't even try to run Cython on Windows
    MSVC doesn't always provide stdint.h
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=120928800

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -15,16 +15,22 @@
 """Python Dataflow SDK and Worker setup configuration."""
 
 import os
+import platform
 import re
 import setuptools
 
+
 # Currently all compiled modules are optional  (for performance only).
 # Cython is available on the workers, but we don't require it for development.
-try:
-  # pylint: disable=g-statement-before-imports,g-import-not-at-top
-  from Cython.Build import cythonize
-except ImportError:
+if platform.system() == 'Windows':
+  # Windows doesn't always provide int64_t.
   cythonize = lambda *args, **kwargs: []
+else:
+  try:
+    # pylint: disable=g-statement-before-imports,g-import-not-at-top
+    from Cython.Build import cythonize
+  except ImportError:
+    cythonize = lambda *args, **kwargs: []
 
 
 # Configure the required packages and scripts to install.
