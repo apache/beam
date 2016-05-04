f60be71aef6715b940c7006507b7548d57d07d8a
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-05-04 12:44:25 +0200

    #! Attempt at merging

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/dataset/AbstractWindowing.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/dataset/AbstractWindowing.java
@@ -1,47 +1,11 @@
 
 package cz.seznam.euphoria.core.client.dataset;
 
-import cz.seznam.euphoria.core.client.functional.UnaryFunction;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * An abstract state aware windowing.
  */
 public abstract class AbstractWindowing<T, KEY, W extends Window<KEY>>
-    implements Windowing<T, KEY, W> {
-
-//  final Map<KEY, Set<W>> activeWindows = new HashMap<>();
-
-//  @Override
-//  public Set<W> getActive(KEY key) {
-//    Set<W> windows = activeWindows.get(key);
-//    if (windows == null) {
-//      return new HashSet<>();
-//    }
-//    return windows;
-//  }
-
-  @Override
-  public void close(W window) {
-    window.flushAll();
-//    activeWindows.get(window.getKey()).remove(window);
-  }
-
-//  protected W addNewWindow(W window, Triggering triggering,
-//      UnaryFunction<Window<?>, Void> evict) {
-//    KEY key = window.getKey();
-//    Set<W> s = activeWindows.get(key);
-//    if (s == null) {
-//      s = new HashSet<>();
-//      activeWindows.put(key, s);
-//    }
-//    s.add(window);
-//    window.registerTrigger(triggering, evict);
-//    return window;
-//  }
+    implements Windowing<T, KEY, W>
+{
 
 }
