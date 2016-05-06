30818e8f09375bbaa1691962c1a61b7d9206ef86
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2016-05-06 16:39:02 +0200

    Fix Flink Batch Partial Combine/Combine
    We're now using a PerKeyCombineFnRunner for all interaction with the
    CombineFn. This required adding a proper ProcessContext in
    FlinkReduceFunction and FlinkPartialReduceFunction, along with adding
    support for side inputs there.

--- runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkReduceFunction.java
+++ runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkReduceFunction.java
@@ -17,15 +17,32 @@
  */
 package org.apache.beam.runners.flink.translation.functions;
 
+import org.apache.beam.runners.flink.translation.wrappers.SerializableFnAggregatorWrapper;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.transforms.Aggregator;
 import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.CombineFnBase;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
+import org.apache.beam.sdk.transforms.windowing.PaneInfo;
+import org.apache.beam.sdk.util.PerKeyCombineFnRunner;
+import org.apache.beam.sdk.util.PerKeyCombineFnRunners;
+import org.apache.beam.sdk.util.WindowedValue;
+import org.apache.beam.sdk.util.WindowingInternals;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.sdk.values.TupleTag;
 
 import com.google.common.collect.ImmutableList;
 
-import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.functions.RichGroupReduceFunction;
 import org.apache.flink.util.Collector;
+import org.joda.time.Instant;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Flink {@link org.apache.flink.api.common.functions.GroupReduceFunction} for executing a
@@ -33,16 +50,31 @@ import java.util.Iterator;
  * {@link org.apache.beam.sdk.values.KV} elements, extracts the key and merges the
  * accumulators resulting from the PartialReduce which produced the input VA.
  */
-public class FlinkReduceFunction<K, VA, VO> implements GroupReduceFunction<KV<K, VA>, KV<K, VO>> {
+public class FlinkReduceFunction<K, VA, VO>
+    extends RichGroupReduceFunction<KV<K, VA>, KV<K, VO>> {
 
-  private final Combine.KeyedCombineFn<K, ?, VA, VO> keyedCombineFn;
+  private final CombineFnBase.PerKeyCombineFn<K, ?, VA, VO> combineFn;
 
-  public FlinkReduceFunction(Combine.KeyedCombineFn<K, ?, VA, VO> keyedCombineFn) {
-    this.keyedCombineFn = keyedCombineFn;
+  private final DoFn<Iterable<KV<K, VA>>, KV<K, VO>> doFn;
+
+  public FlinkReduceFunction(CombineFnBase.PerKeyCombineFn<K, ?, VA, VO> keyedCombineFn) {
+    this.combineFn = keyedCombineFn;
+
+    // dummy DoFn because we need one for ProcessContext
+    this.doFn = new DoFn<Iterable<KV<K, VA>>, KV<K, VO>>() {
+      @Override
+      public void processElement(ProcessContext c) throws Exception {
+
+      }
+    };
   }
 
   @Override
   public void reduce(Iterable<KV<K, VA>> values, Collector<KV<K, VO>> out) throws Exception {
+
+    ProcessContext processContext = new ProcessContext(doFn, values, out);
+    PerKeyCombineFnRunner<K, ?, VA, VO> combineFnRunner = PerKeyCombineFnRunners.create(combineFn);
+
     Iterator<KV<K, VA>> it = values.iterator();
 
     KV<K, VA> current = it.next();
@@ -51,9 +83,104 @@ public class FlinkReduceFunction<K, VA, VO> implements GroupReduceFunction<KV<K,
 
     while (it.hasNext()) {
       current = it.next();
-      keyedCombineFn.mergeAccumulators(k, ImmutableList.of(accumulator, current.getValue()) );
+      combineFnRunner.mergeAccumulators(k,
+          ImmutableList.of(accumulator, current.getValue()),
+          processContext);
     }
 
-    out.collect(KV.of(k, keyedCombineFn.extractOutput(k, accumulator)));
+    out.collect(KV.of(k, combineFnRunner.extractOutput(k, accumulator, processContext)));
+  }
+
+  private class ProcessContext extends DoFn<Iterable<KV<K, VA>>, KV<K, VO>>.ProcessContext {
+
+    private final DoFn<Iterable<KV<K, VA>>, KV<K, VO>> fn;
+
+    private final Collector<KV<K, VO>> collector;
+
+    private final Iterable<KV<K, VA>> element;
+
+    private ProcessContext(
+        DoFn<Iterable<KV<K, VA>>, KV<K, VO>> function,
+        Iterable<KV<K, VA>> element,
+        Collector<KV<K, VO>> outCollector) {
+      function.super();
+      super.setupDelegateAggregators();
+
+      this.fn = function;
+      this.element = element;
+      this.collector = outCollector;
+    }
+
+    @Override
+    public Iterable<KV<K, VA>> element() {
+      return this.element;
+    }
+
+    @Override
+    public Instant timestamp() {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public BoundedWindow window() {
+      throw new UnsupportedOperationException("Not supported.");
+
+    }
+
+    @Override
+    public PaneInfo pane() {
+      return PaneInfo.NO_FIRING;
+    }
+
+    @Override
+    public WindowingInternals<Iterable<KV<K, VA>>, KV<K, VO>> windowingInternals() {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public PipelineOptions getPipelineOptions() {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public <T> T sideInput(PCollectionView<T> view) {
+      List<T> sideInput = getRuntimeContext().getBroadcastVariable(view.getTagInternal().getId());
+      List<WindowedValue<?>> windowedValueList = new ArrayList<>(sideInput.size());
+      for (T input : sideInput) {
+        windowedValueList.add(WindowedValue.of(input, Instant.now(), ImmutableList.of(GlobalWindow.INSTANCE), pane()));
+      }
+      return view.fromIterableInternal(windowedValueList);
+
+    }
+
+    @Override
+    public void output(KV<K, VO> output) {
+      collector.collect(output);
+    }
+
+    @Override
+    public void outputWithTimestamp(KV<K, VO> output, Instant timestamp) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public <T> void sideOutput(TupleTag<T> tag, T output) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public <T> void sideOutputWithTimestamp(TupleTag<T> tag, T output, Instant timestamp) {
+      throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    protected <AggInputT, AggOutputT> Aggregator<AggInputT, AggOutputT> createAggregatorInternal(
+        String name,
+        Combine.CombineFn<AggInputT, ?, AggOutputT> combiner) {
+      SerializableFnAggregatorWrapper<AggInputT, AggOutputT> wrapper =
+          new SerializableFnAggregatorWrapper<>(combiner);
+      getRuntimeContext().addAccumulator(name, wrapper);
+      return wrapper;
+    }
   }
 }
