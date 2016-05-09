95bc262e392c7b18ea4ce2be934ad352b7fddda4
Jesse Anderson <jesse@smokinghand.com>
2016-05-09 16:06:51 -0700

    Removed unused imports.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/coders/BigDecimalCoder.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/coders/BigDecimalCoder.java
@@ -21,11 +21,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UTFDataFormatException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
