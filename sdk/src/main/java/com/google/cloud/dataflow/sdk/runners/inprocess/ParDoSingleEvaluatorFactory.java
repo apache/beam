46298e3ff7f86fb637efdbfd1a6897b3e405ba97
Thomas Groh <tgroh@google.com>
2016-05-10 14:06:27 -0700

    Reuse DoFns in ParDoEvaluators
    This allows the runner to avoid cloning DoFns for every input bundle.

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/inprocess/ParDoSingleEvaluatorFactory.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/inprocess/ParDoSingleEvaluatorFactory.java
@@ -17,10 +17,13 @@ package com.google.cloud.dataflow.sdk.runners.inprocess;
 
 import com.google.cloud.dataflow.sdk.runners.inprocess.InProcessPipelineRunner.CommittedBundle;
 import com.google.cloud.dataflow.sdk.transforms.AppliedPTransform;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.PTransform;
 import com.google.cloud.dataflow.sdk.transforms.ParDo.Bound;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 import com.google.cloud.dataflow.sdk.values.TupleTag;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Collections;
@@ -30,6 +33,13 @@ import java.util.Collections;
  * {@link Bound ParDo.Bound} primitive {@link PTransform}.
  */
 class ParDoSingleEvaluatorFactory implements TransformEvaluatorFactory {
+  private final LoadingCache<DoFn<?, ?>, ThreadLocal<DoFn<?, ?>>> fnClones;
+
+  public ParDoSingleEvaluatorFactory() {
+    fnClones = CacheBuilder.newBuilder()
+        .build(SerializableCloningThreadLocalCacheLoader.<DoFn<?, ?>>create());
+  }
+
   @Override
   public <T> TransformEvaluator<T> forApplication(
       final AppliedPTransform<?, ?, ?> application,
@@ -41,20 +51,28 @@ class ParDoSingleEvaluatorFactory implements TransformEvaluatorFactory {
     return evaluator;
   }
 
-  private static <InputT, OutputT> ParDoInProcessEvaluator<InputT> createSingleEvaluator(
+  private <InputT, OutputT> TransformEvaluator<InputT> createSingleEvaluator(
       @SuppressWarnings("rawtypes") AppliedPTransform<PCollection<InputT>, PCollection<OutputT>,
           Bound<InputT, OutputT>> application,
       CommittedBundle<InputT> inputBundle, InProcessEvaluationContext evaluationContext) {
     TupleTag<OutputT> mainOutputTag = new TupleTag<>("out");
 
-    return ParDoInProcessEvaluator.create(
-        evaluationContext,
-        inputBundle,
-        application,
-        application.getTransform().getFn(),
-        application.getTransform().getSideInputs(),
-        mainOutputTag,
-        Collections.<TupleTag<?>>emptyList(),
-        ImmutableMap.<TupleTag<?>, PCollection<?>>of(mainOutputTag, application.getOutput()));
+    @SuppressWarnings({"unchecked", "rawtypes"}) ThreadLocal<DoFn<InputT, OutputT>> fnLocal =
+        (ThreadLocal) fnClones.getUnchecked(application.getTransform().getFn());
+    try {
+      ParDoInProcessEvaluator<InputT> parDoEvaluator = ParDoInProcessEvaluator.create(
+          evaluationContext,
+          inputBundle,
+          application,
+          fnLocal.get(),
+          application.getTransform().getSideInputs(),
+          mainOutputTag,
+          Collections.<TupleTag<?>>emptyList(),
+          ImmutableMap.<TupleTag<?>, PCollection<?>>of(mainOutputTag, application.getOutput()));
+      return ThreadLocalInvalidatingTransformEvaluator.wrapping(parDoEvaluator, fnLocal);
+    } catch (Exception e) {
+      fnLocal.remove();
+      throw e;
+    }
   }
 }
