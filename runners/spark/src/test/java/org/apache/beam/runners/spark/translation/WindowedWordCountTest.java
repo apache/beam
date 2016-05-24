69da98a93956add8e86d094cb866bf86c5626089
Ilya Ganelin <ilya.ganelin@capitalone.com>
2016-05-24 13:37:04 -0700

    Use Create.of withCoder instead of setCoder on the created PCollection
    One more left..

--- runners/spark/src/test/java/org/apache/beam/runners/spark/translation/WindowedWordCountTest.java
+++ runners/spark/src/test/java/org/apache/beam/runners/spark/translation/WindowedWordCountTest.java
@@ -75,8 +75,8 @@ public class WindowedWordCountTest {
   @Test
   public void testFixed2() throws Exception {
     Pipeline p = Pipeline.create(PipelineOptionsFactory.create());
-    PCollection<String> inputWords = p.apply(Create.timestamped(WORDS, TIMESTAMPS))
-        .setCoder(StringUtf8Coder.of());
+    PCollection<String> inputWords = p.apply(Create.timestamped(WORDS, TIMESTAMPS)
+        .withCoder(StringUtf8Coder.of()));
     PCollection<String> windowedWords = inputWords
         .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(5))));
 
@@ -95,8 +95,8 @@ public class WindowedWordCountTest {
   @Test
   public void testSliding() throws Exception {
     Pipeline p = Pipeline.create(PipelineOptionsFactory.create());
-    PCollection<String> inputWords = p.apply(Create.timestamped(WORDS, TIMESTAMPS))
-        .setCoder(StringUtf8Coder.of());
+    PCollection<String> inputWords = p.apply(Create.timestamped(WORDS, TIMESTAMPS)
+        .withCoder(StringUtf8Coder.of()));
     PCollection<String> windowedWords = inputWords
         .apply(Window.<String>into(SlidingWindows.of(Duration.standardMinutes(2))
         .every(Duration.standardMinutes(1))));
