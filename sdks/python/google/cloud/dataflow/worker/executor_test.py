0e33df14293ea4414426711209de45cf3b5e9363
chamikara <chamikara@google.com>
2016-05-23 23:09:35 -0700

    Dynamic work rebalancing support for InMemory reader.
    ----Release Notes----
    []
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=123075083

--- sdks/python/google/cloud/dataflow/worker/executor_test.py
+++ sdks/python/google/cloud/dataflow/worker/executor_test.py
@@ -95,7 +95,7 @@ class ProgressRequestRecordingInMemoryReader(inmemory.InMemoryReader):
   def get_progress(self):
     next_progress = super(ProgressRequestRecordingInMemoryReader,
                           self).get_progress()
-    self.progress_record.append(next_progress.percent_complete)
+    self.progress_record.append(next_progress.position.record_index)
     return next_progress
 
 
@@ -332,11 +332,7 @@ class ExecutorTest(unittest.TestCase):
     ]))
     self.assertEqual(elements, output_buffer)
 
-    expected_progress_record = []
-    len_elements = len(elements)
-    for i in range(len_elements):
-      expected_progress_record.append(float(i + 1) / len_elements)
-
+    expected_progress_record = range(len(elements))
     self.assertEqual(expected_progress_record,
                      source.last_reader.progress_record)
 
