212fd2e78b0e0228359148f10db3834c30167f79
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2016-06-01 11:56:18 +0200

    [BEAM-321] Fix Flink Comparators
    KvCoderComparator and CoderComparator were hashing the key directly
    while doing comparisons on the encoded form. This lead to
    inconsistencies in GroupByKey results with large numbers of elements per
    key.
    
    This changes the comparators to hash on the encoded form and also adds
    tests to verify the correct behavior.

--- runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/types/EncodedValueTypeInformation.java
+++ runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/types/EncodedValueTypeInformation.java
@@ -19,99 +19,93 @@ package org.apache.beam.runners.flink.translation.types;
 
 import org.apache.beam.sdk.coders.Coder;
 
-import com.google.common.base.Preconditions;
-
+import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeinfo.AtomicType;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.TypeComparator;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.base.array.BytePrimitiveArraySerializer;
+
+import java.util.Objects;
 
 /**
- * Flink {@link org.apache.flink.api.common.typeinfo.TypeInformation} for
- * Dataflow {@link org.apache.beam.sdk.coders.Coder}s.
+ * Flink {@link TypeInformation} for Beam values that have been encoded to byte data
+ * by a {@link Coder}.
  */
-public class CoderTypeInformation<T> extends TypeInformation<T> implements AtomicType<T> {
+public class EncodedValueTypeInformation
+    extends TypeInformation<byte[]>
+    implements AtomicType<byte[]> {
 
-  private final Coder<T> coder;
-
-  public CoderTypeInformation(Coder<T> coder) {
-    Preconditions.checkNotNull(coder);
-    this.coder = coder;
-  }
+  private static final long serialVersionUID = 1L;
 
   @Override
+  @PublicEvolving
   public boolean isBasicType() {
     return false;
   }
 
   @Override
+  @PublicEvolving
   public boolean isTupleType() {
     return false;
   }
 
   @Override
+  @PublicEvolving
   public int getArity() {
-    return 1;
+    return 0;
   }
 
   @Override
-  @SuppressWarnings("unchecked")
-  public Class<T> getTypeClass() {
-    // We don't have the Class, so we have to pass null here. What a shame...
-    return (Class<T>) Object.class;
+  @PublicEvolving
+  public int getTotalFields() {
+    return 0;
   }
 
   @Override
+  @PublicEvolving
+  public Class<byte[]> getTypeClass() {
+    return byte[].class;
+  }
+
+  @Override
+  @PublicEvolving
   public boolean isKeyType() {
     return true;
   }
 
   @Override
-  @SuppressWarnings("unchecked")
-  public TypeSerializer<T> createSerializer(ExecutionConfig config) {
-    return new CoderTypeSerializer<>(coder);
+  @PublicEvolving
+  public TypeSerializer<byte[]> createSerializer(ExecutionConfig executionConfig) {
+    return new EncodedValueSerializer();
   }
 
   @Override
-  public int getTotalFields() {
-    return 2;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-
-    CoderTypeInformation that = (CoderTypeInformation) o;
-
-    return coder.equals(that.coder);
-
+  public boolean equals(Object other) {
+    return other instanceof EncodedValueTypeInformation;
   }
 
   @Override
   public int hashCode() {
-    return coder.hashCode();
+    return this.getClass().hashCode();
   }
 
   @Override
   public boolean canEqual(Object obj) {
-    return obj instanceof CoderTypeInformation;
+    return obj instanceof EncodedValueTypeInformation;
   }
 
   @Override
   public String toString() {
-    return "CoderTypeInformation{coder=" + coder + '}';
+    return "EncodedValueTypeInformation";
   }
 
   @Override
-  public TypeComparator<T> createComparator(boolean sortOrderAscending, ExecutionConfig
-      executionConfig) {
-    throw new UnsupportedOperationException(
-        "Non-encoded values cannot be compared directly.");
+  @PublicEvolving
+  public TypeComparator<byte[]> createComparator(
+      boolean sortOrderAscending,
+      ExecutionConfig executionConfig) {
+    return new EncodedValueComparator(sortOrderAscending);
   }
 }
