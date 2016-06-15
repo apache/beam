b31be38a8a2f839fb37af4963512c50424ac9f13
Thomas Groh <tgroh@google.com>
2016-06-14 17:51:48 -0700

    Remove DoFnRunner from GroupAlsoByWindowsProperties
    DoFnRunner is a runner implementation detail, and core SDK code should
    instead use DoFnTester.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/DoFnTester.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/DoFnTester.java
@@ -18,6 +18,7 @@
 package org.apache.beam.sdk.transforms;
 
 import org.apache.beam.sdk.annotations.Experimental;
+import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Combine.CombineFn;
@@ -25,8 +26,11 @@ import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.transforms.windowing.PaneInfo;
 import org.apache.beam.sdk.util.PTuple;
 import org.apache.beam.sdk.util.SerializableUtils;
+import org.apache.beam.sdk.util.TimerInternals;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.util.WindowingInternals;
+import org.apache.beam.sdk.util.state.InMemoryStateInternals;
+import org.apache.beam.sdk.util.state.StateInternals;
 import org.apache.beam.sdk.values.PCollectionView;
 import org.apache.beam.sdk.values.TimestampedValue;
 import org.apache.beam.sdk.values.TupleTag;
@@ -40,8 +44,10 @@ import com.google.common.collect.Lists;
 
 import org.joda.time.Instant;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -564,9 +570,54 @@ public class DoFnTester<InputT, OutputT> {
 
     @Override
     public WindowingInternals<InT, OutT> windowingInternals() {
-      throw new UnsupportedOperationException(
-          "WindowingInternals is an internal implementation detail of the Beam SDK, "
-              + "and should not be used by user code");
+      return new WindowingInternals<InT, OutT>() {
+        StateInternals<?> stateInternals = InMemoryStateInternals.forKey(new Object());
+
+        @Override
+        public StateInternals<?> stateInternals() {
+          return stateInternals;
+        }
+
+        @Override
+        public void outputWindowedValue(
+            OutT output,
+            Instant timestamp,
+            Collection<? extends BoundedWindow> windows,
+            PaneInfo pane) {
+          context.noteOutput(mainOutputTag, WindowedValue.of(output, timestamp, windows, pane));
+        }
+
+        @Override
+        public TimerInternals timerInternals() {
+          throw
+              new UnsupportedOperationException("Timer Internals are not supported in DoFnTester");
+        }
+
+        @Override
+        public Collection<? extends BoundedWindow> windows() {
+          return element.getWindows();
+        }
+
+        @Override
+        public PaneInfo pane() {
+          return element.getPane();
+        }
+
+        @Override
+        public <T> void writePCollectionViewData(
+            TupleTag<?> tag, Iterable<WindowedValue<T>> data, Coder<T> elemCoder)
+            throws IOException {
+          throw new UnsupportedOperationException(
+              "WritePCollectionViewData is not supported in in the context of DoFnTester");
+        }
+
+        @Override
+        public <T> T sideInput(
+            PCollectionView<T> view, BoundedWindow mainInputWindow) {
+          throw new UnsupportedOperationException(
+              "SideInput from WindowingInternals is not supported in in the context of DoFnTester");
+        }
+      };
     }
 
     @Override
@@ -613,11 +664,6 @@ public class DoFnTester<InputT, OutputT> {
     STARTED,
     FINISHED
   }
-
-  /** The name of the step of a DoFnTester. */
-  static final String STEP_NAME = "stepName";
-  /** The name of the enclosing DoFn PTransform for a DoFnTester. */
-  static final String TRANSFORM_NAME = "transformName";
 
   final PipelineOptions options = PipelineOptionsFactory.create();
 
