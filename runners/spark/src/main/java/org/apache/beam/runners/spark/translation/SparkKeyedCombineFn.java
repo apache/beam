4495b4c87280ecdbe36d06142c0d11e3461c8d32
Sela <ansela@paypal.com>
2016-07-10 17:52:04 +0300

    It seems as if SPARK-15810 is not preventing the use of Option anymore, and using a Scala class is fine by me as long as it remains in the scope of this class.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/SparkKeyedCombineFn.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/SparkKeyedCombineFn.java
@@ -23,83 +23,88 @@ import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.values.KV;
 
-import com.google.common.base.Optional;
-
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.expressions.Aggregator;
 
 import java.util.Arrays;
 import java.util.Collections;
 
+import scala.Option;
 import scala.Tuple2;
 
 /**
  * Spark runner implementation of {@link Combine.KeyedCombineFn}.
  *
- * This implementation uses {@link Optional} because, currently, {@link Aggregator}s
+ * This implementation uses {@link Option} because, currently, {@link Aggregator}s
  * do not allow creating the buffer (aka accumulator in Beam) using the input, and having zero()
  * return null will skip this computation.
- * //TODO: remove usage of Optional once this is possible.
+ * //TODO: remove usage of Option once possible - https://issues.apache.org/jira/browse/SPARK-15810
  */
 class SparkKeyedCombineFn<K, InputT, AccumT, OutputT>
-    extends Aggregator<KV<WindowedValue<K>, InputT>, Optional<Tuple2<WindowedValue<K>, AccumT>>,
+    extends Aggregator<KV<WindowedValue<K>, InputT>, Option<Tuple2<WindowedValue<K>, AccumT>>,
     WindowedValue<OutputT>> {
 
   private final Combine.KeyedCombineFn<K, InputT, AccumT, OutputT> fn;
 
-  public SparkKeyedCombineFn(Combine.KeyedCombineFn<K, InputT, AccumT, OutputT> fn) {
+  SparkKeyedCombineFn(Combine.KeyedCombineFn<K, InputT, AccumT, OutputT> fn) {
     this.fn = fn;
   }
 
   @Override
-  public Optional<Tuple2<WindowedValue<K>, AccumT>> zero() {
-    return Optional.absent();
+  public Option<Tuple2<WindowedValue<K>, AccumT>> zero() {
+    return Option.empty();
   }
 
   @Override
-  public Optional<Tuple2<WindowedValue<K>, AccumT>> reduce(
-      Optional<Tuple2<WindowedValue<K>, AccumT>> accumOpt, KV<WindowedValue<K>, InputT> in) {
+  public Option<Tuple2<WindowedValue<K>, AccumT>>
+  reduce(Option<Tuple2<WindowedValue<K>, AccumT>> accumOpt, KV<WindowedValue<K>, InputT> in) {
+
     AccumT accum;
     WindowedValue<K> wk = in.getKey();
-    if (!accumOpt.isPresent()) {
+    if (accumOpt.isEmpty()) {
       accum = fn.createAccumulator(wk.getValue());
     } else {
       accum = accumOpt.get()._2();
     }
+
     accum = fn.addInput(wk.getValue(), accum, in.getValue());
     Tuple2<WindowedValue<K>, AccumT> output = new Tuple2<>(WindowedValue.of(wk.getValue(),
         wk.getTimestamp(), wk.getWindows(), wk.getPane()), accum);
-    return Optional.of(output);
+
+    return Option.apply(output);
   }
 
   @Override
-  public Optional<Tuple2<WindowedValue<K>, AccumT>> merge(
-      Optional<Tuple2<WindowedValue<K>, AccumT>> accumOpt1,
-      Optional<Tuple2<WindowedValue<K>, AccumT>> accumOpt2) {
-    if (!accumOpt1.isPresent()) {
+  public Option<Tuple2<WindowedValue<K>, AccumT>>
+  merge(Option<Tuple2<WindowedValue<K>, AccumT>> accumOpt1,
+        Option<Tuple2<WindowedValue<K>, AccumT>> accumOpt2) {
+
+    if (accumOpt1.isEmpty()) {
       return accumOpt2;
-    } else if (!accumOpt2.isPresent()) {
+    } else if (accumOpt2.isEmpty()) {
       return accumOpt1;
     } else {
       WindowedValue<K> wk = accumOpt1.get()._1();
-      Iterable<AccumT> accums = Collections.unmodifiableCollection(
+      Iterable<AccumT> accums =
+          Collections.unmodifiableCollection(
           Arrays.asList(accumOpt1.get()._2(), accumOpt2.get()._2()));
       AccumT merged = fn.mergeAccumulators(wk.getValue(), accums);
-      return Optional.of(new Tuple2<>(wk, merged));
+
+      return Option.apply(new Tuple2<>(wk, merged));
     }
   }
 
   @Override
   public WindowedValue<OutputT>
-  finish(Optional<Tuple2<WindowedValue<K>, AccumT>> reduction) {
+  finish(Option<Tuple2<WindowedValue<K>, AccumT>> reduction) {
+
     WindowedValue<K> wk = reduction.get()._1();
-    AccumT accum = reduction.get()._2();
-    return WindowedValue.of(fn.extractOutput(wk.getValue(), accum), wk.getTimestamp(),
-        wk.getWindows(), wk.getPane());
+    return WindowedValue.of(fn.extractOutput(wk.getValue(), reduction.get()._2()),
+            wk.getTimestamp(), wk.getWindows(), wk.getPane());
   }
 
   @Override
-  public Encoder<Optional<Tuple2<WindowedValue<K>, AccumT>>> bufferEncoder() {
+  public Encoder<Option<Tuple2<WindowedValue<K>, AccumT>>> bufferEncoder() {
     return EncoderHelpers.encoder();
   }
 
