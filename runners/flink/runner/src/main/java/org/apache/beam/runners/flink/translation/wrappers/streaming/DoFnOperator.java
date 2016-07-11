e7c731570fd8b451c7433f9b62b242471ff69991
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2016-07-11 14:08:35 +0200

    [BEAM-102] Add Side Inputs in Flink Streaming Runner
    This adds a generic SideInputHandler in runners-core that is only used
    by the Flink runner right now but can be used by other runner
    implementations.

--- runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
+++ runners/flink/runner/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
@@ -17,9 +17,13 @@
  */
 package org.apache.beam.runners.flink.translation.wrappers.streaming;
 
+import avro.shaded.com.google.common.base.Preconditions;
+
+import org.apache.beam.runners.core.SideInputHandler;
 import org.apache.beam.runners.flink.translation.utils.SerializedPipelineOptions;
 import org.apache.beam.runners.flink.translation.wrappers.SerializableFnAggregatorWrapper;
 import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.VoidCoder;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.Aggregator;
 import org.apache.beam.sdk.transforms.Combine;
@@ -27,9 +31,13 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.OldDoFn;
 import org.apache.beam.sdk.transforms.join.RawUnionValue;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.util.CoderUtils;
 import org.apache.beam.sdk.util.DoFnRunner;
 import org.apache.beam.sdk.util.DoFnRunners;
 import org.apache.beam.sdk.util.ExecutionContext;
+import org.apache.beam.sdk.util.NullSideInputReader;
+import org.apache.beam.sdk.util.PushbackSideInputDoFnRunner;
+import org.apache.beam.sdk.util.SideInputReader;
 import org.apache.beam.sdk.util.TimerInternals;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.util.WindowingStrategy;
@@ -37,15 +45,36 @@ import org.apache.beam.sdk.util.state.StateInternals;
 import org.apache.beam.sdk.values.PCollectionView;
 import org.apache.beam.sdk.values.TupleTag;
 
+import com.google.common.collect.Iterables;
+
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.ReducingState;
+import org.apache.flink.api.common.state.ReducingStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeutils.base.LongSerializer;
+import org.apache.flink.api.common.typeutils.base.VoidSerializer;
+import org.apache.flink.api.java.typeutils.GenericTypeInfo;
+import org.apache.flink.runtime.state.AbstractStateBackend;
+import org.apache.flink.runtime.state.KvStateSnapshot;
+import org.apache.flink.runtime.state.StateHandle;
 import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
 import org.apache.flink.streaming.api.operators.ChainingStrategy;
 import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.api.operators.Output;
+import org.apache.flink.streaming.api.operators.TwoInputStreamOperator;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.streaming.runtime.tasks.StreamTaskState;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -58,7 +87,8 @@ import java.util.Map;
  */
 public class DoFnOperator<InputT, FnOutputT, OutputT>
     extends AbstractStreamOperator<OutputT>
-    implements OneInputStreamOperator<WindowedValue<InputT>, OutputT> {
+    implements OneInputStreamOperator<WindowedValue<InputT>, OutputT>,
+      TwoInputStreamOperator<WindowedValue<InputT>, RawUnionValue, OutputT> {
 
   protected OldDoFn<InputT, FnOutputT> doFn;
   protected final SerializedPipelineOptions serializedOptions;
@@ -66,7 +96,8 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
   protected final TupleTag<FnOutputT> mainOutputTag;
   protected final List<TupleTag<?>> sideOutputTags;
 
-  protected final Map<PCollectionView<?>, WindowingStrategy<?, ?>> sideInputs;
+  protected final Collection<PCollectionView<?>> sideInputs;
+  protected final Map<Integer, PCollectionView<?>> sideInputTagMapping;
 
   protected final boolean hasSideInputs;
 
@@ -74,31 +105,51 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
 
   protected final OutputManagerFactory<OutputT> outputManagerFactory;
 
-  protected transient DoFnRunner<InputT, FnOutputT> doFnRunner;
+  protected transient PushbackSideInputDoFnRunner<InputT, FnOutputT> pushbackDoFnRunner;
 
-  /**
-   * To keep track of the current watermark so that we can immediately fire if a trigger
-   * registers an event time callback for a timestamp that lies in the past.
-   */
-  protected transient long currentWatermark = Long.MIN_VALUE;
+  protected transient SideInputHandler sideInputHandler;
+
+  protected transient long currentInputWatermark;
+
+  protected transient long currentOutputWatermark;
+
+  private transient AbstractStateBackend sideInputStateBackend;
+
+  private final ReducingStateDescriptor<Long> pushedBackWatermarkDescriptor;
+
+  private final ListStateDescriptor<WindowedValue<InputT>> pushedBackDescriptor;
+
+  private transient Map<String, KvStateSnapshot<?, ?, ?, ?, ?>> restoredSideInputState;
 
   public DoFnOperator(
       OldDoFn<InputT, FnOutputT> doFn,
+      TypeInformation<WindowedValue<InputT>> inputType,
       TupleTag<FnOutputT> mainOutputTag,
       List<TupleTag<?>> sideOutputTags,
       OutputManagerFactory<OutputT> outputManagerFactory,
       WindowingStrategy<?, ?> windowingStrategy,
-      Map<PCollectionView<?>, WindowingStrategy<?, ?>> sideInputs,
+      Map<Integer, PCollectionView<?>> sideInputTagMapping,
+      Collection<PCollectionView<?>> sideInputs,
       PipelineOptions options) {
     this.doFn = doFn;
     this.mainOutputTag = mainOutputTag;
     this.sideOutputTags = sideOutputTags;
+    this.sideInputTagMapping = sideInputTagMapping;
     this.sideInputs = sideInputs;
     this.serializedOptions = new SerializedPipelineOptions(options);
     this.windowingStrategy = windowingStrategy;
     this.outputManagerFactory = outputManagerFactory;
 
     this.hasSideInputs = !sideInputs.isEmpty();
+
+    this.pushedBackWatermarkDescriptor =
+        new ReducingStateDescriptor<>(
+            "pushed-back-elements-watermark-hold",
+            new LongMinReducer(),
+            LongSerializer.INSTANCE);
+
+    this.pushedBackDescriptor =
+        new ListStateDescriptor<>("pushed-back-values", inputType);
 
     setChainingStrategy(ChainingStrategy.ALWAYS);
   }
@@ -119,7 +170,10 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
 
     this.doFn = getDoFn();
 
-    Aggregator.AggregatorFactory aggregatorFactory = new Aggregator.AggregatorFactory() {
+    currentInputWatermark = Long.MIN_VALUE;
+    currentOutputWatermark = currentInputWatermark;
+
+   	Aggregator.AggregatorFactory aggregatorFactory = new Aggregator.AggregatorFactory() {
       @Override
       public <InputT, AccumT, OutputT> Aggregator<InputT, OutputT> createAggregatorForDoFn(
           Class<?> fnClass,
@@ -134,10 +188,42 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
       }
     };
 
-    doFnRunner = DoFnRunners.createDefault(
+    SideInputReader sideInputReader = NullSideInputReader.of(sideInputs);
+    if (!sideInputs.isEmpty()) {
+      String operatorIdentifier =
+          this.getClass().getSimpleName() + "_"
+              + getRuntimeContext().getIndexOfThisSubtask() + "_sideInput";
+
+      sideInputStateBackend = this
+          .getContainingTask()
+          .createStateBackend(operatorIdentifier,
+              new GenericTypeInfo<>(ByteBuffer.class).createSerializer(new ExecutionConfig()));
+
+      Preconditions.checkState(
+          sideInputStateBackend != null,
+          "Side input state backend cannot be bull");
+
+      if (restoredSideInputState != null) {
+        @SuppressWarnings("unchecked,rawtypes")
+        HashMap<String, KvStateSnapshot> castRestored = (HashMap) restoredSideInputState;
+        sideInputStateBackend.injectKeyValueStateSnapshots(castRestored, 0L);
+        restoredSideInputState = null;
+      }
+
+      sideInputStateBackend.setCurrentKey(
+          ByteBuffer.wrap(CoderUtils.encodeToByteArray(VoidCoder.of(), null)));
+
+      StateInternals<Void> sideInputStateInternals =
+          new FlinkStateInternals<>(sideInputStateBackend, VoidCoder.of());
+
+      sideInputHandler = new SideInputHandler(sideInputs, sideInputStateInternals);
+      sideInputReader = sideInputHandler;
+    }
+
+    DoFnRunner<InputT, FnOutputT> doFnRunner = DoFnRunners.createDefault(
         serializedOptions.getPipelineOptions(),
         doFn,
-        null,
+        sideInputReader,
         outputManagerFactory.create(output),
         mainOutputTag,
         sideOutputTags,
@@ -145,25 +231,177 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
         aggregatorFactory,
         windowingStrategy);
 
-    doFnRunner.startBundle();
+    pushbackDoFnRunner =
+        PushbackSideInputDoFnRunner.create(doFnRunner, sideInputs, sideInputHandler);
+
     doFn.setup();
   }
 
   @Override
   public void close() throws Exception {
     super.close();
-    doFnRunner.finishBundle();
     doFn.teardown();
   }
 
+  protected final long getPushbackWatermarkHold() {
+    // if we don't have side inputs we never hold the watermark
+    if (sideInputs.isEmpty()) {
+      return Long.MAX_VALUE;
+    }
+
+    try {
+      Long result = sideInputStateBackend.getPartitionedState(
+          null,
+          VoidSerializer.INSTANCE,
+          pushedBackWatermarkDescriptor).get();
+      return result != null ? result : Long.MAX_VALUE;
+    } catch (Exception e) {
+      throw new RuntimeException("Error retrieving pushed back watermark state.", e);
+    }
+  }
+
   @Override
-  public final void processElement(StreamRecord<WindowedValue<InputT>> streamRecord) throws Exception {
-    doFnRunner.processElement(streamRecord.getValue());
+  public final void processElement(
+      StreamRecord<WindowedValue<InputT>> streamRecord) throws Exception {
+    pushbackDoFnRunner.startBundle();
+    pushbackDoFnRunner.processElement(streamRecord.getValue());
+    pushbackDoFnRunner.finishBundle();
+  }
+
+  @Override
+  public final void processElement1(
+      StreamRecord<WindowedValue<InputT>> streamRecord) throws Exception {
+    pushbackDoFnRunner.startBundle();
+    Iterable<WindowedValue<InputT>> justPushedBack =
+        pushbackDoFnRunner.processElementInReadyWindows(streamRecord.getValue());
+
+    ListState<WindowedValue<InputT>> pushedBack =
+        sideInputStateBackend.getPartitionedState(
+            null,
+            VoidSerializer.INSTANCE,
+            pushedBackDescriptor);
+
+    ReducingState<Long> pushedBackWatermark =
+        sideInputStateBackend.getPartitionedState(
+            null,
+            VoidSerializer.INSTANCE,
+            pushedBackWatermarkDescriptor);
+
+    for (WindowedValue<InputT> pushedBackValue : justPushedBack) {
+      pushedBackWatermark.add(pushedBackValue.getTimestamp().getMillis());
+      pushedBack.add(pushedBackValue);
+    }
+    pushbackDoFnRunner.finishBundle();
+  }
+
+  @Override
+  public final void processElement2(
+      StreamRecord<RawUnionValue> streamRecord) throws Exception {
+    pushbackDoFnRunner.startBundle();
+
+    @SuppressWarnings("unchecked")
+    WindowedValue<Iterable<?>> value =
+        (WindowedValue<Iterable<?>>) streamRecord.getValue().getValue();
+
+    PCollectionView<?> sideInput = sideInputTagMapping.get(streamRecord.getValue().getUnionTag());
+    sideInputHandler.addSideInputValue(sideInput, value);
+
+    ListState<WindowedValue<InputT>> pushedBack =
+        sideInputStateBackend.getPartitionedState(
+            null,
+            VoidSerializer.INSTANCE,
+            pushedBackDescriptor);
+
+    List<WindowedValue<InputT>> newPushedBack = new ArrayList<>();
+    for (WindowedValue<InputT> elem: pushedBack.get()) {
+
+      // we need to set the correct key in case the operator is
+      // a (keyed) window operator
+      setKeyContextElement1(new StreamRecord<>(elem));
+
+      Iterable<WindowedValue<InputT>> justPushedBack =
+          pushbackDoFnRunner.processElementInReadyWindows(elem);
+      Iterables.addAll(newPushedBack, justPushedBack);
+    }
+
+
+    ReducingState<Long> pushedBackWatermark =
+        sideInputStateBackend.getPartitionedState(
+            null,
+            VoidSerializer.INSTANCE,
+            pushedBackWatermarkDescriptor);
+
+    pushedBack.clear();
+    pushedBackWatermark.clear();
+    for (WindowedValue<InputT> pushedBackValue : newPushedBack) {
+      pushedBackWatermark.add(pushedBackValue.getTimestamp().getMillis());
+      pushedBack.add(pushedBackValue);
+    }
+
+    pushbackDoFnRunner.finishBundle();
+
+    // maybe output a new watermark
+    processWatermark1(new Watermark(currentInputWatermark));
   }
 
   @Override
   public void processWatermark(Watermark mark) throws Exception {
-    output.emitWatermark(mark);
+    processWatermark1(mark);
+  }
+
+  @Override
+  public void processWatermark1(Watermark mark) throws Exception {
+    this.currentInputWatermark = mark.getTimestamp();
+    long potentialOutputWatermark =
+        Math.min(getPushbackWatermarkHold(), currentInputWatermark);
+    if (potentialOutputWatermark > currentOutputWatermark) {
+      currentOutputWatermark = potentialOutputWatermark;
+      output.emitWatermark(new Watermark(currentOutputWatermark));
+    }
+  }
+
+  @Override
+  public void processWatermark2(Watermark mark) throws Exception {
+    // ignore watermarks from the side-input input
+  }
+
+  @Override
+  public StreamTaskState snapshotOperatorState(
+      long checkpointId,
+      long timestamp) throws Exception {
+
+    StreamTaskState streamTaskState = super.snapshotOperatorState(checkpointId, timestamp);
+
+    if (sideInputStateBackend != null) {
+      // we have to manually checkpoint the side-input state backend and store
+      // the handle in the "user state" of the task state
+      HashMap<String, KvStateSnapshot<?, ?, ?, ?, ?>> sideInputSnapshot =
+          sideInputStateBackend.snapshotPartitionedState(checkpointId, timestamp);
+
+      if (sideInputSnapshot != null) {
+        @SuppressWarnings("unchecked,rawtypes")
+        StateHandle<Serializable> sideInputStateHandle =
+            (StateHandle) sideInputStateBackend.checkpointStateSerializable(
+                sideInputSnapshot, checkpointId, timestamp);
+
+        streamTaskState.setFunctionState(sideInputStateHandle);
+      }
+    }
+
+    return streamTaskState;
+  }
+
+  @Override
+  public void restoreState(StreamTaskState state, long recoveryTimestamp) throws Exception {
+    super.restoreState(state, recoveryTimestamp);
+
+    @SuppressWarnings("unchecked,rawtypes")
+    StateHandle<HashMap<String, KvStateSnapshot<?, ?, ?, ?, ?>>> sideInputStateHandle =
+        (StateHandle) state.getFunctionState();
+
+    if (sideInputStateHandle != null) {
+      restoredSideInputState = sideInputStateHandle.getState(getUserCodeClassloader());
+    }
   }
 
   /**
@@ -219,6 +457,16 @@ public class DoFnOperator<InputT, FnOutputT, OutputT>
           output.collect(new StreamRecord<>(new RawUnionValue(intTag, value)));
         }
       };
+    }
+  }
+
+  /**
+   * For determining the pushback watermark in a {@link ReducingStateDescriptor}.
+   */
+  private static class LongMinReducer implements ReduceFunction<Long> {
+    @Override
+    public Long reduce(Long a, Long b) throws Exception {
+      return Math.min(a, b);
     }
   }
 
--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/join/RawUnionValue.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/join/RawUnionValue.java
@@ -48,4 +48,29 @@ public class RawUnionValue {
   public String toString() {
     return unionTag + ":" + value;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    RawUnionValue that = (RawUnionValue) o;
+
+    if (unionTag != that.unionTag) {
+      return false;
+    }
+    return value != null ? value.equals(that.value) : that.value == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = unionTag;
+    result = 31 * result + (value != null ? value.hashCode() : 0);
+    return result;
+  }
 }
