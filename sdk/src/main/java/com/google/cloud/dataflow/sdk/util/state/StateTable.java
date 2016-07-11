07795e4f24b93bf94182cf664e93ebdf29c29f1e
Kurt Alfred Kluever <kak@google.com>
2016-07-11 12:42:32 -0400

    Use HashBasedTable.create() instead of Tables.newCustomTable() (#336)
    * Use HashBasedTable.create() instead of Tables.newCustomTable()
    
    * Update StateTable.java

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/util/state/StateTable.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/util/state/StateTable.java
@@ -16,11 +16,9 @@
 package com.google.cloud.dataflow.sdk.util.state;
 
 import com.google.cloud.dataflow.sdk.util.state.StateTag.StateBinder;
-import com.google.common.base.Supplier;
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
-import com.google.common.collect.Tables;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -30,13 +28,7 @@ import java.util.Set;
 public abstract class StateTable<K> {
 
   private final Table<StateNamespace, StateTag<? super K, ?>, State> stateTable =
-      Tables.newCustomTable(new HashMap<StateNamespace, Map<StateTag<? super K, ?>, State>>(),
-          new Supplier<Map<StateTag<? super K, ?>, State>>() {
-        @Override
-        public Map<StateTag<? super K, ?>, State> get() {
-          return new HashMap<>();
-        }
-      });
+      HashBasedTable.create();
 
   /**
    * Gets the {@link State} in the specified {@link StateNamespace} with the specified {@link
