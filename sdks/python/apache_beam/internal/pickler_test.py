cb2afa4e8bbc28303da7d858bf4ba6d16f514ab3
Robert Bradshaw <robertwb@google.com>
2016-07-12 09:50:15 -0700

    Fix lint error.

--- sdks/python/apache_beam/internal/pickler_test.py
+++ sdks/python/apache_beam/internal/pickler_test.py
@@ -41,7 +41,7 @@ class PicklerTest(unittest.TestCase):
         loads(dumps(module_test.get_lambda_with_globals()))('abc def'))
 
   def test_lambda_with_main_globals(self):
-    self.assertEquals(unittest, loads(dumps(lambda : unittest))())
+    self.assertEquals(unittest, loads(dumps(lambda: unittest))())
 
   def test_lambda_with_closure(self):
     """Tests that the closure of a function is preserved."""
