4da482540bbada2d3e411744c3d96f20f7233d35
lukecwik <lcwik@google.com>
2016-07-13 18:00:43 -0400

    Remove reference to non-existent class. (#338)

--- sdk/src/test/java/com/google/cloud/dataflow/sdk/util/ApiSurfaceTest.java
+++ sdk/src/test/java/com/google/cloud/dataflow/sdk/util/ApiSurfaceTest.java
@@ -45,7 +45,6 @@ public class ApiSurfaceTest {
   @Test
   public void testOurApiSurface() throws Exception {
     ApiSurface checkedApiSurface = ApiSurface.getSdkApiSurface()
-      .pruningClassName("com.google.cloud.dataflow.sdk.runners.worker.StateFetcher")
       .pruningClassName("com.google.cloud.dataflow.sdk.util.common.ReflectHelpers")
       .pruningClassName("com.google.cloud.dataflow.sdk.DataflowMatchers")
       .pruningClassName("com.google.cloud.dataflow.sdk.TestUtils")
