3e1c0b5f634179c63c1d829d6aa403c9001792c7
Przemysław Pastuszka <p.pastuszka@ocado.com>
2016-07-19 13:55:50 +0200

    Add missing apache license

--- sdks/java/io/kinesis/src/main/java/org/apache/beam/sdk/io/kinesis/KinesisIO.java
+++ sdks/java/io/kinesis/src/main/java/org/apache/beam/sdk/io/kinesis/KinesisIO.java
@@ -1,4 +1,3 @@
-package org.apache.beam.sdk.io.kinesis;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,6 +15,8 @@ package org.apache.beam.sdk.io.kinesis;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.beam.sdk.io.kinesis;
+
 
 import org.apache.beam.sdk.io.kinesis.client.KinesisClientProvider;
 import org.apache.beam.sdk.io.kinesis.client.response.KinesisRecord;
