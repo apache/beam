45dad4890a6e12833bad834a78e2feca10383302
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2016-08-01 12:54:00 +0200

    #! refactoring after review

--- sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/translation/OperatorTranslator.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/translation/OperatorTranslator.java
@@ -1,8 +1,6 @@
 package cz.seznam.euphoria.flink.translation;
 
 import cz.seznam.euphoria.core.client.operator.Operator;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
 import org.apache.flink.streaming.api.datastream.DataStream;
 
 interface OperatorTranslator<T extends Operator> {
@@ -13,5 +11,5 @@ interface OperatorTranslator<T extends Operator> {
    * @param context Processing context aware of all inputs of given operator
    * @return Output of transformation in Flink API
    */
-  DataStream translate(T operator, TranslationContext context);
+  DataStream<?> translate(T operator, ExecutorContext context);
 }
