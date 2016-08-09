0158513fdc6e2a3b0b76cabfb7e6fd7deb4654e2
Dusan Rychnovsky <dusan.rychnovsky@firma.seznam.cz>
2016-08-09 10:47:44 +0200

    #! Add SequenceFile factories.

---
+++ sdks/java/extensions/euphoria/euphoria-hadoop/src/main/java/cz/seznam/euphoria/hadoop/output/SequenceFileDataSinkFactory.java
@@ -0,0 +1,26 @@
+package cz.seznam.euphoria.hadoop.output;
+
+import cz.seznam.euphoria.hadoop.SerializableWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+public class SequenceFileDataSinkFactory<K extends Writable, V extends Writable> {
+
+  public HadoopDataSink<K, V> get(Class<K> keyCls, Class<V> valueCls, Path path) {
+
+    Configuration conf = new Configuration();
+    conf.set(FileOutputFormat.OUTDIR, path.toString());
+    conf.set(JobContext.OUTPUT_KEY_CLASS, keyCls.getName());
+    conf.set(JobContext.OUTPUT_VALUE_CLASS, valueCls.getName());
+
+    return
+      new HadoopDataSink<>(
+        (Class) SequenceFileOutputFormat.class,
+        new SerializableWritable<>(conf)
+      );
+  }
+}
