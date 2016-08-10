45f417a367a0119500dd0fb8ce47ae809e2105e0
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2016-08-10 19:51:03 +0200

    #! [euphoria-flink] Separation of streaming and batch executors

---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/FlowTranslator.java
@@ -0,0 +1,37 @@
+package cz.seznam.euphoria.flink;
+
+import cz.seznam.euphoria.core.client.flow.Flow;
+import cz.seznam.euphoria.core.client.graph.DAG;
+import cz.seznam.euphoria.core.client.io.DataSink;
+import cz.seznam.euphoria.core.client.operator.Operator;
+import cz.seznam.euphoria.core.executor.FlowUnfolder;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Translates given {@link Flow} into Flink execution environment
+ */
+public abstract class FlowTranslator {
+
+  /**
+   * Translates given flow to Flink execution environment
+   * @return List of {@link DataSink} processed in given flow (leaf nodes)
+   */
+  @SuppressWarnings("unchecked")
+  public abstract List<DataSink<?>> translateInto(Flow flow,
+                                                  ExecutionEnvironment executionEnvironment);
+
+  /**
+   * Converts {@link Flow} to {@link DAG} of Flink specific {@link FlinkOperator}
+   */
+  protected DAG<FlinkOperator<?>> flowToDag(Flow flow) {
+    DAG<Operator<?, ?>> unfolded = FlowUnfolder.unfold(flow, getSupportedOperators());
+    DAG<FlinkOperator<?>> dag = new FlowOptimizer().optimize(unfolded);
+
+    return dag;
+  }
+
+
+  public abstract Set<Class<? extends Operator<?, ?>>> getSupportedOperators();
+}
