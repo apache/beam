574b2db0f423e6c6c39793579f55916350bf285d
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2016-08-15 17:00:04 +0200

    #! [euphoria-flink] batch input format adapter (without data locality)

---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/batch/InputTranslator.java
@@ -0,0 +1,22 @@
+package cz.seznam.euphoria.flink.batch;
+
+import cz.seznam.euphoria.core.client.io.DataSource;
+import cz.seznam.euphoria.core.executor.FlowUnfolder;
+import cz.seznam.euphoria.flink.FlinkOperator;
+import cz.seznam.euphoria.flink.batch.io.DataSourceWrapper;
+import org.apache.flink.api.java.DataSet;
+
+class InputTranslator implements BatchOperatorTranslator<FlowUnfolder.InputOperator> {
+
+  @Override
+  public DataSet translate(FlinkOperator<FlowUnfolder.InputOperator> operator,
+                           BatchExecutorContext context)
+  {
+    // get original datasource from operator
+    DataSource<?> ds = operator.output().getSource();
+
+    return context.getExecutionEnvironment()
+            .createInput(new DataSourceWrapper<>(ds))
+            .setParallelism(operator.getParallelism());
+  }
+}
