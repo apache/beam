29e88e726fe03c39a21d6e66750361e1b1751873
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-08-18 17:36:12 +0200

    #! moved Datum to public API and changed its name

---
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/dataset/WindowedElement.java
@@ -0,0 +1,32 @@
+package cz.seznam.euphoria.core.client.dataset;
+
+/**
+ * A single data element flowing in dataset. Every such element
+ * is associated with a windowing identifier, i.e. a tuple of window group and label.
+ */
+public class WindowedElement<GROUP, LABEL, T> {
+
+  final WindowID<GROUP, LABEL> windowID;
+  final T element;
+
+  public WindowedElement(WindowID<GROUP, LABEL> windowID, T element) {
+    this.windowID = windowID;
+    this.element = element;
+  }
+  
+  public WindowID<GROUP, LABEL> getWindowID() {
+    return windowID;
+  }
+
+  public T get() {
+    return element;
+  }
+
+  @Override
+  public String toString() {
+    return "WindowedElement{" +
+        "windowID=" + windowID +
+        ", element=" + element +
+        '}';
+  }
+}
