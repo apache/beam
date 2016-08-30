fefd2a1094e64f468a42d6090ded7df64dcd28ca
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-08-30 10:56:44 +0200

    #! [euphoria-inmem] Emit watermarks with every input item

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/inmem/WatermarkEmitStrategy.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/inmem/WatermarkEmitStrategy.java
@@ -13,17 +13,11 @@ public interface WatermarkEmitStrategy {
   /** Default strategy used in inmem executor. */
   static class Default implements WatermarkEmitStrategy {
 
-    static final int COUNT_BEFORE_EMIT = 1;
-
     final static ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(1);
-    int itemsSeen = 0;
 
     @Override
     public void emitIfNeeded(Runnable action) {
-      if (++itemsSeen > COUNT_BEFORE_EMIT) {
-        itemsSeen = 0;
-        action.run();
-      }
+      action.run();
     }
 
     @Override
