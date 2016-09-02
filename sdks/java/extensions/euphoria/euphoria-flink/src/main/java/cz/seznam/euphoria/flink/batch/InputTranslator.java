bb28dd256e51b49c4da85a7abcdc66a5935f7202
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-09-02 16:53:40 +0200

    #! [euphoria-flink] Perform conversion to/from window-elements in sink/sources just as in streaming mode

--- sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/batch/InputTranslator.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/batch/InputTranslator.java
@@ -1,13 +1,9 @@
 package cz.seznam.euphoria.flink.batch;
 
-import cz.seznam.euphoria.core.client.dataset.windowing.Batch;
-import cz.seznam.euphoria.core.client.dataset.windowing.WindowID;
-import cz.seznam.euphoria.core.client.dataset.windowing.WindowedElement;
 import cz.seznam.euphoria.core.client.io.DataSource;
 import cz.seznam.euphoria.core.executor.FlowUnfolder;
 import cz.seznam.euphoria.flink.FlinkOperator;
 import cz.seznam.euphoria.flink.batch.io.DataSourceWrapper;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 
 class InputTranslator implements BatchOperatorTranslator<FlowUnfolder.InputOperator> {
@@ -19,13 +15,9 @@ class InputTranslator implements BatchOperatorTranslator<FlowUnfolder.InputOpera
     // get original datasource from operator
     DataSource<?> ds = operator.output().getSource();
 
-    org.apache.flink.api.java.operators.DataSource fds = context
+    return context
         .getExecutionEnvironment()
         .createInput(new DataSourceWrapper<>(ds))
-        .setParallelism(operator.getParallelism());
-    return fds.map((MapFunction) value ->
-        new WindowedElement<>(WindowID.aligned(Batch.Label.get()), value))
-        .returns(WindowedElement.class)
         .setParallelism(operator.getParallelism());
   }
 }
