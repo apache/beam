d1f3cf950aaee716d79516d9c8c893da3caedbfe
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-09-02 09:34:11 +0200

    #! tuned generic windowing for use with sliding windows

--- sdks/java/extensions/euphoria/euphoria-flink/src/test/java/cz/seznam/euphoria/flink/streaming/StreamWindowerTest.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/test/java/cz/seznam/euphoria/flink/streaming/StreamWindowerTest.java
@@ -1,9 +1,11 @@
 
 package cz.seznam.euphoria.flink.streaming;
 
+import cz.seznam.euphoria.core.client.dataset.windowing.Batch;
 import cz.seznam.euphoria.core.client.dataset.windowing.Time;
 import cz.seznam.euphoria.core.client.dataset.windowing.TimeSliding;
 import cz.seznam.euphoria.core.client.dataset.windowing.WindowID;
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowedElement;
 import cz.seznam.euphoria.core.client.dataset.windowing.Windowing;
 import cz.seznam.euphoria.core.client.io.ListDataSink;
 import cz.seznam.euphoria.core.client.io.ListDataSource;
@@ -103,90 +105,92 @@ public class StreamWindowerTest {
 //
 //  }
 
-//  @Test
-//  public void testTimeSlidingWindowing() throws Exception {
-//
-//    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-//
-//    DataStreamSource<Pair<Integer, Integer>> input = env.addSource(new DataSourceWrapper<>(
-//        ListDataSource.unbounded(
-//            Arrays.asList(Pair.of(1, 1), Pair.of(1, 2), Pair.of(2, 3), Pair.of(3, 4)),
-//            Arrays.asList(Pair.of(1, 8), Pair.of(1, 7), Pair.of(2, 6), Pair.of(3, 5)))
-//    ));
-//
-//    WindowedStream<Pair<WindowID<Void, Long>, Pair<Integer, Integer>>, Integer, FlinkWindow> windowed;
-//
-//    windowed = windower.genericWindow(input, i -> i.getSecond() % 2, i -> 2 * i.getSecond(),
-//        TimeSliding.of(Duration.ofSeconds(2), Duration.ofSeconds(1))
-//            .using(p -> 1000L * p.getFirst()));
-//
-//    DataStream<Pair<WindowID<Void, Long>, Pair<Integer, Integer>>> reduced;
-//    reduced = windowed.reduce(
-//        new ReduceFunction<Pair<WindowID<Void, Long>, Pair<Integer, Integer>>>() {
-//          @Override
-//          public Pair<WindowID<Void, Long>, Pair<Integer, Integer>>
-//          reduce(Pair<WindowID<Void, Long>, Pair<Integer, Integer>> a,
-//              Pair<WindowID<Void, Long>, Pair<Integer, Integer>> b) {
-//            return Pair.of(a.getFirst(),
-//                Pair.of(a.getSecond().getFirst(),
-//                    a.getSecond().getSecond() + b.getSecond().getSecond()));
-//          }
-//        });
-//
-//    reduced.addSink(new DataSinkWrapper(new StdoutSink(false, "\n")));
-//
-//    ListDataSink<WindowedPair<Long, Integer, Integer>> sink = ListDataSink.get(1);
-//
-//    reduced
-//        .map(new MapFunction<Pair<WindowID<Void, Long>, Pair<Integer, Integer>>,
-//                WindowedPair<Long, Integer, Integer>>() {
-//              @Override
-//              public WindowedPair<Long, Integer, Integer> map(
-//                  Pair<WindowID<Void, Long>, Pair<Integer, Integer>> t) {
-//                return WindowedPair.of(
-//                    t.getFirst().getLabel(),
-//                    t.getSecond().getFirst(),
-//                    t.getSecond().getSecond());
-//              }
-//            })
-//        .addSink(new DataSinkWrapper(sink));
-//
-//
-//    JobExecutionResult result = env.execute();
-//    assertTrue(result.isJobExecutionResult());
-//    assertEquals(sink.getOutputs().size(), 1);
-//
-//    SortedMap<Long, Set<Pair<Integer, Integer>>> results = new TreeMap<>();
-//    toResultMap(sink, results);
-//
-//    assertEquals("We should have 4 emitted windows", 4, results.size());
-//
-//    // first window: [1, 2, 8, 7] -> [
-//    //                (1, 2), (0, 4), (0, 16), (1, 14)
-//    //               ]
-//    // second window: [1, 2, 8, 7, 3, 6] -> [
-//    //                (1, 2), (0, 4), (0, 16), (1, 14), (1, 6), (0, 12)
-//    //               ]
-//    // third window: [3, 6, 4, 5] -> [
-//    //                (1, 6), (0, 12), (0, 8), (1, 10)
-//    //               ]
-//    // fourth window: [4, 5} -> [
-//    //                (0, 8), (1, 10)
-//    //               ]
-//
-//    Iterator<Map.Entry<Long , Set<Pair<Integer, Integer>>>> iterator
-//        = results.entrySet().iterator();
-//    Set<Pair<Integer, Integer>> first = iterator.next().getValue();
-//    Set<Pair<Integer, Integer>> second = iterator.next().getValue();
-//    Set<Pair<Integer, Integer>> third = iterator.next().getValue();
-//    Set<Pair<Integer, Integer>> fourth = iterator.next().getValue();
-//
-//    assertEquals(Sets.newHashSet(Pair.of(1, 16), Pair.of(0, 20)), first);
-//    assertEquals(Sets.newHashSet(Pair.of(1, 22), Pair.of(0, 32)), second);
-//    assertEquals(Sets.newHashSet(Pair.of(1, 16), Pair.of(0, 20)), third);
-//    assertEquals(Sets.newHashSet(Pair.of(1, 10), Pair.of(0, 8)), fourth);
-//
-//  }
+  @Test
+  public void testTimeSlidingWindowing() throws Exception {
+
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+    DataStream<WindowedElement<?, ?, Pair<Integer, Integer>>> input
+        = env.addSource(new DataSourceWrapper(ListDataSource.unbounded(
+            Arrays.asList(Pair.of(1, 1), Pair.of(1, 2), Pair.of(2, 3), Pair.of(3, 4)),
+            Arrays.asList(Pair.of(1, 8), Pair.of(1, 7), Pair.of(2, 6), Pair.of(3, 5)))
+    ));
+
+    WindowedStream<WindowedElement<Void, Long, Pair<Integer, Integer>>, Integer, FlinkWindow> windowed;
+    windowed = windower.genericWindow(
+        input, (Pair<Integer, Integer> i) -> i.getSecond() % 2,
+        (Pair<Integer, Integer> i) -> 2 * i.getSecond(),
+        TimeSliding.of(Duration.ofSeconds(2), Duration.ofSeconds(1))
+            .using((Pair<Integer, Integer> p) -> 1000L * p.getFirst()));
+
+    DataStream<WindowedElement<Void, Long, Pair<Integer, Integer>>> reduced;
+    reduced = windowed.reduce(
+        new ReduceFunction<WindowedElement<Void, Long, Pair<Integer, Integer>>>() {
+          @Override
+          public WindowedElement<Void, Long, Pair<Integer, Integer>>
+              reduce(WindowedElement<Void, Long, Pair<Integer, Integer>> a,
+                  WindowedElement<Void, Long, Pair<Integer, Integer>> b) {
+                return new WindowedElement(a.getWindowID(), Pair.of(
+                    a.get().getFirst(),
+                    a.get().getSecond() + b.get().getSecond()));
+              }
+        });
+
+    reduced = reduced.map(
+        new MapFunction<
+            WindowedElement<Void, Long, Pair<Integer, Integer>>,
+            WindowedElement<Void, Long, Pair<Integer, Integer>>>() {
+      @Override
+      public WindowedElement<Void, Long, Pair<Integer, Integer>> map(
+          WindowedElement<Void, Long, Pair<Integer, Integer>> t) throws Exception {
+        return new WindowedElement(t.getWindowID(),
+            WindowedPair.of(t.getWindowID().getLabel(),
+                t.get().getFirst(), t.get().getSecond()));
+      }});
+
+    reduced.addSink(new DataSinkWrapper(new StdoutSink(false, "\n")));
+
+    ListDataSink<WindowedPair<Long, Integer, Integer>> sink = ListDataSink.get(1);
+
+    reduced
+        .addSink(new DataSinkWrapper(sink));
+
+
+    JobExecutionResult result = env.execute();
+    assertTrue(result.isJobExecutionResult());
+    assertEquals(sink.getOutputs().size(), 1);
+
+    SortedMap<Long, Set<Pair<Integer, Integer>>> results = new TreeMap<>();
+    toResultMap(sink, results);
+
+    assertEquals("We should have 4 emitted windows", 4, results.size());
+
+    // first window: [1, 2, 8, 7] -> [
+    //                (1, 2), (0, 4), (0, 16), (1, 14)
+    //               ]
+    // second window: [1, 2, 8, 7, 3, 6] -> [
+    //                (1, 2), (0, 4), (0, 16), (1, 14), (1, 6), (0, 12)
+    //               ]
+    // third window: [3, 6, 4, 5] -> [
+    //                (1, 6), (0, 12), (0, 8), (1, 10)
+    //               ]
+    // fourth window: [4, 5} -> [
+    //                (0, 8), (1, 10)
+    //               ]
+
+    Iterator<Map.Entry<Long , Set<Pair<Integer, Integer>>>> iterator
+        = results.entrySet().iterator();
+    Set<Pair<Integer, Integer>> first = iterator.next().getValue();
+    Set<Pair<Integer, Integer>> second = iterator.next().getValue();
+    Set<Pair<Integer, Integer>> third = iterator.next().getValue();
+    Set<Pair<Integer, Integer>> fourth = iterator.next().getValue();
+
+    assertEquals(Sets.newHashSet(Pair.of(1, 16), Pair.of(0, 20)), first);
+    assertEquals(Sets.newHashSet(Pair.of(1, 22), Pair.of(0, 32)), second);
+    assertEquals(Sets.newHashSet(Pair.of(1, 16), Pair.of(0, 20)), third);
+    assertEquals(Sets.newHashSet(Pair.of(1, 10), Pair.of(0, 8)), fourth);
+
+  }
 
   private <LABEL> void toResultMap(
       ListDataSink<WindowedPair<LABEL, Integer, Integer>> sink,
