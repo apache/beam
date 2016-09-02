9d80dcd8091912b411e50005b6b51201fa9442b1
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-09-02 16:08:01 +0200

    #! add support for state backend to flink

---
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/operator/OptionalMethodBuilder.java
@@ -0,0 +1,20 @@
+
+package cz.seznam.euphoria.core.client.operator;
+
+import cz.seznam.euphoria.core.client.functional.UnaryFunction;
+import java.util.Objects;
+
+/**
+ * Class to be extended by operator builders that want to make use
+ * of `applyIf` call.
+ * @param BUILDER the class of the builder that extends this class
+ */
+public abstract class OptionalMethodBuilder<BUILDER> {
+
+  @SuppressWarnings("unchecked")
+  public BUILDER applyIf(boolean cond, UnaryFunction<BUILDER, BUILDER> apply) {
+    Objects.requireNonNull(apply);
+    return cond ? apply.apply((BUILDER) this) : (BUILDER) this;
+  }
+
+}
