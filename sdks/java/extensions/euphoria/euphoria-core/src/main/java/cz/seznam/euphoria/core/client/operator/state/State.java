52eb3f0f0f5e527d1434ca65327f026c17dce3dc
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-09-20 13:32:39 +0200

    #! code polishing after review

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/operator/state/State.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/client/operator/state/State.java
@@ -2,14 +2,18 @@
 package cz.seznam.euphoria.core.client.operator.state;
 
 import cz.seznam.euphoria.core.client.io.Collector;
+import cz.seznam.euphoria.core.client.operator.Operator;
 
 import java.io.Closeable;
+import java.io.Serializable;
 
 /**
  * A state for stateful operations.
  */
-public abstract class State<IN, OUT> implements Closeable {
+public abstract class State<IN, OUT> implements Closeable, Serializable {
 
+  /** Operator associated with this state. */
+  private final Operator<?, ?> associatedOperator;
   /** Collector of output of this state. */
   private final Collector<OUT> collector;
   /** Provider of state storage. */
@@ -24,9 +28,18 @@ public abstract class State<IN, OUT> implements Closeable {
    */
   public abstract void flush();
 
-  protected State(Collector<OUT> collector, StateStorageProvider storageProvider) {
+  protected State(
+      Operator<?, ?> associatedOperator,
+      Collector<OUT> collector,
+      StateStorageProvider storageProvider) {
+    
+    this.associatedOperator = associatedOperator;
     this.collector = collector;
     this.storageProvider = storageProvider;
+  }
+
+  public Operator<?, ?> getAssociatedOperator() {
+    return associatedOperator;
   }
 
   public Collector<OUT> getCollector() {
@@ -36,7 +49,6 @@ public abstract class State<IN, OUT> implements Closeable {
   public StateStorageProvider getStorageProvider() {
     return storageProvider;
   }
-
 
   /**
    * Closes this state. Invoked after {@link #flush()} and before
--- sdks/java/extensions/euphoria/euphoria-flink/src/test/java/cz/seznam/euphoria/flink/batch/BatchStateStorageProviderTest.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/test/java/cz/seznam/euphoria/flink/batch/BatchStateStorageProviderTest.java
@@ -1,18 +1,23 @@
 
 package cz.seznam.euphoria.flink.batch;
 
+import cz.seznam.euphoria.core.client.operator.Operator;
 import cz.seznam.euphoria.core.client.operator.state.ListStateStorage;
+import cz.seznam.euphoria.core.client.operator.state.State;
 import cz.seznam.euphoria.core.client.operator.state.ValueStateStorage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.shaded.com.google.common.collect.Lists;
+import org.junit.After;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test suite for {@code BatchStateStorageProvider}.
@@ -20,17 +25,23 @@ import static org.junit.Assert.*;
 public class BatchStateStorageProviderTest {
 
   final int MAX_MEMORY_ELEMENTS = 100;
+
   ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
   BatchStateStorageProvider provider;
+  State state;
 
   @Before
   public void setUp() {
     provider = new BatchStateStorageProvider(MAX_MEMORY_ELEMENTS, env);
+    state = mock(State.class);
+    when(state.getAssociatedOperator()).thenReturn(mock(Operator.class));
   }
-
+  
   @Test
+  @SuppressWarnings("unchecked")
   public void testSimpleAddValue() {
-    ValueStateStorage<Integer> storage = provider.getValueStorageFor(Integer.class);
+    ValueStateStorage<Integer> storage = provider.getValueStorage(
+        state, Integer.class);
     assertNull(storage.get());
     storage.set(1);
     assertEquals(1, (int) storage.get());
@@ -38,7 +49,8 @@ public class BatchStateStorageProviderTest {
 
   @Test
   public void testSimpleListAdd() {
-    ListStateStorage<Integer> storage = provider.getListStorageFor(Integer.class);
+    ListStateStorage<Integer> storage = provider.getListStorage(
+        state, Integer.class);
     List<Integer> list = Lists.newArrayList(storage.get());
     assertTrue(list.isEmpty());
 
@@ -51,7 +63,8 @@ public class BatchStateStorageProviderTest {
   @Test
   public void testMemorySpill() {
     List<Integer> data = new ArrayList<>();
-    ListStateStorage<Integer> storage = provider.getListStorageFor(Integer.class);
+    ListStateStorage<Integer> storage = provider.getListStorage(
+        state, Integer.class);
 
     for (int i = 0; i < 1_000_003; i++) {
       data.add(i);
@@ -61,6 +74,8 @@ public class BatchStateStorageProviderTest {
     List<Integer> list = Lists.newArrayList(storage.get());
     assertEquals(data.size(), list.size());
     assertEquals(data, list);
+
+    storage.clear();
   }
 
 }
