eaa6b0caefec4ce20512692df26d634c33c271c0
Sela <ansela@paypal.com>
2016-09-20 22:28:46 +0300

    Add checkpoint duration option.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/SparkPipelineOptions.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/SparkPipelineOptions.java
@@ -50,22 +50,29 @@ public interface SparkPipelineOptions extends PipelineOptions, StreamingOptions,
 
   @Description("A checkpoint directory for streaming resilience, ignored in batch. "
       + "For durability, a reliable filesystem such as HDFS/S3/GS is necessary.")
-  @Default.InstanceFactory(CheckpointDirFactory.class)
+  @Default.InstanceFactory(TmpCheckpointDirFactory.class)
   String getCheckpointDir();
   void setCheckpointDir(String checkpointDir);
 
   /**
-   * Returns the default checkpoint directory of /tmp/${app.name}-${start_time_millis}.
+   * Returns the default checkpoint directory of /tmp/${job.name}.
    * For testing purposes only. Production applications should use a reliable
    * filesystem such as HDFS/S3/GS.
    */
-  public static class CheckpointDirFactory implements DefaultValueFactory<String> {
+  static class TmpCheckpointDirFactory implements DefaultValueFactory<String> {
     @Override
     public String create(PipelineOptions options) {
       SparkPipelineOptions sparkPipelineOptions = options.as(SparkPipelineOptions.class);
-      return "/tmp/" + sparkPipelineOptions.getAppName() + "-" + System.currentTimeMillis();
+      return "file:///tmp/" + sparkPipelineOptions.getJobName();
     }
   }
+
+  @Description("The period to checkpoint (in Millis). If not set, Spark will default "
+      + "to Max(slideDuration, Seconds(10)). This PipelineOptions default (-1) will end-up "
+          + "with the described Spark default.")
+  @Default.Long(-1)
+  Long getCheckpointDurationMillis();
+  void setCheckpointDurationMillis(Long durationMillis);
 
   @Description("Enable/disable sending aggregator values to Spark's metric sinks")
   @Default.Boolean(true)
