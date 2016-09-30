6c0fc49394abd4a42b225ad71771fba368ee4a8a
Dan Halperin <dhalperi@google.com>
2016-09-30 15:51:30 -0700

    Version management: use 1.8.0 worker container

--- sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/DataflowPipelineRunner.java
@@ -221,9 +221,9 @@ public class DataflowPipelineRunner extends PipelineRunner<DataflowPipelineJob> 
   // Default Docker container images that execute Dataflow worker harness, residing in Google
   // Container Registry, separately for Batch and Streaming.
   public static final String BATCH_WORKER_HARNESS_CONTAINER_IMAGE
-      = "dataflow.gcr.io/v1beta3/java-batch:1.7.0";
+      = "dataflow.gcr.io/v1beta3/java-batch:1.8.0";
   public static final String STREAMING_WORKER_HARNESS_CONTAINER_IMAGE
-      = "dataflow.gcr.io/v1beta3/java-streaming:1.7.0";
+      = "dataflow.gcr.io/v1beta3/java-streaming:1.8.0";
 
   // The limit of CreateJob request size.
   private static final int CREATE_JOB_REQUEST_LIMIT_BYTES = 10 * 1024 * 1024;
