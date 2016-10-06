8fbab4ef9f88c412cb5b40f67f5ac7d3c47c2dea
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-10-06 11:25:13 +0200

    #! [euphoria-core] Provide Context#getWindow

--- sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/inmem/WindowedElementCollector.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/main/java/cz/seznam/euphoria/core/executor/inmem/WindowedElementCollector.java
@@ -6,20 +6,25 @@ import cz.seznam.euphoria.core.client.io.Context;
 import java.util.Objects;
 
 class WindowedElementCollector<T> implements Context<T> {
-  private final Context<Datum> wrap;
+  private final Collector<Datum> wrap;
 
   protected WindowID<Object> windowID;
 
-  WindowedElementCollector(Context<Datum> wrap) {
+  WindowedElementCollector(Collector<Datum> wrap) {
     this.wrap = Objects.requireNonNull(wrap);
-  }
-
-  void assignWindowing(WindowID<Object> windowID) {
-    this.windowID = windowID;
   }
 
   @Override
   public void collect(T elem) {
     wrap.collect(Datum.of(windowID, elem));
+  }
+
+  void setWindow(WindowID<Object> windowID) {
+    this.windowID = windowID;
+  }
+
+  @Override
+  public Object getWindow() {
+    return windowID.getLabel();
   }
 }
