429e0f5192a1f329925465a6154b6ccfd13dcd1e
markon <marcusbu@gmail.com>
2016-10-09 14:19:06 +0200

    Replaced assertions with warnings.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -21,6 +21,8 @@ from distutils.version import StrictVersion
 
 import os
 import platform
+import warnings
+
 import setuptools
 
 from pkg_resources import get_distribution, DistributionNotFound
@@ -46,19 +48,28 @@ TBD
 
 REQUIRED_PIP_VERSION = '7.0.0'
 _PIP_VERSION = get_distribution('pip').version
-assert StrictVersion(_PIP_VERSION) >= StrictVersion(REQUIRED_PIP_VERSION), \
-  "This SDK requires 'pip' >= {0}".format(REQUIRED_PIP_VERSION)
+if StrictVersion(_PIP_VERSION) <= StrictVersion(REQUIRED_PIP_VERSION):
+  warnings.warn(
+      "You are using version {0} of pip. " \
+      "However, version {1} is recommended.".format(
+          _PIP_VERSION, REQUIRED_PIP_VERSION
+      )
+  )
+
 
 REQUIRED_CYTHON_VERSION = '0.23.2'
 try:
   _CYTHON_VERSION = get_distribution('cython').version
-  assert StrictVersion(_CYTHON_VERSION) >= \
-         StrictVersion(REQUIRED_CYTHON_VERSION), \
-  "This SDK requires 'cython' >= {0}".format(REQUIRED_CYTHON_VERSION)
+  if StrictVersion(_CYTHON_VERSION) <= StrictVersion(REQUIRED_CYTHON_VERSION):
+    warnings.warn(
+        "You are using version {0} of cython. " \
+        "However, version {1} is recommended.".format(
+            _CYTHON_VERSION, REQUIRED_CYTHON_VERSION
+        )
+    )
 except DistributionNotFound:
   # do nothing if Cython is not installed
   pass
-
 
 # Currently all compiled modules are optional  (for performance only).
 if platform.system() == 'Windows':
