ddf57e5486e6271d20922af5be1296d7937ce077
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-10-10 13:17:16 +0200

    #! [euphoria-flink] Fix windowing for batched based R(S)BK translators

---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/batch/AttachedWindowing.java
@@ -0,0 +1,41 @@
+package cz.seznam.euphoria.flink.batch;
+
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowContext;
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowID;
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowedElement;
+import cz.seznam.euphoria.core.client.dataset.windowing.Windowing;
+
+import java.util.Collections;
+import java.util.Set;
+
+class AttachedWindowing
+    implements Windowing<Object, Object, AttachedWindowing.AttachedWindowContext > {
+
+  static class AttachedWindowContext extends WindowContext<Object> {
+
+    AttachedWindowContext(Object label) {
+      super(new WindowID<>(label));
+    }
+
+    AttachedWindowContext(WindowID<Object> id) {
+      super(id);
+    }
+
+    @Override
+    public String toString() {
+      return "AttachedWindowContext(" + getWindowID() + ")";
+    }
+  }
+
+  static final AttachedWindowing INSTANCE = new AttachedWindowing();
+
+  @Override
+  public Set<WindowID<Object>> assignWindowsToElement(WindowedElement<?, Object> input) {
+    return Collections.singleton((WindowID) input.getWindowID());
+  }
+
+  @Override
+  public AttachedWindowContext createWindowContext(WindowID<Object> id) {
+    return new AttachedWindowContext(id);
+  }
+}
