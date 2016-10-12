f4119f489a6a7bf7f7347f6842800fc6a6dc5b10
bchambers <bchambers@google.com>
2016-10-12 10:29:50 -0700

    Initial Metrics API for Beam Java
    This includes a simple Counter metric and a Distribution metric that
    reports the SUM, COUNT, MIN, MAX and MEAN of the reported values.
    
    The API is labeled @Experimental since metrics will only be reported
    and queryable with the DirectRunner, and the API may change as it is
    implemented on other runners.

---
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/Metrics.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.metrics;
+
+import org.apache.beam.sdk.annotations.Experimental;
+import org.apache.beam.sdk.annotations.Experimental.Kind;
+
+/**
+ * The <code>Metrics</code> is a utility class for producing various kinds of metrics for
+ * reporting properties of an executing pipeline.
+ */
+@Experimental(Kind.METRICS)
+public class Metrics {
+
+  private Metrics() {}
+
+  /**
+   * Create a metric that can be incremented and decremented, and is aggregated by taking the sum.
+   */
+  public static Counter counter(String namespace, String name) {
+    return new DelegatingCounter(MetricName.named(namespace, name));
+  }
+
+  /**
+   * Create a metric that can be incremented and decremented, and is aggregated by taking the sum.
+   */
+  public static Counter counter(Class<?> namespace, String name) {
+    return new DelegatingCounter(MetricName.named(namespace, name));
+  }
+
+  /**
+   * Create a metric that records various statistics about the distribution of reported values.
+   */
+  public static Distribution distribution(String namespace, String name) {
+    return new DelegatingDistribution(MetricName.named(namespace, name));
+  }
+
+  /**
+   * Create a metric that records various statistics about the distribution of reported values.
+   */
+  public static Distribution distribution(Class<?> namespace, String name) {
+    return new DelegatingDistribution(MetricName.named(namespace, name));
+  }
+
+  /** Implementation of {@link Counter} that delegates to the instance for the current context. */
+  private static class DelegatingCounter implements Counter {
+    private final MetricName name;
+
+    private DelegatingCounter(MetricName name) {
+      this.name = name;
+    }
+
+    /** Increment the counter. */
+    @Override public void inc() {
+      inc(1);
+    }
+
+    /** Increment the counter by the given amount. */
+    @Override public void inc(long n) {
+      MetricsContainer container = MetricsEnvironment.getCurrentContainer();
+      if (container != null) {
+        container.getCounter(name).inc(n);
+      }
+    }
+
+    /* Decrement the counter. */
+    @Override public void dec() {
+      inc(-1);
+    }
+
+    /* Decrement the counter by the given amount. */
+    @Override public void dec(long n) {
+      inc(-1 * n);
+    }
+  }
+
+  /**
+   * Implementation of {@link Distribution} that delegates to the instance for the current context.
+   */
+  private static class DelegatingDistribution implements Distribution {
+    private final MetricName name;
+
+    private DelegatingDistribution(MetricName name) {
+      this.name = name;
+    }
+
+    @Override
+    public void update(long value) {
+      MetricsContainer container = MetricsEnvironment.getCurrentContainer();
+      if (container != null) {
+        container.getDistribution(name).update(value);
+      }
+    }
+  }
+}
