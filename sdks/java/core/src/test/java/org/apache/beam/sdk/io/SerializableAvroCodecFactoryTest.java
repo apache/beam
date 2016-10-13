d5a333fa299d2822079e9c2b126324d5562e7c04
Dan Halperin <dhalperi@google.com>
2016-10-12 18:10:25 -0700

    AvroIO.Write: minor doc and test fixes
    * Document the defualt codec.
    * Minor docfix in the SerializableAvroCodecFactory.
    * Swap the order of assertEquals in tests to match the expected pattern (expected, actual).

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/SerializableAvroCodecFactoryTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/SerializableAvroCodecFactoryTest.java
@@ -50,7 +50,7 @@ public class SerializableAvroCodecFactoryTest {
       SerializableAvroCodecFactory codecFactory = new SerializableAvroCodecFactory(
           CodecFactory.fromString(codec));
 
-      assertEquals(codecFactory.getCodec().toString(), (CodecFactory.fromString(codec).toString()));
+      assertEquals((CodecFactory.fromString(codec).toString()), codecFactory.getCodec().toString());
     }
   }
 
@@ -62,7 +62,7 @@ public class SerializableAvroCodecFactoryTest {
 
       SerializableAvroCodecFactory serdeC = SerializableUtils.clone(codecFactory);
 
-      assertEquals(serdeC.getCodec().toString(), CodecFactory.fromString(codec).toString());
+      assertEquals(CodecFactory.fromString(codec).toString(), serdeC.getCodec().toString());
     }
   }
 
@@ -74,7 +74,7 @@ public class SerializableAvroCodecFactoryTest {
 
       SerializableAvroCodecFactory serdeC = SerializableUtils.clone(codecFactory);
 
-      assertEquals(serdeC.getCodec().toString(), CodecFactory.deflateCodec(i).toString());
+      assertEquals(CodecFactory.deflateCodec(i).toString(), serdeC.getCodec().toString());
     }
   }
 
@@ -86,7 +86,7 @@ public class SerializableAvroCodecFactoryTest {
 
       SerializableAvroCodecFactory serdeC = SerializableUtils.clone(codecFactory);
 
-      assertEquals(serdeC.getCodec().toString(), CodecFactory.xzCodec(i).toString());
+      assertEquals(CodecFactory.xzCodec(i).toString(), serdeC.getCodec().toString());
     }
   }
 
