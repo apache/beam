34f45f66a1fc695fc4cd5eb5d20679be7c89ad9b
Geetha Bijjam <venbijjam@users.noreply.github.com>
2016-10-24 09:24:47 -0500

    Update multiple_output_pardo_test.py

--- sdks/python/apache_beam/examples/cookbook/multiple_output_pardo_test.py
+++ sdks/python/apache_beam/examples/cookbook/multiple_output_pardo_test.py
@@ -32,28 +32,28 @@ class MultipleOutputParDoTest(unittest.TestCase):
   SAMPLE_TEXT_Iterable = [SAMPLE_TEXT]
   EXPECTED_SHORT_WORDS = ['A: 2', 'new: 2']
   EXP_WORDS = ['point: 1', 'whole: 1', 'world: 1']
-  
+
   def test_multiple_output_pardo(self):
     p = beam.Pipeline('DirectPipelineRunner')
     sample_text = p | beam.Create(self.SAMPLE_TEXT_Iterable)
-    res = (sample_text 
+    res = (sample_text
            | beam.ParDo(multiple_output_pardo.SplitLinesToWordsFn())
            .with_outputs('tag_short_words', 'tag_character_count', main='words'))
-    res_cnt = (res.tag_character_count 
+    res_cnt = (res.tag_character_count
                | 'pair_with_key' >> beam.Map(lambda x: ('chars_temp_key', x))
                | beam.GroupByKey()
                | 'count chars' >> beam.Map(lambda (_, counts): sum(counts)))
-    res_words = (res.words 
+    res_words = (res.words
                  | 'count words' >> multiple_output_pardo.CountWords())
-    res_sh_wrd = (res.tag_short_words 
+    res_sh_wrd = (res.tag_short_words
                   | 'count short words' >> multiple_output_pardo.CountWords())
     beam.assert_that(res_words, beam.equal_to(self.EXP_WORDS))
     beam.assert_that(res_sh_wrd, beam.equal_to(self.EXPECTED_SHORT_WORDS),
-                     label='assert:tag_short_words')   
+                     label='assert:tag_short_words')
     beam.assert_that(res_cnt, beam.equal_to([self.text_len]),
                      label='assert:tag_character_count')
     p.run()
-    
+
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
   unittest.main()
