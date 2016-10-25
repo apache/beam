ab6bc9b98ec24de44d2e35a1d93bd55e3cd3e257
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2016-10-25 16:49:33 +0200

    #! [euphoria-flink] RBK: Support for merging windows (streaming)

--- sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/storage/Descriptors.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/storage/Descriptors.java
@@ -3,10 +3,19 @@ package cz.seznam.euphoria.flink.storage;
 import cz.seznam.euphoria.core.client.operator.state.ListStorageDescriptor;
 import cz.seznam.euphoria.core.client.operator.state.ValueStorageDescriptor;
 import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.ReducingStateDescriptor;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 
 /** Helper around storage descriptors. */
 public class Descriptors {
+
+  public static <T> ReducingStateDescriptor<T>
+  from(ValueStorageDescriptor.MergingValueStorageDescriptor<T> descriptor) {
+    return new ReducingStateDescriptor<T>(
+        descriptor.getName(),
+        new ReducingMerger<T>(descriptor.getValueMerger()),
+        descriptor.getValueClass());
+  }
 
   /** Converts the given euphoria descriptor into its flink equivalent. */
   public static <T> ValueStateDescriptor<T> from(ValueStorageDescriptor<T> descriptor) {
