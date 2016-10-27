70ca711f129dca372f017fe620077fe37df714db
Geetha Bijjam <venbijjam@users.noreply.github.com>
2016-10-27 09:23:54 -0500

    Update group_with_coder_test.py

--- sdks/python/apache_beam/examples/cookbook/group_with_coder_test.py
+++ sdks/python/apache_beam/examples/cookbook/group_with_coder_test.py
@@ -18,9 +18,10 @@
 """Test for the custom coders example."""
 
 import logging
-import tempfile
 import unittest
 
+import apache_beam as beam
+from apache_beam import coders
 from apache_beam.examples.cookbook import group_with_coder
 
 
@@ -36,53 +37,49 @@ class GroupWithCoderTest(unittest.TestCase):
       'joe,20', 'fred,6', 'ann,5',
       'joe,30', 'ann,10', 'mary,1']
 
-  def create_temp_file(self, records):
-    with tempfile.NamedTemporaryFile(delete=False) as f:
-      for record in records:
-        f.write('%s\n' % record)
-      return f.name
-
   def test_basics_with_type_check(self):
     # Run the workflow with --pipeline_type_check option. This will make sure
     # the typehints associated with all transforms will have non-default values
     # and therefore any custom coders will be used. In our case we want to make
     # sure the coder for the Player class will be used.
-    temp_path = self.create_temp_file(self.SAMPLE_RECORDS)
-    group_with_coder.run([
-        '--pipeline_type_check',
-        '--input=%s*' % temp_path,
-        '--output=%s.result' % temp_path])
-    # Parse result file and compare.
-    results = []
-    with open(temp_path + '.result-00000-of-00001') as result_file:
-      for line in result_file:
-        name, points = line.split(',')
-        results.append((name, int(points)))
-      logging.info('result: %s', results)
-    self.assertEqual(
-        sorted(results),
-        sorted([('x:ann', 15), ('x:fred', 9), ('x:joe', 60), ('x:mary', 8)]))
+
+    coders.registry.register_coder(group_with_coder.Player,
+                                   group_with_coder.PlayerCoder)
+
+    p = beam.Pipeline('DirectPipelineRunner')
+
+    results = (p
+               | 'create_sample' >>  beam.Create([self.SAMPLE_RECORDS])
+               | beam.Map(group_with_coder.get_players)
+               | beam.CombinePerKey(sum)
+               | beam.Map(lambda (k, v): '%s,%d' % (k.name, v)))
+
+    beam.assert_that(results,
+                     beam.equal_to([('x:ann', 15),
+                                    ('x:fred', 9),
+                                    ('x:joe', 60),
+                                    ('x:mary', 8)]),
+                     label='assert:tag_basics_with_type_check')
 
   def test_basics_without_type_check(self):
     # Run the workflow without --pipeline_type_check option. This will make sure
     # the typehints associated with all transforms will have default values and
     # therefore any custom coders will not be used. The default coder (pickler)
     # will be used instead.
-    temp_path = self.create_temp_file(self.SAMPLE_RECORDS)
-    group_with_coder.run([
-        '--no_pipeline_type_check',
-        '--input=%s*' % temp_path,
-        '--output=%s.result' % temp_path])
-    # Parse result file and compare.
-    results = []
-    with open(temp_path + '.result-00000-of-00001') as result_file:
-      for line in result_file:
-        name, points = line.split(',')
-        results.append((name, int(points)))
-      logging.info('result: %s', results)
-    self.assertEqual(
-        sorted(results),
-        sorted([('ann', 15), ('fred', 9), ('joe', 60), ('mary', 8)]))
+    p = beam.Pipeline('DirectPipelineRunner')
+
+    results_1 = (p
+                 | 'create_sample_1' >>  beam.Create([self.SAMPLE_RECORDS])
+                 | beam.Map(group_with_coder.get_players)
+                 | beam.CombinePerKey(sum)
+                 | beam.Map(lambda (k, v): '%s,%d' % (k.name, v)))
+
+    beam.assert_that(results_1,
+                     beam.equal_to([('x:ann', 15),
+                                    ('x:fred', 9),
+                                    ('x:joe', 60),
+                                    ('x:mary', 8)]),
+                     label='assert:tag_basics_without_type_check')
 
 
 if __name__ == '__main__':
