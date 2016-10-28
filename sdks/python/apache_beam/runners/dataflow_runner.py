0a339653b0c7f6225a2b12e6c0988af8ad758098
Sourabh Bajaj <sourabhbajaj@google.com>
2016-10-28 13:59:23 -0700

    BEAM-873 Support for BigQuery 2 SQL

--- sdks/python/apache_beam/runners/dataflow_runner.py
+++ sdks/python/apache_beam/runners/dataflow_runner.py
@@ -528,6 +528,8 @@ class DataflowPipelineRunner(PipelineRunner):
                             transform.source.table_reference.projectId)
       elif transform.source.query is not None:
         step.add_property(PropertyNames.BIGQUERY_QUERY, transform.source.query)
+        step.add_property(PropertyNames.BIGQUERY_USE_LEGACY_SQL,
+                          transform.source.use_legacy_sql)
       else:
         raise ValueError('BigQuery source %r must specify either a table or'
                          ' a query',
