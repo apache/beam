f92c39f14b49556f8e67c22c408520af0c204cce
Luke Cwik <lcwik@google.com>
2016-10-31 13:24:49 -0700

    fixup! minor whitespace clean up.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroCoder.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroCoder.java
@@ -182,9 +182,11 @@ public class AvroCoder<T> extends StandardCoder<T> {
       extends ThreadLocal<T> implements Serializable {
     private void writeObject(java.io.ObjectOutputStream out) throws IOException {
     }
+
     private void readObject(java.io.ObjectInputStream in)
         throws IOException, ClassNotFoundException {
     }
+
     private void readObjectNoData() throws ObjectStreamException {
     }
   }
