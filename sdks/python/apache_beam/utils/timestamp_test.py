8fd651f05d1e769000fa093e6ab626ef2ac87dec
Robert Bradshaw <robertwb@gmail.com>
2016-10-31 16:47:24 -0700

    Move Timestamp and related classes into apache_beam/utils.

--- sdks/python/apache_beam/utils/timestamp_test.py
+++ sdks/python/apache_beam/utils/timestamp_test.py
@@ -21,8 +21,8 @@ from __future__ import absolute_import
 
 import unittest
 
-from apache_beam.transforms.timeutil import Duration
-from apache_beam.transforms.timeutil import Timestamp
+from apache_beam.utils.timestamp import Duration
+from apache_beam.utils.timestamp import Timestamp
 
 
 class TimestampTest(unittest.TestCase):
