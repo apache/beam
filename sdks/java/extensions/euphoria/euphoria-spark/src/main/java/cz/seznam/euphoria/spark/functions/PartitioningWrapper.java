2efb58dfd6753d7c1ea8786cde822d3b8cef28cb
Jan Lukavsky <jan.lukavsky@firma.seznam.cz>
2016-11-02 16:15:17 +0100

    #! [euphoria-ios] added support for spark, finished presentation

--- sdks/java/extensions/euphoria/euphoria-spark/src/main/java/cz/seznam/euphoria/spark/functions/PartitioningWrapper.java
+++ sdks/java/extensions/euphoria/euphoria-spark/src/main/java/cz/seznam/euphoria/spark/functions/PartitioningWrapper.java
@@ -1,29 +1,19 @@
 package cz.seznam.euphoria.spark.functions;
 
 import cz.seznam.euphoria.core.client.dataset.Partitioning;
-import cz.seznam.euphoria.core.client.functional.UnaryFunction;
+import cz.seznam.euphoria.spark.KeyedWindow;
 import org.apache.spark.Partitioner;
-
-import java.util.Objects;
 
 /**
  * Wraps Euphoria {@link Partitioning} to be used in Spark API
  */
 public class PartitioningWrapper extends Partitioner {
 
+  private static final int bitShift = Integer.BYTES * 4;
   private final Partitioning partitioning;
-
-  /** Function extracting key that will be used in the partitioner */
-  private final UnaryFunction partitionBy;
 
   public PartitioningWrapper(Partitioning partitioning) {
     this.partitioning = partitioning;
-    this.partitionBy = e -> e; // identity by default
-  }
-
-  public PartitioningWrapper(Partitioning partitioning, UnaryFunction partitionBy) {
-    this.partitioning = Objects.requireNonNull(partitioning);
-    this.partitionBy = Objects.requireNonNull(partitionBy);
   }
 
   @Override
@@ -34,7 +24,10 @@ public class PartitioningWrapper extends Partitioner {
   @Override
   @SuppressWarnings("unchecked")
   public int getPartition(Object el) {
-    return partitioning.getPartitioner()
-            .getPartition(partitionBy.apply(el)) % numPartitions();
+    KeyedWindow window = (KeyedWindow) el;
+    int partition = partitioning.getPartitioner().getPartition(window.key());
+    int windowHash = window.window().hashCode();
+    int swapped = (windowHash >> bitShift) | (windowHash << bitShift);
+    return ((31 * partition ^ swapped) & Integer.MAX_VALUE) % numPartitions();
   }
 }
