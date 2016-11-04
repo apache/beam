8542f16cd086bb1289b8fd9a5a6872f04637478d
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2016-11-04 11:37:03 +0100

    #! [euphoria-hadoop] DataSourceInputFormat/DataSinkOutputFormat don't operate with Pair anymore

--- sdks/java/extensions/euphoria/euphoria-hadoop/src/main/java/cz/seznam/euphoria/hadoop/output/DataSinkOutputFormat.java
+++ sdks/java/extensions/euphoria/euphoria-hadoop/src/main/java/cz/seznam/euphoria/hadoop/output/DataSinkOutputFormat.java
@@ -3,11 +3,13 @@ package cz.seznam.euphoria.hadoop.output;
 
 import cz.seznam.euphoria.core.client.io.DataSink;
 import cz.seznam.euphoria.core.client.io.Writer;
-import cz.seznam.euphoria.core.client.util.Pair;
+
 import cz.seznam.euphoria.hadoop.utils.Serializer;
 import java.io.IOException;
-import org.apache.commons.codec.binary.Base64;
+import java.util.Base64;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.JobStatus;
 import org.apache.hadoop.mapreduce.OutputCommitter;
@@ -17,33 +19,41 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
 /**
  * {@code OutputFormat} created from {@code DataSink}.
- * Because of the hadoop outputformat contract, we need to be able to
+ * Because of the hadoop output format contract, we need to be able to
  * instantiate the format from {@code Class} object, therefore we
  * need to serialize the underlying {@code DataSink} to bytes and
  * store in to configuration.
  */
-public class DataSinkOutputFormat<K, V> extends OutputFormat<K, V> {
+public class DataSinkOutputFormat<V> extends OutputFormat<NullWritable, V> {
 
   private static final String DATA_SINK = "cz.seznam.euphoria.hadoop.data-sink-serialized";
 
-  public static <K, V> void configure(
-      Configuration conf, DataSink<Pair<K, V>> sink) throws IOException {
+  /**
+   * Sets given {@link DataSink} into Hadoop configuration. Note that original
+   * configuration is modified.
+   * @param conf Instance of Hadoop configuration
+   * @param sink Euphoria sink
+   * @return Modified configuration
+   */
+  public static <V> Configuration configure(
+      Configuration conf, DataSink<V> sink) throws IOException {
     
     conf.set(DATA_SINK, toBase64(sink));
+    return conf;
   }
 
-  private static class HadoopRecordWriter<K, V> extends RecordWriter<K, V> {
+  private static class HadoopRecordWriter<V> extends RecordWriter<NullWritable, V> {
 
-    private final Writer<Pair<K, V>> writer;
+    private final Writer<V> writer;
 
     @SuppressWarnings("unchecked")
-    HadoopRecordWriter(Writer<? extends Pair<K, V>> writer) {
-      this.writer = (Writer<Pair<K, V>>) writer;
+    HadoopRecordWriter(Writer<V> writer) {
+      this.writer = writer;
     }
 
     @Override
-    public void write(K k, V v) throws IOException {
-      writer.write(Pair.of(k, v));
+    public void write(NullWritable k, V v) throws IOException {
+      writer.write(v);
     }
 
     @Override
@@ -56,25 +66,25 @@ public class DataSinkOutputFormat<K, V> extends OutputFormat<K, V> {
   private static String toBase64(DataSink<?> sink) throws IOException {
 
     byte[] bytes = Serializer.toBytes(sink);
-    return Base64.encodeBase64String(bytes);
+    return Base64.getEncoder().encodeToString(bytes);
   }
 
   @SuppressWarnings("unchecked")
-  private static <K, V> DataSink<? extends Pair<K, V>> fromBase64(String base64Bytes)
+  private static <V> DataSink<V> fromBase64(String base64Bytes)
       throws IOException, ClassNotFoundException {
 
-    byte[] bytes = Base64.decodeBase64(base64Bytes);
+    byte[] bytes = Base64.getDecoder().decode(base64Bytes);
     return Serializer.fromBytes(bytes);
   }
 
   // instance of the data sink
-  private DataSink<? extends Pair<K, V>> sink;
-  // the single writer per outputformat instance and thread
-  private Writer<? extends Pair<K, V>> writer;
+  private DataSink<V> sink;
+  // the single writer per output format instance and thread
+  private Writer<V> writer;
 
   @Override
   @SuppressWarnings("unchecked")
-  public RecordWriter<K, V> getRecordWriter(TaskAttemptContext tac)
+  public RecordWriter<NullWritable, V> getRecordWriter(TaskAttemptContext tac)
       throws IOException, InterruptedException {
 
     instantiateWriter(tac);
