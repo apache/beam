3cb7d0261a2beb854277615ddb5fcfa8a88a47c4
Pei He <peihe@google.com>
2016-11-09 13:22:02 -0800

    fixup! address comments.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/Sink.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/Sink.java
@@ -262,8 +262,7 @@ public abstract class Sink<T> implements Serializable, HasDisplayData {
      * Aborts writing the bundle. Removes resources used for writing the bundle,
      * and closes this {@link Writer}.
      *
-     * <p>Called for each failed bundle with best effort. However, there is no strong guarantee in
-     * the events such as worker crashes.
+     * <p>When a bundle fails, runners will {@link #abort} the {@link Writer} if possible.
      */
     public abstract void abort() throws Exception;
 
