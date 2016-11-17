abd96b8986ef922057375279312fcd0fa66ab7ba
Kenneth Knowles <klk@google.com>
2016-11-17 15:19:16 -0800

    Autogenerate examples archetype during build

--- sdks/java/maven-archetypes/examples/generate-sources.sh
+++ sdks/java/maven-archetypes/examples/generate-sources.sh
@@ -24,10 +24,13 @@
 HERE="$(dirname $0)"
 
 # The directory of the examples-java module
-EXAMPLES_ROOT="${HERE}/../../../examples/java"
+EXAMPLES_ROOT="${HERE}/../../../../examples/java"
 
 # The root of the examples archetype
-ARCHETYPE_ROOT="${HERE}/examples/src/main/resources/archetype-resources"
+ARCHETYPE_ROOT="${HERE}/src/main/resources/archetype-resources"
+
+mkdir -p "${ARCHETYPE_ROOT}/src/main/java"
+mkdir -p "${ARCHETYPE_ROOT}/src/test/java"
 
 #
 # Copy the Java subset of the examples project verbatim. 
@@ -44,16 +47,22 @@ rsync -a --exclude cookbook --exclude complete --exclude '*IT.java'  \
 # Replace 'package org.apache.beam.examples' with 'package ${package}' in all Java code
 #
 find "${ARCHETYPE_ROOT}/src/main/java" -name '*.java' -print0 \
-    | xargs -0 sed -i 's/^package org\.apache\.beam\.examples/package ${package}/g'
+    | xargs -0 sed -i.bak 's/^package org\.apache\.beam\.examples/package ${package}/g'
 
 find "${ARCHETYPE_ROOT}/src/test/java" -name '*.java' -print0 \
-    | xargs -0 sed -i 's/^package org\.apache\.beam\.examples/package ${package}/g'
+    | xargs -0 sed -i.bak 's/^package org\.apache\.beam\.examples/package ${package}/g'
 
 #
 # Replace 'import org.apache.beam.examples.' with 'import ${package}.' in all Java code
 #
 find "${ARCHETYPE_ROOT}/src/main/java" -name '*.java' -print0 \
-    | xargs -0 sed -i 's/^import org\.apache\.beam\.examples/import ${package}/g'
+    | xargs -0 sed -i.bak 's/^import org\.apache\.beam\.examples/import ${package}/g'
 
 find "${ARCHETYPE_ROOT}/src/test/java" -name '*.java' -print0 \
-    | xargs -0 sed -i 's/^import org\.apache\.beam\.examples/import ${package}/g'
+    | xargs -0 sed -i.bak 's/^import org\.apache\.beam\.examples/import ${package}/g'
+
+#
+# The use of -i.bak is necessary for the above to work with both GNU and BSD sed. 
+# Delete the files now.
+#
+find "${ARCHETYPE_ROOT}/src" -name '*.bak' -delete
