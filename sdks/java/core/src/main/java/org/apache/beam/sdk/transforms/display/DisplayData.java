b5f4bdf76ae995d5da7b9113d436b729b3581cfb
Sam McVeety <sgmc@google.com>
2016-12-06 15:09:52 -0800

    Address comment

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/display/DisplayData.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/display/DisplayData.java
@@ -866,7 +866,7 @@ public class DisplayData implements Serializable {
   /**
    * Create a display item for the specified key and {@link ValueProvider}.
    */
-  public static ItemSpec item(String key, ValueProvider<?> value) {
+  public static ItemSpec<?> item(String key, ValueProvider<?> value) {
     if (value.isAccessible()) {
       Object got = value.get();
       Type type = inferType(got);
