dff45ea3ce332a93f44c1ba511287ce900e9e6ef
Luke Cwik <lcwik@google.com>
2016-12-08 15:22:35 -0800

    Add support for having an empty CountingInput/CountingSource

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/CountingInputTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/CountingInputTest.java
@@ -71,9 +71,19 @@ public class CountingInputTest {
   public void testBoundedInput() {
     Pipeline p = TestPipeline.create();
     long numElements = 1000;
-    PCollection<Long> input = p.apply(CountingInput.upTo(numElements));
+    PCollection<Long> input = p.apply(Read.from(CountingSource.upTo(numElements)));
 
     addCountingAsserts(input, 0, numElements);
+    p.run();
+  }
+
+  @Test
+  @Category(RunnableOnService.class)
+  public void testEmptyBoundedSource() {
+    Pipeline p = TestPipeline.create();
+    PCollection<Long> input = p.apply(CountingInput.upTo(0));
+
+    PAssert.that(input).empty();
     p.run();
   }
 
