615605665b5f421e870c1fa8c0e37b9e4850fd90
Thomas Groh <tgroh@google.com>
2016-12-09 15:34:25 -0800

    Migrate AppliedPTransform to use AutoValue

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/AppliedPTransform.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/AppliedPTransform.java
@@ -17,8 +17,7 @@
  */
 package org.apache.beam.sdk.transforms;
 
-import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
+import com.google.auto.value.AutoValue;
 import org.apache.beam.sdk.values.PInput;
 import org.apache.beam.sdk.values.POutput;
 
@@ -32,70 +31,26 @@ import org.apache.beam.sdk.values.POutput;
  * @param <OutputT> transform output type
  * @param <TransformT> transform type
  */
-public class AppliedPTransform
+@AutoValue
+public abstract class AppliedPTransform
     <InputT extends PInput, OutputT extends POutput,
      TransformT extends PTransform<? super InputT, OutputT>> {
 
-  private final String fullName;
-  private final InputT input;
-  private final OutputT output;
-  private final TransformT transform;
-
-  private AppliedPTransform(String fullName, InputT input, OutputT output, TransformT transform) {
-    this.input = input;
-    this.output = output;
-    this.transform = transform;
-    this.fullName = fullName;
+  public static <
+          InputT extends PInput,
+          OutputT extends POutput,
+          TransformT extends PTransform<? super InputT, OutputT>>
+      AppliedPTransform<InputT, OutputT, TransformT> of(
+          String fullName, InputT input, OutputT output, TransformT transform) {
+    return new AutoValue_AppliedPTransform<InputT, OutputT, TransformT>(
+        fullName, input, output, transform);
   }
 
-  public static <InputT extends PInput, OutputT extends POutput,
-                 TransformT extends PTransform<? super InputT, OutputT>>
-  AppliedPTransform<InputT, OutputT, TransformT> of(
-      String fullName, InputT input, OutputT output, TransformT transform) {
-    return new AppliedPTransform<InputT, OutputT, TransformT>(fullName, input, output, transform);
-  }
+  public abstract String getFullName();
 
-  public String getFullName() {
-    return fullName;
-  }
+  public abstract InputT getInput();
 
-  public InputT getInput() {
-    return input;
-  }
+  public abstract OutputT getOutput();
 
-  public OutputT getOutput() {
-    return output;
-  }
-
-  public TransformT getTransform() {
-    return transform;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(getFullName(), getInput(), getOutput(), getTransform());
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (other instanceof AppliedPTransform) {
-      AppliedPTransform<?, ?, ?> that = (AppliedPTransform<?, ?, ?>) other;
-      return Objects.equal(this.getFullName(), that.getFullName())
-          && Objects.equal(this.getInput(), that.getInput())
-          && Objects.equal(this.getOutput(), that.getOutput())
-          && Objects.equal(this.getTransform(), that.getTransform());
-    } else {
-      return false;
-    }
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(getClass())
-        .add("fullName", getFullName())
-        .add("input", getInput())
-        .add("output", getOutput())
-        .add("transform", getTransform())
-        .toString();
-  }
+  public abstract TransformT getTransform();
 }
