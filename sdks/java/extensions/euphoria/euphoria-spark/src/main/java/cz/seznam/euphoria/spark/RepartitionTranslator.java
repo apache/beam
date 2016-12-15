0f908c0e510d91a1e4077c37940b1ce7a348f0d7
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2016-12-15 18:17:06 +0100

    #! [euphoria-spark] Fix partitioner when partitionId is negative

--- sdks/java/extensions/euphoria/euphoria-spark/src/main/java/cz/seznam/euphoria/spark/RepartitionTranslator.java
+++ sdks/java/extensions/euphoria/euphoria-spark/src/main/java/cz/seznam/euphoria/spark/RepartitionTranslator.java
@@ -47,8 +47,9 @@ class RepartitionTranslator implements SparkOperatorTranslator<Repartition> {
     @SuppressWarnings("unchecked")
     public Tuple2<Integer, WindowedElement> call(WindowedElement el)  {
       Partitioner partitioner = partitioning.getPartitioner();
-      int partition = partitioner.getPartition(el.getElement()) % partitioning.getNumPartitions();
-      return new Tuple2<>(partition, el);
+      int partitionId = partitioner.getPartition(el.getElement());
+      return new Tuple2<>(
+              (partitionId & Integer.MAX_VALUE) % partitioning.getNumPartitions(), el);
     }
   }
 
