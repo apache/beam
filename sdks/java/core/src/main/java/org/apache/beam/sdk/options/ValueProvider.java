0ea9aa491a3366d1799c91ef1945bee82ee904e8
Sam McVeety <sgmc@google.com>
2016-12-27 12:22:48 -0800

    Fixup

--- sdks/java/core/src/main/java/org/apache/beam/sdk/options/ValueProvider.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/options/ValueProvider.java
@@ -108,7 +108,7 @@ public interface ValueProvider<T> extends Serializable {
 
     private final ValueProvider<X> value;
     private final SerializableFunction<X, T> translator;
-    private transient T cachedValue;
+    private transient volatile T cachedValue;
 
     NestedValueProvider(ValueProvider<X> value, SerializableFunction<X, T> translator) {
       this.value = checkNotNull(value);
