d9c0e6de1c5edce1b9238badd7422e06dcd805ed
feczo <szabolcs.feczak@gmail.com>
2017-01-04 16:24:16 +1100

    Update DataflowPipelineWorkerPoolOptions.java
    since this is a get method doc, slightly adjusting the wording to reflect that
    "should be used" sounds like something passed onto set, that at least a few people found to be confusing

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/options/DataflowPipelineWorkerPoolOptions.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/options/DataflowPipelineWorkerPoolOptions.java
@@ -203,21 +203,19 @@ public interface DataflowPipelineWorkerPoolOptions extends PipelineOptions {
   @JsonIgnore
   List<String> getFilesToStage();
   void setFilesToStage(List<String> value);
-
-  String getWorkerDiskType();
-
+  
   /**
-   * Specifies what type of persistent disk should be used. The value should be a
-   * full disk type resource, e.g.,
-   * compute.googleapis.com/projects//zones//diskTypes/pd-ssd. For more information,
+   * Specifies what type of persistent disk is used. The value is a full disk type resource,
+   * e.g., compute.googleapis.com/projects//zones//diskTypes/pd-ssd. For more information,
    * see the <a href="https://cloud.google.com/compute/docs/reference/latest/diskTypes">API
    * reference documentation for DiskTypes</a>.
    */
-  @Description("Specifies what type of persistent disk should be used. The "
-      + "value should be a full URL of a disk type resource, e.g., "
+  @Description("Specifies what type of persistent disk is used. The "
+      + "value is a full URL of a disk type resource, e.g., "
       + "compute.googleapis.com/projects//zones//diskTypes/pd-ssd. For more "
       + "information, see the API reference documentation for DiskTypes: "
       + "https://cloud.google.com/compute/docs/reference/latest/diskTypes")
+  String getWorkerDiskType();
   void setWorkerDiskType(String value);
 
   /**
