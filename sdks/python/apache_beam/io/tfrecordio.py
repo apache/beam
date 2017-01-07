6caa2aaafcd434c3f69c391579dba61a7f49435f
Younghee Kwon <younghee.kwon@gmail.com>
2017-01-07 01:21:43 -0800

    Addressed first round of comments.

--- sdks/python/apache_beam/io/tfrecordio.py
+++ sdks/python/apache_beam/io/tfrecordio.py
@@ -32,14 +32,21 @@ import crcmod
 __all__ = ['ReadFromTFRecord', 'WriteToTFRecord']
 
 
-_crc32c_fn = None
-try:
-  import snappy  # pylint: disable=import-error
-  _crc32c_fn = snappy._crc32c  # pylint: disable=protected-access
-except ImportError:
-  logging.warning('Couldn\'t find python-snappy so the implementation of '
-                  '_TFRecordUtil._masked_crc32c is not as fast as it could be.')
-  _crc32c_fn = crcmod.predefined.mkPredefinedCrcFun('crc-32c')
+def _default_crc32c_fn(value):
+  """Calculates crc32c by either snappy or crcmod based on installation."""
+
+  if _default_crc32c_fn.fn:
+    return _default_crc32c_fn.fn(value)
+  try:
+    import snappy  # pylint: disable=import-error
+    _default_crc32c_fn.fn = snappy._crc32c  # pylint: disable=protected-access
+  except ImportError:
+    logging.warning('Couldn\'t find python-snappy so the implementation of '
+                    '_TFRecordUtil._masked_crc32c is not as fast as it could '
+                    'be.')
+    _default_crc32c_fn.fn = crcmod.predefined.mkPredefinedCrcFun('crc-32c')
+  return _default_crc32c_fn.fn(value)
+_default_crc32c_fn.fn = None
 
 
 class _TFRecordUtil(object):
@@ -51,10 +58,8 @@ class _TFRecordUtil(object):
   Note that masks and length are represented in LittleEndian order.
   """
 
-  # This code is performance critical so make sure to run the corresponding
-  # benchmark when updating it.
   @classmethod
-  def _masked_crc32c(cls, value, crc32c_fn=_crc32c_fn):
+  def _masked_crc32c(cls, value, crc32c_fn=_default_crc32c_fn):
     """Compute a masked crc32c checksum for a value.
 
     Args:
@@ -65,6 +70,7 @@ class _TFRecordUtil(object):
     Returns:
       Masked crc32c checksum.
     """
+
     crc = crc32c_fn(value)
     return (((crc >> 15) | (crc << 17)) + 0xa282ead8) & 0xffffffff
 
@@ -178,18 +184,16 @@ class ReadFromTFRecord(PTransform):
     """Initialize a ReadFromTFRecord transform.
 
     Args:
-      file_pattern: The file pattern to read from as a local file path or a GCS
-        gs:// path. The path can contain glob characters (*, ?, and [...]
-        sets).
+      file_pattern: A file glob pattern to read TFRecords from.
       coder: Coder used to decode each record.
-      compression_type: Used to handle compressed input files. Typical value
+      compression_type: Used to handle compressed input files. Default value
           is CompressionTypes.AUTO, in which case the file_path's extension will
           be used to detect the compression.
       **kwargs: optional args dictionary. These are passed through to parent
         constructor.
 
     Returns:
-      A WriteToTFRecord transform object.
+      A ReadFromTFRecord transform object.
     """
     super(ReadFromTFRecord, self).__init__(**kwargs)
     self._args = (file_pattern, coder, compression_type)
@@ -236,27 +240,21 @@ class WriteToTFRecord(PTransform):
     """Initialize WriteToTFRecord transform.
 
     Args:
-
       file_path_prefix: The file path to write to. The files written will begin
         with this prefix, followed by a shard identifier (see num_shards), and
-        end in a common extension, if given by file_name_suffix. In most cases,
-        only this argument is specified and num_shards, shard_name_template, and
-        file_name_suffix use default values.
+        end in a common extension, if given by file_name_suffix.
       coder: Coder used to encode each record.
       file_name_suffix: Suffix for the files written.
       num_shards: The number of files (shards) used for output. If not set, the
-        service will decide on the optimal number of shards.
-        Constraining the number of shards is likely to reduce
-        the performance of a pipeline.  Setting this value is not recommended
-        unless you require a specific number of output files.
+        default value will be used.
       shard_name_template: A template string containing placeholders for
         the shard number and shard count. Currently only '' and
-        '-SSSSS-of-NNNNN' are patterns accepted by the service.
+        '-SSSSS-of-NNNNN' are patterns allowed.
         When constructing a filename for a particular shard number, the
         upper-case letters 'S' and 'N' are replaced with the 0-padded shard
         number and shard count respectively.  This argument can be '' in which
         case it behaves as if num_shards was set to 1 and only one file will be
-        generated. The default pattern used is '-SSSSS-of-NNNNN'.
+        generated. The default pattern is '-SSSSS-of-NNNNN'.
       compression_type: Used to handle compressed output files. Typical value
           is CompressionTypes.AUTO, in which case the file_path's extension will
           be used to detect the compression.
