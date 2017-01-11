b3456996ad2363de6d27181b03cdd08634a32aad
Stephen Sisk <sisk@google.com>
2017-01-11 11:21:47 -0800

    Update JdbcIOIT example so it uses real pipeline options & demonstrates a bug in JdbcIO

--- sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcTestOptions.java
+++ sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcTestOptions.java
@@ -4,6 +4,9 @@ import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.testing.TestPipelineOptions;
 
+/**
+ * TODO - non horrible javadoc.
+ */
 public interface JdbcTestOptions extends TestPipelineOptions {
     @Description("IP for postgres server")
     @Default.String("postgres-ip")
@@ -19,4 +22,20 @@ public interface JdbcTestOptions extends TestPipelineOptions {
     @Default.String("postgres-password")
     String getPostgresPassword();
     void setPostgresPassword(String value);
+
+    @Description("Database name for postgres server")
+    @Default.String("postgres-database-name")
+    String getPostgresDatabaseName();
+    void setPostgresDatabaseName(String value);
+
+    @Description("Port for postgres server")
+    @Default.Integer(0)
+    Integer getPostgresPort();
+    void setPostgresPort(Integer value);
+
+    @Description("Whether the postgres server uses SSL")
+    @Default.Boolean(true)
+    Boolean getPostgresSsl();
+    void setPostgresSsl(Boolean value);
+
 }
