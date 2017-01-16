cde550fe9b90be5fc9548735d5854359fca6a9cf
Stas Levin <staslevin@gmail.com>
2017-01-16 16:20:25 +0200

    [BEAM-882,BEAM-883,BEAM-878] Simplified API surface verifications.

--- sdks/java/core/src/test/java/org/apache/beam/sdk/util/ApiSurfaceTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/util/ApiSurfaceTest.java
@@ -17,138 +17,50 @@
  */
 package org.apache.beam.sdk.util;
 
-import static org.hamcrest.Matchers.anyOf;
+import static org.apache.beam.sdk.util.ApiSurface.containsOnlyClassesMatching;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.emptyIterable;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
-import com.google.common.base.Joiner;
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import org.hamcrest.Description;
+import javax.annotation.Nonnull;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/**
- * Tests for ApiSurface. These both test the functionality and also that our
- * public API is conformant to a hard-coded policy.
- */
+/** Functionality tests for ApiSurface. */
 @RunWith(JUnit4.class)
 public class ApiSurfaceTest {
 
-  @Test
-  public void testOurApiSurface() throws Exception {
-    ApiSurface checkedApiSurface = ApiSurface.getSdkApiSurface();
-
-    Map<Class<?>, List<Class<?>>> disallowedClasses = Maps.newHashMap();
-    for (Class<?> clazz : checkedApiSurface.getExposedClasses()) {
-      if (!classIsAllowed(clazz)) {
-        disallowedClasses.put(clazz, checkedApiSurface.getAnyExposurePath(clazz));
-      }
-    }
-
-    List<String> disallowedMessages = Lists.newArrayList();
-    for (Map.Entry<Class<?>, List<Class<?>>> entry : disallowedClasses.entrySet()) {
-      disallowedMessages.add(entry.getKey() + " exposed via:\n\t\t"
-      + Joiner.on("\n\t\t").join(entry.getValue()));
-    }
-    Collections.sort(disallowedMessages);
-
-    if (!disallowedMessages.isEmpty()) {
-      fail("The following disallowed classes appear in the public API surface of the SDK:\n\t"
-        + Joiner.on("\n\t").join(disallowedMessages));
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private static final Set<Matcher<Class<?>>> ALLOWED_PACKAGES =
-      ImmutableSet.of(
-          inPackage("org.apache.beam"),
-          inPackage("com.google.api.client"),
-          inPackage("com.google.api.services.bigquery"),
-          inPackage("com.google.api.services.cloudresourcemanager"),
-          inPackage("com.google.api.services.dataflow"),
-          inPackage("com.google.api.services.pubsub"),
-          inPackage("com.google.api.services.storage"),
-          inPackage("com.google.auth"),
-          inPackage("com.google.bigtable.v1"),
-          inPackage("com.google.cloud.bigtable.config"),
-          inPackage("com.google.cloud.bigtable.grpc"),
-          inPackage("com.google.datastore"),
-          inPackage("com.google.protobuf"),
-          inPackage("com.google.rpc"),
-          inPackage("com.google.type"),
-          inPackage("com.fasterxml.jackson.annotation"),
-          inPackage("com.fasterxml.jackson.core"),
-          inPackage("com.fasterxml.jackson.databind"),
-          inPackage("com.fasterxml.jackson.deser"),
-          inPackage("io.grpc"),
-          inPackage("org.apache.avro"),
-          inPackage("org.apache.commons.logging"), // via BigTable
-          inPackage("org.hamcrest"), // via DataflowMatchers
-          inPackage("org.codehaus.jackson"), // via Avro
-          inPackage("org.joda.time"),
-          inPackage("org.junit"),
-          inPackage("java"));
-
   @SuppressWarnings({"rawtypes", "unchecked"})
-  private boolean classIsAllowed(Class<?> clazz) {
-    // Safe cast inexpressible in Java without rawtypes
-    return anyOf((Iterable) ALLOWED_PACKAGES).matches(clazz);
+  private void assertExposed(final Class classToExamine, final Class... exposedClasses) {
+
+    final ApiSurface apiSurface = ApiSurface.ofClass(classToExamine).pruningPrefix("java");
+
+    final ImmutableSet<Matcher<Class<?>>> allowed =
+        FluentIterable.from(
+                Iterables.concat(Sets.newHashSet(classToExamine), Sets.newHashSet(exposedClasses)))
+            .transform(
+                new Function<Class, Matcher<Class<?>>>() {
+
+                  @Override
+                  public Matcher<Class<?>> apply(@Nonnull final Class input) {
+                    return Matchers.<Class<?>>equalTo(input);
+                  }
+                })
+            .toSet();
+
+    assertThat(apiSurface, containsOnlyClassesMatching(allowed));
   }
 
-  private static Matcher<Class<?>> inPackage(String packageName) {
-    return new ClassInPackage(packageName);
-  }
-
-  private static class ClassInPackage extends TypeSafeDiagnosingMatcher<Class<?>> {
-
-    private final String packageName;
-
-    public ClassInPackage(String packageName) {
-      this.packageName = packageName;
-    }
-
-    @Override
-    public void describeTo(Description description) {
-      description.appendText("Class in package \"");
-      description.appendText(packageName);
-      description.appendText("\"");
-    }
-
-    @Override
-    protected boolean matchesSafely(Class<?> clazz, Description mismatchDescription) {
-      return clazz.getName().startsWith(packageName + ".");
-    }
-
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////
-
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  private void assertExposed(Class classToExamine, Class... exposedClasses) {
-    ApiSurface apiSurface = ApiSurface
-        .ofClass(classToExamine)
-        .pruningPrefix("java");
-
-    Set<Class> expectedExposed = Sets.newHashSet(classToExamine);
-    for (Class clazz : exposedClasses) {
-      expectedExposed.add(clazz);
-    }
-    assertThat(apiSurface.getExposedClasses(), containsInAnyOrder(expectedExposed.toArray()));
-  }
-
-  private interface Exposed { }
+  private interface Exposed {}
 
   private interface ExposedReturnType {
     Exposed zero();
@@ -177,7 +89,7 @@ public class ApiSurfaceTest {
     assertExposed(ExposedWildcardBound.class, Exposed.class);
   }
 
-  private interface ExposedActualTypeArgument extends List<Exposed> { }
+  private interface ExposedActualTypeArgument extends List<Exposed> {}
 
   @Test
   public void testExposedActualTypeArgument() throws Exception {
@@ -186,25 +98,27 @@ public class ApiSurfaceTest {
 
   @Test
   public void testIgnoreAll() throws Exception {
-    ApiSurface apiSurface = ApiSurface.ofClass(ExposedWildcardBound.class)
-        .includingClass(Object.class)
-        .includingClass(ApiSurface.class)
-        .pruningPattern(".*");
+    ApiSurface apiSurface =
+        ApiSurface.ofClass(ExposedWildcardBound.class)
+            .includingClass(Object.class)
+            .includingClass(ApiSurface.class)
+            .pruningPattern(".*");
     assertThat(apiSurface.getExposedClasses(), emptyIterable());
   }
 
-  private interface PrunedPattern { }
-  private interface NotPruned extends PrunedPattern { }
+  private interface PrunedPattern {}
+
+  private interface NotPruned extends PrunedPattern {}
 
   @Test
   public void testprunedPattern() throws Exception {
-    ApiSurface apiSurface = ApiSurface.ofClass(NotPruned.class)
-        .pruningClass(PrunedPattern.class);
+    ApiSurface apiSurface = ApiSurface.ofClass(NotPruned.class).pruningClass(PrunedPattern.class);
     assertThat(apiSurface.getExposedClasses(), containsInAnyOrder((Class) NotPruned.class));
   }
 
   private interface ExposedTwice {
     Exposed zero();
+
     Exposed one();
   }
 
