17eddf1c794d0f1eea634009fca20d9be515a5f3
Dipti Kulkarni <dipti_dkulkarni@persistent.co.in>
2017-01-20 20:16:24 +0530

    Dependencies to pom.xml, and changes to PostgresIT

--- sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/integration/tests/HIFIOWithPostgresIT.java
+++ sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/integration/tests/HIFIOWithPostgresIT.java
@@ -1,7 +1,12 @@
 package org.apache.beam.sdk.io.hadoop.inputformat.integration.tests;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.apache.beam.sdk.io.hadoop.inputformat.HadoopInputFormatIO;
 import org.apache.beam.sdk.testing.PAssert;
@@ -12,7 +17,9 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.lib.db.DBWritable;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,25 +28,28 @@ import org.junit.runners.JUnit4;
 import org.junit.runners.MethodSorters;
 
 /**
- * Runs integration test to validate HadoopInputFromatIO for Postgres instance on GCP.
+ * Runs integration test to validate HadoopInputFromatIO for Postgres instance
+ * on GCP.
  *
  *
  * <p>
- * You can run just this test by doing the following:
- * mvn test-Dtest=HIFIOWithPostgresIT.java
+ * You can run just this test by doing the following: mvn
+ * test-Dtest=HIFIOWithPostgresIT.java
  *
  */
 @RunWith(JUnit4.class)
 @FixMethodOrder(MethodSorters.JVM)
 public class HIFIOWithPostgresIT implements Serializable {
-	
-	/*private static final long serialVersionUID = 1L;
-	private static final String DRIVER_CLASS_PROPERTY = "org.postgresql.Driver";
-	private static final String URL_PROPERTY = "jdbc:postgresql://104.197.246.66:5432/postgres";
-	private static final String USERNAME_PROPERTY = "postgres";
-	private static final String PASSWORD_PROPERTY = "euthS3M1";
-	private static final String INPUT_TABLE_NAME_PROPERTY = "mytable";
-*/
+
+	/*
+	 * private static final long serialVersionUID = 1L; private static final
+	 * String DRIVER_CLASS_PROPERTY = "org.postgresql.Driver"; private static
+	 * final String URL_PROPERTY =
+	 * "jdbc:postgresql://104.197.246.66:5432/postgres"; private static final
+	 * String USERNAME_PROPERTY = "postgres"; private static final String
+	 * PASSWORD_PROPERTY = "euthS3M1"; private static final String
+	 * INPUT_TABLE_NAME_PROPERTY = "mytable";
+	 */
 	private static final String DRIVER_CLASS_PROPERTY = "org.postgresql.Driver";
 	private static final String URL_PROPERTY = "jdbc:postgresql://localhost:5432/beamdb";
 	private static final String USERNAME_PROPERTY = "postgres";
@@ -72,17 +82,18 @@ public class HIFIOWithPostgresIT implements Serializable {
 		conf.set("mapreduce.jdbc.username", USERNAME_PROPERTY);
 		conf.set("mapreduce.jdbc.password", PASSWORD_PROPERTY);
 		conf.set("mapreduce.jdbc.input.table.name", INPUT_TABLE_NAME_PROPERTY);
-		conf.set("mapreduce.jdbc.input.query", "SELECT * FROM "+INPUT_TABLE_NAME_PROPERTY);
-	//	conf.set("mapreduce.jdbc.input.count.query",
-		//		"SELECT count(*) FROM mytable");
+		conf.set("mapreduce.jdbc.input.query", "SELECT * FROM "
+				+ INPUT_TABLE_NAME_PROPERTY);
+		// conf.set("mapreduce.jdbc.input.count.query",
+		// "SELECT count(*) FROM mytable");
 		conf.setClass("mapreduce.job.inputformat.class",
 				org.apache.hadoop.mapreduce.lib.db.DBInputFormat.class,
 				InputFormat.class);
-		conf.setClass("key.class", org.apache.hadoop.io.Text.class,
+		conf.setClass("key.class", org.apache.hadoop.io.LongWritable.class,
 				Object.class);
-		conf.setClass("value.class", org.apache.hadoop.io.IntWritable.class,
-				Object.class);
+		conf.setClass("value.class", DBInputWritable.class, Object.class);
 		return conf;
 	}
 
 }
+
