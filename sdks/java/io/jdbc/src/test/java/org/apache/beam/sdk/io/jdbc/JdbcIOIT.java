5dfc7cc97f7739ce4847b0a18fc2a4208df7b0c1
Stephen Sisk <sisk@google.com>
2017-01-24 16:24:30 -0800

    Add JdbcIOIT testWrite, uses data loading script
    JdbcTestDataSet allows users to load data for the read tests,
    and is shared with JdbcIOTest

--- sdks/java/io/jdbc/src/test/java/org/apache/beam/sdk/io/jdbc/JdbcIOIT.java
+++ sdks/java/io/jdbc/src/test/java/org/apache/beam/sdk/io/jdbc/JdbcIOIT.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import javax.sql.DataSource;
 
@@ -18,9 +19,11 @@ import org.apache.beam.sdk.testing.NeedsRunner;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -29,12 +32,10 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 
 /**
- * Runs tests to validate JdbcIO works with a real instance of a datastore. You'll need
- * a running instance of postgres to validate this test.
- *
- * <p>Cleanup notes: this test cleans up after itself, but only if it gets a chance. It
- * is intended to be run in an db that is cleaned up regularly (ie, server reset every
- * week.)
+ * Runs tests to validate JdbcIO works with a real instance of Postgres. You'll need
+ * a running instance of postgres to validate this test and you'll need to make sure the test data
+ * set exists in the database - if not, you can run JdbcTestDataSet and it will create the read
+ * table for you.
  *
  * <p>You can run just this test by doing the following:
  * mvn test-compile compile failsafe:integration-test -D beamTestPipelineOptions='[
@@ -43,7 +44,7 @@ import org.postgresql.ds.PGSimpleDataSource;
  * "--postgresDatabaseName=myfancydb",
  * "--postgresPassword=yourpassword",
  * "--postgresSsl=false"
- * ]'
+ * ]' -DskipITs=false -Dit.test=org.apache.beam.sdk.io.jdbc.JdbcIOIT -DfailIfNoTests=false
  */
 @RunWith(JUnit4.class)
 public class JdbcIOIT {
@@ -67,81 +68,103 @@ public class JdbcIOIT {
     return dataSource;
   }
 
-  public String createDataTable(DataSource dataSource) throws SQLException {
-    String tableName = "BEAMTEST".concat(new Long(new Date().getTime()).toString());
-    try (Connection connection = dataSource.getConnection()) {
-      // something like this will need to happen in tests on a newly created postgres server,
-      // but likely it will happen in perfkit, not here
-      // alternatively, we may have a pipelineoption indicating whether we want to
-      // re-use the database or create a new one
-      try (Statement statement = connection.createStatement()) {
-        statement.execute(
-            String.format("create table %s (id INT, name VARCHAR(500))", tableName));
-      }
+  // These static classes are used since if they were inlined using anonymous classes, they would cause the JdbcIOIT
+  // class as a whole to be serialized.
 
-
-      // We would want to move this into shared code with JdbcIOTest if
-      // it doesn't happen in perfkit code
-      String[] scientists = {"Einstein", "Darwin", "Copernicus", "Pasteur", "Curie", "Faraday",
-          "Newton", "Bohr", "Galilei", "Maxwell"};
-      connection.setAutoCommit(false);
-      try (PreparedStatement preparedStatement =
-               connection.prepareStatement(
-                   String.format("insert into %s values (?,?)", tableName))) {
-          for (int i = 0; i < 1000; i++) {
-            int index = i % scientists.length;
-            preparedStatement.clearParameters();
-            preparedStatement.setInt(1, i);
-            preparedStatement.setString(2, scientists[index]);
-            preparedStatement.executeUpdate();
-          }
-        }
-        connection.commit();
-      }
-      return tableName;
+  private static class TestRowMapper implements JdbcIO.RowMapper<KV<String, Integer>> {
+    @Override
+    public KV<String, Integer> mapRow(ResultSet resultSet) throws Exception {
+      KV<String, Integer> kv =
+          KV.of(resultSet.getString("name"), resultSet.getInt("id"));
+      return kv;
     }
+  }
 
-    private static class TestRowMapper implements JdbcIO.RowMapper<KV<String, Integer>> {
-      @Override
-      public KV<String, Integer> mapRow(ResultSet resultSet) throws Exception {
-        KV<String, Integer> kv =
-            KV.of(resultSet.getString("name"), resultSet.getInt("id"));
-        return kv;
+  private static class ValidateCountFn implements SerializableFunction<Iterable<KV<String, Long>>, Void> {
+    @Override
+    public Void apply(Iterable<KV<String, Long>> input) {
+      for (KV<String, Long> element : input) {
+        assertEquals(element.getKey(), 100L, element.getValue().longValue());
       }
+      return null;
     }
+  };
 
-    private static class ValidateCountFn implements SerializableFunction<Iterable<KV<String, Long>>, Void> {
-        @Override
-        public Void apply(Iterable<KV<String, Long>> input) {
-          for (KV<String, Long> element : input) {
-            assertEquals(element.getKey(), 100L, element.getValue().longValue());
-          }
-          return null;
-        }
-      };
+  private static class TestStatementSetter implements JdbcIO.PreparedStatementSetter<KV<Integer, String>> {
+    @Override
+    public void setParameters(KV<Integer, String> element, PreparedStatement statement)
+                    throws SQLException {
+      statement.setInt(1, element.getKey());
+      statement.setString(2, element.getValue());
+    }
+  }
 
+  /**
+   * Does a test read of a few rows from a postgres database.
+   *
+   * <p>Note that IT read tests must not do any data table manipulation (setup/clean up.)
+   * @throws SQLException
+   */
   @Test
   @Category(NeedsRunner.class)
-  public void testRead() throws Exception {
-    // TODO - this method still does some stuff that doesn't belong in the IT
+  public void testRead() throws SQLException {
+    DataSource dataSource = getDataSource();
+
+    String tableName = JdbcTestDataSet.READ_TABLE_NAME;
+
+    TestPipeline pipeline = TestPipeline.create();
+
+    PCollection<KV<String, Integer>> output = pipeline.apply(JdbcIO.<KV<String, Integer>>read()
+            .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration.create(dataSource))
+            .withQuery("select name,id from " + tableName)
+            .withRowMapper(new TestRowMapper())
+            .withCoder(KvCoder.of(StringUtf8Coder.of(), BigEndianIntegerCoder.of())));
+
+    // TODO: validate actual contents of rows, not just count.
+    PAssert.thatSingleton(
+        output.apply("Count All", Count.<KV<String, Integer>>globally()))
+        .isEqualTo(1000L);
+
+    PAssert.that(output
+        .apply("Count Scientist", Count.<String, Integer>perKey())
+    ).satisfies(new ValidateCountFn());
+
+    pipeline.run().waitUntilFinish();
+  }
+
+
+  /**
+   * Tests writes to a real live postgres database.
+   *
+   * Write Tests must clean up their data - in this case, it uses a new table every test run so
+   * that it won't interfere with read tests/other write tests. It uses finally to attempt to clean up data at the end
+   * of the test run.
+   * @throws SQLException
+   */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testWrite() throws SQLException {
     DataSource dataSource = getDataSource();
 
     String tableName = null;
 
     try {
-      tableName = createDataTable(dataSource);
+      tableName = JdbcTestDataSet.createWriteDataTable(dataSource);
 
       TestPipeline pipeline = TestPipeline.create();
 
-      PCollection<KV<String, Integer>> output = pipeline.apply(JdbcIO.<KV<String, Integer>>read()
+      ArrayList<KV<Integer, String>> data = new ArrayList<>();
+      for (int i = 0; i < 1000; i++) {
+        KV<Integer, String> kv = KV.of(i, "Test");
+        data.add(kv);
+      }
+      pipeline.apply(Create.of(data))
+          .apply(JdbcIO.<KV<Integer, String>>write()
               .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration.create(dataSource))
-              .withQuery("select name,id from " + tableName)
-              .withRowMapper(new TestRowMapper())
-              .withCoder(KvCoder.of(StringUtf8Coder.of(), BigEndianIntegerCoder.of())));
+              .withStatement(String.format("insert into %s values(?, ?)", tableName))
+              .withPreparedStatementSetter(new TestStatementSetter()));
 
-      PAssert.thatSingleton(
-          output.apply("Count All", Count.<KV<String, Integer>>globally()))
-          .isEqualTo(1000L);
+      pipeline.run().waitUntilFinish();
 
       PAssert.that(output
           .apply("Count Scientist", Count.<String, Integer>perKey())
@@ -157,6 +180,10 @@ public class JdbcIOIT {
           }
         }
       }
+      // TODO: Actually verify contents of the rows.
+    } finally {
+      JdbcTestDataSet.cleanUpDataTable(dataSource, tableName);
     }
   }
+
 }
