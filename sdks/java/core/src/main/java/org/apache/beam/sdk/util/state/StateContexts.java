b12e5ffbaf9bacebc5d80b3ed24c37a22e37524f
Kenneth Knowles <klk@google.com>
2017-01-26 21:05:09 -0800

    Delete long-deprecated StateContexts methods

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/state/StateContexts.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/state/StateContexts.java
@@ -17,10 +17,8 @@
  */
 package org.apache.beam.sdk.util.state;
 
-import javax.annotation.Nullable;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
-import org.apache.beam.sdk.util.WindowingInternals;
 import org.apache.beam.sdk.values.PCollectionView;
 
 /**
@@ -49,66 +47,5 @@ public class StateContexts {
   @SuppressWarnings("unchecked")
   public static <W extends BoundedWindow> StateContext<W> nullContext() {
     return (StateContext<W>) NULL_CONTEXT;
-  }
-
-  /**
-   * @deprecated This exists only for temporary compatibility with Dataflow worker and should be
-   *     deleted once a worker image is released that uses runners-core build after
-   *     https://github.com/apache/beam/pull/1353.
-   */
-  @Deprecated
-  public static <W extends BoundedWindow> StateContext<W> windowOnly(final W window) {
-    return new StateContext<W>() {
-      @Override
-      public PipelineOptions getPipelineOptions() {
-        throw new IllegalArgumentException(
-            "cannot call getPipelineOptions() in a window only context");
-      }
-
-      @Override
-      public <T> T sideInput(PCollectionView<T> view) {
-        throw new IllegalArgumentException("cannot call sideInput() in a window only context");
-      }
-
-      @Override
-      public W window() {
-        return window;
-      }
-    };
-  }
-
-  /**
-   * @deprecated This exists only for temporary compatibility with Dataflow worker and should be
-   *     deleted once a worker image is released that uses runners-core build after
-   *     https://github.com/apache/beam/pull/1353.
-   */
-  @Deprecated
-  public static <W extends BoundedWindow> StateContext<W> createFromComponents(
-      @Nullable final PipelineOptions options,
-      final WindowingInternals<?, ?> windowingInternals,
-      final W window) {
-    @SuppressWarnings("unchecked")
-    StateContext<W> typedNullContext = (StateContext<W>) NULL_CONTEXT;
-    if (options == null) {
-      return typedNullContext;
-    } else {
-      return new StateContext<W>() {
-
-        @Override
-        public PipelineOptions getPipelineOptions() {
-          return options;
-        }
-
-        @Override
-        public <T> T sideInput(PCollectionView<T> view) {
-          return windowingInternals.sideInput(view, window);
-        }
-
-        @Override
-        public W window() {
-          return window;
-        }
-      };
-    }
   }
 }
