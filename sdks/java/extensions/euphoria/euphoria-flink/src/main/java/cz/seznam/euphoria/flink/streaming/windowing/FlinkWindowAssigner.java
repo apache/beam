e5a3c110e312b5cb11645cb2a2184844ccdce2eb
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2017-01-30 10:50:44 +0100

    #! Correct name of copyright owner

--- sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/FlinkWindowAssigner.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/FlinkWindowAssigner.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Seznam a.s.
+ * Copyright 2016 Seznam.cz, a.s.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
