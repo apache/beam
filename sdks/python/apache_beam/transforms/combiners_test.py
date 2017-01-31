7f266ff0835efb96e55785f2a2eb93e4eab9224c
Robert Bradshaw <robertwb@gmail.com>
2017-01-31 12:51:27 -0800

    Reduce cost of sample combine test
    There's no need to run this test 300 times, which takes tens of seconds.
    
    Also split global and per-key sampling into separate tests.

--- sdks/python/apache_beam/transforms/combiners_test.py
+++ sdks/python/apache_beam/transforms/combiners_test.py
@@ -218,10 +218,8 @@ class CombineTest(unittest.TestCase):
     assert_that(result_kbot, equal_to([('a', [0, 1, 1, 1])]), label='k:bot')
     pipeline.run()
 
-  def test_sample(self):
-
-    # First test global samples (lots of them).
-    for ix in xrange(300):
+  def test_global_sample(self):
+    for ix in xrange(30):
       pipeline = TestPipeline()
       pcoll = pipeline | 'start' >> Create([1, 1, 2, 2])
       result = pcoll | combine.Sample.FixedSizeGlobally('sample-%d' % ix, 3)
@@ -240,7 +238,7 @@ class CombineTest(unittest.TestCase):
       assert_that(result, matcher())
       pipeline.run()
 
-    # Now test per-key samples.
+  def test_per_key_sample(self):
     pipeline = TestPipeline()
     pcoll = pipeline | 'start-perkey' >> Create(
         sum(([(i, 1), (i, 1), (i, 2), (i, 2)] for i in xrange(300)), []))
