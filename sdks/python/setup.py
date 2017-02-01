949689ce5a67fecb36b1aac16ab2f39a4b4e03bc
Sergio Fernández <sergio@wikier.org>
2017-02-01 09:20:51 +0100

    BEAM-1368: replaced usage of execfile() by exec(), which is the official recommendation for portability

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -31,8 +31,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 def get_version():
   global_names = {}
-  execfile(os.path.normpath('./apache_beam/version.py'),
-           global_names)
+  exec(open(os.path.normpath('./apache_beam/version.py')).read(), global_names)
   return global_names['__version__']
 
 PACKAGE_NAME = 'apache-beam-sdk'
