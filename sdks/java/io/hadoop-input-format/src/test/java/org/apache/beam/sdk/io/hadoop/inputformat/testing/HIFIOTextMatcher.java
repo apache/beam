1e81e33fdd6c60b569a2c0853fc9b800994559a6
Dipti Kulkarni <dipti_dkulkarni@persistent.co.in>
2017-02-06 10:41:22 +0530

    Apache license details added

--- sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/testing/HIFIOTextMatcher.java
+++ sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/testing/HIFIOTextMatcher.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.beam.sdk.io.hadoop.inputformat.testing;
 
 import java.io.File;
@@ -47,14 +61,16 @@ public class HIFIOTextMatcher extends TypeSafeMatcher<PipelineResult> implements
   }
 
   @Override
-  public void describeTo(Description arg0) {
-    // TODO Auto-generated method stub
-
+  public void describeTo(Description description) {
+    description
+    .appendText("Expected checksum is (")
+    .appendText(expectedChecksum)
+    .appendText(")");
   }
 
   /**
-   * This method checks if the checksum of expected list of Strings matches with the checksum of
-   * file content.
+   * This method checks if the checksum of provided expected list of Strings/ provided expected
+   * checksum matches with the checksum of file content.
    */
   @Override
   protected boolean matchesSafely(PipelineResult arg0) {
