00a61e4731fe1853c2366c4fb3c9c57af1224c8f
Dipti Kulkarni <dipti_dkulkarni@persistent.co.in>
2017-02-08 12:31:17 +0530

    Use the new HashingFn to validate value checksums

--- sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/HIFIOWithCassandraTest.java
+++ sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/HIFIOWithCassandraTest.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,18 +14,13 @@
  */
 package org.apache.beam.sdk.io.hadoop.inputformat;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.io.hadoop.inputformat.testing.HIFIOTextMatcher;
+import org.apache.beam.sdk.io.hadoop.inputformat.testing.HashingFn;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.Values;
@@ -58,23 +53,108 @@ public class HIFIOWithCassandraTest implements Serializable {
   private static final String CASSANDRA_KEYSPACE = "beamdb";
   private static final String CASSANDRA_HOST = "127.0.0.1";
   private static final String CASSANDRA_TABLE = "scientists";
-  private static final String CASSANDRA_THRIFT_PORT_PROPERTY="cassandra.input.thrift.port";
-  private static final String CASSANDRA_THRIFT_ADDRESS_PROPERTY="cassandra.input.thrift.address";
-  private static final String CASSANDRA_PARTITIONER_CLASS_PROPERTY="cassandra.input.partitioner.class";
-  private static final String CASSANDRA_PARTITIONER_CLASS_VALUE="Murmur3Partitioner";
-  private static final String CASSANDRA_KEYSPACE_PROPERTY="cassandra.input.keyspace";
-  private static final String CASSANDRA_COLUMNFAMILY_PROPERTY="cassandra.input.columnfamily";
-  private static final String CASSANDRA_PORT="9061";
-  private static final String OUTPUT_WRITE_FILE_PATH = "output";
-  private static List<String> expectedList = new ArrayList<>();
+  private static final String CASSANDRA_THRIFT_PORT_PROPERTY = "cassandra.input.thrift.port";
+  private static final String CASSANDRA_THRIFT_ADDRESS_PROPERTY = "cassandra.input.thrift.address";
+  private static final String CASSANDRA_PARTITIONER_CLASS_PROPERTY =
+      "cassandra.input.partitioner.class";
+  private static final String CASSANDRA_PARTITIONER_CLASS_VALUE = "Murmur3Partitioner";
+  private static final String CASSANDRA_KEYSPACE_PROPERTY = "cassandra.input.keyspace";
+  private static final String CASSANDRA_COLUMNFAMILY_PROPERTY = "cassandra.input.columnfamily";
+  private static final String CASSANDRA_PORT = "9061";
   private static transient Cluster cluster;
   private static transient Session session;
-
+  private static long TEST_DATA_ROW_COUNT = 10L;
   // Setting Cassandra embedded server startup timeout to 2 min
   private static final long CASSANDRA_SERVER_STARTUP_TIMEOUT = 120000L;
 
   @Rule
   public final transient TestPipeline p = TestPipeline.create();
+
+  /**
+   * Test to read data from embedded Cassandra instance and verify whether data is read
+   * successfully.
+   */
+  @Test
+  public void testHIFReadForCassandra() throws Exception {
+    // Expected hashcode is evaluated during insertion time one time and hardcoded here.
+    String expectedHashCode = "4651110ba1ef2cd3a7315091ca27877b18fceb0e";
+    Pipeline p = TestPipeline.create();
+    Configuration conf = getConfiguration();
+    SimpleFunction<Row, String> myValueTranslate = new SimpleFunction<Row, String>() {
+      @Override
+      public String apply(Row input) {
+        String scientistRecord = input.getInt("id") + "|" + input.getString("scientist");
+        return scientistRecord;
+      }
+    };
+    PCollection<KV<Long, String>> cassandraData =
+        p.apply(HadoopInputFormatIO.<Long, String>read().withConfiguration(conf)
+            .withValueTranslation(myValueTranslate));
+    // Verify the count of data retrieved from Cassandra matches expected count.
+    PAssert.thatSingleton(cassandraData.apply("Count", Count.<KV<Long, String>>globally()))
+        .isEqualTo(TEST_DATA_ROW_COUNT);
+    PCollection<String> textValues = cassandraData.apply(Values.<String>create());
+    // Verify the output values using checksum comparison.
+    PCollection<String> consolidatedHashcode =
+        textValues.apply(Combine.globally(new HashingFn()).withoutDefaults());
+    PAssert.that(consolidatedHashcode).containsInAnyOrder(expectedHashCode);
+    p.run().waitUntilFinish();
+  }
+
+  /**
+   * Test to read data from embedded Cassandra instance based on query and verify whether data is
+   * read successfully.
+   */
+  @Test
+  public void testHIFReadForCassandraQuery() throws Exception {
+    Long expectedCount = 1L;
+    String expectedChecksum = "6a62f24ccce0713004889aec1cf226949482d188";
+    Pipeline p = TestPipeline.create();
+    Configuration conf = getConfiguration();
+    conf.set("cassandra.input.cql", "select * from " + CASSANDRA_KEYSPACE + "." + CASSANDRA_TABLE
+        + " where token(id) > ? and token(id) <= ? and scientist='Faraday1' allow filtering");
+    SimpleFunction<Row, String> myValueTranslate = new SimpleFunction<Row, String>() {
+      @Override
+      public String apply(Row input) {
+        String scientistRecord = input.getInt("id") + "|" + input.getString("scientist");
+        return scientistRecord;
+      }
+    };
+    PCollection<KV<Long, String>> cassandraData =
+        p.apply(HadoopInputFormatIO.<Long, String>read().withConfiguration(conf)
+            .withValueTranslation(myValueTranslate));
+    // Verify the count of data retrieved from Cassandra matches expected count.
+    PAssert.thatSingleton(cassandraData.apply("Count", Count.<KV<Long, String>>globally()))
+        .isEqualTo(expectedCount);
+    PCollection<String> textValues = cassandraData.apply(Values.<String>create());
+    // Verify the output values using checksum comparison.
+    PCollection<String> consolidatedHashcode =
+        textValues.apply(Combine.globally(new HashingFn()).withoutDefaults());
+    PAssert.that(consolidatedHashcode).containsInAnyOrder(expectedChecksum);
+    p.run().waitUntilFinish();
+  }
+
+  /**
+   * Returns configuration of CqlInutFormat. Mandatory parameters required apart from inputformat
+   * class name, key class, value class are thrift port, thrift address, partitioner class, keyspace
+   * and columnfamily name
+   */
+  public Configuration getConfiguration() {
+    Configuration conf = new Configuration();
+    conf.set(CASSANDRA_THRIFT_PORT_PROPERTY, CASSANDRA_PORT);
+    conf.set(CASSANDRA_THRIFT_ADDRESS_PROPERTY, CASSANDRA_HOST);
+    conf.set(CASSANDRA_PARTITIONER_CLASS_PROPERTY, CASSANDRA_PARTITIONER_CLASS_VALUE);
+    conf.set(CASSANDRA_KEYSPACE_PROPERTY, CASSANDRA_KEYSPACE);
+    conf.set(CASSANDRA_COLUMNFAMILY_PROPERTY, CASSANDRA_TABLE);
+    conf.setClass(HadoopInputFormatIOConstants.INPUTFORMAT_CLASSNAME,
+        org.apache.cassandra.hadoop.cql3.CqlInputFormat.class, InputFormat.class);
+    conf.setClass(HadoopInputFormatIOConstants.KEY_CLASS, java.lang.Long.class, Object.class);
+    conf.setClass(HadoopInputFormatIOConstants.VALUE_CLASS, com.datastax.driver.core.Row.class,
+        Object.class);
+    return conf;
+  }
+
+  public static void dropTable() throws Exception {}
 
   @BeforeClass
   public static void startEmbeddedCassandra() throws Exception {
@@ -82,89 +162,19 @@ public class HIFIOWithCassandraTest implements Serializable {
         CASSANDRA_SERVER_STARTUP_TIMEOUT);
     cluster = Cluster.builder().addContactPoint(CASSANDRA_HOST).withClusterName("beam").build();
     session = cluster.connect();
-    createTable();
+    createCassandraData();
   }
 
-  public static void createTable() throws Exception {
+  public static void createCassandraData() throws Exception {
     session.execute("CREATE KEYSPACE " + CASSANDRA_KEYSPACE
         + " WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};");
     session.execute("USE " + CASSANDRA_KEYSPACE);
     session.execute("CREATE TABLE " + CASSANDRA_TABLE
         + "(id int, scientist text, PRIMARY KEY(id));");
-    session.execute("INSERT INTO " + CASSANDRA_TABLE + "(id, scientist) values(0, 'Faraday');");
-    expectedList.add("0|Faraday");
-    session.execute("INSERT INTO " + CASSANDRA_TABLE + "(id, scientist) values(1, 'Newton');");
-    expectedList.add("1|Newton");
-  }
-
-  /**
-   * Test to read data from embedded Cassandra instance and verify whether data is read
-   * successfully.
-   * @throws Exception
-   */
-  @Test
-  public void testHIFReadForCassandra() throws Exception {
-    Pipeline p = TestPipeline.create();
-    Configuration conf = getConfiguration();
-    SimpleFunction<Row, String> myValueTranslate = new SimpleFunction<Row, String>() {
-      @Override
-      public String apply(Row input) {
-        String scientistRecord=input.getInt("id")+"|"+input.getString("scientist");
-        return scientistRecord;
-      }
-    };
-    PCollection<KV<Long, String>> cassandraData = p
-                  .apply(HadoopInputFormatIO.<Long, String> read()
-                      .withConfiguration(conf)
-                      .withValueTranslation(myValueTranslate));
-    PAssert.thatSingleton(cassandraData.apply("Count", Count.<KV<Long, String>>globally()))
-        .isEqualTo(2L);
-
-    PCollection<String> textValues = cassandraData.apply(Values.<String>create());
-
-    // Write Pcollection of Strings to a file using TextIO Write transform.
-    textValues.apply(TextIO.Write.to(OUTPUT_WRITE_FILE_PATH).withNumShards(1).withSuffix("txt"));
-    PipelineResult result = p.run();
-    result.waitUntilFinish();
-
-    // Verify the output values using checksum comparison
-    HIFIOTextMatcher matcher =
-        new HIFIOTextMatcher(OUTPUT_WRITE_FILE_PATH + "-00000-of-00001.txt", expectedList);
-    assertThat(result, matcher);
-
-  }
-
-  /**
-   * Test to read data from embedded Cassandra instance based on query and verify whether data is
-   * read successfully.
-   *
-   * @throws Exception
-   */
-  @Test
-  public void testHIFReadForCassandraQuery() throws Exception {
-    Pipeline p = TestPipeline.create();
-    Configuration conf = getConfiguration();
-    conf.set("cassandra.input.cql", "select * from " + CASSANDRA_KEYSPACE + "." + CASSANDRA_TABLE
-        + " where token(id) > ? and token(id) <= ? and scientist='Newton' allow filtering");
-    SimpleFunction<Row, String> myValueTranslate = new SimpleFunction<Row, String>() {
-      @Override
-      public String apply(Row input) {
-        return input.getString("scientist");
-      }
-    };
-    PCollection<KV<Long, String>> cassandraData = p
-                  .apply(HadoopInputFormatIO.<Long, String>read()
-                      .withConfiguration(conf)
-                      .withValueTranslation(myValueTranslate));
-    PAssert.thatSingleton(cassandraData.apply("Count", Count.<KV<Long, String>>globally()))
-        .isEqualTo(1L);
-
-    p.run().waitUntilFinish();
-  }
-
-  public static void dropTable() throws Exception {
-    session.execute("Drop TABLE " + CASSANDRA_TABLE);
-    session.execute("Drop KEYSPACE " + CASSANDRA_KEYSPACE);
+    for (int i = 0; i < TEST_DATA_ROW_COUNT; i++) {
+      session.execute("INSERT INTO " + CASSANDRA_TABLE + "(id, scientist) values(" + i
+          + ", 'Faraday" + i + "');");
+    }
   }
 
   @AfterClass
@@ -201,25 +211,4 @@ public class HIFIOWithCassandraTest implements Serializable {
       return id + ":" + name;
     }
   }
-
-  /**
-   * Returns configuration of CqlInutFormat. Mandatory parameters required apart from inputformat
-   * class name, key class, value class are thrift port, thrift address, partitioner class, keyspace
-   * and columnfamily name
-   */
-  public Configuration getConfiguration() {
-    Configuration conf = new Configuration();
-    conf.set(CASSANDRA_THRIFT_PORT_PROPERTY, CASSANDRA_PORT );
-    conf.set(CASSANDRA_THRIFT_ADDRESS_PROPERTY, CASSANDRA_HOST);
-    conf.set(CASSANDRA_PARTITIONER_CLASS_PROPERTY, CASSANDRA_PARTITIONER_CLASS_VALUE);
-    conf.set(CASSANDRA_KEYSPACE_PROPERTY, CASSANDRA_KEYSPACE);
-    conf.set(CASSANDRA_COLUMNFAMILY_PROPERTY, CASSANDRA_TABLE);
-    conf.setClass(HadoopInputFormatIOConstants.INPUTFORMAT_CLASSNAME,
-        org.apache.cassandra.hadoop.cql3.CqlInputFormat.class, InputFormat.class);
-    conf.setClass(HadoopInputFormatIOConstants.KEY_CLASS, java.lang.Long.class, Object.class);
-    conf.setClass(HadoopInputFormatIOConstants.VALUE_CLASS, com.datastax.driver.core.Row.class,
-        Object.class);
-    return conf;
-  }
-
 }
