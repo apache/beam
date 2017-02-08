01efda4b3c5dc53ad90fd73fc41abb01de0f518f
Dipti Kulkarni <dipti_dkulkarni@persistent.co.in>
2017-02-08 10:45:01 +0530

    Changes suggested by Stephen- spacings, extra lines removed

--- sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/custom/options/HIFTestOptions.java
+++ sdks/java/io/hadoop-input-format/src/test/java/org/apache/beam/sdk/io/hadoop/inputformat/custom/options/HIFTestOptions.java
@@ -20,32 +20,25 @@ import org.apache.beam.sdk.testing.TestPipelineOptions;
 
 /**
  * Properties needed when using HadoopInputFormatIO with the Beam SDK.
- *
  */
 public interface HIFTestOptions extends TestPipelineOptions {
-
   @Description("Server IP")
   @Default.String("serverIp")
   String getServerIp();
-
   void setServerIp(String serverIp);
 
   @Description("Server port")
   @Default.Integer(0)
   Integer getServerPort();
-
   void setServerPort(Integer serverPort);
 
   @Description("User name")
   @Default.String("userName")
   String getUserName();
-
   void setUserName(String userName);
 
   @Description("Password")
   @Default.String("password")
   String getPassword();
-
   void setPassword(String password);
-
 }
