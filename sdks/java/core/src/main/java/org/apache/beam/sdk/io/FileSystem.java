ca13960bd08863e87d7d58fe280f486fe8585941
Pei He <peihe@google.com>
2017-02-08 16:08:26 -0800

    [BEAM-59] Beam FileSystem: initial set of methods and their implementation for local and gcs.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/FileSystem.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/FileSystem.java
@@ -17,6 +17,15 @@
  */
 package org.apache.beam.sdk.io;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Collection;
+import java.util.List;
+import org.apache.beam.sdk.io.fs.CreateOptions;
+import org.apache.beam.sdk.io.fs.ResourceId;
+
 /**
  * File system interface in Beam.
  *
@@ -25,5 +34,79 @@ package org.apache.beam.sdk.io;
  * <p>All methods are protected, and they are for file system providers to implement.
  * Clients should use {@link FileSystems} utility.
  */
-public abstract class FileSystem {
+public abstract class FileSystem<ResourceIdT extends ResourceId> {
+
+  /**
+   * Returns a write channel for the given {@link ResourceIdT}.
+   *
+   * <p>The resource is not expanded; it is used verbatim.
+   *
+   * @param resourceId the reference of the file-like resource to create
+   * @param createOptions the configuration of the create operation
+   */
+  protected abstract WritableByteChannel create(
+      ResourceIdT resourceId, CreateOptions createOptions) throws IOException;
+
+  /**
+   * Returns a read channel for the given {@link ResourceIdT}.
+   *
+   * <p>The resource is not expanded; it is used verbatim.
+   *
+   * <p>If seeking is supported, then this returns a
+   * {@link java.nio.channels.SeekableByteChannel}.
+   *
+   * @param resourceId the reference of the file-like resource to open
+   */
+  protected abstract ReadableByteChannel open(ResourceIdT resourceId) throws IOException;
+
+  /**
+   * Copies a {@link List} of file-like resources from one location to another.
+   *
+   * <p>The number of source resources must equal the number of destination resources.
+   * Destination resources will be created recursively.
+   *
+   * @param srcResourceIds the references of the source resources
+   * @param destResourceIds the references of the destination resources
+   *
+   * @throws FileNotFoundException if the source resources are missing. When copy throws,
+   * each resource might or might not be copied. In such scenarios, callers can use {@code match()}
+   * to determine the state of the resources.
+   */
+  protected abstract void copy(
+      List<ResourceIdT> srcResourceIds,
+      List<ResourceIdT> destResourceIds) throws IOException;
+
+  /**
+   * Renames a {@link List} of file-like resources from one location to another.
+   *
+   * <p>The number of source resources must equal the number of destination resources.
+   * Destination resources will be created recursively.
+   *
+   * @param srcResourceIds the references of the source resources
+   * @param destResourceIds the references of the destination resources
+   *
+   * @throws FileNotFoundException if the source resources are missing. When rename throws,
+   * the state of the resources is unknown but safe:
+   * for every (source, destination) pair of resources, the following are possible:
+   * a) source exists, b) destination exists, c) source and destination both exist.
+   * Thus no data is lost, however, duplicated resource are possible.
+   * In such scenarios, callers can use {@code match()} to determine the state of the resource.
+   */
+  protected abstract void rename(
+      List<ResourceIdT> srcResourceIds,
+      List<ResourceIdT> destResourceIds) throws IOException;
+
+  /**
+   * Deletes a collection of resources.
+   *
+   * <p>It is allowed but not recommended to delete directories recursively.
+   * Callers depends on {@link FileSystems} and uses {@code DeleteOptions}.
+   *
+   * @param resourceIds the references of the resources to delete.
+   *
+   * @throws FileNotFoundException if resources are missing. When delete throws,
+   * each resource might or might not be deleted. In such scenarios, callers can use {@code match()}
+   * to determine the state of the resources.
+   */
+  protected abstract void delete(Collection<ResourceIdT> resourceIds) throws IOException;
 }
