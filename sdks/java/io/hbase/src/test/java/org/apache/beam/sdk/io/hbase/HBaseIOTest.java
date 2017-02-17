8f2be281b84d3d47e5d7db8bf7c1f8d20013e209
Dan Halperin <dhalperi@google.com>
2017-02-17 09:10:41 -0800

    another hostname change

--- sdks/java/io/hbase/src/test/java/org/apache/beam/sdk/io/hbase/HBaseIOTest.java
+++ sdks/java/io/hbase/src/test/java/org/apache/beam/sdk/io/hbase/HBaseIOTest.java
@@ -99,6 +99,7 @@ public class HBaseIOTest {
         conf.setInt(HConstants.HBASE_CLIENT_RETRIES_NUMBER, 1);
         // Try to bind to localhost (this is apparently only for the web console)
         conf.setStrings("hbase.master.hostname", "localhost");
+        conf.setStrings("hbase.regionserver.hostname", "localhost");
         // Try to get a free port and bind the master port to it (HBase is supposed to already
         // do this)
         try (ServerSocket serverSocket = new ServerSocket(0)) {
