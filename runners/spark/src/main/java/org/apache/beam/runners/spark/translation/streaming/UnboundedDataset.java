9f143504f7ade5bb0a966dcef645a834277632ef
Sela <ansela@paypal.com>
2017-02-18 22:01:58 +0200

    Fix streaming translation of Flatten and Window, make CreateStream eager.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/streaming/UnboundedDataset.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/streaming/UnboundedDataset.java
@@ -18,22 +18,12 @@
 
 package org.apache.beam.runners.spark.translation.streaming;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
-import javax.annotation.Nullable;
-import org.apache.beam.runners.spark.coders.CoderHelpers;
 import org.apache.beam.runners.spark.translation.Dataset;
-import org.apache.beam.runners.spark.translation.WindowingHelpers;
-import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,14 +34,8 @@ import org.slf4j.LoggerFactory;
 public class UnboundedDataset<T> implements Dataset {
 
   private static final Logger LOG = LoggerFactory.getLogger(UnboundedDataset.class);
-  private static final AtomicInteger queuedStreamIds = new AtomicInteger();
 
-  // only set if creating a DStream from a static collection
-  @Nullable private transient JavaStreamingContext jssc;
-
-  private Iterable<Iterable<T>> values;
-  private Coder<T> coder;
-  private JavaDStream<WindowedValue<T>> dStream;
+  private final JavaDStream<WindowedValue<T>> dStream;
   // points to the input streams that created this UnboundedDataset,
   // should be greater > 1 in case of Flatten for example.
   // when using GlobalWatermarkHolder this information helps to take only the relevant watermarks
@@ -63,41 +47,11 @@ public class UnboundedDataset<T> implements Dataset {
     this.streamSources.addAll(streamSources);
   }
 
-  public UnboundedDataset(Iterable<Iterable<T>> values, JavaStreamingContext jssc, Coder<T> coder) {
-    this.values = values;
-    this.jssc = jssc;
-    this.coder = coder;
-    // QueuedStream will have a negative (decreasing) unique id.
-    this.streamSources.add(queuedStreamIds.decrementAndGet());
-  }
-
-  @VisibleForTesting
-  public static void resetQueuedStreamIds() {
-    queuedStreamIds.set(0);
-  }
-
-  @SuppressWarnings("ConstantConditions")
   JavaDStream<WindowedValue<T>> getDStream() {
-    if (dStream == null) {
-      WindowedValue.ValueOnlyWindowedValueCoder<T> windowCoder =
-          WindowedValue.getValueOnlyCoder(coder);
-      // create the DStream from queue
-      Queue<JavaRDD<WindowedValue<T>>> rddQueue = new LinkedBlockingQueue<>();
-      for (Iterable<T> v : values) {
-        Iterable<WindowedValue<T>> windowedValues =
-            Iterables.transform(v, WindowingHelpers.<T>windowValueFunction());
-        JavaRDD<WindowedValue<T>> rdd = jssc.sc().parallelize(
-            CoderHelpers.toByteArrays(windowedValues, windowCoder)).map(
-            CoderHelpers.fromByteFunction(windowCoder));
-        rddQueue.offer(rdd);
-      }
-      // create DStream from queue, one at a time.
-      dStream = jssc.queueStream(rddQueue, true);
-    }
     return dStream;
   }
 
-  public List<Integer> getStreamSources() {
+  List<Integer> getStreamSources() {
     return streamSources;
   }
 
