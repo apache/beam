314f338db94e1c298a8e4a313bc10ff7009663ab
Ahmet Altay <altay@google.com>
2017-02-19 00:15:16 -0800

    Add wildcard import back again.

--- sdks/python/apache_beam/io/google_cloud_platform/internal/clients/storage/__init__.py
+++ sdks/python/apache_beam/io/google_cloud_platform/internal/clients/storage/__init__.py
@@ -20,6 +20,7 @@
 
 import pkgutil
 
+from apitools.base.py import *
 from apache_beam.io.google_cloud_platform.internal.clients.storage.storage_v1_client import *
 from apache_beam.io.google_cloud_platform.internal.clients.storage.storage_v1_messages import *
 
