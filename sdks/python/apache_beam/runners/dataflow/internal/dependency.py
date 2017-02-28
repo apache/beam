f118232bb13e5ed8b9813bc2cc4af427c63e9486
Valentyn Tymofieiev <valentyn@google.com>
2017-02-27 16:23:47 -0800

    Clean up the info message.

--- sdks/python/apache_beam/runners/dataflow/internal/dependency.py
+++ sdks/python/apache_beam/runners/dataflow/internal/dependency.py
@@ -382,7 +382,7 @@ def stage_job_resources(
           raise RuntimeError('Cannot find default Dataflow SDK tar file "%s"',
                              sdk_path)
         elif not setup_options.sdk_location:
-          logging.info('Dataflow SDK will not be staged since '
+          logging.info('SDK will not be staged since '
                        '--sdk_location is empty.')
         else:
           raise RuntimeError(
