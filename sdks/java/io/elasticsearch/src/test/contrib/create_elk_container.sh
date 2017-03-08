bf06e3021fa6f8200514a1feb36591b245d51e09
Etienne Chauchot <echauchot@gmail.com>
2017-03-08 09:54:57 +0100

    remove ES v5 related script.

--- sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
+++ sdks/java/io/elasticsearch/src/test/contrib/create_elk_container.sh
@@ -22,16 +22,3 @@
 #bind then on host ports, allow shell access to container and mount current directory on /home/$USER inside the container
 
 docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-2.4  sebp/elk:es240_l240_k460
-
-
-#Create an ELK (Elasticsearch Logstash Kibana) container for ES v5.0 and compatible Logstash and Kibana versions,
-#bind then on host ports, allow shell access to container and mount current directory on /home/$USER inside the container
-
-docker create -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -p 9300:9300 -it -v $(pwd):/home/$USER/ --name elk-5.0  sebp/elk:es500_l500_k500
-
-# set system vm.max_map_count to the minimal value acceptable by ES 5.0
-echo "increasing virtual memory for ES 5.0 ..."
-sudo sysctl -w vm.max_map_count=262144
-cp /etc/sysctl.conf /tmp/sysctl.conf
-echo "vm.max_map_count=262144" >> /tmp/sysctl.conf
-sudo cp /tmp/sysctl.conf /etc/sysctl.conf
