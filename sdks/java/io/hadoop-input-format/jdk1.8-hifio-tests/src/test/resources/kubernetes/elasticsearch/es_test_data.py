5d627e1cf80b1e76b9af64479b77e0bababec1de
Dipti Kulkarni <dipti_dkulkarni@persistent.co.in>
2017-03-09 18:44:32 +0530

    Changes in resources files, added apache license , comments , renamed files as per Stephens comments, added comment to pom for child module

--- sdks/java/io/hadoop-input-format/jdk1.8-hifio-tests/src/test/resources/kubernetes/elasticsearch/es_test_data.py
+++ sdks/java/io/hadoop-input-format/jdk1.8-hifio-tests/src/test/resources/kubernetes/elasticsearch/es_test_data.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Script to load data on Elasticsearch instance.
+
 #!/usr/bin/python
 
 import json
