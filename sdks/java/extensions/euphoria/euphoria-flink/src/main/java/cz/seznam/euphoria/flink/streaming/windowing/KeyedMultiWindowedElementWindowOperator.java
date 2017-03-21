4a602287ef22678a440e59771660c104b5fff22d
xitep <xitep@users.noreply.github.com>
2017-03-21 16:49:58 +0100

    #47 [euphoria-flink] Alternatively execute windowing/valueOf fully after shuffle

---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/KeyedMultiWindowedElementWindowOperator.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2016 Seznam.cz, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.seznam.euphoria.flink.streaming.windowing;
+
+import cz.seznam.euphoria.core.client.dataset.windowing.Window;
+import cz.seznam.euphoria.core.client.dataset.windowing.Windowing;
+import cz.seznam.euphoria.core.client.functional.CombinableReduceFunction;
+import cz.seznam.euphoria.core.client.functional.StateFactory;
+import cz.seznam.euphoria.core.client.operator.state.State;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+
+/**
+ * A {@link cz.seznam.euphoria.flink.streaming.windowing.AbstractWindowOperator}
+ * expecting the input elements to be already of type {@link KeyedMultiWindowedElement}.
+ */
+public class KeyedMultiWindowedElementWindowOperator<KEY, WID extends Window>
+        extends AbstractWindowOperator<KeyedMultiWindowedElement<WID, KEY, ?>, KEY, WID> {
+
+    public KeyedMultiWindowedElementWindowOperator(
+            Windowing<?, WID> windowing,
+            StateFactory<?, State> stateFactory,
+            CombinableReduceFunction<State> stateCombiner,
+            boolean localMode) {
+        super(windowing, stateFactory, stateCombiner, localMode);
+    }
+
+    @Override
+    protected KeyedMultiWindowedElement<WID, KEY, ?>
+    recordValue(StreamRecord<KeyedMultiWindowedElement<WID, KEY, ?>> record) {
+        return record.getValue();
+    }
+}
---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/WindowedElementWindowOperator.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2016 Seznam.cz, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.seznam.euphoria.flink.streaming.windowing;
+
+import cz.seznam.euphoria.core.client.dataset.windowing.Window;
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowedElement;
+import cz.seznam.euphoria.core.client.dataset.windowing.Windowing;
+import cz.seznam.euphoria.core.client.functional.CombinableReduceFunction;
+import cz.seznam.euphoria.core.client.functional.StateFactory;
+import cz.seznam.euphoria.core.client.operator.state.State;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+
+import java.util.Objects;
+
+/**
+ * An {@link AbstractWindowOperator} implementation which expects input
+ * elements to be of type {@link WindowedElement} and transforms these
+ * into {@link KeyedMultiWindowedElement} on the fly.
+ */
+public class WindowedElementWindowOperator<KEY, WID extends Window>
+        extends AbstractWindowOperator<WindowedElement<WID, ?>, KEY, WID> {
+
+    MapFunction<WindowedElement<WID, ?>, KeyedMultiWindowedElement<WID, KEY, ?>> mapper;
+
+    public WindowedElementWindowOperator(
+            MapFunction<WindowedElement<WID, ?>, KeyedMultiWindowedElement<WID, KEY, ?>> mapper,
+            Windowing<?, WID> windowing,
+            StateFactory<?, State> stateFactory,
+            CombinableReduceFunction<State> stateCombiner,
+            boolean localMode) {
+        super(windowing, stateFactory, stateCombiner, localMode);
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    @Override
+    protected KeyedMultiWindowedElement<WID, KEY, ?>
+    recordValue(StreamRecord<WindowedElement<WID, ?>> record) throws Exception {
+        return mapper.map(record.getValue());
+    }
+}
