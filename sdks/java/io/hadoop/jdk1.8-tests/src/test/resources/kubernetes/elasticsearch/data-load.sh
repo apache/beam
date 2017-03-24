a256902d1775e77aa377c2d9588ca4a3dfee99d3
Radhika S Kulkarni <radhika_kulkarni1@persistent.co.in>
2017-03-24 17:55:04 +0530

    Addressed Stephen's comments on the Kubernetes elastic scripts.

--- sdks/java/io/hadoop/jdk1.8-tests/src/test/resources/kubernetes/elasticsearch/data-load.sh
+++ sdks/java/io/hadoop/jdk1.8-tests/src/test/resources/kubernetes/elasticsearch/data-load.sh
@@ -15,10 +15,15 @@
 # limitations under the License.
 #!/bin/sh
 
-# Identify external IP of the pod
+# Identify external IP
 external_ip="$(kubectl get svc elasticsearch -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+while [ -z "$external_ip" ]
+do
+   sleep 10s
+   external_ip="$(kubectl get svc elasticsearch -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+done
 echo "External IP - $external_ip"
 echo
 
-#run the script
+# Run the script
 /usr/bin/python es_test_data.py --count=1000 --format=Txn_ID:int,Item_Code:int,Item_ID:int,User_Name:str,last_updated:ts,Price:int,Title:str,Description:str,Age:int,Item_Name:str,Item_Price:int,Availability:bool,Batch_Num:int,Last_Ordered:tstxt,City:text --es_url=http://$external_ip:9200 &
