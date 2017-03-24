54a7de13dc36cf3894cb1d34f4e3869a550e801a
Thomas Groh <tgroh@google.com>
2017-03-24 15:14:27 -0700

    Explicitly duplicate input PCollections in Dataflow

--- runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/PTransformMatchersTest.java
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/PTransformMatchersTest.java
@@ -24,6 +24,7 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
 import java.io.Serializable;
 import java.util.Collections;
 import org.apache.beam.sdk.coders.VarIntCoder;
@@ -383,6 +384,72 @@ public class PTransformMatchersTest implements Serializable {
                     p);
 
     assertThat(PTransformMatchers.emptyFlatten().matches(application), is(false));
+  }
+
+  @Test
+  public void flattenWithDuplicateInputsWithoutDuplicates() {
+    AppliedPTransform application =
+        AppliedPTransform
+            .<PCollectionList<Object>, PCollection<Object>, Flatten.PCollections<Object>>
+                of(
+                "Flatten",
+                Collections.singletonList(
+                    TaggedPValue.of(
+                        new TupleTag<Object>(),
+                        PCollection.createPrimitiveOutputInternal(
+                            p, WindowingStrategy.globalDefault(), IsBounded.BOUNDED))),
+                Collections.singletonList(
+                    TaggedPValue.of(
+                        new TupleTag<Object>(),
+                        PCollection.createPrimitiveOutputInternal(
+                            p, WindowingStrategy.globalDefault(), IsBounded.BOUNDED))),
+                Flatten.pCollections(),
+                p);
+
+    assertThat(PTransformMatchers.flattenWithDuplicateInputs().matches(application), is(false));
+  }
+
+  @Test
+  public void flattenWithDuplicateInputsWithDuplicates() {
+    PCollection<Object> duplicate = PCollection.createPrimitiveOutputInternal(p,
+        WindowingStrategy.globalDefault(),
+        IsBounded.BOUNDED);
+    AppliedPTransform application =
+        AppliedPTransform
+            .<PCollectionList<Object>, PCollection<Object>, Flatten.PCollections<Object>>of(
+                "Flatten",
+                ImmutableList.of(
+                    TaggedPValue.of(new TupleTag<Object>(), duplicate),
+                    TaggedPValue.of(new TupleTag<Object>(), duplicate)),
+                Collections.singletonList(
+                    TaggedPValue.of(
+                        new TupleTag<Object>(),
+                        PCollection.createPrimitiveOutputInternal(
+                            p, WindowingStrategy.globalDefault(), IsBounded.BOUNDED))),
+                Flatten.pCollections(),
+                p);
+
+    assertThat(PTransformMatchers.flattenWithDuplicateInputs().matches(application), is(true));
+  }
+
+  @Test
+  public void flattenWithDuplicateInputsNonFlatten() {
+    AppliedPTransform application =
+        AppliedPTransform
+            .<PCollection<Iterable<Object>>, PCollection<Object>, Flatten.Iterables<Object>>
+                of(
+                "EmptyFlatten",
+                Collections.<TaggedPValue>emptyList(),
+                Collections.singletonList(
+                    TaggedPValue.of(
+                        new TupleTag<Object>(),
+                        PCollection.createPrimitiveOutputInternal(
+                            p, WindowingStrategy.globalDefault(), IsBounded.BOUNDED))),
+                Flatten.iterables() /* This isn't actually possible to construct,
+                                 * but for the sake of example */,
+                p);
+
+    assertThat(PTransformMatchers.flattenWithDuplicateInputs().matches(application), is(false));
   }
 
   @Test
