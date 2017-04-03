12d7c12110657eb38c7b6c83301aa13abb828b3b
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2017-04-03 08:45:47 +0200

    #! [euphoria-flink] Do not cache states

--- sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/WindowedStorageProvider.java
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/streaming/windowing/WindowedStorageProvider.java
@@ -18,6 +18,7 @@ package cz.seznam.euphoria.flink.streaming.windowing;
 import cz.seznam.euphoria.core.client.dataset.windowing.Window;
 import cz.seznam.euphoria.core.client.operator.state.ListStorage;
 import cz.seznam.euphoria.core.client.operator.state.ListStorageDescriptor;
+import cz.seznam.euphoria.core.client.operator.state.StorageDescriptor;
 import cz.seznam.euphoria.core.client.operator.state.StorageProvider;
 import cz.seznam.euphoria.core.client.operator.state.ValueStorage;
 import cz.seznam.euphoria.core.client.operator.state.ValueStorageDescriptor;
@@ -26,11 +27,16 @@ import cz.seznam.euphoria.flink.storage.FlinkListStorage;
 import cz.seznam.euphoria.flink.storage.FlinkReducingValueStorage;
 import cz.seznam.euphoria.flink.storage.FlinkValueStorage;
 import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.common.state.ReducingState;
 import org.apache.flink.api.common.state.ReducingStateDescriptor;
+import org.apache.flink.api.common.state.StateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.runtime.state.KeyedStateBackend;
+
+import java.util.IdentityHashMap;
 
 /**
  * Storage provider using flink's state API. All states are namespaced by window.
@@ -41,6 +47,8 @@ class WindowedStorageProvider<WID extends Window> implements StorageProvider {
   private final TypeSerializer<WID> windowSerializer;
   private Window window;
 
+  private final IdentityHashMap<StorageDescriptor, StateDescriptor>
+          storageToStateDescriptors = new IdentityHashMap<>();
 
   public WindowedStorageProvider(KeyedStateBackend keyedStateBackend,
                                  TypeSerializer<WID> windowSerializer) {
@@ -57,18 +65,20 @@ class WindowedStorageProvider<WID extends Window> implements StorageProvider {
   public <T> ValueStorage<T> getValueStorage(ValueStorageDescriptor<T> descriptor) {
     try {
       if (descriptor instanceof ValueStorageDescriptor.MergingValueStorageDescriptor) {
-        ReducingStateDescriptor<T> flinkDescriptor = Descriptors.from(
-                (ValueStorageDescriptor.MergingValueStorageDescriptor<T>) descriptor);
+        ReducingStateDescriptor<T> flinkDescriptor =
+                (ReducingStateDescriptor) storageToStateDescriptors.computeIfAbsent(
+                        descriptor, k -> Descriptors.from((ValueStorageDescriptor.MergingValueStorageDescriptor<T>) k));
 
         return new FlinkReducingValueStorage<>((ReducingState)
-                keyedStateBackend.getPartitionedState(window,
-                        windowSerializer, flinkDescriptor),
+                keyedStateBackend.getPartitionedState(window, windowSerializer, flinkDescriptor),
                 descriptor.getDefaultValue(),
                 window);
       } else {
+        ValueStateDescriptor<T> flinkDescriptor =
+                (ValueStateDescriptor<T>) storageToStateDescriptors.computeIfAbsent(
+                        descriptor, k -> Descriptors.from((ValueStorageDescriptor<T>) k));
         return new FlinkValueStorage<>((ValueState)
-                keyedStateBackend.getPartitionedState(window,
-                        windowSerializer, Descriptors.from(descriptor)),
+                keyedStateBackend.getPartitionedState(window, windowSerializer, flinkDescriptor),
                 window);
       }
     } catch (Exception e) {
@@ -80,9 +90,11 @@ class WindowedStorageProvider<WID extends Window> implements StorageProvider {
   @SuppressWarnings("unchecked")
   public <T> ListStorage<T> getListStorage(ListStorageDescriptor<T> descriptor) {
     try {
+      ListStateDescriptor<T> flinkDescriptor =
+              (ListStateDescriptor<T>) storageToStateDescriptors.computeIfAbsent(
+                      descriptor, k -> Descriptors.from((ListStorageDescriptor<T>) k));
       return new FlinkListStorage<>((ListState)
-              keyedStateBackend.getPartitionedState(window,
-              windowSerializer, Descriptors.from(descriptor)),
+              keyedStateBackend.getPartitionedState(window, windowSerializer, flinkDescriptor),
               window);
     } catch (Exception e) {
       throw new RuntimeException(e);
