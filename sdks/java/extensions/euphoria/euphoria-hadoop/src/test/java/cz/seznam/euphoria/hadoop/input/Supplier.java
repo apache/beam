e86bebc0a192104230970ed4e92eba4cf58fc942
xitep <xitep@users.noreply.github.com>
2017-04-03 12:44:43 +0200

    #! Drop Supplier from public API

--- sdks/java/extensions/euphoria/euphoria-hadoop/src/test/java/cz/seznam/euphoria/hadoop/input/Supplier.java
+++ sdks/java/extensions/euphoria/euphoria-hadoop/src/test/java/cz/seznam/euphoria/hadoop/input/Supplier.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cz.seznam.euphoria.core.client.functional;
+package cz.seznam.euphoria.hadoop.input;
 
 import java.io.Serializable;
 
