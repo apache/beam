dc97e1e0e3f5d046bb138c8089d784bfa9979070
MOLIG004 <guy.molinari@disney.com>
2017-04-07 07:30:00 -0700

    More review cleanup, etc.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerIO.java
@@ -5,11 +5,11 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.api.client.util.BackOff;
 import com.google.api.client.util.BackOffUtils;
 import com.google.api.client.util.Sleeper;
+import com.google.cloud.spanner.AbortedException;
 import com.google.cloud.spanner.DatabaseClient;
 import com.google.cloud.spanner.DatabaseId;
 import com.google.cloud.spanner.Mutation;
 import com.google.cloud.spanner.Spanner;
-import com.google.cloud.spanner.SpannerException;
 import com.google.cloud.spanner.SpannerOptions;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
@@ -50,7 +50,10 @@ public class SpannerIO {
  * <p>For example:
  *
  * <pre> {@code
- * // Write data to a table.
+ *
+ * Write data to a table.  Input read from {@link TextIO} will be parsed
+ * and transformed to a {@link PCollection} of {@link Mutation}s.
+ *
  * PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
  * options.getInput = <CSV text file location>;
  * PCollection<Mutation> mutations = ...;
@@ -58,14 +61,13 @@ public class SpannerIO {
  * Pipeline p = Pipeline.create(options);
  *   p.apply(TextIO.Read.from(options.getInput()))
  *       .apply(mutations)
- *       .apply(SpannerIO.writeTo(options.getProjectId(),
- *           options.getInstanceId(), options.getDatabaseId()));
+ *       .apply(SpannerIO.writeTo(options.getInstanceId(), options.getDatabaseId()));
  *   p.run();
  *
  * } </pre>
  */
-  public static Writer writeTo(String projectId, String instanceId, String databaseId) {
-    return new Writer(projectId, instanceId, databaseId, SPANNER_MUTATIONS_PER_COMMIT_LIMIT);
+  public static Writer writeTo(String instanceId, String databaseId) {
+    return new Writer(instanceId, databaseId, SPANNER_MUTATIONS_PER_COMMIT_LIMIT);
   }
 
   /**
@@ -75,13 +77,11 @@ public class SpannerIO {
    */
   public static class Writer extends PTransform<PCollection<Mutation>, PDone> {
 
-    private final String projectId;
     private final String instanceId;
     private final String databaseId;
     private int batchSize;
 
-    Writer(String projectId, String instanceId, String databaseId, int batchSize) {
-      this.projectId = projectId;
+    Writer(String instanceId, String databaseId, int batchSize) {
       this.instanceId = instanceId;
       this.databaseId = databaseId;
       this.batchSize = batchSize;
@@ -89,23 +89,22 @@ public class SpannerIO {
 
     /**
      * Returns a new {@link Write} with a limit on the number of mutations per batch.
-     * Defaults to SPANNER_MUTATIONS_PER_COMMIT_LIMIT.
+     * Defaults to {@link SpannerIO#SPANNER_MUTATIONS_PER_COMMIT_LIMIT}.
      */
     public Writer withBatchSize(Integer batchSize) {
-      return new Writer(projectId, instanceId, databaseId, batchSize);
+      return new Writer(instanceId, databaseId, batchSize);
     }
 
     @Override
     public PDone expand(PCollection<Mutation> input) {
       input.apply("Write mutations to Spanner", ParDo.of(
-              new SpannerWriterFn(projectId, instanceId, databaseId, batchSize)));
+              new SpannerWriterFn(instanceId, databaseId, batchSize)));
 
       return PDone.in(input.getPipeline());
     }
 
     @Override
     public void validate(PCollection<Mutation> input) {
-      checkNotNull(projectId, "projectId");
       checkNotNull(instanceId, "instanceId");
       checkNotNull(databaseId, "databaseId");
     }
@@ -113,7 +112,6 @@ public class SpannerIO {
     @Override
     public String toString() {
       return MoreObjects.toStringHelper(getClass())
-          .add("projectId", projectId)
           .add("instanceId", instanceId)
           .add("databaseId", databaseId)
           .toString();
@@ -123,16 +121,10 @@ public class SpannerIO {
     public void populateDisplayData(DisplayData.Builder builder) {
       super.populateDisplayData(builder);
       builder
-          .addIfNotNull(DisplayData.item("projectId", projectId)
-              .withLabel("Output Project"))
           .addIfNotNull(DisplayData.item("instanceId", instanceId)
               .withLabel("Output Instance"))
           .addIfNotNull(DisplayData.item("databaseId", databaseId)
               .withLabel("Output Database"));
-    }
-
-    public String getProjectId() {
-      return projectId;
     }
 
     public String getInstanceId() {
@@ -164,7 +156,6 @@ public class SpannerIO {
   static class SpannerWriterFn<T, V> extends DoFn<Mutation, Void> {
     private static final Logger LOG = LoggerFactory.getLogger(SpannerWriterFn.class);
     private transient Spanner spanner;
-    private final String projectId;
     private final String instanceId;
     private final String databaseId;
     private final int batchSize;
@@ -178,8 +169,7 @@ public class SpannerIO {
             .withMaxRetries(MAX_RETRIES).withInitialBackoff(Duration.standardSeconds(5));
 
     @VisibleForTesting
-    SpannerWriterFn(String projectId, String instanceId, String databaseId, int batchSize) {
-      this.projectId = checkNotNull(projectId, "projectId");
+    SpannerWriterFn(String instanceId, String databaseId, int batchSize) {
       this.instanceId = checkNotNull(instanceId, "instanceId");
       this.databaseId = checkNotNull(databaseId, "databaseId");
       this.batchSize = batchSize;
@@ -189,7 +179,8 @@ public class SpannerIO {
     public void setup() throws Exception {
         SpannerOptions options = SpannerOptions.newBuilder().build();
         spanner = options.getService();
-        dbClient = getDbClient(spanner, DatabaseId.of(projectId, instanceId, databaseId));
+        dbClient = spanner.getDatabaseClient(
+            DatabaseId.of(options.getProjectId(), instanceId, databaseId));
     }
 
     @ProcessElement
@@ -224,10 +215,10 @@ public class SpannerIO {
      * If the retry limit is exceeded, the last exception from Cloud Spanner will be
      * thrown.
      *
-     * @throws SpannerException if the commit fails or IOException or InterruptedException if
+     * @throws AbortedException if the commit fails or IOException or InterruptedException if
      * backing off between retries fails.
      */
-    private void flushBatch() throws SpannerException, IOException, InterruptedException {
+    private void flushBatch() throws AbortedException, IOException, InterruptedException {
       LOG.debug("Writing batch of {} mutations", mutations.size());
       Sleeper sleeper = Sleeper.DEFAULT;
       BackOff backoff = BUNDLE_WRITE_BACKOFF.backoff();
@@ -239,7 +230,7 @@ public class SpannerIO {
 
           // Break if the commit threw no exception.
           break;
-        } catch (SpannerException exception) {
+        } catch (AbortedException exception) {
           // Only log the code and message for potentially-transient errors. The entire exception
           // will be propagated upon the last retry.
           LOG.error("Error writing to Spanner ({}): {}", exception.getCode(),
@@ -258,29 +249,10 @@ public class SpannerIO {
     public void populateDisplayData(Builder builder) {
       super.populateDisplayData(builder);
       builder
-          .addIfNotNull(DisplayData.item("projectId", projectId)
-              .withLabel("Project"))
           .addIfNotNull(DisplayData.item("instanceId", instanceId)
               .withLabel("Instance"))
           .addIfNotNull(DisplayData.item("databaseId", databaseId)
               .withLabel("Database"));
-    }
-  }
-
-  private static DatabaseClient getDbClient(Spanner spanner, DatabaseId databaseId)
-          throws IOException {
-
-    try {
-      String clientProject = spanner.getOptions().getProjectId();
-      if (!databaseId.getInstanceId().getProject().equals(clientProject)) {
-        String err = "Invalid project specified. Project in the database id should match"
-            + "the project name set in the environment variable GCLOUD_PROJECT. Expected: "
-            + clientProject;
-        throw new IllegalArgumentException(err);
-      }
-      return spanner.getDatabaseClient(databaseId);
-    } catch (Exception e) {
-        throw new IOException(e);
     }
   }
 }
