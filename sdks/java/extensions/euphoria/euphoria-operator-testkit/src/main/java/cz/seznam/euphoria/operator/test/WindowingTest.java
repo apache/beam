467759cfa774b26dff33dffdf9fcc933e031916b
xitep <xitep@users.noreply.github.com>
2017-04-08 11:31:30 +0200

    # [euphoria-flink] Fix trigger state clean-up of merged windows
    * #70 Add (failing) proof-of-problem test
    
    * #70 [euphoria-flink] Fix trigger state clean-up of merged windows
    
    * #70 [euphoria-inmem] Limit window-trigger-test to stream processing only

--- sdks/java/extensions/euphoria/euphoria-operator-testkit/src/main/java/cz/seznam/euphoria/operator/test/WindowingTest.java
+++ sdks/java/extensions/euphoria/euphoria-operator-testkit/src/main/java/cz/seznam/euphoria/operator/test/WindowingTest.java
@@ -16,8 +16,11 @@
 package cz.seznam.euphoria.operator.test;
 
 import cz.seznam.euphoria.core.client.dataset.Dataset;
+import cz.seznam.euphoria.core.client.dataset.windowing.MergingWindowing;
+import cz.seznam.euphoria.core.client.dataset.windowing.Session;
 import cz.seznam.euphoria.core.client.dataset.windowing.Time;
 import cz.seznam.euphoria.core.client.dataset.windowing.TimeInterval;
+import cz.seznam.euphoria.core.client.dataset.windowing.WindowedElement;
 import cz.seznam.euphoria.core.client.dataset.windowing.Windowing;
 import cz.seznam.euphoria.core.client.io.Context;
 import cz.seznam.euphoria.core.client.operator.Distinct;
@@ -29,19 +32,26 @@ import cz.seznam.euphoria.core.client.operator.state.State;
 import cz.seznam.euphoria.core.client.operator.state.StorageProvider;
 import cz.seznam.euphoria.core.client.operator.state.ValueStorage;
 import cz.seznam.euphoria.core.client.operator.state.ValueStorageDescriptor;
+import cz.seznam.euphoria.core.client.triggers.TimeTrigger;
+import cz.seznam.euphoria.core.client.triggers.Trigger;
+import cz.seznam.euphoria.core.client.triggers.TriggerContext;
 import cz.seznam.euphoria.core.client.util.Pair;
 import cz.seznam.euphoria.core.client.util.Sums;
 import cz.seznam.euphoria.core.client.util.Triple;
 import cz.seznam.euphoria.operator.test.junit.AbstractOperatorTest;
 import cz.seznam.euphoria.operator.test.junit.Processing;
 import cz.seznam.euphoria.shaded.guava.com.google.common.base.Preconditions;
+import cz.seznam.euphoria.shaded.guava.com.google.common.collect.Lists;
 import cz.seznam.euphoria.shaded.guava.com.google.common.collect.Sets;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Tests capabilities of {@link Windowing}
@@ -254,5 +264,161 @@ public class WindowingTest extends AbstractOperatorTest {
     private int compare(Object obj1, Object obj2) {
       return ((Comparable) obj1).compareTo(obj2);
     }
+  }
+
+  static class CSession<T> implements MergingWindowing<T, TimeInterval>, Trigger<TimeInterval> {
+
+    static final ValueStorageDescriptor<Integer> TR_STATE
+            = ValueStorageDescriptor.of("quux", Integer.class, 0, (x, y) -> x + y);
+
+    private final TimeTrigger trigger = new TimeTrigger();
+    private final Session<T> wrap;
+
+    CSession(Duration gap) {
+      wrap = Session.of(gap);
+    }
+
+    @Override
+    public Collection<Pair<Collection<TimeInterval>, TimeInterval>> mergeWindows(Collection<TimeInterval> actives) {
+      return wrap.mergeWindows(actives);
+    }
+
+    @Override
+    public Iterable<TimeInterval> assignWindowsToElement(WindowedElement<?, T> el) {
+      return wrap.assignWindowsToElement(el);
+    }
+
+    @Override
+    public Trigger<TimeInterval> getTrigger() {
+      return this;
+    }
+
+    @Override
+    public TriggerResult onElement(long time, TimeInterval window, TriggerContext ctx) {
+      return trigger.onElement(time, window, ctx);
+    }
+
+    @Override
+    public TriggerResult onTimer(long time, TimeInterval window, TriggerContext ctx) {
+      return trigger.onTimer(time, window, ctx);
+    }
+
+    @Override
+    public void onClear(TimeInterval window, TriggerContext ctx) {
+      trigger.onClear(window, ctx);
+    }
+
+    @Override
+    public TriggerResult onMerge(TimeInterval window, TriggerContext.TriggerMergeContext ctx) {
+      return trigger.onMerge(window, ctx);
+    }
+  }
+
+  static final AtomicBoolean ON_CLEAR_VALIDATED = new AtomicBoolean(false);
+
+  /**
+   * Validates a trigger's #onClear method operates in the right context of
+   * merged windows.<p>
+   *
+   * A trigger's lifecycle is guaranteed only on stream processing; batch
+   * processing has more freedom and doesn't necessarily invoke the
+   * trigger#onClear method.
+   */
+  @Processing(Processing.Type.UNBOUNDED)
+  @Test
+  public void testSessionWindowingTriggerStateConsistency() {
+    ON_CLEAR_VALIDATED.set(false);
+    execute(new AbstractTestCase<Pair<Instant, String>, Triple<Instant, Instant, Integer>>() {
+      @SuppressWarnings("unchecked")
+      @Override
+      protected Dataset<Triple<Instant, Instant, Integer>>
+      getOutput(Dataset<Pair<Instant, String>> input) {
+        CSession windowing = new CSession(Duration.ofMinutes(5)) {
+          @Override
+          public TriggerResult onElement(long time, TimeInterval window, TriggerContext ctx) {
+            ValueStorage<Integer> str = ctx.getValueStorage(CSession.TR_STATE);
+            str.set(str.get() + 1);
+            return super.onElement(time, window, ctx);
+          }
+
+          @Override
+          public TriggerResult onMerge(TimeInterval window, TriggerContext.TriggerMergeContext ctx) {
+            ctx.mergeStoredState(CSession.TR_STATE);
+            return super.onMerge(window, ctx);
+          }
+
+          @Override
+          public TriggerResult onTimer(long time, TimeInterval window, TriggerContext ctx) {
+            assertTrState(window, ctx);
+            return super.onTimer(time, window, ctx);
+          }
+
+          @Override
+          public void onClear(TimeInterval window, TriggerContext ctx) {
+            // ~ 7 minutes is the size of the final target window
+            if (window.getDurationMillis() == Duration.ofMinutes(7).toMillis()) {
+              assertTrState(window, ctx);
+              if (!ON_CLEAR_VALIDATED.compareAndSet(false, true)) {
+                fail("!ON_CLEAR_VALIDATED!");
+              }
+            }
+            ctx.getValueStorage(TR_STATE).clear();
+            super.onClear(window, ctx);
+          }
+
+          private void assertTrState(TimeInterval window, TriggerContext ctx) {
+            ValueStorage<Integer> str = ctx.getValueStorage(CSession.TR_STATE);
+            assertEquals(3, str.get().intValue());
+          }
+        };
+
+        Dataset<Pair<String, Integer>> pairs =
+                ReduceByKey.of(input)
+                .keyBy(e -> "")
+                .valueBy(e -> 1)
+                .combineBy(Sums.ofInts())
+                .windowBy(windowing, t -> t.getFirst().toEpochMilli())
+                .output();
+
+        // extract window timestamp
+        return FlatMap.of(pairs)
+                .using((Pair<String, Integer> in, Context<Triple<Instant, Instant, Integer>> out) -> {
+                  long windowBegin = ((TimeInterval) out.getWindow()).getStartMillis();
+                  long windowEnd = ((TimeInterval) out.getWindow()).getEndMillis();
+                  out.collect(Triple.of(
+                          Instant.ofEpochMilli(windowBegin),
+                          Instant.ofEpochMilli(windowEnd),
+                          in.getSecond()));
+                })
+                .output();
+      }
+
+      @Override
+      protected Partitions<Pair<Instant, String>> getInput() {
+        return Partitions.add(
+                Pair.of(Instant.parse("2016-12-19T10:10:00.000Z"), "foo"),
+                Pair.of(Instant.parse("2016-12-19T10:11:00.000Z"), "foo"),
+                Pair.of(Instant.parse("2016-12-19T10:12:00.000Z"), "foo"))
+                .build();
+      }
+
+      @Override
+      public int getNumOutputPartitions() {
+        return 1;
+      }
+
+      @SuppressWarnings("unchecked")
+      @Override
+      public void validate(Partitions partitions) {
+        assertEquals(1, partitions.size());
+        assertEquals(
+                Lists.newArrayList(Triple.of(
+                        Instant.parse("2016-12-19T10:10:00.000Z"),
+                        Instant.parse("2016-12-19T10:17:00.000Z"),
+                        3)),
+                partitions.get(0));
+      }
+    });
+    assertEquals(true, ON_CLEAR_VALIDATED.get());
   }
 }
