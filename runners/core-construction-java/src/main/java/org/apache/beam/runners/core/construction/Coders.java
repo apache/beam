48ec9f20c403cbea06c08bd15ab94e8949beed2e
Thomas Groh <tgroh@google.com>
2017-04-10 16:29:58 -0700

    squash! Add Coder utilities for Proto conversions
    Add Known Coders

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/Coders.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/Coders.java
@@ -19,29 +19,72 @@
 package org.apache.beam.runners.core.construction;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.BytesValue;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.beam.sdk.coders.ByteArrayCoder;
 import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.IterableCoder;
+import org.apache.beam.sdk.coders.KvCoder;
+import org.apache.beam.sdk.coders.VarIntCoder;
+import org.apache.beam.sdk.coders.VarLongCoder;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.Components;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.FunctionSpec;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.SdkFunctionSpec;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
+import org.apache.beam.sdk.transforms.windowing.IntervalWindow.IntervalWindowCoder;
 import org.apache.beam.sdk.util.CloudObject;
 import org.apache.beam.sdk.util.Serializer;
+import org.apache.beam.sdk.util.WindowedValue;
+import org.apache.beam.sdk.util.WindowedValue.FullWindowedValueCoder;
 
 /** Utilities for working with {@link Coder Coders}. */
 public class Coders {
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
   // This URN says that the coder is just a UDF blob the indicated SDK understands
   // TODO: standardize such things
   public static final String CUSTOM_CODER_URN = "urn:beam:coders:javasdk:0.1";
 
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  // The URNs for coders are shared across languages
+  private static final BiMap<Class<? extends Coder>, String> KNOWN_CODER_URNS =
+      ImmutableBiMap.<Class<? extends Coder>, String>builder()
+          .put(ByteArrayCoder.class, "urn:beam:coders:bytes:0.1")
+          .put(KvCoder.class, "urn:beam:coders:kv:0.1")
+          .put(VarIntCoder.class, "urn:beam:coders:varint:0.1")
+          .put(IntervalWindowCoder.class, "urn:beam:coders:interval_window:0.1")
+          .put(IterableCoder.class, "urn:beam:coders:stream:0.1")
+          .put(GlobalWindow.Coder.class, "urn:beam:coders:global_window:0.1")
+          .put(FullWindowedValueCoder.class, "urn:beam:coders:windowed_value:0.1")
+          .build();
 
   public static RunnerApi.Coder toProto(
       Coder<?> coder, @SuppressWarnings("unused") SdkComponents components) throws IOException {
+    if (KNOWN_CODER_URNS.containsKey(coder.getClass())) {
+      return toKnownCoder(coder, components);
+    }
     return toCustomCoder(coder);
+  }
+
+  private static RunnerApi.Coder toKnownCoder(Coder<?> coder, SdkComponents components)
+      throws IOException {
+    List<String> componentIds = new ArrayList<>();
+    for (Coder<?> componentCoder : coder.getCoderArguments()) {
+      componentIds.add(components.registerCoder(componentCoder));
+    }
+    return RunnerApi.Coder.newBuilder()
+        .addAllComponentCoderIds(componentIds)
+        .setSpec(
+            SdkFunctionSpec.newBuilder()
+                .setSpec(FunctionSpec.newBuilder().setUrn(KNOWN_CODER_URNS.get(coder.getClass()))))
+        .build();
   }
 
   private static RunnerApi.Coder toCustomCoder(Coder<?> coder) throws IOException {
@@ -68,8 +111,37 @@ public class Coders {
     if (coderSpecUrn.equals(CUSTOM_CODER_URN)) {
       return fromCustomCoder(protoCoder, components);
     }
-    throw new IllegalArgumentException(
-        String.format("Unknown %s URN %s", Coder.class.getSimpleName(), coderSpecUrn));
+    return fromKnownCoder(protoCoder, components);
+  }
+
+  private static Coder<?> fromKnownCoder(RunnerApi.Coder coder, Components components)
+      throws IOException {
+    String coderUrn = coder.getSpec().getSpec().getUrn();
+    List<Coder<?>> coderComponents = new LinkedList<>();
+    for (String componentId : coder.getComponentCoderIdsList()) {
+      Coder<?> innerCoder = fromProto(components.getCodersOrThrow(componentId), components);
+      coderComponents.add(innerCoder);
+    }
+    switch (coderUrn) {
+      case "urn:beam:coders:bytes:0.1":
+        return ByteArrayCoder.of();
+      case "urn:beam:coders:kv:0.1":
+        return KvCoder.of(coderComponents);
+      case "urn:beam:coders:varint:0.1":
+        return VarLongCoder.of();
+      case "urn:beam:coders:interval_window:0.1":
+        return IntervalWindowCoder.of();
+      case "urn:beam:coders:stream:0.1":
+        return IterableCoder.of(coderComponents);
+      case "urn:beam:coders:global_window:0.1":
+        return GlobalWindow.Coder.INSTANCE;
+      case "urn:beam:coders:windowed_value:0.1":
+        return WindowedValue.FullWindowedValueCoder.of(coderComponents);
+      default:
+        throw new IllegalStateException(
+            String.format(
+                "Unknown coder URN %s. Known URNs: %s", coderUrn, KNOWN_CODER_URNS.values()));
+    }
   }
 
   private static Coder<?> fromCustomCoder(
