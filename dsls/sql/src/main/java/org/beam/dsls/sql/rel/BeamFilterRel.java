fba26af58c3e78e2d393c0212df02fab41798356
mingmxu <mingmxu@ebay.com>
2017-04-09 19:49:08 -0700

    [BEAM-301] Initial skeleton for Beam SQL

---
+++ dsls/sql/src/main/java/org/beam/dsls/sql/rel/BeamFilterRel.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.beam.dsls.sql.rel;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.core.Filter;
+import org.apache.calcite.rex.RexNode;
+import org.beam.dsls.sql.interpreter.BeamSQLExpressionExecutor;
+import org.beam.dsls.sql.interpreter.BeamSQLSpELExecutor;
+import org.beam.dsls.sql.planner.BeamPipelineCreator;
+import org.beam.dsls.sql.planner.BeamSQLRelUtils;
+import org.beam.dsls.sql.schema.BeamSQLRow;
+import org.beam.dsls.sql.transform.BeamSQLFilterFn;
+
+/**
+ * BeamRelNode to replace a {@code Filter} node.
+ *
+ */
+public class BeamFilterRel extends Filter implements BeamRelNode {
+
+  public BeamFilterRel(RelOptCluster cluster, RelTraitSet traits, RelNode child,
+      RexNode condition) {
+    super(cluster, traits, child, condition);
+  }
+
+  @Override
+  public Filter copy(RelTraitSet traitSet, RelNode input, RexNode condition) {
+    return new BeamFilterRel(getCluster(), traitSet, input, condition);
+  }
+
+  @Override
+  public Pipeline buildBeamPipeline(BeamPipelineCreator planCreator) throws Exception {
+
+    RelNode input = getInput();
+    BeamSQLRelUtils.getBeamRelInput(input).buildBeamPipeline(planCreator);
+
+    String stageName = BeamSQLRelUtils.getStageName(this);
+
+    PCollection<BeamSQLRow> upstream = planCreator.getLatestStream();
+
+    BeamSQLExpressionExecutor executor = new BeamSQLSpELExecutor(this);
+
+    PCollection<BeamSQLRow> projectStream = upstream.apply(stageName,
+        ParDo.of(new BeamSQLFilterFn(getRelTypeName(), executor)));
+
+    planCreator.setLatestStream(projectStream);
+
+    return planCreator.getPipeline();
+  }
+
+}
