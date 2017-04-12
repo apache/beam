67ff5bb04b39ee0a5f65dcbdebac307af6d91453
xitep <xitep@users.noreply.github.com>
2017-04-12 15:42:21 +0200

    #! Update copyright

--- sdks/java/extensions/euphoria/euphoria-core/src/test/java/cz/seznam/euphoria/core/client/operator/RepartitionTest.java
+++ sdks/java/extensions/euphoria/euphoria-core/src/test/java/cz/seznam/euphoria/core/client/operator/RepartitionTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Seznam.cz, a.s.
+ * Copyright 2016-2017 Seznam.cz, a.s.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
