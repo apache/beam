9f966e60bb29b65588a7aed5267d53e375853887
Uwe Jugel <uwe.jugel@lovoo.com>
2017-04-12 14:56:50 +0200

    add temp dataset location for non-query BigQuerySource

--- sdks/python/apache_beam/io/gcp/bigquery.py
+++ sdks/python/apache_beam/io/gcp/bigquery.py
@@ -588,7 +588,7 @@ class BigQueryReader(dataflow_io.NativeSourceReader):
 
   def __enter__(self):
     self.client = BigQueryWrapper(client=self.test_bigquery_client)
-    self.client.create_temporary_dataset(self.executing_project)
+    self.client.create_temporary_dataset(self.executing_project, self.source.table_reference)
     return self
 
   def __exit__(self, exception_type, exception_value, traceback):
@@ -782,7 +782,7 @@ class BigQueryWrapper(object):
   @retry.with_exponential_backoff(
       num_retries=MAX_RETRIES,
       retry_filter=retry.retry_on_server_errors_and_timeout_filter)
-  def get_or_create_dataset(self, project_id, dataset_id):
+  def get_or_create_dataset(self, project_id, dataset_id, location=None):
     # Check if dataset already exists otherwise create it
     try:
       dataset = self.client.datasets.Get(bigquery.BigqueryDatasetsGetRequest(
@@ -790,9 +790,11 @@ class BigQueryWrapper(object):
       return dataset
     except HttpError as exn:
       if exn.status_code == 404:
-        dataset = bigquery.Dataset(
-            datasetReference=bigquery.DatasetReference(
-                projectId=project_id, datasetId=dataset_id))
+        dr = bigquery.DatasetReference(
+            projectId=project_id, datasetId=dataset_id)
+        dataset = bigquery.Dataset(datasetReference=dr)
+        if location is not None:
+          dataset.location = location
         request = bigquery.BigqueryDatasetsInsertRequest(
             projectId=project_id, dataset=dataset)
         response = self.client.datasets.Insert(request)
@@ -848,8 +850,22 @@ class BigQueryWrapper(object):
   @retry.with_exponential_backoff(
       num_retries=MAX_RETRIES,
       retry_filter=retry.retry_on_server_errors_and_timeout_filter)
-  def create_temporary_dataset(self, project_id):
+  def create_temporary_dataset(self, project_id, source_table_reference=None):
     dataset_id = BigQueryWrapper.TEMP_DATASET + self._temporary_table_suffix
+    location = None
+
+    # Get region of source dataset
+    try:
+      tr = source_table_reference
+      if tr is not None:
+        if tr.projectId is None: table_project_id = project_id
+        else:                    table_project_id = tr.projectId
+
+        table = self._get_table(table_project_id, tr.datasetId, tr.tableId)
+        location = table.location
+        #TODO (uj): extract region from acquired table
+    except HttpError as exn: pass
+
     # Check if dataset exists to make sure that the temporary id is unique
     try:
       self.client.datasets.Get(bigquery.BigqueryDatasetsGetRequest(
@@ -864,7 +880,7 @@ class BigQueryWrapper(object):
         logging.warning(
             'Dataset %s:%s does not exist so we will create it as temporary',
             project_id, dataset_id)
-        self.get_or_create_dataset(project_id, dataset_id)
+        self.get_or_create_dataset(project_id, dataset_id, location=location)
       else:
         raise
 
