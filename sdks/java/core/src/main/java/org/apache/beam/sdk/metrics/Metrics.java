f4d3baf21593a512cde0560faefff860b2c1ae30
bchambers <bchambers@google.com>
2017-04-19 14:24:27 -0700

    Add javadoc to the Metric interfaces

--- sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/Metrics.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/Metrics.java
@@ -24,6 +24,33 @@ import org.apache.beam.sdk.annotations.Experimental.Kind;
 /**
  * The <code>Metrics</code> is a utility class for producing various kinds of metrics for
  * reporting properties of an executing pipeline.
+ *
+ * <p>Metrics are created by calling one of the static methods in this class. Each metric is
+ * associated with a namespace and a name. The namespace allows grouping related metrics together
+ * based on the definition while also disambiguating common names based on where they are defined.
+ *
+ * <p>Reported metrics are implicitly scoped to the transform within the pipeline that reported
+ * them. This allows reporting the same metric name in multiple places and identifying the value
+ * each transform reported, as well as aggregating the metric across
+ *
+ * <p>It is runner-dependent whether Metrics are accessible during pipeline execution or only after
+ * jobs have completed.
+ *
+ * <p>Example:
+ * <pre>{@code
+ * class SomeDoFn extends DoFn<String, String> {
+ *   private Counter counter = Metrics.counter(SomeDoFn.class, "my-counter");
+ *
+ *   {@literal @}ProcessElement
+ *   public void processElement(ProcessContext c) {
+ *     counter.inc();
+ *     Metrics.counter(SomeDoFn.class, "my-counter2").inc();
+ *   }
+ * }
+ * }</pre>
+ *
+ * <p>See {@link MetricResults} (available from the {@code PipelineResults} interface) for an
+ * example off how to query metrics.
  */
 @Experimental(Kind.METRICS)
 public class Metrics {
