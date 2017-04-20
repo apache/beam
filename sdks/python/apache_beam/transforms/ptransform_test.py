b0da056815f78a531fd6996a521d0e46bb634f47
Pablo <pabloem@google.com>
2017-04-20 11:31:04 -0700

    Reusing existing source.

--- sdks/python/apache_beam/transforms/ptransform_test.py
+++ sdks/python/apache_beam/transforms/ptransform_test.py
@@ -173,46 +173,7 @@ class PTransformTest(unittest.TestCase):
 
   @attr('ValidatesRunner')
   def test_read_from_text_metrics(self):
-    from apache_beam.io import iobase
-    from apache_beam.io.range_trackers import OffsetRangeTracker
-    class CountingSource(iobase.BoundedSource):
-
-      def __init__(self, count):
-        self.records_read = Metrics.counter(self.__class__, 'recordsRead')
-        self._count = count
-
-      def estimate_size(self):
-        return self._count
-
-      def get_range_tracker(self, start_position, stop_position):
-        if start_position is None:
-          start_position = 0
-        if stop_position is None:
-          stop_position = self._count
-        return OffsetRangeTracker(start_position, stop_position)
-
-      def read(self, range_tracker):
-        for i in range(self._count):
-          if not range_tracker.try_claim(i):
-            return
-          self.records_read.inc()
-          yield i
-
-      def split(self, desired_bundle_size, start_position=None,
-                stop_position=None):
-        if start_position is None:
-          start_position = 0
-        if stop_position is None:
-          stop_position = self._count
-
-        bundle_start = start_position
-        while bundle_start < self._count:
-          bundle_stop = max(self._count, bundle_start + desired_bundle_size)
-          yield iobase.SourceBundle(weight=(bundle_stop - bundle_start),
-                                    source=self,
-                                    start_position=bundle_start,
-                                    stop_position=bundle_stop)
-          bundle_start = bundle_stop
+    from apache_beam.examples.snippets.snippets import CountingSource
 
     class CounterDoFn(beam.DoFn):
       def __init__(self):
