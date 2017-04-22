7379db04fbdbed3f8460d991b7789b08d8334745
Stephen Sisk <sisk@google.com>
2017-04-21 17:23:55 -0700

    Add HadoopResourceId

--- sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
+++ sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
@@ -17,26 +17,71 @@
  */
 package org.apache.beam.sdk.io.hdfs;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import org.apache.beam.sdk.io.fs.ResolveOptions;
 import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.hadoop.fs.Path;
 
 /**
  * {@link ResourceId} implementation for the {@link HadoopFileSystem}.
  */
 public class HadoopResourceId implements ResourceId {
 
+  private final Path path;
+
+  /**
+   * Constructs a HadoopResourceId from the provided absolute path. If only a relative path is
+   * available, you can create a {@link HadoopResourceId} from the absolute path of the root of the
+   * server, and then use resolve to add the relative path to the root.
+   * @param path
+   * @return
+   */
+  public static HadoopResourceId fromPath(Path path) {
+    checkNotNull(path);
+    checkArgument(path.isAbsolute());
+    return new HadoopResourceId(path);
+  }
+
+  private HadoopResourceId(Path path) {
+    this.path = path;
+  }
+
   @Override
   public ResourceId resolve(String other, ResolveOptions resolveOptions) {
-    throw new UnsupportedOperationException();
+    checkArgument(
+        resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_FILE)
+            || resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_DIRECTORY),
+        String.format("ResolveOptions: [%s] is not supported. "
+            + "Supported ResolveOptions are RESOLVE_FILE and RESOLVE_DIRECTORY.", resolveOptions));
+    if (resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_FILE)) {
+      checkArgument(
+          !other.endsWith("/"),
+          "ResolveOptions: [%s] ends with '/', which is not supported for RESOLVE_FILE.",
+          other);
+    }
+    return new HadoopResourceId(new Path(path, other));
   }
 
   @Override
   public ResourceId getCurrentDirectory() {
+    // See BEAM-2069. Possible workaround: inject FileSystem into this class, and call
+    // org.apache.hadoop.fs.FileSystem#isDirectory.
     throw new UnsupportedOperationException();
   }
 
   @Override
   public String getScheme() {
-    throw new UnsupportedOperationException();
+    return path.toUri().getScheme();
+  }
+
+  public Path getPath() {
+    return path;
+  }
+
+  @Override
+  public String toString() {
+    return path.toString();
   }
 }
