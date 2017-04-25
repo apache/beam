46c4caf4bd188f0da752b5320c424a65521d1c02
Thomas Groh <tgroh@google.com>
2017-04-25 11:35:16 -0700

    Revert "Add HadoopResourceId"
    This reverts commit 512b2c0abb68c777c207dcec39de5c44e6ff34da.
    
    Breaks FindBugs, RAT

--- sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
+++ sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
@@ -17,69 +17,26 @@
  */
 package org.apache.beam.sdk.io.hdfs;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.apache.beam.sdk.io.fs.ResolveOptions;
 import org.apache.beam.sdk.io.fs.ResourceId;
-import org.apache.hadoop.fs.Path;
 
 /**
  * {@link ResourceId} implementation for the {@link HadoopFileSystem}.
  */
 public class HadoopResourceId implements ResourceId {
 
-  private final Path path;
-
-  /**
-   * Constructs a HadoopResourceId from the provided absolute path. If only a relative path is
-   * available, you can create a {@link HadoopResourceId} from the absolute path of the root of the
-   * server, and then use resolve to add the relative path to the root.
-   */
-  public static HadoopResourceId fromPath(Path path) {
-    checkNotNull(path, "path must not be null");
-    checkArgument(path.isAbsolute(), "path must be absolute");
-    return new HadoopResourceId(path);
-  }
-
-  private HadoopResourceId(Path path) {
-    this.path = path;
-  }
-
   @Override
   public ResourceId resolve(String other, ResolveOptions resolveOptions) {
-    checkArgument(
-        resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_FILE)
-            || resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_DIRECTORY),
-        String.format("ResolveOptions: [%s] is not supported. "
-            + "Supported ResolveOptions are RESOLVE_FILE and RESOLVE_DIRECTORY.", resolveOptions));
-    if (resolveOptions.equals(ResolveOptions.StandardResolveOptions.RESOLVE_FILE)) {
-      checkArgument(
-          !other.endsWith("/"),
-          "ResolveOptions: [%s] ends with '/', which is not supported for RESOLVE_FILE.",
-          other);
-    }
-    return new HadoopResourceId(new Path(path, other));
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public ResourceId getCurrentDirectory() {
-    // See BEAM-2069. Possible workaround: inject FileSystem into this class, and call
-    // org.apache.hadoop.fs.FileSystem#isDirectory.
     throw new UnsupportedOperationException();
   }
 
   @Override
   public String getScheme() {
-    return path.toUri().getScheme();
-  }
-
-  public Path getPath() {
-    return path;
-  }
-
-  @Override
-  public String toString() {
-    return path.toString();
+    throw new UnsupportedOperationException();
   }
 }
