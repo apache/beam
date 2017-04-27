8b86ba76534eed3476b4eead48dbab48698cab5f
Jára Vaněk <jaromir.vanek2@firma.seznam.cz>
2017-04-27 16:33:59 +0200

    #13 [euphoria-flink] Copy input instance in ReduceByKeyTranslator because of Flink's object reuse

---
+++ sdks/java/extensions/euphoria/euphoria-flink/src/main/java/cz/seznam/euphoria/flink/batch/TimestampedElement.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2016-2017 Seznam.cz, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.seznam.euphoria.flink.batch;
+
+final class TimestampedElement {
+
+  private long timestamp;
+  private Object el;
+
+  public TimestampedElement(long timestamp, Object el) {
+    this.timestamp = timestamp;
+    this.el = el;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public Object getElement() {
+    return el;
+  }
+
+  public void setElement(Object el) {
+    this.el = el;
+  }
+}
