eb860388a8626837655e82171c8480421384e419
Thomas Weise <thw@apache.org>
2017-04-29 01:17:22 -0700

    BEAM-2022 fix triggering for processing time timers

--- runners/apex/src/test/java/org/apache/beam/runners/apex/translation/operators/ApexTimerInternalsTest.java
+++ runners/apex/src/test/java/org/apache/beam/runners/apex/translation/operators/ApexTimerInternalsTest.java
@@ -22,8 +22,12 @@ import static org.junit.Assert.assertNotNull;
 
 import com.datatorrent.lib.util.KryoCloneUtils;
 import com.datatorrent.netlet.util.Slice;
-import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.apache.beam.runners.apex.translation.operators.ApexTimerInternals.TimerProcessor;
 import org.apache.beam.runners.core.StateNamespaces;
 import org.apache.beam.runners.core.TimerInternals.TimerData;
 import org.apache.beam.runners.core.TimerInternals.TimerDataCoder;
@@ -40,6 +44,15 @@ public class ApexTimerInternalsTest {
 
   @Test
   public void testEventTimeTimers() {
+
+    final Map<String, Collection<TimerData>> firedTimers = new HashMap<>();
+    TimerProcessor<String> timerProcessor = new TimerProcessor<String>() {
+      @Override
+      public void fireTimer(String key, Collection<TimerData> timerData) {
+        firedTimers.put(key, timerData);
+      }
+    };
+
     TimerDataCoder timerDataCoder = TimerDataCoder.of(GlobalWindow.Coder.INSTANCE);
     String key1 = "key1";
     Instant instant0 = new Instant(0);
@@ -57,17 +70,60 @@ public class ApexTimerInternalsTest {
         instant1, TimeDomain.EVENT_TIME);
     timerInternals.setTimer(timerData1);
 
-    Multimap<Slice, TimerData> timers = timerInternals.getTimersReadyToProcess(
-        instant0.getMillis());
-    assertEquals(0, timers.size());
+    timerInternals.fireReadyTimers(instant0.getMillis(), timerProcessor, TimeDomain.EVENT_TIME);
+    assertEquals(0, firedTimers.size());
+    firedTimers.clear();
 
-    timers = timerInternals.getTimersReadyToProcess(instant1.getMillis());
-    assertEquals(1, timers.size());
-    assertEquals(Sets.newHashSet(timerData0), Sets.newHashSet(timers.values()));
+    timerInternals.fireReadyTimers(instant1.getMillis(), timerProcessor,
+        TimeDomain.PROCESSING_TIME);
+    assertEquals(0, firedTimers.size());
+    timerInternals.fireReadyTimers(instant1.getMillis(), timerProcessor, TimeDomain.EVENT_TIME);
+    assertEquals(1, firedTimers.size());
+    assertEquals(Sets.newHashSet(timerData0),
+        Sets.newHashSet(firedTimers.values().iterator().next()));
+    firedTimers.clear();
 
-    timers = timerInternals.getTimersReadyToProcess(instant2.getMillis());
-    assertEquals(1, timers.size());
-    assertEquals(Sets.newHashSet(timerData1), Sets.newHashSet(timers.values()));
+    timerInternals.fireReadyTimers(instant2.getMillis(), timerProcessor, TimeDomain.EVENT_TIME);
+    assertEquals(1, firedTimers.size());
+    assertEquals(Sets.newHashSet(timerData1),
+        Sets.newHashSet(firedTimers.values().iterator().next()));
+    firedTimers.clear();
+  }
+
+  @Test
+  public void testDeleteTimer() {
+    TimerDataCoder timerDataCoder = TimerDataCoder.of(GlobalWindow.Coder.INSTANCE);
+    String key1 = "key1";
+    Instant instant0 = new Instant(0);
+    Instant instant1 = new Instant(1);
+
+    ApexTimerInternals<String> timerInternals = new ApexTimerInternals<>(timerDataCoder);
+    timerInternals.setContext(key1, StringUtf8Coder.of(), Instant.now());
+
+    TimerData timerData0 = TimerData.of("timerData0", StateNamespaces.global(),
+        instant0, TimeDomain.EVENT_TIME);
+    timerInternals.setTimer(timerData0);
+
+    TimerData timerData1 = TimerData.of("timerData1", StateNamespaces.global(),
+        instant1, TimeDomain.EVENT_TIME);
+    timerInternals.setTimer(timerData1);
+
+    Map<?, Set<Slice>> timerMap = timerInternals.getTimerSet(TimeDomain.EVENT_TIME).getMap();
+    assertEquals(1, timerMap.size());
+    assertEquals(2, timerMap.values().iterator().next().size());
+
+    timerInternals.deleteTimer(timerData0.getNamespace(), timerData0.getTimerId());
+    assertEquals(1, timerMap.size());
+    assertEquals(1, timerMap.values().iterator().next().size());
+
+    timerInternals.deleteTimer(timerData1.getNamespace(), timerData1.getTimerId(),
+        TimeDomain.PROCESSING_TIME);
+    assertEquals(1, timerMap.size());
+    assertEquals(1, timerMap.values().iterator().next().size());
+
+    timerInternals.deleteTimer(timerData1.getNamespace(), timerData1.getTimerId(),
+        TimeDomain.EVENT_TIME);
+    assertEquals(0, timerMap.size());
   }
 
   @Test
@@ -82,7 +138,7 @@ public class ApexTimerInternalsTest {
     ApexTimerInternals<String> cloned;
     assertNotNull("Serialization", cloned = KryoCloneUtils.cloneObject(timerInternals));
     cloned.setContext(key, StringUtf8Coder.of(), Instant.now());
-    Multimap<Slice, TimerData> timers = cloned.getTimersReadyToProcess(new Instant(1).getMillis());
+    Map<?, Set<Slice>> timers = cloned.getTimerSet(TimeDomain.EVENT_TIME).getMap();
     assertEquals(1, timers.size());
   }
 
