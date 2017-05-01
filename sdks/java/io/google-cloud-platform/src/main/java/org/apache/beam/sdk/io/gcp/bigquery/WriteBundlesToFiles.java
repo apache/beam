53daced87d00fc418412f590e5033c4421039296
Vikas Kedigehalli <vikasrk@google.com>
2017-04-30 23:29:20 -0700

    Remove IOChannelUtils from BigQuery TableRowWriter

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteBundlesToFiles.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteBundlesToFiles.java
@@ -140,7 +140,7 @@ class WriteBundlesToFiles extends DoFn<KV<TableDestination, TableRow>, WriteBund
   public void finishBundle(Context c) throws Exception {
     for (Map.Entry<TableDestination, TableRowWriter> entry : writers.entrySet()) {
       TableRowWriter.Result result = entry.getValue().close();
-      c.output(new Result(result.filename, result.byteSize, entry.getKey()));
+      c.output(new Result(result.resourceId.toString(), result.byteSize, entry.getKey()));
     }
     writers.clear();
   }
