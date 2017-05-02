e99a1598e03e86d29a7a9ce3b4bcdaca6a495742
Dan Halperin <dhalperi@google.com>
2017-05-02 11:08:16 -0700

    [BEAM-59] Move GcsFileSystem to gcp-core
    It is used by both runner and IO, so should be in core.

--- sdks/java/extensions/google-cloud-platform-core/src/test/java/org/apache/beam/sdk/extensions/gcp/storage/GcsResourceIdTest.java
+++ sdks/java/extensions/google-cloud-platform-core/src/test/java/org/apache/beam/sdk/extensions/gcp/storage/GcsResourceIdTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.sdk.io.gcp.storage;
+package org.apache.beam.sdk.extensions.gcp.storage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
