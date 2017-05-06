b25b800d8d35db15ddbbbc6ae8aadea780fb25fb
Robert Bradshaw <robertwb@gmail.com>
2017-05-05 17:35:35 -0700

    Remove en/decodeOuter and default encode/decode methods.
    Now only the context-free encode() and decode() methods are abstract.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/coders/Coder.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/coders/Coder.java
@@ -127,18 +127,6 @@ public abstract class Coder<T> implements Serializable {
 
   /**
    * Encodes the given value of type {@code T} onto the given output stream
-   * in the outer context.
-   *
-   * @throws IOException if writing to the {@code OutputStream} fails
-   * for some reason
-   * @throws CoderException if the value could not be encoded for some reason
-   */
-  @Deprecated
-  public abstract void encodeOuter(T value, OutputStream outStream)
-      throws CoderException, IOException;
-
-  /**
-   * Encodes the given value of type {@code T} onto the given output stream
    * in the given context.
    *
    * @throws IOException if writing to the {@code OutputStream} fails
@@ -146,8 +134,10 @@ public abstract class Coder<T> implements Serializable {
    * @throws CoderException if the value could not be encoded for some reason
    */
   @Deprecated
-  public abstract void encode(T value, OutputStream outStream, Context context)
-      throws CoderException, IOException;
+  public void encode(T value, OutputStream outStream, Context context)
+      throws CoderException, IOException {
+    encode(value, outStream);
+  }
 
   /**
    * Decodes a value of type {@code T} from the given input stream in
@@ -161,17 +151,6 @@ public abstract class Coder<T> implements Serializable {
 
   /**
    * Decodes a value of type {@code T} from the given input stream in
-   * the outer context.  Returns the decoded value.
-   *
-   * @throws IOException if reading from the {@code InputStream} fails
-   * for some reason
-   * @throws CoderException if the value could not be decoded for some reason
-   */
-  @Deprecated
-  public abstract T decodeOuter(InputStream inStream) throws CoderException, IOException;
-
-  /**
-   * Decodes a value of type {@code T} from the given input stream in
    * the given context.  Returns the decoded value.
    *
    * @throws IOException if reading from the {@code InputStream} fails
@@ -179,8 +158,10 @@ public abstract class Coder<T> implements Serializable {
    * @throws CoderException if the value could not be decoded for some reason
    */
   @Deprecated
-  public abstract T decode(InputStream inStream, Context context)
-      throws CoderException, IOException;
+  public T decode(InputStream inStream, Context context)
+      throws CoderException, IOException {
+    return decode(inStream);
+  }
 
   /**
    * If this is a {@code Coder} for a parameterized type, returns the
