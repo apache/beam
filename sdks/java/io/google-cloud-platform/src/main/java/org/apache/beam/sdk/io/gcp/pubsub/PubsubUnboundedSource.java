4064048868a626fb68920e2efe0adff60ffc0950
Robert Bradshaw <robertwb@gmail.com>
2017-05-08 10:48:40 -0700

    Reviewer comments + a couple of extra fixes. All compiles.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsub/PubsubUnboundedSource.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsub/PubsubUnboundedSource.java
@@ -371,28 +371,16 @@ public class PubsubUnboundedSource extends PTransform<PBegin, PCollection<Pubsub
     @Override
     public void encode(PubsubCheckpoint value, OutputStream outStream)
         throws IOException {
-      encode(value, outStream, Context.NESTED);
-    }
-
-    @Override
-    public void encode(PubsubCheckpoint value, OutputStream outStream, Context context)
-        throws IOException {
       SUBSCRIPTION_PATH_CODER.encode(
           value.subscriptionPath,
-          outStream,
-          context.nested());
-      LIST_CODER.encode(value.notYetReadIds, outStream, context);
+          outStream);
+      LIST_CODER.encode(value.notYetReadIds, outStream);
     }
 
     @Override
     public PubsubCheckpoint decode(InputStream inStream) throws IOException {
-      return decode(inStream, Context.NESTED);
-    }
-
-    @Override
-    public PubsubCheckpoint decode(InputStream inStream, Context context) throws IOException {
       String path = SUBSCRIPTION_PATH_CODER.decode(inStream);
-      List<String> notYetReadIds = LIST_CODER.decode(inStream, context);
+      List<String> notYetReadIds = LIST_CODER.decode(inStream);
       return new PubsubCheckpoint(path, null, null, notYetReadIds);
     }
   }
