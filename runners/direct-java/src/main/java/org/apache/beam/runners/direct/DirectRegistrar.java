7d0a5632e51d4bc0bd96cc685a15e4e0393a6221
Thomas Groh <tgroh@google.com>
2017-05-09 18:04:01 -0700

    Improve DirectRunner Javadoc

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/DirectRegistrar.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/DirectRegistrar.java
@@ -26,31 +26,31 @@ import org.apache.beam.sdk.runners.PipelineRunnerRegistrar;
 
 /**
  * Contains the {@link PipelineRunnerRegistrar} and {@link PipelineOptionsRegistrar} for the
- * {@link org.apache.beam.runners.direct.DirectRunner}.
+ * {@link DirectRunner}.
  */
 public class DirectRegistrar {
   private DirectRegistrar() {}
   /**
-   * Registers the {@link org.apache.beam.runners.direct.DirectRunner}.
+   * Registers the {@link DirectRunner}.
    */
   @AutoService(PipelineRunnerRegistrar.class)
   public static class Runner implements PipelineRunnerRegistrar {
     @Override
     public Iterable<Class<? extends PipelineRunner<?>>> getPipelineRunners() {
       return ImmutableList.<Class<? extends PipelineRunner<?>>>of(
-          org.apache.beam.runners.direct.DirectRunner.class);
+          DirectRunner.class);
     }
   }
 
   /**
-   * Registers the {@link org.apache.beam.runners.direct.DirectOptions}.
+   * Registers the {@link DirectOptions}.
    */
   @AutoService(PipelineOptionsRegistrar.class)
   public static class Options implements PipelineOptionsRegistrar {
     @Override
     public Iterable<Class<? extends PipelineOptions>> getPipelineOptions() {
       return ImmutableList.<Class<? extends PipelineOptions>>of(
-          org.apache.beam.runners.direct.DirectOptions.class);
+          DirectOptions.class);
     }
   }
 }
