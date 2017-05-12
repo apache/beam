f4d1aefe7fecd18345c23be9b409aece7a665e8e
James Xu <xumingmingv@gmail.com>
2017-05-13 01:07:01 +0800

    [BEAM-2149] Improved kafka table implemention.
    1. use robust CSV library to parse & print.
    2. support different data types rather than just `String`.
    3. a little cleanup for TextTable (to extract common methods).

--- dsls/sql/src/main/java/org/apache/beam/dsls/sql/schema/text/BeamTextCSVTableIOReader.java
+++ dsls/sql/src/main/java/org/apache/beam/dsls/sql/schema/text/BeamTextCSVTableIOReader.java
@@ -18,11 +18,10 @@
 
 package org.apache.beam.dsls.sql.schema.text;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.io.StringReader;
+import static org.apache.beam.dsls.sql.schema.BeamTableUtils.csvLine2BeamSQLRow;
 
-import org.apache.beam.dsls.sql.exception.BeamSqlUnsupportedException;
+import java.io.Serializable;
+
 import org.apache.beam.dsls.sql.schema.BeamSQLRecordType;
 import org.apache.beam.dsls.sql.schema.BeamSQLRow;
 import org.apache.beam.sdk.io.TextIO;
@@ -31,12 +30,7 @@ import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * IOReader for {@code BeamTextCSVTable}.
@@ -44,7 +38,6 @@ import org.slf4j.LoggerFactory;
 public class BeamTextCSVTableIOReader
     extends PTransform<PBegin, PCollection<BeamSQLRow>>
     implements Serializable {
-  private static final Logger LOG = LoggerFactory.getLogger(BeamTextCSVTableIOReader.class);
   private String filePattern;
   protected BeamSQLRecordType beamSqlRecordType;
   protected CSVFormat csvFormat;
@@ -63,61 +56,8 @@ public class BeamTextCSVTableIOReader
           @ProcessElement
           public void processElement(ProcessContext ctx) {
             String str = ctx.element();
-
-            try (StringReader reader = new StringReader(str)) {
-              CSVRecord rawRecord = null;
-              try {
-                CSVParser parser = csvFormat.parse(reader);
-                rawRecord = parser.getRecords().get(0);
-              } catch (IOException e) {
-                throw new IllegalArgumentException("Invalid text filePattern: " + filePattern, e);
-              }
-
-              BeamSQLRow row = new BeamSQLRow(beamSqlRecordType);
-              if (rawRecord.size() != beamSqlRecordType.size()) {
-                throw new IllegalArgumentException(String.format(
-                    "Invalid filePattern: {}, expect %d fields, but actually %d", str,
-                    filePattern, beamSqlRecordType.size(), rawRecord.size()
-                ));
-              } else {
-                for (int idx = 0; idx < beamSqlRecordType.size(); idx++) {
-                  String raw = rawRecord.get(idx);
-                  addFieldWithAutoTypeCasting(row, idx, raw);
-                }
-                ctx.output(row);
-              }
-            }
+            ctx.output(csvLine2BeamSQLRow(csvFormat, str, beamSqlRecordType));
           }
         }));
-  }
-
-  public void addFieldWithAutoTypeCasting(BeamSQLRow row, int idx, String raw) {
-    SqlTypeName columnType = row.getDataType().getFieldsType().get(idx);
-    switch (columnType) {
-      case TINYINT:
-        row.addField(idx, Byte.valueOf(raw));
-        break;
-      case SMALLINT:
-        row.addField(idx, Short.valueOf(raw));
-        break;
-      case INTEGER:
-        row.addField(idx, Integer.valueOf(raw));
-        break;
-      case BIGINT:
-        row.addField(idx, Long.valueOf(raw));
-        break;
-      case FLOAT:
-        row.addField(idx, Float.valueOf(raw));
-        break;
-      case DOUBLE:
-        row.addField(idx, Double.valueOf(raw));
-        break;
-      case VARCHAR:
-        row.addField(idx, raw);
-        break;
-      default:
-        throw new BeamSqlUnsupportedException(String.format(
-            "Column type %s is not supported yet!", columnType));
-    }
   }
 }
