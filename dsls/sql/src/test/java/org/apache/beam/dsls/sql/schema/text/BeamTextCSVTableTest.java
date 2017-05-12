f1e346899f9e15f0d8a26154971a951e91378d99
James Xu <xumingmingv@gmail.com>
2017-05-13 01:07:01 +0800

    [BEAM-2149] Improved kafka table implemention.
    1. use robust CSV library to parse & print.
    2. support different data types rather than just `String`.
    3. a little cleanup for TextTable (to extract common methods).

--- dsls/sql/src/test/java/org/apache/beam/dsls/sql/schema/text/BeamTextCSVTableTest.java
+++ dsls/sql/src/test/java/org/apache/beam/dsls/sql/schema/text/BeamTextCSVTableTest.java
@@ -31,7 +31,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.beam.dsls.sql.planner.BeamQueryPlanner;
 import org.apache.beam.dsls.sql.schema.BeamSQLRecordType;
@@ -75,7 +74,6 @@ public class BeamTextCSVTableTest {
       add(buildRow(data));
     }
   }};
-  private static ConcurrentLinkedQueue<Object[]> actualData = new ConcurrentLinkedQueue<>();
 
   private static Path tempFolder;
   private static File readerSourceFile;
