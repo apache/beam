d425b2792f754ed6150f7b47eddf743286a45401
Kenneth Knowles <klk@google.com>
2017-05-23 11:10:42 -0700

    Revise StepContext javadoc

--- runners/core-java/src/main/java/org/apache/beam/runners/core/StepContext.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/StepContext.java
@@ -18,7 +18,12 @@
 package org.apache.beam.runners.core;
 
 /**
- * Per-step, per-key context used for retrieving state.
+ * The context in which a specific step is executing, including access to state and timers.
+ *
+ * <p>This interface exists as the API between a runner and the support code, but is not user
+ * facing.
+ *
+ * <p>These will often be scoped to a particular step and key, though it is not required.
  */
 public interface StepContext {
 
