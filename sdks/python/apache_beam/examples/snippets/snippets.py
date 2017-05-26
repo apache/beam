58a92624acd6f7f9314b41874f6f47ff60dd3d79
Maria Garcia Herrero <mariagh@google.com>
2017-05-26 10:37:42 -0700

    Fix Jenkins test

--- sdks/python/apache_beam/examples/snippets/snippets.py
+++ sdks/python/apache_beam/examples/snippets/snippets.py
@@ -619,7 +619,7 @@ def examples_ptransforms_templated(renames):
   import apache_beam as beam
   from apache_beam.io import WriteToText
   from apache_beam.options.pipeline_options import PipelineOptions
-  from apache_beam.utils.value_provider import StaticValueProvider
+  from apache_beam.options.value_provider import StaticValueProvider
 
   class TemplatedUserOptions(PipelineOptions):
     @classmethod
