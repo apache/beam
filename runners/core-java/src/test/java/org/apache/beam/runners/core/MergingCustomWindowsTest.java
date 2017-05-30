65ae0f76f16eae3ba3f4ef5db7712ddef2897de5
Etienne Chauchot <echauchot@gmail.com>
2017-05-30 11:30:30 +0200

    Move class to correct package

--- runners/core-java/src/test/java/org/apache/beam/runners/core/MergingCustomWindowsTest.java
+++ runners/core-java/src/test/java/org/apache/beam/runners/core/MergingCustomWindowsTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.runners.core.triggers;
+package org.apache.beam.runners.core;
 
 import java.io.IOException;
 import java.io.InputStream;
