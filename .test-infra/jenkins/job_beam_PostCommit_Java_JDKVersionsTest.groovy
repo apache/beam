f7fbabbc479ef73fb6f10da9946ee169a3165978
Mark Liu <markliu@google.com>
2017-05-30 09:33:39 -0700

    fixup! Schedule job instead of trigger by PR

--- .test-infra/jenkins/job_beam_PostCommit_Java_JDKVersionsTest.groovy
+++ .test-infra/jenkins/job_beam_PostCommit_Java_JDKVersionsTest.groovy
@@ -34,7 +34,10 @@ matrixJob('beam_PostCommit_Java_JDK_Versions_Test') {
   }
 
   // Sets that this is a PostCommit job.
-  common_job_properties.setPostCommit(delegate)
+  common_job_properties.setPostCommit(
+      delegate,
+      '0 */6 * * *',
+      false)
 
   // Allows triggering this build against pull requests.
   common_job_properties.enablePhraseTriggeringFromPullRequest(
