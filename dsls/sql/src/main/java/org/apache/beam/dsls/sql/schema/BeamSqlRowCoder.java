c48b0cb3d0b9d47d3a7bf4fe6ce2ee4b544f903d
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-06-06 10:27:03 +0200

    [BEAM-2375] Rebase and upgrade to Beam 2.x

--- dsls/sql/src/main/java/org/apache/beam/dsls/sql/schema/BeamSqlRowCoder.java
+++ dsls/sql/src/main/java/org/apache/beam/dsls/sql/schema/BeamSqlRowCoder.java
@@ -27,7 +27,6 @@ import org.apache.beam.sdk.coders.BigDecimalCoder;
 import org.apache.beam.sdk.coders.BigEndianIntegerCoder;
 import org.apache.beam.sdk.coders.BigEndianLongCoder;
 import org.apache.beam.sdk.coders.Coder;
-import org.apache.beam.sdk.coders.CoderException;
 import org.apache.beam.sdk.coders.CustomCoder;
 import org.apache.beam.sdk.coders.DoubleCoder;
 import org.apache.beam.sdk.coders.InstantCoder;
@@ -54,9 +53,8 @@ public class BeamSqlRowCoder extends CustomCoder<BeamSQLRow> {
   }
 
   @Override
-  public void encode(BeamSQLRow value, OutputStream outStream,
-      org.apache.beam.sdk.coders.Coder.Context context) throws CoderException, IOException {
-    listCoder.encode(value.getNullFields(), outStream, context.nested());
+  public void encode(BeamSQLRow value, OutputStream outStream) throws IOException {
+    listCoder.encode(value.getNullFields(), outStream);
 
     for (int idx = 0; idx < value.size(); ++idx) {
       if (value.getNullFields().contains(idx)) {
@@ -65,36 +63,35 @@ public class BeamSqlRowCoder extends CustomCoder<BeamSQLRow> {
 
       switch (value.getDataType().getFieldsType().get(idx)) {
         case INTEGER:
-          intCoder.encode(value.getInteger(idx), outStream, context.nested());
+          intCoder.encode(value.getInteger(idx), outStream);
           break;
         case SMALLINT:
-          intCoder.encode((int) value.getShort(idx), outStream, context.nested());
+          intCoder.encode((int) value.getShort(idx), outStream);
           break;
         case TINYINT:
-          intCoder.encode((int) value.getByte(idx), outStream, context.nested());
+          intCoder.encode((int) value.getByte(idx), outStream);
           break;
         case DOUBLE:
-          doubleCoder.encode(value.getDouble(idx), outStream, context.nested());
+          doubleCoder.encode(value.getDouble(idx), outStream);
           break;
         case FLOAT:
-          doubleCoder.encode((double) value.getFloat(idx), outStream, context.nested());
+          doubleCoder.encode((double) value.getFloat(idx), outStream);
           break;
         case DECIMAL:
-          bigDecimalCoder.encode(value.getBigDecimal(idx), outStream, context.nested());
+          bigDecimalCoder.encode(value.getBigDecimal(idx), outStream);
           break;
         case BIGINT:
-          longCoder.encode(value.getLong(idx), outStream, context.nested());
+          longCoder.encode(value.getLong(idx), outStream);
           break;
         case VARCHAR:
         case CHAR:
-          stringCoder.encode(value.getString(idx), outStream, context.nested());
+          stringCoder.encode(value.getString(idx), outStream);
           break;
         case TIME:
-          longCoder.encode(value.getGregorianCalendar(idx).getTime().getTime(),
-              outStream, context.nested());
+          longCoder.encode(value.getGregorianCalendar(idx).getTime().getTime(), outStream);
           break;
         case TIMESTAMP:
-          longCoder.encode(value.getDate(idx).getTime(), outStream, context.nested());
+          longCoder.encode(value.getDate(idx).getTime(), outStream);
           break;
 
         default:
@@ -102,14 +99,13 @@ public class BeamSqlRowCoder extends CustomCoder<BeamSQLRow> {
       }
     }
 
-    instantCoder.encode(value.getWindowStart(), outStream, context.nested());
-    instantCoder.encode(value.getWindowEnd(), outStream, context);
+    instantCoder.encode(value.getWindowStart(), outStream);
+    instantCoder.encode(value.getWindowEnd(), outStream);
   }
 
   @Override
-  public BeamSQLRow decode(InputStream inStream, org.apache.beam.sdk.coders.Coder.Context context)
-      throws CoderException, IOException {
-    List<Integer> nullFields = listCoder.decode(inStream, context.nested());
+  public BeamSQLRow decode(InputStream inStream) throws IOException {
+    List<Integer> nullFields = listCoder.decode(inStream);
 
     BeamSQLRow record = new BeamSQLRow(tableSchema);
     record.setNullFields(nullFields);
@@ -121,37 +117,37 @@ public class BeamSqlRowCoder extends CustomCoder<BeamSQLRow> {
 
       switch (tableSchema.getFieldsType().get(idx)) {
         case INTEGER:
-          record.addField(idx, intCoder.decode(inStream, context.nested()));
+          record.addField(idx, intCoder.decode(inStream));
           break;
         case SMALLINT:
-          record.addField(idx, intCoder.decode(inStream, context.nested()).shortValue());
+          record.addField(idx, intCoder.decode(inStream).shortValue());
           break;
         case TINYINT:
-          record.addField(idx, intCoder.decode(inStream, context.nested()).byteValue());
+          record.addField(idx, intCoder.decode(inStream).byteValue());
           break;
         case DOUBLE:
-          record.addField(idx, doubleCoder.decode(inStream, context.nested()));
+          record.addField(idx, doubleCoder.decode(inStream));
           break;
         case FLOAT:
-          record.addField(idx, doubleCoder.decode(inStream, context.nested()).floatValue());
+          record.addField(idx, doubleCoder.decode(inStream).floatValue());
           break;
         case BIGINT:
-          record.addField(idx, longCoder.decode(inStream, context.nested()));
+          record.addField(idx, longCoder.decode(inStream));
           break;
         case DECIMAL:
-          record.addField(idx, bigDecimalCoder.decode(inStream, context.nested()));
+          record.addField(idx, bigDecimalCoder.decode(inStream));
           break;
         case VARCHAR:
         case CHAR:
-          record.addField(idx, stringCoder.decode(inStream, context.nested()));
+          record.addField(idx, stringCoder.decode(inStream));
           break;
         case TIME:
           GregorianCalendar calendar = new GregorianCalendar();
-          calendar.setTime(new Date(longCoder.decode(inStream, context.nested())));
+          calendar.setTime(new Date(longCoder.decode(inStream)));
           record.addField(idx, calendar);
           break;
         case TIMESTAMP:
-          record.addField(idx, new Date(longCoder.decode(inStream, context.nested())));
+          record.addField(idx, new Date(longCoder.decode(inStream)));
           break;
 
         default:
@@ -159,8 +155,8 @@ public class BeamSqlRowCoder extends CustomCoder<BeamSQLRow> {
       }
     }
 
-    record.setWindowStart(instantCoder.decode(inStream, context.nested()));
-    record.setWindowEnd(instantCoder.decode(inStream, context));
+    record.setWindowStart(instantCoder.decode(inStream));
+    record.setWindowEnd(instantCoder.decode(inStream));
 
     return record;
   }
