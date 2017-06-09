cae1ccd1cffe56d8f08c022f270f93b5fb247ea5
Robert Bradshaw <robertwb@google.com>
2017-06-09 16:44:55 -0700

    Actually test the fn_api_runner.
    The test suite was not being run due to a typo.
    Fix breakage due to changes in the code in the meantime.

--- sdks/python/apache_beam/runners/worker/sdk_worker.py
+++ sdks/python/apache_beam/runners/worker/sdk_worker.py
@@ -359,7 +359,7 @@ class SdkWorker(object):
               source=SideInputSource(
                   self.state_handler,
                   beam_fn_api_pb2.StateKey(
-                      function_spec_reference=si.view_fn.id),
+                      key=si.view_fn.id.encode('utf-8')),
                   coder=unpack_and_deserialize_py_fn(si.view_fn)))
         output_tags = list(transform.outputs.keys())
         spec = operation_specs.WorkerDoFn(
