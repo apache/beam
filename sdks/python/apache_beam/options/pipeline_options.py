88022ee5d7b7f86766f19cc4bcec3477c6db2635
Marian Dvorsky <mariand@google.com>
2017-06-16 16:03:21 +0200

    Fixed lint error.

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -466,13 +466,13 @@ class WorkerOptions(PipelineOptions):
         '--use_public_ips',
         default=None,
         action='store_true',
-        help='Whether to assign public IP addresses to the worker machines.')
+        help='Whether to assign public IP addresses to the worker VMs.')
     parser.add_argument(
         '--no_use_public_ips',
         dest='use_public_ips',
         default=None,
         action='store_false',
-        help='Whether to assign only private IP addresses to the worker machines.')
+        help='Whether to assign only private IP addresses to the worker VMs.')
 
   def validate(self, validator):
     errors = []
