a21a6d797777ad38b927bd5d44c63306d85a752a
Mairbek Khadikov <mairbek@google.com>
2017-06-19 13:28:52 -0700

    Read api with naive implementation

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerConfig.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerConfig.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.gcp.spanner;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -16,8 +33,6 @@ import org.apache.beam.sdk.transforms.display.DisplayData;
 /** Configuration for a Cloud Spanner client. */
 @AutoValue
 public abstract class SpannerConfig implements Serializable {
-
-  private static final long serialVersionUID = -5680874609304170301L;
 
   @Nullable
   abstract ValueProvider<String> getProjectId();
@@ -49,7 +64,7 @@ public abstract class SpannerConfig implements Serializable {
     return builder().build();
   }
 
-  public static Builder builder() {
+  static Builder builder() {
     return new AutoValue_SpannerConfig.Builder();
   }
 
@@ -79,13 +94,11 @@ public abstract class SpannerConfig implements Serializable {
   @AutoValue.Builder
   public abstract static class Builder {
 
-
     abstract Builder setProjectId(ValueProvider<String> projectId);
 
     abstract Builder setInstanceId(ValueProvider<String> instanceId);
 
     abstract Builder setDatabaseId(ValueProvider<String> databaseId);
-
 
     abstract Builder setServiceFactory(ServiceFactory<Spanner, SpannerOptions> serviceFactory);
 
@@ -115,4 +128,10 @@ public abstract class SpannerConfig implements Serializable {
   public SpannerConfig withDatabaseId(String databaseId) {
     return withDatabaseId(ValueProvider.StaticValueProvider.of(databaseId));
   }
+
+  @VisibleForTesting
+  SpannerConfig withServiceFactory(ServiceFactory<Spanner, SpannerOptions> serviceFactory) {
+    return toBuilder().setServiceFactory(serviceFactory).build();
+  }
+
 }
