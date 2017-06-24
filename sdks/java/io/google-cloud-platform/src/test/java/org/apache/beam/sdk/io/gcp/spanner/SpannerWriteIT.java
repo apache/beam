bfd16b42a95e478e70097cd28f859dcbaa9df22e
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-06-24 07:33:05 +0200

    Add commons-text to shaded jar to fix SpannerIO WriteIT

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerWriteIT.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerWriteIT.java
@@ -149,7 +149,9 @@ public class SpannerWriteIT {
 
   @After
   public void tearDown() throws Exception {
-    databaseAdminClient.dropDatabase(options.getInstanceId(), databaseName);
+    if (databaseAdminClient != null) {
+      databaseAdminClient.dropDatabase(options.getInstanceId(), databaseName);
+    }
     spanner.close();
   }
 
