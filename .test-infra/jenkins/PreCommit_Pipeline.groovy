33349b727de02b371dbe48e8bb1329dad9e9297c
Jason Kuster <jasonkuster@google.com>
2017-07-10 17:58:11 -0700

    update Python pipelines
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/PreCommit_Pipeline.groovy
+++ .test-infra/jenkins/PreCommit_Pipeline.groovy
@@ -1,35 +1,33 @@
 #!groovy
 import hudson.model.Result
 
+def ghprbArgs = [
+    string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
+    string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
+    string(name: 'ghprbPullId', value: "${ghprbPullId}")
+]
+
+def sha1Arg = [string(name: 'sha1', value: "origin/pr/${ghprbPullId}/head")]
+
 try {
     javaBuildNum = -1
     stage('Build') {
         parallel (
             java: {
-                def javaBuild = build job: 'beam_PreCommit_Java_Build', parameters: [
-                    string(name: 'sha1', value: "origin/pr/${ghprbPullId}/head"),
-                    string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                    string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                    string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                ]
+                def javaBuild = build job: 'beam_PreCommit_Java_Build', parameters: sha1Arg + ghprbArgs
                 if(javaBuild.getResult() == Result.SUCCESS.toString()) {
                     javaBuildNum = javaBuild.getNumber()
                 }
             }
         )
     }
+    javaBuildArg = [string(name: 'buildNum', value: "${javaBuildNum}")]
     javaUnitPassed = false
-    pythonUnitPassed = false
     stage('Unit Test / Code Health') {
         parallel (
             java_unit: {
                 if(javaBuildNum != -1) {
-                    def javaTest = build job: 'beam_PreCommit_Java_UnitTest', parameters: [
-                        string(name: 'buildNum', value: "${javaBuildNum}"),
-                        string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                        string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                        string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                    ]
+                    def javaTest = build job: 'beam_PreCommit_Java_UnitTest', parameters: javaBuildArg + ghprbArgs
                     if(javaTest.getResult() == Result.SUCCESS.toString()) {
                         javaUnitPassed = true
                     }
@@ -37,24 +35,11 @@ try {
             },
             java_codehealth: {
                 if(javaBuildNum != -1) {
-                    build job: 'beam_PreCommit_Java_CodeHealth', parameters: [
-                        string(name: 'buildNum', value: "${javaBuildNum}"),
-                        string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                        string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                        string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                    ]
+                    build job: 'beam_PreCommit_Java_CodeHealth', parameters: javaBuildArg + ghprbArgs
                 }
             },
             python_unit: {
-                def pythonTest = build job: 'beam_PreCommit_Python_UnitTest', parameters: [
-                    string(name: 'buildNum', value: "${pythonBuildNum}"),
-                    string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                    string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                    string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                ]
-                if(pythonTest.getResult() == Result.SUCCESS.toString()) {
-                    pythonUnitPassed = true
-                }
+                build job: 'beam_PreCommit_Python_UnitTest', parameters: sha1Arg + ghprbArgs
             }
         )
     }
@@ -62,22 +47,7 @@ try {
         parallel (
             java_integration: {
                 if(javaUnitPassed) {
-                    build job: 'beam_PreCommit_Java_IntegrationTest', parameters: [
-                        string(name: 'buildNum', value: "${javaBuildNum}"),
-                        string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                        string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                        string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                    ]
-                }
-            },
-            python_integration: {
-                if(pythonUnitPassed) {
-                    build job: 'beam_PreCommit_Python_IntegrationTest', parameters: [
-                        string(name: 'buildNum', value: "${pythonBuildNum}"),
-                        string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
-                        string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                        string(name: 'ghprbPullId', value: "${ghprbPullId}")
-                    ]
+                    build job: 'beam_PreCommit_Java_IntegrationTest', parameters: javaBuildArg + ghprbArgs
                 }
             }
         )
