ed779158b5f2fdf325cd9129fbd98c015f4cbb0c
Pei He <pei@apache.org>
2017-07-14 16:10:29 +0800

    jstorm-runner: move jstorm state implementations to JStormStateInternals inner classes.

--- runners/jstorm/src/main/java/org/apache/beam/runners/jstorm/translation/JStormStateInternals.java
+++ runners/jstorm/src/main/java/org/apache/beam/runners/jstorm/translation/JStormStateInternals.java
@@ -20,8 +20,13 @@ package org.apache.beam.runners.jstorm.translation;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.alibaba.jstorm.cache.ComposedKey;
+import com.alibaba.jstorm.cache.IKvStore;
 import com.alibaba.jstorm.cache.IKvStoreManager;
+import com.alibaba.jstorm.cache.KvStoreIterable;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import javax.annotation.Nullable;
 import org.apache.beam.runners.core.StateInternals;
 import org.apache.beam.runners.core.StateNamespace;
@@ -29,7 +34,9 @@ import org.apache.beam.runners.core.StateTag;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.state.BagState;
 import org.apache.beam.sdk.state.CombiningState;
+import org.apache.beam.sdk.state.GroupingState;
 import org.apache.beam.sdk.state.MapState;
+import org.apache.beam.sdk.state.ReadableState;
 import org.apache.beam.sdk.state.SetState;
 import org.apache.beam.sdk.state.State;
 import org.apache.beam.sdk.state.StateBinder;
@@ -42,11 +49,15 @@ import org.apache.beam.sdk.transforms.Combine.BinaryCombineFn;
 import org.apache.beam.sdk.transforms.CombineWithContext;
 import org.apache.beam.sdk.transforms.windowing.TimestampCombiner;
 import org.joda.time.Instant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * JStorm implementation of {@link StateInternals}.
  */
 class JStormStateInternals<K> implements StateInternals {
+
+  private static final Logger LOG = LoggerFactory.getLogger(JStormStateInternals.class);
 
   private static final String STATE_INFO = "state-info:";
 
@@ -181,6 +192,459 @@ class JStormStateInternals<K> implements StateInternals {
         }
       }
     });
+  }
+
+  /**
+   * JStorm implementation of {@link ValueState}.
+   */
+  private static class JStormValueState<K, T> implements ValueState<T> {
+
+    @Nullable
+    private final K key;
+    private final StateNamespace namespace;
+    private final IKvStore<ComposedKey, T> kvState;
+
+    JStormValueState(@Nullable K key, StateNamespace namespace, IKvStore<ComposedKey, T> kvState) {
+      this.key = key;
+      this.namespace = namespace;
+      this.kvState = kvState;
+    }
+
+    @Override
+    public void write(T t) {
+      try {
+        kvState.put(getComposedKey(), t);
+      } catch (IOException e) {
+        throw new RuntimeException(String.format(
+            "Failed to write key: %s, namespace: %s, value: %s.", key, namespace, t));
+      }
+    }
+
+    @Override
+    public T read() {
+      try {
+        return kvState.get(getComposedKey());
+      } catch (IOException e) {
+        throw new RuntimeException(String.format(
+            "Failed to read key: %s, namespace: %s.", key, namespace));
+      }
+    }
+
+    @Override
+    public ValueState<T> readLater() {
+      // TODO: support prefetch.
+      return this;
+    }
+
+    @Override
+    public void clear() {
+      try {
+        kvState.remove(getComposedKey());
+      } catch (IOException e) {
+        throw new RuntimeException(String.format(
+            "Failed to clear key: %s, namespace: %s.", key, namespace));
+      }
+    }
+
+    private ComposedKey getComposedKey() {
+      return ComposedKey.of(key, namespace);
+    }
+  }
+
+  /**
+   * Implementation of {@link BagState} in JStorm runner.
+   */
+  private static class JStormBagState<K, T> implements BagState<T> {
+
+    @Nullable
+    private final K key;
+    private final StateNamespace namespace;
+    private final IKvStore<ComposedKey, T> kvState;
+    private final IKvStore<ComposedKey, Object> stateInfoKvState;
+    private int elemIndex;
+
+    JStormBagState(
+        @Nullable K key,
+        StateNamespace namespace,
+        IKvStore<ComposedKey, T> kvState,
+        IKvStore<ComposedKey, Object> stateInfoKvState) throws IOException {
+      this.key = key;
+      this.namespace = checkNotNull(namespace, "namespace");
+      this.kvState = checkNotNull(kvState, "kvState");
+      this.stateInfoKvState = checkNotNull(stateInfoKvState, "stateInfoKvState");
+
+      Integer index = (Integer) stateInfoKvState.get(getComposedKey());
+      this.elemIndex = index != null ? ++index : 0;
+    }
+
+    @Override
+    public void add(T input) {
+      try {
+        kvState.put(getComposedKey(elemIndex), input);
+        stateInfoKvState.put(getComposedKey(), elemIndex);
+        elemIndex++;
+      } catch (IOException e) {
+        throw new RuntimeException(e.getCause());
+      }
+    }
+
+    @Override
+    public ReadableState<Boolean> isEmpty() {
+      return new ReadableState<Boolean>() {
+        @Override
+        public Boolean read() {
+          return elemIndex <= 0;
+        }
+
+        @Override
+        public ReadableState<Boolean> readLater() {
+          // TODO: support prefetch.
+          return this;
+        }
+      };
+    }
+
+    @Override
+    public Iterable<T> read() {
+      return new BagStateIterable(elemIndex);
+    }
+
+    @Override
+    public BagState readLater() {
+      // TODO: support prefetch.
+      return this;
+    }
+
+    @Override
+    public void clear() {
+      try {
+        for (int i = 0; i < elemIndex; i++) {
+          kvState.remove(getComposedKey(i));
+        }
+        stateInfoKvState.remove(getComposedKey());
+        elemIndex = 0;
+      } catch (IOException e) {
+        throw new RuntimeException(e.getCause());
+      }
+    }
+
+    private ComposedKey getComposedKey() {
+      return ComposedKey.of(key, namespace);
+    }
+
+    private ComposedKey getComposedKey(int elemIndex) {
+      return ComposedKey.of(key, namespace, elemIndex);
+    }
+
+    /**
+     * Implementation of Bag state Iterable.
+     */
+    private class BagStateIterable implements KvStoreIterable<T> {
+
+      private class BagStateIterator implements Iterator<T> {
+        private final int size;
+        private int cursor = 0;
+
+        BagStateIterator() {
+          Integer s = null;
+          try {
+            s = (Integer) stateInfoKvState.get(getComposedKey());
+          } catch (IOException e) {
+            LOG.error("Failed to get elemIndex for key={}", getComposedKey());
+          }
+          this.size = s != null ? ++s : 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+          return cursor < size;
+        }
+
+        @Override
+        public T next() {
+          if (cursor >= size) {
+            throw new NoSuchElementException();
+          }
+
+          T value = null;
+          try {
+            value = kvState.get(getComposedKey(cursor));
+          } catch (IOException e) {
+            LOG.error("Failed to read composed key-[{}]", getComposedKey(cursor));
+          }
+          cursor++;
+          return value;
+        }
+
+        @Override
+        public void remove() {
+          throw new UnsupportedOperationException();
+        }
+      }
+
+      private final int size;
+
+      BagStateIterable(int size) {
+        this.size = size;
+      }
+
+      @Override
+      public Iterator<T> iterator() {
+        return new BagStateIterator();
+      }
+
+      @Override
+      public String toString() {
+        return String.format("BagStateIterable: composedKey=%s", getComposedKey());
+      }
+    }
+  }
+
+  /**
+   * JStorm implementation of {@link CombiningState}.
+   */
+  private static class JStormCombiningState<InputT, AccumT, OutputT>
+      implements CombiningState<InputT, AccumT, OutputT> {
+
+    @Nullable
+    private final BagState<AccumT> accumBagState;
+    private final Combine.CombineFn<InputT, AccumT, OutputT> combineFn;
+
+    JStormCombiningState(
+        BagState<AccumT> accumBagState,
+        Combine.CombineFn<InputT, AccumT, OutputT> combineFn) {
+      this.accumBagState = checkNotNull(accumBagState, "accumBagState");
+      this.combineFn = checkNotNull(combineFn, "combineFn");
+    }
+
+    @Override
+    public AccumT getAccum() {
+      // TODO: replacing the accumBagState with the merged accum.
+      return combineFn.mergeAccumulators(accumBagState.read());
+    }
+
+    @Override
+    public void addAccum(AccumT accumT) {
+      accumBagState.add(accumT);
+    }
+
+    @Override
+    public AccumT mergeAccumulators(Iterable<AccumT> iterable) {
+      return combineFn.mergeAccumulators(iterable);
+    }
+
+    @Override
+    public void add(InputT input) {
+      accumBagState.add(
+          combineFn.addInput(combineFn.createAccumulator(), input));
+    }
+
+    @Override
+    public ReadableState<Boolean> isEmpty() {
+      return accumBagState.isEmpty();
+    }
+
+    @Override
+    public OutputT read() {
+      return combineFn.extractOutput(
+          combineFn.mergeAccumulators(accumBagState.read()));
+    }
+
+    @Override
+    public CombiningState<InputT, AccumT, OutputT> readLater() {
+      // TODO: support prefetch.
+      return this;
+    }
+
+    @Override
+    public void clear() {
+      accumBagState.clear();
+    }
+  }
+
+  /**
+   * Implementation of {@link MapState} in JStorm runner.
+   * @param <K>
+   * @param <V>
+   */
+  private static class JStormMapState<K, V> implements MapState<K, V> {
+
+    private final K key;
+    private final StateNamespace namespace;
+    private IKvStore<K, V> kvStore;
+
+    JStormMapState(K key, StateNamespace namespace, IKvStore<K, V> kvStore) {
+      this.key = key;
+      this.namespace = namespace;
+      this.kvStore = kvStore;
+    }
+
+    @Override
+    public void put(K var1, V var2) {
+      try {
+        kvStore.put(var1, var2);
+      } catch (IOException e) {
+        reportError(String.format("Failed to put key=%s, value=%s", var1, var2), e);
+      }
+    }
+
+    @Override
+    public ReadableState<V> putIfAbsent(K var1, V var2) {
+      ReadableState<V> ret = null;
+      try {
+        V value = kvStore.get(var1);
+        if (value == null) {
+          kvStore.put(var1, var2);
+          ret = new MapReadableState<>(null);
+        } else {
+          ret = new MapReadableState<>(value);
+        }
+      } catch (IOException e) {
+        reportError(String.format("Failed to putIfAbsent key=%s, value=%s", var1, var2), e);
+      }
+      return ret;
+    }
+
+    @Override
+    public void remove(K var1) {
+      try {
+        kvStore.remove(var1);
+      } catch (IOException e) {
+        reportError(String.format("Failed to remove key=%s", var1), e);
+      }
+    }
+
+    @Override
+    public ReadableState<V> get(K var1) {
+      ReadableState<V> ret = new MapReadableState<>(null);
+      try {
+        ret = new MapReadableState(kvStore.get(var1));
+      } catch (IOException e) {
+        reportError(String.format("Failed to get value for key=%s", var1), e);
+      }
+      return ret;
+    }
+
+    @Override
+    public ReadableState<Iterable<K>> keys() {
+      ReadableState<Iterable<K>> ret = new MapReadableState<>(null);
+      try {
+        ret = new MapReadableState<>(kvStore.keys());
+      } catch (IOException e) {
+        reportError(String.format("Failed to get keys"), e);
+      }
+      return ret;
+    }
+
+    @Override
+    public ReadableState<Iterable<V>> values() {
+      ReadableState<Iterable<V>> ret = new MapReadableState<>(null);
+      try {
+        ret = new MapReadableState<>(kvStore.values());
+      } catch (IOException e) {
+        reportError(String.format("Failed to get values"), e);
+      }
+      return ret;
+    }
+
+    @Override
+    public ReadableState<Iterable<Map.Entry<K, V>>> entries() {
+      ReadableState<Iterable<Map.Entry<K, V>>> ret = new MapReadableState<>(null);
+      try {
+        ret = new MapReadableState<>(kvStore.entries());
+      } catch (IOException e) {
+        reportError(String.format("Failed to get values"), e);
+      }
+      return ret;
+    }
+
+    @Override
+    public void clear() {
+      try {
+        Iterable<K> keys = kvStore.keys();
+        kvStore.removeBatch(keys);
+      } catch (IOException e) {
+        reportError(String.format("Failed to clear map state"), e);
+      }
+    }
+
+    private void reportError(String errorInfo, IOException e) {
+      LOG.error(errorInfo, e);
+      throw new RuntimeException(errorInfo);
+    }
+
+    private class MapReadableState<T> implements ReadableState<T> {
+      private T value;
+
+      public MapReadableState(T value) {
+        this.value = value;
+      }
+
+      @Override
+      public T read() {
+        return value;
+      }
+
+      @Override
+      public ReadableState<T> readLater() {
+        return this;
+      }
+    }
+  }
+
+  /**
+   * JStorm implementation of {@link WatermarkHoldState}.
+   */
+  private static class JStormWatermarkHoldState implements WatermarkHoldState {
+
+    private final StateNamespace namespace;
+    private final GroupingState<Instant, Instant> watermarkHoldsState;
+    private final TimestampCombiner timestampCombiner;
+    private final TimerService timerService;
+
+    JStormWatermarkHoldState(
+        StateNamespace namespace,
+        GroupingState<Instant, Instant> watermarkHoldsState,
+        TimestampCombiner timestampCombiner,
+        TimerService timerService) {
+      this.namespace = checkNotNull(namespace, "namespace");
+      this.watermarkHoldsState = checkNotNull(watermarkHoldsState, "watermarkHoldsState");
+      this.timestampCombiner = checkNotNull(timestampCombiner, "timestampCombiner");
+      this.timerService = checkNotNull(timerService, "timerService");
+    }
+
+    @Override
+    public TimestampCombiner getTimestampCombiner() {
+      return timestampCombiner;
+    }
+
+    @Override
+    public void add(Instant instant) {
+      timerService.addWatermarkHold(namespace.stringKey(), instant);
+      watermarkHoldsState.add(instant);
+    }
+
+    @Override
+    public ReadableState<Boolean> isEmpty() {
+      return watermarkHoldsState.isEmpty();
+    }
+
+    @Override
+    public Instant read() {
+      return watermarkHoldsState.read();
+    }
+
+    @Override
+    public WatermarkHoldState readLater() {
+      // TODO: support prefetch.
+      return this;
+    }
+
+    @Override
+    public void clear() {
+      timerService.clearWatermarkHold(namespace.stringKey());
+      watermarkHoldsState.clear();
+    }
   }
 
   private String getStoreId(String stateId) {
