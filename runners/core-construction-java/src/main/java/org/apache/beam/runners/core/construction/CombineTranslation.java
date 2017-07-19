c2110c97d530b8e90387bf99b3ac7d36201d85c7
Thomas Groh <tgroh@google.com>
2017-07-19 10:55:33 -0700

    Register a PTransformTranslator for Combine
    Include the Combine Payload in the Runner API Graph.
    
    Add getCombineFn(AppliedPTransform) to extract the CombineFn from an
    arbitrary transform.
    
    Update Pipeline Translation tests to include accumulator coders.

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/CombineTranslation.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/CombineTranslation.java
@@ -19,19 +19,24 @@
 package org.apache.beam.runners.core.construction;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.beam.runners.core.construction.PTransformTranslation.COMBINE_TRANSFORM_URN;
 
+import com.google.auto.service.AutoService;
 import com.google.common.collect.Iterables;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.BytesValue;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.beam.runners.core.construction.PTransformTranslation.TransformPayloadTranslator;
 import org.apache.beam.sdk.coders.CannotProvideCoderException;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.CombinePayload;
+import org.apache.beam.sdk.common.runner.v1.RunnerApi.Components;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.FunctionSpec;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.SdkFunctionSpec;
 import org.apache.beam.sdk.common.runner.v1.RunnerApi.SideInput;
@@ -49,7 +54,47 @@ import org.apache.beam.sdk.values.PCollection;
  * RunnerApi.CombinePayload} protos.
  */
 public class CombineTranslation {
-  public static final String JAVA_SERIALIZED_COMBINE_FN_URN = "urn:beam:java:combinefn:v1";
+  public static final String JAVA_SERIALIZED_COMBINE_FN_URN = "urn:beam:combinefn:javasdk:v1";
+
+   /**
+   * A {@link TransformPayloadTranslator} for {@link Combine.PerKey}.
+   */
+  public static class CombinePayloadTranslator
+      implements PTransformTranslation.TransformPayloadTranslator<Combine.PerKey<?, ?, ?>> {
+    public static TransformPayloadTranslator create() {
+      return new CombinePayloadTranslator();
+    }
+
+    private CombinePayloadTranslator() {}
+
+    @Override
+    public String getUrn(Combine.PerKey<?, ?, ?> transform) {
+      return COMBINE_TRANSFORM_URN;
+    }
+
+    @Override
+    public FunctionSpec translate(
+        AppliedPTransform<?, ?, Combine.PerKey<?, ?, ?>> transform, SdkComponents components)
+        throws IOException {
+      CombinePayload payload = toProto(transform, components);
+      return RunnerApi.FunctionSpec.newBuilder()
+          .setUrn(COMBINE_TRANSFORM_URN)
+          .setParameter(Any.pack(payload))
+          .build();
+    }
+
+    /**
+     * Registers {@link CombinePayloadTranslator}.
+     */
+    @AutoService(TransformPayloadTranslatorRegistrar.class)
+    public static class Registrar implements TransformPayloadTranslatorRegistrar {
+      @Override
+      public Map<? extends Class<? extends PTransform>, ? extends TransformPayloadTranslator>
+          getTransformPayloadTranslators() {
+        return Collections.singletonMap(Combine.PerKey.class, new CombinePayloadTranslator());
+      }
+    }
+  }
 
   public static CombinePayload toProto(
       AppliedPTransform<?, ?, Combine.PerKey<?, ?, ?>> combine, SdkComponents sdkComponents)
@@ -72,10 +117,11 @@ public class CombineTranslation {
       GlobalCombineFn<InputT, AccumT, ?> combineFn,
       AppliedPTransform<PCollection<KV<K, InputT>>, ?, Combine.PerKey<K, InputT, ?>> transform)
       throws CannotProvideCoderException {
-    KvCoder<K, InputT> inputCoder =
-        (KvCoder<K, InputT>)
-            ((PCollection<KV<K, InputT>>) Iterables.getOnlyElement(transform.getInputs().values()))
-                .getCoder();
+    @SuppressWarnings("unchecked")
+    PCollection<KV<K, InputT>> mainInput =
+        (PCollection<KV<K, InputT>>)
+            Iterables.getOnlyElement(TransformInputs.nonAdditionalInputs(transform));
+    KvCoder<K, InputT> inputCoder = (KvCoder<K, InputT>) mainInput.getCoder();
     return AppliedCombineFn.withInputCoder(
             combineFn,
             transform.getPipeline().getCoderRegistry(),
@@ -108,6 +154,14 @@ public class CombineTranslation {
     return CoderTranslation.fromProto(components.getCodersOrThrow(id), components);
   }
 
+  public static Coder<?> getAccumulatorCoder(
+      AppliedPTransform<?, ?, ?> transform) throws IOException {
+    SdkComponents sdkComponents = SdkComponents.create();
+    String id = getCombinePayload(transform, sdkComponents).getAccumulatorCoderId();
+    Components components = sdkComponents.toComponents();
+    return CoderTranslation.fromProto(components.getCodersOrThrow(id), components);
+  }
+
   public static GlobalCombineFn<?, ?, ?> getCombineFn(CombinePayload payload)
       throws IOException {
     checkArgument(payload.getCombineFn().getSpec().getUrn().equals(JAVA_SERIALIZED_COMBINE_FN_URN));
@@ -121,5 +175,24 @@ public class CombineTranslation {
                 .getValue()
                 .toByteArray(),
             "CombineFn");
+  }
+
+  public static GlobalCombineFn<?, ?, ?> getCombineFn(AppliedPTransform<?, ?, ?> transform)
+      throws IOException {
+    return getCombineFn(getCombinePayload(transform));
+  }
+
+  private static CombinePayload getCombinePayload(AppliedPTransform<?, ?, ?> transform)
+      throws IOException {
+    return getCombinePayload(transform, SdkComponents.create());
+  }
+
+  private static CombinePayload getCombinePayload(
+      AppliedPTransform<?, ?, ?> transform, SdkComponents components) throws IOException {
+    return PTransformTranslation.toProto(
+            transform, Collections.<AppliedPTransform<?, ?, ?>>emptyList(), components)
+        .getSpec()
+        .getParameter()
+        .unpack(CombinePayload.class);
   }
 }
