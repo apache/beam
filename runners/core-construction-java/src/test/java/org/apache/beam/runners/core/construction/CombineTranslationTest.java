99332c9c174cb2f7273963a9c9a6384fa4de6960
Thomas Groh <tgroh@google.com>
2017-07-19 10:55:33 -0700

    Register a PTransformTranslator for Combine
    Include the Combine Payload in the Runner API Graph.
    
    Add getCombineFn(AppliedPTransform) to extract the CombineFn from an
    arbitrary transform.
    
    Update Pipeline Translation tests to include accumulator coders.

--- runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/CombineTranslationTest.java
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/CombineTranslationTest.java
@@ -35,13 +35,19 @@ import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.Combine.BinaryCombineIntegerFn;
 import org.apache.beam.sdk.transforms.Combine.CombineFn;
+import org.apache.beam.sdk.transforms.CombineWithContext.CombineFnWithContext;
+import org.apache.beam.sdk.transforms.CombineWithContext.Context;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.Sum;
+import org.apache.beam.sdk.transforms.View;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
@@ -49,46 +55,100 @@ import org.junit.runners.Parameterized.Parameters;
 /**
  * Tests for {@link CombineTranslation}.
  */
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class CombineTranslationTest {
-  @Parameters(name = "{index}: {0}")
-  public static Iterable<Combine.CombineFn<Integer, ?, ?>> params() {
-    BinaryCombineIntegerFn sum = Sum.ofIntegers();
-    CombineFn<Integer, ?, Long> count = Count.combineFn();
-    TestCombineFn test = new TestCombineFn();
-    return ImmutableList.<CombineFn<Integer, ?, ?>>builder().add(sum).add(count).add(test).build();
+
+  /**
+   * Tests that simple {@link CombineFn CombineFns} can be translated to and from proto.
+   */
+  @RunWith(Parameterized.class)
+  public static class TranslateSimpleCombinesTest {
+    @Parameters(name = "{index}: {0}")
+    public static Iterable<Combine.CombineFn<Integer, ?, ?>> params() {
+      BinaryCombineIntegerFn sum = Sum.ofIntegers();
+      CombineFn<Integer, ?, Long> count = Count.combineFn();
+      TestCombineFn test = new TestCombineFn();
+      return ImmutableList.<CombineFn<Integer, ?, ?>>builder()
+          .add(sum)
+          .add(count)
+          .add(test)
+          .build();
+    }
+
+    @Rule public TestPipeline pipeline = TestPipeline.create();
+
+    @Parameter(0)
+    public Combine.CombineFn<Integer, ?, ?> combineFn;
+
+    @Test
+    public void testToFromProto() throws Exception {
+      PCollection<Integer> input = pipeline.apply(Create.of(1, 2, 3));
+      input.apply(Combine.globally(combineFn));
+      final AtomicReference<AppliedPTransform<?, ?, Combine.PerKey<?, ?, ?>>> combine =
+          new AtomicReference<>();
+      pipeline.traverseTopologically(
+          new PipelineVisitor.Defaults() {
+            @Override
+            public void leaveCompositeTransform(Node node) {
+              if (node.getTransform() instanceof Combine.PerKey) {
+                checkState(combine.get() == null);
+                combine.set((AppliedPTransform) node.toAppliedPTransform(getPipeline()));
+              }
+            }
+          });
+      checkState(combine.get() != null);
+      assertEquals(combineFn, CombineTranslation.getCombineFn(combine.get()));
+
+      SdkComponents sdkComponents = SdkComponents.create();
+      CombinePayload combineProto = CombineTranslation.toProto(combine.get(), sdkComponents);
+      RunnerApi.Components componentsProto = sdkComponents.toComponents();
+
+      assertEquals(combineFn.getAccumulatorCoder(pipeline.getCoderRegistry(), input.getCoder()),
+          CombineTranslation.getAccumulatorCoder(combineProto, componentsProto));
+      assertEquals(combineFn, CombineTranslation.getCombineFn(combineProto));
+    }
   }
 
-  @Rule public TestPipeline pipeline = TestPipeline.create();
-  @Parameter(0)
-  public Combine.CombineFn<Integer, ?, ?> combineFn;
 
-  @Test
-  public void testToFromProto() throws Exception {
-    PCollection<Integer> input = pipeline.apply(Create.of(1, 2, 3));
-    input.apply(Combine.globally(combineFn));
-    final AtomicReference<AppliedPTransform<?, ?, Combine.PerKey<?, ?, ?>>> combine =
-        new AtomicReference<>();
-    pipeline.traverseTopologically(
-        new PipelineVisitor.Defaults() {
-          @Override
-          public void leaveCompositeTransform(Node node) {
-            if (node.getTransform() instanceof Combine.PerKey) {
-              checkState(combine.get() == null);
-              combine.set((AppliedPTransform) node.toAppliedPTransform(getPipeline()));
+  /**
+   * Tests that a {@link CombineFnWithContext} can be translated.
+   */
+  @RunWith(JUnit4.class)
+  public static class ValidateCombineWithContextTest {
+    @Rule
+    public TestPipeline pipeline = TestPipeline.create();
+
+    @Test
+    public void testToFromProtoWithSideInputs() throws Exception {
+      PCollection<Integer> input = pipeline.apply(Create.of(1, 2, 3));
+      final PCollectionView<Iterable<String>> sideInput =
+          pipeline.apply(Create.of("foo")).apply(View.<String>asIterable());
+      CombineFnWithContext<Integer, int[], Integer> combineFn = new TestCombineFnWithContext();
+      input.apply(Combine.globally(combineFn).withSideInputs(sideInput).withoutDefaults());
+      final AtomicReference<AppliedPTransform<?, ?, Combine.PerKey<?, ?, ?>>> combine =
+          new AtomicReference<>();
+      pipeline.traverseTopologically(
+          new PipelineVisitor.Defaults() {
+            @Override
+            public void leaveCompositeTransform(Node node) {
+              if (node.getTransform() instanceof Combine.PerKey) {
+                checkState(combine.get() == null);
+                combine.set((AppliedPTransform) node.toAppliedPTransform(getPipeline()));
+              }
             }
-          }
-        });
-    checkState(combine.get() != null);
+          });
+      checkState(combine.get() != null);
+      assertEquals(combineFn, CombineTranslation.getCombineFn(combine.get()));
 
-    SdkComponents sdkComponents = SdkComponents.create();
-    CombinePayload combineProto = CombineTranslation.toProto(combine.get(), sdkComponents);
-    RunnerApi.Components componentsProto = sdkComponents.toComponents();
+      SdkComponents sdkComponents = SdkComponents.create();
+      CombinePayload combineProto = CombineTranslation.toProto(combine.get(), sdkComponents);
+      RunnerApi.Components componentsProto = sdkComponents.toComponents();
 
-    assertEquals(
-        combineFn.getAccumulatorCoder(pipeline.getCoderRegistry(), input.getCoder()),
-        CombineTranslation.getAccumulatorCoder(combineProto, componentsProto));
-    assertEquals(combineFn, CombineTranslation.getCombineFn(combineProto));
+      assertEquals(
+          combineFn.getAccumulatorCoder(pipeline.getCoderRegistry(), input.getCoder()),
+          CombineTranslation.getAccumulatorCoder(combineProto, componentsProto));
+      assertEquals(combineFn, CombineTranslation.getCombineFn(combineProto));
+    }
   }
 
   private static class TestCombineFn extends Combine.CombineFn<Integer, Void, Void> {
@@ -127,4 +187,43 @@ public class CombineTranslationTest {
       return TestCombineFn.class.hashCode();
     }
   }
+
+  private static class TestCombineFnWithContext
+      extends CombineFnWithContext<Integer, int[], Integer> {
+
+    @Override
+    public int[] createAccumulator(Context c) {
+      return new int[1];
+    }
+
+    @Override
+    public int[] addInput(int[] accumulator, Integer input, Context c) {
+      accumulator[0] += input;
+      return accumulator;
+    }
+
+    @Override
+    public int[] mergeAccumulators(Iterable<int[]> accumulators, Context c) {
+      int[] res = new int[1];
+      for (int[] accum : accumulators) {
+        res[0] += accum[0];
+      }
+      return res;
+    }
+
+    @Override
+    public Integer extractOutput(int[] accumulator, Context c) {
+      return accumulator[0];
+    }
+
+    @Override
+    public boolean equals(Object other) {
+      return other instanceof TestCombineFnWithContext;
+    }
+
+    @Override
+    public int hashCode() {
+      return TestCombineFnWithContext.class.hashCode();
+    }
+  };
 }
