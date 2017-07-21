3a277fd31bc7d474830c94d56dca0ecd1691776a
David Cavazos <dcavazos@google.com>
2017-07-21 09:58:11 -0700

    Changed sphinx API reference theme

--- sdks/python/generate_pydoc.sh
+++ sdks/python/generate_pydoc.sh
@@ -31,43 +31,88 @@ rm -rf target/docs/*
 
 mkdir -p target/docs/source
 
-# Exclude internal/experimental files from the documentation.
+# Sphinx apidoc autodoc options
+export SPHINX_APIDOC_OPTIONS=\
+members,\
+undoc-members,\
+show-inheritance
+
+# Exclude internal/experimental/Cython paths/patterns from the documentation.
 excluded_internal_code=(
+    apache_beam/coders/stream.py*
     apache_beam/examples/
-    apache_beam/internal/clients/
-    apache_beam/io/gcp/internal/clients/
+    apache_beam/internal/
+    apache_beam/io/gcp/internal/
+    apache_beam/io/gcp/tests/
     apache_beam/runners/api/
     apache_beam/runners/test/
+    apache_beam/runners/dataflow/internal/
     apache_beam/runners/portability/
     apache_beam/runners/worker/
-    apache_beam/runners/dataflow/internal/clients/
-    apache_beam/testing/data/)
+    apache_beam/testing/
+    *_pb2.py
+    *_test.py
+    *_test_common.py
+    *_test_utils.py
+)
 
-python $(type -p sphinx-apidoc) -f -o target/docs/source apache_beam \
-    "${excluded_internal_code[@]}" "*_test.py"
+python $(type -p sphinx-apidoc) -fMeT -o target/docs/source apache_beam \
+    "${excluded_internal_code[@]}"
 
-# Remove Cython modules from doc template; they won't load
-sed -i -e '/.. automodule:: apache_beam.coders.stream/d' \
-    target/docs/source/apache_beam.coders.rst
-
-# Create the configuration and index files
+#=== conf.py ===#
 cat > target/docs/source/conf.py <<'EOF'
 import os
 import sys
 
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../../..'))
+
+exclude_patterns = [
+    '_build',
+    'target/docs/source/apache_beam.rst',
+]
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
 master_doc = 'index'
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 project = 'Apache Beam'
+
+autoclass_content = 'both'
+autodoc_member_order = 'bysource'
+
+doctest_global_setup = '''
+import apache_beam as beam
+import sys
+'''
+
+intersphinx_mapping = {
+  'python': ('https://docs.python.org/2', None),
+}
+
+nitpicky = True
+nitpick_ignore = [
+  # Ignore internal private classes. If there's any reference to them within the
+  # documentation, it will report as a broken cross reference. This can happen
+  # if a class inherits from a private class.
+  ('py:class', 'apache_beam.coders.coders._PickleCoderBase'),
+  ('py:class', 'apache_beam.io.gcp.datastore.v1.datastoreio._Mutate'),
+  ('py:class', 'apache_beam.runners.direct.executor.CallableTask'),
+  ('py:class', 'apache_beam.transforms.trigger._ParallelTriggerFn'),
+]
 EOF
+
+#=== index.rst ===#
 cat > target/docs/source/index.rst <<'EOF'
-.. include:: ./modules.rst
+.. include:: ./apache_beam.rst
+   :start-line: 2
 EOF
 
 # Build the documentation using sphinx
@@ -76,10 +121,21 @@ python $(type -p sphinx-build) -v -a -E -q target/docs/source \
   target/docs/_build -c target/docs/source \
   -w "target/docs/sphinx-build.warnings.log"
 
+# Fail if there are errors or warnings in docs
+! grep -q "ERROR:" target/docs/sphinx-build.warnings.log || exit 1
+! grep -q "WARNING:" target/docs/sphinx-build.warnings.log || exit 1
+
+# Run tests for code samples, these can be:
+# - Code blocks using '.. testsetup::', '.. testcode::' and '.. testoutput::'
+# - Interactive code starting with '>>>'
+python -msphinx -M doctest target/docs/source \
+  target/docs/_build -c target/docs/source \
+  -w "target/docs/sphinx-doctest.warnings.log"
+
+# Fail if there are errors or warnings in docs
+! grep -q "ERROR:" target/docs/sphinx-doctest.warnings.log || exit 1
+! grep -q "WARNING:" target/docs/sphinx-doctest.warnings.log || exit 1
+
 # Message is useful only when this script is run locally.  In a remote
 # test environment, this path will be removed when the test completes.
 echo "Browse to file://$PWD/target/docs/_build/index.html"
-
-# Fail if there are errors or warnings in docs
-! grep -q "ERROR:" target/docs/sphinx-build.warnings.log
-! grep -q "WARNING:" target/docs/sphinx-build.warnings.log
