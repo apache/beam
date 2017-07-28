f08d7d5cd3d10e549d1423a098532cffd588c357
Etienne Chauchot <echauchot@gmail.com>
2017-07-28 15:00:23 +0200

    put BatchSize lower case in ElasticsearchIO.Read.populateDisplayData

--- sdks/java/io/elasticsearch/src/main/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIO.java
+++ sdks/java/io/elasticsearch/src/main/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIO.java
@@ -408,7 +408,7 @@ public class ElasticsearchIO {
     public void populateDisplayData(DisplayData.Builder builder) {
       super.populateDisplayData(builder);
       builder.addIfNotNull(DisplayData.item("query", getQuery()));
-      builder.addIfNotNull(DisplayData.item("BatchSize", getBatchSize()));
+      builder.addIfNotNull(DisplayData.item("batchSize", getBatchSize()));
       builder.addIfNotNull(DisplayData.item("scrollKeepalive", getScrollKeepalive()));
       getConnectionConfiguration().populateDisplayData(builder);
     }
