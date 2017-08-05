d297a2605019d6d0eb682b444f62a18f45d04941
Innocent Djiofack <djiofack007@gmail.com>
2017-08-04 22:51:23 -0400

    made the comparison of structural values based on cloned values in MutationDetectors.java. Added addition al equals comparison of the cloned values without them,some BigTableIoTest fail

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/MutationDetectors.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/MutationDetectors.java
@@ -16,10 +16,11 @@
  * limitations under the License.
  */
 package org.apache.beam.sdk.util;
-import com.google.common.io.BaseEncoding;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.CoderException;
-import org.apache.beam.sdk.coders.StructuralByteArray;
+
+
+
 
 /**
  * Static methods for creating and working with {@link MutationDetector}.
@@ -75,12 +76,25 @@ public class MutationDetectors {
   private static class CodedValueMutationDetector<T> implements MutationDetector {
 
     private final Coder<T> coder;
-
+    private final T clonedOriginalValue;
     /**
      * A saved pointer to an in-memory value provided upon construction, which we will check for
      * forbidden mutations.
      */
     private final T possiblyModifiedObject;
+
+    /**
+     * A saved encoded copy of the same value as {@link #possiblyModifiedObject}. Naturally, it
+     * will not change if {@link #possiblyModifiedObject} is mutated.
+     */
+      private final byte[] encodedOriginalObject;
+
+    /**
+     * The object decoded from {@link #encodedOriginalObject}. It will be used during every call to
+     * {@link #verifyUnmodified}, which could be called many times throughout the lifetime of this
+     * {@link CodedValueMutationDetector}.
+     */
+      private final T clonedOriginalObject;
     /**
      * The structural value from {@link #possiblyModifiedObject}.
      * It will be used during every call to
@@ -88,15 +102,18 @@ public class MutationDetectors {
      * {@link CodedValueMutationDetector}.
      */
     private final Object originalStructuralValue;
-
     /**
      * Create a mutation detector for the provided {@code value}, using the provided {@link Coder}
      * for cloning and checking serialized forms for equality.
      */
+
     public CodedValueMutationDetector(T value, Coder<T> coder) throws CoderException {
       this.coder = coder;
-      this.originalStructuralValue = coder.structuralValue(value);
+      clonedOriginalValue = CoderUtils.clone(coder, value);
+      this.originalStructuralValue = coder.structuralValue(clonedOriginalValue);
       this.possiblyModifiedObject = value;
+      this.encodedOriginalObject = CoderUtils.encodeToByteArray(coder, value);
+      this.clonedOriginalObject = CoderUtils.decodeFromByteArray(coder, encodedOriginalObject);
 
     }
 
@@ -110,34 +127,30 @@ public class MutationDetectors {
     }
 
     private void verifyUnmodifiedThrowingCheckedExceptions() throws CoderException {
-      // If either object believes they are equal, we trust that and short-circuit deeper checks.
-      Object newStructuralValue = coder.structuralValue(possiblyModifiedObject);
+      // Since there is not guarantee that cloning an object via the coder will
+      // return the exact same type as value, We are cloning the possiblyModifiedObject
+      // before getting it's structural value. This way we are guarantee to compare the same
+      // types.
+      T possiblyModifiedClonedValue = CoderUtils.clone(coder, possiblyModifiedObject);
+      Object newStructuralValue = coder.structuralValue(possiblyModifiedClonedValue);
       if (originalStructuralValue.equals(newStructuralValue)) {
           return;
       }
-      illegalMutation(originalStructuralValue, newStructuralValue);
-    }
-    private String encodeToBase64 (StructuralByteArray stream) {
-        return BaseEncoding.base64Url().omitPadding().encode(stream.getValue());
-    }
-    private void illegalMutation(Object previousStructuralValue,
-                                   Object newStructuralValue) throws CoderException {
-      String previousValue;
-      String newValue;
-      if (previousStructuralValue instanceof StructuralByteArray) {
-          previousValue = encodeToBase64((StructuralByteArray) previousStructuralValue);
-          newValue = encodeToBase64((StructuralByteArray) newStructuralValue);
-        } else {
-          previousValue = CoderUtils.encodeToBase64(coder, (T) previousStructuralValue);
-          newValue = CoderUtils.encodeToBase64(coder, (T) newStructuralValue);
+      if (possiblyModifiedClonedValue.equals(clonedOriginalValue)){
+          return;
       }
-      throw new IllegalMutationException(
-              String.format("Value %s mutated illegally, new value was %s."
-                              + " Encoding was %s, now %s.",
-                      previousStructuralValue, newStructuralValue,
-                      previousValue,
-                      newValue),
-              previousStructuralValue, newStructuralValue);
+
+        illegalMutation(clonedOriginalObject, possiblyModifiedClonedValue);
+    }
+
+    private void illegalMutation(T previousValue, T newValue) throws CoderException {
+        throw new IllegalMutationException(
+                String.format("Value %s mutated illegally, new value was %s."
+                                + " Encoding was %s, now %s.",
+                        previousValue, newValue,
+                        CoderUtils.encodeToBase64(coder, previousValue),
+                        CoderUtils.encodeToBase64(coder, newValue)),
+                previousValue, newValue);
       }
 
     @Override
