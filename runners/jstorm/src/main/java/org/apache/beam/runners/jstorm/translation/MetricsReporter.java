240f61bc6baab9e698b2b6144688853a8371658f
basti.lj <basti.lj@alibaba-inc.com>
2017-08-09 16:03:50 +0800

    jstorm-runner: Fix duplicated update of metric counter

--- runners/jstorm/src/main/java/org/apache/beam/runners/jstorm/translation/MetricsReporter.java
+++ runners/jstorm/src/main/java/org/apache/beam/runners/jstorm/translation/MetricsReporter.java
@@ -74,6 +74,7 @@ class MetricsReporter {
         AsmCounter counter = metricClient.registerCounter(metricName);
         Long incValue = (oldValue == null ? updateValue : updateValue - oldValue);
         counter.update(incValue);
+        reportedCounters.put(metricName, incValue);
       }
     }
   }
