aa1710c5cdd6689ea332a0e8207874847f445342
Chris Hebert <Chris.hebert-int@digitalreasoning.com>
2017-08-16 14:15:10 -0500

    [BEAM-2750][BEAM-2751] Use tmp dirs in WholeFileIO
    Formerly, WholeFileIO wrote directly to intended output files.
    This commit makes WholeFileIO write to a temporary directory
    first, then rename the temporary directory to the intended
    name.
    
    This required copying a TemporaryDirectoryBuilder marked private
    in FileBasedSink.WriteOperation.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/WholeFileIO.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/WholeFileIO.java
@@ -27,22 +27,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.channels.Channels;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.annotation.Nullable;
 
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.fs.MatchResult;
+import org.apache.beam.sdk.io.fs.MoveOptions;
+import org.apache.beam.sdk.io.fs.ResolveOptions;
 import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.util.StreamUtils;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
+import org.joda.time.Instant;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // TODO: Write comment on what this is and how to use it.
 // TODO: Mention that if multiple files have the same filename at write, only one will survive.
@@ -128,6 +138,7 @@ public class WholeFileIO {
    */
   @AutoValue
   public abstract static class Write extends PTransform<PCollection<KV<String, byte[]>>, PDone> {
+    private static final Logger LOG = LoggerFactory.getLogger(Write.class);
 
     @Nullable abstract ValueProvider<ResourceId> getOutputDir();
 
@@ -162,26 +173,57 @@ public class WholeFileIO {
       input.apply(
           ParDo.of(
               new DoFn<KV<String, byte[]>, Void>() {
+                ValueProvider<ResourceId> tmpDir;
+
+                @Setup
+                public void setup() {
+                  tmpDir = ValueProvider.NestedValueProvider.of(
+                      getOutputDir(),
+                      new TemporaryDirectoryBuilder()
+                  );
+                }
+
                 @ProcessElement
                 public void processElement(ProcessContext c) throws IOException {
                   KV<String, byte[]> kv = c.element();
 
-                  ResourceId outputDir = getOutputDir().get();
                   String filename = kv.getKey();
-                  // TODO: Write to tmp files. Once tmp file write finished, rename to filename.
-                  // TODO: ^ Alternative (faster): setup() create tmp dir, processElement()
-                  //    write each file to tmp dir, teardown() rename tmp dir to outputDir
-                  // (Or, instead of setup() and teardown(), use startBundle() and finishBundle()
-                  //    except that you mv all files inside tmp dir to inside the outputDir
-                  ResourceId outputFile = outputDir.resolve(filename, RESOLVE_FILE);
+                  ResourceId tmpFile = tmpDir.get().resolve(filename, RESOLVE_FILE);
 
                   byte[] bytes = kv.getValue();
                   try (
                       OutputStream outStream =
-                          Channels.newOutputStream(FileSystems.create(outputFile, BINARY))
+                          Channels.newOutputStream(FileSystems.create(tmpFile, BINARY))
                   ) {
                     outStream.write(bytes);
                     outStream.flush();
+                  } catch (IOException e) {
+                    LOG.error(
+                        "Failed to write to temporary file [{}] for [{}].",
+                        tmpFile,
+                        getOutputDir().get().resolve(filename, RESOLVE_FILE)
+                    );
+                    FileSystems.delete(
+                        Collections.singletonList(tmpFile),
+                        MoveOptions.StandardMoveOptions.IGNORE_MISSING_FILES
+                    );
+                    throw e;
+                  }
+                }
+
+                @Teardown
+                public void teardown() throws IOException {
+                  try {
+                    FileSystems.rename(
+                        Collections.singletonList(tmpDir.get()),
+                        Collections.singletonList(getOutputDir().get())
+                    );
+                  } catch (IOException e) {
+                    LOG.error(
+                        "Failed to rename temporary directory [{}] to [{}].",
+                        tmpDir.get(), getOutputDir().get()
+                    );
+                    throw e;
                   }
                 }
               }
@@ -189,6 +231,28 @@ public class WholeFileIO {
       );
 
       return PDone.in(input.getPipeline());
+    }
+
+    private static class TemporaryDirectoryBuilder
+        implements SerializableFunction<ResourceId, ResourceId> {
+      private static final AtomicLong TEMP_COUNT = new AtomicLong(0);
+      private static final DateTimeFormatter TEMPDIR_TIMESTAMP =
+          DateTimeFormat.forPattern("yyyy-MM-DD_HH-mm-ss");
+      // The intent of the code is to have a consistent value of tempDirectory across
+      // all workers, which wouldn't happen if now() was called inline.
+      private final String timestamp = Instant.now().toString(TEMPDIR_TIMESTAMP);
+      // Multiple different sinks may be used in the same output directory; use tempId to create a
+      // separate temp directory for each.
+      private final Long tempId = TEMP_COUNT.getAndIncrement();
+
+      @Override
+      public ResourceId apply(ResourceId tempDirectory) {
+        // Temp directory has a timestamp and a unique ID
+        String tempDirName = String.format(".temp-beam-%s-%s", timestamp, tempId);
+        return tempDirectory
+            .getCurrentDirectory()
+            .resolve(tempDirName, ResolveOptions.StandardResolveOptions.RESOLVE_DIRECTORY);
+      }
     }
   }
 
