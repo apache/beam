2d1905fa5f23a29710bc7d680079018af085b704
ksalant <kobi.salant@gmail.com>
2017-08-23 17:42:10 +0300

    Add BoundedWindow with serializable as it is a member of WindowedValue

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/windowing/BoundedWindow.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/windowing/BoundedWindow.java
@@ -17,6 +17,7 @@
  */
 package org.apache.beam.sdk.transforms.windowing;
 
+import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 import org.joda.time.Instant;
 
@@ -35,7 +36,7 @@ import org.joda.time.Instant;
  * windows that are logically equal will be treated as equal by {@code equals()} and {@code
  * hashCode()}.
  */
-public abstract class BoundedWindow {
+public abstract class BoundedWindow implements Serializable {
   // The min and max timestamps that won't overflow when they are converted to
   // usec.
 
