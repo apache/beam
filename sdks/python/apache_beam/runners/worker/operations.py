43ad4faac25433b04c0ab1a096aa32ab3db49004
Holden Karau <holden@us.ibm.com>
2017-08-27 18:25:49 -0700

    Remove deprecated use of filter and replace with for

--- sdks/python/apache_beam/runners/worker/operations.py
+++ sdks/python/apache_beam/runners/worker/operations.py
@@ -270,7 +270,7 @@ class DoOperation(Operation):
       # while the variable has the value assigned by the current iteration of
       # the for loop.
       # pylint: disable=cell-var-from-loop
-      filtered_tags = filter(lambda o: o.tag == side_tag, self.spec.side_inputs)
+      filtered_tags = [o for o in self.spec.side_inputs if o.tag == side_tag]
       for si in filtered_tags:
         if not isinstance(si, operation_specs.WorkerSideInputSource):
           raise NotImplementedError('Unknown side input type: %r' % si)
