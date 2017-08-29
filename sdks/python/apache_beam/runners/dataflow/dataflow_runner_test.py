5b1c62da008f7fa143994d316de392c4be706fac
Holden Karau <holden@us.ibm.com>
2017-08-29 13:38:59 -0700

    Manual clean up of some isort issues

--- sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
+++ sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
@@ -30,8 +30,8 @@ from apache_beam.pipeline import AppliedPTransform, Pipeline
 from apache_beam.pvalue import PCollection
 from apache_beam.runners import (DataflowRunner, TestDataflowRunner,
                                  create_runner)
-from apache_beam.runners.dataflow.dataflow_runner import (DataflowPipelineResult,
-                                                          DataflowRuntimeException)
+from apache_beam.runners.dataflow.dataflow_runner import \
+    DataflowPipelineResult, DataflowRuntimeException
 from apache_beam.runners.dataflow.internal.clients import \
     dataflow as dataflow_api
 from apache_beam.runners.runner import PipelineState
