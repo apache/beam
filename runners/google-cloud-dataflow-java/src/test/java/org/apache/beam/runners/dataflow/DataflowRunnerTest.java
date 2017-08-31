dfbd12b141ba1f401a0def9ba0bb824646803a9d
Reuven Lax <relax@relax-macbookpro2.roam.corp.google.com>
2017-08-31 15:27:05 -0700

    Fix DataflowRunnerTest.

--- runners/google-cloud-dataflow-java/src/test/java/org/apache/beam/runners/dataflow/DataflowRunnerTest.java
+++ runners/google-cloud-dataflow-java/src/test/java/org/apache/beam/runners/dataflow/DataflowRunnerTest.java
@@ -81,6 +81,7 @@ import org.apache.beam.sdk.io.FileBasedSink;
 import org.apache.beam.sdk.io.FileSystems;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.WriteFiles;
+import org.apache.beam.sdk.io.WriteFilesResult;
 import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptions.CheckEnabled;
@@ -1268,7 +1269,7 @@ public class DataflowRunnerTest implements Serializable {
         new StreamingShardedWriteFactory<>(p.getOptions());
     WriteFiles<Object, Void, Object> original = WriteFiles.to(new TestSink(tmpFolder.toString()));
     PCollection<Object> objs = (PCollection) p.apply(Create.empty(VoidCoder.of()));
-    AppliedPTransform<PCollection<Object>, PDone, WriteFiles<Object, Void, Object>>
+    AppliedPTransform<PCollection<Object>, WriteFilesResult, WriteFiles<Object, Void, Object>>
         originalApplication =
             AppliedPTransform.of(
                 "writefiles",
