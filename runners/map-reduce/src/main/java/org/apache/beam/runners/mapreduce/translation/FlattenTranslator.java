99bffd2a75b7461d15723567a57db6d3b17367cd
Pei He <pei@apache.org>
2017-09-01 14:11:30 +0800

    mr-runner: translate empty flatten into EmptySource, this fixes few empty FalttenTests.

--- runners/map-reduce/src/main/java/org/apache/beam/runners/mapreduce/translation/FlattenTranslator.java
+++ runners/map-reduce/src/main/java/org/apache/beam/runners/mapreduce/translation/FlattenTranslator.java
@@ -17,6 +17,14 @@
  */
 package org.apache.beam.runners.mapreduce.translation;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.VoidCoder;
+import org.apache.beam.sdk.io.BoundedSource;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.Flatten;
 
 /**
@@ -26,11 +34,68 @@ public class FlattenTranslator<T> extends TransformTranslator.Default<Flatten.PC
   @Override
   public void translateNode(Flatten.PCollections<T> transform, TranslationContext context) {
     TranslationContext.UserGraphContext userGraphContext = context.getUserGraphContext();
-
-    Operation<?> operation = new FlattenOperation();
+    List<Graphs.Tag> inputTags = userGraphContext.getInputTags();
+    Operation<?> operation;
+    if (inputTags.isEmpty()) {
+      // Create a empty source
+      operation = new SourceReadOperation(new EmptySource(), userGraphContext.getOnlyOutputTag());
+    } else {
+      operation = new FlattenOperation();
+    }
     context.addInitStep(
         Graphs.Step.of(userGraphContext.getStepName(), operation),
-        userGraphContext.getInputTags(),
+        inputTags,
         userGraphContext.getOutputTags());
+  }
+
+  private static class EmptySource extends BoundedSource<Void> {
+    @Override
+    public List<? extends BoundedSource<Void>> split(
+        long desiredBundleSizeBytes, PipelineOptions options) throws Exception {
+      return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public long getEstimatedSizeBytes(PipelineOptions options) throws Exception {
+      return 0;
+    }
+
+    @Override
+    public BoundedReader<Void> createReader(PipelineOptions options) throws IOException {
+      return new BoundedReader<Void>() {
+        @Override
+        public BoundedSource<Void> getCurrentSource() {
+          return EmptySource.this;
+        }
+
+        @Override
+        public boolean start() throws IOException {
+          return false;
+        }
+
+        @Override
+        public boolean advance() throws IOException {
+          return false;
+        }
+
+        @Override
+        public Void getCurrent() throws NoSuchElementException {
+          throw new NoSuchElementException();
+        }
+
+        @Override
+        public void close() throws IOException {
+        }
+      };
+    }
+
+    @Override
+    public void validate() {
+    }
+
+    @Override
+    public Coder<Void> getDefaultOutputCoder() {
+      return VoidCoder.of();
+    }
   }
 }
