76e570f39a4c64e734171e4aade6697d2e7f4fa6
Robert Bradshaw <robertwb@gmail.com>
2017-09-08 16:31:35 -0700

    Add some timing information to testing scripts.

--- sdks/python/gen_protos.py
+++ sdks/python/gen_protos.py
@@ -25,6 +25,7 @@ import pkg_resources
 import shutil
 import subprocess
 import sys
+import time
 import warnings
 
 
@@ -112,9 +113,12 @@ def _install_grpcio_tools_and_generate_proto_files():
     shutil.rmtree(build_path)
   logging.warning('Installing grpcio-tools into %s' % install_path)
   try:
+    start = time.time()
     subprocess.check_call(
         ['pip', 'install', '--target', install_path, '--build', build_path,
          '--upgrade', GRPC_TOOLS])
+    logging.warning(
+        'Installing grpcio-tools took %0.2f seconds.' % (time.time() - start))
   finally:
     shutil.rmtree(build_path)
   sys.path.append(install_path)
