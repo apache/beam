2237847df1e94c95802ffeda5a44f0679a6d5202
Bill Neubauer <wcn@google.com>
2017-09-21 08:44:14 -0700

    Fix test to not use path of package.
    Encoding the package path makes this test brittle to refactoring.
    
    Change-Id: I5fb5d56fd1fd5f7ef75fb564485ae42999ffdab6
    Reviewed-on: https://team-review.git.corp.google.com/120273
    Verified-Style: kokoro <noreply+kokoro@google.com>
    Verified-BuildAndTest: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Flavio Fiszman <flaviocf@google.com>

--- pkg/beam/core/runtime/types_test.go
+++ pkg/beam/core/runtime/types_test.go
@@ -11,6 +11,13 @@ type S struct {
 	a int
 }
 
+var sKey string
+
+func init() {
+	s := reflect.TypeOf(&S{}) // *S
+	sKey = RegisterType(s)
+}
+
 func TestKey(t *testing.T) {
 	tests := []struct {
 		T   reflect.Type
@@ -20,7 +27,7 @@ func TestKey(t *testing.T) {
 		{reflectx.Int, "", false},                      // predeclared type
 		{reflectx.String, "", false},                   // predeclared type
 		{reflect.TypeOf(struct{ A int }{}), "", false}, // unnamed struct
-		{reflect.TypeOf(S{}), "github.com/apache/beam/sdks/go/pkg/beam/core/runtime.S", true},
+		{reflect.TypeOf(S{}), sKey, true},
 		{reflect.TypeOf(&S{}), "", false},  // ptr (= no name)
 		{reflect.TypeOf([]S{}), "", false}, // slice (= no name)
 	}
@@ -36,15 +43,13 @@ func TestKey(t *testing.T) {
 func TestRegister(t *testing.T) {
 	s := reflect.TypeOf(&S{}) // *S
 
-	RegisterType(s)
-
 	for bad, key := range []string{"S", "graph.S", "foo", ""} {
 		if _, ok := LookupType(key); ok {
 			t.Fatalf("LookupType(%v) = (%v, true), want false", key, bad)
 		}
 	}
 
-	actual, ok := LookupType("github.com/apache/beam/sdks/go/pkg/beam/core/runtime.S")
+	actual, ok := LookupType(sKey)
 	if !ok {
 		t.Fatalf("LookupType(S) failed")
 	}
