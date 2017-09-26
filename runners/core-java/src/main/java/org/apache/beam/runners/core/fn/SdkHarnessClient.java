e9f60c44518d12a198107c577fbc638f856614cc
Thomas Groh <tgroh@google.com>
2017-09-26 09:34:09 -0700

    Move Runners Core Fn Api classes into a package

--- runners/core-java/src/main/java/org/apache/beam/runners/core/fn/SdkHarnessClient.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/fn/SdkHarnessClient.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.runners.core;
+package org.apache.beam.runners.core.fn;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.base.Function;
