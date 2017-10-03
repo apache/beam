18f28eede81f01a2af0065b9879410a3e2e0f21d
Mark Liu <markliu@google.com>
2017-10-03 15:07:49 -0700

    fixup! Clean up requirement configuration

--- sdks/python/run_validatesrunner.sh
+++ sdks/python/run_validatesrunner.sh
@@ -50,10 +50,6 @@ python setup.py sdist
 
 SDK_LOCATION=$(find dist/apache-beam-*.tar.gz)
 
-# Install test dependencies for ValidatesRunner tests.
-echo "pyhamcrest" > postcommit_requirements.txt
-echo "mock" >> postcommit_requirements.txt
-
 # Run ValidatesRunner tests on Google Cloud Dataflow service
 echo ">>> RUNNING DATAFLOW RUNNER VALIDATESRUNNER TESTS"
 python setup.py nosetests \
@@ -67,5 +63,4 @@ python setup.py nosetests \
     --staging_location=$GCS_LOCATION/staging-validatesrunner-test \
     --temp_location=$GCS_LOCATION/temp-validatesrunner-test \
     --sdk_location=$SDK_LOCATION \
-    --requirements_file=postcommit_requirements.txt \
     --num_workers=1"
