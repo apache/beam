b850ef6d72658021812d9a1fbfd11405a538b46b
Kenneth Knowles <kenn@apache.org>
2017-10-16 18:15:07 -0700

    Add streaming Dataflow ValidatesRunner job

---
+++ .test-infra/jenkins/job_beam_PostCommit_Java_ValidatesRunner_Dataflow_Streaming.groovy
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import common_job_properties
+
+// This job runs the suite of ValidatesRunner tests against the Dataflow
+// runner.
+mavenJob('beam_PostCommit_Java_ValidatesRunner_Dataflow_Streaming') {
+  description('Runs the ValidatesRunner suite on the Dataflow runner in streaming mode.')
+
+  // Set common parameters.
+  common_job_properties.setTopLevelMainJobProperties(delegate, 'master', 120)
+
+  // Set maven parameters.
+  common_job_properties.setMavenConfig(delegate)
+
+  // Sets that this is a PostCommit job.
+  common_job_properties.setPostCommit(delegate)
+
+  // Allows triggering this build against pull requests.
+  common_job_properties.enablePhraseTriggeringFromPullRequest(
+    delegate,
+    'Google Cloud Dataflow Runner Streaming ValidatesRunner Tests',
+    'Run Dataflow ValidatesRunner Streaming')
+
+  // Maven goals for this job.
+  goals([
+    'clean',
+    'verify',
+    '--projects runners/google-cloud-dataflow-java',
+    '--batch-mode',
+    '--errors',
+    '--also-make',
+    '-DforkCount=0',
+    '-Ddataflow.skipStreamingITs=false',
+    '-Ddataflow.skipBatchITs=true',
+    '''-DvalidatesRunnerPipelineOptions='[
+        "--runner=TestDataflowRunner",
+        "--project=apache-beam-testing",
+        "--tempRoot=gs://temp-storage-for-validates-runner-tests/"
+    ]' '''
+  ].join(' '))
+}
