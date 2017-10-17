b8616e8ac79578ad12caa5bf83041d99ce2e9338
Henning Rohde <herohde@google.com>
2017-10-17 09:14:52 -0700

    Add 3 progressive wordcount examples
    Change-Id: I596f41a024701316e26adc7ea8ba190db9413323
    Reviewed-on: https://team-review.git.corp.google.com/137262
    Verified-Style: kokoro <noreply+kokoro@google.com>
    Verified-BuildAndTest: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Bill Neubauer <wcn@google.com>

---
+++ examples/minimal_wordcount/minimal_wordcount.go
@@ -0,0 +1,85 @@
+// minimal_wordcount is an example that counts words in Shakespeare.
+//
+// This example is the first in a series of four successively more detailed
+// 'word count' examples. Here, for simplicity, we don't show any
+// error-checking or argument processing, and focus on construction of the
+// pipeline, which chains together the application of core transforms.
+//
+// Next, see the wordcount pipeline, then the debugging_wordcount, and finally
+// the windowed_wordcount pipeline, for more detailed examples that introduce
+// additional concepts.
+//
+// Concepts:
+//
+//   1. Reading data from text files
+//   2. Specifying 'inline' transforms
+//   3. Counting items in a PCollection
+//   4. Writing data to text files
+//
+// No arguments are required to run this pipeline. It will be executed with
+// the local runner. You can see the results in the output file named
+// "wordcounts.txt" in your current working directory.
+package main
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	"github.com/apache/beam/sdks/go/pkg/beam"
+	"github.com/apache/beam/sdks/go/pkg/beam/io/textio"
+	_ "github.com/apache/beam/sdks/go/pkg/beam/io/textio/gcs"
+	_ "github.com/apache/beam/sdks/go/pkg/beam/io/textio/local"
+	"github.com/apache/beam/sdks/go/pkg/beam/runners/local"
+	"github.com/apache/beam/sdks/go/pkg/beam/transforms/stats"
+)
+
+var wordRE = regexp.MustCompile(`[a-zA-Z]+('[a-z])?`)
+
+func main() {
+	// Create the Pipeline object.
+	p := beam.NewPipeline()
+
+	// Apply the pipeline's transforms.
+
+	// Concept #1: Invoke a root transform with the pipeline; in this case,
+	// textio.Read to read a set of input text file. textio.Read returns a
+	// PCollection where each element is one line from the input text
+	// (one of of Shakespeare's texts).
+
+	// This example reads a public data set consisting of the complete works
+	// of Shakespeare.
+	lines := textio.Read(p, "gs://apache-beam-samples/shakespeare/*")
+
+	// Concept #2: Invoke a ParDo transform on our PCollection of text lines.
+	// This ParDo invokes a DoFn (defined in-line) on each element that
+	// tokenizes the text line into individual words. The ParDo returns a
+	// PCollection of type string, where each element is an individual word in
+	// Shakespeare's collected texts.
+	words := beam.ParDo(p, func(line string, emit func(string)) {
+		for _, word := range wordRE.FindAllString(line, -1) {
+			emit(word)
+		}
+	}, lines)
+
+	// Concept #3: Invoke the stats.Count transform on our PCollection of
+	// individual words. The Count transform returns a new PCollection of
+	// key/value pairs, where each key represents a unique word in the text.
+	// The associated value is the occurrence count for that word.
+	counted := stats.Count(p, words)
+
+	// Use a ParDo to format our PCollection of word counts into a printable
+	// string, suitable for writing to an output file. When each element
+	// produces exactly one element, the DoFn can simply return it.
+	formatted := beam.ParDo(p, func(w string, c int) string {
+		return fmt.Sprintf("%s: %v", w, c)
+	}, counted)
+
+	// Concept #4: Invoke textio.Write at the end of the pipeline to write
+	// the contents of a PCollection (in this case, our PCollection of
+	// formatted strings) to a text file.
+	textio.Write(p, "wordcounts.txt", formatted)
+
+	// Run the pipeline on the local runner.
+	local.Execute(context.Background(), p)
+}
