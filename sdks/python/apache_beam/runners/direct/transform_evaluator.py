87c7c568ede703397fabeafd25bc3cb4595b733a
Robert Bradshaw <robertwb@gmail.com>
2017-10-20 18:04:17 -0700

    Type hint fixes.

--- sdks/python/apache_beam/runners/direct/transform_evaluator.py
+++ sdks/python/apache_beam/runners/direct/transform_evaluator.py
@@ -584,10 +584,9 @@ class _GroupByKeyOnlyEvaluator(_TransformEvaluator):
     assert len(self._outputs) == 1
     self.output_pcollection = list(self._outputs)[0]
 
-    # The input type of a GroupByKey will be KV[Any, Any] or more specific.
-    kv_type_hint = (
-        self._applied_ptransform.transform.get_type_hints().input_types[0])
-    self.key_coder = coders.registry.get_coder(kv_type_hint[0].tuple_types[0])
+    # The output type of a GroupByKey will be KV[Any, Any] or more specific.
+    kv_type_hint = self._applied_ptransform.outputs[None].element_type
+    self.key_coder = coders.registry.get_coder(kv_type_hint.tuple_types[0])
 
   def process_timer(self, timer_firing):
     # We do not need to emit a KeyedWorkItem to process_element().
