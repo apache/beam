df5ecc7c3631997fb7ced99f8d8e5748ccea7d9d
Henning Rohde <herohde@google.com>
2017-10-26 18:26:30 -0700

    Add apache 2.0 license to files
    Change-Id: I6867172b94a33cf946820e967e8a37095c2bcbbc
    Reviewed-on: https://team-review.git.corp.google.com/145307
    Verified-BuildAndTest: kokoro <noreply+kokoro@google.com>
    Verified-Style: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Robert Burke <rebo@google.com>

--- examples/cookbook/tornadoes/tornadoes.go
+++ examples/cookbook/tornadoes/tornadoes.go
@@ -1,3 +1,18 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // tornadoes is an example that reads the public samples of weather data from
 // BigQuery, counts the number of tornadoes that occur in each month, and
 // writes the results to BigQuery.
