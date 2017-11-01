9c34d59078b21b727966b86037bd4395f2a64fac
Thomas Groh <tgroh@google.com>
2017-11-01 10:45:51 -0700

    Update Website Precommits to new RVM instructions

--- .test-infra/jenkins/job_beam_PreCommit_Website_Merge.groovy
+++ .test-infra/jenkins/job_beam_PreCommit_Website_Merge.groovy
@@ -33,9 +33,10 @@ job('beam_PreCommit_Website_Merge') {
     // Run the following shell script as a build step.
     shell '''
         # Install RVM per instructions at https://rvm.io/rvm/install.
-        RVM_GPG_KEY=409B6B1796C275462A1703113804BB82D39DC0E3
-        gpg --keyserver hkp://keys.gnupg.net --recv-keys $RVM_GPG_KEY
-            
+        gpg --keyserver hkp://keys.gnupg.net --recv-keys \\
+            409B6B1796C275462A1703113804BB82D39DC0E3 \\
+            7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
         \\curl -sSL https://get.rvm.io | bash
         source /home/jenkins/.rvm/scripts/rvm
 
--- .test-infra/jenkins/job_beam_PreCommit_Website_Test.groovy
+++ .test-infra/jenkins/job_beam_PreCommit_Website_Test.groovy
@@ -41,7 +41,9 @@ job('beam_PreCommit_Website_Test') {
     shell '''
         # Install RVM.
         gpg --keyserver hkp://keys.gnupg.net --recv-keys \\
-            409B6B1796C275462A1703113804BB82D39DC0E3
+            409B6B1796C275462A1703113804BB82D39DC0E3 \\
+            7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
         \\curl -sSL https://get.rvm.io | bash
         source /home/jenkins/.rvm/scripts/rvm
 
