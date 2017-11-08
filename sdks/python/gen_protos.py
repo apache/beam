241d7e4be6fe3d027fb1661c3831e9322b9e265b
Holden Karau <holdenkarau@google.com>
2017-11-07 23:29:34 -0800

    Run futurize regardless

--- sdks/python/gen_protos.py
+++ sdks/python/gen_protos.py
@@ -110,12 +110,14 @@ def generate_proto_files(force=False):
         raise RuntimeError(
             'Protoc returned non-zero status (see logs for details): '
             '%s' % ret_code)
-      ret_code = subprocess.call(
-          ["futurize", "-0", "-wv", "--no-diff", out_dir])
 
-      if ret_code:
-        raise RuntimeError(
-            'Error applying futurize to pb2 files.')
+
+    ret_code = subprocess.call(
+        ["futurize", "-0", "-wv", "--no-diff", out_dir])
+
+    if ret_code:
+      raise RuntimeError(
+          'Error applying futurize to generated protobuf python files.')
 
 
 # Though wheels are available for grpcio-tools, setup_requires uses
