65a27bddc73579bfddf024516cf604bb8cfa6ebf
Lukasz Cwik <lcwik@google.com>
2017-11-20 22:35:57 -0800

    Add precommit integration tests to examples/java
    Also make Jenkins run a common root task which enumerates all the tasks that it should run.

--- .test-infra/jenkins/job_beam_PreCommit_Java_GradleBuild.groovy
+++ .test-infra/jenkins/job_beam_PreCommit_Java_GradleBuild.groovy
@@ -32,11 +32,6 @@ job('beam_PreCommit_Java_GradleBuild') {
     'master',
     240)
 
-  def root_projects = [
-    ':beam-sdks-parent:beam-sdks-java-parent:beam-sdks-java-core',
-    ':beam-runners-parent:beam-runners-direct-java',
-    ':beam-sdks-parent:beam-sdks-java-parent:beam-sdks-java-fn-execution',
-  ]
   def gradle_switches = [
     // Continue the build even if there is a failure to show as many potential failures as possible.
     '--continue',
@@ -44,17 +39,13 @@ job('beam_PreCommit_Java_GradleBuild') {
     '--rerun-tasks',
   ]
 
-  def gradle_command_line = './gradlew ' + gradle_switches.join(' ') + ' ' + root_projects.join(':buildNeeded ') + ' ' + root_projects.join(':buildDependents ')
+  def gradle_command_line = './gradlew ' + gradle_switches.join(' ') + ' :javaPreCommit'
   // Sets that this is a PreCommit job.
   common_job_properties.setPreCommit(delegate, gradle_command_line, 'Run Java Gradle PreCommit')
-
   steps {
     gradle {
       rootBuildScriptDir(common_job_properties.checkoutDir)
-      for (String root_project : root_projects) {
-        tasks(root_project + ':buildNeeded')
-        tasks(root_project + ':buildDependents')
-      }
+      tasks(':javaPreCommit')
       for (String gradle_switch : gradle_switches) {
         switches(gradle_switch)
       }
