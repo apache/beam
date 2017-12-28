a3415426c4db69c045b8b8fa9c13651120d8a979
David Sabater <david.sabater@gmail.com>
2017-12-29 00:11:17 +0100

    Correct os.environ.get call Fix Docstring conventions as per PEP 257

--- sdks/python/apache_beam/io/gcp/gcsio.py
+++ sdks/python/apache_beam/io/gcp/gcsio.py
@@ -89,10 +89,12 @@ MAX_BATCH_OPERATION_SIZE = 100
 
 def proxy_info_from_environment_var(proxy_env_var):
   """Reads proxy info from the environment and converts to httplib2.ProxyInfo.
+
   Args:
     proxy_env_var: environment variable string to read, http_proxy or
        https_proxy (in lower case).
        Example: http://myproxy.domain.com:8080
+
   Returns:
     httplib2.ProxyInfo constructed from the environment string.
   """
@@ -104,21 +106,20 @@ def proxy_info_from_environment_var(proxy_env_var):
     logging.warn("proxy_info_from_url requires a protocol, which is always "
                  "http or https.")
     proxy_url = proxy_protocol + '://' + proxy_url
-
   return httplib2.proxy_info_from_url(proxy_url, method=proxy_protocol)
 
 
 def get_new_http():
   """Creates and returns a new httplib2.Http instance.
+  
   Returns:
     An initialized httplib2.Http instance.
   """
   proxy_info = None
   for proxy_env_var in ['http_proxy', 'https_proxy']:
-    if os.environ(proxy_env_var):
+    if os.environ.get(proxy_env_var):
       proxy_info = proxy_info_from_environment_var(proxy_env_var)
       break
-
   # Use a non-infinite SSL timeout to avoid hangs during network flakiness.
   return httplib2.Http(proxy_info=proxy_info,
                        timeout=DEFAULT_HTTP_TIMEOUT_SECONDS)
