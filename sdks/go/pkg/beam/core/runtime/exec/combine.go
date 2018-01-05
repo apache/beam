933f6efb086fd726f63bae3ace612163ea931c61
Henning Rohde <herohde@google.com>
2018-01-04 18:02:39 -0800

    Initial version of type-specialized general reflective calls

--- sdks/go/pkg/beam/core/runtime/exec/combine.go
+++ sdks/go/pkg/beam/core/runtime/exec/combine.go
@@ -39,6 +39,8 @@ type Combine struct {
 	first             bool
 	isPerKey, usesKey bool
 
+	mergeFn Caller // optimized caller in the case of binary merge accumulators
+
 	status Status
 	err    errorx.GuardedError
 }
@@ -56,6 +58,8 @@ func (n *Combine) Up(ctx context.Context) error {
 	if _, err := Invoke(ctx, n.Edge.CombineFn.SetupFn(), nil); err != nil {
 		return n.fail(err)
 	}
+
+	n.mergeFn = makeCaller(n.Edge.CombineFn.MergeAccumulatorsFn().Fn)
 
 	// TODO(herohde) 6/28/2017: maybe record the per-key mode in the Edge
 	// instead of inferring it here?
@@ -210,11 +214,8 @@ func (n *Combine) addInput(ctx context.Context, accum, key, value reflect.Value,
 		a := reflectx.UnderlyingType(accum)
 		b := reflectx.UnderlyingType(value)
 
-		val, err := Invoke(ctx, n.Edge.CombineFn.MergeAccumulatorsFn(), nil, a, b)
-		if err != nil {
-			return reflect.Value{}, n.fail(fmt.Errorf("MergeAccumulators failed: %v", err))
-		}
-		return val.Elm, err
+		vals := n.mergeFn.Call([]reflect.Value{a, b})
+		return vals[0], nil
 	}
 
 	opt := &MainInput{
