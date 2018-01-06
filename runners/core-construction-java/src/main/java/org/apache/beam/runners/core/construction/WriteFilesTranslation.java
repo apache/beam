6e90e809b0fc95d6beb1ecc53044584a85c1d17d
Kenneth Knowles <klk@google.com>
2018-01-06 11:05:36 -0800

    Clarify names and visibility of ParDoTranslation helpers

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/WriteFilesTranslation.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/WriteFilesTranslation.java
@@ -78,7 +78,7 @@ public class WriteFilesTranslation {
             Map<String, SideInput> sideInputs = new HashMap<>();
             for (PCollectionView<?> view :
                 transform.getSink().getDynamicDestinations().getSideInputs()) {
-              sideInputs.put(view.getTagInternal().getId(), ParDoTranslation.toProto(view));
+              sideInputs.put(view.getTagInternal().getId(), ParDoTranslation.translateView(view));
             }
             return sideInputs;
           }
