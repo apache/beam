6070a93cfa7f290583318b80d277b3a8d0cd176f
Thomas Groh <tgroh@google.com>
2018-01-09 15:51:44 -0800

    Add wire_coder_id to RemoteGrpcPort
    Use this ID in BeamFnDataReadRunner and BeamFnDataWriteRunner when
    populated.

--- sdks/java/harness/src/main/java/org/apache/beam/fn/harness/BeamFnDataReadRunner.java
+++ sdks/java/harness/src/main/java/org/apache/beam/fn/harness/BeamFnDataReadRunner.java
@@ -34,8 +34,10 @@ import org.apache.beam.fn.harness.data.MultiplexingFnDataReceiver;
 import org.apache.beam.fn.harness.fn.ThrowingRunnable;
 import org.apache.beam.fn.harness.state.BeamFnStateClient;
 import org.apache.beam.model.fnexecution.v1.BeamFnApi;
+import org.apache.beam.model.fnexecution.v1.BeamFnApi.RemoteGrpcPort;
 import org.apache.beam.model.pipeline.v1.Endpoints;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
+import org.apache.beam.model.pipeline.v1.RunnerApi.Components;
 import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
 import org.apache.beam.model.pipeline.v1.RunnerApi.PTransform;
 import org.apache.beam.runners.core.construction.CoderTranslation;
@@ -121,7 +123,7 @@ public class BeamFnDataReadRunner<OutputT> {
   private final Endpoints.ApiServiceDescriptor apiServiceDescriptor;
   private final FnDataReceiver<WindowedValue<OutputT>> receiver;
   private final Supplier<String> processBundleInstructionIdSupplier;
-  private final BeamFnDataClient beamFnDataClientFactory;
+  private final BeamFnDataClient beamFnDataClient;
   private final Coder<WindowedValue<OutputT>> coder;
   private final BeamFnApi.Target inputTarget;
 
@@ -133,28 +135,37 @@ public class BeamFnDataReadRunner<OutputT> {
       BeamFnApi.Target inputTarget,
       RunnerApi.Coder coderSpec,
       Map<String, RunnerApi.Coder> coders,
-      BeamFnDataClient beamFnDataClientFactory,
+      BeamFnDataClient beamFnDataClient,
       Collection<FnDataReceiver<WindowedValue<OutputT>>> consumers)
-          throws IOException {
-    this.apiServiceDescriptor =
-        RemoteGrpcPortRead.fromPTransform(grpcReadNode).getPort().getApiServiceDescriptor();
+      throws IOException {
+    RemoteGrpcPort port = RemoteGrpcPortRead.fromPTransform(grpcReadNode).getPort();
+    this.apiServiceDescriptor = port.getApiServiceDescriptor();
     this.inputTarget = inputTarget;
     this.processBundleInstructionIdSupplier = processBundleInstructionIdSupplier;
-    this.beamFnDataClientFactory = beamFnDataClientFactory;
+    this.beamFnDataClient = beamFnDataClient;
     this.receiver = MultiplexingFnDataReceiver.forConsumers(consumers);
 
+    RehydratedComponents components =
+        RehydratedComponents.forComponents(Components.newBuilder().putAllCoders(coders).build());
     @SuppressWarnings("unchecked")
-    Coder<WindowedValue<OutputT>> coder =
-        (Coder<WindowedValue<OutputT>>)
-            CoderTranslation.fromProto(
-                coderSpec,
-                RehydratedComponents.forComponents(
-                    RunnerApi.Components.newBuilder().putAllCoders(coders).build()));
+    Coder<WindowedValue<OutputT>> coder;
+    if (!port.getCoderId().isEmpty()) {
+      coder =
+          (Coder<WindowedValue<OutputT>>)
+              CoderTranslation.fromProto(coders.get(port.getCoderId()), components);
+    } else {
+      // TODO: Remove this path once it is no longer used
+      coder =
+          (Coder<WindowedValue<OutputT>>)
+              CoderTranslation.fromProto(
+                  coderSpec,
+                  components);
+    }
     this.coder = coder;
   }
 
   public void registerInputLocation() {
-    this.readFuture = beamFnDataClientFactory.receive(
+    this.readFuture = beamFnDataClient.receive(
         apiServiceDescriptor,
         LogicalEndpoint.of(processBundleInstructionIdSupplier.get(), inputTarget),
         coder,
