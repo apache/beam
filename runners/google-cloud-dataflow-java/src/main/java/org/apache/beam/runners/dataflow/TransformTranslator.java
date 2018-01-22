faefd36b73eb29d0161fbd198459748436ab91ca
Luke Cwik <lcwik@google.com>
2018-01-22 12:10:17 -0800

    fixup! Fix broken javadoc.

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/TransformTranslator.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/TransformTranslator.java
@@ -103,7 +103,7 @@ public interface TransformTranslator<TransformT extends PTransform> {
      * <p>The input {@link PValue} must have already been produced by a step earlier in this
      * {@link Pipeline}. If the input value has not yet been produced yet (by a call to either
      * {@link StepTranslationContext#addOutput(String, PCollection)} or
-     * {@link StepTranslationContext#addCollectionToSingletonOutput(PCollection, PCollectionView)})
+     * {@link StepTranslationContext#addCollectionToSingletonOutput(PCollection, String, PCollectionView)})
      * this method will throw an exception.
      */
     void addInput(String name, PInput value);
