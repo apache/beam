09f75b23df5a13cbdf315b424b8a91b318eca240
Thomas Groh <tgroh@google.com>
2018-01-22 16:34:36 -0800

    Register Environments in SdkComponents
    The current environment registered is a placeholder URL, but can be used
    to identify distinct environments.

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/ParDoTranslation.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/ParDoTranslation.java
@@ -41,6 +41,7 @@ import java.util.Set;
 import javax.annotation.Nullable;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
 import org.apache.beam.model.pipeline.v1.RunnerApi.Components;
+import org.apache.beam.model.pipeline.v1.RunnerApi.Environment;
 import org.apache.beam.model.pipeline.v1.RunnerApi.FunctionSpec;
 import org.apache.beam.model.pipeline.v1.RunnerApi.ParDoPayload;
 import org.apache.beam.model.pipeline.v1.RunnerApi.Parameter.Type;
@@ -146,6 +147,11 @@ public class ParDoTranslation {
           @Override
           public SdkFunctionSpec translateDoFn(SdkComponents newComponents) {
             return ParDoTranslation.translateDoFn(parDo.getFn(), parDo.getMainOutputTag());
+          }
+
+          @Override
+          public Environment getEnvironment() {
+            return Environments.JAVA_SDK_HARNESS_ENVIRONMENT;
           }
 
           @Override
@@ -327,7 +333,7 @@ public class ParDoTranslation {
                 .setCombiningSpec(
                     RunnerApi.CombiningStateSpec.newBuilder()
                         .setAccumulatorCoderId(registerCoderOrThrow(components, accumCoder))
-                        .setCombineFn(CombineTranslation.toProto(combineFn)))
+                        .setCombineFn(CombineTranslation.toProto(combineFn, components)))
                 .build();
           }
 
@@ -591,6 +597,11 @@ public class ParDoTranslation {
     }
 
     @Override
+    public Environment getEnvironment() {
+      return rehydratedComponents.getEnvironment(payload.getDoFn().getEnvironmentId());
+    }
+
+    @Override
     public List<RunnerApi.Parameter> translateParameters() {
       return MoreObjects.firstNonNull(
           payload.getParametersList(), Collections.<RunnerApi.Parameter>emptyList());
@@ -625,6 +636,8 @@ public class ParDoTranslation {
   /** These methods drive to-proto translation from Java and from rehydrated ParDos. */
   public interface ParDoLike {
     SdkFunctionSpec translateDoFn(SdkComponents newComponents);
+
+    Environment getEnvironment();
 
     List<RunnerApi.Parameter> translateParameters();
 
