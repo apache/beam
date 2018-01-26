2f4ddeafb44a783572e935a28db34403f5a8099d
Daniel Kulp <dkulp@apache.org>
2018-01-26 14:24:05 -0500

    Set the proper package for the Snippets example so eclipse won't raise an error

--- examples/java/src/main/java/org/apache/beam/examples/snippets/Snippets.java
+++ examples/java/src/main/java/org/apache/beam/examples/snippets/Snippets.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.examples;
+package org.apache.beam.examples.snippets;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- examples/java/src/test/java/org/apache/beam/examples/snippets/SnippetsTest.java
+++ examples/java/src/test/java/org/apache/beam/examples/snippets/SnippetsTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.examples;
+package org.apache.beam.examples.snippets;
 
 import java.io.IOException;
 import java.io.Serializable;
