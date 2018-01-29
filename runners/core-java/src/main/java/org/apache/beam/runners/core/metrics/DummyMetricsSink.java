703d7bf4813e53e2c837b21953db4dd0d9c60884
Etienne Chauchot <echauchot@apache.org>
2018-01-29 13:57:03 +0100

    Improve MetricsPusher teardown to enable multiple pipelines in a single JVM. Cleaning

--- runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/DummyMetricsSink.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/DummyMetricsSink.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.beam.runners.core.metrics;
 
 import org.apache.beam.sdk.metrics.MetricQueryResults;
@@ -16,16 +34,19 @@ public class DummyMetricsSink extends MetricsSink<Long> {
       public Long serializeMetrics(MetricQueryResults metricQueryResults) throws Exception {
         return metricQueryResults.counters().iterator().hasNext()
             ? metricQueryResults.counters().iterator().next().attempted()
-            : null;
+            : 0L;
       }
     };
   }
 
   @Override protected void writeSerializedMetrics(Long metrics) throws Exception {
-    counterValue = metrics != null ? metrics : 0L;
+    setCounterValue(metrics);
   }
   public static long getCounterValue(){
     return counterValue;
+  }
+  private static void setCounterValue(Long value){
+    counterValue = value;
   }
 
   public static void clear(){
