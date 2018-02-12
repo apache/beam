d41fec205fa34b23f2b9b019b0ec41740f7093c0
Andrew Pilloud <apilloud@google.com>
2018-02-12 14:27:39 -0800

    [Nexmark][SQL] Implement Query5

--- sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/NexmarkLauncher.java
+++ sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/NexmarkLauncher.java
@@ -82,6 +82,7 @@ import org.apache.beam.sdk.nexmark.queries.sql.SqlQuery0;
 import org.apache.beam.sdk.nexmark.queries.sql.SqlQuery1;
 import org.apache.beam.sdk.nexmark.queries.sql.SqlQuery2;
 import org.apache.beam.sdk.nexmark.queries.sql.SqlQuery3;
+import org.apache.beam.sdk.nexmark.queries.sql.SqlQuery5;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -1206,7 +1207,9 @@ public class NexmarkLauncher<OptionT extends NexmarkOptions> {
         new NexmarkSqlQuery(configuration, new SqlQuery0()),
         new NexmarkSqlQuery(configuration, new SqlQuery1()),
         new NexmarkSqlQuery(configuration, new SqlQuery2(configuration.auctionSkip)),
-        new NexmarkSqlQuery(configuration, new SqlQuery3(configuration)));
+        new NexmarkSqlQuery(configuration, new SqlQuery3(configuration)),
+        null,
+        new NexmarkSqlQuery(configuration, new SqlQuery5(configuration)));
   }
 
   private List<NexmarkQuery> createJavaQueries() {
