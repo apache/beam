017942bbae0f5de7a6af21f295652e3d833f5212
Pavel Slechta <pavel.slechta@firma.seznam.cz>
2018-02-13 12:58:30 +0100

    Fix KafkaSourceTest

--- sdks/java/extensions/euphoria/euphoria-kafka/src/test/java/cz/seznam/euphoria/kafka/KafkaSourceTest.java
+++ sdks/java/extensions/euphoria/euphoria-kafka/src/test/java/cz/seznam/euphoria/kafka/KafkaSourceTest.java
@@ -45,14 +45,14 @@ public class KafkaSourceTest {
   public void testPartitions() {
     Node leaderNode = new Node(1, "localhost", 3333);
     PartitionInfo pi = new PartitionInfo("topic", 0, leaderNode, null, null);
-    tryGetPartitions(Collections.emptyList());
+    tryGetPartitions(Collections.singletonList(pi));
   }
 
   @Test(expected = IllegalStateException.class)
   @SuppressWarnings("unchecked")
   public void testNoLeader() {
     PartitionInfo pi = new PartitionInfo("topic", 0, Node.noNode(), null, null);
-    tryGetPartitions(Collections.emptyList());
+    tryGetPartitions(Collections.singletonList(pi));
   }
 
   public void tryGetPartitions(List<PartitionInfo> partitions) {
