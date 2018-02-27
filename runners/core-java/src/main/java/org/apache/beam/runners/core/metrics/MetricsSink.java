dc2605765e0732fd6d69d6923a1099737e990d19
Etienne Chauchot <echauchot@apache.org>
2018-02-27 16:41:24 +0100

    Remove unneeded null check, format

--- runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/MetricsSink.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/MetricsSink.java
@@ -21,21 +21,17 @@ package org.apache.beam.runners.core.metrics;
 import java.io.Serializable;
 import org.apache.beam.sdk.metrics.MetricQueryResults;
 
-/**
- * Abstract base class for all metric sinks it can use different serializers.
- */
-
-public abstract class MetricsSink<OutputT> implements Serializable{
+/** Abstract base class for all metric sinks it can use different serializers. */
+public abstract class MetricsSink<OutputT> implements Serializable {
 
   private MetricsSerializer<OutputT> metricsSerializer = provideSerializer();
 
   void writeMetrics(MetricQueryResults metricQueryResults) throws Exception {
-    if (metricsSerializer != null) {
-      OutputT serializedMetrics = metricsSerializer.serializeMetrics(metricQueryResults);
-      writeSerializedMetrics(serializedMetrics);
-    }
+    OutputT serializedMetrics = metricsSerializer.serializeMetrics(metricQueryResults);
+    writeSerializedMetrics(serializedMetrics);
   }
+
   protected abstract MetricsSerializer<OutputT> provideSerializer();
+
   protected abstract void writeSerializedMetrics(OutputT metrics) throws Exception;
 }
-
