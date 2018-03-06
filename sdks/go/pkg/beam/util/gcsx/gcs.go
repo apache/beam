56515fadc6a9ee433b2f243a75fb4ed036124fc6
Kenn Knowles <kenn@kennknowles.com>
2018-03-06 14:27:07 -0800

    Revert "Merge master into go-sdk"

--- sdks/go/pkg/beam/util/gcsx/gcs.go
+++ sdks/go/pkg/beam/util/gcsx/gcs.go
@@ -23,10 +23,7 @@ import (
 	"io/ioutil"
 	"net/url"
 
-	"net/http"
-
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/googleapi"
 	"google.golang.org/api/storage/v1"
 )
 
@@ -37,43 +34,6 @@ func NewClient(ctx context.Context, scope string) (*storage.Service, error) {
 		return nil, err
 	}
 	return storage.New(cl)
-}
-
-// Upload writes the given content to GCS. If the specified bucket does not
-// exist, it is created first. Returns the full path of the object.
-func Upload(client *storage.Service, project, bucket, object string, r io.Reader) (string, error) {
-	exists, err := BucketExists(client, bucket)
-	if err != nil {
-		return "", err
-	}
-	if !exists {
-		if err = CreateBucket(client, project, bucket); err != nil {
-			return "", err
-		}
-	}
-
-	if err := WriteObject(client, bucket, object, r); err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("gs://%s/%s", bucket, object), nil
-}
-
-// CreateBucket creates a bucket in GCS.
-func CreateBucket(client *storage.Service, project, bucket string) error {
-	b := &storage.Bucket{
-		Name: bucket,
-	}
-	_, err := client.Buckets.Insert(project, b).Do()
-	return err
-}
-
-// BucketExists returns true iff the given bucket exists.
-func BucketExists(client *storage.Service, bucket string) (bool, error) {
-	_, err := client.Buckets.Get(bucket).Do()
-	if e, ok := err.(*googleapi.Error); ok && e.Code == http.StatusNotFound {
-		return false, nil
-	}
-	return err == nil, err
 }
 
 // WriteObject writes the given content to the specified object. If the object
