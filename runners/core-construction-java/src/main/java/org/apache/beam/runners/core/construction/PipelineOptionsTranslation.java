bd128793089b4a7444e796a2c6e81a10d99b37c5
Luke Cwik <lcwik@google.com>
2018-03-12 15:45:28 -0700

    Use the beam:option:value:v1 as the portable pipeline options representation

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/PipelineOptionsTranslation.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/PipelineOptionsTranslation.java
@@ -18,14 +18,27 @@
 
 package org.apache.beam.runners.core.construction;
 
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.JsonObject;
 import com.google.protobuf.Struct;
 import com.google.protobuf.util.JsonFormat;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.util.common.ReflectHelpers;
 
-/** Utilities for going to/from Runner API pipeline options. */
+/**
+ * Utilities for going to/from Runner API pipeline options.
+ *
+ * <p>TODO: Make this the default to/from translation for PipelineOptions.
+ */
 public class PipelineOptionsTranslation {
   private static final ObjectMapper MAPPER =
       new ObjectMapper()
@@ -34,10 +47,24 @@ public class PipelineOptionsTranslation {
   /** Converts the provided {@link PipelineOptions} to a {@link Struct}. */
   public static Struct toProto(PipelineOptions options) {
     Struct.Builder builder = Struct.newBuilder();
+
     try {
+      // TODO: Officially define URNs for options and their scheme.
+      TreeNode treeNode = MAPPER.valueToTree(options);
+      TreeNode rootOptions = treeNode.get("options");
+      Iterator<String> optionsKeys = rootOptions.fieldNames();
+      Map<String, TreeNode> optionsUsingUrns = new HashMap<String, TreeNode>();
+      while (optionsKeys.hasNext()) {
+        String optionKey = optionsKeys.next();
+        TreeNode optionValue = rootOptions.get(optionKey);
+        optionsUsingUrns.put(
+            "beam:option:" + CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, optionKey) + ":v1",
+            optionValue);
+      }
+
       // The JSON format of a Protobuf Struct is the JSON object that is equivalent to that struct
       // (with values encoded in a standard json-codeable manner). See Beam PR 3719 for more.
-      JsonFormat.parser().merge(MAPPER.writeValueAsString(options), builder);
+      JsonFormat.parser().merge(MAPPER.writeValueAsString(optionsUsingUrns), builder);
       return builder.build();
     } catch (IOException e) {
       throw new RuntimeException(e);
@@ -46,6 +73,21 @@ public class PipelineOptionsTranslation {
 
   /** Converts the provided {@link Struct} into {@link PipelineOptions}. */
   public static PipelineOptions fromProto(Struct protoOptions) throws IOException {
-    return MAPPER.readValue(JsonFormat.printer().print(protoOptions), PipelineOptions.class);
+    Map<String, TreeNode> mapWithoutUrns = new HashMap<String, TreeNode>();
+    TreeNode rootOptions = MAPPER.readTree(
+        JsonFormat.printer().print(protoOptions));
+    Iterator<String> optionsKeys = rootOptions.fieldNames();
+    while (optionsKeys.hasNext()) {
+      String optionKey = optionsKeys.next();
+      TreeNode optionValue = rootOptions.get(optionKey);
+      mapWithoutUrns.put(
+          CaseFormat.LOWER_UNDERSCORE.to(
+              CaseFormat.LOWER_CAMEL,
+              optionKey.substring("beam:option:".length(), optionKey.length() - ":v1".length())),
+          optionValue);
+    }
+    return MAPPER.readValue(
+        MAPPER.writeValueAsString(ImmutableMap.of("options", mapWithoutUrns)),
+        PipelineOptions.class);
   }
 }
