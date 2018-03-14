2419e9bc02ee2666981a23c24f20d898a4cf38cc
Henning Rohde <herohde@google.com>
2018-03-14 15:20:33 -0700

    Add Go support for universal runners, incl Flink
     * Extracted a runnerlib for runners that wish to use the portable
       model, but adds special logic or integration.

---
+++ sdks/go/pkg/beam/runners/universal/runnerlib/execute.go
@@ -0,0 +1,79 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package runnerlib
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/apache/beam/sdks/go/pkg/beam/log"
+	jobpb "github.com/apache/beam/sdks/go/pkg/beam/model/jobmanagement_v1"
+	pb "github.com/apache/beam/sdks/go/pkg/beam/model/pipeline_v1"
+	"github.com/apache/beam/sdks/go/pkg/beam/util/grpcx"
+)
+
+// Execute executes a pipeline on the universal runner serving the given endpoint.
+// Convenience function.
+func Execute(ctx context.Context, p *pb.Pipeline, endpoint string, opt *JobOptions, async bool) (string, error) {
+	// (1) Prepare job to obtain artifact staging instructions.
+
+	cc, err := grpcx.Dial(ctx, endpoint, 2*time.Minute)
+	if err != nil {
+		return "", fmt.Errorf("failed to connect to job service: %v", err)
+	}
+	defer cc.Close()
+	client := jobpb.NewJobServiceClient(cc)
+
+	prepID, artifactEndpoint, err := Prepare(ctx, client, p, opt)
+	if err != nil {
+		return "", err
+	}
+
+	log.Infof(ctx, "Prepared job with id: %v", prepID)
+
+	// (2) Stage artifacts.
+
+	worker, err := BuildTempWorkerBinary(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(worker)
+
+	token, err := Stage(ctx, prepID, artifactEndpoint, worker)
+	if err != nil {
+		return "", err
+	}
+
+	log.Infof(ctx, "Staged binary artifact with token: %v", token)
+
+	// (3) Submit job
+
+	jobID, err := Submit(ctx, client, prepID, token)
+	if err != nil {
+		return "", err
+	}
+
+	log.Infof(ctx, "Submitted job: %v", jobID)
+
+	// (4) Wait for completion.
+
+	if async {
+		return jobID, nil
+	}
+	return jobID, WaitForCompletion(ctx, client, jobID)
+}
---
+++ sdks/go/pkg/beam/runners/universal/universal.go
@@ -0,0 +1,59 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package universal contains a general-purpose runner that can submit jobs
+// to any portable Beam runner.
+package universal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/apache/beam/sdks/go/pkg/beam"
+	"github.com/apache/beam/sdks/go/pkg/beam/core/runtime/graphx"
+	_ "github.com/apache/beam/sdks/go/pkg/beam/core/runtime/harness/init"
+	"github.com/apache/beam/sdks/go/pkg/beam/options/jobopts"
+	"github.com/apache/beam/sdks/go/pkg/beam/runners/universal/runnerlib"
+)
+
+func init() {
+	// Note that we also _ import harness/init to setup the remote execution hook.
+	beam.RegisterRunner("universal", Execute)
+}
+
+// Execute execute the pipeline on a universal beam runner.
+func Execute(ctx context.Context, p *beam.Pipeline) error {
+	endpoint, err := jobopts.GetEndpoint()
+	if err != nil {
+		return err
+	}
+
+	edges, _, err := p.Build()
+	if err != nil {
+		return err
+	}
+	pipeline, err := graphx.Marshal(edges, &graphx.Options{ContainerImageURL: jobopts.GetContainerImage(ctx)})
+	if err != nil {
+		return fmt.Errorf("failed to generate model pipeline: %v", err)
+	}
+
+	opt := &runnerlib.JobOptions{
+		Name:               jobopts.GetJobName(),
+		Experiments:        jobopts.GetExperiments(),
+		InternalJavaRunner: *jobopts.InternalJavaRunner,
+	}
+	_, err = runnerlib.Execute(ctx, pipeline, endpoint, opt, *jobopts.Async)
+	return err
+}
