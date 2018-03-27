e62120a57fcb78429095a4e1422106d5b7f45051
Andrew Pilloud <apilloud@google.com>
2018-03-27 09:33:35 -0700

    [BEAM-3983][SQL] Add BigQuery table provider
    This partially reverts commit 0e1dcccf2fbc3737c72cd52fd3eb2018b586c85c.

---
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/meta/provider/bigquery/BigQueryTableProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.beam.sdk.extensions.sql.meta.provider.bigquery;
+
+import org.apache.beam.sdk.extensions.sql.BeamSqlTable;
+import org.apache.beam.sdk.extensions.sql.meta.Table;
+import org.apache.beam.sdk.extensions.sql.meta.provider.InMemoryMetaTableProvider;
+import org.apache.beam.sdk.schemas.Schema;
+
+/**
+ * BigQuery table provider.
+ *
+ * <p>A sample of text table is:
+ * <pre>{@code
+ * CREATE TABLE ORDERS(
+ *   ID INT COMMENT 'this is the primary key',
+ *   NAME VARCHAR(127) COMMENT 'this is the name'
+ * )
+ * TYPE 'bigquery'
+ * COMMENT 'this is the table orders'
+ * LOCATION '[PROJECT_ID]:[DATASET].[TABLE]'
+ * }</pre>
+ */
+public class BigQueryTableProvider extends InMemoryMetaTableProvider {
+
+  @Override public String getTableType() {
+    return "bigquery";
+  }
+
+  @Override public BeamSqlTable buildBeamSqlTable(Table table) {
+    Schema schema = table.getSchema();
+    String filePattern = table.getLocation();
+
+    return new BeamBigQueryTable(schema, filePattern);
+  }
+}
