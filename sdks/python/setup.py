2ba7e2cc4e985fe1c4d355634ed6661ce88d34f2
Udi Meiri (Ehud) <udim@users.noreply.github.com>
2018-03-27 15:43:23 -0700

    [BEAM-3744] Expand Pubsub read API for Python. (#4901)
    * Expand Pubsub read API for Python.
    
    New features:
    - Timestamp attributes (user attribute containing message timestamp)
      - In either milliseconds from epoch or RFC 3339.
    - Label IDs (user attribute used as a unique message ID)
    - Raw payloads
    - Source API can return either:
      - Payloads (raw or decoded UTF-8)
      - PubSubMessage type, encapsulating message payload and attributes
    
    * Address review comments.
    
    * Move to a single read PTransform: ReadFromPubSub
    
    ReadStringsFromPubSub will be removed in the future.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -106,6 +106,7 @@ REQUIRED_PACKAGES = [
     'oauth2client>=2.0.1,<5',
     # grpcio 1.8.1 and above requires protobuf 3.5.0.post1.
     'protobuf>=3.5.0.post1,<4',
+    'pytz>=2018.3',
     'pyyaml>=3.12,<4.0.0',
     'pyvcf>=0.6.8,<0.7.0',
     'six>=1.9,<1.12',
