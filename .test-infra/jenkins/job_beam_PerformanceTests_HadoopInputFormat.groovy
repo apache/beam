26fe64f8c82832009d6fb18cae8837fd630dfc34
Łukasz Gajowy <lukasz.gajowy@polidea.com>
2018-04-17 15:24:12 +0200

    [BEAM-4099] Rebuild project before running Performance tests on Jenkins

--- .test-infra/jenkins/job_beam_PerformanceTests_HadoopInputFormat.groovy
+++ .test-infra/jenkins/job_beam_PerformanceTests_HadoopInputFormat.groovy
@@ -51,7 +51,7 @@ job('beam_PerformanceTests_HadoopInputFormat') {
             beam_it_timeout         : '1200',
             benchmarks              : 'beam_integration_benchmark',
             beam_it_profile         : 'io-it',
-            beam_prebuilt           : 'true',
+            beam_prebuilt           : 'false',
             beam_sdk                : 'java',
             beam_it_module          : 'sdks/java/io/hadoop-input-format',
             beam_it_class           : 'org.apache.beam.sdk.io.hadoop.inputformat.HadoopInputFormatIOIT',
