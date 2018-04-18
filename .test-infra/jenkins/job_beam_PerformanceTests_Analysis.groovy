f41d17bd3a24fee98e2dbedad8ca8962927a6d11
Kamil Szewczyk <szewinho@gmail.com>
2018-04-18 21:44:45 +0200

    Fixed groovy parsing issue

--- .test-infra/jenkins/job_beam_PerformanceTests_Analysis.groovy
+++ .test-infra/jenkins/job_beam_PerformanceTests_Analysis.groovy
@@ -75,12 +75,11 @@ job(testConfiguration.jobName) {
         // Install job requirements for analysis script
         shell('.env/bin/pip install requests google.cloud.bigquery')
         // Launch performance tests analysis.
-        shell(".env/bin/python " + common_job_properties.checkoutDir +
-                '.test-infra/jenkins/verify_performance_test_results.py '
-                + '--bqtables=\'['+ testConfiguration.bqTables + ']\' ' +
-                + '--metric=\'run_time\''
-                + '--mode=report '
-                + '--notification=false '
+        shell('.env/bin/python ' + common_job_properties.checkoutDir +
+                '.test-infra/jenkins/verify_performance_test_results.py ' +
+                '--bqtables=\'['+ testConfiguration.bqTables + ']\' ' +
+                '--metric=\'run_time\' ' +
+                '--mode=report '
         )
     }
 }
