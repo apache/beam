580aa2456220fd175ad96230f22b191adbeed6a4
Braden Bassingthwaite <bbassingthwaite@vendasta.com>
2018-04-19 19:28:11 -0600

    - Add package doc

--- sdks/go/pkg/beam/io/datastoreio/datastore.go
+++ sdks/go/pkg/beam/io/datastoreio/datastore.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package datastoreio provides transformations and utilities to interact with
+// Google Datastore. See also: https://cloud.google.com/datastore/docs.
 package datastoreio
 
 import (
