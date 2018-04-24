f809cc089c13a571c3ada9d0a88456110ac4db76
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-04-24 10:44:30 -0700

    BEAM-4165, remove node restrictions of Python Jenkins tests

--- .test-infra/jenkins/job_beam_PostCommit_Python_ValidatesRunner_Dataflow.groovy
+++ .test-infra/jenkins/job_beam_PostCommit_Python_ValidatesRunner_Dataflow.groovy
@@ -35,18 +35,6 @@ job('beam_PostCommit_Python_ValidatesRunner_Dataflow') {
       'Google Cloud Dataflow Runner Python ValidatesRunner Tests',
       'Run Python Dataflow ValidatesRunner')
 
-  // Allow the test to only run on particular nodes
-  // TODO(BEAM-1817): Remove once the tests can run on all nodes
-  parameters {
-    nodeParam('TEST_HOST') {
-      description('select test host as either beam1, 2 or 3')
-      defaultNodes(['beam1', 'beam2', 'beam3'])
-      allowedNodes(['beam1', 'beam2', 'beam3'])
-      trigger('multiSelectionDisallowed')
-      eligibility('IgnoreOfflineNodeEligibility')
-    }
-  }
-
   // Execute shell command to test Python SDK.
   steps {
     shell('cd ' + common_job_properties.checkoutDir + ' && bash sdks/python/run_validatesrunner.sh')
