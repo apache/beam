9334baa18e7d2e4e089bd29785b99d7b5a9faf37
Ben Sidhom <sidhom@google.com>
2018-04-25 09:54:19 -0700

    [BEAM-3972] Translate portable batch pipelines by proto
    In order to translate portable pipelines we need direct access to
    pipeline components and need to be able to match components with their
    respective ids. This change adds a translator that translates pipeline
    protos rather than rehydrated SDK pipelines into Flink execution plans.

---
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkPortablePipelineTranslator.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.flink;
+
+import org.apache.beam.model.pipeline.v1.RunnerApi;
+
+/**
+ * Interface for portable Flink translators. This allows for a uniform invocation pattern for
+ * pipeline translation between streaming and portable runners.
+ *
+ * <p>Pipeline translators will generally provide a mechanism to produce the translation contexts
+ * that they use for pipeline translation. Post translation, the translation context should contain
+ * a pipeline plan that has not yet been executed.
+ */
+public interface FlinkPortablePipelineTranslator<
+    T extends FlinkPortablePipelineTranslator.TranslationContext> {
+
+  /** The context used for pipeline translation. */
+  interface TranslationContext {
+    FlinkPipelineOptions getPipelineOptions();
+  }
+
+  /** Translates the given pipeline. */
+  void translate(T context, RunnerApi.Pipeline pipeline);
+}
---
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkExecutableStagePruningFunction.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.flink.translation.functions;
+
+import org.apache.beam.sdk.transforms.join.RawUnionValue;
+import org.apache.beam.sdk.util.WindowedValue;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.util.Collector;
+
+/** A Flink function that demultiplexes output from a {@link FlinkExecutableStageFunction}. */
+public class FlinkExecutableStagePruningFunction<T>
+    implements FlatMapFunction<RawUnionValue, WindowedValue<T>> {
+
+  private final int unionTag;
+
+  /**
+   * Creates a {@link FlinkExecutableStagePruningFunction} that extracts elements of the given union
+   * tag.
+   */
+  public FlinkExecutableStagePruningFunction(int unionTag) {
+    this.unionTag = unionTag;
+  }
+
+  @Override
+  public void flatMap(RawUnionValue rawUnionValue, Collector<WindowedValue<T>> collector) {
+    if (rawUnionValue.getUnionTag() == unionTag) {
+      collector.collect((WindowedValue<T>) rawUnionValue.getValue());
+    }
+  }
+}
