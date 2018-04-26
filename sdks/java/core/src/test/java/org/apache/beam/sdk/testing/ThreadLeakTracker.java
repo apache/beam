1404ff206a8d9247f749d02c180f358ad9b3ed1f
Etienne Chauchot <echauchot@apache.org>
2018-04-26 14:54:14 +0200

    [BEAM-4088] Blocks until the threads in metricsExecutorService is done, then evaluate the ThreadLeakTracker rule

--- sdks/java/core/src/test/java/org/apache/beam/sdk/testing/ThreadLeakTracker.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/testing/ThreadLeakTracker.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.sdk.test;
+package org.apache.beam.sdk.testing;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
