79c53a207505041fbc3be413698f3dec45bb2853
Colm O hEigeartaigh <coheigea@apache.org>
2018-05-03 14:41:58 +0100

    A few small fixes to catch a few regressions - put String literals first, and avoid unnecessary autoboxing etc.

--- sdks/java/io/hadoop-file-system/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
+++ sdks/java/io/hadoop-file-system/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopResourceId.java
@@ -40,7 +40,7 @@ class HadoopResourceId implements ResourceId {
   public ResourceId resolve(String other, ResolveOptions resolveOptions) {
     if (resolveOptions == StandardResolveOptions.RESOLVE_DIRECTORY) {
       if (!other.endsWith("/")) {
-        other += '/';
+        other += "/";
       }
       return new HadoopResourceId(uri.resolve(other));
     } else if (resolveOptions == StandardResolveOptions.RESOLVE_FILE) {
