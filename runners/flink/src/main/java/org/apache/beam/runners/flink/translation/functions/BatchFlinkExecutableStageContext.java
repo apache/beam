047171392595007964c4f8ed1ae37204ac301c93
Ben Sidhom <sidhom@google.com>
2018-05-02 17:55:19 -0700

    [BEAM-4285] Implement Flink batch side input handler
    This implements a state request handler for multimap side inputs.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/BatchFlinkExecutableStageContext.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/BatchFlinkExecutableStageContext.java
@@ -20,16 +20,23 @@ package org.apache.beam.runners.flink.translation.functions;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import java.io.IOException;
+import org.apache.beam.model.pipeline.v1.Endpoints;
 import org.apache.beam.runners.core.construction.graph.ExecutableStage;
 import org.apache.beam.runners.fnexecution.control.DockerJobBundleFactory;
 import org.apache.beam.runners.fnexecution.control.JobBundleFactory;
+import org.apache.beam.runners.fnexecution.control.ProcessBundleDescriptors;
+import org.apache.beam.runners.fnexecution.control.ProcessBundleDescriptors.ExecutableProcessBundleDescriptor;
 import org.apache.beam.runners.fnexecution.control.StageBundleFactory;
 import org.apache.beam.runners.fnexecution.provisioning.JobInfo;
 import org.apache.beam.runners.fnexecution.state.StateRequestHandler;
+import org.apache.beam.runners.fnexecution.state.StateRequestHandlers;
+import org.apache.beam.runners.fnexecution.state.StateRequestHandlers.MultimapSideInputHandlerFactory;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO: Rename this to FlinkBatchExecutableStageContext for consistency.
 /** Implementation of a {@link FlinkExecutableStageContext} for batch jobs. */
 class BatchFlinkExecutableStageContext implements FlinkExecutableStageContext {
   private static final Logger LOG = LoggerFactory.getLogger(BatchFlinkExecutableStageContext.class);
@@ -53,7 +60,26 @@ class BatchFlinkExecutableStageContext implements FlinkExecutableStageContext {
   @Override
   public StateRequestHandler getStateRequestHandler(
       ExecutableStage executableStage, RuntimeContext runtimeContext) {
-    return FlinkBatchStateRequestHandler.forStage(executableStage, runtimeContext);
+    MultimapSideInputHandlerFactory sideInputHandlerFactory =
+        FlinkBatchSideInputHandlerFactory.forStage(executableStage, runtimeContext);
+    ExecutableProcessBundleDescriptor processBundleDescriptor;
+    try {
+      // NOTE: We require an executable bundle descriptor for the StateRequestHandlers construction
+      // below. This only uses the bundle descriptor for side input specs and effectively ignores
+      // data and state endpoints. We rely on the fact that PCollections and coders are structurally
+      // identical between instantiations here to prevent having to wire the original executable
+      // bundle descriptor here. The correct long-term fix is to move side input logic out of
+      // ExecutableProcessBundleDescriptor and into ExecutableStage.
+      processBundleDescriptor =
+          ProcessBundleDescriptors.fromExecutableStage(
+              "id", executableStage, Endpoints.ApiServiceDescriptor.getDefaultInstance());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    StateRequestHandler stateRequestHandler =
+        StateRequestHandlers.forMultimapSideInputHandlerFactory(
+            processBundleDescriptor, sideInputHandlerFactory);
+    return stateRequestHandler;
   }
 
   @Override
