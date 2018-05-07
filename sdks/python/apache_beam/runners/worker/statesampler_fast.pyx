b0b8469f626e81454a3a2a94072cf44a1b0bce85
Pablo <pabloem@google.com>
2018-05-07 13:51:10 -0700

    Improving state sampler Cython annotations.

--- sdks/python/apache_beam/runners/worker/statesampler_fast.pyx
+++ sdks/python/apache_beam/runners/worker/statesampler_fast.pyx
@@ -70,23 +70,6 @@ cdef inline int64_t get_nsec_time() nogil:
 
 cdef class StateSampler(object):
   """Tracks time spent in states during pipeline execution."""
-  cdef int _sampling_period_ms
-
-  cdef list scoped_states_by_index
-
-  cdef public bint started
-  cdef public bint finished
-  cdef object sampling_thread
-
-  # This lock guards members that are shared between threads, specificaly
-  # finished, scoped_states_by_index, and the nsecs field of each state therein.
-  cdef pythread.PyThread_type_lock lock
-
-  cdef public int64_t state_transition_count
-  cdef public int64_t time_since_transition
-
-  cdef int32_t current_state_index
-
   def __init__(self, sampling_period_ms, *args):
     self._sampling_period_ms = sampling_period_ms
     self.started = False
@@ -157,7 +140,7 @@ cdef class StateSampler(object):
     return self.scoped_states_by_index[self.current_state_index]
 
   cpdef _scoped_state(self, counter_name, output_counter,
-                      metrics_container=None):
+                      metrics_container):
     """Returns a context manager managing transitions for a given state.
     Args:
      counter_name: A CounterName object with information about the execution
@@ -185,14 +168,6 @@ cdef class StateSampler(object):
 
 cdef class ScopedState(object):
   """Context manager class managing transitions for a given sampler state."""
-
-  cdef readonly StateSampler sampler
-  cdef readonly int32_t state_index
-  cdef readonly object counter
-  cdef readonly object name
-  cdef readonly int64_t _nsecs
-  cdef int32_t old_state_index
-  cdef readonly MetricsContainer _metrics_container
 
   def __init__(
       self, sampler, name, state_index, counter=None, metrics_container=None):
