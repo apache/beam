03f7394f9def354c305b0a758a71b313fd70a41a
Alan Myrvold <amyrvold@google.com>
2018-05-08 15:14:59 -0700

    path

--- sdks/python/setup_virtualenv.sh
+++ sdks/python/setup_virtualenv.sh
@@ -3,6 +3,6 @@ set -e
 envdir="$@"
 virtualenv $envdir
 . $envdir/bin/activate
-pip -V
-pip install --upgrade tox==3.0.0 grpcio-tools==1.3.5
+$envdir/bin/pip -V
+$envdir/bin/pip install --upgrade tox==3.0.0 grpcio-tools==1.3.5
 deactivate
