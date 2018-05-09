e4649656d0878b66987955302875be87aeb31b11
marek.simunek <marek.simunek@firma.seznam.cz>
2018-05-09 10:34:20 +0200

    beamphoria integration part 1

--- sdks/java/extensions/euphoria/cd/before-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_86a2d9f3b234_key -iv $encrypted_86a2d9f3b234_iv -in cd/code-signing.asc.enc -out cd/code-signing.asc -d
-    gpg --fast-import cd/code-signing.asc
-fi
