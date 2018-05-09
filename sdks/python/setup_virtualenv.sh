5b7cd018b62b91ba144cc21f71f20b1ad8932268
Alan Myrvold <amyrvold@google.com>
2018-05-09 09:23:57 -0700

    more trace

--- sdks/python/setup_virtualenv.sh
+++ sdks/python/setup_virtualenv.sh
@@ -6,6 +6,6 @@ virtualenv $envdir
 . $envdir/bin/activate
 ls -l $envdir/bin
 cat $envdir/bin/pip
-$envdir/bin/python -m trace --trace $envdir/bin/pip -v -V
-$envdir/bin/pip install --upgrade tox==3.0.0 grpcio-tools==1.3.5
+$envdir/bin/pip -v -V || (echo "!!! trace" ; $envdir/bin/python -m trace --trace $envdir/bin/pip -v -V ; exit)
+$envdir/bin/pip install --upgrade tox==3.0.0 grpcio-tools==1.3.5 || (echo "!!! trace" ; $envdir/bin/python -m trace --trace $envdir/bin/pip install --upgrade tox==3.0.0 grpcio-tools==1.3.5 ; exit)
 deactivate
