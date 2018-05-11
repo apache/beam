4e3329f4f2e5cbbd6ca79d45d1085c57c5605b5e
Robbe Sneyders <robbe.sneyders@ml6.eu>
2018-05-11 13:08:05 +0200

    Futurize internal subpackage

--- sdks/python/apache_beam/internal/gcp/json_value.py
+++ sdks/python/apache_beam/internal/gcp/json_value.py
@@ -17,6 +17,10 @@
 
 """JSON conversion utility functions."""
 
+from __future__ import absolute_import
+
+from apache_beam.options.value_provider import ValueProvider
+
 # Protect against environments where apitools library is not available.
 # pylint: disable=wrong-import-order, wrong-import-position
 try:
@@ -25,9 +29,12 @@ except ImportError:
   extra_types = None
 # pylint: enable=wrong-import-order, wrong-import-position
 
-import six
-
-from apache_beam.options.value_provider import ValueProvider
+try:              # Python 2
+  unicode         # pylint: disable=unicode-builtin
+  long            # pylint: disable=long-builtin
+except NameError: # Python 3
+  unicode = str
+  long = int
 
 _MAXINT64 = (1 << 63) - 1
 _MININT64 = - (1 << 63)
@@ -49,7 +56,7 @@ def get_typed_value_descriptor(obj):
     ~exceptions.TypeError: if the Python object has a type that is not
       supported.
   """
-  if isinstance(obj, six.string_types):
+  if isinstance(obj, (str, unicode)):
     type_name = 'Text'
   elif isinstance(obj, bool):
     type_name = 'Boolean'
@@ -101,11 +108,11 @@ def to_json_value(obj, with_type=False):
     return extra_types.JsonValue(object_value=json_object)
   elif with_type:
     return to_json_value(get_typed_value_descriptor(obj), with_type=False)
-  elif isinstance(obj, six.string_types):
+  elif isinstance(obj, (str, unicode)):
     return extra_types.JsonValue(string_value=obj)
   elif isinstance(obj, bool):
     return extra_types.JsonValue(boolean_value=obj)
-  elif isinstance(obj, six.integer_types):
+  elif isinstance(obj, (int, long)):
     if _MININT64 <= obj <= _MAXINT64:
       return extra_types.JsonValue(integer_value=obj)
     else:
