7907e18432f9530a6a94fd0351ded069fa532e25
Ankur Goenka <ankurgoenka@gmail.com>
2018-05-10 18:13:59 -0700

    Converting ArtifactServiceFileHandler to PortableStager based on discussion in parent PR

--- sdks/python/apache_beam/runners/portability/portable_stager_test.py
+++ sdks/python/apache_beam/runners/portability/portable_stager_test.py
@@ -33,10 +33,10 @@ import grpc
 
 from apache_beam.portability.api import beam_artifact_api_pb2
 from apache_beam.portability.api import beam_artifact_api_pb2_grpc
-from apache_beam.runners.portability import artifact_service_client
+from apache_beam.runners.portability import portable_stager
 
 
-class ArtifactStagingFileHandlerTest(unittest.TestCase):
+class PortableStagerTest(unittest.TestCase):
 
   def setUp(self):
     self._temp_dir = tempfile.mkdtemp()
@@ -48,37 +48,37 @@ class ArtifactStagingFileHandlerTest(unittest.TestCase):
     if self._remote_dir:
       shutil.rmtree(self._remote_dir)
 
-  def copy_files(self, files):
+  def stage_files(self, files):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     beam_artifact_api_pb2_grpc.add_ArtifactStagingServiceServicer_to_server(
         TestLocalFileSystemArtifactStagingServiceServicer(self._remote_dir),
         server)
     test_port = server.add_insecure_port('[::]:0')
     server.start()
-    with artifact_service_client.ArtifactStagingFileHandler(
-        grpc.insecure_channel('localhost:%s' % test_port)) as file_handler:
-      for from_file, to_file in files:
-        file_handler.file_copy(
-            from_path=os.path.join(self._temp_dir, from_file), to_path=to_file)
+    stager = portable_stager.PortableStager(
+        grpc.insecure_channel('localhost:%s' % test_port))
+    for from_file, to_file in files:
+      stager.stage_artifact(
+          local_path_to_artifact=os.path.join(self._temp_dir, from_file),
+          artifact_name=to_file)
 
-    self.assertTrue(file_handler._closed, 'FileHandler was not closed.')
-    return file_handler._artifacts
+    return stager._artifacts
 
-  def test_upload_single_file(self):
+  def test_stage_single_file(self):
     from_file = 'test_local.txt'
     to_file = 'test_remote.txt'
 
     with open(os.path.join(self._temp_dir, from_file), 'wb') as f:
       f.write(b'abc')
 
-    copied_files = self.copy_files([('test_local.txt', 'test_remote.txt')])
+    copied_files = self.stage_files([('test_local.txt', 'test_remote.txt')])
     self.assertTrue(
         filecmp.cmp(
             os.path.join(self._temp_dir, from_file),
             os.path.join(self._remote_dir, to_file)))
     self.assertEqual([to_file], [manifest.name for manifest in copied_files])
 
-  def test_upload_multiple_files(self):
+  def test_stage_multiple_files(self):
 
     files = [
         ('test_local_100.txt', 'test_remote_100.txt', 101, 's'),  #
@@ -106,7 +106,7 @@ class ArtifactStagingFileHandlerTest(unittest.TestCase):
             buffering=2 << 22) as f:
           f.write(''.join(chars))
 
-    copied_files = self.copy_files(
+    copied_files = self.stage_files(
         [(from_file, to_file) for (from_file, to_file, _, _) in files])
 
     for from_file, to_file, _, _ in files:
