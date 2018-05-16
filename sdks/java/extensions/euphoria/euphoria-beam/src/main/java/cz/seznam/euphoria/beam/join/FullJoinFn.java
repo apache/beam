e97b4c17297b6a277a12e692154a58b81a9688dc
Vaclav Plajt <vaclav.plajt@firma.seznam.cz>
2018-05-16 10:02:31 +0200

    [BEAM-4294] Left, right and full joins implemeted.

---
+++ sdks/java/extensions/euphoria/euphoria-beam/src/main/java/cz/seznam/euphoria/beam/join/FullJoinFn.java
@@ -0,0 +1,60 @@
+package cz.seznam.euphoria.beam.join;
+
+import cz.seznam.euphoria.beam.SingleValueCollector;
+import cz.seznam.euphoria.core.client.functional.BinaryFunctor;
+import cz.seznam.euphoria.core.client.util.Pair;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.TupleTag;
+
+/**
+ * Full join implementation of {@link JoinFn}.
+ */
+public class FullJoinFn<LeftT, RightT, K, OutputT> extends JoinFn<LeftT, RightT, K, OutputT> {
+
+  public FullJoinFn(BinaryFunctor<LeftT, RightT, OutputT> joiner, TupleTag<LeftT> leftTag,
+      TupleTag<RightT> rightTag) {
+    super(joiner, leftTag, rightTag);
+  }
+
+  @Override
+  public void processElement(ProcessContext c) {
+
+    KV<K, CoGbkResult> element = c.element();
+    CoGbkResult value = element.getValue();
+    K key = element.getKey();
+
+    Iterable<LeftT> leftSideIter = value.getAll(leftTag);
+    Iterable<RightT> rightSIdeIter = value.getAll(rightTag);
+
+    SingleValueCollector<OutputT> outCollector = new SingleValueCollector<>();
+
+    boolean leftHasValues = leftSideIter.iterator().hasNext();
+    boolean rightHasValues = rightSIdeIter.iterator().hasNext();
+
+    if (leftHasValues && rightHasValues) {
+      for (RightT rightValue : rightSIdeIter) {
+        for (LeftT leftValue : leftSideIter) {
+          joiner.apply(leftValue, rightValue, outCollector);
+          c.output(Pair.of(key, outCollector.get()));
+        }
+      }
+    } else if (leftHasValues && !rightHasValues) {
+      for (LeftT leftValue : leftSideIter) {
+        joiner.apply(leftValue, null, outCollector);
+        c.output(Pair.of(key, outCollector.get()));
+      }
+    } else if (!leftHasValues && rightHasValues) {
+      for (RightT rightValue : rightSIdeIter) {
+        joiner.apply(null, rightValue, outCollector);
+        c.output(Pair.of(key, outCollector.get()));
+      }
+    }
+
+  }
+
+  @Override
+  public String getFnName() {
+    return "::full-join";
+  }
+}
--- sdks/java/extensions/euphoria/euphoria-beam/src/test/java/cz/seznam/euphoria/beam/JoinTest.java
+++ sdks/java/extensions/euphoria/euphoria-beam/src/test/java/cz/seznam/euphoria/beam/JoinTest.java
@@ -6,9 +6,13 @@ import cz.seznam.euphoria.core.client.flow.Flow;
 import cz.seznam.euphoria.core.client.functional.BinaryFunctor;
 import cz.seznam.euphoria.core.client.io.ListDataSink;
 import cz.seznam.euphoria.core.client.io.ListDataSource;
+import cz.seznam.euphoria.core.client.operator.FullJoin;
 import cz.seznam.euphoria.core.client.operator.Join;
+import cz.seznam.euphoria.core.client.operator.LeftJoin;
+import cz.seznam.euphoria.core.client.operator.RightJoin;
 import cz.seznam.euphoria.core.client.util.Pair;
 import cz.seznam.euphoria.testing.DatasetAssert;
+import java.util.Optional;
 import org.junit.Test;
 
 /**
@@ -59,24 +63,144 @@ public class JoinTest {
 
   }
 
-//  public void someFutureTest() {
-//
-//    ListDataSource<Pair<Integer, String>> left =
-//        ListDataSource.unbounded(
-//            asList(
-//                Pair.of(1, "L v1"), Pair.of(1, "L v2"), Pair.of(1, "L v3"),
-//                Pair.of(2, "L v1"), Pair.of(2, "L v2"),
-//                Pair.of(3, "L v1")
-//            ));
-//
-//    ListDataSource<Pair<Integer, Integer>> right =
-//        ListDataSource.unbounded(
-//            asList(
-//                Pair.of(1, 1), Pair.of(1, 10),
-//                Pair.of(2, 20),
-//                Pair.of(3, 30), Pair.of(3, 300), Pair.of(3, 3000)
-//            ));
-//  }
+  @Test
+  public void simpleLeftJoinTest() {
+    final Flow flow = Flow.create();
 
+    ListDataSource<Pair<Integer, String>> left =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, "L v1"), Pair.of(1, "L v2"),
+                Pair.of(2, "L v1"), Pair.of(2, "L v2"),
+                Pair.of(3, "L v1")
+            ));
 
+    ListDataSource<Pair<Integer, Integer>> right =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, 1), Pair.of(1, 10),
+                Pair.of(2, 20),
+                Pair.of(4, 40)
+            ));
+
+    ListDataSink<Pair<Integer, Pair<String, Integer>>> output = ListDataSink.get();
+
+    BinaryFunctor<Pair<Integer, String>, Optional<Pair<Integer, Integer>>, Pair<String, Integer>>
+        joiner = (l, r, c) ->
+        c.collect(Pair.of(l.getSecond(), r.orElse(Pair.of(null, null)).getSecond()));
+
+    LeftJoin.of(flow.createInput(left), flow.createInput(right))
+        .by(Pair::getFirst, Pair::getFirst)
+        .using(joiner)
+        .output()
+        .persist(output);
+
+    BeamExecutor executor = TestUtils.createExecutor();
+    executor.execute(flow);
+
+    DatasetAssert.unorderedEquals(output.getOutputs(),
+        Pair.of(1, Pair.of("L v1", 1)), Pair.of(1, Pair.of("L v1", 10)),
+        Pair.of(1, Pair.of("L v2", 1)), Pair.of(1, Pair.of("L v2", 10)),
+
+        Pair.of(2, Pair.of("L v1", 20)), Pair.of(2, Pair.of("L v2", 20)),
+
+        Pair.of(3, Pair.of("L v1", null))
+    );
+
+  }
+
+  @Test
+  public void simpleRightJoinTest() {
+    final Flow flow = Flow.create();
+
+    ListDataSource<Pair<Integer, String>> left =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, "L v1"), Pair.of(1, "L v2"),
+                Pair.of(2, "L v1"), Pair.of(2, "L v2"),
+                Pair.of(3, "L v1")
+            ));
+
+    ListDataSource<Pair<Integer, Integer>> right =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, 1), Pair.of(1, 10),
+                Pair.of(2, 20),
+                Pair.of(4, 40)
+            ));
+
+    ListDataSink<Pair<Integer, Pair<String, Integer>>> output = ListDataSink.get();
+
+    BinaryFunctor<Optional<Pair<Integer, String>>, Pair<Integer, Integer>, Pair<String, Integer>>
+        joiner = (l, r, c) ->
+        c.collect(Pair.of(l.orElse(Pair.of(null, null)).getSecond(), r.getSecond()));
+
+    RightJoin.of(flow.createInput(left), flow.createInput(right))
+        .by(Pair::getFirst, Pair::getFirst)
+        .using(joiner)
+        .output()
+        .persist(output);
+
+    BeamExecutor executor = TestUtils.createExecutor();
+    executor.execute(flow);
+
+    DatasetAssert.unorderedEquals(output.getOutputs(),
+        Pair.of(1, Pair.of("L v1", 1)), Pair.of(1, Pair.of("L v1", 10)),
+        Pair.of(1, Pair.of("L v2", 1)), Pair.of(1, Pair.of("L v2", 10)),
+
+        Pair.of(2, Pair.of("L v1", 20)), Pair.of(2, Pair.of("L v2", 20)),
+
+        Pair.of(4, Pair.of(null, 40))
+    );
+
+  }
+
+  @Test
+  public void simpleFullJoinTest() {
+    final Flow flow = Flow.create();
+
+    ListDataSource<Pair<Integer, String>> left =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, "L v1"), Pair.of(1, "L v2"),
+                Pair.of(2, "L v1"), Pair.of(2, "L v2"),
+                Pair.of(3, "L v1")
+            ));
+
+    ListDataSource<Pair<Integer, Integer>> right =
+        ListDataSource.bounded(
+            asList(
+                Pair.of(1, 1), Pair.of(1, 10),
+                Pair.of(2, 20),
+                Pair.of(4, 40)
+            ));
+
+    ListDataSink<Pair<Integer, Pair<String, Integer>>> output = ListDataSink.get();
+
+    BinaryFunctor<
+        Optional<Pair<Integer, String>>, Optional<Pair<Integer, Integer>>, Pair<String, Integer>>
+        joiner = (l, r, c) -> c.collect(Pair.of(
+        l.orElse(Pair.of(null, null)).getSecond(), r.orElse(Pair.of(null, null)).getSecond()));
+
+    FullJoin.of(flow.createInput(left), flow.createInput(right))
+        .by(Pair::getFirst, Pair::getFirst)
+        .using(joiner)
+        .output()
+        .persist(output);
+
+    BeamExecutor executor = TestUtils.createExecutor();
+    executor.execute(flow);
+
+    DatasetAssert.unorderedEquals(output.getOutputs(),
+        Pair.of(1, Pair.of("L v1", 1)), Pair.of(1, Pair.of("L v1", 10)),
+        Pair.of(1, Pair.of("L v2", 1)), Pair.of(1, Pair.of("L v2", 10)),
+
+        Pair.of(2, Pair.of("L v1", 20)), Pair.of(2, Pair.of("L v2", 20)),
+
+        Pair.of(3, Pair.of("L v1", null)),
+        Pair.of(4, Pair.of(null, 40))
+    );
+
+  }
+  
 }
