583e0b145f9e95793bf5841dc253539321272740
Pablo <pabloem@google.com>
2018-05-21 14:05:33 -0700

    Python Bash scripts are smart about running directory

--- sdks/python/scripts/run_postcommit.sh
+++ sdks/python/scripts/run_postcommit.sh
@@ -23,6 +23,17 @@
 # PROJECT      -> Project name to use for service jobs.
 #
 
+# Check that the script is running in a known directory.
+if [[ $PWD != *sdks/python* ]]; then
+  echo 'Unable to locate Apache Beam Python SDK root directory'
+  exit 1
+fi
+
+# Go to the Apache Beam Python SDK root
+if [[ "*sdks/python" != $PWD ]]; then
+  cd $(pwd | sed 's/sdks\/python.*/sdks\/python/')
+fi
+
 if [ -z "$1" ]; then
   printf "Usage: \n$> ./scripts/run_postcommit.sh <test_type> [gcp_location] [gcp_project]"
   printf "\n\ttest_type: ValidatesRunner or IT"
