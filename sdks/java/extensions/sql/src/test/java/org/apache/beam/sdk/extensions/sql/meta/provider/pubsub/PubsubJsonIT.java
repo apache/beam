8a07b98535783ec694017375f68d2d8fbc4935a1
akedin <kedin@google.com>
2018-05-31 09:04:12 -0700

    [SQL] Fix PubsubsJsonIT
    Make schema fields nullable, increase timeout to 5 min

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/meta/provider/pubsub/PubsubJsonIT.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/meta/provider/pubsub/PubsubJsonIT.java
@@ -52,7 +52,10 @@ import org.junit.runners.JUnit4;
 public class PubsubJsonIT implements Serializable {
 
   private static final Schema PAYLOAD_SCHEMA =
-      Schema.builder().addInt32Field("id").addStringField("name").build();
+      Schema.builder()
+          .addNullableField("id", Schema.FieldType.INT32)
+          .addNullableField("name", Schema.FieldType.STRING)
+          .build();
 
   @Rule public transient TestPubsub eventsTopic = TestPubsub.create();
   @Rule public transient TestPubsub dlqTopic = TestPubsub.create();
@@ -100,7 +103,7 @@ public class PubsubJsonIT implements Serializable {
 
     pipeline.run();
     eventsTopic.publish(messages);
-    signal.waitForSuccess(Duration.standardSeconds(120));
+    signal.waitForSuccess(Duration.standardMinutes(5));
   }
 
   @Test
@@ -153,7 +156,7 @@ public class PubsubJsonIT implements Serializable {
 
     pipeline.run();
     eventsTopic.publish(messages);
-    signal.waitForSuccess(Duration.standardSeconds(120));
+    signal.waitForSuccess(Duration.standardMinutes(5));
   }
 
   private static Boolean containsAll(Set<PubsubMessage> set, PubsubMessage... subsetCandidate) {
