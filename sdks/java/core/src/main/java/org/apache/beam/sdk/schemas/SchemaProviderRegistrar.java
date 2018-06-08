0c57695b1b92d8460a32770a86ff8422aa72f28a
Reuven Lax <relax@relax-macbookpro2.roam.corp.google.com>
2018-06-08 16:09:47 +0300

    Add DefaultSchema and fix failures turned up by plugins.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/SchemaProviderRegistrar.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/SchemaProviderRegistrar.java
@@ -22,21 +22,21 @@ import com.google.auto.service.AutoService;
 import java.util.List;
 import java.util.ServiceLoader;
 import org.apache.beam.sdk.annotations.Experimental;
+import org.apache.beam.sdk.annotations.Experimental.Kind;
 
 /**
- * {@link SchemaProvider} creators have the ability to automatically have their
- * {@link SchemaProvider schemaProvider} registered with this SDK by creating a
- * {@link ServiceLoader} entry and a concrete implementation of this interface.
+ * {@link SchemaProvider} creators have the ability to automatically have their {@link
+ * SchemaProvider schemaProvider} registered with this SDK by creating a {@link ServiceLoader} entry
+ * and a concrete implementation of this interface.
  *
- * <p>It is optional but recommended to use one of the many build time tools such as
- * {@link AutoService} to generate the necessary META-INF files automatically.
+ * <p>It is optional but recommended to use one of the many build time tools such as {@link
+ * AutoService} to generate the necessary META-INF files automatically.
  */
-@Experimental
+@Experimental(Kind.SCHEMAS)
 public interface SchemaProviderRegistrar {
   /**
-   * Returns a list of {@link SchemaProvider schema providers} which
-   * will be registered by default within each {@link SchemaRegistry schema registry} instance.
-   *
+   * Returns a list of {@link SchemaProvider schema providers} which will be registered by default
+   * within each {@link SchemaRegistry schema registry} instance.
    */
   List<SchemaProvider> getSchemaProviders();
 }
