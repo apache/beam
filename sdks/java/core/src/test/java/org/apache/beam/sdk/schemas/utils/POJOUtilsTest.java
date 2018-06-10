2a2dbe4e107c9b7a0a018005babf9592e70865bf
Reuven Lax <relax@relax-macbookpro2.roam.corp.google.com>
2018-06-10 20:23:33 +0300

    Add schema inference from POJO.

---
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/schemas/utils/POJOUtilsTest.java
@@ -0,0 +1,127 @@
+package org.apache.beam.sdk.schemas.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.Schema.FieldType;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+public class POJOUtilsTest {
+  static class SimplePojo {
+    String str;
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    boolean aBoolean;
+    DateTime dateTime;
+  }
+  static final Schema SIMPLE_SCHEMA = Schema.builder()
+      .addStringField("str")
+      .addByteField("aByte")
+      .addInt16Field("aShort")
+      .addInt32Field("anInt")
+      .addInt64Field("aLong")
+      .addBooleanField("aBoolean")
+      .addDateTimeField("dateTime")
+      .build();
+
+  @Test
+  public void testSimplePOJO() {
+    Schema schema = POJOUtils.schemaFromClass(SimplePojo.class);
+    assertEquals(SIMPLE_SCHEMA, schema);
+  }
+
+  static class NestedPojo {
+    int anInt;
+    SimplePojo nested;
+  }
+
+  @Test
+  public void testNestedPOJO() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addRowField("nested", SIMPLE_SCHEMA)
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(NestedPojo.class);
+    assertEquals(expected, schema);
+  }
+
+  static class PrimitiveArrayPojo {
+    int anInt;
+    String[] strings;
+  }
+
+  @Test
+  public void testPrimitiveArray() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addArrayField("strings", FieldType.STRING)
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(PrimitiveArrayPojo.class);
+    assertEquals(expected, schema);
+  }
+
+  static class NestedArrayPojo {
+    int anInt;
+    SimplePojo[] simples;
+  }
+
+  @Test
+  public void testNestedArray() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addArrayField("simples", FieldType.row(SIMPLE_SCHEMA))
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(NestedArrayPojo.class);
+    assertEquals(expected, schema);
+  }
+
+  static class NestedCollectionPojo {
+    int anInt;
+    List<SimplePojo> simples;
+  }
+
+  @Test
+  public void testNestedCollection() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addArrayField("simples", FieldType.row(SIMPLE_SCHEMA))
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(NestedCollectionPojo.class);
+    assertEquals(expected, schema);
+  }
+
+  static class PrimitiveMapPojo {
+    int anInt;
+    Map<String, Integer> map;
+  }
+
+  @Test
+  public void testPrimitiveMap() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addMapField("map", FieldType.STRING, FieldType.INT32)
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(PrimitiveMapPojo.class);
+    assertEquals(expected, schema);
+  }
+
+  static class NestedMapPojo {
+    int anInt;
+    Map<String, SimplePojo> map;
+  }
+
+  @Test
+  public void testNestedMap() {
+    Schema expected = Schema.builder()
+        .addInt32Field("anInt")
+        .addMapField("map", FieldType.STRING, FieldType.row(SIMPLE_SCHEMA))
+        .build();
+    Schema schema = POJOUtils.schemaFromClass(NestedMapPojo.class);
+    assertEquals(expected, schema);
+  }
+}
