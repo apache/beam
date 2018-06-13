149f5a2875f4c9788a48a92de7765037583f2509
Robert Bradshaw <robertwb@gmail.com>
2018-06-13 09:35:10 -0700

    Remove references to Flink distributed file cache.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkJobInvocation.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkJobInvocation.java
@@ -19,30 +19,20 @@ package org.apache.beam.runners.flink;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import com.google.common.io.Files;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.SettableFuture;
-import io.grpc.stub.StreamObserver;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 import javax.annotation.Nullable;
-import org.apache.beam.model.jobmanagement.v1.ArtifactApi;
 import org.apache.beam.model.jobmanagement.v1.JobApi.JobMessage;
 import org.apache.beam.model.jobmanagement.v1.JobApi.JobState.Enum;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
 import org.apache.beam.runners.core.construction.PipelineOptionsTranslation;
 import org.apache.beam.runners.core.construction.graph.GreedyPipelineFuser;
-import org.apache.beam.runners.fnexecution.artifact.ArtifactSource;
 import org.apache.beam.runners.fnexecution.jobsubmission.JobInvocation;
 import org.apache.beam.runners.fnexecution.provisioning.JobInfo;
 import org.apache.beam.sdk.PipelineResult;
@@ -58,16 +48,14 @@ public class FlinkJobInvocation implements JobInvocation {
   private static final Logger LOG = LoggerFactory.getLogger(FlinkJobInvocation.class);
 
   public static FlinkJobInvocation create(String id, ListeningExecutorService executorService,
-      RunnerApi.Pipeline pipeline, FlinkPipelineOptions pipelineOptions,
-      ArtifactSource artifactSource) {
-    return new FlinkJobInvocation(id, executorService, pipeline, pipelineOptions, artifactSource);
+      RunnerApi.Pipeline pipeline, FlinkPipelineOptions pipelineOptions) {
+    return new FlinkJobInvocation(id, executorService, pipeline, pipelineOptions);
   }
 
   private final String id;
   private final ListeningExecutorService executorService;
   private final RunnerApi.Pipeline pipeline;
   private final FlinkPipelineOptions pipelineOptions;
-  private final ArtifactSource artifactSource;
   private Enum jobState;
   private List<Consumer<Enum>> stateObservers;
 
@@ -75,13 +63,11 @@ public class FlinkJobInvocation implements JobInvocation {
   private ListenableFuture<PipelineResult> invocationFuture;
 
   private FlinkJobInvocation(String id, ListeningExecutorService executorService,
-      RunnerApi.Pipeline pipeline, FlinkPipelineOptions pipelineOptions,
-      ArtifactSource artifactSource) {
+      RunnerApi.Pipeline pipeline, FlinkPipelineOptions pipelineOptions) {
     this.id = id;
     this.executorService = executorService;
     this.pipeline = pipeline;
     this.pipelineOptions = pipelineOptions;
-    this.artifactSource = artifactSource;
     this.invocationFuture = null;
     this.jobState = Enum.STOPPED;
     this.stateObservers = new ArrayList<>();
@@ -107,13 +93,6 @@ public class FlinkJobInvocation implements JobInvocation {
       FlinkBatchPortablePipelineTranslator.BatchTranslationContext context =
           FlinkBatchPortablePipelineTranslator.createTranslationContext(jobInfo);
       translator.translate(context, fusedPipeline);
-      LOG.info("Registering pipeline artifacts in Flink program.");
-      try {
-        loadStagedArtifacts(artifactSource, context.getExecutionEnvironment()::registerCachedFile);
-      } catch (Exception e) {
-        LOG.error("Artifact registration failed", e);
-        throw new RuntimeException("Artifact registration failed", e);
-      }
       result = context.getExecutionEnvironment().execute(pipelineOptions.getJobName());
     } else {
       // streaming translation
@@ -122,13 +101,6 @@ public class FlinkJobInvocation implements JobInvocation {
       FlinkStreamingPortablePipelineTranslator.StreamingTranslationContext context =
           FlinkStreamingPortablePipelineTranslator.createTranslationContext(jobInfo);
       translator.translate(context, fusedPipeline);
-      LOG.info("Registering pipeline artifacts in Flink program.");
-      try {
-        loadStagedArtifacts(artifactSource, context.getExecutionEnvironment()::registerCachedFile);
-      } catch (Exception e) {
-        LOG.error("Artifact registration failed", e);
-        throw new RuntimeException("Artifact registration failed", e);
-      }
       result = context.getExecutionEnvironment().execute(pipelineOptions.getJobName());
     }
 
@@ -199,76 +171,6 @@ public class FlinkJobInvocation implements JobInvocation {
     for (Consumer<Enum> observer : stateObservers) {
       observer.accept(state);
     }
-  }
-
-  private static class ArtifactWriter implements StreamObserver<ArtifactApi.ArtifactChunk> {
-
-    private final FileOutputStream outputStream;
-    public final SettableFuture<Void> result;
-
-    public ArtifactWriter(FileOutputStream outputStream) {
-      this.outputStream = outputStream;
-      this.result = SettableFuture.create();
-    }
-
-    @Override
-    public void onNext(ArtifactApi.ArtifactChunk artifactChunk) {
-      try {
-        outputStream.write(artifactChunk.getData().toByteArray());
-      } catch (IOException e) {
-        onError(e);
-      }
-    }
-
-    @Override
-    public void onError(Throwable throwable) {
-      result.setException(throwable);
-    }
-
-    @Override
-    public void onCompleted() {
-      result.set(null);
-    }
-  }
-
-  public void loadStagedArtifacts(
-      ArtifactSource artifactSource, FileCache fileCache) throws IOException {
-    // get temp directory for cached files
-    File tempDir = Files.createTempDir();
-    Path tempDirPath = tempDir.toPath();
-    FlinkCachedArtifactNames cachedArtifactNames = FlinkCachedArtifactNames.createDefault();
-
-    // store and register manifest
-    ArtifactApi.Manifest manifest = artifactSource.getManifest();
-    Path manifestPath = tempDirPath.resolve("MANIFEST");
-    String manifestHandle = cachedArtifactNames.getManifestHandle();
-    try (FileOutputStream fileOutputStream = new FileOutputStream(manifestPath.toFile())) {
-      manifest.writeTo(fileOutputStream);
-    }
-    fileCache.registerCachedFile(manifestPath.toUri().toString(), manifestHandle);
-
-    // store and register artifacts
-    for (ArtifactApi.ArtifactMetadata metadata : manifest.getArtifactList()) {
-      String artifactName = metadata.getName();
-      String artifactHandle = cachedArtifactNames.getArtifactHandle(artifactName);
-      Path artifactPath = tempDirPath.resolve(artifactHandle);
-      try (FileOutputStream fileOutputStream = new FileOutputStream(artifactPath.toFile())) {
-        ArtifactWriter writer = new ArtifactWriter(fileOutputStream);
-        artifactSource.getArtifact(artifactName, writer);
-        // TODO: use sane timeout
-        writer.result.get();
-        fileCache.registerCachedFile(artifactPath.toUri().toString(), artifactHandle);
-      } catch (InterruptedException e) {
-        LOG.warn("Interrupted while writing artifact with name %s", artifactName);
-      } catch (ExecutionException e) {
-        // TODO: replace with more specific exception
-        throw new RuntimeException("Unexpected exception while writing artifact", e);
-      }
-    }
-  }
-
-  private interface FileCache {
-    void registerCachedFile(String fileUri, String name);
   }
 
   /** Indicates whether the given pipeline has any unbounded PCollections. */
