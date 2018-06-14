eb60dba63cec9c3403f3973936e296280d776779
Joachim van der Herten <joachim.vanderherten@ugent.be>
2018-06-14 10:46:50 +0200

    Adding documentation and license header

--- sdks/python/apache_beam/examples/complete/distrib_optimization.py
+++ sdks/python/apache_beam/examples/complete/distrib_optimization.py
@@ -1,3 +1,23 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Example illustrating the use of Apache Beam for distributing optimization tasks.
+"""
 import argparse
 import logging
 import string
@@ -118,6 +138,7 @@ class OptimizeProductParameters(beam.DoFn):
     """
 
     def _optimize_production_parameters(self, sim):
+        print('Alpha')
         # setup initial starting point & bounds
         x0 = 0.5 * np.ones(3)
         bounds = zip(np.zeros(3), np.ones(3))
