416d756fc9077292c83cb471af110d17030bf451
Scott Wegner <swegner@google.com>
2018-06-19 13:31:25 -0700

    fixup! Fix ErrorProne violations in fn harness and enable failOnWarnings
    Catch InterruptedExceptions in test cleanup

--- runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/environment/InProcessEnvironmentFactory.java
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/environment/InProcessEnvironmentFactory.java
@@ -106,9 +106,8 @@ public class InProcessEnvironmentFactory implements EnvironmentFactory {
                     InProcessEnvironmentFactory.class.getSimpleName(),
                     e);
                 throw e;
-              } catch (Exception e) {
-                throw new RuntimeException(e);
               }
+              return (Object) null;
             });
     executor.submit(
         () -> {
