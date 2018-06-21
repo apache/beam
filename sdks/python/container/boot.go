1d1a21e807986bc523366c57886c5018f8eeb40c
Robert Bradshaw <robertwb@gmail.com>
2018-06-21 13:33:38 -0700

    Don't require a staged SDK for the Python portable worker.

--- sdks/python/container/boot.go
+++ sdks/python/container/boot.go
@@ -135,7 +135,7 @@ func installSetupPackages(mds []*pbjob.ArtifactMetadata, workDir string) error {
 	// Install the Dataflow Python SDK and worker packages.
 	// We install the extra requirements in case of using the beam sdk. These are ignored by pip
 	// if the user is using an SDK that does not provide these.
-	if err := installSdk(files, workDir, sdkSrcFile, acceptableWhlSpecs); err != nil {
+	if err := installSdk(files, workDir, sdkSrcFile, acceptableWhlSpecs, false); err != nil {
 		return fmt.Errorf("failed to install SDK: %v", err)
 	}
 	// The staged files will not disappear due to restarts because workDir is a
