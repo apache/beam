c437d5ded6a6dcc711af13003e00d4ae62800c99
amaliujia <amaliujia@163.com>
2018-06-21 10:07:13 -0700

    Support BigQuery read from SQL.

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/meta/provider/bigquery/BigQueryReadWriteIT.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/meta/provider/bigquery/BigQueryReadWriteIT.java
@@ -27,6 +27,7 @@ import static org.apache.beam.sdk.schemas.Schema.FieldType.INT32;
 import static org.apache.beam.sdk.schemas.Schema.FieldType.INT64;
 import static org.apache.beam.sdk.schemas.Schema.FieldType.STRING;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import com.google.common.collect.ImmutableMap;
@@ -35,6 +36,8 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.PipelineResult.State;
 import org.apache.beam.sdk.extensions.sql.impl.BeamSqlEnv;
 import org.apache.beam.sdk.extensions.sql.impl.rel.BeamSqlRelUtils;
 import org.apache.beam.sdk.extensions.sql.impl.schema.BeamPCollectionTable;
@@ -43,6 +46,7 @@ import org.apache.beam.sdk.extensions.sql.meta.provider.TableProvider;
 import org.apache.beam.sdk.io.gcp.bigquery.TestBigQuery;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.FieldType;
+import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.values.PCollection;
@@ -57,7 +61,7 @@ import org.junit.runners.JUnit4;
 
 /** Integration tests form writing to BigQuery with Beam SQL. */
 @RunWith(JUnit4.class)
-public class BigQueryWriteIT implements Serializable {
+public class BigQueryReadWriteIT implements Serializable {
   private static final Schema SOURCE_SCHEMA =
       Schema.builder()
           .addNullableField("id", INT64)
@@ -82,8 +86,78 @@ public class BigQueryWriteIT implements Serializable {
           .build();
 
   @Rule public transient TestPipeline pipeline = TestPipeline.create();
+  @Rule public transient TestPipeline readPipeline = TestPipeline.create();
   @Rule public transient TestBigQuery bigQuery = TestBigQuery.create(SOURCE_SCHEMA);
   @Rule public transient TestBigQuery bigQueryTestingTypes = TestBigQuery.create(SOURCE_SCHEMA_TWO);
+
+  @Test
+  public void testSQLRead() {
+    BeamSqlEnv sqlEnv = BeamSqlEnv.inMemory(new BigQueryTableProvider());
+
+    String createTableStatement =
+        "CREATE TABLE TEST( \n"
+            + "   c_bigint BIGINT, \n"
+            + "   c_tinyint TINYINT, \n"
+            + "   c_smallint SMALLINT, \n"
+            + "   c_integer INTEGER, \n"
+            + "   c_float FLOAT, \n"
+            + "   c_double DOUBLE, \n"
+            + "   c_decimal DECIMAL, \n"
+            + "   c_boolean BOOLEAN, \n"
+            + "   c_timestamp TIMESTAMP, \n"
+            + "   c_varchar VARCHAR, \n "
+            + "   c_char CHAR, \n"
+            + "   c_arr ARRAY<VARCHAR> \n"
+            + ") \n"
+            + "TYPE 'bigquery' \n"
+            + "LOCATION '"
+            + bigQueryTestingTypes.tableSpec()
+            + "'";
+    sqlEnv.executeDdl(createTableStatement);
+
+    String insertStatement =
+        "INSERT INTO TEST VALUES ("
+            + "9223372036854775807, "
+            + "127, "
+            + "32767, "
+            + "2147483647, "
+            + "1.0, "
+            + "1.0, "
+            + "123.45, "
+            + "TRUE, "
+            + "TIMESTAMP '2018-05-28 20:17:40.123', "
+            + "'varchar', "
+            + "'char', "
+            + "ARRAY['123', '456']"
+            + ")";
+
+    sqlEnv.parseQuery(insertStatement);
+    BeamSqlRelUtils.toPCollection(pipeline, sqlEnv.parseQuery(insertStatement));
+    pipeline.run().waitUntilFinish(Duration.standardMinutes(5));
+
+    String selectTableStatement = "SELECT * FROM TEST";
+    PCollection<Row> output =
+        BeamSqlRelUtils.toPCollection(readPipeline, sqlEnv.parseQuery(selectTableStatement));
+
+    PAssert.that(output)
+        .containsInAnyOrder(
+            row(
+                SOURCE_SCHEMA_TWO,
+                9223372036854775807L,
+                (byte) 127,
+                (short) 32767,
+                2147483647,
+                (float) 1.0,
+                1.0,
+                BigDecimal.valueOf(123.45),
+                true,
+                new DateTime(2018, 05, 28, 20, 17, 40, 123, ISOChronology.getInstanceUTC()),
+                "varchar",
+                "char",
+                Arrays.asList("123", "456")));
+    PipelineResult.State state = readPipeline.run().waitUntilFinish(Duration.standardMinutes(5));
+    assertEquals(state, State.DONE);
+  }
 
   @Test
   public void testSQLTypes() {
@@ -146,33 +220,6 @@ public class BigQueryWriteIT implements Serializable {
                 "varchar",
                 "char",
                 Arrays.asList("123", "456"))));
-  }
-
-  @Test
-  public void testInsertValues() throws Exception {
-    BeamSqlEnv sqlEnv = BeamSqlEnv.inMemory(new BigQueryTableProvider());
-
-    String createTableStatement =
-        "CREATE TABLE ORDERS( \n"
-            + "   id BIGINT, \n"
-            + "   name VARCHAR, \n "
-            + "   arr ARRAY<VARCHAR> \n"
-            + ") \n"
-            + "TYPE 'bigquery' \n"
-            + "LOCATION '"
-            + bigQuery.tableSpec()
-            + "'";
-    sqlEnv.executeDdl(createTableStatement);
-
-    String insertStatement = "INSERT INTO ORDERS VALUES (1, 'foo', ARRAY['123', '456'])";
-
-    BeamSqlRelUtils.toPCollection(pipeline, sqlEnv.parseQuery(insertStatement));
-
-    pipeline.run().waitUntilFinish(Duration.standardMinutes(5));
-
-    assertThat(
-        bigQuery.getFlatJsonRows(SOURCE_SCHEMA),
-        containsInAnyOrder(row(SOURCE_SCHEMA, 1L, "foo", Arrays.asList("123", "456"))));
   }
 
   @Test
