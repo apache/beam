df3ae7cba0dc4dbf89104a55b4606a3e6cd1cb7c
Robert Bradshaw <robertwb@gmail.com>
2018-07-03 23:34:12 -0700

    Split bounded sources in larger than 1k chunks.
    A simple sub-linear formula gives a "reasonable" split size
    over a wide range of total input sizes.

--- sdks/python/apache_beam/io/iobase.py
+++ sdks/python/apache_beam/io/iobase.py
@@ -30,6 +30,7 @@ the sink.
 """
 
 import logging
+import math
 import random
 import uuid
 from collections import namedtuple
@@ -847,12 +848,21 @@ class Read(ptransform.PTransform):
 
     debug_options = self.pipeline._options.view_as(DebugOptions)
     if debug_options.experiments and 'beam_fn_api' in debug_options.experiments:
-      NUM_SPLITS = 1000
       source = self.source
+
+      def split_source(unused_impulse):
+        total_size = source.estimate_size()
+        if total_size:
+          # 1MB = 1 shard, 1GB = 32 shards, 1TB = 1000 shards, 1PB = 32k shards
+          chunk_size = max(1 << 20, 1000 * int(math.sqrt(total_size)))
+        else:
+          chunk_size = 64 << 20  # 64mb
+        return source.split(chunk_size)
+
       return (
           pbegin
           | core.Impulse()
-          | 'Split' >> core.FlatMap(lambda _: source.split(NUM_SPLITS))
+          | 'Split' >> core.FlatMap(split_source)
           | util.Reshuffle()
           | 'ReadSplits' >> core.FlatMap(lambda split: split.source.read(
               split.source.get_range_tracker(
