71ccdaa868e75409e9298dc8aef303022d7cd2c5
Łukasz Gajowy <lukasz.gajowy@polidea.com>
2018-07-10 16:03:25 +0200

    Try to divide separate directories

--- .test-infra/jenkins/job_00_seed.groovy
+++ .test-infra/jenkins/job_00_seed.groovy
@@ -106,6 +106,7 @@ job('beam_SeedJob') {
     dsl {
       // A list or a glob of other groovy files to process.
       external('.test-infra/jenkins/job_*.groovy')
+      external('.test-infra/jenkins/**/*Job.groovy')
 
       // If a job is removed from the script, disable it (rather than deleting).
       removeAction('DISABLE')
