0177d26bf166948f391f857ec7af4539e68aa057
Ahmet Altay <aaltay@gmail.com>
2018-07-13 11:53:48 -0700

    Revert "[BEAM-4752] Add support for newer dill dependency (#5931)"
    This reverts commit c14c975224af417dcdc74fed8b0d893be742e9d7.

--- sdks/python/apache_beam/internal/pickler.py
+++ sdks/python/apache_beam/internal/pickler.py
@@ -39,12 +39,6 @@ import zlib
 
 import dill
 
-# Dill 0.28.0 renamed dill.dill to dill._dill:
-# https://github.com/uqfoundation/dill/commit/f0972ecc7a41d0b8acada6042d557068cac69baa
-# TODO: Remove this once Beam depends on dill >= 0.2.8
-if not getattr(dill, 'dill', None):
-  dill.dill = dill._dill
-
 
 def _is_nested_class(cls):
   """Returns true if argument is a class object that appears to be nested."""
