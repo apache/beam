1af6d314f6b6a3334477be98116d42f65d30020e
Mikhail Gryzykhin <migryz@google.com>
2018-07-23 13:31:05 -0700

    Cleanup script comments

--- .test-infra/dockerized-jenkins/basic-security.groovy
+++ .test-infra/dockerized-jenkins/basic-security.groovy
@@ -6,9 +6,7 @@ import jenkins.install.InstallState
 
 def instance = Jenkins.getInstance()
 
-println "--> creating local user 'admin'"
 // Create user with custom pass
-
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount('admin','jenadmin')
 instance.setSecurityRealm(hudsonRealm)
@@ -17,8 +15,8 @@ def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 
+// Disable setup wizard
 if (!instance.installState.isSetupComplete()) {
-  println '--> Neutering SetupWizard'
   InstallState.INITIAL_SETUP_COMPLETED.initializeState()
 }
 
