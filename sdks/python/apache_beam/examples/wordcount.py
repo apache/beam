a9d47f24383f9bdfae60fde2ff2356693bc0bde8
Luis Enrique Ortíz Ramirez <luisortramirez2211@gmail.com>
2018-08-02 15:02:51 -0500

    Changing regular expression in wordcount to look for unicode characters. (#6092)
    * Changing regular expression in wordcount to look for unicode characters.

--- sdks/python/apache_beam/examples/wordcount.py
+++ sdks/python/apache_beam/examples/wordcount.py
@@ -59,7 +59,7 @@ class WordExtractingDoFn(beam.DoFn):
     text_line = element.strip()
     if not text_line:
       self.empty_line_counter.inc(1)
-    words = re.findall(r'[A-Za-z0-9\']+', text_line)
+    words = re.findall(r'[\w\']+', text_line, re.UNICODE)
     for w in words:
       self.words_counter.inc()
       self.word_lengths_counter.inc(len(w))
