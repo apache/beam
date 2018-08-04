fb6510d34690352a7c2547a9f3e311ea55d5100e
Yifan Zou <yifanzou@yifanzou-linuxworkstation.sea.corp.google.com>
2018-08-03 18:46:46 -0700

    [BEAM-5071] Replace the bigquery with restful APIs to query dependency release dates

--- .test-infra/jenkins/dependency_check/dependency_check_report_generator.py
+++ .test-infra/jenkins/dependency_check/dependency_check_report_generator.py
@@ -21,11 +21,14 @@ import os.path
 import re
 import traceback
 import logging
+import requests
+import time
 from datetime import datetime
 from dependency_check.bigquery_client_utils import BigQueryClientUtils
 from jira_utils.jira_manager import JiraManager
 from dependency_check.report_generator_config import ReportGeneratorConfig
-
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 _MAX_STALE_DAYS = 360
 _MAX_MINOR_VERSION_DIFF = 3
@@ -105,18 +108,26 @@ def prioritize_dependencies(deps, sdk_type):
     try:
       logging.info("\n\nStart processing: " + dep)
       dep_name, curr_ver, latest_ver = extract_single_dep(dep)
-      curr_release_date, latest_release_date = query_dependency_release_dates(bigquery_client,
-                                                                              dep_name,
-                                                                              curr_ver,
-                                                                              latest_ver)
+      curr_release_date = None
+      latest_release_date = None
       group_id = None
+
       if sdk_type == 'Java':
         # extract the groupid and artifactid
         group_id, artifact_id = dep_name.split(":")
         dep_details_url = "{0}g:\"{1}\" AND a:\"{2}\"".format(_MAVEN_CENTRAL_URL, group_id, artifact_id)
+        curr_release_date = find_release_time_from_maven_central(group_id, artifact_id, curr_ver)
+        latest_release_date = find_release_time_from_maven_central(group_id, artifact_id, latest_ver)
       else:
         dep_details_url = _PYPI_URL + dep_name
+        curr_release_date = find_release_time_from_python_compatibility_checking_service(dep_name, curr_ver)
+        latest_release_date = find_release_time_from_python_compatibility_checking_service(dep_name, curr_ver)
 
+      if not curr_release_date or not latest_release_date:
+        curr_release_date, latest_release_date = query_dependency_release_dates_from_bigquery(bigquery_client,
+                                                                                dep_name,
+                                                                                curr_ver,
+                                                                                latest_ver)
       dep_info = """<tr>
         <td><a href=\'{0}\'>{1}</a></td>
         <td>{2}</td>
@@ -176,7 +187,90 @@ def compare_dependency_versions(curr_ver, latest_ver):
   return False
 
 
-def query_dependency_release_dates(bigquery_client, dep_name, curr_ver_in_beam, latest_ver):
+def find_release_time_from_maven_central(group_id, artifact_id, version):
+  """
+  Find release dates from Maven Central REST API.
+  Args:
+    group_id:
+    artifact_id:
+    version:
+  Return:
+    release date
+  """
+  url = "http://search.maven.org/solrsearch/select?q=g:{0}+AND+a:{1}+AND+v:{2}".format(
+      group_id,
+      artifact_id,
+      version
+  )
+  logging.info('Finding release date of {0}:{1} {2} from the Maven Central').format(
+      group_id,
+      artifact_id,
+      version
+  )
+  try:
+    response = request_session_with_retries().get(url)
+    if not response.ok:
+      logging.error("""Failed finding the release date of {0}:{1} {2}.
+        The response status code is not ok: {4}""".format(group_id,
+                                                          artifact_id,
+                                                          version,
+                                                          str(response.status_code)))
+      return None
+    response_data = response.json()
+    release_timestamp_mills = response_data['response']['docs'][0]['timestamp']
+    release_date = datetime.fromtimestamp(release_timestamp_mills/1000).date()
+    return release_date
+  except Exception as e:
+    logging.error("Errors while extracting the release date: " + str(e))
+    return None
+
+
+def find_release_time_from_python_compatibility_checking_service(dep_name, version):
+  """
+  Query release dates by using Python compatibility checking service.
+  Args:
+    dep_name:
+    version:
+  Return:
+    release date
+  """
+  url = 'http://104.197.8.72/?package={0}=={1}&python-version=2'.format(
+      dep_name,
+      version
+  )
+  logging.info('Finding release time of {0} {1} from the python compatibility checking service.').format(
+      dep_name,
+      version
+  )
+  try:
+    response = request_session_with_retries().get(url)
+    if not response.ok:
+      logging.error("""Failed finding the release date of {0} {2}. 
+        The response status code is not ok: {3}""".format(dep_name,
+                                                          version,
+                                                          str(response.status_code)))
+      return None
+    response_data = response.json()
+    release_datetime = response_data['dependency_info'][dep_name]['installed_version_time']
+    release_date = datetime.strptime(release_datetime, '%Y-%m-%dT%H:%M:%S').date()
+    return release_date
+  except Exception as e:
+    logging.error("Errors while extracting the release date: " + str(e))
+    return None
+
+
+def request_session_with_retries():
+  """
+  Create a http session with retries
+  """
+  session = requests.Session()
+  retries = Retry(total=3)
+  session.mount('http://', HTTPAdapter(max_retries=retries))
+  session.mount('https://', HTTPAdapter(max_retries=retries))
+  return session
+
+
+def query_dependency_release_dates_from_bigquery(bigquery_client, dep_name, curr_ver_in_beam, latest_ver):
   """
   Query release dates of current version and the latest version from BQ tables.
   Args:
