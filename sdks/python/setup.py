5f3693e73ae4fccfa4b879eec1deb84173f0e780
Rafael Fernández <rfernand@google.com>
2018-08-07 18:13:24 -0400

    Proto
    0.15.4 -> 1.20.0

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -134,7 +134,7 @@ GCP_REQUIREMENTS = [
     'proto-google-cloud-datastore-v1>=0.90.0,<=0.90.4',
     'googledatastore==7.0.1',
     'google-cloud-pubsub==0.35.4',
-    'proto-google-cloud-pubsub-v1==0.15.4',
+    'proto-google-cloud-pubsub-v1==1.20.0',
     # GCP packages required by tests
     'google-cloud-bigquery==0.25.0',
 ]
