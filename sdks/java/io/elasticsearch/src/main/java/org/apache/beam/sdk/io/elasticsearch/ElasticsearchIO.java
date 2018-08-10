30e7f23384c615ce9b5bf61c71694b3c53c70735
torhaa <tha@capraconsulting.no>
2018-08-10 15:12:52 +0200

    [BEAM-5147] Java elasticsearch client support for reading entire documents with metadata

--- sdks/java/io/elasticsearch/src/main/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIO.java
+++ sdks/java/io/elasticsearch/src/main/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIO.java
@@ -138,6 +138,7 @@ public class ElasticsearchIO {
     // default batchSize to 100 as recommended by ES dev team as a safe value when dealing
     // with big documents and still a good compromise for performances
     return new AutoValue_ElasticsearchIO_Read.Builder()
+        .setWithMetadata(false)
         .setScrollKeepalive("5m")
         .setBatchSize(100L)
         .build();
@@ -367,6 +368,8 @@ public class ElasticsearchIO {
     @Nullable
     abstract String getQuery();
 
+    abstract boolean isWithMetadata();
+
     abstract String getScrollKeepalive();
 
     abstract long getBatchSize();
@@ -378,6 +381,8 @@ public class ElasticsearchIO {
       abstract Builder setConnectionConfiguration(ConnectionConfiguration connectionConfiguration);
 
       abstract Builder setQuery(String query);
+
+      abstract Builder setWithMetadata(boolean withMetadata);
 
       abstract Builder setScrollKeepalive(String scrollKeepalive);
 
@@ -403,6 +408,13 @@ public class ElasticsearchIO {
       checkArgument(query != null, "query can not be null");
       checkArgument(!query.isEmpty(), "query can not be empty");
       return builder().setQuery(query).build();
+    }
+
+    /**
+     * Include metadata in result json documents. Document source will be under json node _source.
+     */
+    public Read withMetadata() {
+      return builder().setWithMetadata(true).build();
     }
 
     /**
@@ -446,6 +458,7 @@ public class ElasticsearchIO {
     public void populateDisplayData(DisplayData.Builder builder) {
       super.populateDisplayData(builder);
       builder.addIfNotNull(DisplayData.item("query", getQuery()));
+      builder.addIfNotNull(DisplayData.item("withMetadata", isWithMetadata()));
       builder.addIfNotNull(DisplayData.item("batchSize", getBatchSize()));
       builder.addIfNotNull(DisplayData.item("scrollKeepalive", getScrollKeepalive()));
       getConnectionConfiguration().populateDisplayData(builder);
@@ -677,9 +690,14 @@ public class ElasticsearchIO {
       }
       // list behind iterator is empty
       List<String> batch = new ArrayList<>();
+      boolean withMetadata = source.spec.isWithMetadata();
       for (JsonNode hit : hits) {
-        String document = hit.path("_source").toString();
-        batch.add(document);
+        if (withMetadata) {
+          batch.add(hit.toString());
+        } else {
+          String document = hit.path("_source").toString();
+          batch.add(document);
+        }
       }
       batchIterator = batch.listIterator();
       current = batchIterator.next();
