c0a6be791d56d0aff8768c8db9abfbcbc25a2d58
Juan Carlos <h4rry@juans-mbp.gdl.mex.ibm.com>
2018-08-16 16:28:35 -0500

    Raise a Type Value error when result is not a list

--- sdks/python/apache_beam/runners/common.py
+++ sdks/python/apache_beam/runners/common.py
@@ -31,7 +31,7 @@ from builtins import object
 from builtins import zip
 
 from future.utils import raise_
-from past.builtins import unicode
+from past.builtins import basestring
 
 from apache_beam.internal import util
 from apache_beam.options.value_provider import RuntimeValueProvider
@@ -672,12 +672,14 @@ class _OutputProcessor(OutputProcessor):
 
     output_element_count = 0
     for result in results:
+      if not isinstance(result.value,list):
+        raise TypeError("This is not a list")
       # results here may be a generator, which cannot call len on it.
       output_element_count += 1
       tag = None
       if isinstance(result, TaggedOutput):
         tag = result.tag
-        if not isinstance(tag, (str, unicode)):
+        if not isinstance(tag, basestring):
           raise TypeError('In %s, tag %s is not a string' % (self, tag))
         result = result.value
       if isinstance(result, WindowedValue):
@@ -724,7 +726,7 @@ class _OutputProcessor(OutputProcessor):
       tag = None
       if isinstance(result, TaggedOutput):
         tag = result.tag
-        if not isinstance(tag, (str, unicode)):
+        if not isinstance(tag, basestring):
           raise TypeError('In %s, tag %s is not a string' % (self, tag))
         result = result.value
 
