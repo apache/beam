dced68cae9583df2f0eb5a6e8b069ad2bcb63d20
Charles Chen <ccy@google.com>
2018-08-29 12:14:22 -0700

    Moving to 2.8.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -232,7 +232,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.7.0'
+    project.version = '2.8.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
