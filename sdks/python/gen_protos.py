2944a2ee3b5a15bf10306eb4004ff5fc33e379e8
Robbe Sneyders <robbe.sneyders@ml6.eu>
2018-08-30 20:49:57 +0200

    Regenerate proto files when gen_protos.py is updated

--- sdks/python/gen_protos.py
+++ sdks/python/gen_protos.py
@@ -72,10 +72,11 @@ def generate_proto_files(force=False):
       raise RuntimeError(
           'No proto files found in %s.' % proto_dirs)
 
-  # Regenerate iff the proto files are newer.
+  # Regenerate iff the proto files or this file are newer.
   elif force or not out_files or len(out_files) < len(proto_files) or (
       min(os.path.getmtime(path) for path in out_files)
-      <= max(os.path.getmtime(path) for path in proto_files)):
+      <= max(os.path.getmtime(path)
+             for path in proto_files + [os.path.realpath(__file__)])):
     try:
       from grpc_tools import protoc
     except ImportError:
