b31e4be5a49d26c64740112e14a11eb4860e7a74
Ankur Goenka <ankurgoenka@gmail.com>
2018-09-05 15:11:09 -0700

    Renaming the flink portable validates runner jenkins task

--- .test-infra/jenkins/job_PostCommit_Python_Flink_PortableValidatesRunner.groovy
+++ .test-infra/jenkins/job_PostCommit_Python_Flink_PortableValidatesRunner.groovy
@@ -20,14 +20,14 @@ import CommonJobProperties as commonJobProperties
 import PostcommitJobBuilder
 
 // This job runs the suite of ValidatesRunner tests against the Flink runner.
-PostcommitJobBuilder.postCommitJob('beam_PostCommit_Python_PortableValidatesRunner_Flink_Gradle',
+PostcommitJobBuilder.postCommitJob('beam_PostCommit_Python_PVR_Flink_Gradle',
   'Run Python Flink PortableValidatesRunner', 'Apache Python Flink Runner PortableValidatesRunner Tests', this) {
   description('Runs Python PortableValidatesRunner suite on the Flink runner.')
 
   // Set common parameters.
   commonJobProperties.setTopLevelMainJobProperties(delegate)
 
-  // Execute gradle task to test Python SDK.
+  // Execute gradle task to test Python Flink Portable Runner.
   steps {
     gradle {
       rootBuildScriptDir(commonJobProperties.checkoutDir)
