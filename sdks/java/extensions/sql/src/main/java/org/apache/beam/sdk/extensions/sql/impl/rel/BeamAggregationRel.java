f6c9d8aa98faa5c9bfb1e5afec5d83fa5e1f1f13
Andrew Pilloud <apilloud@google.com>
2018-09-11 14:40:06 -0700

    Simplify BeamAggregationRule

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamAggregationRel.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamAggregationRel.java
@@ -22,9 +22,7 @@ import static org.apache.beam.sdk.schemas.Schema.toSchema;
 import static org.apache.beam.sdk.values.PCollection.IsBounded.BOUNDED;
 
 import java.util.List;
-import java.util.Optional;
 import org.apache.beam.sdk.coders.KvCoder;
-import org.apache.beam.sdk.extensions.sql.impl.rule.AggregateWindowField;
 import org.apache.beam.sdk.extensions.sql.impl.transform.BeamAggregationTransforms;
 import org.apache.beam.sdk.extensions.sql.impl.utils.CalciteUtils;
 import org.apache.beam.sdk.schemas.Schema;
@@ -37,7 +35,9 @@ import org.apache.beam.sdk.transforms.WithKeys;
 import org.apache.beam.sdk.transforms.WithTimestamps;
 import org.apache.beam.sdk.transforms.windowing.DefaultTrigger;
 import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.transforms.windowing.WindowFn;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
@@ -55,8 +55,8 @@ import org.joda.time.Duration;
 
 /** {@link BeamRelNode} to replace a {@link Aggregate} node. */
 public class BeamAggregationRel extends Aggregate implements BeamRelNode {
+  private WindowFn<Row, IntervalWindow> windowFn;
   private final int windowFieldIndex;
-  private Optional<AggregateWindowField> windowField;
 
   public BeamAggregationRel(
       RelOptCluster cluster,
@@ -66,11 +66,13 @@ public class BeamAggregationRel extends Aggregate implements BeamRelNode {
       ImmutableBitSet groupSet,
       List<ImmutableBitSet> groupSets,
       List<AggregateCall> aggCalls,
-      Optional<AggregateWindowField> windowField) {
+      WindowFn<Row, IntervalWindow> windowFn,
+      int windowFieldIndex) {
 
     super(cluster, traits, child, indicator, groupSet, groupSets, aggCalls);
-    this.windowField = windowField;
-    this.windowFieldIndex = windowField.map(AggregateWindowField::fieldIndex).orElse(-1);
+
+    this.windowFn = windowFn;
+    this.windowFieldIndex = windowFieldIndex;
   }
 
   @Override
@@ -88,7 +90,8 @@ public class BeamAggregationRel extends Aggregate implements BeamRelNode {
           BeamAggregationRel.class.getSimpleName(),
           pinput);
       PCollection<Row> upstream = pinput.get(0);
-      if (windowField.isPresent()) {
+      PCollection<Row> windowedStream = upstream;
+      if (windowFn != null) {
         upstream =
             upstream
                 .apply(
@@ -97,12 +100,8 @@ public class BeamAggregationRel extends Aggregate implements BeamRelNode {
                             new BeamAggregationTransforms.WindowTimestampFn(windowFieldIndex))
                         .withAllowedTimestampSkew(new Duration(Long.MAX_VALUE)))
                 .setCoder(upstream.getCoder());
+        windowedStream = upstream.apply(Window.into(windowFn));
       }
-
-      PCollection<Row> windowedStream =
-          windowField.isPresent()
-              ? upstream.apply(Window.into(windowField.get().windowFn()))
-              : upstream;
 
       validateWindowIsSupported(windowedStream);
 
@@ -196,6 +195,14 @@ public class BeamAggregationRel extends Aggregate implements BeamRelNode {
       List<ImmutableBitSet> groupSets,
       List<AggregateCall> aggCalls) {
     return new BeamAggregationRel(
-        getCluster(), traitSet, input, indicator, groupSet, groupSets, aggCalls, windowField);
+        getCluster(),
+        traitSet,
+        input,
+        indicator,
+        groupSet,
+        groupSets,
+        aggCalls,
+        windowFn,
+        windowFieldIndex);
   }
 }
