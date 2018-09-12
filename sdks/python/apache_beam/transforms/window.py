e42e30c9fd58e048685b3f6bcd36a5c37e7325a5
Maximilian Michels <mxm@apache.org>
2018-09-12 21:29:11 +0200

    [BEAM-5299] Define max timestamp for global window in proto
    This defines the maximum timestamp for the GlobalWindow in the Runner API
    protobuf. The SDKs pull the timestamp out of the Runner API proto. This allows
    us to change the value only at a single place and avoids having different
    values in the SDKs.

--- sdks/python/apache_beam/transforms/window.py
+++ sdks/python/apache_beam/transforms/window.py
@@ -66,7 +66,6 @@ from apache_beam.portability.api import standard_window_fns_pb2
 from apache_beam.transforms import timeutil
 from apache_beam.utils import proto_utils
 from apache_beam.utils import urns
-from apache_beam.utils.timestamp import MAX_TIMESTAMP
 from apache_beam.utils.timestamp import MIN_TIMESTAMP
 from apache_beam.utils.timestamp import Duration
 from apache_beam.utils.timestamp import Timestamp
@@ -295,11 +294,8 @@ class TimestampedValue(object):
 class GlobalWindow(BoundedWindow):
   """The default window into which all data is placed (via GlobalWindows)."""
   _instance = None
-  # The maximum timestamp for global windows is MAX_TIMESTAMP - 1 day.
-  # This is due to timers triggering when the watermark passes the trigger
-  # time, which is only possible for timestamps < MAX_TIMESTAMP.
-  # See also GlobalWindow in the Java SDK.
-  _END_OF_GLOBAL_WINDOW = MAX_TIMESTAMP - (24 * 60 * 60)
+  _END_OF_GLOBAL_WINDOW = Timestamp(micros=int(
+      common_urns.constants.GLOBAL_WINDOW_MAX_TIMESTAMP_MILLIS.constant)*1000)
 
   def __new__(cls):
     if cls._instance is None:
