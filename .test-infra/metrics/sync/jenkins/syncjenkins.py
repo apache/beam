f2d948354986e87b31db54a9778f08262b64b7b4
Mikhail Gryzykhin <migryz@google.com>
2018-09-19 17:10:15 -0700

    Address PR comments

--- .test-infra/metrics/sync/jenkins/syncjenkins.py
+++ .test-infra/metrics/sync/jenkins/syncjenkins.py
@@ -58,13 +58,10 @@ primary key(job_name, build_id)
 )
 """
 
-# returns (jobName, lastBuildId, jobUrl)
 def fetchJobs():
   url = 'https://builds.apache.org/view/A-D/view/Beam/api/json?tree=jobs[name,url,lastCompletedBuild[id]]&depth=1'
   r = requests.get(url)
   jobs = r.json()[u'jobs']
-  print("---")
-  print(jobs)
   result = map(lambda x: (x['name'], int(x['lastCompletedBuild']['id']) if x['lastCompletedBuild'] is not None else -1, x['url']), jobs)
   return result
 
@@ -76,21 +73,13 @@ def tableExists(cursor, tableName):
   cursor.execute(f"select * from information_schema.tables where table_name='{tableName}';")
   return bool(cursor.rowcount)
 
-def createTable(connection, tableName, tableSchema):
-  cursor = connection.cursor()
-  cmd = f"create table {tableName} ({tableSchema});"
-  cursor.execute(cmd)
-  cursor.close()
-  connection.commit()
-  return bool(cursor.rowcount)
-
 
 def initDbTablesIfNeeded():
   connection = initConnection()
   cursor = connection.cursor()
 
-  res = tableExists(cursor, jenkinsBuildsTableName)
-  if not res:
+  buildsTableExists = tableExists(cursor, jenkinsBuildsTableName)
+  if not buildsTableExists:
     cursor.execute(jenkinsJobsCreateTableQuery)
     if not bool(cursor.rowcount):
       raise Exception(f"Failed to create table {jenkinsBuildsTableName}")
@@ -99,6 +88,7 @@ def initDbTablesIfNeeded():
   connection.commit()
 
   connection.close()
+
 
 def fetchSyncedJobsBuildVersions(cursor):
   fetchQuery = f'''
@@ -110,12 +100,14 @@ def fetchSyncedJobsBuildVersions(cursor):
   cursor.execute(fetchQuery)
   return dict(cursor.fetchall())
 
+
 def fetchBuildsForJob(jobUrl):
   durFields = 'blockedDurationMillis,buildableDurationMillis,buildingDurationMillis,executingTimeMillis,queuingDurationMillis,totalDurationMillis,waitingDurationMillis'
   fields = f'result,timestamp,id,url,builtOn,building,duration,estimatedDuration,fullDisplayName,actions[{durFields}]'
   url = f'{jobUrl}api/json?depth=1&tree=builds[{fields}]'
   r = requests.get(url)
   return r.json()[u'builds']
+
 
 def buildRowValuesArray(jobName, build):
   timings = next((x for x in build[u'actions'] if (u'_class' in x) and (x[u'_class'] == u'jenkins.metrics.impl.TimeInQueueAction')), None)
@@ -137,6 +129,7 @@ def buildRowValuesArray(jobName, build):
           timings[u'waitingDurationMillis'] if timings is not None else -1]
   return values
 
+
 def insertRow(cursor, rowValues):
   cursor.execute(f'insert into {jenkinsBuildsTableName} values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', rowValues)
 
@@ -150,9 +143,7 @@ def fetchNewData():
 
   for newJobName, newJobLastBuildId, newJobUrl in newJobs:
     syncedJobId = syncedJobs[newJobName] if newJobName in syncedJobs else -1
-    print(syncedJobId, newJobLastBuildId)
     if newJobLastBuildId > syncedJobId:
-      print(newJobName, newJobLastBuildId, newJobUrl)
       builds = fetchBuildsForJob(newJobUrl)
       builds = [x for x in builds if int(x[u'id']) > syncedJobId]
       for build in builds:
