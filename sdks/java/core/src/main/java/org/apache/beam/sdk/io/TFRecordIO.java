ffa7371a1b4cb243cb01e307703e2d692f373ba8
Chamikara Jayalath <chamikara@chamikara-linux.svl.corp.google.com>
2018-09-19 18:03:25 -0700

    Addresses reviewer comments.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/TFRecordIO.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/TFRecordIO.java
@@ -619,32 +619,39 @@ public class TFRecordIO {
         return null;
       }
       checkState(headerBytes == HEADER_LEN, "Not a valid TFRecord. Fewer than 12 bytes.");
+
       header.rewind();
       long length = header.getLong();
+      long lengthHash = hashLong(length);
       int maskedCrc32OfLength = header.getInt();
-      checkState(hashLong(length) == maskedCrc32OfLength, "Mismatch of length mask");
-
-      ByteBuffer data = ByteBuffer.allocate((int) length);
-      long totalRead = 0;
-      while (true) {
-        long read = inChannel.read(data);
-        if (read == 0) {
-          break;
-        }
-        totalRead += read;
-      }
-      if (totalRead != length) {
+      if (lengthHash != maskedCrc32OfLength) {
         throw new IOException(
             String.format(
-                "Expected a record of length %d but only read %d bytes.", length, totalRead));
+                "Mistmatch of length mask when reading a record. Expected %d but received %d.",
+                maskedCrc32OfLength, lengthHash));
+      }
+
+      ByteBuffer data = ByteBuffer.allocate((int) length);
+      while (data.hasRemaining() && inChannel.read(data) >= 0) {}
+      if (data.hasRemaining()) {
+        throw new IOException(
+            String.format(
+                "EOF while reading record of length %d. Read only %d bytes. Input might be truncated.",
+                length, data.position()));
       }
 
       footer.clear();
       inChannel.read(footer);
       footer.rewind();
-      int maskedCrc32OfData = footer.getInt();
 
-      checkState(hashBytes(data.array()) == maskedCrc32OfData, "Mismatch of data mask");
+      int maskedCrc32OfData = footer.getInt();
+      int dataHash = hashBytes(data.array());
+      if (dataHash != maskedCrc32OfData) {
+        throw new IOException(
+            String.format(
+                "Mistmatch of data mask when reading a record. Expected %d but received %d.",
+                maskedCrc32OfData, dataHash));
+      }
       return data.array();
     }
 
