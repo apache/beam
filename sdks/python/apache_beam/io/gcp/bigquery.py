70cad53f3828f090ce623746b277eee52c9d7406
Pablo <pabloem@users.noreply.github.com>
2018-09-26 09:17:44 -0700

    Adding clarification to use WriteToBigQuery

--- sdks/python/apache_beam/io/gcp/bigquery.py
+++ sdks/python/apache_beam/io/gcp/bigquery.py
@@ -468,6 +468,8 @@ class BigQuerySink(dataflow_io.NativeSink):
 
   This BigQuery sink triggers a Dataflow native sink for BigQuery
   that only supports batch pipelines.
+  Instead of using this sink directly, please use WriteToBigQuery 
+  transform that works for both batch and streaming pipelines.
   """
 
   def __init__(self, table, dataset=None, project=None, schema=None,
