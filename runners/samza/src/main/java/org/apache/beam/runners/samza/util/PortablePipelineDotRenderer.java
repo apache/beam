2abcf73dd39017a1d4224f3462496a75cf1b11fe
xiliu <xiliu@linkedin.com>
2018-10-04 15:32:44 -0700

    Fix a bug in the Beam pipeline dot renderer

--- runners/samza/src/main/java/org/apache/beam/runners/samza/util/PortablePipelineDotRenderer.java
+++ runners/samza/src/main/java/org/apache/beam/runners/samza/util/PortablePipelineDotRenderer.java
@@ -1,4 +1,22 @@
-package org.apache.beam.runners.direct.portable;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.beam.runners.samza.util;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,17 +59,18 @@ public class PortablePipelineDotRenderer {
   private void visitTransform(PipelineNode.PTransformNode node) {
     final int nodeId = nextNodeId++;
     final RunnerApi.PTransform transform = node.getTransform();
+    writeLine(
+        "%d [label=\"%s\\n%s\"]",
+        nodeId,
+        escapeString(transform.getUniqueName()),
+        escapeString(transform.getSpec().getUrn()));
+
     transform
         .getOutputs()
         .values()
         .forEach(
             x -> {
               valueToProducerNodeId.put(x, nodeId);
-              writeLine(
-                  "%d [label=\"%s\\n%s\"]",
-                  nodeId,
-                  escapeString(transform.getUniqueName()),
-                  escapeString(transform.getSpec().getUrn()));
             });
 
     transform
