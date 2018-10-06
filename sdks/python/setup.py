75c8c3d63b9ec553c124c375e410a25100bc1ce7
splovyt <plovytsimon@gmail.com>
2018-10-06 14:58:01 +0200

    update setup.py: add version qualifier

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -140,7 +140,7 @@ GCP_REQUIREMENTS = [
     # oauth2client >=4 only works with google-apitools>=0.5.18.
     'google-apitools>=0.5.18,<=0.5.20',
     'proto-google-cloud-datastore-v1>=0.90.0,<=0.90.4',
-    'googledatastore==7.0.1',
+    'googledatastore==7.0.1; python_version < "3.5.0"',
     'google-cloud-pubsub==0.35.4',
     # GCP packages required by tests
     'google-cloud-bigquery==0.25.0',
