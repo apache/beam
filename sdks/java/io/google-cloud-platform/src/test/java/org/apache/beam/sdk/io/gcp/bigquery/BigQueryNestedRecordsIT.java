4ce4395a8ec74975a0abb7374e79b8bb72b8f5da
Pablo <pabloem@users.noreply.github.com>
2018-10-08 15:51:40 -0700

    Adding license to BQNestedRecords and temp location option

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryNestedRecordsIT.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryNestedRecordsIT.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.gcp.bigquery;
 
 import com.google.api.services.bigquery.model.TableRow;
@@ -9,6 +26,7 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.testing.TestPipelineOptions;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -49,7 +67,10 @@ public class BigQueryNestedRecordsIT {
   @Test
   public void testNestedRecords() throws Exception {
     PipelineOptionsFactory.register(Options.class);
-    Options options = TestPipeline.testingPipelineOptions().as(Options.class);
+    TestPipelineOptions testOptions =
+        TestPipeline.testingPipelineOptions().as(TestPipelineOptions.class);
+    Options options = testOptions.as(Options.class);
+    options.setTempLocation(testOptions.getTempRoot() + "/temp-it/");
     runPipeline(options);
   }
 
