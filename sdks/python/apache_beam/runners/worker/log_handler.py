f3c80de52ee5409c0fb4699ac940f4d262698d2f
Micah Wylde <mwylde@lyft.com>
2018-10-08 11:09:28 -0700

    Review updates

--- sdks/python/apache_beam/runners/worker/log_handler.py
+++ sdks/python/apache_beam/runners/worker/log_handler.py
@@ -17,6 +17,7 @@
 """Beam fn API log handler."""
 
 from __future__ import absolute_import
+from __future__ import print_function
 
 import logging
 import math
@@ -53,18 +54,18 @@ class FnApiLogRecordHandler(logging.Handler):
 
   def __init__(self, log_service_descriptor):
     super(FnApiLogRecordHandler, self).__init__()
-    # Make sure the channel is ready to avoid [BEAM-4649]
-    self._log_entry_queue = queue.Queue()
+    # If average log size is 1KB, this may use up to 1MB of memory
+    self._log_entry_queue = queue.Queue(maxsize=1000)
 
     ch = grpc.insecure_channel(log_service_descriptor.url)
+    # Make sure the channel is ready to avoid [BEAM-4649]
     grpc.channel_ready_future(ch).result(timeout=60)
     self._log_channel = grpc.intercept_channel(ch, WorkerIdInterceptor())
     self._logging_stub = beam_fn_api_pb2_grpc.BeamFnLoggingStub(
         self._log_channel)
 
-    log_control_messages = self.connect()
     self._reader = threading.Thread(
-        target=lambda: self._read_log_control_messages(log_control_messages),
+        target=lambda: self._read_log_control_messages(),
         name='read_log_control_messages')
     self._reader.daemon = True
     self._reader.start()
@@ -82,14 +83,14 @@ class FnApiLogRecordHandler(logging.Handler):
     nanoseconds = 1e9 * fraction
     log_entry.timestamp.seconds = int(seconds)
     log_entry.timestamp.nanos = int(nanoseconds)
-    self._log_entry_queue.put(log_entry)
+    self._log_entry_queue.put(log_entry, block=False)
 
   def close(self):
     """Flush out all existing log entries and unregister this handler."""
     # Acquiring the handler lock ensures ``emit`` is not run until the lock is
     # released.
     self.acquire()
-    self._log_entry_queue.put(self._FINISHED)
+    self._log_entry_queue.put(self._FINISHED, timeout=1)
     # wait on server to close.
     self._reader.join()
     self.release()
@@ -111,18 +112,17 @@ class FnApiLogRecordHandler(logging.Handler):
       if log_entries:
         yield beam_fn_api_pb2.LogEntry.List(log_entries=log_entries)
 
-  def _read_log_control_messages(self, log_control_iterator):
+  def _read_log_control_messages(self):
     # TODO(vikasrk): Handle control messages.
     while True:
+      log_control_iterator = self.connect()
+
       try:
         for _ in log_control_iterator:
           pass
         # iterator is closed
         return
       except Exception as ex:
-        # reset the stream
-        print >> sys.stderr, "Logging client failed: {}... resetting".format(ex)
-        self.acquire()
-        log_control_iterator = self.connect()
-        self.release()
+        print("Logging client failed: {}... resetting".format(ex),
+              file=sys.stderr)
 
