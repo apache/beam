3e753aebea3267b808d4ac453f572e1e76a27841
Micah Wylde <mwylde@lyft.com>
2018-10-09 22:13:14 -0700

    fix example job

--- sdks/python/apache_beam/examples/streaming_impulse.py
+++ sdks/python/apache_beam/examples/streaming_impulse.py
@@ -1,10 +1,29 @@
-"""A streaming word-counting workflow.
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""A streaming workflow that uses a synthetic streaming source.
 """
 
 from __future__ import absolute_import
 
+import argparse
 import logging
-import sys
+
+from apache_beam.runners.portability.portable_runner import PortableRunner
 
 from apache_beam.io.streaming_impulse_source import StreamingImpulseSource
 from apache_beam.transforms.trigger import AfterProcessingTime, AccumulationMode, Repeatedly
@@ -17,7 +36,6 @@ def split(s):
   a = s.split("-")
   return a[0], int(a[1])
 
-
 def count(x):
   return x[0], sum(x[1])
 
@@ -28,13 +46,16 @@ def apply_timestamp(element):
 
 def run(argv=None):
   """Build and run the pipeline."""
-  options_string = sys.argv.extend([
-    "--runner=PortableRunner",
-    "--job_endpoint=localhost:8099",
-    "--streaming"
-  ])
+  args = ["--runner=PortableRunner",
+          "--job_endpoint=localhost:8099",
+          "--streaming"]
+  if argv:
+    args.extend(argv)
 
-  pipeline_options = PipelineOptions(options_string)
+  parser = argparse.ArgumentParser()
+  known_args, pipeline_args = parser.parse_known_args(args)
+
+  pipeline_options = PipelineOptions(pipeline_args)
 
   p = beam.Pipeline(options=pipeline_options)
 
@@ -46,7 +67,7 @@ def run(argv=None):
                                  accumulation_mode=AccumulationMode.DISCARDING)
    | 'group' >> beam.GroupByKey()
    | 'count' >> beam.Map(count)
-   | 'log' >> beam.Map(lambda x: logging.info("%d" % x)))
+   | 'log' >> beam.Map(lambda x: logging.info("%d" % x[1])))
 
   result = p.run()
   result.wait_until_finish()
