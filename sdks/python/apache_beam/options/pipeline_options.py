ab46d02fd56199b7cc631e390cc2dc291b68f325
Charles Chen <ccy@google.com>
2018-10-12 10:47:06 -0700

    Revert "Merge pull request #6589: [BEAM-5442] Attempt to fix postcommit breakage."
    This reverts commit fe501db1c8919c68579b9ad0548b50fb1e4a9f2e, reversing
    changes made to db48d3e007a36345197903615c83d49673092e1a.

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -220,13 +220,12 @@ class PipelineOptions(HasDisplayData):
     if unknown_args and unknown_args[0] != '':
       logging.info("Parsing unknown args: %s", unknown_args)
       for arg in unknown_args:
-        # https://issues.apache.org/jira/browse/BEAM-5442
-        if arg.startswith('--') and not arg.startswith('--beam_plugins'):
+        if arg.startswith('--'):
           parser.add_argument(arg.split('=', 1)[0], nargs='?')
       # repeat parsing with unknown options added
       known_args, unknown_args = parser.parse_known_args(self._flags)
       if unknown_args:
-        logging.warning("Discarding unparseable args: %s", unknown_args)
+        logging.warn("Discarding unparseable args: %s", unknown_args)
 
     result = vars(known_args)
 
