67a67565f48f8d1819b782a06f525d1a08482199
Etienne Chauchot <echauchot@apache.org>
2018-10-16 14:47:07 +0200

    [BEAM-4553] Refactor MetricsGraphiteSink for code deduplication.

--- runners/extensions-java/metrics/src/test/java/org/apache/beam/runners/extensions/metrics/MetricsGraphiteSinkTest.java
+++ runners/extensions-java/metrics/src/test/java/org/apache/beam/runners/extensions/metrics/MetricsGraphiteSinkTest.java
@@ -73,14 +73,14 @@ public class MetricsGraphiteSinkTest {
             + "beam.gauge.ns1.n3.committed.value 100 [0-9]+\\n"
             + "beam.gauge.ns1.n3.attempted.value 120 [0-9]+\\n"
             + "beam.distribution.ns1.n2.committed.min 5 [0-9]+\\n"
-            + "beam.distribution.ns1.n2.committed.max 8 [0-9]+\\n"
-            + "beam.distribution.ns1.n2.committed.count 2 [0-9]+\\n"
-            + "beam.distribution.ns1.n2.committed.sum 10 [0-9]+\\n"
-            + "beam.distribution.ns1.n2.committed.mean 5.0 [0-9]+\\n"
             + "beam.distribution.ns1.n2.attempted.min 3 [0-9]+\\n"
+            + "beam.distribution.ns1.n2.committed.max 8 [0-9]+\\n"
             + "beam.distribution.ns1.n2.attempted.max 9 [0-9]+\\n"
+            + "beam.distribution.ns1.n2.committed.count 2 [0-9]+\\n"
             + "beam.distribution.ns1.n2.attempted.count 4 [0-9]+\\n"
+            + "beam.distribution.ns1.n2.committed.sum 10 [0-9]+\\n"
             + "beam.distribution.ns1.n2.attempted.sum 25 [0-9]+\\n"
+            + "beam.distribution.ns1.n2.committed.mean 5.0 [0-9]+\\n"
             + "beam.distribution.ns1.n2.attempted.mean 6.25 [0-9]+";
     assertTrue(join.matches(regexpr));
   }
