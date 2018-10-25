4da3980be12f92f87504a52ffc6a62629654e5f4
Mikhail Gryzykhin <12602502+Ardagan@users.noreply.github.com>
2018-10-25 15:27:31 -0700

    fixup! Incorporate feedback suggestion
    Co-Authored-By: swegner <scott@apache.org>

--- .test-infra/metrics/src/test/groovy/ProberTests.groovy
+++ .test-infra/metrics/src/test/groovy/ProberTests.groovy
@@ -38,7 +38,7 @@ class ProberTests {
     def dashboardNames = allDashboards.title
     // Validate at least one expected dashboard exists
     assert dashboardNames.contains('Post-commit Tests') : 'Expected dashboard does not exist'
-    assert dashboardNames.size >= 4 : "Number of dashboards is less than expected ${dashboardNames}"
+    assert dashboardNames.size > 0 : "No dashboards found. Check Grafana dashboard initialization script."
   }
 
   @Test
