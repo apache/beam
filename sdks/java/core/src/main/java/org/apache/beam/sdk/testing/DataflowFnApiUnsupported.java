5c5b5f737d94876f9d34d6e59289d405bc48c4ec
Boyuan Zhang <boyuanz@google.com>
2018-10-26 16:31:45 -0700

    Apply Spotless

--- sdks/java/core/src/main/java/org/apache/beam/sdk/testing/DataflowFnApiUnsupported.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/testing/DataflowFnApiUnsupported.java
@@ -18,6 +18,4 @@
 package org.apache.beam.sdk.testing;
 
 /** Category tag for the dataflow fn-api worker unsupported tests. */
-public interface DataflowFnApiUnsupported {
-
-}
+public interface DataflowFnApiUnsupported {}
