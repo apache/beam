80e9d91777232159d5e0ea0d2d8ff3da714962e5
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2018-10-30 12:34:48 +0100

    [BEAM-5906] Use dedicated API in Nexmark to publish results to BigQuery

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -309,6 +309,7 @@ class BeamModulePlugin implements Plugin<Project> {
     def generated_grpc_beta_version = "0.19.0"
     def generated_grpc_ga_version = "1.18.0"
     def google_cloud_bigdataoss_version = "1.9.0"
+    def google_cloud_bigquery_version = "1.48.0"
     def bigtable_version = "1.4.0"
     def google_clients_version = "1.24.1"
     def google_auth_version = "0.10.0"
@@ -374,6 +375,7 @@ class BeamModulePlugin implements Plugin<Project> {
         google_api_services_storage                 : "com.google.apis:google-api-services-storage:v1-rev136-$google_clients_version",
         google_auth_library_credentials             : "com.google.auth:google-auth-library-credentials:$google_auth_version",
         google_auth_library_oauth2_http             : "com.google.auth:google-auth-library-oauth2-http:$google_auth_version",
+        google_cloud_bigquery                       : "com.google.cloud:google-cloud-bigquery:$google_cloud_bigquery_version",
         google_cloud_core                           : "com.google.cloud:google-cloud-core:$google_cloud_core_version",
         google_cloud_core_grpc                      : "com.google.cloud:google-cloud-core-grpc:$google_cloud_core_version",
         google_cloud_dataflow_java_proto_library_all: "com.google.cloud.dataflow:google-cloud-dataflow-java-proto-library-all:0.5.160304",
