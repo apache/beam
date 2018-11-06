4128fdc85213ee8443d29e0aae284464e625b080
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2018-11-06 15:12:32 +0100

    [BEAM-5981] Move Source & sink related methods to LoadTest class

--- sdks/java/testing/load-tests/src/main/java/org/apache/beam/sdk/loadtests/LoadTest.java
+++ sdks/java/testing/load-tests/src/main/java/org/apache/beam/sdk/loadtests/LoadTest.java
@@ -17,14 +17,18 @@
  */
 package org.apache.beam.sdk.loadtests;
 
-import static org.apache.beam.sdk.loadtests.SyntheticUtils.fromJsonString;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Optional;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.synthetic.SyntheticBoundedIO;
+import org.apache.beam.sdk.io.synthetic.SyntheticOptions;
 import org.apache.beam.sdk.io.synthetic.SyntheticStep;
 import org.apache.beam.sdk.loadtests.metrics.MetricsPublisher;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
 
 /**
  * Base class for all load tests. Provides common operations such as initializing source/step
@@ -68,5 +72,31 @@ abstract class LoadTest<OptionsT extends LoadTestOptions> {
     MetricsPublisher.toConsole(result, metricsNamespace);
 
     return result;
+  }
+
+  <T extends SyntheticOptions> T fromJsonString(String json, Class<T> type) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    T result = mapper.readValue(json, type);
+    result.validate();
+    return result;
+  }
+
+  Optional<SyntheticStep> createStep(String stepOptions) throws IOException {
+    if (stepOptions != null && !stepOptions.isEmpty()) {
+      return Optional.of(
+          new SyntheticStep(fromJsonString(stepOptions, SyntheticStep.Options.class)));
+    } else {
+      return Optional.empty();
+    }
+  }
+
+  PCollection<KV<byte[], byte[]>> applyStepIfPresent(
+      PCollection<KV<byte[], byte[]>> input, String name, Optional<SyntheticStep> syntheticStep) {
+
+    if (syntheticStep.isPresent()) {
+      return input.apply(name, ParDo.of(syntheticStep.get()));
+    } else {
+      return input;
+    }
   }
 }
