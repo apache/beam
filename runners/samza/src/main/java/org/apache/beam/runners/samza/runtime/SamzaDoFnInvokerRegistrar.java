0303193c42fb41ae567b19014a29b3b72e25ebd2
xiliu <xiliu@linkedin.com>
2018-11-09 10:56:11 -0800

    Allow Samza DoFnInvoker to generate configs and pass in TaskContext

--- runners/samza/src/main/java/org/apache/beam/runners/samza/runtime/SamzaDoFnInvokerRegistrar.java
+++ runners/samza/src/main/java/org/apache/beam/runners/samza/runtime/SamzaDoFnInvokerRegistrar.java
@@ -17,10 +17,18 @@
  */
 package org.apache.beam.runners.samza.runtime;
 
+import java.util.Map;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.reflect.DoFnInvoker;
+import org.apache.samza.task.TaskContext;
 
 /** A registrar for Samza DoFnInvoker. */
 public interface SamzaDoFnInvokerRegistrar {
-  <InputT, OutputT> DoFnInvoker<InputT, OutputT> invokerFor(DoFn<InputT, OutputT> fn);
+
+  /** Returns the invoker for a {@link DoFn}. */
+  <InputT, OutputT> DoFnInvoker<InputT, OutputT> invokerFor(
+      DoFn<InputT, OutputT> fn, TaskContext context);
+
+  /** Returns the configs for a {@link DoFn}. */
+  <InputT, OutputT> Map<String, String> configFor(DoFn<InputT, OutputT> fn);
 }
