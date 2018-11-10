d1c8010c3d33cd071c57d257eb7f555dd27159c0
Robert Burke <robert@frantil.com>
2018-11-10 00:48:50 +0000

    !fixup starcgen generated files always include the apache license

--- sdks/go/cmd/starcgen/starcgen.go
+++ sdks/go/cmd/starcgen/starcgen.go
@@ -57,6 +57,13 @@ func Generate(w io.Writer, filename, pkg string, ids []string, fset *token.FileS
 
 	e.Print("*/\n")
 	data := e.Generate(filename)
+	if err := write(w, []byte(license)); err != nil {
+		return err
+	}
+	return write(w, data)
+}
+
+func write(w io.Writer, data []byte) error {
 	n, err := w.Write(data)
 	if err != nil && n < len(data) {
 		return fmt.Errorf("short write of data got %d, want %d", n, len(data))
@@ -127,3 +134,20 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+const license = `// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`
