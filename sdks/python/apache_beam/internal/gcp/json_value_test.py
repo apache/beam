04bfb802b79a8c23484ece46f28177fe383571f9
Mark Liu <markliu@google.com>
2018-11-12 12:15:06 -0800

    [BEAM-5953] Fix DataflowRunner in Python 3 - type errors

--- sdks/python/apache_beam/internal/gcp/json_value_test.py
+++ sdks/python/apache_beam/internal/gcp/json_value_test.py
@@ -41,6 +41,9 @@ class JsonValueTest(unittest.TestCase):
   def test_string_to(self):
     self.assertEquals(JsonValue(string_value='abc'), to_json_value('abc'))
 
+  def test_bytes_to(self):
+    self.assertEquals(JsonValue(string_value='abc'), to_json_value(b'abc'))
+
   def test_true_to(self):
     self.assertEquals(JsonValue(boolean_value=True), to_json_value(True))
 
