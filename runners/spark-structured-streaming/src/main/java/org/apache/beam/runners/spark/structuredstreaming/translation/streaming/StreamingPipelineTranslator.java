52d0c3fd2e23ae0e417f795821c01e9285b55e01
Etienne Chauchot <echauchot@apache.org>
2018-11-21 12:13:21 +0100

    [BEAM-8470] Initialise BatchTranslationContext

--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/streaming/StreamingPipelineTranslator.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/streaming/StreamingPipelineTranslator.java
@@ -1,7 +1,10 @@
 package org.apache.beam.runners.spark.structuredstreaming.translation.streaming;
 
+import org.apache.beam.runners.spark.structuredstreaming.SparkPipelineOptions;
 import org.apache.beam.runners.spark.structuredstreaming.translation.PipelineTranslator;
 
 public class StreamingPipelineTranslator extends PipelineTranslator {
-//TODO impl
+
+  public StreamingPipelineTranslator(SparkPipelineOptions options) {
+  }
 }
