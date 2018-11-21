9bb309675b009f1d059e90e264d2d3860970767f
Chamikara Jayalath <chamikara@google.com>
2018-11-21 11:26:50 -0800

    Moving to 2.10.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -265,7 +265,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.9.0'
+    project.version = '2.10.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
