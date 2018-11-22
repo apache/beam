e4a647eb3d43fd78956bb9d94cd6dc9d7e142ee2
Etienne Chauchot <echauchot@apache.org>
2018-11-22 12:01:28 +0100

    Move common translation context components to superclass

--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
@@ -1,17 +1,44 @@
 package org.apache.beam.runners.spark.structuredstreaming.translation;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.beam.runners.spark.structuredstreaming.SparkPipelineOptions;
 import org.apache.beam.sdk.runners.AppliedPTransform;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PValue;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
 
 /**
- * Base class that gives a context for {@link PTransform} translation.
+ * Base class that gives a context for {@link PTransform} translation: keeping track of the datasets,
+ * the {@link SparkSession}, the current transform being translated.
  */
 public class TranslationContext {
 
   private AppliedPTransform<?, ?, ?> currentTransform;
+  private final Map<PValue, Dataset<?>> datasets;
+  private SparkSession sparkSession;
+  private final SparkPipelineOptions options;
 
   public void setCurrentTransform(AppliedPTransform<?, ?, ?> currentTransform) {
     this.currentTransform = currentTransform;
+  }
+
+  public TranslationContext(SparkPipelineOptions options) {
+    SparkConf sparkConf = new SparkConf();
+    sparkConf.setMaster(options.getSparkMaster());
+    sparkConf.setAppName(options.getAppName());
+    if (options.getFilesToStage() != null && !options.getFilesToStage().isEmpty()) {
+      sparkConf.setJars(options.getFilesToStage().toArray(new String[0]));
+    }
+
+    this.sparkSession = SparkSession
+        .builder()
+        .config(sparkConf)
+        .getOrCreate();
+    this.options = options;
+    this.datasets = new HashMap<>();
   }
 
 }
