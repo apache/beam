b65a9da7140c452dbe27ad3a49e96b9489393f78
Etienne Chauchot <echauchot@apache.org>
2018-11-22 11:50:17 +0100

    [BEAM-8470] Improve javadocs

--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/TranslationContext.java
@@ -1,7 +1,11 @@
 package org.apache.beam.runners.spark.structuredstreaming.translation;
 
 import org.apache.beam.sdk.runners.AppliedPTransform;
+import org.apache.beam.sdk.transforms.PTransform;
 
+/**
+ * Base class that gives a context for {@link PTransform} translation.
+ */
 public class TranslationContext {
 
   private AppliedPTransform<?, ?, ?> currentTransform;
--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/streaming/StreamingPipelineTranslator.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/streaming/StreamingPipelineTranslator.java
@@ -3,7 +3,12 @@ package org.apache.beam.runners.spark.structuredstreaming.translation.streaming;
 import org.apache.beam.runners.spark.structuredstreaming.SparkPipelineOptions;
 import org.apache.beam.runners.spark.structuredstreaming.translation.PipelineTranslator;
 import org.apache.beam.runners.spark.structuredstreaming.translation.TransformTranslator;
+import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.runners.TransformHierarchy;
+
+/** {@link PipelineTranslator} for executing a {@link Pipeline} in Spark in streaming mode.
+ * This contains only the components specific to streaming: {@link StreamingTranslationContext},
+ * registry of batch {@link TransformTranslator} and registry lookup code. */
 
 public class StreamingPipelineTranslator extends PipelineTranslator {
 
