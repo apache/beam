03b66e79054e6a3c7354a08174fbf0e52d8cf2ca
Reuven Lax <relax@google.com>
2018-12-16 22:15:52 -0800

    Implement schemas for AutoValue classes.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/utils/ReflectUtils.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/utils/ReflectUtils.java
@@ -69,7 +69,10 @@ public class ReflectUtils {
   private static final Map<Class, Method> ANNOTATED_CONSTRUCTORS = Maps.newHashMap();
   private static final Map<Class, List<Field>> DECLARED_FIELDS = Maps.newHashMap();
 
-  /** Returns the list of public, non-static methods in the class, caching the results. */
+  /**
+   * Returns the list of non private/protected, non-static methods in the class, caching the
+   * results.
+   */
   public static List<Method> getMethods(Class clazz) {
     return DECLARED_METHODS.computeIfAbsent(
         clazz,
@@ -156,16 +159,28 @@ public class ReflectUtils {
   }
 
   public static boolean isSetter(Method method) {
-    return Void.TYPE.equals(method.getReturnType())
-        && method.getParameterCount() == 1
-        && method.getName().startsWith("set");
+    return method.getParameterCount() == 1 && method.getName().startsWith("set");
   }
 
   public static String stripPrefix(String methodName, String prefix) {
+    if (!methodName.startsWith(prefix)) {
+      return methodName;
+    }
     String firstLetter = methodName.substring(prefix.length(), prefix.length() + 1).toLowerCase();
 
     return (methodName.length() == prefix.length() + 1)
         ? firstLetter
         : (firstLetter + methodName.substring(prefix.length() + 1, methodName.length()));
+  }
+
+  public static String stripGetterPrefix(String method) {
+    if (method.startsWith("get")) {
+      return stripPrefix(method, "get");
+    }
+    return stripPrefix(method, "is");
+  }
+
+  public static String stripSetterPrefix(String method) {
+    return stripPrefix(method, "set");
   }
 }
