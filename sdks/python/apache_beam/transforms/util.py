d8c22923c332b640a55c822ace27e704236e7df5
Ankur Goenka <ankurgoenka@gmail.com>
2018-12-18 16:03:54 -0800

    [BEAM-6179] Fixing itterable comparison

--- sdks/python/apache_beam/transforms/util.py
+++ sdks/python/apache_beam/transforms/util.py
@@ -281,7 +281,9 @@ class _BatchSizeEstimator(object):
     n = float(len(xs))
     xbar = sum(xs) / n
     ybar = sum(ys) / n
-    if [xs[0]] * len(xs) == xs:
+    if xbar == 0:
+      return ybar, 0
+    if all(xs[0] == x for x in xs):
       # Simply use the mean if all values in xs are same.
       return 0, ybar/xbar
     b = (sum([(x - xbar) * (y - ybar) for x, y in zip(xs, ys)])
@@ -295,7 +297,7 @@ class _BatchSizeEstimator(object):
     import numpy as np
     from numpy import sum
     n = len(xs)
-    if [xs[0]] * n == xs:
+    if all(xs[0] == x for x in xs):
       # If all values of xs are same then fallback to linear_regression_no_numpy
       return _BatchSizeEstimator.linear_regression_no_numpy(xs, ys)
     xs = np.asarray(xs, dtype=float)
