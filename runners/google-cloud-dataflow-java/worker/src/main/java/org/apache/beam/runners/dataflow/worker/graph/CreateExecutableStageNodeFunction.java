a1d4e72a0aa10fd11cafac10ba6d5b5abe9ad206
Sam Rohde <rohde.samuel@gmail.com>
2018-12-19 16:48:31 -0800

    spotless

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/graph/CreateExecutableStageNodeFunction.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/graph/CreateExecutableStageNodeFunction.java
@@ -256,7 +256,6 @@ public class CreateExecutableStageNodeFunction
             e);
       }
 
-
       // TODO(BEAM-6275): Set correct IsBounded on generated PCollections
       String pcollectionId = node.getPcollectionId();
       RunnerApi.PCollection pCollection =
