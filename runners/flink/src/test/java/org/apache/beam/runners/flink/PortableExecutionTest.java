4adb728535719fa93a775f03dc9023556f008e0f
Maximilian Michels <mxm@apache.org>
2018-12-24 09:09:40 +0100

    [BEAM-5396] Assign portable operator uids
    This adds operator uids to portable operators with state. A test verifies
    restoring from a Savepoint for both legacy and portable pipelines.

--- runners/flink/src/test/java/org/apache/beam/runners/flink/PortableExecutionTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/PortableExecutionTest.java
@@ -86,6 +86,7 @@ public class PortableExecutionTest implements Serializable {
     options.as(FlinkPipelineOptions.class).setFlinkMaster("[local]");
     options.as(FlinkPipelineOptions.class).setStreaming(isStreaming);
     options.as(FlinkPipelineOptions.class).setParallelism(2);
+    options.as(FlinkPipelineOptions.class).setShutdownSourcesOnFinalWatermark(true);
     options
         .as(PortablePipelineOptions.class)
         .setDefaultEnvironmentType(Environments.ENVIRONMENT_EMBEDDED);
