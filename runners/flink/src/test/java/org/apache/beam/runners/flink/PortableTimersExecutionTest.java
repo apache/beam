c289c4fd5056bba91623c5d7147797a34571b085
Maximilian Michels <mxm@apache.org>
2019-01-10 15:41:38 -0500

    [BEAM-6248] Add Flink v1.7 build target to Flink Runner  (#7300)

--- runners/flink/src/test/java/org/apache/beam/runners/flink/PortableTimersExecutionTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/PortableTimersExecutionTest.java
@@ -92,6 +92,7 @@ public class PortableTimersExecutionTest implements Serializable {
     options.setRunner(CrashingRunner.class);
     options.as(FlinkPipelineOptions.class).setFlinkMaster("[local]");
     options.as(FlinkPipelineOptions.class).setStreaming(isStreaming);
+    options.as(FlinkPipelineOptions.class).setParallelism(2);
     options
         .as(PortablePipelineOptions.class)
         .setDefaultEnvironmentType(Environments.ENVIRONMENT_EMBEDDED);
