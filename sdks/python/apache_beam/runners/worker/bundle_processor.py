f0b503b281e646b44535872b8810e87a6a56efcb
Robert Bradshaw <robertwb@google.com>
2019-01-11 17:15:22 +0100

    Reduce level of logging for unnamed IO port operations.

--- sdks/python/apache_beam/runners/worker/bundle_processor.py
+++ sdks/python/apache_beam/runners/worker/bundle_processor.py
@@ -583,7 +583,13 @@ class BeamTransformFactory(object):
   def create_operation(self, transform_id, consumers):
     transform_proto = self.descriptor.transforms[transform_id]
     if not transform_proto.unique_name:
-      logging.warn("No unique name set for transform %s" % transform_id)
+      if transform_proto.spec.urn in (DATA_INPUT_URN, DATA_OUTPUT_URN):
+        # These are not user operations, and runners aren't good at
+        # setting their names.
+        log = logging.debug
+      else:
+        log = logging.warn
+      log("No unique name set for transform %s" % transform_id)
       transform_proto.unique_name = transform_id
     creator, parameter_type = self._known_urns[transform_proto.spec.urn]
     payload = proto_utils.parse_Bytes(
