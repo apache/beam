5536c2c4efb272bcc5a7833f875216fe812e80f8
Tianyang Hu <htyleo@gmail.com>
2019-01-15 12:51:35 -0800

    Fix a bug: certain error in LiftedCombine.FinishBundle is not propagated out

--- sdks/go/pkg/beam/core/runtime/exec/combine.go
+++ sdks/go/pkg/beam/core/runtime/exec/combine.go
@@ -319,7 +319,9 @@ func (n *LiftedCombine) FinishBundle(ctx context.Context) error {
 	// Need to run n.Out.ProcessElement for all the cached precombined KVs, and
 	// then finally Finish bundle as normal.
 	for _, a := range n.cache {
-		n.Out.ProcessElement(ctx, a)
+		if err := n.Out.ProcessElement(ctx, a); err != nil {
+			return n.fail(err)
+		}
 	}
 
 	if err := n.Out.FinishBundle(ctx); err != nil {
