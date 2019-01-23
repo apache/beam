43459fb0377aaf4ed4d1a010bffe6f249a9e7a79
Daniel Oliveira <younghoono@gmail.com>
2019-01-22 17:10:41 -0800

    [BEAM-6491] Fixup

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/FileIOTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/FileIOTest.java
@@ -55,6 +55,7 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionView;
 import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Charsets;
 import org.joda.time.Duration;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
