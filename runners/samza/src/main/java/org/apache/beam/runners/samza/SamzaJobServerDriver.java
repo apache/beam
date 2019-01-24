3c909401e9a8d1bb3dc7c1d129a24749099dbd8f
Hai Lu <halu@halu-ld2.linkedin.biz>
2019-01-24 13:18:30 -0800

    add portable pipeline option and use that for job server driver RB=1540310

--- runners/samza/src/main/java/org/apache/beam/runners/samza/SamzaJobServerDriver.java
+++ runners/samza/src/main/java/org/apache/beam/runners/samza/SamzaJobServerDriver.java
@@ -18,22 +18,18 @@
 package org.apache.beam.runners.samza;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
-import org.apache.beam.runners.core.construction.PipelineOptionsTranslation;
 import org.apache.beam.runners.fnexecution.GrpcFnServer;
 import org.apache.beam.runners.fnexecution.ServerFactory;
 import org.apache.beam.runners.fnexecution.artifact.BeamFileSystemArtifactStagingService;
 import org.apache.beam.runners.fnexecution.jobsubmission.InMemoryJobService;
 import org.apache.beam.runners.fnexecution.jobsubmission.JobInvocation;
 import org.apache.beam.runners.fnexecution.jobsubmission.JobInvoker;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.vendor.grpc.v1p13p1.com.google.protobuf.Struct;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,38 +37,34 @@ import org.slf4j.LoggerFactory;
 public class SamzaJobServerDriver {
   private static final Logger LOG = LoggerFactory.getLogger(SamzaJobServerDriver.class);
 
-  private final ServerConfiguration config;
+  private final SamzaPortablePipelineOptions pipelineOptions;
 
-  /** Configuration for the jobServer. */
-  private static class ServerConfiguration {
-    @Option(name = "--job-port", usage = "The job service port. (Default: 11440)")
-    private int jobPort = 11440;
-
-    @Option(name = "--control-port", usage = "The FnControl port. (Default: 11441)")
-    private int controlPort = 11441;
-  }
-
-  private SamzaJobServerDriver(ServerConfiguration config) {
-    this.config = config;
+  private SamzaJobServerDriver(SamzaPortablePipelineOptions pipelineOptions) {
+    this.pipelineOptions = pipelineOptions;
   }
 
   public static void main(String[] args) throws Exception {
-    ServerConfiguration configuration = new ServerConfiguration();
-    CmdLineParser parser = new CmdLineParser(configuration);
+    SamzaPortablePipelineOptions pipelineOptions =
+        PipelineOptionsFactory.fromArgs(args).as(SamzaPortablePipelineOptions.class);
+    Map<String, String> overrideConfig =
+        pipelineOptions.getConfigOverride() != null
+            ? pipelineOptions.getConfigOverride()
+            : new HashMap<>();
+    overrideConfig.put(SamzaRunnerOverrideConfigs.IS_PORTABLE_MODE, String.valueOf(true));
+    overrideConfig.put(
+        SamzaRunnerOverrideConfigs.FN_CONTROL_PORT,
+        String.valueOf(pipelineOptions.getControlPort()));
+    pipelineOptions.setConfigOverride(overrideConfig);
     try {
-      parser.parseArgument(args);
-      fromConfig(configuration).run();
-    } catch (CmdLineException e) {
-      LOG.error("Unable to parse command line arguments {}", Arrays.asList(args), e);
-      throw new IllegalArgumentException("Unable to parse command line arguments.", e);
+      fromOptions(pipelineOptions).run();
     } catch (Exception e) {
       LOG.error("Hit exception with SamzaJobServer. Exiting...", e);
       throw e;
     }
   }
 
-  public static SamzaJobServerDriver fromConfig(ServerConfiguration config) {
-    return new SamzaJobServerDriver(config);
+  public static SamzaJobServerDriver fromOptions(SamzaPortablePipelineOptions pipelineOptions) {
+    return new SamzaJobServerDriver(pipelineOptions);
   }
 
   private InMemoryJobService createJobService() throws IOException {
@@ -82,17 +74,7 @@ public class SamzaJobServerDriver {
           public JobInvocation invoke(
               RunnerApi.Pipeline pipeline, Struct options, @Nullable String retrievalToken)
               throws IOException {
-            SamzaPipelineOptions samzaPipelineOptions =
-                PipelineOptionsTranslation.fromProto(options).as(SamzaPipelineOptions.class);
-            Map<String, String> overrideConfig =
-                samzaPipelineOptions.getConfigOverride() != null
-                    ? samzaPipelineOptions.getConfigOverride()
-                    : new HashMap<>();
-            overrideConfig.put(SamzaRunnerOverrideConfigs.IS_PORTABLE_MODE, String.valueOf(true));
-            overrideConfig.put(
-                SamzaRunnerOverrideConfigs.FN_CONTROL_PORT, String.valueOf(config.controlPort));
-            samzaPipelineOptions.setConfigOverride(overrideConfig);
-            return new SamzaJobInvocation(pipeline, samzaPipelineOptions);
+            return new SamzaJobInvocation(pipeline, pipelineOptions);
           }
         };
     return InMemoryJobService.create(
@@ -113,7 +95,7 @@ public class SamzaJobServerDriver {
     InMemoryJobService service = createJobService();
     GrpcFnServer<InMemoryJobService> jobServiceGrpcFnServer =
         GrpcFnServer.allocatePortAndCreateFor(
-            service, ServerFactory.createWithPortSupplier(() -> config.jobPort));
+            service, ServerFactory.createWithPortSupplier(pipelineOptions::getJobPort));
     LOG.info("JobServer started on {}", jobServiceGrpcFnServer.getApiServiceDescriptor().getUrl());
     try {
       jobServiceGrpcFnServer.getServer().awaitTermination();
