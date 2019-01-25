ca9ce6b910352f7d2422e1695c3b121413ae1927
Ryan Williams <ryan.blake.williams@gmail.com>
2019-01-25 20:19:36 +0000

    move urn->MetricName parsing to own file

--- runners/flink/src/main/java/org/apache/beam/runners/flink/metrics/FlinkMetricContainer.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/metrics/FlinkMetricContainer.java
@@ -17,8 +17,8 @@
  */
 package org.apache.beam.runners.flink.metrics;
 
+import static org.apache.beam.runners.core.metrics.MetricUrns.parseUrn;
 import static org.apache.beam.runners.core.metrics.MetricsContainerStepMap.asAttemptedOnlyMetricResults;
-import static org.apache.beam.runners.core.metrics.SimpleMonitoringInfoBuilder.USER_COUNTER_URN_PREFIX;
 
 import java.util.HashMap;
 import java.util.List;
@@ -87,26 +87,6 @@ public class FlinkMetricContainer {
     return metricsAccumulator != null
         ? metricsAccumulator.getLocalValue().getContainer(stepName)
         : null;
-  }
-
-  /**
-   * Parse a {@link MetricName} from a {@link
-   * org.apache.beam.model.fnexecution.v1.BeamFnApi.MonitoringInfoUrns.Enum}
-   *
-   * <p>Should be consistent with {@code parse_namespace_and_name} in monitoring_infos.py
-   *
-   * <p>TODO: not flink-specific; where should it live?
-   */
-  public static MetricName parseUrn(String urn) {
-    if (urn.startsWith(USER_COUNTER_URN_PREFIX)) {
-      urn = urn.substring(USER_COUNTER_URN_PREFIX.length());
-    }
-    // If it is not a user counter, just use the first part of the URN, i.e. 'beam'
-    String[] pieces = urn.split(":", 2);
-    if (pieces.length != 2) {
-      throw new IllegalArgumentException("Invalid metric URN: " + urn);
-    }
-    return MetricName.named(pieces[0], pieces[1]);
   }
 
   /**
