f05bd397ac919948e25a5c75d15b47359f8a0781
Ryan Williams <ryan.blake.williams@gmail.com>
2019-01-27 18:59:12 +0000

    add toString()s to Metrics classes
    - MetricKey
    - MetricName
    - MetricQueryResults
    - MetricsContainerStepMap

--- sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/MetricQueryResults.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/MetricQueryResults.java
@@ -18,8 +18,12 @@
 package org.apache.beam.sdk.metrics;
 
 import com.google.auto.value.AutoValue;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
 import org.apache.beam.sdk.annotations.Experimental;
 import org.apache.beam.sdk.annotations.Experimental.Kind;
+import org.apache.beam.vendor.grpc.v1p13p1.com.google.common.collect.ImmutableList;
 
 /**
  * The results of a query for metrics. Allows accessing all of the metrics that matched the filter.
@@ -35,6 +39,46 @@ public abstract class MetricQueryResults {
 
   /** Return the metric results for the getGauges that matched the filter. */
   public abstract Iterable<MetricResult<GaugeResult>> getGauges();
+
+  static <T> void printMetrics(String type, Iterable<MetricResult<T>> metrics, PrintStream ps) {
+    List<MetricResult<T>> metricsList = ImmutableList.copyOf(metrics);
+    if (!metricsList.isEmpty()) {
+      ps.print(type + "(");
+      // ps.println(metricsList.size() + " " + type + ":");
+      boolean first = true;
+      for (MetricResult<T> metricResult : metricsList) {
+        if (first) {
+          first = false;
+        } else {
+          ps.print(", ");
+        }
+        MetricName name = metricResult.getName();
+        ps.print(metricResult.getStep() + ":" + name.getNamespace() + ":" + name.getName());
+        ps.print(": ");
+        ps.print(metricResult.getAttempted());
+        try {
+          T committed = metricResult.getCommitted();
+          ps.print(", ");
+          ps.print(committed);
+        } catch (UnsupportedOperationException ignored) {
+        }
+      }
+      ps.print(")");
+    }
+  }
+
+  @Override
+  public String toString() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    ps.print("MetricQueryResults(");
+    printMetrics("Counters", getCounters(), ps);
+    printMetrics("Distributions", getDistributions(), ps);
+    printMetrics("Gauges", getGauges(), ps);
+    ps.print(")");
+    ps.close();
+    return baos.toString();
+  }
 
   public static MetricQueryResults create(
       Iterable<MetricResult<Long>> counters,
