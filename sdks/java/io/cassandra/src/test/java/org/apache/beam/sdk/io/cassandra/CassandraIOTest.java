ef43d67e8c2f0309d98fc509b40471aa7788af88
Etienne Chauchot <echauchot@apache.org>
2019-01-29 17:54:01 +0100

    [BEAM-4164] Split back getEstimatedSizeBytesFromTokenRanges to be able to test it

--- sdks/java/io/cassandra/src/test/java/org/apache/beam/sdk/io/cassandra/CassandraIOTest.java
+++ sdks/java/io/cassandra/src/test/java/org/apache/beam/sdk/io/cassandra/CassandraIOTest.java
@@ -18,26 +18,20 @@
 package org.apache.beam.sdk.io.cassandra;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.apache.beam.sdk.io.cassandra.CassandraIO.CassandraSource.distance;
+import static org.apache.beam.sdk.io.cassandra.CassandraIO.CassandraSource.getEstimatedSizeBytesFromTokenRanges;
+import static org.apache.beam.sdk.io.cassandra.CassandraIO.CassandraSource.getRingFraction;
+import static org.apache.beam.sdk.io.cassandra.CassandraIO.CassandraSource.isMurmur3Partitioner;
 import static org.junit.Assert.assertEquals;
 
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ColumnMetadata;
-import com.datastax.driver.core.KeyspaceMetadata;
-import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.TableMetadata;
 import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.Table;
 import java.io.Serializable;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ConcurrentHashMap;
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -45,8 +39,7 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import org.apache.beam.sdk.coders.SerializableCoder;
-import org.apache.beam.sdk.io.BoundedSource;
-import org.apache.beam.sdk.io.BoundedSource.BoundedReader;
+import org.apache.beam.sdk.io.cassandra.CassandraIO.CassandraSource.TokenRange;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
@@ -59,16 +52,16 @@ import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Objects;
 import org.apache.cassandra.service.StorageServiceMBean;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /** Tests of {@link CassandraIO}. */
 public class CassandraIOTest implements Serializable {
@@ -293,64 +286,64 @@ public class CassandraIOTest implements Serializable {
 
   @Test
   public void testValidPartitioner() {
-    Assert.assertTrue(CassandraServiceImpl.isMurmur3Partitioner(cluster));
+    Assert.assertTrue(isMurmur3Partitioner(cluster));
   }
 
   @Test
   public void testDistance() {
     BigInteger distance =
-        CassandraServiceImpl.distance(new BigInteger("10"), new BigInteger("100"));
+        CassandraIO.CassandraSource.distance(new BigInteger("10"), new BigInteger("100"));
     assertEquals(BigInteger.valueOf(90), distance);
 
-    distance = CassandraServiceImpl.distance(new BigInteger("100"), new BigInteger("10"));
+    distance = distance(new BigInteger("100"), new BigInteger("10"));
     assertEquals(new BigInteger("18446744073709551526"), distance);
   }
 
   @Test
   public void testRingFraction() {
     // simulate a first range taking "half" of the available tokens
-    List<CassandraServiceImpl.TokenRange> tokenRanges = new ArrayList<>();
+    List<TokenRange> tokenRanges = new ArrayList<>();
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1, BigInteger.valueOf(Long.MIN_VALUE), new BigInteger("0")));
-    assertEquals(0.5, CassandraServiceImpl.getRingFraction(tokenRanges), 0);
+    assertEquals(0.5, getRingFraction(tokenRanges), 0);
 
     // add a second range to cover all tokens available
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1, new BigInteger("0"), BigInteger.valueOf(Long.MAX_VALUE)));
-    assertEquals(1.0, CassandraServiceImpl.getRingFraction(tokenRanges), 0);
+    assertEquals(1.0, getRingFraction(tokenRanges), 0);
   }
 
   @Test
   public void testEstimatedSizeBytesFromTokenRanges() {
-    List<CassandraServiceImpl.TokenRange> tokenRanges = new ArrayList<>();
+    List<TokenRange> tokenRanges = new ArrayList<>();
     // one partition containing all tokens, the size is actually the size of the partition
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1000, BigInteger.valueOf(Long.MIN_VALUE), BigInteger.valueOf(Long.MAX_VALUE)));
-    assertEquals(1000, CassandraServiceImpl.getEstimatedSizeBytes(tokenRanges));
+    assertEquals(1000, getEstimatedSizeBytesFromTokenRanges(tokenRanges));
 
     // one partition with half of the tokens, we estimate the size to the double of this partition
     tokenRanges = new ArrayList<>();
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1000, BigInteger.valueOf(Long.MIN_VALUE), new BigInteger("0")));
-    assertEquals(2000, CassandraServiceImpl.getEstimatedSizeBytes(tokenRanges));
+    assertEquals(2000, getEstimatedSizeBytesFromTokenRanges(tokenRanges));
 
     // we have three partitions covering all tokens, the size is the sum of partition size *
     // partition count
     tokenRanges = new ArrayList<>();
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1000, BigInteger.valueOf(Long.MIN_VALUE), new BigInteger("-3")));
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             1, 1000, new BigInteger("-2"), new BigInteger("10000")));
     tokenRanges.add(
-        new CassandraServiceImpl.TokenRange(
+        new TokenRange(
             2, 3000, new BigInteger("10001"), BigInteger.valueOf(Long.MAX_VALUE)));
-    assertEquals(8000, CassandraServiceImpl.getEstimatedSizeBytes(tokenRanges));
+    assertEquals(8000, getEstimatedSizeBytesFromTokenRanges(tokenRanges));
   }
 
   /** Simple Cassandra entity used in test. */
