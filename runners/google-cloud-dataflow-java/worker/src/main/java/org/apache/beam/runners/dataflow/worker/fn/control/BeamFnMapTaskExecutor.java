fb4d1f22193d334ded914a7eb975bb12acb4f15f
Kenneth Knowles <klk@google.com>
2019-01-30 22:32:41 -0800

    [BEAM-5658] Fix erroneous cases of integer division in Dataflow worker

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/fn/control/BeamFnMapTaskExecutor.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/fn/control/BeamFnMapTaskExecutor.java
@@ -619,7 +619,7 @@ public class BeamFnMapTaskExecutor extends DataflowMapTaskExecutor {
         T[] newYs = (T[]) new Object[ys.length];
         newXs[0] = xs[0];
         newYs[0] = ys[0];
-        double dx = (xs[numPoints - 1] - xs[0]) / (numPoints / 2);
+        double dx = (xs[numPoints - 1] - xs[0]) / (numPoints / 2.0);
         for (int i = 1; i < numPoints / 2; i++) {
           double x = xs[0] + i * dx;
           newXs[i] = x;
