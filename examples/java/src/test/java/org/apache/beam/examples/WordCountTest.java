01bb1e7440b9c6a86d010ab196bfa0bd701eed44
Harshit Dwivedi <harshithdwivedi@gmail.com>
2019-02-12 13:11:38 +0530

    fix : failing tests
    Signed-off-by: Harshit Dwivedi <harshithdwivedi@gmail.com>

--- examples/java/src/test/java/org/apache/beam/examples/WordCountTest.java
+++ examples/java/src/test/java/org/apache/beam/examples/WordCountTest.java
@@ -19,6 +19,7 @@ package org.apache.beam.examples;
 
 import java.util.Arrays;
 import java.util.List;
+
 import org.apache.beam.examples.java.WordCount.CountWords;
 import org.apache.beam.examples.java.WordCount.ExtractWordsFn;
 import org.apache.beam.examples.java.WordCount.FormatAsTextFn;
