72c2259c70325803702dfd5308355505a22282c0
Etienne Chauchot <echauchot@apache.org>
2019-02-13 17:19:08 +0100

    [BEAM-6663] Add the ability to transform to/from json SerializablePipelineOptions and remove no more needed PipelineOptionsSerializationUtils

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/SerializablePipelineOptions.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/SerializablePipelineOptions.java
@@ -17,24 +17,36 @@
  */
 package org.apache.beam.runners.core.construction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 import org.apache.beam.sdk.io.FileSystems;
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.util.common.ReflectHelpers;
 
 /**
  * Holds a {@link PipelineOptions} in JSON serialized form and calls {@link
  * FileSystems#setDefaultPipelineOptions(PipelineOptions)} on construction or on deserialization.
  */
 public class SerializablePipelineOptions implements Serializable {
+  private static final ObjectMapper MAPPER =
+      new ObjectMapper()
+          .registerModules(ObjectMapper.findModules(ReflectHelpers.findClassLoader()));
 
   private final String serializedPipelineOptions;
   private transient PipelineOptions options;
 
   public SerializablePipelineOptions(PipelineOptions options) {
-    this.serializedPipelineOptions = PipelineOptionsSerializationUtils.serializeToJson(options);
+    this.serializedPipelineOptions = serializeToJson(options);
     this.options = options;
+    FileSystems.setDefaultPipelineOptions(options);
+  }
+
+  public SerializablePipelineOptions(String json) {
+    this.serializedPipelineOptions = json;
+    this.options = deserializeFromJson(json);
     FileSystems.setDefaultPipelineOptions(options);
   }
 
@@ -44,8 +56,29 @@ public class SerializablePipelineOptions implements Serializable {
 
   private void readObject(ObjectInputStream is) throws IOException, ClassNotFoundException {
     is.defaultReadObject();
-    this.options = PipelineOptionsSerializationUtils.deserializeFromJson(serializedPipelineOptions);
+    this.options = deserializeFromJson(serializedPipelineOptions);
     // TODO https://issues.apache.org/jira/browse/BEAM-2712: remove this call.
     FileSystems.setDefaultPipelineOptions(options);
+  }
+
+  private static String serializeToJson(PipelineOptions options) {
+    try {
+      return MAPPER.writeValueAsString(options);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException("Failed to serialize PipelineOptions", e);
+    }
+  }
+
+  private static PipelineOptions deserializeFromJson(String options) {
+    try {
+      return MAPPER.readValue(options, PipelineOptions.class);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Failed to deserialize PipelineOptions", e);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return serializedPipelineOptions;
   }
 }
