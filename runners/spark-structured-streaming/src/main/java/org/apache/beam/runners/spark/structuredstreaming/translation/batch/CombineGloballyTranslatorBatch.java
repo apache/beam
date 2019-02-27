182db6b76d99d4683e4fe9f5af5ec6a95b5f1012
Etienne Chauchot <echauchot@apache.org>
2019-02-27 18:30:23 +0100

    [BEAM-8470] Fix combine. For unknown reason GenericRowWithSchema is used as input of combine so extract its content to be able to proceed

--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/batch/CombineGloballyTranslatorBatch.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/translation/batch/CombineGloballyTranslatorBatch.java
@@ -25,7 +25,6 @@ import org.apache.beam.runners.spark.structuredstreaming.translation.helpers.Win
 import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.util.WindowedValue;
-import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
