917431183461763d807569409e4870afadd20cc3
Kasia Kucharczyk <katarzyna.kucharczyk@polidea.com>
2019-03-06 15:31:48 +0100

    [BEAM-5995] Separated smoke load test and added GBK suite with 7 scenarios.

--- .test-infra/jenkins/job_LoadTests_Python.groovy
+++ .test-infra/jenkins/job_LoadTests_Python.groovy
@@ -22,56 +22,170 @@ import PhraseTriggeringPostCommitBuilder
 
 def now = new Date().format("MMddHHmmss", TimeZone.getTimeZone('UTC'))
 
-def smokeTestConfigurations = [
+def testConfigurations = [
         [
-                title        : 'GroupByKey Python load test Direct',
-                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
-                runner       : CommonTestProperties.Runner.DIRECT,
-                sdk          : CommonTestProperties.SDK.PYTHON,
-                jobProperties: [
-                        publish_to_big_query: true,
-                        project             : 'apache-beam-testing',
-                        metrics_dataset     : 'load_test_SMOKE',
-                        metrics_table       : 'python_direct_gbk',
-                        input_options       : '\'{"num_records": 100000,' +
-                                '"key_size": 1,' +
-                                '"value_size":1,' +
-                                '"bundle_size_distribution_type": "const",' +
-                                '"bundle_size_distribution_param": 1,' +
-                                '"force_initial_num_bundles": 10}\'',
-
-                ]
-        ],
-        [
-                title        : 'GroupByKey Python load test Dataflow',
+                title        : 'GroupByKey Python Load test: 2GB of 10B records',
                 itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
                 runner       : CommonTestProperties.Runner.DATAFLOW,
                 sdk          : CommonTestProperties.SDK.PYTHON,
                 jobProperties: [
-                        job_name            : 'load-tests-python-dataflow-batch-gbk-smoke-' + now,
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-1-' + now,
                         project             : 'apache-beam-testing',
-                        temp_location       : 'gs://temp-storage-for-perf-tests/smoketests',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
                         publish_to_big_query: true,
-                        metrics_dataset     : 'load_test_SMOKE',
-                        metrics_table       : 'python_dataflow_gbk',
-                        input_options       : '\'{"num_records": 100000,' +
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_1',
+                        input_options       : '\'{"num_records": 200000000,' +
                                 '"key_size": 1,' +
-                                '"value_size":1,' +
-                                '"bundle_size_distribution_type": "const",' +
-                                '"bundle_size_distribution_param": 1,' +
-                                '"force_initial_num_bundles": 10}\'',
-                        maxNumWorkers       : 10,
+                                '"value_size": 9}\'',
+                        iterations          : 1,
+                        fanout              : 1,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: 2GB of 100B records',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-2-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_2',
+                        input_options       : '\'{"num_records": 20000000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations          : 1,
+                        fanout              : 1,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: 2GB of 100kB records',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-3-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_3',
+                        input_options       : '\'{"num_records": 2000,' +
+                                '"key_size": 100000,' +
+                                '"value_size": 900000}\'',
+                        iterations          : 1,
+                        fanout              : 1,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: fanout 4 times with 2GB 10-byte records total',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-4-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_4',
+                        input_options       : '\'{"num_records": 5000000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations          : 1,
+                        fanout              : 4,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: fanout 8 times with 2GB 10-byte records total',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-5-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_5',
+                        input_options       : '\'{"num_records": 2500000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations          : 1,
+                        fanout              : 8,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: reiterate 4 times 10kB values',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-6-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_6',
+                        input_options       : '\'{"num_records": 20000000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations          : 4,
+                        fanout              : 1,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
+                ]
+        ],
+        [
+                title        : 'GroupByKey Python Load test: reiterate 4 times 2MB values',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name            : 'load-tests-python-dataflow-batch-gbk-7-' + now,
+                        project             : 'apache-beam-testing',
+                        temp_location       : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query: true,
+                        metrics_dataset     : 'load_test',
+                        metrics_table       : 'python_dataflow_batch_gbk_6',
+                        input_options       : '\'{"num_records": 20000000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations          : 4,
+                        fanout              : 1,
+                        maxNumWorkers       : 5,
+                        numWorkers          : 5,
+                        autoscalingAlgorithm: "NONE"
                 ]
         ],
 ]
 
 PhraseTriggeringPostCommitBuilder.postCommitJob(
-        'beam_Python_LoadTests_Smoke',
-        'Run Python Load Tests Smoke',
-        'Python Load Tests Smoke',
+        'beam_Python_LoadTests_Batch_Combine',
+        'Run Python Load Tests Batch Combine',
+        'Python Load Tests Batch Combine',
         this
 ) {
-    description("Runs Python load tests in \"smoke\" mode to check if everything works well")
+    description("Runs Python batch combine load tests")
     commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 120)
 
     for (testConfiguration in smokeTestConfigurations) {
