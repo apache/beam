c31bbe6be515498fb332e8970a50babbf4403404
Juta <juta.staes@gmail.com>
2019-03-18 09:46:36 +0100

    [BEAM-6619] [BEAM-6593] [BEAM-6711] Add bigquery integration tests to postcommit

--- sdks/python/apache_beam/examples/complete/game/game_stats_it_test.py
+++ sdks/python/apache_beam/examples/complete/game/game_stats_it_test.py
@@ -33,8 +33,6 @@ Usage:
 from __future__ import absolute_import
 
 import logging
-import os
-import sys
 import time
 import unittest
 import uuid
@@ -51,10 +49,6 @@ from apache_beam.testing.pipeline_verifiers import PipelineStateMatcher
 from apache_beam.testing.test_pipeline import TestPipeline
 
 
-@unittest.skipIf(sys.version_info[0] == 3 and
-                 os.environ.get('RUN_SKIPPED_PY3_TESTS') != '1',
-                 'This test still needs to be fixed on Python 3'
-                 'TODO: BEAM-6711')
 class GameStatsIT(unittest.TestCase):
 
   # Input events containing user, team, score, processing time, window start.
@@ -102,7 +96,8 @@ class GameStatsIT(unittest.TestCase):
 
     for _ in range(message_count):
       self.pub_client.publish(topic.name,
-                              self.INPUT_EVENT % self._test_timestamp)
+                              (self.INPUT_EVENT % self._test_timestamp
+                              ).encode('utf-8'))
 
   def _cleanup_pubsub(self):
     test_utils.cleanup_subscriptions(self.sub_client, [self.input_sub])
