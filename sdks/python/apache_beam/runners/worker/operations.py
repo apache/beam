464e62afd948767003316e18ce6d1742b7630c23
Robert Bradshaw <robertwb@google.com>
2019-03-19 12:10:26 +0100

    Distinct sizing/sized SDF operations, with renames.

--- sdks/python/apache_beam/runners/worker/operations.py
+++ sdks/python/apache_beam/runners/worker/operations.py
@@ -612,10 +612,10 @@ class DoOperation(Operation):
     return infos
 
 
-class SdfProcessElements(DoOperation):
+class SdfProcessSizedElements(DoOperation):
 
   def __init__(self, *args, **kwargs):
-    super(SdfProcessElements, self).__init__(*args, **kwargs)
+    super(SdfProcessSizedElements, self).__init__(*args, **kwargs)
     self.lock = threading.RLock()
     self.element_start_output_bytes = None
 
@@ -628,7 +628,7 @@ class SdfProcessElements(DoOperation):
             receiver.opcounter.restart_sampling()
         # Actually processing the element can be expensive; do it without
         # the lock.
-        delayed_application = self.dofn_runner.process_with_restriction(o)
+        delayed_application = self.dofn_runner.process_with_sized_restriction(o)
         if delayed_application:
           self.execution_context.delayed_applications.append(
               (self, delayed_application))
@@ -650,7 +650,7 @@ class SdfProcessElements(DoOperation):
 
   def progress_metrics(self):
     with self.lock:
-      metrics = super(SdfProcessElements, self).progress_metrics()
+      metrics = super(SdfProcessSizedElements, self).progress_metrics()
       current_element_progress = self.current_element_progress()
     if current_element_progress:
       metrics.active_elements.measured.input_element_counts[
