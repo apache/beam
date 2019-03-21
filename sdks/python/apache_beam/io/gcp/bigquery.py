378ae0894349713bebe9429d333650122e190cd5
Pablo <pabloem@users.noreply.github.com>
2019-03-21 11:57:44 -0700

    Reverting unnecessary change.

--- sdks/python/apache_beam/io/gcp/bigquery.py
+++ sdks/python/apache_beam/io/gcp/bigquery.py
@@ -687,11 +687,9 @@ class BigQueryWriteFn(DoFn):
     else:
       schema = self.schema
 
-    table_ref = bigquery_tools.parse_table_reference(destination)
-    if not table_ref.projectId:
-      table_ref.projectId = vp.RuntimeValueProvider.get_value('project',
-                                                              str, '')
-    self._create_table_if_needed(table_ref, schema)
+    self._create_table_if_needed(
+        bigquery_tools.parse_table_reference(destination),
+        schema)
 
     row = element[1]
     self._rows_buffer[destination].append(row)
