2accfeafa01b252d38aef30d0fb1e44397527a49
Etienne Chauchot <echauchot@apache.org>
2019-04-03 11:21:59 +0200

    [BEAM-8470] Specify checkpointLocation at the pipeline start

--- runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/SparkPipelineOptions.java
+++ runners/spark-structured-streaming/src/main/java/org/apache/beam/runners/spark/structuredstreaming/SparkPipelineOptions.java
@@ -47,21 +47,9 @@ public interface SparkPipelineOptions
   @Description(
       "A checkpoint directory for streaming resilience, ignored in batch. "
           + "For durability, a reliable filesystem such as HDFS/S3/GS is necessary.")
-  @Default.InstanceFactory(TmpCheckpointDirFactory.class)
   String getCheckpointDir();
 
   void setCheckpointDir(String checkpointDir);
-
-  /**
-   * Returns the default checkpoint directory of /tmp/${job.name}. For testing purposes only.
-   * Production applications should use a reliable filesystem such as HDFS/S3/GS.
-   */
-  class TmpCheckpointDirFactory implements DefaultValueFactory<String> {
-    @Override
-    public String create(PipelineOptions options) {
-      return "/tmp/" + options.as(SparkPipelineOptions.class).getJobName();
-    }
-  }
 
   @Description(
       "The period to checkpoint (in Millis). If not set, Spark will default "
