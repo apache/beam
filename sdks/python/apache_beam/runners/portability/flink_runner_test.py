7495aa719037445a17a5af1d503766f5c5781d74
Maximilian Michels <mxm@apache.org>
2019-04-08 16:16:28 +0200

    [BEAM-7029] Add KafkaIO.Read as an external transform
    This adds KafkaIO.Read as an external transform and includes a Python
    wrapper (ReadFromkafka) for convenience. The transform only returns the
    key/value data for a Kafka topic. It does not include the meta data such as
    partition id, offset, or timestamp.
    
    By default, the data is returned as `KV<byte[], byte[]>`. Users can supply a
    Kafka Deserializer in ReadFromkafka such as LongDeserializer which will infer a
    different coder. Only a limited amount of Deserializers are supported.
    Alternatively, users can implement their own decoding in the target SDK.

--- sdks/python/apache_beam/runners/portability/flink_runner_test.py
+++ sdks/python/apache_beam/runners/portability/flink_runner_test.py
@@ -29,6 +29,7 @@ from tempfile import mkdtemp
 
 import apache_beam as beam
 from apache_beam.io.external.generate_sequence import GenerateSequence
+from apache_beam.io.external.kafka import ReadFromKafka
 from apache_beam.metrics import Metrics
 from apache_beam.options.pipeline_options import DebugOptions
 from apache_beam.options.pipeline_options import PortableOptions
@@ -157,7 +158,7 @@ if __name__ == '__main__':
     def test_no_subtransform_composite(self):
       raise unittest.SkipTest("BEAM-4781")
 
-    def test_external_transform(self):
+    def test_external_transforms(self):
       options = self.create_options()
       options._all_options['parallelism'] = 1
       options._all_options['streaming'] = True
@@ -171,6 +172,28 @@ if __name__ == '__main__':
                                expansion_service=expansion_address))
 
         assert_that(res, equal_to([i for i in range(1, 10)]))
+
+      # We expect to fail here because we do not have a Kafka cluster handy.
+      # Nevertheless, we check that the transform is expanded by the
+      # ExpansionService and that the pipeline fails during execution.
+      with self.assertRaises(Exception) as ctx:
+        with self.create_pipeline() as p:
+          # pylint: disable=expression-not-assigned
+          (p
+           | ReadFromKafka(consumer_config={'bootstrap.servers':
+                                            'notvalid1:7777, notvalid2:3531'},
+                           topics=['topic1', 'topic2'],
+                           key_deserializer='org.apache.kafka.'
+                                            'common.serialization.'
+                                            'ByteArrayDeserializer',
+                           value_deserializer='org.apache.kafka.'
+                                              'common.serialization.'
+                                              'LongDeserializer',
+                           expansion_service=expansion_address))
+      self.assertTrue('No resolvable bootstrap urls given in bootstrap.servers'
+                      in str(ctx.exception),
+                      'Expected to fail due to invalid bootstrap.servers, but '
+                      'failed due to:\n%s' % str(ctx.exception))
 
     def test_flattened_side_input(self):
       # Blocked on support for transcoding
