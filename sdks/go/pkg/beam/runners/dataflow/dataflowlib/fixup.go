2b624e1c9294b7ae6ee4efeda90261117e102e7a
Lukasz Cwik <lcwik@google.com>
2019-04-08 16:37:42 -0700

    [BEAM-7011] Update Beam SDKs to use the StandardSideInputType enums. (#8232)
    * [BEAM-7011] Update Beam to use the StandardSideInputType enums.

--- sdks/go/pkg/beam/runners/dataflow/dataflowlib/fixup.go
+++ sdks/go/pkg/beam/runners/dataflow/dataflowlib/fixup.go
@@ -16,44 +16,10 @@
 package dataflowlib
 
 import (
-	"github.com/apache/beam/sdks/go/pkg/beam/core/runtime/graphx"
-	"github.com/apache/beam/sdks/go/pkg/beam/core/runtime/pipelinex"
-	"github.com/apache/beam/sdks/go/pkg/beam/core/util/protox"
 	pb "github.com/apache/beam/sdks/go/pkg/beam/model/pipeline_v1"
-	"github.com/golang/protobuf/proto"
 )
 
 // Fixup proto pipeline with Dataflow quirks.
 func Fixup(p *pb.Pipeline) (*pb.Pipeline, error) {
-	upd := make(map[string]*pb.PTransform)
-
-	for id, t := range p.GetComponents().GetTransforms() {
-		if t.GetSpec().GetUrn() != graphx.URNParDo {
-			continue
-		}
-		var payload pb.ParDoPayload
-		if err := proto.Unmarshal(t.GetSpec().GetPayload(), &payload); err != nil {
-			continue // ignore: unexpected payload
-		}
-		if len(payload.SideInputs) == 0 {
-			continue
-		}
-
-		// ParDo w/ side input. Fixup URN.
-
-		fixedPayload := pipelinex.ShallowCloneParDoPayload(&payload)
-		for k, v := range payload.SideInputs {
-			fixedV := pipelinex.ShallowCloneSideInput(v)
-			fixedV.AccessPattern = pipelinex.ShallowCloneFunctionSpec(v.AccessPattern)
-			fixedV.AccessPattern.Urn = "urn:beam:sideinput:materialization:multimap:0.1"
-
-			fixedPayload.SideInputs[k] = fixedV
-		}
-		fixed := pipelinex.ShallowClonePTransform(t)
-		fixed.Spec = pipelinex.ShallowCloneFunctionSpec(t.Spec)
-		fixed.Spec.Payload = protox.MustEncode(fixedPayload)
-
-		upd[id] = fixed
-	}
-	return pipelinex.Update(p, &pb.Components{Transforms: upd})
+	return p, nil
 }
