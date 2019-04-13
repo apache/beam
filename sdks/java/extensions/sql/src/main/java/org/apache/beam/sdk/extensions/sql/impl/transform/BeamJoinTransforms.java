a5f3a75333daa59077c4ef182c29b93362fc2062
amaliujia <amaliujia@163.com>
2019-04-13 10:35:11 -0700

    [BEAM-7070] JOIN condition should accept field access

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/transform/BeamJoinTransforms.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/transform/BeamJoinTransforms.java
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.apache.beam.sdk.extensions.sql.BeamSqlSeekableTable;
+import org.apache.beam.sdk.extensions.sql.impl.utils.SerializableRexInputRef;
+import org.apache.beam.sdk.extensions.sql.impl.utils.SerializableRexNode;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
@@ -44,25 +46,47 @@ public class BeamJoinTransforms {
 
   /** A {@code SimpleFunction} to extract join fields from the specified row. */
   public static class ExtractJoinFields extends SimpleFunction<Row, KV<Row, Row>> {
-    private final List<Integer> joinColumns;
+    private final List<SerializableRexNode> joinColumns;
     private final Schema schema;
+    private int leftRowColumnCount;
 
     public ExtractJoinFields(
-        boolean isLeft, List<Pair<Integer, Integer>> joinColumns, Schema schema) {
+        boolean isLeft,
+        List<Pair<RexNode, RexNode>> joinColumns,
+        Schema schema,
+        int leftRowColumnCount) {
       this.joinColumns =
-          joinColumns.stream().map(pair -> isLeft ? pair.left : pair.right).collect(toList());
+          joinColumns.stream()
+              .map(pair -> SerializableRexNode.builder(isLeft ? pair.left : pair.right).build())
+              .collect(toList());
       this.schema = schema;
+      this.leftRowColumnCount = leftRowColumnCount;
     }
 
     @Override
     public KV<Row, Row> apply(Row input) {
-      Row row = joinColumns.stream().map(input::getValue).collect(toRow(schema));
+      Row row =
+          joinColumns.stream()
+              .map(v -> getValue(v, input, leftRowColumnCount))
+              .collect(toRow(schema));
       return KV.of(row, input);
     }
 
+    @SuppressWarnings("unused")
     private Schema.Field toField(Schema schema, Integer fieldIndex) {
       Schema.Field original = schema.getField(fieldIndex);
       return original.withName("c" + fieldIndex);
+    }
+
+    private Object getValue(
+        SerializableRexNode serializableRexNode, Row input, int leftRowColumnCount) {
+      if (serializableRexNode instanceof SerializableRexInputRef) {
+        return input.getValue(serializableRexNode.getIndex() - leftRowColumnCount);
+      } else {
+        // It can only be SerializableFieldAccess.
+        Row rowField = input.getValue(serializableRexNode.getReferenceNode().getIndex());
+        return rowField.getValue(serializableRexNode.getIndex());
+      }
     }
   }
 
