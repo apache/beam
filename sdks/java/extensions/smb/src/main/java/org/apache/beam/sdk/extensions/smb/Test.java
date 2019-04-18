5a47084e3c25fe85b30f02ae2d212f0cdd36250e
Neville Li <neville@spotify.com>
2019-04-18 15:00:19 -0400

    add ser/de helpers for BucketMetadata

--- sdks/java/extensions/smb/src/main/java/org/apache/beam/sdk/extensions/smb/Test.java
+++ sdks/java/extensions/smb/src/main/java/org/apache/beam/sdk/extensions/smb/Test.java
@@ -1,30 +1,18 @@
 package org.apache.beam.sdk.extensions.smb;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.coders.CannotProvideCoderException;
-import org.apache.beam.sdk.coders.CoderProvider;
-import org.apache.beam.sdk.coders.CoderProviders;
-import org.apache.beam.sdk.coders.CoderRegistry;
-import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Lists;
 
 import java.io.IOException;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
 
 public class Test {
   public static void main(String[] args) throws IOException, CannotProvideCoderException {
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    AvroBucketMetadata<String> metadata = new AvroBucketMetadata<>(
+    BucketMetadata<String, GenericRecord> metadata = new AvroBucketMetadata<>(
             10, String.class, BucketMetadata.HashType.MURMUR3_32, "a.b.c");
-    String json = objectMapper.writeValueAsString(metadata);
     System.out.println("==========");
-    System.out.println(json);
+    System.out.println(metadata);
 
-    metadata = objectMapper.readerFor(BucketMetadata.class).readValue(json);
+    metadata = BucketMetadata.from(metadata.toString());
     System.out.println("==========");
     System.out.println(metadata.getNumBuckets());
     System.out.println(metadata.getSortingKeyClass());
