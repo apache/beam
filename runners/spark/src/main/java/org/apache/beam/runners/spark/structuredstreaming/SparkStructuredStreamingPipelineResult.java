d0f2ba3f8602dd63e6cf3876ce2d6f3d00af8849
Ismaël Mejía <iemejia@gmail.com>
2019-04-24 10:22:04 +0200

    [BEAM-8470] Rename SparkPipelineResult to SparkStructuredStreamingPipelineResult This is done to avoid an eventual collision with the one in SparkRunner. However this cannot happen at this moment because it is package private, so it is also done for consistency.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/SparkStructuredStreamingPipelineResult.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/structuredstreaming/SparkStructuredStreamingPipelineResult.java
@@ -24,7 +24,7 @@ import org.apache.beam.sdk.metrics.MetricResults;
 import org.joda.time.Duration;
 
 /** Represents a Spark pipeline execution result. */
-class SparkPipelineResult implements PipelineResult {
+class SparkStructuredStreamingPipelineResult implements PipelineResult {
 
   @Nullable // TODO: remove once method will be implemented
   @Override
