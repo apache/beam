bfb0bbea996634d0b0805ee5df8fad4bf0ec539e
Mark Liu <markliu@google.com>
2019-04-26 11:23:16 -0700

    Refactor Python performance test groovy file for easy configuration.

--- .test-infra/jenkins/job_PerformanceTests_Python.groovy
+++ .test-infra/jenkins/job_PerformanceTests_Python.groovy
@@ -18,46 +18,105 @@
 
 import CommonJobProperties as commonJobProperties
 
-// This job runs the Beam Python performance tests on PerfKit Benchmarker.
-job('beam_PerformanceTests_Python'){
-  // Set default Beam job properties.
-  commonJobProperties.setTopLevelMainJobProperties(delegate)
 
-  // Run job in postcommit every 6 hours, don't trigger every push.
-  commonJobProperties.setAutoJob(
-      delegate,
-      'H */6 * * *')
+class PerformanceTestConfigurations {
+  String jobName
+  String jobDescription
+  String jobTriggerPhrase
+  String buildSchedule = 'H */6 * * *'  // every 6 hours
+  String benchmarkName = 'beam_integration_benchmark'
+  String sdk = 'python'
+  String bigqueryTable
+  String itClass
+  String itModule
+  Boolean skipPrebuild = false
+  String pythonSdkLocation
+  String runner = 'TestDataflowRunner'
+  Integer itTimeout = 1200
+  Map extraPipelineArgs
+}
 
-  // Allows triggering this build against pull requests.
-  commonJobProperties.enablePhraseTriggeringFromPullRequest(
-      delegate,
-      'Python SDK Performance Test',
-      'Run Python Performance Test')
 
-  def pipelineArgs = [
-      project: 'apache-beam-testing',
-      staging_location: 'gs://temp-storage-for-end-to-end-tests/staging-it',
-      temp_location: 'gs://temp-storage-for-end-to-end-tests/temp-it',
-      output: 'gs://temp-storage-for-end-to-end-tests/py-it-cloud/output'
-  ]
-  def pipelineArgList = []
-  pipelineArgs.each({
-    key, value -> pipelineArgList.add("--$key=$value")
-  })
-  def pipelineArgsJoined = pipelineArgList.join(',')
+def testConfigurations = [
+    new PerformanceTestConfigurations(
+        jobName           : 'beam_PerformanceTests_Python',
+        jobDescription    : 'Python SDK Performance Test',
+        jobTriggerPhrase  : 'Run Python Performance Test',
+        bigqueryTable     : 'beam_performance.wordcount_py_pkb_results',
+        skipPrebuild      : true,
+        pythonSdkLocation : 'build/apache-beam.tar.gz',
+        itClass           : 'apache_beam.examples.wordcount_it_test:WordCountIT.test_wordcount_it',
+        itModule          : 'sdks/python',
+        extraPipelineArgs : [
+            project         : 'apache-beam-testing',
+            staging_location: 'gs://temp-storage-for-end-to-end-tests/staging-it',
+            temp_location   : 'gs://temp-storage-for-end-to-end-tests/temp-it',
+            output          : 'gs://temp-storage-for-end-to-end-tests/py-it-cloud/output',
+        ],
+    ),
+    new PerformanceTestConfigurations(
+        jobName           : 'beam_PerformanceTests_Python35',
+        jobDescription    : 'Python35 SDK Performance Test',
+        jobTriggerPhrase  : 'Run Python35 Performance Test',
+        bigqueryTable     : 'beam_performance.wordcount_py35_pkb_results',
+        skipPrebuild      : true,
+        pythonSdkLocation : 'test-suites/dataflow/py35/build/apache-beam.tar.gz',
+        itClass           : 'apache_beam.examples.wordcount_it_test:WordCountIT.test_wordcount_it',
+        itModule          : 'sdks/python/test-suites/dataflow/py35',
+        extraPipelineArgs : [
+            project         : 'apache-beam-testing',
+            staging_location: 'gs://temp-storage-for-end-to-end-tests/staging-it',
+            temp_location   : 'gs://temp-storage-for-end-to-end-tests/temp-it',
+            output          : 'gs://temp-storage-for-end-to-end-tests/py-it-cloud/output',
+        ],
+    )
+]
 
-  def argMap = [
-      beam_sdk                 : 'python',
-      benchmarks               : 'beam_integration_benchmark',
-      bigquery_table           : 'beam_performance.wordcount_py_pkb_results',
-      beam_it_class            : 'apache_beam.examples.wordcount_it_test:WordCountIT.test_wordcount_it',
-      beam_it_module           : 'sdks/python',
-      beam_prebuilt            : 'true',  // skip beam prebuild
-      beam_python_sdk_location : 'build/apache-beam.tar.gz',
-      beam_runner              : 'TestDataflowRunner',
-      beam_it_timeout          : '1200',
-      beam_it_args             : pipelineArgsJoined,
-  ]
 
-  commonJobProperties.buildPerformanceTest(delegate, argMap)
+for (testConfig in testConfigurations) {
+  createPythonPerformanceTestJob(testConfig)
+}
+
+
+private void createPythonPerformanceTestJob(PerformanceTestConfigurations testConfig) {
+  // This job runs the Beam Python performance tests on PerfKit Benchmarker.
+  job(testConfig.jobName) {
+    // Set default Beam job properties.
+    commonJobProperties.setTopLevelMainJobProperties(delegate)
+
+    // Run job in postcommit, don't trigger every push.
+    commonJobProperties.setAutoJob(
+        delegate,
+        testConfig.buildSchedule)
+
+    // Allows triggering this build against pull requests.
+    commonJobProperties.enablePhraseTriggeringFromPullRequest(
+        delegate,
+        testConfig.jobDescription,
+        testConfig.jobTriggerPhrase)
+
+    // Helper function to join pipeline args from a map.
+    def joinPipelineArgs = { pipelineArgs ->
+      def pipelineArgList = []
+      pipelineArgs.each({
+        key, value -> pipelineArgList.add("--$key=$value")
+      })
+      return pipelineArgList.join(',')
+    }
+
+    def argMap = [
+        beam_sdk                : testConfig.sdk,
+        benchmarks              : testConfig.benchmarkName,
+        bigquery_table          : testConfig.bigqueryTable,
+        beam_it_class           : testConfig.itClass,
+        beam_it_module          : testConfig.itModule,
+        beam_prebuilt           : testConfig.skipPrebuild.toString(),
+        beam_python_sdk_location: testConfig.pythonSdkLocation,
+        beam_runner             : testConfig.runner,
+        beam_it_timeout         : testConfig.itTimeout.toString(),
+        beam_it_args            : joinPipelineArgs(testConfig.extraPipelineArgs),
+    ]
+
+    commonJobProperties.buildPerformanceTest(delegate, argMap)
+  }
 }
