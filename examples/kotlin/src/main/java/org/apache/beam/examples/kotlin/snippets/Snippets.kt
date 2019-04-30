ae2ee40a18dcba7f171b93e63f2a12bd24c02fea
Harshit Dwivedi <harshit@pitech.app>
2019-04-30 20:00:40 +0530

    fix : findbugs errors
    Signed-off-by: Harshit Dwivedi <harshit@pitech.app>

--- examples/kotlin/src/main/java/org/apache/beam/examples/kotlin/snippets/Snippets.kt
+++ examples/kotlin/src/main/java/org/apache/beam/examples/kotlin/snippets/Snippets.kt
@@ -18,20 +18,23 @@
 package org.apache.beam.examples.kotlin.snippets
 
 import com.google.api.services.bigquery.model.*
+import com.google.common.collect.ImmutableList
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.coders.AvroCoder
 import org.apache.beam.sdk.coders.DefaultCoder
 import org.apache.beam.sdk.coders.DoubleCoder
-import org.apache.beam.sdk.io.gcp.bigquery.*
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition
+import org.apache.beam.sdk.io.gcp.bigquery.DynamicDestinations
+import org.apache.beam.sdk.io.gcp.bigquery.TableDestination
+import org.apache.beam.sdk.io.gcp.bigquery.WriteResult
 import org.apache.beam.sdk.transforms.*
 import org.apache.beam.sdk.transforms.join.CoGbkResult
 import org.apache.beam.sdk.transforms.join.CoGroupByKey
 import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple
 import org.apache.beam.sdk.values.*
-import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableList
-import java.util.*
 
 /** Code snippets used in webdocs.  */
 object Snippets {
@@ -51,8 +54,10 @@ object Snippets {
     )
 
     @JvmOverloads
+    @SuppressFBWarnings("SE_BAD_FIELD")
+    //Apparently findbugs doesn't like that a non-serialized object i.e. pipeline is being used inside the run{} block
     fun modelBigQueryIO(
-            p: Pipeline, writeProject: String = "", writeDataset: String = "", writeTable: String = "") {
+            pipeline: Pipeline, writeProject: String = "", writeDataset: String = "", writeTable: String = "") {
         run {
             // [START BigQueryTableSpec]
             val tableSpec = "clouddataflow-readonly:samples.weather_stations"
@@ -77,19 +82,22 @@ object Snippets {
         run {
             val tableSpec = "clouddataflow-readonly:samples.weather_stations"
             // [START BigQueryReadTable]
-            val maxTemperatures = p.apply(BigQueryIO.readTableRows().from(tableSpec))
+            val maxTemperatures = pipeline.apply(BigQueryIO.readTableRows().from(tableSpec))
                     // Each row is of type TableRow
                     .apply<PCollection<Double>>(
                             MapElements.into(TypeDescriptors.doubles())
-                                    .via(SerializableFunction<TableRow, Double> { input -> input?.get("max_temperature") as Double }))
+                                    .via(SerializableFunction<TableRow, Double> {
+                                        it["max_temperature"] as Double
+                                    })
+                    )
             // [END BigQueryReadTable]
         }
 
         run {
             val tableSpec = "clouddataflow-readonly:samples.weather_stations"
             // [START BigQueryReadFunction]
-            val maxTemperatures = p.apply(
-                    BigQueryIO.read { elem: SchemaAndRecord -> elem.record.get("max_temperature") as Double }
+            val maxTemperatures = pipeline.apply(
+                    BigQueryIO.read { it.record["max_temperature"] as Double }
                             .from(tableSpec)
                             .withCoder(DoubleCoder.of()))
             // [END BigQueryReadFunction]
@@ -97,8 +105,8 @@ object Snippets {
 
         run {
             // [START BigQueryReadQuery]
-            val maxTemperatures = p.apply(
-                    BigQueryIO.read { elem: SchemaAndRecord -> elem.record.get("max_temperature") as Double }
+            val maxTemperatures = pipeline.apply(
+                    BigQueryIO.read { it.record["max_temperature"] as Double }
                             .fromQuery(
                                     "SELECT max_temperature FROM [clouddataflow-readonly:samples.weather_stations]")
                             .withCoder(DoubleCoder.of()))
@@ -107,8 +115,8 @@ object Snippets {
 
         run {
             // [START BigQueryReadQueryStdSQL]
-            val maxTemperatures = p.apply(
-                    BigQueryIO.read { elem: SchemaAndRecord -> elem.record.get("max_temperature") as Double }
+            val maxTemperatures = pipeline.apply(
+                    BigQueryIO.read { it.record["max_temperature"] as Double }
                             .fromQuery(
                                     "SELECT max_temperature FROM `clouddataflow-readonly.samples.weather_stations`")
                             .usingStandardSql()
@@ -164,7 +172,7 @@ object Snippets {
              */
 
 
-            val quotes = p.apply(
+            val quotes = pipeline.apply(
                     Create.of(
                             Quote("Mahatma Gandhi", "My life is my message."),
                             Quote("Yoda", "Do, or do not. There is no 'try'.")))
@@ -174,7 +182,7 @@ object Snippets {
             quotes
                     .apply<PCollection<TableRow>>(
                             MapElements.into(TypeDescriptor.of(TableRow::class.java))
-                                    .via(SerializableFunction<Quote, TableRow> { input -> TableRow().set("source", input.source).set("quote", input.quote) }))
+                                    .via(SerializableFunction<Quote, TableRow> { TableRow().set("source", it.source).set("quote", it.quote) }))
                     .apply<WriteResult>(
                             BigQueryIO.writeTableRows()
                                     .to(tableSpec)
@@ -188,7 +196,7 @@ object Snippets {
                     BigQueryIO.write<Quote>()
                             .to(tableSpec)
                             .withSchema(tableSchema)
-                            .withFormatFunction { elem: Quote -> TableRow().set("source", elem.source).set("quote", elem.quote) }
+                            .withFormatFunction { TableRow().set("source", it.source).set("quote", it.quote) }
                             .withCreateDisposition(CreateDisposition.CREATE_IF_NEEDED)
                             .withWriteDisposition(WriteDisposition.WRITE_TRUNCATE))
             // [END BigQueryWriteFunction]
@@ -198,7 +206,7 @@ object Snippets {
                     BigQueryIO.write<Quote>()
                             .to(tableSpec)
                             .withJsonSchema(tableSchemaJson)
-                            .withFormatFunction { elem: Quote -> TableRow().set("source", elem.source).set("quote", elem.quote) }
+                            .withFormatFunction { TableRow().set("source", it.source).set("quote", it.quote) }
                             .withCreateDisposition(CreateDisposition.CREATE_IF_NEEDED)
                             .withWriteDisposition(WriteDisposition.WRITE_TRUNCATE))
             // [END BigQueryWriteJsonSchema]
@@ -216,9 +224,9 @@ object Snippets {
                     val maxTemp: Double = 0.0
             )
             */
-            val weatherData = p.apply(
-                    BigQueryIO.read { elem: SchemaAndRecord ->
-                        val record = elem.record
+            val weatherData = pipeline.apply(
+                    BigQueryIO.read {
+                        val record = it.record
                         WeatherData(
                                 record.get("year") as Long,
                                 record.get("month") as Long,
@@ -272,12 +280,12 @@ object Snippets {
                                                                             .setMode("NULLABLE")))
                                         }
                                     })
-                            .withFormatFunction { elem: WeatherData ->
+                            .withFormatFunction {
                                 TableRow()
-                                        .set("year", elem.year)
-                                        .set("month", elem.month)
-                                        .set("day", elem.day)
-                                        .set("maxTemp", elem.maxTemp)
+                                        .set("year", it.year)
+                                        .set("month", it.month)
+                                        .set("day", it.day)
+                                        .set("maxTemp", it.maxTemp)
                             }
                             .withCreateDisposition(CreateDisposition.CREATE_IF_NEEDED)
                             .withWriteDisposition(WriteDisposition.WRITE_TRUNCATE))
@@ -285,7 +293,7 @@ object Snippets {
 
             var tableSpec = "clouddataflow-readonly:samples.weather_stations"
             if (writeProject.isNotEmpty() && writeDataset.isNotEmpty() && writeTable.isNotEmpty()) {
-                tableSpec = writeProject + ":" + writeDataset + "." + writeTable + "_partitioning"
+                tableSpec = "$writeProject:$writeDataset.${writeTable}_partitioning"
             }
 
             val tableSchema = TableSchema()
@@ -307,12 +315,12 @@ object Snippets {
                     BigQueryIO.write<WeatherData>()
                             .to("${tableSpec}_partitioning")
                             .withSchema(tableSchema)
-                            .withFormatFunction { elem: WeatherData ->
+                            .withFormatFunction {
                                 TableRow()
-                                        .set("year", elem.year)
-                                        .set("month", elem.month)
-                                        .set("day", elem.day)
-                                        .set("maxTemp", elem.maxTemp)
+                                        .set("year", it.year)
+                                        .set("month", it.month)
+                                        .set("day", it.day)
+                                        .set("maxTemp", it.maxTemp)
                             }
                             // NOTE: an existing table without time partitioning set up will not work
                             .withTimePartitioning(TimePartitioning().setType("DAY"))
