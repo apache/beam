156358a6c562bd57cf060148ade362ee836a34c1
mf2199 <mf2199@columbia.edu>
2019-05-01 19:01:24 -0400

    Changed import package name

--- sdks/python/apache_beam/io/gcp/bigtableio_it_test.py
+++ sdks/python/apache_beam/io/gcp/bigtableio_it_test.py
@@ -36,7 +36,7 @@ from apache_beam.transforms.combiners import Count
 
 try:
   from google.cloud.bigtable import enums, row, column_family, Client
-  import beam_bigtable as bigtableio
+  import bigtableio
 except ImportError:
   Client = None
   bigtableio = None
