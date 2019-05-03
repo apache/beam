7e8c477f2f4fdf9ca888cdd6bb4cffe59df1fab3
mf2199 <mf2199@columbia.edu>
2019-05-02 21:52:50 -0400

    REVERT BACK

--- sdks/python/apache_beam/io/gcp/bigtableio.py
+++ sdks/python/apache_beam/io/gcp/bigtableio.py
@@ -37,24 +37,16 @@ those generated rows in the table.
 """
 from __future__ import absolute_import
 
-# from collections import namedtuple
-# from random import shuffle
-
 import apache_beam as beam
-# from apache_beam.io import iobase
-# from apache_beam.io.range_trackers import LexicographicKeyRangeTracker
 from apache_beam.metrics import Metrics
 from apache_beam.transforms.display import DisplayDataItem
 
 try:
   from google.cloud.bigtable import Client
-  # from google.cloud.bigtable.row_set import RowRange
-  # from google.cloud.bigtable.row_set import RowSet
 except ImportError:
-  Client = None
+  pass
 
 __all__ = ['WriteToBigTable']
-# __all__ = ['BigtableSource', 'WriteToBigTable']
 
 
 class _BigTableWriteFn(beam.DoFn):
@@ -149,129 +141,3 @@ class WriteToBigTable(beam.PTransform):
             | beam.ParDo(_BigTableWriteFn(beam_options['project_id'],
                                           beam_options['instance_id'],
                                           beam_options['table_id'])))
-
-#
-# class BigtableSource(iobase.BoundedSource):
-#   def __init__(self, project_id, instance_id, table_id, filter_=None):
-#     """ Constructor of the Read connector of Bigtable
-#
-#     Args:
-#       project_id: [string] GCP Project of to write the Rows
-#       instance_id: [string] GCP Instance to write the Rows
-#       table_id: [string] GCP Table to write the `DirectRows`
-#       filter_: [RowFilter] Filter to apply to columns in a row.
-#     """
-#     super(self.__class__, self).__init__()
-#     self._init({'project_id': project_id,
-#                 'instance_id': instance_id,
-#                 'table_id': table_id,
-#                 'filter_': filter_})
-#
-#   def __getstate__(self):
-#     return self.beam_options
-#
-#   def __setstate__(self, options):
-#     self._init(options)
-#
-#   def _init(self, options):
-#     self.beam_options = options
-#     self.table = None
-#     self.sample_row_keys = None
-#     self.row_count = Metrics.counter(self.__class__.__name__, 'Row count')
-#
-#   def _get_table(self):
-#     if self.table is None:
-#       self.table = Client(project=self.beam_options['project_id'])\
-#                       .instance(self.beam_options['instance_id'])\
-#                       .table(self.beam_options['table_id'])
-#     return self.table
-#
-#   def get_sample_row_keys(self):
-#     """ Get a sample of row keys in the table.
-#
-#     The returned row keys will delimit contiguous sections of the table of
-#     approximately equal size, which can be used to break up the data for
-#     distributed tasks like mapreduces.
-#     :returns: A cancel-able iterator. Can be consumed by calling ``next()``
-#     			  or by casting to a :class:`list` and can be cancelled by
-#     			  calling ``cancel()``.
-#
-#     ***** NOTE: For unclear reasons, the function returns generator even
-#     after wrapping the result as a list. In order to be used as a list, the
-#     result should be wrapped as a list AGAIN! E.g., see 'estimate_size()'
-#     """
-#     if self.sample_row_keys is None:
-#       self.sample_row_keys = list(self._get_table().sample_row_keys())
-#     return self.sample_row_keys
-#
-#   def get_range_tracker(self, start_position=b'', stop_position=b''):
-#     if stop_position == b'':
-#       return LexicographicKeyRangeTracker(start_position)
-#     else:
-#       return LexicographicKeyRangeTracker(start_position, stop_position)
-#
-#   def estimate_size(self):
-#     return list(self.get_sample_row_keys())[-1].offset_bytes
-#
-#   def split(self, desired_bundle_size, start_position=None, stop_position=None):
-#     """ Splits the source into a set of bundles, using the row_set if it is set.
-#
-#     *** At this point, only splitting an entire table into samples based on the sample row keys is supported ***
-#
-#     :param desired_bundle_size: the desired size (in bytes) of the bundles returned.
-#     :param start_position: if specified, the position must be used as the starting position of the first bundle.
-#     :param stop_position: if specified, the position must be used as the ending position of the last bundle.
-#     Returns:
-#     	an iterator of objects of type 'SourceBundle' that gives information about the generated bundles.
-#     """
-#
-#     if start_position is not None or stop_position is not None:
-#       raise NotImplementedError
-#
-#     # TODO: Use the desired bundle size to split accordingly
-#     # TODO: Allow users to provide their own row sets
-#
-#     sample_row_keys = list(self.get_sample_row_keys())
-#
-#     if len(sample_row_keys) > 1 and sample_row_keys[0].row_key != b'':
-#       SampleRowKey = namedtuple("SampleRowKey", "row_key offset_bytes")
-#       first_key = SampleRowKey(b'', 0)
-#       sample_row_keys.insert(0, first_key)
-#       sample_row_keys = list(sample_row_keys)
-#
-#     bundles = []
-#     for i in range(1, len(sample_row_keys)):
-#       key_1 = sample_row_keys[i - 1].row_key
-#       key_2 = sample_row_keys[i].row_key
-#       size = sample_row_keys[i].offset_bytes - sample_row_keys[i - 1].offset_bytes
-#       bundles.append(iobase.SourceBundle(size, self, key_1, key_2))
-#
-#     # Shuffle is needed to allow reading from different locations of the table for better efficiency
-#     shuffle(bundles)
-#     return bundles
-#
-#   def read(self, range_tracker):
-#     for row in self._get_table().read_rows(start_key=range_tracker.start_position(),
-#                                            end_key=range_tracker.stop_position(),
-#                                            filter_=self.beam_options['filter_']):
-#       if range_tracker.try_claim(row.row_key):
-#         self.row_count.inc()
-#         yield row
-#       else:
-#         # TODO: Modify the client ot be able to cancel read_row request
-#         break
-#
-#   def display_data(self):
-#     return {'projectId': DisplayDataItem(self.beam_options['project_id'],
-#                                          label='Bigtable Project Id',
-#                                          key='projectId'),
-#             'instanceId': DisplayDataItem(self.beam_options['instance_id'],
-#                                           label='Bigtable Instance Id',
-#                                           key='instanceId'),
-#             'tableId': DisplayDataItem(self.beam_options['table_id'],
-#                                        label='Bigtable Table Id',
-#                                        key='tableId'),
-#             'filter_': DisplayDataItem(self.beam_options['filter_'],
-#                                        label='Bigtable Filter',
-#                                        key='filter_')
-#             }
