b752d6ab0cf993a807611b5a67f7728fc9389d5f
Reuven Lax <relax@relax-macbookpro3.roam.corp.google.com>
2019-05-03 13:20:52 -0700

    fix imports

--- sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/transforms/AddFields.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/transforms/AddFields.java
@@ -22,7 +22,11 @@ import static org.apache.beam.vendor.guava.v20_0.com.google.common.base.Precondi
 
 import com.google.auto.value.AutoValue;
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import org.apache.beam.sdk.schemas.FieldAccessDescriptor;
