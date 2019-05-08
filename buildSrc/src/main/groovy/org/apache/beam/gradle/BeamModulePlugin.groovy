6a45d90cda5b83d9f4f552da85b9fb521a7469b4
akedin <kedin@google.com>
2019-05-07 17:29:33 -0700

    [SQL] Upgrade DataCatalog client to 0.4.0-alpha

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -368,7 +368,7 @@ class BeamModulePlugin implements Plugin<Project> {
     def cassandra_driver_version = "3.6.0"
     def generated_grpc_beta_version = "0.44.0"
     def generated_grpc_ga_version = "1.43.0"
-    def generated_grpc_dc_beta_version = "0.1.0-alpha"
+    def generated_grpc_dc_beta_version = "0.4.0-alpha"
     def google_auth_version = "0.12.0"
     def google_clients_version = "1.27.0"
     def google_cloud_bigdataoss_version = "1.9.16"
