238659bce8043e6a64619a959ab44453dbe22dff
Henry Suryawirawan <hsuryawirawan@google.com>
2019-05-15 04:35:00 +0800

    Merge pull request #8358: Beam Kata for Java and Python SDKs

---
+++ learning/katas/java/Introduction/Hello Beam/Hello Beam/test/TaskTest.java
@@ -0,0 +1,41 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import java.io.Serializable;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TaskTest implements Serializable {
+
+  @Rule
+  public final transient TestPipeline testPipeline = TestPipeline.create();
+
+  @Test
+  public void helloWorld() {
+    PCollection<String> results = Task.setupPipeline(testPipeline);
+
+    PAssert.that(results)
+        .containsInAnyOrder("Hello Beam");
+
+    testPipeline.run().waitUntilFinish();
+  }
+
+}
