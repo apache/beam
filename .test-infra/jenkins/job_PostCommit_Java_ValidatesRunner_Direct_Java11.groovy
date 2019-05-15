ff66a66d99490afa2f4ba01dc84d8ee73da5333e
Michal Walenia <michal.walenia@polidea.com>
2019-05-15 08:17:47 +0200

    [BEAM-7287] Change JAVA_HOME to match new Jenkins agents

--- .test-infra/jenkins/job_PostCommit_Java_ValidatesRunner_Direct_Java11.groovy
+++ .test-infra/jenkins/job_PostCommit_Java_ValidatesRunner_Direct_Java11.groovy
@@ -27,8 +27,8 @@ PostcommitJobBuilder.postCommitJob('beam_PostCommit_Java11_ValidatesRunner_Direc
 
     description('Builds the Direct Runner with Java 8 and runs ValidatesRunner test suite in Java 11.')
 
-    def JAVA_11_HOME = '/home/jenkins/tools/java/latest11'
-    def JAVA_8_HOME = '/home/jenkins/tools/java/latest1.8'
+    def JAVA_11_HOME = '/usr/lib/jvm/java-11-openjdk-amd64'
+    def JAVA_8_HOME = '/usr/lib/jvm/java-8-openjdk-amd64'
     commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 180)
 
     publishers {
