7b3b347c4271a1569c651ab1451e6980c3f19df9
Hannah Jiang <hannahjiang@google.com>
2019-05-20 15:27:59 -0700

    BEAM-6693 move display_data() to ApproximateUniqueCombineFn class, add upper bound to mmh3 and reorder import by alphabet order

--- sdks/python/apache_beam/transforms/__init__.py
+++ sdks/python/apache_beam/transforms/__init__.py
@@ -25,8 +25,8 @@ from apache_beam.transforms.core import *
 from apache_beam.transforms.external import *
 from apache_beam.transforms.ptransform import *
 from apache_beam.transforms.timeutil import TimeDomain
-from apache_beam.transforms.util import *
 from apache_beam.transforms.stats import *
+from apache_beam.transforms.util import *
 
 # No backwards compatibility guarantees.
 try:
