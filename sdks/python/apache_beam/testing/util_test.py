c37db33b6e43d5717aaf56d2b17b4eafea797c58
Robert Bradshaw <robertwb@gmail.com>
2019-06-03 10:48:36 +0200

    Fix import ordering.

--- sdks/python/apache_beam/testing/util_test.py
+++ sdks/python/apache_beam/testing/util_test.py
@@ -23,8 +23,8 @@ import unittest
 
 from apache_beam import Create
 from apache_beam.testing.test_pipeline import TestPipeline
-from apache_beam.testing.util import TestWindowedValue
 from apache_beam.testing.util import BeamAssertException
+from apache_beam.testing.util import TestWindowedValue
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 from apache_beam.testing.util import is_empty
