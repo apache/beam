247f04e73705efe3d522eb91772caa04b32ce0dd
Pablo <pabloem@users.noreply.github.com>
2019-06-12 10:55:49 -0700

    Removing unused logging.

--- sdks/python/apache_beam/runners/portability/fn_api_runner_transforms.py
+++ sdks/python/apache_beam/runners/portability/fn_api_runner_transforms.py
@@ -1013,9 +1013,6 @@ def greedily_fuse(stages, pipeline_context):
       for output in transform.outputs.values():
         producers_by_pcoll[output] = stage
 
-  #logging.debug('consumers\n%s', consumers_by_pcoll)
-  #logging.debug('producers\n%s', producers_by_pcoll)
-
   # Now try to fuse away all pcollections.
   for pcoll, producer in producers_by_pcoll.items():
     write_pcoll = None
