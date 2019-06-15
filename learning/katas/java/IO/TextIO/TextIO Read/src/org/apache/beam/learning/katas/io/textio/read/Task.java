7e1f1e8c5cef000a10833911bb358f0a82c9c2ee
Henry Suryawirawan <hsuryawirawan@google.com>
2019-06-15 22:49:27 +0800

    Rename 'util' package to 'org.apache.beam.learning.katas.util'

--- learning/katas/java/IO/TextIO/TextIO Read/src/org/apache/beam/learning/katas/io/textio/read/Task.java
+++ learning/katas/java/IO/TextIO/TextIO Read/src/org/apache/beam/learning/katas/io/textio/read/Task.java
@@ -20,13 +20,13 @@ package org.apache.beam.learning.katas.io.textio.read;
 
 import static org.apache.beam.sdk.values.TypeDescriptors.strings;
 
+import org.apache.beam.learning.katas.util.Log;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
-import util.Log;
 
 public class Task {
 
