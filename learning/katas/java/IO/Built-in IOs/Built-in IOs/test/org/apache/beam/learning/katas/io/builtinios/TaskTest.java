ec8c72c7923e29d5c51277fe4b0c2a649ec17486
Henry Suryawirawan <hsuryawirawan@google.com>
2019-06-16 14:01:59 +0700

    Move Built-in IOs Task into a package

--- learning/katas/java/IO/Built-in IOs/Built-in IOs/test/org/apache/beam/learning/katas/io/builtinios/TaskTest.java
+++ learning/katas/java/IO/Built-in IOs/Built-in IOs/test/org/apache/beam/learning/katas/io/builtinios/TaskTest.java
@@ -16,19 +16,7 @@
  * limitations under the License.
  */
 
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+package org.apache.beam.learning.katas.io.builtinios;
 
-public class Task {
-
-  public static void main(String[] args) {
-    PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
-    Pipeline pipeline = Pipeline.create(options);
-
-
-
-    pipeline.run();
-  }
-
+public class TaskTest {
 }
