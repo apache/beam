710f52bda57f531a0ed448aa411375e3c39dc2bf
Hannah Jiang <hannahjiang@google.com>
2019-06-26 17:59:59 -0700

    BEAM-7548 add retries to all tests

--- sdks/python/apache_beam/transforms/stats_test.py
+++ sdks/python/apache_beam/transforms/stats_test.py
@@ -35,7 +35,10 @@ from apache_beam.testing.util import equal_to
 
 
 class ApproximateUniqueTest(unittest.TestCase):
-  """Unit tests for ApproximateUnique.Globally and ApproximateUnique.PerKey."""
+  """Unit tests for ApproximateUnique.Globally and ApproximateUnique.PerKey.
+  Hash() with Python3 is nondeterministic, so Approximation algorithm generates
+  different result each time and sometimes error rate is out of range, so add
+  retries for all tests who actually running approximation algorithm."""
 
   def test_approximate_unique_global_by_invalid_size(self):
     # test if the transformation throws an error as expected with an invalid
@@ -179,6 +182,7 @@ class ApproximateUniqueTest(unittest.TestCase):
                 label='assert:global_by_size')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_global_by_sample_size_with_duplicates(self):
     # test if estimation error with a given sample size is not greater than
     # expected max error with duplicated input.
@@ -200,6 +204,7 @@ class ApproximateUniqueTest(unittest.TestCase):
                 label='assert:global_by_size_with_duplicates')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_global_by_sample_size_with_small_population(self):
     # test if estimation is exactly same to actual value when sample size is
     # not smaller than population size (sample size > 100% of population).
@@ -242,6 +247,7 @@ class ApproximateUniqueTest(unittest.TestCase):
     assert_that(result, equal_to([True]), label='assert:global_by_error')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_global_by_error_with_small_population(self):
     # test if estimation error from input error of a small dataset is not
     # greater than input error. Sample size is always not smaller than 16, so
@@ -262,6 +268,7 @@ class ApproximateUniqueTest(unittest.TestCase):
                 label='assert:global_by_error_with_small_population')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_perkey_by_size(self):
     # test if est error per key from sample size is in a expected range.
     sample_size = 20
@@ -292,6 +299,7 @@ class ApproximateUniqueTest(unittest.TestCase):
                 label='assert:perkey_by_size')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_perkey_by_error(self):
     # test if estimation error per key from input err is in the expected range.
     est_err = 0.01
@@ -317,6 +325,7 @@ class ApproximateUniqueTest(unittest.TestCase):
                 label='assert:perkey_by_error')
     pipeline.run()
 
+  @retry(reraise=True, stop=stop_after_attempt(3))
   def test_approximate_unique_globally_by_error_with_skewed_data(self):
     # test if estimation error is within the expected range with skewed data.
     est_err = 0.01
