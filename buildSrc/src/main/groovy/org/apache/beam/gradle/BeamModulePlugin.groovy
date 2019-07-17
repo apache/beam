a76236e50efd691606f0daeec5025f1b0d6bf3eb
Udi Meiri <udim@users.noreply.github.com>
2019-07-17 13:52:36 -0700

    [BEAM-7641] Collect xunit statistics for Py ITs (#8952)
    Affects only ITs run using run_integration_test.sh.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1848,6 +1848,7 @@ class BeamModulePlugin implements Plugin<Project> {
               argMap["pipeline_opts"] = config.pipelineOptions
             if (config.kmsKeyName)
               argMap["kms_key_name"] = config.kmsKeyName
+            argMap["suite"] = "integrationTest-perf"
 
             def cmdArgs = project.mapToArgString(argMap)
             def runScriptsDir = "${pythonRootDir}/scripts"
