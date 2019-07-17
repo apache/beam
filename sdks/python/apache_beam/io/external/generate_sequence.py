7925feaa244ead37d319ca0003f6b209cb949a89
Chad Dombrova <chadrik@gmail.com>
2019-07-17 00:48:15 -0700

    Create a more user friendly external transform API
    Standardize and reduce boilerplate

--- sdks/python/apache_beam/io/external/generate_sequence.py
+++ sdks/python/apache_beam/io/external/generate_sequence.py
@@ -17,15 +17,11 @@
 
 from __future__ import absolute_import
 
-from apache_beam import ExternalTransform
-from apache_beam import pvalue
-from apache_beam.coders import VarIntCoder
-from apache_beam.portability.api.external_transforms_pb2 import ConfigValue
-from apache_beam.portability.api.external_transforms_pb2 import ExternalConfigurationPayload
-from apache_beam.transforms import ptransform
+from apache_beam.transforms.external import ExternalTransform
+from apache_beam.transforms.external import ImplicitSchemaPayloadBuilder
 
 
-class GenerateSequence(ptransform.PTransform):
+class GenerateSequence(ExternalTransform):
   """
     An external PTransform which provides a bounded or unbounded stream of
     integers.
@@ -49,47 +45,19 @@ class GenerateSequence(ptransform.PTransform):
 
     Experimental; no backwards compatibility guarantees.
   """
-
   URN = 'beam:external:java:generate_sequence:v1'
 
   def __init__(self, start, stop=None,
                elements_per_period=None, max_read_time=None,
-               expansion_service='localhost:8097'):
-    super(GenerateSequence, self).__init__()
-    self.start = start
-    self.stop = stop
-    self.elements_per_period = elements_per_period
-    self.max_read_time = max_read_time
-    self.expansion_service = expansion_service
-
-  def expand(self, pbegin):
-    if not isinstance(pbegin, pvalue.PBegin):
-      raise Exception("GenerateSequence must be a root transform")
-
-    coder = VarIntCoder()
-    coder_urn = ['beam:coder:varint:v1']
-    args = {
-        'start':
-        ConfigValue(
-            coder_urn=coder_urn,
-            payload=coder.encode(self.start))
-    }
-    if self.stop:
-      args['stop'] = ConfigValue(
-          coder_urn=coder_urn,
-          payload=coder.encode(self.stop))
-    if self.elements_per_period:
-      args['elements_per_period'] = ConfigValue(
-          coder_urn=coder_urn,
-          payload=coder.encode(self.elements_per_period))
-    if self.max_read_time:
-      args['max_read_time'] = ConfigValue(
-          coder_urn=coder_urn,
-          payload=coder.encode(self.max_read_time))
-
-    payload = ExternalConfigurationPayload(configuration=args)
-    return pbegin.apply(
-        ExternalTransform(
-            self.URN,
-            payload.SerializeToString(),
-            self.expansion_service))
+               expansion_service=None):
+    super(GenerateSequence, self).__init__(
+        self.URN,
+        ImplicitSchemaPayloadBuilder(
+            {
+                'start': start,
+                'stop': stop,
+                'elements_per_period': elements_per_period,
+                'max_read_time': max_read_time,
+            }
+        ),
+        expansion_service)
