a6e69c397ae45bb57dbeeffe8795d8303492bd9f
Muhammad Talha <talha.jamil@venturedive.com>
2019-07-24 15:27:16 +0500

    BEAM-6855: run spotless apply

--- runners/core-java/src/test/java/org/apache/beam/runners/core/SimplePushbackSideInputDoFnRunnerTest.java
+++ runners/core-java/src/test/java/org/apache/beam/runners/core/SimplePushbackSideInputDoFnRunnerTest.java
@@ -73,21 +73,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.emptyIterable;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-
 /** Tests for {@link SimplePushbackSideInputDoFnRunner}. */
 @RunWith(JUnit4.class)
 public class SimplePushbackSideInputDoFnRunnerTest {
@@ -148,7 +133,7 @@ public class SimplePushbackSideInputDoFnRunnerTest {
   }
 
   private SimplePushbackSideInputDoFnRunner<Integer, Integer> createRunner(
-          ImmutableList<PCollectionView<?>> views) {
+      ImmutableList<PCollectionView<?>> views) {
     SimplePushbackSideInputDoFnRunner<Integer, Integer> runner =
         SimplePushbackSideInputDoFnRunner.create(underlying, views, reader);
     runner.startBundle();
@@ -509,6 +494,6 @@ public class SimplePushbackSideInputDoFnRunnerTest {
         Collections.emptyMap(),
         WINDOWING_STRATEGY,
         DoFnSchemaInformation.create(),
-        Collections.emptyMap());
+  Collections.emptyMap());
   }
 }
