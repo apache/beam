5504aa76e142e6dcf5f2e7d65f80e29b610adce1
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-08-07 18:52:45 -0400

    [BEAM-7918] adding nested row implementation for unnest and uncollect (#9288)
    * adding nested row implementation for unnest and uncollect. Includes tests for both

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamUnnestRelTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamUnnestRelTest.java
@@ -20,59 +20,22 @@ package org.apache.beam.sdk.extensions.sql.impl.rel;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.beam.sdk.extensions.sql.TestUtils;
-import org.apache.beam.sdk.extensions.sql.impl.planner.NodeStats;
 import org.apache.beam.sdk.extensions.sql.meta.provider.test.TestBoundedTable;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.Row;
-import org.apache.calcite.rel.RelNode;
-import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
-/** Tests for {@code BeamUncollectRel}. */
-public class BeamUncollectRelTest extends BaseRelTest {
-
+/** Test for {@code BeamUnnestRel}. */
+public class BeamUnnestRelTest extends BaseRelTest {
   @Rule public final TestPipeline pipeline = TestPipeline.create();
 
-  private NodeStats getEstimateOf(String sql) {
-    RelNode root = env.parseQuery(sql);
-
-    while (!(root instanceof BeamUncollectRel)) {
-      root = root.getInput(0);
-    }
-
-    return BeamSqlRelUtils.getNodeStats(root, root.getCluster().getMetadataQuery());
-  }
-
-  @Test
-  public void testNodeStats() {
-    NodeStats estimate =
-        getEstimateOf(
-            "SELECT * FROM UNNEST (SELECT * FROM (VALUES (ARRAY ['a', 'b', 'c']),(ARRAY ['a', 'b', 'c']))) t1");
-
-    Assert.assertEquals(4d, estimate.getRowCount(), 0.001);
-    Assert.assertEquals(4d, estimate.getWindow(), 0.001);
-    Assert.assertEquals(0., estimate.getRate(), 0.001);
-  }
-
-  @Test
-  public void testUncollectPrimitive() {
-    String sql = "SELECT * FROM unnest(ARRAY [1, 2, 3])";
-
-    PCollection<Row> rows = compilePipeline(sql, pipeline);
-    PAssert.that(rows)
-        .containsInAnyOrder(
-            TestUtils.RowsBuilder.of(Schema.FieldType.INT32, "intField")
-                .addRows(1, 2, 3)
-                .getRows());
-    pipeline.run();
-  }
-
-  @Test
-  public void testUncollectNested() {
+  @BeforeClass
+  public static void prepare() {
     Schema rowSchema =
         Schema.builder().addStringField("stringField").addInt32Field("intField").build();
     List<Row> nestedRows =
@@ -87,16 +50,21 @@ public class BeamUncollectRelTest extends BaseRelTest {
                 Schema.FieldType.array(Schema.FieldType.row(rowSchema)),
                 "nested")
             .addRows("1", nestedRows));
+  }
 
-    String sql = "SELECT intField, stringField FROM unnest(SELECT nested from NESTED)";
+  @Test
+  public void testUnnest() {
+    String sql =
+        "SELECT user_id, p.intField, p.stringField FROM NESTED as t, unnest(t.nested) as p";
 
     PCollection<Row> rows = compilePipeline(sql, pipeline);
     PAssert.that(rows)
         .containsInAnyOrder(
             TestUtils.RowsBuilder.of(
+                    Schema.FieldType.STRING, "user_id",
                     Schema.FieldType.INT32, "intField",
                     Schema.FieldType.STRING, "stringField")
-                .addRows(1, "test1", 2, "test2")
+                .addRows("1", 1, "test1", "1", 2, "test2")
                 .getRows());
     pipeline.run();
   }
