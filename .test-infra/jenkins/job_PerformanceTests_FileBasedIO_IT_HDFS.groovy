b2cd084845b92e20152c091d8157308ceead64ea
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2019-08-13 12:27:40 +0200

    [BEAM-7772] Refactor: rename consistently with job_PerformanceTests_FileBasedIO_IT.groovy

--- .test-infra/jenkins/job_PerformanceTests_FileBasedIO_IT_HDFS.groovy
+++ .test-infra/jenkins/job_PerformanceTests_FileBasedIO_IT_HDFS.groovy
@@ -16,17 +16,16 @@
  * limitations under the License.
  */
 
-import CommonJobProperties as commonJobProperties
+import CommonJobProperties as comon
 import Kubernetes
 
-def testsConfigurations = [
+def jobs = [
         [
-                jobName           : 'beam_PerformanceTests_TextIOIT_HDFS',
-                jobDescription    : 'Runs PerfKit tests for TextIOIT on HDFS',
-                itClass           : 'org.apache.beam.sdk.io.text.TextIOIT',
-                bqTable           : 'beam_performance.textioit_hdfs_pkb_results',
-                prCommitStatusName: 'Java TextIO Performance Test on HDFS',
-                prTriggerPhase    : 'Run Java TextIO Performance Test HDFS',
+                name           : 'beam_PerformanceTests_TextIOIT_HDFS',
+                description    : 'Runs PerfKit tests for TextIOIT on HDFS',
+                test           : 'org.apache.beam.sdk.io.text.TextIOIT',
+                githubTitle: 'Java TextIO Performance Test on HDFS',
+                githubTriggerPhrase    : 'Run Java TextIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'textioit_hdfs_results',
@@ -35,12 +34,11 @@ def testsConfigurations = [
 
         ],
         [
-                jobName            : 'beam_PerformanceTests_Compressed_TextIOIT_HDFS',
-                jobDescription     : 'Runs PerfKit tests for TextIOIT with GZIP compression on HDFS',
-                itClass            : 'org.apache.beam.sdk.io.text.TextIOIT',
-                bqTable            : 'beam_performance.compressed_textioit_hdfs_pkb_results',
-                prCommitStatusName : 'Java CompressedTextIO Performance Test on HDFS',
-                prTriggerPhase     : 'Run Java CompressedTextIO Performance Test HDFS',
+                name            : 'beam_PerformanceTests_Compressed_TextIOIT_HDFS',
+                description     : 'Runs PerfKit tests for TextIOIT with GZIP compression on HDFS',
+                test            : 'org.apache.beam.sdk.io.text.TextIOIT',
+                githubTitle : 'Java CompressedTextIO Performance Test on HDFS',
+                githubTriggerPhrase     : 'Run Java CompressedTextIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'compressed_textioit_hdfs_results',
@@ -49,12 +47,11 @@ def testsConfigurations = [
                 ]
         ],
         [
-                jobName           : 'beam_PerformanceTests_ManyFiles_TextIOIT_HDFS',
-                jobDescription    : 'Runs PerfKit tests for TextIOIT with many output files on HDFS',
-                itClass           : 'org.apache.beam.sdk.io.text.TextIOIT',
-                bqTable           : 'beam_performance.many_files_textioit_hdfs_pkb_results',
-                prCommitStatusName: 'Java ManyFilesTextIO Performance Test on HDFS',
-                prTriggerPhase    : 'Run Java ManyFilesTextIO Performance Test HDFS',
+                name           : 'beam_PerformanceTests_ManyFiles_TextIOIT_HDFS',
+                description    : 'Runs PerfKit tests for TextIOIT with many output files on HDFS',
+                test           : 'org.apache.beam.sdk.io.text.TextIOIT',
+                githubTitle: 'Java ManyFilesTextIO Performance Test on HDFS',
+                githubTriggerPhrase    : 'Run Java ManyFilesTextIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'many_files_textioit_hdfs_results',
@@ -66,12 +63,11 @@ def testsConfigurations = [
 
         ],
         [
-                jobName           : 'beam_PerformanceTests_AvroIOIT_HDFS',
-                jobDescription    : 'Runs PerfKit tests for AvroIOIT on HDFS',
-                itClass           : 'org.apache.beam.sdk.io.avro.AvroIOIT',
-                bqTable           : 'beam_performance.avroioit_hdfs_pkb_results',
-                prCommitStatusName: 'Java AvroIO Performance Test on HDFS',
-                prTriggerPhase    : 'Run Java AvroIO Performance Test HDFS',
+                name           : 'beam_PerformanceTests_AvroIOIT_HDFS',
+                description    : 'Runs PerfKit tests for AvroIOIT on HDFS',
+                test           : 'org.apache.beam.sdk.io.avro.AvroIOIT',
+                githubTitle: 'Java AvroIO Performance Test on HDFS',
+                githubTriggerPhrase    : 'Run Java AvroIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'avroioit_hdfs_results',
@@ -81,23 +77,21 @@ def testsConfigurations = [
 // TODO(BEAM-3945) TFRecord performance test is failing only when running on hdfs.
 // We need to fix this before enabling this job on jenkins.
 //        [
-//                jobName           : 'beam_PerformanceTests_TFRecordIOIT_HDFS',
-//                jobDescription    : 'Runs PerfKit tests for beam_PerformanceTests_TFRecordIOIT on HDFS',
-//                itClass           : 'org.apache.beam.sdk.io.tfrecord.TFRecordIOIT',
-//                bqTable           : 'beam_performance.tfrecordioit_hdfs_pkb_results',
-//                prCommitStatusName: 'Java TFRecordIO Performance Test on HDFS',
-//                prTriggerPhase    : 'Run Java TFRecordIO Performance Test HDFS',
+//                name           : 'beam_PerformanceTests_TFRecordIOIT_HDFS',
+//                description    : 'Runs PerfKit tests for beam_PerformanceTests_TFRecordIOIT on HDFS',
+//                test           : 'org.apache.beam.sdk.io.tfrecord.TFRecordIOIT',
+//                githubTitle: 'Java TFRecordIO Performance Test on HDFS',
+//                githubTriggerPhrase    : 'Run Java TFRecordIO Performance Test HDFS',
 //                pipelineOptions: [
 //                        numberOfRecords: '1000000'
 //                ]
 //        ],
         [
-                jobName           : 'beam_PerformanceTests_XmlIOIT_HDFS',
-                jobDescription    : 'Runs PerfKit tests for beam_PerformanceTests_XmlIOIT on HDFS',
-                itClass           : 'org.apache.beam.sdk.io.xml.XmlIOIT',
-                bqTable           : 'beam_performance.xmlioit_hdfs_pkb_results',
-                prCommitStatusName: 'Java XmlIOPerformance Test on HDFS',
-                prTriggerPhase    : 'Run Java XmlIO Performance Test HDFS',
+                name           : 'beam_PerformanceTests_XmlIOIT_HDFS',
+                description    : 'Runs PerfKit tests for beam_PerformanceTests_XmlIOIT on HDFS',
+                test           : 'org.apache.beam.sdk.io.xml.XmlIOIT',
+                githubTitle: 'Java XmlIOPerformance Test on HDFS',
+                githubTriggerPhrase    : 'Run Java XmlIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'xmlioit_hdfs_results',
@@ -106,12 +100,11 @@ def testsConfigurations = [
                 ]
         ],
         [
-                jobName           : 'beam_PerformanceTests_ParquetIOIT_HDFS',
-                jobDescription    : 'Runs PerfKit tests for beam_PerformanceTests_ParquetIOIT on HDFS',
-                itClass           : 'org.apache.beam.sdk.io.parquet.ParquetIOIT',
-                bqTable           : 'beam_performance.parquetioit_hdfs_pkb_results',
-                prCommitStatusName: 'Java ParquetIOPerformance Test on HDFS',
-                prTriggerPhase    : 'Run Java ParquetIO Performance Test HDFS',
+                name           : 'beam_PerformanceTests_ParquetIOIT_HDFS',
+                description    : 'Runs PerfKit tests for beam_PerformanceTests_ParquetIOIT on HDFS',
+                test           : 'org.apache.beam.sdk.io.parquet.ParquetIOIT',
+                githubTitle: 'Java ParquetIOPerformance Test on HDFS',
+                githubTriggerPhrase    : 'Run Java ParquetIO Performance Test HDFS',
                 pipelineOptions: [
                         bigQueryDataset: 'beam_performance',
                         bigQueryTable: 'parquetioit_hdfs_results',
@@ -120,37 +113,23 @@ def testsConfigurations = [
         ]
 ]
 
-for (testConfiguration in testsConfigurations) {
-    create_filebasedio_performance_test_job(testConfiguration)
+for (job in jobs) {
+    createFileBasedIOITTestJob(job)
 }
 
+private void createFileBasedIOITTestJob(testJob) {
 
-private void create_filebasedio_performance_test_job(testConfiguration) {
+    job(testJob.name) {
+        description(testJob.description)
+        comon.setTopLevelMainJobProperties(delegate)
+        comon.enablePhraseTriggeringFromPullRequest(delegate, testJob.githubTitle, testJob.githubTriggerPhrase)
+        comon.setAutoJob(delegate, 'H */6 * * *')
 
-    // This job runs the file-based IOs performance tests on PerfKit Benchmarker.
-    job(testConfiguration.jobName) {
-        description(testConfiguration.jobDescription)
-
-        // Set default Beam job properties.
-        commonJobProperties.setTopLevelMainJobProperties(delegate)
-
-        // Allows triggering this build against pull requests.
-        commonJobProperties.enablePhraseTriggeringFromPullRequest(
-                delegate,
-                testConfiguration.prCommitStatusName,
-                testConfiguration.prTriggerPhase)
-
-        // Run job in postcommit every 6 hours, don't trigger every push, and
-        // don't email individual committers.
-        commonJobProperties.setAutoJob(
-                delegate,
-                'H */6 * * *')
-
-        String namespace = commonJobProperties.getKubernetesNamespace(testConfiguration.jobName)
-        String kubeconfig = commonJobProperties.getKubeconfigLocationForNamespace(namespace)
+        String namespace = comon.getKubernetesNamespace(testJob.name)
+        String kubeconfig = comon.getKubeconfigLocationForNamespace(namespace)
         Kubernetes k8s = Kubernetes.create(delegate, kubeconfig, namespace)
 
-        k8s.apply(commonJobProperties.makePathAbsolute("src/.test-infra/kubernetes/hadoop/LargeITCluster/hdfs-multi-datanode-cluster.yml"))
+        k8s.apply(comon.makePathAbsolute("src/.test-infra/kubernetes/hadoop/LargeITCluster/hdfs-multi-datanode-cluster.yml"))
         String hostName = "LOAD_BALANCER_IP"
         k8s.loadBalancerIP("hadoop", hostName)
 
@@ -162,20 +141,20 @@ private void create_filebasedio_performance_test_job(testConfiguration) {
                 filenamePrefix   : "hdfs://\$${hostName}:9000/TEXTIO_IT_"
         ]
 
-        Map allPipelineOptions = testConfiguration.pipelineOptions << additionalOptions
+        Map allPipelineOptions = testJob.pipelineOptions << additionalOptions
         String runner = "dataflow"
         String filesystem = "hdfs"
         String testTask = ":sdks:java:io:file-based-io-tests:integrationTest"
 
         steps {
           gradle {
-            rootBuildScriptDir(commonJobProperties.checkoutDir)
-            commonJobProperties.setGradleSwitches(delegate)
+            rootBuildScriptDir(comon.checkoutDir)
+            comon.setGradleSwitches(delegate)
             switches("--info")
-            switches("-DintegrationTestPipelineOptions=\'${commonJobProperties.joinPipelineOptions(allPipelineOptions)}\'")
+            switches("-DintegrationTestPipelineOptions=\'${comon.joinPipelineOptions(allPipelineOptions)}\'")
             switches("-Dfilesystem=\'${filesystem}\'")
             switches("-DintegrationTestRunner=\'${runner}\'")
-            tasks("${testTask} --tests ${testConfiguration.itClass}")
+            tasks("${testTask} --tests ${testJob.test}")
           }
         }
     }
