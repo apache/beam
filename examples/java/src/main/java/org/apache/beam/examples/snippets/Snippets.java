dcf0270ae9a253057a44a21cb982728d53cb05db
rarokni <rezarokni@google.com>
2019-08-22 14:31:45 +0800

    Add snippets for BEAM-8031
    [BEAM-8031] Revert python changes as Python does not yet support merg windows

--- examples/java/src/main/java/org/apache/beam/examples/snippets/Snippets.java
+++ examples/java/src/main/java/org/apache/beam/examples/snippets/Snippets.java
@@ -23,7 +23,9 @@ import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.api.services.bigquery.model.TimePartitioning;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -31,8 +33,10 @@ import java.util.Map;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.DefaultCoder;
 import org.apache.beam.sdk.coders.DoubleCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.Compression;
 import org.apache.beam.sdk.io.FileIO;
 import org.apache.beam.sdk.io.GenerateSequence;
@@ -61,11 +65,16 @@ import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
 import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.transforms.windowing.Repeatedly;
 import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.transforms.windowing.WindowFn;
+import org.apache.beam.sdk.transforms.windowing.WindowFn.MergeContext;
+import org.apache.beam.sdk.transforms.windowing.WindowMappingFn;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.sdk.values.TimestampedValue;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
@@ -667,4 +676,126 @@ public class Snippets {
   }
 
   // [END AccessingValueProviderInfoAfterRunSnip1]
+
+  public static Duration gapDuration;
+
+  // [START CustomSessionWindow1]
+
+  public Collection<IntervalWindow> assignWindows(WindowFn.AssignContext c) {
+
+    // Assign each element into a window from its timestamp until gapDuration in the
+    // future.  Overlapping windows (representing elements within gapDuration of
+    // each other) will be merged.
+    return Arrays.asList(new IntervalWindow(c.timestamp(), gapDuration));
+  }
+  // [END CustomSessionWindow1]
+
+  // [START CustomSessionWindow2]
+  public static class DynamicSessions extends WindowFn<Object, IntervalWindow> {
+    /** Duration of the gaps between sessions. */
+    private final Duration gapDuration;
+
+    /** Pub/Sub attribute that modifies session gap. */
+    private final String gapAttribute;
+
+    /** Creates a {@code DynamicSessions} {@link WindowFn} with the specified gap duration. */
+    private DynamicSessions(Duration gapDuration, String gapAttribute) {
+      this.gapDuration = gapDuration;
+      this.gapAttribute = gapAttribute;
+    }
+
+    // [END CustomSessionWindow2]
+
+    // [START CustomSessionWindow3]
+    @Override
+    public Collection<IntervalWindow> assignWindows(AssignContext c) {
+      // Assign each element into a window from its timestamp until gapDuration in the
+      // future.  Overlapping windows (representing elements within gapDuration of
+      // each other) will be merged.
+      Duration dataDrivenGap;
+      Pojo message = (Pojo) c.element();
+
+      try {
+        dataDrivenGap = Duration.standardSeconds(message.gapAttribute);
+      } catch (Exception e) {
+        dataDrivenGap = gapDuration;
+      }
+      return Arrays.asList(new IntervalWindow(c.timestamp(), dataDrivenGap));
+    }
+    // [END CustomSessionWindow3]
+
+    // [START CustomSessionWindow4]
+    /** Creates a {@code DynamicSessions} {@link WindowFn} with the specified gap duration. */
+    public static DynamicSessions withDefaultGapDuration(Duration gapDuration) {
+      return new DynamicSessions(gapDuration, "");
+    }
+
+    public DynamicSessions withGapAttribute(String gapAttribute) {
+      return new DynamicSessions(gapDuration, gapAttribute);
+    }
+
+    // [START CustomSessionWindow4]
+
+    public static class Pojo {
+      Long gapAttribute;
+    }
+
+    @Override
+    public void mergeWindows(MergeContext c) throws Exception {}
+
+    @Override
+    public boolean isCompatible(WindowFn<?, ?> other) {
+      return false;
+    }
+
+    @Override
+    public Coder<IntervalWindow> windowCoder() {
+      return null;
+    }
+
+    @Override
+    public WindowMappingFn<IntervalWindow> getDefaultWindowMappingFn() {
+      return null;
+    }
+  }
+
+  public static class CustomSessionPipeline {
+
+    public static void main(String args[]) {
+
+      // [START CustomSessionWindow5]
+
+      PCollection<String> p =
+          Pipeline.create()
+              .apply(
+                  "Create data",
+                  Create.timestamped(
+                          TimestampedValue.of(
+                              "{\"user\":\"link\",\"score\":\"12\",\"gap\":\"5\"}", new Instant()),
+                          TimestampedValue.of(
+                              "{\"user\":\"bowser\",\"score\":\"4\"}", new Instant()),
+                          TimestampedValue.of(
+                              "{\"user\":\"link\",\"score\":\"-3\",\"gap\":\"5\"}",
+                              new Instant().plus(2000)),
+                          TimestampedValue.of(
+                              "{\"user\":\"link\",\"score\":\"2\",\"gap\":\"5\"}",
+                              new Instant().plus(9000)),
+                          TimestampedValue.of(
+                              "{\"user\":\"link\",\"score\":\"7\",\"gap\":\"5\"}",
+                              new Instant().plus(12000)),
+                          TimestampedValue.of(
+                              "{\"user\":\"bowser\",\"score\":\"10\"}", new Instant().plus(12000)))
+                      .withCoder(StringUtf8Coder.of()));
+      // [END CustomSessionWindow5]
+
+      // [START CustomSessionWindow6]
+      p.apply(
+          "Window into sessions",
+          Window.into(
+              DynamicSessions.withDefaultGapDuration(Duration.standardSeconds(10))
+                  .withGapAttribute("gap")));
+      // [END CustomSessionWindow6]
+
+    }
+  }
 }
