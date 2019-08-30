609aaceef4562e61c146685f7c2f60e55e805e2b
Etienne Chauchot <echauchot@apache.org>
2019-08-30 17:34:13 +0200

    [BEAM-8470] Add a simple spark native test to test Beam coders wrapping into Spark Encoders

---
+++ runners/spark/src/test/java/org/apache/beam/runners/spark/structuredstreaming/utils/EncodersTest.java
@@ -0,0 +1,29 @@
+package org.apache.beam.runners.spark.structuredstreaming.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.beam.runners.spark.structuredstreaming.translation.helpers.EncoderHelpers;
+import org.apache.beam.sdk.coders.VarIntCoder;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+/**
+ * Test of the wrapping of Beam Coders as Spark ExpressionEncoders.
+ */
+public class EncodersTest {
+
+  @Test
+  public void beamCoderToSparkEncoderTest() {
+    SparkSession sparkSession = SparkSession.builder().appName("beamCoderToSparkEncoderTest")
+        .master("local[4]").getOrCreate();
+    List<Integer> data = new ArrayList<>();
+    data.add(1);
+    data.add(2);
+    data.add(3);
+//    sparkSession.createDataset(data, EncoderHelpers.fromBeamCoder(VarIntCoder.of()));
+    sparkSession.createDataset(data, EncoderHelpers.genericEncoder());
+  }
+}
