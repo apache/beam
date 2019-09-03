7be43ec92e2058e758594b0d4617931fea0ee1f2
David Cavazos <dcavazos@google.com>
2019-09-03 11:11:41 -0700

    Use unicode_literals to make it Python 2 compatible

--- sdks/python/apache_beam/examples/snippets/transforms/element_wise/pardo.py
+++ sdks/python/apache_beam/examples/snippets/transforms/element_wise/pardo.py
@@ -18,6 +18,7 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import unicode_literals
 
 
 def pardo_dofn(test=None):
--- sdks/python/apache_beam/examples/snippets/transforms/element_wise/pardo_test.py
+++ sdks/python/apache_beam/examples/snippets/transforms/element_wise/pardo_test.py
@@ -18,6 +18,7 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import io
 import platform
