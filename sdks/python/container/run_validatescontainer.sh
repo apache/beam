b3f372e12c81eb8120e33aa9705589106f24c144
Hannah Jiang <hannahjiang@google.com>
2019-09-04 14:15:12 -0700

    [BEAM-7909] fix python container image name

--- sdks/python/container/run_validatescontainer.sh
+++ sdks/python/container/run_validatescontainer.sh
@@ -47,15 +47,25 @@ PROJECT=${PROJECT:-apache-beam-testing}
 
 # Other variables branched by Python version.
 if [[ $1 == "python2" ]]; then
-  IMAGE_NAME="python"       # Use this to create CONTAINER_IMAGE variable.
-  CONTAINER_PROJECT="sdks:python:container"  # Use this to build container by Gradle.
+  IMAGE_NAME="python2.7"       # Use this to create CONTAINER_IMAGE variable.
+  CONTAINER_PROJECT="sdks:python:container:py2"  # Use this to build container by Gradle.
   GRADLE_PY3_FLAG=""        # Use this in Gradle command.
   PY_INTERPRETER="python"   # Use this in virtualenv command.
-elif [[ $1 == "python3" ]]; then
-  IMAGE_NAME="python3"          # Use this to create CONTAINER_IMAGE variable.
-  CONTAINER_PROJECT="sdks:python:container:py3"  # Use this to build container by Gradle.
+elif [[ $1 == "python35" ]]; then
+  IMAGE_NAME="python3.5"          # Use this to create CONTAINER_IMAGE variable.
+  CONTAINER_PROJECT="sdks:python:container:py35"  # Use this to build container by Gradle.
   GRADLE_PY3_FLAG="-Ppython3"   # Use this in Gradle command.
   PY_INTERPRETER="python3.5"    # Use this in virtualenv command.
+elif [[ $1 == "python36" ]]; then
+  IMAGE_NAME="python3.6"          # Use this to create CONTAINER_IMAGE variable.
+  CONTAINER_PROJECT="sdks:python:container:py36"  # Use this to build container by Gradle.
+  GRADLE_PY3_FLAG="-Ppython3"   # Use this in Gradle command.
+  PY_INTERPRETER="python3.6"    # Use this in virtualenv command.
+elif [[ $1 == "python37" ]]; then
+  IMAGE_NAME="python3.7"          # Use this to create CONTAINER_IMAGE variable.
+  CONTAINER_PROJECT="sdks:python:container:py37"  # Use this to build container by Gradle.
+  GRADLE_PY3_FLAG="-Ppython3"   # Use this in Gradle command.
+  PY_INTERPRETER="python3.7"    # Use this in virtualenv command.
 else
   echo "Must set Python version with 'python2' or 'python3' from commandline."
   exit 1
