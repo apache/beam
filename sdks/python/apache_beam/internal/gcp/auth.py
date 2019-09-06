3c3d59c46f7ec22498d6799a9693236bf623b8d0
Ahmet Altay <aaltay@gmail.com>
2019-09-06 09:55:37 -0700

    [BEAM-7616] apitools use urllib with the global timeout. Set it to 60 seconds # to prevent network related stuckness issues. (#9401)
    * [BEAM-7616] apitools use urllib with the global timeout. Set it to 60 seconds # to prevent network related stuckness issues.

--- sdks/python/apache_beam/internal/gcp/auth.py
+++ sdks/python/apache_beam/internal/gcp/auth.py
@@ -20,6 +20,7 @@
 from __future__ import absolute_import
 
 import logging
+import socket
 import threading
 
 from oauth2client.client import GoogleCredentials
@@ -96,6 +97,14 @@ class _Credentials(object):
     with cls._credentials_lock:
       if cls._credentials_init:
         return cls._credentials
+
+      # apitools use urllib with the global timeout. Set it to 60 seconds
+      # to prevent network related stuckness issues.
+      if not socket.getdefaulttimeout():
+        logging.info("Setting socket default timeout to 60 seconds.")
+        socket.setdefaulttimeout(60)
+      logging.info(
+          "socket default timeout is % seconds.", socket.getdefaulttimeout())
 
       cls._credentials = cls._get_service_credentials()
       cls._credentials_init = True
