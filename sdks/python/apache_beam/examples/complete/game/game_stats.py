91ac568b3f9815bd44f37cc5072c644356018e47
Valentyn Tymofieiev <valentyn@google.com>
2019-09-09 19:25:38 -0700

    Temporarily replace super() calls with calls to __init__ of parent class in Beam examples.

--- sdks/python/apache_beam/examples/complete/game/game_stats.py
+++ sdks/python/apache_beam/examples/complete/game/game_stats.py
@@ -106,7 +106,9 @@ class ParseGameEventFn(beam.DoFn):
   The human-readable time string is not used here.
   """
   def __init__(self):
-    super(ParseGameEventFn, self).__init__()
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+    # super(ParseGameEventFn, self).__init__()
+    beam.DoFn.__init__(self)
     self.num_parse_errors = Metrics.counter(self.__class__, 'num_parse_errors')
 
   def process(self, elem):
@@ -130,7 +132,9 @@ class ExtractAndSumScore(beam.PTransform):
   extracted.
   """
   def __init__(self, field):
-    super(ExtractAndSumScore, self).__init__()
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+    # super(ExtractAndSumScore, self).__init__()
+    beam.PTransform.__init__(self)
     self.field = field
 
   def expand(self, pcoll):
@@ -167,7 +171,9 @@ class WriteToBigQuery(beam.PTransform):
       schema: Dictionary in the format {'column_name': 'bigquery_type'}
       project: Name of the Cloud project containing BigQuery table.
     """
-    super(WriteToBigQuery, self).__init__()
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+    # super(WriteToBigQuery, self).__init__()
+    beam.PTransform.__init__(self)
     self.table_name = table_name
     self.dataset = dataset
     self.schema = schema
