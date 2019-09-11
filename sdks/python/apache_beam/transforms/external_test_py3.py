e470c4244e01df0f106ca8954af5783db8cd982f
Chad Dombrova <chadrik@gmail.com>
2019-09-11 11:16:37 -0700

    Split dataclass-based external transform tests to run in python 3.7 only

--- sdks/python/apache_beam/transforms/external_test_py3.py
+++ sdks/python/apache_beam/transforms/external_test_py3.py
@@ -26,55 +26,13 @@ import apache_beam as beam
 from apache_beam import typehints
 from apache_beam.portability.api.external_transforms_pb2 import ExternalConfigurationPayload
 from apache_beam.transforms.external import AnnotationBasedPayloadBuilder
-from apache_beam.transforms.external import DataclassBasedPayloadBuilder
 from apache_beam.transforms.external_test import PayloadBase
-
-# pylint: disable=wrong-import-order, wrong-import-position
-try:
-  import dataclasses
-except ImportError:
-  dataclasses = None
-# pylint: enable=wrong-import-order, wrong-import-position
 
 
 def get_payload(cls):
   payload = ExternalConfigurationPayload()
   payload.ParseFromString(cls._payload)
   return payload
-
-
-@unittest.skipIf(dataclasses is None, 'dataclasses library not available')
-class ExternalDataclassesPayloadTest(PayloadBase, unittest.TestCase):
-
-  def get_payload_from_typing_hints(self, values):
-
-    @dataclasses.dataclass
-    class DataclassTransform(beam.ExternalTransform):
-      URN = 'beam:external:fakeurn:v1'
-
-      integer_example: int
-      string_example: str
-      list_of_strings: typing.List[str]
-      optional_kv: typing.Optional[typing.Tuple[str, float]] = None
-      optional_integer: typing.Optional[int] = None
-      expansion_service: dataclasses.InitVar[typing.Optional[str]] = None
-
-    return get_payload(DataclassTransform(**values))
-
-  def get_payload_from_beam_typehints(self, values):
-
-    @dataclasses.dataclass
-    class DataclassTransform(beam.ExternalTransform):
-      URN = 'beam:external:fakeurn:v1'
-
-      integer_example: int
-      string_example: str
-      list_of_strings: typehints.List[str]
-      optional_kv: typehints.Optional[typehints.KV[str, float]] = None
-      optional_integer: typehints.Optional[int] = None
-      expansion_service: dataclasses.InitVar[typehints.Optional[str]] = None
-
-    return get_payload(DataclassTransform(**values))
 
 
 class ExternalAnnotationPayloadTest(PayloadBase, unittest.TestCase):
