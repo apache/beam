19ba386625f0d498e3e0e247a83d29fa6ad4b55b
Shoaib <shoaib.zafaf@venturedive.com>
2019-09-12 21:22:56 +0500

    wip: write

--- sdks/python/apache_beam/io/gcp/spannerio.py
+++ sdks/python/apache_beam/io/gcp/spannerio.py
@@ -17,6 +17,8 @@
 
 
 from __future__ import absolute_import
+import collections
+import time
 
 import apache_beam as beam
 from apache_beam import pvalue
@@ -26,12 +28,108 @@ from apache_beam.transforms import PTransform
 from google.cloud.spanner import Client
 from google.cloud.spanner import KeySet
 from google.cloud.spanner_v1.database import BatchSnapshot
+from google.cloud.spanner_v1 import batch
 from google.cloud.spanner_v1.database import SnapshotCheckout
 
-__all__ = ['WriteToSpanner', 'NewReadFromSpanner',
-           'ReadOperation']
+
+__all__ = ['WriteToSpanner', 'ReadFromSpanner',
+           'ReadOperation', "WriteMutation", "BatchFn"]
 
 T = typehints.TypeVariable('T')
+
+
+# from google.cloud.spanner_v1.proto import mutation_pb2
+# from google.cloud.spanner_v1.proto.mutation_pb2 import Mutation
+from google.cloud.spanner_v1.types import Mutation
+
+
+
+
+class MutationSizeEstimator:
+
+  def __init__(self, m):
+    self._m = m
+
+  @staticmethod
+  def get_operation(m):
+    pass
+
+
+class WriteMutation:
+
+  @staticmethod
+  def insert(table, columns, values):
+    """Insert one or more new table rows.
+
+    :type table: str
+    :param table: Name of the table to be modified.
+
+    :type columns: list of str
+    :param columns: Name of the table columns to be modified.
+
+    :type values: list of lists
+    :param values: Values to be modified.
+    """
+    return Mutation(insert=batch._make_write_pb(table, columns, values))
+
+
+  @staticmethod
+  def update(table, columns, values):
+    """Update one or more existing table rows.
+
+    :type table: str
+    :param table: Name of the table to be modified.
+
+    :type columns: list of str
+    :param columns: Name of the table columns to be modified.
+
+    :type values: list of lists
+    :param values: Values to be modified.
+    """
+    return Mutation(update=batch._make_write_pb(table, columns, values))
+
+  @staticmethod
+  def insert_or_update(table, columns, values):
+    """Insert/update one or more table rows.
+
+    :type table: str
+    :param table: Name of the table to be modified.
+
+    :type columns: list of str
+    :param columns: Name of the table columns to be modified.
+
+    :type values: list of lists
+    :param values: Values to be modified.
+    """
+    return Mutation(insert_or_update=batch._make_write_pb(table, columns, values))
+
+  @staticmethod
+  def replace(table, columns, values):
+    """Replace one or more table rows.
+
+    :type table: str
+    :param table: Name of the table to be modified.
+
+    :type columns: list of str
+    :param columns: Name of the table columns to be modified.
+
+    :type values: list of lists
+    :param values: Values to be modified.
+    """
+    return Mutation(replace=batch._make_write_pb(table, columns, values))
+
+  @staticmethod
+  def delete(self, table, keyset):
+    """Delete one or more table rows.
+
+    :type table: str
+    :param table: Name of the table to be modified.
+
+    :type keyset: :class:`~google.cloud.spanner_v1.keyset.Keyset`
+    :param keyset: Keys/ranges identifying rows to delete.
+    """
+    delete = Mutation.Delete(table=table, key_set=keyset._to_pb())
+    return Mutation(delete=delete)
 
 
 class WriteToSpanner(object):
@@ -44,30 +142,119 @@ class WriteToSpanner(object):
   def insert(self):
     return _Insert(self._project_id, self._instance_id, self._database_id)
 
+  def batch(self):
+    pass
 
-@typehints.with_input_types(
-  typehints.Tuple[str, typehints.List[str],
-                  typehints.List[typehints.Tuple[T, ...]]])
-class _Insert(PTransform):
-  def __init__(self, project_id, instance_id, databse_id):
+
+
+class _BatchWrite(PTransform):
+
+  def __init__(self, project_id, instance_id, database_id):
     self._project_id = project_id
     self._instance_id = instance_id
-    self._database_id = databse_id
+    self._database_id = database_id
 
   def expand(self, pcoll):
-    spanner_client = Client(self._project_id)
-    instance = spanner_client.instance(self._instance_id)
-    database = instance.database(self._database_id)
-    return pcoll | beam.ParDo(BatchFn())
+    return pcoll | beam.ParDo(_WriteToSpanner(
+        self._project_id, self._instance_id, self._database_id
+    ))
 
 
-class BatchFn(beam.DoFn):
+
+# @typehints.with_input_types(
+#   typehints.Tuple[str, typehints.List[str],
+#                   typehints.List[typehints.Tuple[T, ...]]])
+class _Insert(PTransform):
+
+  def __init__(self, project_id, instance_id, database_id):
+    self._project_id = project_id
+    self._instance_id = instance_id
+    self._database_id = database_id
+
+  def expand(self, pcoll):
+
+    """
+    SpannerIO.java:914
+    // First, read the Cloud Spanner schema.
+
+    // Build a set of Mutation groups from the current bundle,
+      // sort them by table/key then split into batches.
+
+    // Merge the batchable and unbatchable mutations and write to Spanner.
+
+
+    """
+    return pcoll | beam.ParDo(_WriteToSpanner(
+        self._project_id, self._instance_id, self._database_id
+    ))
+
+
+
+class MutationSizeEstimator:
 
   def __init__(self):
     pass
 
+  @staticmethod
+  def get_operation(m):
+    ops = ('insert', 'insert_or_update', 'replace', 'update', 'delete')
+    for op in ops:
+      if getattr(m, op).table is not None:
+        return op
+    return ValueError("Operation is not defined!")
+
+
+
+class BatchFn(beam.DoFn):
+
+  def __init__(self, max_batch_size_bytes, max_num_mutations, schema_view):
+    self._max_batch_size_bytes = max_batch_size_bytes
+    self._max_num_mutations = max_num_mutations
+    self._schema_view = schema_view
+
   def process(self, element):
+    batch_size_bytes = 0
+    batch_cells = 0
+
+    for mg in element:
+      group_size = 0
+      print(mg.operaion)
+      group_cell = 0
+
+      x = 1
+    return element
+
+class _WriteToSpanner(beam.DoFn):
+
+  def __init__(self, project_id, instance_id, database_id):
+    self._project_id = project_id
+    self._instance_id = instance_id
+    self._database_id = database_id
+    self._db_instance = None
+
+  def to_runner_api_parameter(self, unused_context):
     pass
+
+  def setup(self):
+    spanner_client = Client(self._project_id)
+    instance = spanner_client.instance(self._instance_id)
+    self._db_instance = instance.database(self._database_id)
+
+
+  def process(self, element):
+    with self._db_instance.batch() as b:
+      b._mutations.extend(element)
+
+    return element
+
+  # def process(self, element):
+  #   _id = int(time.time())
+  #   def _process(transaction):
+  #     sql = "INSERT roles (key, rolename) VALUES ({}, 'insert-role-{}')".format(_id, _id)
+  #     transaction.execute_update(sql)
+  #   self._db_instance.run_in_transaction(_process)
+  #   return element
+
 
 
 
@@ -90,10 +277,6 @@ class BatchFn(beam.DoFn):
 # WriteToSpanner -> insert -> InsertTransform -> Group Mutations -> ParDO
 
 
-
-import collections
-import datetime
-
 class ReadOperation(collections.namedtuple("ReadOperation",
                                            "read_operation batch_action "
                                            "transaction_action kwargs")):
@@ -101,11 +284,11 @@ class ReadOperation(collections.namedtuple("ReadOperation",
   __slots__ = ()
 
   @classmethod
-  def with_query(cls, sql):
+  def with_query(cls, sql, params=None, param_types=None):
     return cls(
         read_operation="process_query_batch",
         batch_action="generate_query_batches", transaction_action="execute_sql",
-        kwargs={'sql': sql}
+        kwargs={'sql': sql, 'params': params, 'param_types': param_types}
     )
 
   @classmethod
@@ -119,7 +302,7 @@ class ReadOperation(collections.namedtuple("ReadOperation",
     )
 
 
-class NewReadFromSpanner(object):
+class ReadFromSpanner(object):
 
   def __init__(self, project_id, instance_id, database_id, snapshot_options=None):
     self._project_id = project_id
@@ -129,8 +312,8 @@ class NewReadFromSpanner(object):
     self._timestamp = None
     self._snapshot_options = snapshot_options
 
-  def with_query(self, sql):
-    read_operation = [ReadOperation.with_query(sql)]
+  def with_query(self, sql, params=None, param_types=None):
+    read_operation = [ReadOperation.with_query(sql, params, param_types)]
     return self.read_all(read_operation)
 
   def with_table(self, table, columns, index="", keyset=None):
@@ -166,23 +349,32 @@ class NewReadFromSpanner(object):
 
 class _NaiveSpannerReadDoFn(beam.DoFn):
 
-  def __init__(self, project_id, instance_id, database_id):
+  def __init__(self, project_id, instance_id, database_id, snapshot_dict):
     self._project_id = project_id
     self._instance_id = instance_id
     self._database_id = database_id
     self._snapshot = None
-    print("--- RUNNING _TransactionReadDoFn ---")
+    self._snapshot_dict = snapshot_dict
 
+  def to_runner_api_parameter(self, unused_context):
+    pass
 
-  def process(self, element, mapping):
+  def setup(self):
     spanner_client = Client(self._project_id)
     instance = spanner_client.instance(self._instance_id)
     database = instance.database(self._database_id)
-    self._snapshot = BatchSnapshot.from_dict(database, mapping)
+    self._snapshot = BatchSnapshot.from_dict(database, self._snapshot_dict)
 
-
+  def process(self, element):
     # for row in getattr(self._snapshot, element.transaction_action)(**element.kwargs):
     #   yield row
+
+    # transaction = self._snapshot._get_session().transaction()
+    # transaction.begin()
+    # for row in getattr(transaction, element.transaction_action)(
+    #     **element.kwargs):
+    #   yield row
+    # transaction.commit()
 
     with self._snapshot._get_session().transaction() as transaction:
       for row in getattr(transaction, element.transaction_action)(
@@ -191,7 +383,6 @@ class _NaiveSpannerReadDoFn(beam.DoFn):
 
   def teardown(self):
     if self._snapshot:
-      print("=== CLOSE ===")
       self._snapshot.close()
 
 
@@ -205,16 +396,15 @@ class _NaiveSpannerRead(PTransform):
     self._instance_id = instance_id
     self._database_id = database_id
     self._sql = ""  #todo: remove this
-    self._i = 0
 
 
   def expand(self, pbegin):
     return (pbegin
-            | 'Generate Partitions' >> beam.Create(self._read_operations)
-            # | 'Reshuffle' >> beam.Reshuffle()
-            | 'Read From Partitions' >> beam.ParDo(
+            | 'Add Read Operations' >> beam.Create(self._read_operations)
+            | 'Reshuffle' >> beam.Reshuffle()
+            | 'Perform Read' >> beam.ParDo(
             _NaiveSpannerReadDoFn(self._project_id, self._instance_id,
-                                 self._database_id), self._transaction)
+                                 self._database_id, self._transaction))
             )
 
 
@@ -224,14 +414,9 @@ class _BatchRead(PTransform):
     self._project_id = project_id
     self._instance_id = instance_id
     self._database_id = database_id
-    self._sql = ""  #todo: remove this
     self._read_operations = read_operations
     self._snapshot_options = snapshot_options or {}
-    self._i = 0
 
-  # @property
-  # def read_operation(self):
-  #   return 'process_query_batch'
 
   def expand(self, pbegin):
     if not isinstance(pbegin, pvalue.PBegin):
@@ -253,27 +438,30 @@ class _BatchRead(PTransform):
             | 'Reshuffle' >> beam.Reshuffle()
             | 'Read From Partitions' >> beam.ParDo(
             _ReadFromPartitionFn(self._project_id, self._instance_id,
-                                 self._database_id), snapshot.to_dict())
+                                 self._database_id, snapshot.to_dict()))
             )
 
 
 class _ReadFromPartitionFn(beam.DoFn):
 
-  def __init__(self, project_id, instance_id, database_id, read_operation=None):
+  def __init__(self, project_id, instance_id, database_id, snapshot_dict):
     self._project_id = project_id
     self._instance_id = instance_id
     self._database_id = database_id
     self._snapshot = None
+    self._snapshot_dict = snapshot_dict
+    self._database = None
 
   def to_runner_api_parameter(self, unused_context):
     pass
 
-  def process(self, element, mapping):
+  def setup(self):
     spanner_client = Client(self._project_id)
     instance = spanner_client.instance(self._instance_id)
-    database = instance.database(self._database_id)
-    self._snapshot = BatchSnapshot.from_dict(database, mapping)
+    self._database = instance.database(self._database_id)
 
+  def process(self, element):
+    self._snapshot = BatchSnapshot.from_dict(self._database, self._snapshot_dict)
     read_operation = element['read_operation']
     elem = element['partitions']
 
@@ -282,5 +470,4 @@ class _ReadFromPartitionFn(beam.DoFn):
 
   def teardown(self):
     if self._snapshot:
-      print("=== CLOSE ===")
       self._snapshot.close()
--- sdks/python/apache_beam/io/gcp/spannerio_test.py
+++ sdks/python/apache_beam/io/gcp/spannerio_test.py
@@ -30,6 +30,9 @@ from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 
 from google.cloud.spanner_v1.database import SnapshotCheckout
+from google.cloud.spanner_v1.transaction import Transaction
+from google.cloud.spanner_v1 import param_types
+
 
 MAX_DB_NAME_LENGTH = 30
 
@@ -37,7 +40,7 @@ TEST_PROJECT_ID = 'apache-beam-testing'
 
 TEST_INSTANCE_ID = 'beam-test'
 
-TEST_DATABASE_PREFIX = 'raheel-testdb-'
+TEST_DATABASE_PREFIX = 'spanner-testdb-'
 
 FAKE_ROWS = [[1, 'Alice'], [2, 'Bob'], [3, 'Carl'], [4, 'Dan'], [5, 'Evan'],
              [6, 'Floyd']]
@@ -85,17 +88,63 @@ def pp(*args, **kwargs):
 
 import datetime
 
-class SpannerReadTest(unittest.TestCase):
+
+_spanner_key = 0
+def _m(v):
+  global _spanner_key
+  _spanner_key += 1
+  return WriteMutation.insert("roles", ("key", "rolename"), [(_spanner_key, v)])
+
+class SpannerWriteTest(unittest.TestCase):
+  TEST_INSTANCE_ID = 'testingdb-shoaib-vd'
+  DB_NAME = "testdb1"  #from gcp https://console.cloud.google.com/spanner/instances/testingdb-shoaib-vd/details/databases?project=apache-beam-testing
+
+
+  def test_read_actual(self):
+    m = [
+        [
+            WriteMutation.insert("roles", ("key", "rolename"), [('1233', "mutations-inset-1233")]),
+            WriteMutation.insert("roles", ("key", "rolename"), [('1234', "mutations-inset-1234")]),
+            WriteMutation.update("roles", ("key", "rolename"), [('1234', "mutations-inset-1233-updated")]),
+        ]
+    ]
+    with TestPipeline() as p:
+      r = p | beam.Create(m) | WriteToSpanner(TEST_PROJECT_ID,
+                                                              self.TEST_INSTANCE_ID,
+                                                              self.DB_NAME).insert()
+
+  def test_batch_size(self):
+    mutation_groups = [
+        [_m(1)],
+        [_m(4)],
+        [_m(5), _m(6), _m(7), _m(8), _m(9)],
+        [_m(3)],
+        [_m(10)],
+        [_m(11)],
+        [_m(2)],
+    ]
+    testfn = BatchFn(8 * 3, 1000, None)
+
+    with TestPipeline() as p:
+      r = p | beam.Create(mutation_groups) | beam.ParDo(testfn)
+
+
+
+
+
+class SpannerReadTestAction(unittest.TestCase):
   TEST_INSTANCE_ID = 'testingdb-shoaib-vd'
   DB_NAME = "testdb1"  #from gcp https://console.cloud.google.com/spanner/instances/testingdb-shoaib-vd/details/databases?project=apache-beam-testing
 
   def test_read_actual(self):
     pipeline = TestPipeline()
     records = pipeline \
-              | NewReadFromSpanner(
+              | ReadFromSpanner(
         TEST_PROJECT_ID,
         self.TEST_INSTANCE_ID,
-        self.DB_NAME).with_query('SELECT * FROM users where Key < 10') | beam.Map(pp)
+        self.DB_NAME).with_query('SELECT * FROM users where Key <= @daz',
+                                 {'ids':2}, {'ids': param_types.INT64}) \
+              | beam.Map(pp)
     pipeline.run()
 
   def test_read_all_actual(self):
@@ -109,7 +158,7 @@ class SpannerReadTest(unittest.TestCase):
         "exact_staleness": datetime.timedelta(seconds=10)
     }
     records = pipeline \
-              | NewReadFromSpanner(
+              | ReadFromSpanner(
         TEST_PROJECT_ID,
         self.TEST_INSTANCE_ID,
         self.DB_NAME, snapshot_options).read_all(reads) | beam.Map(pp)
@@ -126,17 +175,149 @@ class SpannerReadTest(unittest.TestCase):
     snapshot_options = {
         "exact_staleness": datetime.timedelta(seconds=10)
     }
-    transaction = NewReadFromSpanner.create_transaction(
+    transaction = ReadFromSpanner.create_transaction(
         TEST_PROJECT_ID,
         self.TEST_INSTANCE_ID,
         self.DB_NAME,
         snapshot_options=snapshot_options
     )
     records = pipeline \
-              | NewReadFromSpanner(
+              | ReadFromSpanner(
         TEST_PROJECT_ID,
         self.TEST_INSTANCE_ID,
         self.DB_NAME).with_transaction(transaction).read_all(reads) | beam.Map(pp)
     pipeline.run()
+
+  def test_with_query_actual_transaction(self):
+    pipeline = TestPipeline()
+
+    snapshot_options = {
+        "exact_staleness": datetime.timedelta(seconds=10)
+    }
+    transaction = ReadFromSpanner.create_transaction(
+        TEST_PROJECT_ID,
+        self.TEST_INSTANCE_ID,
+        self.DB_NAME,
+        snapshot_options=snapshot_options
+    )
+    records = pipeline \
+              | ReadFromSpanner(
+        TEST_PROJECT_ID,
+        self.TEST_INSTANCE_ID,
+        self.DB_NAME).with_transaction(transaction).with_query("SELECT * FROM users") | beam.Map(pp)
+    pipeline.run()
+
+
+
+class SpannerReadTest(unittest.TestCase):
+
+  @mock.patch('apache_beam.io.gcp.spannerio.Client')
+  @mock.patch('apache_beam.io.gcp.spannerio.BatchSnapshot')
+  def test_read_with_query_batch(self, mock_batch_snapshot_class,
+                                 mock_client_class):
+    mock_client = mock.MagicMock()
+    mock_instance = mock.MagicMock()
+    mock_database = mock.MagicMock()
+    mock_snapshot = mock.MagicMock()
+
+    mock_client_class.return_value = mock_client
+    mock_client.instance.return_value = mock_instance
+    mock_instance.database.return_value = mock_database
+    mock_database.batch_snapshot.return_value = mock_snapshot
+    mock_batch_snapshot_class.return_value = mock_snapshot
+    mock_batch_snapshot_class.from_dict.return_value = mock_snapshot
+
+    mock_snapshot.to_dict.return_value = dict()
+    mock_snapshot.generate_query_batches.return_value = \
+      [{'query': {'sql': 'SELECT * FROM users'},
+        'partition': 'test_partition'} for x in range(3)]
+    mock_snapshot.process_query_batch.side_effect = \
+      [FAKE_ROWS[0:2], FAKE_ROWS[2:4], FAKE_ROWS[4:]]
+
+    pipeline = TestPipeline()
+    records = pipeline \
+              | ReadFromSpanner(
+        TEST_PROJECT_ID,
+        TEST_INSTANCE_ID,
+        _generate_database_name()).with_query('SELECT * FROM users')
+    assert_that(records, equal_to(FAKE_ROWS))
+    pipeline.run()
+
+
+  @mock.patch('apache_beam.io.gcp.spannerio.Client')
+  @mock.patch('apache_beam.io.gcp.spannerio.BatchSnapshot')
+  def test_read_with_table_batch(self, mock_batch_snapshot_class,
+                                 mock_client_class):
+    mock_client = mock.MagicMock()
+    mock_instance = mock.MagicMock()
+    mock_database = mock.MagicMock()
+    mock_snapshot = mock.MagicMock()
+
+    mock_client_class.return_value = mock_client
+    mock_client.instance.return_value = mock_instance
+    mock_instance.database.return_value = mock_database
+    mock_database.batch_snapshot.return_value = mock_snapshot
+    mock_batch_snapshot_class.return_value = mock_snapshot
+    mock_batch_snapshot_class.from_dict.return_value = mock_snapshot
+
+    mock_snapshot.to_dict.return_value = dict()
+    mock_snapshot.generate_read_batches.return_value = \
+      [{
+          'read': {'table': 'users', 'keyset': {'all': True},
+                   'columns': ['Key', 'Value'], 'index': ''},
+          'partition': 'test_partition'} for x in range(3)]
+    mock_snapshot.process_read_batch.side_effect = \
+      [FAKE_ROWS[0:2], FAKE_ROWS[2:4], FAKE_ROWS[4:]]
+
+    pipeline = TestPipeline()
+    records = pipeline | ReadFromSpanner(
+        TEST_PROJECT_ID,
+        TEST_INSTANCE_ID,
+        _generate_database_name()).with_table('users', ['Key', 'Value'])
+    assert_that(records, equal_to(FAKE_ROWS))
+    pipeline.run()
+
+
+  @mock.patch('apache_beam.io.gcp.spannerio.Client')
+  @mock.patch('apache_beam.io.gcp.spannerio.BatchSnapshot')
+  def test_read_with_query_transaction(self, mock_batch_snapshot_class,
+                                       mock_client_class):
+
+    mock_client = mock.MagicMock()
+    mock_instance = mock.MagicMock()
+    mock_database = mock.MagicMock()
+    mock_snapshot = mock.MagicMock()
+
+    mock_client_class.return_value = mock_client
+    mock_client.instance.return_value = mock_instance
+    mock_instance.database.return_value = mock_database
+    mock_database.batch_snapshot.return_value = mock_snapshot
+    mock_batch_snapshot_class.return_value = mock_snapshot
+    mock_batch_snapshot_class.from_dict.return_value = mock_snapshot
+    mock_snapshot.to_dict.return_value = dict()
+
+    mock_snapshot._get_session.return_value = mock_session = mock.MagicMock()
+    mock_session.transaction.return_value = mock_transaction = mock.MagicMock()
+    mock_transaction.__enter__.return_value = mock_transaction_ctx = mock.MagicMock()
+    mock_transaction_ctx.execute_sql.return_value = FAKE_ROWS
+
+
+    snapshot_options = {
+        "exact_staleness": datetime.timedelta(seconds=10)
+    }
+    transaction = ReadFromSpanner.create_transaction(
+        TEST_PROJECT_ID, TEST_INSTANCE_ID, _generate_database_name(),
+        snapshot_options=snapshot_options
+    )
+
+    pipeline = TestPipeline()
+
+    records = (pipeline
+               | ReadFromSpanner(TEST_PROJECT_ID, TEST_INSTANCE_ID,
+                                 _generate_database_name())
+               .with_transaction(transaction).with_query('SELECT * FROM users'))
+    assert_that(records, equal_to(FAKE_ROWS))
+    pipeline.run()
+
 
 
