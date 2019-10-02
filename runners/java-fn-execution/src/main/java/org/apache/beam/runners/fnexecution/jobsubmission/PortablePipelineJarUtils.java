38a08cfea87c7ac5d62dd90ba128193c0ff881d1
Kyle Weaver <kcweaver@google.com>
2019-10-02 14:27:15 -0700

    [BEAM-8312] Flink jars retrieve artifacts from classloader

--- runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/jobsubmission/PortablePipelineJarUtils.java
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/jobsubmission/PortablePipelineJarUtils.java
@@ -17,35 +17,15 @@
  */
 package org.apache.beam.runners.fnexecution.jobsubmission;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
-import org.apache.beam.model.jobmanagement.v1.ArtifactApi.ProxyManifest;
-import org.apache.beam.model.jobmanagement.v1.ArtifactApi.ProxyManifest.Location;
 import org.apache.beam.model.pipeline.v1.RunnerApi.Pipeline;
-import org.apache.beam.runners.core.construction.ArtifactServiceStager;
-import org.apache.beam.runners.core.construction.ArtifactServiceStager.StagedFile;
-import org.apache.beam.runners.fnexecution.GrpcFnServer;
-import org.apache.beam.runners.fnexecution.InProcessServerFactory;
-import org.apache.beam.runners.fnexecution.artifact.BeamFileSystemArtifactStagingService;
-import org.apache.beam.sdk.fn.test.InProcessManagedChannelFactory;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.vendor.grpc.v1p21p0.com.google.protobuf.Message.Builder;
 import org.apache.beam.vendor.grpc.v1p21p0.com.google.protobuf.Struct;
 import org.apache.beam.vendor.grpc.v1p21p0.com.google.protobuf.util.JsonFormat;
-import org.apache.beam.vendor.grpc.v1p21p0.io.grpc.ManagedChannel;
-import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.MoreObjects;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.io.ByteStreams;
-import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -79,7 +59,7 @@ public abstract class PortablePipelineJarUtils {
   private static final String ARTIFACT_STAGING_FOLDER_PATH = "BEAM-ARTIFACT-STAGING";
   static final String ARTIFACT_FOLDER_PATH = ARTIFACT_STAGING_FOLDER_PATH + "/artifacts";
   private static final String PIPELINE_FOLDER_PATH = "BEAM-PIPELINE";
-  static final String ARTIFACT_MANIFEST_PATH =
+  public static final String ARTIFACT_MANIFEST_PATH =
       ARTIFACT_STAGING_FOLDER_PATH + "/artifact-manifest.json";
   static final String PIPELINE_PATH = PIPELINE_FOLDER_PATH + "/pipeline.json";
   static final String PIPELINE_OPTIONS_PATH = PIPELINE_FOLDER_PATH + "/pipeline-options.json";
@@ -87,7 +67,8 @@ public abstract class PortablePipelineJarUtils {
   private static final Logger LOG = LoggerFactory.getLogger(PortablePipelineJarCreator.class);
 
   private static InputStream getResourceFromClassPath(String resourcePath) throws IOException {
-    InputStream inputStream = PortablePipelineJarUtils.class.getResourceAsStream(resourcePath);
+    InputStream inputStream =
+        PortablePipelineJarUtils.class.getClassLoader().getResourceAsStream(resourcePath);
     if (inputStream == null) {
       throw new FileNotFoundException(
           String.format("Resource %s not found on classpath.", resourcePath));
@@ -105,80 +86,13 @@ public abstract class PortablePipelineJarUtils {
 
   public static Pipeline getPipelineFromClasspath() throws IOException {
     Pipeline.Builder builder = Pipeline.newBuilder();
-    parseJsonResource("/" + PIPELINE_PATH, builder);
+    parseJsonResource(PIPELINE_PATH, builder);
     return builder.build();
   }
 
   public static Struct getPipelineOptionsFromClasspath() throws IOException {
     Struct.Builder builder = Struct.newBuilder();
-    parseJsonResource("/" + PIPELINE_OPTIONS_PATH, builder);
+    parseJsonResource(PIPELINE_OPTIONS_PATH, builder);
     return builder.build();
-  }
-
-  public static ProxyManifest getArtifactManifestFromClassPath() throws IOException {
-    ProxyManifest.Builder builder = ProxyManifest.newBuilder();
-    parseJsonResource("/" + ARTIFACT_MANIFEST_PATH, builder);
-    return builder.build();
-  }
-
-  /** Writes artifacts listed in {@code proxyManifest}. */
-  public static String stageArtifacts(
-      ProxyManifest proxyManifest,
-      PipelineOptions options,
-      String invocationId,
-      String artifactStagingPath)
-      throws Exception {
-    Collection<StagedFile> filesToStage =
-        prepareArtifactsForStaging(proxyManifest, options, invocationId);
-    try (GrpcFnServer artifactServer =
-        GrpcFnServer.allocatePortAndCreateFor(
-            new BeamFileSystemArtifactStagingService(), InProcessServerFactory.create())) {
-      ManagedChannel grpcChannel =
-          InProcessManagedChannelFactory.create()
-              .forDescriptor(artifactServer.getApiServiceDescriptor());
-      ArtifactServiceStager stager = ArtifactServiceStager.overChannel(grpcChannel);
-      String stagingSessionToken =
-          BeamFileSystemArtifactStagingService.generateStagingSessionToken(
-              invocationId, artifactStagingPath);
-      String retrievalToken = stager.stage(stagingSessionToken, filesToStage);
-      // Clean up.
-      for (StagedFile file : filesToStage) {
-        if (!file.getFile().delete()) {
-          LOG.warn("Failed to delete file {}", file.getFile());
-        }
-      }
-      grpcChannel.shutdown();
-      return retrievalToken;
-    }
-  }
-
-  /**
-   * Artifacts are expected to exist as resources on the classpath, located using {@code
-   * proxyManifest}. Write them to tmp files so they can be staged.
-   */
-  private static Collection<StagedFile> prepareArtifactsForStaging(
-      ProxyManifest proxyManifest, PipelineOptions options, String invocationId)
-      throws IOException {
-    List<StagedFile> filesToStage = new ArrayList<>();
-    Path outputFolderPath =
-        Paths.get(
-            MoreObjects.firstNonNull(
-                options.getTempLocation(), System.getProperty("java.io.tmpdir")),
-            invocationId);
-    if (!outputFolderPath.toFile().mkdir()) {
-      throw new IOException("Failed to create folder " + outputFolderPath);
-    }
-    for (Location location : proxyManifest.getLocationList()) {
-      try (InputStream inputStream = getResourceFromClassPath(location.getUri())) {
-        Path outputPath = outputFolderPath.resolve(UUID.randomUUID().toString());
-        LOG.trace("Writing artifact {} to file {}", location.getName(), outputPath);
-        File file = outputPath.toFile();
-        try (FileOutputStream outputStream = new FileOutputStream(file)) {
-          IOUtils.copy(inputStream, outputStream);
-          filesToStage.add(StagedFile.of(file, location.getName()));
-        }
-      }
-    }
-    return filesToStage;
   }
 }
