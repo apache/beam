21bd4556fc72742db1d1f52dad7d88d4cb0b70c7
Pablo <pabloem@users.noreply.github.com>
2019-10-15 11:46:47 -0700

    Showing the Java environment used by Dataflow in the UI (#8680)
    * Showing the Java environment used by Dataflow in the UI via userAgent

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/DataflowRunner.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/DataflowRunner.java
@@ -340,9 +340,17 @@ public class DataflowRunner extends PipelineRunner<DataflowPipelineJob> {
       dataflowOptions.setGcsUploadBufferSizeBytes(GCS_UPLOAD_BUFFER_SIZE_BYTES_DEFAULT);
     }
 
+    // Adding the Java version to the SDK name for user's and support convenience.
+    String javaVersion =
+        Float.parseFloat(System.getProperty("java.specification.version")) >= 9
+            ? "(JDK 11 environment)"
+            : "(JRE 8 environment)";
+
     DataflowRunnerInfo dataflowRunnerInfo = DataflowRunnerInfo.getDataflowRunnerInfo();
     String userAgent =
-        String.format("%s/%s", dataflowRunnerInfo.getName(), dataflowRunnerInfo.getVersion())
+        String.format(
+                "%s/%s%s",
+                dataflowRunnerInfo.getName(), dataflowRunnerInfo.getVersion(), javaVersion)
             .replace(" ", "_");
     dataflowOptions.setUserAgent(userAgent);
 
