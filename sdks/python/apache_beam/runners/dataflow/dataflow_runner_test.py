a6936c8bf3de4841782a92c3c3f9899699443767
Udi Meiri <udim@users.noreply.github.com>
2019-11-04 09:22:47 -0800

    [BEAM-3713] Add pytest for unit tests (#9756)
    This is #7949 without IT support.
    
    - Runs unit tests using pytest
      - tox: `tox -e py27-gcp-pytest,py36-pytest,etc.`
      - gradle: `../../gradlew pythonPreCommitPytest`
      - github PR phrase: `run python_pytest precommit`
    - Tests run in parallel (still single-threaded but on multiple worker
    processes).
      - no_xdist marker used for tests that don't work the xdist plugin.
    - Allows specifying test module in tox cmd line. Example:
    ```sh
    tox -e py27-pytest apache_beam.transforms.window_test
    ```

--- sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
+++ sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
@@ -29,6 +29,7 @@ from datetime import datetime
 # patches unittest.TestCase to be python3 compatible
 import future.tests.base  # pylint: disable=unused-import
 import mock
+import pytest
 
 import apache_beam as beam
 import apache_beam.transforms as ptransform
@@ -254,6 +255,8 @@ class DataflowRunnerTest(unittest.TestCase, ExtraAssertionsMixin):
                                 r'source is not currently available'):
       p.run()
 
+  # TODO(BEAM-8095): Segfaults in Python 3.7 with xdist.
+  @pytest.mark.no_xdist
   def test_remote_runner_display_data(self):
     remote_runner = DataflowRunner()
     p = Pipeline(remote_runner,
