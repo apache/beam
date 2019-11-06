28fc82db30a1a6fa52be51a7914de3a90b724db3
Valentyn Tymofieiev <valentyn@google.com>
2019-11-06 08:30:38 -0800

    fixup: update the PR link.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -107,7 +107,7 @@ REQUIRED_PACKAGES = [
     'avro-python3>=1.8.1,<2.0.0; python_version >= "3.0"',
     'crcmod>=1.7,<2.0',
     # Dill doesn't guarantee compatibility between releases within minor version.
-    # REMEMBER: https://github.com/uqfoundation/dill/issues/347
+    # See: https://github.com/uqfoundation/dill/issues/341.
     'dill>=0.3.1.1,<0.3.2',
     'fastavro>=0.21.4,<0.22',
     'funcsigs>=1.0.2,<2; python_version < "3.0"',
