9933a05eb440b08d472d6e9159a0ebae3f5ce77e
Maximilian Michels <mxm@apache.org>
2019-11-06 13:19:02 +0100

    [BEAM-8566] Fix checkpoint buffering when another bundle is started during checkpointing
    As part of a checkpoint, the current bundle is finalized. When the bundle is
    finalized, the watermark, currently held back, may also be progressed which can
    cause the start of another bundle. When a new bundle is started, any
    to-be-buffered items from the previous bundle for the pending checkpoint may be
    emitted. This should not happen.
    
    This only effects portable pipelines where we have to hold back the watermark
    due to the asynchronous processing of elements.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
@@ -908,6 +908,10 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
      * by a lock.
      */
     void flushBuffer() {
+      if (openBuffer) {
+        // Buffering currently in progress, do not proceed
+        return;
+      }
       try {
         pushedBackElementsHandler
             .getElements()
