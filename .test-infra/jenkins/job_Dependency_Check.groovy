46c75d3e274b775751f4a7caf5475c76a87c5563
Tomo Suzuki <suztomo@google.com>
2019-11-19 15:22:00 -0500

    Reverting unnecessary changes

--- .test-infra/jenkins/job_Dependency_Check.groovy
+++ .test-infra/jenkins/job_Dependency_Check.groovy
@@ -58,7 +58,7 @@ job('beam_Dependency_Check') {
     extendedEmail {
       triggers {
         always {
-          recipientList('suztomo@google.com') // TODO: Revert this
+          recipientList('dev@beam.apache.org')
           contentType('text/html')
           subject("Beam Dependency Check Report (${date})")
           content('''${FILE, path="src/build/dependencyUpdates/beam-dependency-check-report.html"}''')
