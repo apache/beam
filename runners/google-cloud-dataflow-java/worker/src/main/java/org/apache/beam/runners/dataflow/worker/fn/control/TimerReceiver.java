e540266752a1df86ea366e3161c029db8efc8111
Rehman <rehman.muradali@venturedive.com>
2019-11-21 10:46:16 +0500

    [BEAM-2535] Revert TimerReceiver outputTimestamp

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/fn/control/TimerReceiver.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/fn/control/TimerReceiver.java
@@ -119,7 +119,6 @@ public class TimerReceiver {
         String timerId = timerSpec.timerId();
 
         TimerInternals timerInternals = stepContext.namespacedToUser().timerInternals();
-        TimerInternals.TimerData timerData = stepContext.getNextFiredTimer(windowCoder);
         timerInternals.setTimer(
             namespace, timerId, timer.getTimestamp(), timer.getTimestamp(), timeDomain);
 
