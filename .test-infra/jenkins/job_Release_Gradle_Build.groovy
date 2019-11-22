ee5a23741f66978c0e4416e5cd6662fe229ff48e
Mikhail Gryzykhin <migryz@google.com>
2019-11-22 08:46:19 -0800

    fix syntax

--- .test-infra/jenkins/job_Release_Gradle_Build.groovy
+++ .test-infra/jenkins/job_Release_Gradle_Build.groovy
@@ -25,7 +25,10 @@ job('beam_Release_Gradle_Build') {
   description('Verify Gradle build against the official release version.')
 
   // Set common parameters.
-  commonJobProperties.setTopLevelMainJobProperties(delegate,
+  commonJobProperties
+  .setTopLevelMainJobProperties(
+    delegate,
+    defaultBranch='master',
     defaultTimeout=150)
 
   // Allows triggering this build against pull requests.
