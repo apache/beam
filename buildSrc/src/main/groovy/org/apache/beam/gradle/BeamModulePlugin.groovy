228fc80e3a7324939651e066d1a3d634a5e53a1c
Stefan Djelekar <stefan.djelekar@redbox.com>
2019-11-25 15:54:09 +0100

    [BEAM-8376] Added missing firestore dependencies

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -371,6 +371,7 @@ class BeamModulePlugin implements Plugin<Project> {
     def google_cloud_bigdataoss_version = "1.9.16"
     def google_cloud_core_version = "1.61.0"
     def google_cloud_spanner_version = "1.6.0"
+    def google_cloud_firestore_version = "1.31.0"
     def grpc_version = "1.17.1"
     def guava_version = "20.0"
     def hadoop_version = "2.7.3"
@@ -453,6 +454,7 @@ class BeamModulePlugin implements Plugin<Project> {
         google_cloud_core_grpc                      : "com.google.cloud:google-cloud-core-grpc:$google_cloud_core_version",
         google_cloud_dataflow_java_proto_library_all: "com.google.cloud.dataflow:google-cloud-dataflow-java-proto-library-all:0.5.160304",
         google_cloud_datastore_v1_proto_client      : "com.google.cloud.datastore:datastore-v1-proto-client:1.6.0",
+        google_cloud_firestore                      : "com.google.cloud:google-cloud-firestore:$google_cloud_firestore_version",
         google_cloud_spanner                        : "com.google.cloud:google-cloud-spanner:$google_cloud_spanner_version",
         google_http_client                          : "com.google.http-client:google-http-client:$google_clients_version",
         google_http_client_jackson                  : "com.google.http-client:google-http-client-jackson:$google_clients_version",
