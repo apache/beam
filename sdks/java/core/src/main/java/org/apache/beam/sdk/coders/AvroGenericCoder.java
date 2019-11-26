d210c202e64ba2d2904dcee606438e1a8d0f05c3
Piotr Szczepanik <piotr.szczepanik@allegro.pl>
2019-11-26 11:35:50 +0100

    Fixed code style violations

--- sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroGenericCoder.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroGenericCoder.java
@@ -20,7 +20,7 @@ package org.apache.beam.sdk.coders;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 
-/** AvroCoder specialisation for GenericRecord */
+/** AvroCoder specialisation for GenericRecord. */
 public class AvroGenericCoder extends AvroCoder<GenericRecord> {
   AvroGenericCoder(Schema schema) {
     super(GenericRecord.class, schema);
