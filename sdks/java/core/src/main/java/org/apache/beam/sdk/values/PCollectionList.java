ac9a2d54cba8fe4dec383e9077d9be86f83dcdd9
Heejong Lee <heejong@gmail.com>
2019-12-05 16:53:30 -0800

    [BEAM-8905] matching Java PCollectionTuple translation naming convention in expansion service with index only

--- sdks/java/core/src/main/java/org/apache/beam/sdk/values/PCollectionList.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/values/PCollectionList.java
@@ -115,7 +115,7 @@ public class PCollectionList<T> implements PInput, POutput {
         pipeline,
         ImmutableList.<TaggedPValue>builder()
             .addAll(pcollections)
-            .add(TaggedPValue.of(new TupleTag<T>(), pc))
+            .add(TaggedPValue.of(new TupleTag<T>(Integer.toString(pcollections.size())), pc))
             .build());
   }
 
@@ -130,11 +130,13 @@ public class PCollectionList<T> implements PInput, POutput {
   public PCollectionList<T> and(Iterable<PCollection<T>> pcs) {
     ImmutableList.Builder<TaggedPValue> builder = ImmutableList.builder();
     builder.addAll(pcollections);
+    int nextIndex = pcollections.size();
     for (PCollection<T> pc : pcs) {
       if (pc.getPipeline() != pipeline) {
         throw new IllegalArgumentException("PCollections come from different Pipelines");
       }
-      builder.add(TaggedPValue.of(new TupleTag<T>(), pc));
+      builder.add(TaggedPValue.of(new TupleTag<T>(Integer.toString(nextIndex)), pc));
+      nextIndex += 1;
     }
     return new PCollectionList<>(pipeline, builder.build());
   }
