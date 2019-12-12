3c9a94652f43dc373861dae360754aec82a77e91
Etienne Chauchot <echauchot@apache.org>
2019-12-12 11:10:02 +0100

    [BEAM-5192] Minor change of ESIO public configuration API: MaxRetryTimeout support was removed in ES 7.0. So, as we use the same REST client for all versions of ESIO, change the socketAndRetryTimeout parameter to only socketTimeout parameter.

--- sdks/java/io/elasticsearch-tests/elasticsearch-tests-2/src/test/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIOTest.java
+++ sdks/java/io/elasticsearch-tests/elasticsearch-tests-2/src/test/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIOTest.java
@@ -87,7 +87,7 @@ public class ElasticsearchIOTest implements Serializable {
     connectionConfiguration =
         ConnectionConfiguration.create(
                 new String[] {"http://" + ES_IP + ":" + esHttpPort}, getEsIndex(), ES_TYPE)
-            .withSocketAndRetryTimeout(120000)
+            .withSocketTimeout(120000)
             .withConnectTimeout(5000);
     restClient = connectionConfiguration.createClient();
     elasticsearchIOTestCommon =
