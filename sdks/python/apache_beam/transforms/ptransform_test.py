dda0b2d986477de146a1202ebc76a8cd12c82ebc
Wenjia Liu <wenjialiu@google.com>
2019-12-20 16:04:00 -0800

    [BEAM-8575] Refactor test_do_fn_with_windowing_in_finish_bundle to work with Dataflow runner

--- sdks/python/apache_beam/transforms/ptransform_test.py
+++ sdks/python/apache_beam/transforms/ptransform_test.py
@@ -347,14 +347,14 @@ class PTransformTest(unittest.TestCase):
 
     pipeline = TestPipeline()
     result = (pipeline
-              | 'Start' >> beam.Create([x for x in range(3)])
+              | 'Start' >> beam.Create([1])
               | beam.ParDo(MyDoFn())
               | WindowInto(windowfn)
               | 'create tuple' >> beam.Map(
                   lambda v, t=beam.DoFn.TimestampParam, w=beam.DoFn.WindowParam:
                   (v, t, w.start, w.end)))
-    expected_process = [('process'+ str(x), Timestamp(5), Timestamp(4),
-                         Timestamp(6)) for x in range(3)]
+    expected_process = [('process'+ str(1), Timestamp(5), Timestamp(4),
+                         Timestamp(6))]
     expected_finish = [('finish', Timestamp(1), Timestamp(0), Timestamp(2))]
 
     assert_that(result, equal_to(expected_process + expected_finish))
