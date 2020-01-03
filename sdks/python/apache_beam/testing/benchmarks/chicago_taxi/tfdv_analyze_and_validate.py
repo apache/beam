cc76fc0d408230d02f3f1cd1fe70d76ec6bf8764
Kamil Wasilewski <kamil.wasilewski@polidea.com>
2020-01-03 16:01:21 +0100

    [BEAM-8108] Use _ReadFromBigQuery transform

--- sdks/python/apache_beam/testing/benchmarks/chicago_taxi/tfdv_analyze_and_validate.py
+++ sdks/python/apache_beam/testing/benchmarks/chicago_taxi/tfdv_analyze_and_validate.py
@@ -25,6 +25,7 @@ import tensorflow_data_validation as tfdv
 from tensorflow_metadata.proto.v0 import statistics_pb2
 
 import apache_beam as beam
+from apache_beam.io.gcp.bigquery import _ReadFromBigQuery as ReadFromBigQuery
 from apache_beam.metrics.metric import MetricsFilter
 from apache_beam.testing.load_tests.load_test_metrics_utils import MeasureTime
 from apache_beam.testing.load_tests.load_test_metrics_utils import MetricsReader
@@ -106,8 +107,8 @@ def compute_stats(input_handle,
       table_name=input_handle, max_rows=max_rows, for_eval=for_eval)
   raw_data = (
       pipeline
-      | 'ReadBigQuery' >> beam.io.Read(
-          beam.io.BigQuerySource(query=query, use_standard_sql=True))
+      | 'ReadBigQuery' >> ReadFromBigQuery(query=query, project=project,
+                                           use_standard_sql=True)
       | 'Measure time: Start' >> beam.ParDo(MeasureTime(namespace))
       | 'ConvertToTFDVInput' >> beam.Map(
           lambda x: {key: np.asarray([x[key]])
