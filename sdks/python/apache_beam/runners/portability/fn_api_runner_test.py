b3f33d96d571c0e455bea8e08e4ed7b2ff2e7f79
Hannah Jiang <hannahjiang@google.com>
2020-01-07 15:54:37 -0800

    BEAM-7861 add direct_running_mode option
    [BEAM-7861] add in_memory choice

--- sdks/python/apache_beam/runners/portability/fn_api_runner_test.py
+++ sdks/python/apache_beam/runners/portability/fn_api_runner_test.py
@@ -1183,10 +1183,10 @@ class FnApiRunnerTestWithMultiWorkers(FnApiRunnerTest):
 class FnApiRunnerTestWithGrpcAndMultiWorkers(FnApiRunnerTest):
 
   def create_pipeline(self):
-    pipeline_options = PipelineOptions(direct_num_workers=2)
+    pipeline_options = PipelineOptions(direct_num_workers=2,
+                                       direct_running_mode='multi_threading')
     p = beam.Pipeline(
-        runner=fn_api_runner.FnApiRunner(
-            default_environment=environments.EmbeddedPythonGrpcEnvironment()),
+        runner=fn_api_runner.FnApiRunner(),
         options=pipeline_options)
     #TODO(BEAM-8444): Fix these tests..
     p.options.view_as(DebugOptions).experiments.remove('beam_fn_api')
@@ -1543,10 +1543,10 @@ class ExpandStringsProvider(beam.transforms.core.RestrictionProvider):
 class FnApiRunnerSplitTestWithMultiWorkers(FnApiRunnerSplitTest):
 
   def create_pipeline(self):
-    pipeline_options = PipelineOptions(direct_num_workers=2)
+    pipeline_options = PipelineOptions(direct_num_workers=2,
+                                       direct_running_mode='multi_threading')
     p = beam.Pipeline(
-        runner=fn_api_runner.FnApiRunner(
-            default_environment=environments.EmbeddedPythonGrpcEnvironment()),
+        runner=fn_api_runner.FnApiRunner(),
         options=pipeline_options)
     #TODO(BEAM-8444): Fix these tests..
     p.options.view_as(DebugOptions).experiments.remove('beam_fn_api')
