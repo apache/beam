021e7b3b3d4b30d8d7230cf0c230c278b922ec69
Maximilian Michels <mxm@apache.org>
2020-01-07 15:43:23 +0100

    [BEAM-9060] Restore stdout/stderr in case Flink's OptimizedPlanEnvironment is used
    To extract the JobGraph, Flink calls the main method of a JAR and suppresses the
    stdout/stderr. It only prints the output on errors, or when no JobGraph was
    found. This is often not desired for debugging purposes and logging purposes.
    
    This change restores the original stdout/stderr when we detect that we are
    running inside the `OptimizedPlanEnvironment`.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/Workarounds.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/Workarounds.java
@@ -18,7 +18,11 @@
 package org.apache.beam.runners.flink.translation.utils;
 
 import com.fasterxml.jackson.databind.type.TypeFactory;
-
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.client.program.OptimizerPlanEnvironment;
 
 /** Workarounds for dealing with limitations of Flink or its libraries. */
 public class Workarounds {
@@ -27,5 +31,18 @@ public class Workarounds {
     // Clear cache to get rid of any references to the Flink Classloader
     // See https://jira.apache.org/jira/browse/BEAM-6460
     TypeFactory.defaultInstance().clearCache();
+  }
+
+  /**
+   * Flink uses the {@link org.apache.flink.client.program.OptimizerPlanEnvironment} which replaces
+   * stdout/stderr during job graph creation. This was intended only for previewing the plan, but
+   * other parts of Flink, e.g. the Rest API have started to use this code as well. To be able to
+   * inspect the output before execution, we use this method to restore the original stdout/stderr.
+   */
+  public static void restoreOriginalStdOutAndStdErrIfApplicable() {
+    if (ExecutionEnvironment.getExecutionEnvironment() instanceof OptimizerPlanEnvironment) {
+      System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+      System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+    }
   }
 }
