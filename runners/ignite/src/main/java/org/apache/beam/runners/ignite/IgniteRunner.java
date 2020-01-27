50a5a808956712c96a0dcbd3e51cecc88d331413
samaitra <saikat.maitra@gmail.com>
2020-01-26 21:23:50 -0600

    IGNITE-12228 Implement an Apache Beam runner on top of Ignite compute grid

---
+++ runners/ignite/src/main/java/org/apache/beam/runners/ignite/IgniteRunner.java
@@ -0,0 +1,14 @@
+package org.apache.beam.runners.ignite;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.PipelineRunner;
+
+public class IgniteRunner extends PipelineRunner<PipelineResult> {
+
+
+    @Override
+    public PipelineResult run(Pipeline pipeline) {
+        return null;
+    }
+}
