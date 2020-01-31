aa06bba012f960e1076862c8a8c79eb6e71dcb65
pabloem <pabloem@apache.org>
2020-01-31 10:03:35 -0800

    Fixups

--- sdks/python/apache_beam/io/gcp/big_query_query_to_table_it_test.py
+++ sdks/python/apache_beam/io/gcp/big_query_query_to_table_it_test.py
@@ -148,7 +148,6 @@ class BigQueryQueryToTableIT(unittest.TestCase):
                   'output': self.output_table,
                   'output_schema': DIALECT_OUTPUT_SCHEMA,
                   'use_standard_sql': False,
-                  'native': True,
                   'wait_until_finish_duration': WAIT_UNTIL_FINISH_DURATION_MS,
                   'on_success_matcher': all_of(*pipeline_verifiers)}
     options = self.test_pipeline.get_full_options_as_args(**extra_opts)
@@ -168,7 +167,6 @@ class BigQueryQueryToTableIT(unittest.TestCase):
                   'output_schema': DIALECT_OUTPUT_SCHEMA,
                   'use_standard_sql': True,
                   'wait_until_finish_duration': WAIT_UNTIL_FINISH_DURATION_MS,
-                  'native': True,
                   'on_success_matcher': all_of(*pipeline_verifiers)}
     options = self.test_pipeline.get_full_options_as_args(**extra_opts)
     big_query_query_to_table_pipeline.run_bq_pipeline(options)
