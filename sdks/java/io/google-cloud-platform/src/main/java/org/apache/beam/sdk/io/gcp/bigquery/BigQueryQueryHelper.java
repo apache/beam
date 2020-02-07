cef74a24cccdc9c6165126b2457376063d07af24
Israel Herraiz <ihr@google.com>
2020-02-07 16:47:53 +0100

    If user specifies existing temp dataset, don't overwrite tables
    When the user specifies the temp dataset, Beam does not control its creation,
    and it might write over an existing table if the generated table name collides
    with an existing table (unlikely, but not impossible)

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryQueryHelper.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryQueryHelper.java
@@ -19,12 +19,14 @@ package org.apache.beam.sdk.io.gcp.bigquery;
 
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers.createJobIdToken;
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers.createTempTableReference;
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
 
 import com.google.api.services.bigquery.model.EncryptionConfiguration;
 import com.google.api.services.bigquery.model.Job;
 import com.google.api.services.bigquery.model.JobConfigurationQuery;
 import com.google.api.services.bigquery.model.JobReference;
 import com.google.api.services.bigquery.model.JobStatistics;
+import com.google.api.services.bigquery.model.Table;
 import com.google.api.services.bigquery.model.TableReference;
 import java.io.IOException;
 import java.util.List;
@@ -108,14 +110,16 @@ class BigQueryQueryHelper {
       }
     }
 
-    // Step 2: Create a temporary dataset in the query location.
+    // Step 2: Create a temporary dataset in the query location only if the user has not specified a
+    // temp dataset.
     String jobIdToken = createJobIdToken(options.getJobName(), stepUuid);
     Optional<String> queryTempDatasetOpt = Optional.ofNullable(queryTempDatasetId);
     TableReference queryResultTable =
         createTempTableReference(options.getProject(), jobIdToken, queryTempDatasetOpt);
 
+    boolean beamToCreateTempDataset = !queryTempDatasetOpt.isPresent();
     // Create dataset only if it has not been set by the user
-    if (!queryTempDatasetOpt.isPresent()) {
+    if (beamToCreateTempDataset) {
       LOG.info("Creating temporary dataset {} for query results", queryResultTable.getDatasetId());
 
       tableService.createDataset(
@@ -124,6 +128,14 @@ class BigQueryQueryHelper {
           effectiveLocation,
           "Temporary tables for query results of job " + options.getJobName(),
           TimeUnit.DAYS.toMillis(1));
+    } else { // If the user specified a temp dataset, check that the destination table does not
+      // exist
+      Table destTable = tableService.getTable(queryResultTable);
+      checkArgument(
+          destTable == null,
+          "Refusing to write on existing table {} in the specified temp dataset {}",
+          queryResultTable.getTableId(),
+          queryResultTable.getDatasetId());
     }
 
     // Step 3: Execute the query. Generate a transient (random) query job ID, because this code may
