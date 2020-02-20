29f3498cdf93fe5f5de838815a85822ed6395665
EDjur <elias.djurfeldt@mirado.com>
2020-02-20 13:43:38 +0100

    AnnotateImage and AnnotateImageWithContext PTransforms

--- sdks/python/apache_beam/ml/gcp/visionml.py
+++ sdks/python/apache_beam/ml/gcp/visionml.py
@@ -21,17 +21,23 @@ A connector for sending API requests to the GCP Vision API.
 """
 
 # TODO: Add Batching support https://google-cloud-python.readthedocs.io/en/0.32.0/vision/gapic/v1/api.html#google.cloud.vision_v1.ImageAnnotatorClient.batch_annotate_images
+# TODO: Add more types https://googleapis.dev/python/vision/0.41.0/gapic/v1/types.html
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import
 
-from cachetools.func import ttl_cache
-from future.utils import binary_type, text_type
+from typing import Optional
 from typing import Tuple
 from typing import Union
 
+from future.utils import binary_type
+from future.utils import text_type
+
 from apache_beam import typehints
 from apache_beam.metrics import Metrics
-from apache_beam.transforms import DoFn, ParDo, PTransform
+from apache_beam.transforms import DoFn
+from apache_beam.transforms import ParDo
+from apache_beam.transforms import PTransform
+from cachetools.func import ttl_cache
 
 try:
   from google.cloud import vision
@@ -40,7 +46,7 @@ except ImportError:
       'Google Cloud Vision not supported for this execution environment '
       '(could not import google.cloud.vision).')
 
-__all__ = ['AnnotateImage']
+__all__ = ['AnnotateImage', 'AnnotateImageWithContext']
 
 
 @ttl_cache(maxsize=128, ttl=3600)
@@ -53,56 +59,171 @@ def get_vision_client(client_options=None):
 class AnnotateImage(PTransform):
   """A ``PTransform`` for annotating images using the GCP Vision API
   ref: https://cloud.google.com/vision/docs/
+
+  Sends each element to the GCP Vision API. Element is a
+  Union[text_type, binary_type] of either an URI (e.g. a GCS URI) or binary_type
+  base64-encoded image data.
+  Accepts an `AsDict` side input that maps each image to an image context.
   """
-  def __init__(self, features, retry=None, timeout=120, client_options=None):
+  def __init__(
+      self,
+      features,
+      retry=None,
+      timeout=120,
+      client_options=None,
+      context_side_input=None):
+    """
+    Args:
+      features: (List[``vision.types.Feature.enums.Feature``]) Required.
+        The Vision API features to detect
+      retry: (google.api_core.retry.Retry) Optional.
+        A retry object used to retry requests.
+        If None is specified (default), requests will not be retried.
+      timeout: (float) Optional.
+        The time in seconds to wait for the response from the
+        Vision API
+      client_options: (Union[dict, google.api_core.client_options.ClientOptions])
+        Client options used to set user options on the client.
+        API Endpoint should be set through client_options.
+      context_side_input: (beam.pvalue.AsDict) Optional.
+        An ``AsDict`` of a PCollection to be passed to the
+        _ImageAnnotateFn as the image context mapping containing additional
+        image context and/or feature-specific parameters.
+        Example usage::
+
+          image_contexts =
+            [(''gs://cloud-samples-data/vision/ocr/sign.jpg'', Union[dict,
+            ``vision.types.ImageContext()``]),
+            (''gs://cloud-samples-data/vision/ocr/sign.jpg'', Union[dict,
+            ``vision.types.ImageContext()``]),]
+
+          context_side_input =
+            (
+              p
+              | "Image contexts" >> beam.Create(image_contexts)
+            )
+
+          visionml.AnnotateImage(features,
+            context_side_input=beam.pvalue.AsDict(context_side_input)))
+    """
     super(AnnotateImage, self).__init__()
     self.features = features
     self.retry = retry
     self.timeout = timeout
     self.client_options = client_options
+    self.context_side_input = context_side_input
 
   def expand(self, pvalue):
     return pvalue | ParDo(
-        self._ImageAnnotateFn(
+        _ImageAnnotateFn(
             features=self.features,
             retry=self.retry,
             timeout=self.timeout,
             client_options=self.client_options))
 
-  @typehints.with_input_types(
-      Union[Tuple[Union[text_type, binary_type], vision.types.ImageContext],
-            Union[text_type, binary_type]])
-  class _ImageAnnotateFn(DoFn):
-    """ A DoFn that sends each input element to the GCP Vision API
-        service and outputs an element with the return result of the API
-        (``google.cloud.vision_v1.types.BatchAnnotateImagesResponse``).
-     """
-    def __init__(self, features, retry, timeout, client_options):
-      super(AnnotateImage._ImageAnnotateFn, self).__init__()
-      self._client = None
-      self.features = features
-      self.retry = retry
-      self.timeout = timeout
-      self.client_options = client_options
-      self.image_context = None
-      self.counter = Metrics.counter(self.__class__, "API Calls")
 
-    def start_bundle(self):
-      self._client = get_vision_client(self.client_options)
+@typehints.with_input_types(
+    Union[text_type, binary_type], Optional[vision.types.ImageContext])
+class _ImageAnnotateFn(DoFn):
+  """A DoFn that sends each input element to the GCP Vision API
+  service and outputs an element with the return result of the API
+  (``google.cloud.vision_v1.types.AnnotateImageResponse``).
+  """
+  def __init__(self, features, retry, timeout, client_options):
+    super(_ImageAnnotateFn, self).__init__()
+    self._client = None
+    self.features = features
+    self.retry = retry
+    self.timeout = timeout
+    self.client_options = client_options
+    self.counter = Metrics.counter(self.__class__, "API Calls")
 
-    def process(self, element, *args, **kwargs):
-      if isinstance(element, Tuple):  # Unpack (element, ImageContext) tuple
-        element, self.image_context = element
-      if isinstance(element, text_type):  # Is element an URI
-        image = vision.types.Image(
-            source=vision.types.ImageSource(image_uri=element))
-      elif isinstance(element, binary_type):  # Is element raw bytes
-        image = vision.types.Image(content=element)
+  def start_bundle(self):
+    self._client = get_vision_client(self.client_options)
 
-      request = vision.types.AnnotateImageRequest(
-          image=image, features=self.features, image_context=self.image_context)
-      response = self._client.annotate_image(
-          request=request, retry=self.retry, timeout=self.timeout)
+  def _annotate_image(self, element, image_context):
+    if isinstance(element, text_type):
+      image = vision.types.Image(
+          source=vision.types.ImageSource(image_uri=element))
+    else:  # Typehint checks only allows text_type or binary_type
+      image = vision.types.Image(content=element)
 
-      self.counter.inc()
-      yield response
+    request = vision.types.AnnotateImageRequest(
+        image=image, features=self.features, image_context=image_context)
+    response = self._client.annotate_image(
+        request=request, retry=self.retry, timeout=self.timeout)
+
+    return response
+
+  def process(self, element, context_side_input=None, *args, **kwargs):
+    if context_side_input:  # If we have a side input image context, use that
+      image_context = context_side_input.get(element)
+    else:
+      image_context = None
+    response = self._annotate_image(element, image_context)
+    self.counter.inc()
+    yield response
+
+
+class AnnotateImageWithContext(AnnotateImage):
+  """A ``PTransform`` for annotating images using the GCP Vision API
+  ref: https://cloud.google.com/vision/docs/
+
+  Sends each element to the GCP Vision API. Element is a tuple of
+
+      (Union[text_type, binary_type],
+      Optional[``vision.types.ImageContext``])
+
+  where the former is either an URI (e.g. a GCS URI) or binary_type
+  base64-encoded image data
+  """
+  def __init__(self, features, retry=None, timeout=120, client_options=None):
+    """
+     Args:
+      features: (List[``vision.types.Feature.enums.Feature``]) Required.
+        The Vision API features to detect
+      retry: (google.api_core.retry.Retry) Optional.
+        A retry object used to retry requests.
+        If None is specified (default), requests will not be retried.
+      timeout: (float) Optional.
+        The time in seconds to wait for the response from the
+        Vision API
+      client_options: (Union[dict, google.api_core.client_options.ClientOptions])
+        Client options used to set user options on the client.
+        API Endpoint should be set through client_options.
+    """
+    super(AnnotateImageWithContext, self).__init__(
+        features=features,
+        retry=retry,
+        timeout=timeout,
+        client_options=client_options)
+
+  def expand(self, pvalue):
+    return pvalue | ParDo(
+        _ImageAnnotateFnWithContext(
+            features=self.features,
+            retry=self.retry,
+            timeout=self.timeout,
+            client_options=self.client_options))
+
+
+@typehints.with_input_types(
+    Tuple[Union[text_type, binary_type], Optional[vision.types.ImageContext]])
+class _ImageAnnotateFnWithContext(_ImageAnnotateFn):
+  """A DoFn that unpacks each input tuple to element, image_context variables
+  and sends these to the GCP Vision API service and outputs an element with the
+  return result of the API
+  (``google.cloud.vision_v1.types.AnnotateImageResponse``).
+  """
+  def __init__(self, features, retry, timeout, client_options):
+    super(_ImageAnnotateFnWithContext, self).__init__(
+        features=features,
+        retry=retry,
+        timeout=timeout,
+        client_options=client_options)
+
+  def process(self, element, *args, **kwargs):
+    element, image_context = element  # Unpack (image, image_context) tuple
+    response = self._annotate_image(element, image_context)
+    self.counter.inc()
+    yield response
--- sdks/python/apache_beam/ml/gcp/visionml_test.py
+++ sdks/python/apache_beam/ml/gcp/visionml_test.py
@@ -20,8 +20,10 @@
 
 # pytype: skip-file
 
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
+import logging
 import unittest
 
 import mock
@@ -50,11 +52,101 @@ class VisionTest(unittest.TestCase):
         vision.types.Feature(
             type=feature_type, max_results=3, model="builtin/stable")
     ]
+    self.img_ctx = vision.types.ImageContext()
 
-  def test_AnnotateImage_URI_with_ImageContext(self):
-    img_ctx = vision.types.ImageContext()
+  def test_AnnotateImage_URI_with_side_input_context(self):
     images_to_annotate = [
-        ('gs://cloud-samples-data/vision/ocr/sign.jpg', img_ctx)
+        'gs://cloud-samples-data/vision/ocr/sign.jpg',
+        'gs://cloud-samples-data/vision/ocr/sign.jpg'
+    ]
+
+    image_contexts = [(
+        'gs://cloud-samples-data/vision/ocr/sign.jpg',
+        self.img_ctx,
+        'gs://cloud-samples-data/vision/ocr/sign.jpg',
+        self.img_ctx,
+    )]
+
+    expected_counter = len(images_to_annotate)
+    with mock.patch.object(visionml,
+                           'get_vision_client',
+                           return_value=self._mock_client):
+      p = beam.Pipeline()
+      context_side_input = (p | "Image contexts" >> beam.Create(image_contexts))
+
+      _ = (
+          p
+          | "Create data" >> beam.Create(images_to_annotate)
+          | "Annotate image" >> visionml.AnnotateImage(
+              self.features,
+              context_side_input=beam.pvalue.AsDict(context_side_input)))
+      result = p.run()
+      result.wait_until_finish()
+
+      read_filter = MetricsFilter().with_name('API Calls')
+      query_result = result.metrics().query(read_filter)
+      if query_result['counters']:
+        read_counter = query_result['counters'][0]
+        self.assertTrue(read_counter.committed == expected_counter)
+
+  def test_AnnotateImage_URIs(self):
+    images_to_annotate = [
+        'gs://cloud-samples-data/vision/ocr/sign.jpg',
+        'gs://cloud-samples-data/vision/ocr/sign.jpg'
+    ]
+
+    expected_counter = len(images_to_annotate)
+    with mock.patch.object(visionml,
+                           'get_vision_client',
+                           return_value=self._mock_client):
+      p = beam.Pipeline()
+      _ = (
+          p
+          | "Create data" >> beam.Create(images_to_annotate)
+          | "Annotate image" >> visionml.AnnotateImage(self.features))
+      result = p.run()
+      result.wait_until_finish()
+
+      read_filter = MetricsFilter().with_name('API Calls')
+      query_result = result.metrics().query(read_filter)
+      if query_result['counters']:
+        read_counter = query_result['counters'][0]
+        self.assertTrue(read_counter.committed == expected_counter)
+
+  def test_AnnotateImageWithContext_b64_content(self):
+    base_64_encoded_image = \
+      b'YmVnaW4gNjQ0IGNhdC12aWRlby5tcDRNICAgICgmOVQ+NyFNPCMwUi4uZmFrZV92aWRlb'
+    images_to_annotate = [
+        (base_64_encoded_image, self.img_ctx),
+        (base_64_encoded_image, None),
+        (base_64_encoded_image, self.img_ctx),
+    ]
+    expected_counter = len(images_to_annotate)
+    with mock.patch.object(visionml,
+                           'get_vision_client',
+                           return_value=self._mock_client):
+      p = beam.Pipeline()
+      _ = (
+          p
+          | "Create data" >> beam.Create(images_to_annotate)
+          |
+          "Annotate image" >> visionml.AnnotateImageWithContext(self.features))
+      result = p.run()
+      result.wait_until_finish()
+
+    read_filter = MetricsFilter().with_name('API Calls')
+    query_result = result.metrics().query(read_filter)
+    if query_result['counters']:
+      read_counter = query_result['counters'][0]
+      self.assertTrue(read_counter.committed == expected_counter)
+
+  def test_AnnotateImage_b64_content(self):
+    base_64_encoded_image = \
+      b'YmVnaW4gNjQ0IGNhdC12aWRlby5tcDRNICAgICgmOVQ+NyFNPCMwUi4uZmFrZV92aWRlb'
+    images_to_annotate = [
+        (base_64_encoded_image),
+        (base_64_encoded_image),
+        (base_64_encoded_image),
     ]
     expected_counter = len(images_to_annotate)
     with mock.patch.object(visionml,
@@ -74,74 +166,24 @@ class VisionTest(unittest.TestCase):
         read_counter = query_result['counters'][0]
         self.assertTrue(read_counter.committed == expected_counter)
 
-  def test_AnnotateImage_URI(self):
-    images_to_annotate = ['gs://cloud-samples-data/vision/ocr/sign.jpg']
-    expected_counter = len(images_to_annotate)
+  def test_AnnotateImageWithContext_bad_input(self):
+    """AnnotateImageWithContext should not accept images without context"""
+    images_to_annotate = [
+        'gs://cloud-samples-data/vision/ocr/sign.jpg',
+        'gs://cloud-samples-data/vision/ocr/sign.jpg'
+    ]
     with mock.patch.object(visionml,
                            'get_vision_client',
                            return_value=self._mock_client):
-      p = beam.Pipeline()
-      _ = (
-          p
-          | "Create data" >> beam.Create(images_to_annotate)
-          | "Annotate image" >> visionml.AnnotateImage(self.features))
-      result = p.run()
-      result.wait_until_finish()
-
-      read_filter = MetricsFilter().with_name('API Calls')
-      query_result = result.metrics().query(read_filter)
-      if query_result['counters']:
-        read_counter = query_result['counters'][0]
-        self.assertTrue(read_counter.committed == expected_counter)
-
-  def test_AnnotateImage_byte_content(self):
-    img_ctx = vision.types.ImageContext()
-    base_64_encoded_bytes = \
-      (
-      b'begin 644 sign.jpg _]C_X  02D9)1@ ! 0$ 2 !(  #_X2EZ fake_image_content',
-      img_ctx)
-
-    images_to_annotate = [base_64_encoded_bytes]
-    expected_counter = len(images_to_annotate)
-    with mock.patch.object(visionml,
-                           'get_vision_client',
-                           return_value=self._mock_client):
-      p = beam.Pipeline()
-      _ = (
-          p
-          | "Create data" >> beam.Create(images_to_annotate)
-          | "Annotate image" >> visionml.AnnotateImage(self.features))
-      result = p.run()
-      result.wait_until_finish()
-
-      read_filter = MetricsFilter().with_name('API Calls')
-      query_result = result.metrics().query(read_filter)
-      if query_result['counters']:
-        read_counter = query_result['counters'][0]
-        self.assertTrue(read_counter.committed == expected_counter)
-
-  def test_AnnotateImage_byte_content_with_ImageContext(self):
-    base_64_encoded_bytes = \
-      b'begin 644 sign.jpg _]C_X  02D9)1@ ! 0$ 2 !(  #_X2EZ fake_image_content'
-
-    images_to_annotate = [base_64_encoded_bytes]
-    expected_counter = len(images_to_annotate)
-    with mock.patch.object(visionml,
-                           'get_vision_client',
-                           return_value=self._mock_client):
-      p = beam.Pipeline()
-      _ = (
-          p
-          | "Create data" >> beam.Create(images_to_annotate)
-          | "Annotate image" >> visionml.AnnotateImage(self.features))
-      result = p.run()
-      result.wait_until_finish()
-
-      read_filter = MetricsFilter().with_name('API Calls')
-      query_result = result.metrics().query(read_filter)
-      if query_result['counters']:
-        read_counter = query_result['counters'][0]
-        self.assertTrue(read_counter.committed == expected_counter)
+      with self.assertRaises(TypeCheckError):
+        p = beam.Pipeline()
+        _ = (
+            p
+            | "Create data" >> beam.Create(images_to_annotate)
+            | "Annotate image" >> visionml.AnnotateImageWithContext(
+                self.features))
+        result = p.run()
+        result.wait_until_finish()
 
   def test_AnnotateImage_bad_input(self):
     images_to_annotate = [123456789, 123456789, 123456789]
@@ -156,3 +198,29 @@ class VisionTest(unittest.TestCase):
             | "Annotate image" >> visionml.AnnotateImage(self.features))
         result = p.run()
         result.wait_until_finish()
+
+  def test_AnnotateImageWithContext_URI_with_ImageContext(self):
+    images_to_annotate = [
+        ('gs://cloud-samples-data/vision/ocr/sign.jpg', self.img_ctx)
+    ]
+    expected_counter = len(images_to_annotate)
+    with mock.patch.object(visionml,
+                           'get_vision_client',
+                           return_value=self._mock_client):
+      p = beam.Pipeline()
+      _ = (
+          p
+          | "Create data" >> beam.Create(images_to_annotate)
+          | "Annotate image" >> visionml.AnnotateImageWithContext(self.features))
+      result = p.run()
+      result.wait_until_finish()
+
+      read_filter = MetricsFilter().with_name('API Calls')
+      query_result = result.metrics().query(read_filter)
+      if query_result['counters']:
+        read_counter = query_result['counters'][0]
+        self.assertTrue(read_counter.committed == expected_counter)
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  unittest.main()
