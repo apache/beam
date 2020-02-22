4066791d4b9205e07b4734ba7a359305d5403a25
Jozef Vilcek <jvilcek@zetaglobal.com>
2020-02-22 10:03:53 +0100

    [BEAM-9360] Fix equivalence check for FieldType

--- sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/Schema.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/Schema.java
@@ -815,9 +815,6 @@ public class Schema implements Serializable {
       if (!getTypeName().equals(other.getTypeName())) {
         return false;
       }
-      if (!Objects.equals(getMetadata(), other.getMetadata())) {
-        return false;
-      }
 
       switch (getTypeName()) {
         case ROW:
