b934faf1af37d59244e91be860a8d773292fdb07
bipinupd <bipinupd@gmail.com>
2020-02-23 09:53:19 -0500

    Checkstyles, clean the build file and removed unused codes

--- sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/GenerateData.java
+++ sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/GenerateData.java
@@ -1,6 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.influxdb;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -8,51 +29,58 @@ import java.util.concurrent.TimeUnit;
 
 public class GenerateData {
 
-    public static Random random = new Random();
-    public static List<String> getMetric(int number){
-        List<String> element = new ArrayList<>();
-        for (int i = 0; i< number; i++){
-            Model m = new Model();
-            m.setMeasurement("test_m");
-            m.addFeild("test1", random.nextInt(100));
-            m.addFeild("test2", random.nextInt(100));
-            LocalDateTime time = LocalDateTime.of(LocalDate.now(),
-                    LocalTime.of(random.nextInt(24), random.nextInt(60),
-                            random.nextInt(60), random.nextInt(999999999 + 1)));
-            ZonedDateTime
-                    zdt = time.atZone(ZoneId.of("America/Los_Angeles"));
-            m.setTime(zdt.toInstant().toEpochMilli());
-            m.setTimeUnit(TimeUnit.MILLISECONDS);
-            element.add(m.getLineProtocol());
-        }
-        return element;
+  public static Random random = new Random();
+
+  public static List<String> getMetric(int number) {
+    List<String> element = new ArrayList<>();
+    for (int i = 0; i < number; i++) {
+      Model m = new Model();
+      m.setMeasurement("test_m");
+      m.addFeild("test1", random.nextInt(100));
+      m.addFeild("test2", random.nextInt(100));
+      LocalDateTime time =
+          LocalDateTime.of(
+              LocalDate.now(),
+              LocalTime.of(
+                  random.nextInt(24),
+                  random.nextInt(60),
+                  random.nextInt(60),
+                  random.nextInt(999999999 + 1)));
+      ZonedDateTime zdt = time.atZone(ZoneId.of("America/Los_Angeles"));
+      m.setTime(zdt.toInstant().toEpochMilli());
+      m.setTimeUnit(TimeUnit.MILLISECONDS);
+      element.add(m.getLineProtocol());
     }
+    return element;
+  }
 
-    public static List<String> getMultipleMetric(int number){
-        List<String> element = new ArrayList<>();
-        for (int i = 0; i< number; i++){
-            Model m = new Model();
-            Model m1 = new Model();
-            m.setMeasurement("test_m");
-            m.addFeild("test1", random.nextInt(100));
-            m.addFeild("test2", random.nextInt(100));
-            m1.setMeasurement("test_m1");
-            m1.addFeild("test1", random.nextInt(100));
-            m1.addFeild("test2", random.nextInt(100));
-            LocalDateTime time = LocalDateTime.of(LocalDate.now(),
-                    LocalTime.of(random.nextInt(24), random.nextInt(60),
-                            random.nextInt(60), random.nextInt(999999999 + 1)));
-            ZonedDateTime
-                    zdt = time.atZone(ZoneId.of("America/Los_Angeles"));
-            m.setTime(zdt.toInstant().toEpochMilli());
-            m.setTimeUnit(TimeUnit.MILLISECONDS);
-            m1.setTime(zdt.toInstant().toEpochMilli());
-            m1.setTimeUnit(TimeUnit.MILLISECONDS);
-            element.add(m.getLineProtocol());
-            element.add(m1.getLineProtocol());
-        }
-        return element;
+  public static List<String> getMultipleMetric(int number) {
+    List<String> element = new ArrayList<>();
+    for (int i = 0; i < number; i++) {
+      Model m = new Model();
+      Model m1 = new Model();
+      m.setMeasurement("test_m");
+      m.addFeild("test1", random.nextInt(100));
+      m.addFeild("test2", random.nextInt(100));
+      m1.setMeasurement("test_m1");
+      m1.addFeild("test1", random.nextInt(100));
+      m1.addFeild("test2", random.nextInt(100));
+      LocalDateTime time =
+          LocalDateTime.of(
+              LocalDate.now(),
+              LocalTime.of(
+                  random.nextInt(24),
+                  random.nextInt(60),
+                  random.nextInt(60),
+                  random.nextInt(999999999 + 1)));
+      ZonedDateTime zdt = time.atZone(ZoneId.of("America/Los_Angeles"));
+      m.setTime(zdt.toInstant().toEpochMilli());
+      m.setTimeUnit(TimeUnit.MILLISECONDS);
+      m1.setTime(zdt.toInstant().toEpochMilli());
+      m1.setTimeUnit(TimeUnit.MILLISECONDS);
+      element.add(m.getLineProtocol());
+      element.add(m1.getLineProtocol());
     }
-
-
+    return element;
+  }
 }
--- sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/InfluxDBIOIT.java
+++ sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/InfluxDBIOIT.java
@@ -1,24 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.influxdb;
 
+import java.util.Arrays;
 import org.apache.beam.sdk.PipelineResult;
-
 import org.apache.beam.sdk.io.common.IOTestPipelineOptions;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
-import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.values.PCollection;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Query;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.Arrays;
-
 
 /**
  * A test of {@link org.apache.beam.sdk.io.influxdb.InfluxDBIO} on an independent InfluxDB instance.
@@ -35,156 +54,179 @@ import java.util.Arrays;
  *  --tests org.apache.beam.sdk.io.influxdb.InfluxDBIOIT
  *  -DintegrationTestRunner=direct
  * </pre>
- *
  */
-
-
 @RunWith(JUnit4.class)
 public class InfluxDBIOIT {
 
-    private static InfluxDBPipelineOptions options;
+  private static InfluxDBPipelineOptions options;
 
-    @Rule
-    public final TestPipeline writePipeline = TestPipeline.create();
-    @Rule
-    public final TestPipeline readPipeline = TestPipeline.create();
+  @Rule public final TestPipeline writePipeline = TestPipeline.create();
+  @Rule public final TestPipeline readPipeline = TestPipeline.create();
 
-    /**
-     * InfluxDBIO options.
-     */
-    public interface InfluxDBPipelineOptions extends IOTestPipelineOptions {
-        @Description("InfluxDB host (host name/ip address)")
-        @Default.String("http://localhost:8086")
-        String getInfluxDBURL();
-        void setInfluxDBURL(String value);
+  /** InfluxDBIO options. */
+  public interface InfluxDBPipelineOptions extends IOTestPipelineOptions {
+    @Description("InfluxDB host (host name/ip address)")
+    @Default.String("http://localhost:8086")
+    String getInfluxDBURL();
 
-        @Description("Username for InfluxDB")
-        @Default.String("superadmin")
-        String getInfluxDBUserName();
-        void setInfluxDBUserName(String value);
+    void setInfluxDBURL(String value);
 
-        @Description("Password for InfluxDB")
-        @Default.String("supersecretpassword")
-        String getInfluxDBPassword();
-        void setInfluxDBPassword(String value);
+    @Description("Username for InfluxDB")
+    @Default.String("superadmin")
+    String getInfluxDBUserName();
 
-        @Description("InfluxDB database name")
-        @Default.String("db0")
-        String getDatabaseName();
-        void setDatabaseName(String value);
+    void setInfluxDBUserName(String value);
 
+    @Description("Password for InfluxDB")
+    @Default.String("supersecretpassword")
+    String getInfluxDBPassword();
+
+    void setInfluxDBPassword(String value);
+
+    @Description("InfluxDB database name")
+    @Default.String("db0")
+    String getDatabaseName();
+
+    void setDatabaseName(String value);
+  }
+
+  @BeforeClass
+  public static void setUp() {
+    PipelineOptionsFactory.register(InfluxDBPipelineOptions.class);
+    options = TestPipeline.testingPipelineOptions().as(InfluxDBPipelineOptions.class);
+  }
+
+  @After
+  public void clear() {
+    try (InfluxDB connection =
+        InfluxDBFactory.connect(
+            options.getInfluxDBURL(),
+            options.getInfluxDBUserName(),
+            options.getInfluxDBPassword())) {
+      connection.query(new Query("DROP DATABASE \"" + options.getDatabaseName() + "\""));
     }
-    @BeforeClass
-    public static void setUp() {
-        PipelineOptionsFactory.register(InfluxDBPipelineOptions.class);
-        options = TestPipeline.testingPipelineOptions().as(InfluxDBPipelineOptions.class);
+  }
+
+  @Before
+  public void initTest() {
+    try (InfluxDB connection =
+        InfluxDBFactory.connect(
+            options.getInfluxDBURL(),
+            options.getInfluxDBUserName(),
+            options.getInfluxDBPassword())) {
+      connection.query(new Query("CREATE DATABASE \"" + options.getDatabaseName() + "\""));
     }
+  }
 
-    @After
-    public  void clear(){
-        try (InfluxDB connection = InfluxDBFactory.connect(options.getInfluxDBURL(),
-                options.getInfluxDBUserName(), options.getInfluxDBPassword())){
-            connection.query(new Query("DROP DATABASE \"" + options.getDatabaseName() + "\""));
-        }
-        }
-    @Before
-    public  void initTest(){
-        try (InfluxDB connection = InfluxDBFactory.connect(options.getInfluxDBURL(),
-                options.getInfluxDBUserName(), options.getInfluxDBPassword())){
-            connection.query(new Query("CREATE DATABASE \"" + options.getDatabaseName() + "\""));
-        }
-    }
-    @Test
-    public void testWriteAndRead() {
-        final int noofElementsToReadAndWrite = 1000;
-        writePipeline
-                .apply("Generate data", Create.of(GenerateData.getMetric(noofElementsToReadAndWrite)))
-                .apply(
-                        "Write data to InfluxDB",
-                        InfluxDBIO.write()
-                                .withConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                        options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                .withDatabase(options.getDatabaseName())
-                                .withSslInvalidHostNameAllowed(false)
-                               .withSslEnabled(false));
-        writePipeline.run().waitUntilFinish();
-        PCollection<String> readVals =
-                readPipeline
-                        .apply(
-                                "Read all points in Influxdb",
-                                InfluxDBIO.read()
-                                        .withDataSourceConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                                options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                        .withDatabase(options.getDatabaseName()).withQuery("SELECT * FROM \"test_m\"")
-                                        .withSslInvalidHostNameAllowed(false)
-                                        .withSslEnabled(false));
+  @Test
+  public void testWriteAndRead() {
+    final int noofElementsToReadAndWrite = 1000;
+    writePipeline
+        .apply("Generate data", Create.of(GenerateData.getMetric(noofElementsToReadAndWrite)))
+        .apply(
+            "Write data to InfluxDB",
+            InfluxDBIO.write()
+                .withConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    writePipeline.run().waitUntilFinish();
+    PCollection<String> readVals =
+        readPipeline.apply(
+            "Read all points in Influxdb",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withQuery("SELECT * FROM \"test_m\"")
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
 
-        PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
-                .isEqualTo((long) noofElementsToReadAndWrite);
-        PipelineResult readResult = readPipeline.run();
-        readResult.waitUntilFinish();
-    }
+    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
+        .isEqualTo((long) noofElementsToReadAndWrite);
+    PipelineResult readResult = readPipeline.run();
+    readResult.waitUntilFinish();
+  }
 
-    @Test
-    public void testWriteAndReadWithSingleMetric() {
-        final int noofElementsToReadAndWrite = 1000;
-        writePipeline
-                .apply("Generate data", Create.of(GenerateData.getMetric(noofElementsToReadAndWrite)))
-                .apply(
-                        "Write data to InfluxDB",
-                        InfluxDBIO.write()
-                                .withConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                        options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                .withDatabase(options.getDatabaseName())
-                                .withSslInvalidHostNameAllowed(false)
-                                .withSslEnabled(false));
-        writePipeline.run().waitUntilFinish();
-        PCollection<String> readVals =
-                readPipeline
-                        .apply(
-                                "Read all points in InfluxDB",
-                                InfluxDBIO.read()
-                                        .withDataSourceConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                                options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                        .withDatabase(options.getDatabaseName()).withMetric(Arrays.asList("test_m"))
-                                        .withSslInvalidHostNameAllowed(false)
-                                        .withSslEnabled(false));
+  @Test
+  public void testWriteAndReadWithSingleMetric() {
+    final int noofElementsToReadAndWrite = 1000;
+    writePipeline
+        .apply("Generate data", Create.of(GenerateData.getMetric(noofElementsToReadAndWrite)))
+        .apply(
+            "Write data to InfluxDB",
+            InfluxDBIO.write()
+                .withConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    writePipeline.run().waitUntilFinish();
+    PCollection<String> readVals =
+        readPipeline.apply(
+            "Read all points in InfluxDB",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withMetric(Arrays.asList("test_m"))
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
 
-        PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
-                .isEqualTo((long) noofElementsToReadAndWrite);
-        PipelineResult readResult = readPipeline.run();
-        readResult.waitUntilFinish();
-    }
+    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
+        .isEqualTo((long) noofElementsToReadAndWrite);
+    PipelineResult readResult = readPipeline.run();
+    readResult.waitUntilFinish();
+  }
 
-    @Test
-    public void testWriteAndReadWithMultipleMetric() {
-        final int noofElementsToReadAndWrite = 1000;
-        writePipeline
-                .apply("Generate data", Create.of(GenerateData.getMultipleMetric(noofElementsToReadAndWrite)))
-                .apply(
-                        "Write data to InfluxDB",
-                        InfluxDBIO.write()
-                                .withConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                        options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                .withDatabase(options.getDatabaseName())
-                                .withSslInvalidHostNameAllowed(false)
-                                .withSslEnabled(false));
-        writePipeline.run().waitUntilFinish();
-        PCollection<String> readVals =
-                readPipeline
-                        .apply(
-                                "Read all points in InfluxDB",
-                                InfluxDBIO.read()
-                                        .withDataSourceConfiguration(InfluxDBIO.DataSourceConfiguration.create(options.getInfluxDBURL(),
-                                                options.getInfluxDBUserName(), options.getInfluxDBPassword()))
-                                        .withDatabase(options.getDatabaseName()).withMetric(Arrays.asList("test_m", "test_m1"))
-                                        .withSslInvalidHostNameAllowed(false)
-                                        .withSslEnabled(false));
+  @Test
+  public void testWriteAndReadWithMultipleMetric() {
+    final int noofElementsToReadAndWrite = 1000;
+    writePipeline
+        .apply(
+            "Generate data", Create.of(GenerateData.getMultipleMetric(noofElementsToReadAndWrite)))
+        .apply(
+            "Write data to InfluxDB",
+            InfluxDBIO.write()
+                .withConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    writePipeline.run().waitUntilFinish();
+    PCollection<String> readVals =
+        readPipeline.apply(
+            "Read all points in InfluxDB",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withMetric(Arrays.asList("test_m", "test_m1"))
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
 
-        PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
-                .isEqualTo((long) noofElementsToReadAndWrite*2);
-        PipelineResult readResult = readPipeline.run();
-        readResult.waitUntilFinish();
-    }
-
+    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
+        .isEqualTo((long) noofElementsToReadAndWrite * 2);
+    PipelineResult readResult = readPipeline.run();
+    readResult.waitUntilFinish();
+  }
 }
