f33d91644762ae548d9019daf38966a00fd92ca6
bipinupd <bipinupd@gmail.com>
2020-02-23 09:53:19 -0500

    Checkstyles, clean the build file and removed unused codes

--- sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/Model.java
+++ sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/Model.java
@@ -1,75 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.influxdb;
 
-import org.apache.beam.sdk.coders.AvroCoder;
-import org.apache.beam.sdk.coders.DefaultCoder;
-import org.influxdb.dto.Point;
-
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import org.influxdb.dto.Point;
 
-@DefaultCoder(AvroCoder.class)
-public class Model implements LineProtocolConvertable {
-    private String measurement;
-    private Map<String, String> tags;
-    private Map<String, Object> fields;
-    private Long time;
-    private TimeUnit timeUnit;
+public class Model implements LineProtocolConvertable, Serializable {
+  private String measurement;
+  private Map<String, String> tags;
+  private Map<String, Object> fields;
+  private Long time;
+  private TimeUnit timeUnit;
 
+  Model() {
+    tags = new HashMap();
+    fields = new HashMap();
+  }
 
-    Model() {
-        tags = new HashMap();
-        fields = new HashMap();
-    }
+  public Long getTime() {
+    return time;
+  }
 
+  public void setTime(Long time) {
+    this.time = time;
+  }
 
-        public Long getTime() {
-            return time;
-        }
+  public TimeUnit getTimeUnit() {
+    return timeUnit;
+  }
 
-        public void setTime(Long time) {
-            this.time = time;
-        }
+  public void setTimeUnit(TimeUnit timeUnit) {
+    this.timeUnit = timeUnit;
+  }
 
-        public TimeUnit getTimeUnit() {
-            return timeUnit;
-        }
+  public void addFeild(String name, Object value) {
+    fields.put(name, value);
+  }
 
-        public void setTimeUnit(TimeUnit timeUnit) {
-            this.timeUnit = timeUnit;
-        }
+  public void addTags(String name, String value) {
+    tags.put(name, value);
+  }
 
-        public void addFeild(String name, Object value){
-            fields.put(name,value);
-        }
-        public void addTags(String name, String value){
-            tags.put(name, value);
-        }
-        public String getMeasurement() {
-            return measurement;
-        }
+  public String getMeasurement() {
+    return measurement;
+  }
 
-        public void setMeasurement(String measurement) {
-            this.measurement = measurement;
-        }
+  public void setMeasurement(String measurement) {
+    this.measurement = measurement;
+  }
 
-        public Map<String, String> getTags() {
-            return tags;
-        }
+  public Map<String, String> getTags() {
+    return tags;
+  }
 
-        public void setTags(Map<String, String> tags) {
-            this.tags = tags;
-        }
+  public void setTags(Map<String, String> tags) {
+    this.tags = tags;
+  }
 
-        public Map<String, Object> getFields() {
-            return fields;
-        }
+  public Map<String, Object> getFields() {
+    return fields;
+  }
 
-        public void setFields(Map<String, Object> fields) {
-            this.fields = fields;
-        }
-        @Override
-        public String getLineProtocol() {
-            return Point.measurement(measurement).time(time,timeUnit).fields(fields).tag(tags).build().lineProtocol();
-        }
+  public void setFields(Map<String, Object> fields) {
+    this.fields = fields;
+  }
+
+  @Override
+  public String getLineProtocol() {
+    return Point.measurement(measurement)
+        .time(time, timeUnit)
+        .fields(fields)
+        .tag(tags)
+        .build()
+        .lineProtocol();
+  }
 }
