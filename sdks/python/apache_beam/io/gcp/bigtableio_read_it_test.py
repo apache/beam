7acbacdf087e146730150fda6d292edac66fe15b
mf2199 <38331387+mf2199@users.noreply.github.com>
2020-02-24 18:05:49 -0500

    [WIP]

--- sdks/python/apache_beam/io/gcp/bigtableio_read_it_test.py
+++ sdks/python/apache_beam/io/gcp/bigtableio_read_it_test.py
@@ -15,21 +15,20 @@
 # limitations under the License.
 #
 
-""" Integration test for GCP Bigtable testing."""
+""" Integration test for GC Bigtable connector [read]."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import apache_beam as beam
 import argparse
 import logging
 import unittest
 
-import apache_beam as beam
 from apache_beam.metrics.metric import MetricsFilter
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.runners.runner import PipelineState
-
 from nose.plugins.attrib import attr
 
 try:
@@ -42,85 +41,69 @@ except ImportError:
 from bigtableio_read import ReadFromBigtable
 
 
-@unittest.skipIf(Client is None, 'GCP Bigtable dependencies are not installed')
+@unittest.skipIf(Client is None, 'GC Bigtable dependencies are not installed')
 class BigtableReadTest(unittest.TestCase):
   """ Bigtable Read Connector Test
 
-  This tests the connector both ways, first writing rows to a new table,
-  then reading them and comparing the counters
+  This tests the ReadFromBigtable connector class via reading rows from
+  a Bigtable table and comparing the `Rows Read` metrics with the row
+  count known a priori.
   """
   def setUp(self):
-    logging.info('\nProject ID:  %s' % PROJECT_ID)
-    logging.info('\nInstance ID: %s' % INSTANCE_ID)
-    logging.info('\nTable ID:    %s' % TABLE_ID)
+    logging.info('\nProject ID:  {}'.format(options['project']))
+    logging.info('\nInstance ID: {}'.format(options['instance']))
+    logging.info('\nTable ID:    {}'.format(options['table']))
+
+    self._p_options = PipelineOptions(**options)
+    self._p_options.view_as(SetupOptions).save_main_session = True
+
+    # [OPTIONAL] Uncomment this to allow logging the pipeline options
+    # for key, value in self.p_options.get_all_options().items():
+    #   logging.info('Pipeline option {:32s} : {}'.format(key, value))
 
   @attr('IT')
   def test_bigtable_read(self):
+    logging.info('Reading table "{}" of {} rows...'
+                 .format(options['table'], options['row_count']))
 
-    p_options = PipelineOptions(
-      project=PROJECT_ID,
-      instance=INSTANCE_ID,
-      table=TABLE_ID,
-      runner=RUNNER,
-      region=REGION,
-      staging_location=STAGING_LOCATION,
-      temp_location=TEMP_LOCATION,
-      setup_file=SETUP_FILE,
-      extra_package=EXTRA_PACKAGE,
-      num_workers=NUM_WORKERS,
-      autoscaling_algorithm=AUTOSCALING_ALGORITHM,
-      disk_size_gb=DISK_SIZE_GB,
-      job_name=JOB_NAME,
-      log_level=LOG_LEVEL,
-    )
-    p_options.view_as(SetupOptions).save_smain_session = True
-
-    for key, value in p_options.get_all_options().items():
-      logging.info('Pipeline option {:32s} : {}'.format(key, value))
-
-    logging.info('Attempting to read from CBT table "{}"...'.format(TABLE_ID))
-
-    p = beam.Pipeline(options=p_options)
-
-    _ = (p | 'Read Test' >> ReadFromBigtable(project_id=PROJECT_ID,
-                                             instance_id=INSTANCE_ID,
-                                             table_id=TABLE_ID))
-
+    p = beam.Pipeline(options=self._p_options)
+    _ = (p | 'Read Test' >> ReadFromBigtable(project_id=options['project'],
+                                             instance_id=options['instance'],
+                                             table_id=options['table'],
+                                             filter_=options['filter']))
     self.result = p.run()
     self.result.wait_until_finish()
     assert self.result.state == PipelineState.DONE
 
-    query_result = self.result.metrics()\
-      .query(MetricsFilter().with_name('Rows Read'))
+    query_result = self.result.metrics().query(
+      MetricsFilter().with_name('Rows Read'))
+
     if query_result['counters']:
       read_counter = query_result['counters'][0]
       final_count = read_counter.committed
-      assert final_count == ROW_COUNT
-      logging.info('{} rows were read successfully'.format(final_count))
+      assert final_count == options['row_count']
+      logging.info('{} out of {} rows were read successfully.'
+                   .format(final_count, options['row_count']))
 
-    logging.info('ROW_COUNT = {}'.format(ROW_COUNT))
     logging.info('DONE!')
 
-    print('READ test complete.')
 
-
-if __name__ == '__main__':
+def parse_commane_line_arguments():
   parser = argparse.ArgumentParser()
 
   parser.add_argument('--project', type=str)
   parser.add_argument('--instance', type=str)
   parser.add_argument('--table', type=str)
+  parser.add_argument('--filter', type=str, default=None)
   parser.add_argument('--region', type=str)
   parser.add_argument('--setup_file', type=str)
   parser.add_argument('--extra_package', type=str)
   parser.add_argument('--staging_location', type=str)
   parser.add_argument('--temp_location', type=str)
-
   parser.add_argument('--runner', type=str, default='dataflow')
   parser.add_argument('--num_workers', type=int, default=50)
   parser.add_argument('--autoscaling_algorithm', type=str, default='NONE')
   parser.add_argument('--disk_size_gb', type=int, default=50)
-
   parser.add_argument('--job_name', type=str, default='bigtable-read-it-test')
   parser.add_argument('--row_count', type=int)
   parser.add_argument('--log_level', type=int, default=logging.INFO)
@@ -128,41 +111,48 @@ if __name__ == '__main__':
 
   args, argv = parser.parse_known_args()
 
-  PROJECT_ID = args.project
-  INSTANCE_ID = args.instance
-  TABLE_ID = args.table
-  REGION = args.region
-  STAGING_LOCATION = args.staging_location
-  TEMP_LOCATION = args.temp_location
+  return {
+    'project' : args.project,
+    'instance' : args.instance,
+    'table' : args.table,
+    'filter' : args.filter,
+    'region' : args.region,
+    'staging_location' : args.staging_location,
+    'temp_location' : args.temp_location,
+    'setup_file' : args.setup_file,
+    'extra_package' : args.extra_package,
+    'runner' : args.runner,
+    'num_workers' : args.num_workers,
+    'autoscaling_algorithm' : args.autoscaling_algorithm,
+    'disk_size_gb' : args.disk_size_gb,
+    'row_count' : args.row_count,
+    'job_name' : args.job_name,
+    'log_level' : args.log_level,
+    'log_directory' : args.log_dir,
+  }
 
-  RUNNER = args.runner
-  NUM_WORKERS = args.num_workers
-  AUTOSCALING_ALGORITHM = args.autoscaling_algorithm
-  DISK_SIZE_GB = args.disk_size_gb
+def setup_log_file():
+  if options['log_directory']:
+    # logging.basicConfig(
+    #   filename='{}{}.log'.format(options['log_directory'], options['table']),
+    #   filemode='w',
+    #   level=logging.DEBUG
+    # )
 
-  ROW_COUNT = args.row_count
-  SETUP_FILE = args.setup_file
-  EXTRA_PACKAGE = args.extra_package
+    # Forward all the logs to a file
+    fh = logging.FileHandler(
+      filename='{}test_log_{}_RUNNER={}.log'.format(options['log_directory'],
+                                                    options['table'][-15:],
+                                                    options['runner']))
+    fh.setLevel(logging.DEBUG)
+    logging.getLogger().addHandler(fh)
 
-  JOB_NAME = args.job_name
-  LOG_LEVEL = args.log_level
-  LOG_DIR = args.log_dir
+if __name__ == '__main__':
+  options = parse_commane_line_arguments()
 
-  logging.getLogger().setLevel(LOG_LEVEL)
-
-  # [OPTIONAL] Uncomment the following lines to save the log data into a file
-  # if LOG_DIR:
-  #   # logging.basicConfig(filename='{}{}.log'.format(args.log_dir, TABLE_ID),
-  #   #                     filemode='w',
-  #   #                     level=logging.DEBUG)
-  #
-  #   # Forward all logs to a file
-  #   fh = logging.FileHandler(
-  #     filename='{}test_log_{}_RUNNER={}.log'.format(args.log_dir,
-  #                                                   TABLE_ID[-15:],
-  #                                                   args.runner))
-  #   fh.setLevel(logging.DEBUG)
-  #   logging.getLogger().addHandler(fh)
+  logging.getLogger().setLevel(options['log_level'])
+  # [OPTIONAL] Uncomment to save logs into a file
+  # setup_log_file()
 
   test_suite = unittest.TestSuite()
   test_suite.addTest(BigtableReadTest('test_bigtable_read'))
