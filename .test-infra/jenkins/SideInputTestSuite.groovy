4072d9d779a775670e997c8c729715db6833e75d
Kamil Wasilewski <kamil.wasilewski@polidea.com>
2020-03-18 13:12:41 +0100

    [BEAM-7505] Create SideInput Python Jenkins jobs

---
+++ .test-infra/jenkins/SideInputTestSuite.groovy
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Sextend(template)tware Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy extend(template) the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, sextend(template)tware
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import LoadTestConfig
+
+
+class SideInputTestSuite {
+    static def configurations = { LoadTestConfig template -> [
+        LoadTestConfig.extend(template) {
+            title 'SideInput 2MB 100 byte records: global window'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 20000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'iter',
+                    side_input_size: 2000,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 2MB 100 byte records: 1000 windows'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 20000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'iter',
+                    side_input_size: 2000,
+                    window_count: 1000,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 byte records: small list'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 2000000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'list',
+                    side_input_size: 2000,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 byte records: large list, 16 workers'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 2000000
+                    keySize 10
+                    valueSize 90
+                }
+                numWorkers 16
+                parallelism 16
+                specificParameters([
+                    side_input_type: 'list',
+                    side_input_size: 20000,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 byte records: large list'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 2000000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'list',
+                    side_input_size: 20000,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 kilobyte records: large keys'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 200
+                    keySize 100000
+                    valueSize 900000
+                }
+                specificParameters([
+                    side_input_type: 'iter',
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 byte records: dictionary 1% random lookup'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 2000000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'dict',
+                    side_input_size: 2000,
+                    access_percentage: 1,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 200MB 100 byte records: dictionary 99% random lookup'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 2000000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'dict',
+                    side_input_size: 2000,
+                    access_percentage: 99,
+                ])
+            }
+        },
+        LoadTestConfig.extend(template) {
+            title 'SideInput 2MB 100 byte records: iterable 1% first lookup'
+            pipelineOptions {
+                inputOptions {
+                    numRecords 20000
+                    keySize 10
+                    valueSize 90
+                }
+                specificParameters([
+                    side_input_type: 'iter',
+                    side_input_size: 2000,
+                    access_percentage: 1,
+                ])
+            }
+        },
+    ]}
+}
---
+++ .test-infra/jenkins/job_LoadTests_SideInput_Python.groovy
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import CommonJobProperties as commonJobProperties
+import LoadTestsBuilder
+import PhraseTriggeringPostCommitBuilder
+import LoadTestConfig
+import SideInputTestSuite
+
+
+LoadTestConfig template = LoadTestConfig.templateConfig {
+    test 'apache_beam.testing.load_tests.sideinput_test'
+    dataflow()
+    pipelineOptions {
+        python()
+        project 'apache-beam-testing'
+        publishToBigQuery true
+        numWorkers 5
+        autoscalingAlgorithm 'NONE'
+        tempLocation 'gs://temp-storage-for-perf-tests/loadtests'
+    }
+}
+
+def batchLoadTestJob = { scope, triggeringContext ->
+    String datasetName = LoadTestsBuilder.getBigQueryDataset('load_test', triggeringContext)
+    def configurations = SideInputTestSuite.configurations(template)
+    def now = new Date().format("MMddHHmmss", TimeZone.getTimeZone('UTC'))
+
+    def finalConfigurations = []
+    configurations.eachWithIndex { configuration, i ->
+        finalConfigurations.add(
+            LoadTestConfig.fromTemplate(configuration) {
+                pipelineOptions {
+                    jobName "load-tests-python-dataflow-batch-sideinput-${i + 1}-" + now
+                    metricsDataset datasetName
+                    metricsTable "python_dataflow_batch_sideinput_${i + 1}"
+                }
+            }
+        )
+    }
+
+    LoadTestsBuilder.loadTests(scope, CommonTestProperties.SDK.PYTHON, finalConfigurations, 'SideInput', 'batch')
+}
+
+PhraseTriggeringPostCommitBuilder.postCommitJob(
+        'beam_LoadTests_Python_SideInput_Dataflow_Batch',
+        'Run Python Load Tests SideInput Dataflow Batch',
+        'Load Tests Python SideInput Dataflow Batch suite',
+        this
+) {
+    batchLoadTestJob(delegate, CommonTestProperties.TriggeringContext.PR)
+}
+
+CronJobBuilder.cronJob('beam_LoadTests_Python_SideInput_Dataflow_Batch', 'H 11 * * *', this) {
+    batchLoadTestJob(delegate, CommonTestProperties.TriggeringContext.POST_COMMIT)
+}
