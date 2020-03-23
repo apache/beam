a2b611a332cdbeb59f78a6c18d2c93e5c667dbc2
KevinGG <kawaigin@gmail.com>
2020-03-23 12:56:29 -0700

    Reuse the obfuscated ids in display ids.

--- sdks/python/apache_beam/runners/interactive/display/pcoll_visualization.py
+++ sdks/python/apache_beam/runners/interactive/display/pcoll_visualization.py
@@ -247,11 +247,10 @@ class PCollectionVisualization(object):
     if not self._pcoll_var:
       self._pcoll_var = 'Value'
     self._cache_key = self._pin.cache_key(self._pcoll)
-    self._dive_display_id = 'facets_dive_{}'.format(
-        obfuscate(self._cache_key, id(self)))
-    self._overview_display_id = 'facets_overview_{}'.format(
-        obfuscate(self._cache_key, id(self)))
-    self._df_display_id = 'df_{}'.format(obfuscate(self._cache_key, id(self)))
+    obfuscated_id = obfuscate(self._cache_key, id(self))
+    self._dive_display_id = 'facets_dive_{}'.format(obfuscated_id)
+    self._overview_display_id = 'facets_overview_{}'.format(obfuscated_id)
+    self._df_display_id = 'df_{}'.format(obfuscated_id)
     self._include_window_info = include_window_info
     self._display_facets = display_facets
     self._is_datatable_empty = True
