4381e750ee3d37058865b0e6ed397b93633871b1
Aleksei Vysotin <aleksei_vysotin@epam.com>
2020-03-23 21:11:16 +0300

    Added save_main_session option as was hard-coded

--- sdks/python/apache_beam/examples/streaming_wordcount_debugging.py
+++ sdks/python/apache_beam/examples/streaming_wordcount_debugging.py
@@ -87,7 +87,7 @@ class AddTimestampFn(beam.DoFn):
     yield beam.window.TimestampedValue(element, elem)
 
 
-def run(argv=None):
+def run(argv=None, save_main_session=True):
   """Build and run the pipeline."""
   parser = argparse.ArgumentParser()
   parser.add_argument(
@@ -112,7 +112,7 @@ def run(argv=None):
   # We use the save_main_session option because one or more DoFn's in this
   # workflow rely on global context (e.g., a module imported at module level).
   pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = True
+  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
   pipeline_options.view_as(StandardOptions).streaming = True
   with beam.Pipeline(options=pipeline_options) as p:
 
