71b84f8a02def4931e6fe938291078fb546ad6f5
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2020-03-23 20:55:38 -0700

    [BEAM-9579] Fix numpy logic operators

--- sdks/python/apache_beam/transforms/stats.py
+++ sdks/python/apache_beam/transforms/stats.py
@@ -430,11 +430,11 @@ class ApproximateQuantilesCombineFn(CombineFn):
   def __init__(
       self, num_quantiles, buffer_size, num_buffers, key=None, reverse=False):
     if key:
-      self._comparator = lambda a, b: (key(a) < key(b)) - (key(a) > key(b)) \
-        if reverse else (key(a) > key(b)) - (key(a) < key(b))
+      self._comparator = lambda a, b: (key(a) < key(b)) ^ (key(a) > key(b)) \
+        if reverse else (key(a) > key(b)) ^ (key(a) < key(b))
     else:
-      self._comparator = lambda a, b: (a < b) - (a > b) if reverse \
-        else (a > b) - (a < b)
+      self._comparator = lambda a, b: (a < b) ^ (a > b) if reverse \
+        else (a > b) ^ (a < b)
 
     self._num_quantiles = num_quantiles
     self._buffer_size = buffer_size
