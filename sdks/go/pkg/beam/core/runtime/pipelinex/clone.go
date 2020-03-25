0a565d6e976629abee656cea366dbe5b34c27850
Robert Bradshaw <robertwb@google.com>
2020-03-25 09:08:10 -0700

    Go changes for model updates. (#11211)

--- sdks/go/pkg/beam/core/runtime/pipelinex/clone.go
+++ sdks/go/pkg/beam/core/runtime/pipelinex/clone.go
@@ -64,10 +64,8 @@ func ShallowCloneParDoPayload(p *pb.ParDoPayload) *pb.ParDoPayload {
 
 	ret := &pb.ParDoPayload{
 		DoFn:               p.DoFn,
-		Splittable:         p.Splittable,
 		RestrictionCoderId: p.RestrictionCoderId,
 	}
-	ret.Parameters, _ = reflectx.ShallowClone(p.Parameters).([]*pb.Parameter)
 	ret.SideInputs, _ = reflectx.ShallowClone(p.SideInputs).(map[string]*pb.SideInput)
 	ret.StateSpecs, _ = reflectx.ShallowClone(p.StateSpecs).(map[string]*pb.StateSpec)
 	ret.TimerSpecs, _ = reflectx.ShallowClone(p.TimerSpecs).(map[string]*pb.TimerSpec)
