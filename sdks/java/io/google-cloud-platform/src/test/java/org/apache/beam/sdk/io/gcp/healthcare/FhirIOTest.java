b4bc60e2a52934fefbdb99c0e719a9c43c00bbe4
Jacob Ferriero <jferriero@google.com>
2020-04-09 18:19:48 -0700

    fhirio test

---
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/healthcare/FhirIOTest.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.io.gcp.healthcare;
+
+import com.google.api.services.healthcare.v1beta1.model.HttpBody;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FhirIOTest {
+  @Rule public final transient TestPipeline pipeline = TestPipeline.create();
+
+  @Test
+  public void test_FhirIO_failedReads() {
+    List<String> badMessageIDs = Arrays.asList("foo", "bar");
+    FhirIO.Read.Result readResult =
+        pipeline.apply(Create.of(badMessageIDs)).apply(FhirIO.readResources());
+
+    PCollection<HealthcareIOError<String>> failed = readResult.getFailedReads();
+
+    PCollection<HttpBody> messages = readResult.getResources();
+
+    PCollection<String> failedMsgIds =
+        failed.apply(
+            MapElements.into(TypeDescriptors.strings()).via(HealthcareIOError::getDataResource));
+
+    PAssert.that(failedMsgIds).containsInAnyOrder(badMessageIDs);
+    PAssert.that(messages).empty();
+    pipeline.run();
+  }
+
+  @Test
+  public void test_FhirIO_failedWrites() {
+    HttpBody badBundle = new HttpBody().setData("bad");
+    List<HttpBody> emptyMessages = Collections.singletonList(badBundle);
+
+    PCollection<HttpBody> fhirBundles =
+        pipeline.apply(Create.of(emptyMessages).withCoder(new HttpBodyCoder()));
+
+    FhirIO.Write.Result writeResult =
+        fhirBundles.apply(
+            FhirIO.Write.executeBundles(
+                "projects/foo/locations/us-central1/datasets/bar/hl7V2Stores/baz"));
+
+    PCollection<HealthcareIOError<HttpBody>> failedInserts =
+        writeResult.getFailedInsertsWithErr();
+
+    PAssert.thatSingleton(failedInserts)
+        .satisfies((HealthcareIOError<HttpBody> err) ->{
+            Assert.assertEquals("bad", err.getDataResource().getData());
+          return null;
+        });
+    PCollection<Long> numFailedInserts = failedInserts.apply(Count.globally());
+
+    PAssert.thatSingleton(numFailedInserts).isEqualTo(1L);
+
+    pipeline.run();
+  }
+}
