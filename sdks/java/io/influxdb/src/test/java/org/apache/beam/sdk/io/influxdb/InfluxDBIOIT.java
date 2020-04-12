f2912618fb1ceae41efb648114f07eecb793c5f1
bipinupd <bipinupd@gmail.com>
2020-04-12 14:39:47 -0400

    Addressing the comments

--- sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/InfluxDBIOIT.java
+++ sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/InfluxDBIOIT.java
@@ -1,56 +1,38 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.beam.sdk.io.influxdb;
 
 import java.util.Arrays;
-import org.apache.beam.sdk.PipelineResult;
+import java.util.Collections;
 import org.apache.beam.sdk.io.common.IOTestPipelineOptions;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.PCollection;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Query;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
  * A test of {@link org.apache.beam.sdk.io.influxdb.InfluxDBIO} on an independent InfluxDB instance.
+ * Run the docker container using the following command
  *
- * <p>This test requires a running instance of InfluxDB. Pass in connection information using
- * PipelineOptions:
+ * <pre>
+ * docker run -p 8086:8086 -p 2003:2003 -p 8083:8083 -e INFLUXDB_GRAPHITE_ENABLED=true -e INFLUXDB_USER=supersadmin -e INFLUXDB_USER_PASSWORD=supersecretpassword influxdb
+ * </pre>
+ *
+ * <p>This test requires a running instance of InfluxDB.
  *
  * <pre>
  *  ./gradlew integrationTest -p sdks/java/io/influxdb -DintegrationTestPipelineOptions='[
  *  "--influxdburl=http://localhost:8086",
  *  "--infuxDBDatabase=mypass",
- *  "--username=username"
- *  "--password=password"]'
+ *  "--username=supersadmin"
+ *  "--password=supersecretpassword"]'
  *  --tests org.apache.beam.sdk.io.influxdb.InfluxDBIOIT
  *  -DintegrationTestRunner=direct
  * </pre>
@@ -84,7 +66,7 @@ public class InfluxDBIOIT {
     void setInfluxDBPassword(String value);
 
     @Description("InfluxDB database name")
-    @Default.String("db0")
+    @Default.String("db3")
     String getDatabaseName();
 
     void setDatabaseName(String value);
@@ -114,7 +96,21 @@ public class InfluxDBIOIT {
             options.getInfluxDBURL(),
             options.getInfluxDBUserName(),
             options.getInfluxDBPassword())) {
-      connection.query(new Query("CREATE DATABASE \"" + options.getDatabaseName() + "\""));
+      connection.query(new Query(String.format("CREATE DATABASE %s", options.getDatabaseName())));
+    }
+  }
+
+  private void createRetentionPolicyInDB(String dbName, String retentionPolicyName) {
+    try (InfluxDB connection =
+        InfluxDBFactory.connect(
+            options.getInfluxDBURL(),
+            options.getInfluxDBUserName(),
+            options.getInfluxDBPassword())) {
+      connection.query(
+          new Query(
+              String.format(
+                  "CREATE RETENTION POLICY %s ON %s DURATION 1d REPLICATION 1",
+                  retentionPolicyName, dbName)));
     }
   }
 
@@ -135,7 +131,7 @@ public class InfluxDBIOIT {
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
     writePipeline.run().waitUntilFinish();
-    PCollection<String> readVals =
+    PCollection<String> values =
         readPipeline.apply(
             "Read all points in Influxdb",
             InfluxDBIO.read()
@@ -149,17 +145,16 @@ public class InfluxDBIOIT {
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
 
-    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
+    PAssert.thatSingleton(values.apply("Count All", Count.globally()))
         .isEqualTo((long) noofElementsToReadAndWrite);
-    PipelineResult readResult = readPipeline.run();
-    readResult.waitUntilFinish();
+    readPipeline.run().waitUntilFinish();
   }
 
   @Test
   public void testWriteAndReadWithSingleMetric() {
-    final int noofElementsToReadAndWrite = 1000;
+    final int noOfElementsToReadAndWrite = 2000;
     writePipeline
-        .apply("Generate data", Create.of(GenerateData.getMetric(noofElementsToReadAndWrite)))
+        .apply("Generate data", Create.of(GenerateData.getMetric(noOfElementsToReadAndWrite)))
         .apply(
             "Write data to InfluxDB",
             InfluxDBIO.write()
@@ -172,7 +167,7 @@ public class InfluxDBIOIT {
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
     writePipeline.run().waitUntilFinish();
-    PCollection<String> readVals =
+    PCollection<String> values =
         readPipeline.apply(
             "Read all points in InfluxDB",
             InfluxDBIO.read()
@@ -182,22 +177,21 @@ public class InfluxDBIOIT {
                         options.getInfluxDBUserName(),
                         options.getInfluxDBPassword()))
                 .withDatabase(options.getDatabaseName())
-                .withMetric(Arrays.asList("test_m"))
+                .withMetrics(Collections.singletonList("test_m"))
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
 
-    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
-        .isEqualTo((long) noofElementsToReadAndWrite);
-    PipelineResult readResult = readPipeline.run();
-    readResult.waitUntilFinish();
+    PAssert.thatSingleton(values.apply("Count All", Count.globally()))
+        .isEqualTo((long) noOfElementsToReadAndWrite);
+    readPipeline.run().waitUntilFinish();
   }
 
   @Test
   public void testWriteAndReadWithMultipleMetric() {
-    final int noofElementsToReadAndWrite = 1000;
+    final int noOfElementsToReadAndWrite = 2000;
     writePipeline
         .apply(
-            "Generate data", Create.of(GenerateData.getMultipleMetric(noofElementsToReadAndWrite)))
+            "Generate data", Create.of(GenerateData.getMultipleMetric(noOfElementsToReadAndWrite)))
         .apply(
             "Write data to InfluxDB",
             InfluxDBIO.write()
@@ -210,7 +204,7 @@ public class InfluxDBIOIT {
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
     writePipeline.run().waitUntilFinish();
-    PCollection<String> readVals =
+    PCollection<String> values =
         readPipeline.apply(
             "Read all points in InfluxDB",
             InfluxDBIO.read()
@@ -220,13 +214,105 @@ public class InfluxDBIOIT {
                         options.getInfluxDBUserName(),
                         options.getInfluxDBPassword()))
                 .withDatabase(options.getDatabaseName())
-                .withMetric(Arrays.asList("test_m", "test_m1"))
+                .withMetrics(Arrays.asList("test_m", "test_m1"))
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    PAssert.thatSingleton(values.apply("Count All", Count.globally()))
+        .isEqualTo((long) noOfElementsToReadAndWrite * 2);
+    readPipeline.run().waitUntilFinish();
+  }
+
+  @Test
+  public void testWriteAndReadWithSingleMetricWithCustomRetentionPolicy() {
+    final int noOfElementsToReadAndWrite = 1000;
+    createRetentionPolicyInDB(options.getDatabaseName(), "test_retention");
+    writePipeline
+        .apply("Generate data", Create.of(GenerateData.getMetric(noOfElementsToReadAndWrite)))
+        .apply(
+            "Write data to InfluxDB",
+            InfluxDBIO.write()
+                .withConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withRetentionPolicy("test_retention")
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    writePipeline.run().waitUntilFinish();
+    PCollection<String> values =
+        readPipeline.apply(
+            "Read all points in InfluxDB",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withRetentionPolicy("test_retention")
+                .withMetrics(Collections.singletonList("test_m"))
                 .withSslInvalidHostNameAllowed(false)
                 .withSslEnabled(false));
 
-    PAssert.thatSingleton(readVals.apply("Count All", Count.globally()))
-        .isEqualTo((long) noofElementsToReadAndWrite * 2);
-    PipelineResult readResult = readPipeline.run();
-    readResult.waitUntilFinish();
+    PAssert.thatSingleton(values.apply("Count All", Count.globally()))
+        .isEqualTo((long) noOfElementsToReadAndWrite);
+    readPipeline.run().waitUntilFinish();
+  }
+
+  @Test
+  public void testWriteAndReadWithSQLForMultipleMetric() {
+    final int noOfElementsToReadAndWrite = 2000;
+    createRetentionPolicyInDB(options.getDatabaseName(), "test_rp");
+    writePipeline
+        .apply(
+            "Generate data", Create.of(GenerateData.getMultipleMetric(noOfElementsToReadAndWrite)))
+        .apply(
+            "Write data to InfluxDB",
+            InfluxDBIO.write()
+                .withConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withSslInvalidHostNameAllowed(false)
+                .withRetentionPolicy("test_rp")
+                .withSslEnabled(false));
+    writePipeline.run().waitUntilFinish();
+    PCollection<String> valuesFortest_m =
+        readPipeline.apply(
+            "Read all points in InfluxDB For test_m metric",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withMetrics(Collections.singletonList("test_m"))
+                .withRetentionPolicy("test_rp")
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    PCollection<String> valuesFortest_m1 =
+        readPipeline.apply(
+            "Read all points in InfluxDB For test_m1 metric",
+            InfluxDBIO.read()
+                .withDataSourceConfiguration(
+                    InfluxDBIO.DataSourceConfiguration.create(
+                        options.getInfluxDBURL(),
+                        options.getInfluxDBUserName(),
+                        options.getInfluxDBPassword()))
+                .withDatabase(options.getDatabaseName())
+                .withMetrics(Collections.singletonList("test_m1"))
+                .withRetentionPolicy("test_rp")
+                .withSslInvalidHostNameAllowed(false)
+                .withSslEnabled(false));
+    PAssert.thatSingleton(valuesFortest_m1.apply("Count All For test_m1 metric", Count.globally()))
+        .isEqualTo((long) noOfElementsToReadAndWrite);
+    PAssert.thatSingleton(valuesFortest_m.apply("Count All For test_m metric", Count.globally()))
+        .isEqualTo((long) noOfElementsToReadAndWrite);
+    readPipeline.run().waitUntilFinish();
   }
 }
--- sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/Model.java
+++ sdks/java/io/influxdb/src/test/java/org/apache/beam/sdk/io/influxdb/Model.java
@@ -1,29 +1,14 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.beam.sdk.io.influxdb;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.coders.DefaultCoder;
 import org.influxdb.dto.Point;
 
-class Model implements LineProtocolConvertable, Serializable {
+@DefaultCoder(AvroCoder.class)
+class Model implements LineProtocolConvertable {
   private String measurement;
   private Map<String, String> tags;
   private Map<String, Object> fields;
@@ -31,8 +16,8 @@ class Model implements LineProtocolConvertable, Serializable {
   private TimeUnit timeUnit;
 
   Model() {
-    tags = new HashMap();
-    fields = new HashMap();
+    tags = new HashMap<>();
+    fields = new HashMap<>();
   }
 
   public Long getTime() {
