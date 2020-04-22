e3749c8a99d6af03fca1c2112508550c61eb646e
Robert Bradshaw <robertwb@gmail.com>
2020-04-21 17:13:00 -0700

    [BEAM-9577] Ensure required directories exist when staging.

--- sdks/go/pkg/beam/artifact/materialize.go
+++ sdks/go/pkg/beam/artifact/materialize.go
@@ -128,6 +128,11 @@ func (a artifact) retrieve(ctx context.Context, dest string) error {
 		return errors.Wrapf(err, "failed to stat %v", filename)
 	}
 
+	err = os.MkdirAll(filepath.Dir(filename), os.ModePerm)
+	if err != nil {
+		return err
+	}
+
 	stream, err := a.client.GetArtifact(ctx, &jobpb.GetArtifactRequest{Artifact: a.dep})
 	if err != nil {
 		return err
