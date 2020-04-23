ee14564c84f9c947ad305bbebbaad5970ec9befd
KevinGG <kawaigin@gmail.com>
2020-04-23 14:21:05 -0700

    fix lint

--- sdks/python/apache_beam/runners/interactive/testing/integration/screen_diff.py
+++ sdks/python/apache_beam/runners/interactive/testing/integration/screen_diff.py
@@ -45,7 +45,6 @@ try:
   from needle.driver import NeedleChrome
   from selenium.webdriver.chrome.options import Options
   from selenium.webdriver.common.by import By
-  from selenium.webdriver.common.keys import Keys
   from selenium.webdriver.support import expected_conditions
   from selenium.webdriver.support.ui import WebDriverWait
   _interactive_integration_ready = (
