214acb197783918ad75cf30f898d47d95304c77e
Maximilian Michels <mxm@apache.org>
2020-04-27 12:15:15 +0200

    [BEAM-9827] Ensure minimum watermark hold is computed across all keys
    For computing the current output watermark we need to compute the minimum
    watermark hold across all watermark holds. We maintain an in-memory view of all
    watermark holds because iterating over all keys is an expensive operation which
    we only want to do when restoring from a checkpoint, not on every watermark.
    
    The watermark view may not be in sync with its backing managed state. This
    change corrects this by maintaining a TreeMap with all watermark holds and
    adding additional tests.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
@@ -711,8 +711,8 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
     if (keyCoder == null) {
       potentialOutputWatermark = inputWatermarkHold;
     } else {
-      Instant watermarkHold = keyedStateInternals.watermarkHold();
-      long combinedWatermarkHold = Math.min(watermarkHold.getMillis(), inputWatermarkHold);
+      long combinedWatermarkHold =
+          Math.min(keyedStateInternals.minWatermarkHoldMs(), inputWatermarkHold);
       potentialOutputWatermark =
           Math.min(combinedWatermarkHold, timerInternals.getMinOutputTimestampMs());
     }
@@ -1208,6 +1208,7 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
       while ((internalTimer = processingTimeTimersQueue.poll()) != null) {
         keyedStateBackend.setCurrentKey(internalTimer.getKey());
         TimerData timer = internalTimer.getNamespace();
+        checkInvokeStartBundle();
         fireTimer(timer);
       }
     }
