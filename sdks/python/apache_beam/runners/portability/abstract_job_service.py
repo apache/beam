e7371d56e7163a0832e1d44ba8deddd0135b9819
Robert Bradshaw <robertwb@gmail.com>
2020-05-01 12:09:56 -0700

    [BEAM-9577] Artifact v2 support for uber jars.

--- sdks/python/apache_beam/runners/portability/abstract_job_service.py
+++ sdks/python/apache_beam/runners/portability/abstract_job_service.py
@@ -18,6 +18,7 @@
 
 from __future__ import absolute_import
 
+import copy
 import itertools
 import json
 import logging
@@ -278,6 +279,23 @@ class AbstractBeamJob(object):
         state=self.state)
 
 
+class JarArtifactManager(object):
+  def __init__(self, jar_path, root):
+    self._root = root
+    self._zipfile_handle = zipfile.ZipFile(jar_path, 'a')
+
+  def close(self):
+    self._zipfile_handle.close()
+
+  def file_writer(self, path):
+    full_path = '%s/%s' % (self._root, path)
+    return self._zipfile_handle.open(
+        full_path, 'w', force_zip64=True), 'classpath://%s' % full_path
+
+  def zipfile_handle(self):
+    return self._zipfile_handle
+
+
 class UberJarBeamJob(AbstractBeamJob):
   """Abstract baseclass for creating a Beam job. The resulting job will be
   packaged and run in an executable uber jar."""
@@ -291,8 +309,6 @@ class UberJarBeamJob(AbstractBeamJob):
   PIPELINE_PATH = '/'.join([PIPELINE_FOLDER, PIPELINE_NAME, "pipeline.json"])
   PIPELINE_OPTIONS_PATH = '/'.join(
       [PIPELINE_FOLDER, PIPELINE_NAME, 'pipeline-options.json'])
-  ARTIFACT_MANIFEST_PATH = '/'.join(
-      [PIPELINE_FOLDER, PIPELINE_NAME, 'artifact-manifest.json'])
   ARTIFACT_FOLDER = '/'.join([PIPELINE_FOLDER, PIPELINE_NAME, 'artifacts'])
 
   def __init__(
@@ -313,39 +329,58 @@ class UberJarBeamJob(AbstractBeamJob):
     with tempfile.NamedTemporaryFile(suffix='.jar') as tout:
       self._jar = tout.name
     shutil.copy(self._executable_jar, self._jar)
-    with zipfile.ZipFile(self._jar, 'a', compression=zipfile.ZIP_DEFLATED) as z:
-      with z.open(self.PIPELINE_PATH, 'w') as fout:
-        fout.write(
-            json_format.MessageToJson(self._pipeline_proto).encode('utf-8'))
-      with z.open(self.PIPELINE_OPTIONS_PATH, 'w') as fout:
-        fout.write(
-            json_format.MessageToJson(self._pipeline_options).encode('utf-8'))
-      with z.open(self.PIPELINE_MANIFEST, 'w') as fout:
-        fout.write(
-            json.dumps({
-                'defaultJobName': self.PIPELINE_NAME
-            }).encode('utf-8'))
     self._start_artifact_service(self._jar, self._artifact_port)
 
   def _start_artifact_service(self, jar, requested_port):
-    self._artifact_staging_service = artifact_service.ZipFileArtifactService(
-        jar, self.ARTIFACT_FOLDER)
+    self._artifact_manager = JarArtifactManager(self._jar, self.ARTIFACT_FOLDER)
+    self._artifact_staging_service = artifact_service.ArtifactStagingService(
+        self._artifact_manager.file_writer)
+    self._artifact_staging_service.register_job(
+        self._job_id,
+        {env_id: env.dependencies
+         for (env_id, env) in self._pipeline_proto.components.environments.items()})
     self._artifact_staging_server = grpc.server(futures.ThreadPoolExecutor())
     port = self._artifact_staging_server.add_insecure_port(
         '[::]:%s' % requested_port)
-    beam_artifact_api_pb2_grpc.add_LegacyArtifactStagingServiceServicer_to_server(
+    beam_artifact_api_pb2_grpc.add_ArtifactStagingServiceServicer_to_server(
         self._artifact_staging_service, self._artifact_staging_server)
     self._artifact_staging_endpoint = endpoints_pb2.ApiServiceDescriptor(
         url='localhost:%d' % port)
     self._artifact_staging_server.start()
     _LOGGER.info('Artifact server started on port %s', port)
+    _LOGGER.error('Artifact server started on port %s', port)
     return port
 
   def _stop_artifact_service(self):
     self._artifact_staging_server.stop(1)
-    self._artifact_staging_service.close()
-    self._artifact_manifest_location = (
-        self._artifact_staging_service.retrieval_token(self._job_id))
+    self._artifact_manifest_location = None
+
+    # Update dependencies to point to staged files.
+    pipeline = copy.copy(self._pipeline_proto)
+    if any(
+        env.dependencies for env in pipeline.components.environments.values()):
+      for env_id, deps in self._artifact_staging_service.resolved_deps(self._job_id).items():
+        # Slice assignment not supported for repeated fields.
+        env = self._pipeline_proto.components.environments[env_id]
+        del env.dependencies[:]
+        env.dependencies.extend(deps)
+
+    # Copy the pipeline definition and metadata into the jar.
+    z = self._artifact_manager.zipfile_handle()
+    with z.open(self.PIPELINE_PATH, 'w') as fout:
+      fout.write(
+          json_format.MessageToJson(self._pipeline_proto).encode('utf-8'))
+    with z.open(self.PIPELINE_OPTIONS_PATH, 'w') as fout:
+      fout.write(
+          json_format.MessageToJson(self._pipeline_options).encode('utf-8'))
+    with z.open(self.PIPELINE_MANIFEST, 'w') as fout:
+      fout.write(
+          json.dumps({
+              'defaultJobName': self.PIPELINE_NAME
+          }).encode('utf-8'))
+
+    # Closes the jar file.
+    self._artifact_manager.close()
 
   def artifact_staging_endpoint(self):
     return self._artifact_staging_endpoint
--- sdks/python/apache_beam/runners/portability/flink_uber_jar_job_server.py
+++ sdks/python/apache_beam/runners/portability/flink_uber_jar_job_server.py
@@ -146,12 +146,6 @@ class FlinkBeamJob(abstract_job_service.UberJarBeamJob):
 
   def run(self):
     self._stop_artifact_service()
-    # Move the artifact manifest to the expected location.
-    with zipfile.ZipFile(self._jar, 'a', compression=zipfile.ZIP_DEFLATED) as z:
-      with z.open(self._artifact_manifest_location) as fin:
-        manifest_contents = fin.read()
-      with z.open(self.ARTIFACT_MANIFEST_PATH, 'w') as fout:
-        fout.write(manifest_contents)
 
     # Upload the jar and start the job.
     with open(self._jar, 'rb') as jar_file:
