d7675d573d6bf2ec1db74fa4e1fc1ce1436c11f8
darshan jani <darshanjani100@gmail.com>
2020-05-05 16:10:27 +0800

    [BEAM-9825] | Implement Intersect,Union,Except transforms

---
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/SetFns.java
@@ -0,0 +1,261 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.transforms;
+
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkNotNull;
+
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Iterables;
+
+public class SetFns {
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the intersection with provided
+   * {@code PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} will all distinct elements that present in
+   * both pipeline is constructed and provided {@link PCollection<T>}.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.intersect(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> intersect(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> intersectFn =
+        (inFirst, inSecond) -> (inFirst > 0 && inSecond > 0) ? 1L : 0L;
+    return new SetImpl<>(rightCollection, intersectFn);
+  }
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the intersection all with
+   * provided {@code PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} which will follow EXCEPT_ALL Semantics as
+   * follows: Given there are m elements on pipeline which is constructed {@link PCollection<T>}
+   * (left) and n elements on in provided {@link PCollection<T>} (right): - it will output MIN(m -
+   * n, 0) elements of left for all elements which are present in both left and right.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.intersectAll(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> intersectAll(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> intersectFn =
+        (inFirst, inSecond) -> (inFirst > 0 && inSecond > 0) ? Math.min(inFirst, inSecond) : 0L;
+    return new SetImpl<>(rightCollection, intersectFn);
+  }
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the difference (except) with
+   * provided {@code PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} will all distinct elements that present in
+   * pipeline is constructed {@link PCollection<T>} but not present in provided {@link
+   * PCollection<T>}.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.except(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> except(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> exceptFn =
+        (inFirst, inSecond) -> inFirst > 0 && inSecond == 0 ? 1L : 0L;
+    return new SetImpl<>(rightCollection, exceptFn);
+  }
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the difference all (exceptAll)
+   * with provided {@code PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} which will follow EXCEPT_ALL Semantics as
+   * follows: Given there are m elements on pipeline which is constructed {@link PCollection<T>}
+   * (left) and n elements on in provided {@link PCollection<T>} (right): - it will output m
+   * elements of left for all elements which are present in left but not in right. - it will output
+   * MAX(m - n, 0) elements of left for all elements which are present in both left and right.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.exceptAll(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> exceptAll(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> exceptFn =
+        (inFirst, inSecond) -> {
+          if (inFirst > 0 && inSecond == 0) {
+            return inFirst;
+          } else if (inFirst > 0 && inSecond > 0) {
+            return Math.max(inFirst - inSecond, 0L);
+          }
+          return 0L;
+        };
+    return new SetImpl<>(rightCollection, exceptFn);
+  }
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the union with provided {@code
+   * PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} will all distinct elements that present in
+   * pipeline is constructed {@link PCollection<T>} and {@link PCollection<T>}.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.union(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> union(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> unionFn = (inFirst, inSecond) -> 1L;
+    return new SetImpl<>(rightCollection, unionFn);
+  }
+
+  /**
+   * Returns a new {@code SetFns.SetImpl<T>} transform that compute the unionAll with provided
+   * {@code PCollection<T>}.
+   *
+   * <p>The argument should not be modified after this is called.
+   *
+   * <p>The elements of the output {@link PCollection<T>} which will follow UNION_ALL semantics as
+   * follows: Given there are m elements on pipeline which is constructed {@link PCollection<T>}
+   * (left) and n elements on in provided {@link PCollection<T>} (right): - it will output m
+   * elements of left and m elements of right.
+   *
+   * <pre>{@code
+   * Pipeline p = ...;
+   *
+   * PCollection<String> left = p.apply(Create.of("1", "2", "3", "4", "5"));
+   * PCollection<String> right = p.apply(Create.of("1", "3", "4", "6"));
+   *
+   * PCollection<String> results =
+   *     left.apply(SetFns.unionAll(right));
+   * }</pre>
+   */
+  public static <T> SetImpl<T> unionAll(PCollection<T> rightCollection) {
+    checkNotNull(rightCollection, "rightCollection argument is null");
+    SerializableBiFunction<Long, Long, Long> unionFn = Long::sum;
+    return new SetImpl<>(rightCollection, unionFn);
+  }
+
+  private static <T> PCollection<T> performSetOperation(
+      PCollection<T> leftCollection,
+      PCollection<T> rightCollection,
+      SerializableBiFunction<Long, Long, Long> fn) {
+
+    TupleTag<Void> leftCollectionTag = new TupleTag<>();
+    TupleTag<Void> rightCollectionTag = new TupleTag<>();
+
+    MapElements<T, KV<T, Void>> elementToVoid =
+        MapElements.via(
+            new SimpleFunction<T, KV<T, Void>>() {
+              @Override
+              public KV<T, Void> apply(T element) {
+                return KV.of(element, null);
+              }
+            });
+
+    PCollection<KV<T, Void>> left = leftCollection.apply("PrepareLeftKV", elementToVoid);
+    PCollection<KV<T, Void>> right = rightCollection.apply("PrepareRightKV", elementToVoid);
+
+    PCollection<KV<T, CoGbkResult>> coGbkResults =
+        KeyedPCollectionTuple.of(leftCollectionTag, left)
+            .and(rightCollectionTag, right)
+            .apply(CoGroupByKey.create());
+
+    return coGbkResults.apply(
+        ParDo.of(
+            new DoFn<KV<T, CoGbkResult>, T>() {
+
+              @ProcessElement
+              public void processElement(ProcessContext c) {
+                KV<T, CoGbkResult> elementGroups = c.element();
+
+                CoGbkResult value = elementGroups.getValue();
+                long inFirstSize = Iterables.size(value.getAll(leftCollectionTag));
+                long inSecondSize = Iterables.size(value.getAll(rightCollectionTag));
+
+                T element = elementGroups.getKey();
+                for (long i = 0L; i < fn.apply(inFirstSize, inSecondSize); i++) {
+                  c.output(element);
+                }
+              }
+            }));
+  }
+
+  public static class SetImpl<T> extends PTransform<PCollection<T>, PCollection<T>> {
+    private final PCollection<T> rightCollection;
+    private final SerializableBiFunction<Long, Long, Long> fn;
+
+    private SetImpl(PCollection<T> rightCollection, SerializableBiFunction<Long, Long, Long> fn) {
+      this.rightCollection = rightCollection;
+      this.fn = fn;
+    }
+
+    @Override
+    public PCollection<T> expand(PCollection<T> leftCollection) {
+      return performSetOperation(leftCollection, rightCollection, fn)
+          .setCoder(leftCollection.getCoder());
+    }
+  }
+}
---
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/transforms/SetFnsTest.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.transforms;
+
+import static junit.framework.TestCase.assertEquals;
+
+import java.util.Arrays;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.testing.NeedsRunner;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Iterables;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SetFnsTest {
+
+  @Rule public final TestPipeline p = TestPipeline.create();
+
+  Schema schema = Schema.builder().addStringField("alphabet").build();
+
+  static PCollection<String> left;
+  static PCollection<String> right;
+  static PCollection<Row> leftRow;
+  static PCollection<Row> rightRow;
+
+  private Iterable<Row> toRows(String... values) {
+    return Iterables.transform(
+        Arrays.asList(values), (elem) -> Row.withSchema(schema).addValues(elem).build());
+  }
+
+  @Before
+  public void setup() {
+
+    String[] leftData = {"a", "a", "a", "b", "b", "c", "d", "d", "g", "g", "h", "h"};
+    String[] rightData = {"a", "a", "b", "b", "b", "c", "d", "d", "e", "e", "f", "f"};
+
+    left = p.apply("left", Create.of(Arrays.asList(leftData)).withCoder(StringUtf8Coder.of()));
+    right = p.apply("right", Create.of(Arrays.asList(rightData)));
+    leftRow = p.apply("leftRow", Create.of(toRows(leftData)).withRowSchema(schema));
+    rightRow = p.apply("rightRow", Create.of(toRows(rightData)).withRowSchema(schema));
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testIntersection() {
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.intersect(right)))
+        .containsInAnyOrder("a", "b", "c", "d");
+
+    PCollection<Row> results =
+        leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.intersect(rightRow));
+    PAssert.that(results).containsInAnyOrder(toRows("a", "b", "c", "d"));
+    assertEquals(schema, results.getSchema());
+
+    p.run();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testIntersectionAll() {
+
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.intersectAll(right)))
+        .containsInAnyOrder("a", "a", "b", "b", "c", "d", "d");
+    PAssert.that(
+            leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.intersectAll(rightRow)))
+        .containsInAnyOrder(toRows("a", "a", "b", "b", "c", "d", "d"));
+
+    p.run();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testExcept() {
+
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.except(right)))
+        .containsInAnyOrder("g", "h");
+    PAssert.that(leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.except(rightRow)))
+        .containsInAnyOrder(toRows("g", "h"));
+    p.run();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testExceptAll() {
+
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.exceptAll(right)))
+        .containsInAnyOrder("a", "g", "g", "h", "h");
+    PAssert.that(leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.exceptAll(rightRow)))
+        .containsInAnyOrder(toRows("a", "g", "g", "h", "h"));
+
+    p.run();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testUnion() {
+
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.union(right)))
+        .containsInAnyOrder("a", "b", "c", "d", "e", "f", "g", "h");
+    PAssert.that(leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.union(rightRow)))
+        .containsInAnyOrder(toRows("a", "b", "c", "d", "e", "f", "g", "h"));
+
+    p.run();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testUnionAll() {
+
+    PAssert.that(left.apply("strings", org.apache.beam.sdk.transforms.SetFns.unionAll(right)))
+        .containsInAnyOrder(
+            "a", "a", "a", "a", "a", "b", "b", "b", "b", "b", "c", "c", "d", "d", "d", "d", "e",
+            "e", "f", "f", "g", "g", "h", "h");
+    PAssert.that(leftRow.apply("rows", org.apache.beam.sdk.transforms.SetFns.unionAll(rightRow)))
+        .containsInAnyOrder(
+            toRows(
+                "a", "a", "a", "a", "a", "b", "b", "b", "b", "b", "c", "c", "d", "d", "d", "d", "e",
+                "e", "f", "f", "g", "g", "h", "h"));
+
+    p.run();
+  }
+}
