31d8ce8087e57ea5dd96d4f42579a2f36837061c
Rion Williams <rionmonster@gmail.com>
2020-05-18 14:33:34 -0500

    converted Side Input section to Kotlin

---
+++ learning/katas/kotlin/Core Transforms/Side Input/Side Input/src/org/apache/beam/learning/katas/coretransforms/sideinput/Person.kt
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.learning.katas.coretransforms.sideinput
+
+import java.io.Serializable
+import java.util.*
+
+class Person(val name: String?, val city: String?, val country: String? = null) : Serializable {
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (o == null || javaClass != o.javaClass) return false
+        val person = o as Person
+        return name == person.name &&
+                city == person.city &&
+                country == person.country
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(name, city, country)
+    }
+
+    override fun toString(): String {
+        return "Person{name='$name', city='$city', country='$country'}"
+    }
+}
