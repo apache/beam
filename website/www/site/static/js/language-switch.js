1d6149e72648b95f65a5b70c5e8e78d899f7fede
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-05-26 12:19:18 -0400

    Update website/www/site/static/js/language-switch.js

--- website/www/site/static/js/language-switch.js
+++ website/www/site/static/js/language-switch.js
@@ -113,7 +113,7 @@ $(document).ready(function() {
                }
 
                 // Swapping visibility of code blocks.
-                $(this.selector).not(".language-md").hide();
+                $(this.selector).hide();
                 $("nav"+this.selector).show();
                 $("." + pref).show();
             },
