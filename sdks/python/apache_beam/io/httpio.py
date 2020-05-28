bcec159232683d3ad2de21ead8c5197d3cc770c7
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-05-27 20:43:20 -0400

    documentation, slightly change logic for exists()

--- sdks/python/apache_beam/io/httpio.py
+++ sdks/python/apache_beam/io/httpio.py
@@ -15,7 +15,14 @@
 # limitations under the License.
 #
 
-"""HTTP client
+"""This class implements methods to interact with files at HTTP URLs.
+
+This I/O only implements methods to read with files at HTTP URLs, because
+of the variability in methods by which HTTP content can be written
+to a server. If you need to write your results to an HTTP endpoint,
+you might want to make your own I/O or use another, more specific,
+I/O connector.
+
 """
 
 # pytype: skip-file
@@ -45,6 +52,19 @@ class HttpIO(object):
       uri,
       mode='r',
       read_buffer_size=16 * 1024 * 1024):
+      """Open a URL for reading or writing.
+
+      Args:
+        uri (str): HTTP URL in the form ``http://[path]`` or ``https://[path]``.
+        mode (str): ``'r'`` or ``'rb'`` for reading.
+        read_buffer_size (int): Buffer size to use during read operations.
+
+      Returns:
+        A file object representing the response.
+
+      Raises:
+        ValueError: Invalid open file mode.
+      """
     if mode == 'r' or mode == 'rb':
       downloader = HttpDownloader(uri, self._client)
       return io.BufferedReader(
@@ -53,10 +73,15 @@ class HttpIO(object):
       raise ValueError('Invalid file open mode: %s.' % mode)
 
   def list_prefix(self, path):
-    """Lists files matching the prefix. For now, just returns a single file at the given URL.
+    """Lists files matching the prefix.
+    
+    Because there is no common standard for listing files at a given
+    HTTP URL, this method just returns a single file at the given URL.
+    This means that listing files only works with an exact path, not
+    with a glob expression.
 
     Args:
-      path: S3 file path pattern in the form http://[url] or https://[url].
+      path: HTTP URL in the form http://[path] or https://[path].
 
     Returns:
       Dictionary of file name -> size.
@@ -64,6 +89,18 @@ class HttpIO(object):
     return {path: self.size(path)}
 
   def size(self, uri):
+    """Returns the size of a single file stored at a HTTP URL.
+
+    First, the client attempts to make a HEAD request for a non-gzipped version of the file,
+    and uses the Content-Length header to retrieve the size. If that fails because the server
+    does not attempt HEAD requests, the client just does a GET requuest to retrieve the length. 
+
+    Args:
+      path: HTTP URL in the form http://[path] or https://[path].
+
+    Returns:
+      Size of the HTTP file in bytes.
+    """
     try:
       # Pass in "" for "Accept-Encoding" because we want the non-gzipped content-length.
       resp, content = self._client.request(uri, method='HEAD', headers={"Accept-Encoding": ""})
@@ -79,15 +116,36 @@ class HttpIO(object):
       return int(resp["content-length"])
 
   def exists(self, uri):
+    """Returns whether the file at the given HTTP URL exists.
+
+    The client attempts to make a HEAD request, and if that fails, a GET request.
+    If the server returns 404, this function returns false, and it returns
+    true only if the server returns 200.
+
+    Args:
+      path: HTTP URL in the form http://[path] or https://[path].
+
+    Returns:
+      Size of the HTTP file in bytes.
+    """
     try:
-      # Pass in "" for "Accept-Encoding" because we want the non-gzipped content-length.
-      resp, content = self._client.request(uri, method='HEAD', headers={"Accept-Encoding": ""})
-      return resp.status != 404
+      resp, content = self._client.request(uri, method='HEAD')
+      if resp.status == 200:
+        return True
+      elif resp.status == 404:
+        return False
+      else:
+        raise Exception(resp.status, resp.reason)
     except Exception:
       # Server doesn't support HEAD method;
       # use GET method instead to prefetch the result.
       resp, content = self._client.request(uri, method='GET')
-      return resp.status != 404
+      if resp.status == 200:
+        return True
+      elif resp.status == 404:
+        return False
+      else:
+        raise Exception(resp.status, resp.reason)
 
 
 class HttpDownloader(Downloader):
