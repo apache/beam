5e127f77265f1b5feeedf2180447a2ed0b1e55d2
AldairCoronel <aldaircr@ciencias.unam.mx>
2020-06-24 20:45:28 -0500

    Add BlobStorageFileSystem class method definitions

--- sdks/python/apache_beam/io/azure/blobstoragefilesystem.py
+++ sdks/python/apache_beam/io/azure/blobstoragefilesystem.py
@@ -17,3 +17,78 @@
 """Azure Blob Storage Implementation for accesing files on Azure Blob Storage."""
 
 from __future__ import absolute_import
+
+
+from apache_beam.io.filesystem import BeamIOError
+from apache_beam.io.filesystem import CompressedFile
+from apache_beam.io.filesystem import CompressionTypes
+from apache_beam.io.filesystem import FileMetadata
+from apache_beam.io.filesystem import FileSystem
+
+
+__all__ = ['BlobStorageFileSystem']
+
+
+class BlobStorageFileSystem(FileSystem):
+  """An Azure Blob Storage ``FileSystem`` implementation for accesing files on Azure
+  Blob Storage.
+  """
+
+  CHUNK_SIZE = 1 
+  AZURE_FILE_SYSTEM_PREFIX = 'azfs://'
+
+  @classmethod
+  def scheme(cls):
+    """URI scheme for the FileSystem
+    """
+    raise NotImplementedError
+
+
+  def join(self, basepath, *paths):
+    # type: (str, *str) -> str
+
+    """Join two or more pathname components for the filesystem
+
+    Args:
+      basepath: string path of the first component of the path
+      paths: path components to be added
+
+    Returns: full path after combining all the passed components
+    """
+    raise NotImplementedError
+
+
+  def split(self, path):
+    # type: (str) -> Tuple[str, str]
+
+    """Splits the given path into two parts.
+
+    Splits the path into a pair (head, tail) such that tail contains the last
+    component of the path and head contains everything up to that.
+    For file-systems other than the local file-system, head should include the
+    prefix.
+
+    Args:
+      path: path as a string
+
+    Returns:
+      a pair of path components as strings.
+    """
+    raise NotImplementedError
+
+
+  def mkdirs(self, path):
+    """Recursively create directories for the provided path.
+
+    Args:
+      path: string path of the directory structure that should be created
+
+    Raises:
+      IOError: if leaf directory already exists.
+    """
+    raise NotImplementedError
+
+  
+  def has_dirs(self):
+    """Whether this FileSystem supports directories."""
+    raise NotImplementedError
