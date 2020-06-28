0aaa77ceed03198f6a759a5ad3096b7c1eaba62b
Damon Douglas <douglas.damon@gmail.com>
2020-06-27 19:20:13 -0700

    Add Branching to Go SDK katas

---
+++ learning/katas/go/Core Transforms/Branching/Branching/pkg/task/task.go
@@ -0,0 +1,58 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package task
+
+import (
+	"github.com/apache/beam/sdks/go/pkg/beam"
+	"strings"
+)
+
+func ApplyTransform(s beam.Scope, input beam.PCollection) (reversed, toUpper beam.PCollection) {
+	reversed = reverseString(s, input)
+	toUpper = toUpperString(s, input)
+	return
+}
+
+func reverseString(s beam.Scope, input beam.PCollection) beam.PCollection {
+	return beam.ParDo(s, func(element string) string {
+		return reverseFn(element)
+	}, input)
+}
+
+func toUpperString(s beam.Scope, input beam.PCollection) beam.PCollection {
+	return beam.ParDo(s, func(element string) string {
+		return toUpperFn(element)
+	}, input)
+}
+
+func reverseFn(s string) string {
+	runes := []rune(s)
+	n := len(s)
+	i := -1
+	for {
+		n--
+		i++
+		if n <= i {
+			break
+		}
+		runes[i], runes[n] = runes[n], runes[i]
+	}
+	return string(runes)
+}
+
+func toUpperFn(s string) string {
+	return strings.ToUpper(s)
+}
