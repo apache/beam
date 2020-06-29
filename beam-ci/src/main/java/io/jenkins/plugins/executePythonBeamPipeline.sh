af2a6ed052041650a53ad8778a7e3a9554726094
Justine Koa <justinekoa@gmail.com>
2020-06-29 19:03:58 +0000

    add apache beam licenses

--- beam-ci/src/main/java/io/jenkins/plugins/executePythonBeamPipeline.sh
+++ beam-ci/src/main/java/io/jenkins/plugins/executePythonBeamPipeline.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-# Script to install virtual environment, set up virtual environment, and run Python command to execute Beam pipeline on Dataflow
+#
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-WORKSPACE=$1
-BUILD_DIRECTORY=$2
-MAIN_CLASS=$3
-PIPELINE_OPTIONS=$4
-BUILD_RELEASE_OPTIONS=$5
+#####################################################################################
+# Script to install virtual environment, set up virtual environment, and run Python command
+# to execute Beam pipeline on Dataflow.
+
+WORKSPACE=$1                # Path to the workspace with the source code
+BUILD_DIRECTORY=$2          # Path to the build directory to activate the virtual environment in
+MAIN_CLASS=$3               # Path to main class with the pipeline code
+PIPELINE_OPTIONS=$4         # Pipeline options
+BUILD_RELEASE_OPTIONS=$5    # Build release options
 
 PYTHON_VERSION3="python3 -c 'import sys; print(sys.version_info.major == 3 and sys.version_info.minor>=5)'"
 echo temp
