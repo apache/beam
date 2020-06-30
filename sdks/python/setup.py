be55d6eeca8a086d8492ad59950ea8764399bef4
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-06-30 11:37:40 +0200

    fixup! Dummy commit to trigger build

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -21,7 +21,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import os
-import platform
 import sys
 import warnings
 from distutils.errors import DistutilsError
