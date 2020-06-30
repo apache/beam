cbf82dec409593e9e6223091feb669e10eb1fe0d
Piotr Szuberski <piotr.szuberski@polidea.com>
2020-06-30 18:09:20 +0200

    python

---
+++ sdks/python/apache_beam/io/external/jdbc.py
@@ -0,0 +1,135 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+  PTransforms for supporting Jdbc in Python pipelines. These transforms do not
+  run a Jdbc client in Python. Instead, they expand to ExternalTransforms
+  which the Expansion Service resolves to the Java SDK's JdbcIO.
+
+  Note: To use these transforms, you need to start a Java Expansion Service.
+  Please refer to the portability documentation on how to do that. Flink Users
+  can use the built-in Expansion Service of the Flink Runner's Job Server. The
+  expansion service address has to be provided when instantiating the
+  transforms.
+
+  If you start Flink's Job Server, the expansion service will be started on
+  port 8097. This is also the configured default for this transform. For a
+  different address, please set the expansion_service parameter.
+
+  For more information see:
+  - https://beam.apache.org/documentation/runners/flink/
+  - https://beam.apache.org/roadmap/portability/
+"""
+
+# pytype: skip-file
+
+from __future__ import absolute_import
+
+import typing
+
+from past.builtins import unicode
+
+from apache_beam.transforms.external import BeamJarExpansionService
+from apache_beam.transforms.external import ExternalTransform
+from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
+
+__all__ = ['WriteToJdbc']
+
+
+def default_io_expansion_service():
+  return BeamJarExpansionService('sdks:java:io:expansion-service:shadowJar')
+
+
+WriteToJdbcSchema = typing.NamedTuple(
+    'WriteToJdbcSchema',
+    [
+        ('driver_class_name', unicode),
+        ('jdbc_url', unicode),
+        ('username', unicode),
+        ('password', unicode),
+        ('connection_properties', typing.Optional[unicode]),
+        ('connection_init_sqls', typing.Optional[typing.List[unicode]]),
+        ('statement', unicode),
+    ],
+)
+
+
+class WriteToJdbc(ExternalTransform):
+  """
+  An external PTransform which writes Rows to the specified database.
+
+  This transform receives Rows defined as NamedTuple type and registered in
+  the coders registry to use RowCoder, e.g.
+
+    import typing
+    from apache_beam import coders
+
+    ExampleRow = typing.NamedTuple(
+      "ExampleRow",
+      [
+        ("id", int),
+        ("name", unicode),
+        ("budget", float),
+      ],
+    )
+    coders.registry.register_coder(ExampleRow, coders.RowCoder)
+
+  Experimental; no backwards compatibility guarantees.  It requires special
+  preparation of the Java SDK.  See BEAM-7870.
+  """
+
+  URN = 'beam:external:java:jdbc:write:v1'
+
+  def __init__(
+      self,
+      driver_class_name,
+      jdbc_url,
+      username,
+      password,
+      statement,
+      **kwargs):
+    """
+    Initializes a write operation to Jdbc.
+
+    :param driver_class_name: name of the jdbc driver class
+    :param jdbc_url: full jdbc url to the database.
+    :param username: database username
+    :param password: database password
+    :param statement: sql statement to be executed
+    :param connection_properties: properties of the jdbc connection
+    :param connection_init_sqls: required only for MySql and MariaDB
+    :param expansion_service: The address (host:port) of the ExpansionService.
+    """
+    connection_properties = kwargs.get('connection_properties')
+    connection_init_sqls = kwargs.get('connection_init_sqls')
+    expansion_service = kwargs.get('expansion_service')
+
+    super(WriteToJdbc, self).__init__(
+        self.URN,
+        NamedTupleBasedPayloadBuilder(
+            WriteToJdbcSchema(
+                driver_class_name=driver_class_name,
+                jdbc_url=jdbc_url,
+                username=username,
+                password=password,
+                statement=statement,
+                connection_properties=connection_properties,
+                connection_init_sqls=connection_init_sqls,
+            ),
+        ),
+        expansion_service or default_io_expansion_service(),
+    )
---
+++ sdks/python/apache_beam/io/external/xlang_jdbcio_it_test.py
@@ -0,0 +1,133 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# pytype: skip-file
+
+from __future__ import absolute_import
+
+import logging
+import typing
+import unittest
+
+from nose.plugins.attrib import attr
+from past.builtins import unicode
+
+import apache_beam as beam
+from apache_beam import coders
+from apache_beam.io.external.jdbc import WriteToJdbc
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.testing.test_pipeline import TestPipeline
+
+# pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+try:
+  import sqlalchemy
+except ImportError:
+  sqlalchemy = None
+# pylint: enable=wrong-import-order, wrong-import-position, ungrouped-imports
+
+# pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+try:
+  from testcontainers.postgres import PostgresContainer
+except ImportError:
+  PostgresContainer = None
+# pylint: enable=wrong-import-order, wrong-import-position, ungrouped-imports
+
+ROW_COUNT = 10
+
+JdbcWriteTestRow = typing.NamedTuple(
+    "JdbcWriteTestRow",
+    [
+        ("f_id", int),
+        ("f_real", float),
+        ("f_string", unicode),
+    ],
+)
+coders.registry.register_coder(JdbcWriteTestRow, coders.RowCoder)
+
+
+@attr('UsesCrossLanguageTransforms')
+@unittest.skipIf(sqlalchemy is None, "sql alchemy package is not installed.")
+@unittest.skipIf(
+    PostgresContainer is None, "testcontainers package is not installed")
+class JdbcExternalTransformTest(unittest.TestCase):
+  """Tests that exercise the cross-language JdbcIO Transform
+   (implemented in java).
+
+  To run with the local expansion service, flink job server you need to
+  build it, e.g. via command:
+    ./gradlew :sdks:java:io:expansion-service:shadowJar
+    ./gradlew :runners:flink:1.10:job-server:shadowJar
+
+  Also, beam java sdk container is necessary. You can build it via:
+    ./gradlew :sdks:java:container:docker
+  """
+
+  def setUp(self):
+    self.postgres = PostgresContainer('postgres:latest')
+    self.postgres.start()
+    self.engine = sqlalchemy.create_engine(self.postgres.get_connection_url())
+    self.username = 'test'
+    self.password = 'test'
+    self.host = self.postgres.get_container_host_ip()
+    self.port = self.postgres.get_exposed_port(5432)
+    self.database_name = 'test'
+    self.driver_class_name = 'org.postgresql.Driver'
+    self.jdbc_url = 'jdbc:postgresql://{}:{}/{}'.format(
+        self.host, self.port, self.database_name)
+
+  def tearDown(self):
+    self.postgres.stop()
+
+  def test_xlang_jdbc_write(self):
+    table_name = 'jdbc_external_test_write'
+    self.engine.execute(
+        "CREATE TABLE {}(f_id INTEGER, f_real REAL, f_string VARCHAR)".
+        format(table_name))
+
+    inserted_rows = [
+        JdbcWriteTestRow(i, i + 0.1, 'Test{}'.format(i))
+        for i in range(ROW_COUNT)
+    ]
+    with TestPipeline() as p:
+      p.not_use_test_runner_api = True
+      _ = (
+          p
+          | beam.Create(inserted_rows).with_output_types(JdbcWriteTestRow)
+          | 'Write to jdbc' >> WriteToJdbc(
+              driver_class_name=self.driver_class_name,
+              jdbc_url=self.jdbc_url,
+              username=self.username,
+              password=self.password,
+              statement='INSERT INTO {} VALUES(?, ?, ?)'.format(table_name),
+          ))
+
+    fetched_data = self.engine.execute("SELECT * FROM {}".format(table_name))
+    fetched_rows = [
+        JdbcWriteTestRow(int(row[0]), float(row[1]), str(row[2]))
+        for row in fetched_data
+    ]
+
+    self.assertEqual(
+        set(fetched_rows),
+        set(inserted_rows),
+        'Inserted data does not fit data fetched from table',
+    )
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.DEBUG)
+  unittest.main()
