15f1f48d859daea5fa8a0adc45a84ae2eee583a1
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2020-07-03 15:19:21 +1000

    [BEAM-10166] Catching of DoFnError in callNoPanic
    As a partt of BEAM-10166, a new type of error doFnError was introduced to
    make errors generated from DoFns during bundle execution more digestable
    to the user.
    
    In this patch, we implement the catching of this error in the callNoPanic
    function, which wraps bundle execution. WEe explicitly check the type of
    the error generated to see if its of type doFnError and return that error.
    Where it is not the case, the default behaviour of returning the stack
    trace is perserved.

--- sdks/go/pkg/beam/core/runtime/exec/util.go
+++ sdks/go/pkg/beam/core/runtime/exec/util.go
@@ -46,7 +46,6 @@ type doFnError struct {
 	uid  UnitID
 	pid  string
 }
-
 func (e *doFnError) Error() string {
 	return fmt.Sprintf("%v caused error %v, uid is %v pid is %v", e.doFn, e.err, e.uid, e.pid)
 }
@@ -61,9 +60,14 @@ func (g *GenID) New() UnitID {
 func callNoPanic(ctx context.Context, fn func(context.Context) error) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			// Top level error is the panic itself, but also include the stack trace as the original error.
-			// Higher levels can then add appropriate context without getting pushed down by the stack trace.
-			err = errors.SetTopLevelMsgf(errors.Errorf("panic: %v %s", r, debug.Stack()), "panic: %v", r)
+			// If the recovered error is of type doFnError, then return doFnErr
+			if dfe, ok := r.(doFnError); ok {
+				err = &dfe
+			} else {
+				// Top level error is the panic itself, but also include the stack trace as the original error.
+				// Higher levels can then add appropriate context without getting pushed down by the stack trace.
+				err = errors.SetTopLevelMsgf(errors.Errorf("panic: %v %s", r, debug.Stack()), "panic: %v", r)
+			}
 		}
 	}()
 	return fn(ctx)
