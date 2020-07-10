24e1ba573c34fa744dddef708a203d2b875c7805
Kyle Weaver <kcweaver@google.com>
2020-07-10 11:58:44 -0700

    fix lint/format

--- sdks/python/apache_beam/runners/portability/local_job_service_test.py
+++ sdks/python/apache_beam/runners/portability/local_job_service_test.py
@@ -60,9 +60,10 @@ class LocalJobServerTest(unittest.TestCase):
     ]
     self.assertEqual([s.state for s in state_results], expected_states)
 
-    stateful_messages = filter(
-        lambda m: m.HasField('state_response'), message_results)
-    self.assertEqual([s.state_response.state for s in stateful_messages],
+    self.assertEqual([
+        s.state_response.state
+        for s in message_results if s.HasField('state_response')
+    ],
                      expected_states)
 
   def test_error_messages_after_pipeline_failure(self):
