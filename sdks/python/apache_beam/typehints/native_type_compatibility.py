12f206cb0a23bb26a3891e437739b9a810515061
Saavan Nanavati <66381097+saavannanavati@users.noreply.github.com>
2020-07-10 14:16:00 -0500

    [BEAM-10197] Support typehints for Python's frozenset (#11939)
    * BEAM 10197 - Support typehints for frozenset
    
    * Added unit tests
    
    * Add additional unit tests for FrozenSet by refactoring existing Set tests with inheritance
    
    * Fixup: apply YAPF
    
    * Update Python type safety documentation
    
    * Refactor unit test design from multiple inheritance to single inheritance
    
    * Remove unnecessary *args, **kwargs
    
    * Fixed broken test by adding a test-only kwarg used in TypeVariable equality checks
    
    * Rebase with master
    
    * Fixing TypeVariable's __eq__ and adding unit tests
    
    * Alphabetically sort imports
    
    * Fix lint errors
    
    * Alphabetically order imports

--- sdks/python/apache_beam/typehints/native_type_compatibility.py
+++ sdks/python/apache_beam/typehints/native_type_compatibility.py
@@ -237,6 +237,10 @@ def convert_to_beam_type(typ):
       _TypeMapEntry(
           match=_match_issubclass(typing.Set), arity=1,
           beam_type=typehints.Set),
+      _TypeMapEntry(
+          match=_match_issubclass(typing.FrozenSet),
+          arity=1,
+          beam_type=typehints.FrozenSet),
       # NamedTuple is a subclass of Tuple, but it needs special handling.
       # We just convert it to Any for now.
       # This MUST appear before the entry for the normal Tuple.
@@ -369,6 +373,8 @@ def convert_to_typing_type(typ):
     return typing.Union[tuple(convert_to_typing_types(typ.union_types))]
   if isinstance(typ, typehints.SetTypeConstraint):
     return typing.Set[convert_to_typing_type(typ.inner_type)]
+  if isinstance(typ, typehints.FrozenSetTypeConstraint):
+    return typing.FrozenSet[convert_to_typing_type(typ.inner_type)]
   if isinstance(typ, typehints.TupleConstraint):
     return typing.Tuple[tuple(convert_to_typing_types(typ.tuple_types))]
   if isinstance(typ, typehints.TupleSequenceConstraint):
