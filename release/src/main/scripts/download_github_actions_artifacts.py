a1d3e00a5d9a0763798b90438c370a009f3902e5
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-07-15 10:33:07 +0200

    fixup! Refactor to do not use global vars, ask for gh-token

--- release/src/main/scripts/download_github_actions_artifacts.py
+++ release/src/main/scripts/download_github_actions_artifacts.py
@@ -29,12 +29,9 @@ import zipfile
 import dateutil.parser
 import requests
 
-GH_API_URL_WORKLOW_FMT = (
-    "https://api.github.com/repos/{repo_url}/actions/workflows/build_wheels.yml"
-)
-GH_API_URL_WORKFLOW_RUNS_FMT = (
-    "https://api.github.com/repos/{repo_url}/actions/workflows/{workflow_id}/runs"
-)
+GH_API_URL_WORKLOW_FMT = "https://api.github.com/repos/{repo_url}/actions/workflows/build_wheels.yml"
+GH_API_URL_WORKFLOW_RUNS_FMT = "https://api.github.com/repos/{repo_url}/actions/workflows/{workflow_id}/runs"
+GH_API_URL_WORKFLOW_RUN_FMT = "https://api.github.com/repos/{repo_url}/actions/runs/{run_id}"
 GH_WEB_URL_WORKLOW_RUN_FMT = "https://github.com/{repo_url}/actions/runs/{run_id}"
 
 
@@ -54,10 +51,11 @@ def parse_arguments():
   parser.add_argument("--artifacts_dir", required=True)
 
   args = parser.parse_args()
-
   github_token = ask_for_github_token()
+
   print("You passed following arguments:")
   pprint.pprint({**vars(args), **{"github_token": github_token}})
+
   if not get_yes_or_no_answer("Do you want to continue?"):
     print("You said NO. Quitting ...")
     sys.exit(1)
@@ -72,7 +70,7 @@ def parse_arguments():
 
 
 def ask_for_github_token():
-  """Ask for github token and print basic information about it"""
+  """Ask for github token and print basic information about it."""
   url = "https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token"
   message = (
       f"You need to have a github access token with public_repo scope. "
@@ -85,7 +83,7 @@ def ask_for_github_token():
 
 
 def request_url(url, github_token, return_json=True, *args, **kwargs):
-  """Helper function form making requests authorized by GitHub token"""
+  """Helper function form making requests authorized by GitHub token."""
   r = requests.get(url, *args, auth=("token", github_token), **kwargs)
   if return_json:
     r.raise_for_status()
@@ -104,27 +102,33 @@ def safe_get(data, key, url=None):
 
 
 def get_yes_or_no_answer(question):
-  """Asks yes or no question"""
+  """Asks yes or no question."""
   reply = str(input(question + " 'y' or 'n'): ")).lower().strip()
   if reply == "y":
     return True
-  if reply == "n":
+  elif reply == "n":
     return False
   else:
     return get_yes_or_no_answer("Uhhhh... please enter")
 
 
 def get_build_wheels_workflow_id(repo_url, github_token):
-  """Gets workflow id"""
+  """Gets workflow id."""
   url = GH_API_URL_WORKLOW_FMT.format(repo_url=repo_url)
   data = request_url(url, github_token)
   return safe_get(data, "id", url)
 
 
-def get_last_run(
+def get_single_workflow_run_data(run_id, repo_url, github_token):
+  """Gets single workflow run data (github api payload)."""
+  url = GH_API_URL_WORKFLOW_RUN_FMT.format(repo_url=repo_url, run_id=run_id)
+  return request_url(url, github_token)
+
+
+def get_last_run_id(
     workflow_id, repo_url, release_branch, release_commit, github_token):
   """
-  Gets data of last run for given repo, branch and commit.
+  Gets id of last run for given workflow, repo, branch and commit.
   Raises exception when no run found.
   """
   url = GH_API_URL_WORKFLOW_RUNS_FMT.format(
@@ -133,10 +137,7 @@ def get_last_run(
       url,
       github_token,
       params={
-          # FIXME change to push
-          "event": "pull_request",
-          "branch": release_branch
-          # "event": "push", "branch": release_branch
+          "event": "push", "branch": release_branch
       },
   )
   runs = safe_get(data, "workflow_runs", url)
@@ -144,11 +145,12 @@ def get_last_run(
   filtered_commit_runs = [
       r for r in runs if r.get("head_sha", "") == release_commit
   ]
+
   if not filtered_commit_runs:
-    workflow_web_url = GH_API_URL_WORKFLOW_RUNS_FMT.format(
+    workflow_run_web_url = GH_API_URL_WORKFLOW_RUNS_FMT.format(
         repo_url=repo_url, workflow_id=workflow_id)
     raise Exception(
-        f"No runs for workflow (branch {release_branch}, commit {release_commit}). Verify at {workflow_web_url}"
+        f"No runs for workflow (branch {release_branch}, commit {release_commit}). Verify at {workflow_run_web_url}"
     )
 
   sorted_runs = sorted(
@@ -160,28 +162,41 @@ def get_last_run(
   print(
       f"Found last run. SHA: {release_commit}, created_at: '{last_run['created_at']}', id: {last_run['id']}"
   )
-  workflow_web_url = GH_WEB_URL_WORKLOW_RUN_FMT.format(
+  workflow_run_web_url = GH_WEB_URL_WORKLOW_RUN_FMT.format(
       repo_url=repo_url, run_id=last_run["id"])
-  print(f"Verify at {workflow_web_url}")
+  print(f"Verify at {workflow_run_web_url}")
   print(
-      f"Upload to GCS available at:\n"
-      f"\tgs://beam-wheels-staging/{release_branch}/{release_commit}-{workflow_id}/"
+      f"Upload to GCS available at: gs://beam-wheels-staging/{release_branch}/{release_commit}-{workflow_id}/"
   )
-  return last_run
+  return safe_get(last_run, "id")
 
 
-def validate_run(run_data, repo_url, github_token):
-  """Validates workflow run. Verifies status and waits if run is not finished."""
+def validate_run(run_id, repo_url, github_token):
+  """Validates workflow run. Verifies succesfull status and waits if run is not finished."""
+  run_data = get_single_workflow_run_data(run_id, repo_url, github_token)
   status = safe_get(run_data, "status")
   conclusion = safe_get(run_data, "conclusion")
-  if status == "completed" and conclusion == "success":
-    return run_data
 
-  url = safe_get(run_data, "url")
-  workflow_web_url = GH_WEB_URL_WORKLOW_RUN_FMT.format(
-      repo_url=repo_url, run_id=run_data["id"])
+  if status == "completed" and conclusion == "success":
+    return run_id
+  elif status in ["queued", "in_progress"]:
+    wait_for_workflow_run_to_finish(
+        run_id, repo_url, status, conclusion, github_token)
+  else:
+    run_web_url = GH_WEB_URL_WORKLOW_RUN_FMT.format(
+        repo_url=repo_url, run_id=run_id)
+    raise Exception(
+        f"Run unsuccessful. Status: {status}. Conclusion: {conclusion}. Check at: {run_web_url}"
+    )
+
+
+def wait_for_workflow_run_to_finish(
+    run_id, repo_url, status, conclusion, github_token):
+  """Waits for given workflow run to finish succesfully"""
+  run_web_url = GH_WEB_URL_WORKLOW_RUN_FMT.format(
+      repo_url=repo_url, run_id=run_id)
   print(
-      f"Started waiting for Workflow run {run_data['id']} to finish. Check on {workflow_web_url}"
+      f"Started waiting for Workflow run {run_id} to finish. Check on {run_web_url}"
   )
   start_time = time.time()
   last_request = start_time
@@ -200,7 +215,7 @@ def validate_run(run_data, repo_url, github_token):
     time.sleep(0.3)
     if (now - last_request) > request_interval:
       last_request = now
-      run_data = request_url(url, github_token)
+      run_data = get_single_workflow_run_data(run_id, repo_url, github_token)
       status = safe_get(run_data, "status")
       conclusion = safe_get(run_data, "conclusion")
       if status in ["queued", "in_progress"]:
@@ -210,11 +225,12 @@ def validate_run(run_data, repo_url, github_token):
             f"\rFinished in: {elapsed_time}. "
             f"Last state: status: `{status}`, conclusion: `{conclusion}`.",
         )
-        return run_data
+        return run_id
       else:
         print("\r")
         raise Exception(
-            f"Run unsuccessful. Conclusion: {conclusion}. Payload: {run_data}")
+            f"Run unsuccessful. Conclusion: {conclusion}. Check at: {run_web_url}"
+        )
 
 
 def reset_directory(artifacts_dir):
@@ -231,9 +247,11 @@ def reset_directory(artifacts_dir):
     sys.exit(1)
 
 
-def download_artifacts(artifacts_url, artifacts_dir, github_token):
-  """Downloads github artifacts from given url."""
+def download_artifacts(run_id, repo_url, artifacts_dir, github_token):
+  """Downloads github artifacts from given run."""
   print("Starting downloading artifacts ... (it may take a while)")
+  run_data = get_single_workflow_run_data(run_id, repo_url, github_token)
+  artifacts_url = safe_get(run_data, "artifacts_url")
   data_artifacts = request_url(artifacts_url, github_token)
   artifacts = safe_get(data_artifacts, "artifacts", artifacts_url)
   filtered_artifacts = [
@@ -244,21 +262,30 @@ def download_artifacts(artifacts_url, artifacts_dir, github_token):
   for artifact in filtered_artifacts:
     url = safe_get(artifact, "archive_download_url")
     name = safe_get(artifact, "name")
-    artifacts_size_mb = round(artifact["size_in_bytes"] / (1024 * 1024), 2)
-    print(
-        f"\tDownloading {name}.zip artifact (size: {artifacts_size_mb} megabytes)"
-    )
+    size_in_bytes = safe_get(artifact, "size_in_bytes")
 
-    with tempfile.NamedTemporaryFile(
-        "wb", prefix=name, suffix=".zip"
-    ) as f, request_url(
-        url, github_token, return_json=False, allow_redirects=True, stream=True
-    ) as r:
-      with open(f.name, "wb") as f:
-        shutil.copyfileobj(r.raw, f)
-      with zipfile.ZipFile(f.name, "r") as zip_ref:
-        print(f"\tUnzipping {len(zip_ref.filelist)} files")
-        zip_ref.extractall(artifacts_dir)
+    download_single_artifact(
+        url, name, size_in_bytes, artifacts_dir, github_token)
+
+
+def download_single_artifact(
+    url, name, size_in_bytes, artifacts_dir, github_token):
+  """Downloads single github artifact."""
+  artifacts_size_mb = round(size_in_bytes / (1024 * 1024), 2)
+  print(
+      f"\tDownloading {name}.zip artifact (size: {artifacts_size_mb} megabytes)"
+  )
+
+  with tempfile.NamedTemporaryFile(
+    "wb", prefix=name, suffix=".zip"
+  ) as f, request_url(
+    url, github_token, return_json=False, allow_redirects=True, stream=True
+  ) as r:
+    with open(f.name, "wb") as f:
+      shutil.copyfileobj(r.raw, f)
+    with zipfile.ZipFile(f.name, "r") as zip_ref:
+      print(f"\tUnzipping {len(zip_ref.filelist)} files")
+      zip_ref.extractall(artifacts_dir)
 
 
 if __name__ == "__main__":
@@ -276,12 +303,11 @@ if __name__ == "__main__":
 
   try:
     workflow_id = get_build_wheels_workflow_id(repo_url, github_token)
-    run = get_last_run(
+    run_id = get_last_run_id(
         workflow_id, repo_url, release_branch, release_commit, github_token)
-    run = validate_run(run, repo_url, github_token)
-    artifacts_url = run["artifacts_url"]
+    validate_run(run_id, repo_url, github_token)
     reset_directory(artifacts_dir)
-    download_artifacts(artifacts_url, artifacts_dir, github_token)
+    download_artifacts(run_id, repo_url, artifacts_dir, github_token)
     print("Script finished successfully!")
     print(f"Artifacts available in directory: {artifacts_dir}")
   except KeyboardInterrupt as e:
