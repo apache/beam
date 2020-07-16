aa4d89d22806e9d835bef617b4dd7571aa9c1e6b
Kenneth Knowles <klk@google.com>
2020-07-15 20:43:36 -0700

    Convert static Function fields on ReflectHelpers to static methods
    Previously, these were Guava Functions. Now they can just be static methods and
    use a method reference to use them for any functional interface.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/common/ReflectHelpers.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/common/ReflectHelpers.java
@@ -21,7 +21,6 @@ import static java.util.Arrays.asList;
 import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
 import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkNotNull;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
@@ -35,8 +34,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.Queue;
 import java.util.ServiceLoader;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Function;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Joiner;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.FluentIterable;
@@ -49,119 +46,95 @@ public class ReflectHelpers {
 
   private static final Joiner COMMA_SEPARATOR = Joiner.on(", ");
 
-  /** A {@link Function} that turns a method into a simple method signature. */
-  public static final Function<Method, String> METHOD_FORMATTER =
-      new Function<Method, String>() {
-        @SuppressFBWarnings(
-            value = "NP_METHOD_PARAMETER_TIGHTENS_ANNOTATION",
-            justification = "https://github.com/google/guava/issues/920")
-        @Override
-        public String apply(@Nonnull Method input) {
-          String parameterTypes =
-              FluentIterable.from(asList(input.getParameterTypes()))
-                  .transform(Class::getSimpleName)
-                  .join(COMMA_SEPARATOR);
-          return String.format("%s(%s)", input.getName(), parameterTypes);
-        }
-      };
+  /** Returns a string representation of the signature of a {@link Method}. */
+  public static String formatMethod(Method input) {
+    String parameterTypes =
+        FluentIterable.from(asList(input.getParameterTypes()))
+            .transform(Class::getSimpleName)
+            .join(COMMA_SEPARATOR);
+    return String.format("%s(%s)", input.getName(), parameterTypes);
+  }
 
-  /** A {@link Function} that turns a method into the declaring class + method signature. */
-  public static final Function<Method, String> CLASS_AND_METHOD_FORMATTER =
-      new Function<Method, String>() {
-        @SuppressFBWarnings(
-            value = "NP_METHOD_PARAMETER_TIGHTENS_ANNOTATION",
-            justification = "https://github.com/google/guava/issues/920")
-        @Override
-        public String apply(@Nonnull Method input) {
-          return String.format(
-              "%s#%s", input.getDeclaringClass().getName(), METHOD_FORMATTER.apply(input));
-        }
-      };
+  /** Returns a string representation of the class + method signature for a {@link Method}. */
+  public static String formatMethodWithClass(Method input) {
+    return String.format("%s#%s", input.getDeclaringClass().getName(), formatMethod(input));
+  }
 
   /** A {@link Function} that returns a concise string for a {@link Annotation}. */
-  public static final Function<Annotation, String> ANNOTATION_FORMATTER =
-      annotation -> {
-        String annotationName = annotation.annotationType().getName();
-        String annotationNameWithoutPackage =
-            annotationName.substring(annotationName.lastIndexOf('.') + 1).replace('$', '.');
-        String annotationToString = annotation.toString();
-        String values = annotationToString.substring(annotationToString.indexOf('('));
-        return String.format("%s%s", annotationNameWithoutPackage, values);
-      };
+  public static String formatAnnotation(Annotation annotation) {
+    String annotationName = annotation.annotationType().getName();
+    String annotationNameWithoutPackage =
+        annotationName.substring(annotationName.lastIndexOf('.') + 1).replace('$', '.');
+    String annotationToString = annotation.toString();
+    String values = annotationToString.substring(annotationToString.indexOf('('));
+    return String.format("%s%s", annotationNameWithoutPackage, values);
+  }
 
   /** A {@link Function} that formats types. */
-  public static final Function<Type, String> TYPE_SIMPLE_DESCRIPTION =
-      new Function<Type, String>() {
-        @SuppressFBWarnings(
-            value = "NP_METHOD_PARAMETER_TIGHTENS_ANNOTATION",
-            justification = "https://github.com/google/guava/issues/920")
-        @Override
-        @Nullable
-        public String apply(@Nonnull Type input) {
-          StringBuilder builder = new StringBuilder();
-          format(builder, input);
-          return builder.toString();
-        }
+  public static String simpleTypeDescription(Type input) {
+    StringBuilder builder = new StringBuilder();
+    format(builder, input);
+    return builder.toString();
+  }
 
-        private void format(StringBuilder builder, Type t) {
-          if (t instanceof Class) {
-            formatClass(builder, (Class<?>) t);
-          } else if (t instanceof TypeVariable) {
-            formatTypeVariable(builder, (TypeVariable<?>) t);
-          } else if (t instanceof WildcardType) {
-            formatWildcardType(builder, (WildcardType) t);
-          } else if (t instanceof ParameterizedType) {
-            formatParameterizedType(builder, (ParameterizedType) t);
-          } else if (t instanceof GenericArrayType) {
-            formatGenericArrayType(builder, (GenericArrayType) t);
-          } else {
-            builder.append(t.toString());
-          }
-        }
+  private static void format(StringBuilder builder, Type t) {
+    if (t instanceof Class) {
+      formatClass(builder, (Class<?>) t);
+    } else if (t instanceof TypeVariable) {
+      formatTypeVariable(builder, (TypeVariable<?>) t);
+    } else if (t instanceof WildcardType) {
+      formatWildcardType(builder, (WildcardType) t);
+    } else if (t instanceof ParameterizedType) {
+      formatParameterizedType(builder, (ParameterizedType) t);
+    } else if (t instanceof GenericArrayType) {
+      formatGenericArrayType(builder, (GenericArrayType) t);
+    } else {
+      builder.append(t.toString());
+    }
+  }
 
-        private void formatClass(StringBuilder builder, Class<?> clazz) {
-          builder.append(clazz.getSimpleName());
-        }
+  private static void formatClass(StringBuilder builder, Class<?> clazz) {
+    builder.append(clazz.getSimpleName());
+  }
 
-        private void formatTypeVariable(StringBuilder builder, TypeVariable<?> t) {
-          builder.append(t.getName());
-        }
+  private static void formatTypeVariable(StringBuilder builder, TypeVariable<?> t) {
+    builder.append(t.getName());
+  }
 
-        private void formatWildcardType(StringBuilder builder, WildcardType t) {
-          builder.append("?");
-          for (Type lowerBound : t.getLowerBounds()) {
-            builder.append(" super ");
-            format(builder, lowerBound);
-          }
-          for (Type upperBound : t.getUpperBounds()) {
-            if (!Object.class.equals(upperBound)) {
-              builder.append(" extends ");
-              format(builder, upperBound);
-            }
-          }
-        }
+  private static void formatWildcardType(StringBuilder builder, WildcardType t) {
+    builder.append("?");
+    for (Type lowerBound : t.getLowerBounds()) {
+      builder.append(" super ");
+      format(builder, lowerBound);
+    }
+    for (Type upperBound : t.getUpperBounds()) {
+      if (!Object.class.equals(upperBound)) {
+        builder.append(" extends ");
+        format(builder, upperBound);
+      }
+    }
+  }
 
-        private void formatParameterizedType(StringBuilder builder, ParameterizedType t) {
-          if (t.getOwnerType() != null) {
-            format(builder, t.getOwnerType());
-            builder.append('.');
-          }
-          format(builder, t.getRawType());
-          if (t.getActualTypeArguments().length > 0) {
-            builder.append('<');
-            COMMA_SEPARATOR.appendTo(
-                builder,
-                FluentIterable.from(asList(t.getActualTypeArguments()))
-                    .transform(TYPE_SIMPLE_DESCRIPTION));
-            builder.append('>');
-          }
-        }
+  private static void formatParameterizedType(StringBuilder builder, ParameterizedType t) {
+    if (t.getOwnerType() != null) {
+      format(builder, t.getOwnerType());
+      builder.append('.');
+    }
+    format(builder, t.getRawType());
+    if (t.getActualTypeArguments().length > 0) {
+      builder.append('<');
+      COMMA_SEPARATOR.appendTo(
+          builder,
+          FluentIterable.from(asList(t.getActualTypeArguments()))
+              .transform(ReflectHelpers::simpleTypeDescription));
+      builder.append('>');
+    }
+  }
 
-        private void formatGenericArrayType(StringBuilder builder, GenericArrayType t) {
-          format(builder, t.getGenericComponentType());
-          builder.append("[]");
-        }
-      };
+  private static void formatGenericArrayType(StringBuilder builder, GenericArrayType t) {
+    format(builder, t.getGenericComponentType());
+    builder.append("[]");
+  }
 
   /** A {@link Comparator} that uses the object's class' canonical name to compare them. */
   public static class ObjectsClassComparator implements Comparator<Object> {
