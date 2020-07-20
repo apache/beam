192aa1e550147932778d8e116f0f649889bf70ad
Heejong Lee <heejong@gmail.com>
2020-07-20 13:45:30 -0700

    add testcase

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryServicesImpl.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryServicesImpl.java
@@ -401,7 +401,9 @@ class BigQueryServicesImpl implements BigQueryServices {
     private static final FluentBackoff INSERT_BACKOFF_FACTORY =
         FluentBackoff.DEFAULT.withInitialBackoff(Duration.millis(200)).withMaxRetries(5);
 
-    // A backoff for rate limit exceeded errors. Only retry upto approximately 2 minutes.
+    // A backoff for rate limit exceeded errors. Only retry upto approximately 2 minutes
+    // and propagate errors afterward. Otherwise, Dataflow UI cannot display rate limit
+    // errors since they are silently retried in Callable threads.
     private static final FluentBackoff RATE_LIMIT_BACKOFF_FACTORY =
         FluentBackoff.DEFAULT.withInitialBackoff(Duration.standardSeconds(1)).withMaxRetries(13);
 
@@ -724,6 +726,7 @@ class BigQueryServicesImpl implements BigQueryServices {
         List<ValueInSingleWindow<TableRow>> rowList,
         @Nullable List<String> insertIdList,
         BackOff backoff,
+        FluentBackoff rateLimitBackoffFactory,
         final Sleeper sleeper,
         InsertRetryPolicy retryPolicy,
         List<ValueInSingleWindow<T>> failedInserts,
@@ -797,9 +800,9 @@ class BigQueryServicesImpl implements BigQueryServices {
             futures.add(
                 executor.submit(
                     () -> {
-                      // A backoff for rate limit exceeded errors. Retries forever.
+                      // A backoff for rate limit exceeded errors.
                       BackOff backoff1 =
-                          BackOffAdapter.toGcpBackOff(RATE_LIMIT_BACKOFF_FACTORY.backoff());
+                          BackOffAdapter.toGcpBackOff(rateLimitBackoffFactory.backoff());
                       while (true) {
                         try {
                           return insert.execute().getInsertErrors();
@@ -903,6 +906,7 @@ class BigQueryServicesImpl implements BigQueryServices {
           rowList,
           insertIdList,
           BackOffAdapter.toGcpBackOff(INSERT_BACKOFF_FACTORY.backoff()),
+          RATE_LIMIT_BACKOFF_FACTORY,
           Sleeper.DEFAULT,
           retryPolicy,
           failedInserts,
