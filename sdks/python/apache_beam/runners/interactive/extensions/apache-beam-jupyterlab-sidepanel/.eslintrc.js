83c525702b428e00b36375c5fc5c3608a6f1cb3f
Ning Kang <ningk@google.com>
2020-07-21 10:54:40 -0700

    [BEAM-10545] Initialize an empty extension
    1. Files were generated from: cookiecutter
       https://github.com/jupyterlab/extension-cookiecutter-ts.
    2. Modifications are done to swap in Apache-2.0 license and add
       supplemental instructions in README.
    3. .eslint* and .prettier* files are for formatting and linting, details
       seen in README.
    4. This is a node package, thus package.json.
    5. This package is written in TypeScript, thus tsconfig.json.
    6. Sources are located in src/* and CSS are located in style/*.
    7. `jlpm` is an alias of `yarn` locked by JupyterLab. Dependencies are
       managed by `yarn`, thus yarn.lock.
    8. The empty extension, when activated, logs something in console when
       JupyterLab is started.

---
+++ sdks/python/apache_beam/runners/interactive/extensions/apache-beam-jupyterlab-sidepanel/.eslintrc.js
@@ -0,0 +1,44 @@
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': [
+      'error',
+      { prefixWithI: 'always' }
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false }
+    ],
+    curly: ['error', 'all'],
+    eqeqeq: 'error',
+    'prefer-arrow-callback': 'error'
+  }
+};
