51dd0e7e1c17500076961669a580a2501849878e
Scott Lukas <slukas@google.com>
2020-07-22 17:14:42 +0000

    Implemented SchemaIO and SchemaIOProvider for Avro

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/GenericRecordWriteConverter.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/GenericRecordWriteConverter.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.sdk.extensions.sql.meta.provider.avro;
+package org.apache.beam.sdk.io;
 
 import com.google.auto.value.AutoValue;
 import java.io.Serializable;
