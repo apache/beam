e4bfe7ce062572703a1441fde4fee53b41013755
Piotr Szuberski <piotr.szuberski@polidea.com>
2020-07-24 15:16:45 +0200

    Use testcontainers with localstack docker image

--- sdks/python/apache_beam/io/external/xlang_kinesisio_it_test.py
+++ sdks/python/apache_beam/io/external/xlang_kinesisio_it_test.py
@@ -22,37 +22,62 @@
 from __future__ import absolute_import
 
 import logging
+import time
 import unittest
-
-from testcontainers.core.container import DockerContainer
 
 import apache_beam as beam
 from apache_beam.io.kinesis import InitialPositionInStream
 from apache_beam.io.kinesis import ReadDataFromKinesis
 from apache_beam.io.kinesis import WriteToKinesis
+
+from apache_beam.options.pipeline_options import StandardOptions
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 from apache_beam.transforms.combiners import Count
 
+# pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+try:
+  import boto3
+except ImportError:
+  boto3 = None
+# pylint: enable=wrong-import-order, wrong-import-position, ungrouped-imports
+
+# pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+try:
+  from testcontainers.core.container import DockerContainer
+except ImportError:
+  DockerContainer = None
+# pylint: enable=wrong-import-order, wrong-import-position, ungrouped-imports
+
 MAX_NUM_RECORDS = 10
-MAX_READ_TIME = 120
-INITIAL_POSITION_IN_STREAM = InitialPositionInStream.TRIM_HORIZON
 
-
+@unittest.skipIf(DockerContainer is None, 'testcontainers is not installed.')
+@unittest.skipIf(boto3 is None, 'boto3 is not installed.')
+@unittest.skipIf(
+    TestPipeline().get_pipeline_options().view_as(StandardOptions).runner is
+    None,
+    'Do not run this test on precommit suites.')
 class CrossLanguageKinesisIOTest(unittest.TestCase):
   def setUp(self):
-    self.localstack = DockerContainer('localstack:localstack:0.11.3')\
-      .with_env('SERVICES', 'kinesis')
-    self.service_endpoint = '{}:{}'.format(
+    self.localstack = DockerContainer('localstack/localstack:0.8.6')\
+      .with_env('SERVICES', 'kinesis')\
+      .with_env('KINESIS_PORT', '4568')\
+      .with_exposed_ports(4568)
+    self.localstack.start()
+    self.service_endpoint = 'http://{}:{}'.format(
         self.localstack.get_container_host_ip(),
-        self.localstack.get_exposed_port(4568),
+        self.localstack.get_exposed_port('4568'),
     )
     self.access_key = 'accesskey'
     self.secret_key = 'secretkey'
-    self.region = 'us-east-1'
+    self.region = 'US_EAST_1'
+    self.aws_region = 'us-east-1'
     self.partition_key = 'a'
-    self.localstack.start()
+    self.max_read_time = 120  # 2min
+    self.initial_position_in_stream = InitialPositionInStream.TRIM_HORIZON
+    self.stream_name = 'beam'
+    self.create_kinesis_stream()
 
   def tearDown(self):
     self.localstack.stop()
@@ -73,6 +98,7 @@ class CrossLanguageKinesisIOTest(unittest.TestCase):
               stream_name=self.stream_name,
               aws_access_key=self.access_key,
               aws_secret_key=self.secret_key,
+              service_endpoint=self.service_endpoint,
               region=self.region,
               partition_key=self.partition_key,
           ))
@@ -83,35 +109,47 @@ class CrossLanguageKinesisIOTest(unittest.TestCase):
           p
           | 'ReadFromKinesis' >> ReadDataFromKinesis(
               stream_name=self.stream_name,
-              aws_access_key=self.aws_access_key,
-              aws_secret_key=self.aws_secret_key,
+              aws_access_key=self.access_key,
+              aws_secret_key=self.secret_key,
+              service_endpoint=self.service_endpoint,
               region=self.region,
               max_num_records=MAX_NUM_RECORDS,
-              max_read_time=MAX_READ_TIME,
-              initial_position_in_stream=INITIAL_POSITION_IN_STREAM,
+              max_read_time=self.max_read_time,
+              initial_position_in_stream=self.initial_position_in_stream,
           )
           | 'Count elements' >> Count.Globally())
 
-      # There could already be some records in the stream so we can't ensure
-      # that the ones we read are the ones we've just written so just count
-      # them to verify that requested number of messages's been read.
-      assert_that(result, equal_to([MAX_NUM_RECORDS]))
+      assert_that(
+          result, equal_to([str(i).encode() for i in range(MAX_NUM_RECORDS)]))
 
-  def get_kinesis_options(self):
-    p = TestPipeline()
+  def create_kinesis_stream(self):
+    client = boto3.client(
+        service_name='kinesis',
+        region_name='us-east-1',
+        endpoint_url=self.service_endpoint,
+        aws_access_key_id=self.access_key,
+        aws_secret_access_key=self.secret_key,
 
-    def get_required_option(option_name):
-      option = p.get_option(option_name)
-      if not option:
-        logging.error('--%s argument is required.', option_name)
-        sys.exit(1)
-      return option
+    )
+    # localstack could not have initialized yet so try few times
+    retries = 5
+    for i in range(retries):
+      try:
+        client.create_stream(
+            StreamName=self.stream_name,
+            ShardCount=1,
+        )
+        time.sleep(2)
+        break
+      except Exception as e:
+        if i == retries:
+          raise e
 
-    self.stream_name = get_required_option('stream_name')
-    self.aws_access_key = get_required_option('aws_access_key')
-    self.aws_secret_key = get_required_option('aws_secret_key')
-    self.region = get_required_option('aws_region')
-
+    # ensure stream is initialized before proceeding
+    stream = client.describe_stream(StreamName=self.stream_name)
+    while stream['StreamDescription']['StreamStatus'] != 'ACTIVE':
+      time.sleep(2)
+      stream = client.describe_stream(StreamName=self.stream_name)
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
