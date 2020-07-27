7958f85f121d9ae21699a9c76d907f1bec05110c
Saavan <saavan@google.com>
2020-07-27 18:53:54 -0500

    For testing purposes, default to performance_runtime_type_check when both flags are on

--- sdks/python/apache_beam/pipeline.py
+++ sdks/python/apache_beam/pipeline.py
@@ -522,10 +522,10 @@ class Pipeline(object):
 
       if (self._options.view_as(TypeOptions).runtime_type_check and
           self._options.view_as(TypeOptions).performance_runtime_type_check):
-        raise ValueError(
-            'You cannot turn on runtime_type_check '
-            'and performance_runtime_type_check simultaneously. '
-            'Pick one or the other.')
+        print('You cannot turn on runtime_type_check '
+              'and performance_runtime_type_check simultaneously. '
+              'Beam will default to using performance_runtime_type_check.')
+        self._options.view_as(TypeOptions).runtime_type_check = False
 
       if self._options.view_as(TypeOptions).runtime_type_check:
         from apache_beam.typehints import typecheck
