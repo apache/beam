5867b3a2beefb865ba729d0145ea357b864f951a
pawel.urbanowicz <pawel.urbanowicz@polidea.com>
2020-07-28 16:00:44 +0200

    feat: add custom expansion-service

--- sdks/python/apache_beam/io/external/snowflake.py
+++ sdks/python/apache_beam/io/external/snowflake.py
@@ -15,19 +15,6 @@
 # limitations under the License.
 #
 
-# pytype: skip-file
-
-from __future__ import absolute_import
-
-import typing
-
-from past.builtins import unicode
-
-import apache_beam as beam
-from apache_beam.transforms.external import BeamJarExpansionService
-from apache_beam.transforms.external import ExternalTransform
-from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
-
 """Snowflake transforms tested against Flink portable runner.
   **Setup**
   Transforms provided in this module are cross-language transforms
@@ -40,10 +27,9 @@ from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
   * Option 2: specify a custom expansion service
   See below for details regarding each of these options.
   *Option 1: Use the default expansion service*
-  This is the recommended and easiest setup option for using Python Kafka
-  transforms. This option is only available for Beam 2.22.0 and later.
-  This option requires following pre-requisites before running the Beam
-  pipeline.
+  This is the recommended and easiest setup option for using Python Snowflake
+  transforms.This option requires following pre-requisites
+  before running the Beam pipeline.
   * Install Java runtime in the computer from where the pipeline is constructed
     and make sure that 'java' command is available.
   In this option, Python SDK will either download (for released Beam version) or
@@ -69,11 +55,25 @@ from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
   - https://beam.apache.org/documentation/runners/flink/
 """
 
+# pytype: skip-file
+
+from __future__ import absolute_import
+
+import typing
+
+from past.builtins import unicode
+
+import apache_beam as beam
+from apache_beam.transforms.external import BeamJarExpansionService
+from apache_beam.transforms.external import ExternalTransform
+from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
+
 __all__ = ['ReadFromSnowflake']
 
 
 def default_io_expansion_service():
-  return BeamJarExpansionService('sdks:java:io:expansion-service:shadowJar')
+  return BeamJarExpansionService(
+      'sdks:java:io:snowflake:expansion-service:shadowJar')
 
 
 ReadFromSnowflakeSchema = typing.NamedTuple(
@@ -116,46 +116,48 @@ class ReadFromSnowflake(beam.PTransform):
       query=None,
       expansion_service=None):
     """
-    Initializes a read operation from Snowflake.
+        Initializes a read operation from Snowflake.
 
-    Required parameters:
-    :param server_name: full Snowflake server name with the following format
-        account.region.gcp.snowflakecomputing.com.
-    :param schema: name of the Snowflake schema in the database to use.
-    :param database: name of the Snowflake database to use.
-    :param staging_bucket_name: name of the Google Cloud Storage bucket.
-        Bucket will be used as a temporary location for storing CSV files.
-        Those temporary directories will be named
-        `sf_copy_csv_DATE_TIME_RANDOMSUFFIX`
-        and they will be removed automatically once Read operation finishes.
-    :param storage_integration_name: is the name of storage integration
-        object created according to Snowflake documentation.
-    :param csv_mapper: specifies a function which must translate
-        user-defined object to array of strings.
-        SnowflakeIO uses a COPY INTO <location> statement to
-        move data from a Snowflake table to Google Cloud Storage as CSV files.
-        These files are then downloaded via FileIO and processed line by line.
-        Each line is split into an array of Strings using the OpenCSV
-        The csv_mapper function job is to give the user the possibility to
-        convert the array of Strings to a user-defined type,
-        ie. GenericRecord for Avro or Parquet files, or custom objects.
+        Required parameters:
+        :param server_name: full Snowflake server name with the following format
+            account.region.gcp.snowflakecomputing.com.
+        :param schema: name of the Snowflake schema in the database to use.
+        :param database: name of the Snowflake database to use.
+        :param staging_bucket_name: name of the Google Cloud Storage bucket.
+            Bucket will be used as a temporary location for storing CSV files.
+            Those temporary directories will be named
+            `sf_copy_csv_DATE_TIME_RANDOMSUFFIX`
+            and they will be removed automatically once Read operation finishes.
+        :param storage_integration_name: is the name of storage integration
+            object created according to Snowflake documentation.
+        :param csv_mapper: specifies a function which must translate
+            user-defined object to array of strings.
+            SnowflakeIO uses a COPY INTO <location> statement to move data from
+            a Snowflake table to Google Cloud Storage as CSV files.These files
+            are then downloaded via FileIO and processed line by line.
+            Each line is split into an array of Strings using the OpenCSV
+            The csv_mapper function job is to give the user the possibility to
+            convert the array of Strings to a user-defined type,
+            ie. GenericRecord for Avro or Parquet files, or custom objects.
             Example:
-                ```
-                    def csv_mapper(strings_array):
- 		                return User(strings_array[0], int(strings_array[1])))
-                ```
-    :param table or query: specifies a Snowflake table name or custom SQL query
-    :param expansion_service: specifies URL of expansion service.
+            ```
+                def csv_mapper(strings_array):
+                     return User(strings_array[0], int(strings_array[1])))
+            ```
+        :param table or query: specifies a Snowflake table name or
+            custom SQL query
+        :param expansion_service: specifies URL of expansion service.
 
-    Authentication parameters:
-    It's required to pass one of the following combinations of valid parameters:
-    :param username and password: specifies username and password
-        for username/password authentication method.
-    :param private_key_path and private_key_passphrase:
-        specifies a private key file and password
-        for key/ pair authentication method.
-    :param o_auth_token: specifies access token for OAuth authentication method.
-    """
+        Authentication parameters:
+        It's required to pass one of the following combinations of parameters:
+        :param username and password: specifies username and password
+            for username/password authentication method.
+        :param private_key_path and private_key_passphrase:
+            specifies a private key file and password
+            for key/ pair authentication method.
+        :param o_auth_token: specifies access token for
+            OAuth authentication method.
+        """
 
     self.params = ReadFromSnowflakeSchema(
         server_name=server_name,
