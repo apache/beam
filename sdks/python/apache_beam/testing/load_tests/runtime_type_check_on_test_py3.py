3d7f58852c8b62178ce705e5e203f53169f29e6f
Saavan <saavan@google.com>
2020-07-28 18:16:01 -0500

    Fix imports and folder structure

--- sdks/python/apache_beam/testing/load_tests/runtime_type_check_on_test_py3.py
+++ sdks/python/apache_beam/testing/load_tests/runtime_type_check_on_test_py3.py
@@ -1,6 +1,19 @@
-import logging
-
-from .runtime_type_check.base_runtime_type_check import BaseRunTimeTypeCheckTest
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 """
 For more information, see documentation in the parent class: BaseRunTimeTypeCheckTest.
@@ -22,6 +35,12 @@ python -m apache_beam.testing.load_tests.runtime_type_check_on_test_py3 \
     \"value_size\": 15
     }'"
 """
+
+# pytype: skip-file
+
+import logging
+
+from apache_beam.testing.load_tests.base_runtime_type_check_test_py3 import BaseRunTimeTypeCheckTest
 
 
 class RunTimeTypeCheckOnTest(BaseRunTimeTypeCheckTest):
