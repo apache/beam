d884c5feb851d22d8f5e181e1c91c1a5a324f22c
terryxian78 <txian@google.com>
2020-08-01 07:58:56 -0700

    Revert version bump of google-cloud-core-http

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -483,7 +483,6 @@ class BeamModulePlugin implements Plugin<Project> {
         google_cloud_bigtable_client_core           : "com.google.cloud.bigtable:bigtable-client-core:1.15.0",
         google_cloud_core                           : "com.google.cloud:google-cloud-core:$google_cloud_core_version",
         google_cloud_core_grpc                      : "com.google.cloud:google-cloud-core-grpc:$google_cloud_core_version",
-        google_cloud_core_http                      : "com.google.cloud:google-cloud-core-http:$google_cloud_core_version",
         google_cloud_datacatalog_v1beta1            : "com.google.cloud:google-cloud-datacatalog:$google_cloud_datacatalog_version",
         google_cloud_dataflow_java_proto_library_all: "com.google.cloud.dataflow:google-cloud-dataflow-java-proto-library-all:0.5.160304",
         google_cloud_datastore_v1_proto_client      : "com.google.cloud.datastore:datastore-v1-proto-client:1.6.3",
