775570b404c6a33017d7446a5bc92cf5587180cc
Yichi Zhang <zyichi@google.com>
2020-08-03 11:40:33 -0700

    Minor comment fix

--- sdks/python/apache_beam/internal/pickler.py
+++ sdks/python/apache_beam/internal/pickler.py
@@ -190,9 +190,8 @@ if 'save_module' in dir(dill.dill):
       if obj_id not in known_module_dicts:
         # Trigger loading of lazily loaded modules (such as pytest vendored
         # modules).
-        # This first pass over sys.modules needs to iterate on a copy of
-        # sys.modules since lazy loading modifies the dictionary, hence the use
-        # of list().
+        # This pass over sys.modules needs to iterate on a copy of sys.modules
+        # since lazy loading modifies the dictionary, hence the use of list().
         for m in list(sys.modules.values()):
           try:
             _ = m.__dict__
