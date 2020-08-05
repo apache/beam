73891931f165e8bf86df0210ab252ef8d56aa345
Saavan <saavan@google.com>
2020-08-05 01:37:04 -0500

    Add test for DoOutputsTuple

--- sdks/python/apache_beam/typehints/typed_pipeline_test_py3.py
+++ sdks/python/apache_beam/typehints/typed_pipeline_test_py3.py
@@ -371,6 +371,23 @@ class MainInputTest(unittest.TestCase):
 
     _ = [1, 2, 3] | IntToInt() | IntToStr()
 
+  def test_typed_ptransform_with_do_outputs_tuple_compiles(self):
+    class MyDoFn(beam.DoFn):
+      def process(self, element: int, *args, **kwargs):
+        if element % 2:
+          yield beam.pvalue.TaggedOutput('odd', 1)
+        else:
+          yield beam.pvalue.TaggedOutput('even', 1)
+
+    class MyPTransform(beam.PTransform):
+      def expand(
+          self,
+          pcoll: beam.pvalue.PCollection[int]):
+        return pcoll | beam.ParDo(MyDoFn()).with_outputs('odd', 'even')
+
+    # This test fails if you remove the following line from ptransform.py
+    # if isinstance(pvalue_, DoOutputsTuple): continue
+    _ = [1, 2, 3] | MyPTransform()
 
 class AnnotationsTest(unittest.TestCase):
   def test_pardo_dofn(self):
