ea5093aed115d7d7a6ef14a53288d258dd31845a
Etta Rapp <ettarapp@google.com>
2020-08-06 22:15:34 +0000

    removed duplicate tests

--- sdks/java/io/amazon-web-services/src/test/java/org/apache/beam/sdk/io/aws/s3/S3ResourceIdTest.java
+++ sdks/java/io/amazon-web-services/src/test/java/org/apache/beam/sdk/io/aws/s3/S3ResourceIdTest.java
@@ -252,20 +252,16 @@ public class S3ResourceIdTest {
   public void testEquals() {
     S3ResourceId a = S3ResourceId.fromComponents("bucket", "a/b/c");
     S3ResourceId b = S3ResourceId.fromComponents("bucket", "a/b/c");
-    assertEquals(a, a);
     assertEquals(a, b);
 
     b = S3ResourceId.fromComponents(a.getBucket(), "a/b/c/");
     assertNotEquals(a, b);
-    assertNotEquals(b, a);
 
     b = S3ResourceId.fromComponents(a.getBucket(), "x/y/z");
     assertNotEquals(a, b);
-    assertNotEquals(b, a);
 
     b = S3ResourceId.fromComponents("other-bucket", a.getKey());
     assertNotEquals(a, b);
-    assertNotEquals(b, a);
   }
 
   @Test
