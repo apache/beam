7730b459aca8b4d6586080b1184445ed81fd7d25
Luke Cwik <lcwik@google.com>
2020-08-10 20:54:56 -0700

    [BEAM-10688] Euphoria assumes that all type descriptors are resolvable to coders (and serializable) which isn't true.
    Euphoria should be propagating forward coders and not type descriptors as a longer term solution.
    I also fixed the tests to specify @RunWith because in certain scenarios JUnit may fail to detect the tests.

--- sdks/java/extensions/euphoria/src/test/java/org/apache/beam/sdk/extensions/euphoria/core/client/operator/JoinTest.java
+++ sdks/java/extensions/euphoria/src/test/java/org/apache/beam/sdk/extensions/euphoria/core/client/operator/JoinTest.java
@@ -39,8 +39,11 @@ import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode;
 import org.joda.time.Duration;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /** Test operator Join. */
+@RunWith(JUnit4.class)
 public class JoinTest {
 
   @Rule public TestPipeline pipeline = TestPipeline.create();
