4671058ff78bf94de81eccf08ccb0c263894a82f
Brian Hulette <bhulette@google.com>
2020-08-10 17:02:18 -0700

    Add verifyDeterministic test to SchemaCoderTest

--- sdks/java/core/src/test/java/org/apache/beam/sdk/schemas/SchemaCoderTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/schemas/SchemaCoderTest.java
@@ -18,18 +18,27 @@
 package org.apache.beam.sdk.schemas;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import com.google.auto.value.AutoValue;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Objects;
 import org.apache.avro.reflect.AvroSchema;
+import org.apache.beam.sdk.coders.Coder.NonDeterministicException;
+import org.apache.beam.sdk.coders.RowCoder;
+import org.apache.beam.sdk.schemas.Schema.Field;
+import org.apache.beam.sdk.schemas.Schema.FieldType;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+import org.apache.beam.sdk.schemas.logicaltypes.NanosInstant;
 import org.apache.beam.sdk.schemas.utils.SchemaTestUtils;
 import org.apache.beam.sdk.testing.CoderProperties;
 import org.apache.beam.sdk.values.Row;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.hamcrest.Matchers;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -40,8 +49,13 @@ import org.junit.runners.Parameterized;
 /** Unit tests for {@link Schema}. */
 @RunWith(Enclosed.class)
 public class SchemaCoderTest {
+
   private static final Schema INT32_SCHEMA =
       Schema.builder().addInt32Field("a").addInt32Field("b").build();
+  public static final Schema LOGICAL_NANOS_SCHEMA =
+      Schema.of(Field.of("logicalNanos", FieldType.logicalType(new NanosInstant())));
+  public static final Schema FLOATING_POINT_SCHEMA =
+      Schema.of(Field.of("float", FieldType.FLOAT), Field.of("double", FieldType.DOUBLE));
 
   @RunWith(JUnit4.class)
   public static class SingletonTests {
@@ -226,13 +240,16 @@ public class SchemaCoderTest {
   }
 
   @RunWith(Parameterized.class)
-  @DefaultSchema(AutoValueSchema.class)
-  public static class SchemaProviderTests {
+  public static class ParameterizedTests {
+
     @Parameterized.Parameter(0)
     public SchemaCoder coder;
 
     @Parameterized.Parameter(1)
     public ImmutableList<Object> testValues;
+
+    @Parameterized.Parameter(2)
+    public boolean expectDeterministic;
 
     @Parameterized.Parameters(name = "{index}: coder = {0}")
     public static Collection<Object[]> data() throws NoSuchSchemaException {
@@ -241,7 +258,8 @@ public class SchemaCoderTest {
             SchemaCoder.of(INT32_SCHEMA),
             ImmutableList.of(
                 Row.withSchema(INT32_SCHEMA).addValues(9001, 9002).build(),
-                Row.withSchema(INT32_SCHEMA).addValues(3, 4).build())
+                Row.withSchema(INT32_SCHEMA).addValues(3, 4).build()),
+            true
           },
           new Object[] {
             coderFrom(TypeDescriptor.of(SimpleAutoValue.class)),
@@ -249,7 +267,8 @@ public class SchemaCoderTest {
                 SimpleAutoValue.of(
                     "foo", 9001, 0L, new DateTime().withDate(1979, 3, 14).withTime(10, 30, 0, 0)),
                 SimpleAutoValue.of(
-                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0)))
+                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0))),
+            true
           },
           new Object[] {
             coderFrom(TypeDescriptor.of(SimpleBean.class)),
@@ -257,7 +276,8 @@ public class SchemaCoderTest {
                 new SimpleBean(
                     "foo", 9001, 0L, new DateTime().withDate(1979, 3, 14).withTime(10, 30, 0, 0)),
                 new SimpleBean(
-                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0)))
+                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0))),
+            true
           },
           new Object[] {
             coderFrom(TypeDescriptor.of(SimplePojo.class)),
@@ -265,7 +285,8 @@ public class SchemaCoderTest {
                 new SimplePojo(
                     "foo", 9001, 0L, new DateTime().withDate(1979, 3, 14).withTime(10, 30, 0, 0)),
                 new SimplePojo(
-                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0)))
+                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0))),
+            true
           },
           new Object[] {
             coderFrom(TypeDescriptor.of(SimpleAvro.class)),
@@ -273,7 +294,25 @@ public class SchemaCoderTest {
                 new SimpleAvro(
                     "foo", 9001, 0L, new DateTime().withDate(1979, 3, 14).withTime(10, 30, 0, 0)),
                 new SimpleAvro(
-                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0)))
+                    "bar", 9002, 1L, new DateTime().withDate(1989, 3, 14).withTime(10, 30, 0, 0))),
+            true
+          },
+          new Object[] {
+            RowCoder.of(LOGICAL_NANOS_SCHEMA),
+            ImmutableList.of(
+                Row.withSchema(LOGICAL_NANOS_SCHEMA)
+                    .withFieldValue("logicalNanos", Instant.ofEpochMilli(9001))
+                    .build()),
+            true
+          },
+          new Object[] {
+            RowCoder.of(FLOATING_POINT_SCHEMA),
+            ImmutableList.of(
+                Row.withSchema(FLOATING_POINT_SCHEMA)
+                    .withFieldValue("float", (float) 1.0)
+                    .withFieldValue("double", 2.0)
+                    .build()),
+            false
           });
     }
 
@@ -289,6 +328,32 @@ public class SchemaCoderTest {
           CoderProperties.coderConsistentWithEquals(coder, testValueA, testValueB);
         }
       }
+    }
+
+    @Test
+    public void verifyDeterministic() throws Exception {
+      if (expectDeterministic) {
+        assertDeterministic(coder);
+      } else {
+        assertNonDeterministic(coder);
+      }
+    }
+  }
+
+  private static void assertDeterministic(SchemaCoder coder) {
+    try {
+      coder.verifyDeterministic();
+    } catch (NonDeterministicException e) {
+      fail("Expected " + coder + " to be deterministic, but got:\n" + e);
+    }
+  }
+
+  private static void assertNonDeterministic(SchemaCoder<?> coder) {
+    try {
+      coder.verifyDeterministic();
+      fail("Expected " + coder + " to be non-deterministic.");
+    } catch (NonDeterministicException e) {
+      assertThat(e.getReasons(), Matchers.iterableWithSize(1));
     }
   }
 }
