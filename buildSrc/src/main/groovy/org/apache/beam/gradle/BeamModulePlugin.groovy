7d26e8c3b69f85862305a4bc22f74bd05c96cb78
Daniel Oliveira <daniel.o.programmer@gmail.com>
2020-08-12 17:42:15 -0700

    Moving to 2.25.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -322,7 +322,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.24.0'
+    project.version = '2.25.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
