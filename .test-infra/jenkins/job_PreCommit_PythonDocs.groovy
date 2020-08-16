0c819978a687a512ff84504cd29faa052c7e8b57
Pablo <pabloem@users.noreply.github.com>
2020-08-15 21:03:34 -0700

    Merge pull request #12575: [BEAM-10707] Adding Python docs precommit as separate suite.
    * Adding Python docs precommit as separate suite.
    
    * Adding Python docs precommit as separate suite.
    
    * Adding Python docs precommit as separate suite.
    
    * Adding Python docs precommit as separate suite.
    
    * Adding Python docs precommit as separate suite.
    
    * Adding Python docs precommit as separate suite.
    
    * Adding to PR template
    
    * Adding to jenkins readme
    
    * indentation
    
    * fix spotless

--- .test-infra/jenkins/job_PreCommit_PythonDocs.groovy
+++ .test-infra/jenkins/job_PreCommit_PythonDocs.groovy
@@ -4,29 +4,30 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * License); you may not use this file except in compliance
+ * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-/**
- * Unit tests for commont Python components.
- */
+import PrecommitJobBuilder
+import CommonJobProperties as common
 
-plugins { id 'org.apache.beam.module' }
-applyPythonNature()
-
-toxTask "docs", "py38-docs"
-assemble.dependsOn docs
-
-task preCommitPyCommon() {
+PrecommitJobBuilder builder = new PrecommitJobBuilder(
+    scope: this,
+    nameBase: 'PythonDocs',
+    gradleTask: ':pythonDocsPreCommit',
+    timeoutMins: 30,
+    triggerPathPatterns: [
+      '^sdks/python/.*$',
+    ]
+    )
+builder.build {
+  publishers {}
 }
-
-toxTask "dependency-check", "py3-dependency-check"
