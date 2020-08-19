717d90cdf472d4d83256cd071b222038ad7ac519
Etta Rapp <ettarapp@google.com>
2020-08-19 22:07:05 +0000

    creating a module for azure

--- sdks/java/io/azure/src/main/java/org/apache/beam/sdk/io/azure/options/AzureModule.java
+++ sdks/java/io/azure/src/main/java/org/apache/beam/sdk/io/azure/options/AzureModule.java
@@ -29,5 +29,8 @@ import com.google.auto.service.AutoService;
  */
 @AutoService(Module.class)
 public class AzureModule extends SimpleModule {
-    
+
+    public AzureModule() {
+        super("AzureModule");
+    }
 }
