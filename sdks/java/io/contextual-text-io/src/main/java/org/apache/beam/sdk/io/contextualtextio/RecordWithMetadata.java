38871a93de3da8260cd1d08b1f35bd1e68fbb93e
Abhishek Yadav <abhiy@google.com>
2020-08-20 23:33:20 +0000

    [BEAM-10124] Refactor Code and Add option for recordOffset * Refactored requested changes * Add recordOffset feild that gives the offset of a record in the file

--- sdks/java/io/contextual-text-io/src/main/java/org/apache/beam/sdk/io/contextualtextio/RecordWithMetadata.java
+++ sdks/java/io/contextual-text-io/src/main/java/org/apache/beam/sdk/io/contextualtextio/RecordWithMetadata.java
@@ -19,20 +19,44 @@ package org.apache.beam.sdk.io.contextualtextio;
 
 import com.google.auto.value.AutoValue;
 import org.apache.beam.sdk.annotations.Experimental;
-import org.apache.beam.sdk.annotations.Internal;
 import org.apache.beam.sdk.schemas.AutoValueSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 
-@Internal
+/**
+ * Helper Class based on {@link AutoValueSchema}, it provides Metadata associated with each Record
+ * when reading from file(s) using {@link ContextualTextIO}.
+ *
+ * <h3>Fields:</h3>
+ *
+ * <ul>
+ *   <li>recordOffset: The offset of a record (the byte at which the record begins) in a file. This
+ *       information can be useful if you wish to reconstruct the file. {@link
+ *       RecordWithMetadata#getRecordOffset()}
+ *   <li>recordNum: The record number of the record from its ordinal file. {@link
+ *       RecordWithMetadata#getRecordNum()}
+ *   <li>recordValue: The value / contents of the records {@link
+ *       RecordWithMetadata#getRecordValue()}
+ *   <li>startingOffset: The starting offset of the range to which the record belongs. {@link
+ *       RecordWithMetadata#getStartingOffset()}
+ *   <li>recordNumInOffset: The record number relative to the Range. (line number within the range)
+ *       {@link RecordWithMetadata#getRecordNumInOffset()}
+ *   <li>fileName: Name of the file to which the record belongs (this is the full filename,
+ *       eg:path/to/file.txt) {@link RecordWithMetadata#getFileName()}
+ * </ul>
+ */
 @Experimental(Experimental.Kind.SCHEMAS)
 @DefaultSchema(AutoValueSchema.class)
 @AutoValue
 public abstract class RecordWithMetadata {
-  public abstract Range getRange();
+  public abstract Long getRecordOffset();
 
   public abstract Long getRecordNum();
 
   public abstract String getRecordValue();
+
+  public abstract Long getStartingOffset();
+
+  public abstract Long getRecordNumInOffset();
 
   public abstract Builder toBuilder();
 
@@ -44,13 +68,17 @@ public abstract class RecordWithMetadata {
 
   @AutoValue.Builder
   public abstract static class Builder {
-    public abstract Builder setRange(Range range);
-
     public abstract Builder setRecordNum(Long lineNum);
+
+    public abstract Builder setRecordOffset(Long recordOffset);
 
     public abstract Builder setRecordValue(String line);
 
     public abstract Builder setFileName(String file);
+
+    public abstract Builder setRecordNumInOffset(Long recordNumInOffset);
+
+    public abstract Builder setStartingOffset(Long startingOffset);
 
     public abstract RecordWithMetadata build();
   }
