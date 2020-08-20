7dbdf437cd79964736c70c0098524576f48af523
Kevin Puthusseri <kevinsijo@google.com>
2020-08-20 00:24:28 +0000

    documentation(beam/sdks/go): added apache license and function docstring

--- sdks/go/pkg/beam/core/runtime/xlangx/resolve.go
+++ sdks/go/pkg/beam/core/runtime/xlangx/resolve.go
@@ -1,3 +1,18 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package xlangx
 
 import (
@@ -12,7 +27,8 @@ import (
 	pipepb "github.com/apache/beam/sdks/go/pkg/beam/model/pipeline_v1"
 )
 
-func Resolve(ctx context.Context, edges []*graph.MultiEdge, p *pipepb.Pipeline) {
+// ResolveArtifacts acquires all dependencies for a cross-language transform
+func ResolveArtifacts(ctx context.Context, edges []*graph.MultiEdge, p *pipepb.Pipeline) {
 	path, err := filepath.Abs("/tmp/artifacts")
 	if err != nil {
 		panic(err)
