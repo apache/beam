1e7f4bc305a5bc73ba1c9011cf5f2ef92655e33d
yoshiki.obata <yoshiki.obata@gmail.com>
2020-08-25 01:43:38 +0900

    [BEAM-9944] made run_pytest allow options to indicate specific test package

--- sdks/python/scripts/run_pytest.sh
+++ sdks/python/scripts/run_pytest.sh
@@ -21,19 +21,71 @@
 # exit statuses of runs, special-casing 5, which says that no tests were
 # selected.
 #
+# Arguments:
 # $1 - suite base name
 # $2 - additional arguments to pass to pytest
+#
+# Options:
+# --cov         - code directory to report coverage
+# --cov-report  - code coverage report format
+# -p, --package - package names need to be tested
+#
+# Example usages:
+# - Run all tests and generate coverage report
+#   `$ ./run_pytest.sh envname posargs --cov-report xml:codecov.xml --cov apache_beam`
+# - Run tests under apache_beam.typehints and apache_beam.options packages
+#   `$ ./run_pytest.sh envname posargs -p apache_beam.typehints -p apache_beam.options
 
-envname=${1?First argument required: suite base name}
-posargs=$2
-coverage_report=$3
-coverage_root=$4
+test_packages=""
+
+check_argument_existence() {
+  if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+    echo "Argument is required for this option: $1" 1>&2
+    exit 1
+  fi
+}
+
+for OPT in "$@"
+do
+  case $OPT in
+    -p | --package)
+      check_argument_existence "$1" "$2"
+      test_packages="${test_packages}${2} "
+      shift 2
+      ;;
+    --cov-report)
+      check_argument_existence "$1" "$2"
+      coverage_report="--cov-report=${2}"
+      shift 2
+      ;;
+    --cov)
+      check_argument_existence "$1" "$2"
+      coverage_root="--cov=${2}"
+      shift 2
+      ;;
+    *)
+      if [[ ! "$1" =~ ^-+ ]]; then
+        if ! ${envname+:} false; then
+          envname=$1
+        elif ! ${posargs+:} false; then
+          posargs=$1
+        fi
+        shift 1
+      fi
+      ;;
+  esac
+done
+
+if [[ -z "${envname}" ]]; then
+  echo "First argument required: suite base name" 1>&2
+  exit 1
+fi
 
 # Run with pytest-xdist and without.
-pytest -o junit_suite_name=${envname} \
+pytest ${test_packages} -o junit_suite_name=${envname} \
   --junitxml=pytest_${envname}.xml -m 'not no_xdist' -n 6 ${coverage_report} ${coverage_root} --pyargs ${posargs}
 status1=$?
-pytest -o junit_suite_name=${envname}_no_xdist \
+pytest ${test_packages} -o junit_suite_name=${envname}_no_xdist \
   --junitxml=pytest_${envname}_no_xdist.xml -m 'no_xdist' ${coverage_report} ${coverage_root} --pyargs ${posargs}
 status2=$?
 
