720d79608974b836fc92e885c717023563f059a3
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-08-27 11:30:04 +0200

    yapf

--- .test-infra/jenkins/metrics_report/dashboards_parser.py
+++ .test-infra/jenkins/metrics_report/dashboards_parser.py
@@ -20,101 +20,96 @@ import unittest
 
 
 class Dashboard:
-    def __init__(self, file):
-        self.file = file
-        self.uid, self.queries = self.get_dashboard_uid_and_queries(file)
-        self.regexes = set(self.parse_query_to_regex(query) for query in self.queries)
+  def __init__(self, file):
+    self.file = file
+    self.uid, self.queries = self.get_dashboard_uid_and_queries(file)
+    self.regexes = set(
+        self.parse_query_to_regex(query) for query in self.queries)
 
-    @staticmethod
-    def get_dashboard_uid_and_queries(file):
-        queries = []
-        with open(file, "r") as f:
-            data = json.load(f)
-            uid = data.get("uid")
-            for panel in data.get("panels", []):
-                for target in panel.get("targets", []):
-                    query = target.get("query")
-                    queries.append(query)
+  @staticmethod
+  def get_dashboard_uid_and_queries(file):
+    queries = []
+    with open(file, "r") as f:
+      data = json.load(f)
+      uid = data.get("uid")
+      for panel in data.get("panels", []):
+        for target in panel.get("targets", []):
+          query = target.get("query")
+          queries.append(query)
 
-        return uid, queries
+    return uid, queries
 
-    @staticmethod
-    def parse_query_to_regex(query):
-        select_pattern = r"(.*FROM\s)(.*)(\sWHERE.*)"
-        match = re.match(select_pattern, query)
-        if match:
-            from_ = match.group(2)
-            without_quotes = re.sub(r"\"", "", from_)
-            without_retention_policy = without_quotes
-            if re.match(r"(\w+.\.)(.*)", without_quotes):
-                without_retention_policy = re.sub(r"(\w+.)(.*)", r"\2", without_quotes)
+  @staticmethod
+  def parse_query_to_regex(query):
+    select_pattern = r"(.*FROM\s)(.*)(\sWHERE.*)"
+    match = re.match(select_pattern, query)
+    if match:
+      from_ = match.group(2)
+      without_quotes = re.sub(r"\"", "", from_)
+      without_retention_policy = without_quotes
+      if re.match(r"(\w+.\.)(.*)", without_quotes):
+        without_retention_policy = re.sub(r"(\w+.)(.*)", r"\2", without_quotes)
 
-            replaced_parameters = re.sub(
-                r"\$\{\w+\}", r"[\\w\\d]*", without_retention_policy
-            )
-            return replaced_parameters
+      replaced_parameters = re.sub(
+          r"\$\{\w+\}", r"[\\w\\d]*", without_retention_policy)
+      return replaced_parameters
 
-    @staticmethod
-    def _get_json_files_from_directory(directory):
-        return [
-            os.path.join(directory, i)
-            for i in os.listdir(directory)
-            if i.endswith(".json")
-        ]
+  @staticmethod
+  def _get_json_files_from_directory(directory):
+    return [
+        os.path.join(directory, i) for i in os.listdir(directory)
+        if i.endswith(".json")
+    ]
 
-    @classmethod
-    def get_dashboards_from_directory(cls, directory):
-        for file in cls._get_json_files_from_directory(directory):
-            yield cls(file)
+  @classmethod
+  def get_dashboards_from_directory(cls, directory):
+    for file in cls._get_json_files_from_directory(directory):
+      yield cls(file)
 
 
 def guess_dashboard_by_measurement(
-    measurement, directory, additional_query_substrings=None
-):
-    """
-    Guesses dashboard by measurement name by parsing queries and matching it with measurement.
-    It is done by using regular expressions obtained from queries.
-    Additionally query can be checked for presence of any of the substrings.
-    """
-    dashboards = list(Dashboard.get_dashboards_from_directory(directory))
-    ret = []
-    for dashboard in dashboards:
-        for regex in dashboard.regexes:
-            if additional_query_substrings and not any(
-                substring.lower() in query.lower()
-                for substring in additional_query_substrings
-                for query in dashboard.queries
-            ):
-                continue
-            if regex and re.match(regex, measurement):
-                ret.append(dashboard)
-    return list(set(ret))
+    measurement, directory, additional_query_substrings=None):
+  """
+  Guesses dashboard by measurement name by parsing queries and matching it with measurement.
+  It is done by using regular expressions obtained from queries.
+  Additionally query can be checked for presence of any of the substrings.
+  """
+  dashboards = list(Dashboard.get_dashboards_from_directory(directory))
+  ret = []
+  for dashboard in dashboards:
+    for regex in dashboard.regexes:
+      if additional_query_substrings and not any(
+          substring.lower() in query.lower()
+          for substring in additional_query_substrings
+          for query in dashboard.queries):
+        continue
+      if regex and re.match(regex, measurement):
+        ret.append(dashboard)
+  return list(set(ret))
 
 
 class TestParseQueryToRegex(unittest.TestCase):
-    def test_parse_query_to_regex_1(self):
-        query = (
-            'SELECT "runtimeMs" FROM "forever"."nexmark_${ID}_${processingType}" WHERE '
-            '"runner" =~ /^$runner$/ AND $timeFilter GROUP BY "runner"'
-        )
-        expected = r"nexmark_[\w\d]*_[\w\d]*"
-        result = Dashboard.parse_query_to_regex(query)
-        self.assertEqual(expected, result)
+  def test_parse_query_to_regex_1(self):
+    query = (
+        'SELECT "runtimeMs" FROM "forever"."nexmark_${ID}_${processingType}" WHERE '
+        '"runner" =~ /^$runner$/ AND $timeFilter GROUP BY "runner"')
+    expected = r"nexmark_[\w\d]*_[\w\d]*"
+    result = Dashboard.parse_query_to_regex(query)
+    self.assertEqual(expected, result)
 
-    def test_parse_query_to_regex_2(self):
-        query = (
-            'SELECT mean("value") FROM "python_bqio_read_10GB_results" WHERE "metric" '
-            '=~ /runtime/ AND $timeFilter GROUP BY time($__interval), "metric"'
-        )
-        expected = "python_bqio_read_10GB_results"
-        result = Dashboard.parse_query_to_regex(query)
-        self.assertEqual(expected, result)
+  def test_parse_query_to_regex_2(self):
+    query = (
+        'SELECT mean("value") FROM "python_bqio_read_10GB_results" WHERE "metric" '
+        '=~ /runtime/ AND $timeFilter GROUP BY time($__interval), "metric"')
+    expected = "python_bqio_read_10GB_results"
+    result = Dashboard.parse_query_to_regex(query)
+    self.assertEqual(expected, result)
 
-    def test_parse_query_to_regex_3(self):
-        query = (
-            'SELECT mean("value") FROM "${sdk}_${processingType}_cogbk_3" WHERE '
-            '"metric" =~ /runtime/ AND $timeFilter GROUP BY time($__interval), "metric"'
-        )
-        expected = "[\w\d]*_[\w\d]*_cogbk_3"
-        result = Dashboard.parse_query_to_regex(query)
-        self.assertEqual(expected, result)
+  def test_parse_query_to_regex_3(self):
+    query = (
+        'SELECT mean("value") FROM "${sdk}_${processingType}_cogbk_3" WHERE '
+        '"metric" =~ /runtime/ AND $timeFilter GROUP BY time($__interval), "metric"'
+    )
+    expected = "[\w\d]*_[\w\d]*_cogbk_3"
+    result = Dashboard.parse_query_to_regex(query)
+    self.assertEqual(expected, result)
--- .test-infra/jenkins/metrics_report/report_generator.py
+++ .test-infra/jenkins/metrics_report/report_generator.py
@@ -29,11 +29,11 @@ INFLUXDB_USER = os.getenv("INFLUXDB_USER")
 INFLUXDB_USER_PASSWORD = os.getenv("INFLUXDB_USER_PASSWORD")
 WORKING_SPACE = os.getenv("WORKSPACE", "")
 FINAL_REPORT = os.path.join(
-    WORKING_SPACE, "/src/.test-infra/jenkins/metrics_report/beam-metrics_report.html"
-)
+    WORKING_SPACE,
+    "/src/.test-infra/jenkins/metrics_report/beam-metrics_report.html")
 PERF_DASHBOARDS = os.path.join(
-    WORKING_SPACE, "/src/.test-infra/metrics/grafana/dashboards/perftests_metrics/"
-)
+    WORKING_SPACE,
+    "/src/.test-infra/metrics/grafana/dashboards/perftests_metrics/")
 
 TABLE_FIELD_NAMES = [
     "Measurement",
@@ -65,175 +65,164 @@ QUERY_RUNTIME_MS = """SELECT mean("runtimeMs") AS "mean_value"
 
 
 def parse_arguments():
-    """
-    Gets all necessary data.
-    Return: influx_host, influx_port, influx_db
-    """
-    parser = argparse.ArgumentParser(
-        description="Script for generating Beam Metrics Report."
-    )
-    parser.add_argument("--influx-host", required=True)
-    parser.add_argument("--influx-port", required=True)
-    parser.add_argument("--influx-db", required=True)
+  """
+  Gets all necessary data.
+  Return: influx_host, influx_port, influx_db
+  """
+  parser = argparse.ArgumentParser(
+      description="Script for generating Beam Metrics Report.")
+  parser.add_argument("--influx-host", required=True)
+  parser.add_argument("--influx-port", required=True)
+  parser.add_argument("--influx-db", required=True)
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    influx_host = args.influx_host
-    influx_port = args.influx_port
-    influx_db = args.influx_db
+  influx_host = args.influx_host
+  influx_port = args.influx_port
+  influx_db = args.influx_db
 
-    return influx_host, influx_port, influx_db
+  return influx_host, influx_port, influx_db
 
 
 def get_retention_policies_names(client, database):
-    return (
-        i.get("name") for i in client.get_list_retention_policies(database=database)
-    )
+  return (
+      i.get("name")
+      for i in client.get_list_retention_policies(database=database))
 
 
 def get_measurements_names(client):
-    return (i.get("name") for i in client.get_list_measurements())
+  return (i.get("name") for i in client.get_list_measurements())
 
 
 def calc_diff(prev, curr):
-    """Returns percentage difference between two values."""
-    return (
-        (curr - prev) / prev * 100.0
-        if prev != 0
-        else float("inf") * abs(curr) / curr
-        if curr != 0
-        else 0.0
-    )
+  """Returns percentage difference between two values."""
+  return ((curr - prev) / prev * 100.0 if prev != 0 else float("inf") *
+          abs(curr) / curr if curr != 0 else 0.0)
 
 
 def _get_query_runtime_data(client, bind_params):
-    """Returns data for measurements with runtime, write_time or read_time metrics"""
-    data = []
-    result = client.query(QUERY_RUNTIME.format(**bind_params))
-    for i in result.items():
-        measurement = i[0][0]
-        metric = i[0][1].get("metric")
-        runner = "-"
-        measurement_data = list(i[1])
+  """Returns data for measurements with runtime, write_time or read_time metrics"""
+  data = []
+  result = client.query(QUERY_RUNTIME.format(**bind_params))
+  for i in result.items():
+    measurement = i[0][0]
+    metric = i[0][1].get("metric")
+    runner = "-"
+    measurement_data = list(i[1])
 
-        if all(m not in metric for m in ["runtime", "write_time", "read_time"]):
-            continue
+    if all(m not in metric for m in ["runtime", "write_time", "read_time"]):
+      continue
 
-        if len(measurement_data) >= 2:
-            previous = measurement_data[-2]["mean_value"]
-            current = measurement_data[-1]["mean_value"]
-            diff = calc_diff(previous, current)
-            dashboards = [
-                "http://metrics.beam.apache.org/d/{}".format(dashboard.uid)
-                for dashboard in guess_dashboard_by_measurement(
-                    measurement,
-                    PERF_DASHBOARDS,
-                    ["runtime", "write_time", "read_time"],
-                )
-            ]
-            data.append(
-                [
-                    measurement,
-                    metric,
-                    runner,
-                    round(previous, 2),
-                    round(current, 2),
-                    round(diff, 2),
-                    dashboards,
-                ]
-            )
+    if len(measurement_data) >= 2:
+      previous = measurement_data[-2]["mean_value"]
+      current = measurement_data[-1]["mean_value"]
+      diff = calc_diff(previous, current)
+      dashboards = [
+          "http://metrics.beam.apache.org/d/{}".format(dashboard.uid)
+          for dashboard in guess_dashboard_by_measurement(
+              measurement,
+              PERF_DASHBOARDS,
+              ["runtime", "write_time", "read_time"],
+          )
+      ]
+      data.append([
+          measurement,
+          metric,
+          runner,
+          round(previous, 2),
+          round(current, 2),
+          round(diff, 2),
+          dashboards,
+      ])
 
-    return data
+  return data
 
 
 def _get_query_runtime_ms_data(client, bind_params):
-    """Returns data for measurements with RuntimeMs metrics"""
-    data = []
-    result = client.query(QUERY_RUNTIME_MS.format(**bind_params))
-    for i in result.items():
-        measurement = i[0][0]
-        metric = "RuntimeMs"
-        runner = i[0][1].get("runner")
-        measurement_data = list(i[1])
+  """Returns data for measurements with RuntimeMs metrics"""
+  data = []
+  result = client.query(QUERY_RUNTIME_MS.format(**bind_params))
+  for i in result.items():
+    measurement = i[0][0]
+    metric = "RuntimeMs"
+    runner = i[0][1].get("runner")
+    measurement_data = list(i[1])
 
-        if len(measurement_data) >= 2:
-            previous = measurement_data[-2]["mean_value"]
-            current = measurement_data[-1]["mean_value"]
-            diff = calc_diff(previous, current)
-            dashboards = [
-                "http://metrics.beam.apache.org/d/{}".format(dashboard.uid)
-                for dashboard in guess_dashboard_by_measurement(
-                    measurement, PERF_DASHBOARDS, [metric]
-                )
-            ]
-            data.append(
-                [
-                    measurement,
-                    metric,
-                    runner,
-                    round(previous, 2),
-                    round(current, 2),
-                    round(diff, 2),
-                    dashboards,
-                ]
-            )
+    if len(measurement_data) >= 2:
+      previous = measurement_data[-2]["mean_value"]
+      current = measurement_data[-1]["mean_value"]
+      diff = calc_diff(previous, current)
+      dashboards = [
+          "http://metrics.beam.apache.org/d/{}".format(dashboard.uid)
+          for dashboard in guess_dashboard_by_measurement(
+              measurement, PERF_DASHBOARDS, [metric])
+      ]
+      data.append([
+          measurement,
+          metric,
+          runner,
+          round(previous, 2),
+          round(current, 2),
+          round(diff, 2),
+          dashboards,
+      ])
 
-    return data
+  return data
 
 
 def get_metrics_data(client, database):
-    data = []
-    for retention_policy, measurements_name in product(
-        get_retention_policies_names(client, database), get_measurements_names(client)
-    ):
-        bind_params = {
-            "database": database,
-            "measurement": measurements_name,
-            "retention_policy": retention_policy,
-        }
+  data = []
+  for retention_policy, measurements_name in product(
+      get_retention_policies_names(client, database), get_measurements_names(client)
+  ):
+    bind_params = {
+        "database": database,
+        "measurement": measurements_name,
+        "retention_policy": retention_policy,
+    }
 
-        data.extend(_get_query_runtime_data(client, bind_params))
-        data.extend(_get_query_runtime_ms_data(client, bind_params))
+    data.extend(_get_query_runtime_data(client, bind_params))
+    data.extend(_get_query_runtime_ms_data(client, bind_params))
 
-    return [d for d in data if d]
+  return [d for d in data if d]
 
 
 def print_table(data):
-    table = PrettyTable()
-    table.field_names = TABLE_FIELD_NAMES
-    for d in data:
-        table.add_row(d)
-    print(table)
+  table = PrettyTable()
+  table.field_names = TABLE_FIELD_NAMES
+  for d in data:
+    table.add_row(d)
+  print(table)
 
 
 def generate_report(data):
-    logging.info("Generating {}".format(FINAL_REPORT))
-    env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates")
-        ),
-    )
-    template = env.get_template("Metrics_Report.template")
-    with open(FINAL_REPORT, "w") as file:
-        file.write(template.render(headers=TABLE_FIELD_NAMES, metrics_data=data))
-    logging.info("{} saved.".format(FINAL_REPORT))
+  logging.info("Generating {}".format(FINAL_REPORT))
+  env = jinja2.Environment(
+      loader=jinja2.FileSystemLoader(
+          os.path.join(
+              os.path.dirname(os.path.realpath(__file__)), "templates")),
+  )
+  template = env.get_template("Metrics_Report.template")
+  with open(FINAL_REPORT, "w") as file:
+    file.write(template.render(headers=TABLE_FIELD_NAMES, metrics_data=data))
+  logging.info("{} saved.".format(FINAL_REPORT))
 
 
 def main():
-    influx_host, influx_port, influx_db = parse_arguments()
+  influx_host, influx_port, influx_db = parse_arguments()
 
-    client = InfluxDBClient(
-        host=influx_host,
-        port=influx_port,
-        database=influx_db,
-        username=INFLUXDB_USER,
-        password=INFLUXDB_USER_PASSWORD,
-    )
+  client = InfluxDBClient(
+      host=influx_host,
+      port=influx_port,
+      database=influx_db,
+      username=INFLUXDB_USER,
+      password=INFLUXDB_USER_PASSWORD,
+  )
 
-    data = get_metrics_data(client, influx_db)
-    print_table(data)
-    generate_report(data)
+  data = get_metrics_data(client, influx_db)
+  print_table(data)
+  generate_report(data)
 
 
 if __name__ == "__main__":
-    main()
+  main()
