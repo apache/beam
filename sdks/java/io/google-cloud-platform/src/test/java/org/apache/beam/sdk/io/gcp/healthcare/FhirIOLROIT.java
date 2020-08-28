f5181fb77f9af4df70c12a355543a9ed43424a92
trucleduc <trucleduc@google.com>
2020-08-28 16:44:25 -0400

    Add deidentify FHIR resources IO connector.

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/healthcare/FhirIOLROIT.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/healthcare/FhirIOLROIT.java
@@ -22,7 +22,6 @@ import static org.apache.beam.sdk.io.gcp.healthcare.HL7v2IOTestUtil.HEALTHCARE_D
 
 import com.google.api.services.healthcare.v1beta1.model.DeidentifyConfig;
 import com.google.api.services.healthcare.v1beta1.model.FhirStore;
-import com.google.auth.oauth2.GoogleCredentials;
 import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.List;
@@ -74,7 +73,6 @@ public class FhirIOLROIT {
 
   @After
   public void deleteAllFhirStores() throws IOException {
-    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
     HealthcareApiClient client = new HttpHealthcareApiClient();
     List<FhirStore> fhirStores = client.listAllFhirStores(healthcareDataset);
     for (FhirStore fs : fhirStores) {
