8e1b00e9545465bd80a27bc9740bc8549fa0762c
yoshiki.obata <yoshiki.obata@gmail.com>
2020-09-01 23:15:04 +0900

    [BEAM-9943] [BEAM-9944] define priority for python versions and configure  precommit/postcommit tasks according to it.

---
+++ .test-infra/jenkins/job_PostCommit_Python_Tox.groovy
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import CommonJobProperties as commonJobProperties
+import PostcommitJobBuilder
+import CronJobBuilder
+
+def toxPostCommitJob = { scope ->
+  scope.description('Runs the Python tox suite post commit tasks.')
+  commonJobProperties.setTopLevelMainJobProperties(scope)
+
+  scope.steps {
+    gradle {
+      rootBuildScriptDir(commonJobProperties.checkoutDir)
+      tasks(':pythonToxPostCommit')
+      commonJobProperties.setGradleSwitches(scope)
+    }
+  }
+}
+
+// This job runs the suite of Python Tox PostCommit tests.
+PostcommitJobBuilder.postCommitJob('beam_PostCommit_Python_Tox',
+    'Run Python Tox PostCommit', 'Python Tox PostCommit Tests', this) {
+      toxPostCommitJob(delegate)
+    }
+
+CronJobBuilder.cronJob(
+    'beam_PostCommit_Python_Tox',
+    'H 16 * * *',
+    this
+    ) {
+      toxPostCommitJob(delegate)
+    }
