23fa8bcb8d29d5df820af4e5bc493f8864690f12
Pablo Estrada <pabloem@apache.org>
2020-09-01 11:59:28 -0700

    Fixup

--- sdks/python/apache_beam/utils/retry.py
+++ sdks/python/apache_beam/utils/retry.py
@@ -71,10 +71,10 @@ class PermanentException(Exception):
   pass
 
 
-PERMANENT_ERROR_TYPES = PermanentException
-
-if InvalidDataError:
-  PERMANENT_ERROR_TYPES = (PERMANENT_ERROR_TYPES, InvalidDataError)
+if isinstance(InvalidDataError, type):
+  PERMANENT_ERROR_TYPES = [PermanentException, InvalidDataError]
+else:
+  PERMANENT_ERROR_TYPES = [PermanentException]
 
 
 class FuzzedExponentialIntervals(object):
@@ -128,7 +128,7 @@ def retry_on_server_errors_filter(exception):
     return exception.status_code >= 500
   if (S3ClientError is not None) and isinstance(exception, S3ClientError):
     return exception.code >= 500
-  return not isinstance(exception, PERMANENT_ERROR_TYPES)
+  return not isinstance(exception, tuple(PERMANENT_ERROR_TYPES))
 
 
 # TODO(BEAM-6202): Dataflow returns 404 for job ids that actually exist.
