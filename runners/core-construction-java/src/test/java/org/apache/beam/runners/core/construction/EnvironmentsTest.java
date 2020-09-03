5ff242b087b84cf04587ed3cf7290bce8066f2ac
Boyuan Zhang <boyuanz@google.com>
2020-09-03 15:54:14 -0700

    [BEAM-10505][BEAM-10530] Add truncate capability.

--- runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/EnvironmentsTest.java
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/EnvironmentsTest.java
@@ -104,13 +104,12 @@ public class EnvironmentsTest implements Serializable {
     assertThat(
         Environments.getJavaCapabilities(),
         hasItem(BeamUrns.getUrn(RunnerApi.StandardProtocols.Enum.MULTI_CORE_BUNDLE_PROCESSING)));
-    // TODO(BEAM-10505): Add the check back.
-    // assertThat(
-    //     Environments.getJavaCapabilities(),
-    //     hasItem(
-    //         BeamUrns.getUrn(
-    //             RunnerApi.StandardPTransforms.SplittableParDoComponents
-    //                 .TRUNCATE_SIZED_RESTRICTION)));
+    assertThat(
+        Environments.getJavaCapabilities(),
+        hasItem(
+            BeamUrns.getUrn(
+                RunnerApi.StandardPTransforms.SplittableParDoComponents
+                    .TRUNCATE_SIZED_RESTRICTION)));
   }
 
   @Test
