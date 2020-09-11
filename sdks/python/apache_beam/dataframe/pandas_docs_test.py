fc9e793091570a2a4196dc5fdd10d518feff9a9a
Robert Bradshaw <robertwb@gmail.com>
2020-09-11 12:54:17 -0700

    avoid unitttest

--- sdks/python/apache_beam/dataframe/pandas_docs_test.py
+++ sdks/python/apache_beam/dataframe/pandas_docs_test.py
@@ -16,6 +16,8 @@
 
 """A module for running the pandas docs (such as the users guide) against our
 dataframe implementation.
+
+Run as python -m apache_beam.dataframe.pandas_docs_test [getting_started ...]
 """
 
 from __future__ import absolute_import
@@ -41,14 +43,16 @@ PANDAS_VERSION = '1.1.1'
 PANDAS_DIR = os.path.expanduser("~/.apache_beam/cache/pandas-" + PANDAS_VERSION)
 PANDAS_DOCS_SOURCE = os.path.join(PANDAS_DIR, 'doc', 'source')
 
-# This is here to populate PANDAS_DIR early when called manually.
 
-if __name__ == '__main__':
+def main():
   if not os.path.exists(PANDAS_DIR):
+    # Download the pandas source.
     os.makedirs(os.path.dirname(PANDAS_DIR), exist_ok=True)
     zip = os.path.join(PANDAS_DIR + '.zip')
     if not os.path.exists(zip):
-      url = 'https://github.com/pandas-dev/pandas/archive/v%s.zip' % PANDAS_VERSION
+      url = (
+          'https://github.com/pandas-dev/pandas/archive/v%s.zip' %
+          PANDAS_VERSION)
       print('Downloading', url)
       with urllib.request.urlopen(url) as fin:
         with open(zip, 'wb') as fout:
@@ -58,60 +62,38 @@ if __name__ == '__main__':
     with zipfile.ZipFile(zip, 'r') as handle:
       handle.extractall(os.path.dirname(PANDAS_DIR))
 
+  tests = sys.argv[1:] or ['getting_started', 'user_guide']
+  paths = []
+  filters = []
 
-@unittest.skipIf(sys.version_info <= (3, ), 'Requires contextlib.ExitStack.')
-@unittest.skipIf(
-    not os.path.exists(PANDAS_DIR),
-    'Run python -m apache_beam.dataframe.pandas_docs_test to download pandas tests.'
-)
-class PandasDocsTest(unittest.TestCase):
-  @classmethod
-  def setUpClass(cls):
-    unittest.TestCase.setUpClass()
-    cls._running_summary = doctests.Summary()
+  # Explicit paths.
+  for test in tests:
+    if os.path.exists(test):
+      paths.append(test)
+    else:
+      filters.append(test)
 
-  @classmethod
-  def tearDownClass(cls):
-    print("Final summary:")
-    cls._running_summary.summarize()
-    unittest.TestCase.tearDownClass()
-
-  def _run_rst_tests(
-      self, path, report=True, wont_implement_ok=['*'], skip=[], **kwargs):
-
-    with open(os.path.join(PANDAS_DOCS_SOURCE, *path.split('/'))) as f:
-      rst = f.read()
-
-    result = doctests.test_rst_ipython(
-        rst, path, report=report, wont_implement_ok=wont_implement_ok, **kwargs)
-    type(self)._running_summary += result.summary
-    self.assertEqual(result.failed, 0)
-
-  # TODO(robertwb): Auto-generate?
-  def test_10min(self):
-    self._run_rst_tests('user_guide/10min.rst', use_beam=False)
-
-  def test_basics(self):
-    self._run_rst_tests('user_guide/basics.rst', use_beam=False)
-
-  def test_groupby(self):
-    self._run_rst_tests('user_guide/groupby.rst', use_beam=False)
-
-
-if os.path.exists(PANDAS_DOCS_SOURCE):
+  # Names of pandas source files.
   for root, dirs, files in os.walk(PANDAS_DOCS_SOURCE):
-    if 'getting_started' not in root and 'user_guide' not in root:
-      continue
     for name in files:
       base, ext = os.path.splitext(name)
       if ext == '.rst':
-        setattr(
-            PandasDocsTest,
-            'test_%s' % base,
-            lambda self,
-            path=os.path.join(root[len(PANDAS_DOCS_SOURCE):], name): self._run_rst_tests(
-                path, use_beam=False))
+        path = os.path.join(root, name)
+        if any(filter in path for filter in filters):
+          paths.append(path)
+
+  # Now run all the tests.
+  running_summary = doctests.Summary()
+  for path in paths:
+    with open(path) as f:
+      rst = f.read()
+    running_summary += doctests.test_rst_ipython(
+        rst, path, report=True, wont_implement_ok=['*'], use_beam=False).summary
+
+  print('*' * 70)
+  print("Final summary:")
+  running_summary.summarize()
 
 
 if __name__ == '__main__':
-  unittest.main()
+  main()
