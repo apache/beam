adb01d617442ca0543a003b9a41f087300182b73
Yixing Zhang <yixzhang@linkedin.com>
2020-09-28 15:26:37 -0700

    Fix setupVirtualenv
    RB=2304256
    BUG=LISAMZA-18496
    G=samza-reviewers
    R=dchen1
    A=dchen1

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1961,7 +1961,7 @@ class BeamModulePlugin implements Plugin<Project> {
           project.exec { commandLine virtualenvCmd }
           project.exec {
             executable 'sh'
-            args '-c', ". ${project.ext.envdir}/bin/activate && pip install --retries 10 --upgrade tox==3.11.1 grpcio-tools==1.3.5 dill==0.2.9 future crcmod fastavro oauth2client mock pytz protobuf grpcio pydot PyYAML PyVCF hdfs avro-python3"
+            args '-c', ". ${project.ext.envdir}/bin/activate && pip install --retries 10 --upgrade tox==3.11.1 -r ${project.rootDir}/sdks/python/build-requirements.txt -r ${project.rootDir}/sdks/python/build-requirements-linkedin.txt"
           }
         }
         // Gradle will delete outputs whenever it thinks they are stale. Putting a
