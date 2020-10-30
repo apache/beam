57df93a21803e1d06abeee2669e78a45c46ef95e
Tomasz Szerszeń <tomasz.szerszen@polidea.com>
2020-10-30 11:27:51 +0100

    [BEAM-5939] - Deduplicate constants

--- sdks/python/apache_beam/runners/dataflow/internal/names.py
+++ sdks/python/apache_beam/runners/dataflow/internal/names.py
@@ -27,6 +27,7 @@ from __future__ import absolute_import
 # Standard file names used for staging files.
 from builtins import object
 
+# Referenced by Dataflow legacy worker.
 from apache_beam.runners.internal.names import PICKLED_MAIN_SESSION_FILE  # pylint: disable=unused-import
 
 # String constants related to sources framework
