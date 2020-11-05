f4db36e9db442316a0b634d5ed1ed1e611dd00e8
Kamil Wasilewski <kamil.wasilewski@polidea.com>
2020-11-05 04:02:00 +0100

    [BEAM-3736] Disable CombineFnVisitor (#13266)
    CombineFnVisitor is not working properly and triggers an exception for correct pipelines. It should be disabled until being fixed.

--- sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
+++ sdks/python/apache_beam/runners/dataflow/dataflow_runner_test.py
@@ -757,6 +757,7 @@ class DataflowRunnerTest(unittest.TestCase, ExtraAssertionsMixin):
         out = p | beam.Create([1]) | beam.io.WriteToBigQuery('some.table')
         out['destination_file_pairs'] | 'MyTransform' >> beam.Map(lambda _: _)
 
+  @unittest.skip('BEAM-3736: enable once CombineFnVisitor is fixed')
   def test_unsupported_combinefn_fail(self):
     class CombinerWithNonDefaultSetupTeardown(combiners.CountCombineFn):
       def setup(self, *args, **kwargs):
