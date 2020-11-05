50904ded6bfe25ae0b8f66e5df4cdf8ec24ea869
lostluck <13907733+lostluck@users.noreply.github.com>
2020-11-05 13:33:18 -0800

    Moving to 2.27.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -341,7 +341,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.26.0'
+    project.version = '2.27.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
