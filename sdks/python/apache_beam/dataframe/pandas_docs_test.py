05f520074b3a456f2ecf5df44f1602ca14bcb415
Robert Bradshaw <robertwb@google.com>
2020-11-11 16:23:18 -0800

    [BEAM-9561] Run pandas doctests in parallel. (#13286)

--- sdks/python/apache_beam/dataframe/pandas_docs_test.py
+++ sdks/python/apache_beam/dataframe/pandas_docs_test.py
@@ -23,8 +23,14 @@ Run as python -m apache_beam.dataframe.pandas_docs_test [getting_started ...]
 from __future__ import absolute_import
 from __future__ import print_function
 
+import argparse
+import contextlib
+import io
+import multiprocessing
 import os
 import sys
+import time
+import urllib.request
 import zipfile
 
 from apache_beam.dataframe import doctests
@@ -35,8 +41,17 @@ PANDAS_DOCS_SOURCE = os.path.join(PANDAS_DIR, 'doc', 'source')
 
 
 def main():
-  # Not available for Python 2.
-  import urllib.request
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '-p',
+      '--parallel',
+      type=int,
+      default=0,
+      help='Number of tests to run in parallel. '
+      'Defaults to 0, meaning the number of cores on the machine.')
+  parser.add_argument('docs', nargs='*')
+  args = parser.parse_args()
+
   if not os.path.exists(PANDAS_DIR):
     # Download the pandas source.
     os.makedirs(os.path.dirname(PANDAS_DIR), exist_ok=True)
@@ -55,7 +70,7 @@ def main():
     with zipfile.ZipFile(zip, 'r') as handle:
       handle.extractall(os.path.dirname(PANDAS_DIR))
 
-  tests = sys.argv[1:] or ['getting_started', 'user_guide']
+  tests = args.docs or ['getting_started', 'user_guide']
   paths = []
   filters = []
 
@@ -74,22 +89,67 @@ def main():
         if any(filter in path for filter in filters):
           paths.append(path)
 
+  # Using a global here is a bit hacky, but avoids pickling issues when used
+  # with multiprocessing.
+  parallelism = max(args.parallel or multiprocessing.cpu_count(), len(paths))
+
+  if parallelism > 1:
+    pool_map = multiprocessing.pool.Pool(parallelism).imap_unordered
+    run_tests = run_tests_capturing_stdout
+    # Make sure slow tests get started first.
+    paths.sort(
+        key=lambda path: ('enhancingperf' in path, os.path.getsize(path)),
+        reverse=True)
+  else:
+    pool_map = map
+    run_tests = run_tests_streaming_stdout
+
   # Now run all the tests.
   running_summary = doctests.Summary()
-  for path in paths:
-    with open(path) as f:
-      rst = f.read()
-    running_summary += doctests.test_rst_ipython(
-        rst,
-        path,
-        report=True,
-        wont_implement_ok=['*'],
-        not_implemented_ok=['*'],
-        use_beam=False).summary
+  for count, (summary, stdout) in enumerate(pool_map(run_tests, paths)):
+    running_summary += summary
+    if stdout:
+      print(stdout)
+    print(count, '/', len(paths), 'done.')
 
-  print('*' * 70)
+  print('*' * 72)
   print("Final summary:")
   running_summary.summarize()
+
+
+def run_tests_capturing_stdout(path):
+  with deferred_stdout() as stdout:
+    return run_tests(path), stdout()
+
+
+def run_tests_streaming_stdout(path):
+  return run_tests(path), None
+
+
+def run_tests(path):
+  # Optionally capture the stdout as interleaved test errors are painful
+  # to debug.  On the other hand, if there is no parallelism, let the
+  # output be streamed.
+  start = time.time()
+  with open(path) as f:
+    rst = f.read()
+  res = doctests.test_rst_ipython(
+      rst,
+      path,
+      report=True,
+      wont_implement_ok=['*'],
+      not_implemented_ok=['*'],
+      use_beam=False).summary
+  print("Total time for {}: {:.2f} secs".format(path, time.time() - start))
+  return res
+
+
+@contextlib.contextmanager
+def deferred_stdout():
+  captured = io.StringIO()
+  old_stdout, sys.stdout = sys.stdout, captured
+  yield captured.getvalue
+  sys.stdout = old_stdout
 
 
 if __name__ == '__main__':
