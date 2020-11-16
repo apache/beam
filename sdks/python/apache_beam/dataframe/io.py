b4ca7ffdd2ff902c43c06461b2adc631bb236c05
Brian Hulette <bhulette@google.com>
2020-11-16 15:04:19 -0800

    Reshuffle in pd.read_* (#13346) (#13347)

--- sdks/python/apache_beam/dataframe/io.py
+++ sdks/python/apache_beam/dataframe/io.py
@@ -169,6 +169,7 @@ class _ReadFromPandas(beam.PTransform):
     pcoll = (
         paths_pcoll
         | fileio.MatchFiles(self.path)
+        | beam.Reshuffle()
         | fileio.ReadMatches()
         | beam.ParDo(
             _ReadFromPandasDoFn(
