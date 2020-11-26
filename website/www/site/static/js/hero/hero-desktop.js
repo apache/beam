d018e7b32f992d7f149e68077c79c73292a6e253
bntnam <bntnam.app@gmail.com>
2020-11-26 15:57:06 +0700

    Implemented Hero component

---
+++ website/www/site/static/js/hero/hero-desktop.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("hero-desktop"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "/js/hero/hero-desktop.json",
+});
---
+++ website/www/site/static/js/hero/hero-mobile.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("hero-mobile"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "/js/hero/hero-mobile.json",
+});
