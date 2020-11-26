9a803f76d825322aa9164708ccb6f4ea4baaa1a9
Tomasz Szerszeń <tomasz.szerszen@polidea.com>
2020-11-26 09:59:19 +0100

    look for kv type

--- sdks/go/test/load/sideinput/sideinput.go
+++ sdks/go/test/load/sideinput/sideinput.go
@@ -18,7 +18,6 @@ package main
 import (
     "context"
     "flag"
-    "fmt"
     "github.com/apache/beam/sdks/go/pkg/beam"
     "github.com/apache/beam/sdks/go/pkg/beam/io/synthetic"
     "github.com/apache/beam/sdks/go/pkg/beam/log"
@@ -26,12 +25,14 @@ import (
 )
 
 var (
-    windowCount = flag.Int("window_count", 1,
+    windowCount = flag.Int(
+        "window_count",
+        1,
         "The number of fixed sized windows to subdivide the side input into.")
-    accessPercentage = flag.Int("access_percentage", 100,
+    accessPercentage = flag.Int(
+        "access_percentage",
+        100,
         "Specifies the percentage of elements in the side input to be accessed.")
-    sdfInitialElements = flag.Int("sdf_initial_elements", 1000,
-        "The number of SDF Initial Elements.")
     syntheticSourceConfig = flag.String(
         "input_options",
         "{"+
@@ -50,55 +51,11 @@ func parseSyntheticSourceConfig() synthetic.SourceConfig {
     }
 }
 
-
-type sourceSingleFromSourceConfig struct {
-    s beam.Scope
-}
-
-func (fn *sourceSingleFromSourceConfig) ProcessElement (ctx context.Context, _ []uint8, config synthetic.SourceConfig) beam.PCollection {
-    return synthetic.SourceSingle(fn.s, config)
-}
-
-type syntheticSDFAsSource struct {
-    s beam.Scope
-}
-
-func (fn *syntheticSDFAsSource) ProcessElement (ctx context.Context, _ []int) beam.PCollection {
-    return beam.ParDo(fn.s, &sourceSingleFromSourceConfig{s: fn.s}, beam.Impulse(fn.s),
-        beam.SideInput{Input: beam.Create(fn.s, parseSyntheticSourceConfig())})
-}
-
-type sequenceSideInputTestDoFn struct {
-    s beam.Scope
-    elementsToAccess int
-}
-
 type doFn struct {
-    s beam.Scope
     elementsToAccess int
 }
 
-type innerDoFn struct {
-    elementsToAccess int
-}
-
-func (fn *sequenceSideInputTestDoFn) ProcessElement (ctx context.Context, _ []byte, collection beam.PCollection) {
-    beam.ParDo0(fn.s, &doFn{s: fn.s, elementsToAccess: fn.elementsToAccess}, collection)
-}
-
-func (fn *doFn) ProcessElement (ctx context.Context, inner beam.PCollection) {
-    beam.ParDo0(fn.s, &innerDoFn{elementsToAccess: fn.elementsToAccess}, inner)
-}
-
-func (fn *innerDoFn) ProcessElement (ctx context.Context, key []uint8) {
-    i := 0
-    for true {
-        // if i >= fn.elementsToAccess {
-        //    break
-        // }
-        fmt.Println(key[i])
-        i += 1
-    }
+func (fn *doFn) ProcessElement(key []byte){
 }
 
 
@@ -109,31 +66,13 @@ func main() {
     p, s := beam.NewPipelineWithRoot()
 
     syntheticSourceConfig := parseSyntheticSourceConfig()
-    elementsPerWindow := syntheticSourceConfig.NumElements / *windowCount
-    elementsToAccess := elementsPerWindow * *accessPercentage / 100
+    // elementsPerWindow := syntheticSourceConfig.NumElements / *windowCount
+    // elementsToAccess := elementsPerWindow * *accessPercentage / 100
 
-    var mainInputValues []int
-    for i := 0; i < *windowCount; i++ {
-        mainInputValues = append(mainInputValues, i)
-    }
-    mainInput := beam.Create(s, mainInputValues)
-
-    var sideInput beam.PCollection
-    initialElements := *sdfInitialElements
-    if *windowCount > 1 {
-        sideInput = mainInput
-        initialElements = *windowCount
-    } else {
-        var sideInputValues []int
-        for i := 0; i < initialElements; i++ {
-            sideInputValues = append(sideInputValues, i)
-        }
-        sideInput = beam.Create(s, sideInputValues)
-    }
-    sideInput = beam.ParDo(s, &syntheticSDFAsSource{s: s}, sideInput)
-
-    beam.ParDo0(s, &sequenceSideInputTestDoFn{s: s, elementsToAccess: elementsToAccess}, beam.Impulse(s),
-        beam.SideInput{Input: sideInput})
+    beam.ParDo0(s, func(_ []byte, kv func (key *[]byte) bool) {
+        return
+    }, beam.Impulse(s),
+        beam.SideInput{Input: synthetic.SourceSingle(s, syntheticSourceConfig)})
 
     if err := beamx.Run(ctx, p); err != nil {
         log.Exitf(ctx, "Failed to execute job: %v", err)
