cf52062bf92bf1b741b9123ca1e3a666f5859882
Niels Basjes <niels@basjes.nl>
2020-11-30 15:54:59 +0100

    Fix javadoc

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/SqlTransform.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/SqlTransform.java
@@ -192,7 +192,8 @@ public abstract class SqlTransform extends PTransform<PInput, PCollection<Row>> 
    * <p>Any available implementation of {@link QueryPlanner} can be used as the query planner in
    * {@link SqlTransform}. An implementation can be specified globally for the entire pipeline with
    * {@link BeamSqlPipelineOptions#getPlannerName()}. The global planner can be overridden
-   * per-transform with {@link #withQueryPlannerClass}(Class<? extends {@link QueryPlanner}>).
+   * per-transform with {@link #withQueryPlannerClass(Class) #withQueryPlannerClass(Class<? extends
+   * QueryPlanner>)}.
    */
   public static SqlTransform query(String queryString) {
     return builder()
