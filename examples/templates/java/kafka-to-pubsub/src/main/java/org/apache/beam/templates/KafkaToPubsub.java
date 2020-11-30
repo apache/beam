3c63298179b3f7a485885e2de59994f6cf05897d
Ilya Kozyrev <ilya.kozyrev@akvelon.com>
2020-11-30 17:02:38 +0300

    fix getting ssl credentials from Vault

--- examples/templates/java/kafka-to-pubsub/src/main/java/org/apache/beam/templates/KafkaToPubsub.java
+++ examples/templates/java/kafka-to-pubsub/src/main/java/org/apache/beam/templates/KafkaToPubsub.java
@@ -156,22 +156,23 @@ public class KafkaToPubsub {
   public static PipelineResult run(KafkaToPubsubOptions options) {
     // Configure Kafka consumer properties
     Map<String, Object> kafkaConfig = new HashMap<>();
+    Map<String, String> sslConfig = new HashMap<>();
     if (options.getSecretStoreUrl() != null && options.getVaultToken() != null) {
       Map<String, Map<String, String>> credentials =
           getKafkaCredentialsFromVault(options.getSecretStoreUrl(), options.getVaultToken());
       kafkaConfig = configureKafka(credentials.get(KafkaPubsubConstants.KAFKA_CREDENTIALS));
+      sslConfig = credentials.get(KafkaPubsubConstants.SSL_CREDENTIALS);
     } else {
       LOG.warn(
           "No information to retrieve Kafka credentials was provided. "
               + "Trying to initiate an unauthorized connection.");
     }
 
-    Map<String, String> sslConfig = new HashMap<>();
     if (isSslSpecified(options)) {
       sslConfig.putAll(configureSsl(options));
     } else {
       LOG.info(
-          "No information to retrieve SSL certificate was provided. "
+          "No information to retrieve SSL certificate was provided by parameters."
               + "Trying to initiate a plain text connection.");
     }
 
