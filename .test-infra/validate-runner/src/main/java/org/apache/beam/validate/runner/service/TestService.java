daf36a64f03af73eec17f7343684999435e184d3
Sruthi Sree Kumar <sruthiskumar92@gmail.com>
2020-12-03 16:33:03 +0100

    added not run tests

--- .test-infra/validate-runner/src/main/java/org/apache/beam/validate/runner/service/TestService.java
+++ .test-infra/validate-runner/src/main/java/org/apache/beam/validate/runner/service/TestService.java
@@ -8,20 +8,19 @@ import net.sf.json.JSONObject;
 import org.apache.beam.validate.runner.model.CaseResult;
 import org.apache.beam.validate.runner.model.Configuration;
 import org.apache.beam.validate.runner.model.TestResult;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public interface TestService {
 
-    default List<CaseResults> getAllTests(TestResult testResult) {
-        List<CaseResult> caseResults = new ArrayList<>();
+    default Set<CaseResult> getAllTests(TestResult testResult) {
+        Set<CaseResult> caseResults = new HashSet<>();
         Optional.ofNullable(testResult.getSuites()).ifPresent(suites -> suites.forEach(item -> caseResults.addAll(item.getCases())));
 //        List<String> tests = new ArrayList<>();
 //        Optional.ofNullable(caseResults).ifPresent(cases -> cases.forEach(item -> tests.add(item.getClassName() + "." + item.getName() + " : " + item.getStatus())));
@@ -34,15 +33,41 @@ public interface TestService {
         return new URL(jobWithDetails.getLastSuccessfulBuild().getUrl() + configuration.getJsonapi());
     }
 
-    default JSONObject getBatchObject(Map<String, String> job, TestResult result) {
-        JSONArray tests = new JSONArray();
-        tests.addAll(getAllTests(result));
+    default Set<Pair<String, String>> getTestNames(TestResult testResult) {
+        Set<Pair<String, String>> caseResults = new HashSet<>();
+        Optional.ofNullable(testResult.getSuites()).ifPresent(suites -> suites.forEach(item -> item.getCases().forEach(caseResult -> caseResults.add(new ImmutablePair<>(caseResult.getName(), caseResult.getClassName())))));
+//        List<String> tests = new ArrayList<>();
+//        Optional.ofNullable(caseResults).ifPresent(cases -> cases.forEach(item -> tests.add(item.getClassName() + "." + item.getName() + " : " + item.getStatus())));
+        return caseResults;
+    }
 
-        JSONObject jsonOut = new JSONObject();
-        jsonOut.put("testCases", tests);
+    default JSONObject process(Set<Pair<String, String>> testnames, HashMap<String, Set<CaseResult>> map) {
+        map.forEach((k, v) -> {
+            Set<CaseResult> caseResults = v;
+            for(Pair<String, String> pair : testnames) {
+                boolean found = false;
+                for(CaseResult caseResult : caseResults) {
+                    if (caseResult.getName().equals(pair.getKey())) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(found) {
+                    found = false;
+                } else {
+                    caseResults.add(new CaseResult(pair.getValue(), "NOT RUN", pair.getKey()));
+                }
+            }
+        });
+
         JSONObject jsonMain = new JSONObject();
-        jsonMain.put(job.keySet().toArray()[0], jsonOut);
-
+        map.forEach((k, v) -> {
+            JSONArray tests = new JSONArray();
+            tests.addAll(v);
+            JSONObject jsonOut = new JSONObject();
+            jsonOut.put("testCases", tests);
+            jsonMain.put(k, jsonOut);
+        });
         return jsonMain;
     }
 }
