538f49382505a554725ec65b754b5641235880fc
Yichi Zhang <zyichi@google.com>
2020-12-04 12:29:36 -0800

    Fix format.

--- sdks/python/apache_beam/runners/portability/sdk_container_builder.py
+++ sdks/python/apache_beam/runners/portability/sdk_container_builder.py
@@ -237,24 +237,24 @@ class _SdkContainerImageCloudBuilder(SdkContainerImageBuilder):
     now = time.time()
     operation = client.create_build(project_id=project_id, build=build)
     _LOGGER.info(
-      'Building sdk container with google cloud build, this may '
-      'take a few minutes...')
+        'Building sdk container with Google Cloud Build, this may '
+        'take a few minutes...')
     # TODO: we shouldn't need to query build log url with list_builds if log
-    #  url is included in operation or build object.
+    # url is included in operation or build object.
     build_lists = client.list_builds(
-      project_id=project_id,
-      filter="source.storage_source.bucket=\"%s\" AND source"
-             ".storage_source.object=\"%s\"" % (gcs_bucket, gcs_object))
+        project_id=project_id,
+        filter="source.storage_source.bucket=\"%s\" AND source"
+        ".storage_source.object=\"%s\"" % (gcs_bucket, gcs_object))
     for build in build_lists:
-      _LOGGER.info("Check google cloud build log at %s" % build.log_url)
+      _LOGGER.info("Check Google Cloud Build log at %s" % build.log_url)
 
     # block until build finish, if build fails exception will be raised and
     # stops the job submission.
     operation.result()
 
     _LOGGER.info(
-        "Python SDK container pre-build finished in %.2f seconds" % (
-            time.time() - now))
+        "Python SDK container pre-build finished in %.2f seconds" %
+        (time.time() - now))
     _LOGGER.info(
         "Python SDK container built and pushed as %s." % container_image_name)
 
