fb6e950b9a471d17575204ea483163c86082dc58
Tomasz Szerszeń <tomasz.szerszen@polidea.com>
2020-12-08 23:17:33 +0100

    rm run.sh

--- sdks/go/test/load/group_by_key/run.sh
+++
@@ -1,2 +0,0 @@
-
-go run group_by_key.go --runner=flink --endpoint=localhost:8099 --parallelism 5 --iterations 4 --input_options='{"num_records": 20000000, "key_size": 10, "value_size": 90, "num_hot_keys": 200, "hot_key_fraction": 1}'
