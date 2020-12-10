b05e40c066410681d145383189df376750c64259
yoshiki.obata <yoshiki.obata@gmail.com>
2020-12-10 22:12:06 +0900

    [BEAM-11461] update tox and simplify tox.ini

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -2092,7 +2092,7 @@ class BeamModulePlugin implements Plugin<Project> {
           project.exec { commandLine virtualenvCmd }
           project.exec {
             executable 'sh'
-            args '-c', ". ${project.ext.envdir}/bin/activate && pip install --retries 10 --upgrade tox==3.11.1 -r ${project.rootDir}/sdks/python/build-requirements.txt"
+            args '-c', ". ${project.ext.envdir}/bin/activate && pip install --retries 10 --upgrade tox==3.20.1 -r ${project.rootDir}/sdks/python/build-requirements.txt"
           }
         }
         // Gradle will delete outputs whenever it thinks they are stale. Putting a
