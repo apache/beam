18fd95e3c569c33d9cf402dd04a13b80a0df8cb9
Daniel Collins <dpcollins@google.com>
2020-12-14 14:40:17 -0500

    fix: linters

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/TopicBacklogReaderImpl.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/TopicBacklogReaderImpl.java
@@ -32,7 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 final class TopicBacklogReaderImpl implements TopicBacklogReader {
-  private static final Logger logger = LoggerFactory.getLogger(TopicBacklogReaderImpl.class);
+  private static final Logger LOG = LoggerFactory.getLogger(TopicBacklogReaderImpl.class);
   private final TopicStatsClient client;
   private final TopicPath topicPath;
   private final Partition partition;
@@ -63,7 +63,7 @@ final class TopicBacklogReaderImpl implements TopicBacklogReader {
     try {
       client.close();
     } catch (Exception e) {
-      logger.warn("Failed to close topic stats client.", e);
+      LOG.warn("Failed to close topic stats client.", e);
     }
   }
 }
