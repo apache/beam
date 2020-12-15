98fb3b4a05634652e176da1d3a3edd022fe2ae49
Sonam Ramchand <sonam.ramchand@venturedive.com>
2020-12-15 23:07:21 +0500

    Modified implementation of array_Agg

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/udaf/ArrayAgg.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/udaf/ArrayAgg.java
@@ -1,37 +1,40 @@
 package org.apache.beam.sdk.extensions.sql.impl.udaf;
 
 import org.apache.beam.sdk.transforms.Combine;
+import java.lang.Object;
 
 public class ArrayAgg {
 
     public static class ArrayAggArray extends Combine.CombineFn<Object, Object[], Object[]> {
 
+
         @Override
         public Object[] createAccumulator() {
-            return new Object[0];
+            return new Object[10]; //size is kept constant for now, will later make it dynamic
         }
 
         @Override
         public Object[] addInput(Object[] mutableAccumulator, Object input) {
-
-            if(input != null){
-                if(mutableAccumulator != null) {
-                    mutableAccumulator[mutableAccumulator.length + 1] = input;
-                }
-            }
-            return  mutableAccumulator;
+            mutableAccumulator[0] = input;
+            return mutableAccumulator;
         }
 
         @Override
         public Object[] mergeAccumulators(Iterable<Object[]> accumulators) {
             Object[] mergeObject= new Object[]{};
-
+            for (Object[] accum: accumulators ){
+                if (accum != null){
+                    mergeObject = accum;
+                }else{
+                    mergeObject = accum;
+                }
+            }
+            return  mergeObject;
         }
 
         @Override
         public Object[] extractOutput(Object[] accumulator) {
-            return accumulator;
+            return new Object[0];
         }
     }
-
-    }
+}
