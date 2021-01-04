6dd9caa44650e36eb8aa48a3de990f2fa45becfd
Anant Damle <anant.damle@gmail.com>
2021-01-05 01:11:28 +0800

    Remove unnecessary class  and replace with

--- sdks/java/io/parquet/src/main/java/org/apache/beam/sdk/io/parquet/ParquetSchemaIOProvider.java
+++ sdks/java/io/parquet/src/main/java/org/apache/beam/sdk/io/parquet/ParquetSchemaIOProvider.java
@@ -19,11 +19,11 @@ package org.apache.beam.sdk.io.parquet;
 
 import com.google.auto.service.AutoService;
 import java.io.Serializable;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.annotations.Internal;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.io.SchemaIO;
 import org.apache.beam.sdk.schemas.io.SchemaIOProvider;
+import org.apache.beam.sdk.schemas.transforms.Convert;
 import org.apache.beam.sdk.schemas.utils.AvroUtils;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PBegin;
@@ -95,14 +95,11 @@ public class ParquetSchemaIOProvider implements SchemaIOProvider {
       return new PTransform<PBegin, PCollection<Row>>() {
         @Override
         public PCollection<Row> expand(PBegin begin) {
-          PTransform<PCollection<GenericRecord>, PCollection<Row>> readConverter =
-              GenericRecordReadConverter.builder().beamSchema(dataSchema).build();
-
           return begin
               .apply(
                   "ParquetIORead",
                   ParquetIO.read(AvroUtils.toAvroSchema(dataSchema)).from(location))
-              .apply("GenericRecordToRow", readConverter);
+              .apply("GenericRecordToRow", Convert.toRows());
         }
       };
     }
