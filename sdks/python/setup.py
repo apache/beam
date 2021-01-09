6be5aae2a62c8ab66045f91150cbdebce2d11eeb
Ning Kang <ningk@google.com>
2021-01-08 17:23:01 -0800

    [BEAM-11588] Fix the dependency
    1. Newer ipykernel now depends on jupyter-client and their transitive
       dependencies on jedi have conflicts, thus explicitly selecting a
       jupyter-client version without the conflict.
    2. Removed jupyter-client version limit from INTERACTIVE_BEAM_TEST deps
       because INTERACTIVE_BEAM now transitively installs jupyter-client.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -206,6 +206,9 @@ INTERACTIVE_BEAM = [
     'facets-overview>=1.0.0,<2',
     'ipython>=5.8.0,<8',
     'ipykernel>=5.2.0,<6',
+    # TODO(ningk): Remove the upper limit once
+    # https://github.com/jupyter/jupyter_client/issues/597 is fixed.
+    'jupyter-client>=6.1.2,<6.1.8',
     'timeloop>=1.0.2,<2',
 ]
 
@@ -213,7 +216,6 @@ INTERACTIVE_BEAM_TEST = [
     # notebok utils
     'nbformat>=5.0.5,<6',
     'nbconvert>=5.6.1,<6',
-    'jupyter-client>=6.1.2,<7',
     # headless chrome based integration tests
     'selenium>=3.141.0,<4',
     'needle>=0.5.0,<1',
