171ba9d31f6cbd1893a2cd41a2252d5e8b28fcec
Heejong Lee <heejong@gmail.com>
2021-01-20 19:26:06 -0800

    add API_REQUEST_LATENCIES, match by MetricName

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/StreamingWriteFn.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/StreamingWriteFn.java
@@ -25,8 +25,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.apache.beam.runners.core.metrics.MonitoringInfoConstants;
-import org.apache.beam.sdk.extensions.gcp.util.LatencyRecordingHttpRequestInitializer;
+import org.apache.beam.runners.core.metrics.MonitoringInfoMetricName;
 import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.MetricName;
 import org.apache.beam.sdk.metrics.MetricsContainer;
 import org.apache.beam.sdk.metrics.MetricsEnvironment;
 import org.apache.beam.sdk.metrics.MetricsLogger;
@@ -42,6 +43,7 @@ import org.apache.beam.sdk.values.ShardedKey;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.ValueInSingleWindow;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableSet;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Lists;
 import org.joda.time.Instant;
@@ -63,6 +65,7 @@ class StreamingWriteFn<ErrorT, ElementT>
   private final boolean ignoreInsertIds;
   private final SerializableFunction<ElementT, TableRow> toTableRow;
   private final SerializableFunction<ElementT, TableRow> toFailsafeTableRow;
+  private final Set<MetricName> metricFilter;
 
   /** JsonTableRows to accumulate BigQuery rows in order to batch writes. */
   private transient Map<String, List<FailsafeValueInSingleWindow<TableRow, TableRow>>> tableRows;
@@ -72,15 +75,6 @@ class StreamingWriteFn<ErrorT, ElementT>
 
   /** Tracks bytes written, exposed as "ByteCount" Counter. */
   private Counter byteCounter = SinkMetrics.bytesWritten();
-
-  private Set<KV<String, String>> metricFilter =
-      ImmutableSet.of(
-          KV.of(
-              LatencyRecordingHttpRequestInitializer.HISTOGRAM_URN.split(":", 2)[0],
-              LatencyRecordingHttpRequestInitializer.HISTOGRAM_URN.split(":", 2)[1]),
-          KV.of(
-              MonitoringInfoConstants.Urns.API_REQUEST_COUNT.split(":", 2)[0],
-              MonitoringInfoConstants.Urns.API_REQUEST_COUNT.split(":", 2)[1]));
 
   StreamingWriteFn(
       BigQueryServices bqServices,
@@ -101,6 +95,30 @@ class StreamingWriteFn<ErrorT, ElementT>
     this.ignoreInsertIds = ignoreInsertIds;
     this.toTableRow = toTableRow;
     this.toFailsafeTableRow = toFailsafeTableRow;
+    ImmutableSet.Builder<MetricName> setBuilder = ImmutableSet.builder();
+    setBuilder.add(
+        MonitoringInfoMetricName.named(
+            MonitoringInfoConstants.Urns.API_REQUEST_LATENCIES,
+            BigQueryServicesImpl.API_METRIC_LABEL));
+    for (String status : BigQueryServicesImpl.DatasetServiceImpl.CANONICAL_STATUS_MAP.values()) {
+      setBuilder.add(
+          MonitoringInfoMetricName.named(
+              MonitoringInfoConstants.Urns.API_REQUEST_COUNT,
+              ImmutableMap.<String, String>builder()
+                  .putAll(BigQueryServicesImpl.API_METRIC_LABEL)
+                  .put(MonitoringInfoConstants.Labels.STATUS, status)
+                  .build()));
+    }
+    setBuilder.add(
+        MonitoringInfoMetricName.named(
+            MonitoringInfoConstants.Urns.API_REQUEST_COUNT,
+            ImmutableMap.<String, String>builder()
+                .putAll(BigQueryServicesImpl.API_METRIC_LABEL)
+                .put(
+                    MonitoringInfoConstants.Labels.STATUS,
+                    BigQueryServicesImpl.DatasetServiceImpl.CANONICAL_STATUS_UNKNOWN)
+                .build()));
+    this.metricFilter = setBuilder.build();
   }
 
   /** Prepares a target BigQuery table. */
