a074aaed56b9011db86fda2a270dd8e20abbdbea
Sam R <rohde.samuel@gmail.com>
2021-02-17 15:14:27 -0800

    Add comments and a README

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# X11 instructions from https://stackoverflow.com/questions/16296753/can-you-run-gui-applications-in-a-docker-container
+
 # This creates a new virtual volume for the container. This is useful to have
 # changes persist across container runs.
 VOLUMNE_NAME=beam_docker
@@ -15,6 +17,12 @@ USER=user
 #  -v links the container's /home/$USER/beam directory to the volume
 #  dev:latest the container created from the build.sh file
 #  /bin/bash starts a shell in the container
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 docker run -it --rm \
+  -p 8888:8888 \
+  -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
   -v $VOLUMNE_NAME:/home/$USER/beam \
   dev:latest /bin/bash
