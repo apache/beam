8030f3fc18920d246aaebdd803847f5379bfbca8
yoshiki.obata <yoshiki.obata@gmail.com>
2021-02-18 22:46:41 +0900

    [BEAM-7372] fix imports

--- sdks/python/apache_beam/dataframe/frame_base.py
+++ sdks/python/apache_beam/dataframe/frame_base.py
@@ -17,7 +17,8 @@
 from __future__ import absolute_import
 
 import functools
-from inspect import getfullargspec, unwrap
+from inspect import getfullargspec
+from inspect import unwrap
 from typing import Any
 from typing import Callable
 from typing import Dict
