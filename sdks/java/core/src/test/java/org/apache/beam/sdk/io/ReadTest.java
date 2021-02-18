db6dd9bf0cfd27e26a8f29446b43ba33f47e5b47
Jan Lukavsky <je.ik@seznam.cz>
2021-02-18 14:37:10 +0100

    [BEAM-11833] Fix reported watermark after restriction split in UnboundedSourceAsSDFRestrictionTracker

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/ReadTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/ReadTest.java
@@ -17,18 +17,30 @@
  */
 package org.apache.beam.sdk.io;
 
+import static org.apache.beam.sdk.testing.SerializableMatchers.greaterThanOrEqualTo;
 import static org.apache.beam.sdk.transforms.display.DisplayDataMatchers.hasDisplayItem;
 import static org.apache.beam.sdk.transforms.display.DisplayDataMatchers.includesDisplayDataFor;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.CoderException;
+import org.apache.beam.sdk.coders.CustomCoder;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.coders.VarLongCoder;
 import org.apache.beam.sdk.io.CountingSource.CounterMark;
@@ -38,11 +50,17 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.testing.NeedsRunner;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.display.DisplayData;
 import org.apache.beam.sdk.transforms.reflect.DoFnInvokers;
+import org.apache.beam.sdk.transforms.splittabledofn.WatermarkEstimators;
+import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.MoreObjects;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.joda.time.Duration;
@@ -61,6 +79,10 @@ import org.junit.runners.JUnit4;
   "nullness" // TODO(https://issues.apache.org/jira/browse/BEAM-10402)
 })
 public class ReadTest implements Serializable {
+
+  private static final Map<String, List<Instant>> STATIC_INSTANT_LIST_MAP =
+      new ConcurrentHashMap<>();
+
   @Rule public transient ExpectedException thrown = ExpectedException.none();
   @Rule public final transient TestPipeline pipeline = TestPipeline.create();
 
@@ -127,7 +149,7 @@ public class ReadTest implements Serializable {
 
   @Test
   @Category(NeedsRunner.class)
-  public void testUnboundedSdfWrapperCacheStartedReaders() throws Exception {
+  public void testUnboundedSdfWrapperCacheStartedReaders() {
     long numElements = 1000L;
     PCollection<Long> input =
         pipeline.apply(Read.from(new ExpectCacheUnboundedSource(numElements)));
@@ -140,6 +162,67 @@ public class ReadTest implements Serializable {
     pipeline
         .runWithAdditionalOptionArgs(ImmutableList.of("--targetParallelism=1"))
         .waitUntilFinish();
+  }
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testWatermarkAdvanceOnClaimFail() {
+    // NOTE: this test is supposed to run only against DirectRunner
+    // as for other runners it might not be working the interception of watermark
+    // through the STATIC_INSTANT_LIST_MAP
+    int numElements = 1000;
+    final String uuid = UUID.randomUUID().toString();
+    List<Instant> interceptedWatermark =
+        STATIC_INSTANT_LIST_MAP.computeIfAbsent(uuid, tmp -> new ArrayList<>());
+    PCollection<Long> counted =
+        pipeline
+            .apply(
+                newUnboundedReadInterceptingWatermark(
+                    numElements,
+                    (Serializable & Consumer<Instant>)
+                        (instant -> STATIC_INSTANT_LIST_MAP.get(uuid).add(instant))))
+            .apply(
+                Window.<Long>into(new GlobalWindows())
+                    .discardingFiredPanes()
+                    .triggering(AfterWatermark.pastEndOfWindow()))
+            .apply(Count.globally());
+    PAssert.that(counted).containsInAnyOrder((long) numElements);
+    pipeline.run().waitUntilFinish();
+    // verify that the observed watermark gradually moves
+    assertThat(interceptedWatermark.size(), greaterThanOrEqualTo(numElements));
+    Instant watermark = interceptedWatermark.get(0);
+    for (int i = 1; i < interceptedWatermark.size(); i++) {
+      assertThat(
+          "Watermarks should be non-decreasing sequence, got " + interceptedWatermark,
+          !watermark.isAfter(interceptedWatermark.get(i)));
+      watermark = interceptedWatermark.get(i);
+    }
+  }
+
+  private <T extends Serializable & Consumer<Instant>>
+      Read.Unbounded<Long> newUnboundedReadInterceptingWatermark(
+          long numElements, T interceptedWatermarkReceiver) {
+
+    UnboundedLongSource source = new UnboundedLongSource(numElements);
+    return new Read.Unbounded<Long>(null, source) {
+      @Override
+      @SuppressWarnings("unchecked")
+      Read.UnboundedSourceAsSDFWrapperFn<Long, CheckpointMark> createUnboundedSdfWrapper() {
+        return new Read.UnboundedSourceAsSDFWrapperFn<Long, CheckpointMark>(
+            (Coder) source.getCheckpointMarkCoder()) {
+          @Override
+          public WatermarkEstimators.Manual newWatermarkEstimator(Instant watermarkEstimatorState) {
+            return new WatermarkEstimators.Manual(watermarkEstimatorState) {
+              @Override
+              public void setWatermark(Instant watermark) {
+                super.setWatermark(watermark);
+                interceptedWatermarkReceiver.accept(watermark);
+              }
+            };
+          }
+        };
+      }
+    };
   }
 
   private abstract static class CustomBoundedSource extends BoundedSource<String> {
@@ -312,4 +395,114 @@ public class ReadTest implements Serializable {
   private static class SerializableUnboundedSource extends CustomUnboundedSource {}
 
   private static class NotSerializableClass {}
+
+  private static class OffsetCheckpointMark implements CheckpointMark {
+
+    private static final Coder<OffsetCheckpointMark> CODER =
+        new CustomCoder<OffsetCheckpointMark>() {
+          private final VarLongCoder longCoder = VarLongCoder.of();
+
+          @Override
+          public void encode(OffsetCheckpointMark value, OutputStream outStream)
+              throws CoderException, IOException {
+            longCoder.encode(value.offset, outStream);
+          }
+
+          @Override
+          public OffsetCheckpointMark decode(InputStream inStream)
+              throws CoderException, IOException {
+            return new OffsetCheckpointMark(longCoder.decode(inStream));
+          }
+        };
+
+    private final long offset;
+
+    OffsetCheckpointMark(Long offset) {
+      this.offset = MoreObjects.firstNonNull(offset, -1L);
+    }
+
+    @Override
+    public void finalizeCheckpoint() {}
+  }
+
+  private class UnboundedLongSource extends UnboundedSource<Long, OffsetCheckpointMark> {
+
+    private final long numElements;
+
+    public UnboundedLongSource(long numElements) {
+      this.numElements = numElements;
+    }
+
+    @Override
+    public List<? extends UnboundedSource<Long, OffsetCheckpointMark>> split(
+        int desiredNumSplits, PipelineOptions options) {
+
+      return Collections.singletonList(this);
+    }
+
+    @Override
+    public UnboundedReader<Long> createReader(
+        PipelineOptions options, @Nullable OffsetCheckpointMark checkpointMark) {
+
+      return new UnboundedLongSourceReader(
+          Optional.ofNullable(checkpointMark).map(m -> m.offset).orElse(-1L));
+    }
+
+    @Override
+    public Coder<Long> getOutputCoder() {
+      return VarLongCoder.of();
+    }
+
+    @Override
+    public Coder<OffsetCheckpointMark> getCheckpointMarkCoder() {
+      return OffsetCheckpointMark.CODER;
+    }
+
+    private class UnboundedLongSourceReader extends UnboundedReader<Long> {
+      private final Instant now = Instant.now();
+      private long current;
+
+      UnboundedLongSourceReader(long current) {
+        this.current = current;
+      }
+
+      @Override
+      public Long getCurrent() throws NoSuchElementException {
+        return current;
+      }
+
+      @Override
+      public Instant getCurrentTimestamp() throws NoSuchElementException {
+        return current < 0 ? now : now.plus(current);
+      }
+
+      @Override
+      public void close() throws IOException {}
+
+      @Override
+      public boolean start() throws IOException {
+        return advance();
+      }
+
+      @Override
+      public boolean advance() throws IOException {
+        return ++current < numElements;
+      }
+
+      @Override
+      public Instant getWatermark() {
+        return current < numElements ? getCurrentTimestamp() : BoundedWindow.TIMESTAMP_MAX_VALUE;
+      }
+
+      @Override
+      public CheckpointMark getCheckpointMark() {
+        return new OffsetCheckpointMark(current);
+      }
+
+      @Override
+      public UnboundedSource<Long, ?> getCurrentSource() {
+        return UnboundedLongSource.this;
+      }
+    }
+  }
 }
