1a4cb59d65e4b00dbbfc7dca8f9ab05586d5992e
Mackenzie Clark <mackenzieclark@google.com>
2021-03-16 21:08:16 +0000

    spotless apply

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/healthcare/FhirIOLROIT.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/healthcare/FhirIOLROIT.java
@@ -28,7 +28,6 @@ import org.apache.beam.sdk.values.PCollection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
