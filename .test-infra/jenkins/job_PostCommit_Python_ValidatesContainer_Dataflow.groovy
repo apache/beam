0716889e21e76743b9e5c7cbc710676c17f965b2
tvalentyn <tvalentyn@users.noreply.github.com>
2021-03-18 13:52:26 -0700

    Revert "Increase the timeout  for ValidatesContainer suite" (#14219)
    This reverts commit 33d59b34bcf789f196e23d259ca52d2fa057d3a8.

--- .test-infra/jenkins/job_PostCommit_Python_ValidatesContainer_Dataflow.groovy
+++ .test-infra/jenkins/job_PostCommit_Python_ValidatesContainer_Dataflow.groovy
@@ -28,7 +28,7 @@ PostcommitJobBuilder.postCommitJob('beam_PostCommit_Py_ValCont',
       description('Runs Python ValidatesContainer suite on the Dataflow runner.')
 
       // Set common parameters.
-      commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 180)
+      commonJobProperties.setTopLevelMainJobProperties(delegate)
 
       publishers {
         archiveJunit('**/nosetests*.xml')
