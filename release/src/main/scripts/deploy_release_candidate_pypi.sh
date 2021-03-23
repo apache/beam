40be7a3dae039787dac64d5329ad26c44f692d98
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-03-23 17:07:53 -0600

    [BEAM-9185] Add args to deploy_release_candidate.sh

--- release/src/main/scripts/deploy_release_candidate_pypi.sh
+++ release/src/main/scripts/deploy_release_candidate_pypi.sh
@@ -22,6 +22,83 @@
 
 set -e
 
+function usage() {
+  echo 'Usage: deploy_release_candidate.sh --release <version> --rc <rc> --commit <commit> --user <user> [--deploy]'
+}
+
+RELEASE=
+RC=
+COMMIT=
+USER_GITHUB_ID=
+DEPLOY=no
+PYTHON_ARTIFACTS_DIR=python
+BEAM_ROOT_DIR=beam
+GIT_REPO_BASE_URL=benWize/beam
+
+while [[ $# -gt 0 ]] ; do
+  arg="$1"
+
+  case $arg in
+      --release)
+      shift
+      RELEASE=$1
+      shift
+      ;;
+
+      --rc)
+      shift
+      RC=$1
+      shift
+      ;;
+
+      --commit)
+      shift
+      COMMIT=$1
+      shift
+      ;;
+
+      --user)
+      shift
+      USER_GITHUB_ID=$1
+      shift
+      ;;
+
+      --deploy)
+      DEPLOY=yes
+      shift
+      ;;
+
+      *)
+      usage
+      exit 1
+      ;;
+   esac
+done
+
+if [[ -z "$RELEASE" ]] ; then
+  echo 'No release version supplied.'
+  usage
+  exit 1
+fi
+
+if [[ -z "$RC" ]] ; then
+  echo 'No RC number supplied'
+  usage
+  exit 1
+fi
+
+if [[ -z "$COMMIT" ]] ; then
+  echo 'No commit hash supplied.'
+  usage
+  exit 1
+fi
+
+if [[ -z "$USER_GITHUB_ID" ]] ; then
+  echo 'No github user supplied.'
+  usage
+  exit 1
+fi
+
 function clean_up(){
   echo "Do you want to clean local clone repo ${LOCAL_CLONE_DIR}? [y|N]"
   read confirmation
@@ -32,35 +109,14 @@ function clean_up(){
   fi
 }
 
-GIT_REPO_BASE_URL=apache/beam
-PYTHON_ARTIFACTS_DIR=python
-BEAM_ROOT_DIR=beam
-
-echo "================Setting Up Environment Variables==========="
-if [[ -z "${RELEASE}" ]]
-then
-  echo "Which release version are you working on: "
-  read RELEASE
-fi
-RELEASE_BRANCH="release-${RELEASE}"
-if [[ -z "${RC_NUM}" ]]
-then
-  echo "Which release candidate number(e.g. 1) are you going to create: "
-  read RC_NUM
-fi
-if [[ -z "${USER_GITHUB_ID}" ]]
-then
-  echo "Please enter your github username(ID): "
-  read USER_GITHUB_ID
-fi
-echo "${RELEASE_BRANCH}"
-RELEASE_COMMIT=$(git rev-parse --verify "${RELEASE_BRANCH}")
-LOCAL_CLONE_DIR="beam_release_${RELEASE}rc${RC_NUM}"
+RC_TAG="v${RELEASE}-RC${RC}"
+LOCAL_CLONE_DIR="beam_release_${RC_TAG}"
+SCRIPT_DIR=$(dirname $0)
 
 echo "================Checking Environment Variables=============="
 echo "working on release version: ${RELEASE}"
-echo "working on release branch: ${RELEASE_BRANCH}"
-echo "will create release candidate: RC${RC_NUM}"
+echo "working on release branch: ${RC_TAG}"
+echo "will create release candidate: RC${RC}"
 echo "Please review all environment variables and confirm: [y|N]"
 read confirmation
 if [[ $confirmation != "y" ]]; then
@@ -79,18 +135,18 @@ LOCAL_CLONE_DIR_ROOT=$(pwd)/${LOCAL_CLONE_DIR}
 
 echo "================Download python artifacts======================"
 cd -
-python ./download_github_actions_artifacts.py \
+python "${SCRIPT_DIR}/download_github_actions_artifacts.py" \
   --github-user "${USER_GITHUB_ID}" \
   --repo-url "${GIT_REPO_BASE_URL}" \
-  --release-branch "${RELEASE_BRANCH}" \
-  --release-commit "${RELEASE_COMMIT}" \
+  --release-branch "${RC_TAG}" \
+  --release-commit "${COMMIT}" \
   --artifacts_dir "${LOCAL_CLONE_DIR_ROOT}" \
   --is_rc_version
 
 cd ${LOCAL_CLONE_DIR_ROOT}
 
-echo "------Checking Hash Value for apache-beam-${RELEASE}rc${RC_NUM}.zip-----"
-sha512sum -c "apache-beam-${RELEASE}rc${RC_NUM}.zip.sha512"
+echo "------Checking Hash Value for apache-beam-${RELEASE}rc${RC}.zip-----"
+sha512sum -c "apache-beam-${RELEASE}rc${RC}.zip.sha512"
 
 for artifact in *.whl; do
   echo "----------Checking Hash Value for ${artifact} wheel-----------"
@@ -115,5 +171,11 @@ if [[ $confirmation != "y" ]]; then
   clean_up
   exit
 fi
-twine upload *
+
+if [[ "$DEPLOY" == yes ]] ; then
+  twine upload *
+else
+  echo "Not deploying to pypi with tag $RC."
+fi
+
 clean_up
