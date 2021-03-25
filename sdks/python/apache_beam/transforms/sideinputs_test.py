2d2514681bb10bf94f23b0d9fb428ebaa3e8f0fa
Robert Bradshaw <robertwb@gmail.com>
2021-03-25 09:03:18 -0700

    lint

--- sdks/python/apache_beam/transforms/sideinputs_test.py
+++ sdks/python/apache_beam/transforms/sideinputs_test.py
@@ -220,7 +220,7 @@ class SideInputsTest(unittest.TestCase):
     pipeline = self.create_pipeline()
     pcol = pipeline | 'start' >> beam.Create(['A', 'B'])
     side = pipeline | 'side' >> beam.Create(expected_side)
-    result = pcol | 'check' >> beam.Map(
+    _ = pcol | 'check' >> beam.Map(
         check_reiteration, beam.pvalue.AsIter(side))
     pipeline.run()
 
