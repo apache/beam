c5ef612b863bd0418b470b98a92bf051ea9876c9
Kyle Weaver <kcweaver@google.com>
2021-03-25 10:52:50 -0700

    fix test failures

--- sdks/java/extensions/sql/zetasql/src/test/java/org/apache/beam/sdk/extensions/sql/zetasql/ZetaSqlNumberTypesTest.java
+++ sdks/java/extensions/sql/zetasql/src/test/java/org/apache/beam/sdk/extensions/sql/zetasql/ZetaSqlNumberTypesTest.java
@@ -18,6 +18,7 @@
 package org.apache.beam.sdk.extensions.sql.zetasql;
 
 import com.google.zetasql.Value;
+import org.apache.beam.sdk.extensions.sql.impl.SqlConversionException;
 import org.apache.beam.sdk.extensions.sql.impl.rel.BeamRelNode;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
@@ -44,7 +45,7 @@ public class ZetaSqlNumberTypesTest extends ZetaSqlTestBase {
     double val = 1.7976931348623157e+308;
     String sql = "SELECT CAST(@p0 AS NUMERIC) AS ColA";
 
-    thrown.expect(UnsupportedOperationException.class);
+    thrown.expect(SqlConversionException.class);
     thrown.expectMessage("Casting TYPE_DOUBLE as TYPE_NUMERIC would cause overflow of literal");
 
     ZetaSQLQueryPlanner zetaSQLQueryPlanner = new ZetaSQLQueryPlanner(config);
@@ -58,7 +59,7 @@ public class ZetaSqlNumberTypesTest extends ZetaSqlTestBase {
     double val = -1.7976931348623157e+308;
     String sql = "SELECT CAST(@p0 AS NUMERIC) AS ColA";
 
-    thrown.expect(UnsupportedOperationException.class);
+    thrown.expect(SqlConversionException.class);
     thrown.expectMessage("Casting TYPE_DOUBLE as TYPE_NUMERIC would cause underflow of literal");
 
     ZetaSQLQueryPlanner zetaSQLQueryPlanner = new ZetaSQLQueryPlanner(config);
@@ -72,7 +73,7 @@ public class ZetaSqlNumberTypesTest extends ZetaSqlTestBase {
     double val = 2.2250738585072014e-308;
     String sql = "SELECT CAST(@p0 AS NUMERIC) AS ColA";
 
-    thrown.expect(UnsupportedOperationException.class);
+    thrown.expect(SqlConversionException.class);
     thrown.expectMessage("Cannot cast TYPE_DOUBLE as TYPE_NUMERIC: scale 1022 exceeds 9");
 
     ZetaSQLQueryPlanner zetaSQLQueryPlanner = new ZetaSQLQueryPlanner(config);
