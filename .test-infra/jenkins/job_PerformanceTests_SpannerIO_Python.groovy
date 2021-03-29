7a0d3f937fc19d751843a9ffff0a0b04c3facf7d
Shoaib Zafar <shoaib.zafar@venturedive.com>
2021-03-29 16:00:17 +0500

    adds 2gb spanner io perf test job

--- .test-infra/jenkins/job_PerformanceTests_SpannerIO_Python.groovy
+++ .test-infra/jenkins/job_PerformanceTests_SpannerIO_Python.groovy
@@ -23,7 +23,7 @@ import InfluxDBCredentialsHelper
 
 def now = new Date().format("MMddHHmmss", TimeZone.getTimeZone('UTC'))
 
-def spannerio_read_test = [
+def spannerio_read_test_10gb = [
   title          : 'SpannerIO Read Performance Test Python 10 GB',
   test           : 'apache_beam.io.gcp.experimental.spannerio_read_perf_test',
   runner         : CommonTestProperties.Runner.DATAFLOW,
@@ -49,7 +49,7 @@ def spannerio_read_test = [
   ]
 ]
 
-def spannerio_write_test = [
+def spannerio_write_test_10gb = [
   title          : 'SpannerIO Write Performance Test Python Batch 10 GB',
   test           : 'apache_beam.io.gcp.experimental.spannerio_write_perf_test',
   runner         : CommonTestProperties.Runner.DATAFLOW,
@@ -75,34 +75,119 @@ def spannerio_write_test = [
   ]
 ]
 
-def executeJob = { scope, testConfig ->
+def spannerio_read_test_2gb = [
+  title          : 'SpannerIO Read Performance Test Python 2 GB',
+  test           : 'apache_beam.io.gcp.experimental.spannerio_read_perf_test',
+  runner         : CommonTestProperties.Runner.DATAFLOW,
+  pipelineOptions: [
+    job_name             : 'performance-tests-spanner-read-python-2gb' + now,
+    project              : 'apache-beam-testing',
+    region               : 'us-central1',
+    temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
+    spanner_instance     : 'beam-test',
+    spanner_database     : 'pyspanner_read_2gb',
+    publish_to_big_query : true,
+    metrics_dataset      : 'beam_performance',
+    metrics_table        : 'pyspanner_read_2GB_results',
+    influx_measurement   : 'python_spannerio_read_2GB_results',
+    influx_db_name       : InfluxDBCredentialsHelper.InfluxDBDatabaseName,
+    influx_hostname      : InfluxDBCredentialsHelper.InfluxDBHostUrl,
+    input_options        : '\'{' +
+    '"num_records": 10485760,' +
+    '"key_size": 1,' +
+    '"value_size": 1024}\'',
+    num_workers          : 5,
+    autoscaling_algorithm: 'NONE',  // Disable autoscale the worker pool.
+  ]
+]
+
+def spannerio_write_test_2gb = [
+  title          : 'SpannerIO Write Performance Test Python Batch 2 GB',
+  test           : 'apache_beam.io.gcp.experimental.spannerio_write_perf_test',
+  runner         : CommonTestProperties.Runner.DATAFLOW,
+  pipelineOptions: [
+    job_name             : 'performance-tests-spannerio-write-python-batch-2gb' + now,
+    project              : 'apache-beam-testing',
+    region               : 'us-central1',
+    temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
+    spanner_instance     : 'beam-test',
+    spanner_database     : 'pyspanner_write_2gb',
+    publish_to_big_query : true,
+    metrics_dataset      : 'beam_performance',
+    metrics_table        : 'pyspanner_write_2GB_results',
+    influx_measurement   : 'python_spanner_write_2GB_results',
+    influx_db_name       : InfluxDBCredentialsHelper.InfluxDBDatabaseName,
+    influx_hostname      : InfluxDBCredentialsHelper.InfluxDBHostUrl,
+    input_options        : '\'{' +
+    '"num_records": 10485760,' +
+    '"key_size": 1,' +
+    '"value_size": 1024}\'',
+    num_workers          : 5,
+    autoscaling_algorithm: 'NONE',  // Disable autoscale the worker pool.
+  ]
+]
+
+def executeJob10Gb = { scope, testConfig ->
+  commonJobProperties.setTopLevelMainJobProperties(scope, 'master', 360)
+
+  loadTestsBuilder.loadTest(scope, testConfig.title, testConfig.runner, CommonTestProperties.SDK.PYTHON, testConfig.pipelineOptions, testConfig.test)
+}
+
+def executeJob2Gb = { scope, testConfig ->
   commonJobProperties.setTopLevelMainJobProperties(scope, 'master', 240)
 
   loadTestsBuilder.loadTest(scope, testConfig.title, testConfig.runner, CommonTestProperties.SDK.PYTHON, testConfig.pipelineOptions, testConfig.test)
 }
 
 PhraseTriggeringPostCommitBuilder.postCommitJob(
-    'beam_PerformanceTests_SpannerIO_Read_Python',
-    'Run SpannerIO Read Performance Test Python',
-    'SpannerIO Read Performance Test Python',
+    'beam_PerformanceTests_SpannerIO_Read_10GB_Python',
+    'Run SpannerIO Read 10GB Performance Test Python',
+    'SpannerIO Read 10GB Performance Test Python',
     this
     ) {
-      executeJob(delegate, spannerio_read_test)
+      executeJob10Gb(delegate, spannerio_read_test_10gb)
     }
 
-CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Read_Python', 'H 15 * * *', this) {
-  executeJob(delegate, spannerio_read_test)
+CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Read_10GB_Python', 'H 15 * * *', this) {
+  executeJob10Gb(delegate, spannerio_read_test_10gb)
 }
 
 PhraseTriggeringPostCommitBuilder.postCommitJob(
-    'beam_PerformanceTests_SpannerIO_Write_Python_Batch',
-    'Run SpannerIO Write Performance Test Python Batch',
-    'SpannerIO Write Performance Test Python Batch',
+    'beam_PerformanceTests_SpannerIO_Write_10GB_Python_Batch',
+    'Run SpannerIO Write 10GB Performance Test Python Batch',
+    'SpannerIO Write 10GB Performance Test Python Batch',
     this
     ) {
-      executeJob(delegate, spannerio_write_test)
+      executeJob10Gb(delegate, spannerio_write_test_10gb)
     }
 
-CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Write_Python_Batch', 'H 15 * * *', this) {
-  executeJob(delegate, spannerio_write_test)
+CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Write_10GB_Python_Batch', 'H 15 * * *', this) {
+  executeJob10Gb(delegate, spannerio_write_test_10gb)
+}
+
+
+PhraseTriggeringPostCommitBuilder.postCommitJob(
+    'beam_PerformanceTests_SpannerIO_Read_2GB_Python',
+    'Run SpannerIO Read 2GB Performance Test Python',
+    'SpannerIO Read 2GB Performance Test Python',
+    this
+    ) {
+      executeJob2Gb(delegate, spannerio_read_test_2gb)
+    }
+
+CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Read_2GB_Python', 'H 15 * * *', this) {
+  executeJob2Gb(delegate, spannerio_read_test_2gb)
+}
+
+PhraseTriggeringPostCommitBuilder.postCommitJob(
+    'beam_PerformanceTests_SpannerIO_Write_2GB_Python_Batch',
+    'Run SpannerIO Write 2GB Performance Test Python Batch',
+    'SpannerIO Write 2GB Performance Test Python Batch',
+    this
+    ) {
+      executeJob2Gb(delegate, spannerio_write_test_2gb)
+    }
+
+CronJobBuilder.cronJob('beam_PerformanceTests_SpannerIO_Write_2GB_Python_Batch', 'H 15 * * *', this) {
+  executeJob2Gb(delegate, spannerio_write_test_2gb)
 }
