d0901dd5408a7dadf67ef6651db0fd2c8e561339
Heejong Lee <heejong@gmail.com>
2021-03-29 11:54:06 -0700

    Update StorageApiWriteUnshardedRecords.java

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/StorageApiWriteUnshardedRecords.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/StorageApiWriteUnshardedRecords.java
@@ -234,7 +234,7 @@ public class StorageApiWriteUnshardedRecords<DestinationT, ElementT>
               return RetryType.RETRY_ALL_OPERATIONS;
             },
             response -> {
-              LOG.info("Append to stream " + streamName + " succeded.");
+              LOG.info("Append to stream " + streamName + " succeeded.");
             },
             new Context<>());
         // TODO: Do we have to wait on every append?
