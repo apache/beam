5320bb9a772269dc9fefd4629b575f0e578938d2
Brian Hulette <bhulette@google.com>
2021-04-06 14:00:10 -0700

    fix skip

--- sdks/python/apache_beam/dataframe/frames_test.py
+++ sdks/python/apache_beam/dataframe/frames_test.py
@@ -921,9 +921,9 @@ class DeferredFrameTest(unittest.TestCase):
         GROUPBY_DF)
 
 
+  @parameterized.expand([(True, ), (False, ), (None, )])
   @unittest.skipIf(PD_VERSION < (1, 2),
                    "pandas 1.1.0 produces different dtypes for these examples")
-  @parameterized.expand([(True, ), (False, ), (None, )])
   def test_dataframe_agg_numeric_only(self, numeric_only):
     # Note other aggregation functions can fail on this input with
     # numeric_only={False,None}. These are the only ones that actually work for
