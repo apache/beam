77eb968bc778c51f57031ade77adf1263cd8a7e9
yoshiki.obata <1285728+lazylynx@users.noreply.github.com>
2021-04-08 12:27:26 +0900

    [BEAM-7372] cleanup codes for py2 compatibility from apache_beam/examples/snippets/*.py and apache_beam/examples/*.py (#14444)
    * usage of future package
     * usage of past package
     * unnecessary builtin import
     * workarounds for py2

--- sdks/python/apache_beam/examples/snippets/transforms/aggregation/combineglobally.py
+++ sdks/python/apache_beam/examples/snippets/transforms/aggregation/combineglobally.py
@@ -18,10 +18,6 @@
 
 # pytype: skip-file
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 
 def combineglobally_function(test=None):
   # [START combineglobally_function]
--- sdks/python/apache_beam/examples/snippets/util.py
+++ sdks/python/apache_beam/examples/snippets/util.py
@@ -17,8 +17,6 @@
 
 # pytype: skip-file
 
-from __future__ import absolute_import
-
 import ast
 import shlex
 import subprocess as sp
