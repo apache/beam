6b592c21b992cfb77e7337750e40dcad9e52fc19
Valentyn Tymofieiev <valentyn@google.com>
2021-04-10 16:14:24 -0700

    Adds tentative resource hints to be recognized by Dataflow Runner.

--- sdks/python/apache_beam/transforms/resources.py
+++ sdks/python/apache_beam/transforms/resources.py
@@ -14,7 +14,16 @@
 # limitations under the License.
 #
 
-"""A module for defining resource requirements for execution of transforms."""
+"""A module for defining resource requirements for execution of transforms.
+
+Pipeline authors can use resource hints to provide additional information to
+runners about the desired aspects of the execution environment.
+
+Resource hints can be specified on a transform level for parts of the pipeline,
+or globally via --resource_hint pipeline option.
+
+See also: PTransforms.with_resource_hints().
+"""
 
 from typing import Any
 from typing import Callable
@@ -22,7 +31,7 @@ from typing import Dict
 
 from apache_beam.portability.common_urns import resource_hints
 
-__all__ = ['parse_resource_hints']
+__all__ = ['parse_resource_hints', 'get_merged_hint_value']
 
 
 def _parse_str(value):
@@ -44,13 +53,40 @@ def _parse_any(_):
   return b'1'
 
 
+def _parse_storage_size_str(value):  # type: (str) -> bytes
+  """Parses a human-friendly storage size string into a number of bytes.
+  """
+  if not isinstance(value, str):
+    value = str(value)
+  value = value.strip().replace(" ", "")
+  units = {
+      'PiB': 2**50,
+      'TiB': 2**40,
+      'GiB': 2**30,
+      'MiB': 2**20,
+      'KiB': 2**10,
+      'PB': 10**15,
+      'TB': 10**12,
+      'GB': 10**9,
+      'MB': 10**6,
+      'KB': 10**3,
+  }
+  multiplier = 1
+  for suffix in units:
+    if value.endswith(suffix):
+      multiplier = units[suffix]
+      value = value[:-len(suffix)]
+      break
+
+  return str(round(float(value) * multiplier)).encode('ascii')
+
+
 def _use_max(v1, v2):
   return str(max(int(v1), int(v2))).encode('ascii')
 
 
-def get_merged_hint_value(hint_urn, outer_value, inner_value):
-  # type: (str, bytes, bytes) -> bytes
-
+def get_merged_hint_value(
+    hint_urn, outer_value, inner_value):  # type: (str, bytes, bytes) -> bytes
   """Reconciles values of a hint defined on a composite and its subtransform."""
   if (outer_value == inner_value or
       hint_urn not in _HINTS_WITH_CUSTOM_MERGING_LOGIC):
@@ -61,12 +97,10 @@ def get_merged_hint_value(hint_urn, outer_value, inner_value):
 
 # Describes how to parse known resource hints, and which URNs to assign.
 _KNOWN_HINTS = dict(
-    accelerator_type=lambda value:
-    {resource_hints.ACCELERATOR_TYPE.urn: _parse_str(value)},
-    accelerator_count=lambda value:
-    {resource_hints.ACCELERATOR_COUNT.urn: _parse_int(value)},
-    accelerator_metadata=lambda value:
-    {resource_hints.ACCELERATOR_METADATA.urn: _parse_str(value)},
+    accelerator=lambda value:
+    {resource_hints.ACCELERATOR.urn: _parse_str(value)},
+    min_ram_per_vcpu=lambda value:
+    {resource_hints.MIN_RAM_PER_VCPU_BYTES.urn: _parse_storage_size_str(value)},
 )  # type: Dict[str, Callable[[Any], Dict[str, bytes]]]
 
 # Describes how resource hint values should be reconciled when the same hint
@@ -75,12 +109,11 @@ _KNOWN_HINTS = dict(
 # command-line specified hints) will override pipeline-defined hints and are not
 # subject to the merging logic.
 _HINTS_WITH_CUSTOM_MERGING_LOGIC = {
-    resource_hints.ACCELERATOR_COUNT.urn: _use_max
+    resource_hints.MIN_RAM_PER_VCPU_BYTES.urn: _use_max
 }  # type: Dict[str, Callable[[bytes, bytes], bytes]]
 
 
-def parse_resource_hints(hints):
-  # type: (Dict[Any, Any]) -> Dict[str, bytes]
+def parse_resource_hints(hints):  # type: (Dict[Any, Any]) -> Dict[str, bytes]
   parsed_hints = {}
   for hint, value in hints.items():
     try:
