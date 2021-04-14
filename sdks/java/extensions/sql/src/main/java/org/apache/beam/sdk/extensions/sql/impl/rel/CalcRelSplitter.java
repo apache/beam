2c9f7443cb7eb17028433e6a6399baf568c005fe
Kyle Weaver <kcweaver@google.com>
2021-04-14 13:11:03 -0700

    [BEAM-12009] Move CalcRelSplitter to rel subpackage.

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/rel/CalcRelSplitter.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/rel/CalcRelSplitter.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.sdk.extensions.sql.impl;
+package org.apache.beam.sdk.extensions.sql.impl.rel;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
