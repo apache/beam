b3f881d2c1a7a8c7b1cb8fe3bd63fd09b0b7d1c0
kileys <kileysok@google.com>
2021-04-15 18:00:32 +0000

    Move nightly snapshot job off of ubuntu nodes

--- .test-infra/jenkins/job_Release_NightlySnapshot.groovy
+++ .test-infra/jenkins/job_Release_NightlySnapshot.groovy
@@ -28,7 +28,7 @@ job('beam_Release_NightlySnapshot') {
   concurrentBuild()
 
   // Set common parameters. Timeout is longer, to avoid [BEAM-5774].
-  commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 200, true, 'ubuntu')
+  commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 200)
 
   // This is a post-commit job that runs once per day, not for every push.
   commonJobProperties.setAutoJob(
@@ -48,18 +48,13 @@ job('beam_Release_NightlySnapshot') {
       rootBuildScriptDir(commonJobProperties.checkoutDir)
       tasks('clean')
     }
-    /*
-     * Skipping verification on 'ubuntu' labelled nodes since they don't have access to the
-     * some required GCP resouces.
-     * TODO: Uncomment this after we publishing snapshots on 'beam' nodes.
      gradle {
-     rootBuildScriptDir(commonJobProperties.checkoutDir)
-     tasks('build')
-     commonJobProperties.setGradleSwitches(delegate)
-     switches('--no-parallel')
-     switches('--continue')
+       rootBuildScriptDir(commonJobProperties.checkoutDir)
+       tasks('build')
+       commonJobProperties.setGradleSwitches(delegate)
+       switches('--no-parallel')
+       switches('--continue')
      }
-     */
     gradle {
       rootBuildScriptDir(commonJobProperties.checkoutDir)
       tasks('publish')
