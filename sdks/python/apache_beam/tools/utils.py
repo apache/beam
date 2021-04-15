a4efc0b4ef2eb8f417dbb34a1e62a5a3def850ea
yoshiki.obata <1285728+lazylynx@users.noreply.github.com>
2021-04-16 06:56:07 +0900

    [BEAM-7372] cleanup py2 codepath from apache_beam/tool, apache_beam/utils and concerning files (#14497)
    * usage of future package
    * usage of past package
    * unnecessary builtins import
    * workaround for python2

--- sdks/python/apache_beam/tools/utils.py
+++ sdks/python/apache_beam/tools/utils.py
@@ -19,10 +19,6 @@
 
 # pytype: skip-file
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import collections
 import gc
 import importlib
