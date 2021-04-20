10694b2daa9dcce107e3f5dbf6eb1d916018063c
Alexey Romanenko <aromanenko.dev@gmail.com>
2021-04-20 18:39:08 +0200

    [BEAM-12197] TPC-DS: Fix SQL-queries syntax

--- sdks/java/testing/tpcds/src/main/resources/queries/query32.sql
+++ sdks/java/testing/tpcds/src/main/resources/queries/query32.sql
@@ -14,28 +14,28 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-select  sum(cs_ext_discount_amt)  as "excess discount amount"
-from 
-   catalog_sales 
-   ,item 
+select  sum(cs_ext_discount_amt)  as `excess discount amount`
+from
+   catalog_sales
+   ,item
    ,date_dim
 where
 i_manufact_id = 269
-and i_item_sk = cs_item_sk 
-and d_date between '1998-03-18' and 
-        (cast('1998-03-18' as date) + 90 days)
-and d_date_sk = cs_sold_date_sk 
-and cs_ext_discount_amt  
-     > ( 
-         select 
-            1.3 * avg(cs_ext_discount_amt) 
-         from 
-            catalog_sales 
+and i_item_sk = cs_item_sk
+and d_date between '1998-03-18' and
+        (cast('1998-03-18' as date) + interval '90' day)
+and d_date_sk = cs_sold_date_sk
+and cs_ext_discount_amt
+     > (
+         select
+            1.3 * avg(cs_ext_discount_amt)
+         from
+            catalog_sales
            ,date_dim
-         where 
-              cs_item_sk = i_item_sk 
+         where
+              cs_item_sk = i_item_sk
           and d_date between '1998-03-18' and
-                             (cast('1998-03-18' as date) + 90 days)
-          and d_date_sk = cs_sold_date_sk 
-      ) 
+                             (cast('1998-03-18' as date) + interval '90' day)
+          and d_date_sk = cs_sold_date_sk
+      )
 limit 100
--- sdks/java/testing/tpcds/src/main/resources/queries/query62.sql
+++ sdks/java/testing/tpcds/src/main/resources/queries/query62.sql
@@ -18,14 +18,14 @@ select
    substr(w_warehouse_name,1,20)
   ,sm_type
   ,web_name
-  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk <= 30 ) then 1 else 0 end)  as "30 days" 
-  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk > 30) and 
-                 (ws_ship_date_sk - ws_sold_date_sk <= 60) then 1 else 0 end )  as "31-60 days" 
-  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk > 60) and 
-                 (ws_ship_date_sk - ws_sold_date_sk <= 90) then 1 else 0 end)  as "61-90 days" 
+  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk <= 30 ) then 1 else 0 end)  as `30 days`
+  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk > 30) and
+                 (ws_ship_date_sk - ws_sold_date_sk <= 60) then 1 else 0 end )  as `31-60 days`
+  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk > 60) and
+                 (ws_ship_date_sk - ws_sold_date_sk <= 90) then 1 else 0 end)  as `61-90 days`
   ,sum(case when (ws_ship_date_sk - ws_sold_date_sk > 90) and
-                 (ws_ship_date_sk - ws_sold_date_sk <= 120) then 1 else 0 end)  as "91-120 days" 
-  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk  > 120) then 1 else 0 end)  as ">120 days" 
+                 (ws_ship_date_sk - ws_sold_date_sk <= 120) then 1 else 0 end)  as `91-120 days`
+  ,sum(case when (ws_ship_date_sk - ws_sold_date_sk  > 120) then 1 else 0 end)  as `>120 days`
 from
    web_sales
   ,warehouse
