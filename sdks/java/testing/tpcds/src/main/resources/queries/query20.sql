cc5c07deb06791de3913ebe6e328ecd972a0e45d
Alexey Romanenko <aromanenko.dev@gmail.com>
2021-04-20 18:39:08 +0200

    [BEAM-12197] TPC-DS: Fix SQL-queries syntax

--- sdks/java/testing/tpcds/src/main/resources/queries/query20.sql
+++ sdks/java/testing/tpcds/src/main/resources/queries/query20.sql
@@ -14,24 +14,23 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-select  i_item_id
-       ,i_item_desc 
-       ,i_category 
-       ,i_class 
+select  i_item_desc
+       ,i_category
+       ,i_class
        ,i_current_price
-       ,sum(cs_ext_sales_price) as itemrevenue 
+       ,sum(cs_ext_sales_price) as itemrevenue
        ,sum(cs_ext_sales_price)*100/sum(sum(cs_ext_sales_price)) over
            (partition by i_class) as revenueratio
- from	catalog_sales
-     ,item 
+ from catalog_sales
+     ,item
      ,date_dim
- where cs_item_sk = i_item_sk 
+ where cs_item_sk = i_item_sk
    and i_category in ('Jewelry', 'Sports', 'Books')
    and cs_sold_date_sk = d_date_sk
- and d_date between cast('2001-01-12' as date) 
- 				and (cast('2001-01-12' as date) + 30 days)
+ and d_date between cast('2001-01-12' as date)
+ 				and (cast('2001-01-12' as date) + interval '30' day)
  group by i_item_id
-         ,i_item_desc 
+         ,i_item_desc
          ,i_category
          ,i_class
          ,i_current_price
