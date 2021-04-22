3f978df0ad92efef5868508dd9ccc2d097d38fd2
Miguel Anzo <miguel.anzo@wizeline.com>
2021-04-22 12:35:19 -0500

    Made visit method return value nullable

--- sdks/java/extensions/arrow/src/main/java/org/apache/beam/sdk/extensions/arrow/ArrowConversion.java
+++ sdks/java/extensions/arrow/src/main/java/org/apache/beam/sdk/extensions/arrow/ArrowConversion.java
@@ -317,7 +317,7 @@ public class ArrowConversion {
       }
 
       @Override
-      public Function<Object, Object> visit(ArrowType.Struct type) {
+      public @Nullable Function<Object, Object> visit(ArrowType.Struct type) {
         // TODO: code to create a row.
         return null;
       }
