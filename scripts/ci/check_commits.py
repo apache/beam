fdc8688a090b1acb7e4d3037c31899d150637c9f
Robert Bradshaw <robertwb@gmail.com>
2021-04-30 13:51:33 -0700

    yapf

--- scripts/ci/check_commits.py
+++ scripts/ci/check_commits.py
@@ -22,49 +22,49 @@ fixup_words = 'fixup', 'typo', 'lint', 'reviewer', 'spotless', 'mypy', 'yapf'
 
 
 def main(url):
-    url = sys.argv[-1]
-    info = requests.get(url).json()
-    if is_approved(info):
-        print(merge_advice(requests.get(url + '/commits').json()))
+  url = sys.argv[-1]
+  info = requests.get(url).json()
+  if is_approved(info):
+    print(merge_advice(requests.get(url + '/commits').json()))
 
 
 def is_approved(info):
-    for review in requests.get(info['_links']['self']['href'] +
-                               '/reviews').json():
-        if review['state'] == 'APPROVED':
-            return True
-        elif 'LGTM' in review['body']:
-            return True
-    for comment in requests.get(info['_links']['comments']['href']).json():
-        if 'LGTM' in comment['body']:
-            return True
+  for review in requests.get(info['_links']['self']['href'] +
+                             '/reviews').json():
+    if review['state'] == 'APPROVED':
+      return True
+    elif 'LGTM' in review['body']:
+      return True
+  for comment in requests.get(info['_links']['comments']['href']).json():
+    if 'LGTM' in comment['body']:
+      return True
 
 
 def merge_advice(commits):
-    if len(commits) == 1:
-      return
-    fixup_commits = sum(is_fixup_commit(c) for c in commits)
-    if fixup_commits:
-        return "Looks like there are some fixup commits. Squash and merge?"
-    elif len(commits) > 5:
-      return (
+  if len(commits) == 1:
+    return
+  fixup_commits = sum(is_fixup_commit(c) for c in commits)
+  if fixup_commits:
+    return "Looks like there are some fixup commits. Squash and merge?"
+  elif len(commits) > 5:
+    return (
         "That's a lot of commits. "
         "Are they each independent and well-described? "
         "If not, clean up the history, or use squash and merge if there isn't "
         "any finer-grained history worth preserving.")
-    else:
-      return (
+  else:
+    return (
         "It seems like each commit is independent and well-described. "
         "I'm just a bot so I might be wrong. "
         "If I am correct, consider using the merge button on this one. "
         "If not, please help improve to recognize that these commits should be "
-        "squashed."
+        "squashed.")
 
 
 def is_fixup_commit(commit):
-    msg = commit['commit']['message'].lower()
-    return len(msg) < 30 or any(word in msg for word in fixup_words)
+  msg = commit['commit']['message'].lower()
+  return len(msg) < 30 or any(word in msg for word in fixup_words)
 
 
 if __name__ == '__main__':
-    main(sys.argv[-1])
+  main(sys.argv[-1])
