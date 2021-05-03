69bc025277cd20dc50d29752f407787e741b394c
Pablo Estrada <pabloem@apache.org>
2021-05-03 11:18:22 -0700

    Revert "Merge pull request #13617 from [BEAM-11289] [Python] Integrate Google Cloud Recommendations AI functionality"
    This reverts commit d9da8a4dc818b01a86d2dce2e78c0d78b47038bb.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -200,7 +200,6 @@ GCP_REQUIREMENTS = [
     'google-cloud-language>=1.3.0,<2',
     'google-cloud-videointelligence>=1.8.0,<2',
     'google-cloud-vision>=0.38.0,<2',
-    'google-cloud-recommendations-ai>=0.1.0,<=0.2.0'
 ]
 
 INTERACTIVE_BEAM = [
