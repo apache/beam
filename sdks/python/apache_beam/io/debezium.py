223e35f028254817e8bcd6250cb8d777f5c53aa6
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-05-06 17:54:26 -0500

    [BEAM-11838] Add DocString to debezium python transform

--- sdks/python/apache_beam/io/debezium.py
+++ sdks/python/apache_beam/io/debezium.py
@@ -15,24 +15,85 @@
 # limitations under the License.
 #
 
-# TODO(BEAM-11838)
+""" Unbounded source transform for
+    `Debezium <href="https://debezium.io/"/>`_.
 
-# TODO(BEAM-11838) DocString
-import typing
+  This transform is currently supported by Beam portable
+  Flink, Spark, and Dataflow v2 runners.
+
+  **Setup**
+
+  Transform provided in this module is cross-language transform
+  implemented in the Beam Java SDK. During the pipeline construction, Python SDK
+  will connect to a Java expansion service to expand this transform.
+  To facilitate this, a small amount of setup is needed before using this
+  transform in a Beam Python pipeline.
+
+  There are several ways to setup cross-language Debezium transform.
+
+  * Option 1: use the default expansion service
+  * Option 2: specify a custom expansion service
+
+  See below for details regarding each of these options.
+
+  *Option 1: Use the default expansion service*
+
+  This is the recommended and easiest setup option for using Python Debezium
+  transform. This option requires following pre-requisites
+  before running the Beam pipeline.
+
+  * Install Java runtime in the computer from where the pipeline is constructed
+    and make sure that 'java' command is available.
+
+  In this option, Python SDK will either download (for released Beam version) or
+  build (when running from a Beam Git clone) a expansion service jar and use
+  that to expand transforms. Currently Debezium transform use the
+  'beam-sdks-java-io-expansion-service' jar for this purpose.
+
+  *Option 2: specify a custom expansion service*
+
+  In this option, you startup your own expansion service and provide that as
+  a parameter when using the transform provided in this module.
+
+  This option requires following pre-requisites before running the Beam
+  pipeline.
+
+  * Startup your own expansion service.
+  * Update your pipeline to provide the expansion service address when
+    initiating Debezium transform provided in this module.
+
+  Flink Users can use the built-in Expansion Service of the Flink Runner's
+  Job Server. If you start Flink's Job Server, the expansion service will be
+  started on port 8097. For a different address, please set the
+  expansion_service parameter.
+
+  **More information**
+
+  For more information regarding cross-language transforms see:
+  - https://beam.apache.org/roadmap/portability/
+
+  For more information specific to Flink runner see:
+  - https://beam.apache.org/documentation/runners/flink/
+"""
+
+# pytype: skip-file
+
 from enum import Enum
 
-from apache_beam.coders import RowCoder
+from typing import Optional
+from typing import NamedTuple
+from typing import List
+
+import apache_beam as beam
 from apache_beam.transforms.external import BeamJarExpansionService
 from apache_beam.transforms.external import ExternalTransform
 from apache_beam.transforms.external import NamedTupleBasedPayloadBuilder
 from apache_beam.typehints.schemas import typing_to_runner_api
 
-# TODO(BEAM-11838) List all methods
 __all__ = [
     'ReadFromDebezium',
     'Connector'
 ]
-
 
 def default_io_expansion_service():
     return BeamJarExpansionService('sdks:java:io:expansion-service:shadowJar')
@@ -45,21 +106,22 @@ class DriverClassName(Enum):
     DB2 = 'Db2'
 
 
-ReadFromDebeziumSchema = typing.NamedTuple(
+ReadFromDebeziumSchema = NamedTuple(
     'ReadFromDebeziumSchema',
     [('connector_class', str),
      ('username', str),
      ('password', str),
      ('host', str),
      ('port', str),
-     ('max_number_of_records', typing.Optional[int]),
-     ('connection_properties', typing.List[str])]
+     ('max_number_of_records', Optional[int]),
+     ('connection_properties', List[str])]
 )
 
 
 class ReadFromDebezium(ExternalTransform):
     """
         An external PTransform which reads from Debezium and returns a JSON string
+        for each item in the specified database connection.
 
         Experimental; no backwards compatibility guarantees.
     """
@@ -76,7 +138,7 @@ class ReadFromDebezium(ExternalTransform):
             connection_properties=[],
             expansion_service=None):
         """
-        Initializes a read operation to Debezium.
+        Initializes a read operation from Debezium.
 
         :param connector_class: name of the jdbc driver class
         :param username: database username
@@ -101,5 +163,4 @@ class ReadFromDebezium(ExternalTransform):
                     max_number_of_records=max_number_of_records,
                     connection_properties=connection_properties
                 )),
-            #     TODO(BEAM-11838) Add connection properties as param
             expansion_service or default_io_expansion_service())
