7a0e83ab8af93c8c5aadb13511ad0bab74e49b3b
hari <hrajagopal@lyft.com>
2021-05-11 13:02:19 -0700

    Commit for release 2.21.0-lyft202105111620763339

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -306,7 +307,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.21.0-lyft202104231619194328-lyft202104231619193945-lyft202104231619193878-lyft202104231619181671'
+    project.version = '2.21.0-lyft202105111620763339'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
@@ -554,7 +555,7 @@ class BeamModulePlugin implements Plugin<Project> {
         maven_exec_plugin    : "maven-plugins:maven-exec-plugin:1.6.0",
         maven_jar_plugin     : "maven-plugins:maven-jar-plugin:3.0.2",
         maven_shade_plugin   : "maven-plugins:maven-shade-plugin:3.1.0",
-        maven_surefire_plugin: "maven-plugins:maven-surefire-plugin:2.21.0-lyft202104231619194328-lyft202104231619193945-lyft202104231619193878-lyft202104231619181671",
+        maven_surefire_plugin: "maven-plugins:maven-surefire-plugin:2.21.0-lyft202105111620763339",
       ],
     ]
 
