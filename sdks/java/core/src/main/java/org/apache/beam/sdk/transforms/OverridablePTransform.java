b4420624ba9668b4a0fe56edb93d35754753d764
Cheng <yuhcheng@linkedin.com>
2021-05-17 10:06:05 -0700

    refactor OverriablePTransform to interface
    RB=2597789
    G=samza-reviewers
    R=xiliu
    A=xiliu

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/OverridablePTransform.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/OverridablePTransform.java
@@ -5,17 +5,15 @@ import org.apache.beam.sdk.values.PInput;
 import org.apache.beam.sdk.values.POutput;
 
 /**
- * A {@code OverridablePTransform} is a {@link PTransform} that provides a API for users to override
- * a {@link PTransform} when needed (e.g. local tests). When urn provided by a {@code
+ * A {@code OverridablePTransform} is an interface that provides a API for users to override
+ * a {@link PTransform} when needed (e.g. local tests). When tag provided by a {@code
  * OverridablePTransform} is registered in {@link PTransformOverrideRegistrar}, the {@link
  * OverridablePTransform} will be replaced.
  *
  * LI-specific class.
  */
 @Experimental
-public abstract class OverridablePTransform<IN extends PInput, OUT extends POutput>
-        extends PTransform<IN, OUT> {
-
-    public abstract String getTag();
+public interface OverridablePTransform<IN extends PInput, OUT extends POutput> {
+    String getTag();
 }
 
