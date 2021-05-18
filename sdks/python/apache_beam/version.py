b2db9a2daee03b1a18f453bb3bd957c886c2bdc5
hari <hrajagopal@lyft.com>
2021-05-18 10:04:52 -0700

    Commit for release 2.21.0-lyft202105181621357492

--- sdks/python/apache_beam/version.py
+++ sdks/python/apache_beam/version.py
@@ -17,4 +17,4 @@
 
 """Apache Beam SDK version information and utilities."""
 #__version__ = '2.21.0+lyft202105181621357000
-__version__ = '2.21.0+lyft202105181621357112
+__version__ = '2.21.0+lyft202105181621357492'
