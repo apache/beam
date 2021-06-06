26118781a52c8bf1b79689bf2fcbf2eb69c53479
Chuck Yang <chuck.yang@getcruise.com>
2021-06-06 08:59:58 -0700

    Add adapter to use google-auth creds with apitools

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -145,7 +145,6 @@ REQUIRED_PACKAGES = [
     'httplib2>=0.8,<0.20.0',
     'numpy>=1.14.3,<1.21.0',
     'pymongo>=3.8.0,<4.0.0',
-    'oauth2client>=2.0.1,<5',
     'protobuf>=3.12.2,<4',
     'pyarrow>=0.15.1,<5.0.0',
     'pydot>=1.2.0,<2',
