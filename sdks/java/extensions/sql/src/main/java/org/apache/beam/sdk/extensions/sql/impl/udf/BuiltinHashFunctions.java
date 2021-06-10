923e59963a6fd8709d4e67b378c3279ab36a559f
Kyle Weaver <kcweaver@google.com>
2021-06-10 14:15:46 -0700

    [BEAM-9379] Use byte[] instead of ByteString for (VAR)BINARY in UDFs.

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/udf/BuiltinHashFunctions.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/impl/udf/BuiltinHashFunctions.java
@@ -19,7 +19,6 @@ package org.apache.beam.sdk.extensions.sql.impl.udf;
 
 import com.google.auto.service.AutoService;
 import org.apache.beam.sdk.schemas.Schema;
-import org.apache.beam.vendor.calcite.v1_26_0.org.apache.calcite.avatica.util.ByteString;
 import org.apache.beam.vendor.calcite.v1_26_0.org.apache.calcite.linq4j.function.Strict;
 
 /** Hash Functions. */
@@ -29,112 +28,112 @@ public class BuiltinHashFunctions extends BeamBuiltinFunctionProvider {
   /**
    * MD5(X)
    *
-   * <p>Calculates the MD5 digest and returns the value as a 16 element {@code ByteString}.
+   * <p>Calculates the MD5 digest and returns the value as a 16 element {@code byte[]}.
    */
   @UDF(
       funcName = "MD5",
       parameterArray = {Schema.TypeName.STRING},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString md5String(String str) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.md5(str));
+  public byte[] md5String(String str) {
+    return org.apache.commons.codec.digest.DigestUtils.md5(str);
   }
 
   /**
    * MD5(X)
    *
-   * <p>Calculates the MD5 digest and returns the value as a 16 element {@code ByteString}.
+   * <p>Calculates the MD5 digest and returns the value as a 16 element {@code byte[]}.
    */
   @UDF(
       funcName = "MD5",
       parameterArray = {Schema.TypeName.BYTES},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString md5Bytes(byte[] bytes) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.md5(bytes));
+  public byte[] md5Bytes(byte[] bytes) {
+    return org.apache.commons.codec.digest.DigestUtils.md5(bytes);
   }
 
   /**
    * SHA1(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA1",
       parameterArray = {Schema.TypeName.STRING},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha1String(String str) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha1(str));
+  public byte[] sha1String(String str) {
+    return org.apache.commons.codec.digest.DigestUtils.sha1(str);
   }
 
   /**
    * SHA1(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA1",
       parameterArray = {Schema.TypeName.BYTES},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha1Bytes(byte[] bytes) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha1(bytes));
+  public byte[] sha1Bytes(byte[] bytes) {
+    return org.apache.commons.codec.digest.DigestUtils.sha1(bytes);
   }
 
   /**
    * SHA256(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA256",
       parameterArray = {Schema.TypeName.STRING},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha256String(String str) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha256(str));
+  public byte[] sha256String(String str) {
+    return org.apache.commons.codec.digest.DigestUtils.sha256(str);
   }
 
   /**
    * SHA256(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA256",
       parameterArray = {Schema.TypeName.BYTES},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha256Bytes(byte[] bytes) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha256(bytes));
+  public byte[] sha256Bytes(byte[] bytes) {
+    return org.apache.commons.codec.digest.DigestUtils.sha256(bytes);
   }
 
   /**
    * SHA512(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA512",
       parameterArray = {Schema.TypeName.STRING},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha512String(String str) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha512(str));
+  public byte[] sha512String(String str) {
+    return org.apache.commons.codec.digest.DigestUtils.sha512(str);
   }
 
   /**
    * SHA512(X)
    *
-   * <p>Calculates the SHA-1 digest and returns the value as a {@code ByteString}.
+   * <p>Calculates the SHA-1 digest and returns the value as a {@code byte[]}.
    */
   @UDF(
       funcName = "SHA512",
       parameterArray = {Schema.TypeName.BYTES},
       returnType = Schema.TypeName.BYTES)
   @Strict
-  public ByteString sha512Bytes(byte[] bytes) {
-    return new ByteString(org.apache.commons.codec.digest.DigestUtils.sha512(bytes));
+  public byte[] sha512Bytes(byte[] bytes) {
+    return org.apache.commons.codec.digest.DigestUtils.sha512(bytes);
   }
 }
