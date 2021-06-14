be72055ade17a958fb4f2fc8b74e362e3a1b5eb6
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-06-14 16:35:25 -0500

    [BEAM-12068] Run Dataflow V2 performance benchmarks on Java 11

--- .test-infra/jenkins/LoadTestsBuilder.groovy
+++ .test-infra/jenkins/LoadTestsBuilder.groovy
@@ -24,23 +24,29 @@ import InfluxDBCredentialsHelper
 import static PythonTestProperties.LOAD_TEST_PYTHON_VERSION
 
 class LoadTestsBuilder {
+  final static String DEFAULT_JAVA_RUNTIME_VERSION = "1.8";
+  final static String JAVA_11_RUNTIME_VERSION = "11";
   final static String DOCKER_CONTAINER_REGISTRY = 'gcr.io/apache-beam-testing/beam_portability'
   final static String DOCKER_BEAM_SDK_IMAGE = "beam_python${LOAD_TEST_PYTHON_VERSION}_sdk:latest"
 
-  static void loadTests(scope, CommonTestProperties.SDK sdk, List testConfigurations, String test, String mode){
+  static void loadTests(scope, CommonTestProperties.SDK sdk, List testConfigurations, String test, String mode,
+      List<String> jobSpecificSwitches = null,
+      String javaRuntimeVersion = DEFAULT_JAVA_RUNTIME_VERSION) {
     scope.description("Runs ${sdk.toString().toLowerCase().capitalize()} ${test} load tests in ${mode} mode")
 
     commonJobProperties.setTopLevelMainJobProperties(scope, 'master', 240)
 
     for (testConfiguration in testConfigurations) {
       loadTest(scope, testConfiguration.title, testConfiguration.runner, sdk, testConfiguration.pipelineOptions,
-          testConfiguration.test, testConfiguration.withDataflowWorkerJar ?: false)
+          testConfiguration.test, jobSpecificSwitches,
+          javaRuntimeVersion)
     }
   }
 
 
   static void loadTest(context, String title, Runner runner, SDK sdk, Map<String, ?> options,
-      String mainClass, Boolean withDataflowWorkerJar = false) {
+      String mainClass, List<String> jobSpecificSwitches = null,
+      String javaRuntimeVersion = DEFAULT_JAVA_RUNTIME_VERSION) {
     options.put('runner', runner.option)
     InfluxDBCredentialsHelper.useCredentials(context)
 
@@ -48,7 +54,8 @@ class LoadTestsBuilder {
       shell("echo \"*** ${title} ***\"")
       gradle {
         rootBuildScriptDir(commonJobProperties.checkoutDir)
-        setGradleTask(delegate, runner, sdk, options, mainClass, withDataflowWorkerJar)
+        setGradleTask(delegate, runner, sdk, options, mainClass,
+            jobSpecificSwitches, javaRuntimeVersion)
         commonJobProperties.setGradleSwitches(delegate)
       }
     }
@@ -74,22 +81,28 @@ class LoadTestsBuilder {
   }
 
   private static void setGradleTask(context, Runner runner, SDK sdk, Map<String, ?> options,
-      String mainClass, Boolean withDataflowWorkerJar) {
-    context.tasks(getGradleTaskName(sdk))
+      String mainClass, List<String> jobSpecificSwitches, String javaRuntimeVersion) {
+    context.tasks(getGradleTaskName(sdk, javaRuntimeVersion))
     context.switches("-PloadTest.mainClass=\"${mainClass}\"")
     context.switches("-Prunner=${runner.getDependencyBySDK(sdk)}")
-    context.switches("-PwithDataflowWorkerJar=\"${withDataflowWorkerJar}\"")
     context.switches("-PloadTest.args=\"${parseOptions(options)}\"")
-
+    if (jobSpecificSwitches != null) {
+      jobSpecificSwitches.each {
+        context.switches(it)
+      }
+    }
 
     if (sdk == SDK.PYTHON) {
       context.switches("-PpythonVersion=${LOAD_TEST_PYTHON_VERSION}")
     }
   }
 
-  private static String getGradleTaskName(SDK sdk) {
+  private static String getGradleTaskName(SDK sdk, String runtimeVersion) {
     switch (sdk) {
       case SDK.JAVA:
+        if (runtimeVersion.equals(JAVA_11_RUNTIME_VERSION)) {
+          return ':sdks:java:testing:load-tests:runJava11'
+        }
         return ':sdks:java:testing:load-tests:run'
       case SDK.PYTHON:
         return ':sdks:python:apache_beam:testing:load_tests:run'
