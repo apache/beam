f59458731cea8bbd6b553ea70276d5272cfd687f
Matthias Baetens <baetensmatthias@gmail.com>
2021-06-21 09:53:55 +0200

    Add missing comma to setup.py

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -200,7 +200,7 @@ GCP_REQUIREMENTS = [
     'google-cloud-videointelligence>=1.8.0,<2',
     'google-cloud-vision>=0.38.0,<2',
     # GCP Package required by Google Cloud Profiler.
-    'google-cloud-profiler>=3.0.4,<4'
+    'google-cloud-profiler>=3.0.4,<4',
     'google-cloud-recommendations-ai>=0.1.0,<=0.2.0'
 ]
 
