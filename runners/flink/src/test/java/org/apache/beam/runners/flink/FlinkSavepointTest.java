55c4370feb83d10e47f18d26d70854f66ee2a850
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-07-02 09:56:43 -0500

    [BEAM-12515] Revert 15107

--- runners/flink/src/test/java/org/apache/beam/runners/flink/FlinkSavepointTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/FlinkSavepointTest.java
@@ -68,6 +68,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -141,6 +142,7 @@ public class FlinkSavepointTest implements Serializable {
     ensureNoJobRunning();
   }
 
+  @Ignore("https://issues.apache.org/jira/projects/BEAM/issues/BEAM-10955")
   @Test
   public void testSavepointRestoreLegacy() throws Exception {
     runSavepointAndRestore(false);
@@ -191,7 +193,7 @@ public class FlinkSavepointTest implements Serializable {
   private JobID executeLegacy(Pipeline pipeline) throws Exception {
     JobGraph jobGraph = getJobGraph(pipeline);
     flinkCluster.submitJob(jobGraph).get();
-    return waitForJobToBeReady();
+    return jobGraph.getJobID();
   }
 
   private JobID executePortable(Pipeline pipeline) throws Exception {
