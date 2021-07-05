5482a0891d1dcad80d58c7e7cfea67babc2a6980
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-07-05 11:12:57 -0500

    [BEAM-10955] Spotless apply

--- runners/flink/src/test/java/org/apache/beam/runners/flink/FlinkSavepointTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/FlinkSavepointTest.java
@@ -157,7 +157,8 @@ public class FlinkSavepointTest implements Serializable {
     // Initial parallelism
     options.setParallelism(2);
     options.setRunner(FlinkRunner.class);
-    // Enable checkpointing interval for streaming non portable pipeline to avoid checkpointCoordinator shutdown
+    // Enable checkpointing interval for streaming non portable pipeline to avoid
+    // checkpointCoordinator shutdown
     if (!isPortablePipeline) options.setCheckpointingInterval(1000 * 10L);
     // Avoid any task from shutting down which would prevent savepointing
     options.setShutdownSourcesAfterIdleMs(Long.MAX_VALUE);
