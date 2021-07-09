22918c4ba7d6c73673ef551b234c814249dc76c8
Saksham Gupta <sakshamgupta05@gmail.com>
2021-07-10 04:12:16 +0530

    RabbitMqIO fix race condition causing duplicate acks
    RabbitMqIO.java uses ArrayList to store sessionIds which is not thread
    safe and thus causes duplicate acks in finalizeCheckpoint method. This
    leads to the channel getting closed. ArrayList is replaced with
    ConcurrentLinkedQueue which ensures that only one thread can
    process a sessionId for a channel.

--- sdks/java/io/rabbitmq/src/main/java/org/apache/beam/sdk/io/rabbitmq/RabbitMqIO.java
+++ sdks/java/io/rabbitmq/src/main/java/org/apache/beam/sdk/io/rabbitmq/RabbitMqIO.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeoutException;
 import org.apache.beam.sdk.annotations.Experimental;
 import org.apache.beam.sdk.annotations.Experimental.Kind;
@@ -416,7 +417,7 @@ public class RabbitMqIO {
       implements UnboundedSource.CheckpointMark, Serializable {
     transient Channel channel;
     Instant latestTimestamp = Instant.now();
-    final List<Long> sessionIds = new ArrayList<>();
+    final ConcurrentLinkedQueue<Long> sessionIds = new ConcurrentLinkedQueue<>();
 
     /**
      * Advances the watermark to the provided time, provided said time is after the current
@@ -432,12 +433,13 @@ public class RabbitMqIO {
 
     @Override
     public void finalizeCheckpoint() throws IOException {
-      for (Long sessionId : sessionIds) {
+      Long sessionId = sessionIds.poll();
+      while (sessionId != null) {
         channel.basicAck(sessionId, false);
+        sessionId = sessionIds.poll();
       }
       channel.txCommit();
       latestTimestamp = Instant.now();
-      sessionIds.clear();
     }
   }
 
