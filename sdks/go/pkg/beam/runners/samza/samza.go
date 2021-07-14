e542d1fd7deb20faf2232a33426b18df1f554890
Ke Wu <kwu@linkedin.com>
2021-07-13 18:13:08 -0700

    [BEAM-12556] Enable Go Build Tests in Samza Runner (#15167)

--- sdks/go/pkg/beam/runners/samza/samza.go
+++ sdks/go/pkg/beam/runners/samza/samza.go
@@ -13,29 +13,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package regression
+// Package samza contains the Samza runner.
+package samza
 
 import (
-	"testing"
+	"context"
 
 	"github.com/apache/beam/sdks/go/pkg/beam"
-	"github.com/apache/beam/sdks/go/pkg/beam/testing/passert"
-	"github.com/apache/beam/sdks/go/pkg/beam/testing/ptest"
-	"github.com/apache/beam/sdks/go/test/integration"
-
-	_ "github.com/apache/beam/sdks/go/pkg/beam/runners/dataflow"
-	_ "github.com/apache/beam/sdks/go/pkg/beam/runners/flink"
-	_ "github.com/apache/beam/sdks/go/pkg/beam/runners/samza"
-	_ "github.com/apache/beam/sdks/go/pkg/beam/runners/spark"
+	"github.com/apache/beam/sdks/go/pkg/beam/runners/universal"
 )
 
-func TestLPErrorPipeline(t *testing.T) {
-	integration.CheckFilters(t)
+func init() {
+	beam.RegisterRunner("samza", Execute)
+	beam.RegisterRunner("SamzaRunner", Execute)
+}
 
-	pipeline, s := beam.NewPipelineWithRoot()
-	want := beam.CreateList(s, []int{0})
-	got := LPErrorPipeline(s)
-	passert.Equals(s, got, want)
-
-	ptest.RunAndValidate(t, pipeline)
+// Execute runs the given pipeline on Samza. Convenience wrapper over the
+// universal runner.
+func Execute(ctx context.Context, p *beam.Pipeline) (beam.PipelineResult, error) {
+	return universal.Execute(ctx, p)
 }
