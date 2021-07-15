f0edfc97fc5ccab7dc17d8570bb50b5de60ac2c5
Ahmet Altay <aaltay@gmail.com>
2021-07-14 20:11:20 -0700

    Reject sessions in side inputs

--- sdks/python/apache_beam/transforms/sideinputs.py
+++ sdks/python/apache_beam/transforms/sideinputs.py
@@ -54,6 +54,9 @@ def default_window_mapping_fn(target_window_fn):
   # type: (window.WindowFn) -> WindowMappingFn
   if target_window_fn == window.GlobalWindows():
     return _global_window_mapping_fn
+  
+  if isinstance(target_window_fn, window.Sessions):
+    raise RuntimeError("Sessions is not allowed in side inputs")
 
   def map_via_end(source_window):
     # type: (window.BoundedWindow) -> window.BoundedWindow
