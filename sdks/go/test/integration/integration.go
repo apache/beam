8c2a10d1393016ef8e47f0173e42ed5974768d73
Luke Cwik <lcwik@google.com>
2021-07-19 10:31:13 -0700

    Fix formatting using go fmt

--- sdks/go/test/integration/integration.go
+++ sdks/go/test/integration/integration.go
@@ -72,9 +72,9 @@ var flinkFilters = []string{
 }
 
 var samzaFilters = []string{
-    // TODO(BEAM-12608): Samza tests invalid encoding.
-    "TestReshuffle",
-    "TestReshuffleKV",
+	// TODO(BEAM-12608): Samza tests invalid encoding.
+	"TestReshuffle",
+	"TestReshuffleKV",
 }
 
 var sparkFilters = []string{
