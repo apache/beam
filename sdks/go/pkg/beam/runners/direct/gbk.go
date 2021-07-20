1ce60f72637fa83ab2fc8648e518abb9b5e75eeb
Jack McCluskey <jrmccluskey@google.com>
2021-07-20 19:07:44 +0000

    [BEAM-4152] Implement window merging for the Go direct runner

--- sdks/go/pkg/beam/runners/direct/gbk.go
+++ sdks/go/pkg/beam/runners/direct/gbk.go
@@ -19,8 +19,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/apache/beam/sdks/go/pkg/beam/core/graph"
+	"github.com/apache/beam/sdks/go/pkg/beam/core/graph/window"
 	"github.com/apache/beam/sdks/go/pkg/beam/core/runtime/exec"
 	"github.com/apache/beam/sdks/go/pkg/beam/core/typex"
 	"github.com/apache/beam/sdks/go/pkg/beam/internal/errors"
@@ -37,9 +39,11 @@ type CoGBK struct {
 	Edge *graph.MultiEdge
 	Out  exec.Node
 
-	enc  exec.ElementEncoder // key encoder for coder-equality
-	wEnc exec.WindowEncoder  // window encoder for windowing
-	m    map[string]*group
+	enc      exec.ElementEncoder // key encoder for coder-equality
+	wEnc     exec.WindowEncoder  // window encoder for windowing
+	m        map[string]*group
+	wins     []typex.Window
+	mergeMap map[typex.Window]int
 }
 
 func (n *CoGBK) ID() exec.UnitID {
@@ -63,6 +67,7 @@ func (n *CoGBK) ProcessElement(ctx context.Context, elm *exec.FullValue, _ ...ex
 
 	for _, w := range elm.Windows {
 		ws := []typex.Window{w}
+		n.wins = append(n.wins, ws...)
 
 		var buf bytes.Buffer
 		if err := n.enc.Encode(&exec.FullValue{Elm: value.Elm}, &buf); err != nil {
@@ -87,6 +92,15 @@ func (n *CoGBK) ProcessElement(ctx context.Context, elm *exec.FullValue, _ ...ex
 }
 
 func (n *CoGBK) FinishBundle(ctx context.Context) error {
+	winKind := n.Edge.Input[0].From.WindowingStrategy().Fn.Kind
+	if winKind == window.Sessions {
+		if mergeErr := n.mergeWindows(); mergeErr != nil {
+			return errors.Errorf("failed to merge windows, got: %v", mergeErr)
+		}
+		if reprocessErr := n.reprocessByWindow(); reprocessErr != nil {
+			return errors.Errorf("failed to reprocess with merged windows, got :%v", reprocessErr)
+		}
+	}
 	for key, g := range n.m {
 		values := make([]exec.ReStream, len(g.values))
 		for i, list := range g.values {
@@ -98,6 +112,68 @@ func (n *CoGBK) FinishBundle(ctx context.Context) error {
 		delete(n.m, key)
 	}
 	return n.Out.FinishBundle(ctx)
+}
+
+func (n *CoGBK) mergeWindows() error {
+	sort.Slice(n.wins, func(i int, j int) bool {
+		return n.wins[i].MaxTimestamp() < n.wins[j].MaxTimestamp()
+	})
+	n.mergeMap = make(map[typex.Window]int)
+	mergedWins := []typex.Window{}
+	for i := 0; i < len(n.wins); {
+		intWin, ok := n.wins[i].(window.IntervalWindow)
+		if !ok {
+			return errors.Errorf("tried to merge non-interval window type")
+		}
+		mergeStart := intWin.Start
+		mergeEnd := intWin.End
+		j := i + 1
+		for j < len(n.wins) {
+			candidateWin := n.wins[j].(window.IntervalWindow)
+			if candidateWin.Start <= mergeEnd {
+				mergeEnd = candidateWin.End
+				j++
+			} else {
+				break
+			}
+		}
+		for k := i; k < j; k++ {
+			n.mergeMap[n.wins[k]] = len(mergedWins)
+		}
+		mergedWins = append(mergedWins, window.IntervalWindow{Start: mergeStart, End: mergeEnd})
+		i = j
+	}
+	n.wins = mergedWins
+	return nil
+}
+
+func (n *CoGBK) reprocessByWindow() error {
+	newGroups := make(map[string]*group)
+	for _, g := range n.m {
+		ws := []typex.Window{n.wins[n.mergeMap[g.key.Windows[0]]]}
+		var buf bytes.Buffer
+		if err := n.enc.Encode(&exec.FullValue{Elm: g.key.Elm}, &buf); err != nil {
+			return errors.WithContextf(err, "encoding key %v for CoGBK", g.key.Elm)
+		}
+		if err := n.wEnc.Encode(ws, &buf); err != nil {
+			return errors.WithContextf(err, "encoding window %v for CoGBK", ws[0])
+		}
+		key := buf.String()
+
+		gr, ok := newGroups[key]
+		if !ok {
+			gr = &group{
+				key:    exec.FullValue{Elm: g.key.Elm, Timestamp: g.key.Timestamp, Windows: ws},
+				values: make([][]exec.FullValue, len(n.Edge.Input)),
+			}
+			newGroups[key] = gr
+		}
+		for i, list := range g.values {
+			gr.values[i] = append(gr.values[i], list...)
+		}
+	}
+	n.m = newGroups
+	return nil
 }
 
 func (n *CoGBK) Down(ctx context.Context) error {
