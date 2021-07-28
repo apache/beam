ca866afa2737abca84aade05cafff6e5126a66f3
Ankur <angoenka@users.noreply.github.com>
2021-07-28 13:07:52 -0700

    [BEAM-12399] Move CPython license to own file. (#15201) (#15211)
    * [BEAM-12399] Move CPython license to own file.
    * [BEAM-12399] Add LICENSE.python to python MANIFEST
    * [BEAM-12399] Filter LICENSE.python from RAT check.
    * [BEAM-12399] Refer to file by proximity rather than URL.
    
    Co-authored-by: Robert Burke <lostluck@users.noreply.github.com>

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1738,6 +1738,7 @@ class BeamModulePlugin implements Plugin<Project> {
       project.docker { noCache true }
       project.tasks.create(name: "copyLicenses", type: Copy) {
         from "${project.rootProject.projectDir}/LICENSE"
+        from "${project.rootProject.projectDir}/LICENSE.python"
         from "${project.rootProject.projectDir}/NOTICE"
         into "build/target"
       }
