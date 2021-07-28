3178f51c23918d0ed61deea488c09707b13c6dde
Ritesh Ghorse <riteshghorse@gmail.com>
2021-07-28 14:07:20 -0400

    [BEAM-3304] added struct field for trigger

--- sdks/go/pkg/beam/core/graph/window/strategy.go
+++ sdks/go/pkg/beam/core/graph/window/strategy.go
@@ -16,12 +16,16 @@
 // Package window contains window representation, windowing strategies and utilities.
 package window
 
+import (
+	pipepb "github.com/apache/beam/sdks/go/pkg/beam/model/pipeline_v1"
+)
+
 // WindowingStrategy defines the types of windowing used in a pipeline and contains
 // the data to support executing a windowing strategy.
 type WindowingStrategy struct {
 	Fn *Fn
-
 	// TODO(BEAM-3304): trigger support
+	Trigger *pipepb.Trigger
 }
 
 func (ws *WindowingStrategy) Equals(o *WindowingStrategy) bool {
@@ -34,5 +38,9 @@ func (ws *WindowingStrategy) String() string {
 
 // DefaultWindowingStrategy returns the default windowing strategy.
 func DefaultWindowingStrategy() *WindowingStrategy {
-	return &WindowingStrategy{Fn: NewGlobalWindows()}
+	return &WindowingStrategy{Fn: NewGlobalWindows(), Trigger: &pipepb.Trigger{
+		Trigger: &pipepb.Trigger_Always_{
+			Always: &pipepb.Trigger_Always{},
+		},
+	}}
 }
