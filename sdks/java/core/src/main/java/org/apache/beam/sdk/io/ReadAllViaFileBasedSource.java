5b4fa25c6aa2b4ed5ca771eb489a6579fc5df73d
dhercher <dhercher@google.com>
2021-08-03 23:00:25 +0000

    linting

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/ReadAllViaFileBasedSource.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/ReadAllViaFileBasedSource.java
@@ -77,8 +77,10 @@ public class ReadAllViaFileBasedSource<T>
     // }
     return input
         .apply("Split into ranges", ParDo.of(new SplitIntoRangesFn(desiredBundleSizeBytes)))
-        .apply("Reshuffle", Reshuffle.<KV<ReadableFile, OffsetRange>>viaRandomKey()
-            .withNumBuckets((Integer) readerThreadCount))
+        .apply(
+            "Reshuffle",
+            Reshuffle.<KV<ReadableFile, OffsetRange>>viaRandomKey()
+                .withNumBuckets((Integer) readerThreadCount))
         .apply("Read ranges", ParDo.of(new ReadFileRangesFn<>(createSource)))
         .setCoder(coder);
   }
