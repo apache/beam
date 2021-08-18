bd9f5d927ea1ea68521033e9407f9330b5bd74fd
Daniel Chen <dchen1@linkedin.com>
2021-08-18 00:13:17 -0700

    Fix artifacts publishing (#6)
    * Pin scalatra dependency, remove non required subprojects, stop publishing checksum files
    
    * Include test dependencies

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -363,7 +363,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.28.0.1'
+    project.version = '2.28.0.2'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
