ba2561d7e592b5bf354b651df3ac9cede8d7dc6b
matthiasa4 <baetensmatthias@gmail.com>
2021-08-24 23:17:52 +0300

    Addressing comments

--- sdks/java/extensions/ml/src/test/java/org/apache/beam/sdk/extensions/ml/RecommendationAICatalogItemIT.java
+++ sdks/java/extensions/ml/src/test/java/org/apache/beam/sdk/extensions/ml/RecommendationAICatalogItemIT.java
@@ -23,10 +23,12 @@ import static org.junit.Assert.assertTrue;
 import com.google.api.client.json.GenericJson;
 import com.google.cloud.recommendationengine.v1beta1.CatalogItem;
 import com.google.cloud.recommendationengine.v1beta1.CatalogServiceClient;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
@@ -43,10 +45,12 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class RecommendationAICatalogItemIT {
-  @Rule public TestPipeline testPipeline = TestPipeline.create();
-  static String randomId = Integer.toString(new Random().nextInt());
+  @Rule public static TestPipeline testPipeline = TestPipeline.create();
+  static String projectId = testPipeline.getOptions().as(GcpOptions.class).getProject();
+  private static final String randomId =
+      "aitest-" + Instant.now().getEpochSecond() + UUID.randomUUID().toString();
 
-  private static GenericJson getCatalogItem() {
+  private final GenericJson getCatalogItem() {
     List<Object> categories = new ArrayList<Object>();
     categories.add(new GenericJson().set("categories", Arrays.asList("Electronics", "Computers")));
     categories.add(new GenericJson().set("categories", Arrays.asList("Laptops")));
@@ -61,7 +65,6 @@ public class RecommendationAICatalogItemIT {
   @Ignore("https://issues.apache.org/jira/browse/BEAM-12733")
   @Test
   public void createCatalogItem() {
-    String projectId = testPipeline.getOptions().as(GcpOptions.class).getProject();
     GenericJson catalogItem = getCatalogItem();
 
     PCollectionTuple createCatalogItemResult =
@@ -78,7 +81,6 @@ public class RecommendationAICatalogItemIT {
   @Ignore("Import method causing issues")
   @Test
   public void importCatalogItems() {
-    String projectId = testPipeline.getOptions().as(GcpOptions.class).getProject();
     ArrayList<KV<String, GenericJson>> catalogItems = new ArrayList<>();
 
     GenericJson catalogItem1 = getCatalogItem();
@@ -100,7 +102,9 @@ public class RecommendationAICatalogItemIT {
   public static void tearDownAfterClass() throws Exception {
     try (CatalogServiceClient catalogServiceClient = CatalogServiceClient.create()) {
       catalogServiceClient.deleteCatalogItem(
-          "projects/matthias-sandbox/locations/global/catalogs/default_catalog/catalogItems/"
+          "projects/"
+              + projectId
+              + "/locations/global/catalogs/default_catalog/catalogItems/"
               + randomId);
     }
   }
