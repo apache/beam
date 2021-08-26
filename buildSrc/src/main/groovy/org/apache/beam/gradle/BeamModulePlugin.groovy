ac23de49c1eb3c1061682293016d720f94fa5d9c
Daniel Chen <dchen1@linkedin.com>
2021-08-26 14:08:50 -0700

    Fix nosigning change

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -367,7 +367,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.28.0.4'
+    project.version = '2.28.0.5'
 
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
