33e4b0bb60116dd2c0cd9cf06f1fe02b99eb7713
Daniel Oliveira <daniel.o.programmer@gmail.com>
2021-08-29 20:32:08 -0700

    Fixup, skipping failing test with Flink

--- sdks/go/test/integration/integration.go
+++ sdks/go/test/integration/integration.go
@@ -79,6 +79,8 @@ var portableFilters = []string{
 var flinkFilters = []string{
 	// TODO(BEAM-11500): Flink tests timing out on reads.
 	"TestXLang_Combine.*",
+	// TODO(BEAM-12815): Test fails: "Insufficient number of network buffers".
+	"TestXLang_Multi",
 	// TODO(BEAM-12753): Flink test stream fails for non-string/byte slice inputs
 	"TestTestStream.*Sequence.*",
 	// Triggers are not yet supported
