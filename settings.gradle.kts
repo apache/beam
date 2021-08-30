09c7f989d08ca4ba0b9fd1f51fa133a4fada238a
zhoufek <zhoufek@google.com>
2021-08-30 16:06:57 -0400

    [BEAM-12697] Add module for SBE extension

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -226,3 +226,5 @@ include("beam-test-jenkins")
 project(":beam-test-jenkins").projectDir = file(".test-infra/jenkins")
 include("beam-validate-runner")
 project(":beam-validate-runner").projectDir = file(".test-infra/validate-runner")
+include("sdks:java:extensions:sbe")
+findProject(":sdks:java:extensions:sbe")?.name = "sbe"
