a84d9cb285c5d15eb34f4a3126015ae1f1da003f
Kyle Weaver <kcweaver@google.com>
2021-09-21 16:15:48 -0700

    run postbuild regardless of test result

--- .test-infra/jenkins/Flink.groovy
+++ .test-infra/jenkins/Flink.groovy
@@ -99,6 +99,7 @@ class Flink {
         buildSteps {
           postBuildStep {
             stopOnFailure(false)
+            results(['SUCCESS','UNSTABLE','FAILURE','NOT_BUILT','ABORTED'])
             buildSteps {
               shell {
                 command("cd ${FLINK_DIR}; ./${FLINK_SCRIPT} delete")
