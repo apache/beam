43866d5a1ac130cad7eb61ecaee7cf19d561042d
Aydar Farrakhov <stranniknm@gmail.com>
2021-10-04 18:14:38 +0300

    [BEAM-12964]: extract playground page providers

---
+++ playground/frontend/lib/pages/playground/components/playground_page_providers.dart
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:playground/modules/examples/repositories/example_repository.dart';
+import 'package:playground/pages/playground/states/examples_state.dart';
+import 'package:playground/pages/playground/states/playground_state.dart';
+
+class PlaygroundPageProviders extends StatelessWidget {
+  final Widget child;
+
+  const PlaygroundPageProviders({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ExampleState>(
+          create: (context) => ExampleState(ExampleRepository()),
+        ),
+        ChangeNotifierProxyProvider<ExampleState, PlaygroundState>(
+          create: (context) => PlaygroundState(),
+          update: (context, exampleState, playground) {
+            if (playground == null) {
+              return PlaygroundState();
+            }
+            if (exampleState.examples?.isNotEmpty ?? false) {
+              return PlaygroundState(
+                  playground.sdk, exampleState.examples!.first);
+            }
+            return playground;
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+}
