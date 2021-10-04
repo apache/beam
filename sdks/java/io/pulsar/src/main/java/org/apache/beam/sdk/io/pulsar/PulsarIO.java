96bb873e1dbce7aed36221ffcae74193ae6a7579
marco.robles <marco.robles@wizeline.com>
2021-10-04 16:25:58 -0500

    change offset to timestamp for restriction

--- sdks/java/io/pulsar/src/main/java/org/apache/beam/sdk/io/pulsar/PulsarIO.java
+++ sdks/java/io/pulsar/src/main/java/org/apache/beam/sdk/io/pulsar/PulsarIO.java
@@ -4,8 +4,6 @@ import org.apache.beam.sdk.io.range.OffsetRange;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.splittabledofn.GrowableOffsetRangeTracker;
 import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
-import org.apache.pulsar.client.admin.PulsarAdmin;
-import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.api.*;
 import org.apache.pulsar.client.util.MessageIdUtils;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Supplier;
@@ -21,15 +19,14 @@ public class PulsarIO<K, V> extends DoFn<PulsarSource, PulsarRecord<K,V>> {
     private PulsarClient client;
     private String topic;
     private List<String> topics;
-    private PulsarAdmin admin;
+    //private PulsarAdmin admin;
 
     private String clientUrl;
-    private String adminUrl;
+    //private String adminUrl;
 
 
-    public void setServiceUrl(String clientUrl, String adminUrl) throws PulsarClientException {
+    public void setServiceUrl(String clientUrl) {
        this.clientUrl = clientUrl;
-       this.adminUrl = adminUrl;
     }
 
     public void setTopic(String topic) {
@@ -40,15 +37,16 @@ public class PulsarIO<K, V> extends DoFn<PulsarSource, PulsarRecord<K,V>> {
         this.topics = topics;
     }
 
+    // Open connection to Pulsar clients
+    @Setup
     private void initPulsarClients() throws PulsarClientException {
-        if(this.adminUrl == null && this.clientUrl == null) {
-            this.adminUrl = PulsarIOUtils.SERVICE_HTTP_URL;
+        if(this.clientUrl == null) {
             this.clientUrl = PulsarIOUtils.SERVICE_URL;
         }
         this.client = PulsarClient.builder()
                 .serviceUrl(clientUrl)
                 .build();
-
+        /*
         //TODO fix auth for admin connection
         boolean tlsAllowInsecureConnection = false;
         String tlsTrustCertsFilePath = null;
@@ -58,18 +56,16 @@ public class PulsarIO<K, V> extends DoFn<PulsarSource, PulsarRecord<K,V>> {
                 .tlsTrustCertsFilePath(tlsTrustCertsFilePath)
                 .allowTlsInsecureConnection(tlsAllowInsecureConnection)
                 .build();
+
+         */
     }
 
     private void closePulsarClients() throws PulsarClientException {
-        this.admin.close();
+        //this.admin.close();
         this.client.close();
     }
 
-    // Open connection to Pulsar clients
-    @Setup
-    public void setup() throws Exception {
-        this.initPulsarClients();
-    }
+
     // Close connection to Pulsar clients
     @Teardown
     public void teardown() throws Exception {
@@ -94,15 +90,15 @@ public class PulsarIO<K, V> extends DoFn<PulsarSource, PulsarRecord<K,V>> {
      See DoFn.GetSize and RestrictionTracker.HasProgress for further details.
      */
     @GetSize
-    public double getSize(@Element PulsarSource pulsarSource, @Restriction OffsetRange offsetRange) {
+    public double getSize(@Element PulsarSource pulsarSource, @Restriction OffsetRange offsetRange) throws PulsarClientException {
         //TODO improve getsize estiamate, check pulsar stats to improve get size estimate
         // https://pulsar.apache.org/docs/en/admin-api-topics/#get-stats
         double estimateRecords = restrictionTracker(pulsarSource, offsetRange).getProgress().getWorkRemaining();
         return estimateRecords;
     }
 
-    private Reader<byte[]> newReader(PulsarClient client, MessageId startMessageId) throws PulsarClientException {
-        ReaderBuilder<byte[]> builder = client.newReader().topic(topic).startMessageId(startMessageId);
+    private Reader<byte[]> newReader(PulsarClient client, String topicPartition) throws PulsarClientException {
+        ReaderBuilder<byte[]> builder = client.newReader().topic(topicPartition).startMessageId(MessageId.earliest);
         return builder.create();
     }
 
@@ -112,73 +108,87 @@ public class PulsarIO<K, V> extends DoFn<PulsarSource, PulsarRecord<K,V>> {
             RestrictionTracker<OffsetRange, Long> tracker,
             OutputReceiver<PulsarRecord> output) throws IOException {
 
-        long startOffset = tracker.currentRestriction().getFrom();
+        long startTimestamp = tracker.currentRestriction().getFrom();
+        String topicPartition = pulsarRecord.getTopic();
         //long expectedOffset = startOffset;
-        MessageId startMessageId = (startOffset != 0) ?
-                        MessageIdUtils.getMessageId(startOffset) : MessageId.earliest;
 
-        //TODO: if topic is partitioned need to create n readers for n topic-partitions
-        try(Reader<byte[]> reader = newReader(client, startMessageId)) {
+        //List<String> topicPartitions = client.getPartitionsForTopic(pulsarRecord.getTopic()).join();
+        //for(String topicPartition:topicPartitions) {
+            try(Reader<byte[]> reader = newReader(client, topicPartition)) {
+                reader.seek(startTimestamp);
+                while (true) {
+                    Message<byte[]> message = reader.readNext();
+                    MessageId messageId = message.getMessageId();
+                    long currentOffset = MessageIdUtils.getOffset(messageId);
+                    // if tracker.tryclaim() return true, sdf must execute work otherwise
+                    // doFn must exit processElement() without doing any work associated
+                    // or claiming more work
+                    if (!tracker.tryClaim(currentOffset)) {
+                        return ProcessContinuation.stop();
+                    }
 
-            while (true) {
-                Message<byte[]> message = reader.readNext();
-                MessageId messageId = message.getMessageId();
-                long currentOffset = MessageIdUtils.getOffset(messageId);
-                // if tracker.tryclaim() return true, sdf must execute work otherwise
-                // doFn must exit processElement() without doing any work associated
-                // or claiming more work
-                if (!tracker.tryClaim(currentOffset)) {
-                    return ProcessContinuation.stop();
+                    PulsarRecord<K, V> newPulsarRecord =
+                            new PulsarRecord<K,V>(
+                                    message.getTopicName(),
+                                    message,
+                                    message.getSequenceId());
+
+                    //expectedOffset = currentOffset + 1;
+                    output.output(newPulsarRecord);
+                    reader.close();
                 }
-                PulsarRecord<K, V> newPulsarRecord =
-                        new PulsarRecord<K,V>(
-                                message.getTopicName(),
-                                message,
-                                message.getSequenceId());
-
-                //expectedOffset = currentOffset + 1;
-
-                output.output(newPulsarRecord);
             }
-        }
-
+        //}
     }
 
     @NewTracker
     public GrowableOffsetRangeTracker restrictionTracker(@Element PulsarSource pulsarSource,
-                                                         @Restriction OffsetRange restriction) {
+                                                         @Restriction OffsetRange restriction) throws PulsarClientException {
 
-        PulsarLatestOffsetEstimator offsetEstimator =  new PulsarLatestOffsetEstimator(admin, pulsarSource.getTopic());
+        PulsarLatestOffsetEstimator offsetEstimator =  new PulsarLatestOffsetEstimator(client, pulsarSource.getTopic());
         return new GrowableOffsetRangeTracker(restriction.getFrom(), offsetEstimator);
 
     }
 
     private static class PulsarLatestOffsetEstimator implements GrowableOffsetRangeTracker.RangeEndEstimator {
 
-        private final PulsarAdmin admin;
-        private final Supplier<MessageId> memoizedBacklog;
+        private final Supplier<Message> memoizedBacklog;
+        private final Reader readerLatestMsg;
 
-        private PulsarLatestOffsetEstimator(PulsarAdmin admin,
-                                            String topic) {
-            this.admin = admin;
+        private PulsarLatestOffsetEstimator(PulsarClient client, String topic) throws PulsarClientException {
+            this.readerLatestMsg = client.newReader()
+                                        .startMessageId(MessageId.latest)
+                                        .subscriptionName("PulsarLatestOffsetEstimatorReader")
+                                        .topic(topic)
+                                        .create();
             this.memoizedBacklog = Suppliers.memoizeWithExpiration(
                     () -> {
-                        MessageId latestMessageIdCommitted = null;
+                        Message<byte[]> latestMessage = null;
                         try {
-                            latestMessageIdCommitted =  this.admin.topics().getLastMessageId(topic);
-                        } catch (PulsarAdminException e) {
+                            latestMessage = readerLatestMsg.readNext();
+                        } catch (PulsarClientException e) {
                             //TODO change error log
                             e.printStackTrace();
                         }
-                        return latestMessageIdCommitted;
+                        return latestMessage;
                     }, 1,
                     TimeUnit.SECONDS);
         }
 
         @Override
+        protected void finalize() {
+            try {
+                this.readerLatestMsg.close();
+            } catch (IOException e) {
+                //TODO add error log
+                e.printStackTrace();
+            }
+        }
+
+        @Override
         public long estimate() {
-            long offset = MessageIdUtils.getOffset(memoizedBacklog.get());
-            return offset;
+            Message msg = memoizedBacklog.get();
+            return msg.getPublishTime();
         }
     }
 
