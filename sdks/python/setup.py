2b96eca56b4590eb0916d92e2c7edbe888b723f3
Vachan <52260220+vachan-shetty@users.noreply.github.com>
2021-10-06 00:47:10 -0400

    Merge pull request #15602 from [BEAM-10917] Add support for BigQuery Read API in Python BEAM
    * Enable reading from BigQuery using BQ Storage Read API.
    
    * Add error handling for temp dataset deletion and some lint fixes.
    
    * Adding some EXPORT source code that was mistakenly removed.
    
    * Remove table labelling and other minor fixes.
    
    * Adding the "method" of reading to display data.
    
    * Fix for failing pre-commit tests.
    
    * Adding query priority to ReadAPI source.
    
    * Doc fix.
    
    * Adding retries to Read API source query execution.
    
    * Use unique labels for temp datasets and remove retries for query execution in source.
    
    * Updating CHANGES.md.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -190,6 +190,7 @@ GCP_REQUIREMENTS = [
     'google-cloud-pubsub>=0.39.0,<2',
     # GCP packages required by tests
     'google-cloud-bigquery>=1.6.0,<3',
+    'google-cloud-bigquery-storage>=2.6.3',
     'google-cloud-core>=0.28.1,<2',
     'google-cloud-bigtable>=0.31.1,<2',
     'google-cloud-spanner>=1.13.0,<2',
