2ad9e3c3334eaea8783e84a767c8628aad92d8cd
Aydar Farrakhov <stranniknm@gmail.com>
2021-10-14 19:38:03 +0300

    Merge pull request #15722 from [BEAM-13022] [Playground] Run code on the backend
    * [BEAM-13022]: run code
    
    * [BEAM-13022]: remove empty lines
    
    * [BEAM-13022]: remove empty lines
    
    * [BEAM-13022]: add scala support

--- playground/frontend/lib/modules/editor/repository/code_repository/run_code_error.dart
+++ playground/frontend/lib/modules/editor/repository/code_repository/run_code_error.dart
@@ -16,25 +16,8 @@
  * limitations under the License.
  */
 
-// spacings
-const double kZeroSpacing = 0.0;
-const double kSmSpacing = 4.0;
-const double kMdSpacing = 8.0;
-const double kLgSpacing = 16.0;
+class RunCodeError implements Exception {
+  final String? message;
 
-// sizes
-const kHeaderButtonHeight = 46.0;
-const kRunButtonWidth = 150.0;
-const kRunButtonHeight = 40.0;
-const kIconButtonSplashRadius = 24.0;
-const kFooterHeight = 32.0;
-
-// border radius
-const double kBorderRadius = 8.0;
-
-// elevation
-const int kElevation = 1;
-
-// icon sizes
-const double kIconSizeSm = 16.0;
-const double kIconSizeMd = 24.0;
+  RunCodeError(this.message);
+}
