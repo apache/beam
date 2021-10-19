9d3a3e41e63dec3c1afdc0ee535806ceabf5adb9
Ilya Kozyrev <ilya.kozyrev@akvelon.com>
2021-10-19 12:40:51 +0300

    Implement initial server with grpcweb wrapper

---
+++ playground/backend/cmd/server/controller.go
@@ -0,0 +1,55 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	pb "beam.apache.org/playground/backend/internal/api"
+	"context"
+	"github.com/google/uuid"
+)
+
+type playgroundController struct {
+	pb.UnimplementedPlaygroundServiceServer
+}
+
+//RunCode is running code from requests using a particular SDK
+func (controller *playgroundController) RunCode(ctx context.Context, info *pb.RunCodeRequest) (*pb.RunCodeResponse, error) {
+	// TODO implement this method
+	pipelineInfo := pb.RunCodeResponse{PipelineUuid: uuid.NewString()}
+
+	return &pipelineInfo, nil
+}
+
+//CheckStatus is checking status for the specific pipeline by PipelineUuid
+func (controller *playgroundController) CheckStatus(ctx context.Context, info *pb.CheckStatusRequest) (*pb.CheckStatusResponse, error) {
+	// TODO implement this method
+	status := pb.CheckStatusResponse{Status: pb.Status_STATUS_FINISHED}
+	return &status, nil
+}
+
+//GetRunOutput is returning output of execution for specific pipeline by PipelineUuid
+func (controller *playgroundController) GetRunOutput(ctx context.Context, info *pb.GetRunOutputRequest) (*pb.GetRunOutputResponse, error) {
+	// TODO implement this method
+	pipelineResult := pb.GetRunOutputResponse{Output: "Test Pipeline Result"}
+
+	return &pipelineResult, nil
+}
+
+//GetCompileOutput is returning output of compilation for specific pipeline by PipelineUuid
+func (controller *playgroundController) GetCompileOutput(ctx context.Context, info *pb.GetCompileOutputRequest) (*pb.GetCompileOutputResponse, error) {
+	// TODO implement this method
+	compileOutput := pb.GetCompileOutputResponse{Output: "test compile output"}
+	return &compileOutput, nil
+}
