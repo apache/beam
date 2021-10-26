3bbcf5afbaa1da5c0878fc8535104adec3962051
AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>
2021-10-26 18:58:29 +0300

    [BEAM-12970][Playground] add setupCache() method; implement RunCode API method; change ExecutableName to a variable of LifeCycle instead of method;

--- playground/backend/cmd/server/controller_test.go
+++ playground/backend/cmd/server/controller_test.go
@@ -16,18 +16,33 @@ package main
 
 import (
 	pb "beam.apache.org/playground/backend/internal/api/v1"
+	"beam.apache.org/playground/backend/internal/cache"
+	"beam.apache.org/playground/backend/internal/cache/local"
+	"beam.apache.org/playground/backend/internal/environment"
+	"beam.apache.org/playground/backend/internal/executors"
+	"beam.apache.org/playground/backend/internal/fs_tool"
+	"beam.apache.org/playground/backend/internal/validators"
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
+	"io/fs"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
 )
 
-const bufSize = 1024 * 1024
+const (
+	bufSize    = 1024 * 1024
+	javaConfig = "{\n  \"compile_cmd\": \"javac\",\n  \"run_cmd\": \"java\",\n  \"compile_args\": [\"-d\", \"bin\", \"-classpath\"],\n  \"run_args\": [\"-cp\", \"bin:\"]\n}"
+)
 
 var lis *bufconn.Listener
+var cacheService cache.Cache
 
 func TestMain(m *testing.M) {
 	server := setup()
@@ -38,7 +53,44 @@ func TestMain(m *testing.M) {
 func setup() *grpc.Server {
 	lis = bufconn.Listen(bufSize)
 	s := grpc.NewServer()
-	pb.RegisterPlaygroundServiceServer(s, &playgroundController{})
+
+	// create configs for java
+	err := os.MkdirAll("configs", fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	filePath := filepath.Join("configs", pb.Sdk_SDK_JAVA.String()+".json")
+	err = os.WriteFile(filePath, []byte(javaConfig), 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	// setup cache
+	cacheService = local.New(context.Background())
+
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	os.Setenv("BEAM_SDK", pb.Sdk_SDK_JAVA.String())
+	os.Setenv("APP_WORK_DIR", path)
+
+	networkEnv, err := environment.GetNetworkEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	sdkEnv, err := environment.GetSdkEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	appEnv, err := environment.GetApplicationEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	pb.RegisterPlaygroundServiceServer(s, &playgroundController{
+		env:          environment.NewEnvironment(*networkEnv, *sdkEnv, *appEnv),
+		cacheService: cacheService,
+	})
 	go func() {
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("Server exited with error: %v", err)
@@ -49,28 +101,86 @@ func setup() *grpc.Server {
 
 func teardown(server *grpc.Server) {
 	server.Stop()
+
+	err := os.RemoveAll("configs")
+	if err != nil {
+		fmt.Errorf("error during test setup: %s", err.Error())
+	}
 }
 
 func bufDialer(context.Context, string) (net.Conn, error) {
 	return lis.Dial()
 }
+
 func TestPlaygroundController_RunCode(t *testing.T) {
-	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
-	if err != nil {
-		t.Fatalf("Failed to dial bufnet: %v", err)
+	type args struct {
+		ctx     context.Context
+		request *pb.RunCodeRequest
 	}
-	defer conn.Close()
-	client := pb.NewPlaygroundServiceClient(conn)
-	code := pb.RunCodeRequest{
-		Code: "test",
-		Sdk:  pb.Sdk_SDK_JAVA,
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus pb.Status
+		wantErr    bool
+	}{
+		{
+			name: "RunCode with incorrect sdk",
+			args: args{
+				ctx: context.Background(),
+				request: &pb.RunCodeRequest{
+					Code: "MOCK_CODE",
+					Sdk:  pb.Sdk_SDK_UNSPECIFIED,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "RunCode with correct sdk",
+			args: args{
+				ctx: context.Background(),
+				request: &pb.RunCodeRequest{
+					Code: "MOCK_CODE",
+					Sdk:  pb.Sdk_SDK_JAVA,
+				},
+			},
+			wantStatus: pb.Status_STATUS_EXECUTING,
+			wantErr:    false,
+		},
 	}
-	pipelineMeta, err := client.RunCode(ctx, &code)
-	if err != nil {
-		t.Fatalf("runCode failed: %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conn, err := grpc.DialContext(tt.args.ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+			defer conn.Close()
+			client := pb.NewPlaygroundServiceClient(conn)
+			response, err := client.RunCode(tt.args.ctx, tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PlaygroundController_RunCode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil {
+				if response == nil {
+					t.Errorf("PlaygroundController_RunCode() response shoudn't be nil")
+				} else {
+					if response.PipelineUuid == "" {
+						t.Errorf("PlaygroundController_RunCode() response.pipeLineId shoudn't be nil")
+					} else {
+						path := os.Getenv("APP_WORK_DIR") + "/executable_files"
+						os.RemoveAll(path)
+					}
+					status, _ := cacheService.GetValue(tt.args.ctx, uuid.MustParse(response.PipelineUuid), cache.Status)
+					if status == nil {
+						t.Errorf("PlaygroundController_RunCode() status shoudn't be nil")
+					}
+					if !reflect.DeepEqual(status, tt.wantStatus) {
+						t.Errorf("PlaygroundController_RunCode() status = %v, wantStatus %v", status, tt.wantStatus)
+					}
+
+				}
+			}
+		})
 	}
-	log.Printf("Response: %+v", pipelineMeta)
 }
 
 func TestPlaygroundController_CheckStatus(t *testing.T) {
@@ -125,4 +235,129 @@ func TestPlaygroundController_GetRunOutput(t *testing.T) {
 		t.Fatalf("runCode failed: %v", err)
 	}
 	log.Printf("Response: %+v", runOutput)
+}
+
+func Test_processCode(t *testing.T) {
+	pipelineId := uuid.New()
+	sdkEnv, err := environment.GetSdkEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	networkEnvs, err := environment.GetNetworkEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	appEnvs, err := environment.GetApplicationEnvsFromOsEnvs()
+	if err != nil {
+		panic(err)
+	}
+	env := environment.NewEnvironment(*networkEnvs, *sdkEnv, *appEnvs)
+
+	lc, _ := fs_tool.NewLifeCycle(pb.Sdk_SDK_JAVA, pipelineId, os.Getenv("APP_WORK_DIR"))
+	filePath := lc.GetAbsoluteExecutableFilePath()
+	workingDir := lc.GetAbsoluteExecutableFilesFolderPath()
+
+	exec := executors.NewExecutorBuilder().
+		WithValidator().
+		WithSdkValidators(validators.GetJavaValidators(filePath)).
+		WithCompiler().
+		WithCommand(sdkEnv.ExecutorConfig.CompileCmd).
+		WithArgs(sdkEnv.ExecutorConfig.CompileArgs).
+		WithFileName(filePath).
+		WithWorkingDir(workingDir)
+
+	type args struct {
+		ctx context.Context
+		env *environment.Environment
+	}
+	tests := []struct {
+		name                  string
+		createExecFile        bool
+		code                  string
+		expectedStatus        pb.Status
+		expectedRunOutput     interface{}
+		expectedCompileOutput interface{}
+		args                  args
+	}{
+		{
+			name:                  "validation failed",
+			createExecFile:        false,
+			code:                  "",
+			expectedStatus:        pb.Status_STATUS_ERROR,
+			expectedCompileOutput: nil,
+			expectedRunOutput:     nil,
+			args: args{
+				ctx: context.Background(),
+				env: env,
+			},
+		},
+		{
+			name:                  "compilation failed",
+			createExecFile:        true,
+			code:                  "MOCK_CODE",
+			expectedStatus:        pb.Status_STATUS_COMPILE_ERROR,
+			expectedCompileOutput: fmt.Sprintf("error: exit status 1, output: %s:1: error: reached end of file while parsing\nMOCK_CODE\n^\n1 error\n", lc.GetAbsoluteExecutableFilePath()),
+			expectedRunOutput:     nil,
+			args: args{
+				ctx: context.Background(),
+				env: env,
+			},
+		},
+		{
+			name:                  "run failed",
+			createExecFile:        true,
+			code:                  "class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(1/0);\n    }\n}",
+			expectedStatus:        pb.Status_STATUS_ERROR,
+			expectedCompileOutput: "",
+			expectedRunOutput:     fmt.Sprintf("error: exit status 1, output: Exception in thread \"main\" java.lang.ArithmeticException: / by zero\n\tat HelloWorld.main(%s.java:3)\n", pipelineId),
+			args: args{
+				ctx: context.Background(),
+				env: env,
+			},
+		},
+		{
+			name:                  "all success",
+			createExecFile:        true,
+			code:                  "class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(\"Hello world!\");\n    }\n}",
+			expectedStatus:        pb.Status_STATUS_FINISHED,
+			expectedCompileOutput: "",
+			expectedRunOutput:     "Hello world!\n",
+			args: args{
+				ctx: context.Background(),
+				env: env,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := lc.CreateFolders()
+			if err != nil {
+				t.Fatalf("error during prepare folders: %s", err.Error())
+			}
+			if tt.createExecFile {
+				_, _ = lc.CreateExecutableFile(tt.code)
+			}
+
+			processCode(tt.args.ctx, cacheService, lc, exec, pipelineId, tt.args.env)
+
+			status, _ := cacheService.GetValue(tt.args.ctx, pipelineId, cache.Status)
+			if !reflect.DeepEqual(status, tt.expectedStatus) {
+				t.Errorf("processCode() set status: %s, but expectes: %s", status, tt.expectedStatus)
+			}
+
+			compileOutput, _ := cacheService.GetValue(tt.args.ctx, pipelineId, cache.CompileOutput)
+			if !reflect.DeepEqual(compileOutput, tt.expectedCompileOutput) {
+				t.Errorf("processCode() set compileOutput: %s, but expectes: %s", compileOutput, tt.expectedCompileOutput)
+			}
+
+			runOutput, _ := cacheService.GetValue(tt.args.ctx, pipelineId, cache.RunOutput)
+			if !reflect.DeepEqual(runOutput, tt.expectedRunOutput) {
+				t.Errorf("processCode() set runOutput: %s, but expectes: %s", runOutput, tt.expectedRunOutput)
+			}
+
+			// remove
+			path := os.Getenv("APP_WORK_DIR") + "/executable_files"
+			os.RemoveAll(path)
+		})
+	}
 }
