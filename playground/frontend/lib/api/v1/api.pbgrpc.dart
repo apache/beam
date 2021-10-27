d7715e95c8e1c8e8f74d12a4ba11fe76a547b1bd
Aydar Farrakhov <stranniknm@gmail.com>
2021-10-27 15:56:57 +0300

    [BEAM-13071] fix generated files

--- playground/frontend/lib/api/v1/api.pbgrpc.dart
+++ playground/frontend/lib/api/v1/api.pbgrpc.dart
@@ -15,19 +15,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 ///
-//  Generated code. Do not modify.
-//  source: api/v1/api.proto
-//
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
-
 import 'dart:async' as $async;
-
 import 'dart:core' as $core;
 
 import 'package:grpc/service_api.dart' as $grpc;
+
 import 'api.pb.dart' as $0;
+
 export 'api.pb.dart';
 
 class PlaygroundServiceClient extends $grpc.Client {
--- playground/frontend/lib/api/v1/api.pbjson.dart
+++ playground/frontend/lib/api/v1/api.pbjson.dart
@@ -15,16 +15,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-///
-//  Generated code. Do not modify.
-//  source: api/v1/api.proto
-//
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
 
-import 'dart:core' as $core;
+///
 import 'dart:convert' as $convert;
+import 'dart:core' as $core;
 import 'dart:typed_data' as $typed_data;
+
 @$core.Deprecated('Use sdkDescriptor instead')
 const Sdk$json = const {
   '1': 'Sdk',
