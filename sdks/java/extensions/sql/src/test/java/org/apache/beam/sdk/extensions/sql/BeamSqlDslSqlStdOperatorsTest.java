5f2b3c7d74b0a8ceaa5428851cf488e0f818fc13
Brian Hulette <bhulette@google.com>
2021-10-28 09:10:34 -0700

    Switch to ExpressionChecker in testStringTest (better assertions)

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/BeamSqlDslSqlStdOperatorsTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/BeamSqlDslSqlStdOperatorsTest.java
@@ -721,37 +721,35 @@ public class BeamSqlDslSqlStdOperatorsTest extends BeamSqlBuiltinFunctionsIntegr
     @SqlOperatorTest(name = "||", kind = "OTHER"),
   })
   public void testStringFunctions() throws Exception {
-    SqlExpressionChecker checker =
-        new SqlExpressionChecker()
-            .addExpr("'hello' || ' world' = 'hello world'")
-            .addExpr("'hello' || '' = 'hello'")
-            .addExpr("'' || '' = ''")
-            .addExpr("CHAR_LENGTH('hello') = 5")
-            .addExpr("CHARACTER_LENGTH('hello') = 5")
-            .addExpr("INITCAP('hello world') = 'Hello World'")
-            .addExpr("LOWER('HELLO') = 'hello'")
-            .addExpr("POSITION('world' IN 'helloworld') = 6")
-            .addExpr("POSITION('world' IN 'helloworldworld' FROM 7) = 11")
-            .addExpr("POSITION('world' IN 'hello') = 0")
-            .addExpr("TRIM(' hello ') = 'hello'")
+    ExpressionChecker checker =
+        new ExpressionChecker()
+            .addExpr("'hello' || ' world'", "hello world")
+            .addExpr("'hello' || ''", "hello")
+            .addExpr("'' || ''", "")
+            .addExpr("CHAR_LENGTH('hello')", 5)
+            .addExpr("CHARACTER_LENGTH('hello')", 5)
+            .addExpr("INITCAP('hello world')", "Hello World")
+            .addExpr("LOWER('HELLO')", "hello")
+            .addExpr("POSITION('world' IN 'helloworld')", 6)
+            .addExpr("POSITION('world' IN 'helloworldworld' FROM 7)", 11)
+            .addExpr("POSITION('world' IN 'hello')", 0)
+            .addExpr("TRIM(' hello ')", "hello")
             // https://issues.apache.org/jira/browse/BEAM-4704
-            .addExpr("TRIM(LEADING 'eh' FROM 'hehe__hehe') = '__hehe'")
-            .addExpr("TRIM(TRAILING 'eh' FROM 'hehe__hehe') = 'hehe__'")
-            .addExpr("TRIM(BOTH 'eh' FROM 'hehe__hehe') = '__'")
-            .addExpr("TRIM(BOTH ' ' FROM ' hello ') = 'hello'")
-            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3) = 'w3resou3rce'")
-            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 4) = 'w3resou33rce'")
-            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 5) = 'w3resou3rce'")
-            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 7) = 'w3resouce'")
-            .addExpr("SUBSTRING('hello' FROM 2) = 'ello'")
-            .addExpr("SUBSTRING('hello' FROM -1) = 'o'")
-            .addExpr("SUBSTRING('hello' FROM 2 FOR 2) = 'el'")
-            .addExpr("SUBSTRING('hello' FROM 2 FOR 100) = 'ello'")
-            .addExpr("SUBSTRING('hello' FROM -3 for 2) = 'll'")
-            .addExpr("UPPER('hello') = 'HELLO'");
+            .addExpr("TRIM(LEADING 'eh' FROM 'hehe__hehe')", "__hehe")
+            .addExpr("TRIM(BOTH 'eh' FROM 'hehe__hehe')", "__")
+            .addExpr("TRIM(BOTH ' ' FROM ' hello ')", "hello")
+            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3)", "w3resou3rce")
+            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 4)", "w3resou33rce")
+            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 5)", "w3resou3rce")
+            .addExpr("OVERLAY('w3333333rce' PLACING 'resou' FROM 3 FOR 7)", "w3resouce")
+            .addExpr("SUBSTRING('hello' FROM 2)", "ello")
+            .addExpr("SUBSTRING('hello' FROM -1)", "o")
+            .addExpr("SUBSTRING('hello' FROM 2 FOR 2)", "el")
+            .addExpr("SUBSTRING('hello' FROM 2 FOR 100)", "ello")
+            .addExpr("SUBSTRING('hello' FROM -3 for 2)", "ll")
+            .addExpr("UPPER('hello')", "HELLO");
 
-    checker.check(pipeline);
-    pipeline.run();
+    checker.buildRunAndCheck();
   }
 
   @Test
