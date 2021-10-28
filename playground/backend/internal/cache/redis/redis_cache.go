afc5a637bd0e2e9193bea534b850ae2ff67c5158
AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>
2021-10-28 08:58:50 +0300

    [BEAM-13023][Playground] add using logger

--- playground/backend/internal/cache/redis/redis_cache.go
+++ playground/backend/internal/cache/redis/redis_cache.go
@@ -18,12 +18,12 @@ package redis
 import (
 	pb "beam.apache.org/playground/backend/internal/api/v1"
 	"beam.apache.org/playground/backend/internal/cache"
+	"beam.apache.org/playground/backend/internal/logger"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
-	"log"
 	"time"
 )
 
@@ -37,7 +37,7 @@ func New(ctx context.Context, addr string) (*Cache, error) {
 	rc := Cache{redis.NewClient(&redis.Options{Addr: addr})}
 	_, err := rc.Ping(ctx).Result()
 	if err != nil {
-		log.Printf("Redis Cache: connect to Redis: error during Ping operation, err: %s\n", err.Error())
+		logger.Errorf("Redis Cache: connect to Redis: error during Ping operation, err: %s\n", err.Error())
 		return nil, err
 	}
 	return &rc, nil
@@ -46,12 +46,12 @@ func New(ctx context.Context, addr string) (*Cache, error) {
 func (rc *Cache) GetValue(ctx context.Context, pipelineId uuid.UUID, subKey cache.SubKey) (interface{}, error) {
 	marshSubKey, err := json.Marshal(subKey)
 	if err != nil {
-		log.Printf("Redis Cache: get value: error during marshal subKey: %s, err: %s\n", subKey, err.Error())
+		logger.Errorf("Redis Cache: get value: error during marshal subKey: %s, err: %s\n", subKey, err.Error())
 		return nil, err
 	}
 	value, err := rc.HGet(ctx, pipelineId.String(), string(marshSubKey)).Result()
 	if err != nil {
-		log.Printf("Redis Cache: get value: error during HGet operation for key: %s, subKey: %s, err: %s\n", pipelineId.String(), subKey, err.Error())
+		logger.Errorf("Redis Cache: get value: error during HGet operation for key: %s, subKey: %s, err: %s\n", pipelineId.String(), subKey, err.Error())
 		return nil, err
 	}
 
@@ -61,17 +61,17 @@ func (rc *Cache) GetValue(ctx context.Context, pipelineId uuid.UUID, subKey cach
 func (rc *Cache) SetValue(ctx context.Context, pipelineId uuid.UUID, subKey cache.SubKey, value interface{}) error {
 	subKeyMarsh, err := json.Marshal(subKey)
 	if err != nil {
-		log.Printf("Redis Cache: set value: error during marshal subKey: %s, err: %s\n", subKey, err.Error())
+		logger.Errorf("Redis Cache: set value: error during marshal subKey: %s, err: %s\n", subKey, err.Error())
 		return err
 	}
 	valueMarsh, err := json.Marshal(value)
 	if err != nil {
-		log.Printf("Redis Cache: set value: error during marshal value: %s, err: %s\n", value, err.Error())
+		logger.Errorf("Redis Cache: set value: error during marshal value: %s, err: %s\n", value, err.Error())
 		return err
 	}
 	_, err = rc.HSet(ctx, pipelineId.String(), subKeyMarsh, valueMarsh).Result()
 	if err != nil {
-		log.Printf("Redis Cache: set value: error during HSet operation, err: %s\n", err.Error())
+		logger.Errorf("Redis Cache: set value: error during HSet operation, err: %s\n", err.Error())
 		return err
 	}
 	return nil
@@ -80,17 +80,17 @@ func (rc *Cache) SetValue(ctx context.Context, pipelineId uuid.UUID, subKey cach
 func (rc *Cache) SetExpTime(ctx context.Context, pipelineId uuid.UUID, expTime time.Duration) error {
 	exists, err := rc.Exists(ctx, pipelineId.String()).Result()
 	if err != nil {
-		log.Printf("Redis Cache: set expiration time value: error during Exists operation for key: %s, err: %s\n", pipelineId, err.Error())
+		logger.Errorf("Redis Cache: set expiration time value: error during Exists operation for key: %s, err: %s\n", pipelineId, err.Error())
 		return err
 	}
 	if exists == 0 {
-		log.Printf("Redis Cache: set expiration time value: key doesn't exist, key: %s\n", pipelineId)
+		logger.Errorf("Redis Cache: set expiration time value: key doesn't exist, key: %s\n", pipelineId)
 		return fmt.Errorf("key: %s doesn't exist", pipelineId)
 	}
 
 	_, err = rc.Expire(ctx, pipelineId.String(), expTime).Result()
 	if err != nil {
-		log.Printf("Redis Cache: set expiration time value: error during Expire operation for key: %s, err: %s\n", pipelineId, err.Error())
+		logger.Errorf("Redis Cache: set expiration time value: error during Expire operation for key: %s, err: %s\n", pipelineId, err.Error())
 		return err
 	}
 	return nil
@@ -107,7 +107,7 @@ func unmarshalBySubKey(subKey cache.SubKey, value string) (result interface{}, e
 		err = json.Unmarshal([]byte(value), &result)
 	}
 	if err != nil {
-		log.Printf("Redis Cache: get value: error during unmarshal value, err: %s\n", err.Error())
+		logger.Errorf("Redis Cache: get value: error during unmarshal value, err: %s\n", err.Error())
 	}
 	return
 }
