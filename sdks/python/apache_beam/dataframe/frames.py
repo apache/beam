0eb777d0116dc1504b887f4e39e83753fdbae808
svetakvsundhar <svetak.sundhar@gmail.com>
2021-10-30 16:58:39 -0400

    [BEAM-12550] Implement Parallelizable Skew and Kurtosis (Kurtosis implementation)

--- sdks/python/apache_beam/dataframe/frames.py
+++ sdks/python/apache_beam/dataframe/frames.py
@@ -1435,131 +1435,80 @@ class DeferredSeries(DeferredDataFrameOrSeries):
   @frame_base.with_docs_from(pd.Series)
   @frame_base.args_to_kwargs(pd.Series)
   @frame_base.populate_defaults(pd.Series)
-  def skew(self, axis, skipna, level, numeric_only, **kwargs):
-      if level is not None:
-          raise NotImplementedError("per-level aggregation")
-      if skipna is None or skipna:
-          self = self.dropna()  # pylint: disable=self-cls-assignment
-      # See the online, numerically stable formulae at
-      # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Higher-order_statistics
-      def compute_moments(x):
-          n = len(x)
-          if n == 0:
-              m, s, third_moment = 0, 0, 0
-          elif n < 3:
-              m = x.std(ddof=0)**2 * n
-              s = x.sum()
-              third_moment = (((x-x.mean())**3).sum())
-          else:
-              m = x.std(ddof=0) ** 2 * n
-              s = x.sum()
-              third_moment = (((x-x.mean())**3).sum())
-          return pd.DataFrame(dict(m=[m], s=[s], n=[n], third_moment=[third_moment]))
-
-      def combine_moments(data):
-          m = s = n = third_moment = 0.0
-          for datum in data.itertuples():
-            if datum.n == 0:
-                continue
-            elif n == 0:
-                m, s, n, third_moment = datum.m, datum.s, datum.n, datum.third_moment
-            else:
-                mean_b = s / n
-                mean_a = datum.s / datum.n
-                delta = mean_b - mean_a
-                n_a = datum.n
-                n_b = n
-                combined_n = n + datum.n
-                third_moment += datum.third_moment + ((delta ** 3 * ((n_a * n_b) * (n_a - n_b)) / ((combined_n) ** 2)) +
-                                                      ((3 * delta) * ((n_a * m) - (n_b * datum.m)) / (combined_n)))
-                m += datum.m + delta ** 2 * n * datum.n / (n + datum.n)
-                s += datum.s
-                n += datum.n
-
-          if n < 3:
-              return float('nan')
-          elif m == 0:
-              return float(0)
-          else:
-              return combined_n*math.sqrt(combined_n-1)/(combined_n-2)*third_moment/(m**(3/2))
-
-      moments = expressions.ComputedExpression(
-          'compute_moments',
-          compute_moments, [self._expr],
-          requires_partition_by=partitionings.Arbitrary())
-      with expressions.allow_non_parallel_operations(True):
-          return frame_base.DeferredFrame.wrap(
-              expressions.ComputedExpression(
-                  'combine_moments',
-                  combine_moments, [moments],
-                  requires_partition_by=partitionings.Singleton()))
-
-  @frame_base.with_docs_from(pd.Series)
-  @frame_base.args_to_kwargs(pd.Series)
-  @frame_base.populate_defaults(pd.Series)
   def kurtosis(self, axis, skipna, level, numeric_only, **kwargs):
-      if level is not None:
-          raise NotImplementedError("per-level aggregation")
-      if skipna is None or skipna:
-          self = self.dropna()  # pylint: disable=self-cls-assignment
+    if level is not None:
+      raise NotImplementedError("per-level aggregation")
+    if skipna is None or skipna:
+      self = self.dropna()  # pylint: disable=self-cls-assignment
 
-      # See the online, numerically stable formulae at
-      # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Higher-order_statistics
-      def compute_moments(x):
-          n = len(x)
-          if n == 0:
-              m, s, third_moment, fourth_moment = 0, 0, 0, 0
-          else:
-              m = x.std(ddof=0) ** 2 * n
-              s = x.sum()
-              third_moment = (((x-x.mean())**3).sum())
-              fourth_moment = (((x-x.mean())**4).sum())
-          return pd.DataFrame(dict(m=[m], s=[s], n=[n], third_moment=[third_moment], fourth_moment=[fourth_moment]))
+    # See the online, numerically stable formulae at
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Higher-order_statistics
+    def compute_moments(x):
+      n = len(x)
+      if n == 0:
+        m, s, third_moment, fourth_moment = 0, 0, 0, 0
+      else:
+        m = x.std(ddof=0)**2 * n
+        s = x.sum()
+        third_moment = (((x - x.mean())**3).sum())
+        fourth_moment = (((x - x.mean())**4).sum())
+      return pd.DataFrame(
+          dict(
+              m=[m],
+              s=[s],
+              n=[n],
+              third_moment=[third_moment],
+              fourth_moment=[fourth_moment]))
 
-      def combine_moments(data):
-          m = s = n = third_moment = fourth_moment = 0.0
-          for datum in data.itertuples():
-              if datum.n == 0:
-                  continue
-              elif n == 0:
-                  m, s, n, third_moment, fourth_moment = datum.m, datum.s, datum.n, datum.third_moment, datum.fourth_moment
-              else:
-                  mean_b = s / n
-                  mean_a = datum.s / datum.n
-                  delta = mean_b - mean_a
-                  n_a = datum.n
-                  n_b = n
-                  combined_n = n + datum.n
-                  fourth_moment += datum.fourth_moment + (
-                              (delta ** 4) * (n_a * n_b) * (n_a ** 2 - (n_a * n_b) + n_b ** 2) / combined_n ** 3) + (
-                                               (6 * delta ** 2) * (
-                                                   (n_a ** 2 * m) + (n_b ** 2 * datum.m)) / combined_n ** 2) + (
-                                               (4 * delta) * (
-                                                   (n_a * third_moment) - (n_b * datum.third_moment)) / combined_n)
-                  third_moment += datum.third_moment + (
-                          (delta ** 3 * ((n_a * n_b) * (n_a - n_b)) / ((combined_n) ** 2)) +
-                          ((3 * delta) * ((n_a * m) - (n_b * datum.m)) / (combined_n)))
-                  m += datum.m + delta ** 2 * n * datum.n / (n + datum.n)
-                  s += datum.s
-                  n += datum.n
+    def combine_moments(data):
+      m = s = n = third_moment = fourth_moment = 0.0
+      for datum in data.itertuples():
+        if datum.n == 0:
+          continue
+        elif n == 0:
+          m, s, n, third_moment, fourth_moment = datum.m, datum.s, datum.n, datum.third_moment, datum.fourth_moment
+        else:
+          mean_b = s / n
+          mean_a = datum.s / datum.n
+          delta = mean_b - mean_a
+          n_a = datum.n
+          n_b = n
+          combined_n = n + datum.n
+          fourth_moment += datum.fourth_moment + (
+              (delta**4) * (n_a * n_b) *
+              (n_a**2 -
+               (n_a * n_b) + n_b**2) / combined_n**3) + ((6 * delta**2) * (
+                   (n_a**2 * m) + (n_b**2 * datum.m)) / combined_n**2) + (
+                       (4 * delta) * ((n_a * third_moment) -
+                                      (n_b * datum.third_moment)) / combined_n)
+          third_moment += datum.third_moment + (
+              (delta**3 * ((n_a * n_b) * (n_a - n_b)) / ((combined_n)**2)) +
+              ((3 * delta) * ((n_a * m) - (n_b * datum.m)) / (combined_n)))
+          m += datum.m + delta**2 * n * datum.n / (n + datum.n)
+          s += datum.s
+          n += datum.n
 
-          if n < 4:
-              return float('nan')
-          elif m == 0:
-              return float(0)
-          else:
-              return (((combined_n+1)*(combined_n)*(combined_n-1))/((combined_n-2)*(combined_n-3)))*(fourth_moment/(np.floor(m)**2))-((3*(combined_n-1)**2)/((combined_n-2)*(combined_n-3)))
+      if n < 4:
+        return float('nan')
+      elif m == 0:
+        return float(0)
+      else:
+        return (((combined_n + 1) * (combined_n) * (combined_n - 1)) /
+                ((combined_n - 2) *
+                 (combined_n - 3))) * (fourth_moment / (np.floor(m)**2)) - (
+                     (3 * (combined_n - 1)**2) / ((combined_n - 2) *
+                                                  (combined_n - 3)))
 
-      moments = expressions.ComputedExpression(
-          'compute_moments',
-          compute_moments, [self._expr],
-          requires_partition_by=partitionings.Arbitrary())
-      with expressions.allow_non_parallel_operations(True):
-          return frame_base.DeferredFrame.wrap(
-              expressions.ComputedExpression(
-                  'combine_moments',
-                  combine_moments, [moments],
-                  requires_partition_by=partitionings.Singleton()))
+    moments = expressions.ComputedExpression(
+        'compute_moments',
+        compute_moments, [self._expr],
+        requires_partition_by=partitionings.Arbitrary())
+    with expressions.allow_non_parallel_operations(True):
+      return frame_base.DeferredFrame.wrap(
+          expressions.ComputedExpression(
+              'combine_moments',
+              combine_moments, [moments],
+              requires_partition_by=partitionings.Singleton()))
 
   def _corr_aligned(self, other, min_periods):
     std_x = self.std()
@@ -1823,9 +1772,8 @@ class DeferredSeries(DeferredDataFrameOrSeries):
   median = _agg_method(pd.Series, 'median')
   sem = _agg_method(pd.Series, 'sem')
   mad = _agg_method(pd.Series, 'mad')
-  #skew = _agg_method(pd.Series, 'skew')
+  skew = _agg_method(pd.Series, 'skew')
   kurt = _agg_method(pd.Series, 'kurt')
-  #kurtosis = _agg_method(pd.Series, 'kurtosis')
 
   argmax = frame_base.wont_implement_method(
       pd.Series, 'argmax', reason='order-sensitive')
--- sdks/python/apache_beam/dataframe/frames_test.py
+++ sdks/python/apache_beam/dataframe/frames_test.py
@@ -720,7 +720,6 @@ class DeferredFrameTest(_AbstractFrameTest):
       self._run_test(lambda s: s.corr(s + 1), s)
       self._run_test(lambda s: s.corr(s * s), s)
       self._run_test(lambda s: s.cov(s * s), s)
-      self._run_test(lambda s: s.skew(), s)
       self._run_test(lambda s: s.kurtosis(), s)
 
   def test_dataframe_cov_corr(self):
@@ -1393,15 +1392,7 @@ class AggregationTest(_AbstractFrameTest):
     s = pd.Series(list(range(16)))
 
     nonparallel = agg_method in (
-        'quantile',
-        'mean',
-        'describe',
-        'median',
-        'sem',
-        'mad',
-        'skew',
-        'kurtosis',
-        'kurt')
+        'quantile', 'mean', 'describe', 'median', 'sem', 'mad', 'skew', 'kurt')
 
     # TODO(BEAM-12379): max and min produce the wrong proxy
     check_proxy = agg_method not in ('max', 'min')
@@ -1420,15 +1411,7 @@ class AggregationTest(_AbstractFrameTest):
     s = pd.Series(list(range(16)))
 
     nonparallel = agg_method in (
-        'quantile',
-        'mean',
-        'describe',
-        'median',
-        'sem',
-        'mad',
-        'skew',
-        'kurtosis',
-        'kurt')
+        'quantile', 'mean', 'describe', 'median', 'sem', 'mad', 'skew', 'kurt')
 
     # TODO(BEAM-12379): max and min produce the wrong proxy
     check_proxy = agg_method not in ('max', 'min')
@@ -1444,15 +1427,7 @@ class AggregationTest(_AbstractFrameTest):
     df = pd.DataFrame({'A': [1, 2, 3, 4], 'B': [2, 3, 5, 7]})
 
     nonparallel = agg_method in (
-        'quantile',
-        'mean',
-        'describe',
-        'median',
-        'sem',
-        'mad',
-        'skew',
-        'kurtosis',
-        'kurt')
+        'quantile', 'mean', 'describe', 'median', 'sem', 'mad', 'skew', 'kurt')
 
     # TODO(BEAM-12379): max and min produce the wrong proxy
     check_proxy = agg_method not in ('max', 'min')
@@ -1469,15 +1444,7 @@ class AggregationTest(_AbstractFrameTest):
     df = pd.DataFrame({'A': [1, 2, 3, 4], 'B': [2, 3, 5, 7]})
 
     nonparallel = agg_method in (
-        'quantile',
-        'mean',
-        'describe',
-        'median',
-        'sem',
-        'mad',
-        'skew',
-        'kurtosis',
-        'kurt')
+        'quantile', 'mean', 'describe', 'median', 'sem', 'mad', 'skew', 'kurt')
 
     # TODO(BEAM-12379): max and min produce the wrong proxy
     check_proxy = agg_method not in ('max', 'min')
