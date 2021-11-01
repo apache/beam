dbade066b16371f28bccbd2d0f2daaaea8384c11
Robert Bradshaw <robertwb@gmail.com>
2021-11-01 10:07:26 -0700

    Allow wildcards for java class lookup transform providers.
    This is especially useful for cases where the expansion service is implicitly
    started by the user for ad-hoc invocation of cross-language transforms (in which
    case there is no security benefit to an explicit allow list).
    
    Also automatically starts up an expansion service for Python's
    JavaExternalTransform and adds the ability to manually specify a classpath.

--- sdks/java/expansion-service/src/main/java/org/apache/beam/sdk/expansion/service/ExpansionServiceOptions.java
+++ sdks/java/expansion-service/src/main/java/org/apache/beam/sdk/expansion/service/ExpansionServiceOptions.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import org.apache.beam.sdk.expansion.service.JavaClassLookupTransformProvider.AllowList;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.DefaultValueFactory;
@@ -53,6 +52,9 @@ public interface ExpansionServiceOptions extends PipelineOptions {
       String allowListFile =
           options.as(ExpansionServiceOptions.class).getJavaClassLookupAllowlistFile();
       if (allowListFile != null) {
+        if (allowListFile.equals("*")) {
+          return AllowList.everything();
+        }
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         File allowListFileObj = new File(allowListFile);
         if (!allowListFileObj.exists()) {
@@ -68,8 +70,7 @@ public interface ExpansionServiceOptions extends PipelineOptions {
       }
 
       // By default produces an empty allow-list.
-      return new AutoValue_JavaClassLookupTransformProvider_AllowList(
-          JavaClassLookupTransformProvider.ALLOW_LIST_VERSION, new ArrayList<>());
+      return AllowList.nothing();
     }
   }
 }
