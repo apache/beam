eb0461b926c07d40cb53547d454d49f4d7e649e6
Aydar Zainutdinov <aydar.zaynutdinov@akvelon.com>
2021-11-03 22:11:14 +0300

    Merge pull request #15839 from [BEAM-13041][Playground] Prepare files with java code
    * [BEAM-13041][Playground]
    Add Prepare step as one of the executor's steps;
    Add Java preparation methods to remove "package ..." string from file;
    Add Java preparation methods to change "public class" strings to "class" string into file;
    
    * [BEAM-13041][Playground]
    minor fix
    
    * [BEAM-13041][Playground]
    add using of logger;
    change the package pattern;

--- playground/backend/internal/executors/executor.go
+++ playground/backend/internal/executors/executor.go
@@ -17,6 +17,7 @@
 package executors
 
 import (
+	"beam.apache.org/playground/backend/internal/preparators"
 	"beam.apache.org/playground/backend/internal/validators"
 	"os/exec"
 )
@@ -34,13 +35,27 @@ type Executor struct {
 	compileArgs CmdConfiguration
 	runArgs     CmdConfiguration
 	validators  []validators.Validator
+	preparators []preparators.Preparator
 }
 
-// Validate return the function that apply all validators of executor
+// Validate returns the function that applies all validators of executor
 func (ex *Executor) Validate() func() error {
 	return func() error {
 		for _, validator := range ex.validators {
 			err := validator.Validator(validator.Args...)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+// Prepare returns the function that applies all preparations of executor
+func (ex *Executor) Prepare() func() error {
+	return func() error {
+		for _, preparator := range ex.preparators {
+			err := preparator.Prepare(preparator.Args...)
 			if err != nil {
 				return err
 			}
