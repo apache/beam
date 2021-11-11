ee19e4aed232d87db8bbd2221ac28c9f14b14179
Ritesh Ghorse <riteshghorse@gmail.com>
2021-11-11 12:54:53 -0500

    [BEAM-13228] fix data race in metrics.store

--- sdks/go/pkg/beam/core/metrics/store.go
+++ sdks/go/pkg/beam/core/metrics/store.go
@@ -128,9 +128,11 @@ func (e Extractor) ExtractFrom(store *Store) error {
 		}
 	}
 	if e.MsecsInt64 != nil {
+		store.mu.Lock()
 		for l, es := range store.stateRegistry {
 			e.MsecsInt64(l, es)
 		}
+		store.mu.Unlock()
 	}
 	return nil
 }
