fdedc787dd345d7c2fe32facc4a4765ed8d97208
tvalentyn <tvalentyn@users.noreply.github.com>
2021-11-16 10:45:34 -0800

    Generate Python container dependencies in an automated way. (#15927)

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -166,7 +166,7 @@ REQUIRED_TEST_PACKAGES = [
     'pandas<2.0.0',
     'parameterized>=0.7.1,<0.8.0',
     'pyhamcrest>=1.9,!=1.10.0,<2.0.0',
-    'pyyaml>=3.12,<6.0.0',
+    'pyyaml>=3.12,<7.0.0',
     'requests_mock>=1.7,<2.0',
     'tenacity>=5.0.2,<6.0',
     'pytest>=4.4.0,<5.0',
