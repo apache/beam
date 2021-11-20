b6bc6424c68fc173c9f06f5a12cdabf43dd8801d
Alexandr Zhuravlev <alexander.zhuravlev@akvelon.com>
2021-11-20 04:05:28 +0400

    Merge pull request #15768 from [BEAM-13064] [Playground] Implement search & filter logic
    * [BEAM-12967]: Created Examples Component
    
    * [BEAM-12967]: Wrapped CategoryBubble and ExpansionPanelItem in MouseRegion
    
    * [BEAM-13064]: Implemented Search field and Filter by type components logic
    
    * [BEAM-13064]: Fixed names to clarify code, refactored Category model (added const keyword)
    
    * [BEAM-13064]: Increased test coverage of Example Dropdown module
    
    * [BEAM-12967] Added generated files
    
    * [BEAM-12967]: Added license to generated files
    
    * [BEAM-13064]: Fixed sorting functionality, updated tests & mocks
    
    * [BEAM-13064]: Refactored code
    
    * [BEAM-12967]: Deleted duplicate files
    
    * [BEAM-13064] Fixes after merge
    
    * [Playground][BEAM-12941][Bugfix] Fix workflows for playground applications (#83)
    
    * Update workflows for playground
    
    * Attempt to fix tests
    
    * Remove continue on error to catch errors
    
    * Fix linter problem for backend dockerfile
    
    * Update folder to run backend go linter
    
    * Moved flutter test to execution via gradle tasks
    
    * Revert "[Playground][BEAM-12941][Bugfix] Fix workflows for playground applications (#83)" (#88)
    
    This reverts commit b73f5f70ac4184b56a0a03922731c5f2f69b9566.
    
    * [BEAM-13064] Fixed analyze issues
    
    * [BEAM-13064] Fixed example selector state test
    
    Co-authored-by: Sergey Kalinin <91209855+snkalinin@users.noreply.github.com>
    Co-authored-by: Ilya <ilya.kozyrev@akvelon.com>
    Co-authored-by: Aydar Zainutdinov <aydar.zaynutdinov@akvelon.com>
    Co-authored-by: daria.malkova <daria.malkova@akvelon.com>

---
+++ playground/frontend/test/pages/playground/states/mocks/categories_mock.dart
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'package:playground/modules/examples/models/category_model.dart';
+import 'package:playground/modules/examples/models/example_model.dart';
+import 'package:playground/modules/sdk/models/sdk.dart';
+
+const javaHelloWorld = '''class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}''';
+
+const pythonHelloWorld = 'print(‘Hello World’)';
+
+const goHelloWorld = '''package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+  fmt.Println("Hello World")
+}''';
+
+const scioHelloWorld = ''' 
+object Hello {
+    def main(args: Array[String]) = {
+        println("Hello, world")
+    }
+}''';
+
+const List<CategoryModel> sortedCategories = [
+  CategoryModel('Side Inputs', [
+    ExampleModel(
+      {
+        SDK.java: javaHelloWorld,
+        SDK.go: goHelloWorld,
+        SDK.python: pythonHelloWorld,
+        SDK.scio: scioHelloWorld,
+      },
+      'HelloWorld',
+      ExampleType.example,
+    ),
+  ])
+];
+
+const List<ExampleModel> unsortedExamples = [
+  ExampleModel(
+    {
+      SDK.java: javaHelloWorld,
+      SDK.go: goHelloWorld,
+      SDK.python: pythonHelloWorld,
+      SDK.scio: scioHelloWorld,
+    },
+    'HelloWorld',
+    ExampleType.example,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 1',
+      SDK.go: 'GO  Source code 1',
+      SDK.python: 'PYTHON  Source code 1',
+      SDK.scio: 'SCIO  Source code 1',
+    },
+    'KATA Source code 1',
+    ExampleType.kata,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 2',
+      SDK.go: 'GO  Source code 2',
+      SDK.python: 'PYTHON  Source code 2',
+      SDK.scio: 'SCIO  Source code 2',
+    },
+    'UNIT TEST Source code 2',
+    ExampleType.test,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 3',
+      SDK.go: 'GO  Source code 3',
+      SDK.python: 'PYTHON  Source code 3',
+      SDK.scio: 'SCIO  Source code 3',
+    },
+    'EXAMPLE Source code 3',
+    ExampleType.example,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 4',
+      SDK.go: 'GO  Source code 4',
+      SDK.python: 'PYTHON  Source code 4',
+      SDK.scio: 'SCIO  Source code 4',
+    },
+    'KATA Source code 4',
+    ExampleType.kata,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 5',
+      SDK.go: 'GO  Source code 5',
+      SDK.python: 'PYTHON  Source code 5',
+      SDK.scio: 'SCIO  Source code 5',
+    },
+    'UNIT TEST Source code 5',
+    ExampleType.test,
+  ),
+];
+
+const List<ExampleModel> examplesSortedByTypeMock = [
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 1',
+      SDK.go: 'GO  Source code 1',
+      SDK.python: 'PYTHON  Source code 1',
+      SDK.scio: 'SCIO  Source code 1',
+    },
+    'KATA Source code 1',
+    ExampleType.kata,
+  ),
+  ExampleModel(
+    {
+      SDK.java: 'JAVA Source code 4',
+      SDK.go: 'GO  Source code 4',
+      SDK.python: 'PYTHON  Source code 4',
+      SDK.scio: 'SCIO  Source code 4',
+    },
+    'KATA Source code 4',
+    ExampleType.kata,
+  ),
+];
+
+const List<ExampleModel> examplesSortedByNameMock = [
+    ExampleModel(
+      {
+        SDK.java: javaHelloWorld,
+        SDK.go: goHelloWorld,
+        SDK.python: pythonHelloWorld,
+        SDK.scio: scioHelloWorld,
+      },
+      'HelloWorld',
+      ExampleType.example,
+    ),
+];
