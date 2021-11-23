cc6e6bacf635ad6f98c7ec965800fa152925cdcd
Aydar Zainutdinov <aydar.zaynutdinov@akvelon.com>
2021-11-23 06:22:27 +0300

    Merge pull request #15996 from [BEAM-13156][Playground] Implement streaming output for the run code logs
    * [Playground][BEAM-12941][Bugfix] Fix workflows for playground applications (#83)
    
    * Update workflows for playground
    
    * Attempt to fix tests
    
    * Remove continue on error to catch errors
    
    * Fix linter problem for backend dockerfile
    
    * Update folder to run backend go linter
    
    * Moved flutter test to execution via gradle tasks
    
    * Revert "[Playground][BEAM-12941][Bugfix] Fix workflows for playground applications (#83)" (#88)
    
    This reverts commit b73f5f70ac4184b56a0a03922731c5f2f69b9566.
    
    * [BEAM-13156][Playground]
    implementation of streaming output for run code output
    
    Co-authored-by: Sergey Kalinin <91209855+snkalinin@users.noreply.github.com>
    Co-authored-by: Ilya <ilya.kozyrev@akvelon.com>

--- playground/backend/internal/code_processing/code_processing_test.go
+++ playground/backend/internal/code_processing/code_processing_test.go
@@ -445,3 +445,82 @@ func TestGetProcessingStatus(t *testing.T) {
 		})
 	}
 }
+
+func TestGetRunOutputLastIndex(t *testing.T) {
+	defer goleak.VerifyNone(t, opt)
+	pipelineId := uuid.New()
+	incorrectConvertPipelineId := uuid.New()
+	err := cacheService.SetValue(context.Background(), pipelineId, cache.RunOutputIndex, 2)
+	if err != nil {
+		panic(err)
+	}
+	err = cacheService.SetValue(context.Background(), incorrectConvertPipelineId, cache.RunOutputIndex, "MOCK_LAST_INDEX")
+	if err != nil {
+		panic(err)
+	}
+
+	type args struct {
+		ctx          context.Context
+		cacheService cache.Cache
+		key          uuid.UUID
+		errorTitle   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			// Test case with calling GetRunOutputLastIndex with pipelineId which doesn't contain status.
+			// As a result, want to receive an error.
+			name: "get last index with incorrect pipelineId",
+			args: args{
+				ctx:          context.Background(),
+				cacheService: cacheService,
+				key:          uuid.New(),
+				errorTitle:   "",
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			// Test case with calling GetRunOutputLastIndex with pipelineId which contains incorrect status value in cache.
+			// As a result, want to receive an error.
+			name: "get last index with incorrect cache value",
+			args: args{
+				ctx:          context.Background(),
+				cacheService: cacheService,
+				key:          incorrectConvertPipelineId,
+				errorTitle:   "",
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			// Test case with calling GetRunOutputLastIndex with pipelineId which contains status.
+			// As a result, want to receive an expected last index.
+			name: "get last index with correct pipelineId",
+			args: args{
+				ctx:          context.Background(),
+				cacheService: cacheService,
+				key:          pipelineId,
+				errorTitle:   "",
+			},
+			want:    2,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetRunOutputLastIndex(tt.args.ctx, tt.args.cacheService, tt.args.key, tt.args.errorTitle)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetRunOutputLastIndex() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetRunOutputLastIndex() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
