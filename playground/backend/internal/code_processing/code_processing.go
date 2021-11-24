42a47e9368471de1da556193629a591736c365a9
AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>
2021-11-24 19:36:13 +0300

    [BEAM-13307][Playground] Fix tests; Update processing of the compile step for python sdk;

--- playground/backend/internal/code_processing/code_processing.go
+++ playground/backend/internal/code_processing/code_processing.go
@@ -90,6 +90,8 @@ func Process(ctx context.Context, cacheService cache.Cache, lc *fs_tool.LifeCycl
 		if err := processStep(ctxWithTimeout, pipelineId, cacheService, cancelChannel, successChannel, &compileOutput, &compileError, errorChannel, pb.Status_STATUS_COMPILE_ERROR, pb.Status_STATUS_EXECUTING); err != nil {
 			return
 		}
+	case pb.Sdk_SDK_PYTHON:
+		processSuccess(ctx, []byte(""), pipelineId, cacheService, pb.Status_STATUS_EXECUTING)
 	}
 
 	runBuilder, err := run_builder.Setup(pipelineId, lc, appEnv.WorkingDir(), sdkEnv, compileBuilder)
