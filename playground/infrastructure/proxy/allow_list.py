91d5fabc577259a6367f640258c952ff19f97bef
Artur Khanin <artur.khanin@akvelon.com>
2021-11-25 16:38:15 +0300

    Added proxy server with allow-list using mitmproxy

---
+++ playground/infrastructure/proxy/allow_list.py
@@ -0,0 +1,7 @@
+# ALLOW_LIST contains all hosts that are allowed to make requests to from Beam Playground
+ALLOW_LIST = [
+    'localhost',
+    '127.0.0.1',
+    'dataflow-samples',
+    'playground-precompiled-objects',
+]
---
+++ playground/infrastructure/proxy/allow_list_proxy.py
@@ -0,0 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mitmproxy import http
+from allow_list import ALLOW_LIST
+
+
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.pretty_host not in ALLOW_LIST:
+        flow.response = http.Response.make(
+            status_code=403,
+            content='Making requests to the hosts that are not listed in the allowed list is forbidden.'
+        )
