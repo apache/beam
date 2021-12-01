281ac0d1c584877d4f3046024f305295a8fc9edf
daria.malkova <daria.malkova@akvelon.com>
2021-12-01 21:37:35 +0300

    Merge pull request #16086 from [BEAM-13258][Playground] Get and store precompiled examples
    * Revert "[Playground][BEAM-12941][Bugfix] Fix workflows for playground applications (#83)" (#88)
    
    This reverts commit b73f5f70ac4184b56a0a03922731c5f2f69b9566.
    
    * implement cd step
    
    * [BEAM-13135][Playground]
    implemented method to find all tagged files;
    added categories.yaml file;
    added requirements.txt file;
    
    * [BEAM-13267][Playground]
    implemented a method to send examples to the backend to verify them
    
    * CD step
    
    * Fix merge conflict
    
    Co-authored-by: Ilya <ilya.kozyrev@akvelon.com>
    Co-authored-by: AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>

--- playground/infrastructure/test_cd_helper.py
+++ playground/infrastructure/test_cd_helper.py
@@ -13,16 +13,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock
+import os
+import shutil
+
+import pytest
 
 from cd_helper import CDHelper
+from api.v1.api_pb2 import SDK_JAVA, STATUS_UNSPECIFIED
+from config import Config
+from helper import Example, Tag
 
 
-@mock.patch('cd_helper.CDHelper._save_to_cloud')
-@mock.patch('cd_helper.CDHelper._run_code')
-def test_store_examples(mock_run_code, mock_save_to_cloud):
-    helper = CDHelper()
-    helper.store_examples([])
+@pytest.fixture
+def delete_temp_folder():
+    """
+    Create temp folder for tests with storing files
+    """
+    yield delete_temp_folder
+    if os.path.exists(Config.TEMP_FOLDER):
+        shutil.rmtree(Config.TEMP_FOLDER)
 
-    mock_run_code.assert_called_once_with([])
-    mock_save_to_cloud.assert_called_once_with([])
+
+@pytest.fixture
+def upload_blob():
+    """
+    Fake method for mocking
+    Returns: None
+    """
+    pass
+
+
+def test__get_gcs_object_name():
+    """
+    Test getting the path where file will be stored at the bucket
+    """
+    expected_result = "SDK_JAVA/base_folder/file.java"
+    expected_result_with_extension = "SDK_JAVA/base_folder/file.output"
+    assert CDHelper()._get_gcs_object_name(SDK_JAVA, "base_folder", "file") == expected_result
+    assert CDHelper()._get_gcs_object_name(SDK_JAVA, "base_folder", "file", "output") == expected_result_with_extension
+
+
+def test__write_to_local_fs(delete_temp_folder):
+    """
+    Test writing code of an example, output and meta info to the filesystem (in temp folder)
+    Args:
+        delete_temp_folder: python fixture to clean up temp folder after method execution
+    """
+    object_meta = {"name": "name", "description": "description", "multifile": False,
+                   "categories": ["category-1", "category-2"]}
+    example = Example("name", "pipeline_id", SDK_JAVA, "filepath", "code_of_example",
+                      "output_of_example", STATUS_UNSPECIFIED, Tag(**object_meta))
+    expected_result = {"SDK_JAVA/name/name.java": "temp/pipeline_id/SDK_JAVA/name/name.java",
+                       "SDK_JAVA/name/name.output": "temp/pipeline_id/SDK_JAVA/name/name.output",
+                       "SDK_JAVA/name/meta.info": "temp/pipeline_id/SDK_JAVA/name/meta.info"}
+    assert CDHelper()._write_to_local_fs(example) == expected_result
+
+
+def test__save_to_cloud_storage(mocker):
+    """
+    Test saving examples, outputs and meta to bucket
+    Args:
+        mocker: mocker fixture from pytest-mocker
+    """
+    upload_blob_mock = mocker.patch(
+        "cd_helper.CDHelper._upload_blob",
+        return_value=upload_blob
+    )
+    write_to_os_mock = mocker.patch(
+        "cd_helper.CDHelper._write_to_local_fs",
+        return_value={"": ""}
+    )
+    example = Example("name", "pipeline_id", SDK_JAVA, "filepath", "code_of_example",
+                      "output_of_example", STATUS_UNSPECIFIED, None)
+
+    CDHelper()._save_to_cloud_storage([example])
+    write_to_os_mock.assert_called_with(example)
+    upload_blob_mock.assert_called_with(source_file="", destination_blob_name="")
