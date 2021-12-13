06d43c2aebb1ebb0ed5910bbba81bfa769285045
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2021-12-13 16:50:42 -0600

    [BEAM-12572] Add filters example test

--- sdks/python/apache_beam/examples/cookbook/filters_test.py
+++ sdks/python/apache_beam/examples/cookbook/filters_test.py
@@ -20,16 +20,28 @@
 # pytype: skip-file
 
 import logging
+import time
 import unittest
+
+import pytest
+from hamcrest.core.core.allof import all_of
 
 import apache_beam as beam
 from apache_beam.examples.cookbook import filters
+from apache_beam.io.gcp.tests import utils
+from apache_beam.io.gcp.tests.bigquery_matcher import BigqueryMatcher
+from apache_beam.testing.pipeline_verifiers import PipelineStateMatcher
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 
 
 class FiltersTest(unittest.TestCase):
+
+  # The default checksum is a SHA-1 hash generated from sorted rows reading
+  # from expected Bigquery table.
+  DEFAULT_CHECKSUM = '813b6da1624334732fad4467d74a7c8a62559c6b'
+
   # Note that 'removed' should be projected away by the pipeline
   input_data = [
       {
@@ -54,7 +66,7 @@ class FiltersTest(unittest.TestCase):
     results = filters.filter_cold_days(rows, month)
     return results
 
-  def test_basic(self):
+  def test_basics(self):
     """Test that the correct result is returned for a simple dataset."""
     with TestPipeline() as p:
       results = self._get_result_for_month(p, 1)
@@ -78,6 +90,35 @@ class FiltersTest(unittest.TestCase):
       results = self._get_result_for_month(p, 4)
       assert_that(results, equal_to([]))
 
+  @pytest.mark.examples_postcommit
+  def test_filters_it(self):
+    test_pipeline = TestPipeline(is_integration_test=True)
+
+    # Set extra options to the pipeline for test purpose
+    project = test_pipeline.get_option('project')
+
+    dataset = 'FiltersTestIT'
+    table = 'cold_days_%s' % int(round(time.time() * 1000))
+    output_table = '.'.join([dataset, table])
+    query = 'SELECT year, month, day, mean_temp FROM `%s`' % output_table
+
+    pipeline_verifiers = [
+        PipelineStateMatcher(),
+        BigqueryMatcher(
+            project=project, query=query, checksum=self.DEFAULT_CHECKSUM)
+    ]
+    extra_opts = {
+        'output': output_table,
+        'on_success_matcher': all_of(*pipeline_verifiers)
+    }
+
+    # Register cleanup before pipeline execution.
+    # Note that actual execution happens in reverse order.
+    self.addCleanup(utils.delete_bq_table, project, dataset, table)
+
+    # Get pipeline options from command argument: --test-pipeline-options,
+    # and start pipeline job by calling pipeline main function.
+    filters.run(test_pipeline.get_full_options_as_args(**extra_opts))
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
