548b11af67c8d84d0e5f7f476b744f0c73492196
Kyle Weaver <kcweaver@google.com>
2021-12-13 17:38:04 -0800

    WIP finish basic pushdown optimizer implementation

---
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/graph/ProjectionPushdownOptimizer.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.core.construction.graph;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.runners.AppliedPTransform;
+import org.apache.beam.sdk.runners.PTransformMatcher;
+import org.apache.beam.sdk.runners.PTransformOverride;
+import org.apache.beam.sdk.runners.PTransformOverrideFactory;
+import org.apache.beam.sdk.schemas.FieldAccessDescriptor;
+import org.apache.beam.sdk.schemas.ProjectionProducer;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.values.TaggedPValue;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Iterables;
+
+/** TODO(ibzib) javadoc */
+public class ProjectionPushdownOptimizer {
+
+  /** Performs all known projection pushdown optimizations in-place on a Pipeline. */
+  public static void optimize(Pipeline pipeline) {
+    // Compute which Schema fields are (or conversely, are not) accessed in a pipeline.
+    FieldAccessVisitor fieldAccessVisitor = new FieldAccessVisitor();
+    pipeline.traverseTopologically(fieldAccessVisitor);
+
+    // Find transforms in this pipeline which both: 1. support projection pushdown and 2. output
+    // unused fields.
+    ProjectionProducerVisitor pushdownProjectorVisitor =
+        new ProjectionProducerVisitor(fieldAccessVisitor.getPCollectionFieldAccess());
+    pipeline.traverseTopologically(pushdownProjectorVisitor);
+    Map<ProjectionProducer<PTransform<?, ?>>, Map<PCollection<?>, FieldAccessDescriptor>>
+        pushdownOpportunities = pushdownProjectorVisitor.getPushdownOpportunities();
+
+    // TODO(ibzib) comment
+    PCollectionOutputTagVisitor outputTagVisitor =
+        new PCollectionOutputTagVisitor(pushdownOpportunities);
+    pipeline.traverseTopologically(outputTagVisitor);
+    Map<ProjectionProducer<PTransform<?, ?>>, Map<TupleTag<?>, FieldAccessDescriptor>>
+        taggedFieldAccess = outputTagVisitor.getTaggedFieldAccess();
+
+    // For each eligible transform, replace it with a modified transform that omits the unused
+    // fields.
+    for (Entry<ProjectionProducer<PTransform<?, ?>>, Map<TupleTag<?>, FieldAccessDescriptor>>
+        entry : taggedFieldAccess.entrySet()) {
+      PTransformMatcher matcher = application -> application.getTransform() == entry.getKey();
+      PushdownOverrideFactory<?> overrideFactory = new PushdownOverrideFactory<>(entry.getValue());
+      pipeline.replaceAll(ImmutableList.of(PTransformOverride.of(matcher, overrideFactory)));
+    }
+  }
+
+  @SuppressWarnings("unused") // pls
+  private static class PushdownOverrideFactory<
+          TransformT extends PTransform<PBegin, PCollection<Row>>>
+      implements PTransformOverrideFactory<PBegin, PCollection<Row>, TransformT> {
+    private final Map<TupleTag<?>, FieldAccessDescriptor> fields;
+
+    PushdownOverrideFactory(Map<TupleTag<?>, FieldAccessDescriptor> fields) {
+      this.fields = fields;
+    }
+
+    @Override
+    public PTransformReplacement<PBegin, PCollection<Row>> getReplacementTransform(
+        AppliedPTransform<PBegin, PCollection<Row>, TransformT> transform) {
+      // TODO(ibzib) make this work for upstream transforms that aren't sources.
+      return PTransformReplacement.of(
+          transform.getPipeline().begin(),
+          ((ProjectionProducer<PTransform<PBegin, PCollection<Row>>>) transform.getTransform())
+              .actuateProjectionPushdown(fields));
+    }
+
+    @Override
+    public Map<PCollection<?>, ReplacementOutput> mapOutputs(
+        Map<TupleTag<?>, PCollection<?>> outputs, PCollection<Row> newOutput) {
+      // TODO(ibzib) handle mutlipel outputs.
+      Map.Entry<TupleTag<?>, PCollection<?>> original =
+          Iterables.getOnlyElement(outputs.entrySet());
+      Map.Entry<TupleTag<?>, PCollection<?>> replacement =
+          (Map.Entry) Iterables.getOnlyElement(newOutput.expand().entrySet());
+      return Collections.singletonMap(
+          newOutput,
+          ReplacementOutput.of(
+              TaggedPValue.of(original.getKey(), original.getValue()),
+              TaggedPValue.of(replacement.getKey(), replacement.getValue())));
+    }
+  }
+}
---
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/graph/ProjectionPushdownOptimizerTest.java
@@ -0,0 +1,273 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.core.construction.graph;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.Pipeline.PipelineVisitor;
+import org.apache.beam.sdk.Pipeline.PipelineVisitor.Defaults;
+import org.apache.beam.sdk.runners.TransformHierarchy.Node;
+import org.apache.beam.sdk.schemas.FieldAccessDescriptor;
+import org.apache.beam.sdk.schemas.FieldAccessDescriptor.FieldDescriptor;
+import org.apache.beam.sdk.schemas.ProjectionProducer;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Impulse;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link ProjectionPushdownOptimizer}. */
+@RunWith(JUnit4.class)
+public class ProjectionPushdownOptimizerTest {
+
+  @Test
+  public void testSourceDoesNotImplementPushdownProjector() {
+    Pipeline p = Pipeline.create();
+    p.apply(new SimpleSource(FieldAccessDescriptor.withAllFields()))
+        .apply(new FieldAccessTransform(FieldAccessDescriptor.withFieldNames("foo", "bar")));
+
+    ProjectionPushdownOptimizer.optimize(p);
+    // TODO(ibzib) check
+  }
+
+  @Test
+  public void testSimpleProjectionPushdown() {
+    Pipeline p = Pipeline.create();
+    SimpleSourceWithPushdown originalSource =
+        new SimpleSourceWithPushdown(FieldAccessDescriptor.withAllFields());
+    FieldAccessDescriptor downstreamFieldAccess =
+        FieldAccessDescriptor.withFieldNames("foo", "bar");
+    p.apply(originalSource).apply(new FieldAccessTransform(downstreamFieldAccess));
+
+    SimpleSourceWithPushdown expectedSource = new SimpleSourceWithPushdown(downstreamFieldAccess);
+    testProjectionPushdown(
+        p,
+        ImmutableList.of(expectedSource),
+        ImmutableList.of(originalSource) /* unexpectedSources */);
+  }
+
+  @Test
+  public void testBranchedProjectionPushdown() {
+    Pipeline p = Pipeline.create();
+    SimpleSourceWithPushdown originalSource =
+        new SimpleSourceWithPushdown(FieldAccessDescriptor.withAllFields());
+    PCollection<Row> input = p.apply(originalSource);
+    input.apply(new FieldAccessTransform(FieldAccessDescriptor.withFieldNames("foo")));
+    input.apply(new FieldAccessTransform(FieldAccessDescriptor.withFieldNames("bar")));
+
+    SimpleSourceWithPushdown expectedSource =
+        new SimpleSourceWithPushdown(FieldAccessDescriptor.withFieldNames("foo", "bar"));
+    testProjectionPushdown(p, ImmutableList.of(expectedSource), ImmutableList.of(originalSource));
+  }
+
+  // TODO(ibzib) test pardo with side inputs
+  @Test
+  public void testBranchedProjectionPushdow2n() {
+    Pipeline p = Pipeline.create();
+    SimpleSourceWithPushdown originalSource =
+        new SimpleSourceWithPushdown(FieldAccessDescriptor.withAllFields());
+    PCollection<Row> input = p.apply(originalSource);
+    input.apply(new FieldAccessTransform(FieldAccessDescriptor.withFieldNames("foo")));
+    input.apply(new FieldAccessTransform(FieldAccessDescriptor.withFieldNames("bar")));
+
+    SimpleSourceWithPushdown expectedSource =
+        new SimpleSourceWithPushdown(FieldAccessDescriptor.withFieldNames("foo", "bar"));
+    testProjectionPushdown(p, ImmutableList.of(expectedSource), ImmutableList.of(originalSource));
+  }
+
+  // TODO(ibzib) test unknown ptransform
+
+  private void testProjectionPushdown(
+      Pipeline p, List<SimpleSource> expectedSources, List<SimpleSource> unexpectedSources) {
+    ProjectionPushdownOptimizer.optimize(p);
+
+    PipelineVisitor pipelineVisitor =
+        new Defaults() {
+          final List<PTransform<?, ?>> foundSources = new ArrayList<>();
+
+          @Override
+          public CompositeBehavior enterCompositeTransform(Node node) {
+            for (SimpleSource expected : expectedSources) {
+              if (expected.equals(node.getTransform())) {
+                foundSources.add(node.getTransform());
+              }
+            }
+            for (SimpleSource unexpected : unexpectedSources) {
+              Assert.assertNotEquals(unexpected, node.getTransform());
+            }
+            return CompositeBehavior.ENTER_TRANSFORM;
+          }
+
+          @Override
+          public void leavePipeline(Pipeline p) {
+            Assert.assertEquals(
+                "Not all expected sources found in optimized pipeline.",
+                expectedSources,
+                foundSources);
+            super.leavePipeline(p);
+          }
+        };
+
+    p.traverseTopologically(pipelineVisitor);
+  }
+
+  static class FieldAccessTransform extends PTransform<PCollection<Row>, PCollection<Row>> {
+    private final FieldAccessDescriptor fieldAccessDescriptor;
+
+    FieldAccessTransform(FieldAccessDescriptor fieldAccessDescriptor) {
+      this.fieldAccessDescriptor = fieldAccessDescriptor;
+    }
+
+    @Override
+    public PCollection<Row> expand(PCollection<Row> input) {
+      return input
+          .apply(
+              ParDo.of(
+                  new DoFn<Row, Row>() {
+                    @SuppressWarnings("unused") // used by reflection
+                    @FieldAccess("row")
+                    private final FieldAccessDescriptor fieldAccessDescriptor =
+                        FieldAccessTransform.this.fieldAccessDescriptor;
+
+                    @ProcessElement
+                    public void processElement(
+                        @FieldAccess("row") Row row, OutputReceiver<Row> outputReceiver)
+                        throws Exception {
+                      // Do nothing; we don't need to execute this DoFn.
+                    }
+                  }))
+          .setRowSchema(createStringSchema(fieldAccessDescriptor));
+    }
+  }
+
+  static Schema createStringSchema(FieldAccessDescriptor fieldAccessDescriptor) {
+    if (fieldAccessDescriptor.getAllFields()) {
+      return createStringSchema(FieldAccessDescriptor.withFieldNames("foo", "bar", "baz"));
+    }
+    Schema.Builder schemaBuilder = Schema.builder();
+    for (FieldDescriptor field : fieldAccessDescriptor.getFieldsAccessed()) {
+      schemaBuilder.addStringField(field.getFieldName());
+    }
+    return schemaBuilder.build();
+  }
+
+  private static class SimpleSource extends PTransform<PBegin, PCollection<Row>> {
+    private final FieldAccessDescriptor fieldAccessDescriptor;
+    private final Schema schema;
+
+    SimpleSource(FieldAccessDescriptor fieldAccessDescriptor) {
+      this.fieldAccessDescriptor = fieldAccessDescriptor;
+      this.schema = createStringSchema(fieldAccessDescriptor);
+    }
+
+    @Override
+    public PCollection<Row> expand(PBegin input) {
+      return input.apply(Impulse.create()).apply(ParDo.of(new NoOpDoFn<>())).setRowSchema(schema);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      SimpleSource that = (SimpleSource) o;
+      return Objects.equals(
+          fieldAccessDescriptor.fieldNamesAccessed(),
+          that.fieldAccessDescriptor.fieldNamesAccessed());
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(fieldAccessDescriptor.fieldNamesAccessed());
+    }
+  }
+
+  private static class SimpleSourceWithPushdown extends SimpleSource
+      implements ProjectionProducer<PTransform<PBegin, PCollection<Row>>> {
+
+    SimpleSourceWithPushdown(FieldAccessDescriptor fieldAccessDescriptor) {
+      super(fieldAccessDescriptor);
+    }
+
+    @Override
+    public boolean supportsProjectionPushdown() {
+      return true;
+    }
+
+    @Override
+    public PTransform<PBegin, PCollection<Row>> actuateProjectionPushdown(
+        Map<TupleTag<?>, FieldAccessDescriptor> fields) {
+      Assert.assertEquals(new TupleTag<>("output"), Iterables.getOnlyElement(fields.keySet()));
+      return new SimpleSourceWithPushdown(Iterables.getOnlyElement(fields.values()));
+    }
+  }
+
+  private static class NoOpDoFn<T> extends DoFn<T, Row> {
+    @ProcessElement
+    public void processElement(ProcessContext c) {}
+  }
+
+  // TODO(ibzib) test this
+  @SuppressWarnings("unused") // no
+  private static class MultipleOutputSourceWithPushdown extends PTransform<PBegin, PCollectionTuple>
+      implements ProjectionProducer<PTransform<PBegin, PCollectionTuple>> {
+
+    @Override
+    public PCollectionTuple expand(PBegin input) {
+      PCollectionTuple outputs =
+          input
+              .apply(Impulse.create())
+              .apply(
+                  ParDo.of(new NoOpDoFn<>())
+                      .withOutputTags(
+                          new TupleTag<>("output1"), TupleTagList.of(new TupleTag<>("output2"))));
+      outputs.get("output1").setRowSchema(Schema.builder().build());
+      outputs.get("output2").setRowSchema(Schema.builder().build());
+      return outputs;
+    }
+
+    @Override
+    public boolean supportsProjectionPushdown() {
+      return true;
+    }
+
+    @Override
+    public PTransform<PBegin, PCollectionTuple> actuateProjectionPushdown(
+        Map<TupleTag<?>, FieldAccessDescriptor> fields) {
+      return this;
+    }
+  }
+}
