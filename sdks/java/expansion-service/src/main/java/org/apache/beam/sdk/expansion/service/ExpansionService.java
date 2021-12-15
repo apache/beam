87eae0c68f8cd16557227e00866d88625076ec95
Ke Wu <ke.wu.cs@icloud.com>
2021-12-15 13:53:31 -0800

    Revert ExpansionService change

--- sdks/java/expansion-service/src/main/java/org/apache/beam/sdk/expansion/service/ExpansionService.java
+++ sdks/java/expansion-service/src/main/java/org/apache/beam/sdk/expansion/service/ExpansionService.java
@@ -525,7 +525,7 @@ public class ExpansionService extends ExpansionServiceGrpc.ExpansionServiceImplB
             .withNewIdPrefix(request.getNamespace());
     sdkComponents.registerEnvironment(
         Environments.createOrGetDefaultEnvironment(
-            pipelineOptions.as(PortablePipelineOptions.class)));
+            pipeline.getOptions().as(PortablePipelineOptions.class)));
     Map<String, String> outputMap =
         outputs.entrySet().stream()
             .collect(
