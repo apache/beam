40955599b4968d791d8a6111171b6b703735b65a
Sergey Kalinin <sergey.kalinin@akvelon.com>
2021-12-16 13:43:10 +0000

    Add python certificat

--- playground/backend/containers/python/entrypoint.sh
+++ playground/backend/containers/python/entrypoint.sh
@@ -7,4 +7,5 @@ done
 openssl x509 -in /root/.mitmproxy/mitmproxy-ca.pem -inform PEM -out /root/.mitmproxy/mitmproxy-ca.crt
 cp /root/.mitmproxy/mitmproxy-ca.crt /usr/local/share/ca-certificates/extra/
 update-ca-certificates
+cat /root/.mitmproxy/mitmproxy-ca.pem >> /usr/local/lib/python3.7/site-packages/certifi/cacert.pem
 /opt/playground/backend/server_python_backend
