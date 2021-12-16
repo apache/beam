112b3cd220bfa987428d5992ee89713483090a2e
Jack McCluskey <34928439+jrmccluskey@users.noreply.github.com>
2021-12-15 20:02:34 -0500

    [BEAM-13399] Add infrastructure to start JARs from Go functions (#16214)
    * [BEAM-13399] Add infrastructure to start JARs from Go functions
    
    * address feedback, add infra for specifying ports for JAR

---
+++ sdks/go/pkg/beam/core/runtime/xlangx/expansionx/process_test.go
@@ -0,0 +1,85 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package expansionx
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestNewExpansionServiceRunner(t *testing.T) {
+	testPath := "path/to/jar"
+	testPort := "8097"
+	serviceRunner := NewExpansionServiceRunner(testPath, testPort)
+	if serviceRunner.jarPath != testPath {
+		t.Errorf("JAR path mismatch: wanted %v, got %v", testPath, serviceRunner.jarPath)
+	}
+	if serviceRunner.servicePort != testPort {
+		t.Errorf("service port mismatch: wanted %v, got %v", testPort, serviceRunner.servicePort)
+	}
+	commandString := strings.Join(serviceRunner.serviceCommand.Args, " ")
+	if !strings.Contains(commandString, "java") {
+		t.Errorf("command string missing java invocation, got %v", commandString)
+	}
+	if !strings.Contains(commandString, "-jar") {
+		t.Errorf("command string missing -jar flag, got %v", commandString)
+	}
+	if !strings.Contains(commandString, testPath) {
+		t.Errorf("command string missing test path: want %v, got %v", testPath, commandString)
+	}
+}
+
+func TestStartService_badCommand(t *testing.T) {
+	serviceRunner := NewExpansionServiceRunner("", "")
+	serviceRunner.serviceCommand = exec.Command("jahva", "-jar")
+	err := serviceRunner.StartService()
+	if err == nil {
+		t.Error("StartService succeeded when it should have failed")
+	}
+}
+
+func TestStartService_good(t *testing.T) {
+	serviceRunner := NewExpansionServiceRunner("", "")
+	serviceRunner.serviceCommand = exec.Command("which", "go")
+	err := serviceRunner.StartService()
+	if err != nil {
+		t.Errorf("StartService failed when it should have succeeded, got %v", err)
+	}
+}
+
+func makeTestServiceRunner() *ExpansionServiceRunner {
+	return &ExpansionServiceRunner{jarPath: "", serviceCommand: &exec.Cmd{}}
+}
+
+func TestStopService_NoStart(t *testing.T) {
+	processStruct := makeTestServiceRunner()
+	err := processStruct.StopService()
+	if err == nil {
+		t.Errorf("StopService succeeded when it should have failed")
+	}
+}
+
+func TestStopService_AlreadyStopped(t *testing.T) {
+	processStruct := makeTestServiceRunner()
+	// Set ProcessState to non-nil, indicating that the process exited
+	processStruct.serviceCommand.ProcessState = &os.ProcessState{}
+	err := processStruct.StopService()
+	if err == nil {
+		t.Errorf("StopSercvice succeeded when it should have failed")
+	}
+}
