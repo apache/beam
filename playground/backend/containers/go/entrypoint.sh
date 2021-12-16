ad6de320ec32b91399bbccc56acf668a769da223
Artur Khanin <artur.khanin@akvelon.com>
2021-12-16 14:27:46 +0300

    Added proxy for Go backend

---
+++ playground/backend/containers/go/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+nohup /opt/mitmproxy/mitmdump -s /opt/mitmproxy/allow_list_proxy.py -p 8081 &
+while [ ! -f /root/.mitmproxy/mitmproxy-ca.pem ] ;
+do
+      sleep 2
+done
+openssl x509 -in /root/.mitmproxy/mitmproxy-ca.pem -inform PEM -out /root/.mitmproxy/mitmproxy-ca.crt
+cp /root/.mitmproxy/mitmproxy-ca.crt /usr/local/share/ca-certificates/extra/
+update-ca-certificates
+/opt/playground/backend/server_go_backend
