f364ecd0f89b77a2e38fd585f0fcfb378437b917
daria-malkova <daria.malkova@akvelon.com>
2021-12-20 15:50:08 +0300

    fix spotless check

--- examples/java/src/main/java/org/apache/beam/examples/DebuggingWordCount.java
+++ examples/java/src/main/java/org/apache/beam/examples/DebuggingWordCount.java
@@ -19,7 +19,8 @@ package org.apache.beam.examples;
 
 // beam-playground:
 //   name: DebuggingWordCount
-//   description: An example that counts words in Shakespeare/kinglear.txt includes regex filter("Flourish|stomach").
+//   description: An example that counts words in Shakespeare/kinglear.txt includes regex
+//     filter("Flourish|stomach").
 //   multifile: false
 //   pipeline_options: --output output.txt
 //   categories:
