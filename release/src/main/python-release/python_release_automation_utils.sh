176ffb145c1d89ff749ca19de40943b4ea763680
Valentyn Tymofieiev <valentyn@google.com>
2021-12-22 22:20:26 -0800

    Upgrade pip, wheel.

--- release/src/main/python-release/python_release_automation_utils.sh
+++ release/src/main/python-release/python_release_automation_utils.sh
@@ -161,6 +161,8 @@ function install_sdk() {
   gsutil version -l
   rm -rf ./temp_virtualenv_${2}
   $2 -m venv temp_virtualenv_${2}
+  pip install --upgrade pip
+  pip install wheel
   . temp_virtualenv_${2}/bin/activate
   gcloud_version=$(gcloud --version | head -1 | awk '{print $4}')
   if [[ "$gcloud_version" < "189" ]]; then
