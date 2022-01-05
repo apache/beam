f0f792c7c4b640cc5cf7f69bf75743f76a75f211
Pablo Estrada <pabloem@apache.org>
2022-01-04 21:34:29 -0800

    Starting expansion of primitive transforms

---
+++ sdks/node-ts/src/apache_beam/internal/translations.ts
@@ -0,0 +1,6 @@
+
+export const IMPULSE_BUFFER = new TextEncoder().encode("impulse");
+
+export const DATA_INPUT_URN = 'beam:runner:source:v1';
+export const DATA_OUTPUT_URN = 'beam:runner:sink:v1';
+export const IDENTITY_DOFN_URN = 'beam:dofn:identity:0.1';
