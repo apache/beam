3f4baef0deab16245ce04fc7ca266b4c73761573
Chamikara Jayalath <chamikaramj@gmail.com>
2022-01-05 11:36:45 -0800

    Updates google-cloud-spanner library

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -193,7 +193,7 @@ GCP_REQUIREMENTS = [
     'google-cloud-bigquery-storage>=2.6.3',
     'google-cloud-core>=0.28.1,<2',
     'google-cloud-bigtable>=0.31.1,<2',
-    'google-cloud-spanner>=1.13.0,<2',
+    'google-cloud-spanner>=3.9.0,<4',
     'grpcio-gcp>=0.2.2,<1',
     # GCP Packages required by ML functionality
     'google-cloud-dlp>=3.0.0,<4',
