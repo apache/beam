02361a8779d1a25930a2a23be12405d59fa58701
Robert Bradshaw <robertwb@gmail.com>
2022-01-05 17:38:09 -0800

    npx tsfmt -r src/apache_beam/base.ts src/apache_beam/transforms/core.ts

--- sdks/node-ts/src/apache_beam/transforms/core.ts
+++ sdks/node-ts/src/apache_beam/transforms/core.ts
@@ -3,7 +3,7 @@ import * as translations from '../internal/translations';
 import * as runnerApi from '../proto/beam_runner_api';
 import { BytesCoder, KVCoder } from "../coders/standard_coders";
 
-import {GroupByKey} from '../base'
+import { GroupByKey } from '../base'
 
 export class GroupBy extends PTransform {
     keyFn: (element: any) => any;
@@ -17,18 +17,18 @@ export class GroupBy extends PTransform {
     }
 
     expand(input: PCollection): PCollection {
-        let kvPcoll = input.map(function (x) { return {'key': this.keyFn(x), 'value': x};});
-        
+        let kvPcoll = input.map(function(x) { return { 'key': this.keyFn(x), 'value': x }; });
+
         const inputCoderId = (input as PCollection).proto.coderId;
 
         // TODO(pabloem): Find the appropriate coder for the key
         const keyCoderId = translations.registerPipelineCoder(
-            runnerApi.Coder.create({'spec': runnerApi.FunctionSpec.create({'urn': BytesCoder.URN}),}),
+            runnerApi.Coder.create({ 'spec': runnerApi.FunctionSpec.create({ 'urn': BytesCoder.URN }), }),
             input.pipeline.proto.components!);
         input.pipeline.coders[keyCoderId] = new BytesCoder();
 
         const kvCoderProto = runnerApi.Coder.create({
-            'spec': runnerApi.FunctionSpec.create({'urn': KVCoder.URN}),
+            'spec': runnerApi.FunctionSpec.create({ 'urn': KVCoder.URN }),
             'componentCoderIds': [keyCoderId, inputCoderId]
         })
         const kvCoderId = translations.registerPipelineCoder(kvCoderProto, input.pipeline.proto.components!);
@@ -36,7 +36,7 @@ export class GroupBy extends PTransform {
         kvPcoll.proto.coderId = kvCoderId;
         const kvCoder = new KVCoder(input.pipeline.coders[keyCoderId], input.pipeline.coders[input.proto.coderId]);
         input.pipeline.coders[kvCoderId] = kvCoder;
-        
+
         return kvPcoll.apply(new GroupByKey());
     }
 }
