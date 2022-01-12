64c14bfe04d703314d044de19d3232a1226dcbe4
Fernando Morales <fernando.morales@wizeline.com>
2022-01-12 09:04:27 -0700

    Changes comprising BEAM-12313 and BEAM-12314

---
+++ .test-infra/jenkins/job_PostCommit_Java_ValidatesRunner_Dataflow_Java17.groovy
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import CommonJobProperties as commonJobProperties
+import PostcommitJobBuilder
+
+
+PostcommitJobBuilder.postCommitJob('beam_PostCommit_Java_ValidatesRunner_Dataflow_Java17',
+    'Run Dataflow ValidatesRunner Java 17', 'Google Cloud Dataflow Runner ValidatesRunner Tests On Java 17', this) {
+
+      description('Runs the ValidatesRunner suite on the Dataflow runner with Java 17 worker harness.')
+
+      commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 480)
+      publishers {
+        archiveJunit('**/build/test-results/**/*.xml')
+      }
+
+      steps {
+        gradle {
+          rootBuildScriptDir(commonJobProperties.checkoutDir)
+          tasks(':runners:google-cloud-dataflow-java:setJavaSDK8AsCompiler')
+          tasks(':runners:google-cloud-dataflow-java:testJar')
+          tasks(':runners:google-cloud-dataflow-java:worker:legacy-worker:shadowJar')
+        }
+
+        gradle {
+          rootBuildScriptDir(commonJobProperties.checkoutDir)
+          tasks(':runners:google-cloud-dataflow-java:validatesRunnerJavaSDK17')
+          switches('-x shadowJar')
+          switches('-x shadowTestJar')
+          switches('-x compileJava')
+          switches('-x compileTestJava')
+          switches('-x jar')
+          switches('-x testJar')
+          switches('-x classes')
+          switches('-x testClasses')
+
+          commonJobProperties.setGradleSwitches(delegate, 3 * Runtime.runtime.availableProcessors())
+        }
+      }
+    }
