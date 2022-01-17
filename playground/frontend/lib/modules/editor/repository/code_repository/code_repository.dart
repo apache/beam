70ce67684810d38c8c3c46d0efb721ac1a9a7109
Aydar Farrakhov <stranniknm@gmail.com>
2022-01-17 11:43:17 +0300

    [BEAM-13556] fix tests

--- playground/frontend/lib/modules/editor/repository/code_repository/code_repository.dart
+++ playground/frontend/lib/modules/editor/repository/code_repository/code_repository.dart
@@ -167,6 +167,7 @@ class CodeRepository {
         final log = responses[1];
         final error = responses[2];
         return RunCodeResult(
+          pipelineUuid: pipelineUuid,
           status: status,
           output: prevOutput + output.output + error.output,
           log: prevLog + log.output,
