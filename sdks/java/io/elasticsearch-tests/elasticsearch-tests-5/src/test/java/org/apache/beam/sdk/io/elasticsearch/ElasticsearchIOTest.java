76f281834c4566f829728108e0ebbc54d9c22707
Kyle Hersey <kmhersey951@gmail.com>
2022-01-19 04:18:19 -0500

    Merge pull request #16309: [BEAM-13503] Set a default value to throwWriteErrors in BulkIO constructor
    [BEAM-13503] Set a default value to throwWriteErrors in BulkIO constructor

--- sdks/java/io/elasticsearch-tests/elasticsearch-tests-5/src/test/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIOTest.java
+++ sdks/java/io/elasticsearch-tests/elasticsearch-tests-5/src/test/java/org/apache/beam/sdk/io/elasticsearch/ElasticsearchIOTest.java
@@ -265,6 +265,12 @@ public class ElasticsearchIOTest implements Serializable {
   }
 
   @Test
+  public void testDocToBulkAndBulkIO() throws Exception {
+    elasticsearchIOTestCommon.setPipeline(pipeline);
+    elasticsearchIOTestCommon.testDocToBulkAndBulkIO();
+  }
+
+  @Test
   public void testDocumentCoder() throws Exception {
     elasticsearchIOTestCommon.testDocumentCoder();
   }
