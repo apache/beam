4d766671ea26957a3cb4f7487067293afedcf7f6
darshan-sj <darshan-sj@users.noreply.github.com>
2022-01-22 02:32:50 +0530

    Merge pull request #16530 from Adding JSON support in SpannerIO and Spanner schema parser
    * Adding JSON support in SpannerIO and Spanner schema parser
    
    * formatting changes

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerSchemaTest.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerSchemaTest.java
@@ -19,6 +19,7 @@ package org.apache.beam.sdk.io.gcp.spanner;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.cloud.spanner.Type;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -35,11 +36,13 @@ public class SpannerSchemaTest {
             .addKeyPart("test", "pk", false)
             .addColumn("test", "maxKey", "STRING(MAX)")
             .addColumn("test", "numericVal", "NUMERIC")
+            .addColumn("test", "jsonVal", "JSON")
             .build();
 
     assertEquals(1, schema.getTables().size());
-    assertEquals(3, schema.getColumns("test").size());
+    assertEquals(4, schema.getColumns("test").size());
     assertEquals(1, schema.getKeyParts("test").size());
+    assertEquals(Type.json(), schema.getColumns("test").get(3).getType());
   }
 
   @Test
