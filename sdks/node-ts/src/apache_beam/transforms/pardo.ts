9ac8826c2b36cebafd7cf5b29117a95a3fa63ebf
Robert Bradshaw <robertwb@gmail.com>
2022-01-24 23:33:24 -0800

    Implement side inputs.

--- sdks/node-ts/src/apache_beam/transforms/pardo.ts
+++ sdks/node-ts/src/apache_beam/transforms/pardo.ts
@@ -50,6 +50,40 @@ export class ParDo<InputT, OutputT, ContextT = undefined> extends PTransform<
     transformProto: runnerApi.PTransform,
     input: PCollection<InputT>
   ) {
+    // Extract side inputs.
+    var context;
+    const sideInputs = {};
+    if (typeof this.context == "object") {
+      context = Object.create(this.context as Object);
+      for (const [name, value] of Object.entries(this.context)) {
+        if (value instanceof SideInputParam) {
+          const inputName = "side." + name;
+          transformProto.inputs[inputName] = value.pcoll.getId();
+          context[name] = copySideInputWithId(value, inputName);
+          sideInputs[inputName] = {
+            accessPattern: {
+              urn: value.accessor.accessPattern,
+              payload: new Uint8Array(),
+            },
+            // TODO: The viewFn is stored in the side input object. Unclear what benefit there is to putting it here.
+            viewFn: { urn: "unused", payload: new Uint8Array() },
+            // TODO: Possibly place this in the accessor.
+            // Default should be mapping of endpoint to window according to the
+            // windowFn of the side input PCollection.
+            // TODO: In the short term, at least check that they agree.
+            windowMappingFn: {
+              urn: "beam:window_mapping_fn:identity:v1",
+              value: new Uint8Array(),
+            },
+          };
+        } else {
+          context[name] = value;
+        }
+      }
+    } else {
+      context = this.context;
+    }
+
     transformProto.spec = runnerApi.FunctionSpec.create({
       urn: ParDo.urn,
       payload: runnerApi.ParDoPayload.toBinary(
@@ -58,9 +92,10 @@ export class ParDo<InputT, OutputT, ContextT = undefined> extends PTransform<
             urn: urns.SERIALIZED_JS_DOFN_INFO,
             payload: fakeSeralize({
               doFn: this.doFn,
-              context: this.context,
+              context: context,
             }),
           }),
+          sideInputs: sideInputs,
         })
       ),
     });
@@ -122,11 +157,11 @@ export class Split<T> extends PTransform<
 }
 
 export interface ParamProvider {
-  provide(paramName: string): any;
+  provide<T>(param: ParDoParam<T>): T;
 }
 
-abstract class ParDoParam<T> {
-  private parDoParamName: string;
+export abstract class ParDoParam<T> {
+  readonly parDoParamName: string;
 
   // Provided externally.
   private provider: ParamProvider | undefined;
@@ -135,13 +170,89 @@ abstract class ParDoParam<T> {
     this.parDoParamName = parDoParamName;
   }
 
-  // TODO: Name? get seems to be special.
+  // TODO: Name? "get" seems to be special.
   lookup(): T {
     if (this.provider == undefined) {
       throw new Error("Cannot be called outside of a DoFn's process method.");
     }
 
-    return this.provider.provide(this.parDoParamName);
+    return this.provider.provide(this);
+  }
+}
+
+interface SideInputAccessor<PCollT, AccessorT, ValueT> {
+  // This should be a value of runnerApi.StandardSideInputTypes, and specifies
+  // the relationship between PCollT (the type fo the PCollection's elements)
+  // and AccessorT (the type returned when fetching side inputs).
+  accessPattern: string;
+  // This transforms the runner type into the user's type.
+  toValue: (AccessorT) => ValueT;
+}
+
+// TODO: Support side inputs that are composites of multiple more primitive
+// side inputs.
+export class SideInputParam<
+  PCollT,
+  AccessorT,
+  ValueT
+> extends ParDoParam<ValueT> {
+  // Populated by user.
+  pcoll: PCollection<any>;
+  // Typically populated by subclass.
+  accessor: SideInputAccessor<PCollT, AccessorT, ValueT>;
+
+  constructor(
+    pcoll: PCollection<PCollT>,
+    accessor: SideInputAccessor<PCollT, AccessorT, ValueT>
+  ) {
+    super("sideInput");
+    this.pcoll = pcoll;
+    this.accessor = accessor;
+  }
+
+  // Internal.
+  // TODO: Consistency between name and id.
+  sideInputId: string;
+}
+
+function copySideInputWithId<PCollT, AccessorT, ValueT>(
+  sideInput: SideInputParam<PCollT, AccessorT, ValueT>,
+  id: string
+): SideInputParam<PCollT, AccessorT, ValueT> {
+  const copy = Object.create(sideInput);
+  copy.sideInputId = id;
+  delete copy.pcoll;
+  return copy;
+}
+
+export class IterableSideInput<T> extends SideInputParam<
+  T,
+  Iterable<T>,
+  Iterable<T>
+> {
+  constructor(pcoll: PCollection<T>) {
+    super(pcoll, {
+      accessPattern: "beam:side_input:iterable:v1",
+      toValue: (iter: Iterable<T>) => iter,
+    });
+  }
+}
+
+export class SingletonSideInput<T> extends SideInputParam<T, Iterable<T>, T> {
+  constructor(pcoll: PCollection<T>, defaultValue: T | undefined = undefined) {
+    super(pcoll, {
+      accessPattern: "beam:side_input:iterable:v1",
+      toValue: (iter: Iterable<T>) => {
+        const asArray = Array.from(iter);
+        if (asArray.length == 0 && defaultValue != undefined) {
+          return defaultValue;
+        } else if (asArray.length == 1) {
+          return asArray[0];
+        } else {
+          throw new Error("Expected a single element, got " + asArray.length);
+        }
+      },
+    });
   }
 }
 
