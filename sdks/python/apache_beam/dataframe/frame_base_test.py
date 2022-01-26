3b858571df594a6cf835e1a8f00b9e76fec159cf
Andoni Guzman <andoni.guzman@wizeline.com>
2022-01-26 14:39:27 -0600

    Disable checks that broke tests

--- sdks/python/apache_beam/dataframe/frame_base_test.py
+++ sdks/python/apache_beam/dataframe/frame_base_test.py
@@ -65,9 +65,9 @@ class FrameBaseTest(unittest.TestCase):
     x = frames.DeferredSeries(original_expr)
     x.add_one()
     self.assertIs(x._expr, original_expr)
-    x.add_one(frame=False)
+    x.add_one(inplace=False)
     self.assertIs(x._expr, original_expr)
-    x.add_one(frame=False)
+    x.add_one(inplace=False)
     self.assertIsNot(x._expr, original_expr)
 
   def test_args_to_kwargs(self):
