57703e2018fd34143336486793351cb6299ebf8b
Danny McCormick <dannymccormick@google.com>
2022-01-28 14:11:40 -0500

    Ensure we don't get creds from env in tests that rely on it

--- sdks/go/pkg/beam/options/gcpopts/options_test.go
+++ sdks/go/pkg/beam/options/gcpopts/options_test.go
@@ -23,7 +23,7 @@ import (
 )
 
 func TestGetProjectFromFlagOrEnvironmentWithNoProjectSet(t *testing.T) {
-	os.Unsetenv("GOOGLE_APPLICATION_CREDENTIALS")
+	setupFakeCredentialFile(t, "")
 	*Project = ""
 	projectId := GetProjectFromFlagOrEnvironment(nil)
 	if projectId != "" {
@@ -32,7 +32,7 @@ func TestGetProjectFromFlagOrEnvironmentWithNoProjectSet(t *testing.T) {
 }
 
 func TestGetProjectFromFlagOrEnvironmentWithProjectFlagSet(t *testing.T) {
-	os.Unsetenv("GOOGLE_APPLICATION_CREDENTIALS")
+	setupFakeCredentialFile(t, "")
 	*Project = "test"
 	projectId := GetProjectFromFlagOrEnvironment(nil)
 	if projectId != "test" {
