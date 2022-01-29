cd4cda293636da12a05f1cc1f6085ded12af65b7
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2022-01-28 23:34:05 -0500

    Update sdks/python/apache_beam/options/pipeline_options.py
    Co-authored-by: tvalentyn <tvalentyn@users.noreply.github.com>

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -1055,7 +1055,7 @@ class SetupOptions(PipelineOptions):
             'Path to a folder to cache the packages specified in '
             'the requirements file using the --requirements_file option.'
             'If you want to skip populating requirements cache, please '
-            'specify --requirements_cache skip. This would install all'
+            'specify --requirements_cache="skip". This would install all'
             'the packages from requirements file on the worker.'))
     parser.add_argument(
         '--requirements_cache_only_sources',
