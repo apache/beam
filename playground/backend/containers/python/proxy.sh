ffa02872c2ecfc60e3ed8238035e3ed541863ab8
AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>
2022-01-31 18:20:19 +0300

    [BEAM-13774][Playground] Change Dockerfile for each supported SDK Separate entrypoint.sh and proxy.sh

--- playground/backend/containers/python/proxy.sh
+++ playground/backend/containers/python/proxy.sh
@@ -14,4 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/opt/playground/backend/server_java_backend
+nohup /opt/mitmproxy/mitmdump -s /opt/mitmproxy/allow_list_proxy.py -p 8081 &
+while [ ! -f /root/.mitmproxy/mitmproxy-ca.pem ] ;
+do
+      sleep 2
+done
+openssl x509 -in /root/.mitmproxy/mitmproxy-ca.pem -inform PEM -out /root/.mitmproxy/mitmproxy-ca.crt
+cp /root/.mitmproxy/mitmproxy-ca.crt /usr/local/share/ca-certificates/extra/
+update-ca-certificates
+cat /root/.mitmproxy/mitmproxy-ca.pem >> /usr/local/lib/python3.7/site-packages/certifi/cacert.pem
