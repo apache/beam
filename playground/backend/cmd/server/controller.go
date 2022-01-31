aa526de8ad02cff6ac75322686b6a2f204e8a84a
Artur Khanin <artur.khanin@akvelon.com>
2022-01-31 21:27:47 +0300

    Merge pull request #16493 from [BEAM-13632][Playground] Save catalog data to the cache
    * Added saving of precompiled objects catalog to cache at the server startup
    
    * Added caching of the catalog only in case of unspecified SDK
    
    * Update regarding comments
    
    * Update regarding comments
    
    * Simplified logging regarding comment
    
    * Updates regarding comments
    
    * Update for environment_service_test.go
    
    * Docstring update for GetPrecompiledObjects

--- playground/backend/cmd/server/controller.go
+++ playground/backend/cmd/server/controller.go
@@ -255,23 +255,25 @@ func (controller *playgroundController) Cancel(ctx context.Context, info *pb.Can
 	return &pb.CancelResponse{}, nil
 }
 
-// GetPrecompiledObjects returns the list of examples
+// GetPrecompiledObjects returns the catalog with examples
+// Tries to get the whole catalog from the cache
+// - If there is no catalog in the cache, gets the catalog from the Storage and saves it to the cache
+// - If SDK or category is specified in the request, gets the catalog from the cache and filters it by SDK and category
 func (controller *playgroundController) GetPrecompiledObjects(ctx context.Context, info *pb.GetPrecompiledObjectsRequest) (*pb.GetPrecompiledObjectsResponse, error) {
-	bucket := cloud_bucket.New()
-	sdkToCategories, err := bucket.GetPrecompiledObjects(ctx, info.Sdk, info.Category)
+	catalog, err := controller.cacheService.GetCatalog(ctx)
 	if err != nil {
-		logger.Errorf("GetPrecompiledObjects(): cloud storage error: %s", err.Error())
-		return nil, errors.InternalError("Error during getting Precompiled Objects", "Error with cloud connection")
-	}
-	response := pb.GetPrecompiledObjectsResponse{SdkCategories: make([]*pb.Categories, 0)}
-	for sdkName, categories := range *sdkToCategories {
-		sdkCategory := pb.Categories{Sdk: pb.Sdk(pb.Sdk_value[sdkName]), Categories: make([]*pb.Categories_Category, 0)}
-		for categoryName, precompiledObjects := range categories {
-			utils.PutPrecompiledObjectsToCategory(categoryName, &precompiledObjects, &sdkCategory)
+		logger.Errorf("GetPrecompiledObjects(): cache error: %s", err.Error())
+		catalog, err = utils.GetCatalogFromStorage(ctx)
+		if err != nil {
+			return nil, errors.InternalError("Error during getting Precompiled Objects", "Error with cloud connection")
 		}
-		response.SdkCategories = append(response.SdkCategories, &sdkCategory)
+		if err = controller.cacheService.SetCatalog(ctx, catalog); err != nil {
+			logger.Errorf("GetPrecompiledObjects(): cache error: %s", err.Error())
+		}
 	}
-	return &response, nil
+	return &pb.GetPrecompiledObjectsResponse{
+		SdkCategories: utils.FilterCatalog(catalog, info.Sdk, info.Category),
+	}, nil
 }
 
 // GetPrecompiledObjectCode returns the code of the specific example
--- playground/backend/internal/cache/redis/redis_cache.go
+++ playground/backend/internal/cache/redis/redis_cache.go
@@ -96,6 +96,36 @@ func (rc *Cache) SetExpTime(ctx context.Context, pipelineId uuid.UUID, expTime t
 	return nil
 }
 
+func (rc *Cache) SetCatalog(ctx context.Context, catalog []*pb.Categories) error {
+	catalogMarsh, err := json.Marshal(catalog)
+	if err != nil {
+		logger.Errorf("Redis Cache: set catalog: error during marshal catalog: %s, err: %s\n", catalog, err.Error())
+		return err
+	}
+	err = rc.Set(ctx, cache.ExamplesCatalog, catalogMarsh, 0).Err()
+	if err != nil {
+		logger.Errorf("Redis Cache: set catalog: error during Set operation, err: %s\n", err.Error())
+		return err
+	}
+	return nil
+
+}
+
+func (rc *Cache) GetCatalog(ctx context.Context) ([]*pb.Categories, error) {
+	value, err := rc.Get(ctx, cache.ExamplesCatalog).Result()
+	if err != nil {
+		logger.Errorf("Redis Cache: get catalog: error during Get operation for key: %s, err: %s\n", cache.ExamplesCatalog, err.Error())
+		return nil, err
+	}
+	var result []*pb.Categories
+	err = json.Unmarshal([]byte(value), &result)
+	if err != nil {
+		logger.Errorf("Redis Cache: get catalog: error during unmarshal catalog, err: %s\n", err.Error())
+	}
+
+	return result, nil
+}
+
 // unmarshalBySubKey unmarshal value by subKey
 func unmarshalBySubKey(subKey cache.SubKey, value string) (interface{}, error) {
 	var result interface{}
