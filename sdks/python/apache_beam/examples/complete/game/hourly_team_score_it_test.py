febcc7dd89d16890ee87878c03039c703b2d6c38
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2022-02-02 19:15:52 -0600

    [BEAM-13811] Fix save_main_session arg in tests examples

--- sdks/python/apache_beam/examples/complete/game/hourly_team_score_it_test.py
+++ sdks/python/apache_beam/examples/complete/game/hourly_team_score_it_test.py
@@ -87,7 +87,8 @@ class HourlyTeamScoreIT(unittest.TestCase):
     # Get pipeline options from command argument: --test-pipeline-options,
     # and start pipeline job by calling pipeline main function.
     hourly_team_score.run(
-        self.test_pipeline.get_full_options_as_args(**extra_opts))
+        self.test_pipeline.get_full_options_as_args(**extra_opts),
+        save_main_session=False)
 
   @pytest.mark.examples_postcommit
   def test_hourly_team_score_output_checksum_on_small_input(self):
