20db0f5c0b491dfbbd7d1ac6ae7f69065bd55e7e
Moritz Mack <mmack@talend.com>
2022-02-03 11:18:23 +0100

    [BEAM-13203] Deprecate SnsIO.writeAsync for AWS Sdk v2 due to risk of data loss.

--- sdks/java/io/amazon-web-services2/src/main/java/org/apache/beam/sdk/io/aws2/sns/SnsIO.java
+++ sdks/java/io/amazon-web-services2/src/main/java/org/apache/beam/sdk/io/aws2/sns/SnsIO.java
@@ -61,7 +61,7 @@ import software.amazon.awssdk.services.sns.model.PublishResponse;
 /**
  * {@link PTransform}s for writing to <a href="https://aws.amazon.com/sns/">SNS</a>.
  *
- * <h3>Writing to SNS Synchronously</h3>
+ * <h3>Writing to SNS</h3>
  *
  * <p>Example usage:
  *
@@ -87,38 +87,6 @@ import software.amazon.awssdk.services.sns.model.PublishResponse;
  * AwsResponseMetadata}, you can call {@link Write#withFullPublishResponse()}. If you need the HTTP
  * status code only but no headers, you can use {@link
  * Write#withFullPublishResponseWithoutHeaders()}.
- *
- * <h3>Writing to SNS Asynchronously</h3>
- *
- * <p>Example usage:
- *
- * <pre>{@code
- * PCollection<String> data = ...;
- *
- * data.apply(SnsIO.<String>writeAsync()
- * 		.withElementCoder(StringUtf8Coder.of())
- * 		.withPublishRequestFn(createPublishRequestFn())
- * 		.withSnsClientProvider(new BasicSnsClientProvider(awsCredentialsProvider, region));
- *
- * }</pre>
- *
- * <pre>{@code
- * PCollection<String> data = ...;
- *
- * PCollection<SnsResponse<String>> responses = data.apply(SnsIO.<String>writeAsync()
- *      .withElementCoder(StringUtf8Coder.of())
- *      .withPublishRequestFn(createPublishRequestFn())
- *  *   .withSnsClientProvider(new BasicSnsClientProvider(awsCredentialsProvider, region));
- *
- * }</pre>
- *
- * <p>As a client, you need to provide at least the following things:
- *
- * <ul>
- *   <li>Coder for element T.
- *   <li>publishRequestFn, a function to convert your message into PublishRequest
- *   <li>SnsClientProvider, a provider to create an async client.
- * </ul>
  */
 @Experimental(Kind.SOURCE_SINK)
 @SuppressWarnings({
@@ -127,11 +95,17 @@ import software.amazon.awssdk.services.sns.model.PublishResponse;
 })
 public final class SnsIO {
 
-  // Write data to SNS (synchronous)
+  // Write data to SNS
   public static <T> Write<T> write() {
     return new AutoValue_SnsIO_Write.Builder().build();
   }
 
+  /**
+   * @deprecated Please use {@link SnsIO#write()} to avoid the risk of data loss.
+   * @see <a href="https://issues.apache.org/jira/browse/BEAM-13824">BEAM-13824</a>, <a
+   *     href="https://issues.apache.org/jira/browse/BEAM-13203">BEAM-13203</a>
+   */
+  @Deprecated
   public static <T> WriteAsync<T> writeAsync() {
     return new AutoValue_SnsIO_WriteAsync.Builder().build();
   }
@@ -473,7 +447,14 @@ public final class SnsIO {
     }
   }
 
-  /** Implementation of {@link #writeAsync}. */
+  /**
+   * Implementation of {@link #writeAsync}.
+   *
+   * @deprecated Please use {@link SnsIO#write()} to avoid the risk of data loss.
+   * @see <a href="https://issues.apache.org/jira/browse/BEAM-13824">BEAM-13824</a>, <a
+   *     href="https://issues.apache.org/jira/browse/BEAM-13203">BEAM-13203</a>
+   */
+  @Deprecated
   @AutoValue
   public abstract static class WriteAsync<T>
       extends PTransform<PCollection<T>, PCollection<SnsResponse<T>>> {
