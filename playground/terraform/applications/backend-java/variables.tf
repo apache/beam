fda0b0064f0947e43a5bf3eaad97ab062bcd7372
andreukus <andreykus@mail.ru>
2022-02-04 08:36:09 +0300

    Merge pull request #16605 from [BEAM-13634][Playground] Create a separate Dockerfile for the routing service
    * Added saving of precompiled objects catalog to cache at the server startup
    
    * Added caching of the catalog only in case of unspecified SDK
    
    * Update regarding comments
    
    * Update regarding comments
    
    * Simplified logging regarding comment
    
    * Updates regarding comments
    
    * Update for environment_service_test.go
    
    * Docstring update for GetPrecompiledObjects
    
    * Add router services as separate container
    
    * too stage container builder
    
    * add empty string to build_playground_backend.yml
    
    * fix comments
    
    Co-authored-by: Artur Khanin <artur.khanin@akvelon.com>
    Co-authored-by: akustov <akustov@diasoft.ru>
    Co-authored-by: Ilya <ilya.kozyrev@akvelon.com>

--- playground/terraform/applications/backend-java/variables.tf
+++ playground/terraform/applications/backend-java/variables.tf
@@ -27,7 +27,7 @@ variable "docker_registry_address" {
 
 variable "docker_image_name" {
   description = "Docker Image Name To Be Deployed"
-  default = "beam_playground-backend"
+  default = "beam_playground-backend-java"
 }
 
 variable "docker_image_tag" {
