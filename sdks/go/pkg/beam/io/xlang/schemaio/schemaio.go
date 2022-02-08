74b675f1937fef042268ba0ebc107a7043836a70
Daniel Oliveira <daniel.o.programmer@gmail.com>
2022-02-07 20:40:33 -0800

    [BEAM-13321] Refactor SchemaIO package and expansion address parameter.
    Adjust Schema IO package to have a different entry point, fix a bug in how dataSchema is encoded, and add a unit test. Also adjust BigQuery IO to have Expansion Address as an optional parameter.

--- sdks/go/pkg/beam/io/xlang/schemaio/schemaio.go
+++ sdks/go/pkg/beam/io/xlang/schemaio/schemaio.go
@@ -28,7 +28,9 @@ import (
 
 	"github.com/apache/beam/sdks/v2/go/pkg/beam"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/graph/coder"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/runtime/graphx/schema"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/internal/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 // Payload is a struct matching the expected cross-language payload of a Schema IO.
@@ -49,33 +51,69 @@ type Payload struct {
 	DataSchema *[]byte `beam:"dataSchema"`
 }
 
-// EncodeAsRow encodes a struct as a Beam schema Row, to embed within a cross language payload.
-func EncodeAsRow(config interface{}) []byte {
+// encodeAsRow encodes a struct as a Beam schema Row, to embed within a cross language payload.
+func encodeAsRow(config interface{}) ([]byte, error) {
 	rt := reflect.TypeOf(config)
 	enc, err := coder.RowEncoderForStruct(rt)
 	if err != nil {
 		err = errors.WithContextf(err, "getting Row encoder for type %s", rt.Name())
-		panic(err)
+		return nil, err
 	}
 	var buf bytes.Buffer
 	if err := enc(config, &buf); err != nil {
 		err = errors.WithContextf(err, "encoding type %s as Row", rt.Name())
-		panic(err)
+		return nil, err
 	}
-	return buf.Bytes()
+	return buf.Bytes(), nil
 }
 
-// EncodePayload encodes a Schema IO payload. It takes a location for the Schema IO's data, an
+// encodeAsSchema retrieves a schema of a type, and encodes that schema into bytes.
+func encodeAsSchema(rt reflect.Type) ([]byte, error) {
+	scm, err := schema.FromType(rt)
+	if err != nil {
+		err = errors.WithContextf(err, "retrieving schema of type %s", rt.Name())
+		return nil, err
+	}
+	encScm, err := proto.Marshal(scm)
+	if err != nil {
+		err = errors.WithContextf(err, "encoding schema of type %s", rt.Name())
+		return nil, err
+	}
+	return encScm, nil
+}
+
+// EncodePayload encodes a SchemaIO payload. It takes a location for the SchemaIO's data, an
 // IO-specific configuration struct, and an optional struct representing the Beam schema for the
 // data.
-func EncodePayload(location string, config interface{}, dataSchema *interface{}) []byte {
+func EncodePayload(location string, config interface{}, dataSchema reflect.Type) ([]byte, error) {
+	encCfg, err := encodeAsRow(config)
+	if err != nil {
+		err = errors.WithContext(err, "encoding config for SchemaIO payload")
+		return nil, err
+	}
 	pl := Payload{
 		Location: location,
-		Config:   EncodeAsRow(config),
+		Config:   encCfg,
 	}
+
 	if dataSchema != nil {
-		row := EncodeAsRow(*dataSchema)
-		pl.DataSchema = &row
+		encScm, err := encodeAsSchema(dataSchema)
+		if err != nil {
+			err = errors.WithContext(err, "encoding dataSchema for SchemaIO payload")
+			return nil, err
+		}
+		pl.DataSchema = &encScm
 	}
-	return beam.CrossLanguagePayload(pl)
+	return beam.CrossLanguagePayload(pl), err
+}
+
+// MustEncodePayload encodes a SchemaIO payload. It takes a location for the SchemaIO's data, an
+// IO-specific configuration struct, and an optional struct representing the Beam schema for the
+// data. Unlike EncodePayload, this panics if an error occurs.
+func MustEncodePayload(location string, config interface{}, dataSchema reflect.Type) []byte {
+	pl, err := EncodePayload(location, config, dataSchema)
+	if err != nil {
+		panic(err)
+	}
+	return pl
 }
