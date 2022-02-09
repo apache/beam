086ceea09da1bf16cff037074f3ad11d49a451f9
Pablo Estrada <pabloem@apache.org>
2022-02-08 16:13:34 -0800

    asd

--- sdks/java/io/common/src/main/java/org/apache/beam/sdk/io/common/GenericIO.java
+++ sdks/java/io/common/src/main/java/org/apache/beam/sdk/io/common/GenericIO.java
@@ -17,14 +17,27 @@
  */
 package org.apache.beam.sdk.io.common;
 
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
+
 import com.google.auto.value.AutoValue;
 import java.io.Serializable;
 import java.util.UUID;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.GroupIntoBatches;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.util.ShardedKey;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.joda.time.Duration;
 
 public class GenericIO {
+
+  public final static Integer DEFAULT_BATCH_SIZE = 1000;
 
   public static <InputT, OutputT> Write<InputT, OutputT> write() {
     return Write.<InputT, OutputT>create();
@@ -39,21 +52,55 @@ public class GenericIO {
     }
 
     abstract BeamIOClientFactory<InputT, OutputT> getClientFactory();
+    abstract long getElementsPerPeriod();
+    abstract Duration getPeriod();
+    abstract SerializableFunction<InputT, String> getDestinationFn();
 
     abstract Builder<InputT, OutputT> toBuilder();
 
     @AutoValue.Builder
     abstract static class Builder<InputT, OutputT> {
       abstract Builder<InputT, OutputT> setClientFactory(BeamIOClientFactory<InputT, OutputT> factory);
+      abstract Builder<InputT, OutputT> setElementsPerPeriod(long elements);
+      abstract Builder<InputT, OutputT> setPeriod(Duration period);
+      abstract Builder<InputT, OutputT> setDestinationFn(SerializableFunction<InputT, String> destinationFn);
       abstract Write<InputT, OutputT> build();
+    }
+
+    /** Specifies the maximum throughput for the transform. */
+    public Write<InputT, OutputT> withMaxThroughput(long numElements, Duration periodLength) {
+      checkArgument(
+          numElements > 0,
+          "Number of elements in withMaxThroughput must be positive, but was: %s",
+          numElements);
+      checkArgument(periodLength != null, "periodLength can not be null");
+      return toBuilder().setElementsPerPeriod(numElements).setPeriod(periodLength).build();
     }
 
     public Write<InputT, OutputT> withClientFactory(BeamIOClientFactory<InputT, OutputT> clientFactory) {
       return toBuilder().setClientFactory(clientFactory).build();
     }
 
+    public Write<InputT, OutputT> withDestination(String destination) {
+      return toBuilder().setDestinationFn(elm -> destination).build();
+    }
+
+    public Write<InputT, OutputT> withDestination(SerializableFunction<InputT, String> destinationFn) {
+      return toBuilder().setDestinationFn(destinationFn).build();
+    }
+
     @Override
     public PCollection<OutputT> expand(PCollection<InputT> input) {
+      input
+          .apply(MapElements.via(new SimpleFunction<InputT, KV<String, InputT>>() {
+        @Override
+        public KV<String, InputT> apply(InputT input) {
+          return KV.of(getDestinationFn().apply(input), input);
+        }
+      }))
+          .apply(GroupIntoBatches.<String, InputT>ofSize(DEFAULT_BATCH_SIZE).withShardedKey())
+          // TODO(pabloem) Need a way to produce a stable UUID.
+          .apply(ParDo.of(new WriteFn<InputT, OutputT>(getClientFactory())));
       return null;
     }
   }
@@ -84,10 +131,53 @@ public class GenericIO {
   }
 
   public static class BeamIOException extends Exception {
+    private final Type type;
+    private final Throwable e;
+    public Type getType() {
+      return type;
+    }
+    public Throwable getUnderlyingException() {
+      return e;
+    }
+    BeamIOException(Type type, Throwable e) {
+      this.type = type;
+      this.e = e;
+    }
     public enum Type {
       THROTTLED,
       RETRIABLE_EXCEPTION,
       PERMANENT_EXCEPTION
+    }
+  }
+
+  private static class WriteFn<InputT, OutputT> extends DoFn<KV<ShardedKey<String>, Iterable<InputT>>, OutputT> {
+    private final BeamIOClientFactory<InputT, OutputT> clientFactory;
+    private transient BeamIOClient<InputT, OutputT> client;
+
+    WriteFn(BeamIOClientFactory<InputT, OutputT> clientFactory) {
+      this.clientFactory = clientFactory;
+    }
+
+    @DoFn.ProcessElement
+    public void process(@DoFn.Element KV<ShardedKey<String>, Iterable<InputT>> elm,
+        OutputReceiver<OutputT> receiver) {
+      if (this.client == null) {
+        this.client = this.clientFactory.connection();
+      }
+      try {
+        this.client.startBatch(elm.getKey().getKey(), elm.getValue(), UUID.randomUUID());
+      } catch (BeamIOException e) {
+        switch (e.getType()) {
+          case THROTTLED:
+            break;
+          case PERMANENT_EXCEPTION:
+            break;
+          case RETRIABLE_EXCEPTION:
+            break;
+          default:
+            break;
+        }
+      }
     }
   }
 
