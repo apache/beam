6882994f5c9b2a2ce3db19b69f87d4794d0b82ac
Aydar Zainutdinov <aydar.zaynutdinov@akvelon.com>
2022-02-10 01:34:35 +0300

    Merge pull request #16659 from [BEAM-13774][Playground] Add user to container
    * [BEAM-13774][Playground]
    Change Dockerfile for each supported SDK
    Separate entrypoint.sh and proxy.sh
    
    * [BEAM-13774][Playground]
    Update Dockerfiles;
    Add user for Scio SDK;
    
    * [BEAM-13774][Playground]
    small fixes
    
    * [BEAM-13774][Playground]
    Update Dockerfiles

--- playground/backend/containers/python/entrypoint.sh
+++ playground/backend/containers/python/entrypoint.sh
@@ -14,13 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-nohup /opt/mitmproxy/mitmdump -s /opt/mitmproxy/allow_list_proxy.py -p 8081 &
-while [ ! -f /root/.mitmproxy/mitmproxy-ca.pem ] ;
-do
-      sleep 2
-done
-openssl x509 -in /root/.mitmproxy/mitmproxy-ca.pem -inform PEM -out /root/.mitmproxy/mitmproxy-ca.crt
-cp /root/.mitmproxy/mitmproxy-ca.crt /usr/local/share/ca-certificates/extra/
-update-ca-certificates
-cat /root/.mitmproxy/mitmproxy-ca.pem >> /usr/local/lib/python3.7/site-packages/certifi/cacert.pem
 /opt/playground/backend/server_python_backend
