df907de8519e6a23bb6b016ff8593f103e739e61
Pablo <pabloem@users.noreply.github.com>
2022-02-09 09:52:25 -0800

    Merge pull request #16613 from Supporting JdbcIO driver in classpath for x-lang
    * Supporting JdbcIO driver in classpath
    
    * Logging added
    
    * Adding test with two different DBs
    
    * Improving documentation
    
    * Addressing comments

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -175,7 +175,8 @@ REQUIRED_TEST_PACKAGES = [
     'pytest-timeout>=1.3.3,<2',
     'sqlalchemy>=1.3,<2.0',
     'psycopg2-binary>=2.8.5,<3.0.0',
-    'testcontainers>=3.0.3,<4.0.0',
+    'testcontainers[mysql]>=3.0.3,<4.0.0',
+    'cryptography>=36.0.0',
 ]
 
 GCP_REQUIREMENTS = [
