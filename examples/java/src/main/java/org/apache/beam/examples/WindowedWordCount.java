4fcba6aae4b9c9dd71aa679506883078fd2893b8
AydarZaynutdinov <aydar.zaynutdinov@akvelon.com>
2022-02-16 16:28:03 +0300

    Add tag for TfIdf.java Add tag for TopWikipediaSessions.java Add tag for TrafficMaxLaneFlow.java Add tag for TrafficRoutes.java Update tag for WindowedWordCount.java Update tag for ReadFromText python kata

--- examples/java/src/main/java/org/apache/beam/examples/WindowedWordCount.java
+++ examples/java/src/main/java/org/apache/beam/examples/WindowedWordCount.java
@@ -22,7 +22,7 @@ package org.apache.beam.examples;
 //   description: An example that counts words in text, and can run over either unbounded or bounded input collections.
 //   multifile: true
 //   pipeline_options: --output output.txt
-//   context_line: 102
+//   context_line: 103
 //   categories:
 //     - Combiners
 //     - Options
--- examples/java/src/main/java/org/apache/beam/examples/complete/TrafficMaxLaneFlow.java
+++ examples/java/src/main/java/org/apache/beam/examples/complete/TrafficMaxLaneFlow.java
@@ -17,6 +17,20 @@
  */
 package org.apache.beam.examples.complete;
 
+// beam-playground:
+//   name: TrafficMaxLaneFlow
+//   description: An example that analyzes traffic sensor data using SlidingWindows. For each window, it finds the lane
+//     that had the highest flow recorded, for each sensor station. It writes those max values along with auxiliary info
+//     to a BigQuery table.
+//   multifile: true
+//   context_line: 93
+//   categories:
+//     - Combiners
+//     - Batch
+//     - Streaming
+//     - Options
+//     - Windowing
+
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableReference;
 import com.google.api.services.bigquery.model.TableRow;
