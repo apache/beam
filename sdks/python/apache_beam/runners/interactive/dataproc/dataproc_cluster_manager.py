0e45de72ed7455bc486fda023418c9a2d317e238
Victor <career@victorplusc.com>
2022-02-18 16:05:58 -0500

    [BEAM-13812] Integrate DataprocClusterManager into Interactive Environment

--- sdks/python/apache_beam/runners/interactive/dataproc/dataproc_cluster_manager.py
+++ sdks/python/apache_beam/runners/interactive/dataproc/dataproc_cluster_manager.py
@@ -18,15 +18,32 @@
 # pytype: skip-file
 
 import logging
+from dataclasses import dataclass
 from typing import Optional
 
-try:
-  from google.cloud import dataproc_v1
-except ImportError:
-  raise ImportError(
-      'Google Cloud Dataproc not supported for this execution environment.')
+from apache_beam.runners.interactive import interactive_environment as ie
 
 _LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class MasterURLIdentifier:
+  project_id: Optional[str] = None
+  region: Optional[str] = None
+  cluster_name: Optional[str] = None
+
+  def __key(self):
+    return (self.project_id, self.region, self.cluster_name)
+
+  def __hash__(self):
+    return hash(self.__key())
+
+  def __eq__(self, other):
+    if isinstance(other, MasterURLIdentifier):
+      return self.__key() == other.__key()
+    raise NotImplementedError(
+        'Comparisons are only supported between '
+        'instances of MasterURLIdentifier.')
 
 
 class DataprocClusterManager:
@@ -34,43 +51,36 @@ class DataprocClusterManager:
   required for creating and deleting Dataproc clusters for use
   under Interactive Beam.
   """
-  DEFAULT_NAME = 'interactive-beam-cluster'
-
-  def __init__(
-      self,
-      project_id: Optional[str] = None,
-      region: Optional[str] = None,
-      cluster_name: Optional[str] = None) -> None:
+  def __init__(self, cluster_metadata: MasterURLIdentifier) -> None:
     """Initializes the DataprocClusterManager with properties required
     to interface with the Dataproc ClusterControllerClient.
     """
-
-    self._project_id = project_id
-    if region == 'global':
+    self.cluster_metadata = cluster_metadata
+    if self.cluster_metadata.region == 'global':
       # The global region is unsupported as it will be eventually deprecated.
       raise ValueError('Clusters in the global region are not supported.')
-    elif region:
-      self._region = region
-    else:
+    elif not self.cluster_metadata.region:
       _LOGGER.warning(
           'No region information was detected, defaulting Dataproc cluster '
           'region to: us-central1.')
-      self._region = 'us-central1'
+      self.cluster_metadata.region = 'us-central1'
 
-    if cluster_name:
-      _LOGGER.warning(
-          'A user-specified cluster_name has been detected. '
-          'Please note that you will have to manually delete the Dataproc '
-          'cluster that will be created under the name: %s',
-          cluster_name)
-      self._cluster_name = cluster_name
-    else:
-      self._cluster_name = self.DEFAULT_NAME
+    if not self.cluster_metadata.cluster_name:
+      self.cluster_metadata.cluster_name = ie.current_env(
+      ).clusters.default_cluster_name
 
+    from google.cloud import dataproc_v1
     self._cluster_client = dataproc_v1.ClusterControllerClient(
         client_options={
-            'api_endpoint': f'{self._region}-dataproc.googleapis.com:443'
+            'api_endpoint': \
+            f'{self.cluster_metadata.region}-dataproc.googleapis.com:443'
         })
+
+    if self.cluster_metadata in ie.current_env().clusters.master_urls.inverse:
+      self.master_url = ie.current_env().clusters.master_urls.inverse[
+          self.cluster_metadata]
+    else:
+      self.master_url = None
 
   def create_cluster(self, cluster: dict) -> None:
     """Attempts to create a cluster using attributes that were
@@ -78,52 +88,65 @@ class DataprocClusterManager:
 
     Args:
       cluster: Dictionary representing Dataproc cluster. Read more about the
-      schema for clusters here:
-      https://cloud.google.com/python/docs/reference/dataproc/latest/google.cloud.dataproc_v1.types.Cluster
+          schema for clusters here:
+          https://cloud.google.com/python/docs/reference/dataproc/latest/google.cloud.dataproc_v1.types.Cluster
     """
+    if self.master_url:
+      return
     try:
       self._cluster_client.create_cluster(
           request={
-              'project_id': self._project_id,
-              'region': self._region,
+              'project_id': self.cluster_metadata.project_id,
+              'region': self.cluster_metadata.region,
               'cluster': cluster
           })
-      _LOGGER.info('Cluster created successfully: %s', self._cluster_name)
+      _LOGGER.info(
+          'Cluster created successfully: %s',
+          self.cluster_metadata.cluster_name)
+      self.master_url = self.get_master_url(self.cluster_metadata)
     except Exception as e:
       if e.code == 409:
-        if self._cluster_name == self.DEFAULT_NAME:
-          _LOGGER.info(
-              'Cluster %s already exists. Continuing...', self.DEFAULT_NAME)
-        else:
-          _LOGGER.error(
-              'Cluster already exists - unable to create cluster: %s',
-              self._cluster_name)
-          raise ValueError(
-              'Cluster {} already exists!'.format(self._cluster_name))
+        _LOGGER.info(
+            'Cluster %s already exists. Continuing...',
+            ie.current_env().clusters.default_cluster_name)
       elif e.code == 403:
         _LOGGER.error(
             'Due to insufficient project permissions, '
             'unable to create cluster: %s',
-            self._cluster_name)
+            self.cluster_metadata.cluster_name)
         raise ValueError(
             'You cannot create a cluster in project: {}'.format(
-                self._project_id))
+                self.cluster_metadata.project_id))
       elif e.code == 501:
-        _LOGGER.error('Invalid region provided: %s', self._region)
-        raise ValueError('Region {} does not exist!'.format(self._region))
+        _LOGGER.error(
+            'Invalid region provided: %s', self.cluster_metadata.region)
+        raise ValueError(
+            'Region {} does not exist!'.format(self.cluster_metadata.region))
       else:
-        _LOGGER.error('Unable to create cluster: %s', self._cluster_name)
+        _LOGGER.error(
+            'Unable to create cluster: %s', self.cluster_metadata.cluster_name)
         raise e
 
   # TODO(victorhc): Add support for user-specified pip packages
   def create_flink_cluster(self) -> None:
     """Calls _create_cluster with a configuration that enables FlinkRunner."""
     cluster = {
-        'project_id': self._project_id,
-        'cluster_name': self._cluster_name,
+        'project_id': self.cluster_metadata.project_id,
+        'cluster_name': self.cluster_metadata.cluster_name,
         'config': {
             'software_config': {
                 'optional_components': ['DOCKER', 'FLINK']
+            },
+            'gce_cluster_config': {
+                'metadata': {
+                    'flink-start-yarn-session': 'true'
+                },
+                'service_account_scopes': [
+                    'https://www.googleapis.com/auth/cloud-platform'
+                ]
+            },
+            'endpoint_config': {
+                'enable_http_port_access': True
             }
         }
     }
@@ -135,30 +158,43 @@ class DataprocClusterManager:
     try:
       self._cluster_client.delete_cluster(
           request={
-              'project_id': self._project_id,
-              'region': self._region,
-              'cluster_name': self._cluster_name,
+              'project_id': self.cluster_metadata.project_id,
+              'region': self.cluster_metadata.region,
+              'cluster_name': self.cluster_metadata.cluster_name,
           })
     except Exception as e:
       if e.code == 403:
         _LOGGER.error(
             'Due to insufficient project permissions, '
             'unable to clean up the default cluster: %s',
-            self._cluster_name)
+            self.cluster_metadata.cluster_name)
         raise ValueError(
             'You cannot delete a cluster in project: {}'.format(
-                self._project_id))
+                self.cluster_metadata.project_id))
       elif e.code == 404:
-        _LOGGER.error('Cluster does not exist: %s', self._cluster_name)
-        raise ValueError('Cluster was not found: {}'.format(self._cluster_name))
+        _LOGGER.error(
+            'Cluster does not exist: %s', self.cluster_metadata.cluster_name)
+        raise ValueError(
+            'Cluster was not found: {}'.format(
+                self.cluster_metadata.cluster_name))
       else:
-        _LOGGER.error('Failed to delete cluster: %s', self._cluster_name)
+        _LOGGER.error(
+            'Failed to delete cluster: %s', self.cluster_metadata.cluster_name)
         raise e
 
-  def cleanup_if_default(self) -> None:
-    """Checks if the cluster_name initialized with the
-    DataprocClusterManager instance is the default
-    cluster_name. If it is, then the cleanup() method
-    is invoked."""
-    if self._cluster_name == self.DEFAULT_NAME:
-      self.cleanup()
+  def describe(self) -> None:
+    """Returns a dictionary describing the cluster."""
+    return {
+        'cluster_metadata': self.cluster_metadata,
+        'master_url': self.master_url
+    }
+
+  def get_master_url(self, identifier) -> None:
+    """Returns the master_url of the current cluster."""
+    # TODO(victorhc): Implement the following method to fetch the cluster
+    # master_url from Dataproc.
+    return '.'.join([
+        self.cluster_metadata.project_id,
+        self.cluster_metadata.region,
+        self.cluster_metadata.cluster_name
+    ])
