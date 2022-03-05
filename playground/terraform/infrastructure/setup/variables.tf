b4e4bac79dc3612f19db0f893a3d81b746d88559
akustov <akustov@diasoft.ru>
2022-03-05 13:33:04 +0300

    fix default value variable for start over dir

--- playground/terraform/infrastructure/setup/variables.tf
+++ playground/terraform/infrastructure/setup/variables.tf
@@ -23,6 +23,7 @@ variable "project_id" {
 
 variable "service_account_id" {
   description = "Service account ID"
+  default     = "beam-playground"
 }
 
 
--- playground/terraform/infrastructure/variables.tf
+++ playground/terraform/infrastructure/variables.tf
@@ -39,6 +39,7 @@ variable "service_account_id" {
   description = "Service account ID"
   default     = "beam-playground"
 }
+
 variable "service_account" {
   description = "Service account email"
   default     = "service-account-playground"
