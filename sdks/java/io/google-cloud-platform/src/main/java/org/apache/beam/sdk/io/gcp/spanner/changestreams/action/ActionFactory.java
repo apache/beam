d6e64906f2b9b940aefa0265e8126f14c2535b20
nancyxu123 <nancyxu1996@gmail.com>
2022-03-09 09:30:45 -0800

    Merge pull request #17036 from [BEAM-12164] Convert all static instances to be transient in the connector in order to enable concurrent testing
    * Convert all static instances to be transient in the connector in order to enable concurrent testing
    
    * Initialized fields to null
    
    * nullness
    
    * Suppress uninitialized warnings
    
    * Remove resetting dao factory fields in SpannerChangeStreamErrorTest.java
    
    * Add validation package

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/action/ActionFactory.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/action/ActionFactory.java
@@ -29,17 +29,17 @@ import org.joda.time.Duration;
  * Factory class for creating instances that will handle each type of record within a change stream
  * query. The instances created are all singletons.
  */
-// static fields are un-initialized, because we start them during the first fetch call (with the
-// singleton pattern)
-@SuppressWarnings("initialization.static.fields.uninitialized")
+// transient fields are un-initialized, because we start them during the first fetch call (with the
+// singleton pattern).
+@SuppressWarnings("initialization.fields.uninitialized")
 public class ActionFactory implements Serializable {
 
   private static final long serialVersionUID = -4060958761369602619L;
-  private static DataChangeRecordAction dataChangeRecordActionInstance;
-  private static HeartbeatRecordAction heartbeatRecordActionInstance;
-  private static ChildPartitionsRecordAction childPartitionsRecordActionInstance;
-  private static QueryChangeStreamAction queryChangeStreamActionInstance;
-  private static DetectNewPartitionsAction detectNewPartitionsActionInstance;
+  private transient DataChangeRecordAction dataChangeRecordActionInstance;
+  private transient HeartbeatRecordAction heartbeatRecordActionInstance;
+  private transient ChildPartitionsRecordAction childPartitionsRecordActionInstance;
+  private transient QueryChangeStreamAction queryChangeStreamActionInstance;
+  private transient DetectNewPartitionsAction detectNewPartitionsActionInstance;
 
   /**
    * Creates and returns a singleton instance of an action class capable of processing {@link
