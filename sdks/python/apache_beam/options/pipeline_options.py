80579f46dab1e19d72af70a3955a7c7fc3bef394
Rahul Iyer <rahuliyer573@gmail.com>
2022-03-10 10:58:04 -0800

    [BEAM-13993] [BEAM-10039] Import beam plugins before starting SdkHarness
    This reverts commit e105098a4a18674a0bd3db79b1c4449f513a9752.

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -1108,7 +1108,7 @@ class SetupOptions(PipelineOptions):
             'custom code.'))
     parser.add_argument(
         '--beam_plugin',
-        '--beam_plugin',
+        '--beam_plugins',
         dest='beam_plugins',
         action='append',
         default=None,
