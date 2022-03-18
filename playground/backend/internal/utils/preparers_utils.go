33fd554efed0e0070443156d6d4604eaba37178d
daria.malkova <daria.malkova@akvelon.com>
2022-03-19 02:52:09 +0300

    Merge pull request #16895 from [BEAM-13882][Playground] More tests for validators package
    * Add preparer for graph saving
    
    * add goroutine to read graph file
    
    * Update playground/backend/internal/code_processing/code_processing.go
    
    Co-authored-by: Artur Khanin <artur.khanin@akvelon.com>
    
    * Update playground/backend/internal/preparers/python_preparers.go
    
    Co-authored-by: Artur Khanin <artur.khanin@akvelon.com>
    
    * fix comments
    
    * fix comments
    
    * Add graph extraction for java pipelint
    
    * fix comments
    
    * make Wrap method not public
    
    * graph
    
    * move method GetPreparers to avoid cycle dependencies
    
    * move method GetValidators to avoid cycle dependencies
    
    * fix merge
    
    * fix tests
    
    * Add tests
    
    * Add tests
    
    * Add comments for tests
    
    * Fix comments
    
    * fix tests
    
    * add tests
    
    * fix RAT
    
    * fix comments
    
    * fix test after merge
    
    * Names conversion
    
    * Resolve conflicts
    
    Co-authored-by: Artur Khanin <artur.khanin@akvelon.com>
    Co-authored-by: Ilya Kozyrev <ilya.kozyrev@akvelon.com>
    Co-authored-by: Pavel Avilov <avilovpavel6@gmail.com>

--- playground/backend/internal/utils/preparers_utils.go
+++ playground/backend/internal/utils/preparers_utils.go
@@ -33,6 +33,7 @@ const (
 	GraphFileName          = "graph.dot"
 	pythonGraphCodePattern = "$0# Write graph to file\n$0from apache_beam.runners.interactive.display import pipeline_graph\n$0dot = pipeline_graph.PipelineGraph(%s).get_dot()\n$0with open('%s', 'w') as file:\n$0  file.write(dot)\n"
 	newLinePattern         = "\n"
+	tmpFileSuffix          = "tmp"
 )
 
 type PipelineDefinitionType int
@@ -156,4 +157,26 @@ func RenameSourceCodeFile(filePath string, className string) error {
 	newFilePath := strings.Replace(filePath, currentFileName, fmt.Sprintf("%s%s", className, filepath.Ext(currentFileName)), 1)
 	err := os.Rename(filePath, newFilePath)
 	return err
+}
+
+// CreateTempFile creates temporary file next to originalFile
+func CreateTempFile(originalFilePath string) (*os.File, error) {
+	fileName := filepath.Base(originalFilePath)
+	tmpFileName := fmt.Sprintf("%s_%s", tmpFileSuffix, fileName)
+	tmpFilePath := strings.Replace(originalFilePath, fileName, tmpFileName, 1)
+	return os.Create(tmpFilePath)
+}
+
+func ChangeTestFileName(args ...interface{}) error {
+	filePath := args[0].(string)
+	publicClassNamePattern := args[1].(string)
+	className, err := GetPublicClassName(filePath, publicClassNamePattern)
+	if err != nil {
+		return err
+	}
+	err = RenameSourceCodeFile(filePath, className)
+	if err != nil {
+		return err
+	}
+	return nil
 }
