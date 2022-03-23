f0c3c2c2ff131704a1fed7986a55f6cea361467d
Pavel Avilov <avilovpavel6@gmail.com>
2022-03-23 13:22:08 +0300

    Refactoring code

--- playground/backend/internal/setup_tools/life_cycle/life_cycle_setuper.go
+++ playground/backend/internal/setup_tools/life_cycle/life_cycle_setuper.go
@@ -70,17 +70,17 @@ func Setup(sdk pb.Sdk, code string, pipelineId uuid.UUID, workingDir, pipelinesF
 	case pb.Sdk_SDK_GO:
 		if err = prepareGoFiles(lc, preparedModDir, pipelineId); err != nil {
 			lc.DeleteFolders()
-			return nil, fmt.Errorf("error during create necessary files for the Go sdk: %v", err)
+			return nil, fmt.Errorf("error during create necessary files for the Go sdk: %s", err.Error())
 		}
 	case pb.Sdk_SDK_JAVA:
 		if err = prepareJavaFiles(lc, workingDir, pipelineId); err != nil {
 			lc.DeleteFolders()
-			return nil, fmt.Errorf("error during create necessary files for the Java sdk: %v", err)
+			return nil, fmt.Errorf("error during create necessary files for the Java sdk: %s", err.Error())
 		}
 	case pb.Sdk_SDK_SCIO:
 		if lc, err = prepareSbtFiles(lc, lc.Paths.AbsoluteBaseFolderPath, workingDir); err != nil {
 			lc.DeleteFolders()
-			return nil, fmt.Errorf("error during create necessary files for the Scio sdk: %v", err)
+			return nil, fmt.Errorf("error during create necessary files for the Scio sdk: %s", err.Error())
 		}
 	}
 
