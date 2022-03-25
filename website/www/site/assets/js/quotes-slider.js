f087e17bd4307396a5af66eef9ba4847cfbd7d60
bulat safiullin <bulat.safiullin@akvelon.com>
2022-03-25 23:01:22 +0600

    [BEAM-14145] [Website] add slider to quotes desktop

--- website/www/site/assets/js/quotes-slider.js
+++ website/www/site/assets/js/quotes-slider.js
@@ -10,33 +10,62 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-var slider = new KeenSlider("#my-keen-slider", {
-  loop: true,
-  created: function (instance) {
-    var dots_wrapper = document.getElementById("dots");
-    var slides = document.querySelectorAll(".keen-slider__slide");
-    slides.forEach(function (t, idx) {
-      var dot = document.createElement("button");
-      dot.classList.add("dot");
-      dots_wrapper.appendChild(dot);
-      dot.addEventListener("click", function () {
-        instance.moveToSlide(idx);
-      });
+(function () {
+
+    // var scrollBarWidth = 17;
+    // var tabletWidth = 1024 - scrollBarWidth;
+    // var sliderSelector = '.keen-slider-JS';
+
+    var desktopSliderSelector = '.quotes-desktop.keen-slider-JS';
+    var mobileSliderSelector = '.quote-mobile.keen-slider-JS';
+    var classVisible = 'visible';
+    var tabletWidth = 1024;
+    var bodyWidth = window.innerWidth && document.documentElement.clientWidth ?
+        Math.min(window.innerWidth, document.documentElement.clientWidth) :
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.getElementsByTagName('body')[0].clientWidth;
+
+    var currentSliderSelector = bodyWidth >= tabletWidth
+        ? desktopSliderSelector
+        : mobileSliderSelector;
+
+    var countOfSlides = bodyWidth >= tabletWidth
+        ? 3
+        : 1;
+
+    document.querySelector(currentSliderSelector).classList.add(classVisible);
+
+    var slider = new KeenSlider(currentSliderSelector, {
+        // slides: countOfSlides,
+        loop: true,
+        created: function (instance) {
+            var dots_wrapper = document.getElementById("dots");
+            var slides = document.querySelectorAll(".keen-slider__slide");
+            slides.forEach(function (t, idx) {
+                var dot = document.createElement("button");
+                dot.classList.add("dot");
+                dots_wrapper.appendChild(dot);
+                dot.addEventListener("click", function () {
+                    instance.moveToSlide(idx);
+                });
+            });
+            updateClasses(instance);
+        },
+        slideChanged(instance) {
+            updateClasses(instance);
+        }
     });
-    updateClasses(instance);
-  },
-  slideChanged(instance) {
-    updateClasses(instance);
-  }
-});
 
-function updateClasses(instance) {
-  var slide = instance.details().relativeSlide;
+    function updateClasses(instance) {
+        var slide = instance.details().relativeSlide;
 
-  var dots = document.querySelectorAll(".dot");
-  dots.forEach(function (dot, idx) {
-    idx === slide
-      ? dot.classList.add("dot--active")
-      : dot.classList.remove("dot--active");
-  });
-}
+        var dots = document.querySelectorAll(".dot");
+        dots.forEach(function (dot, idx) {
+            idx === slide
+                ? dot.classList.add("dot--active")
+                : dot.classList.remove("dot--active");
+        });
+    }
+
+}())
