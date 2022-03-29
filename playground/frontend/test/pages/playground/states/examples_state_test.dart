470d05f935b443e8f40955af98328e04419bbc99
Alexander <alexander.zhuravlev@akvelon.com>
2022-03-29 14:24:52 +0400

    Fixed constants in example state test

--- playground/frontend/test/pages/playground/states/examples_state_test.dart
+++ playground/frontend/test/pages/playground/states/examples_state_test.dart
@@ -18,47 +18,17 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-import 'package:playground/modules/examples/models/category_model.dart';
-import 'package:playground/modules/examples/models/example_model.dart';
 import 'package:playground/modules/examples/repositories/example_repository.dart';
 import 'package:playground/modules/sdk/models/sdk.dart';
 import 'package:playground/pages/playground/states/examples_state.dart';
 
 import 'examples_state_test.mocks.dart';
+import 'mocks/categories_mock.dart';
 
 @GenerateMocks([ExampleRepository])
 void main() {
   late ExampleState state;
   late MockExampleRepository mockRepo;
-
-  final ExampleModel example = ExampleModel(
-    name: 'Test example',
-    path: 'Test path',
-    description: 'Test description',
-    type: ExampleType.example,
-  );
-
-  final List<CategoryModel> categories = [
-    CategoryModel(
-      name: 'Test category 1',
-      examples: [example],
-    ),
-    CategoryModel(
-      name: 'Test category 2',
-      examples: [example, example],
-    ),
-    CategoryModel(
-      name: 'Test category 3',
-      examples: [example, example, example],
-    ),
-  ];
-
-  final sdkCategoriesMapFromServer = {
-    SDK.java: categories,
-    SDK.python: categories,
-    SDK.go: categories,
-    SDK.scio: categories,
-  };
 
   setUp(() {
     mockRepo = MockExampleRepository();
@@ -77,9 +47,9 @@ void main() {
     'Example state should notify all listeners about sdkCategories is set',
     () {
       state.addListener(() {
-        expect(state.sdkCategories, sdkCategoriesMapFromServer);
+        expect(state.sdkCategories, sdkCategoriesFromServerMock);
       });
-      state.setSdkCategories(sdkCategoriesMapFromServer);
+      state.setSdkCategories(sdkCategoriesFromServerMock);
     },
   );
 
@@ -96,11 +66,11 @@ void main() {
   test(
     'Example state getCategories should get the categories list for each SDK',
     () {
-      state.setSdkCategories(sdkCategoriesMapFromServer);
-      expect(state.getCategories(SDK.java), categories);
-      expect(state.getCategories(SDK.go), categories);
-      expect(state.getCategories(SDK.python), categories);
-      expect(state.getCategories(SDK.scio), categories);
+      state.setSdkCategories(sdkCategoriesFromServerMock);
+      expect(state.getCategories(SDK.java), categoriesMock);
+      expect(state.getCategories(SDK.go), categoriesMock);
+      expect(state.getCategories(SDK.python), categoriesMock);
+      expect(state.getCategories(SDK.scio), categoriesMock);
     },
   );
 }
