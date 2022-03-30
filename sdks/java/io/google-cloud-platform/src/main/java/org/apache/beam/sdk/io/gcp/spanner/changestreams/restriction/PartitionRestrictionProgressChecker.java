14c5f19977100ecddf01fa54ea2d19e026e1cc83
Nancy Xu <nancyxu@google.com>
2022-03-30 22:04:38 +0000

    Added new restriction tracker

---
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/restriction/PartitionRestrictionProgressChecker.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction;
+
+import static java.math.MathContext.DECIMAL128;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.DONE;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.QUERY_CHANGE_STREAM;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.STOP;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.UPDATE_STATE;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.WAIT_FOR_CHILD_PARTITIONS;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.TimestampUtils.toNanos;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker.Progress;
+
+public class PartitionRestrictionProgressChecker {
+
+  private static final BigDecimal TOTAL_MODE_TRANSITIONS = BigDecimal.valueOf(3L);
+
+  /**
+   * Indicates how many mode transitions have been completed for the current mode. The transitions
+   * are as follows: * (1) QUERY_CHANGE_STREAM, (2) WAIT_FOR_CHILD_PARTITIONS, (3) FINISH_PARTITION,
+   * (4) WAIT_FOR_PARENT_PARTITIONS, (5) DELETE_PARTITION, (6) DONE.
+   *
+   * <p>This is used to calculate the units of work left, meaning that 1 transition = 1 unit of
+   * work.
+   */
+  private final Map<PartitionMode, BigDecimal> modeToTransitionsCompleted;
+
+  public PartitionRestrictionProgressChecker() {
+    modeToTransitionsCompleted = new HashMap<>();
+    modeToTransitionsCompleted.put(UPDATE_STATE, BigDecimal.valueOf(0L));
+    modeToTransitionsCompleted.put(QUERY_CHANGE_STREAM, BigDecimal.valueOf(1L));
+    modeToTransitionsCompleted.put(WAIT_FOR_CHILD_PARTITIONS, BigDecimal.valueOf(2L));
+    modeToTransitionsCompleted.put(DONE, BigDecimal.valueOf(3L));
+  }
+
+  public Progress getProgress(
+      PartitionRestriction restriction, PartitionPosition lastClaimedPosition) {
+    final PartitionMode currentMode =
+        Optional.ofNullable(lastClaimedPosition)
+            .map(PartitionPosition::getMode)
+            .orElse(restriction.getMode());
+    if (currentMode == STOP) {
+      // Return progress indicating that there is no more work to be done for this SDF.
+      final double workCompleted = 1D;
+      final double workRemaining = 0D;
+
+      return Progress.from(workCompleted, workRemaining);
+    }
+    final BigDecimal transitionsCompleted =
+        modeToTransitionsCompleted.getOrDefault(currentMode, BigDecimal.ZERO);
+
+    final BigDecimal startTimestampAsNanos = toNanos(restriction.getStartTimestamp());
+    final BigDecimal endTimestampAsNanos = toNanos(restriction.getEndTimestamp());
+    final BigDecimal currentTimestampAsNanos =
+        Optional.ofNullable(lastClaimedPosition)
+            .flatMap(PartitionPosition::getTimestamp)
+            .map(TimestampUtils::toNanos)
+            .orElse(
+                (currentMode == UPDATE_STATE || currentMode == QUERY_CHANGE_STREAM)
+                    ? startTimestampAsNanos
+                    : endTimestampAsNanos);
+
+    final BigDecimal totalWork =
+        endTimestampAsNanos
+            .subtract(startTimestampAsNanos, DECIMAL128)
+            .add(TOTAL_MODE_TRANSITIONS, DECIMAL128);
+
+    final BigDecimal workCompleted =
+        currentTimestampAsNanos.subtract(startTimestampAsNanos).add(transitionsCompleted);
+    final BigDecimal workLeft =
+        endTimestampAsNanos
+            .subtract(startTimestampAsNanos)
+            .add(TOTAL_MODE_TRANSITIONS)
+            .subtract(workCompleted);
+
+    return Progress.from(
+        workCompleted.divide(totalWork, DECIMAL128).doubleValue(),
+        workLeft.divide(totalWork, DECIMAL128).doubleValue());
+  }
+}
---
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/restriction/PartitionRestrictionTracker.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction;
+
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.DONE;
+import static org.apache.beam.sdk.io.gcp.spanner.changestreams.restriction.PartitionMode.STOP;
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkState;
+
+import java.util.Optional;
+import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
+import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker.HasProgress;
+import org.apache.beam.sdk.transforms.splittabledofn.SplitResult;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.annotations.VisibleForTesting;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// TODO: Add java docs
+// TODO: Implement duration waiting for returning false on try claim
+public class PartitionRestrictionTracker
+    extends RestrictionTracker<PartitionRestriction, PartitionPosition> implements HasProgress {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PartitionRestrictionTracker.class);
+  private final PartitionRestrictionSplitter splitter;
+  private final PartitionRestrictionClaimer claimer;
+  private final PartitionRestrictionProgressChecker progressChecker;
+  protected PartitionRestriction restriction;
+  private PartitionPosition lastClaimedPosition;
+
+  public PartitionRestrictionTracker(PartitionRestriction restriction) {
+    this(
+        restriction,
+        new PartitionRestrictionSplitter(),
+        new PartitionRestrictionClaimer(),
+        new PartitionRestrictionProgressChecker());
+  }
+
+  PartitionRestrictionTracker(
+      PartitionRestriction restriction,
+      PartitionRestrictionSplitter splitter,
+      PartitionRestrictionClaimer claimer,
+      PartitionRestrictionProgressChecker progressChecker) {
+    this.splitter = splitter;
+    this.claimer = claimer;
+    this.restriction = restriction;
+    this.progressChecker = progressChecker;
+  }
+
+  @Override
+  public @Nullable SplitResult<PartitionRestriction> trySplit(double fractionOfRemainder) {
+    final String token =
+        Optional.ofNullable(restriction.getMetadata())
+            .map(PartitionRestrictionMetadata::getPartitionToken)
+            .orElse(null);
+    final SplitResult<PartitionRestriction> splitResult =
+        splitter.trySplit(fractionOfRemainder, lastClaimedPosition, restriction);
+    LOG.debug(
+        "["
+            + token
+            + "] Try split "
+            + lastClaimedPosition
+            + ", "
+            + restriction
+            + ": "
+            + splitResult);
+    if (splitResult != null) {
+      this.restriction = splitResult.getPrimary();
+    }
+    return splitResult;
+  }
+
+  @Override
+  public boolean tryClaim(PartitionPosition position) {
+    final boolean canClaim = claimer.tryClaim(restriction, lastClaimedPosition, position);
+
+    if (canClaim) {
+      this.lastClaimedPosition = position;
+    }
+
+    return canClaim;
+  }
+
+  @Override
+  public Progress getProgress() {
+    final String token =
+        Optional.ofNullable(restriction.getMetadata())
+            .map(PartitionRestrictionMetadata::getPartitionToken)
+            .orElse(null);
+    final Progress progress = progressChecker.getProgress(restriction, lastClaimedPosition);
+    LOG.debug("[" + token + "] Progress is " + progress);
+    return progress;
+  }
+
+  @Override
+  public PartitionRestriction currentRestriction() {
+    return restriction;
+  }
+
+  @Override
+  public void checkDone() throws IllegalStateException {
+    if (restriction.getMode() == STOP) {
+      return;
+    }
+    // If nothing was attempted, throws an exception
+    checkState(
+        lastClaimedPosition != null,
+        "restriction is non-empty %s and no keys have been attempted.",
+        restriction.toString());
+
+    final PartitionMode currentMode = lastClaimedPosition.getMode();
+
+    checkState(
+        currentMode == DONE, "Restriction %s does not have mode DONE", restriction.toString());
+  }
+
+  @Override
+  public IsBounded isBounded() {
+    return IsBounded.UNBOUNDED;
+  }
+
+  @VisibleForTesting
+  PartitionRestriction getRestriction() {
+    return restriction;
+  }
+
+  @VisibleForTesting
+  PartitionPosition getLastClaimedPosition() {
+    return lastClaimedPosition;
+  }
+}
