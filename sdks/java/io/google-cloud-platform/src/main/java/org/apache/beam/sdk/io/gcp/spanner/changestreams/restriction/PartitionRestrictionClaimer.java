88029e2ba782730f36fc1ddeb8e14d4920005acf
Nancy Xu <nancyxu@google.com>
2022-03-31 16:52:51 +0000

    made fix

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/restriction/PartitionRestrictionClaimer.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/restriction/PartitionRestrictionClaimer.java
@@ -98,7 +98,7 @@ public class PartitionRestrictionClaimer {
 
         checkArgument(
             lastClaimedPosition == null
-                || lastClaimedPosition.getTimestamp().isEmpty()
+                || lastClaimedPosition.getTimestamp().empty()
                 || attemptedTimestamp.compareTo(lastClaimedPosition.getTimestamp().get()) >= 0,
             "Trying to claim offset %s while last attempted was %s",
             position,
