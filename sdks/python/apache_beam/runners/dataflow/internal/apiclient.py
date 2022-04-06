a3a138c44e46dd84bde9de8cf950a76d200bbd41
Ryan Thompson <ryanthompson591@gmail.com>
2022-04-06 10:52:19 -0400

    lint

--- sdks/python/apache_beam/runners/dataflow/internal/apiclient.py
+++ sdks/python/apache_beam/runners/dataflow/internal/apiclient.py
@@ -554,8 +554,9 @@ class DataflowApplicationClient(object):
     if self.google_cloud_options.no_auth:
       credentials = None
     else:
-      set_impersonation_accounts(self._google_cloud_options.target_principal,
-                                 self._google_cloud_options.delegate_accounts)
+      set_impersonation_accounts(
+          self._google_cloud_options.target_principal,
+          self._google_cloud_options.delegate_accounts)
       credentials = get_service_credentials()
 
     http_client = get_new_http()
