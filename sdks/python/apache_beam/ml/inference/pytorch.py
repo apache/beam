49ba91abeb435fa7aa9f26624a7fa4b290bd3556
Andy Ye <andyye333@gmail.com>
2022-04-07 09:16:45 -0500

    Remove numpy checks/conversions; Address PR comments

--- sdks/python/apache_beam/ml/inference/pytorch.py
+++ sdks/python/apache_beam/ml/inference/pytorch.py
@@ -19,12 +19,8 @@
 
 from typing import Iterable
 from typing import List
-from typing import Union
 
-import numpy as np
-import pickle
 import torch
-from torch import nn
 
 from apache_beam.ml.inference.base import InferenceRunner
 from apache_beam.ml.inference.base import ModelLoader
@@ -34,39 +30,25 @@ class PytorchInferenceRunner(InferenceRunner):
   """
   Implements Pytorch inference method
   """
-  def __init__(self, input_dim: int, device: torch.device):
-    self._input_dim = input_dim
+  def __init__(self, device: torch.device):
     self._device = device
 
-  def run_inference(
-      self, batch: List[Union[np.ndarray, torch.Tensor]],
-      model: nn.Module) -> Iterable[torch.Tensor]:
+  def run_inference(self, batch: List[torch.Tensor],
+                    model: torch.nn.Module) -> Iterable[torch.Tensor]:
     """
     Runs inferences on a batch of examples and returns an Iterable of
     Predictions."""
-    if batch:
-      if isinstance(batch[0], np.ndarray):
-        batch = torch.Tensor(batch)
-      elif isinstance(batch[0], torch.Tensor):
-        batch = torch.stack(batch)
-      else:
-        raise ValueError("PCollection must be an numpy array or a torch Tensor")
 
+    if not batch:
+      return []
+    batch = torch.stack(batch)
     if batch.device != self._device:
       batch = batch.to(self._device)
     return model(batch)
 
   def get_num_bytes(self, batch: List[torch.Tensor]) -> int:
     """Returns the number of bytes of data for a batch."""
-    total_size = 0
-    for el in batch:
-      if isinstance(el, np.ndarray):
-        total_size += el.itemsize
-      elif isinstance(el, torch.Tensor):
-        total_size += el.element_size()
-      else:
-        total_size += len(pickle.dumps(el))
-    return total_size
+    return sum((el.element_size() for tensor in batch for el in tensor))
 
   def get_metrics_namespace(self) -> str:
     """
@@ -79,13 +61,11 @@ class PytorchModelLoader(ModelLoader):
   """Loads a Pytorch Model."""
   def __init__(
       self,
-      input_dim: int,
       state_dict_path: str,
-      model_class: nn.Module,
+      model_class: torch.nn.Module,
       device: str = 'CPU',
       map_location=None):
     """
-    input_dim: dimension (# of features) of the data
     state_dict_path: path to the saved dictionary of the model state.
     model_class: class of the Pytorch model that defines the model structure.
     device: the device on which you wish to run the model. If ``device = GPU``
@@ -95,7 +75,6 @@ class PytorchModelLoader(ModelLoader):
     See https://pytorch.org/tutorials/beginner/saving_loading_models.html
     for details
     """
-    self._input_dim = input_dim
     self._state_dict_path = state_dict_path
     if device == 'GPU' and torch.cuda.is_available():
       self._device = torch.device('cuda')
@@ -105,7 +84,7 @@ class PytorchModelLoader(ModelLoader):
     self._model_class.to(self._device)
     self._map_location = map_location
 
-  def load_model(self) -> nn.Module:
+  def load_model(self) -> torch.nn.Module:
     """Loads and initializes a Pytorch model for processing."""
     model = self._model_class
     model.load_state_dict(
@@ -115,5 +94,4 @@ class PytorchModelLoader(ModelLoader):
 
   def get_inference_runner(self) -> InferenceRunner:
     """Returns a Pytorch implementation of InferenceRunner."""
-    return PytorchInferenceRunner(
-        input_dim=self._input_dim, device=self._device)
+    return PytorchInferenceRunner(device=self._device)
--- sdks/python/apache_beam/ml/inference/pytorch_test.py
+++ sdks/python/apache_beam/ml/inference/pytorch_test.py
@@ -29,7 +29,7 @@ import torch
 
 import apache_beam as beam
 from apache_beam.ml.inference import base
-from apache_beam.ml.inference.pytorch_impl import PytorchModelLoader
+from apache_beam.ml.inference.pytorch import PytorchModelLoader
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
@@ -67,7 +67,6 @@ class PytorchRunInferenceTest(unittest.TestCase):
       output_dim = 1
 
       model_loader = PytorchModelLoader(
-          input_dim=input_dim,
           state_dict_path=path,
           model_class=PytorchLinearRegression(input_dim, output_dim))
 
@@ -76,10 +75,9 @@ class PytorchRunInferenceTest(unittest.TestCase):
       assert_that(actual, equal_to(expected))
 
   def test_invalid_input_type(self):
-    with self.assertRaisesRegex(
-        ValueError, "PCollection must be an numpy array or a torch Tensor"):
+    with self.assertRaisesRegex(TypeError, "expected Tensor as element"):
       with TestPipeline() as pipeline:
-        examples = [1, 5, 3, 10]
+        examples = np.array([1, 5, 3, 10], dtype="float32").reshape(-1, 1)
 
         state_dict = OrderedDict([('linear.weight', torch.Tensor([[2.0]])),
                                   ('linear.bias', torch.Tensor([0.5]))])
@@ -90,35 +88,12 @@ class PytorchRunInferenceTest(unittest.TestCase):
         output_dim = 1
 
         model_loader = PytorchModelLoader(
-            input_dim=input_dim,
             state_dict_path=path,
             model_class=PytorchLinearRegression(input_dim, output_dim))
 
         pcoll = pipeline | 'start' >> beam.Create(examples)
         # pylint: disable=expression-not-assigned
         pcoll | base.RunInference(model_loader)
-
-  def test_simple_single_numpy_feature(self):
-    with TestPipeline() as pipeline:
-      examples = np.array([1, 5, 3, 10], dtype="float32").reshape(-1, 1)
-      expected = torch.Tensor([example * 2.0 + 0.5 for example in examples])
-
-      state_dict = OrderedDict([('linear.weight', torch.Tensor([[2.0]])),
-                                ('linear.bias', torch.Tensor([0.5]))])
-      path = os.path.join(self.tmpdir, 'my_state_dict_path')
-      torch.save(state_dict, path)
-
-      input_dim = 1
-      output_dim = 1
-
-      model_loader = PytorchModelLoader(
-          input_dim=input_dim,
-          state_dict_path=path,
-          model_class=PytorchLinearRegression(input_dim, output_dim))
-
-      pcoll = pipeline | 'start' >> beam.Create(examples)
-      actual = pcoll | base.RunInference(model_loader)
-      assert_that(actual, equal_to(expected))
 
   def test_simple_multiple_tensor_features(self):
     with TestPipeline() as pipeline:
@@ -136,30 +111,6 @@ class PytorchRunInferenceTest(unittest.TestCase):
       output_dim = 1
 
       model_loader = PytorchModelLoader(
-          input_dim=input_dim,
-          state_dict_path=path,
-          model_class=PytorchLinearRegression(input_dim, output_dim))
-
-      pcoll = pipeline | 'start' >> beam.Create(examples)
-      actual = pcoll | base.RunInference(model_loader)
-      assert_that(actual, equal_to(expected))
-
-  def test_simple_multiple_numpy_features(self):
-    with TestPipeline() as pipeline:
-      examples = np.array([1, 5, 3, 10, -14, 0, 0.5, 0.5],
-                          dtype="float32").reshape(-1, 2)
-      expected = torch.Tensor([f1 * 2.0 + f2 * 3 + 0.5 for f1, f2 in examples])
-
-      state_dict = OrderedDict([('linear.weight', torch.Tensor([[2.0, 3]])),
-                                ('linear.bias', torch.Tensor([0.5]))])
-      path = os.path.join(self.tmpdir, 'my_state_dict_path')
-      torch.save(state_dict, path)
-
-      input_dim = 2
-      output_dim = 1
-
-      model_loader = PytorchModelLoader(
-          input_dim=input_dim,
           state_dict_path=path,
           model_class=PytorchLinearRegression(input_dim, output_dim))
 
@@ -185,7 +136,6 @@ class PytorchRunInferenceTest(unittest.TestCase):
       output_dim = 1
 
       model_loader = PytorchModelLoader(
-          input_dim=input_dim,
           state_dict_path=path,
           model_class=PytorchLinearRegression(input_dim, output_dim))
 
@@ -212,7 +162,6 @@ class PytorchRunInferenceTest(unittest.TestCase):
       output_dim = 1
 
       model_loader = PytorchModelLoader(
-          input_dim=input_dim,
           state_dict_path=path,
           model_class=PytorchLinearRegression(input_dim, output_dim))
 
