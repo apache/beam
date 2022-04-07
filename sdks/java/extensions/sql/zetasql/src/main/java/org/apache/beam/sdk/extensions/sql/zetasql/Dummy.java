e607c81efb007a80e6a5512d5c22cf08994fb55c
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2022-04-07 10:49:45 -0500

    [BEAM-11714] Remove dummy class used for testing

--- sdks/java/extensions/sql/zetasql/src/main/java/org/apache/beam/sdk/extensions/sql/zetasql/Dummy.java
+++
@@ -1,8 +0,0 @@
-package org.apache.beam.sdk.extensions.sql.zetasql;
-
-// Dummy class for testing spotbugs results in Jenkins.
-// TODO(BEAM-11714) Delete class before merge
-
-public class Dummy implements Cloneable {
-
-}
