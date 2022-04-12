ba973830ad2dc29ef98d6d9b08dd47845d9af288
Ryan Thompson <ryanthompson591@gmail.com>
2022-04-12 13:57:22 -0400

    moved impersonation into a single parameter

--- sdks/python/apache_beam/runners/interactive/utils.py
+++ sdks/python/apache_beam/runners/interactive/utils.py
@@ -453,11 +453,10 @@ def assert_bucket_exists(bucket_name, pipeline_options=None):
     from apitools.base.py.exceptions import HttpError
     if pipeline_options:
       gcs_options = pipeline_options.view_as(GoogleCloudOptions)
-      target_principal = gcs_options.target_principal
-      delegate_accounts = gcs_options.delegate_accounts
-      auth.set_impersonation_accounts(target_principal, delegate_accounts)
+      impersonate_service_account = gcs_options.impersonate_service_account
+      auth.set_impersonation_accounts(impersonate_service_account)
     else:
-      auth.set_impersonation_accounts(None, None)
+      auth.set_impersonation_accounts(None)
     storage_client = storage.StorageV1(
         credentials=auth.get_service_credentials(),
         get_credentials=False,
