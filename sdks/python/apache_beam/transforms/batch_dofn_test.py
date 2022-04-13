46c9bbd813fd18cecb313fc2c095410cf4d180f2
Brian Hulette <bhulette@google.com>
2022-04-13 14:56:13 -0700

    Remove BatchDoFnTestCase, verify beam.Map DoFn

--- sdks/python/apache_beam/transforms/batch_dofn_test.py
+++ sdks/python/apache_beam/transforms/batch_dofn_test.py
@@ -22,7 +22,6 @@
 import unittest
 from typing import Iterator
 from typing import List
-from typing import NamedTuple
 from typing import Optional
 
 from parameterized import parameterized_class
@@ -31,14 +30,14 @@ import apache_beam as beam
 
 
 class ElementDoFn(beam.DoFn):
-  def process(self, element: int, *args, **kwargs) -> Iterator[int]:
-    yield element
+  def process(self, element: int, *args, **kwargs) -> Iterator[float]:
+    yield element / 2
 
 
 class BatchDoFn(beam.DoFn):
   def process_batch(self, batch: List[int], *args,
-                    **kwargs) -> Iterator[List[int]]:
-    yield [element * 2 for element in batch]
+                    **kwargs) -> Iterator[List[float]]:
+    yield [element / 2 for element in batch]
 
 
 class BatchDoFnNoReturnAnnotation(beam.DoFn):
@@ -47,64 +46,50 @@ class BatchDoFnNoReturnAnnotation(beam.DoFn):
 
 
 class EitherDoFn(beam.DoFn):
-  def process(self, element: int, *args, **kwargs) -> Iterator[int]:
-    yield element
+  def process(self, element: int, *args, **kwargs) -> Iterator[float]:
+    yield element / 2
 
   def process_batch(self, batch: List[int], *args,
-                    **kwargs) -> Iterator[List[int]]:
-    yield [element * 2 for element in batch]
+                    **kwargs) -> Iterator[List[float]]:
+    yield [element / 2 for element in batch]
 
 
-class BatchDoFnTestCase(NamedTuple):
-  dofn: beam.DoFn
-  process_defined: bool
-  process_batch_defined: bool
-  input_batch_type: Optional[type]
-  output_batch_type: Optional[type]
+def get_test_class_name(cls, num, params_dict):
+  return "%s_%s" % (cls.__name__, params_dict['dofn'].__class__.__name__)
 
 
-def make_map_pardo():
-  return beam.Map(lambda x: x * 2).dofn
-
-
-@parameterized_class(
-    BatchDoFnTestCase.__annotations__.keys(),
-    [
-        BatchDoFnTestCase(
-            dofn=ElementDoFn(),
-            process_defined=True,
-            process_batch_defined=False,
-            input_batch_type=None,
-            output_batch_type=None),
-        BatchDoFnTestCase(
-            dofn=BatchDoFn(),
-            process_defined=False,
-            process_batch_defined=True,
-            input_batch_type=beam.typehints.List[int],
-            output_batch_type=beam.typehints.List[int]),
-        BatchDoFnTestCase(
-            dofn=BatchDoFnNoReturnAnnotation(),
-            process_defined=False,
-            process_batch_defined=True,
-            input_batch_type=beam.typehints.List[int],
-            output_batch_type=beam.typehints.List[int]),
-        BatchDoFnTestCase(
-            dofn=EitherDoFn(),
-            process_defined=True,
-            process_batch_defined=True,
-            input_batch_type=beam.typehints.List[int],
-            output_batch_type=beam.typehints.List[int]),
-        #BatchDoFnTestCase(
-        #    dofn=make_map_pardo().dofn,
-        #    process_defined=True,
-        #    process_batch_defined=False,
-        #    input_batch_type=None,
-        #    batch_output_type=None),
-    ],
-    class_name_func=lambda _,
-    __,
-    params: params['dofn'].__class__.__name__)
-class BatchDoFnTest(unittest.TestCase):
+@parameterized_class([
+    {
+        "dofn": ElementDoFn(),
+        "process_defined": True,
+        "process_batch_defined": False,
+        "input_batch_type": None,
+        "output_batch_type": None
+    },
+    {
+        "dofn": BatchDoFn(),
+        "process_defined": False,
+        "process_batch_defined": True,
+        "input_batch_type": beam.typehints.List[int],
+        "output_batch_type": beam.typehints.List[float]
+    },
+    {
+        "dofn": BatchDoFnNoReturnAnnotation(),
+        "process_defined": False,
+        "process_batch_defined": True,
+        "input_batch_type": beam.typehints.List[int],
+        "output_batch_type": beam.typehints.List[int]
+    },
+    {
+        "dofn": EitherDoFn(),
+        "process_defined": True,
+        "process_batch_defined": True,
+        "input_batch_type": beam.typehints.List[int],
+        "output_batch_type": beam.typehints.List[float]
+    },
+],
+                     class_name_func=get_test_class_name)
+class BatchDoFnParameterizedTest(unittest.TestCase):
   def test_process_defined(self):
     self.assertEqual(self.dofn.process_defined, self.process_defined)
 
@@ -117,6 +102,18 @@ class BatchDoFnTest(unittest.TestCase):
 
   def test_get_output_batch_type(self):
     self.assertEqual(self.dofn.get_output_batch_type(), self.output_batch_type)
+
+
+class BatchDoFnTest(unittest.TestCase):
+  def test_map_pardo(self):
+    # verify batch dofn accessors work well with beam.Map generated DoFn
+    # checking this in parameterized test causes a circular reference issue
+    dofn = beam.Map(lambda x: x * 2).dofn
+
+    self.assertTrue(dofn.process_defined)
+    self.assertFalse(dofn.process_batch_defined)
+    self.assertEqual(dofn.get_input_batch_type(), None)
+    self.assertEqual(dofn.get_output_batch_type(), None)
 
 
 if __name__ == '__main__':
