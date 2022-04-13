0ea7e263ba0481f4fcfcf453935e09f7c06df2ce
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2022-04-13 14:25:56 +0700

    add apache license

--- sdks/go/pkg/beam/io/parquetio/parquetio.go
+++ sdks/go/pkg/beam/io/parquetio/parquetio.go
@@ -1,3 +1,22 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// This module contains all Go code used for Beam's SDKs. This file is placed
+// in this directory in order to cover the go code required for Java and Python
+// containers, as well as the entire Go SDK. Placing this file in the repository
+// root is not possible because it causes conflicts with a pre-existing vendor
+// directory.
 package parquetio
 
 import (
