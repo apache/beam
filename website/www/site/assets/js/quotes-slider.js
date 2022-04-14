8942cce620fa3d1c9a01d4a6dcdf4c641ccda1e1
bulat safiullin <bulat.safiullin@akvelon.com>
2022-04-14 20:51:33 +0600

    [BEAM-14145] [Website] delete keen-slider, add slicker, change css, html structure

--- website/www/site/assets/js/quotes-slider.js
+++ website/www/site/assets/js/quotes-slider.js
@@ -10,123 +10,32 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-const tabletWidth = 1024;
-
-function calcBodyWidth() {
-  if (window.innerWidth && document.documentElement.clientWidth) {
-    return Math.min(window.innerWidth, document.documentElement.clientWidth);
-  } else {
-      return (
-        window.innerWidth ||
-        document.documentElement.clientWidth ||
-        document.getElementsByTagName('body')[0].clientWidth
-      );
+$(document).ready(function(){
+  const SelectorNames = {
+    slider: 'slick-JS',
+    dotsWrapper: 'dots-wrapper',
+    dots: 'dots',
   }
-}
 
-function addWrapperOverGroupOfCards(wrappingData) {
-  const {
-    isDesktopWidth,
-    actualCountOfSlides,
-    CountOfSlides,
-    classOneSlide,
-    classOneSlideExtra,
-    slidesDOM,
-    currentSliderDOM
-  } = wrappingData;
+  $(`.${SelectorNames.slider}`).slick({
+    mobileFirst: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
 
-  if (isDesktopWidth) {
-    var numOfExtraGroupWrappers = Math.ceil(actualCountOfSlides / CountOfSlides.renderedOnDesktop);
-    var extraGroupWrappers = new Array(numOfExtraGroupWrappers).fill(null).map(() => {
-      var extraGroupWrapperDOM = document.createElement('div');
-      extraGroupWrapperDOM.classList.add(classOneSlide, classOneSlideExtra);
-      return extraGroupWrapperDOM;
-    });
-    slidesDOM.forEach((oneSlideDOM, idx) => {
-      oneSlideDOM.classList.remove(classOneSlide);
-      extraGroupWrappers[Math.floor(idx / CountOfSlides.renderedOnDesktop)].append(oneSlideDOM);
-    });
-    extraGroupWrappers.forEach((oneExtraGroupWrapper) => {
-      currentSliderDOM.append(oneExtraGroupWrapper);
-    });
-  }
-}
-
-function updateClasses(instance) {
-  var slide = instance.details().relativeSlide;
-  var dots = document.querySelectorAll(".dot");
-  dots.forEach(function (dot, idx) {
-    idx === slide
-      ? dot.classList.add("dot--active")
-      : dot.classList.remove("dot--active");
+    arrows: false,
+    dots: true,
+    appendDots: `.${SelectorNames.dotsWrapper}`,
+    dotsClass: SelectorNames.dots,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          infinite: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+    ]
   });
-}
-
-(function () {
-  var CountOfSlides = {
-    min: 1,
-    renderedOnDesktop: 3,
-  }
-
-  var Selectors = {
-    desktopSlider: '.quotes-desktop.keen-slider-JS',
-    mobileSlider: '.quote-mobile.keen-slider-JS',
-    oneSlide: '.keen-slider__slide',
-  }
-
-  var classOneSlideExtra = 'wrap-slide';
-  var classOneSlide = 'keen-slider__slide';
-  var classVisible = 'visible';
-
-  var bodyWidth = calcBodyWidth();
-  var isDesktopWidth = bodyWidth >= tabletWidth;
-
-  var currentSliderSelector = isDesktopWidth ? Selectors.desktopSlider : Selectors.mobileSlider;
-  var currentSliderDOM = document.querySelector(currentSliderSelector);
-  currentSliderDOM.classList.add(classVisible);
-
-  var slidesDOM = currentSliderDOM.querySelectorAll(".keen-slider__slide");
-  var actualCountOfSlides = currentSliderDOM.querySelectorAll(Selectors.oneSlide).length;
-
-  addWrapperOverGroupOfCards({
-    isDesktopWidth,
-    actualCountOfSlides,
-    CountOfSlides,
-    classOneSlide,
-    classOneSlideExtra,
-    slidesDOM,
-    currentSliderDOM
-  });
-
-  var isLoop = true;
-  if (isDesktopWidth && actualCountOfSlides <= CountOfSlides.renderedOnDesktop) {
-    isLoop = false;
-  }
-  if (!isDesktopWidth && actualCountOfSlides === CountOfSlides.min) {
-    isLoop = false;
-  }
-
-  new KeenSlider(currentSliderSelector, {
-    slidesPerView: CountOfSlides.min,
-    loop: isLoop,
-    created: function (instance) {
-      if (isLoop) {
-        var dots_wrapper = document.getElementById("dots");
-        var slides = currentSliderDOM.querySelectorAll(".keen-slider__slide");
-        slides.forEach(function (_, idx) {
-          var dot = document.createElement("button");
-          dot.classList.add("dot");
-          dots_wrapper.appendChild(dot);
-          dot.addEventListener("click", function () {
-            instance.moveToSlide(idx);
-          });
-        });
-        updateClasses(instance);
-      }
-    },
-    slideChanged(instance) {
-      updateClasses(instance);
-    }
-  });
-
-}());
+});
