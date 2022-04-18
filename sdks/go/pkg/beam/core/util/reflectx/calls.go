1deeaa7e54568c343fc874186ab0dd446773ce14
Jack McCluskey <34928439+jrmccluskey@users.noreply.github.com>
2022-04-18 16:59:05 -0400

    [BEAM-14290] Address staticcheck warnings in the reflectx package (#17373)

--- sdks/go/pkg/beam/core/util/reflectx/calls.go
+++ sdks/go/pkg/beam/core/util/reflectx/calls.go
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//lint:file-ignore S1023 redundant return statements are generated
+
 package reflectx
 
 import (
