5ca09d3c8b929b0f715a871650e9056eea93b7ec
Daniel Oliveira <daniel.o.programmer@gmail.com>
2022-04-20 15:28:38 -0700

    Fixup: Fix gradle build and undo VR script changes.

--- sdks/go/test/run_validatesrunner_tests.sh
+++ sdks/go/test/run_validatesrunner_tests.sh
@@ -114,9 +114,6 @@ exit_background_processes () {
   if [[ ! -z "$DEBEZIUMIO_EXPANSION_PID" ]]; then
     kill -9 $DEBEZIUMIO_EXPANSION_PID
   fi
-  if [[ ! -z "$GCPIO_EXPANSION_PID" ]]; then
-    kill -9 $GCPIO_EXPANSION_PID
-  fi
 }
 trap exit_background_processes SIGINT SIGTERM EXIT
 
@@ -229,16 +226,6 @@ case $key in
         shift # past argument
         shift # past value
         ;;
-    --gcpio_expansion_jar)
-        GCPIO_EXPANSION_JAR="$2"
-        shift # past argument
-        shift # past value
-        ;;
-    --gcpio_expansion_addr)
-        GCPIO_EXPANSION_ADDR="$2"
-        shift # past argument
-        shift # past value
-        ;;
     --sdk_overrides)
         SDK_OVERRIDES="$2"
         shift # past argument
@@ -345,13 +332,6 @@ if [[ "$RUNNER" != "direct" ]]; then
       java -jar $DEBEZIUMIO_EXPANSION_JAR $EXPANSION_PORT &
       DEBEZIUMIO_EXPANSION_PID=$!
   fi
-  if [[ -z "$GCPIO_EXPANSION_ADDR" && -n "$GCPIO_EXPANSION_JAR" ]]; then
-      EXPANSION_PORT=$(python3 -c "$SOCKET_SCRIPT")
-      GCPIO_EXPANSION_ADDR="localhost:$EXPANSION_PORT"
-      echo "No GCP IO expansion address specified; starting a new GCP IO expansion server on $GCPIO_EXPANSION_ADDR"
-      java -jar $GCPIO_EXPANSION_JAR $EXPANSION_PORT &
-      GCPIO_EXPANSION_PID=$!
-  fi
 fi
 
 # Disable parallelism on runners that don't support it.
@@ -393,7 +373,7 @@ if [[ "$RUNNER" == "dataflow" ]]; then
   # Push the container
   gcloud docker -- push $CONTAINER:$TAG
 
-  if [[ -n "$TEST_EXPANSION_ADDR" || -n "$IO_EXPANSION_ADDR" || -n "$SCHEMAIO_EXPANSION_ADDR" || -n "$DEBEZIUMIO_EXPANSION_ADDR" || -n "$GCPIO_EXPANSION_ADDR" ]]; then
+  if [[ -n "$TEST_EXPANSION_ADDR" || -n "$IO_EXPANSION_ADDR" || -n "$SCHEMAIO_EXPANSION_ADDR" || -n "$DEBEZIUMIO_EXPANSION_ADDR" ]]; then
     ARGS="$ARGS --experiments=use_portable_job_submission"
 
     if [[ -z "$SDK_OVERRIDES" ]]; then
@@ -445,9 +425,6 @@ fi
 if [[ -n "$DEBEZIUMIO_EXPANSION_ADDR" ]]; then
   ARGS="$ARGS --debeziumio_expansion_addr=$DEBEZIUMIO_EXPANSION_ADDR"
 fi
-if [[ -n "$GCPIO_EXPANSION_ADDR" ]]; then
-  ARGS="$ARGS --gcpio_expansion_addr=$GCPIO_EXPANSION_ADDR"
-fi
 if [[ -n "$SDK_OVERRIDES" ]]; then
   OVERRIDE=--sdk_harness_container_image_override="$SDK_OVERRIDES"
   ARGS="$ARGS $OVERRIDE"
@@ -465,7 +442,7 @@ if [[ "$RUNNER" == "dataflow" ]]; then
   docker rmi $CONTAINER:$TAG || echo "Failed to remove container"
   gcloud --quiet container images delete $CONTAINER:$TAG || echo "Failed to delete container"
 
-  if [[ -n "$TEST_EXPANSION_ADDR" || -n "$IO_EXPANSION_ADDR" || -n "$SCHEMAIO_EXPANSION_ADDR" || -n "$DEBEZIUMIO_EXPANSION_ADDR" || -n "$GCPIO_EXPANSION_ADDR" ]]; then
+  if [[ -n "$TEST_EXPANSION_ADDR" || -n "$IO_EXPANSION_ADDR" || -n "$SCHEMAIO_EXPANSION_ADDR" || -n "$DEBEZIUMIO_EXPANSION_ADDR" ]]; then
     # Delete the java cross-language container locally and remotely
     docker rmi $JAVA_CONTAINER:$JAVA_TAG || echo "Failed to remove container"
     gcloud --quiet container images delete $JAVA_CONTAINER:$JAVA_TAG || echo "Failed to delete container"
