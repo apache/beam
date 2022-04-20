e72e7a7df2611083656261fc316720405d7c6eb5
ahmedabu98 <ahmedabualsaud@google.com>
2022-04-20 16:43:40 +0000

    created test data

--- sdks/python/apache_beam/io/gcp/bigquery_json_it_test.py
+++ sdks/python/apache_beam/io/gcp/bigquery_json_it_test.py
@@ -20,6 +20,7 @@ Integration tests for BigQuery's JSON data type
 
 import logging
 import unittest
+import json
 
 from apache_beam.testing.test_pipeline import TestPipeline
 
@@ -31,6 +32,226 @@ class BigQueryJsonIT(unittest.TestCase):
   def setUpClass(cls):
     cls.test_pipeline = TestPipeline(is_integration_test=True)
     cls.args = cls.test_pipeline.get_full_options_as_args()
+
+  # Expected data for query test
+  def generate_query_data(self):
+    JSON_QUERY_DATA = [
+      {'country_code': 'usa',
+       'past_leader': '\"George W. Bush\"',
+       'gdp': '58559.675',
+       'city_name': '\"Los Angeles\"',
+       'landmark_name': '\"Golden Gate Bridge\"'},
+      {'country_code': 'aus',
+       'past_leader': '\"Kevin Rudd\"',
+       'gdp': '58043.581',
+       'city_name': '\"Melbourne\"',
+       'landmark_name': '\"Great Barrier Reef\"'},
+      {'country_code': 'special',
+       'past_leader': '\"!@#$%^&*()_+\"',
+       'gdp': '421.7',
+       'city_name': '\"Bikini Bottom\"',
+       'landmark_name': "\"Willy Wonka's Factory\""}
+    ]
+    return JSON_QUERY_DATA
+
+  def generate_data(self):
+    # Raw country data
+    usa = {
+      "name": "United States of America",
+      "population": 329484123,
+      "cities": {
+        "nyc": {
+          "name": "New York City",
+          "state": "NY",
+          "population": 8622357
+        },
+        "la": {
+          "name": "Los Angeles",
+          "state": "CA",
+          "population": 4085014
+        },
+        "chicago": {
+          "name": "Chicago",
+          "state": "IL",
+          "population": 2670406
+        },
+      },
+      "past_leaders": [
+        "Donald Trump",
+        "Barack Obama",
+        "George W. Bush",
+        "Bill Clinton"
+      ],
+      "in_northern_hemisphere": True
+    }
+
+    aus = {
+      "name": "Australia",
+      "population": 25687041,
+      "cities": {
+        "sydney": {
+          "name": "Sydney",
+          "state": "New South Wales",
+          "population": 5367206
+        },
+        "melbourne": {
+          "name": "Melbourne",
+          "state": "Victoria",
+          "population": 5159211
+        },
+        "brisbane": {
+          "name": "Brisbane",
+          "state": "Queensland",
+          "population": 2560720
+        }
+      },
+      "past_leaders": [
+        "Malcolm Turnbull",
+        "Tony Abbot",
+        "Kevin Rudd",
+      ],
+      "in_northern_hemisphere": False
+    }
+
+    special = {
+      "name": "newline\n, form\f, tab\t, \"quotes\", \\backslash\\, backspace\b, \u0000_hex_\u0f0f",
+      "population": -123456789,
+      "cities": {
+        "basingse": {
+          "name": "Ba Sing Se",
+          "state": "The Earth Kingdom",
+          "population": 200000
+        },
+        "bikinibottom": {
+          "name": "Bikini Bottom",
+          "state": "The Pacific Ocean",
+          "population": 50000
+        }
+      },
+      "past_leaders": [
+        "1",
+        "2",
+        "!@#$%^&*()_+",
+      ],
+      "in_northern_hemisphere": True
+    }
+
+    landmarks = {
+      "usa_0": {
+        "name": "Statue of Liberty",
+        "cool rating": None
+      },
+      "usa_1": {
+        "name": "Golden Gate Bridge",
+        "cool rating": "very cool"
+      },
+      "usa_2": {
+        "name": "Grand Canyon",
+        "cool rating": "very very cool"
+      },
+      "aus_0": {
+        "name": "Sydney Opera House",
+        "cool rating": "amazing"
+      },
+      "aus_1": {
+        "name": "Great Barrier Reef",
+        "cool rating": None
+      },
+      "special_0": {
+        "name": "Hogwarts School of WitchCraft and Wizardry",
+        "cool rating": "magical"
+      },
+      "special_1": {
+        "name": "Willy Wonka's Factory",
+        "cool rating": None
+      },
+      "special_2": {
+        "name": "Rivendell",
+        "cool rating": "precious"
+      },
+    }
+    stats = {
+      "usa_gdp_per_capita": {
+        "gdp_per_capita": 58559.675,
+        "currency": "constant 2015 US$"
+      },
+      "usa_co2_emissions": {
+        "co2 emissions": 15.241,
+        "measurement": "metric tons per capita",
+        "year": 2018
+      },
+      "aus_gdp_per_capita": {
+        "gdp_per_capita": 58043.581,
+        "currency": "constant 2015 US$"
+      },
+      "aus_co2_emissions": {
+        "co2 emissions": 15.476,
+        "measurement": "metric tons per capita",
+        "year": 2018
+      },
+      "special_gdp_per_capita": {
+        "gdp_per_capita": 421.70,
+        "currency": "constant 200 BC gold"
+      },
+      "special_co2_emissions": {
+        "co2 emissions": -10.79,
+        "measurement": "metric tons per capita",
+        "year": 2018
+      }
+    }
+
+    JSON_DATA = {
+      "usa": {
+        "country": json.dumps(usa),
+        "cities": {
+          "nyc": json.dumps(usa["cities"]["nyc"]),
+          "la": json.dumps(usa["cities"]["la"]),
+          "chicago": json.dumps(usa["cities"]["chicago"])
+        },
+        "landmarks": [
+          json.dumps(landmarks["usa_0"]),
+          json.dumps(landmarks["usa_1"]),
+          json.dumps(landmarks["usa_2"])
+        ],
+        "stats": {
+          "gdp_per_capita": json.dumps(stats["usa_gdp_per_capita"]),
+          "co2_emissions": json.dumps(stats["usa_co2_emissions"])
+        }
+      },
+      "aus": {
+        "country": json.dumps(aus),
+        "cities": {
+          "sydney": json.dumps(aus["cities"]["sydney"]),
+          "melbourne": json.dumps(aus["cities"]["melbourne"]),
+          "brisbane": json.dumps(aus["cities"]["brisbane"])
+        },
+        "landmarks": [
+          json.dumps(landmarks["aus_0"]),
+          json.dumps(landmarks["aus_1"])
+        ],
+        "stats": {
+          "gdp_per_capita": json.dumps(stats["aus_gdp_per_capita"]),
+          "co2_emissions": json.dumps(stats["aus_co2_emissions"])
+        }
+      },
+      "special": {
+        "country": json.dumps(special),
+        "cities": {
+          "basingse": json.dumps(special["cities"]["basingse"]),
+          "bikinibottom": json.dumps(special["cities"]["bikinibottom"])
+        },
+        "landmarks": [
+          json.dumps(landmarks["special_0"]),
+          json.dumps(landmarks["special_1"]),
+          json.dumps(landmarks["special_2"])
+        ],
+        "stats": {
+          "gdp_per_capita": json.dumps(stats["special_gdp_per_capita"]),
+          "co2_emissions": json.dumps(stats["special_co2_emissions"])
+        }
+      }
+    }
+    return JSON_DATA
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
