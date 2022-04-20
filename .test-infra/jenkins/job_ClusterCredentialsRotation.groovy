05506a84fc6676632b9df5d3ed213f56fe8109ab
Elias Segundo <elias.segundo@luisrazo.local>
2022-04-20 13:10:23 -0500

    Switching to --quiet instead of printf

--- .test-infra/jenkins/job_ClusterCredentialsRotation.groovy
+++ .test-infra/jenkins/job_ClusterCredentialsRotation.groovy
@@ -32,32 +32,32 @@ job('Rotate Cluster Credentials') {
     //Credentials rotation for metrics and io-datastores
 
     //Set a maintenance window
-    shell('''printf 'yes'| gcloud container clusters update metrics \
-    --zone=us-central1-a --maintenance-window=06:00''')
+    shell('''gcloud container clusters update metrics \
+    --zone=us-central1-a --maintenance-window=06:00 --quiet''')
 
-    shell('''printf 'yes'| gcloud container clusters update io-datastores \
-    --zone=us-central1-a --maintenance-window=06:00''')
+    shell('''gcloud container clusters update io-datastores \
+    --zone=us-central1-a --maintenance-window=06:00 --quiet''')
 
     //Starting credential rotation
     // it's necessary to rebuild the nodes after rotation to avoid apiservices issues
-    shell('''printf 'yes' | gcloud container clusters update metrics \
-    --start-credential-rotation --zone=us-central1-a''')
+    shell('''gcloud container clusters update metrics \
+    --start-credential-rotation --zone=us-central1-a --quiet''')
 
-    shell('''printf 'yes' | gcloud container clusters update io-datastores \
-    --start-credential-rotation --zone=us-central1-a''')
+    shell('''gcloud container clusters update io-datastores \
+    --start-credential-rotation --zone=us-central1-a --quiet''')
 
     //Rebuilding the nodes
-    shell('''printf 'yes' | gcloud container clusters upgrade metrics \
-    --node-pool=default-pool --zone=us-central1-a''')
+    shell('''gcloud container clusters upgrade metrics \
+    --node-pool=default-pool --zone=us-central1-a --quiet''')
 
-    shell('''printf 'yes' | gcloud container clusters upgrade io-datastores \
-    --node-pool=default-pool --zone=us-central1-a''')
+    shell('''gcloud container clusters upgrade io-datastores \
+    --node-pool=default-pool --zone=us-central1-a --quiet''')
 
     //Completing the rotation
-    shell('''printf 'yes' | gcloud container clusters update metrics\
-    --complete-credential-rotation --zone=us-central1-a''')
+    shell('''gcloud container clusters update metrics \
+    --complete-credential-rotation --zone=us-central1-a --quiet''')
 
-    shell('''printf 'yes' | gcloud container clusters update io-datastores \
-    --complete-credential-rotation --zone=us-central1-a''')
+    shell('''gcloud container clusters update io-datastores \
+    --complete-credential-rotation --zone=us-central1-a --quiet''')
   }
 }
