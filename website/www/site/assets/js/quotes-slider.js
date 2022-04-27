bd11b0f1c1d0e02e286daa1c12032a81a9b2ed53
bulat safiullin <bulat.safiullin@akvelon.com>
2022-04-27 16:53:59 +0600

    [BEAM-14145] [Website] add comment for swiper 1024px breakpoint

--- website/www/site/assets/js/quotes-slider.js
+++ website/www/site/assets/js/quotes-slider.js
@@ -21,7 +21,7 @@ new Swiper('.swiper', {
     bulletActiveClass: "bullet-active-class-custom",
   },
   breakpoints: {
-    // when window width is > 1024px
+    // switches from mobile to desktop when window width is > 1024px
     1025: {
       slidesPerView: 3,
       slidesPerGroup: 3,
