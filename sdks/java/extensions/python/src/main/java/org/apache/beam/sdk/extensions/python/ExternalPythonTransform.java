5e24a969f2cc2befabb9b65a01fcbf7726bdf1dc
Andrei Gurau <andreigurau@google.com>
2022-04-27 13:43:55 -0400

    made sure python extension folder compiled successfully

--- sdks/java/extensions/python/src/main/java/org/apache/beam/sdk/extensions/python/ExternalPythonTransform.java
+++ sdks/java/extensions/python/src/main/java/org/apache/beam/sdk/extensions/python/ExternalPythonTransform.java
@@ -18,6 +18,7 @@
 package org.apache.beam.sdk.extensions.python;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
@@ -244,7 +245,8 @@ public class ExternalPythonTransform<InputT extends PInput, OutputT extends POut
             fieldName,
             StaticSchemaInference.fieldFromType(
                 TypeDescriptor.of(field.getClass()),
-                JavaFieldSchema.JavaFieldTypeSupplier.INSTANCE));
+                JavaFieldSchema.JavaFieldTypeSupplier.INSTANCE,
+                new HashSet<>()));
       }
 
       counter++;
