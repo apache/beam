9c6f78124e1de8de082805fcdca40b4906761c8f
Anand Inguva <anandinguva98@gmail.com>
2022-04-27 17:11:26 -0400

    Add __init__ file to make the inference as package

---
+++ sdks/python/apache_beam/ml/inference/__init__.py
@@ -0,0 +1,27 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Beam RunInference API
+- For high-level documentation see
+  TODO(BEAM-14046): Add and link to help documentation
+- :mod:`apache_beam.ml.inference.api`: Main RunInference interface
+- :mod:`apache_beam.ml.inference.base`: RunInference base class
+- :mod:`apache_beam.ml.inference.pytorch_inference`: Pytorch RunInference
+  implementation
+- :mod:`apache_beam.ml.inference.sklearn_inference`: Scikit-learn RunInference
+  implementation
+"""
