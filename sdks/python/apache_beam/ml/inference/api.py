912e8e8d0147c771b01dc416d0a2f779443fc694
Ryan Thompson <ryanthompson591@gmail.com>
2022-04-29 12:41:13 -0400

    added comment about runiference being under development

--- sdks/python/apache_beam/ml/inference/api.py
+++ sdks/python/apache_beam/ml/inference/api.py
@@ -38,6 +38,9 @@ class PredictionResult:
 @beam.typehints.with_output_types(Union[PredictionResult, Tuple[_K, PredictionResult]])  # pylint: disable=line-too-long
 class RunInference(beam.PTransform):
   """
+  NOTE: (04/29/22) This API and its implementation is under development and
+  does not promise backward compatibility.
+
   A transform that takes a PCollection of examples (or features) to be used on
   an ML model. It will then output inferences (or predictions) for those
   examples in a PCollection of PredictionResults, containing the input examples
