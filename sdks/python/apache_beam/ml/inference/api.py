f1e5be21b17b21002cb8ae4aae4c33ebd98ce028
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2022-05-04 08:13:55 +0000

    add __Init__ to inference. (#17514)
    Co-authored-by: Andy Ye <andyye333@gmail.com>

--- sdks/python/apache_beam/ml/inference/api.py
+++ sdks/python/apache_beam/ml/inference/api.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# mypy: ignore-errors
 
 from dataclasses import dataclass
 from typing import Tuple
@@ -51,7 +52,7 @@ class RunInference(beam.PTransform):
 
   TODO(BEAM-14046): Add and link to help documentation
   """
-  def __init__(self, model_loader: base.ModelLoader) -> beam.pvalue.PCollection:
+  def __init__(self, model_loader: base.ModelLoader):
     self._model_loader = model_loader
 
   def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
