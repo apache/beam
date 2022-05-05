10235f2c7282859fbf8b0fc32a35a7498a3b5560
Robert Burke <lostluck@users.noreply.github.com>
2022-05-05 10:02:22 -0700

    [BEAM-14173] Fix Go Loadtests on Dataflow & partial fix for Flink (#17554)

--- .test-infra/jenkins/job_LoadTests_coGBK_Go.groovy
+++ .test-infra/jenkins/job_LoadTests_coGBK_Go.groovy
@@ -22,6 +22,8 @@ import LoadTestsBuilder as loadTestsBuilder
 import PhraseTriggeringPostCommitBuilder
 import InfluxDBCredentialsHelper
 
+import static LoadTestsBuilder.DOCKER_CONTAINER_REGISTRY
+
 String now = new Date().format("MMddHHmmss", TimeZone.getTimeZone('UTC'))
 
 def batchScenarios = {
@@ -53,6 +55,8 @@ def batchScenarios = {
         iterations           : 1,
         num_workers          : 5,
         autoscaling_algorithm: 'NONE',
+        environment_type     : 'DOCKER',
+        environment_config   : "${DOCKER_CONTAINER_REGISTRY}/beam_go_sdk:latest",
       ]
     ],
     [
@@ -82,6 +86,8 @@ def batchScenarios = {
         iterations           : 1,
         num_workers          : 5,
         autoscaling_algorithm: 'NONE',
+        environment_type     : 'DOCKER',
+        environment_config   : "${DOCKER_CONTAINER_REGISTRY}/beam_go_sdk:latest",
       ]
     ],
     [
@@ -111,6 +117,8 @@ def batchScenarios = {
         iterations           : 4,
         num_workers          : 5,
         autoscaling_algorithm: 'NONE',
+        environment_type     : 'DOCKER',
+        environment_config   : "${DOCKER_CONTAINER_REGISTRY}/beam_go_sdk:latest",
       ]
     ],
     [
@@ -140,6 +148,8 @@ def batchScenarios = {
         iterations           : 4,
         num_workers          : 5,
         autoscaling_algorithm: 'NONE',
+        environment_type     : 'DOCKER',
+        environment_config   : "${DOCKER_CONTAINER_REGISTRY}/beam_go_sdk:latest",
       ]
     ],
   ]
