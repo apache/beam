6f924251aa3ea7e938d024180c4388db8de276ec
Ryan Thompson <ryanthompson591@gmail.com>
2022-05-11 15:12:59 -0400

    remove set_impersonation_accounts

--- sdks/python/apache_beam/io/gcp/bigquery_tools.py
+++ sdks/python/apache_beam/io/gcp/bigquery_tools.py
@@ -1360,11 +1360,6 @@ class BigQueryReader(dataflow_io.NativeSourceReader):
           source.pipeline_options.view_as(GoogleCloudOptions).project)
     else:
       self.executing_project = None
-    if hasattr(source, 'pipeline_options'):
-      gcs_options = source.pipeline_options.view_as(GoogleCloudOptions)
-      auth.set_impersonation_accounts(gcs_options.impersonate_service_account)
-    else:
-      auth.set_impersonation_accounts(None)
 
     # TODO(silviuc): Try to automatically get it from gcloud config info.
     if not self.executing_project and test_bigquery_client is None:
