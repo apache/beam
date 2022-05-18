7b29c65c684524b761f47a76bb99a55bbef09078
Elias Segundo <elias.segundo@luisrazo.local>
2022-05-18 15:54:20 -0500

    Adding bash script for cleanup

--- .test-infra/dataproc/cleanup.sh
+++ .test-infra/dataproc/cleanup.sh
@@ -14,6 +14,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+set -e
 GCLOUD_REGION="us-central1"
 clustersList=( )
 toDeleteList=( )
