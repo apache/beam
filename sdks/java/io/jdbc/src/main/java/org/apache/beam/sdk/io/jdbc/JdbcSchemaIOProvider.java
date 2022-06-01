929ec4dbd32d87bd9096683124baf3ce18b61dfa
Pablo <pabloem@users.noreply.github.com>
2022-06-01 18:43:44 -0400

    Support JdbcIO autosharding in Python (#16921)
    * Support JdbcIO autosharding in Python
    
    * moving autosharding to proper spot
    
    * fix spotless
    
    * fix encoding issue

--- sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcSchemaIOProvider.java
+++ sdks/java/io/jdbc/src/main/java/org/apache/beam/sdk/io/jdbc/JdbcSchemaIOProvider.java
@@ -68,6 +68,7 @@ public class JdbcSchemaIOProvider implements SchemaIOProvider {
         .addNullableField("writeStatement", FieldType.STRING)
         .addNullableField("fetchSize", FieldType.INT16)
         .addNullableField("outputParallelization", FieldType.BOOLEAN)
+        .addNullableField("autosharding", FieldType.BOOLEAN)
         .build();
   }
 
@@ -139,11 +140,15 @@ public class JdbcSchemaIOProvider implements SchemaIOProvider {
       return new PTransform<PCollection<Row>, PDone>() {
         @Override
         public PDone expand(PCollection<Row> input) {
-          return input.apply(
+          JdbcIO.Write<Row> writeRows =
               JdbcIO.<Row>write()
                   .withDataSourceConfiguration(getDataSourceConfiguration())
                   .withStatement(generateWriteStatement(input.getSchema()))
-                  .withPreparedStatementSetter(new JdbcUtil.BeamRowPreparedStatementSetter()));
+                  .withPreparedStatementSetter(new JdbcUtil.BeamRowPreparedStatementSetter());
+          if (config.getBoolean("autosharding") != null && config.getBoolean("autosharding")) {
+            writeRows = writeRows.withAutoSharding();
+          }
+          return input.apply(writeRows);
         }
       };
     }
