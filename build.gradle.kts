c6e95bbccb3b26f4da9d6735b5927b69d2157c86
Benjamin Gonzalez <benjamin.gonzalez@wizeline.com>
2022-06-10 14:07:37 -0500

    Sharding Kinesis IO test from the JavaPostCommit task

--- build.gradle.kts
+++ build.gradle.kts
@@ -198,7 +198,6 @@ tasks.register("javaPostCommit") {
   dependsOn(":sdks:java:extensions:zetasketch:postCommit")
   dependsOn(":sdks:java:io:jdbc:integrationTest")
   dependsOn(":sdks:java:io:google-cloud-platform:postCommit")
-  dependsOn(":sdks:java:io:kinesis:integrationTest")
   dependsOn(":sdks:java:io:amazon-web-services:integrationTest")
   dependsOn(":sdks:java:io:amazon-web-services2:integrationTest")
   dependsOn(":sdks:java:extensions:ml:postCommit")
