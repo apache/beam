25ca4f0fddd011bfc593e72eea6d32c040808b29
Yi Hu <yathu@google.com>
2022-06-28 15:23:04 -0400

    Followup sharding javaPostCommit (#22081)
    * Followup sharding javaPostCommit
    
    * Remove integration test depsOn that already sharded
    
    * update readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -196,9 +196,6 @@ tasks.register("javaPreCommitPortabilityApi") {
 tasks.register("javaPostCommit") {
   dependsOn(":sdks:java:extensions:google-cloud-platform-core:postCommit")
   dependsOn(":sdks:java:extensions:zetasketch:postCommit")
-  dependsOn(":sdks:java:io:debezium:integrationTest")
-  dependsOn(":sdks:java:io:jdbc:integrationTest")
-  dependsOn(":sdks:java:io:kinesis:integrationTest")
   dependsOn(":sdks:java:io:amazon-web-services:integrationTest")
   dependsOn(":sdks:java:io:amazon-web-services2:integrationTest")
   dependsOn(":sdks:java:extensions:ml:postCommit")
