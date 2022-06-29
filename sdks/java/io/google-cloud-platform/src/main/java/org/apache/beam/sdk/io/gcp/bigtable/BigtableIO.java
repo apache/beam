0af8af46c1c45dca516a51ef4d657717d0f4a37f
Svetak Sundhar <svetaksundhar@google.com>
2022-06-29 14:46:55 -0400

    added snippet

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigtable/BigtableIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigtable/BigtableIO.java
@@ -1377,7 +1377,8 @@ public class BigtableIO {
     public BigtableWriteException(KV<ByteString, Iterable<Mutation>> record, Throwable cause) {
       super(
           String.format(
-              "Error mutating row %s with mutations %s",
+              "Error mutating row %s with mutations %s. Check BigTable cluster for resource usage
+              and increase the number of nodes on the BigTable cluster, if necessary.",
               record.getKey().toStringUtf8(), record.getValue()),
           cause);
     }
