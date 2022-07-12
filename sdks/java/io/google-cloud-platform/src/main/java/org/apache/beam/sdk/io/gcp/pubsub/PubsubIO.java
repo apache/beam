267cd10980f228af79b7084212343738435c0c11
Denis Pyshev <git@gemelen.net>
2022-07-12 16:20:52 +0300

    Rename transform name according to review comment

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsub/PubsubIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsub/PubsubIO.java
@@ -530,7 +530,7 @@ public class PubsubIO {
    * PubsubMessage#getAttributeMap() attributes}, along with the {@link PubsubMessage#getMessageId()
    * messageId} and {PubsubMessage#getOrderingKey() orderingKey} from PubSub.
    */
-  public static Read<PubsubMessage> readMessagesWithAllAttributesAndMessageIdAndOrderingKey() {
+  public static Read<PubsubMessage> readMessagesWithAttributesAndMessageIdAndOrderingKey() {
     return Read.newBuilder().setCoder(PubsubMessageCoder.of()).setNeedsOrderingKey(true).build();
   }
 
