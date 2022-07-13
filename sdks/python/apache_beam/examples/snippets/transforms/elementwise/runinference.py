954d3ad163fdf6bc74b5ebc9f81527f7a277f56e
Anand Inguva <anandinguva98@gmail.com>
2022-07-13 16:47:03 +0530

    Add RunInference examples

--- sdks/python/apache_beam/examples/snippets/transforms/elementwise/runinference.py
+++ sdks/python/apache_beam/examples/snippets/transforms/elementwise/runinference.py
@@ -19,96 +19,132 @@
 # pytype: skip-file
 
 
-def images(test=None):
-  # [START images]
-import apache_beam as beam
-import torch
-from apache_beam.ml.inference.base import KeyedModelHandler
-from apache_beam.ml.inference.base import RunInference
-from apache_beam.ml.inference.pytorch_inference import PytorchModelHandlerTensor
-from torchvision.models import mobilenet_v2
+def torch_unkeyed_model_handler(test=None):
+  import apache_beam as beam
+  import numpy
+  import torch
+  from apache_beam.ml.inference.base import RunInference
+  from apache_beam.ml.inference.pytorch_inference import PytorchModelHandlerTensor
 
-# Save pretrained weights to a local file
-model = mobilenet_v2(pretrained=True)
-state_dict_path = 'mobilenet_v2_state_dict.pth'
-torch.save(model.state_dict(), 'mobilenet_v2_state_dict.pth')
+  class LinearRegression(torch.nn.Module):
+    def __init__(self, input_dim=1, output_dim=1):
+      super().__init__()
+      self.linear = torch.nn.Linear(input_dim, output_dim)
 
-# Load model and state_dict into the model handler
-model_class = mobilenet_v2
-model_params = {'num_classes': 1000}
-model_handler = KeyedModelHandler(
-   PytorchModelHandlerTensor(
-     state_dict_path=state_dict_path,
-     model_class=model_class,
-     model_params=model_params))
+    def forward(self, x):
+      out = self.linear(x)
+      return out
 
-# Run pipeline
-with beam.Pipeline() as p:
-    images = (
-        p
-        | 'CreateImages' >> beam.Create([
-            (
-              'img1',
-              torch.Tensor([
-                [[-1.5, -1, -.5],[-.5, 0, .5],[.5, 1, 1.5]], 
-                [[-1.5, -1, -.5],[-.5, 0, .5],[.5, 1, 1.5]],
-                [[-1.5, -1, -.5],[-.5, 0, .5],[.5, 1, 1.5]]
-              ])
-            ),
-        ])
-    )
+  model_state_dict_path = 'gs://apache-beam-samples/run_inference/five_times_table_torch.pt'  # pylint: disable=line-too-long
+  model_class = LinearRegression
+  model_params = {'input_dim': 1, 'output_dim': 1}
+  model_handler = PytorchModelHandlerTensor(
+      model_class=model_class,
+      model_params=model_params,
+      state_dict_path=model_state_dict_path)
+
+  unkeyed_data = numpy.array([10, 40, 60, 90],
+                             dtype=numpy.float32).reshape(-1, 1)
+
+  with beam.Pipeline() as p:
     predictions = (
-        images
-        | 'PyTorchRunInference' >> RunInference(model_handler)
-        | 'Print' >> beam.Map(print))
-
-    # [END images]
-    if test:
-      test(images)
-
-
-def digits(test=None):
-  # pylint: disable=line-too-long
-  # [START digits]
-from sklearn import svm
-from sklearn import datasets
-import pickle
-
-import apache_beam as beam
-from apache_beam.ml.inference.base import KeyedModelHandler
-from apache_beam.ml.inference.base import RunInference
-from apache_beam.ml.inference.sklearn_inference import ModelFileType
-from apache_beam.ml.inference.sklearn_inference import SklearnModelHandlerNumpy
-
-# Train and pickle model
-clf = svm.SVC()
-X, y = datasets.load_digits(return_X_y=True)
-clf.fit(X, y)
-model_uri = 'mnist_model.pkl'
-with open(model_uri, 'wb') as file:
-  pickle.dump(clf, file)
-
-# Load pickled model into the model handler
-model_handler = KeyedModelHandler(
-    SklearnModelHandlerNumpy(
-        model_file_type=ModelFileType.PICKLE,
-        model_uri=model_uri))
-
-# Run pipeline
-with beam.Pipeline() as p:
-    pixels = (
         p
-        | 'CreatePixels' >> beam.Create([
-            (y[1], X[1]),
-            (y[2], X[2]),
-        ])
-    )
-    predictions = (
-        pixels
-        | 'SklearnRunInference' >> RunInference(model_handler)
-        | 'Print' >> beam.Map(print))
-
-    # [END digits]
-    # pylint: enable=line-too-long
+        | 'InputData' >> beam.Create(unkeyed_data)
+        | 'ConvertNumpyToTensor' >> beam.Map(torch.Tensor)
+        | 'PytorchRunInference' >> RunInference(model_handler=model_handler)
+        | beam.Map(print))
     if test:
-      test(digits)
+      test(predictions)
+
+
+def torch_keyed_model_handler(test=None):
+  import apache_beam as beam
+  import torch
+  from apache_beam.ml.inference.base import KeyedModelHandler
+  from apache_beam.ml.inference.base import RunInference
+  from apache_beam.ml.inference.pytorch_inference import PytorchModelHandlerTensor
+
+  class LinearRegression(torch.nn.Module):
+    def __init__(self, input_dim=1, output_dim=1):
+      super().__init__()
+      self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+      out = self.linear(x)
+      return out
+
+  model_state_dict_path = 'gs://apache-beam-samples/run_inference/five_times_table_torch.pt'  # pylint: disable=line-too-long
+  model_class = LinearRegression
+  model_params = {'input_dim': 1, 'output_dim': 1}
+  keyed_model_handler = KeyedModelHandler(
+      PytorchModelHandlerTensor(
+          model_class=model_class,
+          model_params=model_params,
+          state_dict_path=model_state_dict_path))
+
+  keyed_data = [("first_question", 105.00), ("second_question", 108.00),
+                ("third_question", 1000.00), ("fourth_question", 1013.00)]
+
+  with beam.Pipeline() as p:
+    predictions = (
+        p
+        | 'KeyedInputData' >> beam.Create(keyed_data)
+        | "ConvertIntToTensor" >>
+        beam.Map(lambda x: (x[0], torch.Tensor([x[1]])))
+        | 'PytorchRunInference' >>
+        RunInference(model_handler=keyed_model_handler)
+        | beam.Map(print))
+    if test:
+      test(predictions)
+
+
+def sklearn_unkeyed_model_handler(test=None):
+  import apache_beam as beam
+  import numpy
+  from apache_beam.ml.inference.base import RunInference
+  from apache_beam.ml.inference.sklearn_inference import ModelFileType
+  from apache_beam.ml.inference.sklearn_inference import SklearnModelHandlerNumpy
+
+  sklearn_model_filename = 'gs://apache-beam-samples/run_inference/five_times_table_sklearn.pkl'  # pylint: disable=line-too-long
+  sklearn_model_handler = SklearnModelHandlerNumpy(
+      model_uri=sklearn_model_filename, model_file_type=ModelFileType.PICKLE)
+
+  unkeyed_data = numpy.array([20, 40, 60, 90],
+                             dtype=numpy.float32).reshape(-1, 1)
+  with beam.Pipeline() as p:
+    predictions = (
+        p
+        | "ReadInputs" >> beam.Create(unkeyed_data)
+        | "RunInferenceSklearn" >>
+        RunInference(model_handler=sklearn_model_handler)
+        | beam.Map(print))
+    if test:
+      test(predictions)
+
+
+def sklearn_keyed_model_handler(test=None):
+  import apache_beam as beam
+  from apache_beam.ml.inference.base import KeyedModelHandler
+  from apache_beam.ml.inference.base import RunInference
+  from apache_beam.ml.inference.sklearn_inference import ModelFileType
+  from apache_beam.ml.inference.sklearn_inference import SklearnModelHandlerNumpy
+
+  sklearn_model_filename = 'gs://apache-beam-samples/run_inference/five_times_table_sklearn.pkl'  # pylint: disable=line-too-long
+  sklearn_model_handler = KeyedModelHandler(
+      SklearnModelHandlerNumpy(
+          model_uri=sklearn_model_filename,
+          model_file_type=ModelFileType.PICKLE))
+
+  keyed_data = [("first_question", 105.00), ("second_question", 108.00),
+                ("third_question", 1000.00), ("fourth_question", 1013.00)]
+
+  with beam.Pipeline() as p:
+    predictions = (
+        p
+        | "ReadInputs" >> beam.Create(keyed_data)
+        | "ConvertDataToList" >> beam.Map(lambda x: (x[0], [x[1]]))
+        | "RunInferenceSklearn" >>
+        RunInference(model_handler=sklearn_model_handler)
+        | beam.Map(print))
+    if test:
+      test(predictions)
