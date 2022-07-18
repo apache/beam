d2176dc48e52bf23c32351c753ab1db1d58896a3
Lucas Nogueira <lnogueir@uwaterloo.ca>
2022-07-18 18:23:54 +0000

    make pubsub topic optional

--- sdks/go/examples/fhirio/read_write_pubsub.go
+++ sdks/go/examples/fhirio/read_write_pubsub.go
@@ -13,22 +13,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// read_write_pubsub is a streaming pipeline example using the fhirio connector
-// to read FHIR resources from GCS, write them to a GCP FHIR store, and, upon
-// store updates through PubSub notifications, read those written resources from
-// the FHIR store and log them.
+// read_write_pubsub is a pipeline example using the fhirio connector to read
+// FHIR resources from GCS, write them to a GCP FHIR store, and, if a PubSub
+// topic is provided, read the written resources from the FHIR store and log them
+// based on the PubSub notifications about store updates.
 //
 // Pre-requisites:
-// 1. A Google Cloud FHIR store with PubSub notifications set up.
+// 1. NDJSON-encoded FHIR resources stored in GCS.
+// 2. Dataflow Runner enabled: https://cloud.google.com/dataflow/docs/quickstarts.
+// 3. A Google Cloud FHIR store. Optionally, PubSub notifications set up on the store.
 // (see: https://cloud.google.com/healthcare-api/docs/concepts/pubsub).
-// 2. NDJSON-encoded FHIR resources stored in GCS.
-// 3. Dataflow Runner enabled: https://cloud.google.com/dataflow/docs/quickstarts.
 //
 // Running this pipeline requires providing a fully qualified GCS address
 // (potentially containing wildcards) to where your FHIR resources are stored, a
-// path to the FHIR store where the resources should be written to, and the
-// PubSub topic name your FHIR store is sending notifications to, in addition to
-// the usual flags for the Dataflow runner.
+// path to the FHIR store where the resources should be written to, and,
+// optionally, the PubSub topic name your FHIR store is sending notifications to,
+// in addition to the usual flags for the Dataflow runner.
 //
 // An example command for executing this pipeline on GCP is as follows:
 //   export PROJECT="$(gcloud config get-value project)"
@@ -78,7 +78,7 @@ var (
 	// "projects/project_id/locations/location/datasets/DATASET_ID/fhirStores/FHIR_STORE_ID"
 	fhirStore = flag.String("fhirStore", "", "The target FHIR Store to write data to, must be of the full format.")
 
-	// Required flag with the pubsub topic of your FHIR store.
+	// Optional flag with the pubsub topic of your FHIR store to read and log upon store updates.
 	pubsubTopic = flag.String("pubsubTopic", "", "The PubSub topic to listen to.")
 )
 
@@ -137,15 +137,17 @@ func main() {
 	// Write resources to store.
 	fhirio.ExecuteBundles(s, *fhirStore, resourceBundles)
 
-	// PubSub notifications will be emitted containing the path of the resource once
-	// it is written to the store. Simultaneously read notifications and resources
-	// from PubSub and store, respectively.
-	resourceNotifications := pubsubio.Read(s, *gcpopts.Project, *pubsubTopic, nil)
-	resourcesInFhirStore, deadLetters := fhirio.Read(s, resourceNotifications)
+	if *pubsubTopic != "" {
+		// PubSub notifications will be emitted containing the path of the resource once
+		// it is written to the store. Simultaneously read notifications and resources
+		// from PubSub and store, respectively.
+		resourceNotifications := pubsubio.Read(s, *gcpopts.Project, *pubsubTopic, nil)
+		resourcesInFhirStore, deadLetters := fhirio.Read(s, resourceNotifications)
 
-	// Log the read resources or read errors to the server.
-	beam.ParDo0(s, &LoggerFn{"Read Resource"}, resourcesInFhirStore)
-	beam.ParDo0(s, &LoggerFn{"Got Dead Letter"}, deadLetters)
+		// Log the read resources or read errors to the server.
+		beam.ParDo0(s, &LoggerFn{"Read Resource"}, resourcesInFhirStore)
+		beam.ParDo0(s, &LoggerFn{"Got Dead Letter"}, deadLetters)
+	}
 
 	ctx := context.Background()
 	if err := beamx.Run(ctx, p); err != nil {
