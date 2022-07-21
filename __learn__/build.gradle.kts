404702288bf9adab1e0ec670b25a2eeadc53e1b0
AgileTec Solutions <28709874+xodiumluma@users.noreply.github.com>
2022-07-21 23:03:52 +1000

    [learn] set up python container build precommit tasks

--- __learn__/build.gradle.kts
+++ __learn__/build.gradle.kts
@@ -260,3 +260,13 @@ tasks.register("pythonPreCommit") {
   dependsOn(":sdks:python:test-suites:dataflow:preCommitIT")
   dependsOn(":sdks:python:test-suites:dataflow:preCommitIT_V2")
 }
+
+tasks.register("pythonDocsPreCommit") {
+  dependsOn(":sdks:python:test-suites:tox:pycommon:docs")
+}
+
+tasks.register("pythonDockerBuildPreCommit") {
+  dependsOn(":sdks:python:container:py37:docker")
+  dependsOn(":sdks:python:container:py38:docker")
+  dependsOn(":sdks:python:container:py39:docker")
+}
