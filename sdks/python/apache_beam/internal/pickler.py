a91e7dc1c9bb8ef22988d3ffdbbd0062c356b827
Anand Inguva <anandinguva98@gmail.com>
2022-07-21 12:55:43 +0530

    Check Java Cross language test

--- sdks/python/apache_beam/internal/pickler.py
+++ sdks/python/apache_beam/internal/pickler.py
@@ -27,6 +27,9 @@ description.
 The pickler module should be used to pickle functions and modules; for values,
 the coders.*PickleCoder classes should be used instead.
 """
+# pylint: skip-file
+
+import logging
 
 from apache_beam.internal import cloudpickle_pickler
 from apache_beam.internal import dill_pickler
@@ -41,13 +44,40 @@ desired_pickle_lib = dill_pickler
 def dumps(o, enable_trace=True, use_zlib=False):
   # type: (...) -> bytes
 
+  logging.info("****************dumps*****************************")
+  serialized_fn = desired_pickle_lib.dumps(
+      o, enable_trace=enable_trace, use_zlib=use_zlib)
+
+  # pylint: disable=line-too-long
+  if serialized_fn == b'QlpoOTFBWSZTWW2DOMwAAoP/0P//////////////8r///+ZjwQAAkABAAvZLMKAIaptKmgD1G1NHqPUGQ9QABoGRtQPUGgeiAyANPU9QaDxINDTT1BKIBAITI1M9U8p6ptPVPTSNGDIjQyGT1GamahoAGAaRoaZpPapk3qQanopooAMgAABoAAyGgAAA0AAA0AaADQEGAAAAAAAAAAATAAAAARgACYAM5MUrCptpkfPPVhjTXwKIU3oFiIJ/CmyKm2KIZ1A18ooB+j4G8x4RVknC4yKFH0rinqKEj8RACpHX3hkYJ5lTMPMdIKIjxIJLJEEKSWKSUBYhCIgBFQxHB/PDjH7GXX5+F4WG8bQ28I1HM8gN8O+MHn9DPGSnGjxh4zcN0oIQI7Yv1G4WNxjRhrCUDLResXxg0wWTJY0RkAs4YoXfYNHiK1PUibLpaDbVdM0lVFU2Ga4xG50HXL9B8IIJ3S3fGpfgZYIeoqgSmvHXY4pat1MZa7ks9yFCSDapYAMsDHfyzSb4zGUw5t70rhZZiNtIu4vMPDECfHMIUodUKP0j5TMfMfEdFQkidA+NLOQI6ArlUgx/Ue4ZtnU3xD5Nogb4vYoAjegh16U641+yaJJHXKN4if4ZnmkolBy1Kpzh29b6NzuvmJ8N0aBQ4vcGtWNS7paBLBRKKJpCPJn6GnMVjYmA1CFsd3GJM1XrYBOkXTQGV0NzA+shJZyobgOKghGWSFYnJQBArEUgOdM9kEYqk2a+e9ILaq+pPzb0YygDFo1mIMh62i3XGgjGQtNQx8cIZTMziESoLDIE9ZYgmTis3lmHF3LxVvmCsgIxROJHR7w4cgVCwvDSD7EMzkHTPIgpMKhMgrVRSByLk/JpwYRpMwGiYOWVIYb/2jyk0i6JqnoRqkVL5chLUoVeScA2PT6YNNPPqzszrZ23SdVGa9b+znUxbwnsfO/Lswr1n1TabWQXvNYuhcqVLvqYsPUxlCyVZPe565HNWiTIQvjZRL9gxDu+vOKUotqgY4SGilwxDlaK0XP0WobRwjhlpcfOTmhckNqGmsXXapFS8Y3MICo8xBAxSe07BsFi2ZUuDHJE49mqRUl7SP1yYtCiWApKQUpaPGR+xRVJBxjaedDdhykYHvA+J/0f8ofsOK9IoUZBk/7TeXorWosw1/i7kinChINsGcZg':
+    import dis
+    logging.info(dis.dis(o[0]))
+
+  deserialized_fn = desired_pickle_lib.loads(
+      serialized_fn, enable_trace=enable_trace, use_zlib=use_zlib)
+
+  logging.info(f"serialized_fn: {serialized_fn}")
+  logging.info(f"deserialized_fn: {deserialized_fn}")
+  import sys
+  logging.info("Python version: {}".format(sys.version))
+  logging.info("****************dumps*****************************")
+
   return desired_pickle_lib.dumps(
       o, enable_trace=enable_trace, use_zlib=use_zlib)
 
 
 def loads(encoded, enable_trace=True, use_zlib=False):
   """For internal use only; no backwards-compatibility guarantees."""
+  logging.info("******************loads****************************")
+  logging.info(f"encoded: {encoded}")
 
+  decoded = desired_pickle_lib.loads(
+      encoded, enable_trace=enable_trace, use_zlib=use_zlib)
+  logging.info(f"decoded: {decoded}")
+
+  import sys
+  logging.info("Python version: {}".format(sys.version))
+  logging.info("******************loads****************************")
   return desired_pickle_lib.loads(
       encoded, enable_trace=enable_trace, use_zlib=use_zlib)
 
