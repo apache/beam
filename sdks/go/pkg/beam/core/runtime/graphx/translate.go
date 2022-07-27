9f6855d643c7c401ee4f013414bbd01d08b62631
riteshghorse <riteshghorse@gmail.com>
2022-07-27 14:22:26 -0400

    plumb allowed lateness

--- sdks/go/pkg/beam/core/runtime/graphx/translate.go
+++ sdks/go/pkg/beam/core/runtime/graphx/translate.go
@@ -1053,7 +1053,7 @@ func MarshalWindowingStrategy(c *CoderMarshaller, w *window.WindowingStrategy) (
 		AccumulationMode: makeAccumulationMode(w.AccumulationMode),
 		OutputTime:       pipepb.OutputTime_END_OF_WINDOW,
 		ClosingBehavior:  pipepb.ClosingBehavior_EMIT_IF_NONEMPTY,
-		AllowedLateness:  0,
+		AllowedLateness:  int64(w.AllowedLateness),
 		OnTimeBehavior:   pipepb.OnTimeBehavior_FIRE_IF_NONEMPTY,
 	}
 	return ws, nil
