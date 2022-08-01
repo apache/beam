9d5ec3225cc4cc0f0714195aa2ff690157e15f47
Vladislav Chunikhin <vladislav.chunikhin@akvelon.com>
2022-08-01 18:15:21 +0300

    [Playground] [Infrastructure] fixed duplicated example names

--- learning/katas/java/Core Transforms/CoGroupByKey/CoGroupByKey/src/org/apache/beam/learning/katas/coretransforms/cogroupbykey/Task.java
+++ learning/katas/java/Core Transforms/CoGroupByKey/CoGroupByKey/src/org/apache/beam/learning/katas/coretransforms/cogroupbykey/Task.java
@@ -19,7 +19,7 @@
 package org.apache.beam.learning.katas.coretransforms.cogroupbykey;
 
 // beam-playground:
-//   name: GroupByKey
+//   name: JoinPCollections
 //   description: Task from katas that joins two PCollections.
 //   multifile: true
 //   context_line: 48
