c4dc372d5c661833b10781c8a107660465237f79
Evgeny Antyshev <eantyshev@gmail.com>
2022-08-03 15:17:53 +0300

    samples path

--- learning/tour-of-beam/backend/internal/storage/mock.go
+++ learning/tour-of-beam/backend/internal/storage/mock.go
@@ -3,7 +3,14 @@ package storage
 import (
 	"encoding/json"
 	"io/ioutil"
+	"path"
+	"runtime"
 )
+
+func getSamplesPath() string {
+	_, filepath, _, _ := runtime.Caller(1)
+	return path.Join(path.Dir(filepath), "..", "..", "samples")
+}
 
 type Mock struct{}
 
@@ -11,13 +18,13 @@ type Mock struct{}
 var _ Iface = &Mock{}
 
 func (d *Mock) GetContentTree(sdk string, userId *string) (ct ContentTree) {
-	content, _ := ioutil.ReadFile("samples/content_tree.json")
+	content, _ := ioutil.ReadFile(path.Join(getSamplesPath(), "content_tree.json"))
 	_ = json.Unmarshal(content, &ct)
 	return ct
 }
 
 func (d *Mock) GetUnitContent(unitId string, userId *string) (u UnitContent) {
-	content, _ := ioutil.ReadFile("samples/unit.json")
+	content, _ := ioutil.ReadFile(path.Join(getSamplesPath(), "unit.json"))
 	_ = json.Unmarshal(content, &u)
 	return u
 }
