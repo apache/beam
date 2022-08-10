7d12f44e95ed8d85ece3a49a006cd819ec116ea5
Daniel Collins <dpcollins@google.com>
2022-08-10 08:15:59 -0400

    Don't throw from finalizeCheckpoint

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/CheckpointMarkImplTest.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/CheckpointMarkImplTest.java
@@ -18,7 +18,6 @@
 package org.apache.beam.sdk.io.gcp.pubsublite.internal;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.verify;
 
 import com.google.cloud.pubsublite.Offset;
