cbd7196cb0c264fb5358136ce30258a5e43408f5
Danny McCormick <dannymccormick@google.com>
2022-08-11 09:00:07 -0400

    Dont merge: Test - figure out if seed job is picking up changes

--- .test-infra/jenkins/committers_list_generator/main.py
+++ .test-infra/jenkins/committers_list_generator/main.py
@@ -141,5 +141,5 @@ if __name__ == "__main__":
         args = _parse_args()
         generate_groovy(args.output_dir, args.ldap_uris, args.cert_path)
     except CommittersGeneratorException as e:
-        logging.exception("Couldn't generate the list of committers")
+        logging.exception(f'Couldnt generate the list of committers with ldap uris: {args.ldap_uris}')
         sys.exit(1)
