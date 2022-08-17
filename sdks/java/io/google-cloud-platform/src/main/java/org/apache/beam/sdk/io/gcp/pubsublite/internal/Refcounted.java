385985485901820c8b095e8c406ecc00b67c6374
Daniel Collins <dpcollins@google.com>
2022-08-17 14:12:21 -0400

    Only close committers after all CheckpointMarks have gone away. This should substantially reduce logspam.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/Refcounted.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/Refcounted.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.gcp.pubsublite.internal;
 
 import com.google.cloud.pubsublite.internal.wire.SystemExecutors;
@@ -5,9 +22,7 @@ import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * A class that safely ensures an object of type T is cleaned up before it is garbage collected.
- */
+/** A class that safely ensures an object of type T is cleaned up before it is garbage collected. */
 class Refcounted<T extends AutoCloseable> implements Supplier<T> {
 
   private static final Logger logger = LoggerFactory.getLogger(Refcounted.class);
@@ -27,15 +42,19 @@ class Refcounted<T extends AutoCloseable> implements Supplier<T> {
     this.object = object;
   }
 
+  @SuppressWarnings("deprecation")
   @Override
   protected void finalize() {
-    SystemExecutors.getFuturesExecutor().execute(() -> {
-      try {
-        object.close();
-      } catch (Exception e) {
-        logger.warn("Failed to close resource with class: " + object.getClass().getCanonicalName(),
-            e);
-      }
-    });
+    SystemExecutors.getFuturesExecutor()
+        .execute(
+            () -> {
+              try {
+                object.close();
+              } catch (Exception e) {
+                logger.warn(
+                    "Failed to close resource with class: " + object.getClass().getCanonicalName(),
+                    e);
+              }
+            });
   }
 }
