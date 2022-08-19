5fbb7d0338b8d86d61c0c916116156f1d8a52d6f
riteshghorse <riteshghorse@gmail.com>
2022-08-19 10:55:05 -0400

    fix return

--- sdks/go/pkg/beam/core/graph/fn.go
+++ sdks/go/pkg/beam/core/graph/fn.go
@@ -1275,11 +1275,12 @@ func validateState(fn *DoFn, numIn mainInputs) error {
 			}
 			stateKeys[k] = s
 		}
-
-		// TODO(#22736) - Remove this once state is fully supported
-		err := errors.Errorf("ProcessElement uses a StateProvider, but state is not supported in this release.")
-		return errors.SetTopLevelMsgf(err, "ProcessElement uses a StateProvider, but state is not supported in this release. "+
-			"Please try upgrading to a newer release if one exists or wait for state support to be released.")
+		if len(ps) > 0 {
+			// TODO(#22736) - Remove this once state is fully supported
+			err := errors.Errorf("ProcessElement uses a StateProvider, but state is not supported in this release.")
+			return errors.SetTopLevelMsgf(err, "ProcessElement uses a StateProvider, but state is not supported in this release. "+
+				"Please try upgrading to a newer release if one exists or wait for state support to be released.")
+		}
 	} else {
 		if len(ps) > 0 {
 			err := errors.Errorf("ProcessElement doesn't use a StateProvider, but State structs are attached to "+
