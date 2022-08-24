4938e202d88cc01a4e4cc7ccdc797c6e365f2842
Brian Hulette <bhulette@google.com>
2022-08-24 11:36:32 -0700

    Test with pandas 1.5.0rc0

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -321,7 +321,7 @@ if __name__ == '__main__':
             'azure-storage-blob >=12.3.2',
             'azure-core >=1.7.0',
           ],
-          'dataframe': ['pandas>=1.0,<1.5']
+          'dataframe': ['pandas>=1.0,<1.6']
       },
       zip_safe=False,
       # PyPI package information.
