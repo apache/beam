71ad47bb4bd7882026aa56296caf56500bd6e2f9
Vladislav Chunikhin <vladislav.chunikhin@akvelon.com>
2022-08-24 11:38:08 +0300

    [Playground] [Backend] fixes after merging

--- playground/backend/cmd/server/server.go
+++ playground/backend/cmd/server/server.go
@@ -201,6 +201,10 @@ func setupCache(ctx context.Context, appEnv environment.ApplicationEnvs) (cache.
 // setupExamplesCatalogFromDatastore saves precompiled objects catalog from the cloud datastore to the cache
 func setupExamplesCatalogFromDatastore(ctx context.Context, cacheService cache.Cache, db db.Database, sdks []*entity.SDKEntity) error {
 	catalog, err := db.GetCatalog(ctx, sdks)
+	if len(catalog) == 0 {
+		logger.Warn("example catalog is empty")
+		return nil
+	}
 	if err != nil {
 		return err
 	}
