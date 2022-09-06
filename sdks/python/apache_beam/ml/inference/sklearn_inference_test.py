88f9f418ad22e1d20357f9105b4f0c9783714f9d
Danny McCormick <dannymccormick@google.com>
2022-09-06 11:04:12 -0400

    WIP: Reenable run-inference tests on windows

--- sdks/python/apache_beam/ml/inference/sklearn_inference_test.py
+++ sdks/python/apache_beam/ml/inference/sklearn_inference_test.py
@@ -173,9 +173,6 @@ class SkLearnRunInferenceTest(unittest.TestCase):
         sys.getsizeof(batched_examples_float[0]) * 3,
         inference_runner.get_num_bytes(batched_examples_float))
 
-  @unittest.skipIf(
-      platform.system() == 'Windows',
-      'https://github.com/apache/beam/issues/21449')
   def test_pipeline_pickled(self):
     temp_file_name = self.tmpdir + os.sep + 'pickled_file'
     with open(temp_file_name, 'wb') as file:
