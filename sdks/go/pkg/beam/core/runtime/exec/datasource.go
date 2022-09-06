c11fec9fa243f40fbbe74e1d886a40336c671b78
Danny McCormick <dannymccormick@google.com>
2022-09-06 09:57:32 -0400

    Comment with TODO

--- sdks/go/pkg/beam/core/runtime/exec/datasource.go
+++ sdks/go/pkg/beam/core/runtime/exec/datasource.go
@@ -78,6 +78,7 @@ func (n *DataSource) ID() UnitID {
 
 // Up initializes this datasource.
 func (n *DataSource) Up(ctx context.Context) error {
+	// TODO(https://github.com/apache/beam/issues/23043) - Reenable single iteration or more fully rip this out.
 	safeToSingleIterate := false
 	switch n.Out.(type) {
 	case *Expand, *Multiplex:
