dde4bf4b33c577af5dd88f836dffcea8ffeafdf2
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-09-09 18:44:54 +0600

    add runner concepts

--- learning/tour-of-beam/learning-content/python/introduction/introduction-concepts/pipeline-concepts/creating-pipeline/example/task.py
+++ learning/tour-of-beam/learning-content/python/introduction/introduction-concepts/pipeline-concepts/creating-pipeline/example/task.py
@@ -15,8 +15,8 @@
 #   limitations under the License.
 
 # beam-playground:
-#   name: HelloBeam
-#   description: Hello Beam example.
+#   name: creating-pipeline
+#   description: Creating pipeline example.
 #   multifile: false
 #   context_line: 35
 #   categories:
