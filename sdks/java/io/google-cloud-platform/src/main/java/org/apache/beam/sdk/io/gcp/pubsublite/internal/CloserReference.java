e6aa96b97c363cf79e42696d2f8883cf8f23f39b
Daniel Collins <dpcollins@google.com>
2022-09-10 15:52:22 +0900

    Only close committers after all CheckpointMarks have gone away. This should substantially reduce logspam.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/CloserReference.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/pubsublite/internal/CloserReference.java
@@ -22,9 +22,7 @@ import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * A class that safely ensures an object of type T is cleaned up before it is garbage collected.
- */
+/** A class that safely ensures an object of type T is cleaned up before it is garbage collected. */
 class CloserReference<T extends AutoCloseable> implements Supplier<T> {
 
   private static final Logger LOG = LoggerFactory.getLogger(CloserReference.class);
