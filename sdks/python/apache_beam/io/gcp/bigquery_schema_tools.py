7e4b41754753fb1fb3df5174f3aa6733c3b1424d
Svetak Sundhar <svetaksundhar@google.com>
2022-09-15 15:02:11 -0400

    registered type in schemas.py

--- sdks/python/apache_beam/io/gcp/bigquery_schema_tools.py
+++ sdks/python/apache_beam/io/gcp/bigquery_schema_tools.py
@@ -20,13 +20,13 @@ Classes, constants and functions in this file are experimental and have no
 backwards compatibility guarantees.
 NOTHING IN THIS FILE HAS BACKWARDS COMPATIBILITY GUARANTEES.
 """
+import datetime
 from typing import Optional
 from typing import Sequence
 
 import numpy as np
 
 import apache_beam as beam
-import apache_beam.utils.timestamp
 from apache_beam.io.gcp.internal.clients import bigquery
 from apache_beam.portability.api import schema_pb2
 
@@ -41,7 +41,7 @@ BIG_QUERY_TO_PYTHON_TYPES = {
     "FLOAT": np.float64,
     "BOOLEAN": bool,
     "BYTES": bytes,
-    "TIMESTAMP": apache_beam.utils.timestamp.Timestamp
+    "TIMESTAMP": datetime.datetime
     #TODO(https://github.com/apache/beam/issues/20810):
     # Finish mappings for all BQ types
 }
