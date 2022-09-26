a1f74f79076ad33600b80e9e973e6f868fca7c23
dannymartinm <danielamartinmtz@gmail.com>
2022-09-26 10:48:00 -0500

    Updated sh file to comment the real trigger phrases insted of the example file. Updated PR body text.

--- scripts/ci/release/comment_pr_trigger_phrases.sh
+++ scripts/ci/release/comment_pr_trigger_phrases.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#file="./jenkins_trigger_phrases.txt"
-file="./example.txt"
+file="./jenkins_trigger_phrases.txt"
 GITHUB_PR=$1
 while IFS= read -r trigger_phrase
 do
-    #printf '%s\n' "$trigger_phrase"
     gh pr comment "$GITHUB_PR" --body "$trigger_phrase"
 done <"$file"
