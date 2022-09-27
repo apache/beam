81ac6ac418d12cbeb2d891b4973cc4a55c79244a
darkhan.nausharipov <darkhan.nausharipov@kzn.akvelon.com>
2022-09-27 15:28:05 +0600

    getSdks in SdkDropdown (#23316)

--- learning/tour-of-beam/frontend/lib/components/sdk_dropdown.dart
+++ learning/tour-of-beam/frontend/lib/components/sdk_dropdown.dart
@@ -16,33 +16,56 @@
  * limitations under the License.
  */
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:playground_components/playground_components.dart';
+
+import '../models/sdk_list.dart';
 
 class SdkDropdown extends StatelessWidget {
   const SdkDropdown();
 
+  // TODO(nausharipov): remove after demo
+  Future<Map<String, dynamic>> _getSdks() async {
+    final response = await http.get(
+      Uri.parse(
+        'https://us-central1-tour-of-beam-2.cloudfunctions.net/getSdkList',
+      ),
+    );
+    final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+    return decodedResponse;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _DropdownWrapper(
-      child: DropdownButton(
-        value: 'Java',
-        onChanged: (sdk) {
-          // TODO(nausharipov): change SDK
-        },
-        items: const ['Java', 'Python', 'Go']
-            .map(
-              (sdk) => DropdownMenuItem(
-                value: sdk,
-                child: Text(sdk),
-              ),
-            )
-            .toList(growable: false),
-        isDense: true,
-        alignment: Alignment.center,
-        focusColor: BeamColors.transparent,
-        borderRadius: BorderRadius.circular(BeamSizes.size6),
-      ),
+    return FutureBuilder(
+      future: _getSdks(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Container();
+        final sdks = SdkListModel.fromJson(snapshot.data!).sdks;
+        return _DropdownWrapper(
+          child: DropdownButton(
+            value: sdks.first.id,
+            onChanged: (sdk) {
+              // TODO(nausharipov): change SDK
+            },
+            items: sdks
+                .map(
+                  (sdk) => DropdownMenuItem(
+                    value: sdk.id,
+                    child: Text(sdk.title),
+                  ),
+                )
+                .toList(growable: false),
+            isDense: true,
+            alignment: Alignment.center,
+            focusColor: BeamColors.transparent,
+            borderRadius: BorderRadius.circular(BeamSizes.size6),
+          ),
+        );
+      },
     );
   }
 }
