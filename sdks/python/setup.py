1427b7dd93ff2787d3798e7a558efbc13d460257
Yi Hu <yathu@google.com>
2022-09-28 19:39:41 -0400

    Support google-cloud-spanner v3 and fixes broken unit tests (#23365)

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -290,7 +290,8 @@ if __name__ == '__main__':
             'google-cloud-bigquery-storage>=2.6.3,<2.14',
             'google-cloud-core>=0.28.1,<3',
             'google-cloud-bigtable>=0.31.1,<2',
-            'google-cloud-spanner>=1.13.0,<2',
+            # google-cloud-spanner 2.x causes dependency parsing backoff
+            'google-cloud-spanner>=1.13.0,!=2,<4',
             'grpcio-gcp>=0.2.2,<1',
             # GCP Packages required by ML functionality
             'google-cloud-dlp>=3.0.0,<4',
