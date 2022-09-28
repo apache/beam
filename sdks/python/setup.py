0a7fff4566ab9ca1129bf6019f2d9dc1b1df1acd
Yi Hu <yathu@google.com>
2022-09-28 14:48:44 -0400

    Set google-cloud-spanner major version upper bound

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
             'google-cloud-core>=0.28.1,<3',
             'google-cloud-bigtable>=0.31.1,<2',
             # google-cloud-spanner 2.x causes dependency parsing backoff
-            'google-cloud-spanner>=1.13.0,!=2,<=3.21.0',
+            'google-cloud-spanner>=1.13.0,!=2,<4',
             'grpcio-gcp>=0.2.2,<1',
             # GCP Packages required by ML functionality
             'google-cloud-dlp>=3.0.0,<4',
