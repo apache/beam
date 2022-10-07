6f4732abce17be0056a1762e8c1172ae8af77b8b
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-10-07 18:48:30 +0500

    Update main.tf

--- playground/terraform/infrastructure/appengine/main.tf
+++ playground/terraform/infrastructure/appengine/main.tf
@@ -15,7 +15,7 @@
 
 resource "google_app_engine_application" "app_playground" {
   project     = var.project_id
-  location_id = "${var.region == us-central1 ? us-central : var.region}"
+  location_id = "var.region == us-central1 ? location_id = us-central : location_id = var.region"
   database_type = "CLOUD_DATASTORE_COMPATIBILITY"
 }
 
