7c41beefb732da4103688a8bd261ca4ce940f71e
pablo rodriguez defino <prodriguezdefino@gmail.com>
2022-10-12 14:58:09 -0700

    spotless apply

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryUtilsTest.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryUtilsTest.java
@@ -592,7 +592,7 @@ public class BigQueryUtilsTest {
     assertThat(field.getMode(), equalTo(Mode.REPEATED.toString()));
     assertThat(field.getFields(), containsInAnyOrder(MAP_KEY, MAP_VALUE));
   }
-  
+
   @Test
   public void testToTableSchema_map_array() {
     TableSchema schema = toTableSchema(MAP_ARRAY_TYPE);
