Polber/DataflowTemplates

Issue 442 - Create Bigtable Resource Manager
Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>

it/src/test/java/com/google/cloud/teleport/it/bigtable/BigtableResourceManagerUtilsTest.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.it.bigtable;
+
+import static com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils.checkValidTableId;
+import static com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils.generateDefaultClusters;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.cloud.bigtable.admin.v2.models.StorageType;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/** Unit tests for {@link com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils}. */
+@RunWith(JUnit4.class)
+public class BigtableResourceManagerUtilsTest {
+  @Rule public final MockitoRule mockito = MockitoJUnit.rule();
+
+  private static final String TEST_ID = "test-id";
+  private static final String ZONE = "us-central1-a";
+  private static final int NUM_NODES = 1;
+  private static final StorageType STORAGE_TYPE = StorageType.SSD;
+
+  @Test
+  public void testGenerateDefaultClustersShouldWorkWhenAllParametersValid() {
+    Iterable<BigtableResourceManagerCluster> cluster =
+        generateDefaultClusters(TEST_ID, ZONE, NUM_NODES, STORAGE_TYPE);
+    BigtableResourceManagerCluster thisCluster = cluster.iterator().next();
+
+    assertThat(thisCluster.getClusterId()).matches(TEST_ID + "-\\d{8}-\\d{6}-\\d{6}");
+    assertThat(thisCluster.getZone()).isEqualTo(ZONE);
+    assertThat(thisCluster.getNumNodes()).isEqualTo(NUM_NODES);
+    assertThat(thisCluster.getStorageType()).isEqualTo(STORAGE_TYPE);
+  }
+
+  @Test
+  public void testGenerateDefaultClustersShouldThrowErrorWhenTestIdIsEmpty() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> generateDefaultClusters("", ZONE, NUM_NODES, STORAGE_TYPE));
+  }
+
+  @Test
+  public void testGenerateDefaultClustersShouldShortenTestIdWhenTooLong() {
+    Iterable<BigtableResourceManagerCluster> cluster =
+        generateDefaultClusters("longer-id", ZONE, NUM_NODES, STORAGE_TYPE);
+    assertThat(cluster.iterator().next().getClusterId()).matches("longer--\\d{8}-\\d{6}-\\d{6}");
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdIsTooShort() {
+    assertThrows(IllegalArgumentException.class, () -> checkValidTableId(""));
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdIsTooLong() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> checkValidTableId("really-really-really-really-long-table-id"));
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdContainsIllegalCharacter() {
+    assertThrows(IllegalArgumentException.class, () -> checkValidTableId("table-id%"));
+  }
+}

apache/beam

src/test/java/com/google/cloud/teleport/it/bigtable/BigtableResourceManagerUtilsTest.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.it.bigtable;
+
+import static com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils.checkValidTableId;
+import static com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils.generateDefaultClusters;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.cloud.bigtable.admin.v2.models.StorageType;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/** Unit tests for {@link com.google.cloud.teleport.it.bigtable.BigtableResourceManagerUtils}. */
+@RunWith(JUnit4.class)
+public class BigtableResourceManagerUtilsTest {
+  @Rule public final MockitoRule mockito = MockitoJUnit.rule();
+
+  private static final String TEST_ID = "test-id";
+  private static final String ZONE = "us-central1-a";
+  private static final int NUM_NODES = 1;
+  private static final StorageType STORAGE_TYPE = StorageType.SSD;
+
+  @Test
+  public void testGenerateDefaultClustersShouldWorkWhenAllParametersValid() {
+    Iterable<BigtableResourceManagerCluster> cluster =
+        generateDefaultClusters(TEST_ID, ZONE, NUM_NODES, STORAGE_TYPE);
+    BigtableResourceManagerCluster thisCluster = cluster.iterator().next();
+
+    assertThat(thisCluster.clusterId()).matches(TEST_ID + "-\\d{8}-\\d{6}-\\d{6}");
+    assertThat(thisCluster.zone()).isEqualTo(ZONE);
+    assertThat(thisCluster.numNodes()).isEqualTo(NUM_NODES);
+    assertThat(thisCluster.storageType()).isEqualTo(STORAGE_TYPE);
+  }
+
+  @Test
+  public void testGenerateDefaultClustersShouldThrowErrorWhenTestIdIsEmpty() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> generateDefaultClusters("", ZONE, NUM_NODES, STORAGE_TYPE));
+  }
+
+  @Test
+  public void testGenerateDefaultClustersShouldShortenTestIdWhenTooLong() {
+    Iterable<BigtableResourceManagerCluster> cluster =
+        generateDefaultClusters("longer-id", ZONE, NUM_NODES, STORAGE_TYPE);
+    assertThat(cluster.iterator().next().clusterId()).matches("longer--\\d{8}-\\d{6}-\\d{6}");
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdIsTooShort() {
+    assertThrows(IllegalArgumentException.class, () -> checkValidTableId(""));
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdIsTooLong() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> checkValidTableId("really-really-really-really-long-table-id"));
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdContainsIllegalCharacter() {
+    assertThrows(IllegalArgumentException.class, () -> checkValidTableId("table-id%"));
+  }
+
+  @Test
+  public void testCheckValidTableIdWhenIdIsValid() {
+    checkValidTableId("table-id_valid.Test1");
+  }
+}