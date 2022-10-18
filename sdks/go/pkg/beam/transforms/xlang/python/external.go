994bd8d7b12eff7d914c2a44911592050ead0b31
riteshghorse <riteshghorse@gmail.com>
2022-10-18 11:39:44 -0400

    enable automatic expansion service

--- sdks/go/pkg/beam/transforms/xlang/python/external.go
+++ sdks/go/pkg/beam/transforms/xlang/python/external.go
@@ -27,7 +27,8 @@ import (
 )
 
 const (
-	pythonCallableUrn = "beam:logical_type:python_callable:v1"
+	pythonCallableUrn      = "beam:logical_type:python_callable:v1"
+	ExpansionServiceModule = "apache_beam.runners.portability.expansion_service_main"
 )
 
 var (
