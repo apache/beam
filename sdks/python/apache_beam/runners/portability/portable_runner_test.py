00e5ecb30c8bc2f436546a51a2b1b184a5f70a4b
Ryan Thompson <ryanthompson591@gmail.com>
2022-10-18 10:28:55 -0400

    Extend timeout on flakey test.

--- sdks/python/apache_beam/runners/portability/portable_runner_test.py
+++ sdks/python/apache_beam/runners/portability/portable_runner_test.py
@@ -91,7 +91,8 @@ class PortableRunnerTest(fn_runner_test.FnApiRunnerTest):
         GRPCChannelFactory.insecure_channel(address))
     _LOGGER.info('Waiting for server to be ready...')
     start = time.time()
-    timeout = 30
+    # This long timeout is based on the test being flakey see issue #22115.
+    timeout = 300
     while True:
       time.sleep(0.1)
       if cls._subprocess.poll() is not None:
