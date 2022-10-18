2b6b95936dac58375f83c58f99ae8608e8984cf5
arnevandendorpe <arne.vandendorpe@ml6.eu>
2022-10-18 17:25:09 +0200

    Change global variables to variables provided by argparse

--- sdks/python/apache_beam/examples/ml-orchestration/tfx/coco_captions_local.py
+++ sdks/python/apache_beam/examples/ml-orchestration/tfx/coco_captions_local.py
@@ -16,8 +16,60 @@
 """Preprocessing example with TFX with the LocalDagRunner and
 either the beam DirectRunner or DataflowRunner"""
 import os
+import argparse
 
 from tfx import v1 as tfx
+
+
+def parse_args():
+  """Parse arguments."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "--gcp-project-id",
+      type=str,
+      help="ID for the google cloud project to deploy the pipeline to.",
+      required=True)
+  parser.add_argument(
+      "--region",
+      type=str,
+      help="Region in which to deploy the pipeline.",
+      required=True)
+  parser.add_argument(
+      "--pipeline-name",
+      type=str,
+      help="Name for the Beam pipeline.",
+      required=True)
+  parser.add_argument(
+      "--pipeline-root",
+      type=str,
+      help="Path to artifact repository where Kubeflow Pipelines stores a pipeline’s artifacts.",
+      required=True)
+  parser.add_argument(
+      "--csv-file",
+      type=str,
+      help="Path to the csv input file.",
+      required=True)
+  parser.add_argument(
+      "--csv-file",
+      type=str,
+      help="Path to the csv input file.",
+      required=True)
+  parser.add_argument(
+      "--module-file",
+      type=str,
+      help="Path to module file containing the preprocessing_fn and run_fn.",
+      default="coco_captions_utils.py")
+  parser.add_argument(
+      "--beam-runner",
+      type=str,
+      help="Beam runner: DataflowRunner or DirectRunner.",
+      default="DirectRunner")
+  parser.add_argument(
+      "--metadata-file",
+      type=str,
+      help="Path to store a metadata file as a mock metadata database",
+      default="metadata.db")
+  return parser.parse_args()
 
 PROJECT_ID = "<project id>"
 PROJECT_REGION = "<project region>"
@@ -28,20 +80,24 @@ MODULE_FILE = "<path to module file containing preprocessing_fn and run_fn>"
 BEAM_RUNNER = "<Beam runner: DataflowRunner or DirectRunner>"
 METADATA_FILE = "<path to store a metadata file as a mock metadata database>"
 
-BEAM_PIPELINE_ARGS_BY_RUNNER = {
-    'DirectRunner': [],
-    'DataflowRunner': [
-        '--runner=DataflowRunner',
-        '--project=' + PROJECT_ID,
-        '--temp_location=' + os.path.join(PIPELINE_ROOT, 'tmp'),
-        '--region=' + PROJECT_REGION,
-    ]
-}
-
 
 # [START tfx_pipeline]
-def create_pipeline():
-  example_gen = tfx.components.CsvExampleGen(input_base=INPUT_DATA_PATH)
+def create_pipeline(gcp_project_id, region, pipeline_name, pipeline_root,
+                    csv_file, module_file, beam_runner, metadata_file):
+  """Create the TFX pipeline.
+
+  Args:
+      gcp_project_id (str): ID for the google cloud project to deploy the pipeline to.
+      region (str): Region in which to deploy the pipeline.
+      pipeline_name (str): Name for the Beam pipeline
+      pipeline_root (str): Path to artifact repository where Kubeflow Pipelines
+        stores a pipeline’s artifacts.
+      csv_file (str): Path to the csv input file.
+      module_file (str): Path to module file containing the preprocessing_fn and run_fn.
+      beam_runner (str): Beam runner: DataflowRunner or DirectRunner.
+      metadata_file (str): Path to store a metadata file as a mock metadata database.
+  """
+  example_gen = tfx.components.CsvExampleGen(input_base=csv_file)
 
   # Computes statistics over data for visualization and example validation.
   statistics_gen = tfx.components.StatisticsGen(
@@ -56,25 +112,37 @@ def create_pipeline():
       module_file=MODULE_FILE)
 
   trainer = tfx.components.Trainer(
-      module_file="coco_captions_utils.py",
+      module_file=module_file,
       examples=transform.outputs['transformed_examples'],
       transform_graph=transform.outputs['transform_graph'])
 
   components = [example_gen, statistics_gen, schema_gen, transform, trainer]
 
+  beam_pipeline_args_by_runner = {
+    'DirectRunner': [],
+    'DataflowRunner': [
+        '--runner=DataflowRunner',
+        '--project=' + gcp_project_id,
+        '--temp_location=' + os.path.join(pipeline_root, 'tmp'),
+        '--region=' + region,
+    ]
+  }
+
   return tfx.dsl.Pipeline(
-      pipeline_name=PIPELINE_NAME,
-      pipeline_root=PIPELINE_ROOT,
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
       components=components,
       enable_cache=True,
       metadata_connection_config=tfx.orchestration.metadata.
-      sqlite_metadata_connection_config(METADATA_FILE),
-      beam_pipeline_args=BEAM_PIPELINE_ARGS_BY_RUNNER[BEAM_RUNNER])
+      sqlite_metadata_connection_config(metadata_file),
+      beam_pipeline_args=beam_pipeline_args_by_runner[beam_runner])
 
 
 # [END tfx_pipeline]
 
 if __name__ == "__main__":
+
   # [START tfx_execute_pipeline]
-  tfx.orchestration.LocalDagRunner().run(create_pipeline())
+  args = parse_args()
+  tfx.orchestration.LocalDagRunner().run(create_pipeline(**vars(args)))
   # [END tfx_execute_pipeline]
