8e520b97ffd5bb9e3d2a24352953d2d53712f095
Ning Kang <ningkang0957@gmail.com>
2022-10-21 12:47:13 -0700

    Updated ipywidgets
    Updated ipywidgets from v7 to v8 to work with JupyterLab.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -324,7 +324,7 @@ if __name__ == '__main__':
             'ipython>=7,<8;python_version<="3.7"',
             'ipython>=8,<9;python_version>"3.7"',
             'ipykernel>=6,<7',
-            'ipywidgets>=7.6.5,<8',
+            'ipywidgets>=8,<9',
             # Skip version 6.1.13 due to
             # https://github.com/jupyter/jupyter_client/issues/637
             'jupyter-client>=6.1.11,<6.1.13',
