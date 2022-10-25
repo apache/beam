951e5e4fd3629c43e16e26a9fbdedbfb00ab6e67
Bjorn Pedersen <bjornpedersen@google.com>
2022-10-25 11:27:52 -0400

    changed error handling and update() implementation

--- sdks/python/apache_beam/ml/inference/base.py
+++ sdks/python/apache_beam/ml/inference/base.py
@@ -337,7 +337,7 @@ class _MetricsCollector:
     # Metrics
     self._inference_counter = beam.metrics.Metrics.counter(
         namespace, 'num_inferences')
-    self._num_failed_inferences = 0
+    self.failed_batches_counter = 0
     self._inference_request_batch_size = beam.metrics.Metrics.distribution(
         namespace, 'inference_request_batch_size')
     self._inference_request_batch_byte_size = (
@@ -374,15 +374,11 @@ class _MetricsCollector:
       self,
       examples_count: int,
       examples_byte_size: int,
-      latency_micro_secs: int,
-      success: bool):
+      latency_micro_secs: int):
     self._inference_batch_latency_micro_secs.update(latency_micro_secs)
-    self._inference_counter.inc(examples_count) # !!! should this increment even if inference fails?
-                                                # or should it increment by the full batch size, even if not all examples ran?
+    self._inference_counter.inc(examples_count)
     self._inference_request_batch_size.update(examples_count)
     self._inference_request_batch_byte_size.update(examples_byte_size)
-    if not success:
-      self._num_failed_inferences.inc()
 
 
 class _RunInferenceDoFn(beam.DoFn, Generic[ExampleT, PredictionT]):
@@ -434,19 +430,19 @@ class _RunInferenceDoFn(beam.DoFn, Generic[ExampleT, PredictionT]):
     try:
       result_generator = self._model_handler.run_inference(
         batch, self._model, inference_args)
-      success = True
-    except BaseException: # !!! may need "as err" here
-      success = False
+    except BaseException:
+      self._metrics_collector.failed_batches_counter.inc()
       raise
-    predictions = list(result_generator)
+    else:
+      predictions = list(result_generator)
 
-    end_time = _to_microseconds(self._clock.time_ns())
-    inference_latency = end_time - start_time
-    num_bytes = self._model_handler.get_num_bytes(batch)
-    num_elements = len(batch)
-    self._metrics_collector.update(num_elements, num_bytes, inference_latency, success) #!!! how should update run if inference fails
+      end_time = _to_microseconds(self._clock.time_ns())
+      inference_latency = end_time - start_time
+      num_bytes = self._model_handler.get_num_bytes(batch)
+      num_elements = len(batch)
+      self._metrics_collector.update(num_elements, num_bytes, inference_latency)
 
-    return predictions
+      return predictions
 
   def finish_bundle(self):
     # TODO(https://github.com/apache/beam/issues/21435): Figure out why there
