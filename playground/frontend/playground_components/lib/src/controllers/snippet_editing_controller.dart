f0e9f9ffea15a581d8d52c625f833782730e8785
Alexey Inkin <alexey.inkin@akvelon.com>
2022-10-25 16:40:56 +0400

    Use Akvelon editor (#23415)

--- playground/frontend/playground_components/lib/src/controllers/snippet_editing_controller.dart
+++ playground/frontend/playground_components/lib/src/controllers/snippet_editing_controller.dart
@@ -16,8 +16,8 @@
  * limitations under the License.
  */
 
-import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_code_editor/flutter_code_editor.dart';
 
 import '../enums/complexity.dart';
 import '../models/example.dart';
