8499af21375983d0de5d2bc412b35c5d6757c6b5
Anand Inguva <anandinguva98@gmail.com>
2022-10-25 21:55:30 -0400

    Add docstring

--- sdks/python/apache_beam/examples/inference/tfx_bsl/build_tensorflow_model.py
+++ sdks/python/apache_beam/examples/inference/tfx_bsl/build_tensorflow_model.py
@@ -24,6 +24,36 @@ import tensorflow as tf
 
 
 class TFModelWrapperWithSignature(tf.keras.Model):
+  """
+  Helper class used to wrap a based tf.keras.Model object with a serving
+  signature that can passed to the tfx_bsl RunInference transform.
+
+  A TF model saved using this helper class expects inputs as
+    images serialized to tf.string using tf.io.parse_tensor
+    and then passing serialized images to the RunInference transform
+    in the tf.train.Example. More about tf.train.Example at
+    https://www.tensorflow.org/api_docs/python/tf/train/Example
+
+  Usage:
+  Step 1:
+  # Save the base TF model with modified signature .
+  signature_model = TFModelWrapperWithSignature(
+      model=model,
+      preprocess_input=preprocess_input,
+      input_dtype=input_dtype,
+      feature_description=feature_description,
+      **kwargs
+      )
+  tf.saved_model.save(signature_model, path)
+
+  Step 2:
+  # Load the saved_model in the beam pipeline to create ModelHandler.
+  saved_model_spec = model_spec_pb2.SavedModelSpec(
+      model_path=known_args.model_path)
+  inferece_spec_type = model_spec_pb2.InferenceSpecType(
+      saved_model_spec=saved_model_spec)
+  model_handler = CreateModelHandler(inferece_spec_type)
+  """
   def __init__(
       self,
       model,
@@ -31,6 +61,18 @@ class TFModelWrapperWithSignature(tf.keras.Model):
       input_dtype=None,
       feature_description=None,
       **kwargs):
+    """
+    model: model: Base tensorflow model used for TFX-BSL RunInference transform.
+    preprocess_input: Preprocess method to be included as part of the
+      model's serving signature.
+    input_dtype: tf dtype of the inputs passed to the model.
+      For eg: tf.int32, tf.uint8.
+    feature_description: Feature spec to parse inputs from tf.train.Example
+      using tf.parse_example(). For more details, please take a look at
+      https://www.tensorflow.org/api_docs/python/tf/io/parse_example
+    If there are extra arguments(for eg: training=False) that should be
+    passed to the base tf model during inference, please pass them in kwargs.
+    """
     super().__init__()
     self.model = model
     self.preprocess_input = preprocess_input
@@ -79,13 +121,20 @@ def save_tf_model_with_signature(
   """
   Helper function used to save the Tensorflow Model with a serving signature.
   This is intended only for internal testing.
+
   Args:
    path_to_save_model: Path to save the model with modified signature.
-   model: Base tensorflow model used for TFX-BSL RunInference transform.
-   preprocess_input: Preprocess method to be included as part of the
-   Model's serving signature.
-   input_dtype: dtype of the inputs to the model.
-   feature_description: Feature spec to parse inputs from tf.train.Example.
+  model: model: Base tensorflow model used for TFX-BSL RunInference transform.
+  preprocess_input: Preprocess method to be included as part of the
+    model's serving signature.
+  input_dtype: tf dtype of the inputs passed to the model.
+    For eg: tf.int32, tf.uint8.
+  feature_description: Feature spec to parse inputs from tf.train.Example using
+    tf.parse_example(). For more details, please take a look at
+    https://www.tensorflow.org/api_docs/python/tf/io/parse_example
+
+  If there are extra arguments(for eg: training=False) that should be passed to
+  the base tf model during inference, please pass them in kwargs.
   """
   if not model:
     model = tf.keras.applications.MobileNetV2(weights='imagenet')
