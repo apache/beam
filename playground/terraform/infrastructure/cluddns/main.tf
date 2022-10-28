62eaf600d2636248f82d07c77e8f7139d87a2804
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-10-28 15:57:33 +0500

    Create main.tf

---
+++ playground/terraform/infrastructure/cluddns/main.tf
@@ -0,0 +1,43 @@
+29 lines (27 sloc)  1.2 KB
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_dns_managed_zone" "playground" {
+  name        = "playground"
+  dns_name    = var.dnsname
+  description = "Playground DNS Zone"
+}
+
+resource "google_dns_record_set" "playground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["data.terraform_remote_state.playground-state.outputs.playground_static_ip_address"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "playground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  for_each = toset(var.names)
+  name    = "each.value.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["data.terraform_remote_state.playground-state.outputs.playground_static_ip_address"]
+  ttl     = 300
+}
+
+resource "random_id" "rnd" {
+  byte_length = 4
+}
