ccd7d5551cb2fd4069f3a38d4b596e09c3c8d1ad
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-10-28 16:54:40 +0500

    Update main.tf

--- playground/terraform/infrastructure/main.tf
+++ playground/terraform/infrastructure/main.tf
@@ -102,5 +102,4 @@ module "api_enable" {
 module "clouddns" {
   depends_on        = [module.setup, module.artifact_registry, module.memorystore, module.network, module.api_enable, module.ip_address]
   source            = "./cluddns"
-  project_id        = var.project_id
 }
