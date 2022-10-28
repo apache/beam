9710bdf60bdd63ef8506d4cb3886af85ea1d94fa
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-10-28 18:49:08 +0500

    Update main.tf

--- playground/terraform/infrastructure/ip_address/main.tf
+++ playground/terraform/infrastructure/ip_address/main.tf
@@ -20,3 +20,61 @@
 resource "google_compute_global_address" "pg-ip" {
  name        = "pg-static-ip"
 }
+
+resource "google_dns_managed_zone" "playground" {
+  name        = "playground"
+  dns_name    = var.dnsname
+  description = "Playground DNS Zone"
+}
+
+resource "google_dns_record_set" "frontplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "goplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "go.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "javaplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "java.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "pythonplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "python.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "scioplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "scio.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "google_dns_record_set" "routerplayground" {
+  managed_zone = google_dns_managed_zone.playground.name
+  name    = "router.${google_dns_managed_zone.playground.dns_name}"
+  type    = "A"
+  rrdatas = ["var.static_ip"]
+  ttl     = 300
+}
+
+resource "random_id" "rnd" {
+  byte_length = 4
+}
