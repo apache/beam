70c3f569e3861577685a59a6bb5ab9ef528a07f4
alexeyinkin <alexey.inkin@akvelon.com>
2022-10-31 23:27:00 +0400

    Upgrade Akvelon editor (#23415) (#23900)
    * Upgrade Akvelon editor (#23415)
    
    * Fix running folded code (#23415)
    
    * Fix sharing folded code (#23415)

--- playground/frontend/playground_components/lib/src/controllers/snippet_editing_controller.dart
+++ playground/frontend/playground_components/lib/src/controllers/snippet_editing_controller.dart
@@ -44,7 +44,7 @@ class SnippetEditingController extends ChangeNotifier {
 
   set selectedExample(Example? value) {
     _selectedExample = value;
-    codeController.text = _selectedExample?.source ?? '';
+    setSource(_selectedExample?.source ?? '');
     _pipelineOptions = _selectedExample?.pipelineOptions ?? '';
     notifyListeners();
   }
@@ -63,7 +63,7 @@ class SnippetEditingController extends ChangeNotifier {
   }
 
   bool _isCodeChanged() {
-    return _selectedExample?.source != codeController.text;
+    return _selectedExample?.source != codeController.fullText;
   }
 
   bool _arePipelineOptionsChanged() {
@@ -82,10 +82,15 @@ class SnippetEditingController extends ChangeNotifier {
     //  user-shared examples, and an empty editor,
     //  https://github.com/apache/beam/issues/23252
     return ContentExampleLoadingDescriptor(
-      content: codeController.text,
+      content: codeController.fullText,
       name: _selectedExample?.name,
       complexity: _selectedExample?.complexity ?? Complexity.unspecified,
       sdk: sdk,
     );
+  }
+
+  void setSource(String source) {
+    codeController.text = source;
+    codeController.historyController.deleteHistory();
   }
 }
