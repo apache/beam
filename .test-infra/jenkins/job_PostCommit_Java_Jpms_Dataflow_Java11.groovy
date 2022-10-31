4adde54edc4a1aa01b8a20d5b1dc7f268ca09342
kileys <kileysok@google.com>
2022-10-31 23:05:11 +0000

    info switch

--- .test-infra/jenkins/job_PostCommit_Java_Jpms_Dataflow_Java11.groovy
+++ .test-infra/jenkins/job_PostCommit_Java_Jpms_Dataflow_Java11.groovy
@@ -43,7 +43,8 @@ PostcommitJobBuilder.postCommitJob('beam_PostCommit_Java_Jpms_Dataflow_Java11', 
           commonJobProperties.setGradleSwitches(delegate)
           switches("-Dorg.gradle.java.home=${commonJobProperties.JAVA_11_HOME}")
           // Specify maven home on Jenkins, needed by Maven archetype integration tests.
-          switches('-Pmaven_home=/home/jenkins/tools/maven/apache-maven-3.5.4')
+            switches('-Pmaven_home=/home/jenkins/tools/maven/apache-maven-3.5.4')
+            switches('--info')
         }
       }
     }
