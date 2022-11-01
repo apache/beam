a73a6240cd64bb405c4e99ad2c574f2d0c603c68
Nivaldo Tokuda <ni.h1@hotmail.com>
2022-10-31 21:03:09 -0300

    Add LogElements as a Beam PTransform (#23879)

--- learning/katas/python/Common Transforms/Aggregation/Sum/task.py
+++ learning/katas/python/Common Transforms/Aggregation/Sum/task.py
@@ -28,10 +28,8 @@
 
 import apache_beam as beam
 
-from log_elements import LogElements
-
 with beam.Pipeline() as p:
 
   (p | beam.Create(range(1, 11))
      | beam.CombineGlobally(sum)
-     | LogElements())
+     | beam.LogElements())
--- learning/katas/python/Common Transforms/Filter/Filter/task.py
+++ learning/katas/python/Common Transforms/Filter/Filter/task.py
@@ -28,10 +28,8 @@
 
 import apache_beam as beam
 
-from log_elements import LogElements
-
 with beam.Pipeline() as p:
 
   (p | beam.Create(range(1, 11))
      | beam.Filter(lambda num: num % 2 == 0)
-     | LogElements())
+     | beam.LogElements())
