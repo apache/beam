d06fdac1727e1e174d300f9521e59b7c7629fbf7
Bjorn Pedersen <bjornpedersen@google.com>
2022-11-04 12:40:54 -0400

    trying to get test_increment_failed_batches_counter to fail as expected

--- sdks/python/apache_beam/ml/inference/base_test.py
+++ sdks/python/apache_beam/ml/inference/base_test.py
@@ -176,15 +176,14 @@ class RunInferenceBaseTest(unittest.TestCase):
       with TestPipeline() as pipeline:
         examples = [7]
         pcoll = pipeline | 'start' >> beam.Create(examples)
-        inference_args = {'key': True}
-        _ = pcoll | base.RunInference(FakeModelHandlerFailsOnInferenceArgs())
+        _ = pcoll | base.RunInference(FakeModelHandlerExpectedInferenceArgs())
         run_result = pipeline.run()
         run_result.wait_until_finish()
 
         metric_results = (
             run_result.metrics().query(MetricsFilter().with_name('failed_batches_counter')))
         num_failed_batches_counter = metric_results['counters'][0]
-        self.assertGreater(num_failed_batches_counter.committed, 0)
+        self.assertEqual(num_failed_batches_counter.committed, 1)
 
   def test_failed_batches_counter_no_failures(self):
     pipeline = TestPipeline()
