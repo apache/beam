76d080556eeefa8535e547b65587c96c916ed92b
ahmedabu98 <ahmedabualsaud@google.com>
2022-11-04 19:51:54 +0000

    storage write schematransform configuration with validation

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/providers/BigQueryStorageWriteApiSchemaTransformProvider.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/providers/BigQueryStorageWriteApiSchemaTransformProvider.java
@@ -17,12 +17,22 @@
  */
 package org.apache.beam.sdk.io.gcp.bigquery.providers;
 
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.api.services.bigquery.model.TableSchema;
 import com.google.auto.service.AutoService;
 import com.google.auto.value.AutoValue;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
 import org.apache.beam.sdk.annotations.Experimental;
 import org.apache.beam.sdk.annotations.Experimental.Kind;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryServices;
 import org.apache.beam.sdk.io.gcp.bigquery.providers.BigQueryStorageWriteApiSchemaTransformProvider.BigQueryStorageWriteApiSchemaTransformConfiguration;
 import org.apache.beam.sdk.schemas.AutoValueSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
@@ -31,6 +41,8 @@ import org.apache.beam.sdk.schemas.transforms.SchemaTransformProvider;
 import org.apache.beam.sdk.schemas.transforms.TypedSchemaTransformProvider;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PCollectionRowTuple;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Strings;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
 
 /**
  * An implementation of {@link TypedSchemaTransformProvider} for BigQuery Storage Write API jobs
@@ -79,7 +91,123 @@ public class BigQueryStorageWriteApiSchemaTransformProvider
   /** Configuration for writing to BigQuery with Storage Write API. */
   @DefaultSchema(AutoValueSchema.class)
   @AutoValue
-  public abstract static class BigQueryStorageWriteApiSchemaTransformConfiguration {}
+  public abstract static class BigQueryStorageWriteApiSchemaTransformConfiguration {
+    static final Map<String, CreateDisposition> CREATE_DISPOSITIONS =
+        ImmutableMap.<String, CreateDisposition>builder()
+            .put(CreateDisposition.CREATE_IF_NEEDED.name(), CreateDisposition.CREATE_IF_NEEDED)
+            .put(CreateDisposition.CREATE_NEVER.name(), CreateDisposition.CREATE_NEVER)
+            .build();
+
+    static final Map<String, WriteDisposition> WRITE_DISPOSITIONS =
+        ImmutableMap.<String, WriteDisposition>builder()
+            .put(WriteDisposition.WRITE_TRUNCATE.name(), WriteDisposition.WRITE_TRUNCATE)
+            .put(WriteDisposition.WRITE_EMPTY.name(), WriteDisposition.WRITE_EMPTY)
+            .put(WriteDisposition.WRITE_APPEND.name(), WriteDisposition.WRITE_APPEND)
+            .build();
+
+    public void validate() {
+      String invalidConfigMessage = "Invalid BigQuery Storage Write configuration: ";
+
+      // validate output table spec
+      checkArgument(
+          !Strings.isNullOrEmpty(this.getOuputTable()),
+          invalidConfigMessage + "Table spec for a BigQuery Write must be specified.");
+      checkNotNull(BigQueryHelpers.parseTableSpec(this.getOuputTable()));
+
+      // validate create and write dispositions
+      CreateDisposition createDisposition = null;
+      WriteDisposition writeDisposition = null;
+      if (!Strings.isNullOrEmpty(this.getCreateDisposition())) {
+        checkNotNull(
+            CREATE_DISPOSITIONS.get(this.getCreateDisposition().toUpperCase()),
+            invalidConfigMessage
+                + "Invalid create disposition was specified. Available dispositions are: ",
+            CREATE_DISPOSITIONS.keySet());
+        createDisposition = CREATE_DISPOSITIONS.get(this.getCreateDisposition().toUpperCase());
+      }
+      if (!Strings.isNullOrEmpty(this.getWriteDisposition())) {
+        checkNotNull(
+            WRITE_DISPOSITIONS.get(this.getWriteDisposition().toUpperCase()),
+            invalidConfigMessage
+                + "Invalid write disposition was specified. Available dispositions are: ",
+            WRITE_DISPOSITIONS.keySet());
+        writeDisposition = WRITE_DISPOSITIONS.get(this.getWriteDisposition().toUpperCase());
+      }
+
+      // validate schema
+      if (!Strings.isNullOrEmpty(this.getJsonSchema())) {
+        // check if a TableSchema can be deserialized from the input schema string
+        checkNotNull(BigQueryHelpers.fromJsonString(this.getJsonSchema(), TableSchema.class));
+      } else if (!this.getUseBeamSchema()) {
+        checkArgument(
+            createDisposition == CreateDisposition.CREATE_NEVER,
+            invalidConfigMessage
+                + "Create disposition is CREATE_IF_NEEDED, but no schema was provided.");
+      }
+    }
+
+    /**
+     * Instantiates a {@link BigQueryStorageWriteApiSchemaTransformConfiguration.Builder} instance.
+     */
+    public static Builder builder() {
+      return new AutoValue_BigQueryStorageWriteApiSchemaTransformProvider_BigQueryStorageWriteApiSchemaTransformConfiguration
+          .Builder();
+    }
+
+    @Nullable
+    public abstract String getOuputTable();
+
+    @Nullable
+    public abstract String getJsonSchema();
+
+    @Nullable
+    public abstract boolean getUseBeamSchema();
+
+    @Nullable
+    public abstract String getCreateDisposition();
+
+    @Nullable
+    public abstract String getWriteDisposition();
+
+    @Nullable
+    public abstract int getNumStorageWriteApiStreams();
+
+    @Nullable
+    public abstract int getNumFileShards();
+
+    @Nullable
+    public abstract int getTriggeringFrequencySeconds();
+
+    @Nullable
+    public abstract BigQueryServices getBigQueryServices();
+
+    /** Builder for {@link BigQueryStorageWriteApiSchemaTransformConfiguration}. */
+    @AutoValue.Builder
+    public abstract static class Builder {
+      public abstract Builder setOuputTable(String tableSpec);
+
+      public abstract Builder setJsonSchema(String jsonSchema);
+
+      public abstract Builder setUseBeamSchema(boolean useBeamSchema);
+
+      public abstract Builder setCreateDisposition(String createDisposition);
+
+      public abstract Builder setWriteDisposition(String writeDisposition);
+
+      public abstract Builder setNumStorageWriteApiStreams(int numStorageWriteApiStreams);
+
+      public abstract Builder setNumFileShards(int numFileShards);
+
+      public abstract Builder setTriggeringFrequencySeconds(int seconds);
+
+      public abstract Builder setBigQueryServices(BigQueryServices bigQueryServices);
+
+      /** Builds a {@link BigQueryStorageWriteApiSchemaTransformConfiguration} instance. */
+      public abstract BigQueryStorageWriteApiSchemaTransformProvider
+              .BigQueryStorageWriteApiSchemaTransformConfiguration
+          build();
+    }
+  }
 
   private static class BigQueryStorageWriteApiSchemaTransform implements SchemaTransform {
     private final BigQueryStorageWriteApiSchemaTransformConfiguration configuration;
