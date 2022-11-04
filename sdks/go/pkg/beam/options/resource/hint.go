7bb2f3adb6c8e2da0d9fffe7a2108919f1007629
lostluck <13907733+lostluck@users.noreply.github.com>
2022-11-04 12:11:26 -0700

    [Go] Add resource package to home hints.

---
+++ sdks/go/pkg/beam/options/resource/hint.go
@@ -0,0 +1,164 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package resource supports Beam resource hints to specify scoped hints or annotations
+// to pipelines.
+//
+// See https://beam.apache.org/documentation/runtime/resource-hints/ for more information.
+package resource
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+// NewHints produces a hints map from a list of hints. If there are multiple hints
+// with the same URN, the last one in the list is used.
+func NewHints(hs ...Hint) Hints {
+	hints := Hints{}
+	for _, h := range hs {
+		hints[h.URN()] = h
+	}
+	return hints
+}
+
+// Hints contains a list of hints for a given scope.
+type Hints map[string]Hint
+
+// MergeWithOuter produces a new list of Hints from this Hints, and the Hints from the outer scope.
+func (hs Hints) MergeWithOuter(outer Hints) Hints {
+	if len(outer) == 0 {
+		return hs
+	}
+	if len(hs) == 0 {
+		return outer
+	}
+	merged := Hints{}
+	for k, o := range outer {
+		if h, ok := hs[k]; ok {
+			merged[k] = h.MergeWith(o)
+		} else {
+			merged[k] = o
+		}
+	}
+	// Always include any from the base, not already merged from outer.
+	for k, h := range hs {
+		if _, ok := outer[k]; !ok {
+			merged[k] = h
+		}
+	}
+	return merged
+}
+
+// Equal checks if two sets of hints are identical. A hint is identical to another if their payloads
+// are the same for a given URN.
+func (hs Hints) Equal(other Hints) bool {
+	if len(hs) != len(other) {
+		return false
+	}
+	for k, h := range hs {
+		o, ok := other[k]
+		if !ok {
+			return false
+		}
+		hp := h.Payload()
+		op := o.Payload()
+		if !bytes.Equal(hp, op) {
+			return false
+		}
+	}
+
+	return true
+}
+
+type Hint interface {
+	// URN returns the name for this hint.
+	URN() string
+	// Payload returns the serialized version of this payload.
+	Payload() []byte
+	// MergeWith an outer scope hint.
+	MergeWith(outer Hint) Hint
+}
+
+// MinRamBytes hints that this scope should be put in a machine with at least this many bytes of memory.
+//
+// Hints are advisory only and runners may not respect them.
+//
+// See https://beam.apache.org/documentation/runtime/resource-hints/ for more information about
+// resource hints.
+func MinRamBytes(v int64) Hint {
+	if v < 0 {
+		panic(fmt.Sprintf("negative min ram hint: %v", v))
+	}
+	return minRamHint{value: v}
+}
+
+type minRamHint struct {
+	value int64
+}
+
+func (minRamHint) URN() string {
+	return "beam:resources:min_ram_bytes:v1"
+}
+
+func (a minRamHint) Payload() []byte {
+	// Go strings are utf8, and if the string is ascii,
+	// byte conversion handles that directly.
+	return []byte(strconv.FormatInt(a.value, 10))
+}
+
+// MergeWith an outer minRamHints by keeping the maximum of the two byte amounts.
+func (h minRamHint) MergeWith(outer Hint) Hint {
+	// Intentional runtime panic from type assertion to catch hint merge errors.
+	if outer.(minRamHint).value > h.value {
+		return outer
+	}
+	return h
+}
+
+// Accelerator hints that this scope should be put in a machine with a given accelerator.
+//
+// Hints for accelerators will have formats that are runner specific.
+// For example, the following is valid accelerator syntax for the Dataflow runner:
+//
+//	accelerator="type:<type>;count:<n>;<options>"
+//
+// Hints are advisory only and runners may not respect them.
+//
+// See https://beam.apache.org/documentation/runtime/resource-hints/ for more information about
+// resource hints.
+func Accelerator(v string) Hint {
+	return acceleratorHint{value: v}
+}
+
+type acceleratorHint struct {
+	value string
+}
+
+func (acceleratorHint) URN() string {
+	return "beam:resources:accelerator:v1"
+}
+
+func (h acceleratorHint) Payload() []byte {
+	// Go strings are utf8, and if the string is ascii,
+	// byte conversion handles that directly.
+	return []byte(h.value)
+}
+
+// MergeWith an outer acceleratorHint by keeping this hint.
+func (h acceleratorHint) MergeWith(outer Hint) Hint {
+	return h
+}
