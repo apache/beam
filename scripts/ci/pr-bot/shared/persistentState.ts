7712968ff50328eccadf4f77f69c7e71018ad0a7
Danny McCormick <dannymccormick@google.com>
2022-11-07 14:53:24 -0500

    Add files then check cached diff to get untracked files

--- scripts/ci/pr-bot/shared/persistentState.ts
+++ scripts/ci/pr-bot/shared/persistentState.ts
@@ -41,9 +41,9 @@ async function commitStateToRepo() {
   }
   // Print changes for observability
   await exec.exec("git status", [], {ignoreReturnCode: true});
-  const changes = await exec.exec("git diff --quiet origin/pr-bot-state state", [], {ignoreReturnCode: true});
+  await exec.exec("git add state/*");
+  const changes = await exec.exec("git diff --quiet --cached origin/pr-bot-state state", [], {ignoreReturnCode: true});
   if (changes == 1) {
-    await exec.exec("git add state/*");
     await exec.exec(`git commit -m "Updating config from bot" --allow-empty`);
     await exec.exec("git push origin pr-bot-state");
   } else {
