7970d84c91d5a99d67d34108973dd5d4f70f6815
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-11-09 18:28:45 +0500

    Updates for apache #24007

--- playground/terraform/infrastructure/cloudbuild_sa/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" {
-  type = string
-  description = "The ID of the Google Cloud project within which resources are provisioned"
-}
-variable "cloudbuild_service_account_id" {
-  type = string
-  description = "The ID of the cloud build service account responsible for provisioning Google Cloud resources"
-}
