c1af66197ea09ff40a345183bb2db092307d922d
Yi Hu <yathu@google.com>
2022-11-11 13:39:50 -0500

    Set grace period to 30 days

--- .test-infra/tools/stale_dataflow_prebuilt_image_cleaner.sh
+++ .test-infra/tools/stale_dataflow_prebuilt_image_cleaner.sh
@@ -24,7 +24,7 @@ set -euo pipefail
 
 PUBLIC_REPOSITORIES=(beam-sdk beam_portability)
 PRIVATE_REPOSITORIES=(java-postcommit-it python-postcommit-it jenkins)
-DELETE_BEFORE_DAY=$(date --iso-8601=s -d '15 days ago')
+DELETE_BEFORE_DAY=$(date --iso-8601=s -d '30 days ago')
 
 REPOSITORIES=("${PUBLIC_REPOSITORIES[@]/#/gcr.io/apache-beam-testing/}" "${PRIVATE_REPOSITORIES[@]/#/us.gcr.io/apache-beam-testing/}")
 
