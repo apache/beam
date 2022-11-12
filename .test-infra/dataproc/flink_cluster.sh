d86cb36a442e9e5b3b9cf119703f02f235c7dc70
Yi Hu <yathu@google.com>
2022-11-12 09:02:28 -0500

    Minor fix flink_cluster script

--- .test-infra/dataproc/flink_cluster.sh
+++ .test-infra/dataproc/flink_cluster.sh
@@ -46,8 +46,9 @@ set -Eeuxo pipefail
 
 # GCloud properties
 GCLOUD_ZONE="${GCLOUD_ZONE:=us-central1-a}"
+# TODO: replace preview once dataproc 2.1 released
 DATAPROC_VERSION="${DATAPROC_VERSION:=preview-debian11}"
-GCLOUD_REGION="us-central1"
+GCLOUD_REGION=`echo $GCLOUD_ZONE | sed -E "s/(-[a-z])?$//"`
 
 MASTER_NAME="$CLUSTER_NAME-m"
 
@@ -131,9 +132,11 @@ function create_cluster() {
 
   # Docker init action restarts yarn so we need to start yarn session after this restart happens.
   # This is why flink init action is invoked last.
-  # TODO(11/11/2022) remove --worker-machine-type=n1-standard-2 once N2 CPUs quota relaxed
+  # TODO(11/11/2022) remove --worker-machine-type and --master-machine-type once N2 CPUs quota relaxed
   # Dataproc 2.1 uses n2-standard-2 by default but there is N2 CPUs=24 quota limit
-  gcloud dataproc clusters create $CLUSTER_NAME --region=$GCLOUD_REGION --num-workers=$FLINK_NUM_WORKERS --worker-machine-type=n1-standard-2 --metadata "${metadata}", --image-version=$image_version --zone=$GCLOUD_ZONE  --optional-components=FLINK,DOCKER  --quiet
+  gcloud dataproc clusters create $CLUSTER_NAME --region=$GCLOUD_REGION --num-workers=$FLINK_NUM_WORKERS \
+  --master-machine-type=n1-standard-2 --worker-machine-type=n1-standard-2 --metadata "${metadata}", \
+  --image-version=$image_version --zone=$GCLOUD_ZONE  --optional-components=FLINK,DOCKER  --quiet
 }
 
 # Runs init actions for Docker, Portability framework (Beam) and Flink cluster
