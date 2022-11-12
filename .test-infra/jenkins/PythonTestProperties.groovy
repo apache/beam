23229ea89bf34fdcc41e75b0d2bf7cc1b996000c
Chamikara Jayalath <chamikaramj@gmail.com>
2022-11-12 09:28:58 -0800

    Temporarily updating Python container generation

--- .test-infra/jenkins/PythonTestProperties.groovy
+++ .test-infra/jenkins/PythonTestProperties.groovy
@@ -23,7 +23,6 @@ class PythonTestProperties {
     '3.7',
     '3.8',
     '3.9',
-    '3.10'
   ]
   final static List<String> SUPPORTED_CONTAINER_TASKS = ALL_SUPPORTED_VERSIONS.collect {
     "py${it.replace('.', '')}"
