1a001d39ae5f87dc934aab39934d238988c9bb7a
Chris Gavin <chrisgavin@outlook.com.au>
2022-11-13 00:06:38 +1100

    Add unit tests for reading and writing. Refactor read/write functions to support injecting a spanner client.

--- sdks/go/pkg/beam/io/spannerio/spanner.go
+++ sdks/go/pkg/beam/io/spannerio/spanner.go
@@ -50,7 +50,7 @@ func columnsFromStruct(t reflect.Type) []string {
 // Read reads all rows from the given table. The table must have a schema
 // compatible with the given type, t, and Read returns a PCollection<t>. If the
 // table has more rows than t, then Read is implicitly a projection.
-func Read(s beam.Scope, database, table string, t reflect.Type) beam.PCollection {
+func Read(s beam.Scope, database string, table string, t reflect.Type) beam.PCollection {
 	s = s.Scope("spanner.Read")
 
 	// TODO(herohde) 7/13/2017: using * is probably too inefficient. We could infer
@@ -58,7 +58,7 @@ func Read(s beam.Scope, database, table string, t reflect.Type) beam.PCollection
 
 	cols := strings.Join(columnsFromStruct(t), ",")
 
-	return query(s, database, fmt.Sprintf("SELECT %v from [%v]", cols, table), t)
+	return query(s, database, nil, fmt.Sprintf("SELECT %v from %v", cols, table), t)
 }
 
 // queryOptions represents additional options for executing a query.
@@ -69,12 +69,13 @@ type queryOptions struct {
 // type, t. It returns a PCollection<t>.
 // Note: Query will be executed on a single worker. Consider performance of query
 // and if downstream splitting is required add beam.Reshuffle.
-func Query(s beam.Scope, database, q string, t reflect.Type, options ...func(*queryOptions) error) beam.PCollection {
+func Query(s beam.Scope, database string, q string, t reflect.Type, options ...func(*queryOptions) error) beam.PCollection {
 	s = s.Scope("spanner.Query")
-	return query(s, database, q, t, options...)
+	return query(s, database, nil, q, t, options...)
 }
 
-func query(s beam.Scope, database, query string, t reflect.Type, options ...func(*queryOptions) error) beam.PCollection {
+// Entrypoint for testing with spanner client injectable.
+func query(s beam.Scope, database string, client *spanner.Client, query string, t reflect.Type, options ...func(*queryOptions) error) beam.PCollection {
 	queryOptions := queryOptions{}
 	for _, opt := range options {
 		if err := opt(&queryOptions); err != nil {
@@ -83,31 +84,40 @@ func query(s beam.Scope, database, query string, t reflect.Type, options ...func
 	}
 
 	imp := beam.Impulse(s)
-	return beam.ParDo(s, &queryFn{Database: database, Query: query, Type: beam.EncodedType{T: t}, Options: queryOptions}, imp, beam.TypeDefinition{Var: beam.XType, T: t})
+	return beam.ParDo(s, &queryFn{Database: database, Query: query, Type: beam.EncodedType{T: t}, Options: queryOptions, Client: client}, imp, beam.TypeDefinition{Var: beam.XType, T: t})
 }
 
 type queryFn struct {
-	// Database is the spanner connection string
-	Database string `json:"database"`
-	// Table is the table identifier.
-	Query string `json:"query"`
-	// Type is the encoded schema type.
-	Type beam.EncodedType `json:"type"`
-	// Options specifies additional query execution options.
-	Options queryOptions `json:"options"`
+	Database string           `json:"database"` // Database is the spanner connection string
+	Query    string           `json:"query"`    // Table is the table identifier.
+	Type     beam.EncodedType `json:"type"`     // Type is the encoded schema type.
+	Options  queryOptions     `json:"options"`  // Options specifies additional query execution options.
+
+	Client *spanner.Client // Spanner Client
+}
+
+func (f *queryFn) Setup() {
+	if f.Client == nil {
+		client, err := spanner.NewClient(context.Background(), f.Database)
+		if err != nil {
+			panic("Failed to initialise Spanner client: " + err.Error())
+		}
+
+		f.Client = client
+	}
+}
+
+func (f *queryFn) Teardown() {
+	if f.Client != nil {
+		f.Client.Close()
+	}
 }
 
 func (f *queryFn) ProcessElement(ctx context.Context, _ []byte, emit func(beam.X)) error {
-	client, err := spanner.NewClient(ctx, f.Database)
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	// todo: Use Batch Read
 
 	stmt := spanner.Statement{SQL: f.Query}
-	it := client.Single().Query(ctx, stmt)
+	it := f.Client.Single().Query(ctx, stmt)
 	defer it.Stop()
 
 	for {
@@ -144,11 +154,18 @@ func UseBatchSize(batchSize int) func(qo *writeOptions) error {
 // Write writes the elements of the given PCollection<T> to spanner. T is required
 // to be the schema type.
 // Note: Writes occur against a single worker machine.
-func Write(s beam.Scope, database, table string, col beam.PCollection, options ...func(*writeOptions) error) {
+func Write(s beam.Scope, database string, table string, col beam.PCollection, options ...func(*writeOptions) error) {
 	if typex.IsCoGBK(col.Type()) || typex.IsKV(col.Type()) {
 		panic("Unsupported collection type - only normal structs supported for writing.")
 	}
 
+	s = s.Scope("spanner.Write")
+
+	write(s, database, nil, table, col, options...)
+}
+
+// Entrypoint for testing with spanner client injectable.
+func write(s beam.Scope, database string, client *spanner.Client, table string, col beam.PCollection, options ...func(*writeOptions) error) {
 	writeOptions := writeOptions{
 		BatchSize: 1000, // default
 	}
@@ -160,11 +177,9 @@ func Write(s beam.Scope, database, table string, col beam.PCollection, options .
 
 	t := col.Type().Type()
 
-	s = s.Scope("spanner.Write")
-
 	pre := beam.AddFixedKey(s, col)
 	post := beam.GroupByKey(s, pre)
-	beam.ParDo0(s, &writeFn{Database: database, Table: table, Type: beam.EncodedType{T: t}, Options: writeOptions}, post)
+	beam.ParDo0(s, &writeFn{Database: database, Table: table, Type: beam.EncodedType{T: t}, Options: writeOptions, Client: client}, post)
 }
 
 type writeFn struct {
@@ -172,15 +187,28 @@ type writeFn struct {
 	Table    string           `json:"table"`    // The table to write to
 	Type     beam.EncodedType `json:"type"`     // Type is the encoded schema type.
 	Options  writeOptions     `json:"options"`  // Spanner write options
+
+	Client *spanner.Client // Spanner Client
+}
+
+func (f *writeFn) Setup() {
+	if f.Client == nil {
+		client, err := spanner.NewClient(context.Background(), f.Database)
+		if err != nil {
+			panic("Failed to initialise Spanner client: " + err.Error())
+		}
+
+		f.Client = client
+	}
+}
+
+func (f *writeFn) Teardown() {
+	if f.Client != nil {
+		f.Client.Close()
+	}
 }
 
 func (f *writeFn) ProcessElement(ctx context.Context, _ int, iter func(*beam.X) bool) error {
-	client, err := spanner.NewClient(ctx, f.Database)
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	var mutations []*spanner.Mutation
 
 	var val beam.X
@@ -193,7 +221,7 @@ func (f *writeFn) ProcessElement(ctx context.Context, _ int, iter func(*beam.X) 
 		mutations = append(mutations, mutation)
 
 		if len(mutations)+1 > f.Options.BatchSize {
-			_, err := client.Apply(ctx, mutations)
+			_, err := f.Client.Apply(ctx, mutations)
 			if err != nil {
 				return err
 			}
@@ -203,7 +231,7 @@ func (f *writeFn) ProcessElement(ctx context.Context, _ int, iter func(*beam.X) 
 	}
 
 	if mutations != nil {
-		_, err := client.Apply(ctx, mutations)
+		_, err := f.Client.Apply(ctx, mutations)
 		if err != nil {
 			return err
 		}
