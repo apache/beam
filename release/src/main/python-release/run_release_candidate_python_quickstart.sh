6edc816852c12a8b56f669b3d3267fb70208d449
Chamikara Jayalath <chamikara@apache.org>
2022-11-16 15:16:06 -0800

    Temporary update Python RC validation job

--- release/src/main/python-release/run_release_candidate_python_quickstart.sh
+++ release/src/main/python-release/run_release_candidate_python_quickstart.sh
@@ -230,7 +230,7 @@ function run_release_candidate_python_quickstart(){
   ASC_FILE_NAME=$(get_asc_name $1)
   SHA512_FILE_NAME=$(get_sha512_name $1)
 
-  verify_hash
+  # verify_hash
   install_sdk $1 $2
   verify_wordcount_direct
   verify_wordcount_dataflow
