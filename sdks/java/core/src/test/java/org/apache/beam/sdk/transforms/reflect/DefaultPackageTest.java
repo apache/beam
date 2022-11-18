6ca51bb0451942c9f7a84c56102c857b5c401ae6
Kenneth Knowles <klk@google.com>
2022-11-17 20:03:57 -0800

    Add package declaration to DefaultPackageTest

--- sdks/java/core/src/test/java/org/apache/beam/sdk/transforms/reflect/DefaultPackageTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/transforms/reflect/DefaultPackageTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.beam.sdk.transforms.reflect;
 
 // Do not add a package declaration: this test excersizes handling of the default package, see
 // https://github.com/apache/beam/issues/22784
