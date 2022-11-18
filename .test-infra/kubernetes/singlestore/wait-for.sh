026563483809edb7d7e9873b729830d7d857e41e
AdalbertMemSQL <55380838+AdalbertMemSQL@users.noreply.github.com>
2022-11-18 22:26:48 +0200

    SingleStoreIO (#23535)
    Summary:
    Implemented SingleStoreIO according to this design doc:
    https://docs.google.com/document/d/1WU-hkoZ93SaGXyOz_UtX0jXzIRl194hCId_IdmEV9jw/edit?usp=sharing
    
    Changed SingleStoreUtil to be final class.
    Changed CHANGES.md file.
    Increased number of rows in the integration test.

---
+++ .test-infra/kubernetes/singlestore/wait-for.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#    Licensed to the Apache Software Foundation (ASF) under one or more
+#    contributor license agreements.  See the NOTICE file distributed with
+#    this work for additional information regarding copyright ownership.
+#    The ASF licenses this file to You under the Apache License, Version 2.0
+#    (the "License"); you may not use this file except in compliance with
+#    the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+#    Set of common operations that CI needs to invoke when using Kubernetes.
+#    The operations can be invoked using a provided kubeconfig file
+#    and kubernetes namespace.
+#
+#    Specify the following environment variables to override defaults:
+#    - KUBECONFIG: path to .kube/config file (default: $HOME/.kube/config)
+#    - KUBERNETES_NAMESPACE: namespace to be used (default: default)
+
+set -euxo pipefail
+
+KUBECONFIG="${KUBECONFIG:=$HOME/.kube/config}"
+KUBERNETES_NAMESPACE="${KUBERNETES_NAMESPACE:=default}"
+KUBECTL="kubectl --kubeconfig=$KUBECONFIG --namespace=$KUBERNETES_NAMESPACE"
+
+resource=$1
+timeout=$2
+
+echo "Waiting for the $(resource) ..."
+
+SECONDS=0
+until [ "$(${KUBECTL} get "${resource}" -o=jsonpath="{.items[*]['status.phase']}")" == "Running" ]
+do
+  if (( SECONDS > timeout ))
+  then
+   echo "Timed out waiting for the $(resource)"
+   return 1
+  fi
+
+  sleep 1
+done
+
+echo "$(resource) is running"
