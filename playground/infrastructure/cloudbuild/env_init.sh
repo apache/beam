86355e7349496803e4a9ce6a918cc572e4b48da7
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-11-23 16:13:40 +0500

    Updates of cloud build logic

--- playground/infrastructure/cloudbuild/env_init.sh
+++ playground/infrastructure/cloudbuild/env_init.sh
@@ -18,10 +18,9 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 
 # Install Terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
-| tee /etc/apt/sources.list.d/hashicorp.list
-apt update -y && apt install -y terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y && sudo apt install -y terraform
 
 # Install kubectl
 apt-get install -y apt-transport-https ca-certificates gnupg
