a4c6ab8b98c2fb7a5d7f4fe344a573510bc01d6b
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-11-23 19:17:46 +0500

    Updates related to PR #24144

--- playground/infrastructure/cloudbuild/env_init.sh
+++ playground/infrastructure/cloudbuild/env_init.sh
@@ -17,32 +17,33 @@
 
 GO_VERSION=1.18
 
-apt update >/dev/null
+apt update > /dev/null
 
 # Install JRE
-apt install -y default-jre && apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg2 wget
+apt install -y default-jre > /dev/null \
+&& apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg2 wget > /dev/null
 
 # Install Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update && apt install -y docker-ce
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /dev/null
+apt update > /dev/null && apt install -y docker-ce > /dev/null
 
 #Install Helm
-curl -fsSLo get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+curl -fsSLo get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 > /dev/null
 chmod 700 get_helm.sh
 ./get_helm.sh
 
 # Install Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt update -y && apt install -y terraform
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+| tee /etc/apt/sources.list.d/hashicorp.list
+apt update -y > /dev/null && apt install -y terraform > /dev/null
 
 # Install kubectl
-apt-get install -y apt-transport-https ca-certificates gnupg
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
 | tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update && apt install -y kubectl
+apt update > /dev/null && apt install -y kubectl > /dev/null
 
 # Install golang
 curl -O https://dl.google.com/go/go"$GO_VERSION".linux-amd64.tar.gz
