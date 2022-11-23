dfb72cf1d121cf8652ff43935594809bc1b50f02
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-11-23 15:40:59 +0500

    Updates with bash scripts for cloud builder

---
+++ playground/infrastructure/cloudbuild/env_init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+GO_VERSION=1.18
+
+apt update >/dev/null
+
+# Install JRE
+apt install -y default-jre && apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt install -y docker-ce
+
+#Install Helm
+curl -fsSLo get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+| tee /etc/apt/sources.list.d/hashicorp.list
+apt update -y && apt install -y terraform
+
+# Install kubectl
+apt-get install -y apt-transport-https ca-certificates gnupg
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
+| tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt install -y kubectl
+
+# Install golang
+curl -O https://dl.google.com/go/go"$GO_VERSION".linux-amd64.tar.gz
+tar -C /usr/local -xvf go"$GO_VERSION".linux-amd64.tar.gz
---
+++ playground/infrastructure/cloudbuild/tfvars_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir playground/terraform/environment/$_ENVIRONMENT_NAME
+printf '%s\n' \
+'project_id = "$PROJECT_ID"' \
+'network_name = "$_NETWORK_NAME"' \
+'gke_name = "$_GKE_NAME"' \
+'region = "$LOCATION"' \
+'location = "$LOCATION-a"' \
+'state_bucket = "$_STATE_BUCKET"' \
+'bucket_examples_name = "$_STATE_BUCKET-examples"' \
+> playground/terraform/environment/$_ENVIRONMENT_NAME/terraform.tfvars
+printf \
+'bucket = "$_STATE_BUCKET"'\
+> playground/terraform/environment/$_ENVIRONMENT_NAME/state.tfbackend
