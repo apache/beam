effc22477c8c9438b94619f6fa56a9d5a6f818a7
Jeffrey Kinard <jeff@thekinards.com>
2022-11-28 18:12:36 -0500

    Create MongoDBToBigQueryIntegrationTest
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>

--- src/main/java/com/google/cloud/teleport/it/TestProperties.java
+++ src/main/java/com/google/cloud/teleport/it/TestProperties.java
@@ -42,6 +42,7 @@ public final class TestProperties {
   public static final String REGION_KEY = "region";
   public static final String STAGE_BUCKET = "stageBucket";
   public static final String SPEC_PATH_KEY = "specPath";
+  public static final String HOST_IP = "hostIp";
 
   // From environment variables
   public static final String ACCESS_TOKEN_KEY = "DT_IT_ACCESS_TOKEN";
@@ -91,6 +92,10 @@ public final class TestProperties {
 
   public static String stageBucket() {
     return getProperty(STAGE_BUCKET, Type.PROPERTY, false);
+  }
+
+  public static String hostIp() {
+    return getProperty(HOST_IP, "localhost", Type.PROPERTY);
   }
 
   /** Gets a property or throws an exception if it is not found. */
