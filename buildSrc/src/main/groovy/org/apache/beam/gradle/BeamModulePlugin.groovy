6c807b3794d73286e749fd04ae494b52ae58e0e9
Kenneth Knowles <klk@google.com>
2022-12-01 12:06:37 -0800

    Moving to 2.45.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -390,7 +390,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.44.0'
+    project.version = '2.45.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
