d060372f844b0fa0783b1c175fc893afca0fe0b0
darkhan.nausharipov <darkhan.nausharipov@kzn.akvelon.com>
2022-12-06 13:46:19 +0600

    removed padding from user menu (#23692)

--- learning/tour-of-beam/frontend/lib/components/profile/user_menu.dart
+++ learning/tour-of-beam/frontend/lib/components/profile/user_menu.dart
@@ -39,9 +39,8 @@ class UserMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OverlayBody(
-      child: Container(
+      child: SizedBox(
         width: TobSizes.authOverlayWidth,
-        padding: const EdgeInsets.all(BeamSizes.size24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
