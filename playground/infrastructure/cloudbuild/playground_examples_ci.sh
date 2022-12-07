4139b1f377080eca2b88fe4bf1f5526fe47d9fed
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-12-07 17:09:00 +0500

    Update playground_examples_ci.sh

--- playground/infrastructure/cloudbuild/playground_examples_ci.sh
+++ playground/infrastructure/cloudbuild/playground_examples_ci.sh
@@ -23,7 +23,8 @@ apt-get update > /dev/null
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y software-properties-common curl unzip > /dev/null
 add-apt-repository -y ppa:deadsnakes/ppa > /dev/null && apt update > /dev/null
-apt install -y python3.8 python3-pip > /dev/null
+apt install -y python3.8 python3.8-distutils python3-pip > /dev/null
+apt install --reinstall python3.8-distutils
 python3.8 -m pip install pip --upgrade
 ln -s /usr/bin/python3.8 /usr/bin/python
 # Install jdk and gradle
