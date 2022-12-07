55519e62bd3836221e1c3df140a7d2a278692a12
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-12-07 18:54:34 +0500

    Update playground_examples_ci.sh

--- playground/infrastructure/cloudbuild/playground_examples_ci.sh
+++ playground/infrastructure/cloudbuild/playground_examples_ci.sh
@@ -37,13 +37,13 @@ unzip gradle-${GRADLE_VERSION}-bin.zip > /dev/null
 export PATH=$PATH:gradle-${GRADLE_VERSION}/bin
 
 # Install go
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt update > /dev/null && apt install -y docker-ce > /dev/null
+curl -OL https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz > /dev/null
+tar -C /usr/local -xvf go$GO_VERSION.linux-amd64.tar.gz > /dev/null
+export PATH=$PATH:/usr/local/go/bin
 
 # Install Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" > /dev/null
 apt update > /dev/null && apt install -y docker-ce > /dev/null
 
 
