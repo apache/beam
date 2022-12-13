3c59653c89ab69592afb7fd3e969cf325e2ed9e6
riteshghorse <riteshghorse@gmail.com>
2022-12-13 22:21:46 +0530

    Revert "remove python specific check"
    This reverts commit 083276b4e2e8de73d04ffe7e37100d91ce7e088f.

--- sdks/go/pkg/beam/core/runtime/graphx/translate.go
+++ sdks/go/pkg/beam/core/runtime/graphx/translate.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/graph"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/graph/coder"
@@ -689,12 +690,16 @@ func (m *marshaller) expandCrossLanguage(namedEdge NamedEdge) (string, error) {
 
 	// Add the coders for output in the marshaller even if expanded is nil
 	// for output coder field in expansion request.
-	for _, out := range edge.Output {
-		id, err := m.coders.Add(out.To.Coder)
-		if err != nil {
-			return "", errors.Wrapf(err, "failed to add output coder to coder registry: %v", m.coders)
+	// We need this specifically for Python External Transforms.
+	names := strings.Split(spec.Urn, ":")
+	if len(names) > 2 && names[2] == "python" {
+		for _, out := range edge.Output {
+			id, err := m.coders.Add(out.To.Coder)
+			if err != nil {
+				return "", errors.Wrapf(err, "failed to add output coder to coder registry: %v", m.coders)
+			}
+			out.To.Coder.ID = id
 		}
-		out.To.Coder.ID = id
 	}
 
 	if edge.External.Expanded != nil {
