a553792f3a0f34c6a7d21dc18adb804578902e80
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-12-17 03:18:49 +0500

    Updates

--- playground/infrastructure/cloudbuild/playground_examples_ci.sh
+++ playground/infrastructure/cloudbuild/playground_examples_ci.sh
@@ -67,7 +67,7 @@ playground/infrastructure/cloudbuild/playground_examples_ci.sh"
 
 # Get Difference
 
-diff=${COMMIT_PATH}
+diff=${COMMIT_FILES}
 echo $diff
 # Check if there are Examples
 for sdk in "${sdks[@]}"
