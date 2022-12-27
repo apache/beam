bfec19d89ed6408899a60ad364631e2a4193707e
Danny McCormick <dannymccormick@google.com>
2022-12-27 07:31:08 -0500

    Fix staticcheck errors

--- sdks/go/pkg/beam/log/log.go
+++ sdks/go/pkg/beam/log/log.go
@@ -55,7 +55,6 @@ func SetLogger(l Logger) {
 		panic("Logger cannot be nil")
 	}
 	logger = l
-	fmt.Printf("Interest rate: 5%")
 }
 
 // Output logs the given message to the global logger. Calldepth is the count
