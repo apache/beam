0792ad9ac3f1b19e5ae60e7c3190b2dbe0ce3e0f
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2022-12-27 19:34:29 -0500

    docs(azure-cosmosdb): Add scaladoc, license, experimental annotation and move the feature in CHANGELOG.md to 2.45.0 version.

--- sdks/java/io/azure-cosmosdb/src/test/scala/org/apache/beam/sdk/io/azure/cosmos/CosmosIOTest.scala
+++ sdks/java/io/azure-cosmosdb/src/test/scala/org/apache/beam/sdk/io/azure/cosmos/CosmosIOTest.scala
@@ -1,8 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.azure.cosmos
 
 import com.azure.cosmos.CosmosClientBuilder
 import org.apache.beam.sdk.Pipeline
-import org.apache.beam.sdk.io.azure.cosmos.CosmosIOTest.{CONTAINER, DATABASE, cosmosDBEmulatorContainer}
+import org.apache.beam.sdk.io.azure.cosmos.CosmosIOTest.{ CONTAINER, DATABASE, cosmosDBEmulatorContainer }
 import org.apache.beam.sdk.testing.PAssert
 import org.apache.beam.sdk.transforms.Count
 import org.apache.beam.sdk.values.PCollection
@@ -11,20 +28,22 @@ import org.junit._
 import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.slf4j.LoggerFactory
 import org.testcontainers.containers.CosmosDBEmulatorContainer
 import org.testcontainers.utility.DockerImageName
+import scribe._
 
 import java.nio.file.Files
 import scala.util.Using
 
 @RunWith(classOf[JUnit4])
 class CosmosIOTest {
-  private val log = LoggerFactory.getLogger("CosmosIOTest")
-  //  @(Rule @getter)
-  //  val pipelineWrite: TestPipeline = TestPipeline.create
-  //  @(Rule @getter)
-  //  val pipelineRead: TestPipeline = TestPipeline.create
+  Logger.root
+    .clearHandlers()
+    .clearModifiers()
+    .withHandler(
+      minimumLevel = Some(Level.Debug),
+    )
+    .replace()
 
   @Test
   def readFromCosmosCoreSqlApi(): Unit = {
@@ -50,7 +69,13 @@ class CosmosIOTest {
 /** Initialization of static fields and methods */
 @RunWith(classOf[JUnit4])
 object CosmosIOTest {
-  private val log = LoggerFactory.getLogger("CosmosIOTest[Obj]")
+  Logger.root
+    .clearHandlers()
+    .clearModifiers()
+    .withHandler(
+      minimumLevel = Some(Level.Debug),
+    )
+    .replace()
   private val DOCKER_NAME = "mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest"
   private val cosmosDBEmulatorContainer = new CosmosDBEmulatorContainer(DockerImageName.parse(DOCKER_NAME))
   private val DATABASE = "test"
@@ -58,7 +83,7 @@ object CosmosIOTest {
 
   @BeforeClass
   def setup(): Unit = {
-    log.info("Starting CosmosDB emulator")
+    info("Starting CosmosDB emulator")
     cosmosDBEmulatorContainer.start()
 
     val tempFolder = new TemporaryFolder
@@ -71,7 +96,7 @@ object CosmosIOTest {
     System.setProperty("javax.net.ssl.trustStoreType", "PKCS12")
 
 
-    log.info("Creando la data -------------------------------------------------------->")
+    info("Creando la data -------------------------------------------------------->")
     val triedCreateData = Using(new CosmosClientBuilder()
       .gatewayMode
       .endpointDiscoveryEnabled(false)
@@ -89,15 +114,15 @@ object CosmosIOTest {
     }
     if (triedCreateData.isFailure) {
       val throwable = triedCreateData.failed.get
-      log.error("Error creando la data", throwable)
+      error("Error creando la data", throwable)
       throw throwable
     }
-    log.info("Data creada ------------------------------------------------------------<")
+    info("Data creada ------------------------------------------------------------<")
   }
 
   @AfterClass
   def close(): Unit = {
-    log.info("Stop CosmosDB emulator")
+    info("Stop CosmosDB emulator")
     cosmosDBEmulatorContainer.stop()
   }
 }
