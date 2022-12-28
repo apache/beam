736e9e7e685bed5bafe21fc052a3f0ebd69d5635
Johanna Ojeling <johanna@ojeling.net>
2022-12-28 17:11:18 +0100

    Add doc comment to containers package

--- sdks/go/test/integration/internal/containers/containers.go
+++ sdks/go/test/integration/internal/containers/containers.go
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package containers contains utilities for running test containers in integration tests.
 package containers
 
 import (
