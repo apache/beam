2cfe8215efa5538717935444603925c062b595b1
Anand Inguva <anandinguva98@gmail.com>
2022-12-31 10:15:00 -0500

    Add running perf alert tool to GHA

--- sdks/python/apache_beam/testing/analyzers/github_issues_utils.py
+++ sdks/python/apache_beam/testing/analyzers/github_issues_utils.py
@@ -32,8 +32,7 @@ except KeyError as e:
       'A Github Personal Access token is required '
       'to create Github Issues.')
 
-# TODO: Change the REPO owner name to apache before merging.
-_BEAM_GITHUB_REPO_OWNER = 'AnandInguva'
+_BEAM_GITHUB_REPO_OWNER = 'apache'
 _BEAM_GITHUB_REPO_NAME = 'beam'
 # Adding GitHub Rest API version to the header to maintain version stability.
 # For more information, please look at
