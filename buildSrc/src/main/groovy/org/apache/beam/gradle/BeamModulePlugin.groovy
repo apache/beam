bc7fc9fd0bd4ddc8fefb6bbe48b27e8a7a1fac83
Zhenglin-Li <1125806272@qq.com>
2022-12-31 09:16:56 +0800

    improve error message

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -2365,7 +2365,7 @@ class BeamModulePlugin implements Plugin<Project> {
       } else if (JavaVersion.current() == JavaVersion.VERSION_17) {
         javaContainerSuffix = 'java17'
       } else {
-        String exceptionMessage = "unsupported Java version. You need Java version of 8 or 11 or 17 to get started, but your Java version is: " + JavaVersion.current();
+        String exceptionMessage = "Your Java version is unsupported. You need Java version of 8 or 11 or 17 to get started, but your Java version is: " + JavaVersion.current();
         throw new GradleException(exceptionMessage)
       }
       def setupTask = project.tasks.register(config.name+"Setup", Exec) {
