aa7ab881ff39b3f80f7d0b452029972dc69dfedf
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-05 19:48:00 +0500

    Adding Tour of Beam IaC

---
+++ learning/tour-of-beam/terraform/cloud_functions/variables.tf
@@ -0,0 +1,40 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+variable "service_account_id" {
+  description = "The name of Service Account to run Cloud Function"
+}
+
+variable "project_id" {
+  description = "The GCP Project ID of function"
+}
+
+variable "region" {
+  description = "The GCP Region of function"
+}
+
+variable "source_archive_bucket" {
+  description = "The GCS bucket containing the zip archive which contains the function"
+}
+variable "source_archive_object" {
+  description = "The source archive object (file) in archive bucket"
+}
+
+variable "entry_point_names" {
+  type = list
+  default = ["getSdkList", "getContentTree", "getUnitContent"]
+}
---
+++ learning/tour-of-beam/terraform/main.tf
@@ -0,0 +1,47 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module "setup" {
+  source = "./setup"
+  project_id = var.project_id
+  service_account_id = var.service_account_id
+  depends_on = [module.api_enable]
+}
+
+module "functions_buckets" {
+  source = "./functions_buckets"
+  project_id = var.project_id
+  region      = var.region
+  bucket_name = var.bucket_name
+  depends_on = [module.setup, module.api_enable]
+}
+
+module "api_enable" {
+  source = "./api_enable"
+  project_id = var.project_id
+}
+
+module "cloud_functions" {
+  source = "./cloud_functions"
+  region = var.region
+  project_id = var.project_id
+  bucket_name = var.bucket_name
+  service_account_id = module.setup.service-account-email
+  source_archive_bucket = module.functions_buckets.functions-bucket-name
+  source_archive_object = module.functions_buckets.function-bucket-object
+  depends_on = [module.functions_buckets, module.setup, module.api_enable]
+}
