5c936c1963208a0a7f9c147903803970f55c1c2b
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-10 17:50:53 +0500

    Update main.tf

--- learning/tour-of-beam/terraform/api_enable/main.tf
+++ learning/tour-of-beam/terraform/api_enable/main.tf
@@ -18,6 +18,7 @@
 resource "google_project_service" "required_services" {
   project   = var.project_id
   for_each  = toset([
+    "cloudresourcemanager",
     "cloudbuild",
     "cloudfunctions",
     "firebase",
