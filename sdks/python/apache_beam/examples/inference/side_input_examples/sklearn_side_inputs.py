bdb6598a859c819ef9d4d88ca1641878dfef9763
Anand Inguva <anandinguva98@gmail.com>
2023-01-13 18:34:13 -0500

    Update example

--- sdks/python/apache_beam/examples/inference/side_input_examples/sklearn_side_inputs.py
+++ sdks/python/apache_beam/examples/inference/side_input_examples/sklearn_side_inputs.py
@@ -15,13 +15,7 @@
 # limitations under the License.
 #
 
-"""
-This file is WIP.
-
-TODOs:
-1. Change the file name.
-2. Add unbounded source for Main input PCollection.
-"""
+# pylint: skip-file
 
 import argparse
 from typing import Iterable
@@ -29,7 +23,6 @@ from typing import List
 from typing import Tuple
 
 import apache_beam as beam
-from apache_beam.io.fileio import MatchContinuously
 from apache_beam.ml.inference.base import KeyedModelHandler
 from apache_beam.ml.inference.base import PredictionResult
 from apache_beam.ml.inference.base import RunInference
@@ -37,7 +30,7 @@ from apache_beam.ml.inference.sklearn_inference import SklearnModelHandlerNumpy
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.transforms import window
-from apache_beam.transforms import trigger
+from apache_beam.ml.inference.utils import WatchFilePattern
 
 
 def process_input(row: str) -> Tuple[int, List[int]]:
@@ -57,57 +50,88 @@ class PostProcessor(beam.DoFn):
     yield '{},{}'.format(label, prediction)
 
 
-# we need a side input that emits model path.
-def get_model_path_side_input(pipeline, file_pattern):
-  model_path = (
-      pipeline
-      | 'MatchContinuously' >> MatchContinuously(
-          file_pattern=file_pattern, interval=30)
-      | 'ApplyGlobalWindow' >> beam.transforms.WindowInto(
-          window.GlobalWindows(),
-          trigger=trigger.Repeatedly(trigger.AfterCount(1)),
-          accumulation_mode=trigger.AccumulationMode.DISCARDING))
-  return beam.pvalue.AsIter(model_path)
+# # we need a side input that emits model path.
+# @experimental()
+# def get_model_path_side_input(pipeline,
+#                               file_pattern,
+#                               interval=360,
+#                               start_timestamp=Timestamp.now(),
+#                               stop_timestamp=MAX_TIMESTAMP,
+#                               match_updated_files=False,
+#                               has_deduplication=True):
+#   """Watch for updates using the file pattern.
+#
+#     Args:
+#       file_pattern: The file path to read from.
+#       interval: Interval at which to check for files in seconds.
+#       has_deduplication: Whether files already read are discarded or not.
+#       start_timestamp: Timestamp for start file checking.
+#       stop_timestamp: Timestamp after which no more files will be checked.
+#       match_updated_files: (When has_deduplication is set to True) whether match
+#         file with timestamp changes.
+#     """
+#   model_path = (
+#       pipeline
+#       | 'MatchContinuously' >> MatchContinuously(
+#     file_pattern=file_pattern,
+#     interval=interval,
+#     start_timestamp=start_timestamp,
+#     stop_timestamp=stop_timestamp,
+#     match_updated_files=match_updated_files,
+#     has_deduplication=has_deduplication
+#   )
+#       | 'ApplyGlobalWindow' >> beam.transforms.WindowInto(
+#           window.GlobalWindows(),
+#           trigger=trigger.Repeatedly(trigger.AfterProcessingTime(5)),
+#           accumulation_mode=trigger.AccumulationMode.DISCARDING)
+#       | "GetModelPath" >> beam.Map(lambda x: x.path))
+#   return beam.pvalue.AsSingleton(model_path)
 
 
 def run(argv=None, save_main_session=True, test_pipeline=None):
-
+  """Entry point for running the pipeline."""
   known_args, pipeline_args = parse_known_args(argv)
   pipeline_options = PipelineOptions(pipeline_args)
   pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
+  interval = known_args.interval
+  file_pattern = known_args.file_pattern
+
   pipeline = test_pipeline
   if not test_pipeline:
     pipeline = beam.Pipeline(options=pipeline_options)
-  # side input pcollection that looks for any files matching file_pattern,
-  si_pcoll = get_model_path_side_input(
-      pipeline, file_pattern='gs://apache-beam-ml/tmp/side_input_test/*')
+
+  si_pcoll = pipeline | WatchFilePattern(
+      file_pattern=file_pattern, interval=interval)
 
   model_handler = KeyedModelHandler(
       SklearnModelHandlerNumpy(model_uri=known_args.model_path))
 
   label_pixel_tuple = (
       pipeline
-      # Implement unbounded source.
-      | "ReadFromText" >> beam.io.ReadFromText(known_args.input)
+      | "ReadFromPubSub" >> beam.io.ReadFromPubSub(known_args.topic)
       | "ApplyMainInputWindow" >> beam.WindowInto(
           beam.transforms.window.FixedWindows(1))
+      | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
       | "PreProcessInputs" >> beam.Map(process_input))
 
   predictions = (
       label_pixel_tuple
       | "RunInference" >> RunInference(
-          model_handler=model_handler, update_model_pcoll=si_pcoll)
+          model_handler=model_handler, model_path_pcoll=si_pcoll)
       | "PostProcessOutputs" >> beam.ParDo(PostProcessor()))
 
-  _ = predictions | "WriteOutput" >> beam.io.WriteToText(
-      known_args.output, shard_name_template='', append_trailing_newlines=True)
+  predictions | beam.Map(print)
 
-  # predictions | beam.Map(print)
+  # _ = predictions | "WriteOutput" >> beam.io.WriteToText(
+  #     known_args.output, shard_name_template='', append_trailing_newlines=True)
+
   result = pipeline.run().wait_until_finish()
   return result
 
 
+#
+#
 def parse_known_args(argv):
   """Parses args for the workflow."""
   parser = argparse.ArgumentParser()
@@ -130,6 +154,15 @@ def parse_known_args(argv):
       '--topic',
       default='projects/apache-beam-testing/topics/anandinguva-model-updates',
       help='Path to Pub/Sub topic.')
+  parser.add_argument(
+      '--file_pattern',
+      default='gs://apache-beam-ml/tmp/side_input_test/*.pickle',
+      help='Glob pattern to watch for an update.')
+  parser.add_argument(
+      '--interval',
+      default=360,
+      type=int,
+      help='interval used to look for updates on a given file_pattern.')
   return parser.parse_known_args(argv)
 
 
