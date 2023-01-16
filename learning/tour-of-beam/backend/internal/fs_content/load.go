e83afc2fae544a3463737a55a22bbe46bc158d05
Timur Sultanov <timur.sultanov@akvelon.com>
2023-01-16 17:14:27 +0400

    Process learning materials through the template engine

--- learning/tour-of-beam/backend/internal/fs_content/load.go
+++ learning/tour-of-beam/backend/internal/fs_content/load.go
@@ -19,7 +19,6 @@ import (
 	"bytes"
 	"fmt"
 	"io/fs"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -79,7 +78,11 @@ func collectUnit(infopath string, ctx *sdkContext) (unit *tob.Unit, err error) {
 				return filepath.SkipDir
 
 			case d.Name() == descriptionMd:
-				content, err := ioutil.ReadFile(path)
+				templateSource, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				content, err := processTemplate(templateSource, ctx.sdk)
 				if err != nil {
 					return err
 				}
@@ -87,7 +90,7 @@ func collectUnit(infopath string, ctx *sdkContext) (unit *tob.Unit, err error) {
 
 			// Here we rely on that WalkDir entries are lexically sorted
 			case regexp.MustCompile(hintMdRegexp).MatchString(d.Name()):
-				content, err := ioutil.ReadFile(path)
+				content, err := os.ReadFile(path)
 				if err != nil {
 					return err
 				}
