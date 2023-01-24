a75ec8779ae72e46ac8f4f17ee0a579be6f14d7a
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-24 20:46:48 +0500

    Update env_ci.sh

--- playground/infrastructure/env_ci.sh
+++ playground/infrastructure/env_ci.sh
@@ -136,6 +136,6 @@ do
             docker stop container-${sdk}
             docker rm container-${sdk}
       else
-            echo "Nothing changed in Examples. CI check is skipped"
+            echo "Nothing has changed in Examples. CI check is skipped"
       fi
 done
