4bfa32ec77b12a4da5cd4e8c88afbbe6d96ee8db
Byron Ellis <byron.ellis@gmail.com>
2023-01-25 15:49:31 -0800

    Basic version of the SQLTransformSchemaTransformProvider. Has basically a complete configuration and I think a (hacky) approach to getting DLQ information out of the transform at least good enough to use for testing. There's no unit test coverage in this module so not clear the existing external transform is tested/working either.

--- sdks/java/extensions/sql/expansion-service/src/main/java/org/apache/beam/sdk/extensions/sql/expansion/SqlTransformSchemaTransformProvider.java
+++ sdks/java/extensions/sql/expansion-service/src/main/java/org/apache/beam/sdk/extensions/sql/expansion/SqlTransformSchemaTransformProvider.java
@@ -17,13 +17,15 @@
  */
 package org.apache.beam.sdk.extensions.sql.expansion;
 
-import avro.shaded.com.google.common.collect.ImmutableList;
-import avro.shaded.com.google.common.collect.ImmutableMap;
 import com.google.auto.service.AutoService;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
 import java.util.stream.Collectors;
-
-import org.apache.beam.model.pipeline.v1.SchemaApi;
 import org.apache.beam.sdk.extensions.sql.SqlTransform;
 import org.apache.beam.sdk.extensions.sql.impl.QueryPlanner;
 import org.apache.beam.sdk.extensions.sql.meta.provider.TableProvider;
@@ -33,8 +35,12 @@ import org.apache.beam.sdk.schemas.logicaltypes.OneOfType;
 import org.apache.beam.sdk.schemas.transforms.SchemaTransform;
 import org.apache.beam.sdk.schemas.transforms.SchemaTransformProvider;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionRowTuple;
+import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.Row;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
 import org.checkerframework.checker.initialization.qual.Initialized;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
@@ -42,11 +48,12 @@ import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
 @AutoService(SchemaTransformProvider.class)
 public class SqlTransformSchemaTransformProvider implements SchemaTransformProvider {
 
-  private static final Map<String,Class<? extends QueryPlanner>> QUERY_PLANNERS = ImmutableMap.of(
-          "zetasql",org.apache.beam.sdk.extensions.sql.zetasql.ZetaSQLQueryPlanner.class,
-          "calcite",org.apache.beam.sdk.extensions.sql.impl.CalciteQueryPlanner.class
-  );
-  private static final EnumerationType QUERY_ENUMERATION = EnumerationType.create(QUERY_PLANNERS.keySet().stream().collect(Collectors.toList()));
+  private static final Map<String, Class<? extends QueryPlanner>> QUERY_PLANNERS =
+      ImmutableMap.of(
+          "zetasql", org.apache.beam.sdk.extensions.sql.zetasql.ZetaSQLQueryPlanner.class,
+          "calcite", org.apache.beam.sdk.extensions.sql.impl.CalciteQueryPlanner.class);
+  private static final EnumerationType QUERY_ENUMERATION =
+      EnumerationType.create(QUERY_PLANNERS.keySet().stream().collect(Collectors.toList()));
 
   private static final OneOfType QUERY_PARAMETER =
       OneOfType.create(
@@ -56,8 +63,7 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
           Schema.Field.nullable("long", Schema.FieldType.INT64),
           Schema.Field.nullable("float", Schema.FieldType.FLOAT),
           Schema.Field.nullable("double", Schema.FieldType.DOUBLE),
-              Schema.Field.nullable("datetime",Schema.FieldType.DATETIME)
-      );
+          Schema.Field.nullable("datetime", Schema.FieldType.DATETIME));
 
   @Override
   public @UnknownKeyFor @NonNull @Initialized String identifier() {
@@ -68,19 +74,20 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
   public @UnknownKeyFor @NonNull @Initialized Schema configurationSchema() {
     List<String> providers = new ArrayList<>();
     ServiceLoader.load(TableProvider.class)
-            .forEach(
-                    (provider) -> {
-                      providers.add(provider.getTableType());
-                    });
+        .forEach(
+            (provider) -> {
+              providers.add(provider.getTableType());
+            });
     EnumerationType providerEnum = EnumerationType.create(providers);
-
 
     return Schema.of(
         Schema.Field.of("query", Schema.FieldType.STRING),
-        Schema.Field.nullable( "ddl", Schema.FieldType.STRING), // TODO: Underlying builder seems more capable?
+        Schema.Field.nullable(
+            "ddl", Schema.FieldType.STRING), // TODO: Underlying builder seems more capable?
         Schema.Field.nullable("dialect", Schema.FieldType.logicalType(QUERY_ENUMERATION)),
         Schema.Field.nullable("autoload", Schema.FieldType.BOOLEAN),
-        Schema.Field.nullable("tableproviders", Schema.FieldType.array(Schema.FieldType.logicalType(providerEnum))),
+        Schema.Field.nullable(
+            "tableproviders", Schema.FieldType.array(Schema.FieldType.logicalType(providerEnum))),
         Schema.Field.nullable(
             "parameters",
             Schema.FieldType.logicalType(
@@ -108,7 +115,22 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
   @Override
   public @UnknownKeyFor @NonNull @Initialized List<@UnknownKeyFor @NonNull @Initialized String>
       outputCollectionNames() {
-    return ImmutableList.of("output", "errors");
+    return ImmutableList.of("output");
+  }
+
+  static class ErrorCapture extends PTransform<PCollection<Row>, PDone> {
+
+    transient List<PCollection<Row>> inputs = new ArrayList<>();
+
+    public List<PCollection<Row>> getInputs() {
+      return inputs;
+    }
+
+    @Override
+    public PDone expand(PCollection<Row> input) {
+      inputs.add(input);
+      return PDone.in(input.getPipeline());
+    }
   }
 
   static class SqlSchemaTransform implements SchemaTransform {
@@ -127,7 +149,6 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
         @Override
         public PCollectionRowTuple expand(PCollectionRowTuple input) {
 
-
           // Start with the query. In theory the exception can't be thrown, but all this nullness
           // stuff
           // isn't actually smart enough to know that. Could just cop and suppress that warning, but
@@ -139,17 +160,19 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
           SqlTransform transform = SqlTransform.query(queryString);
 
           // Allow setting the query planner class via the dialect name.
-          EnumerationType.Value dialect = config.getLogicalTypeValue("dialect",EnumerationType.Value.class);
+          EnumerationType.Value dialect =
+              config.getLogicalTypeValue("dialect", EnumerationType.Value.class);
           if (dialect != null) {
-            Class<? extends QueryPlanner> queryPlannerClass = QUERY_PLANNERS.get(QUERY_ENUMERATION.toString(dialect));
-            if(queryPlannerClass != null) {
+            Class<? extends QueryPlanner> queryPlannerClass =
+                QUERY_PLANNERS.get(QUERY_ENUMERATION.toString(dialect));
+            if (queryPlannerClass != null) {
               transform = transform.withQueryPlannerClass(queryPlannerClass);
             }
           }
 
           // Add any DDL strings
           String ddl = config.getString("ddl");
-          if(ddl != null) {
+          if (ddl != null) {
             transform = transform.withDdlString(ddl);
           }
 
@@ -172,7 +195,9 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
               for (Object nameObj : tableproviderList) {
                 if (nameObj != null) { // This actually could in theory be null...
                   TableProvider p = tableProviders.get(nameObj);
-                  if (p != null) { //TODO: We ignore tableproviders that don't exist, we could change that.
+                  if (p
+                      != null) { // TODO: We ignore tableproviders that don't exist, we could change
+                    // that.
                     transform = transform.withTableProvider(p.getTableType(), p);
                   }
                 }
@@ -180,9 +205,27 @@ public class SqlTransformSchemaTransformProvider implements SchemaTransformProvi
             }
           }
 
+          // TODO: Process query parameters. This is not necessary for Syndeo GA but would be
+          // really nice to have.
 
+          // TODO: See about reimplementing a correct version of SqlTransform
+          ErrorCapture errors = new ErrorCapture();
+          PCollection<Row> output = input.apply(transform.withErrorsTransformer(errors));
 
-          return PCollectionRowTuple.of("output", input.apply(transform));
+          // TODO: One possibility for capturing the required tables would be to inject a
+          // tableprovider
+          // that we control and see which tables are requested during expansion. We could then
+          // modify the output schema to reflect these inputs via options for better validation.
+
+          List<PCollection<Row>> errorList = errors.getInputs();
+          if (errorList.size() == 0) {
+            return PCollectionRowTuple.of("output", output);
+          } else if (errorList.size() == 1) {
+            return PCollectionRowTuple.of("output", output, "errors", errorList.get(0));
+          } else {
+            throw new UnsupportedOperationException(
+                "SqlTransform currently only supports a single dead letter queue collection");
+          }
         }
       };
     }
