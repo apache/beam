68118c546d86ecdbed3c8958c7e2f14fb4d956d8
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-25 20:02:24 +0500

    Update env_ci.sh

--- playground/infrastructure/env_ci.sh
+++ playground/infrastructure/env_ci.sh
@@ -71,7 +71,8 @@ allowlist=("playground/infrastructure" "playground/backend")
 if [ -z $branch_name ] || [ $branch_name == "master" ]; then
   branch_name=origin/master
 fi
-diff=$(git diff --name-only $branch_name $commit_sha | tr '\n' ' ')
+diff=$(git diff --name-only $branch_name $commit_sha)
+diff=$(echo $diff | tr ' ' '\n')
 echo "${diff}"
 
 # Check if there are Examples
