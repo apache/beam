88d79a4ae12937c2707f4b8bb6e4ad339baa237f
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-27 17:54:34 +0500

    Update env_ci.sh

--- playground/infrastructure/env_ci.sh
+++ playground/infrastructure/env_ci.sh
@@ -94,7 +94,7 @@ do
           example_has_changed=False
       fi
 
-# Run commands if examples have been changed
+# Run main logic if examples have been changed
       if [[ $example_has_changed == "True" ]]
       then
             if [ -z ${tag_name} ]
