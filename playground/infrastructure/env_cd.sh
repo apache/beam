c42e2571f4e9f3c4543b25ad31ab6edd3d11a34d
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-30 16:34:04 +0500

    Update env_cd.sh

--- playground/infrastructure/env_cd.sh
+++ playground/infrastructure/env_cd.sh
@@ -42,7 +42,7 @@ for sdk in "${sdks[@]}"
 do
   export SERVER_ADDRESS=https://${sdk}.${dns_name}
   python3 ci_cd.py \
-  --datastore-project $GOOGLE_CLOUD_PROJECT \
+  --datastore-project $PROJECT_ID \
   --step ${STEP} \
   --sdk SDK_"${sdk^^}" \
   --origin ${ORIGIN} \
