e39b2323a4cac74a8ea64830c6d6f06c9b5cafbb
Katie Liu <katiliu@linkedin.com>
2023-02-02 12:50:33 -0800

    Update version to 2.45.2 (#73)

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.45.1'
+    project.version = '2.45.2'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
