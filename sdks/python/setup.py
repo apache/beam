69ddf44e67db279ab49cc88a18dbf48c965bc669
tvalentyn <tvalentyn@users.noreply.github.com>
2023-02-08 11:40:55 -0800

    Update upper bound for numpy. (#24725)

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -140,7 +140,7 @@ except ImportError:
 if sys.platform == 'win32' and sys.maxsize <= 2**32:
   pyarrow_dependency = ''
 else:
-  pyarrow_dependency = 'pyarrow>=0.15.1,<10.0.0'
+  pyarrow_dependency = 'pyarrow>=3.0.0,<10.0.0'
 
 # We must generate protos after setup_requires are installed.
 def generate_protos_first():
@@ -246,7 +246,9 @@ if __name__ == '__main__':
         'grpcio>=1.33.1,!=1.48.0,<2',
         'hdfs>=2.1.0,<3.0.0',
         'httplib2>=0.8,<0.22.0',
-        'numpy>=1.14.3,<1.23.0',
+        # numpy can have breaking changes in minor versions.
+        # Use a strict upper bound.
+        'numpy>=1.14.3,<1.25.0',   # Update build-requirements.txt as well.
         'objsize>=0.6.1,<0.7.0',
         'pymongo>=3.8.0,<4.0.0',
         'proto-plus>=1.7.1,<2',
@@ -343,15 +345,12 @@ if __name__ == '__main__':
             'azure-core >=1.7.0',
             'azure-identity >=1.12.0',
           ],
-        #(TODO): Some tests using Pandas implicitly calls inspect.stack()
-        # with python 3.10 leading to incorrect stacktrace.
-        # This can be removed once dill is updated to version > 0.3.5.1
-        # Issue: https://github.com/apache/beam/issues/23566
+        # Exclude pandas<=1.4.2 since it doesn't work with numpy 1.24.x.
         # Exclude 1.5.0 and 1.5.1 because of
         # https://github.com/pandas-dev/pandas/issues/45725
           'dataframe': [
-            'pandas>=1.0,<1.6,!=1.5.0,!=1.5.1;python_version<"3.10"',
-            'pandas>=1.4.3,<1.6,!=1.5.0,!=1.5.1;python_version>="3.10"'
+            'pandas<1.4.0;python_version=="3.7"',
+            'pandas>=1.4.3,!=1.5.0,!=1.5.1,<1.6;python_version>="3.8"',
           ],
           'dask': [
             'dask >= 2022.6',
