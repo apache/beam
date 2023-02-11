841a7bd983c4ce3140097bad73b828e782b57db1
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-02-11 21:56:38 +0500

    Update build.gradle.kts

--- learning/tour-of-beam/terraform/build.gradle.kts
+++ learning/tour-of-beam/terraform/build.gradle.kts
@@ -27,35 +27,6 @@ terraformPlugin {
     terraformVersion.set("1.0.9")
 }
 
-tasks.register("prepareConfig") {
-    group = "deploy"
-    doLast {
-        var dns_name = ""
-        if (project.hasProperty("dns-name")) {
-            dns_name = project.property("dns-name") as String
-        }
-        val configFileName = "config.g.dart"
-        val modulePath = project(":learning:tour-of-beam:frontend").projectDir.absolutePath
-        var file = File("$modulePath/lib/$configFileName")
-
-        file.writeText(
-                """
-const String kAnalyticsUA = 'UA-73650088-2';
-const String kApiClientURL =
-      'https://router.${dns_name}';
-const String kApiJavaClientURL =
-      'https://java.${dns_name}';
-const String kApiGoClientURL =
-      'https://go.${dns_name}';
-const String kApiPythonClientURL =
-      'https://python.${dns_name}';
-const String kApiScioClientURL =
-      'https://scio.${dns_name}';
-"""
-        )
-    }
-}
-
 tasks {
     /* init Infrastructure for migrate */
     register<TerraformTask>("terraformInit") {
@@ -95,7 +66,34 @@ tasks {
         )
     }
 
-    register<TerraformTask>("terraformApply") {
+    register<TerraformTask>("terraformApplyBackend") {
+        var pg_router_host = ""
+        var environment = "unknown"
+        if (project.extensions.extraProperties.get("pg_router_host") != null) {
+            pg_router_host = project.extensions.extraProperties.get("pg_router_hots") as String
+        }
+        if (project.hasProperty("project_environment")) {
+            environment = project.property("project_environment") as String
+        }
+        args(
+                "apply",
+                "-auto-approve",
+                "-lock=false",
+                "-var=pg_router_host=$pg_router_host",
+                "-target=module.api_enable",
+                "-target=module.setup",
+                "-target=module.functions_buckets",
+                "-target=module.cloud_functions",
+                "-var=environment=$environment",
+                if (file("./environment/$environment/terraform.tfvars").exists()) {
+                    "-var-file=./environment/$environment/terraform.tfvars"
+                } else {
+                    "-no-color"
+                }
+        )
+    }
+
+    register<TerraformTask>("terraformApplyFrontend") {
         var environment = "unknown"
         if (project.hasProperty("project_environment")) {
             environment = project.property("project_environment") as String
@@ -108,6 +106,7 @@ tasks {
                 "-auto-approve",
                 "-lock=false",
                 "-var=environment=$environment",
+                "-target=module.firebase",
                 if (file("./environment/$environment/terraform.tfvars").exists()) {
                     "-var-file=./environment/$environment/terraform.tfvars"
                 } else {
@@ -117,15 +116,42 @@ tasks {
     }
 }
 
+tasks.register("getGKEClusterName") {
+    doLast {
+        val outputFile = File.createTempFile("gke_cluster_name", ".tmp")
+        exec {
+            executable("gcloud")
+            args("container", "clusters", "list", "--format='value(name)'")
+            standardOutput = java.io.FileOutputStream(outputFile)
+        }
+        val gke_cluster_name = outputFile.readText().trim()
+        project.extensions.extraProperties.set("gke_cluster_name", gke_cluster_name)
+    }
+}
+
+tasks.register("getGKEClusterZone") {
+    var gke_cluster_name = ""
+    doLast {
+        val outputFile = File.createTempFile("gke_cluster_zone", ".tmp")
+        exec {
+            executable("gcloud")
+            args("container", "clusters", "describe", "$gke_cluster_name", "--format='value(zone)'")
+            standardOutput = java.io.FileOutputStream(outputFile)
+        }
+        val gke_zone = outputFile.readText().trim()
+        project.extensions.extraProperties.set("gke_zone", gke_zone)
+    }
+}
+
 tasks.register("getCredentials") {
-    var gke_cluster_name = "unknown"
+    var gke_cluster_name = ""
     var gke_zone = "unknown"
     var project_id = "unknown"
-    if (project.hasProperty("gke_cluster_name")) {
-        gke_cluster_name = project.property("gke_cluster_name") as String
+    if (project.extensions.extraProperties.get("gke_cluster_name") != null) {
+        gke_cluster_name = project.extensions.extraProperties.get("gke_cluster_name") as String
     }
-    if (project.hasProperty("gke_zone")) {
-        gke_zone = project.property("gke_cluster_name") as String
+    if (project.extensions.extraProperties.get("gke_zone") != null) {
+        gke_cluster_name = project.extensions.extraProperties.get("gke_zone") as String
     }
     if (project.hasProperty("project_id")) {
         project_id = project.property("project_id") as String
@@ -139,7 +165,7 @@ tasks.register("getCredentials") {
 }
 
 tasks.register("getRouterHost") {
-    var playground_router_host = ""
+    var pg_router_host = ""
     var stdout = ByteArrayOutputStream()
     doLast{
         val result = exec {
@@ -147,7 +173,8 @@ tasks.register("getRouterHost") {
             args("get", "svc", "-l", "app=backend-router-grpc", "-o", "jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[0].port}'")
             standardOutput = stdout
         }
-        playground_router_host = stdout.toString().trim().replace("\"", "")
+        pg_router_host = stdout.toString().trim().replace("\"", "")
+        project.extensions.extraProperties.set("pg_router_host", pg_router_host)
         stdout = ByteArrayOutputStream()
     }
 }
@@ -178,15 +205,15 @@ tasks.register("populateDatastore") {
                 val value = matchResult.groupValues[1]
                 System.setProperty("DATASTORE_PROJECT_ID", value)
                 System.setProperty("GOOGLE_PROJECT_ID", value)
-        System.setProperty("TOB_LEARNING_ROOT", "../learning-content/")
+                System.setProperty("TOB_LEARNING_ROOT", "../learning-content/")
 
-        val process = Runtime.getRuntime().exec(arrayOf("bash", "-c", "go ../backend/cmd/ci_cd/ci_cd.go"))
-        val output = process.inputStream.bufferedReader().use {
-            it.readText().trim()
+                val process = Runtime.getRuntime().exec(arrayOf("bash", "-c", "go ../backend/cmd/ci_cd/ci_cd.go"))
+                val output = process.inputStream.bufferedReader().use {
+                    it.readText().trim()
+                }
+                println("Output of go run cmd/ci_cd/ci_cd.go command: $output")
+            }
         }
-        println("Output of go run cmd/ci_cd/ci_cd.go command: $output")
-    }
-}
 
 // command to run
 // ./gradlew runGoCommand \
@@ -194,22 +221,54 @@ tasks.register("populateDatastore") {
 // -PgoogleProjectId=my-google-project-id \
 // -PtobLearningRoot=path/to/tob-learning-root
 
-tasks.register("readState") {
-    group = "deploy"
-    dependsOn(":learning:tour-of-beam:terraform:terraformInit")
-    dependsOn(":learning:tour-of-beam:terraform:terraformRef")
-}
+        tasks.register("readState") {
+            group = "deploy"
+            dependsOn(":learning:tour-of-beam:terraform:terraformInit")
+            dependsOn(":learning:tour-of-beam:terraform:terraformRef")
+        }
 
-/* initialization infrastructure */
-tasks.register("InitInfrastructure") {
-    group = "deploy"
-    description = "initialization infrastructure"
-    val init = tasks.getByName("terraformInit")
-    val apply = tasks.getByName("terraformApply")
-    val prepare = tasks.getByName("prepareConfig")
-    dependsOn(init)
-    dependsOn(apply)
-    dependsOn(prepare)
-    apply.mustRunAfter(init)
-    prepare.mustRunAfter(apply)
+
+        tasks.register("prepareConfig") {
+            group = "deploy"
+            doLast {
+                var dns_name = ""
+                if (project.hasProperty("dns-name")) {
+                    dns_name = project.property("dns-name") as String
+                }
+                val configFileName = "config.g.dart"
+                val modulePath = project(":learning:tour-of-beam:frontend").projectDir.absolutePath
+                var file = File("$modulePath/lib/$configFileName")
+
+                file.writeText(
+                        """
+const String kAnalyticsUA = 'UA-73650088-2';
+const String kApiClientURL =
+'https://router.${dns_name}';
+const String kApiJavaClientURL =
+'https://java.${dns_name}';
+const String kApiGoClientURL =
+'https://go.${dns_name}';
+const String kApiPythonClientURL =
+'https://python.${dns_name}';
+const String kApiScioClientURL =
+'https://scio.${dns_name}';
+"""
+                )
+            }
+        }
+
+        /* initialization infrastructure */
+        tasks.register("InitInfrastructure") {
+            group = "deploy"
+            description = "initialization infrastructure"
+            val init = tasks.getByName("terraformInit")
+            val apply = tasks.getByName("terraformApply")
+            val prepare = tasks.getByName("prepareConfig")
+            dependsOn(init)
+            dependsOn(apply)
+            dependsOn(prepare)
+            apply.mustRunAfter(init)
+            prepare.mustRunAfter(apply)
+        }
+    }
 }
