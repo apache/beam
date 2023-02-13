4709bbb7d190f60d6aa9eeb58e1cc23a56cd62a5
Danny McCormick <dannymccormick@google.com>
2023-02-13 10:40:55 -0500

    Format

--- sdks/python/apache_beam/utils/timestamp.py
+++ sdks/python/apache_beam/utils/timestamp.py
@@ -104,7 +104,10 @@ class Timestamp(object):
       dt: A ``datetime.datetime`` object in UTC (offset-aware).
     """
     if dt.tzinfo is None:
-      raise ValueError('dt has no timezone info (https://docs.python.org/3/library/datetime.html#datetime.tzinfo:~:text=For%20applications%20requiring,is%20in%20effect.): %s' % dt)
+      raise ValueError(
+          "dt has no timezone info " +
+          "(https://docs.python.org/3/library/datetime.html#datetime.tzinfo" +
+          ":~:text=For%20applications%20requiring,is%20in%20effect.): %s" % dt)
     if dt.tzinfo != pytz.utc and dt.tzinfo != datetime.timezone.utc:
       raise ValueError('dt not in UTC: %s' % dt)
     duration = dt - cls._epoch_datetime_utc()
