a460d5c55ccd7fb0b642441de97220fd0fc0e4f7
Robert Burke <robert@frantil.com>
2023-02-19 11:20:35 -0800

    [prism] comments

--- sdks/go/pkg/beam/runners/prism/internal/handlerunner.go
+++ sdks/go/pkg/beam/runners/prism/internal/handlerunner.go
@@ -81,7 +81,7 @@ func (h *runner) ExecuteWith(t *pipepb.PTransform) string {
 	return t.GetEnvironmentId()
 }
 
-// ExecTransform handles special processing with respect to runner specific transforms
+// ExecuteTransform handles special processing with respect to runner specific transforms
 func (h *runner) ExecuteTransform(tid string, t *pipepb.PTransform, comps *pipepb.Components, watermark mtime.Time, inputData [][]byte) *worker.B {
 	urn := t.GetSpec().GetUrn()
 	var data [][]byte
