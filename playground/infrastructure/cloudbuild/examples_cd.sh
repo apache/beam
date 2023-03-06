b81272b23d527cf3bdebbdbd354271a96c821127
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-03-06 20:22:38 +0500

    Update examples_cd.sh

--- playground/infrastructure/cloudbuild/examples_cd.sh
+++ playground/infrastructure/cloudbuild/examples_cd.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -21,17 +21,17 @@ do
 
    KEY_LENGTH=${#KEY}
    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
-
    export "$KEY"="$VALUE"
 done
 
+export LOG_PATH=${LOG_PATH-"/dev/null"}
 export ORIGIN=${ORIGIN-"PG_EXAMPLES"}
 export STEP=${STEP-"CD"}
 export SUBDIRS=${SUBDIRS-"./learning/katas ./examples ./sdks"}
 export BEAM_ROOT_DIR=${BEAM_ROOT_DIR-"/workspace/beam"}
 export PROJECT_ID=${PROJECT_ID}
 export SDK_CONFIG=${SDK_CONFIG-"$BEAM_ROOT_DIR/playground/sdks.yaml"}
-export BEAM_EXAMPLE_CATEGORIES="$BEAM_ROOT_DIR/playground/categories.yaml"}
+export BEAM_EXAMPLE_CATEGORIES=${BEAM_EXAMPLE_CATEGORIES-"$BEAM_ROOT_DIR/playground/categories.yaml"}
 export BEAM_USE_WEBGRPC=${BEAM_USE_WEBGRPC-"yes"}
 export BEAM_CONCURRENCY=${BEAM_CONCURRENCY-2}
 export SDKS=${SDKS-"java python go"}
@@ -42,22 +42,22 @@ function LogOutput ()
 {
     echo "$(date --utc '+%D %T') $1" >> $LOG_PATH
     # CILOG keyword to simplify search over the global log
-    echo "CILOG $(date --utc '+%D %T') $1"
+    echo "CDLOG $(date --utc '+%D %T') $1"
 }
 
 LogOutput "Input variables:
-            ORIGIN=$ORIGIN
-            STEP=$STEP
-            SUBDIRS=$SUBDIRS
-            BEAM_ROOT_DIR=$BEAM_ROOT_DIR
-            PROJECT_ID=$PROJECT_ID
-            SDK_CONFIG=$SDK_CONFIG
-            BEAM_EXAMPLE_CATEGORIES=$BEAM_EXAMPLE_CATEGORIES
-            BEAM_CONCURRENCY=$BEAM_CONCURRENCY
-            SDKS=$SDKS
-            COMMIT=$COMMIT
-            DNS_NAME=$DNS_NAME
-            BEAM_USE_WEBGRPC=$BEAM_USE_WEBGRPC"
+ORIGIN=$ORIGIN
+STEP=$STEP
+SUBDIRS=$SUBDIRS
+BEAM_ROOT_DIR=$BEAM_ROOT_DIR
+PROJECT_ID=$PROJECT_ID
+SDK_CONFIG=$SDK_CONFIG
+BEAM_EXAMPLE_CATEGORIES=$BEAM_EXAMPLE_CATEGORIES
+BEAM_CONCURRENCY=$BEAM_CONCURRENCY
+SDKS=$SDKS
+COMMIT=$COMMIT
+DNS_NAME=$DNS_NAME
+BEAM_USE_WEBGRPC=$BEAM_USE_WEBGRPC"
 
 # Script starts in a clean environment in Cloud Build. Set minimal required environment variables
 if [ -z "$PATH" ]; then
@@ -73,21 +73,26 @@ export BEAM_EXAMPLE_CATEGORIES="$BEAM_ROOT_DIR/playground/categories.yaml"
 export DNS_NAME=${DNS_NAME}
 
 LogOutput "Installing python and dependencies"
+# Install Python 3 and dependencies
+set -e  # Exit immediately if any command fails
+apt update > /dev/null 2>&1
+apt install -y apt-transport-https ca-certificates software-properties-common curl unzip apt-utils > /dev/null 2>&1
+add-apt-repository -y ppa:deadsnakes/ppa > /dev/null 2>&1
+apt update > /dev/null 2>&1
+apt install -y python3 python3-distutils python3-pip python3-venv > /dev/null 2>&1
+python3 -m pip install --upgrade pip google-api-python-client > /dev/null 2>&1
+ln -s /usr/bin/python3 /usr/bin/python > /dev/null 2>&1
+pip install -r /workspace/beam/playground/infrastructure/requirements.txt > /dev/null 2>&1
 
-# Install Python 3.8 and dependencies
-apt-get update > /dev/null
-apt update > /dev/null
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get install -y apt-transport-https ca-certificates software-properties-common curl unzip apt-utils > /dev/null
-add-apt-repository -y ppa:deadsnakes/ppa > /dev/null && apt update > /dev/null
-apt install -y python3.8 python3.8-distutils python3-pip > /dev/null
-apt install --reinstall python3.8-distutils > /dev/null
-pip install --upgrade google-api-python-client > /dev/null
-python3.8 -m pip install pip --upgrade > /dev/null
-ln -s /usr/bin/python3.8 /usr/bin/python > /dev/null
-apt install python3.8-venv > /dev/null
-pip install -r /workspace/beam/playground/infrastructure/requirements.txt > /dev/null
+# Check if Python and pip commands are available
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed or not in PATH. Aborting."
+    exit 1
+fi
+if ! command -v pip &> /dev/null; then
+    echo "pip is not installed or not in PATH. Aborting."
+    exit 1
+fi
 
 LogOutput "Running Examples deployment to ${DNS_NAME}"
 
