1bd65cd81e4add10c1f9ce4ea0aeb566dca5ae8f
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-03-06 16:34:55 +0500

    Update examples_cd.sh

--- playground/infrastructure/cloudbuild/examples_cd.sh
+++ playground/infrastructure/cloudbuild/examples_cd.sh
@@ -15,23 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env bash
-
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 for ARGUMENT in "$@"
 do
    KEY=$(echo $ARGUMENT | cut -f1 -d=)
@@ -52,8 +35,7 @@ export BEAM_EXAMPLE_CATEGORIES="$BEAM_ROOT_DIR/playground/categories.yaml"
 export BEAM_USE_WEBGRPC=${BEAM_USE_WEBGRPC-"yes"}
 export BEAM_CONCURRENCY=${BEAM_CONCURRENCY-"2"}
 export SDKS=${SDKS-"java python go"}
-export COMMIT=${COMMIT-"HEAD"}
-export DNS_NAME=${DNS_NAME}
+export COMMIT=${COMMIT-"HEAD"}z
 
 function LogOutput ()
 {
