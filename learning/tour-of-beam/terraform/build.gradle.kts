d869dfb2ef6ea2e7906d295d81d66a06d7e0cdcf
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-03-10 15:36:13 +0500

    Update build.gradle.kts

--- learning/tour-of-beam/terraform/build.gradle.kts
+++ learning/tour-of-beam/terraform/build.gradle.kts
@@ -62,13 +62,18 @@ tasks.register<TerraformTask>("terraformRef") {
 tasks.register<TerraformTask>("terraformApplyBackend") {
         group = "backend-deploy"
         var pg_router_host = project.extensions.extraProperties["pg_router_host"] as String
-        var environment = project.property("project_environment") as String
+        var environment = project.property("project_environment") as
+        val gcloudAccount = project.exec {
+            commandLine("gcloud", "config", "get-value", "core/account")
+            standardOutput = ByteArrayOutputStream()
+        }.standardOutput.toString().trim()
         args(
                 "apply",
                 "-auto-approve",
                 "-lock=false",
                 "-parallelism=3",
                 "-var=pg_router_host=$pg_router_host",
+                "-var=gcloud_init_account=$gcloudAccount",
                 "-var=environment=$environment",
                 if (file("./environment/$environment/terraform.tfvars").exists()) {
                     "-var-file=./environment/$environment/terraform.tfvars"
