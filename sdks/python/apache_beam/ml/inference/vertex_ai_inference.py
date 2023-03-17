a17ec737b322b546d521d7efec658ede7d7adaca
jrmccluskey <thejackmccluskey@gmail.com>
2023-03-17 19:03:57 +0000

    Correct package

--- sdks/python/apache_beam/ml/inference/vertex_ai_inference.py
+++ sdks/python/apache_beam/ml/inference/vertex_ai_inference.py
@@ -23,32 +23,32 @@ from typing import Sequence
 
 from apache_beam.ml.inference.base import ModelHandler
 from apache_beam.ml.inference.base import PredictionResult
-from google.cloud import aiplatform_v1
+from google.cloud import aiplatform
 
 
 class VertexAIModelHandlerJSON(ModelHandler[str,
                                             PredictionResult,
-                                            aiplatform_v1.types.Endpoint]):
+                                            aiplatform.types.Endpoint]):
   def __init__(
       self,
       endpoint: str,
       project: Optional[str] = None,
       location: Optional[str] = None,
       experiment: Optional[str] = None):
-    self.endpointName = aiplatform_v1.endpoint_path(project, location, endpoint)
-    self.client = aiplatform_v1.EndpointServiceClient()
+    self.endpointName = aiplatform.endpoint_path(project, location, endpoint)
+    self.client = aiplatform.EndpointServiceClient()
 
     self.endpoint = self._retrieve_endpoint()
 
-  def _retrieve_endpoint(self) -> aiplatform_v1.types.Endpoint:
-    request = aiplatform_v1.GetEndpointRequest(name=self.endpointName)
+  def _retrieve_endpoint(self) -> aiplatform.types.Endpoint:
+    request = aiplatform.GetEndpointRequest(name=self.endpointName)
     response = self.client.get_endpoint(request=request)
     if response is None:
       raise ValueError("Vertex AI endpoint %s not found", self.endpointName)
 
     return response
 
-  def load_model(self) -> aiplatform_v1.types.Endpoint:
+  def load_model(self) -> aiplatform.types.Endpoint:
     # Check to make sure the endpoint is still active since pipeline construction time
     _ = self._retrieve_endpoint()
     return self.endpoint
@@ -56,12 +56,12 @@ class VertexAIModelHandlerJSON(ModelHandler[str,
   def run_inference(
       self,
       batch: Sequence[str],
-      model: aiplatform_v1.types.Endpoint,
+      model: aiplatform.types.Endpoint,
       inference_args: Optional[Dict[str, any]]) -> Iterable[PredictionResult]:
 
     prediction = model.Predict(instances=batch, parameters=inference_args)
 
-    # TODO: implement an "unpack" function to turn the aiplatform_v1.types.Prediction type 
+    # TODO: implement an "unpack" function to turn the aiplatform.types.Prediction type
     # into a PredictionResult. Will involve asking questions about the format, because there's
     # a very real chance the dict is already [JSON str input, prediction output].
-    raise NotImplementedError(type(self))
+    return prediction
