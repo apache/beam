f4b30368a1cec0e23364753171415f0d841c9178
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-03-28 22:24:50 +0500

    Update examples_cd.sh

--- playground/infrastructure/cloudbuild/examples_cd.sh
+++ playground/infrastructure/cloudbuild/examples_cd.sh
@@ -135,11 +135,11 @@ do
     if [ $checker_status -eq 0 ]
     then
         LogOutput "Checker found changed examples for SDK_${sdk^^}"
-        ${sdk}_example_changed=1
+        declare "${sdk}_example_changed=True"
     elif [ $checker_status -eq 11 ]
   then
         LogOutput "Checker did not find any changed examples for SDK_${sdk^^}"
-        "${sdk}_example_changed"=False
+        declare "${sdk}_example_changed=False"
         exit 1
     else
         LogOutput "Error: Checker is broken. Exiting the script."
