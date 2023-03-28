109feb80ee0d1b4b9cd9a4768b87b7ae7c856a8e
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-03-28 18:34:42 +0500

    Update examples_cd.sh

--- playground/infrastructure/cloudbuild/examples_cd.sh
+++ playground/infrastructure/cloudbuild/examples_cd.sh
@@ -111,7 +111,7 @@ LogOutput "All packages and dependencies have been successfully installed. Start
 
 LogOutput "Checking changed files in the PR"
 
-diff=($(git diff --name-only $DIFF_BASE...akvelon/$SOURCE_BRANCH | tr '\n' ' '))
+diff=($(git diff --name-only $DIFF_BASE $SOURCE_BRANCH | tr '\n' ' '))
 
 echo $diff
 for file in "${diff[@]}"; do
