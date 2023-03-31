6d88aa56a296dc0bded7bffa40edd6a5529ba08c
Jasper Van den Bossche <jaspervandenbossche@protonmail.com>
2023-03-31 10:21:44 +0200

    Update documentation windowing pipeline

--- sdks/python/apache_beam/examples/inference/milk_quality_prediction_windowing.py
+++ sdks/python/apache_beam/examples/inference/milk_quality_prediction_windowing.py
@@ -1,10 +1,26 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import argparse
 import logging
 import time
 from typing import NamedTuple
 
 import pandas
-import pandas as pd
 import xgboost
 
 from sklearn.model_selection import train_test_split
@@ -23,10 +39,25 @@ def parse_known_args(argv):
   """Parses args for the workflow."""
   parser = argparse.ArgumentParser()
   parser.add_argument(
-      '--input_data',
-      dest='input_data',
+      '--dataset',
+      dest='dataset',
       required=True,
-      help='Path to the csv containing the dataset.')
+      help='Path to the csv containing Kaggle Milk Quality dataset.')
+  parser.add_argument(
+      '--pipeline_input_data',
+      dest='pipeline_input_data',
+      required=True,
+      help='Path to store the csv containing input data for the pipeline.')
+  parser.add_argument(
+      '--training_set',
+      dest='training_set',
+      required=True,
+      help='Path to store the csv containing the training set')
+  parser.add_argument(
+      '--labels',
+      dest='labels',
+      required=True,
+      help='Path to store the csv containing the labels used in training.')
   parser.add_argument(
       '--model_state',
       dest='model_state',
@@ -35,16 +66,44 @@ def parse_known_args(argv):
   return parser.parse_known_args(argv)
 
 
+def preprocess_data(
+    dataset_path: str,
+    training_set_path: str,
+    labels_path: str,
+    test_set_path: str):
+  """
+    Helper function to split the dataset into a training set
+    and its labels and a test set. The training set and
+    its labels are used to train a lightweight model.
+    The test set is used to create a test streaming pipeline.
+    Args:
+        dataset_path: path to csv file containing the Kaggle
+         milk quality dataset
+        training_set_path: path to output the training samples
+        labels_path:  path to output the labels for the training set
+        test_set_path: path to output the test samples
+    """
+  df = pandas.read_csv(dataset_path)
+  df['Grade'].replace(['low', 'medium', 'high'], [0, 1, 2], inplace=True)
+  x = df.drop(columns=['Grade'])
+  y = df['Grade']
+  x_train, x_test, y_train, _ = \
+      train_test_split(x, y, test_size=0.60, random_state=99)
+  x_train.to_csv(training_set_path, index=False)
+  y_train.to_csv(labels_path, index=False)
+  x_test.to_csv(test_set_path, index=False)
+
+
 def train_model(
-    samples: pandas.DataFrame,
-    labels: pandas.DataFrame,
-    model_state_output_path: str):
+    samples_path: str, labels_path: str, model_state_output_path: str):
   """Function to train the XGBoost model.
     Args:
-      samples: Dataframe contiaing the training data
-      labels: Dataframe containing the labels for the training data
+      samples_path: path to csv file containing the training data
+      labels_path: path to csv file containing the labels for the training data
       model_state_output_path: Path to store the trained model
   """
+  samples = pandas.read_csv(samples_path)
+  labels = pandas.read_csv(labels_path)
   xgb = xgboost.XGBClassifier(max_depth=3)
   xgb.fit(samples, labels)
   xgb.save_model(model_state_output_path)
@@ -58,7 +117,8 @@ class MilkQualityAggregation(NamedTuple):
 
 
 class AggregateMilkQualityResults(beam.CombineFn):
-  """Simple aggregation to keep track of the number of samples with good, bad and medium quality milk."""
+  """Simple aggregation to keep track of the number
+   of samples with good, bad and medium quality milk."""
   def create_accumulator(self):
     return MilkQualityAggregation(0, 0, 0)
 
@@ -110,11 +170,7 @@ def run(
   pipeline_options = PipelineOptions(pipeline_args)
   pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
-  df = pd.read_csv(known_args.input_data)
-  df['Grade'].replace(['low', 'medium', 'high'], [0, 1, 2], inplace=True)
-  x = df.drop(columns=['Grade'])
-  y = df['Grade']
-  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.60, random_state=99)
+  milk_quality_data = pandas.read_csv(known_args.pipeline_input_data)
 
   start = time.mktime(time.strptime('2023/06/29 10:00:00', '%Y/%m/%d %H:%M:%S'))
 
@@ -124,7 +180,10 @@ def run(
   # Watermark is set to 10:00:00
   test_stream.advance_watermark_to(start)
 
-  samples = [x_test.iloc[i:i + 1] for i in range(len(x_test))]
+  # Split the dataframe in individual samples
+  samples = [
+      milk_quality_data.iloc[i:i + 1] for i in range(len(milk_quality_data))
+  ]
 
   for watermark_offset, sample in enumerate(samples, 1):
     test_stream.advance_watermark_to(start + watermark_offset)
@@ -147,4 +206,16 @@ def run(
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
+
+  known_args, _ = parse_known_args(None)
+
+  preprocess_data(
+      known_args.dataset,
+      training_set_path=known_args.training_set,
+      labels_path=known_args.labels,
+      test_set_path=known_args.pipeline_input_data)
+  train_model(
+      samples_path=known_args.training_set,
+      labels_path=known_args.labels,
+      model_state_output_path=known_args.model_state)
   run()
