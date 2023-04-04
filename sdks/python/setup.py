20f4a63b24e2bbe5a7d791dc7c9ed3e088bcc510
Valentyn Tymofieiev <valentyn@google.com>
2023-04-04 15:15:40 -0700

    Include dill as test dependency.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -277,6 +277,8 @@ if __name__ == '__main__':
             'freezegun>=0.3.12',
             'joblib>=1.0.1',
             'mock>=1.0.1,<6.0.0',
+            # Include optional dill dependency on Py3.11+ to run tests using it.
+            'dill>=0.3.1.1,<0.3.7;python_version>="3.11"',
             'pandas<2.0.0',
             'parameterized>=0.7.1,<0.9.0',
             'pyhamcrest>=1.9,!=1.10.0,<2.0.0',
