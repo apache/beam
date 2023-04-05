7b33e916af6aa142a0dfc17993d86571fd03ee51
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-04-05 17:38:09 +0500

    Update variables.tf

--- playground/terraform/infrastructure/cloudbuild-manual-setup/02.builders/variables.tf
+++ playground/terraform/infrastructure/cloudbuild-manual-setup/02.builders/variables.tf
@@ -17,102 +17,88 @@
 
 variable "project_id" {
   description = "The ID of the Google Cloud project within which resources are provisioned"
-  default = "cloudbuildtest-382210"
 }
 
 variable "pg_infra_trigger_name" {
   description = "The name of the trigger that will deploy Playground infrastructure"
-  default     = "playground-infrastructure-trigger"
+  default     = "Playground-infrastructure-trigger"
 }
 
 variable "pg_gke_trigger_name" {
   description = "The name of the trigger that will deploy Playground to GKE"
-  default     = "playground-to-gke-trigger"
+  default     = "Playground-to-gke-trigger"
 }
 
 variable "pg_helm_upd_trigger_name" {
   description = "The name of the trigger that run Helm update"
-  default     = "playground-helm-update-trigger"
+  default     = Playground-helm-update-trigger"
 }
 
 variable "pg_ci_trigger_name" {
   description = "The name of the trigger to run CI checks"
-  default = "playground-ci-trigger"
+  default = "Playground-ci-trigger"
 }
 
 variable "pg_cd_trigger_name" {
   description = "The name of the trigger to run CD checks"
-  default = "playground-cd-trigger"
+  default = "Playground-cd-trigger"
 }
 
 variable "playground_deploy_sa" {
   description = "The ID of the cloud build service account responsible for deploying the Playground"
-  default     = "pg-cloudbuild-deployer-sa"
 }
 
 variable "playground_helm_upd_sa" {
   description = "The ID of the cloud build service account responsible for updating the Helm"
-  default     = "pg-cloudbuild-updater-sa"
 }
 
 variable "playground_cicd_sa" {
   description = "The ID of the cloud build service account responsible for running CI/CD checks and scripts"
-  default = "pg-cloudbuild-cicd-runner-sa"
 }
 
 variable "playground_environment_name" {
-  description = "To define environment name which will have it is own configuration of Playground"
-  default = "test"
+  description = "An environment name which will have it is own configuration of Playground"
 }
 
 variable "playground_dns_name" {
-  description = "The DNS record name for Playground website"
-  default = "playground-dev.xyz"
+  description = "The DNS A-record name for Playground website"
 }
 
 variable "playground_network_name" {
-  description = "The Google Cloud Platform (GCP) VPC Network Name for Playground deployment"
-  default = "playground-vpc"
+  description = "The Google Cloud Platform VPC Name for Playground deployment"
 }
 
 variable "playground_subnetwork_name" {
-  description = "The GCP VPC Subnetwork Name for Playground deployment"
-  default = "playground-vpc-subnet"
+  description = "The Google Cloud Platform VPC Subnetwork Name for Playground deployment"
 }
 
 variable "playground_gke_name" {
-  description = "The Playground GKE Cluster name in Google Cloud Platform (GCP)"
-  default = "playground-gke"
+  description = "The Google Cloud Platform GKE Cluster name for Playground deployment"
 }
 
 variable "state_bucket" {
-  description = "The Google Cloud Platform (GCP) GCS bucket name for Beam Playground temp files and Terraform state"
-  default = "tfstate-pg-ruslan2"
+  description = "The Google Cloud Platform GCS bucket name for Playground Terraform state file"
 }
 
 variable "image_tag" {
-  description = "The docker images tag for Playground containers"
-  default = "release-0.2"
+  description = "The docker images tag for Playground images"
 }
 
 variable "docker_repository_root" {
   description = "The name of Google Cloud Platform (GCP) Artifact Registry Repository where Playground images will be saved to"
-  default = "playground-gar"
 }
 
 variable "playground_region" {
   description = "The Google Cloud Platform (GCP) region (For example: us-central1) where playground infrastructure will be deployed to"
-  default = "us-east1"
 }
 
 variable "playground_zone" {
   description = "The Google Cloud Platform (GCP) zone (For example: us-central1-b) where playground infrastructure will be deployed to"
-  default = "us-east1-b"
 }
 
 variable "sdk_tag" {
   description = <<EOF
-Apache Beam Golang and Python images SDK tag. (For example: 2.43.0)
+Apache Beam Golang and Python images SDK tag. (For example current latest: 2.44.0)
 See more: https://hub.docker.com/r/apache/beam_python3.7_sdk/tags and https://hub.docker.com/r/apache/beam_go_sdk"
   EOF
   default = "2.44.0"
@@ -120,63 +106,56 @@ See more: https://hub.docker.com/r/apache/beam_python3.7_sdk/tags and https://hu
 
 variable "appengine_flag" {
   description = "Boolean. If AppEngine and Datastore need to be installed. Put 'false' if AppEngine and Datastore already installed"
-  default = "true"
 }
 
 variable "gke_machine_type" {
-  description = "Machine type for GKE Nodes"
+  description = "Machine type for GKE Nodes. Default: e2-standard-8"
   default = "e2-standard-8"
 }
 
 variable "ipaddress_name" {
-  description = "Defines Static IP Address name for Playground"
-  default = "playground-static-ip"
+  description = "The GCP Static IP Address name for Playground deployment"
 }
 
 variable "max_count" {
-  description = "Max node count for GKE cluster"
+  description = "Max node count for GKE cluster. Default: 4"
   default = 4
 }
 
 variable "min_count" {
-  description = "Min node count for GKE cluster"
+  description = "Min node count for GKE cluster. Default: 2"
   default = 2
 }
 
 variable "redis_name" {
-  description = "The name of redis instance"
-  default = "playground-redis-db"
+  description = "The Google Cloud Platform redis instance name for Playground"
 }
 
 variable "redis_tier" {
-  description = "The tier of the redis instance (BASIC, STANDARD)"
+  description = "The tier of the GCP redis instance (BASIC, STANDARD)"
   default = "BASIC"
 }
 
 variable "playground_service_account" {
-  description = "Service account name for Playground GKE"
-  default = "playground-gke-sa"
+  description = "GCP service account name for Playground GKE"
 }
 
 variable "datastore_namespace" {
-  description = "The name of Datastore namespace"
-  default = "dev-playground"
+  description = "The name of Playground Datastore namespace"
 }
 
 variable "webhook_trigger_secret_id" {
-  description = "The name of the secret for webhook"
-  default = "secret-webhook-cloudbuild"
+  description = "The name of the secret for webhook config cloud build trigger (CI/CD)"
 }
 
 variable "gh_pat_secret" {
-  description = "The name of secret for GitHub Personal Access Token. Required for cloud build trigger"
-  default = "secret-github-pat"
+  description = "The name of the secret for GitHub Personal Access Token. Required for cloud build trigger (CI/CD)"
 }
 
 variable "data_for_cicd_webhook_secret" {
-  description = "Data for cloud build CI/CD webhooks"
+  description = "Data for cloud build CI/CD secret. Required for secret functionality. Any value can be set"
 }
 
 variable "data_for_github_pat_secret" {
-  description = "The GitHub PAT value"
+  description = "The GitHub generated Personal Access Token value"
 }
