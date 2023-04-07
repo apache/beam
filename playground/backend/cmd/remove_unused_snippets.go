11c78c3e5e03f72e6023ce3ba92ed4863b4d5464
Timur Sultanov <timur.sultanov@akvelon.com>
2023-04-07 13:10:51 +0400

    Improve logging

--- playground/backend/cmd/remove_unused_snippets.go
+++ playground/backend/cmd/remove_unused_snippets.go
@@ -31,7 +31,7 @@ func createDatastoreClient(ctx context.Context, projectId string) (*datastore.Da
 	pcMapper := mapper.NewPrecompiledObjectMapper()
 	db, err := datastore.New(ctx, pcMapper, projectId)
 	if err != nil {
-		fmt.Printf("Couldn't create the database client, err: %s \n", err.Error())
+		logger.Errorf("Couldn't create the database client, err: %s \n", err.Error())
 		return nil, err
 	}
 
@@ -39,6 +39,9 @@ func createDatastoreClient(ctx context.Context, projectId string) (*datastore.Da
 }
 
 func cleanup(dayDiff int, projectId string) error {
+	logger.Infof("Removing unused snippets is running...")
+	startDate := time.Now()
+
 	ctx := context.Background()
 	db, err := createDatastoreClient(ctx, projectId)
 	if err != nil {
@@ -47,14 +50,20 @@ func cleanup(dayDiff int, projectId string) error {
 
 	err = db.DeleteUnusedSnippets(ctx, int32(dayDiff))
 	if err != nil {
-		fmt.Printf("Couldn't delete unused code snippets, err: %s \n", err.Error())
+		logger.Errorf("Couldn't delete unused code snippets, err: %s \n", err.Error())
 		return err
 	}
+
+	diffTime := time.Now().Sub(startDate).Milliseconds()
+	logger.Infof("Removing unused snippets finished, work time: %d ms\n", diffTime)
 
 	return nil
 }
 
 func remove(snippetId string, projectId string) error {
+	logger.Infof("Removing snippet %s is running...", snippetId)
+	startDate := time.Now()
+
 	ctx := context.Background()
 	db, err := createDatastoreClient(ctx, projectId)
 	if err != nil {
@@ -67,6 +76,9 @@ func remove(snippetId string, projectId string) error {
 		return err
 	}
 
+	diffTime := time.Now().Sub(startDate).Milliseconds()
+	logger.Infof("Removing snippet %s finished, work time: %d ms\n", snippetId, diffTime)
+
 	return nil
 }
 
@@ -75,9 +87,6 @@ func main() {
 		fmt.Println("expected 'cleanup' or 'remove' subcommands")
 		os.Exit(1)
 	}
-
-	fmt.Println("Removing unused snippets is running...")
-	startDate := time.Now()
 
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -110,7 +119,7 @@ func main() {
 		logger.SetupLogger(context.Background(), cwd, *projectId)
 		err = cleanup(*dayDiff, *projectId)
 		if err != nil {
-			fmt.Printf("Couldn't cleanup the database, err: %s \n", err.Error())
+			logger.Fatalf("Couldn't cleanup the database, err: %s \n", err.Error())
 			os.Exit(1)
 		}
 	case "remove":
@@ -137,11 +146,8 @@ func main() {
 		logger.SetupLogger(context.Background(), cwd, *projectId)
 		err = remove(*snippetId, *projectId)
 		if err != nil {
-			fmt.Printf("Couldn't remove the code snippet, err: %s \n", err.Error())
+			logger.Fatalf("Couldn't remove the code snippet, err: %s \n", err.Error())
 			os.Exit(1)
 		}
 	}
-
-	diffTime := time.Now().Sub(startDate).Milliseconds()
-	fmt.Printf("Removing unused snippets finished, work time: %d ms\n", diffTime)
 }
