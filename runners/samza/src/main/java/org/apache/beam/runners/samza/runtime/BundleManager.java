38ba41fab4077a13234ad7c6d125e5da7b9d5b0a
Katie Liu <katiliu@linkedin.com>
2023-04-13 14:08:24 -0700

    Spotless

--- runners/samza/src/main/java/org/apache/beam/runners/samza/runtime/BundleManager.java
+++ runners/samza/src/main/java/org/apache/beam/runners/samza/runtime/BundleManager.java
@@ -33,10 +33,10 @@ public interface BundleManager<OutT> {
   /**
    * A listener used to track the lifecycle of a bundle. Typically, the lifecycle of a bundle
    * consists of 1. Start bundle - Invoked when the bundle is started 2. Finish bundle - Invoked
-   * when the bundle is complete. Refer to the docs under {@link BundleManager} for
-   * definition on when a bundle is considered complete. 3. onWatermark - Invoked when watermark is
-   * ready to be propagated to downstream DAG. Refer to the docs under {@link BundleManager}
-   * on when watermark is held vs propagated.
+   * when the bundle is complete. Refer to the docs under {@link BundleManager} for definition on
+   * when a bundle is considered complete. 3. onWatermark - Invoked when watermark is ready to be
+   * propagated to downstream DAG. Refer to the docs under {@link BundleManager} on when watermark
+   * is held vs propagated.
    *
    * @param <OutT>
    */
