dfa3f0aa97d59d741b0794e77be3d88bd378f7a9
Xinyu Liu <xiliu@xiliu-mn1.linkedin.biz>
2023-04-18 17:44:44 -0700

    Update version

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.45.9'
+    project.version = '2.45.10'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
