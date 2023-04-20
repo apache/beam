100a983af5612e27f6ce84dc305403b9fca010ba
Rouslan <115221004+rshamunov@users.noreply.github.com>
2023-04-20 18:45:45 +0400

    test 11 - Storage Object Creator

--- examples/java/src/main/java/org/apache/beam/examples/MinimalWordCount.java
+++ examples/java/src/main/java/org/apache/beam/examples/MinimalWordCount.java
@@ -13,7 +13,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. only
  * See the License for the specific language governing permissions and
- * limitations under the License. test 10 - GCS Bucket Lister only
+ * limitations under the License. test 11 - Storage Object Creator
  */
 package org.apache.beam.examples;
 
