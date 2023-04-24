83a291441f95d32eda47082f7fbebe4c3f1c3fe7
Danny McCormick <dannymccormick@google.com>
2023-04-23 21:11:36 -0400

    Lint

--- sdks/python/apache_beam/runners/dataflow/internal/apiclient_test.py
+++ sdks/python/apache_beam/runners/dataflow/internal/apiclient_test.py
@@ -631,8 +631,8 @@ class UtilTest(unittest.TestCase):
     self.assertEqual(
         env.proto.workerPools[0].workerHarnessContainerImage,
         (
-            names.DATAFLOW_CONTAINER_IMAGE_REPOSITORY + '/beam_python%d.%d_sdk:%s' %
-            (
+            names.DATAFLOW_CONTAINER_IMAGE_REPOSITORY +
+            '/beam_python%d.%d_sdk:%s' % (
                 sys.version_info[0],
                 sys.version_info[1],
                 names.BEAM_FNAPI_CONTAINER_VERSION)))
