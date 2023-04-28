0a13fe12388066a4df33fe549c9c4833c5decbd0
RuiLong J <jrl-china@hotmail.com>
2023-04-28 20:53:46 +0000

    Spotless lint

--- runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorkerTest.java
+++ runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorkerTest.java
@@ -3075,7 +3075,8 @@ public class StreamingDataflowWorkerTest {
       assertThat(
           // The commit will include a timer to clean up state - this timer is irrelevant
           // for the current test. Also remove source_bytes_processed because it's dynamic.
-          setValuesTimestamps(commit.toBuilder().clearOutputTimers().clearSourceBytesProcessed()).build(),
+          setValuesTimestamps(commit.toBuilder().clearOutputTimers().clearSourceBytesProcessed())
+              .build(),
           equalTo(
               setMessagesMetadata(
                       PaneInfo.NO_FIRING,
