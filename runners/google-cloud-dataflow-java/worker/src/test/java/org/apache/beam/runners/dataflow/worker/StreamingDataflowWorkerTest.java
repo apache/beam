38f2b5a99c990cb9c35a8f2d54d7c8a23a6165fa
RuiLong J <jrl-china@hotmail.com>
2023-04-28 19:16:07 +0000

    Spotless lint

--- runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorkerTest.java
+++ runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorkerTest.java
@@ -609,7 +609,8 @@ public class StreamingDataflowWorkerTest {
     return parseCommitRequest(expectedCommitRequestBuilder.toString());
   }
 
-  private StringBuilder initializeExpectedCommitRequest(String key, long shardingKey, int index, bool hasSourceBytesProcessed) {
+  private StringBuilder initializeExpectedCommitRequest(
+      String key, long shardingKey, int index, bool hasSourceBytesProcessed) {
     StringBuilder requestBuilder = new StringBuilder();
 
     requestBuilder.append("key: \"");
@@ -623,8 +624,8 @@ public class StreamingDataflowWorkerTest {
     requestBuilder.append(" ");
     requestBuilder.append("cache_token: 3 ");
     if (hasSourceBytesProcessed) requestBuilder.append("source_bytes_processed: 0 ");
-    
-    return requestBuilder; 
+
+    return requestBuilder;
   }
 
   private StringBuilder initializeExpectedCommitRequest(String key, long shardingKey, int index) {
