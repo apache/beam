57fced51b3b7471a5a59d294d3495169cf10adbe
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2023-05-03 09:47:26 -0400

    draft

--- sdks/python/apache_beam/ml/tft/preprocess.py
+++ sdks/python/apache_beam/ml/tft/preprocess.py
@@ -19,11 +19,14 @@
 
 import tempfile
 import typing
+import logging
 from collections import defaultdict
 from typing import Any
 from typing import Dict
 from typing import List
 from typing import Union
+from typing import Callable
+from typing import Optional
 
 import apache_beam as beam
 import numpy as np
@@ -64,41 +67,37 @@ numpy_to_tensor_type = {
     np.dtype(np.float64): tf.float32
 }
 
-fixed_len_feature_dtypes = (int, str, float, list, bytes, np.ndarray)
-var_len_feature_dtypes = (tf.Tensor)
+fixed_len_feature_dtypes = (int, str, float, bytes)
+var_len_feature_dtypes = (tf.Tensor, list, np.ndarray)
 
 
 class ProcessHandler:
   def process_data(self):
     raise NotImplementedError
 
-  def convert_data(self):
-    """No op here."""
-    def _PTransformFn(pcoll_or_pipeline):
-      return pcoll_or_pipeline
-
-    return beam.ptransform_fn(_PTransformFn)()
-
 
 class MLTransform(ptransform.PTransform):
   """
   1. Implement dead letter queue?
   """
-  def __init__(self, process_handler):
-    self.process_handler = process_handler
+  def __init__(self, process_step=Union[List[Callable], Callable]):
+    self.process_step = process_step
+    # We will use proceess handler to handle the data conversion and etc.
+    self._process_handler = self._get_process_handler()
 
-  def expand(self, pvalue: typing.Dict):
+  def _get_process_handler(self):
+    if callable(self.process_step):
+      self.process_step = [self.process_step]
+    elif self.process_step and isinstance(self.process_step[0], _TFTOperation):
+      raise NotImplementedError
+
+    return TFTProcessHandler(process_fn_config=self.process_step)
+
+  def expand(self, pvalue):
     return (
         pvalue
-        | "BatchElements" >> beam.BatchElements()
-        | "DataConversion" >> beam.ParDo(_DictToRecordBatches())
-        # | "ProcessParDo" >> beam.ParDo(_MLTransformDoFn(self.process_handler))
-    )
-
-
-class _DictToRecordBatches(beam.DoFn):
-  def process(self, element):
-    yield pa.RecordBatch.from_pylist(element)
+        | "ProcessData" >> beam.ParDo(
+            _MLTransformDoFn(process_handler=self._process_handler)))
 
 
 class _MLTransformDoFn(beam.DoFn):
@@ -106,7 +105,7 @@ class _MLTransformDoFn(beam.DoFn):
     self.process_handler = process_handler
 
   def process(self, element):
-    self.process_handler.process_data(element)
+    yield self.process_handler.process_data(element)
 
 
 class TFTProcessHandler(ProcessHandler):
@@ -122,17 +121,11 @@ class TFTProcessHandler(ProcessHandler):
     self.process_fn_config = process_fn_config
     self.output_record_batches = output_record_batches
 
-  def convert_data(self, element):
-    def _PTransformFn():
-      import pyarrow as pa
-      return (pcoll_or_pipeline | beam.Map(pa.RecordBatch.from_pydict))
-
-    return beam.ptransform_fn(_PTransformFn)()
-
   def _get_raw_data_metadata(self, data: Dict[str, Any]):
     """
         Assume we only support tf.string, tf.float32
         """
+    data = data[0]
     raw_data_feature_spec = {}
     for name in data.keys():
 
@@ -146,8 +139,7 @@ class TFTProcessHandler(ProcessHandler):
         raw_data_feature_spec[name] = tf.io.FixedLenFeature(shape, dtype)
       elif isinstance(data[name], var_len_feature_dtypes):
         raw_data_feature_spec[name] = tf.io.VarLenFeature(dtype)
-    return tft.DatasetMetadata(
-        schema_utils.schema_from_feature_spec(raw_data_feature_spec))
+    return tft.DatasetMetadata.from_feature_spec(raw_data_feature_spec)
 
   def _deduce_dtype(self, element):
     # improve the logic here
@@ -156,6 +148,9 @@ class TFTProcessHandler(ProcessHandler):
       dtype = element.dtype
     else:
       dtype = type(element)
+
+      if dtype == list:
+        dtype = type(element[0])
 
       if dtype in primitive_type_to_tensor_type:
         dtype = primitive_type_to_tensor_type[dtype]
@@ -175,72 +170,87 @@ class TFTProcessHandler(ProcessHandler):
           column_ops[column].append(process_fn)
     return column_ops
 
-  def preprocess_fn(self, raw_data):
-    """
-        What should be the type of data in `raw_data`?
-    """
-    # since we will be changing the raw_data, copy it and then change it.
-    output = raw_data.copy()
-    column_ops = self._get_all_transforms_per_column()
-    for column, ops in column_ops.items():
-      o = output[column]
-      for op in ops:
-        o = op.apply(o)
-      output[column] = o
+  # def preprocess_fn(self, raw_data):
+  #   """
+  #       What should be the type of data in `raw_data`?
+  #   """
+  #   # since we will be changing the raw_data, copy it and then change it.
+  #   output = raw_data.copy()
+  #   column_ops = self._get_all_transforms_per_column()
+  #   for column, ops in column_ops.items():
+  #     o = output[column]
+  #     for op in ops:
+  #       o = op.apply(o)
+  #     output[column] = o
+  #   return output
+  def preprocess_fn(self, output):
+    # assuming process_config in the list are to do processed in sequential order
+
+    # output = data.copy()
+    for i in range(len(self.process_fn_config)):
+      op = self.process_fn_config[i]
+      # apply the op on specified column
+      columns = op.columns
+      for col in columns:
+        print("**************")
+        print(op.name)
+        # output[col+ '_' + op.name] = op.apply(output[col])
+        output[col] = op.apply(output[col])
+    # output = data.copy()
+    # output['x'] = self.process_fn_config[0].apply(output['x'])
+    # output['x'] = self.process_fn_config[1].apply(output['x'])
+
     return output
 
-  def process_data(self, raw_data: List[Dict[str, Any]]):
+  def process_data(self, raw_data: Dict[str, Any]):
     """
     This method gets called in the _MLTransformDoFn.
     Every handler should implement this method.
     """
     if not raw_data:
       raise RuntimeError("Provide atlease one data point in the raw data.")
-
-    raw_data_metadata = self._get_raw_data_metadata(raw_data[0])
+    print("raw_data: {}".format(raw_data))
+    raw_data_metadata = self._get_raw_data_metadata(raw_data)
     data = (raw_data, raw_data_metadata)
     with tft_beam.Context(tempfile.mkdtemp()):
-      transformed_dataset, transform_fn = (
+      transformed_dataset, _ = (
           data
           | tft_beam.AnalyzeAndTransformDataset(
         self.preprocess_fn,
-        output_record_batches=self.output_record_batches
-      )
-      )
+        output_record_batches=self.output_record_batches))
     return transformed_dataset
-
-
-def preprocess_fn(inputs):
-  return {
-      'y': tft.apply_buckets(
-          inputs['y'], bucket_boundaries=tf.constant([[2.0, 5.0, 10.0]]))
-  }
 
 
 # Remove the main.
 if __name__ == '__main__':
-  raw_data = [{
-      'x': 1,
-      'y': np.array([[1, 10, 15], [1, 3, 111]], dtype=np.int32),
-      's': 'hello'
-  }, {
-      'x': 2, 'y': 2, 's': 'world'
-  }, {
-      'x': 3, 'y': 3, 's': 'hello'
-  }]
+  # logging.basicConfig(level=logging.WARNING)
+  # raw_data = [{
+  #     'x': 1,
+  #     'y': 2,
+  #     's': 'hello'
+  # },
+  # # {
+  #     # 'x': 2, 'y': 2, 's': 'world'
+  # # },
+  #   # {
+  #     # 'x': 3, 'y': 3, 's': 'hello'
+  # # }
+  # ]
+
+  raw_data = [
+      dict(x=["I", "like", "pie", "pie", "pie"]), dict(x=["yum", "yum", "pie"])
+  ]
 
   # output_record_batches = False
   # tft_handler = TFTProcessHandler(
   #     process_fn_config=[
-  #         # compute_and_apply_vocabulary(columns=['s']),
-  #         apply_buckets(
-  #             columns=['y'], bucket_boundaries=tf.constant([[2.0, 5.0, 10.0]])),
-  #         # scale_to_z_score(columns=['y']),
-  #         # tf.reduce_mean,
+  #         # tfidf(columns=['x']),
+  #         compute_and_apply_vocabulary(columns=['x']),
+  #         get_num_buckets_after_transformation(columns=['x'])
   #     ],
   #     output_record_batches=output_record_batches)
   # transformed_dataset = tft_handler.process_data(raw_data)
-  # print(transformed_dataset[0])
+  # print(transformed_dataset)
 
   # raw_data_feature_spec = {'y' : tf.io.FixedLenFeature((2,3), tf.int64)}
   # raw_data_metadata = tft.DatasetMetadata(
@@ -254,16 +264,42 @@ if __name__ == '__main__':
   #     ))
 
   # print(transformed_dataset[0])
-  tft_process_handler = TFTProcessHandler(process_fn_config=[])
 
-  def tmp_fn(element):
-    return element
+  # with beam.Pipeline() as pipeline:
+  #   (
+  #       pipeline
+  #       | "Create" >> beam.Create(raw_data)
+  #       | "ComputeAndApplyVocab" >> MLTransform(compute_and_apply_vocabulary(columns=['x',]))
+  #       # | "GetNumBucketsForTransformedFeatures" >> MLTransform(get_num_buckets_after_transformation(columns=['x']))
+  #       | "print" >> beam.Map(print)
+  #       )
 
-  with beam.Pipeline() as pipeline:
+
+  def convert_(d=Dict):
+    out = defaultdict(list)
+    for key, value in d.items():
+      out[key].extend(value)
+    return out
+
+  #   return out
+  import pyarrow as pa
+
+  def convert_to_record_batch(list_dict):
+    return pa.RecordBatch.from_pydict(list_dict)
+
+  with beam.Pipeline() as p:
     (
-        pipeline
-        | "Create" >> beam.Create(raw_data)
-        | "ConvertToRow" >> beam.Map(lambda x: beam.Row(**x))
-        # | "ConvertToTFX" >> MLTransform(tft_process_handler)
-        | beam.Map(tmp_fn)
-        | "print" >> beam.Map(print))
+        p | beam.Create(raw_data)
+        # | beam.Map(lambda x: beam.Row(**x))
+        # | beam.BatchElements(min_batch_size=2, max_batch_size=2)
+        # | beam.GroupByKey()
+        | beam.Map(convert_)
+        | beam.Map(convert_to_record_batch)
+        | beam.Map(lambda x: x.to_pandas())
+        | beam.Map(print))
+"""
+We need ease of use transform.
+
+"""
+
+# data | MLTransform(compute_and_apply_vocabulary(columns=['x'])) | MLTransform(apply_buckets(columns=['x']))
--- sdks/python/apache_beam/ml/tft/wrapper.py
+++ sdks/python/apache_beam/ml/tft/wrapper.py
@@ -19,7 +19,7 @@
 This file contains the methods that wrapped around Tensorflow transform
 processing ops such as compute_and_apply_vocabulary, bucketize etc.
 """
-
+# pylint: skip-file
 import typing
 
 import tensorflow_transform as tft
@@ -32,12 +32,26 @@ class _TFTOperation:
     Assume the the data in apply method is not indexed/keyed.
     """
   def __init__(
-      self, columns: typing.Optional[typing.List[str]] = None, **kwargs):
+      self,
+      columns: typing.Optional[typing.List[str]] = None,
+      include_output: bool = False,
+      out_name: str = '',
+      **kwargs):
     self.columns = columns
     self.kwargs = kwargs
+    self.include_output = include_output
+    self.out_name = out_name
+
+  def _validate_args(self):
+    if (self.include_output and not self.out_name) or (self.out_name and
+                                                       not self.include_output):
+      raise NotImplemented
 
   def apply(self, data):
-    pass
+    raise NotImplementedError
+
+  def __call__(self, data):
+    self.apply(data)
 
 
 class _Bucketize(_TFTOperation):
@@ -50,14 +64,36 @@ class _ApplyBuckets(_TFTOperation):
     return tft.apply_buckets(data, **self.kwargs)
 
 
+import tensorflow as tf
+
+
 class _ComputeAndApplyVocabulary(_TFTOperation):
+  name = 'compute_and_apply'
+
   def apply(self, data):
     return tft.compute_and_apply_vocabulary(data)
 
 
+    # return tf.convert_to_tensor([1], dtype=tf.int64)
 class _ScaleToZscore(_TFTOperation):
   def apply(self, data):
     return tft.scale_to_z_score(data, self.kwargs)
+
+
+class _GetNumBucketsForTransformedFeatures(_TFTOperation):
+  name = 'get_num_buckets'
+
+  def apply(self, data):
+    return tft.get_num_buckets_for_transformed_feature(data)
+
+
+class _CustomWrapper(_TFTOperation):
+  def __init__(self, custom_fn, columns, **kwargs):
+    super().__init__(columns=columns, kwargs=kwargs)
+    self.custom_fn = custom_fn
+
+  def apply(self, input_tesor):
+    return self.custom_fn(input_tesor, self.kwargs)
 
 
 # Wrappers visible to the public APIs.
@@ -77,3 +113,21 @@ def compute_and_apply_vocabulary(columns, **kwargs):
 
 def scale_to_z_score(columns, **kwargs):
   return _ScaleToZscore(columns=columns, **kwargs)
+
+
+def get_num_buckets_after_transformation(columns, **kwargs):
+  return _GetNumBucketsForTransformedFeatures(columns=columns)
+
+
+class _TFIDF(_TFTOperation):
+  name = 'TFIDF'
+
+  def apply(self, data, **kwargs):
+    integerized = tft.compute_and_apply_vocabulary(data)
+    vocab_size = tft.get_num_buckets_for_transformed_feature(integerized)
+    vocab_index, tfidf_weight = tft.tfidf(integerized, vocab_size)
+    return vocab_index
+
+
+def tfidf(columns, **kwargs):
+  return _TFIDF(columns, **kwargs)
