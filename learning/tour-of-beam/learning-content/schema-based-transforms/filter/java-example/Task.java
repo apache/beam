d6f0bc21df6c47b56e41c2a558bc4dec9c107138
Oleh Borysevych <oleg.borisevich@akvelon.com>
2023-05-08 20:19:28 +0300

    fixing merge error

--- learning/tour-of-beam/learning-content/schema-based-transforms/filter/java-example/Task.java
+++ learning/tour-of-beam/learning-content/schema-based-transforms/filter/java-example/Task.java
@@ -1,4 +1,4 @@
-package com.example.demo.x;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
