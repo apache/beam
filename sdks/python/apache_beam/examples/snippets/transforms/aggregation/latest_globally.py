34868747517bb496ba8f2fd44d3afc180f1295c4
Timur Sultanov <timur.sultanov@akvelon.com>
2023-05-10 15:36:24 +0400

    Adapt latest.py to work with Playground

--- sdks/python/apache_beam/examples/snippets/transforms/aggregation/latest_globally.py
+++ sdks/python/apache_beam/examples/snippets/transforms/aggregation/latest_globally.py
@@ -18,6 +18,19 @@
 
 # pytype: skip-file
 
+# beam-playground:
+#   name: LatestGlobally
+#   description: Demonstration of Latest transform usage.
+#   multifile: false
+#   default_example: false
+#   context_line: 49
+#   categories:
+#     - Core Transforms
+#   complexity: BASIC
+#   tags:
+#     - transforms
+#     - timestamps
+#     - latest
 
 def latest_globally(test=None):
   # [START latest_globally]
@@ -57,57 +70,5 @@ def latest_globally(test=None):
       test(latest_element)
 
 
-def latest_per_key(test=None):
-  # [START latest_per_key]
-  import apache_beam as beam
-  import time
-
-  def to_unix_time(time_str, format='%Y-%m-%d %H:%M:%S'):
-    return time.mktime(time.strptime(time_str, format))
-
-  with beam.Pipeline() as pipeline:
-    latest_elements_per_key = (
-        pipeline
-        | 'Create crops' >> beam.Create([
-            ('spring', {
-                'item': '🥕', 'harvest': '2020-06-28 00:00:00'
-            }),
-            ('spring', {
-                'item': '🍓', 'harvest': '2020-06-16 00:00:00'
-            }),
-            ('summer', {
-                'item': '🥕', 'harvest': '2020-07-17 00:00:00'
-            }),
-            ('summer', {
-                'item': '🍓', 'harvest': '2020-08-26 00:00:00'
-            }),
-            ('summer', {
-                'item': '🍆', 'harvest': '2020-09-04 00:00:00'
-            }),
-            ('summer', {
-                'item': '🥬', 'harvest': '2020-09-18 00:00:00'
-            }),
-            ('summer', {
-                'item': '🍅', 'harvest': '2020-09-22 00:00:00'
-            }),
-            ('autumn', {
-                'item': '🍅', 'harvest': '2020-10-01 00:00:00'
-            }),
-            ('autumn', {
-                'item': '🥬', 'harvest': '2020-10-20 00:00:00'
-            }),
-            ('autumn', {
-                'item': '🍆', 'harvest': '2020-10-26 00:00:00'
-            }),
-            ('winter', {
-                'item': '🥬', 'harvest': '2020-02-24 00:00:00'
-            }),
-        ])
-        | 'With timestamps' >> beam.Map(
-            lambda pair: beam.window.TimestampedValue(
-                (pair[0], pair[1]['item']), to_unix_time(pair[1]['harvest'])))
-        | 'Get latest elements per key' >> beam.combiners.Latest.PerKey()
-        | beam.Map(print))
-    # [END latest_per_key]
-    if test:
-      test(latest_elements_per_key)
+if __name__ == '__main__':
+  latest_globally()
