5dbb5e73105e58ee59952282138f2cc83bea24eb
mende1esmende1es <mende1esmende1es@gmail.cp>
2023-05-11 15:44:17 +0600

    remove slash

--- learning/tour-of-beam/learning-content/windowing/adding-timestamp/go-example/main.go
+++ learning/tour-of-beam/learning-content/windowing/adding-timestamp/go-example/main.go
@@ -8,24 +8,25 @@
  * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-/*
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 // beam-playground:
-//   name: adding-timestamp
-//	 description: Adding timestamp example.
-//	 multifile: false
-//	 context_line: 82
-//	 categories:
-//	   - Quickstart
-//	 complexity: ADVANCED
-//	 tags:
-//	   - hellobeam
+//
+//	  name: adding-timestamp
+//		 description: Adding timestamp example.
+//		 multifile: false
+//		 context_line: 82
+//		 categories:
+//		   - Quickstart
+//		 complexity: ADVANCED
+//		 tags:
+//		   - hellobeam
 package main
 
 import (
