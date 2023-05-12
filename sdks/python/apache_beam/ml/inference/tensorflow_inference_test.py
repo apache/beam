50fc029ec058378783ec23499beb507983d7e4c1
Kerry Donny-Clark <kerrydc@google.com>
2023-05-12 16:06:19 -0400

    Fix lint errors with imports

--- sdks/python/apache_beam/ml/inference/tensorflow_inference_test.py
+++ sdks/python/apache_beam/ml/inference/tensorflow_inference_test.py
@@ -37,7 +37,8 @@ from apache_beam.ml.inference.base import KeyedModelHandler
 from apache_beam.ml.inference.base import PredictionResult
 from apache_beam.ml.inference.base import RunInference
 from apache_beam.testing.test_pipeline import TestPipeline
-from apache_beam.testing.util import assert_that, equal_to
+from apache_beam.testing.util import assert_that
+from apache_beam.testing.util import equal_to
 
 # pylint: disable=ungrouped-imports
 try:
