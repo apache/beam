d316ff7606abda1906a288e01398ba5f3c9a9c74
riteshghorse <actions@GitHub Actions 20.local>
2023-05-18 13:45:17 +0000

    Moving to 2.49.0-SNAPSHOT on master branch.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -430,7 +430,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.48.0'
+    project.version = '2.49.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
