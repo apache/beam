5b92d8869157b46879987ec5cc8af9fe9ad9bd45
Oleh Borysevych <oleg.borisevich@akvelon.com>
2023-05-22 17:00:42 +0300

    line anding formatting

--- learning/tour-of-beam/cloudbuild/scripts/tob_deploy_infra_backend.sh
+++ learning/tour-of-beam/cloudbuild/scripts/tob_deploy_infra_backend.sh
@@ -18,23 +18,31 @@
 # under the License.
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -qq  update  
+
+apt-get -qq  update
+
 apt-get -qq  install -y wget unzip software-properties-common git curl apt-transport-https ca-certificates gnupg jq lsb-release
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | tee /usr/share/keyrings/cloud.google.gpg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |tee /etc/apt/sources.list.d/docker.list 
 
-wget -nv https://releases.hashicorp.com/terraform/1.4.2/terraform_1.4.2_linux_amd64.zip 
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | tee /usr/share/keyrings/cloud.google.gpg
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |tee /etc/apt/sources.list.d/docker.list
+
+
+wget -nv https://releases.hashicorp.com/terraform/1.4.2/terraform_1.4.2_linux_amd64.zip
+
 unzip terraform_1.4.2_linux_amd64.zip
+
 mv terraform /usr/local/bin/terraform
 
-curl -fsSLo get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod +x get_helm.sh && ./get_helm.sh
-
 apt-get -qq update
+
 apt-get -qq install -y google-cloud-sdk-gke-gcloud-auth-plugin google-cloud-sdk openjdk-11-jdk kubectl docker-ce golang
+
 
 git clone --branch $BRANCH_NAME $REPO_NAME --single-branch
 
@@ -47,6 +55,7 @@ cd beam/learning/tour-of-beam/terraform
 gcloud datastore indexes create ../backend/internal/storage/index.yaml
 
 echo "---- ENV OUTPUT---"
+
 env | grep TF_VAR
 
 terraform init -backend-config='bucket=${STATE_BUCKET}'
@@ -54,6 +63,7 @@ terraform init -backend-config='bucket=${STATE_BUCKET}'
 terraform apply -auto-approve -var "pg_router_host=$(kubectl get svc -l app=backend-router-grpc -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}:{.items[0].spec.ports[0].port}')"
 
 echo "---- UPLOAD LEARNING METERIALS ---"
+
 cd ../backend
 
 go run ./cmd/ci_cd/ci_cd.go
