bd63768e3efe8c844418c3cfde4392416d8ac8da
Oleh Borysevych <oleg.borisevich@akvelon.com>
2023-05-31 14:00:09 +0300

    remove html renderer

--- learning/tour-of-beam/terraform/build.gradle.kts
+++ learning/tour-of-beam/terraform/build.gradle.kts
@@ -324,7 +324,7 @@ tasks.register("flutterBuildWeb") {
     doLast {
         exec {
             executable("flutter")
-            args("build", "web", "-v", "--web-renderer=html", "--release")
+            args("build", "web", "-v", "--release")
             workingDir("../frontend")
         }
     }
