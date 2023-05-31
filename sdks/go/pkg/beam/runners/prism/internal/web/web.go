4c925d8053fb3b1bf18666ea76eeeb99a5ca89fc
lostluck <13907733+lostluck@users.noreply.github.com>
2023-05-31 14:38:17 -0700

    Update styles and tidy up handlers.

--- sdks/go/pkg/beam/runners/prism/internal/web/web.go
+++ sdks/go/pkg/beam/runners/prism/internal/web/web.go
@@ -27,6 +27,7 @@ import (
 	"net/http"
 	"sort"
 	"strings"
+	"sync"
 
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/metrics"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/runtime/metricsx"
@@ -34,6 +35,7 @@ import (
 	pipepb "github.com/apache/beam/sdks/v2/go/pkg/beam/model/pipeline_v1"
 	"golang.org/x/exp/slog"
 	"golang.org/x/sync/errgroup"
+	"google.golang.org/protobuf/proto"
 )
 
 //go:embed index.html
@@ -45,132 +47,189 @@ var jobTemplate string
 //go:embed assets/*
 var assets embed.FS
 
+var (
+	indexPage = template.Must(template.New("index").Parse(indexTemplate))
+	jobPage   = template.Must(template.New("job").Parse(jobTemplate))
+)
+
+type errorSetter interface {
+	SetError(err error)
+}
+
+func renderPage(page *template.Template, data errorSetter, w http.ResponseWriter) {
+	var buf bytes.Buffer
+	if err := page.Execute(&buf, data); err != nil {
+		data.SetError(err)
+	}
+	w.Write(buf.Bytes())
+}
+
 type Tforms struct {
 	ID        string
 	Transform *pipepb.PTransform
 	Metrics   []string
 }
 
+type jobDetailsData struct {
+	JobID, JobName string
+	Error          string
+	Transforms     []Tforms
+	PCols          map[metrics.StepKey]metrics.PColResult
+	DisplayData    []*pipepb.LabelledPayload
+}
+
+func (jd *jobDetailsData) SetError(err error) {
+	jd.Error = err.Error()
+}
+
+type jobDetailsHandler struct {
+	Jobcli     jobpb.JobServiceClient
+	jobDetails sync.Map
+}
+
+func (h *jobDetailsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+
+	path := r.URL.EscapedPath()
+	i := strings.LastIndex(path, "/")
+	jobID := path[i+1:]
+	data := jobDetailsData{
+		JobID: jobID,
+	}
+
+	errg, ctx := errgroup.WithContext(r.Context())
+	errg.Go(func() error {
+		v, ok := h.jobDetails.Load(jobID)
+		if ok {
+			data.JobName = v.(string)
+			return nil
+		}
+		resp, err := h.Jobcli.GetJobs(ctx, &jobpb.GetJobsRequest{})
+		if err != nil {
+			return err
+		}
+		for _, j := range resp.GetJobInfo() {
+			if j.GetJobId() == jobID {
+				data.JobName = j.GetJobName()
+			}
+			h.jobDetails.Store(j.GetJobId(), j.GetJobName())
+		}
+		return nil
+	})
+
+	var pipeResp *jobpb.GetJobPipelineResponse
+	var metsResp *jobpb.GetJobMetricsResponse
+	errg.Go(func() error {
+		resp, err := h.Jobcli.GetPipeline(ctx, &jobpb.GetJobPipelineRequest{JobId: jobID})
+		pipeResp = resp
+		return err
+	})
+	errg.Go(func() error {
+		resp, err := h.Jobcli.GetJobMetrics(ctx, &jobpb.GetJobMetricsRequest{JobId: jobID})
+		metsResp = resp
+		return err
+	})
+
+	if err := errg.Wait(); err != nil {
+		data.Error = err.Error()
+		renderPage(jobPage, &data, w)
+		return
+	}
+
+	for i, dd := range pipeResp.GetPipeline().GetDisplayData() {
+		if dd.GetUrn() != "beam:display_data:labelled:v1" {
+			// There's only one type of display data, but lets take care.
+			continue
+		}
+
+		lbl := &pipepb.LabelledPayload{}
+		if err := proto.Unmarshal(dd.GetPayload(), lbl); err != nil {
+			slog.Debug("unable to decode DisplayData payload", slog.Any("error", err), slog.Any("index", i))
+			continue
+		}
+		data.DisplayData = append(data.DisplayData, lbl)
+	}
+
+	mets := metsResp.GetMetrics()
+	results := metricsx.FromMonitoringInfos(pipeResp.GetPipeline(), mets.GetAttempted(), mets.GetCommitted())
+
+	pcols := map[metrics.StepKey]metrics.PColResult{}
+	for _, res := range results.AllMetrics().PCols() {
+		pcols[res.Key] = res
+	}
+	data.PCols = pcols
+	trs := pipeResp.GetPipeline().GetComponents().GetTransforms()
+	data.Transforms = make([]Tforms, 0, len(trs))
+	for id, pt := range pipeResp.GetPipeline().GetComponents().GetTransforms() {
+		if len(pt.GetSubtransforms()) > 0 {
+			continue
+		}
+		var strMets []string
+		for local := range pt.GetInputs() {
+			name := pt.GetUniqueName() + "." + local
+			r, ok := pcols[metrics.StepKey{Step: name}]
+			if ok {
+				strMets = append(strMets, fmt.Sprintf("Input %v ElementCount: %v", name, r.Committed.ElementCount))
+			}
+		}
+
+		data.Transforms = append(data.Transforms, Tforms{
+			ID:        id,
+			Transform: pt,
+			Metrics:   strMets,
+		})
+	}
+	sort.Slice(data.Transforms, func(i, j int) bool {
+		a, b := data.Transforms[i], data.Transforms[j]
+		return a.Transform.GetUniqueName() < b.Transform.GetUniqueName()
+	})
+
+	renderPage(jobPage, &data, w)
+}
+
+type jobsConsoleHandler struct {
+	Jobcli jobpb.JobServiceClient
+}
+
+type jobsConsoleData struct {
+	Error string
+	Jobs  []*jobpb.JobInfo
+}
+
+func (jd *jobsConsoleData) SetError(err error) {
+	jd.Error = err.Error()
+}
+
+func (h *jobsConsoleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	data := &jobsConsoleData{}
+	resp, err := h.Jobcli.GetJobs(r.Context(), &jobpb.GetJobsRequest{})
+	if err != nil {
+		data.SetError(err)
+		renderPage(indexPage, data, w)
+		return
+	}
+	data.Jobs = resp.GetJobInfo()
+	sort.Slice(data.Jobs, func(i, j int) bool {
+		a, b := data.Jobs[i], data.Jobs[j]
+		return a.JobId < b.JobId
+	})
+
+	renderPage(indexPage, data, w)
+}
+
 // Initialize the web client to talk to the given Job Management Client.
 func Initialize(ctx context.Context, port int, jobcli jobpb.JobServiceClient) {
-	indexPage := template.Must(template.New("index").Parse(indexTemplate))
-	jobPage := template.Must(template.New("job").Parse(jobTemplate))
 	assetsFs := http.FileServer(http.FS(assets))
-
 	mux := http.NewServeMux()
 
 	mux.Handle("/assets/", assetsFs)
-	mux.HandleFunc("/job/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.EscapedPath()
-		i := strings.LastIndex(path, "/")
-		jobID := path[i+1:]
-		w.WriteHeader(200)
-		data := struct {
-			Title          string
-			WelcomeMessage string
-			Transforms     []Tforms
-			PCols          map[metrics.StepKey]metrics.PColResult
-		}{
-			Title:          "Hello from 	Go",
-			WelcomeMessage: "I am embedded in the program!",
-		}
+	mux.Handle("/job/", &jobDetailsHandler{Jobcli: jobcli})
+	mux.Handle("/", &jobsConsoleHandler{Jobcli: jobcli})
 
-		errg, ctx := errgroup.WithContext(ctx)
+	endpoint := fmt.Sprintf("localhost:%d", port)
 
-		var pipeResp *jobpb.GetJobPipelineResponse
-		var metsResp *jobpb.GetJobMetricsResponse
-		errg.Go(func() error {
-			resp, err := jobcli.GetPipeline(ctx, &jobpb.GetJobPipelineRequest{JobId: jobID})
-			pipeResp = resp
-			return err
-		})
-		errg.Go(func() error {
-			resp, err := jobcli.GetJobMetrics(ctx, &jobpb.GetJobMetricsRequest{JobId: jobID})
-			metsResp = resp
-			return err
-		})
-
-		if err := errg.Wait(); err != nil {
-			data.Title = "Error on GetPipeline or GetMetrics"
-			data.WelcomeMessage = err.Error()
-		} else {
-			data.Title = jobID
-			mets := metsResp.GetMetrics()
-			results := metricsx.FromMonitoringInfos(pipeResp.GetPipeline(), mets.GetAttempted(), mets.GetCommitted())
-
-			pcols := map[metrics.StepKey]metrics.PColResult{}
-			for _, res := range results.AllMetrics().PCols() {
-				pcols[res.Key] = res
-			}
-			data.PCols = pcols
-			trs := pipeResp.GetPipeline().GetComponents().GetTransforms()
-			data.Transforms = make([]Tforms, 0, len(trs))
-			for id, pt := range pipeResp.GetPipeline().GetComponents().GetTransforms() {
-				if len(pt.GetSubtransforms()) > 0 {
-					continue
-				}
-				var strMets []string
-				for local := range pt.GetInputs() {
-					name := pt.GetUniqueName() + "." + local
-					r, ok := pcols[metrics.StepKey{Step: name}]
-					if ok {
-						strMets = append(strMets, fmt.Sprintf("Input %v ElementCount: %v", name, r.Committed.ElementCount))
-					}
-				}
-
-				data.Transforms = append(data.Transforms, Tforms{
-					ID:        id,
-					Transform: pt,
-					Metrics:   strMets,
-				})
-			}
-			sort.Slice(data.Transforms, func(i, j int) bool {
-				a, b := data.Transforms[i], data.Transforms[j]
-				return a.Transform.GetUniqueName() < b.Transform.GetUniqueName()
-			})
-		}
-		var buf bytes.Buffer
-		if err := jobPage.Execute(&buf, data); err != nil {
-			data.Title = "Error on Template parsing"
-			data.WelcomeMessage = err.Error()
-		}
-		w.Write(buf.Bytes())
-	})
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		data := struct {
-			Title          string
-			WelcomeMessage string
-			Jobs           []*jobpb.JobInfo
-		}{
-			Title:          "Hello from Go",
-			WelcomeMessage: "I am embedded in the program!",
-		}
-
-		resp, err := jobcli.GetJobs(ctx, &jobpb.GetJobsRequest{})
-		if err != nil {
-			data.Title = "Error on GetJobs"
-			data.WelcomeMessage = err.Error()
-		} else {
-			data.Title = fmt.Sprintf("there are %v jobs", len(resp.GetJobInfo()))
-			data.Jobs = resp.GetJobInfo()
-		}
-		sort.Slice(data.Jobs, func(i, j int) bool {
-			a, b := data.Jobs[i], data.Jobs[j]
-			return a.JobId < b.JobId
-		})
-		var buf bytes.Buffer
-		err = indexPage.Execute(&buf, data)
-		if err != nil {
-			data.Title = "Error on Template parsing"
-			data.WelcomeMessage = err.Error()
-		}
-		w.Write(buf.Bytes())
-	})
-
-	endpoint := fmt.Sprintf(":%d", port)
-
-	slog.Info("Serving WebUI", slog.String("endpoint", endpoint))
+	slog.Info("Serving WebUI", slog.String("endpoint", "http://"+endpoint))
 	err := http.ListenAndServe(endpoint, mux)
 
 	if err != nil {
