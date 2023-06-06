f35a4aeb951e4aeb768a3f851a8e7ff6491e1c3d
David Cavazos <dcavazos@google.com>
2023-06-06 11:48:33 -0700

    revert file

--- sdks/python/apache_beam/runners/portability/sdk_container_builder.py
+++ sdks/python/apache_beam/runners/portability/sdk_container_builder.py
@@ -252,13 +252,7 @@ class _SdkContainerImageCloudBuilder(SdkContainerImageBuilder):
     build.steps = []
     step = cloudbuild.BuildStep()
     step.name = 'gcr.io/kaniko-project/executor:latest'
-    # Disable compression caching to allow for large images to be cached.
-    # See: https://github.com/GoogleContainerTools/kaniko/issues/1669
-    step.args = [
-        '--destination=' + container_image_name,
-        '--cache=true',
-        '--compressed-caching=false',
-    ]
+    step.args = ['--destination=' + container_image_name, '--cache=true']
     step.dir = SOURCE_FOLDER
 
     build.steps.append(step)
