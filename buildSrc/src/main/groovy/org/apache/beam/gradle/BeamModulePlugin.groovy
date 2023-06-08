97562d9d920af221bf615ddc7b8a62fe79c505c4
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2023-06-08 12:10:01 -0400

    Unpin tox version in Beam code base (#26082)
    * Update BeamModulePlugin.groovy
    
    * add run to the tox cmd
    
    * Pin tox upper bound
    
    * Revert "Pin tox upper bound"
    
    This reverts commit 15020171aa6d0224f3a4ddd16106c701c9897c99.
    
    * Add /bin/bash to allow list externals
    
    * Remove tox versioning
    
    * Add comma
    
    * Add curl to the allowlist externals
    
    * Update sdks/python/tox.ini
    
    * Update sdks/python/tox.ini

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -2754,7 +2754,7 @@ class BeamModulePlugin implements Plugin<Project> {
             executable 'sh'
             args '-c', ". ${project.ext.envdir}/bin/activate && " +
                 "pip install --retries 10 --upgrade pip && " +
-                "pip install --retries 10 --upgrade tox==3.20.1 -r ${project.rootDir}/sdks/python/build-requirements.txt"
+                "pip install --retries 10 --upgrade tox -r ${project.rootDir}/sdks/python/build-requirements.txt"
           }
         }
         // Gradle will delete outputs whenever it thinks they are stale. Putting a
