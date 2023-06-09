cb0229dd9e501a54edbd7adbfaf8ed955c5c20ca
Oleh Borysevych <oleg.borisevich@akvelon.com>
2023-06-09 14:13:06 +0300

    xpython

--- playground/backend/containers/xpython/entrypoint.sh
+++ playground/backend/containers/xpython/entrypoint.sh
@@ -1,19 +1,60 @@
 #!/bin/bash
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# set -e
+
+# echo "==> Launching the Docker daemon..."
+# CMD=$*
+# if [ "$CMD" == '' ];then
+#   dind dockerd $DOCKER_EXTRA_OPTS
+#   check_docker
+# else
+#   dind dockerd $DOCKER_EXTRA_OPTS &
+#   while(! docker info > /dev/null 2>&1); do
+#       echo "==> Waiting for the Docker daemon to come online..."
+#       sleep 1
+#   done
+#   echo "==> Docker Daemon is up and running!"
+#   echo "==> Running CMD $CMD!"
+
+  
+#   java -jar beam-sdks-java-extensions-sql-expansion-service-2.43.0.jar 9092 & \
+#   python -m apache_beam.runners.portability.local_job_service_main -p 9091 && fg
+
+#   exec "$CMD"
+# fi
+
+set -e
+
+echo "==> Launching the Docker daemon..."
+
+dind dockerd --iptables=false &
+while(! docker info > /dev/null 2>&1); do
+    echo "==> Waiting for the Docker daemon to come online...???"
+    sleep 1
+done
+
+echo "==> Docker Daemon is up and running!"
+echo "==> Loading pre-pulled docker images!"
+# Import pre-installed images
+for file in /images/*.tar; do
+echo "Loading $file"
+  docker load <$file
+done
+rm -f -r images
+
+docker images
+
+# nohup /opt/mitmproxy/mitmdump -s /opt/mitmproxy/allow_list_proxy.py -p 8081 &
+# while [ ! -f /home/appuser/.mitmproxy/mitmproxy-ca.pem ] ;
+# do
+#       sleep 2
+# done
+# openssl x509 -in /home/appuser/.mitmproxy/mitmproxy-ca.pem -inform PEM -out /home/appuser/.mitmproxy/mitmproxy-ca.crt
+# cp /home/appuser/.mitmproxy/mitmproxy-ca.crt /usr/local/share/ca-certificates/extra/
+# update-ca-certificates
+
+python -m apache_beam.runners.portability.local_job_service_main -p 9091 &> jjs.log  & \
+java -jar beam-sdks-java-extensions-sql-expansion-service-2.45.0.jar 9092 &> jes.log  & \
+python -m apache_beam.runners.portability.local_job_service_main -p 9093 &> pjs.log  & \
+python -m apache_beam.runners.portability.expansion_service_main -p 9094 --fully_qualified_name_glob "*" &> pes.log  && fg
 
 while true; do sleep 1; done
-
-# /opt/playground/backend/server_python_backend
