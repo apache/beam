1f7b7fe422902e1f02d591332e69eed873beea46
Timur Sultanov <timur.sultanov@akvelon.com>
2023-06-12 18:15:08 +0400

    Add xjava to settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -89,6 +89,7 @@ include(":playground:backend:containers:router")
 include(":playground:backend:containers:scio")
 include(":playground:backend:containers:xtransform")
 include(":playground:backend:containers:xpython")
+include(":playground:backend:containers:xjava")
 include(":playground:terraform")
 include(":playground:kafka-emulator")
 
