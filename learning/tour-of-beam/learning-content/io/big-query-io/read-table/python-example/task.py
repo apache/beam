bc47f6ea694596c0c8b2cdc1cc9380b746798c21
Oleh Borysevych <oleg.borisevich@akvelon.com>
2023-06-12 22:17:23 +0300

    fixing incorrect tag

--- learning/tour-of-beam/learning-content/io/big-query-io/read-table/python-example/task.py
+++ learning/tour-of-beam/learning-content/io/big-query-io/read-table/python-example/task.py
@@ -14,7 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# beam-playground-broken:
+# beam-playground:
 #   name: read-table
 #   description: TextIO read table example.
 #   multifile: false
