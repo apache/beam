b272a98c5e25a968221c401638acd80c2fc036d0
Alexey Inkin <alexey.inkin@akvelon.com>
2023-06-15 17:12:47 +0400

    Trigger tests

--- playground/frontend/playground_components/lib/src/widgets/feedback.dart
+++ playground/frontend/playground_components/lib/src/widgets/feedback.dart
@@ -31,7 +31,7 @@ class FeedbackWidget extends StatelessWidget {
   final FeedbackController controller;
   final String title;
 
-  const FeedbackWidget({
+  FeedbackWidget({
     required this.controller,
     required this.title,
   });
@@ -53,7 +53,7 @@ class FeedbackWidget extends StatelessWidget {
       closeNotifier: closeNotifier,
       positioned: Positioned(
         bottom: 50,
-        left: 20,
+        left: 30,
         child: OverlayBody(
           child: FeedbackDropdown(
             close: closeNotifier.notifyPublic,
