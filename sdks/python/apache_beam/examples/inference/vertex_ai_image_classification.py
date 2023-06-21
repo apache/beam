f338e2c051b979ead01d49d42e62c5414f64da33
Jack McCluskey <thejackmccluskey@gmail.com>
2023-06-21 14:09:07 -0400

    new flags, types, framework for eventual file read

--- sdks/python/apache_beam/examples/inference/vertex_ai_image_classification.py
+++ sdks/python/apache_beam/examples/inference/vertex_ai_image_classification.py
@@ -17,7 +17,9 @@
 
 import argparse
 import apache_beam as beam
+import json
 import logging
+import tensorflow as tf
 from apache_beam.ml.inference.base import RunInference
 from apache_beam.ml.inference.vertex_ai_inference import VertexAIModelHandlerJSON
 from apache_beam.runners.runner import PipelineResult
@@ -39,21 +41,31 @@ for more information.)
 def parse_known_args(argv):
   """Parses args for the workflow."""
   parser = argparse.ArgumentParser()
+  # parser.add_argument(
+  #     '--input',
+  #     dest='input',
+  #     type=str,
+  #     required=True,
+  #     help='File path to read images from.'
+  # )
   parser.add_argument(
       '--output',
       dest='output',
+      type=str,
       required=True,
       help='Path to save output predictions.')
   parser.add_argument(
-      '--endpoint',
+      '--endpoint_id',
       dest='endpoint',
+      type=str,
       required=True,
-      help='Vertex AI Endpoint resource ID to query.')
+      help='Vertex AI Endpoint resource ID to query (string).')
   parser.add_argument(
-      '--project', dest='project', required=True, help='GCP Project')
+      '--endpoint_project', dest='project', required=True, help='GCP Project')
   parser.add_argument(
-      '--location',
+      '--endpoint_location',
       dest='location',
+      type=str,
       required=True,
       help='GCP location for the Endpoint')
   parser.add_argument(
@@ -62,6 +74,33 @@ def parse_known_args(argv):
       required=False,
       help='GCP experiment to pass to init')
   return parser.parse_known_args(argv)
+
+
+# Image height and width expected by the model
+IMG_WIDTH = 128
+
+# Column labels for the output probabilities.
+# TODO: map output probability to these labels as post-processing step
+COLUMNS = ['dandelion', 'daisy', 'tulips', 'sunflowers', 'roses']
+
+
+def preprocess_image(data) -> list[float]:
+  """Preprocess the image, resizing it and normalizing it before
+  converting to a list. Currently unusued, will be incorporated
+  once loading images from storage is supported.
+  """
+  image = tf.io.decode_jpeg(data, channels=3)
+  image = tf.image.resize_with_pad(image, IMG_WIDTH, IMG_WIDTH)
+  image = image / 255
+  return image.numpy().tolist()
+
+
+def convert_to_json(data: list[float]) -> str:
+  """ Serializes the list of scalars to JSON for the 
+  request. Currently unused, will be incorporated once
+  loading images from storage is supported.
+  """
+  return json.dumps(data)
 
 
 def run(
