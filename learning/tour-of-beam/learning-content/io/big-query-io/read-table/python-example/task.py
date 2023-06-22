e134a12ccef0e3d132bf087bf91d70b3ff17324a
mende1esmende1es <mende1esmende1es@gmail.cp>
2023-06-22 20:45:06 +0600

    correct

--- learning/tour-of-beam/learning-content/io/big-query-io/read-table/python-example/task.py
+++ learning/tour-of-beam/learning-content/io/big-query-io/read-table/python-example/task.py
@@ -28,12 +28,8 @@
 #     - hellobeam
 
 import argparse
-import os
-import warnings
-
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, SetupOptions
-from apache_beam.io.gcp.bigquery import ReadFromBigQueryRequest, ReadAllFromBigQuery
 
 class WeatherData:
     def __init__(self, station_number, wban_number, year, month, day):
@@ -56,7 +52,7 @@ def run(argv=None):
 
 
     with beam.Pipeline(options=pipeline_options, argv=argv) as p:
-        (p | 'ReadFromBigQuery' >> beam.io.ReadFromBigQuery(query='select * from `apache-beam-testing.clouddataflow_samples.weather_stations`',
+      (p | 'ReadFromBigQuery' >> beam.io.ReadFromBigQuery(table='apache-beam-testing:clouddataflow_samples.weather_stations',
                                                             method=beam.io.ReadFromBigQuery.Method.DIRECT_READ)
          | beam.combiners.Sample.FixedSizeGlobally(5)
          | beam.FlatMap(lambda line: line)
