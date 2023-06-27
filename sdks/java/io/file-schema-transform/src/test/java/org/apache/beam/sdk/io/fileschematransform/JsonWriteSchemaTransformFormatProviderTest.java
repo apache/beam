270e4fe2e2ed326eeb84eea132079202c7ffa184
Nick Li <56149585+nickuncaged1201@users.noreply.github.com>
2023-06-27 10:24:29 -0700

    Add error metrics and DLQ support to GCS/File write schema transforms (#27039)
    * init
    
    * fixing
    
    * fixed avro schema transform
    
    * json error metrics
    
    * parquet error metric
    
    * xml error metrics
    
    * fix comments
    
    * spotless
    
    * add tests
    
    * spotless
    
    * Change name of ErrorCounterFn
    
    * add generic test for BeamRowMapperWithDlq

--- sdks/java/io/file-schema-transform/src/test/java/org/apache/beam/sdk/io/fileschematransform/JsonWriteSchemaTransformFormatProviderTest.java
+++ sdks/java/io/file-schema-transform/src/test/java/org/apache/beam/sdk/io/fileschematransform/JsonWriteSchemaTransformFormatProviderTest.java
@@ -20,17 +20,29 @@ package org.apache.beam.sdk.io.fileschematransform;
 import static org.apache.beam.sdk.io.fileschematransform.FileWriteSchemaTransformFormatProviders.JSON;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.fileschematransform.FileWriteSchemaTransformFormatProviders.BeamRowMapperWithDlq;
+import org.apache.beam.sdk.io.fileschematransform.JsonWriteSchemaTransformFormatProvider.RowToJsonFn;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.io.payloads.JsonPayloadSerializerProvider;
 import org.apache.beam.sdk.schemas.io.payloads.PayloadSerializer;
 import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableMap;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -101,4 +113,52 @@ public class JsonWriteSchemaTransformFormatProviderTest
     return Optional.of(
         "configuration with org.apache.beam.sdk.io.fileschematransform.FileWriteSchemaTransformConfiguration$CsvConfiguration is not compatible with a json format");
   }
+
+  @Test
+  public void testJsonErrorCounterSuccess() {
+    SerializableFunction<Row, String> mapFn = new RowToJsonFn(BEAM_SCHEMA);
+
+    PCollection<Row> input = writePipeline.apply(Create.of(ROWS));
+    PCollectionTuple output =
+        input.apply(
+            ParDo.of(
+                    new BeamRowMapperWithDlq<String>("Json-write-error-counter", mapFn, OUTPUT_TAG))
+                .withOutputTags(OUTPUT_TAG, TupleTagList.of(ERROR_TAG)));
+    output.get(ERROR_TAG).setRowSchema(ERROR_SCHEMA);
+    PCollection<Long> count = output.get(OUTPUT_TAG).apply(Count.globally());
+    PAssert.that(count).containsInAnyOrder(Collections.singleton(3L));
+    writePipeline.run().waitUntilFinish();
+  }
+
+  @Test
+  public void testJsonErrorCounterFailure() {
+    SerializableFunction<Row, String> mapFn = new RowToJsonFn(BEAM_SCHEMA_DLQ);
+
+    PCollection<Row> input = writePipeline.apply(Create.of(ROWS));
+    PCollectionTuple output =
+        input.apply(
+            ParDo.of(
+                    new BeamRowMapperWithDlq<String>("Json-write-error-counter", mapFn, OUTPUT_TAG))
+                .withOutputTags(OUTPUT_TAG, TupleTagList.of(ERROR_TAG)));
+    output.get(ERROR_TAG).setRowSchema(ERROR_SCHEMA);
+    PCollection<Long> count = output.get(ERROR_TAG).apply(Count.globally());
+    PAssert.that(count).containsInAnyOrder(Collections.singletonList(3L));
+    writePipeline.run().waitUntilFinish();
+  }
+
+  private static final TupleTag<String> OUTPUT_TAG =
+      JsonWriteSchemaTransformFormatProvider.ERROR_FN_OUPUT_TAG;
+  private static final TupleTag<Row> ERROR_TAG = FileWriteSchemaTransformProvider.ERROR_TAG;
+
+  private static final Schema BEAM_SCHEMA =
+      Schema.of(Schema.Field.of("name", Schema.FieldType.STRING));
+  private static final Schema BEAM_SCHEMA_DLQ =
+      Schema.of(Schema.Field.of("error", Schema.FieldType.INT32));
+  private static final Schema ERROR_SCHEMA = FileWriteSchemaTransformProvider.ERROR_SCHEMA;
+
+  private static final List<Row> ROWS =
+      Arrays.asList(
+          Row.withSchema(BEAM_SCHEMA).withFieldValue("name", "a").build(),
+          Row.withSchema(BEAM_SCHEMA).withFieldValue("name", "b").build(),
+          Row.withSchema(BEAM_SCHEMA).withFieldValue("name", "c").build());
 }
