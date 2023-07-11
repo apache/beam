c721599ce684ebb5946b7e2a56abbf8613989740
Jack McCluskey <thejackmccluskey@gmail.com>
2023-07-11 15:15:07 -0400

    Whitespace cleanup

--- sdks/python/apache_beam/ml/inference/vertex_ai_inference.py
+++ sdks/python/apache_beam/ml/inference/vertex_ai_inference.py
@@ -71,7 +71,6 @@ class VertexAIModelHandlerJSON(ModelHandler[Any,
     """Implementation of the ModelHandler interface for Vertex AI.
     **NOTE:** This API and its implementation are under development and
     do not provide backward compatibility guarantees.
-
     Unlike other ModelHandler implementations, this does not load the model
     being used onto the worker and instead makes remote queries to a
     Vertex AI endpoint. In that way it functions more like a mid-pipeline
@@ -89,7 +88,6 @@ class VertexAIModelHandlerJSON(ModelHandler[Any,
     """
 
     self._env_vars = kwargs.get('env_vars', {})
-
     # TODO: support the full list of options for aiplatform.init()
     # See https://cloud.google.com/python/docs/reference/aiplatform/latest/google.cloud.aiplatform#google_cloud_aiplatform_init
     aiplatform.init(
