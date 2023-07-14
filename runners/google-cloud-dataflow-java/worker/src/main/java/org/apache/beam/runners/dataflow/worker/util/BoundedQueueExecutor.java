ef61a3039da586834c5171090b5d8f1033530dfc
al97 <albertjk@google.com>
2023-07-14 12:49:54 -0700

    Update runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/util/BoundedQueueExecutor.java
    Co-authored-by: Bruno Volpato <gh@brunovolpato.com>

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/util/BoundedQueueExecutor.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/util/BoundedQueueExecutor.java
@@ -60,10 +60,9 @@ public class BoundedQueueExecutor {
           protected void beforeExecute(Thread t, Runnable r) {
             super.beforeExecute(t, r);
             synchronized (this) {
-              if (activeCount.get() == maximumPoolSize - 1) {
+              if (activeCount.getAndIncrement() == maximumPoolSize - 1) {
                 startTimeMaxActiveThreadsUsed = System.currentTimeMillis();
               }
-              activeCount.incrementAndGet();
             }
           }
 
