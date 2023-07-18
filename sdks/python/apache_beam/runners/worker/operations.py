4032d12b6bbc9fb800771f1efd8c9da4354bdbc3
Jack McCluskey <thejackmccluskey@gmail.com>
2023-07-18 15:04:13 -0400

    Test code change w/o cython upgrade

--- sdks/python/apache_beam/runners/worker/operations.py
+++ sdks/python/apache_beam/runners/worker/operations.py
@@ -1105,12 +1105,6 @@ class SdfProcessSizedElements(DoOperation):
 
   def monitoring_infos(self, transform_id, tag_to_pcollection_id):
     # type: (str, Dict[str, str]) -> Dict[FrozenSet, metrics_pb2.MonitoringInfo]
-
-    def encode_progress(value):
-      # type: (float) -> bytes
-      coder = coders.IterableCoder(coders.FloatCoder())
-      return coder.encode([value])
-
     with self.lock:
       infos = super(SdfProcessSizedElements,
                     self).monitoring_infos(transform_id, tag_to_pcollection_id)
@@ -1124,16 +1118,17 @@ class SdfProcessSizedElements(DoOperation):
           remaining = current_element_progress.fraction_remaining
         assert completed is not None
         assert remaining is not None
+        coder = coders.IterableCoder(coders.FloatCoder())
         completed_mi = metrics_pb2.MonitoringInfo(
             urn=monitoring_infos.WORK_COMPLETED_URN,
             type=monitoring_infos.PROGRESS_TYPE,
             labels=monitoring_infos.create_labels(ptransform=transform_id),
-            payload=encode_progress(completed))
+            payload=coder.encode([completed]))
         remaining_mi = metrics_pb2.MonitoringInfo(
             urn=monitoring_infos.WORK_REMAINING_URN,
             type=monitoring_infos.PROGRESS_TYPE,
             labels=monitoring_infos.create_labels(ptransform=transform_id),
-            payload=encode_progress(remaining))
+            payload=coder.encode([remaining]))
         infos[monitoring_infos.to_key(completed_mi)] = completed_mi
         infos[monitoring_infos.to_key(remaining_mi)] = remaining_mi
     return infos
