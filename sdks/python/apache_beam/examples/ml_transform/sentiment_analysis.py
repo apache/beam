5603c67fa2eaa8cd4be663c3cc6984625a26edcd
Anand Inguva <anandinguva@google.com>
2023-07-24 03:14:12 +0000

    Map index to vocab

--- sdks/python/apache_beam/examples/ml_transform/sentiment_analysis.py
+++ sdks/python/apache_beam/examples/ml_transform/sentiment_analysis.py
@@ -9,14 +9,16 @@ import apache_beam as beam
 from apache_beam.ml.transforms.base import MLTransform
 from apache_beam.ml.transforms.tft import ComputeAndApplyVocabulary
 from apache_beam.ml.transforms.tft import TFIDF
+from apache_beam.ml.transforms.utils import ArtifactsFetcher
 
 # Names of temp files
 SHUFFLED_TRAIN_DATA_FILEBASE = 'train_shuffled'
 SHUFFLED_TEST_DATA_FILEBASE = 'test_shuffled'
+RAW_DATA_KEY = 'raw_data'
 REVIEW_COLUMN = 'review'
 LABEL_COLUMN = 'label'
 DELIMITERS = '.,!?() '
-VOCAB_SIZE = 20000
+VOCAB_SIZE = 200000
 
 
 # pylint: disable=invalid-name
@@ -60,11 +62,13 @@ class ReadAndShuffleData(beam.PTransform):
         shuffled_examples
         | beam.Map(
             lambda label_review: {
-                REVIEW_COLUMN: label_review[0], LABEL_COLUMN: label_review[1]
+                REVIEW_COLUMN: label_review[0],
+                LABEL_COLUMN: label_review[1],
+                RAW_DATA_KEY: label_review[0]
             }))
 
 
-def preprocess_data(file_patterns, pipeline_args, working_dir):
+def preprocess_data(file_patterns, pipeline_args, working_dir, output_dir):
   positive_pattern, negative_pattern = file_patterns
   artifact_location = working_dir
   options = beam.options.pipeline_options.PipelineOptions(pipeline_args)
@@ -77,12 +81,43 @@ def preprocess_data(file_patterns, pipeline_args, working_dir):
         artifact_location=artifact_location,
     ).with_transform(
         ComputeAndApplyVocabulary(
+            top_k=VOCAB_SIZE,
             columns=[REVIEW_COLUMN],
-            split_string_by_delimiter=DELIMITERS,
-            top_k=VOCAB_SIZE)).with_transform(
-                TFIDF(columns=[REVIEW_COLUMN], vocab_size=VOCAB_SIZE + 1))
+            split_string_by_delimiter=DELIMITERS)).with_transform(
+                TFIDF(columns=[REVIEW_COLUMN], vocab_size=VOCAB_SIZE))
     data_pcoll = data_pcoll | 'MLTransform' >> ml_transform
-    return data_pcoll
+
+    data_pcoll = (
+        data_pcoll | beam.ParDo(MapTFIDFScoreToVocab(artifact_location)))
+
+    _ = (data_pcoll | beam.io.WriteToText(output_dir))
+
+    _ = data_pcoll | beam.Map(logging.info)
+
+
+class MapTFIDFScoreToVocab(beam.DoFn):
+  def __init__(self, artifact_location):
+    self.artifact_location = artifact_location
+
+  def process(self, element):
+    index_column_name = REVIEW_COLUMN + '_vocab_index'
+    weight_column_name = REVIEW_COLUMN + '_tfidf_weight'
+    element = element.as_dict()
+    raw_data = element[RAW_DATA_KEY]
+
+    vocab_index = element[index_column_name]
+    weights = element[weight_column_name]
+
+    if not hasattr(self, 'vocab_list'):
+      self.vocab_list = ArtifactsFetcher(
+          self.artifact_location).get_vocab_list()
+    vocabs_with_weights = [(self.vocab_list[vocab_index[i]], weights[i])
+                           for i in range(len(vocab_index))]
+
+    return [(
+        raw_data,
+        vocabs_with_weights,
+    )]
 
 
 def main():
@@ -91,6 +126,10 @@ def main():
       '--input_data_dir', help='path to directory containing input data')
   parser.add_argument(
       '--working_dir',
+      help='path to directory to hold transformed data',
+      default=None)
+  parser.add_argument(
+      '--output_dir',
       help='path to directory to hold transformed data',
       default=None)
   args, pipeline_args = parser.parse_known_args()
@@ -111,3 +150,7 @@ if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
   start_time = time.time()
   main()
+  end_time = time.time()
+  logging.info(
+      "Total time taken to run the pipeline: %s seconds" %
+      (end_time - start_time))
