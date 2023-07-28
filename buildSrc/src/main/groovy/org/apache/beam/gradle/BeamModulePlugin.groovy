dcdb05e5b0381472e728deda4117b2d12a503738
Burke Davison <burkedavison@google.com>
2023-07-28 16:15:37 -0400

    Trivial change. Force Java PreCommit rerun

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -836,7 +836,6 @@ class BeamModulePlugin implements Plugin<Project> {
     ]
 
     /** ***********************************************************************************************/
-
     // Returns a string representing the relocated path to be used with the shadow plugin when
     // given a suffix such as "com.google.common".
     project.ext.getJavaRelocatedPath = { String suffix ->
