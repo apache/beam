6054296e0e32209d233e350686776833f236d893
Jack McCluskey <thejackmccluskey@gmail.com>
2023-08-16 16:07:42 -0400

    More formatting

--- sdks/python/apache_beam/runners/dataflow/internal/apiclient.py
+++ sdks/python/apache_beam/runners/dataflow/internal/apiclient.py
@@ -1116,8 +1116,7 @@ def translate_value(value, metric_update_proto):
 
 
 def _get_container_image_tag():
-  base_version = version.parse(
-      beam_version.__version__).base_version
+  base_version = version.parse(beam_version.__version__).base_version
   if base_version != beam_version.__version__:
     warnings.warn(
         "A non-standard version of Beam SDK detected: %s. "
