b0701bae6d2b379de8b68d21993737ebd8a317d3
Bjorn Pedersen <bjornpedersen@google.com>
2023-08-22 16:16:36 -0400

    fixed name of assert_called()

--- sdks/python/apache_beam/io/gcp/gcsfilesystem_test.py
+++ sdks/python/apache_beam/io/gcp/gcsfilesystem_test.py
@@ -373,7 +373,7 @@ class GCSFileSystemTest(unittest.TestCase):
     # Issue batch delete.
     with self.assertRaisesRegex(BeamIOError, r'^Delete operation failed'):
       self.fs.delete(files)
-    gcsio_mock.delete_batch.assert_called_()
+    gcsio_mock.delete_batch.assert_called()
 
 
 if __name__ == '__main__':
