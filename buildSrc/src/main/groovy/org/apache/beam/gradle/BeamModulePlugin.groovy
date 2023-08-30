27c8b5646c731283a7688d229861c7610b6ac5f5
Celeste Zeng <61256376+celeste-zeng@users.noreply.github.com>
2023-08-30 11:09:09 -0700

    Update BeamModulePlugin.groovy

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1053,9 +1053,9 @@ class BeamModulePlugin implements Plugin<Project> {
 
       project.tasks.withType(JavaCompile).configureEach {
         options.encoding = "UTF-8"
-        // Use --release 8 when targeting Java 8 and running on JDK > 8
+        // Use -source 8 and -target 8 when targeting Java 8 and running on JDK > 8
         //
-        // Consider migrating compilation and testing to use JDK 9+ and setting '--release=8' as
+        // Consider migrating compilation and testing to use JDK 9+ and setting '-source 8 -target 8' as
         // the default allowing 'applyJavaNature' to override it for the few modules that need JDK 9+
         // artifacts. See https://stackoverflow.com/a/43103038/4368200 for additional details.
         if (JavaVersion.VERSION_1_8.compareTo(JavaVersion.toVersion(project.javaVersion)) == 0
@@ -1089,7 +1089,8 @@ class BeamModulePlugin implements Plugin<Project> {
           options.fork = true
           options.forkOptions.javaHome = java11Home as File
           options.compilerArgs += ['-Xlint:-path']
-          options.compilerArgs.addAll(['--release', '11'])
+          options.compilerArgs += ['-source', '11']
+          options.compilerArgs += ['-target', '11']
         }
         project.tasks.withType(Test) {
           useJUnit()
@@ -1489,8 +1490,8 @@ class BeamModulePlugin implements Plugin<Project> {
       if (project.hasProperty("compileAndRunTestsWithJava17")) {
         def java17Home = project.findProperty("java17Home")
         project.tasks.compileTestJava {
-          options.compilerArgs.addAll(['-target', '17'])
-          options.compilerArgs.addAll(['-source', '17'])
+          options.compilerArgs += ['-target', '17']
+          options.compilerArgs += ['-source', '17']
           project.ext.setJava17Options(options)
         }
         project.tasks.withType(Test) {
