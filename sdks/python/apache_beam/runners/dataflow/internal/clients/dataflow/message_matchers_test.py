8599d22f0e38627142f950d638f3a539219ede03
Anand Inguva <anandinguva98@gmail.com>
2023-09-05 08:23:27 -0400

    Change ModuleNotFoundError

--- sdks/python/apache_beam/runners/dataflow/internal/clients/dataflow/message_matchers_test.py
+++ sdks/python/apache_beam/runners/dataflow/internal/clients/dataflow/message_matchers_test.py
@@ -20,15 +20,15 @@ import unittest
 
 import hamcrest as hc
 
-import apache_beam.runners.dataflow.internal.clients.dataflow as dataflow
 from apache_beam.internal.gcp.json_value import to_json_value
+from apache_beam.runners.dataflow.internal.clients import dataflow
 from apache_beam.runners.dataflow.internal.clients.dataflow import message_matchers
 
 # Protect against environments where apitools library is not available.
 # pylint: disable=wrong-import-order, wrong-import-position
 try:
   from apitools.base.py import base_api
-except ImportError:
+except ModuleNotFoundError:
   base_api = None
 # pylint: enable=wrong-import-order, wrong-import-position
 
