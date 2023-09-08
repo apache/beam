f0a9c6252c2767054502e13c489a0ceeda92a0ee
Robert Bradshaw <robertwb@gmail.com>
2023-09-08 11:08:59 -0700

    another post-merge adaptation

--- sdks/python/apache_beam/yaml/yaml_transform.py
+++ sdks/python/apache_beam/yaml/yaml_transform.py
@@ -208,7 +208,7 @@ class Scope(LightweightScope):
       # TODO(yaml): Also trace through outputs and composites.
       for transform in self._transforms:
         if transform['type'] != 'composite':
-          for input in transform.get('input').values():
+          for input in empty_if_explicitly_empty(transform['input']).values():
             transform_id, _ = self.get_transform_id_and_output_name(input)
             self._all_followers[transform_id].append(transform['__uuid__'])
     return self._all_followers[self.get_transform_id(transform_name)]
