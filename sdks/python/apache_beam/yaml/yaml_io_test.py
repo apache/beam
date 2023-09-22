c5c9a06d8d9fbcc16f4963c3479bd145b7adf186
Robert Bradshaw <robertwb@gmail.com>
2023-09-22 11:58:22 -0700

    Add id_attribute, docstrings.

--- sdks/python/apache_beam/yaml/yaml_io_test.py
+++ sdks/python/apache_beam/yaml/yaml_io_test.py
@@ -32,14 +32,27 @@ from apache_beam.yaml.yaml_transform import YamlTransform
 
 class FakeReadFromPubSub:
   def __init__(
-      self, topic, messages, subscription=None, timestamp_attribute=None):
+      self,
+      topic,
+      messages,
+      subscription=None,
+      id_attribute=None,
+      timestamp_attribute=None):
     self._topic = topic
     self._messages = messages
+    self._id_attribute = id_attribute
     self._timestamp_attribute = timestamp_attribute
 
   def __call__(
-      self, *, topic, subscription, with_attributes, timestamp_attribute):
+      self,
+      *,
+      topic,
+      subscription,
+      with_attributes,
+      id_label,
+      timestamp_attribute):
     assert topic == self._topic
+    assert id_label == self._id_attribute
     assert timestamp_attribute == self._timestamp_attribute
     assert subscription == subscription
     if with_attributes:
@@ -50,14 +63,17 @@ class FakeReadFromPubSub:
 
 
 class FakeWriteToPubSub:
-  def __init__(self, topic, messages, timestamp_attribute=None):
+  def __init__(
+      self, topic, messages, id_attribute=None, timestamp_attribute=None):
     self._topic = topic
     self._messages = messages
+    self._id_attribute = id_attribute
     self._timestamp_attribute = timestamp_attribute
 
-  def __call__(self, topic, *, with_attributes, timestamp_attribute):
+  def __call__(self, topic, *, with_attributes, id_label, timestamp_attribute):
     assert topic == self._topic
     assert with_attributes == True
+    assert id_label == self._id_attribute
     assert timestamp_attribute == self._timestamp_attribute
     return AssertThat(equal_to(self._messages))
 
@@ -131,6 +147,27 @@ class YamlPubSubTest(unittest.TestCase):
                 beam.Row(payload=b'msg2', attrMap={'attr': 'value2'})
             ]))
 
+  def test_read_with_id_attribute(self):
+    with beam.Pipeline(options=beam.options.pipeline_options.PipelineOptions(
+        pickle_library='cloudpickle')) as p:
+      with mock.patch('apache_beam.io.ReadFromPubSub',
+                      FakeReadFromPubSub(
+                          topic='my_topic',
+                          messages=[PubsubMessage(b'msg1', {'attr': 'value1'}),
+                                    PubsubMessage(b'msg2', {'attr': 'value2'})],
+                          id_attribute='some_attr')):
+        result = p | YamlTransform(
+            '''
+            type: ReadFromPubSub
+            config:
+              topic: my_topic
+              format: raw
+              id_attribute: some_attr
+            ''')
+        assert_that(
+            result,
+            equal_to([beam.Row(payload=b'msg1'), beam.Row(payload=b'msg2')]))
+
   def test_simple_write(self):
     with beam.Pipeline(options=beam.options.pipeline_options.PipelineOptions(
         pickle_library='cloudpickle')) as p:
@@ -193,6 +230,26 @@ class YamlPubSubTest(unittest.TestCase):
               topic: my_topic
               format: raw
               attributes_map: attrMap
+            '''))
+
+  def test_write_with_id_attribute(self):
+    with beam.Pipeline(options=beam.options.pipeline_options.PipelineOptions(
+        pickle_library='cloudpickle')) as p:
+      with mock.patch('apache_beam.io.WriteToPubSub',
+                      FakeWriteToPubSub(topic='my_topic',
+                                        messages=[PubsubMessage(b'msg1', {}),
+                                                  PubsubMessage(b'msg2', {})],
+                                        id_attribute='some_attr')):
+        _ = (
+            p | beam.Create([beam.Row(a=b'msg1'), beam.Row(a=b'msg2')])
+            | YamlTransform(
+                '''
+            type: WriteToPubSub
+            input: input
+            config:
+              topic: my_topic
+              format: raw
+              id_attribute: some_attr
             '''))
 
 
