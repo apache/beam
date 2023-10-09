5491b5cdabc37e84fb836cfc610e52b7702fdc91
Robert Burke <lostluck@users.noreply.github.com>
2023-10-09 10:49:11 -0700

    Update container version in BeamModulePlugin.groovy (#28879)
    Ensure boot loaders are built using the latest Go patch release.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -2154,7 +2154,7 @@ class BeamModulePlugin implements Plugin<Project> {
       def goRootDir = "${project.rootDir}/sdks/go"
 
       // This sets the whole project Go version.
-      project.ext.goVersion = "go1.21.1"
+      project.ext.goVersion = "go1.21.2"
 
       // Minor TODO: Figure out if we can pull out the GOCMD env variable after goPrepare script
       // completion, and avoid this GOBIN substitution.
