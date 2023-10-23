f2ad377aeba963f3f4589b708ae95f44a6c95830
Joey Tran <joey.tran@schrodinger.com>
2023-10-23 08:45:42 -0400

    update unit test to expect uuids

--- sdks/python/apache_beam/pipeline_test.py
+++ sdks/python/apache_beam/pipeline_test.py
@@ -22,6 +22,7 @@
 import copy
 import platform
 import unittest
+import uuid
 
 import mock
 import pytest
@@ -267,7 +268,11 @@ class PipelineTest(unittest.TestCase):
         'pvalue | "label" >> transform')
 
   def test_auto_unique_labels(self):
+
     opts = PipelineOptions(["--auto_unique_labels"])
+    with mock.patch.object(uuid, 'uuid4') as mock_uuid_gen:
+      mock_uuids = [mock.Mock(hex='UUID01XXX'), mock.Mock(hex='UUID02XXX')]
+      mock_uuid_gen.side_effect = mock_uuids
     with TestPipeline(options=opts) as pipeline:
       pcoll = pipeline | 'pcoll' >> Create([1, 2, 3])
 
@@ -284,8 +289,9 @@ class PipelineTest(unittest.TestCase):
         for label in pipeline.applied_labels if "Map(identity)" in label
     }
     map_id_leaf_labels = {label.split(":")[-1] for label in map_id_full_labels}
+    # Only the first 6 chars of the UUID hex should be used
     assert map_id_leaf_labels == set(
-        ["Map(identity)", "Map(identity)_1", "Map(identity)_2"])
+        ["Map(identity)", "Map(identity)_UUID01", "Map(identity)_UUID02"])
 
   def test_reuse_cloned_custom_transform_instance(self):
     with TestPipeline() as pipeline:
