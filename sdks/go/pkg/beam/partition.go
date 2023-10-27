1e6b7b0f773f392372f9c0af103274857477768b
lostluck <13907733+lostluck@users.noreply.github.com>
2023-10-27 15:04:19 -0700

    !fixup

--- sdks/go/pkg/beam/partition.go
+++ sdks/go/pkg/beam/partition.go
@@ -38,6 +38,18 @@ var (
 // a []PCollection<T> that bundles N PCollection<T>s containing the split elements.
 //
 // A PartitionFn has the signature `func(T) int.`
+// 		
+//		func lenToTen(s string) int {
+//          if len(s) > 9 {
+//      		return 10
+//	        }
+//		    return len(s)
+//		}
+//
+//		// Partition functions must be registered with Beam, and must not be closures.
+//		func init() { register.Function1x1(lenToTen) }
+//
+//		wordsByLength := beam.Partition(s, 11, lenToTen, inputStrings)
 //
 // T is permitted to be a KV.
 func Partition(s Scope, n int, fn any, col PCollection) []PCollection {
