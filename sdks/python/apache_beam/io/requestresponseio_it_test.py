c81c43499178fa95ccc7cee8ac5951a388656c66
Damon Douglas <damondouglas@google.com>
2023-11-07 01:22:51 +0000

    Finish Echo HTTP IT tests

--- sdks/python/apache_beam/io/requestresponseio_it_test.py
+++ sdks/python/apache_beam/io/requestresponseio_it_test.py
@@ -14,39 +14,91 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import json
+
 from dataclasses import dataclass
+import sys
+import unittest
 
-# TODO(damondouglas,riteshghorse): figure out how to add .test-infra/mockapis dependency
-# import proto.echo.v1.echo_pb2 as echo
-# from proto.echo.v1.echo_pb2_grpc import EchoServiceStub
 import urllib3
-from requestresponseio import Caller, UserCodeExecutionException, \
-    UserCodeQuotaException
+
+from apache_beam.io.requestresponseio import Caller
+from apache_beam.io.requestresponseio import UserCodeExecutionException
+from apache_beam.io.requestresponseio import UserCodeQuotaException
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# try:
+#     import proto.echo.v1.echo_pb2 as echo
+#     from proto.echo.v1.echo_pb2_grpc import EchoServiceStub
+# except ImportError as e:
+#     raise
 
 
-# TODO(damondouglas,riteshghorse): use echo.EchoRequest
-#   instead of this custom class
+_HTTP_PATH = "/v1/echo"
+_PAYLOAD = "cGF5bG9hZAo="  # 'payload' encoded as Base64
+
+
 @dataclass
 class EchoRequest:
     id: str
     payload: str
 
 
-# TODO(damondouglas,riteshghorse): use echo.EchoResponse
-#   instead of this custom class
 @dataclass
 class EchoResponse:
     id: str
     payload: str
 
 
-class EchoHTTPClient(Caller):
+class EchoITOptions(PipelineOptions):
+    """Shared options for running integration tests on a deployed
+    ``EchoServiceGrpc`` See https://github.com/apache/beam/tree/master/.test
+    -infra/mock-apis#integration for details on how to acquire values
+    required by ``EchoITOptions``.
+    """
+    @classmethod
+    def _add_argparse_args(cls, parser) -> None:
+        parser.add_argument(
+            '--gRPCEndpointAddress',
+            required=True,
+            dest='grpc_endpoint_address',
+            help='The gRPC address of the Echo API endpoint, typically of the '
+                 'form <host>:<port>.'
+        )
+        parser.add_argument(
+            '--httpEndpointAddress',
+            required=True,
+            dest='http_endpoint_address',
+            help='The HTTP address of the Echo API endpoint; must being with '
+                 'http(s)://'
+        )
+        parser.add_argument(
+            '--neverExceedQuotaId',
+            default='echo-should-never-exceed-quota',
+            dest='never_exceed_quota_id',
+            help='The ID for an allocated quota that should never exceed.'
+        )
+        parser.add_argument(
+            '--shouldExceedQuotaId',
+            default='echo-should-exceed-quota',
+            dest='should_exceed_quota_id',
+            help='The ID for an allocated quota that should exceed.'
+        )
+
+
+class EchoHTTPCaller(Caller):
+    """Implements ``Caller`` to call the ``EchoServiceGrpc``'s HTTP handler.
+    The purpose of ``EchoHTTPCaller`` is to support integration tests.
+    """
 
     def __init__(self, url: str):
-        self.url = url
+        self.url = url + _HTTP_PATH
 
     def call(self, request: EchoRequest) -> EchoResponse:
+        """Overrides ``Caller``'s call method invoking the ``EchoServiceGrpc``'s
+        HTTP handler with an ``EchoRequest``, returning either a successful
+        ``EchoResponse`` or throwing either a ``UserCodeExecutionException``,
+        ``UserCodeTimeoutException``, or a ``UserCodeQuotaException``.
+        """
         try:
             resp = urllib3.request("POST", self.url,
                                    json={
@@ -68,16 +120,68 @@ class EchoHTTPClient(Caller):
             raise UserCodeExecutionException(e)
 
 
-def run(argv=None):
-    client = EchoHTTPClient("http://localhost:8080/v1/echo")
-    req = EchoRequest
-    req.id = "echo-should-exceed-quota"
-    req.payload = "aGkK"
-    resp = client.call(req)
-    print(resp)
-    resp = client.call(req)
-    print(resp)
+class EchoHTTPCallerTestIT(unittest.TestCase):
+    options: EchoITOptions = None
+    client: EchoHTTPCaller = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.options = EchoITOptions()
+        cls.client = EchoHTTPCaller(cls.options.http_endpoint_address)
+
+    def setUp(self) -> None:
+        """
+        The challenge with building and deploying a real quota aware
+        endpoint, the integration with which these tests validate, is that we
+        need a value of at least 1. The allocated quota where we expect to
+        exceed will be shared among many tests and across languages. Code
+        below in this setup ensures that the API is in the state where we can
+        expect a quota exceeded error. There are tests in this file that
+        detect errors in expected responses. We only throw exceptions that
+        are not UserCodeQuotaException.
+        """
+        req = EchoRequest(
+            id=EchoHTTPCallerTestIT.options.should_exceed_quota_id,
+            payload=_PAYLOAD
+        )
+        try:
+            EchoHTTPCallerTestIT.client.call(req)
+            EchoHTTPCallerTestIT.client.call(req)
+            EchoHTTPCallerTestIT.client.call(req)
+        except UserCodeExecutionException as e:
+            if not isinstance(e, UserCodeQuotaException):
+                raise e
+
+    def test_given_valid_request_receives_response(self):
+        req = EchoRequest(
+            id=EchoHTTPCallerTestIT.options.never_exceed_quota_id,
+            payload=_PAYLOAD
+        )
+        response: EchoResponse = EchoHTTPCallerTestIT.client.call(req)
+        self.assertEqual(req.id, response.id)
+        self.assertEqual(req.payload, response.payload)
+
+    def test_given_exceeded_quota_should_raise(self):
+        req = EchoRequest(
+            id=EchoHTTPCallerTestIT.options.should_exceed_quota_id,
+            payload=_PAYLOAD
+        )
+        self.assertRaises(
+            UserCodeQuotaException,
+            lambda: EchoHTTPCallerTestIT.client.call(req)
+        )
+
+    def test_not_found_should_raise(self):
+        req = EchoRequest(
+            id='i-dont-exist-quota-id',
+            payload=_PAYLOAD
+        )
+        self.assertRaisesRegex(
+            UserCodeExecutionException,
+            "Not Found",
+            lambda: EchoHTTPCallerTestIT.client.call(req)
+        )
 
 
 if __name__ == '__main__':
-    run()
+    unittest.main(argv=sys.argv[:1])
