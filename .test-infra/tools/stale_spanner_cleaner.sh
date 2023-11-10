8dde4508076ae3563207753fea9cc454750880dd
Yi Hu <yathu@google.com>
2023-11-10 11:17:38 -0500

    Fix clean up resource workflow; fix XVR Flink (#29358)
    * Fix clean up resource workflow; gix XVR Flink
    
    * Move spanner cleaner to correct workflow
    
    * Fix k8s naming missing cases
    
    * Fix env

--- .test-infra/tools/stale_spanner_cleaner.sh
+++ .test-infra/tools/stale_spanner_cleaner.sh
@@ -16,34 +16,14 @@
 #    limitations under the License.
 #
 #    Deletes stale and old BQ datasets that are left after tests.
-#
-
 set -euo pipefail
 
-# Clean up the stale kubernetes workload of given cluster
-
 PROJECT=apache-beam-testing
-LOCATION=us-central1-a
-CLUSTER=io-datastores
 
-function should_teardown() {
-  if [[ $1 =~ ^([0-9]+)([a-z]) ]]; then
-    local time_scale=${BASH_REMATCH[1]}
-    local time_unit=${BASH_REMATCH[2]}
-    # cutoff = 8 h
-    if [ $time_unit == y ] || [ $time_unit == d ]; then
-      return 0
-    elif [ $time_unit == h ] && [ $time_scale -ge 8 ]; then
-      return 0
-    fi
-  fi
-  return 1
-}
-
-gcloud container clusters get-credentials io-datastores --zone us-central1-a --project apache-beam-testing
-
-while read NAME STATUS AGE; do
-  if [[ $NAME =~ ^beam-.+(test|-it) ]] && should_teardown $AGE; then
-    kubectl delete namespace $NAME
-  fi
-done < <( kubectl get namespaces --context=gke_${PROJECT}_${LOCATION}_${CLUSTER} )
+# Delete spanner databases older than 1 day.
+gcloud spanner databases list \
+--instance beam-test \
+--project $PROJECT \
+--filter="createTime < $(date --iso-8601=s -d '1 day ago')" \
+--format="value(name)" | \
+xargs -I{} gcloud spanner databases delete {} --instance beam-test --quiet
