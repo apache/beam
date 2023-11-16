07313cbd07db81b26657275e68c2aa2f5606090e
Danny McCormick <dannymccormick@google.com>
2023-11-16 08:47:16 -0800

    Fix jenkins precommits

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamDockerPlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamDockerPlugin.groovy
@@ -242,7 +242,7 @@ class BeamDockerPlugin implements Plugin<Project> {
       }
       // Jenkins dependencies aren't up to date enough to accept this flag.
       // Temporarily exclude until we fully move to GHA.
-      if (!project.jenkins.isCIBuild) {
+      if (!ext.project.jenkins.isCIBuild) {
         buildCommandLine.addAll('--provenance=false')
       }
     } else {
