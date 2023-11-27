ac7ea3f2056dc7bdfe3fbce613528df31ce4a482
mzitnik <mark.zitnik@clickhouse.com>
2023-11-27 14:48:35 +0200

    Adding LowCardinality to docs.

--- sdks/java/io/clickhouse/src/main/java/org/apache/beam/sdk/io/clickhouse/ClickHouseIO.java
+++ sdks/java/io/clickhouse/src/main/java/org/apache/beam/sdk/io/clickhouse/ClickHouseIO.java
@@ -112,7 +112,7 @@ import org.slf4j.LoggerFactory;
  * </table>
  *
  * Nullable row columns are supported through Nullable type in ClickHouse.
- *
+ * Adding support for <a href="https://clickhouse.com/docs/en/sql-reference/data-types/lowcardinality">LowCardinality(DataType)</a> in ClickHouse.
  * <p>Nested rows should be unnested using {@link Select#flattenedSchema()}. Type casting should be
  * done using {@link org.apache.beam.sdk.schemas.transforms.Cast} before {@link ClickHouseIO}.
  */
