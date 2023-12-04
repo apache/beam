b2028d9657570858dc688539fe6257f3d6cef91b
Ahmed Abualsaud <ahmedabualsaud@google.com>
2023-12-04 10:44:21 -0500

    lint

--- sdks/python/apache_beam/transforms/wrapper_provider_test.py
+++ sdks/python/apache_beam/transforms/wrapper_provider_test.py
@@ -16,8 +16,10 @@
 #
 
 import os
-import pytest
 import unittest
+
+import pytest
+
 import apache_beam as beam
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
