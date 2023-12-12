f371c80040c44c16e3ffafa0af2c2e9c1c5e9067
Claire McCarthy <clairemccarthy@google.com>
2023-12-12 11:54:50 -0800

    add a javadoc to takeSample now that it's public

--- runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/ExecutionStateTracker.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/metrics/ExecutionStateTracker.java
@@ -284,26 +284,41 @@ public class ExecutionStateTracker implements Comparable<ExecutionStateTracker> 
     numTransitions++;
   }
 
-  /** Return the number of transitions that have been observed by this state tracker. */
+  /**
+   * Return the number of transitions that have been observed by this state tracker.
+   */
   public long getNumTransitions() {
     return numTransitions;
   }
 
-  /** Return the time since the last transition. */
+  /**
+   * Return the time since the last transition.
+   */
   public long getMillisSinceLastTransition() {
     return millisSinceLastTransition;
   }
 
-  /** Return the number of transitions since the last sample. */
+  /**
+   * Return the number of transitions since the last sample.
+   */
   public long getTransitionsAtLastSample() {
     return transitionsAtLastSample;
   }
 
-  /** Return the time of the next lull report. */
+  /**
+   * Return the time of the next lull report.
+   */
   public long getNextLullReportMs() {
     return nextLullReportMs;
   }
 
+  /**
+   * Called periodically by the {@link ExecutionStateSampler} to report time recorded by the
+   * tracker.
+   *
+   * @param millisSinceLastSample the time since the last sample was reported. As an
+   *     approximation, all of that time should be associated with this tracker.
+   */
   public void takeSample(long millisSinceLastSample) {
     if (SAMPLING_UPDATER.compareAndSet(this, 0, 1)) {
       try {
