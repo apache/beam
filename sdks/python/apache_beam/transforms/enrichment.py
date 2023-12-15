e83bad7ece3e9c9db021fff88c22b217be8d1284
riteshghorse <riteshghorse@gmail.com>
2023-12-15 11:28:23 -0500

    add documentation

--- sdks/python/apache_beam/transforms/enrichment.py
+++ sdks/python/apache_beam/transforms/enrichment.py
@@ -15,7 +15,10 @@
 # limitations under the License.
 #
 
-from typing import TypeVar, Callable, Optional
+from typing import Callable
+from typing import Generic
+from typing import Optional
+from typing import TypeVar
 
 import apache_beam as beam
 from apache_beam.io.requestresponseio import Caller
@@ -32,6 +35,17 @@ OutputT = TypeVar('OutputT')
 
 
 def cross_join(element):
+  """cross_join performs a cross join between two beam.Row objects.
+
+    Joins the columns of the right `beam.Row` onto the left `beam.Row`.
+
+    Args:
+      element (Tuple): A tuple containing two beam.Row objects -
+        request and response.
+
+    Returns:
+      beam.Row: `beam.Row` containing the merged columns.
+    """
   right_dict = element[1].as_dict()
   left_dict = element[0].as_dict()
   for k, v in right_dict.items():
@@ -40,19 +54,34 @@ def cross_join(element):
 
 
 class EnrichmentSourceHandler(Caller):
+  """Wrapper class for :class:`apache_beam.io.requestresponseio.Caller`"""
   pass
 
 
-class HTTPSourceHandler(EnrichmentSourceHandler):
-  def __init__(self, url):
-    self._url = url
-
-  def __call__(self, *args, **kwargs):
-    pass
-
-
 class Enrichment(beam.PTransform[beam.PCollection[InputT],
-                                 beam.PCollection[OutputT]]):
+                                 beam.PCollection[OutputT]],
+                 Generic[InputT, OutputT]):
+  """A :class:`Enrichment` transform to enrich elements in a PCollection.
+
+  Uses the :class:`apache_beam.transforms.EnrichmentSourceHandler` to enrich
+  elements by joining the metadata from external source.
+
+  Processes an input :class:`~apache_beam.pvalue.PCollection` of `beam.Row` by
+  applying a :class:`apache_beam.transforms.EnrichmentSourceHandler` to each
+  element and returning the enriched :class:`~apache_beam.pvalue.PCollection`.
+
+  Args:
+    source_handler: Handles source lookup and metadata retrieval.
+      Implements the :class:`apache_beam.transforms.EnrichmentSourceHandler`
+    join_fn: A lambda function to join original element with lookup metadata.
+      Defaults to `CROSS_JOIN`.
+    timeout: (Optional) timeout for source requests. Defaults to 30 seconds.
+    should_backoff: (Optional) backoff strategy function.
+    repeater: (Optional) retry Repeater.
+    cache_reader: (Optional) CacheReader for reading cache.
+    cache_writer: (Optional) CacheWriter for writing cache.
+    throttler: (Optional) Throttler mechanism to throttle source requests.
+  """
   def __init__(
       self,
       source_handler: EnrichmentSourceHandler,
