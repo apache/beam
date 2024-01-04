5862bab9a275daaddafee911a897f4188ff1885c
cheskel <twerskycheskel@gmail.com>
2024-01-04 17:17:48 +0200

    add test without dryrun

--- sdks/python/apache_beam/runners/dataflow/dataflow_job_service_test.py
+++ sdks/python/apache_beam/runners/dataflow/dataflow_job_service_test.py
@@ -56,5 +56,26 @@ class DirectPipelineResultTest(unittest.TestCase):
       job_servicer.stop()
 
 
+@unittest.skipIf(apiclient is None, 'GCP dependencies are not installed')
+class DirectPipelineTemplateTest(unittest.TestCase):
+  def test_template(self):
+    job_servicer = local_job_service.LocalJobServicer(
+        None, beam_job_type=dataflow_job_service.DataflowBeamJob)
+    port = job_servicer.start_grpc_server(0)
+    try:
+      options = PipelineOptions(
+          runner='PortableRunner',
+          job_endpoint=f'localhost:{port}',
+          project='some_project',
+          temp_location='gs://bucket/dir',
+          region='us-central1',
+          template_location='gs://bucket/template',
+      )
+      with beam.Pipeline(options=options) as p:
+        _ = p | beam.Create([1, 2, 3]) | beam.Map(lambda x: x * x)
+    finally:
+      job_servicer.stop()
+
+
 if __name__ == '__main__':
   unittest.main()
