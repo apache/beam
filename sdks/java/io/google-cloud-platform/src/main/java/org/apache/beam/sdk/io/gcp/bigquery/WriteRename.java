ffd6d91eeccfaa80d4b390c658bd7229c6a5fb46
johnjcasey <johnjcasey@google.com>
2024-01-22 11:33:07 -0500

    spotkess

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteRename.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteRename.java
@@ -111,8 +111,8 @@ class WriteRename
                         kmsKey,
                         loadJobProjectId))
                 .withSideInputs(copyJobIdPrefixView))
-        // We apply a fusion break here to ensure that on retries, the temp table renaming won't attempt to
-        // rename a temp table that was previously deleted in TempTableCleanupFn
+        // We apply a fusion break here to ensure that on retries, the temp table renaming won't
+        // attempt to rename a temp table that was previously deleted in TempTableCleanupFn
         .apply(Reshuffle.viaRandomKey())
         .apply("RemoveTempTables", ParDo.of(new TempTableCleanupFn(bqServices)))
         .setCoder(TableDestinationCoder.of());
