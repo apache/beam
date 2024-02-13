c85f00f9526f461fe1dcc5d93190c3de00765c74
Jayaj Poudel <jayajp@google.com>
2024-02-13 00:24:32 +0000

    Update unit tests

--- runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/streaming/StageInfoTest.java
+++ runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/streaming/StageInfoTest.java
@@ -22,12 +22,17 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.apache.beam.runners.dataflow.worker.StreamingStepMetricsContainer;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQuerySinkMetrics;
+import org.apache.beam.sdk.metrics.MetricName;
 import org.apache.beam.sdk.metrics.MetricsEnvironment;
+import org.apache.beam.sdk.util.HistogramData;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class StageInfoTest {
   @Test
-  public void testTranslateKnownPerWorkerCounters() throws Exception {
+  public void testTranslateKnownPerWorkerCounters_validCounters() throws Exception {
     StageInfo stageInfo = StageInfo.create("user_name", "system_name");
     StreamingStepMetricsContainer metricsContainer =
         stageInfo.metricsContainerRegistry().getContainer("s1");
@@ -38,5 +43,28 @@ public class StageInfoTest {
     stageInfo.extractPerWorkerMetricValues();
 
     assertThat(stageInfo.throttledMsecs().getAggregate(), equalTo(100L));
+  }
+
+  /**
+   * Test the scenario where there is a metric with a known metric name - {@code ThrottledTime} -
+   * that is not a counter. {@code translateKnownPerWorkerCounters} should handle this gracefully.
+   */
+  @Test
+  public void testTranslateKnownPerWorkerCounters_malformedCounters() throws Exception {
+    StageInfo stageInfo = StageInfo.create("user_name", "system_name");
+    StreamingStepMetricsContainer metricsContainer =
+        stageInfo.metricsContainerRegistry().getContainer("s1");
+    MetricsEnvironment.setCurrentContainer(metricsContainer);
+    StreamingStepMetricsContainer.setEnablePerWorkerMetrics(true);
+
+    MetricName name =
+        BigQuerySinkMetrics.throttledTimeCounter(BigQuerySinkMetrics.RpcMethod.APPEND_ROWS)
+            .getName();
+
+    HistogramData.BucketType linearBuckets = HistogramData.LinearBuckets.of(0, 10.0, 10);
+    metricsContainer.getPerWorkerHistogram(name, linearBuckets).update(10.0);
+
+    stageInfo.extractPerWorkerMetricValues();
+    assertThat(stageInfo.throttledMsecs().getAggregate(), equalTo(0L));
   }
 }
