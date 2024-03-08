87835615261e2ecc1ea625c1aaa29183e5228be3
Jeffrey Kinard <jeff@thekinards.com>
2024-03-07 21:11:54 -0500

    fix wordcount test
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>

--- sdks/python/apache_beam/yaml/examples/examples_test.py
+++ sdks/python/apache_beam/yaml/examples/examples_test.py
@@ -19,42 +19,42 @@
 import glob
 import logging
 import os
+import random
 import unittest
 from typing import Callable
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Union
 from unittest import mock
 
 import yaml
-from hamcrest.core import assert_that as hamcrest_assert
-from hamcrest.library.collection import has_items
-from yaml import SafeLoader
 
 import apache_beam as beam
 from apache_beam import PCollection
 from apache_beam.examples.snippets.util import assert_matches_stdout
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.testing.test_pipeline import TestPipeline
-from apache_beam.testing.util import assert_that
 from apache_beam.yaml import yaml_transform
+from apache_beam.yaml.readme_test import TestEnvironment
+from apache_beam.yaml.readme_test import replace_recursive
 
 
-def check_output(expected: List[str], matcher: Callable[..., None]):
+def check_output(expected: List[str]):
   def _check_inner(actual: PCollection[str]):
     formatted_actual = actual | beam.Map(
         lambda row: str(beam.Row(**row._asdict())))
-    matcher(formatted_actual, expected)
+    assert_matches_stdout(formatted_actual, expected)
 
   return _check_inner
 
 
 def create_test_method(
     pipeline_spec_file: str,
-    custom_matcher: Optional[Callable[..., None]] = None):
+    custom_preprocessor: Optional[Callable[..., Union[Dict, List]]] = None):
   @mock.patch('apache_beam.Pipeline', TestPipeline)
   def test_yaml_example(self):
-    with open(pipeline_spec_file) as f:
+    with open(pipeline_spec_file, encoding="utf-8") as f:
       lines = f.readlines()
     expected_key = '# Expected:\n'
     if expected_key in lines:
@@ -64,21 +64,24 @@ def create_test_method(
           f"Missing '# Expected:' tag in example file '{pipeline_spec_file}'")
     for i, line in enumerate(expected):
       expected[i] = line.replace('#  ', '').replace('\n', '')
-    modified_yaml = yaml.dump(yaml.load(''.join(lines), Loader=SafeLoader))
-    matcher = assert_matches_stdout if not custom_matcher else custom_matcher
-    options = {
-        'pickle_library': 'cloudpickle',
-        'yaml_experimental_features': ['Combine']
-    }
-    with beam.Pipeline(options=PipelineOptions(**options)) as p:
-      yaml_transform.expand_pipeline(p, modified_yaml)
-      check_output(expected, matcher)
+    pipeline_spec = yaml.load(
+        ''.join(lines), Loader=yaml_transform.SafeLineLoader)
+
+    with TestEnvironment() as env:
+      if custom_preprocessor:
+        pipeline_spec = custom_preprocessor(pipeline_spec, expected, env)
+      with beam.Pipeline(options=PipelineOptions(
+          pickle_library='cloudpickle',
+          **yaml_transform.SafeLineLoader.strip_metadata(pipeline_spec.get(
+              'options', {})))) as p:
+        actual = yaml_transform.expand_pipeline(p, pipeline_spec)
+        check_output(expected)(actual)
 
   return test_yaml_example
 
 
 class YamlExamplesTestSuite:
-  _custom_matchers: Dict[str, Callable[..., None]] = {}
+  _test_preprocessor: Dict[str, Callable[..., Union[Dict, List]]] = {}
 
   def __init__(self, name: str, path: str):
     self._test_suite = self.create_test_suite(name, path)
@@ -92,29 +95,46 @@ class YamlExamplesTestSuite:
     if not files and os.path.exists(path) and os.path.isfile(path):
       files = [path]
     for file in files:
-      test_name = f'test_{file.split("/")[-1].replace(".", "_")}'
-      custom_matcher = cls._custom_matchers.get(test_name, None)
-      yield test_name, create_test_method(file, custom_matcher)
+      test_name = f'test_{file.split(os.sep)[-1].replace(".", "_")}'
+      custom_preprocessor = cls._test_preprocessor.get(test_name, None)
+      yield test_name, create_test_method(file, custom_preprocessor)
 
   @classmethod
   def create_test_suite(cls, name: str, path: str):
     return type(name, (unittest.TestCase, ), dict(cls.parse_test_methods(path)))
 
   @classmethod
-  def register_custom_matcher(cls, test_name: str):
-    def apply(matcher):
-      cls._custom_matchers[test_name] = matcher
-      return matcher
+  def register_test_preprocessor(cls, test_name: str):
+    def apply(preprocessor):
+      cls._test_preprocessor[test_name] = preprocessor
+      return preprocessor
 
     return apply
 
 
-@YamlExamplesTestSuite.register_custom_matcher('test_wordcount_minimal_yaml')
-def _at_least_matcher(actual: PCollection[str], expected: List[str]):
-  def _matches(value):
-    hamcrest_assert(value, has_items(*expected))
+@YamlExamplesTestSuite.register_test_preprocessor('test_wordcount_minimal_yaml')
+def _wordcount_test_preprocessor(
+    test_spec: str, expected: List[str], env: TestEnvironment):
+  all_words = []
+  for element in expected:
+    word = element.split('=')[1].split(',')[0].replace("'", '')
+    count = int(element.split('=')[2].replace(')', ''))
+    all_words += [word] * count
+  random.shuffle(all_words)
 
-  return assert_that(actual, _matches)
+  lines = []
+  while all_words:
+    line_length = random.randint(1, min(10, len(all_words)))
+    line = " ".join(
+        all_words.pop(random.randrange(len(all_words)))
+        for _ in range(line_length))
+    lines.append(line)
+
+  return replace_recursive(
+      test_spec,
+      'ReadFromText',
+      'path',
+      env.input_file('kinglear.txt', '\n'.join(lines)))
 
 
 YAML_DOCS_DIR = os.path.join(os.path.dirname(__file__))
