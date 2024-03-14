df6d6b8eaa770e236ba49a1b616fe3bd93a32189
martin trieu <martinkt@google.com>
2024-03-14 02:16:39 -0700

    Fix flaky streaming dataflow tests (#30572)
    * remove waiting/sleeping arbitratily in tests since it is leading to flakiness

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/windmill/state/WindmillStateCache.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/windmill/state/WindmillStateCache.java
@@ -42,7 +42,6 @@ import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.base.Precondit
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.cache.Cache;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.cache.CacheBuilder;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.cache.CacheStats;
-import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.cache.Weigher;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.collect.MapMaker;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -56,6 +55,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  * thread at a time, so this is safe.
  */
 public class WindmillStateCache implements StatusDataProvider {
+  private static final int STATE_CACHE_CONCURRENCY_LEVEL = 4;
   // Convert Megabytes to bytes
   private static final long MEGABYTES = 1024 * 1024;
   // Estimate of overhead per StateId.
@@ -72,20 +72,28 @@ public class WindmillStateCache implements StatusDataProvider {
   // Contains the current valid ForKey object. Entries in the cache are keyed by ForKey with pointer
   // equality so entries may be invalidated by creating a new key object, rendering the previous
   // entries inaccessible. They will be evicted through normal cache operation.
-  private final ConcurrentMap<WindmillComputationKey, ForKey> keyIndex =
-      new MapMaker().weakValues().concurrencyLevel(4).makeMap();
+  private final ConcurrentMap<WindmillComputationKey, ForKey> keyIndex;
   private final long workerCacheBytes; // Copy workerCacheMb and convert to bytes.
 
-  public WindmillStateCache(long workerCacheMb) {
-    final Weigher<Weighted, Weighted> weigher = Weighers.weightedKeysAndValues();
-    workerCacheBytes = workerCacheMb * MEGABYTES;
-    stateCache =
+  private WindmillStateCache(
+      long workerCacheMb,
+      ConcurrentMap<WindmillComputationKey, ForKey> keyIndex,
+      Cache<StateId, StateCacheEntry> stateCache) {
+    this.workerCacheBytes = workerCacheMb * MEGABYTES;
+    this.stateCache = stateCache;
+    this.keyIndex = keyIndex;
+  }
+
+  public static WindmillStateCache ofSizeMbs(long workerCacheMb) {
+    return new WindmillStateCache(
+        workerCacheMb,
+        new MapMaker().weakValues().concurrencyLevel(STATE_CACHE_CONCURRENCY_LEVEL).makeMap(),
         CacheBuilder.newBuilder()
-            .maximumWeight(workerCacheBytes)
+            .maximumWeight(workerCacheMb * MEGABYTES)
             .recordStats()
-            .weigher(weigher)
-            .concurrencyLevel(4)
-            .build();
+            .weigher(Weighers.weightedKeysAndValues())
+            .concurrencyLevel(STATE_CACHE_CONCURRENCY_LEVEL)
+            .build());
   }
 
   private EntryStats calculateEntryStats() {
