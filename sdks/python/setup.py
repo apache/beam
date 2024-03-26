518b4e84f195d5075991a5311a6c4ab874128f3a
riteshghorse <riteshghorse@gmail.com>
2024-03-26 12:01:22 -0400

    add upper bound

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
           'pydot>=1.2.0,<2',
           'python-dateutil>=2.8.0,<3',
           'pytz>=2018.3',
-          'redis>=5.0.0',
+          'redis>=5.0.0,<6',
           'regex>=2020.6.8',
           'requests>=2.24.0,<3.0.0',
           'typing-extensions>=3.7.0',
