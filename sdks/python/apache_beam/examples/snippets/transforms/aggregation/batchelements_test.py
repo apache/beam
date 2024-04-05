5888aa447c32ba8ff6873fbf2a07f42d560e78d5
Joey Tran <joey.tran@schrodinger.com>
2024-04-05 15:20:20 -0400

    rework asserts for batchelements_test.py

--- sdks/python/apache_beam/examples/snippets/transforms/aggregation/batchelements_test.py
+++ sdks/python/apache_beam/examples/snippets/transforms/aggregation/batchelements_test.py
@@ -19,6 +19,8 @@
 import unittest
 
 import mock
+
+import apache_beam as beam
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that, equal_to
 
@@ -26,8 +28,20 @@ from . import batchelements
 
 
 def check_batches(actual):
-  expected = [['🍓', '🥕', '🍆'], ['🍅', '🥕', '🍅'], ['🌽', '🥕', '🍅', '🍆']]
-  assert_that(actual, equal_to(expected))
+  # How the elements are grouped is not guaranteed, so we just
+  # check that all elements are lists and then count the elements
+  # to make sure none are lost.
+  all_elements_are_lists = (actual
+                            | 'Check type' >> beam.Map(lambda x: isinstance(x, list))
+                            | 'All elements are lists' >> beam.CombineGlobally(all))
+  assert_that(all_elements_are_lists, equal_to([True]))
+
+  assert_that(actual
+              | beam.FlatMap(lambda x: x)
+              | 'Count' >> beam.combiners.Count.PerElement(),
+              equal_to([('🍓', 1), ('🥕', 3), ('🍆', 2), ('🍅', 3), ('🌽', 1)]),
+              label='Check counts')
+
 
 
 def identity(x):
@@ -37,8 +51,8 @@ def identity(x):
 @mock.patch('apache_beam.Pipeline', TestPipeline)
 # pylint: disable=line-too-long
 @mock.patch(
-    'apache_beam.examples.snippets.transforms.aggregation.batchelements.print',
-    identity)
+  'apache_beam.examples.snippets.transforms.aggregation.batchelements.print',
+  identity)
 # pylint: enable=line-too-long
 class BatchElementsTest(unittest.TestCase):
   def test_batchelements(self):
