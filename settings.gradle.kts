54673996c9bf2ee076b04833bbae2729d6cebbaf
Ahmed Abualsaud <65791736+ahmedabu98@users.noreply.github.com>
2024-04-08 06:55:17 -0400

    [Java] ManagedIO  (#30808)
    * managed api for java
    
    * yaml utils

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -353,3 +353,5 @@ include("sdks:java:io:kafka:kafka-100")
 findProject(":sdks:java:io:kafka:kafka-100")?.name = "kafka-100"
 include("sdks:java:io:kafka:kafka-01103")
 findProject(":sdks:java:io:kafka:kafka-01103")?.name = "kafka-01103"
+include("sdks:java:managed")
+findProject(":sdks:java:managed")?.name = "managed"
