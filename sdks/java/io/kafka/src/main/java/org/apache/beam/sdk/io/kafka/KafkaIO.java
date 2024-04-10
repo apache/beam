e762571472606038f39dd439f162b468d58f13c7
xianhualiu <122747878+xianhualiu@users.noreply.github.com>
2024-04-10 09:25:54 -0400

    Update sdks/java/io/kafka/src/main/java/org/apache/beam/sdk/io/kafka/KafkaIO.java
    Co-authored-by: Jonathan Sabbagh <108473809+jbsabbagh@users.noreply.github.com>

--- sdks/java/io/kafka/src/main/java/org/apache/beam/sdk/io/kafka/KafkaIO.java
+++ sdks/java/io/kafka/src/main/java/org/apache/beam/sdk/io/kafka/KafkaIO.java
@@ -2402,6 +2402,8 @@ public class KafkaIO {
 
     /**
      * Sets the timeout time for Kafka consumer polling request in the {@link ReadFromKafkaDoFn}.
+     * A lower timeout optimizes for latency.
+     * Increase the timeout if the consumer is not fetching enough (or any) records. 
      * The default is 2 seconds.
      */
     public ReadSourceDescriptors<K, V> withConsumerPollingTimeout(@Nullable Duration duration) {
