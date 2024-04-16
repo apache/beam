abe854a5f99b15200e93181d76c2ae82d70e605a
Arvind Ram Anantharam <arvindram03@gmail.com>
2024-04-15 17:03:14 -0700

    Adding full thread dump for per message lull tracking

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StackTraceUtil.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StackTraceUtil.java
@@ -17,15 +17,23 @@
  */
 package org.apache.beam.runners.dataflow.worker;
 
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import org.apache.beam.runners.core.SimpleDoFnRunner;
+import org.apache.beam.runners.dataflow.worker.logging.DataflowWorkerLoggingHandler;
+import org.apache.beam.runners.dataflow.worker.logging.DataflowWorkerLoggingInitializer;
 import org.apache.beam.sdk.annotations.Internal;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Utility methods to print the stack traces of all the threads. */
 @Internal
 public final class StackTraceUtil {
   private static final ImmutableSet<String> FRAMEWORK_CLASSES =
       ImmutableSet.of(SimpleDoFnRunner.class.getName(), DoFnInstanceManagers.class.getName());
+  private static final Logger LOG = LoggerFactory.getLogger(StackTraceUtil.class);
 
   public static String getStackTraceForLullMessage(StackTraceElement[] stackTrace) {
     StringBuilder message = new StringBuilder();
@@ -36,6 +44,22 @@ public final class StackTraceUtil {
       message.append("  at ").append(e).append("\n");
     }
     return message.toString();
+  }
+
+  public static void logAllStackTraces() {
+    DataflowWorkerLoggingHandler dataflowLoggingHandler =
+        DataflowWorkerLoggingInitializer.getLoggingHandler();
+    Map<Thread, StackTraceElement[]> threadSet = Thread.getAllStackTraces();
+    for (Map.Entry<Thread, StackTraceElement[]> entry : threadSet.entrySet()) {
+      Thread thread = entry.getKey();
+      StackTraceElement[] stackTrace = entry.getValue();
+      StringBuilder message = new StringBuilder();
+      message.append(thread.toString()).append(":\n");
+      message.append(getStackTraceForLullMessage(stackTrace));
+      LogRecord logRecord = new LogRecord(Level.INFO, message.toString());
+      logRecord.setLoggerName(StackTraceUtil.LOG.getName());
+      dataflowLoggingHandler.publish(logRecord);
+    }
   }
 
   private StackTraceUtil() {}
