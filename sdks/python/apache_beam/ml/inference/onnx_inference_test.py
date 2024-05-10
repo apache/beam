3a61b22a5e5b30507edbe914f41da41a9e33a3c9
Valentyn Tymofieiev <valentyn@google.com>
2024-05-10 16:57:43 -0700

    fixup

--- sdks/python/apache_beam/ml/inference/onnx_inference_test.py
+++ sdks/python/apache_beam/ml/inference/onnx_inference_test.py
@@ -32,7 +32,8 @@ from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 
 if True:  # appease lint
-  raise unittest.SkipTest('TODO: fix https://github.com/apache/beam/issues/31254')
+  raise unittest.SkipTest(
+      'TODO: fix https://github.com/apache/beam/issues/31254')
 
 # Protect against environments where onnx and pytorch library is not available.
 # pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
