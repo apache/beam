53148940f2dafe4e09dbfa8aa707c20b443f3989
pndesai <pndesai@google.com>
2024-05-28 21:22:49 +0000

    change it consuming received data with capability

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/construction/Environments.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/construction/Environments.java
@@ -487,7 +487,7 @@ public class Environments {
     capabilities.add(BeamUrns.getUrn(SplittableParDoComponents.TRUNCATE_SIZED_RESTRICTION));
     capabilities.add(BeamUrns.getUrn(Primitives.TO_STRING));
     capabilities.add(BeamUrns.getUrn(StandardProtocols.Enum.DATA_SAMPLING));
-    capabilities.add(BeamUrns.getUrn(StandardProtocols.Enum.SDK_DATA_CHANNEL_STATUS_SIGNAL));
+    capabilities.add(BeamUrns.getUrn(StandardProtocols.Enum.SDK_CONSUMING_RECEIVED_DATA));
     return capabilities.build();
   }
 
