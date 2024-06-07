a8205d789a8f9da975d051ec90c2bc6a3ca7b1b6
Robert Bradshaw <robertwb@gmail.com>
2024-06-07 11:35:55 -0700

    Allow flags to be used as jinja template variables.

--- sdks/python/apache_beam/yaml/main.py
+++ sdks/python/apache_beam/yaml/main.py
@@ -29,6 +29,45 @@ from apache_beam.typehints.schemas import MillisInstant
 from apache_beam.yaml import yaml_transform
 
 
+def _preparse_jinja_flags(argv):
+  """Promotes any flags to --jinja_variables based on --jinja_variable_flags.
+
+  This is to facilitate tools (such as dataflow templates) that must pass
+  options as un-nested flags.
+  """
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--jinja_variable_flags',
+      default=[],
+      type=lambda s: s.split(','),
+      help='A list of flag names that should be used as jinja variables.')
+  parser.add_argument(
+      '--jinja_variables',
+      default={},
+      type=json.loads,
+      help='A json dict of variables used when invoking the jinja preprocessor '
+      'on the provided yaml pipeline.')
+  jinja_args, other_args = parser.parse_known_args(argv)
+  if not jinja_args.jinja_variable_flags:
+    return argv
+
+  jinja_variable_parser = argparse.ArgumentParser()
+  for flag_name in jinja_args.jinja_variable_flags:
+    jinja_variable_parser.add_argument('--' + flag_name)
+  jinja_flag_variables, pipeline_args = jinja_variable_parser.parse_known_args(
+      other_args)
+  jinja_args.jinja_variables.update(
+      **
+      {k: v
+       for (k, v) in vars(jinja_flag_variables).items() if v is not None})
+  if jinja_args.jinja_variables:
+    pipeline_args = pipeline_args + [
+        '--jinja_variables=' + json.dumps(jinja_args.jinja_variables)
+    ]
+
+  return pipeline_args
+
+
 def _configure_parser(argv):
   parser = argparse.ArgumentParser()
   parser.add_argument(
@@ -90,6 +129,7 @@ def _fix_xlang_instant_coding():
 
 
 def run(argv=None):
+  argv = _preparse_jinja_flags(argv)
   known_args, pipeline_args = _configure_parser(argv)
   pipeline_template = _pipeline_spec_from_args(known_args)
   pipeline_yaml = (  # keep formatting
