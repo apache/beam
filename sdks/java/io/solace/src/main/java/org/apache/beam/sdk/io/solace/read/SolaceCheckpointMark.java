bbade93333e66ad1b30765000a6430c3dd913db9
Bartosz Zablocki <bzablocki@google.com>
2024-06-17 21:47:09 +0200

    Make SolaceCheckpointMark visible for testing

--- sdks/java/io/solace/src/main/java/org/apache/beam/sdk/io/solace/read/SolaceCheckpointMark.java
+++ sdks/java/io/solace/src/main/java/org/apache/beam/sdk/io/solace/read/SolaceCheckpointMark.java
@@ -27,15 +27,17 @@ import org.apache.beam.sdk.annotations.Internal;
 import org.apache.beam.sdk.coders.DefaultCoder;
 import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
 import org.apache.beam.sdk.io.UnboundedSource;
+import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.annotations.VisibleForTesting;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * Checkpoint for an unbounded Solace source. Consists of the Solace messages waiting to be
  * acknowledged.
  */
-@Internal
 @DefaultCoder(AvroCoder.class)
-class SolaceCheckpointMark implements UnboundedSource.CheckpointMark {
+@Internal
+@VisibleForTesting
+public class SolaceCheckpointMark implements UnboundedSource.CheckpointMark {
   private transient AtomicBoolean activeReader;
   // BytesXMLMessage is not serializable so if a job restarts from the checkpoint, we cannot retry
   // these messages here. We relay on Solace's retry mechanism.
