43d44ece696d42b0c14b04faf47d72c86058c81b
Yi Hu <yathu@google.com>
2024-06-18 13:11:56 -0400

    Fix test error message

--- sdks/java/container/boot_test.go
+++ sdks/java/container/boot_test.go
@@ -84,9 +84,9 @@ func TestHeapSizeLimit(t *testing.T) {
 		t.Errorf("HeapSizeLimit(0). Actual (%d). want 1 GB", lim)
 	}
 	if lim := HeapSizeLimit(2 << 30); lim != (2 << 30) * 7 / 10 {
-		t.Errorf("HeapSizeLimit(0). Actual (%d). want 700 MB", lim)
+		t.Errorf("HeapSizeLimit(2 GB). Actual (%d). want 1.4 GB", lim)
 	}
 	if lim := HeapSizeLimit(200 << 30); lim != (200 - 32) << 30 {
-		t.Errorf("HeapSizeLimit(0). Actual (%d). want 168 GB", lim)
+		t.Errorf("HeapSizeLimit(200 GB). Actual (%d). want 168 GB", lim)
 	}
 }
