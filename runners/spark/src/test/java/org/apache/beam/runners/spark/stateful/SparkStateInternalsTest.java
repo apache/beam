5ddcd177e6be5bee74e85a30b87783cd7ffbb30d
twosom <two_somang@icloud.com>
2024-06-27 07:45:00 +0900

    update comment on SparkStateInternalsTest

--- runners/spark/src/test/java/org/apache/beam/runners/spark/stateful/SparkStateInternalsTest.java
+++ runners/spark/src/test/java/org/apache/beam/runners/spark/stateful/SparkStateInternalsTest.java
@@ -25,7 +25,7 @@ import org.junit.runners.JUnit4;
 
 /**
  * Tests for {@link SparkStateInternals}. This is based on {@link StateInternalsTest}. Ignore set
- * and map tests.
+ * tests.
  */
 @RunWith(JUnit4.class)
 public class SparkStateInternalsTest extends StateInternalsTest {
