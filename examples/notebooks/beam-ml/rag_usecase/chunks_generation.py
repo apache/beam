89a651a0cd0936ea02696e4983ca3a5aaef3d68e
Ayush Pandey <pandey.ayu@gmail.com>
2024-07-05 18:59:42 -0600

    Adding Chunking Strategy

---
+++ examples/notebooks/beam-ml/rag_usecase/chunks_generation.py
@@ -0,0 +1,124 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import absolute_import
+
+import apache_beam as beam
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+
+import logging
+
+from apache_beam.transforms import DoFn
+from apache_beam.transforms import PTransform
+
+__all__ = [
+    'ChunksGeneration',
+]
+
+
+class ChunksGeneration(PTransform):
+    """ChunkingStrategy is a ``PTransform`` that takes a ``PCollection`` of
+    key, value tuple or 2-element array and generates different chunks for documents.
+    """
+
+    def __init__(
+            self,
+            chunk_size: int,
+            chunk_overlap: int,
+            chunking_strategy: str,
+
+    ):
+        """
+
+        Args:
+        host (str, ValueProvider): The redis host
+        port (int, ValueProvider): The redis port
+        command : command to be executed with redis client
+        batch_size(int, ValueProvider): Number of key, values pairs to write at once
+        embedded_columns (list, ValueProvider): list of column whose embedding needs to be generated
+
+        Returns:
+        :class:`~apache_beam.transforms.ptransform.PTransform`
+
+        """
+
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.chunking_strategy = chunking_strategy
+
+    def expand(self, pcoll):
+        return pcoll \
+               | "Generate text chunks" >> beam.ParDo(_GenerateChunksFn(self.chunk_size,
+                                                                        self.chunk_overlap,
+                                                                        self.chunking_strategy))
+
+
+class _GenerateChunksFn(DoFn):
+    """Abstract class that takes in ptransform
+    and generate chunks.
+    """
+
+    def __init__(
+            self,
+            chunk_size: int,
+            chunk_overlap: int,
+            chunking_strategy: str
+    ):
+
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.chunking_strategy = chunking_strategy
+
+    def process(self, element, *args, **kwargs):
+
+        # For split by character
+        if self.chunking_strategy == 'split by character':
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=self.chunk_size,
+                chunk_overlap=self.chunk_overlap,
+                length_function=len,
+                is_separator_regex=False,
+            )
+
+        # For  reversesplit by character
+        if self.chunking_strategy == 'reverse split by character':
+            text_splitter = CharacterTextSplitter(
+                chunk_size=self.chunk_size,
+                chunk_overlap=self.chunk_overlap,
+                length_function=len,
+                is_separator_regex=False,
+            )
+
+        # For split by tokens
+        if self.chunking_strategy == 'split by tokens':
+            text_splitter = SentenceTransformersTokenTextSplitter(
+                chunk_overlap=self.chunk_overlap,
+                model_name='all-MiniLM-L6-v2'
+            )
+
+        texts = text_splitter.split_text(element['text'])[:]
+
+        element_copy = element.copy()
+        del element_copy['text']
+        for i, section in enumerate(texts):
+            element_copy['text'] = section
+            element_copy['section_id'] = i + 1
+            # print(f'This is element before going in redis {element_copy}')
+            yield element_copy
+
+
