2c7f3c2905a8fa9d61c21565ef3f800ccf08aa94
lostluck <13907733+lostluck@users.noreply.github.com>
2024-08-06 11:32:00 -0700

    Formatter Config difference.

--- sdks/python/apache_beam/runners/portability/prism_runner_test.py
+++ sdks/python/apache_beam/runners/portability/prism_runner_test.py
@@ -242,8 +242,8 @@ class PrismRunnerTest(portable_runner_test.PortableRunnerTest):
                 timestamp_policy=ReadFromKafka.create_time_policy,
                 expansion_service=self.get_expansion_service()))
     self.assertTrue(
-        'No resolvable bootstrap urls given in bootstrap.servers'
-        in str(ctx.exception),
+        'No resolvable bootstrap urls given in bootstrap.servers' in str(
+            ctx.exception),
         'Expected to fail due to invalid bootstrap.servers, but '
         'failed due to:\n%s' % str(ctx.exception))
 
