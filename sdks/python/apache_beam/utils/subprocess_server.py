2eb7a77f9df540227cede4640220bb275b62ec40
Yi Hu <yathu@google.com>
2024-08-21 13:58:44 -0400

    address comment

--- sdks/python/apache_beam/utils/subprocess_server.py
+++ sdks/python/apache_beam/utils/subprocess_server.py
@@ -385,8 +385,8 @@ class JavaJarServer(SubprocessServer):
           os.rename(cached_jar + '.tmp', cached_jar)
         except URLError as e:
           raise RuntimeError(
-              'Unable to fetch remote job server jar at %s: %s. If no Internet'
-              ' access at runtime, stage the jar at %s' % (url, e, cache_dir))
+              f'Unable to fetch remote job server jar at {url}: {e}. If no '
+              f'Internet access at runtime, stage the jar at {cached_jar}')
       return cached_jar
 
   @classmethod
