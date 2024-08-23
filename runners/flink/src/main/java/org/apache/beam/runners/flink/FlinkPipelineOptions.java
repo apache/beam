d0e7cb37cd5f4fa634d8d929f27974e49535460f
jto <boudhevil@gmail.com>
2024-08-23 17:35:25 +0200

    [Flink] lower default max bundle size in batch mode

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkPipelineOptions.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkPipelineOptions.java
@@ -246,7 +246,7 @@ public interface FlinkPipelineOptions
       if (options.as(StreamingOptions.class).isStreaming()) {
         return 1000L;
       } else {
-        return 1000000L;
+        return 500000L;
       }
     }
   }
