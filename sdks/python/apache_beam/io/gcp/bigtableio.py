28c25242269e53c686c91ef89aa51ef75386d95a
Yi Hu <yathu@google.com>
2024-09-11 12:57:01 -0400

    Add Lineage metrics to Python PubsubIO, BigtableIO, FileIO

--- sdks/python/apache_beam/io/gcp/bigtableio.py
+++ sdks/python/apache_beam/io/gcp/bigtableio.py
@@ -47,6 +47,7 @@ from apache_beam.internal.metrics.metric import ServiceCallMetric
 from apache_beam.io.gcp import resource_identifiers
 from apache_beam.metrics import Metrics
 from apache_beam.metrics import monitoring_infos
+from apache_beam.metrics.metric import Lineage
 from apache_beam.transforms import PTransform
 from apache_beam.transforms.display import DisplayDataItem
 from apache_beam.transforms.external import BeamJarExpansionService
@@ -162,6 +163,12 @@ class _BigTableWriteFn(beam.DoFn):
     if self.batcher:
       self.batcher.close()
       self.batcher = None
+      # Report Lineage metrics on write
+      Lineage.sinks().add(
+          'bigtable',
+          self.beam_options['project_id'],
+          self.beam_options['instance_id'],
+          self.beam_options['table_id'])
 
   def display_data(self):
     return {
