ed84acb2967cf4ffa30a41bd53398ed770271f47
Danny McCormick <dannymccormick@google.com>
2024-09-12 13:41:15 -0400

    Bump spanner max bound back to <4 (#32411)

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -441,7 +441,7 @@ if __name__ == '__main__':
               'google-cloud-bigquery-storage>=2.6.3,<3',
               'google-cloud-core>=2.0.0,<3',
               'google-cloud-bigtable>=2.19.0,<3',
-              'google-cloud-spanner>=3.0.0,<3.48',
+              'google-cloud-spanner>=3.0.0,<4',
               # GCP Packages required by ML functionality
               'google-cloud-dlp>=3.0.0,<4',
               'google-cloud-language>=2.0,<3',
