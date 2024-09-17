cfe3ec7559336e40ad21d696e30722363963254a
Shunping Huang <shunping@google.com>
2024-09-16 22:17:42 -0400

    Parameterize existing tests for the new pipeline options.

--- sdks/python/apache_beam/io/gcp/gcsio_integration_test.py
+++ sdks/python/apache_beam/io/gcp/gcsio_integration_test.py
@@ -33,6 +33,7 @@ import unittest
 import uuid
 
 import mock
+from parameterized import parameterized_class
 import pytest
 
 from apache_beam.io.filesystems import FileSystems
@@ -51,6 +52,8 @@ except ImportError:
 
 
 @unittest.skipIf(gcsio is None, 'GCP dependencies are not installed')
+@parameterized_class(('no_gcsio_throttling_counter', 'enable_gcsio_blob_generation'),
+  [(False, False), (False, True), (True, False), (True, True)])
 class GcsIOIntegrationTest(unittest.TestCase):
 
   INPUT_FILE = 'gs://dataflow-samples/shakespeare/kinglear.txt'
@@ -92,14 +95,41 @@ class GcsIOIntegrationTest(unittest.TestCase):
 
   @pytest.mark.it_postcommit
   def test_copy(self):
+    self.gcsio = gcsio.GcsIO(pipeline_options={
+      "no_gcsio_throttling_counter": self.no_gcsio_throttling_counter,
+      "enable_gcsio_blob_generation": self.enable_gcsio_blob_generation})
     src = self.INPUT_FILE
     dest = self.gcs_tempdir + '/test_copy'
 
     self.gcsio.copy(src, dest)
     self._verify_copy(src, dest)
 
+    unknown_src = self.test_pipeline.get_option('temp_location') + \
+        '/gcs_it-' + str(uuid.uuid4())
+    with self.assertRaises(NotFound):
+      self.gcsio.copy(unknown_src, dest)
+
+  @pytest.mark.it_postcommit
+  def test_copy_and_delete(self):
+    self.gcsio = gcsio.GcsIO(pipeline_options={
+      "no_gcsio_throttling_counter": self.no_gcsio_throttling_counter,
+      "enable_gcsio_blob_generation": self.enable_gcsio_blob_generation})
+    src = self.INPUT_FILE
+    dest = self.gcs_tempdir + '/test_copy'
+
+    self.gcsio.copy(src, dest)
+    self._verify_copy(src, dest)
+
+    self.gcsio.delete(dest)
+
+    # no exception if we delete an nonexistent file.
+    self.gcsio.delete(dest)
+
   @pytest.mark.it_postcommit
   def test_batch_copy_and_delete(self):
+    self.gcsio = gcsio.GcsIO(pipeline_options={
+    "no_gcsio_throttling_counter": self.no_gcsio_throttling_counter,
+    "enable_gcsio_blob_generation": self.enable_gcsio_blob_generation})
     num_copies = 10
     srcs = [self.INPUT_FILE] * num_copies
     dests = [
