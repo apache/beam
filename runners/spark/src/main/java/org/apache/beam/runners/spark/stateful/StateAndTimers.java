23e3e1861c859e2fd3015c45ab0c583ae60ee8e9
twosom <two_somang@icloud.com>
2024-11-28 12:23:19 +0900

    chore : add license header in StateAndTimers - refactor : remove @Nullable annotation in static factory constructor

--- runners/spark/src/main/java/org/apache/beam/runners/spark/stateful/StateAndTimers.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/stateful/StateAndTimers.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.runners.spark.stateful;
 
 import com.google.auto.value.AutoValue;
@@ -11,10 +28,10 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public abstract class StateAndTimers implements Serializable {
   public abstract Table<String, String, byte[]> getState();
 
-  public @Nullable abstract Collection<byte[]> getTimers();
+  public abstract @Nullable Collection<byte[]> getTimers();
 
   public static StateAndTimers of(
-      final Table<String, String, byte[]> state, @Nullable final Collection<byte[]> timers) {
+      final Table<String, String, byte[]> state, final Collection<byte[]> timers) {
     return new AutoValue_StateAndTimers.Builder().setState(state).setTimers(timers).build();
   }
 
