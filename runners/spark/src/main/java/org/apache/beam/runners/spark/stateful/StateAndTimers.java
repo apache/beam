b9747075fac810417ae859dc56ed92974d0894cf
twosom <two_somang@icloud.com>
2024-12-01 23:13:44 +0900

    chore : remove @Nullable annotation

--- runners/spark/src/main/java/org/apache/beam/runners/spark/stateful/StateAndTimers.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/stateful/StateAndTimers.java
@@ -21,14 +21,13 @@ import com.google.auto.value.AutoValue;
 import java.io.Serializable;
 import java.util.Collection;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.collect.Table;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 /** State and Timers wrapper. */
 @AutoValue
 public abstract class StateAndTimers implements Serializable {
   public abstract Table<String, String, byte[]> getState();
 
-  public abstract @Nullable Collection<byte[]> getTimers();
+  public abstract Collection<byte[]> getTimers();
 
   public static StateAndTimers of(
       final Table<String, String, byte[]> state, final Collection<byte[]> timers) {
