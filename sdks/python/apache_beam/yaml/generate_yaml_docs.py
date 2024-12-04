a9a82a1c1ae7a702f25dc98fd3aaca16f50209e3
Jeff Kinard <jeff@thekinards.com>
2024-12-04 10:28:46 -0500

    [yaml] remove PubSubLite from API docs (#33184)
    * [yaml] remove PubSubLite from API docs
    
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>
    
    * add deprecation note
    
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>
    
    ---------
    
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>

--- sdks/python/apache_beam/yaml/generate_yaml_docs.py
+++ sdks/python/apache_beam/yaml/generate_yaml_docs.py
@@ -189,11 +189,8 @@ def io_grouping_key(transform_name):
     return 0, transform_name
 
 
-SKIP = [
-    'Combine',
-    'Filter',
-    'MapToFields',
-]
+# Exclude providers
+SKIP = {}
 
 
 def transform_docs(transform_base, transforms, providers, extra_docs=''):
@@ -236,7 +233,8 @@ def main():
   options = parser.parse_args()
   include = re.compile(options.include).match
   exclude = (
-      re.compile(options.exclude).match if options.exclude else lambda _: False)
+      re.compile(options.exclude).match
+      if options.exclude else lambda x: x in SKIP)
 
   with subprocess_server.SubprocessServer.cache_subprocesses():
     json_config_schemas = []
