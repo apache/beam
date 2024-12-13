1a2e9daad23b69f4cf6e3bb7d51548b26634c36a
Reeba Qureshi <64488642+reeba212@users.noreply.github.com>
2024-12-13 23:36:55 +0530

    Update bigquery_tools.py

--- sdks/python/apache_beam/io/gcp/bigquery_tools.py
+++ sdks/python/apache_beam/io/gcp/bigquery_tools.py
@@ -356,8 +356,7 @@ class BigQueryWrapper(object):
       self,
       client=None,
       temp_dataset_id=None,
-      temp_table_ref=None,
-      project_id=None):
+      temp_table_ref=None):
     self.client = client or BigQueryWrapper._bigquery_client(PipelineOptions())
     self.gcp_bq_client = client or gcp_bigquery.Client(
         client_info=ClientInfo(
