cd136c90db85f541c936de5d1288e142f3c767e5
Reeba Qureshi <64488642+reeba212@users.noreply.github.com>
2024-12-13 23:53:02 +0530

    Update bigquery_tools.py

--- sdks/python/apache_beam/io/gcp/bigquery_tools.py
+++ sdks/python/apache_beam/io/gcp/bigquery_tools.py
@@ -352,11 +352,7 @@ class BigQueryWrapper(object):
 
   HISTOGRAM_METRIC_LOGGER = MetricLogger()
 
-  def __init__(
-      self,
-      client=None,
-      temp_dataset_id=None,
-      temp_table_ref=None):
+  def __init__(self, client=None, temp_dataset_id=None, temp_table_ref=None):
     self.client = client or BigQueryWrapper._bigquery_client(PipelineOptions())
     self.gcp_bq_client = client or gcp_bigquery.Client(
         client_info=ClientInfo(
