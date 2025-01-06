32fbbede72e8da20b2ba06eef1f74733f6086d45
Jack McCluskey <thejackmccluskey@gmail.com>
2025-01-06 10:35:04 -0500

    fix clobbered import from merge

--- sdks/python/apache_beam/typehints/native_type_compatibility.py
+++ sdks/python/apache_beam/typehints/native_type_compatibility.py
@@ -25,8 +25,12 @@ import logging
 import sys
 import types
 import typing
+from typing import Generic
+from typing import TypeVar
 
 from apache_beam.typehints import typehints
+
+T = TypeVar('T')
 
 _LOGGER = logging.getLogger(__name__)
 
