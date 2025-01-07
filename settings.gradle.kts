f332e96c8f7c9e48ac9ae6af66404019056518c3
Clay Johnson <cjohnson@gradle.com>
2025-01-07 13:04:14 -0600

    Update Develocity plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,8 +24,8 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.develocity") version "3.19"
-  id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.1"
+  id("com.gradle.develocity") version "3.18.2"
+  id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
 }
 
 
