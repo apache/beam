9c9010cc523858620d45de5e7a5c3669c66169a9
lostluck <13907733+lostluck@users.noreply.github.com>
2025-01-13 16:31:26 -0800

    Maintain rxplicit endpoint address for python.

--- sdks/go/pkg/beam/runners/prism/internal/jobservices/server.go
+++ sdks/go/pkg/beam/runners/prism/internal/jobservices/server.go
@@ -99,7 +99,7 @@ func (s *Server) getJob(id string) *Job {
 
 func (s *Server) Endpoint() string {
 	_, port, _ := net.SplitHostPort(s.lis.Addr().String())
-	return fmt.Sprintf(":%v", port)
+	return fmt.Sprintf("localhost:%v", port)
 }
 
 // Serve serves on the started listener. Blocks.
