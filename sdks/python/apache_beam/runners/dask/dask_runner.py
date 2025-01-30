dfa2813fe537325dadd0680ea7cf9e764eecaab1
Alex Merose <alex@openathena.ai>
2025-01-30 13:27:03 -0800

    *shakes fist at `isort`*.

--- sdks/python/apache_beam/runners/dask/dask_runner.py
+++ sdks/python/apache_beam/runners/dask/dask_runner.py
@@ -21,10 +21,9 @@ The DaskRunner is a runner implementation that executes a graph of
 transformations across processes and workers via Dask distributed's
 scheduler.
 """
-import dataclasses
-
 import argparse
 import collections
+import dataclasses
 import typing as t
 
 from apache_beam import pvalue
