a420191e04e9d730b6fc504310d50e7f73e1d21b
Sam Whittle <samuelw@google.com>
2025-02-06 15:46:48 +0100

    Add a utility class to enable sharing across all deserialized instances of a DoFn and use it in UnboundedSourceAsSdfWrapperFn to cache Readers across dofn instances

---
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/PerSerializationStatic.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.util;
+
+import java.io.Serializable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.annotation.Nullable;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * An object that simplifies having a variable that behaves like a static object but which is scoped
+ * to deserialized instances.
+ *
+ * <p>In particular this can be useful for use within a DoFn class to maintain shared state across
+ * all instances of the DoFn that are the same step in the graph. This differs from a static
+ * variable which would be shared across all instances of the DoFn and a non-static variable which
+ * is per instance.
+ */
+public class PerSerializationStatic<T> implements Serializable {
+  private static final AtomicInteger idGenerator = new AtomicInteger();
+  private final int id;
+
+  private static final ConcurrentHashMap<Integer, Object> staticCache = new ConcurrentHashMap<>();
+  private final SerializableSupplier<@NonNull T> supplier;
+  private transient volatile @MonotonicNonNull T value;
+
+  public PerSerializationStatic(SerializableSupplier<@NonNull T> supplier) {
+    id = idGenerator.incrementAndGet();
+    this.supplier = supplier;
+  }
+
+  @SuppressWarnings("unchecked")
+  public T get() {
+    @Nullable T result = value;
+    if (result != null) {
+      return result;
+    }
+    @Nullable T mapValue = (T) staticCache.computeIfAbsent(id, ignored -> supplier.get());
+    return value = Preconditions.checkStateNotNull(mapValue);
+  }
+}
