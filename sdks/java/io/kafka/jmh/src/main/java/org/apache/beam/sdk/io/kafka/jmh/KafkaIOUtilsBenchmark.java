519259e901d1f4e55c82e714ef763692cfe4cb9d
Steven van Rossum <sjvanrossum@google.com>
2025-03-05 14:02:34 +0000

    Move JMH content to jmh package

--- sdks/java/io/kafka/jmh/src/main/java/org/apache/beam/sdk/io/kafka/jmh/KafkaIOUtilsBenchmark.java
+++ sdks/java/io/kafka/jmh/src/main/java/org/apache/beam/sdk/io/kafka/jmh/KafkaIOUtilsBenchmark.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.sdk.io.kafka;
+package org.apache.beam.sdk.io.kafka.jmh;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
