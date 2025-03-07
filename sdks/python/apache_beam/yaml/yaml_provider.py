162b039a665b919f6055d695564d4466a53e0b18
Robert Bradshaw <robertwb@gmail.com>
2025-03-07 11:06:46 -0800

    debug github actions

--- sdks/python/apache_beam/yaml/yaml_provider.py
+++ sdks/python/apache_beam/yaml/yaml_provider.py
@@ -1155,7 +1155,7 @@ class PypiExpansionService:
       try:
         clonable_venv = cls._create_venv_to_clone(base_python)
         clonable_python = os.path.join(clonable_venv, 'bin', 'python')
-        subprocess.run(
+        subprocess.check_output(
             [clonable_python, '-m', 'clonevirtualenv', clonable_venv, venv],
             check=True)
         venv_pip = os.path.join(venv, 'bin', 'pip')
