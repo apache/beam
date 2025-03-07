c9b9f32c441bf9c841ade31d61a368ac7dc9e125
Radek Stankiewicz <radoslaws@google.com>
2025-03-07 13:48:14 +0100

    file staging

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -1561,6 +1561,14 @@ class SetupOptions(PipelineOptions):
             'workers will install them in same order they were specified on '
             'the command line.'))
     parser.add_argument(
+        '--files_to_stage',
+        dest='files_to_stage',
+        action='append',
+        default=None,
+        help=('Local path to a file. During job submission, the files will be '
+              'staged in the staging area (--staging_location option) and then '
+              'workers will upload them to the current working directory.'))
+    parser.add_argument(
         '--prebuild_sdk_container_engine',
         help=(
             'Prebuild sdk worker container image before job submission. If '
@@ -1889,10 +1897,6 @@ class TestDataflowOptions(PipelineOptions):
         default=None,
         help='Root URL for use with the Google Cloud Pub/Sub API.',
     )
-
-
-# TODO(silviuc): Add --files_to_stage option.
-# This could potentially replace the --requirements_file and --setup_file.
 
 # TODO(silviuc): Non-standard options. Keep them? If yes, add help too!
 # Remote execution must check that this option is not None.
