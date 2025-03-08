d6fd6fafe1b9824bcf43e43975e77b6a154098e8
liferoad <huxiangqian@gmail.com>
2025-03-08 10:02:38 -0500

    skip pip check for lint

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -359,9 +359,7 @@ if __name__ == '__main__':
           # which might not be reproducible in newer versions.
           # internal bug number: 372274680
           # external issue: https://github.com/grpc/grpc/issues/37710
-          # grpcio rc versions can break the build on Mac. Use 1.71 here.
-          # Error message: grpcio 1.71.0rc2 is not supported on this platform.
-          'grpcio>=1.33.1,<2,!=1.48.0,!=1.59.*,!=1.60.*,!=1.61.*,!=1.62.0,!=1.62.1,<1.71',  # pylint: disable=line-too-long
+          'grpcio>=1.33.1,<2,!=1.48.0,!=1.59.*,!=1.60.*,!=1.61.*,!=1.62.0,!=1.62.1',  # pylint: disable=line-too-long
           'hdfs>=2.1.0,<3.0.0',
           'httplib2>=0.8,<0.23.0',
           'jsonschema>=4.0.0,<5.0.0',
