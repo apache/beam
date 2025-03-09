c2776712bd5d91f1e7f7e05caec01737f3c0b06c
Brijeshthummar02 <brijeshthummar02@gmail.com>
2025-03-09 21:47:59 +0530

    Reduce allowed lateness in FixedWindows from 10 to 2 seconds

--- sdks/python/apache_beam/examples/snippets/snippets_test.py
+++ sdks/python/apache_beam/examples/snippets/snippets_test.py
@@ -1153,7 +1153,7 @@ class SnippetsTest(unittest.TestCase):
           pcollection | WindowInto(
               FixedWindows(1 * 60),
               trigger=AfterWatermark(late=AfterProcessingTime(10 * 60)),
-              allowed_lateness=10,
+              allowed_c=2,
               accumulation_mode=AccumulationMode.DISCARDING)
           # [END model_composite_triggers]
           | 'group' >> beam.GroupByKey()
