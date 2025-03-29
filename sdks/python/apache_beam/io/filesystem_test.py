d3ee3d4b5f752744c3540f1cf4fe9c14141f36e0
Tanu Sharma <tanusharmaa@google.com>
2025-03-29 22:52:54 +0530

    Setting num_bytes to DEFAULT_READ_BUFFER_SIZE if it is None originally (resolves #34435)

--- sdks/python/apache_beam/io/filesystem_test.py
+++ sdks/python/apache_beam/io/filesystem_test.py
@@ -470,6 +470,21 @@ atomized in instants hammered around the
 
         self.assertEqual(first_pass, second_pass)
 
+  def test_read(self):
+    for compression_type in [CompressionTypes.BZIP2,
+                             CompressionTypes.DEFLATE,
+                             CompressionTypes.GZIP,
+                             CompressionTypes.ZSTD,
+                             CompressionTypes.LZMA]:
+      file_name = self._create_compressed_file(compression_type, self.content)
+      with open(file_name, 'rb') as f:
+        compressed_fd = CompressedFile(
+            f, compression_type, read_size=self.read_block_size)
+
+        data = compressed_fd.read()   
+
+        self.assertEqual(data, self.content)      
+
   def test_tell(self):
     lines = [b'line%d\n' % i for i in range(10)]
     tmpfile = self._create_temp_file()
@@ -514,7 +529,7 @@ atomized in instants hammered around the
       byte_list = list(
           b for i in range(4096) for b in random.sample(byte_table, 64))
       byte_list.append(b'\n')
-      return b''.join(byte_list)
+      return b''.join(byte_list)  
 
     def create_test_file(compression_type, lines):
       filenames = []
