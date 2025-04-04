2f01a268b51c22cd0421a0205163495684ee2b2f
DKER2 <pdanghuy03@gmail.com>
2025-04-04 14:57:02 +0800

    fix(java.test): remove unrelevant test

--- sdks/java/core/src/test/java/org/apache/beam/sdk/util/VarIntTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/util/VarIntTest.java
@@ -254,14 +254,6 @@ public class VarIntTest {
   }
 
   @Test
-  public void decodeThrowsExceptionForIntUnderflow() throws IOException {
-    byte[] encoded = encodeLong(-1);
-
-    thrown.expect(IOException.class);
-    decodeInt(encoded);
-  }
-
-  @Test
   public void decodeThrowsExceptionForNonterminated() throws IOException {
     final byte[] nonTerminatedNumber = {(byte) 0xff, (byte) 0xff};
 
