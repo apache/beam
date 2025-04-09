7c975028737cd565265c5aee1320b2e5abe32b67
Pablo Rodriguez Defino <prodriguezdefino@gmail.com>
2025-04-09 03:31:21 +0000

    fixing lint

--- sdks/python/apache_beam/io/aws/sqsio.py
+++ sdks/python/apache_beam/io/aws/sqsio.py
@@ -21,23 +21,26 @@ from apache_beam.transforms.external import BeamJarExpansionService, SchemaAware
 
 __all__ = ['ReadFromSQS']
 
+
 class ReadFromSQS(beam.PTransform):
-    """Reads messages from an AWS SQS queue.
+  """Reads messages from an AWS SQS queue.
     
     Returns a PCollection of Beam Rows, each representing a SqsMessage object.
-    This transform uses the Java SDK implementation, and most of AWS related connectivity configurations 
-    can be set through a custom PipelineOptions object with the same properties seen on the AwsOptions 
-    for Java SDK. For more information see: 
+    This transform uses the Java SDK implementation, and most of AWS related 
+    connectivity configurations can be set through a custom PipelineOptions 
+    object with the same properties seen on the AwsOptions for Java SDK. 
+    For more information see: 
     https://beam.apache.org/releases/javadoc/current/org/apache/beam/sdk/io/aws/sqs/SqsIO.html
     """
-    URN = "beam:schematransform:org.apache.beam:aws:sqs_read:v1"
-    
-    def __init__(self, 
-               queue_url,
-               max_num_records=None,
-               max_read_time_secs=None,
-               expansion_service=None):
-        """Initialize a ReadFromSQS transform.
+  URN = "beam:schematransform:org.apache.beam:aws:sqs_read:v1"
+
+  def __init__(
+      self,
+      queue_url,
+      max_num_records=None,
+      max_read_time_secs=None,
+      expansion_service=None):
+    """Initialize a ReadFromSQS transform.
 
         :param queue_url:
             The SQS queue url to use.
@@ -49,24 +52,23 @@ class ReadFromSQS(beam.PTransform):
             The address of the expansion service. If no expansion service is
             provided, will attempt to run the default AWS expansion service.
         """
-        super().__init__()
-        self._queue_url = queue_url
-        self._max_num_records = max_num_records
-        self._max_read_time_secs = max_read_time_secs
-        self._expansion_service = (
-            expansion_service or BeamJarExpansionService(
-                'sdks:java:io:amazon-web-services2:expansion-service:build'))
-        self.schematransform_config = SchemaAwareExternalTransform.discover_config(
-            self._expansion_service, ReadFromSQS.URN)
+    super().__init__()
+    self._queue_url = queue_url
+    self._max_num_records = max_num_records
+    self._max_read_time_secs = max_read_time_secs
+    self._expansion_service = (
+        expansion_service or BeamJarExpansionService(
+            'sdks:java:io:amazon-web-services2:expansion-service:build'))
+    self.schematransform_config = SchemaAwareExternalTransform.discover_config(
+        self._expansion_service, ReadFromSQS.URN)
 
-    def expand(self, input):
-        return (
-            input.pipeline
-            | "Use external transform" >> SchemaAwareExternalTransform(
-                identifier=self.schematransform_config.identifier,
-                expansion_service=self._expansion_service,
-                rearrange_based_on_discovery=True,
-                max_num_records=self._max_num_records,
-                max_read_time_secs=self._max_read_time_secs,
-                queue_url=self._queue_url))
-        
+  def expand(self, input):
+    return (
+        input.pipeline
+        | "Use external transform" >> SchemaAwareExternalTransform(
+            identifier=self.schematransform_config.identifier,
+            expansion_service=self._expansion_service,
+            rearrange_based_on_discovery=True,
+            max_num_records=self._max_num_records,
+            max_read_time_secs=self._max_read_time_secs,
+            queue_url=self._queue_url))
