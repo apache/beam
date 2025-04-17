715435c9db95457d03937d6fc797511fe22b38ab
Shunping Huang <shunping@google.com>
2025-04-17 10:22:30 -0400

    Fix lints.

--- sdks/python/apache_beam/ml/anomaly/transforms.py
+++ sdks/python/apache_beam/ml/anomaly/transforms.py
@@ -21,7 +21,6 @@ from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Iterable
-from typing import List
 from typing import Optional
 from typing import Tuple
 from typing import TypeVar
