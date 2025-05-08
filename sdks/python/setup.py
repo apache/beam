970846b96bc9e7f6583f1c84f99f8f0330a6bb39
Jack McCluskey <thejackmccluskey@gmail.com>
2025-05-08 11:53:12 -0400

    Support newer versions of PyArrow

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -145,7 +145,7 @@ if sys.platform == 'win32' and sys.maxsize <= 2**32:
   pyarrow_dependency = ['']
 else:
   pyarrow_dependency = [
-      'pyarrow>=3.0.0,<17.0.0',
+      'pyarrow>=3.0.0,<21.0.0',
       # NOTE(https://github.com/apache/beam/issues/29392): We can remove this
       # once Beam increases the pyarrow lower bound to a version that fixes CVE.
       'pyarrow-hotfix<1'
