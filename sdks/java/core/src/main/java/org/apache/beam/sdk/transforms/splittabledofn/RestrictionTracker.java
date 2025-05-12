89fbea44cf0e20b784f288f2c011d34000bc5224
Kenneth Knowles <klk@google.com>
2025-05-12 16:17:03 -0400

    Refactor: separate SplittableTruncateSizedRestrictions

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/splittabledofn/RestrictionTracker.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/splittabledofn/RestrictionTracker.java
@@ -211,6 +211,6 @@ public abstract class RestrictionTracker<RestrictionT, PositionT> {
       return new AutoValue_RestrictionTracker_TruncateResult(restriction);
     }
 
-    public abstract @Nullable RestrictionT getTruncatedRestriction();
+    public abstract RestrictionT getTruncatedRestriction();
   }
 }
