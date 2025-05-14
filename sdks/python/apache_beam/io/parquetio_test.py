4121a668a54f5ca4331bf8edbdc97ddbac9e058d
Jack McCluskey <thejackmccluskey@gmail.com>
2025-05-14 16:18:09 -0400

    yapf

--- sdks/python/apache_beam/io/parquetio_test.py
+++ sdks/python/apache_beam/io/parquetio_test.py
@@ -490,7 +490,7 @@ class TestParquet(unittest.TestCase):
     # This test depends on count being sufficiently large + the ratio of
     # count to row_group_size also being sufficiently large (but the required
     # ratio to pass varies for values of row_group_size and, somehow, the
-    # version of pyarrow being tested against.) 
+    # version of pyarrow being tested against.)
     file_name = self._write_data(count=280, row_group_size=20)
     source = _create_parquet_source(file_name)
 
