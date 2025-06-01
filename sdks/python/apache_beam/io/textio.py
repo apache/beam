fa890df3e4d51c2e150d9afa0aaf5479c76a9782
Sharan Teja Malyala <sharantej@google.com>
2025-06-01 11:24:00 +0000

    added code suggestions

--- sdks/python/apache_beam/io/textio.py
+++ sdks/python/apache_beam/io/textio.py
@@ -1056,7 +1056,8 @@ try:
       kwargs['num_shards'] = num_shards
     if file_naming is not None:
       kwargs['file_naming'] = file_naming
-    kwargs['max_writers_per_bundle'] = 0
+    if 'max_writers_per_bundle' not in kwargs:
+        kwargs['max_writers_per_bundle'] = 0
     if lines is None:
       lines = orient == 'records'
     return 'WriteToJson' >> WriteViaPandas(
