3619ca22ac3a434675834d9bc7ef9de5f4b2cf2f
Derrick Williams <derrickaw@google.com>
2025-06-05 10:54:39 -0400

    Add postcommit yaml xlang workflow and split tests accordingly  (#35119)
    * move precommit yaml xlang to postcommit
    
    * add json file to trigger path
    
    * update pull request targets
    
    * add readme workflow changes
    
    * add cloud sdk setup
    
    * switch out to ubuntu-latest
    
    * add back precommit workflow
    
    * switch names and add postcommit
    
    * switch out to postCommitYamlIT
    
    * add test_files_dir flag
    
    * add conftest.py file for capturing directory flag
    
    * shift yaml tests around to appropriate locations
    
    * add back precommit to readme
    
    * add license for conftest.py
    
    * revert precommit to previous name
    
    * remove github.event.comment.body trigger

--- sdks/python/apache_beam/yaml/integration_tests.py
+++ sdks/python/apache_beam/yaml/integration_tests.py
@@ -53,6 +53,7 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.utils import python_callable
 from apache_beam.yaml import yaml_provider
 from apache_beam.yaml import yaml_transform
+from apache_beam.yaml.conftest import yaml_test_files_dir
 
 
 @contextlib.contextmanager
@@ -603,8 +604,15 @@ def parse_test_files(filepattern):
       globals()[suite_name] = type(suite_name, (unittest.TestCase, ), methods)
 
 
+# Logging setup
 logging.getLogger().setLevel(logging.INFO)
-parse_test_files(os.path.join(os.path.dirname(__file__), 'tests', '*.yaml'))
+
+# Dynamically create test methods from the tests directory.
+# yaml_test_files_dir comes from conftest.py and set by pytest_configure.
+_test_files_dir = yaml_test_files_dir
+_file_pattern = os.path.join(
+    os.path.dirname(__file__), _test_files_dir, '*.yaml')
+parse_test_files(_file_pattern)
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
