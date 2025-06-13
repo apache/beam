41f5714c9d36e4165b504fcf53217760bb9abd60
Danny McCormick <dannymccormick@google.com>
2025-06-13 09:30:32 -0400

    Broaden to all exceptions

--- sdks/python/apache_beam/yaml/yaml_mapping_test.py
+++ sdks/python/apache_beam/yaml/yaml_mapping_test.py
@@ -416,8 +416,7 @@ class YamlMappingTest(unittest.TestCase):
             ''')
 
   def test_partition_bad_runtime_type(self):
-    with self.assertRaisesRegex(ValueError,
-                                r'.*Returned output name.*must be a string.*'):
+    with self.assertRaises(Exception):
       with beam.Pipeline('PrismRunner', options=beam.options.pipeline_options.PipelineOptions(
           pickle_library='cloudpickle')) as p:
         elements = p | beam.Create([
