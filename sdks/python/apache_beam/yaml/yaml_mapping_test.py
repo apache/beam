a2975278e6a1caf30e1176a8e8d5a0ad8146fa99
Danny McCormick <dannymccormick@google.com>
2025-06-17 17:37:08 +0000

    Fix error

--- sdks/python/apache_beam/yaml/yaml_mapping_test.py
+++ sdks/python/apache_beam/yaml/yaml_mapping_test.py
@@ -416,7 +416,7 @@ class YamlMappingTest(unittest.TestCase):
             ''')
 
   def test_partition_bad_runtime_type(self):
-    with self.assertRaisesRegex(Exception, 'must be a string. gcloud storage cp /workspaces/beam/runners/prism/build/tmp/prism gs://apache-beam-damccorm/prism_binary/prism'):
+    with self.assertRaisesRegex(Exception, 'must be a string. '):
       with beam.Pipeline('PrismRunner', options=beam.options.pipeline_options.PrismRunnerOptions(
           pickle_library='cloudpickle', prism_location='gs://apache-beam-damccorm/prism_binary/prism')) as p:
         elements = p | beam.Create([
