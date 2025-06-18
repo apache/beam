f5c7d08b82afd9dd11e2e1e79b201ff05796dc83
Ashok Devireddy <ashokrd@google.com>
2025-06-18 11:21:54 -0400

    ordered sliding window sliding window w/ tests init

---
+++ sdks/python/apache_beam/ml/ts/ordered_sliding_window.py
@@ -0,0 +1,146 @@
+import apache_beam as beam
+from apache_beam.coders import PickleCoder, BooleanCoder
+from apache_beam.transforms.userstate import OrderedListStateSpec, TimerSpec, on_timer, ReadModifyWriteStateSpec
+from apache_beam.utils.timestamp import Timestamp, MIN_TIMESTAMP, MAX_TIMESTAMP
+from apache_beam.transforms.timeutil import TimeDomain
+import typing
+from collections import defaultdict
+import numpy as np
+import logging
+# logging.basicConfig(level=logging.INFO)
+
+class OrderedSlidingWindowFn(beam.DoFn):
+
+  ORDERED_BUFFER_STATE = OrderedListStateSpec('ordered_buffer', PickleCoder())
+  WINDOW_TIMER = TimerSpec('window_timer', TimeDomain.WATERMARK)
+  TIMER_STATE = ReadModifyWriteStateSpec('timer_state', BooleanCoder())
+  EARLIEST_TS_STATE = ReadModifyWriteStateSpec('earliest_ts', PickleCoder())
+
+
+  def __init__(self, window_size, slide_interval):
+    self.window_size = window_size
+    self.slide_interval = slide_interval
+
+  def start_bundle(self):
+    logging.info("start bundle")
+
+  def finish_bundle(self):
+    logging.info("finish bundle")
+
+  def process(self,
+              element,
+              timestamp=beam.DoFn.TimestampParam,
+              ordered_buffer=beam.DoFn.StateParam(ORDERED_BUFFER_STATE),
+              window_timer=beam.DoFn.TimerParam(WINDOW_TIMER),
+              timer_state=beam.DoFn.StateParam(TIMER_STATE),
+              earliest_ts_state=beam.DoFn.StateParam(EARLIEST_TS_STATE)):
+
+    key, value = element
+    ordered_buffer.add((timestamp, value))
+
+    logging.info(f"receive {element} at {timestamp}")
+    timer_started = timer_state.read() # maybe use read_range instead?
+    if not timer_started:
+      earliest_ts_state.write(timestamp)
+
+      first_slide_start = int(
+          timestamp.micros / 1e6 // self.slide_interval) * self.slide_interval
+      first_slide_start_ts = Timestamp.of(first_slide_start)
+
+      first_window_end_ts = first_slide_start_ts + self.window_size
+      logging.info(f"set timer to {first_window_end_ts}")
+      window_timer.set(first_window_end_ts)
+
+      timer_state.write(True)
+
+  @on_timer(WINDOW_TIMER)
+  def on_timer(self,
+               key=beam.DoFn.KeyParam,
+               fire_ts=beam.DoFn.TimestampParam,
+               ordered_buffer=beam.DoFn.StateParam(ORDERED_BUFFER_STATE),
+               window_timer=beam.DoFn.TimerParam(WINDOW_TIMER),
+               timer_state=beam.DoFn.StateParam(TIMER_STATE),
+               earliest_ts_state=beam.DoFn.StateParam(EARLIEST_TS_STATE)):
+    logging.info(f"timer fire at {fire_ts}")
+    window_end_ts = fire_ts
+    window_start_ts = window_end_ts - self.window_size
+
+    # all_values = list(ordered_buffer.read())
+    # logging.info(f"all data in buffer {str(all_values)}")
+    # window_values = [
+    #     i for i in all_values if window_start_ts <= i[0] < 2
+    # ]
+    window_values = list(ordered_buffer.read_range(window_start_ts, window_end_ts))
+    logging.info(f"Window [{window_start_ts}, {window_end_ts}) contains {len(window_values)} elements.")
+
+
+    logging.info(f"window start: {window_start_ts}, window end: {window_end_ts}")
+    logging.info(f"windowed data in buffer {str(window_values)}")
+    if window_values:
+      yield (key, (window_start_ts, window_end_ts, window_values))
+
+    next_window_end_ts = fire_ts + self.slide_interval
+    next_window_start_ts = window_start_ts + self.slide_interval
+
+    earliest_ts = earliest_ts_state.read()
+    ordered_buffer.clear_range(earliest_ts, next_window_start_ts)
+    # ordered_buffer.clear_range(MIN_TIMESTAMP, next_window_start_ts) # use clear_range without mintimestamp
+
+    # remaining_data = list(ordered_buffer.read())
+    remaining_data = list(ordered_buffer.read_range(next_window_start_ts, MAX_TIMESTAMP))
+
+
+    if not remaining_data:
+      timer_state.clear()
+      earliest_ts_state.clear()
+      return
+
+    logging.info(f"set timer to {next_window_end_ts}")
+    window_timer.set(next_window_end_ts)
+
+
+class FillGapsFn(beam.DoFn):
+
+    def __init__(self, expected_interval):
+        self.expected_interval = expected_interval
+
+    def process(self, element):
+        key, (window_start_ts, window_end_ts, window_elements) = element
+
+
+        # If there are no elements, or only one, there are no "middle" gaps to fill.
+        if len(window_elements) <= 1:
+            if window_elements:
+                 # If one element, just yield it back as a list of one
+                 yield (key, (window_start_ts, window_end_ts, [window_elements[0][1]]))
+            return
+
+        # Use a dictionary for efficient lookup, rounding for float precision.
+        received_data = defaultdict(list)
+        for ts, val in window_elements:
+            lookup_ts = round(float(ts.micros / 1e6), 5)
+            received_data[lookup_ts].append(val)
+
+        # Find the boundaries of the data we actually received.
+        min_ts = min(received_data.keys())
+        max_ts = max(received_data.keys())
+        # print("boundaries:", min_ts, max_ts)
+
+        filled_middle_values = []
+        current_ts = min_ts
+
+        # Iterate from the first observed timestamp to the last.
+        while current_ts <= max_ts:
+            lookup_ts = round(current_ts, 5)
+
+            # Use the actual value if it exists, otherwise a placeholder.
+            value = received_data.get(lookup_ts, [np.nan])[0]
+            filled_middle_values.append(float(value))
+
+            # Move to the next expected timestamp.
+            current_ts += self.expected_interval
+
+        # Yield the original window boundaries but with the new, gap-filled list.
+        yield (key, (window_start_ts, window_end_ts, filled_middle_values))
+
+
---
+++ sdks/python/apache_beam/ml/ts/ordered_sliding_window_test.py
@@ -0,0 +1,71 @@
+import apache_beam as beam
+from ordered_sliding_window import OrderedSlidingWindowFn, FillGapsFn
+import unittest
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, equal_to
+import math
+from apache_beam.utils.timestamp import Timestamp, MIN_TIMESTAMP
+from util import PeriodicStream
+import random
+import typing
+import logging
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# logging.basicConfig(level=logging.INFO)
+
+# --- Test Suite ---
+class DoFnTests(unittest.TestCase):
+
+    def test_pipeline_with_periodic_stream_data(self):
+        """Tests the pipeline using the specific data sequence from the user's example."""
+        
+        WINDOW_SIZE =10
+        SLIDE_INTERVAL = 3
+        EXPECTED_INTERVAL = 1 # per second expected interval
+        
+        data = []
+        timestamps_to_drop = {5, 6, 7, 25, 26, 31, 32, 33}
+        for i in range(20):
+            # Check if the integer value should be dropped
+            if i not in timestamps_to_drop:
+                ts = i #* EXPECTED_FREQ don't need this
+                data.append((Timestamp(ts), i))
+
+        # random.shuffle(data)
+        print([i[1] for i in data])
+
+
+        expected = [
+            (0, (0, 5, [1.0, 2.0, 3.0, 4.0])),
+            (0, (2, 7, [2.0, 3.0, 4.0, 5.0, 6.0])),
+            (0, (4, 9, [4.0, 5.0, 6.0, 7.0, 8.0])),
+            (0, (6, 11, [6.0, 7.0, 8.0, 9.0, 10.0])),
+            (0, (8, 13, [8.0, 9.0, 10.0])),
+            (0, (10, 15, [10.0])),
+        ]
+
+        # 3. Pipeline using PeriodicStream
+
+        options = PipelineOptions([
+            "--streaming",
+            "--environment_type=LOOPBACK",
+            "--runner=PrismRunner",
+        ])
+        with beam.Pipeline(options=options) as p:
+            output = (
+                p
+                | PeriodicStream(data, interval=0.01)
+                | beam.WithKeys(0)
+                # | 'Create' >> beam.Create(data)
+                # | 'Add dummy key' >> beam.M2ap(lambda x: (x % 2, x)).with_output_types(typing.Tuple[int, int])
+                | "SlidingWindow" >> beam.ParDo(OrderedSlidingWindowFn(window_size=WINDOW_SIZE, slide_interval=SLIDE_INTERVAL))
+                | "FillGaps" >> beam.ParDo(FillGapsFn(expected_interval=EXPECTED_INTERVAL))
+                | "print" >> beam.Map(print)
+            )
+
+            # assert_that(output, equal_to(expected), label="CheckPeriodicStreamData")
+
+
+if __name__ == '__main__':
+    unittest.main()
