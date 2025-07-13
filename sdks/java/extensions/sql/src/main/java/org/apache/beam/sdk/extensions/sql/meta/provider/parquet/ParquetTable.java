5826071e95b1fb0d668bff36722694e78cb9451a
Talat Uyarer <talat@apache.org>
2025-07-13 05:01:25 -0700

    Initial SQL ParquetTable predicate support

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/meta/provider/parquet/ParquetTable.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/meta/provider/parquet/ParquetTable.java
@@ -19,8 +19,10 @@ package org.apache.beam.sdk.extensions.sql.meta.provider.parquet;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Field;
 import org.apache.avro.generic.GenericRecord;
@@ -31,14 +33,17 @@ import org.apache.beam.sdk.extensions.sql.meta.ProjectSupport;
 import org.apache.beam.sdk.extensions.sql.meta.SchemaBaseBeamTable;
 import org.apache.beam.sdk.extensions.sql.meta.Table;
 import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.parquet.ParquetFilterFactory;
 import org.apache.beam.sdk.io.parquet.ParquetIO;
 import org.apache.beam.sdk.io.parquet.ParquetIO.Read;
 import org.apache.beam.sdk.schemas.transforms.Convert;
+import org.apache.beam.sdk.schemas.transforms.Select;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollection.IsBounded;
 import org.apache.beam.sdk.values.POutput;
 import org.apache.beam.sdk.values.Row;
+import org.apache.beam.vendor.calcite.v1_28_0.org.apache.calcite.rex.RexNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,7 +69,16 @@ class ParquetTable extends SchemaBaseBeamTable implements Serializable {
 
   @Override
   public PCollection<Row> buildIOReader(
-      PBegin begin, BeamSqlTableFilter filters, List<String> fieldNames) {
+      PBegin begin, BeamSqlTableFilter filter, List<String> projectedFieldNames) {
+
+    // Determine ALL fields required for the read (projection + filter fields).
+    Set<String> requiredFieldsForRead = new HashSet<>(projectedFieldNames);
+    if (filter instanceof ParquetFilter) {
+      ParquetFilter parquetFilter = (ParquetFilter) filter;
+      requiredFieldsForRead.addAll(parquetFilter.getReferencedFields(getSchema()));
+    }
+
+    // If no fields are projected or filtered, read the full schema.
     final Schema schema = AvroUtils.toAvroSchema(table.getSchema());
     String filePattern = resolveFilePattern(table.getLocation());
     Read read = ParquetIO.read(schema).withBeamSchemas(true).from(filePattern);
@@ -72,15 +86,58 @@ class ParquetTable extends SchemaBaseBeamTable implements Serializable {
       Schema projectionSchema = projectSchema(schema, fieldNames);
       LOG.info("Projecting fields schema: {}", projectionSchema);
       read = read.withProjection(projectionSchema, projectionSchema);
+    Schema readSchema =
+        requiredFieldsForRead.isEmpty()
+            ? schema
+            : projectSchema(schema, new ArrayList<>(requiredFieldsForRead));
+
+    LOG.info("Projecting fields schema: {}", readSchema);
+
+    Read read = ParquetIO.read(readSchema).withBeamSchemas(true).from(table.getLocation() + "/*");
+    read = read.withProjection(readSchema, readSchema);
+
+    if (filter instanceof ParquetFilter) {
+      ParquetFilter parquetFilter = (ParquetFilter) filter;
+      List<RexNode> supported = parquetFilter.getSupported();
+      if (!supported.isEmpty()) {
+        org.apache.beam.sdk.io.parquet.ParquetFilter predicate =
+            ParquetFilterFactory.create(supported, getSchema());
+        read = read.withFilter(predicate);
+      }
     }
-    return begin.apply("ParquetIORead", read).apply("ToRows", Convert.toRows());
+
+    PCollection<Row> rowsWithRequiredFields =
+        begin.apply("ParquetIORead With Filtering", read).apply("ToRows", Convert.toRows());
+
+    // If we read extra fields for filtering, project them away to match the final SELECT list.
+    if (!projectedFieldNames.isEmpty()
+        && !new HashSet<>(projectedFieldNames).equals(requiredFieldsForRead)) {
+      return rowsWithRequiredFields.apply(
+          Select.fieldNames(projectedFieldNames.toArray(new String[0])));
+    } else {
+      return rowsWithRequiredFields;
+    }
+  }
+
+  @Override
+  public BeamSqlTableFilter constructFilter(List<RexNode> filter) {
+    return new ParquetFilter(filter);
   }
 
   /** Returns a copy of the {@link Schema} with only the fieldNames fields. */
   private static Schema projectSchema(Schema schema, List<String> fieldNames) {
+    if (fieldNames.isEmpty()) {
+      return schema;
+    }
+
+    Set<String> fieldNameSet = new HashSet<>(fieldNames);
     List<Field> selectedFields = new ArrayList<>();
-    for (String fieldName : fieldNames) {
-      selectedFields.add(deepCopyField(schema.getField(fieldName)));
+
+    // Iterate through the original schema's fields to maintain their order.
+    for (Schema.Field field : schema.getFields()) {
+      if (fieldNameSet.contains(field.name())) {
+        selectedFields.add(deepCopyField(field));
+      }
     }
     return Schema.createRecord(
         schema.getName() + "_projected",
