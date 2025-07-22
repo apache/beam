23fa4d8a310c4db3bc2d717bfa0e22acfe3baf90
kristynsmith <kristynsmith@google.com>
2025-07-22 12:14:07 -0400

    Create code_object_pickler_test.py

---
+++ sdks/python/apache_beam/internal/code_object_pickler_test.py
@@ -0,0 +1,155 @@
+from apache_beam.internal import code_object_pickler
+
+from google3.testing.pybase import googletest
+from google3.testing.pybase import parameterized
+
+
+def top_level_function():
+  return 1
+
+
+top_level_lambda = lambda x: 1
+
+
+def get_nested_function():
+  def nested_function():
+    return 1
+  return nested_function
+
+
+def get_lambda_from_dictionary():
+  d = {
+      "a": lambda x: 1,
+      "b": lambda y: 2
+  }
+  return d["a"]
+
+
+def get_lambda_from_dictionary_same_args():
+  d = {
+      "a": lambda x: 1,
+      "b": lambda x: x + 1
+  }
+  return d["a"]
+
+
+def function_with_lambda_default_argument(fn=lambda x: 1):
+  return fn
+
+
+def function_with_function_default_argument(fn=top_level_function):
+  return fn
+
+
+def function_decorator(f):
+  return lambda x: f(f(x))
+
+
+@function_decorator
+def add_one(x):
+  return x + 1
+
+
+class ClassWithFunction:
+  def process(self):
+    return 1
+
+
+class ClassWithStaticMethod:
+  @staticmethod
+  def static_method():  # pylint: disable=staticmethod-use
+    return 1
+
+
+class ClassWithClassMethod:
+  @classmethod
+  def class_method(cls):
+    return 1
+
+
+class ClassWithNestedFunction:
+  def process(self):
+    def nested_function():
+      return 1
+    return nested_function
+
+
+class ClassWithLambda:
+  def process(self):
+    return lambda: 1
+
+
+class ClassWithNestedClass:
+  class InnerClass:
+    def process(self):
+      return 1
+
+
+class ClassWithNestedLambda:
+  def process(self):
+    def get_lambda_from_dictionary():
+      d = {
+          "a": lambda x: 1,
+          "b": lambda y: 2
+      }
+      return d["a"]
+    return get_lambda_from_dictionary()
+
+test_cases = [
+        (top_level_function, "__main__.top_level_function.__code__"),
+        (top_level_lambda, "__main__.top_level_lambda.__code__"),
+        (get_nested_function(),
+         ("__main__.get_nested_function.__code__.co_consts"
+          "[nested_function]")),
+        (get_lambda_from_dictionary(),
+         ("__main__.get_lambda_from_dictionary.__code__.co_consts"
+          "[<lambda>, ('x',)]")),
+        (get_lambda_from_dictionary_same_args(),
+         ("__main__.get_lambda_from_dictionary_same_args.__code__.co_consts"
+          "[<lambda>, ('x',), "
+          "ac455edc80140a7f12ad229519456f71]")),
+        (function_with_lambda_default_argument(),
+         ("__main__.function_with_lambda_default_argument.__defaults__[0]"
+          ".__code__")),
+        (function_with_function_default_argument(),
+         "__main__.top_level_function.__code__"),
+        (add_one,
+         "__main__.function_decorator.__code__.co_consts[<lambda>]"),
+        (ClassWithFunction.process,
+         "__main__.ClassWithFunction.process.__code__"),
+        (ClassWithStaticMethod.static_method,
+         "__main__.ClassWithStaticMethod.static_method.__code__"),
+        (ClassWithClassMethod.class_method,
+         "__main__.ClassWithClassMethod.class_method.__code__"),
+        (ClassWithNestedFunction().process(),
+         ("__main__.ClassWithNestedFunction.process.__code__.co_consts"
+          "[nested_function]")),
+        (ClassWithLambda().process(),
+         "__main__.ClassWithLambda.process.__code__.co_consts[<lambda>]"),
+        (ClassWithNestedClass.InnerClass().process,
+         "__main__.ClassWithNestedClass.InnerClass.process.__code__"),
+        (ClassWithNestedLambda().process(),
+         ("__main__.ClassWithNestedLambda.process.__code__.co_consts"
+          "[get_lambda_from_dictionary].co_consts[<lambda>, ('x',)]")),
+        (ClassWithNestedLambda.process,
+         "__main__.ClassWithNestedLambda.process.__code__"),
+    ]
+
+
+class DillTest(parameterized.TestCase):
+
+  @parameterized.parameters(test_cases)
+  def test_get_code_path(self, callable, expected):
+    actual = code_object_pickler._get_code_path(callable)
+    self.assertEqual(actual, expected)
+
+  @parameterized.parameters(test_cases)
+  def test_get_code_from_stable_reference(self, callable, path):
+    actual = code_object_pickler._get_code_from_stable_reference(path)
+    self.assertEqual(actual, callable.__code__)
+
+  @parameterized.parameters(test_cases)
+  def test_roundtrip(self, callable, _):
+    path = code_object_pickler._get_code_path(callable)
+    actual = code_object_pickler._get_code_from_stable_reference(path)
+    self.assertEqual(actual, callable.__code__)
