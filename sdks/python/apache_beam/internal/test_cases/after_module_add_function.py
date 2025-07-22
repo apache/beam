d17d08a5bf9d856a456af95f13f8ff42cd1c1196
kristynsmith <kristynsmith@google.com>
2025-07-22 13:25:13 -0400

    Create after_module_add_function.py

---
+++ sdks/python/apache_beam/internal/test_cases/after_module_add_function.py
@@ -0,0 +1,13 @@
+"""Module for testing code path generation with functions."""
+
+
+def another_function():
+  a = 1
+  b = lambda: 2
+  return b
+
+
+def my_function():
+  a = 1
+  b = lambda: 2
+  return b
