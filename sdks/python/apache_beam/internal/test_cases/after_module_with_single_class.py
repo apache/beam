40b767c3b1168035aa3147819d1028d1afbf962c
kristynsmith <kristynsmith@google.com>
2025-07-22 13:32:46 -0400

    Create after_module_with_single_class.py

---
+++ sdks/python/apache_beam/internal/test_cases/after_module_with_single_class.py
@@ -0,0 +1,14 @@
+"""Module for testing code path generation with a single class."""
+
+
+class MyClass:
+  def another_function():
+    a = 1
+    b = lambda: 2
+    return b
+
+
+def my_function():
+  a = 1
+  b = lambda: 2
+  return b
