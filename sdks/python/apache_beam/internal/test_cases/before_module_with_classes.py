3ff7cd54fdfefd7201cebe4640a2eee746b22069
kristynsmith <kristynsmith@google.com>
2025-07-23 16:26:09 -0400

    Update before_module_with_classes.py

--- sdks/python/apache_beam/internal/test_cases/before_module_with_classes.py
+++ sdks/python/apache_beam/internal/test_cases/before_module_with_classes.py
@@ -19,61 +19,61 @@
 
 
 class AddLocalVariable:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class RemoveLocalVariable:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class AddLambdaVariable:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class RemoveLambdaVariable:
-  def my_method():
+  def my_method(self):
     a = lambda: 1
     b = lambda: 2
     return b
 
 
 class ClassWithNestedFunction:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class ClassWithNestedFunction2:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class ClassWithTwoMethods:
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
 
 
 class RemoveMethod:
-  def another_method():
+  def another_method(self):
     a = 1
     b = lambda: 2
     return b
 
-  def my_method():
+  def my_method(self):
     a = 1
     b = lambda: 2
     return b
