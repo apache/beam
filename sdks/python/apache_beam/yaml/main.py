b63ae21e0c089abdda1b24e4408f5c25528bc469
Charles Nguyen <phucnh402@gmail.com>
2025-07-28 16:53:11 -0400

    YAML Test CLI flag fix

--- sdks/python/apache_beam/yaml/main.py
+++ sdks/python/apache_beam/yaml/main.py
@@ -100,7 +100,7 @@ def _parse_arguments(argv):
       help='A json dict of variables used when invoking the jinja preprocessor '
       'on the provided yaml pipeline.')
   parser.add_argument(
-      '--test',
+      '--tests',
       action=argparse.BooleanOptionalAction,
       help='Run the tests associated with the given pipeline, rather than the '
       'pipeline itself.')
@@ -290,7 +290,7 @@ def build_pipeline_components_from_yaml(
 if __name__ == '__main__':
   import logging
   logging.getLogger().setLevel(logging.INFO)
-  if '--test' in sys.argv:
+  if '--tests' in sys.argv:
     run_tests()
   else:
     run()
