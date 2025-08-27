apache/beam

commit a7f5898f8850e5d110ccb299830c12a4f0807a73

Default SchemaTransform configs to snake_case (#31374)
* default schematransform configs to snake_case

* add to CHANGES.md

* update Go's bigtable wrapper to export snake_case param names

* make more yaml snake_case changes

sdks/go/pkg/beam/io/xlang/bigtableio/bigtable.go
@@ -62,9 +62,9 @@ import (
 )
 
 type bigtableConfig struct {
-	InstanceId string `beam:"instanceId"`
-	ProjectId  string `beam:"projectId"`
-	TableId    string `beam:"tableId"`
+	InstanceId string `beam:"instance_id"`
+	ProjectId  string `beam:"project_id"`
+	TableId    string `beam:"table_id"`
 }
 
 // Cell represents a single cell in a Bigtable row.

sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/transforms/TypedSchemaTransformProvider.java
@@ -17,8 +17,10 @@
  */
 package org.apache.beam.sdk.schemas.transforms;
 
+import static org.apache.beam.sdk.schemas.annotations.DefaultSchema.DefaultSchemaProvider;
 import static org.apache.beam.sdk.util.Preconditions.checkStateNotNull;
 import static org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.base.Preconditions.checkArgument;
+import static org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.base.Preconditions.checkNotNull;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
@@ -26,9 +28,14 @@ import java.util.Optional;
 import javax.annotation.Nullable;
 import org.apache.beam.sdk.annotations.Internal;
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.schemas.AutoValueSchema;
 import org.apache.beam.sdk.schemas.NoSuchSchemaException;
 import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.SchemaProvider;
 import org.apache.beam.sdk.schemas.SchemaRegistry;
+import org.apache.beam.sdk.schemas.io.InvalidConfigurationException;
+import org.apache.beam.sdk.schemas.io.InvalidSchemaException;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.values.Row;
 
 /**
@@ -38,8 +45,12 @@ import org.apache.beam.sdk.values.Row;
  * <p>ConfigT should be available in the SchemaRegistry.
  *
  * <p>{@link #configurationSchema()} produces a configuration {@link Schema} that is inferred from
- * {@code ConfigT} using the SchemaRegistry. A Beam {@link Row} can still be used produce a {@link
- * SchemaTransform} using {@link #from(Row)}, as long as the Row fits the configuration Schema.
+ * {@code ConfigT} using the SchemaRegistry. A Beam {@link Row} can still be used to produce a
+ * {@link SchemaTransform} using {@link #from(Row)}, as long as the Row fits the configuration
+ * Schema.
+ *
+ * <p>NOTE: The inferred field names in the configuration {@link Schema} and {@link Row} follow the
+ * {@code snake_case} naming convention.
  *
  * <p><b>Internal only:</b> This interface is actively being worked on and it will likely change as
  * we provide implementations for more standard Beam transforms. We provide no backwards
@@ -78,10 +89,11 @@ public abstract class TypedSchemaTransformProvider<ConfigT> implements SchemaTra
   }
 
   @Override
-  public Schema configurationSchema() {
+  public final Schema configurationSchema() {
     try {
       // Sort the fields by name to ensure a consistent schema is produced
-      return SchemaRegistry.createDefault().getSchema(configurationClass()).sorted();
+      // We also establish a `snake_case` convention for all SchemaTransform configurations
+      return SchemaRegistry.createDefault().getSchema(configurationClass()).sorted().toSnakeCase();
     } catch (NoSuchSchemaException e) {
       throw new RuntimeException(
           "Unable to find schema for "
@@ -90,9 +102,12 @@ public abstract class TypedSchemaTransformProvider<ConfigT> implements SchemaTra
     }
   }
 
-  /** Produces a {@link SchemaTransform} from a Row configuration. */
+  /**
+   * Produces a {@link SchemaTransform} from a Row configuration. Row fields are expected to have
+   * `snake_case` naming convention.
+   */
   @Override
-  public SchemaTransform from(Row configuration) {
+  public final SchemaTransform from(Row configuration) {
     return from(configFromRow(configuration));
   }
 
@@ -103,9 +118,22 @@ public abstract class TypedSchemaTransformProvider<ConfigT> implements SchemaTra
 
   private ConfigT configFromRow(Row configuration) {
     try {
-      return SchemaRegistry.createDefault()
-          .getFromRowFunction(configurationClass())
-          .apply(configuration);
+      SchemaRegistry registry = SchemaRegistry.createDefault();
+      SerializableFunction<Row, ConfigT> rowToConfigT =
+          registry.getFromRowFunction(configurationClass());
+
+      // Configuration objects handled by the AutoValueSchema provider will expect Row fields with
+      // camelCase naming convention
+      SchemaProvider schemaProvider = registry.getSchemaProvider(configurationClass());
+      if (schemaProvider.getClass().equals(DefaultSchemaProvider.class)
+          && checkNotNull(
+                  ((DefaultSchemaProvider) schemaProvider)
+                      .getUnderlyingSchemaProvider(configurationClass()))
+              .getClass()
+              .equals(AutoValueSchema.class)) {
+        configuration = configuration.toCamelCase();
+      }
+      return rowToConfigT.apply(configuration);
     } catch (NoSuchSchemaException e) {
       throw new RuntimeException(
           "Unable to find schema for " + identifier() + "SchemaTransformProvider's config");

sdks/java/core/src/test/java/org/apache/beam/sdk/schemas/transforms/TypedSchemaTransformProviderTest.java
@@ -130,8 +130,8 @@ public class TypedSchemaTransformProviderTest {
 
     Row inputConfig =
         Row.withSchema(provider.configurationSchema())
-            .withFieldValue("stringField", "field1")
-            .withFieldValue("integerField", Integer.valueOf(13))
+            .withFieldValue("string_field", "field1")
+            .withFieldValue("integer_field", Integer.valueOf(13))
             .build();
 
     Configuration outputConfig = ((FakeSchemaTransform) provider.from(inputConfig)).config;
@@ -150,8 +150,8 @@ public class TypedSchemaTransformProviderTest {
     SchemaTransformProvider provider = new FakeTypedSchemaIOProvider();
     Row inputConfig =
         Row.withSchema(provider.configurationSchema())
-            .withFieldValue("stringField", "field1")
-            .withFieldValue("integerField", Integer.valueOf(13))
+            .withFieldValue("string_field", "field1")
+            .withFieldValue("integer_field", Integer.valueOf(13))
             .build();
 
     assertEquals(Arrays.asList("field1", "13"), provider.dependencies(inputConfig, null).get());

sdks/java/io/iceberg/src/main/java/org/apache/beam/sdk/io/iceberg/IcebergReadSchemaTransformProvider.java
@@ -25,7 +25,6 @@ import org.apache.beam.sdk.io.iceberg.IcebergReadSchemaTransformProvider.Config;
 import org.apache.beam.sdk.managed.ManagedTransformConstants;
 import org.apache.beam.sdk.schemas.AutoValueSchema;
 import org.apache.beam.sdk.schemas.NoSuchSchemaException;
-import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.SchemaRegistry;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 import org.apache.beam.sdk.schemas.transforms.SchemaTransform;
@@ -131,16 +130,5 @@ public class IcebergReadSchemaTransformProvider extends TypedSchemaTransformProv
 
       return PCollectionRowTuple.of(OUTPUT_TAG, output);
     }
-  }
-
-  // TODO: set global snake_case naming convention and remove these special cases
-  @Override
-  public SchemaTransform from(Row rowConfig) {
-    return super.from(rowConfig.toCamelCase());
-  }
-
-  @Override
-  public Schema configurationSchema() {
-    return super.configurationSchema().toSnakeCase();
   }
 }

sdks/java/io/iceberg/src/main/java/org/apache/beam/sdk/io/iceberg/IcebergWriteSchemaTransformProvider.java
@@ -176,15 +176,4 @@ public class IcebergWriteSchemaTransformProvider extends TypedSchemaTransformPro
       }
     }
   }
-
-  // TODO: set global snake_case naming convention and remove these special cases
-  @Override
-  public SchemaTransform from(Row rowConfig) {
-    return super.from(rowConfig.toCamelCase());
-  }
-
-  @Override
-  public Schema configurationSchema() {
-    return super.configurationSchema().toSnakeCase();
-  }
 }

sdks/java/io/kafka/src/test/java/org/apache/beam/sdk/io/kafka/KafkaReadSchemaTransformProviderTest.java
@@ -121,17 +121,17 @@ public class KafkaReadSchemaTransformProviderTest {
 
     assertEquals(
         Sets.newHashSet(
-            "bootstrapServers",
+            "bootstrap_servers",
             "topic",
             "schema",
-            "autoOffsetResetConfig",
-            "consumerConfigUpdates",
+            "auto_offset_reset_config",
+            "consumer_config_updates",
             "format",
-            "confluentSchemaRegistrySubject",
-            "confluentSchemaRegistryUrl",
-            "errorHandling",
-            "fileDescriptorPath",
-            "messageName"),
+            "confluent_schema_registry_subject",
+            "confluent_schema_registry_url",
+            "error_handling",
+            "file_descriptor_path",
+            "message_name"),
         kafkaProvider.configurationSchema().getFields().stream()
             .map(field -> field.getName())
             .collect(Collectors.toSet()));

sdks/java/managed/src/main/java/org/apache/beam/sdk/managed/ManagedSchemaTransformProvider.java
@@ -258,15 +258,4 @@ public class ManagedSchemaTransformProvider
       throw new RuntimeException(e.getMessage());
     }
   }
-
-  // TODO: set global snake_case naming convention and remove these special cases
-  @Override
-  public SchemaTransform from(Row rowConfig) {
-    return super.from(rowConfig.toCamelCase());
-  }
-
-  @Override
-  public Schema configurationSchema() {
-    return super.configurationSchema().toSnakeCase();
-  }
 }

sdks/java/managed/src/main/java/org/apache/beam/sdk/managed/ManagedTransformConstants.java
@@ -45,27 +45,10 @@ public class ManagedTransformConstants {
   public static final String KAFKA_WRITE = "beam:schematransform:org.apache.beam:kafka_write:v1";
 
   private static final Map<String, String> KAFKA_READ_MAPPINGS =
-      ImmutableMap.<String, String>builder()
-          .put("topic", "topic")
-          .put("bootstrap_servers", "bootstrapServers")
-          .put("consumer_config_updates", "consumerConfigUpdates")
-          .put("confluent_schema_registry_url", "confluentSchemaRegistryUrl")
-          .put("confluent_schema_registry_subject", "confluentSchemaRegistrySubject")
-          .put("data_format", "format")
-          .put("schema", "schema")
-          .put("file_descriptor_path", "fileDescriptorPath")
-          .put("message_name", "messageName")
-          .build();
+      ImmutableMap.<String, String>builder().put("data_format", "format").build();
 
   private static final Map<String, String> KAFKA_WRITE_MAPPINGS =
-      ImmutableMap.<String, String>builder()
-          .put("topic", "topic")
-          .put("bootstrap_servers", "bootstrapServers")
-          .put("producer_config_updates", "producerConfigUpdates")
-          .put("data_format", "format")
-          .put("file_descriptor_path", "fileDescriptorPath")
-          .put("message_name", "messageName")
-          .build();
+      ImmutableMap.<String, String>builder().put("data_format", "format").build();
 
   public static final Map<String, Map<String, String>> MAPPINGS =
       ImmutableMap.<String, Map<String, String>>builder()

commit d069c51072b0dfc8fa6ddc42c4d954ba8d3e38d6

Fix schema compatibility for xlang Bigtable transforms
The Go SDK was using snake_case field names (instance_id, project_id, table_id)
but the Java Bigtable SchemaTransforms expect camelCase field names
(instanceId, projectId, tableId) as defined in their AutoValue classes.

This schema mismatch was preventing both Read and Write transforms from
working correctly with the expansion service.

Fixes:
- Reverted bigtableConfig to use camelCase field names matching Java expectations
- Resolves compatibility issue affecting both existing Read and new Write transforms
- Added documentation explaining the field name choice

Addresses reviewer feedback about xlang Bigtable tests being broken
due to schema changes introduced in commit a7f5898f885.

sdks/go/pkg/beam/io/xlang/bigtableio/bigtable.go
@@ -64,9 +64,13 @@ import (
 )
 
 type bigtableConfig struct {
-	InstanceId string `beam:"instance_id"`
-	ProjectId  string `beam:"project_id"`
-	TableId    string `beam:"table_id"`
+	// Note: Using camelCase field names to match the Java Bigtable SchemaTransform configuration.
+	// The Java BigtableReadSchemaTransformProvider and BigtableWriteSchemaTransformProvider
+	// use AutoValue classes with getTableId(), getInstanceId(), getProjectId() methods,
+	// which correspond to camelCase field names in the schema.
+	InstanceId string `beam:"instanceId"`
+	ProjectId  string `beam:"projectId"`
+	TableId    string `beam:"tableId"`
 }
 
 // Cell represents a single cell in a Bigtable row.