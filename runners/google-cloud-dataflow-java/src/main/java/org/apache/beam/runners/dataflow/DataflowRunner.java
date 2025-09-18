f9625668197ef74afcd752d38c26a471b8a61665
Yi Hu <yathu@google.com>
2025-09-18 14:27:33 -0400

    Correctly override apache/beam containers for RC on Dataflow runner job submission

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/DataflowRunner.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/DataflowRunner.java
@@ -1027,9 +1027,12 @@ public class DataflowRunner extends PipelineRunner<DataflowPipelineJob> {
             && !updated
             // don't update if the container image is already configured by DataflowRunner
             && !containerImage.equals(getV2SdkHarnessContainerImageForJob(options))) {
+          String imageAndTag =
+              normalizeDataflowImageAndTag(
+                  containerImage.substring(containerImage.lastIndexOf("/")));
           containerImage =
               DataflowRunnerInfo.getDataflowRunnerInfo().getContainerImageBaseRepository()
-                  + containerImage.substring(containerImage.lastIndexOf("/"));
+                  + imageAndTag;
         }
         environmentBuilder.setPayload(
             RunnerApi.DockerPayload.newBuilder()
@@ -1040,6 +1043,21 @@ public class DataflowRunner extends PipelineRunner<DataflowPipelineJob> {
       componentsBuilder.putEnvironments(entry.getKey(), environmentBuilder.build());
     }
     return pipelineBuilder.build();
+  }
+
+  static String normalizeDataflowImageAndTag(String imageAndTag) {
+    if (imageAndTag.startsWith("/beam_python") || imageAndTag.startsWith("/beam_go_")) {
+      int tagIdx = imageAndTag.lastIndexOf(":");
+      if (tagIdx > 0) {
+        // For release candidates, apache/beam_ images has rc tag while Dataflow does not
+        String tag = imageAndTag.substring(tagIdx); // e,g, ":2.xx.0"
+        int mayRc = tag.toLowerCase().lastIndexOf("rc");
+        if (mayRc > 0) {
+          imageAndTag = imageAndTag.substring(0, tagIdx) + tag.substring(0, mayRc);
+        }
+      }
+    }
+    return imageAndTag;
   }
 
   @VisibleForTesting
