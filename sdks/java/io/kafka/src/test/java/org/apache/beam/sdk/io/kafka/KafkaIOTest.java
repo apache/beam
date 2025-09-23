1d37af662f050df98cb728056051e1a32314f94e
Tom Stepp <tom.j.stepp@gmail.com>
2025-09-22 20:51:42 -0700

    Add byRecordKey property to Kafka read compatibility.

--- sdks/java/io/kafka/src/test/java/org/apache/beam/sdk/io/kafka/KafkaIOTest.java
+++ sdks/java/io/kafka/src/test/java/org/apache/beam/sdk/io/kafka/KafkaIOTest.java
@@ -387,7 +387,7 @@ public class KafkaIOTest {
 
   static KafkaIO.Read<Integer, Long> mkKafkaReadTransform(
       int numElements, @Nullable SerializableFunction<KV<Integer, Long>, Instant> timestampFn) {
-    return mkKafkaReadTransform(
+    return mkKafkaReadTransformBase(
         numElements,
         numElements,
         timestampFn,
@@ -401,7 +401,7 @@ public class KafkaIOTest {
 
   static KafkaIO.Read<Integer, Long> mkKafkaReadTransformWithOffsetDedup(
       int numElements, @Nullable SerializableFunction<KV<Integer, Long>, Instant> timestampFn) {
-    return mkKafkaReadTransform(
+    return mkKafkaReadTransformBase(
         numElements,
         numElements,
         timestampFn,
@@ -417,7 +417,7 @@ public class KafkaIOTest {
       int numElements,
       @Nullable SerializableFunction<KV<Integer, Long>, Instant> timestampFn,
       boolean byRecordKey) {
-    return mkKafkaReadTransform(
+    return mkKafkaReadTransformBase(
         numElements,
         numElements,
         timestampFn,
@@ -433,7 +433,7 @@ public class KafkaIOTest {
       int numElements,
       @Nullable SerializableFunction<KV<Integer, Long>, Instant> timestampFn,
       List<String> topics) {
-    return mkKafkaReadTransform(
+    return mkKafkaReadTransformBase(
         numElements,
         numElements,
         timestampFn,
@@ -449,7 +449,7 @@ public class KafkaIOTest {
    * Creates a consumer with two topics, with 10 partitions each. numElements are (round-robin)
    * assigned all the 20 partitions.
    */
-  static KafkaIO.Read<Integer, Long> mkKafkaReadTransform(
+  static KafkaIO.Read<Integer, Long> mkKafkaReadTransformBase(
       int numElements,
       @Nullable Integer maxNumRecords,
       @Nullable SerializableFunction<KV<Integer, Long>, Instant> timestampFn,
@@ -741,7 +741,7 @@ public class KafkaIOTest {
 
     PCollection<Long> input =
         p.apply(
-                mkKafkaReadTransform(
+                mkKafkaReadTransformBase(
                         numElements,
                         numElements,
                         new ValueAsTimestampFn(),
@@ -770,7 +770,7 @@ public class KafkaIOTest {
 
     PCollection<Long> input =
         p.apply(
-                mkKafkaReadTransform(
+                mkKafkaReadTransformBase(
                         numElements,
                         numElements,
                         new ValueAsTimestampFn(),
@@ -800,7 +800,7 @@ public class KafkaIOTest {
 
     PCollection<Long> input =
         p.apply(
-                mkKafkaReadTransform(
+                mkKafkaReadTransformBase(
                         numElements,
                         numElements,
                         new ValueAsTimestampFn(),
@@ -2224,7 +2224,7 @@ public class KafkaIOTest {
 
     PCollection<Long> input =
         p.apply(
-                mkKafkaReadTransform(
+                mkKafkaReadTransformBase(
                         numElements,
                         maxNumRecords,
                         new ValueAsTimestampFn(),
@@ -2301,7 +2301,7 @@ public class KafkaIOTest {
     int startTime = numElements / 20;
 
     p.apply(
-            mkKafkaReadTransform(
+            mkKafkaReadTransformBase(
                     numElements,
                     numElements,
                     new ValueAsTimestampFn(),
