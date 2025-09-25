478ead59007f829e6bed80f66edf1b6975410d3d
Jessica Hsiao <136421644+jh1231223@users.noreply.github.com>
2025-09-24 23:49:00 -0500

    fix

--- sdks/python/apache_beam/runners/portability/fn_api_runner/fn_runner_test.py
+++ sdks/python/apache_beam/runners/portability/fn_api_runner/fn_runner_test.py
@@ -78,6 +78,7 @@ from apache_beam.transforms import window
 from apache_beam.transforms.periodicsequence import PeriodicImpulse
 from apache_beam.utils import timestamp
 from apache_beam.utils import windowed_value
+from contextlib import contextmanager
 
 if statesampler.FAST_SAMPLER:
   DEFAULT_SAMPLING_PERIOD_MS = statesampler.DEFAULT_SAMPLING_PERIOD_MS
@@ -86,6 +87,39 @@ else:
 
 _LOGGER = logging.getLogger(__name__)
 
+@contextmanager
+def patch_portable_runner_for_test():
+    captured = {}
+
+    orig_excepthook = getattr(threading, "excepthook", None)
+    def _capture_excepthook(args):
+        captured.setdefault("exc", args.exc_value)
+      
+    if orig_excepthook is not None:
+        threading.excepthook = _capture_excepthook
+
+    orig_pipeline_run = beam.Pipeline.run
+    def wrapped_pipeline_run(pipeline_self, *a, **kw):
+        result = orig_pipeline_run(pipeline_self, *a, **kw)
+        if hasattr(result, "wait_until_finish"):
+            orig_wait = result.wait_until_finish
+            def wrapped_wait(*wa, **wk):
+                try:
+                    return orig_wait(*wa, **wk)
+                finally:
+                    exc = captured.get("exc")
+                    if exc:
+                        raise exc
+            result.wait_until_finish = wrapped_wait
+        return result
+    beam.Pipeline.run = wrapped_pipeline_run
+
+    try:
+        yield
+    finally:
+        beam.Pipeline.run = orig_pipeline_run
+        if orig_excepthook is not None:
+            threading.excepthook = orig_excepthook
 
 def _matcher_or_equal_to(value_or_matcher):
   """Pass-thru for matchers, and wraps value inputs in an equal_to matcher."""
@@ -108,13 +142,14 @@ def has_urn_and_labels(mi, urn, labels):
 class FnApiRunnerTest(unittest.TestCase):
   def create_pipeline(self, is_drain=False):
     return beam.Pipeline(runner=fn_api_runner.FnApiRunner(is_drain=is_drain))
-
+  
   def test_assert_that(self):
     # TODO: figure out a way for fn_api_runner to parse and raise the
     # underlying exception.
-    with self.assertRaisesRegex(Exception, 'Failed assert'):
-      with self.create_pipeline() as p:
-        assert_that(p | beam.Create(['a', 'b']), equal_to(['a']))
+    with patch_portable_runner_for_test():
+      with self.assertRaisesRegex(Exception, 'Failed assert'):
+        with self.create_pipeline() as p:
+          assert_that(p | beam.Create(['a', 'b']), equal_to(['a']))
 
   def test_create(self):
     with self.create_pipeline() as p:
