cbf40db722b4d58ba7d766e4b649dcdb4069c6d1
kristynsmith <kristynsmith@google.com>
2025-10-07 11:04:50 -0400

    change conditional

--- sdks/python/apache_beam/internal/cloudpickle/cloudpickle.py
+++ sdks/python/apache_beam/internal/cloudpickle/cloudpickle.py
@@ -1353,7 +1353,7 @@ class Pickler(pickle.Pickler):
         """
     if _should_pickle_by_reference(obj):
       return NotImplemented
-    elif self.config.get_code_object_identifier(obj):
+    elif self.config.get_code_object_identifier is not None and self.config.get_code_object_identifier(obj):
       return self._stable_identifier_function_reduce(obj)
     else:
       return self._dynamic_function_reduce(obj)
