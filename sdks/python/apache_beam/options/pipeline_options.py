58192127d4fe1b27b81fa604362156fa4bcf18bc
Danny Mccormick <dannymccormick@google.com>
2025-10-09 13:01:36 -0400

    Add pipeline options piece

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -287,6 +287,10 @@ class _CommaSeparatedListAction(argparse.Action):
 
 
 class PipelineOptions(HasDisplayData):
+  # Set of options which should not be overriden when pipeline options are
+  # being merged (see from_runner_api). This primarily comes up when expanding
+  # the Python expansion service
+  NON_OVERIDABLE_OPTIONS = ['runner', 'experiments']
   """This class and subclasses are used as containers for command line options.
 
   These classes are wrappers over the standard argparse Python module
@@ -592,15 +596,21 @@ class PipelineOptions(HasDisplayData):
         })
 
   @classmethod
-  def from_runner_api(cls, proto_options):
+  def from_runner_api(cls, proto_options, original_options = None):
     def from_urn(key):
       assert key.startswith('beam:option:')
       assert key.endswith(':v1')
       return key[12:-3]
 
-    return cls(
-        **{from_urn(key): value
-           for (key, value) in proto_options.items()})
+    parsed = {from_urn(key): value
+            for (key, value) in proto_options.items()}
+    if original_options is None:
+      return cls(
+          **parsed)
+    for (key, value) in parsed.items():
+      if value is not None and value and key not in cls.NON_OVERIDABLE_OPTIONS:
+        original_options._all_options[key] = value
+    return original_options
 
   def display_data(self):
     return self.get_all_options(drop_default=True, retain_unknown_options=True)
