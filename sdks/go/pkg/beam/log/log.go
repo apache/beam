29d4b35f026a38e976220b822dd71fceda56d7f7
Shunping Huang <shunping@google.com>
2025-10-11 23:47:11 -0400

    Move slog setup logic to sdk.

--- sdks/go/pkg/beam/log/log.go
+++ sdks/go/pkg/beam/log/log.go
@@ -21,8 +21,14 @@ package log
 import (
 	"context"
 	"fmt"
+	"log"
+	"log/slog"
 	"os"
+	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/golang-cz/devslog"
 )
 
 // Severity is the severity of the log message.
@@ -35,6 +41,11 @@ const (
 	SevWarn
 	SevError
 	SevFatal
+)
+
+var (
+	LogLevel = "info" // The logging level for slog. Valid values are `debug`, `info`, `warn` or `error`. Default is `info`.
+	LogKind  = "text" // The logging format for slog. Valid values are `dev', 'json', or 'text'. Default is `text`.
 )
 
 // Logger is a context-aware logging backend. The richer context allows for
@@ -189,4 +200,52 @@ func Exitf(ctx context.Context, format string, v ...any) {
 func Exitln(ctx context.Context, v ...any) {
 	Output(ctx, SevFatal, 1, fmt.Sprintln(v...))
 	os.Exit(1)
+}
+
+func SetupLoggingWithDefault() {
+	var logLevel = new(slog.LevelVar)
+	var logHandler slog.Handler
+	loggerOutput := os.Stderr
+	handlerOpts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+	switch strings.ToLower(LogLevel) {
+	case "debug":
+		logLevel.Set(slog.LevelDebug)
+		handlerOpts.AddSource = true
+	case "info":
+		logLevel.Set(slog.LevelInfo)
+	case "warn":
+		logLevel.Set(slog.LevelWarn)
+	case "error":
+		logLevel.Set(slog.LevelError)
+	default:
+		log.Fatalf("Invalid value for log_level: %v, must be 'debug', 'info', 'warn', or 'error'", LogLevel)
+	}
+	switch strings.ToLower(LogKind) {
+	case "dev":
+		logHandler =
+			devslog.NewHandler(loggerOutput, &devslog.Options{
+				TimeFormat:         "[" + time.RFC3339Nano + "]",
+				StringerFormatter:  true,
+				HandlerOptions:     handlerOpts,
+				StringIndentation:  false,
+				NewLineAfterLog:    true,
+				MaxErrorStackTrace: 3,
+			})
+	case "json":
+		logHandler = slog.NewJSONHandler(loggerOutput, handlerOpts)
+	case "text":
+		logHandler = slog.NewTextHandler(loggerOutput, handlerOpts)
+	default:
+		log.Fatalf("Invalid value for log_kind: %v, must be 'dev', 'json', or 'text'", LogKind)
+	}
+
+	slog.SetDefault(slog.New(logHandler))
+}
+
+func SetupLogging(logLevel, logKind string) {
+	LogLevel = logLevel
+	LogKind = logKind
+	SetupLoggingWithDefault()
 }
