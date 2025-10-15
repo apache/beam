0520967a37969d638fe4a24a89a54be6141e1325
liferoad <huxiangqian@gmail.com>
2025-10-15 14:56:31 -0400

    perf(subprocess_server): add grpc keepalive options to improve connection stability
    Add various grpc keepalive and ping-related options to prevent connection drops during long-running operations. The new settings help maintain active connections and detect failures faster.

--- sdks/python/apache_beam/utils/subprocess_server.py
+++ sdks/python/apache_beam/utils/subprocess_server.py
@@ -185,8 +185,16 @@ class SubprocessServer(object):
     try:
       process, endpoint = self.start_process()
       wait_secs = .1
-      channel_options = [("grpc.max_receive_message_length", -1),
-                         ("grpc.max_send_message_length", -1)]
+      channel_options = [
+          ("grpc.max_receive_message_length", -1),
+          ("grpc.max_send_message_length", -1),
+          ("grpc.keepalive_time_ms", 30000),
+          ("grpc.keepalive_timeout_ms", 300000),
+          ("grpc.http2.max_pings_without_data", 0),
+          ("grpc.keepalive_permit_without_calls", True),
+          ("grpc.http2.min_recv_ping_interval_without_data_ms", 300000),
+          ("grpc.http2.min_sent_ping_interval_without_data_ms", 10000),
+      ]
       self._grpc_channel = grpc.insecure_channel(
           endpoint, options=channel_options)
       channel_ready = grpc.channel_ready_future(self._grpc_channel)
