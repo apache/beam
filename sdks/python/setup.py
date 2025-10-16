53eabb583010ae316afc841e1fb2786278c06d4e
Valentyn Tymofieiev <valentyn@google.com>
2025-10-16 14:46:28 -0700

    Attempt to unpin GRPC.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -383,8 +383,9 @@ if __name__ == '__main__':
           'fastavro>=0.23.6,<2',
           'fasteners>=0.3,<1.0',
           # TODO(https://github.com/grpc/grpc/issues/37710): Unpin grpc
-          'grpcio>=1.33.1,<2,!=1.48.0,!=1.59.*,!=1.60.*,!=1.61.*,!=1.62.0,!=1.62.1,<1.66.0; python_version <= "3.12"',  # pylint: disable=line-too-long
-          'grpcio>=1.67.0; python_version >= "3.13"',
+          #'grpcio>=1.33.1,<2,!=1.48.0,!=1.59.*,!=1.60.*,!=1.61.*,!=1.62.0,!=1.62.1,<1.66.0; python_version <= "3.12"',  # pylint: disable=line-too-long
+          #'grpcio>=1.67.0; python_version >= "3.13"',
+          'grpcio==1.76.0rc1',
           'hdfs>=2.1.0,<3.0.0',
           'httplib2>=0.8,<0.23.0',
           'jsonschema>=4.0.0,<5.0.0',
