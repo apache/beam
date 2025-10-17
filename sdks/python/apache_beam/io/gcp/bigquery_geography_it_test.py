68f13ba3112b25f900fbdcffc68590f93bf830ab
liferoad <huxiangqian@gmail.com>
2025-10-17 10:33:04 -0400

    test(bigquery): skip geography test when expansion jars not available
    Skip the geography storage write API test when EXPANSION_JARS environment variable is not set, indicating the required jars haven't been built

--- sdks/python/apache_beam/io/gcp/bigquery_geography_it_test.py
+++ sdks/python/apache_beam/io/gcp/bigquery_geography_it_test.py
@@ -428,7 +428,10 @@ class BigQueryGeographyIntegrationTests(unittest.TestCase):
     hc.assert_that(p, hc.all_of(*pipeline_verifiers))
 
   @pytest.mark.uses_gcp_java_expansion_service
-  @pytest.mark.it_postcommit
+  @unittest.skipUnless(
+      os.environ.get('EXPANSION_JARS'),
+      "EXPANSION_JARS environment var is not provided, "
+      "indicating that jars have not been built")
   def test_geography_storage_write_api(self):
     """Test GEOGRAPHY with Storage Write API method."""
     table_name = 'geography_storage_write'
