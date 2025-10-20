aec1d2a16610567d7791de2116f981d84115282b
liferoad <huxiangqian@gmail.com>
2025-10-20 11:08:44 -0400

    more tunes on the grpc options

--- sdks/python/apache_beam/utils/subprocess_server.py
+++ sdks/python/apache_beam/utils/subprocess_server.py
@@ -188,14 +188,24 @@ class SubprocessServer(object):
       channel_options = [
           ("grpc.max_receive_message_length", -1),
           ("grpc.max_send_message_length", -1),
-          ("grpc.keepalive_time_ms",
-           120000),  # Increased to 120s to further reduce ping frequency
-          ("grpc.keepalive_timeout_ms", 300000),
+          # Default: 30000ms (30s), increased to 180s to reduce ping frequency
+          ("grpc.keepalive_time_ms", 180000),
+          # Default: 5000ms (5s), increased to 10 minutes for stability
+          ("grpc.keepalive_timeout_ms", 600000),
+          # Default: 2, set to 0 to allow unlimited pings without data
           ("grpc.http2.max_pings_without_data", 0),
+          # Default: False, set to True to allow keepalive pings when no calls
           ("grpc.keepalive_permit_without_calls", True),
-          ("grpc.http2.min_recv_ping_interval_without_data_ms", 300000),
-          ("grpc.http2.min_sent_ping_interval_without_data_ms",
-           60000),  # Increased to 60s to be more conservative
+          # Default: 300000ms (5min), increased to 10min for stability
+          ("grpc.http2.min_recv_ping_interval_without_data_ms", 600000),
+          # Default: 300000ms (5min), increased to 120s for conservative pings
+          ("grpc.http2.min_sent_ping_interval_without_data_ms", 120000),
+          # Default: 2, set to 0 to allow unlimited ping strikes
+          ("grpc.http2.max_ping_strikes", 0),
+          # Default: 0 (disabled), enable socket reuse for better handling
+          ("grpc.so_reuseport", 1),
+          # Default: 0 (disabled), set 30s TCP timeout for connection control
+          ("grpc.tcp_user_timeout_ms", 30000),
       ]
       self._grpc_channel = grpc.insecure_channel(
           endpoint, options=channel_options)
