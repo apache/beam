f95294e0d1ea7df6e0747594f771a5fe06eb0782
liferoad <huxiangqian@gmail.com>
2025-10-21 09:45:13 -0400

    only fix flaky tests

--- sdks/python/apache_beam/utils/subprocess_server.py
+++ sdks/python/apache_beam/utils/subprocess_server.py
@@ -185,28 +185,8 @@ class SubprocessServer(object):
     try:
       process, endpoint = self.start_process()
       wait_secs = .1
-      channel_options = [
-          ("grpc.max_receive_message_length", -1),
-          ("grpc.max_send_message_length", -1),
-          # Default: 30000ms (30s), increased to 180s to reduce ping frequency
-          ("grpc.keepalive_time_ms", 180000),
-          # Default: 5000ms (5s), increased to 10 minutes for stability
-          ("grpc.keepalive_timeout_ms", 600000),
-          # Default: 2, set to 0 to allow unlimited pings without data
-          ("grpc.http2.max_pings_without_data", 0),
-          # Default: False, set to True to allow keepalive pings when no calls
-          ("grpc.keepalive_permit_without_calls", True),
-          # Default: 300000ms (5min), increased to 10min for stability
-          ("grpc.http2.min_recv_ping_interval_without_data_ms", 600000),
-          # Default: 300000ms (5min), increased to 120s for conservative pings
-          ("grpc.http2.min_sent_ping_interval_without_data_ms", 120000),
-          # Default: 2, set to 0 to allow unlimited ping strikes
-          ("grpc.http2.max_ping_strikes", 0),
-          # Default: 0 (disabled), enable socket reuse for better handling
-          ("grpc.so_reuseport", 1),
-          # Default: 0 (disabled), set 30s TCP timeout for connection control
-          ("grpc.tcp_user_timeout_ms", 30000),
-      ]
+      channel_options = [("grpc.max_receive_message_length", -1),
+                         ("grpc.max_send_message_length", -1)]
       self._grpc_channel = grpc.insecure_channel(
           endpoint, options=channel_options)
       channel_ready = grpc.channel_ready_future(self._grpc_channel)
