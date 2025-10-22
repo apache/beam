6631ef0d3fb18243ee8137ac2e72ae64c9c49d35
Shunping Huang <shunping@google.com>
2025-10-22 16:34:01 -0400

    Fix lints.

--- sdks/python/apache_beam/examples/cookbook/ordered_window_elements/streaming_test.py
+++ sdks/python/apache_beam/examples/cookbook/ordered_window_elements/streaming_test.py
@@ -25,12 +25,12 @@ from parameterized import parameterized
 from parameterized import parameterized_class
 
 import apache_beam as beam
-from apache_beam.examples.cookbook.ordered_window_elements.streaming import OrderedWindowElements
 from apache_beam.examples.cookbook.ordered_window_elements.streaming import BufferStateType
+from apache_beam.examples.cookbook.ordered_window_elements.streaming import OrderedWindowElements
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
-from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.transforms.periodicsequence import PeriodicImpulse
 from apache_beam.transforms.periodicsequence import RebaseMode
 from apache_beam.utils.timestamp import Timestamp
