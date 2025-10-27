50477d8bd9b5aab14b16158ffbbe10b79453c67a
aIbrahiim <abdoibrahim1017@gmail.com>
2025-10-28 02:57:59 +0300

    ix KafkaIO perf test assertion logic

--- sdks/python/apache_beam/io/external/xlang_kafkaio_perf_test.py
+++ sdks/python/apache_beam/io/external/xlang_kafkaio_perf_test.py
@@ -115,15 +115,16 @@ class _KafkaIOSDFReadPerfTest(LoadTest):
         | 'Measure time' >> beam.ParDo(MeasureTime(self.metrics_namespace)))
 
   def cleanup(self):
-    # assert number of records after test pipeline run
     total_messages = self._metrics_monitor.get_counter_metric(
         self.result, CountMessages.LABEL)
     expected_records = self.input_options['num_records']
 
     assert total_messages >= expected_records, (
-        f"Expected at least {expected_records} messages, but got {total_messages}")
+        f"Expected at least {expected_records} messages, but got "
+        f"{total_messages}")
 
-    _LOGGER.info(f"Read {total_messages} messages (expected: {expected_records})")
+    _LOGGER.info(f"Read {total_messages} messages (expected: "
+                 f"{expected_records})")
 
 
 if __name__ == '__main__':
