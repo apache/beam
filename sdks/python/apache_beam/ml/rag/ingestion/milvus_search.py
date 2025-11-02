119108f44233bc504975b613333f440f560d2659
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-11-02 14:59:41 +0000

    sdks/python: fix linting issues

--- sdks/python/apache_beam/ml/rag/ingestion/milvus_search.py
+++ sdks/python/apache_beam/ml/rag/ingestion/milvus_search.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import logging
 from dataclasses import dataclass
 from dataclasses import field
