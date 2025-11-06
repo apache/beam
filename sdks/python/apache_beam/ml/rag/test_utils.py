17ba353e2fcd29cf418fd97aac2d019b0884b379
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-11-06 17:42:48 +0000

    sdks/python: fix linting issues

--- sdks/python/apache_beam/ml/rag/test_utils.py
+++ sdks/python/apache_beam/ml/rag/test_utils.py
@@ -27,17 +27,18 @@ from typing import Optional
 from typing import cast
 
 import yaml
-from testcontainers.core.config import testcontainers_config
-from testcontainers.core.generic import DbContainer
-from testcontainers.milvus import MilvusContainer
 from pymilvus import CollectionSchema
 from pymilvus import FieldSchema
 from pymilvus import MilvusClient
 from pymilvus.exceptions import MilvusException
 from pymilvus.milvus_client import IndexParams
 
-from apache_beam.ml.rag.types import Chunk
+from testcontainers.core.config import testcontainers_config
+from testcontainers.core.generic import DbContainer
+from testcontainers.milvus import MilvusContainer
+
 from apache_beam.ml.rag.enrichment.milvus_search import MilvusConnectionParameters
+from apache_beam.ml.rag.types import Chunk
 from apache_beam.ml.rag.utils import retry_with_backoff
 
 _LOGGER = logging.getLogger(__name__)
