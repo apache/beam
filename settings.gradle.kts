c3813abda1956776ee09d74135811119040c836e
aIbrahiim <abdoibrahim1017@gmail.com>
2025-11-27 12:53:24 +0200

    Fix flaky Prism Loopback test and remove deprecated Gradle import

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
 
 pluginManagement {
     plugins {
