afd1be745d3bcd01a3f722d02c78b1225c8f4f27
Arunodoy18 <arunodoy630@gmail.com>
2025-12-02 20:45:36 +0530

    Fix Javadoc Unicode rendering issue with RIGHT SINGLE QUOTATION MARK
    - Add docencoding UTF-8 option to Javadoc configuration in BeamModulePlugin
    - Replace U+2019 (RIGHT SINGLE QUOTATION MARK) with ASCII apostrophe in source files
    - Fixes rendering issue where Unicode apostrophes show as â in generated Javadoc
    
    Fixes rendering of text like 'doesn't' which was showing as 'doesnât' in HTML output.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1424,6 +1424,8 @@ class BeamModulePlugin implements Plugin<Project> {
       // Configures javadoc plugin and ensure check runs javadoc.
       project.tasks.withType(Javadoc) {
         options.encoding = 'UTF-8'
+        options.docEncoding = 'UTF-8'
+        options.charSet = 'UTF-8'
         options.addBooleanOption('Xdoclint:-missing', true)
       }
       project.check.dependsOn project.javadoc
