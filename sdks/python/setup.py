86bbb996a716a49bc5406e11d44cc4f98518bf9b
Shunping Huang <shunping@google.com>
2025-12-04 14:23:35 -0500

    Set google-genai upper bound

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -492,6 +492,7 @@ if __name__ == '__main__':
               'google-cloud-vision>=2,<4',
               'google-cloud-recommendations-ai>=0.1.0,<0.11.0',
               'google-cloud-aiplatform>=1.26.0,<1.129.0',
+              'google-genai<1.53.0',
               'cloud-sql-python-connector>=1.18.2,<2.0.0',
               'python-tds>=1.16.1',
               'pg8000>=1.31.5',
