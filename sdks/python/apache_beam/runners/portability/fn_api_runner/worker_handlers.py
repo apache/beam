ad2588a424887feeebfe503fbd0b7548777635fc
Shunping Huang <shunping@google.com>
2025-12-05 12:24:37 -0500

    Fix too-many-pings on FnAPI runner under grpc mode

--- sdks/python/apache_beam/runners/portability/fn_api_runner/worker_handlers.py
+++ sdks/python/apache_beam/runners/portability/fn_api_runner/worker_handlers.py
@@ -468,7 +468,11 @@ class GrpcServer(object):
     options = [("grpc.max_receive_message_length", -1),
                ("grpc.max_send_message_length", -1),
                ("grpc.http2.max_pings_without_data", 0),
-               ("grpc.http2.max_ping_strikes", 0)]
+               ("grpc.http2.max_ping_strikes", 0),
+               # match `grpc.keepalive_time_ms` defined in the client
+               # (channel_factory.py)
+               ("grpc.http2.min_ping_interval_without_data_ms", 20_000),
+               ]
 
     self.state = state
     self.provision_info = provision_info
