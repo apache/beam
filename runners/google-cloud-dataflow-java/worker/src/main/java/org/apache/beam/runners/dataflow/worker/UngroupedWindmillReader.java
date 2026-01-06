f718f9caf0b493782bddb94cccaca1bc510cc0c0
Suvrat1629 <suvrat1629@gmail.com>
2026-01-06 21:32:37 +0530

    fix

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/UngroupedWindmillReader.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/UngroupedWindmillReader.java
@@ -1,13 +1,13 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -152,8 +152,13 @@ class UngroupedWindmillReader<T> extends NativeReader<WindowedValue<T>> {
             (T) KV.of(decode(kvCoder.getKeyCoder(), key), decode(kvCoder.getValueCoder(), data));
 
         return WindowedValues.of(
-            result, timestampMillis, windows, paneInfo,
-            recordId, recordOffset, drainingValueFromUpstream);
+            result,
+            timestampMillis,
+            windows,
+            paneInfo,
+            recordId,
+            recordOffset,
+            drainingValueFromUpstream);
       } else {
         notifyElementRead(data.available() + metadata.available());
         return WindowedValues.of(
@@ -161,7 +166,9 @@ class UngroupedWindmillReader<T> extends NativeReader<WindowedValue<T>> {
             timestampMillis,
             windows,
             paneInfo,
-            recordId, recordOffset, drainingValueFromUpstream);
+            recordId,
+            recordOffset,
+            drainingValueFromUpstream);
       }
     }
 
