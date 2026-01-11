2e5d966079fb97fdf145381dfe346fc6e023c498
XQ Hu <huxiangqian@gmail.com>
2026-01-10 20:59:28 -0500

    lint

--- sdks/python/apache_beam/ml/inference/test_resources/vertex_ai_custom_prediction/echo_server.py
+++ sdks/python/apache_beam/ml/inference/test_resources/vertex_ai_custom_prediction/echo_server.py
@@ -19,18 +19,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    # Echo back the instances
-    return jsonify({
-        "predictions": [{"echo": inst} for inst in data.get('instances', [])],
-        "deployedModelId": "echo-model"
-    })
+  data = request.get_json()
+  # Echo back the instances
+  return jsonify({
+      "predictions": [{
+          "echo": inst
+      } for inst in data.get('instances', [])],
+      "deployedModelId": "echo-model"
+  })
+
 
 @app.route('/health', methods=['GET'])
 def health():
-    return 'OK', 200
+  return 'OK', 200
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
