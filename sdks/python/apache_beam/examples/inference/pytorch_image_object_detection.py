63f8e42002140ea668436a57752880a359b88098
Vitaly Terentyev <vitaly.terentyev@akvelon.com>
2026-01-21 22:21:05 +0400

    Add project argument

--- sdks/python/apache_beam/examples/inference/pytorch_image_object_detection.py
+++ sdks/python/apache_beam/examples/inference/pytorch_image_object_detection.py
@@ -244,6 +244,8 @@ def parse_known_args(argv):
   # I/O & runtime
   parser.add_argument('--mode', default='batch', choices=['batch'])
   parser.add_argument(
+      '--project', default='apache-beam-testing', help='GCP project ID')
+  parser.add_argument(
       '--output_table',
       required=True,
       help='BigQuery output table: dataset.table')
