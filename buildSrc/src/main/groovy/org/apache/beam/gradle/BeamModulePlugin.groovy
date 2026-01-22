b68aedbbfc2aa42e3827dda82a4516cfd5405b09
Danny McCormick <dannymccormick@google.com>
2026-01-22 17:01:28 -0500

    Update Go version from 1.25.2 to 1.25.6

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -2345,7 +2345,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
       // This sets the whole project Go version.
       // The latest stable Go version can be checked at https://go.dev/dl/
-      project.ext.goVersion = "go1.25.2"
+      project.ext.goVersion = "go1.25.6"
 
       // Minor TODO: Figure out if we can pull out the GOCMD env variable after goPrepare script
       // completion, and avoid this GOBIN substitution.
