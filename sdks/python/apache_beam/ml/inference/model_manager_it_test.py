68690217a602984eb00963687a5fe1cef39ec16e
AMOOOMA <ruilong.jiang@outlook.com>
2026-02-04 23:26:22 +0000

    Fix lint

--- sdks/python/apache_beam/ml/inference/model_manager_it_test.py
+++ sdks/python/apache_beam/ml/inference/model_manager_it_test.py
@@ -16,14 +16,17 @@
 #
 
 import unittest
+
 import apache_beam as beam
 from apache_beam.ml.inference.base import RunInference
 from apache_beam.testing.test_pipeline import TestPipeline
-from apache_beam.testing.util import assert_that, equal_to
+from apache_beam.testing.util import assert_that
+from apache_beam.testing.util import equal_to
 
 # pylint: disable=ungrouped-imports
 try:
   import torch
+
   from apache_beam.ml.inference.huggingface_inference import HuggingFacePipelineModelHandler
 except ImportError as e:
   raise unittest.SkipTest(
