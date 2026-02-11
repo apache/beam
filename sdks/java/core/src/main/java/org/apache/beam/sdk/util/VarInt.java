6f593bb1c140f455652a4d476acf2ec3767e3825
Steven van Rossum <sjvanrossum@google.com>
2026-02-11 19:10:24 +0000

    Use vendored protobuf

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/VarInt.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/VarInt.java
@@ -17,11 +17,11 @@
  */
 package org.apache.beam.sdk.util;
 
-import com.google.protobuf.CodedOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import org.apache.beam.vendor.grpc.v1p69p0.com.google.protobuf.CodedOutputStream;
 
 /**
  * Variable-length encoding for integers.
