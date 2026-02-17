80feea40ae67b4e1b6506eaa5ddaf9aa608d7df5
Ahmed Abualsaud <a.abualsaud98@gmail.com>
2026-02-17 17:33:44 -0500

    update rest catalog config

--- sdks/java/io/iceberg/src/test/java/org/apache/beam/sdk/io/iceberg/catalog/RESTCatalogBLMSIT.java
+++ sdks/java/io/iceberg/src/test/java/org/apache/beam/sdk/io/iceberg/catalog/RESTCatalogBLMSIT.java
@@ -17,53 +17,40 @@
  */
 package org.apache.beam.sdk.io.iceberg.catalog;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import java.io.IOException;
 import java.util.Map;
 import org.apache.beam.vendor.guava.v32_1_2_jre.com.google.common.collect.ImmutableMap;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.iceberg.CatalogUtil;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.rest.RESTCatalog;
 import org.junit.BeforeClass;
 
 /** Tests for {@link org.apache.iceberg.rest.RESTCatalog} using BigLake Metastore. */
 public class RESTCatalogBLMSIT extends IcebergCatalogBaseIT {
-  private static final String BIGLAKE_URI =
-      "https://biglake.googleapis.com/iceberg/v1beta/restcatalog";
   private static Map<String, String> catalogProps;
 
   @BeforeClass
-  public static void setup() throws IOException {
-    GoogleCredentials credentials =
-        ServiceAccountCredentials.getApplicationDefault()
-            .createScoped("https://www.googleapis.com/auth/cloud-platform");
-    credentials.refreshIfExpired();
-    String accessToken = credentials.getAccessToken().getTokenValue();
-
+  public static void setup() {
     catalogProps =
         ImmutableMap.<String, String>builder()
             .put("type", "rest")
-            .put("uri", BIGLAKE_URI)
-            .put("warehouse", "gs://managed-iceberg-integration-tests-us-central1")
-            .put("oauth2-server-uri", "https://oauth2.googleapis.com/token")
+            .put("uri", "https://biglake.googleapis.com/iceberg/v1/restcatalog")
+            .put("warehouse", warehouse(RESTCatalogBLMSIT.class))
             .put("header.x-goog-user-project", OPTIONS.getProject())
             .put("rest-metrics-reporting-enabled", "false")
-            .put("token", accessToken)
             .put("io-impl", "org.apache.iceberg.gcp.gcs.GCSFileIO")
+            .put("rest.auth.type", "org.apache.iceberg.gcp.auth.GoogleAuthManager")
             .build();
   }
 
   @Override
   public String type() {
-    return "rest";
+    return "biglake";
   }
 
   @Override
   public Catalog createCatalog() {
-    return CatalogUtil.loadCatalog(
-        RESTCatalog.class.getName(), catalogName, catalogProps, new Configuration());
+    RESTCatalog restCatalog = new RESTCatalog();
+    restCatalog.initialize(catalogName, catalogProps);
+    return restCatalog;
   }
 
   @Override
