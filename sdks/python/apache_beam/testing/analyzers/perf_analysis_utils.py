45ad7401d77d7324f3b25f542cdca67682d7170d
liferoad <huxiangqian@gmail.com>
2026-02-20 14:26:41 -0500

    Remove unused epsilon variable from `_find_valid_change_points`.

--- sdks/python/apache_beam/testing/analyzers/perf_analysis_utils.py
+++ sdks/python/apache_beam/testing/analyzers/perf_analysis_utils.py
@@ -304,7 +304,6 @@ def filter_change_points_by_median_threshold(
   value of threshold is 0.05.
   """
   valid_change_points = []
-  epsilon = 1e-10  # needed to avoid division by zero.
 
   for idx in change_points:
     if idx == 0 or idx == len(data):
