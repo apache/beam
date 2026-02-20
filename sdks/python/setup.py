d1ade2188d19f4251c24dc31699f8bcc142ec48c
Valentyn Tymofieiev <valentyn@google.com>
2026-02-19 18:39:42 -0800

    Relax grpcio constraints even though we know some versions are bad to simplify dependency resolution.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -380,10 +380,10 @@ if __name__ == '__main__':
           'envoy-data-plane<0.3.0; python_version < "3.13"',
           'fastavro>=0.23.6,<2',
           'fasteners>=0.3,<1.0',
-          'grpcio>=1.33.1,<2,!=1.48.0,!=1.59.*,!=1.60.*,!=1.61.*,!=1.62.0,!=1.62.1,!=1.66.*,!=1.67.*,!=1.68.*,!=1.69.*,!=1.70.*,!=1.71.*,!=1.72.*,!=1.73.*,!=1.74.*,!=1.75.*,!=1.76.*,!=1.77.*,!=1.78.0; python_version <= "3.12"',  # pylint: disable=line-too-long
-          # TODO(https://github.com/grpc/grpc/issues/37710): Consolidate bounds
-          # across python versions once 1.78.1 is avaliable.
-          'grpcio>=1.67.0,<2; python_version >= "3.13"',
+          # Known grpcio versions that had issues:
+          # 1.48.x, 1.59.0-1.62.1, 1.66-1.78.0.
+          # Not adding these constraints to simplify the requrirements.
+          'grpcio>=1.33.1,<2',
           'httplib2>=0.8,<0.32.0',
           'jsonpickle>=3.0.0,<4.0.0',
           # numpy can have breaking changes in minor versions.
