20b39c6abeb3b5122395e3de8ea3d5d09cb5b626
Mansi Singh <singh.m1@northeastern.edu>
2026-02-21 16:27:16 -0800

    Address review feedback on GroupBy snippets
    - Fix groupby_attr_expr.py: move if test block inside pipeline context, move beam.Map(print) to else branch
    - Fix groupby_two_exprs.py: same fixes, restore [START/END] markers

--- sdks/python/apache_beam/examples/snippets/transforms/aggregation/groupby_attr_expr.py
+++ sdks/python/apache_beam/examples/snippets/transforms/aggregation/groupby_attr_expr.py
@@ -53,12 +53,13 @@ def groupby_attr_expr(test=None):
     grouped = (
         p
         | beam.Create(GROCERY_LIST)
-        | beam.GroupBy('recipe', is_berry=lambda x: 'berry' in x.fruit)
-        | beam.Map(print))
+        | beam.GroupBy('recipe', is_berry=lambda x: 'berry' in x.fruit))
     # [END groupby_attr_expr]
 
-  if test:
-    test(grouped)
+    if test:
+      test(grouped)
+    else:
+      grouped | beam.Map(print)
 
 
 if __name__ == '__main__':
