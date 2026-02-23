26b18c6e6f14e13082e565f6ef5e766d294fa25f
Derrick Williams <myutat@gmail.com>
2026-02-23 18:52:48 -0500

    Revert "[yaml] : switch js2py to pythonmonkey (#37560)" (#37665)
    This reverts commit 5d6cb04ea05b5e981ac7fbf0122135870d7b2a4c.

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -575,6 +575,8 @@ if __name__ == '__main__':
               'docstring-parser>=0.15,<1.0',
               'jinja2>=3.0,<3.2',
               'virtualenv-clone>=0.5,<1.0',
+              # https://github.com/PiotrDabkowski/Js2Py/issues/317
+              'js2py>=0.74,<1; python_version<"3.12"',
               'jsonschema>=4.0.0,<5.0.0',
           ] + dataframe_dependency,
           # Keep the following dependencies in line with what we test against
