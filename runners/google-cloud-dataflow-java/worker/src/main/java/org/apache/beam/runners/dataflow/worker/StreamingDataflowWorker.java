4904ff5c6bdf7671927132c81e388dfc93c7dbe8
scwhittle <scwhittle@users.noreply.github.com>
2026-03-16 09:44:36 +0100

    add comment

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorker.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorker.java
@@ -463,6 +463,8 @@ public final class StreamingDataflowWorker {
     @SuppressWarnings("methodref.receiver.bound")
     WorkCommitter workCommitter =
         StreamingEngineWorkCommitter.builder()
+            // Use a separate stream pool for each committer. This ensures the commit
+            // threads are fully isolated.
             .setCommitWorkStreamFactory(
                 () ->
                     WindmillStreamPool.create(
