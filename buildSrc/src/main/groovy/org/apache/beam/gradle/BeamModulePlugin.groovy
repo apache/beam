d83275adc6184c462dee35072fdf62651e84a6ae
Radek Stankiewicz <radoslaws@google.com>
2026-03-21 17:38:59 +0100

    remove void

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1581,8 +1581,6 @@ class BeamModulePlugin implements Plugin<Project> {
             "StringCaseLocaleUsage",
             // DoFn methods are executed reflectively at pipeline runtime
             "UnusedMethod",
-            // Void is a valid element type of DoFn elements
-            "VoidUsed",
           ]
           disabledChecks.each {
             options.errorprone.errorproneArgs.add("-Xep:${it}:OFF")
