5ffdb7615c3b818c94785450f4d42e8db8ba8a70
Derrick Williams <derrickaw@google.com>
2026-04-06 01:56:37 +0000

    add google-adk pin

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -164,7 +164,7 @@ milvus_dependency = ['pymilvus>=2.5.10,<3.0.0']
 
 ml_base = [
     'embeddings>=0.0.4', # 0.0.3 crashes setuptools
-    'google-adk',
+    'google-adk>=1.28.1',
     'onnxruntime',
     'langchain',
     'sentence-transformers>=2.2.2',
