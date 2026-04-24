0dc09d1f45582f101851bc960b7d2ee8613d0fcf
bambadiouf1 <bambadiouf@google.com>
2026-04-24 18:16:52 -0500

    default client infos

--- sdks/python/apache_beam/runners/dataflow/internal/clients/dataflow/dataflow_v1b3_client.py
+++ sdks/python/apache_beam/runners/dataflow/internal/clients/dataflow/dataflow_v1b3_client.py
@@ -3,7 +3,6 @@
 from apitools.base.py import base_api
 from . import dataflow_v1b3_messages as messages
 
-
 class DataflowV1b3(base_api.BaseApiClient):
   """Generated client library for service dataflow version v1b3."""
 
@@ -14,8 +13,8 @@ class DataflowV1b3(base_api.BaseApiClient):
   _PACKAGE = 'dataflow'
   _SCOPES = ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/compute']
   _VERSION = 'v1b3'
-  _CLIENT_ID = '1042881264118.apps.googleusercontent.com'
-  _CLIENT_SECRET = 'x_Tw5K8nnjoRAqULM9PFAC2b'
+  _CLIENT_ID = 'CLIENT_ID'
+  _CLIENT_SECRET = 'CLIENT_SECRET'
   _USER_AGENT = 'x_Tw5K8nnjoRAqULM9PFAC2b'
   _CLIENT_CLASS_NAME = 'DataflowV1b3'
   _URL_VERSION = 'v1b3'
