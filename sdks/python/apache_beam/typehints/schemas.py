8b27fcd763ad05b4acebdfe3d73466d3f5b2249a
Yi Hu <yathu@google.com>
2026-04-29 13:35:58 -0400

    fix formatting

--- sdks/python/apache_beam/typehints/schemas.py
+++ sdks/python/apache_beam/typehints/schemas.py
@@ -260,28 +260,32 @@ def schema_field(
 
 def _python_any_schema_pb2(has_repr):
   # A portable schema matches FastPrimitivesCoder encoded values
-  representation = schema_pb2.FieldType(
-      nullable=False,
-      row_type=schema_pb2.RowType(
-          schema=schema_pb2.Schema(
-              fields=[
-                  schema_pb2.Field(
-                      name=_PYTHON_ANY_FIELD_TYPE_BYTE,
-                      type=schema_pb2.FieldType(
-                          atomic_type=schema_pb2.BYTE, nullable=False)),
-                  schema_pb2.Field(
-                      name=_PYTHON_ANY_FIELD_PAYLOAD,
-                      type=schema_pb2.FieldType(
-                          atomic_type=schema_pb2.BYTES, nullable=False))
-              ],
-              options=[
-                  schema_pb2.Option(
-                      name=_SCHEMA_OPTION_STATIC_ENCODING,
-                      type=schema_pb2.FieldType(atomic_type=schema_pb2.BOOLEAN),
-                      value=schema_pb2.FieldValue(
-                          atomic_value=schema_pb2.AtomicTypeValue(
-                              boolean=True)))
-              ]))) if has_repr else None
+  if has_repr:
+    representation = schema_pb2.FieldType(
+        nullable=False,
+        row_type=schema_pb2.RowType(
+            schema=schema_pb2.Schema(
+                fields=[
+                    schema_pb2.Field(
+                        name=_PYTHON_ANY_FIELD_TYPE_BYTE,
+                        type=schema_pb2.FieldType(
+                            atomic_type=schema_pb2.BYTE, nullable=False)),
+                    schema_pb2.Field(
+                        name=_PYTHON_ANY_FIELD_PAYLOAD,
+                        type=schema_pb2.FieldType(
+                            atomic_type=schema_pb2.BYTES, nullable=False))
+                ],
+                options=[
+                    schema_pb2.Option(
+                        name=_SCHEMA_OPTION_STATIC_ENCODING,
+                        type=schema_pb2.FieldType(
+                            atomic_type=schema_pb2.BOOLEAN),
+                        value=schema_pb2.FieldValue(
+                            atomic_value=schema_pb2.AtomicTypeValue(
+                                boolean=True)))
+                ]))) if has_repr else None
+  else:
+    representation = None
 
   return schema_pb2.FieldType(
       logical_type=schema_pb2.LogicalType(
