22c874c785058127d05712010e581909f28acf0b
Tarun Annapareddy <tannapareddy@google.com>
2026-05-04 13:29:07 -0700

    refactor test

--- sdks/go/pkg/beam/artifact/options_test.go
+++ sdks/go/pkg/beam/artifact/options_test.go
@@ -22,48 +22,63 @@ import (
 	structpb "google.golang.org/protobuf/types/known/structpb"
 )
 
-func TestPipelineOptionsAndExperiments(t *testing.T) {
+func TestPipelineOptionsContext(t *testing.T) {
 	ctx := context.Background()
 	if PipelineOptions(ctx) != nil {
 		t.Errorf("empty context should have nil pipeline options")
 	}
-	if len(GetExperiments(PipelineOptions(ctx))) != 0 {
-		t.Errorf("empty context should have no experiments")
-	}
 
+	options, _ := structpb.NewStruct(map[string]interface{}{
+		"options": map[string]interface{}{
+			"experiments": []interface{}{"exp1"},
+		},
+	})
+	ctxWithOpts := WithPipelineOptions(ctx, options)
+	got := PipelineOptions(ctxWithOpts)
+	if got == nil {
+		t.Errorf("context should have pipeline options")
+	}
+}
+
+func TestGetExperiments_Nil(t *testing.T) {
+	if got := GetExperiments(nil); got != nil {
+		t.Errorf("GetExperiments(nil) = %v, want nil", got)
+	}
+}
+
+func TestGetExperiments_Legacy(t *testing.T) {
 	options, _ := structpb.NewStruct(map[string]interface{}{
 		"options": map[string]interface{}{
 			"experiments": []interface{}{"exp1", "exp2"},
 		},
 	})
-	ctx2 := WithPipelineOptions(ctx, options)
-	if PipelineOptions(ctx2) == nil {
-		t.Errorf("context should have pipeline options")
-	}
 	exps := GetExperiments(options)
 	if len(exps) != 2 || exps[0] != "exp1" || exps[1] != "exp2" {
 		t.Errorf("GetExperiments() = %v, want [exp1 exp2]", exps)
 	}
+}
+
+func TestGetExperiments_URN(t *testing.T) {
+	urnOptions, _ := structpb.NewStruct(map[string]interface{}{
+		"beam:option:experiments:v1": []interface{}{"expA", "expB"},
+	})
+	expsURN := GetExperiments(urnOptions)
+	if len(expsURN) != 2 || expsURN[0] != "expA" || expsURN[1] != "expB" {
+		t.Errorf("GetExperiments() = %v, want [expA expB]", expsURN)
+	}
+}
+
+func TestHasExperiment(t *testing.T) {
+	options, _ := structpb.NewStruct(map[string]interface{}{
+		"options": map[string]interface{}{
+			"experiments": []interface{}{"exp1", "exp2"},
+		},
+	})
+
 	if !HasExperiment(options, "exp1") {
 		t.Errorf("HasExperiment(exp1) = false, want true")
 	}
 	if HasExperiment(options, "exp3") {
 		t.Errorf("HasExperiment(exp3) = true, want false")
-	}
-
-	// Test URN Style
-	urnOptions, _ := structpb.NewStruct(map[string]interface{}{
-		"beam:option:experiments:v1": []interface{}{"expA", "expB"},
-	})
-	ctx3 := WithPipelineOptions(ctx, urnOptions)
-	if PipelineOptions(ctx3) == nil {
-		t.Errorf("context should have pipeline options")
-	}
-	expsURN := GetExperiments(urnOptions)
-	if len(expsURN) != 2 || expsURN[0] != "expA" || expsURN[1] != "expB" {
-		t.Errorf("GetExperiments() = %v, want [expA expB]", expsURN)
-	}
-	if !HasExperiment(urnOptions, "expB") {
-		t.Errorf("HasExperiment(expB) = false, want true")
 	}
 }
