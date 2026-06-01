51256dd83541626a801fb9c5cf1af7407ca3533b
Derrick Williams <derrickaw@google.com>
2026-06-01 15:08:40 +0000

    exclude build and gradle files to help with timeouts

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1486,6 +1486,8 @@ class BeamModulePlugin implements Plugin<Project> {
           googleJavaFormat('1.17.0')
           target project.fileTree(project.projectDir) {
             include 'src/*/java/**/*.java'
+            exclude '**/build/**'
+            exclude '**/.gradle/**'
             exclude '**/DefaultPackageTest.java'
           }
           // For spotless:off and spotless:on
@@ -2417,6 +2419,7 @@ class BeamModulePlugin implements Plugin<Project> {
           target project.fileTree(project.projectDir) {
             include '**/*.groovy'
             exclude '**/build/**'
+            exclude '**/.gradle/**'
           }
         }
         groovyGradle { greclipse().configFile(grEclipseConfig) }
