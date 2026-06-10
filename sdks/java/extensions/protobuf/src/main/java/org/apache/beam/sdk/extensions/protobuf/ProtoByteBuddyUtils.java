6e2e31f93fbe9c264133a4817e8fb59bd19d2a53
Liam Miller-Cushon <cushon@google.com>
2026-06-10 23:58:20 +0200

    spotless

--- sdks/java/extensions/protobuf/src/main/java/org/apache/beam/sdk/extensions/protobuf/ProtoByteBuddyUtils.java
+++ sdks/java/extensions/protobuf/src/main/java/org/apache/beam/sdk/extensions/protobuf/ProtoByteBuddyUtils.java
@@ -598,7 +598,9 @@ class ProtoByteBuddyUtils {
       return builder
           .visit(new AsmVisitorWrapper.ForDeclaredMethods().writerFlags(ClassWriter.COMPUTE_FRAMES))
           .make()
-          .load(ReflectHelpers.findClassLoader(protoBuilderClass), getClassLoadingStrategy(protoBuilderClass))
+          .load(
+              ReflectHelpers.findClassLoader(protoBuilderClass),
+              getClassLoadingStrategy(protoBuilderClass))
           .getLoaded()
           .getDeclaredConstructor(List.class)
           .newInstance(setters);
@@ -1116,7 +1118,9 @@ class ProtoByteBuddyUtils {
                   new AsmVisitorWrapper.ForDeclaredMethods()
                       .writerFlags(ClassWriter.COMPUTE_FRAMES))
               .make()
-              .load(ReflectHelpers.findClassLoader(builderClass), getClassLoadingStrategy(builderClass))
+              .load(
+                  ReflectHelpers.findClassLoader(builderClass),
+                  getClassLoadingStrategy(builderClass))
               .getLoaded()
               .getDeclaredConstructor()
               .newInstance();
