5adcccdc399a8d23ba7b1b1beb6dfdbf158c60db
Shunping Huang <shunping@google.com>
2026-06-30 13:20:34 -0400

    Add a test to reproduce the problem.

--- sdks/python/apache_beam/transforms/userstate_test.py
+++ sdks/python/apache_beam/transforms/userstate_test.py
@@ -719,6 +719,69 @@ class StatefulDoFnOnDirectRunnerTest(unittest.TestCase):
       actual_values = (values | beam.ParDo(SetStatefulDoFn()))
       assert_that(actual_values, equal_to([1, 3, 6, 10, 10]))
 
+  # Mock random to always return 1.0 to force compaction on every add.
+  @mock.patch('apache_beam.runners.worker.bundle_processor.random.random', lambda: 1.0)
+  def test_stateful_set_state_compaction_race_portably(self):
+    from apache_beam.runners.worker.sdk_worker import GrpcStateHandler
+    from apache_beam.runners.worker.sdk_worker import _Future
+    import time
+    import threading
+
+    old_request = GrpcStateHandler._request
+
+    def delayed_request(self, request):
+      # Delay append and clear requests to simulate slow state updates.
+      if request.HasField('append') or request.HasField('clear'):
+        future = _Future()
+        instruction_id = getattr(self._context, 'process_instruction_id', None)
+        def run():
+          time.sleep(0.5)
+          self._context.process_instruction_id = instruction_id
+          underlying_future = old_request(self, request)
+          underlying_future.wait()
+          future.set(underlying_future.get())
+        t = threading.Thread(target=run, daemon=True)
+        t.start()
+        return future
+      else:
+        return old_request(self, request)
+
+    GrpcStateHandler._request = delayed_request
+
+
+    class SetStatefulDoFn(beam.DoFn):
+
+      SET_STATE = SetStateSpec('buffer', VarIntCoder())
+
+      def process(self, element, set_state=beam.DoFn.StateParam(SET_STATE)):
+        _, value = element
+        aggregated_value = 0
+        set_state.add(value)
+        for saved_value in set_state.read():
+          aggregated_value += saved_value
+        yield aggregated_value
+
+    try:
+      options = PipelineOptions([
+          '--max_cache_memory_usage_mb=100',
+          '--environment_type=LOOPBACK',
+      ])
+      with TestPipeline(runner='PrismRunner', options=options) as p:
+        test_stream = (
+            TestStream(coder=beam.coders.TupleCoder((beam.coders.StrUtf8Coder(), beam.coders.VarIntCoder())))
+            .advance_watermark_to(10)
+            .add_elements([('key', 1)])
+            .advance_watermark_to(20)
+            .add_elements([('key', 2)])
+            .advance_watermark_to(30)
+        )
+        actual_values = (p | test_stream | beam.ParDo(SetStatefulDoFn()))
+        assert_that(actual_values, equal_to([1, 3]))
+
+
+    finally:
+      GrpcStateHandler._request = old_request
+
   def test_stateful_set_state_clean_portably(self):
     class SetStateClearingStatefulDoFn(beam.DoFn):
 
