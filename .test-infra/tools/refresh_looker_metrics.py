0670c54e131fa9f3de9a1c45f4f72eb6640e59b4
Vitaly Terentyev <vitaly.terentyev@akvelon.com>
2026-07-09 22:17:28 +0400

    Set looker base url

--- .test-infra/tools/refresh_looker_metrics.py
+++ .test-infra/tools/refresh_looker_metrics.py
@@ -43,7 +43,7 @@ LOOKS_TO_DOWNLOAD = [
     ("82", ["263", "264", "265", "266", "267"]),  # PyTorch Sentiment Streaming DistilBERT base uncased
     ("85", ["268", "269", "270", "271", "272"]),  # PyTorch Sentiment Batch DistilBERT base uncased
     ("86", ["284", "285", "286", "287", "288"]),  # VLLM Batch Gemma
-    ("96", ["270", "304", "305", "353", "354"]),   # Table Row Inference Sklearn Batch
+    ("96", ["351", "304", "305", "353", "354"]),   # Table Row Inference Sklearn Batch
     ("106", ["355", "356", "357", "358", "359"]),   # Table Row Inference Sklearn Streaming
     ("107", ["360", "361", "362", "363", "364"]),  # MLTransform Generate Vocab Batch
     ("108", ["365", "366", "367", "368", "369"]),  # MLTransform One-Hot Encoding Batch
