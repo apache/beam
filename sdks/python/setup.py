3354de9368c09ad8adf2b3cb0c2e35a7c6a0ed12
Jack McCluskey <34928439+jrmccluskey@users.noreply.github.com>
2026-07-29 09:25:43 -0400

    Update sdks/python/setup.py
    Co-authored-by: Danny McCormick <dannymccormick@google.com>

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -153,7 +153,7 @@ else:
     # Generally try to cover versions released in the last two years.
     # Update python/sdks/tox.ini to cover the same pyarrow versions
     # when updating the bounds here.
-    'pyarrow>=17.0.0,<26.0.0',
+    'pyarrow>=14.0.1,<26.0.0',
   ]
 
 # Exclude pandas<=1.4.2 since it doesn't work with numpy 1.24.x.
