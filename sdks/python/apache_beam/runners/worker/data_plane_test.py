1220f56821531ddb92bb1be49407d9c1dcd07862
Sam Whittle <samuelw@google.com>
2026-08-31 15:47:42 +0200

    Initial support for named data streams for python sdk.

--- sdks/python/apache_beam/runners/worker/data_plane_test.py
+++ sdks/python/apache_beam/runners/worker/data_plane_test.py
@@ -29,6 +29,7 @@ import grpc
 from apache_beam.portability.api import beam_fn_api_pb2
 from apache_beam.portability.api import beam_fn_api_pb2_grpc
 from apache_beam.runners.worker import data_plane
+from apache_beam.runners.worker.worker_id_interceptor import DataStreamIdInterceptor
 from apache_beam.runners.worker.worker_id_interceptor import WorkerIdInterceptor
 from apache_beam.utils import thread_pool_executor
 
@@ -40,7 +41,10 @@ class DataChannelTest(unittest.TestCase):
   def test_time_based_flush_grpc_data_channel(self):
     self._grpc_data_channel_test(True)
 
-  def _grpc_data_channel_test(self, time_based_flush=False):
+  def test_named_grpc_data_channel(self):
+    self._grpc_data_channel_test(data_stream_id='stream_1')
+
+  def _grpc_data_channel_test(self, time_based_flush=False, data_stream_id=''):
     if time_based_flush:
       data_servicer = data_plane.BeamFnDataServicer(
           data_buffer_time_limit_ms=100)
@@ -48,7 +52,7 @@ class DataChannelTest(unittest.TestCase):
       data_servicer = data_plane.BeamFnDataServicer()
     worker_id = 'worker_0'
     data_channel_service = \
-      data_servicer.get_conn_by_worker_id(worker_id)
+      data_servicer.get_conn_by_worker_id(worker_id, data_stream_id=data_stream_id)
 
     server = grpc.server(thread_pool_executor.shared_unbounded_instance())
     beam_fn_api_pb2_grpc.add_BeamFnDataServicer_to_server(data_servicer, server)
@@ -56,9 +60,10 @@ class DataChannelTest(unittest.TestCase):
     server.start()
 
     grpc_channel = grpc.insecure_channel('localhost:%s' % test_port)
-    # Add workerId to the grpc channel
-    grpc_channel = grpc.intercept_channel(
-        grpc_channel, WorkerIdInterceptor(worker_id))
+    interceptors = [WorkerIdInterceptor(worker_id)]
+    if data_stream_id:
+      interceptors.append(DataStreamIdInterceptor(data_stream_id))
+    grpc_channel = grpc.intercept_channel(grpc_channel, *interceptors)
     data_channel_stub = beam_fn_api_pb2_grpc.BeamFnDataStub(grpc_channel)
     if time_based_flush:
       data_channel_client = data_plane.GrpcClientDataChannel(
@@ -74,6 +79,30 @@ class DataChannelTest(unittest.TestCase):
       data_channel_service.close()
       data_channel_client.wait()
       data_channel_service.wait()
+
+  def test_grpc_client_data_channel_factory_named_streams(self):
+    data_servicer = data_plane.BeamFnDataServicer()
+    server = grpc.server(thread_pool_executor.shared_unbounded_instance())
+    beam_fn_api_pb2_grpc.add_BeamFnDataServicer_to_server(data_servicer, server)
+    test_port = server.add_insecure_port('[::]:0')
+    server.start()
+
+    try:
+      factory = data_plane.GrpcClientDataChannelFactory(worker_id='worker_0')
+      url = 'localhost:%s' % test_port
+      ch_default = factory.create_data_channel_from_url(url)
+      ch_default2 = factory.create_data_channel_from_url(url, data_stream_id='')
+      ch_stream1 = factory.create_data_channel_from_url(url, data_stream_id='stream_1')
+      ch_stream1_dup = factory.create_data_channel_from_url(url, data_stream_id='stream_1')
+      ch_stream2 = factory.create_data_channel_from_url(url, data_stream_id='stream_2')
+
+      self.assertIs(ch_default, ch_default2)
+      self.assertIs(ch_stream1, ch_stream1_dup)
+      self.assertIsNot(ch_default, ch_stream1)
+      self.assertIsNot(ch_stream1, ch_stream2)
+      factory.close()
+    finally:
+      server.stop(0)
 
   def test_in_memory_data_channel(self):
     channel = data_plane.InMemoryDataChannel()
--- sdks/python/apache_beam/transforms/environments_test.py
+++ sdks/python/apache_beam/transforms/environments_test.py
@@ -72,6 +72,7 @@ class RunnerApiTest(unittest.TestCase):
         common_urns.sdf_components.TRUNCATE_SIZED_RESTRICTION.urn,
         sdk_capabilities)
     self.assertIn(common_urns.primitives.TO_STRING.urn, sdk_capabilities)
+    self.assertIn(common_urns.protocols.NAMED_DATA_STREAMS.urn, sdk_capabilities)
 
   def test_default_capabilities(self):
     environment = DockerEnvironment.from_options(
