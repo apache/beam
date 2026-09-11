bcc277ee6306758f547fdb92a87399e0e3d316e2
Andrew Crites <crites@google.com>
2026-09-11 10:22:02 -0700

    Ensure 0 backlog is sent when finishing processing restrictions.

--- runners/core-java/src/main/java/org/apache/beam/runners/core/SplittableParDoViaKeyedWorkItems.java
+++ runners/core-java/src/main/java/org/apache/beam/runners/core/SplittableParDoViaKeyedWorkItems.java
@@ -606,6 +606,9 @@ public class SplittableParDoViaKeyedWorkItems {
         restrictionState.clear();
         watermarkEstimatorState.clear();
         holdState.clear();
+        if (backlogBytesCallback != null) {
+          backlogBytesCallback.accept(0.0);
+        }
         return;
       }
 
